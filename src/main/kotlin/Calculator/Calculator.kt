b9fe8f63feec3478e7a5a45192a79cfb6aa487eb
EthanChae <boschae36@gmail.com>
2020-07-31 04:50:44 +0900

    feat(calculator) : Parsing input

--- src/main/kotlin/Calculator/Calculator.kt
+++ src/main/kotlin/Calculator/Calculator.kt
@@ -1,12 +1,15 @@
 package Calculator
 
 import java.lang.IndexOutOfBoundsException
+import java.lang.StringBuilder
 
 const val FIRST_PREFIX = "//"
 const val SECOND_PREFIX = "\n"
 const val CUSTOM_SPLITTER_LOCATION = 2
 
 object Calculator {
+
+    private val defaultSplitters = listOf(",", ":")
 
     fun calculate(numbers: List<Int>): Int {
         return numbers.sum()
@@ -26,11 +29,31 @@ object Calculator {
         }
     }
 
-    fun getCustomSplitter(numbersInput: String): Char {
-        try {
-            return numbersInput[CUSTOM_SPLITTER_LOCATION]
-        } catch (e: IndexOutOfBoundsException) {
-            throw IndexOutOfBoundsException("입력값을 확인하세요.")
+    @Throws(IndexOutOfBoundsException::class)
+    fun getSplitters(numbersInput: String, hasCustomSplitter: Boolean): List<String> {
+        if (hasCustomSplitter) {
+            return listOf(numbersInput[CUSTOM_SPLITTER_LOCATION].toString())
         }
+        return defaultSplitters
+    }
+
+    fun hasOnlyValidString(numbersInput: String, splitters: List<String>): Boolean {
+        val pattern = StringBuilder("[0-9")
+        splitters.forEach { pattern.append("|" + it) }
+        pattern.append("]*")
+
+        return numbersInput.replace(FIRST_PREFIX, SECOND_PREFIX).matches(Regex(pattern.toString()))
+    }
+
+    fun checkEndString(numbersInput: String): Boolean {
+        return numbersInput.last().toString().matches(Regex("[0-9]"))
+    }
+
+    fun parsing(numbersInput: String): List<Int> {
+        if (defaultSplitters.size == 2) {
+            return numbersInput.replace(defaultSplitters[1], defaultSplitters[0]).split(defaultSplitters[0])
+                .map { it.toInt() }
+        }
+        return numbersInput.split(defaultSplitters[0]).map { it.toInt() }
     }
 }
--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -22,7 +22,30 @@ class Test {
     }
 
     @Test
-    fun `커스텀 구분자 얻기`() {
-        assertThat(Calculator.getCustomSplitter("//x\n")).isEqualTo('x')
+    fun `구분자 얻기`() {
+        assertThat(Calculator.getSplitters("//x\n", Calculator.hasCustomSplitter("//x\n")))
+            .contains("x")
+        assertThat(Calculator.getSplitters("1,2,3", Calculator.hasCustomSplitter("1,2,3")))
+            .contains(",", ":")
+    }
+
+    @Test
+    fun `숫자, 구분자 이외의 값이 입력되었는지 확인`() {
+        assertThat(
+            Calculator.hasOnlyValidString(
+                "1,2,3",
+                Calculator.getSplitters("1,2,3", Calculator.hasCustomSplitter("1,2,3"))
+            )
+        ).isEqualTo(true)
+    }
+
+    @Test
+    fun `문자열 끝이 숫자인`() {
+        assertThat(Calculator.checkEndString("1,2,3,")).isEqualTo(false)
+    }
+
+    @Test
+    fun `파싱`() {
+        assertThat(Calculator.parsing("1,2:3")).isEqualTo(listOf(1, 2, 3))
     }
 }
