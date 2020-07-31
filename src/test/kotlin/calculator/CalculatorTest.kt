ff6e79d369fd5d0712226ca39de7ff82e6df8827
BeokBeok <kekemusa37@nate.com>
2020-07-31 22:06:03 +0900

    feat(test) : setup function

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -1,8 +1,17 @@
 package calculator
 
+import Calculator
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class CalculatorTest {
+
+    private lateinit var calculator: Calculator
+
+    @BeforeEach
+    fun setup() {
+        calculator = Calculator()
+    }
 
     @Test
     fun `빈 문자열 입력`() {
