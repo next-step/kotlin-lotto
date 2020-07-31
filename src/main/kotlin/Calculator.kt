cb9423d3dd0dc1d649d4bd899d7b12dee0b9bd0a
BeokBeok <kekemusa37@nate.com>
2020-07-31 22:51:25 +0900

    feat(refact) : 커스텀 구분자가 사용된 문자열 적용

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,9 +1,16 @@
 class Calculator {
 
+    private val customTokenizerRegex = Regex("//(.)\n(.*)")
+
     fun calculate(expr: String): Int {
         if (expr.isEmpty()) return 0
 
-        val numberGroup = expr.split(",", ":")
+        val numberGroup = mutableListOf<String>()
+        customTokenizerRegex.find(expr)?.let {
+            val customTokenizer = it.groupValues[TOKENIZER_INDEX]
+            numberGroup.addAll(it.groupValues[TOKENIZER_APPLIED_EXPR_INDEX].split(customTokenizer))
+        } ?: numberGroup.addAll(expr.split(",", ":"))
+
         if (numberGroup.size == 1) return numberGroup[0].toInt()
 
         return numberGroup
@@ -11,5 +18,10 @@ class Calculator {
             .reduce { acc, number ->
                 acc + number
             }
+    }
+
+    companion object {
+        private const val TOKENIZER_INDEX = 1
+        private const val TOKENIZER_APPLIED_EXPR_INDEX = 2
     }
 }
