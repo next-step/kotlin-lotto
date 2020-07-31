d98399d8d538263c2831d218d9fe284558468005
EthanChae <boschae36@gmail.com>
2020-07-31 22:00:45 +0900

    Step1 - 문자열 계산기 구현 (#1)
    * Docs(calculator) : Write requirements and step of development
    
    * feat(calculator) : Create calculate method
    
    * feat(calculator) : Check an input value for null or empty
    
    * feat(calculator) : Check whether a custom splitter existed or not
    
    * feat(calculator) : Get a custom splitter
    
    * feat(calculator) : Parsing input
    
    * refactor(calculator) : Send message to instance

---
+++ src/main/kotlin/Calculator/CalculatorException.kt
@@ -0,0 +1,3 @@
+package Calculator
+
+class CalculatorException(message: String) : Exception()
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+import Calculator.Calculator
+
+fun main() {
+    try {
+        println("계산하기 위한 문자열을 입력해주세요.")
+        val calculator = Calculator(readLine()!!)
+        val result = calculator.execute()
+        println("총합은 $result 입니다.")
+    } catch (e: Exception) {
+        println(e.message)
+    }
+}
