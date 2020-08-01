0d1c4cf12f11581c4841817dc57b85b3645eb550
Jonny-Cho <andante2183@gmail.com>
2020-08-01 15:08:47 +0900

    test(Calculator): add netative number test code

--- src/main/kotlin/stringAddCalculator/StringAddCalculator.kt
+++ src/main/kotlin/stringAddCalculator/StringAddCalculator.kt
@@ -1,10 +1,16 @@
 package stringAddCalculator
 
+import java.lang.RuntimeException
+
 class StringAddCalculator {
     fun add(text: String): Int {
         if (text.isBlank()) return 0
         if (text.length == 1) return text.toInt()
-        val texts = text.split(",", ":").map { it.toInt() }
+        val texts = text.split(",", ":").map {
+            val number = it.toInt()
+            if (number < 0) throw RuntimeException("sdf")
+            number
+        }
         return texts.reduce { total, number -> total + number }
     }
 }
--- src/test/kotlin/stringAddCalculator/StringAddCalculatorTest.kt
+++ src/test/kotlin/stringAddCalculator/StringAddCalculatorTest.kt
@@ -1,6 +1,8 @@
 package stringAddCalculator
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EmptySource
 import org.junit.jupiter.params.provider.ValueSource
@@ -32,17 +34,17 @@ class StringAddCalculatorTest {
         assertThat(calculator.add(text)).isEqualTo(6)
     }
 
+    @Test
+    fun `음수를 전달할 경우 RuntimeException 예외가 발생해야 한다`() {
+        assertThatThrownBy { calculator.add("1,-2:3") }
+            .isInstanceOf(RuntimeException::class.java)
+    }
+
     @ParameterizedTest
     @ValueSource(strings = ["//;\\\\n1;2;3"])
     fun `커스텀 구분자를 지정할 수 있다`(text: String) {
         println(text)
         // "//"와 "\n" 문자 사이에 커스텀 구분자를 지정할 수 있다. (예 : “//;\n1;2;3” => 6)
-        TODO("Not yet implemented")
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["-1,2,3"])
-    fun `음수를 전달할 경우 RuntimeException 예외가 발생해야 한다`(text: String) {
         TODO("Not yet implemented")
     }
 }
