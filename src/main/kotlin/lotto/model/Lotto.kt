b69b2f4d328c98d4671467838adaf18bf6146d2d
BeokBeok <kekemusa37@nate.com>
2020-08-01 22:14:25 +0900

    feat(lotto) : [추가] 로또 생성 함수

---
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -0,0 +1,8 @@
+package lotto.model
+
+class Lotto {
+
+    fun generate(): List<Int> {
+        return listOf(1, 2, 3, 4, 5, 6)
+    }
+}
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,9 +1,13 @@
 package lotto
 
+import lotto.model.Lotto
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class LottoTest {
     @Test
     fun `로또 형식 체크`() {
+        val lotto = Lotto()
+        assertThat(lotto.generate().size).isEqualTo(6)
     }
 }
