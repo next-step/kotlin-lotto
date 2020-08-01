0a652e2d0ada215749c7564ac5bf2aed570875e7
Jiwhun Kim <jiwhun.kim@outlook.com>
2020-08-01 16:28:56 +0900

    feature(StringAddCalculator): 기능을 구현하다

--- src/main/kotlin/StringAddCalculator.kt
+++ src/main/kotlin/StringAddCalculator.kt
@@ -1,1 +1,27 @@
-class StringAddCalculator
+class StringAddCalculator {
+    fun add(text: String?): Int {
+        if (text.isNullOrEmpty()) {
+            return 0
+        } else if (text.contains(BASIC_DELIMITER_COMMA) || text.contains(BASIC_DELIMITER_COLON)) {
+            val list = text.split(BASIC_DELIMITER_COMMA, BASIC_DELIMITER_COLON)
+            return list.sumBy { it.toInt() }
+        } else if (CUSTOM_DELIMITTER_REGEX.matches(text)) {
+            val list = text.split(CUSTOM_DELIMITTER)
+            val delimiter = list.first().replace("//", "")
+            val contents = list.last().split(delimiter)
+            return contents.sumBy { it.toInt() }
+        } else if (text.toInt() < 0) {
+            throw RuntimeException("number is must positive")
+        } else {
+            return text.toInt()
+        }
+        return 1
+    }
+
+    companion object {
+        const val BASIC_DELIMITER_COMMA = ","
+        const val BASIC_DELIMITER_COLON = ":"
+        const val CUSTOM_DELIMITTER = "\n"
+        val CUSTOM_DELIMITTER_REGEX = Regex(pattern = "//.\n.*")
+    }
+}
