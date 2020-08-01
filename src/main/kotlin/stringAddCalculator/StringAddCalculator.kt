e5fd84eec1e4101dd159cb473a9b54581f26c397
jonnycho <andante2183@gmail.com>
2020-08-01 23:00:20 +0900

    Step1 - 문자열 덧셈기 TDD (#7)
    * feat(StringAddCalculatorTest): add fail test code
    
    * doc: add readme
    
    * test(calculator): add empty string test code
    
    * text(Calculator): add one string test code
    
    * text(Calculator): add split test code
    
    * test(Calculator): add netative number test code
    
    * test(Calculator): change throw message
    
    * test(Calculator): add custom delimiter test code
    
    * refactor(Calculator): change class to object
    
    * refactor(Calculator): add private methods
    
    * refactor(Calculator): split getNumbers method
    
    * doc: update readme
    
    * refactor(Calculator): change throw message to const value

---
+++ src/main/kotlin/stringAddCalculator/StringAddCalculator.kt
@@ -0,0 +1,44 @@
+package stringAddCalculator
+
+object StringAddCalculator {
+    private const val CUSTOM_DELIMITER =
+        """//(.)\\n(.*)"""
+    private const val ZERO = 0
+    private const val NEGATIVE_NUMBER_THROW_MESSAGE = "can not use nagative number"
+    private val delimiters = mutableListOf(",", ":")
+
+    fun add(text: String): Int {
+        if (text.isBlank()) return 0
+        if (text.length == 1) return text.toInt()
+        var targetText = getTargetText(text)
+        val numbers = getNumbers(targetText, delimiters)
+        return getTotal(numbers)
+    }
+
+    private fun getTargetText(text: String): String {
+        return Regex(CUSTOM_DELIMITER).find(text)?.let {
+            delimiters.add(it.groupValues[1])
+            it.groupValues[2]
+        } ?: text
+    }
+
+    private fun getNumbers(
+        targetText: String,
+        delimiters: MutableList<String>
+    ): List<Int> {
+        val strList = targetText.split(*delimiters.toTypedArray())
+        return toIntList(strList)
+    }
+
+    private fun toIntList(values: List<String>): List<Int> {
+        return values.map { value ->
+            val number = value.toInt()
+            if (isNegativeNumber(number)) throw RuntimeException("$NEGATIVE_NUMBER_THROW_MESSAGE $number")
+            number
+        }
+    }
+
+    private fun isNegativeNumber(number: Int) = number < ZERO
+
+    private fun getTotal(texts: List<Int>) = texts.reduce { total, number -> total + number }
+}
---
+++ src/test/kotlin/stringAddCalculator/StringAddCalculatorTest.kt
@@ -0,0 +1,50 @@
+package stringAddCalculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EmptySource
+import org.junit.jupiter.params.provider.ValueSource
+
+class StringAddCalculatorTest {
+    private val calculator = StringAddCalculator
+
+    @ParameterizedTest
+    @EmptySource
+    fun `빈 문자열을 입력할 경우 0을 반환해야 한다`(text: String) {
+        assertThat(calculator.add(text)).isZero()
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["5"])
+    fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(text: String) {
+        assertThat(calculator.add(text)).isEqualTo(text.toInt())
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["3,5"])
+    fun `쉼표로 구분된 문자열을 더한 값을 리턴한다`(text: String) {
+        assertThat(calculator.add(text)).isEqualTo(8)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2:3"])
+    fun `구분자를 컴마 이외에 콜론을 사용할 수 있다`(text: String) {
+        assertThat(calculator.add(text)).isEqualTo(6)
+    }
+
+    @Test
+    fun `음수를 전달할 경우 RuntimeException 예외가 발생해야 한다`() {
+        assertThatThrownBy { calculator.add("1,-2:3") }
+            .isInstanceOf(RuntimeException::class.java)
+            .hasMessage("can not use nagative number -2")
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["//;\\n1;2,3"])
+    fun `커스텀 구분자를 지정할 수 있다`(text: String) {
+        // "//"와 "\n" 문자 사이에 커스텀 구분자를 지정할 수 있다. (예 : “//;\n1;2;3” => 6)
+        assertThat(calculator.add(text)).isEqualTo(6)
+    }
+}
