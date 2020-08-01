18c3baa11f356742993941da02d66214e8db917e
BeokBeok <kekemusa37@nate.com>
2020-08-01 23:08:53 +0900

    feat(lotto) : [추가] 일치된 숫자 개수에 따른 Prize 전달

--- src/main/kotlin/lotto/model/Prize.kt
+++ src/main/kotlin/lotto/model/Prize.kt
@@ -4,5 +4,11 @@ enum class Prize(val matchCount: Int, val price: Int) {
     NO_ONE(6, 2_000_000_000),
     NO_TWO(5, 1_500_000),
     NO_THREE(4, 50_000),
-    NO_FOUR(3, 5_000);
+    NO_FOUR(3, 5_000),
+    NOTHING(0, 0);
+
+    companion object {
+        fun findByMatchCount(matchCount: Int): Prize =
+            values().firstOrNull { it.matchCount == matchCount } ?: NOTHING
+    }
 }
