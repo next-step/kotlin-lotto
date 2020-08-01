c5dc3a8ef763201d87dc40936d79aa304009e31f
Jonny-Cho <andante2183@gmail.com>
2020-08-01 17:32:02 +0900

    refactor(Calculator): split getNumbers method

--- src/main/kotlin/stringAddCalculator/StringAddCalculator.kt
+++ src/main/kotlin/stringAddCalculator/StringAddCalculator.kt
@@ -9,25 +9,31 @@ object StringAddCalculator {
     fun add(text: String): Int {
         if (text.isBlank()) return 0
         if (text.length == 1) return text.toInt()
+        var targetText = getTargetText(text)
+        val numbers = getNumbers(targetText, delimiters)
+        return getTotal(numbers)
+    }
 
-        var targetText = Regex(CUSTOM_DELIMITER).find(text)?.let {
+    private fun getTargetText(text: String): String {
+        return Regex(CUSTOM_DELIMITER).find(text)?.let {
             delimiters.add(it.groupValues[1])
             it.groupValues[2]
         } ?: text
-
-        val numbers = getNumbers(targetText, delimiters)
-
-        return getTotal(numbers)
     }
 
     private fun getNumbers(
         targetText: String,
         delimiters: MutableList<String>
     ): List<Int> {
-        return targetText.split(*delimiters.toTypedArray()).map { str ->
-            str.toInt().also { number ->
-                if (isNegativeNumber(number)) throw RuntimeException("can not use nagative number $number")
-            }
+        val strList = targetText.split(*delimiters.toTypedArray())
+        return toIntList(strList)
+    }
+
+    private fun toIntList(values: List<String>): List<Int> {
+        return values.map { value ->
+            val number = value.toInt()
+            if (isNegativeNumber(number)) throw RuntimeException("can not use nagative number $number")
+            number
         }
     }
 
