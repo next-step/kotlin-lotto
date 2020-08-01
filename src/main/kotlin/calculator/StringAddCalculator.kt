340fa8fc2a44d306d3b683e22329e2c2250ff03d
bky373 <bo373@naver.com>
2020-08-01 12:04:42 +0900

    feat(calculator): do not allow negative

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -5,9 +5,18 @@ class StringAddCalculator {
     fun add(text: String?): Int {
         if (isBlank(text)) return 0
 
+        checkNegative(text!!)
+
         return -1
     }
 
     private fun isBlank(text: String?): Boolean = text.isNullOrBlank()
 
+    private fun checkNegative(text: String) {
+        if (text.contains('-')) throw RuntimeException(NOT_ALLOW_NEGATIVE)
+    }
+
+    companion object {
+        private const val NOT_ALLOW_NEGATIVE = "음수 입력 불가"
+    }
 }
--- src/test/kotlin/calculator/StringAddCalculatorTest.kt
+++ src/test/kotlin/calculator/StringAddCalculatorTest.kt
@@ -1,8 +1,10 @@
 package calculator
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.NullAndEmptySource
 
@@ -19,5 +21,12 @@ class StringAddCalculatorTest {
     @NullAndEmptySource
     fun `empty or null`(text: String?) {
         assertThat(calculator.add(text)).isZero()
+    }
+
+    @DisplayName("입력값 중 음수가 있을 경우 RuntimeException을 발생시킨다.")
+    @Test
+    fun `negative`() {
+        assertThatExceptionOfType(RuntimeException::class.java)
+            .isThrownBy { calculator.add("-1") }.withMessage("음수 입력 불가")
     }
 }
