32d6d47a4ac3bfb873ae8d3a30ade18768025e42
EthanChae <boschae36@gmail.com>
2020-08-01 23:12:00 +0900

    feat(lotto) : Check a prize

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -5,7 +5,14 @@ private const val MAX_NUMBER = 45
 private const val MIN_NUMBER = 1
 
 object Lotto {
+
     fun getNumbers(): List<Int> {
         return (MIN_NUMBER..MAX_NUMBER).shuffled().subList(0, COUNT_OF_NUMBERS).sorted()
+    }
+
+    fun checkResult(lottoNumbers: List<Int>, prizeNumbers: List<Int>): Int {
+        var count: Int = 0
+        lottoNumbers.forEach { if (prizeNumbers.contains(it)) count++ }
+        return count
     }
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -16,4 +16,11 @@ class LottoTest {
         val numbers: List<Int> = Lotto.getNumbers()
         assertThat(numbers.toSet().size).isEqualTo(numbers.size)
     }
+
+    @Test
+    fun `로또 당첨여부 확인`() {
+        val prizeNumbers = listOf(1, 7, 13, 24, 40, 41)
+        val lottoNumbers = listOf(1, 7, 13, 14, 15, 16)
+        assertThat(Lotto.checkResult(lottoNumbers, prizeNumbers)).isEqualTo(3)
+    }
 }
