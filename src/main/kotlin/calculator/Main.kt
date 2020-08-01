2ff7a25908bdd692d227fbbf3cde14062ae7a030
bky373 <bo373@naver.com>
2020-08-02 03:11:16 +0900

    refactor(calculator): resolve non-null assertions(git add src/main/kotlin/calculator/Main.kt ) issues in StringAddCalculator.kt

--- src/main/kotlin/calculator/Main.kt
+++ src/main/kotlin/calculator/Main.kt
@@ -1,20 +1,26 @@
 package calculator
 
-const val ENTER_TEXT_TO_CALCULATE = "\"계산할 내용을 입력해주세요. (예) 1,2,3 또는 1:2:3  (* 커스텀 구분자 관련 : c 입력)\""
+const val ENTER_TEXT_TO_CALCULATE = "계산할 내용을 입력해주세요. (예) 1,2,3 또는 1:2:3  (* 커스텀 구분자 관련 : c 입력)"
 const val CUSTOM_DELIMITER_INFO = "커스텀 구분자 : // 와 \\n 사이에 입력 => (예) //;\\n1;2;3 "
 const val NEED_INFO_KEYWORD = "c"
+const val ALERT_EMPTY_INPUT = "입력값이 비어있습니다. "
 
 fun main() {
+    var result = 0
 
-    ENTER_TEXT_TO_CALCULATE.also(::println)
+    while (result == 0) {
+        ENTER_TEXT_TO_CALCULATE.also(::println)
+        var text = readLine()
 
-    var text = readLine()
+        if (text.equals(NEED_INFO_KEYWORD)) {
+            CUSTOM_DELIMITER_INFO.also(::println)
+            text = readLine()
+        }
 
-    if (text.equals(NEED_INFO_KEYWORD)) {
-        CUSTOM_DELIMITER_INFO.also(::println)
-        text = readLine()
+        val calculator = StringAddCalculator()
+
+        result = calculator.add(text)
+        if (result == 0) ALERT_EMPTY_INPUT.also(::print)
     }
-
-    val calculator = StringAddCalculator()
-    calculator.add(text).also(::println)
+    result.also(::println)
 }
--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -5,11 +5,13 @@ class StringAddCalculator {
     fun add(text: String?): Int {
         if (text.isNullOrBlank()) return 0
 
-        val text = text!!
-        if (containsOnlyDigit(text)) return text.toInt()
+        text?.let {
+            if (containsOnlyDigit(text)) return text.toInt()
 
-        val numbers = InputParser(text).numbersParsed
-        return numbers.sum()
+            val numbers = InputParser(text).numbersParsed
+            return numbers.sum()
+        }
+        return 0
     }
 
     private fun containsOnlyDigit(text: String): Boolean = text.all { it.isDigit() }
