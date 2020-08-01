68e21bee8a70aa20466946b940abdcc1050d8e58
bky373 <bo373@naver.com>
2020-08-01 13:57:19 +0900

    refactor(calcular): improve performance of checkIllegalLetter

--- src/main/kotlin/calculator/InputUtils.kt
+++ src/main/kotlin/calculator/InputUtils.kt
@@ -3,15 +3,17 @@
 package calculator
 
 const val NOT_ALLOW_LETTER = "정해진 자리 외 문자 입력 불가"
-const val CUSTOM_PATTERN = "//(.)\n(.*)"
+const val CUSTOM_PATTERN = "//(.)\\\\n(.*)"
 
 fun isBlank(text: String?): Boolean = text.isNullOrBlank()
 
 fun checkIllegalLetter(tokens: List<String>) {
-    tokens.forEach { if (it[0].isLetter()) throw RuntimeException(NOT_ALLOW_LETTER) }
+    tokens.forEach {
+        if(Regex("[^0-9]+").find(it) != null) throw RuntimeException(NOT_ALLOW_LETTER)
+    }
 }
 
 fun hasCustomDelimiter(text: String): Boolean {
-    Regex(CUSTOM_PATTERN).find(text) ?: return false
+    Regex("//(.)\\\\n").find(text) ?: return false
     return true
 }
