1b15d5221d0561a0b71d6f1b1297e59a2737f8ad
BeokBeok <kekemusa37@nate.com>
2020-08-02 03:09:28 +0900

    feat(test) : [추가] StoreTest

---
+++ src/test/kotlin/lotto/StoreTest.kt
@@ -0,0 +1,19 @@
+package lotto
+
+import lotto.model.Buyer
+import lotto.model.Prize
+import lotto.model.Store
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class StoreTest {
+    @Test
+    fun `5천원 당첨`() {
+        val buyer = Buyer().apply { markLotto(listOf(1, 2, 3, 7, 8, 9)) }
+        val store = Store(buyer)
+
+        val winnerHistory = store.confirmLottoWining(listOf(1, 2, 3, 4, 5, 6))
+
+        Assertions.assertThat(winnerHistory.contains(Prize.FORTH)).isTrue()
+    }
+}
