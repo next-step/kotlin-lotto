f0872e5cef32e2f0ea7e9f23ec2b84f485b7c163
Jonny-Cho <andante2183@gmail.com>
2020-08-02 22:43:02 +0900

    test(LottoItemsTest): add LottoItemsTest

--- src/main/kotlin/lotto/view/ResultView.kt
+++
@@ -1,3 +0,0 @@
-package lotto.input
-
-object ResultView
--- src/test/kotlin/lotto/domain/LottoItemsTest.kt
+++ src/test/kotlin/lotto/domain/LottoItemsTest.kt
@@ -1,11 +1,21 @@
 package lotto.domain
 
+import lotto.domain.value.LottoNumber
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class LottoItemsTest {
+    private val lottoItems = LottoItems(5)
+
     @Test
     fun getLottoItems() {
-        // val actual = LottoItems(3).getLottoItems()
-        // assertThat(actual).isEqualTo()
+        assertThat(lottoItems.getLottoItems().size).isEqualTo(5)
+    }
+
+    @Test
+    fun getWinLottos() {
+        val winningNumbers = List(6) { i -> LottoNumber(i + 1) }
+        val winnintCount = lottoItems.getWinLottos(winningNumbers)
+        assertThat(winnintCount).isEqualTo(3)
     }
 }
