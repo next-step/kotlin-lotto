4d9a4d684c6144b44e41e1e191040ad0d21cd6be
bky373 <bo373@naver.com>
2020-08-02 10:55:46 +0900

    refactor(calculator): improve performance in order to use defalut and custom delimiter together in InputParser.kt

--- src/main/kotlin/calculator/InputParser.kt
+++ src/main/kotlin/calculator/InputParser.kt
@@ -1,47 +1,42 @@
 package calculator
 
-import java.util.regex.Pattern
-
 class InputParser(private val text: String) {
     var numbersParsed: List<Int>
         private set
 
     init {
-        numbersParsed =
-            if (hasCustomDelimiter(text)) {
-                customNumbers()
-            } else {
-                defaultNumbers()
-            }
+        numbersParsed = numbersFrom(text)
     }
 
-    private fun hasCustomDelimiter(text: String): Boolean {
-        return customPatten.matcher(text).find()
+    private fun numbersFrom(text: String): List<Int> {
+        val result = CUSTOM_PATTERN.toRegex().find(text)
+        var token = text
+        var customDelimiter = DELIMITER_COMMA
+
+        result?.let {
+            customDelimiter = customDelimiter(result)
+            token = result.groupValues[2]
+        }
+
+        val numbers = splitFrom(token, customDelimiter)
+        return numbers
     }
 
-    private fun customNumbers(): List<Int> {
-        val result = CUSTOM_PATTERN.toRegex().find(text) ?: return emptyList()
-
-        val customDelimiter = result.groupValues[1]
-        val tokens = result.groupValues[2].split(customDelimiter)
+    private fun splitFrom(text: String, custom: String): List<Int> {
+        val tokens = text.split(DELIMITER_COMMA, DELIMITER_COLON, custom)
         checkIllegalInput(tokens)
 
         return tokens.map { it.toInt() }
     }
 
-    private fun defaultNumbers(): List<Int> {
-        val tokens = text.split(DELIMITER_COMMA, DELIMITER_COLON)
-        // tokens.forEach { print(it+" ${tokens} ")}
-        checkIllegalInput(tokens)
-
-        return tokens.map { it.toInt() }
-    }
+    private fun customDelimiter(result: MatchResult): String = result.groupValues[1]
 
     private fun checkIllegalInput(tokens: List<String>) {
         if (tokens.any {
                 it.toIntOrNull() == null ||
-                it.toInt() < 0
-            }) {
+                    it.toInt() < 0
+            }
+        ) {
             throw RuntimeException(NOT_ALLOW_LETTER_OR_NEGATIVE)
         }
     }
@@ -51,7 +46,5 @@ class InputParser(private val text: String) {
         private const val DELIMITER_COLON = ":"
         private const val CUSTOM_PATTERN = "//(.)\\\\n(.*)"
         private const val NOT_ALLOW_LETTER_OR_NEGATIVE = "숫자 이외의 값 또는 음수 입력 불가"
-
-        private val customPatten: Pattern = Pattern.compile(CUSTOM_PATTERN)
     }
 }
