03a2ffc907b8b4b3a3a4d11db2aedd7b40560fc7
bky373 <bo373@naver.com>
2020-08-02 11:15:53 +0900

    refactor(calculator): remove containsOnlyDigit function which would be duplication logic in StringAddCalculator.kt

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -6,13 +6,9 @@ class StringAddCalculator {
         if (text.isNullOrBlank()) return 0
 
         text?.let {
-            if (containsOnlyDigit(text)) return text.toInt()
-
             val numbers = InputParser(text).numbersParsed
             return numbers.sum()
         }
         return 0
     }
-
-    private fun containsOnlyDigit(text: String): Boolean = text.all { it.isDigit() }
 }
