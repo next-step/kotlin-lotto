d6378f3370b4085cf5858de1a04dde1eeb8fd9ef
BeokBeok <kekemusa37@nate.com>
2020-08-02 10:28:55 +0900

    feat(lotto) : [추가] 수익률 조회 함수

--- src/main/kotlin/lotto/model/Store.kt
+++ src/main/kotlin/lotto/model/Store.kt
@@ -1,5 +1,7 @@
 package lotto.model
 
+import kotlin.math.floor
+
 class Store(private val buyer: Buyer) {
 
     fun confirmLottoWining(lottoNumbers: List<Int>): List<Prize> {
@@ -9,5 +11,16 @@ class Store(private val buyer: Buyer) {
             results.add(Prize.findByMatchCount(matchCount))
         }
         return results
+    }
+
+    fun getRateOfReturn(price: Int, prizes: List<Prize>): Double {
+        val totalPrize = prizes
+            .map { it.price }
+            .reduce { acc, money ->
+                acc + money
+            }
+            .toDouble()
+
+        return floor(totalPrize / price * 100.0) / 100.0
     }
 }
