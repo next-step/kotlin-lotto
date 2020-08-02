7fd2f5353a58e21dba48b78582bbec43d4841c01
Jonny-Cho <andante2183@gmail.com>
2020-08-02 22:47:25 +0900

    refactor(Lotto): change toString

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,9 +1,11 @@
 package lotto.domain
 
+import lotto.domain.value.LottoNumber
+
 class Lotto {
     private val numbers = List(45) { i -> LottoNumber(i + 1) }.shuffled().subList(0, 6).sortedBy { it.getNumber() }
 
-    override fun toString() = "$numbers"
+    override fun toString() = "$numbers\n"
 
     fun getWinCount(winningNumbers: List<LottoNumber>): Int {
         var count = 0
