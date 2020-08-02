146b135f525723c693084876743b1f328a2d6d92
Malibin <nightmare73@naver.com>
2020-08-02 22:46:40 +0900

    feat(step2): 로또 번호 클래스 작성

---
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -0,0 +1,17 @@
+package lotto
+
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class LottoNumberTest {
+
+    @ValueSource(ints = [0, 46])
+    @ParameterizedTest
+    fun `로또 번호 범위(1~45)가 아닌 숫자가 들어오면 Exception`(inValidNumber: Int) {
+        // then
+        assertThatThrownBy { LottoNumber(inValidNumber) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("$inValidNumber 는 로또 번호(1~45)가 아닙니다.")
+    }
+}
