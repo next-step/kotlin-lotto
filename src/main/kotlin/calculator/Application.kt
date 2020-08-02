d3cbfb1b7e225cf508f9455eefc1aae5ae791adf
kimgaeul <kimgaeul4@gmail.com>
2020-08-02 17:59:58 +0900

    feat(step1):  package명 변경

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,10 +1,10 @@
 package calculator
 
-import calculator.infrastructure.Calculator
-import calculator.infrastructure.Number
-import calculator.infrastructure.findCustomDelimiter
-import calculator.infrastructure.parse
-import calculator.infrastructure.validate
+import calculator.domain.Calculator
+import calculator.domain.Number
+import calculator.domain.findCustomDelimiter
+import calculator.domain.parse
+import calculator.domain.validate
 import calculator.presentation.InputView
 import calculator.presentation.ResultView
 
--- src/test/kotlin/calculator/NumberTest.kt
+++ src/test/kotlin/calculator/NumberTest.kt
@@ -1,10 +1,10 @@
 package calculator
 
-import calculator.infrastructure.Calculator
-import calculator.infrastructure.Number
-import calculator.infrastructure.findCustomDelimiter
-import calculator.infrastructure.parse
-import calculator.infrastructure.validate
+import calculator.domain.Calculator
+import calculator.domain.Number
+import calculator.domain.findCustomDelimiter
+import calculator.domain.parse
+import calculator.domain.validate
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.params.ParameterizedTest
