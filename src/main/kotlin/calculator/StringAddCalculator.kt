10ded998a193ce33f5fd4c9791b2887f74c5229a
bky373 <bo373@naver.com>
2020-08-02 18:15:48 +0900

    refactor(calculator): remove unnecessary null-safe block in StringAddCalculator.kt

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -5,10 +5,7 @@ object StringAddCalculator {
     fun add(text: String?): Int {
         if (text.isNullOrBlank()) return 0
 
-        text?.let {
-            val numbers = InputParser.parse(text)
-            return numbers.sum()
-        }
-        return 0
+        val numbers = InputParser.parse(text)
+        return numbers.sum()
     }
 }
