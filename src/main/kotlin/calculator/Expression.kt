646deeb2c21dba9fc4539ae26a5cfe46ef6c76a1
zion830 <zion830@honglab.org>
2020-08-03 04:38:53 +0900

    refactor(calculator): 반복 사용되는 parse()를 1회만 호출하도록 프로퍼티로 분리

--- src/main/kotlin/calculator/Expression.kt
+++ src/main/kotlin/calculator/Expression.kt
@@ -1,23 +1,20 @@
 package calculator
 
 data class Expression(private val expression: String) {
+    private val parsedExpression = parse()
 
-    fun extractTokens(): List<String> {
-        val parsedExpression = parse()
-
-        return if (hasCustomDelimiter()) {
-            parsedExpression[2].split(parsedExpression[1])
-        } else {
-            expression.split(*delimiters)
-        }.map { it.trim() }
-    }
+    fun extractTokens(): List<String> = if (hasCustomDelimiter()) {
+        parsedExpression[2].split(parsedExpression[1])
+    } else {
+        expression.split(*delimiters)
+    }.map { it.trim() }
 
     fun isValid() = extractTokens().all {
         val number = it.toIntOrNull()
         number != null && number >= 0
     }
 
-    private fun hasCustomDelimiter() = parse().size == CUSTOM_DELIMITER_TOKEN_SIZE
+    private fun hasCustomDelimiter() = parsedExpression.size == CUSTOM_DELIMITER_TOKEN_SIZE
 
     private fun parse() = pattern.find(expression)?.groupValues ?: emptyList()
 
