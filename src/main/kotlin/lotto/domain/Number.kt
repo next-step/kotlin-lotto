c89f57e7349ad6176b69abcb5af6bd519e39652d
Parkjoopika <parkjoohan0603@gmail.com>
2020-08-02 22:12:52 +0900

    feat: Number.kt Blank, Null 체크
    Number는 공백갑과 null값을 가지지 못하도록 구현헀다

--- src/main/kotlin/lotto/domain/Number.kt
+++ src/main/kotlin/lotto/domain/Number.kt
@@ -1,15 +1,23 @@
 package lotto.domain
 
-class Number(string: String) {
-    private val number = string.toInt()
+class Number(string: String?) {
+    private val number = changeInt(string)
 
     init {
         checkMinusAndZero()
     }
 
+    private fun changeInt(string: String?): Int {
+        if (!string.isNullOrBlank()) {
+            return string.toInt()
+        } else {
+            throw NumberFormatException("공백값과 null값은 가질수 없습니다.")
+        }
+    }
+
     private fun checkMinusAndZero() {
         if (number <= 0) {
-            throw RuntimeException("0보다 작은값은 입력하면 안됩니다.")
+            throw NumberFormatException("0보다 작은값은 가질수 없습니다.")
         }
     }
 }
--- src/test/kotlin/lotto/domain/NumberTest.kt
+++ src/test/kotlin/lotto/domain/NumberTest.kt
@@ -8,13 +8,27 @@ class NumberTest {
     fun can_not_has_minus() {
         assertThatThrownBy {
             Number("-1")
-        }.isInstanceOf(RuntimeException::class.java).hasMessageContaining("0보다 작은값은 입력하면 안됩니다.")
+        }.isInstanceOf(NumberFormatException::class.java).hasMessageContaining("0보다 작은값은 가질수 없습니다.")
     }
 
     @Test
     fun can_not_has_zero() {
         assertThatThrownBy {
             Number("0")
-        }.isInstanceOf(RuntimeException::class.java).hasMessageContaining("0보다 작은값은 입력하면 안됩니다.")
+        }.isInstanceOf(NumberFormatException::class.java).hasMessageContaining("0보다 작은값은 가질수 없습니다.")
+    }
+
+    @Test
+    fun can_not_has_blank() {
+        assertThatThrownBy {
+            Number("")
+        }.isInstanceOf(NumberFormatException::class.java).hasMessageContaining("공백값과 null값은 가질수 없습니다.")
+    }
+
+    @Test
+    fun can_not_has_null() {
+        assertThatThrownBy {
+            Number(null)
+        }.isInstanceOf(NumberFormatException::class.java).hasMessageContaining("공백값과 null값은 가질수 없습니다.")
     }
 }
