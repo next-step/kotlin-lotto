e296a3c69ed9733f2a7647557626dc733363d2c5
bky373 <bo373@naver.com>
2020-08-02 18:21:03 +0900

    style(calculator): replace by println in Main.kt

--- src/main/kotlin/calculator/Main.kt
+++ src/main/kotlin/calculator/Main.kt
@@ -2,23 +2,23 @@ package calculator
 
 const val ENTER_TEXT_TO_CALCULATE = "계산할 내용을 입력해주세요. (예) 1,2,3 또는 1:2:3  (* 커스텀 구분자 관련 : c 입력)"
 const val CUSTOM_DELIMITER_INFO = "커스텀 구분자 : // 와 \\n 사이에 입력 => (예) //;\\n1;2;3 "
-const val NEED_INFO_KEYWORD = "c"
-const val ALERT_EMPTY_INPUT = "입력값이 비어있습니다. "
+const val NEED_INFO_KEY = "c"
+const val ALERT_ZERO_OR_EMPTY_INPUT = "입력값이 0이거나 비어있습니다. "
 
 fun main() {
     var result = 0
 
     while (result == 0) {
-        ENTER_TEXT_TO_CALCULATE.also(::println)
+        println(ENTER_TEXT_TO_CALCULATE)
         var text = readLine()
 
-        if (text.equals(NEED_INFO_KEYWORD)) {
-            CUSTOM_DELIMITER_INFO.also(::println)
+        if (text.equals(NEED_INFO_KEY)) {
+            println(CUSTOM_DELIMITER_INFO)
             text = readLine()
         }
 
         result = StringAddCalculator.add(text)
-        if (result == 0) ALERT_EMPTY_INPUT.also(::print)
+        if (result == 0) println(ALERT_ZERO_OR_EMPTY_INPUT)
     }
-    result.also(::println)
+    println(result)
 }
