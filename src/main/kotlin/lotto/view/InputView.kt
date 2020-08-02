396d53288684fd47ec6da4e80ac83e9f8307eefc
BeokBeok <kekemusa37@nate.com>
2020-08-02 11:07:09 +0900

    feat(lotto) : [추가] 지난 주 당첨 번호 입력받는 함수

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -6,4 +6,9 @@ object InputView {
         println("구입 금액을 입력해주세요.")
         return readLine()?.toInt() ?: 0
     }
+
+    fun requestLastWeekLottoNumber(): List<Int> {
+        println("지난 주 당첨 번호를 입력해 주세요.")
+        return readLine()?.split(",")?.map { it.toInt() } ?: emptyList()
+    }
 }
