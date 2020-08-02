b7a047061dfbe56182e076715c6b871467117229
jejun <jjin.dev@gmail.com>
2020-08-02 17:33:08 +0900

    Implement(step2): 5. 자동 생성된 로또 번호를 출력한다.

--- src/main/kotlin/lotto/LottoMain.kt
+++ src/main/kotlin/lotto/LottoMain.kt
@@ -9,4 +9,5 @@ fun main() {
     val lottos = Buyer().buyLotto(money)
 
     ResultView.printLottoCount(lottos)
+    ResultView.printLottos(lottos)
 }
--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -6,4 +6,10 @@ object ResultView {
     fun printLottoCount(lottos: List<Lotto>) {
         println("${lottos.size}개를 구매했습니다.")
     }
+
+    fun printLottos(lottos: List<Lotto>) {
+        lottos.forEach {
+            println("${it.numbers}")
+        }
+    }
 }
