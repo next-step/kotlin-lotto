415b11e0b25359c0aa6b1002d353279f3127c601
jonnycho <andante2183@gmail.com>
2020-08-02 15:50:21 +0900

    Step1 - 문자열 덧셈기 TDD (#16)
    * feat(StringAddCalculatorTest): add fail test code
    
    * doc: add readme
    
    * test(calculator): add empty string test code
    
    * text(Calculator): add one string test code
    
    * text(Calculator): add split test code
    
    * test(Calculator): add netative number test code
    
    * test(Calculator): change throw message
    
    * test(Calculator): add custom delimiter test code
    
    * refactor(Calculator): change class to object
    
    * refactor(Calculator): add private methods
    
    * refactor(Calculator): split getNumbers method
    
    * doc: update readme
    
    * refactor(Calculator): change throw message to const value
    
    * refactor(Calculator): change 0 literal to ZERO const value
    
    * refactor(Calculator): make regex in advance
    
    * refactor(Calculator): remote delimiters parameter in getNumbers
    
    * refactor(Calculator): add Number class

--- src/main/kotlin/stringAddCalculator/StringAddCalculator.kt
+++ src/main/kotlin/stringAddCalculator/StringAddCalculator.kt
@@ -3,42 +3,35 @@ package stringAddCalculator
 object StringAddCalculator {
     private const val CUSTOM_DELIMITER =
         """//(.)\\n(.*)"""
-    private const val ZERO = 0
-    private const val NEGATIVE_NUMBER_THROW_MESSAGE = "can not use nagative number"
+    const val ZERO = 0
     private val delimiters = mutableListOf(",", ":")
+    private val regex = Regex(CUSTOM_DELIMITER)
 
-    fun add(text: String): Int {
-        if (text.isBlank()) return 0
-        if (text.length == 1) return text.toInt()
-        var targetText = getTargetText(text)
-        val numbers = getNumbers(targetText, delimiters)
+    fun add(text: String): Number {
+        if (text.isBlank()) return Number(ZERO)
+        if (text.length == 1) return Number(text.toInt())
+        val targetText = getTargetText(text)
+        val numbers = getNumbers(targetText)
         return getTotal(numbers)
     }
 
     private fun getTargetText(text: String): String {
-        return Regex(CUSTOM_DELIMITER).find(text)?.let {
+        return regex.find(text)?.let {
             delimiters.add(it.groupValues[1])
             it.groupValues[2]
         } ?: text
     }
 
-    private fun getNumbers(
-        targetText: String,
-        delimiters: MutableList<String>
-    ): List<Int> {
+    private fun getNumbers(targetText: String): List<Number> {
         val strList = targetText.split(*delimiters.toTypedArray())
         return toIntList(strList)
     }
 
-    private fun toIntList(values: List<String>): List<Int> {
+    private fun toIntList(values: List<String>): List<Number> {
         return values.map { value ->
-            val number = value.toInt()
-            if (isNegativeNumber(number)) throw RuntimeException("$NEGATIVE_NUMBER_THROW_MESSAGE $number")
-            number
+            Number(value.toInt())
         }
     }
 
-    private fun isNegativeNumber(number: Int) = number < ZERO
-
-    private fun getTotal(texts: List<Int>) = texts.reduce { total, number -> total + number }
+    private fun getTotal(texts: List<Number>) = texts.reduce { total, number -> total.plus(number) }
 }
