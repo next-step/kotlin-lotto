104e1c58a26e182efe97614ce6b298d641df0fa7
Jonny-Cho <andante2183@gmail.com>
2020-08-02 15:49:58 +0900

    refactor(Number): change class to data class

--- src/main/kotlin/stringAddCalculator/Number.kt
+++ src/main/kotlin/stringAddCalculator/Number.kt
@@ -1,11 +1,9 @@
 package stringAddCalculator
 
-class Number(private val number: Int) {
+data class Number(private val number: Int) {
     init {
         if (isNegativeNumber(number)) throw RuntimeException("$NEGATIVE_NUMBER_THROW_MESSAGE $number")
     }
-
-    fun getNumber() = number
 
     fun plus(targetNumber: Number) = Number(number.plus(targetNumber.number))
 
