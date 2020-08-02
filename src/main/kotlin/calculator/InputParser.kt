e56c333271db76ee809d43be5a38126779c43beb
bky373 <bo373@naver.com>
2020-08-02 11:59:47 +0900

    refactor(calculator): modify and rename code in InputParser.kt

--- src/main/kotlin/calculator/InputParser.kt
+++ src/main/kotlin/calculator/InputParser.kt
@@ -10,40 +10,38 @@ class InputParser(private val text: String) {
 
     private fun numbersFrom(text: String): List<Int> {
         val result = CUSTOM_PATTERN.toRegex().find(text)
-        var token = text
-        var customDelimiter = DELIMITER_COMMA
+        var text = text
+        var customDelimiter = COMMA
 
         result?.let {
-            customDelimiter = customDelimiter(result)
-            token = result.groupValues[2]
+            customDelimiter = result.groupValues[1]
+            text = result.groupValues[2]
         }
 
-        val numbers = splitFrom(token, customDelimiter)
+        val numbers = splitFrom(text, customDelimiter)
         return numbers
     }
 
     private fun splitFrom(text: String, custom: String): List<Int> {
-        val tokens = text.split(DELIMITER_COMMA, DELIMITER_COLON, custom)
-        checkIllegalInput(tokens)
+        val tokens = text.split(COMMA, COLON, custom)
+        checkIllegalToken(tokens)
 
         return tokens.map { it.toInt() }
     }
 
-    private fun customDelimiter(result: MatchResult): String = result.groupValues[1]
-
-    private fun checkIllegalInput(tokens: List<String>) {
+    private fun checkIllegalToken(tokens: List<String>) {
         if (tokens.any {
-                it.toIntOrNull() == null ||
-                    it.toInt() < 0
-            }
+            it.toIntOrNull() == null ||
+                it.toInt() < 0
+        }
         ) {
             throw RuntimeException(NOT_ALLOW_LETTER_OR_NEGATIVE)
         }
     }
 
     companion object {
-        private const val DELIMITER_COMMA = ","
-        private const val DELIMITER_COLON = ":"
+        private const val COMMA = ","
+        private const val COLON = ":"
         private const val CUSTOM_PATTERN = "//(.)\\\\n(.*)"
         private const val NOT_ALLOW_LETTER_OR_NEGATIVE = "숫자 이외의 값 또는 음수 입력 불가"
     }
