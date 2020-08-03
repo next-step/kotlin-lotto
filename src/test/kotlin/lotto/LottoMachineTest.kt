77c7883aebaa2676126e69f9da2914ba850a0c6f
influx.pine <influx.pine@gmail.com>
2020-08-04 01:36:01 +0900

    test: 테스트 케이스 수정 및 추가

--- src/test/kotlin/lotto/LottoMachineTest.kt
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -7,13 +7,25 @@ class LottoMachineTest {
 
     @Test
     fun `수익 계산`() {
-        val lottoNumber = LottoNumber(1, 2, 3, 4, 5, 6)
-        val prizeNumber = LottoNumber(3, 4, 5, 6, 7, 8)
 
         val machine = LottoMachine()
-        val prizeStat = machine.calculateStat(listOf(lottoNumber), prizeNumber)
+
+        val prizeStat = mapOf(Pair(4, 1))
         val profit = machine.calculateProfit(prizeStat)
 
         assertThat(profit).isEqualTo(5000)
+    }
+
+    @Test
+    fun `등수 목록 계산`() {
+        val lottoNumber1 = LottoNumber(1, 2, 3, 4, 5, 6)
+        val lottoNumber2 = LottoNumber(3, 4, 5, 6, 7, 8)
+        val prizeNumber = LottoNumber(3, 4, 5, 6, 7, 8)
+
+        val machine = LottoMachine()
+        val prizeStat = machine.calculateStat(listOf(lottoNumber1, lottoNumber2), prizeNumber)
+
+        assertThat(prizeStat).containsEntry(6, 1)
+        assertThat(prizeStat).containsEntry(4, 1)
     }
 }
