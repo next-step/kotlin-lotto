853a2390bbcd026396d38eca0b05482131d3b637
김동환 <66408194+dev-donghwan@users.noreply.github.com>
2020-08-03 21:23:41 +0900

    Step1 (#13)
    * feat:StringAddCalculator
    
    * feat:StringAddCalculator
    
    * String Calculator

---
+++ src/main/kotlin/StringAddCalculator.kt
@@ -0,0 +1,29 @@
+import java.lang.RuntimeException
+
+val delimiters = mutableListOf(",", ":")
+val customDelimiterPattern = Regex("(\\/\\/(.)\\\\n)(.*)")
+
+class StringAddCalculator {
+
+    fun add(text: String): Int {
+        if (text.isEmpty()) return 0
+        if (text.length == 1) return text.toInt()
+        return divideTargetTextAndDelimiter(text).split(*delimiters.toTypedArray())
+            .map {
+                val number = it.toInt()
+                isNegativeCheck(number)
+                number
+            }.sum()
+    }
+
+    private fun isNegativeCheck(number: Int) {
+        if (number < 0) throw RuntimeException()
+    }
+
+    private fun divideTargetTextAndDelimiter(text: String): String {
+        return customDelimiterPattern.find(text)?.let {
+            delimiters.add(it.groupValues[2])
+            text.replace(it.groupValues[1], "")
+        } ?: text
+    }
+}
