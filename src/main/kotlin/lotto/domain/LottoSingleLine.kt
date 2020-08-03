b57d8c8acefff6df1a773f8680c94694331e2841
developer-donghwan <ycdoit@naver.com>
2020-08-03 22:25:28 +0900

    LottoTicket에서 번호 6개가 있는 한줄에대한 아이템 생성

---
+++ src/main/kotlin/lotto/domain/LottoSingleLine.kt
@@ -0,0 +1,11 @@
+package lotto.domain
+
+private val LOTTO_NUMBER_LIST = (1..45).toList()
+
+class LottoSingleLine {
+    val lottoNumbers = createLine()
+
+    private fun createLine(): List<Int> {
+        return LOTTO_NUMBER_LIST.shuffled().take(6)
+    }
+}
