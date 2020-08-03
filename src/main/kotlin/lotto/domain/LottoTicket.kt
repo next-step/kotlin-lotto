a19fa690b448b627efe276d01e742db371ba204c
developer-donghwan <ycdoit@naver.com>
2020-08-03 22:35:18 +0900

    create LottoTicket

---
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -0,0 +1,13 @@
+package lotto.domain
+
+class LottoTicket(private val totalNumber: Int) {
+    val lottoLines = createLottoTicket(totalNumber)
+
+    private fun createLottoTicket(totalNumber: Int): List<LottoSingleLine> {
+        val list = mutableListOf<LottoSingleLine>()
+        for (count in 1..totalNumber) {
+            list.add(LottoSingleLine())
+        }
+        return list
+    }
+}
