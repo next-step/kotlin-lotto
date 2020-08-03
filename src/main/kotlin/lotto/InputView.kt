38e134890fc4c15e00756ed831b6a2e4fffc9c66
bky373 <bo373@naver.com>
2020-08-04 06:24:13 +0900

    feat(lotto); create InputView.kt

---
+++ src/main/kotlin/lotto/InputView.kt
@@ -0,0 +1,17 @@
+package lotto
+
+object InputView {
+    fun readPayment(): Int {
+        println("구입금액을 입력해주세요.")
+        val money = readLine()
+        require(!money.isNullOrBlank()) { "구입금액 입력값이 비어있습니다." }
+        return money.toInt()
+    }
+
+    fun readLuckyNumbers(): List<Int> {
+        println("\n지난 주 당첨 번호를 입력해주세요.")
+        val numbers = readLine()
+        require(!numbers.isNullOrBlank()) { "지난 주 당첨 번호 입력값이 비어있습니다." }
+        return numbers.split(",").map { it.toInt() }
+    }
+}
