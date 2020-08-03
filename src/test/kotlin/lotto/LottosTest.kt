821b0d9b1f4e8dd9e697584aa128ba22a5168a40
bky373 <bo373@naver.com>
2020-08-04 06:17:04 +0900

    test(lotto); create LottosTest.kt

---
+++ src/test/kotlin/lotto/LottosTest.kt
@@ -0,0 +1,24 @@
+package lotto
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class LottosTest {
+    private lateinit var lottos: Lottos
+
+    @BeforeEach
+    fun `set up`() {
+        lottos = Lottos(
+            listOf(
+                Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                Lotto(listOf(7, 8, 9, 10, 11, 12)),
+                Lotto(listOf(13, 14, 15, 16, 17, 18))
+            )
+        )
+    }
+
+    @Test
+    fun `print toString`() {
+        print(lottos.toString())
+    }
+}
