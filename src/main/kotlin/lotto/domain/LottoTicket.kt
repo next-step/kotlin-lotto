d16aa43c764a9fb5665405aa541a7c73424c2a55
developer-donghwan <ycdoit@naver.com>
2020-08-03 23:36:54 +0900

    ResultData 추가

--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -1,7 +1,21 @@
 package lotto.domain
 
+const val FIRST_PRICE = 2000000000
+const val SECOND_PRICE = 1500000
+const val THIRD_PRICE = 500000
+const val FOURTH_PRICE = 5000
+
 class LottoTicket(private val totalNumber: Int) {
     private val lottoLines = createLottoTicket(totalNumber)
+
+    fun checkResult(result: List<Int>): ResultData {
+        val first = getFirst(result)
+        val second = getSecond(result)
+        val third = getThird(result)
+        val fourth = getFourth(result)
+        val price = (first * FIRST_PRICE) + (second * SECOND_PRICE) + (third * THIRD_PRICE) + (fourth * FOURTH_PRICE)
+        return ResultData(first, second, third, fourth, price)
+    }
 
     private fun createLottoTicket(totalNumber: Int): List<LottoSingleLine> {
         val list = mutableListOf<LottoSingleLine>()
@@ -9,5 +23,21 @@ class LottoTicket(private val totalNumber: Int) {
             list.add(LottoSingleLine())
         }
         return list
+    }
+
+    private fun getFirst(result: List<Int>): Int {
+        return lottoLines.filter { it.matching(result) == 6 }.size
+    }
+
+    private fun getSecond(result: List<Int>): Int {
+        return lottoLines.filter { it.matching(result) == 5 }.size
+    }
+
+    private fun getThird(result: List<Int>): Int {
+        return lottoLines.filter { it.matching(result) == 4 }.size
+    }
+
+    private fun getFourth(result: List<Int>): Int {
+        return lottoLines.filter { it.matching(result) == 3 }.size
     }
 }
---
+++ src/main/kotlin/lotto/domain/ResultData.kt
@@ -0,0 +1,3 @@
+package lotto.domain
+
+data class ResultData(val first: Int, val second: Int, val third: Int, val fourth: Int, val price: Int)
