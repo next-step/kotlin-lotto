298ff4cfce576b97b6a9aba1bd37d074bd2cd2ee
bky373 <bo373@naver.com>
2020-08-04 06:19:11 +0900

    test(lotto); create LottoShopTest.kt

---
+++ src/test/kotlin/lotto/LottoShopTest.kt
@@ -0,0 +1,17 @@
+package lotto
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LottoShopTest {
+
+    @Test
+    fun `purchase amount`() {
+        assertThat(LottoShop(5000).makeLottos().size).isEqualTo(5)
+    }
+
+    @Test
+    fun `print toString`() {
+        assertThat(LottoShop(5000).toString()).isEqualTo("5개를 구매했습니다.")
+    }
+}
