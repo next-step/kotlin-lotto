334e01f1e801025d1d712b3bde40ed56ea2a5d4d
jejun <jjin.dev@gmail.com>
2020-08-03 22:24:14 +0900

    Refactor(step1): Number 의 생성조건을 init 내부에 구현

--- src/main/kotlin/calculate/Number.kt
+++ src/main/kotlin/calculate/Number.kt
@@ -2,22 +2,16 @@ package calculate
 
 import java.lang.RuntimeException
 
+private const val CALCULATOR_MIN_NUMBER = 0
+
 class Number(number: String) {
-    val value: Int = getNumber(number)
-
-    private fun getNumber(text: String): Int {
+    init {
         try {
-            val number = text.toInt()
-
-            require(number > CALCULATOR_MIN_NUMBER) { "${CALCULATOR_MIN_NUMBER}보다 큰 숫자만 더할 수 있습니다." }
-
-            return number
+            if (number.toInt() <= CALCULATOR_MIN_NUMBER) throw RuntimeException("${CALCULATOR_MIN_NUMBER}보다 큰 숫자만 더할 수 있습니다.")
         } catch (e: RuntimeException) {
             throw RuntimeException("숫자만 더할 수 있습니다.")
         }
     }
 
-    companion object {
-        private const val CALCULATOR_MIN_NUMBER = 0
-    }
+    val value: Int = number.toInt()
 }
