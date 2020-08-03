a3ba35768d196f0e1b87f17233cb04e03f0e05a9
influx.pine <influx.pine@gmail.com>
2020-08-04 02:33:36 +0900

    feat: 메인 함수 로직 추가

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -2,7 +2,7 @@ package lotto
 
 class LottoMachine {
 
-    fun getRandoms(): List<Int> = LOTTO_NUMBERS.shuffled().subList(0, 5)
+    fun getRandoms(): List<Int> = LOTTO_NUMBERS.shuffled().subList(0, 6)
 
     fun calculateStat(lottoNumbers: List<LottoNumber>, prizeNumber: LottoNumber): List<PrizeMoneyWrapper> {
         return lottoNumbers.groupingBy {
@@ -14,7 +14,31 @@ class LottoMachine {
         return prizeMoneyWrappers.sumBy { it.prizeMoney.getProfit(it.prizeCount) }
     }
 
+    fun createLottoNumbers(buyMoney: Int): List<LottoNumber> {
+        var lottoNumbers = mutableListOf<LottoNumber>()
+        repeat(buyMoney / LOTTO_BASE_PRICE) {
+            lottoNumbers.add(LottoNumber(getRandoms()))
+        }
+        return lottoNumbers
+    }
+
     companion object {
         val LOTTO_NUMBERS = (1..45).toList()
     }
+}
+
+fun main() {
+    val machime = LottoMachine()
+
+    val inpuView = InputView()
+    val buyMoney = inpuView.buyMoney()
+    var lottoNumbers = machime.createLottoNumbers(buyMoney)
+    val prevPrizNumber = LottoNumber(inpuView.prevPrizeLotto())
+    val prizeMoneyWrappers = machime.calculateStat(lottoNumbers, prevPrizNumber)
+
+    val resultView = ResultView()
+
+    resultView.printNumbers(lottoNumbers)
+    resultView.printPrizeStat(prizeMoneyWrappers)
+    resultView.printProfit(machime.calculateProfit(prizeMoneyWrappers), buyMoney)
 }
