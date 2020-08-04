900ae0527be7fdcb0730118c64bddfddb8497597
Jonny-Cho <andante2183@gmail.com>
2020-08-04 20:44:32 +0900

    feat(Money): add funtions of money

--- src/main/kotlin/lotto/domain/value/Money.kt
+++ src/main/kotlin/lotto/domain/value/Money.kt
@@ -1,9 +1,23 @@
 package lotto.domain.value
 
-class Money(private val won: Int) {
+data class Money(private val won: Int) {
     init {
         if (won < ZERO) throw IllegalArgumentException("음수는 Money로 사용될 수 없습니다.")
     }
+
+    fun won() = won
+
+    operator fun plus(money: Money) = Money(won.plus(money.won))
+
+    operator fun minus(money: Money) = Money(won.minus(money.won))
+
+    operator fun times(count: Int) = Money(won * count)
+
+    operator fun div(count: Double) = won.div(count)
+
+    override fun toString() = "${won}원"
+
+    fun toDouble() = won.toDouble()
 
     companion object {
         private const val ZERO = 0
