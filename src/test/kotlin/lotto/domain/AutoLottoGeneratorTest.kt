9af2dab3aca75d7620506679d91ea7dcd5396dac
zion830 <zion830@honglab.org>
2020-08-05 03:57:34 +0900

    test(lotto): 자동 로또 번호 생성 로직 테스트 추가

---
+++ src/test/kotlin/lotto/domain/AutoLottoGeneratorTest.kt
@@ -0,0 +1,14 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+internal class AutoLottoGeneratorTest {
+
+    @DisplayName("랜덤으로 6자리의 번호를 생성한다")
+    @Test
+    fun execute() {
+        assertThat(AutoLottoGenerator.execute()).hasSize(6).allSatisfy { it in 1..45 }
+    }
+}
