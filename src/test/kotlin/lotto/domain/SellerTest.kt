16ccc3bec117ae3a7de37055628fd2400e802433
zion830 <zion830@honglab.org>
2020-08-05 03:53:45 +0900

    test(lotto): 판매원 테스트 케이스 변경

--- src/test/kotlin/lotto/domain/SellerTest.kt
+++ src/test/kotlin/lotto/domain/SellerTest.kt
@@ -2,23 +2,16 @@ package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.api.Test
 
 internal class SellerTest {
     private val seller = Seller()
 
-    @DisplayName(value = "발급할 로또의 갯수를 계산한다")
-    @ParameterizedTest
-    @ValueSource(ints = [10000])
-    fun calculate(payment: Payment) {
-        assertThat(seller.calculate(payment)).isEqualTo(PaymentResult(10, 0))
-    }
-
-    @DisplayName(value = "1000의 배수가 아닌 값을 받으면 거스름돈을 돌려준다")
-    @ParameterizedTest
-    @ValueSource(ints = [12300])
-    fun calculateChange(payment: Payment) {
-        assertThat(seller.calculate(payment)).isEqualTo(PaymentResult(12, 300))
+    @DisplayName(value = "지불 결과(거스름돈, 로또)를 반환한다")
+    @Test
+    fun processOrder() {
+        val paymentResult = seller.processOrder(12300)
+        assertThat(paymentResult.lottoes).hasSize(12).doesNotContainNull()
+        assertThat(paymentResult.change).isEqualTo(300)
     }
 }
