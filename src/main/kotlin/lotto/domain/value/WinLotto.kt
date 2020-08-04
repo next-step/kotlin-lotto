a963b8a755e847a46226cdebe94e1828c71490e7
Jonny-Cho <andante2183@gmail.com>
2020-08-05 02:25:21 +0900

    refactor(WinLotto): change to biginteger

--- src/main/kotlin/lotto/domain/value/WinLotto.kt
+++ src/main/kotlin/lotto/domain/value/WinLotto.kt
@@ -1,20 +1,24 @@
 package lotto.domain.value
 
+import java.math.BigInteger
+
 enum class WinLotto(private val index: Int, val money: Money, private var count: Int = 0) {
-    ZERO(0, Money(0)),
-    ONE(1, Money(0)),
-    TWO(2, Money(0)),
-    THREE(3, Money(5_000)),
-    FOUR(4, Money(50_000)),
-    FIVE(5, Money(1_500_000)),
-    SIX(6, Money(2_000_000_000));
+    ZERO(0, Money(0.toBigInteger())),
+    ONE(1, Money(0.toBigInteger())),
+    TWO(2, Money(0.toBigInteger())),
+    THREE(3, Money(5_000.toBigInteger())),
+    FOUR(4, Money(50_000.toBigInteger())),
+    FIVE(5, Money(1_500_000.toBigInteger())),
+    SIX(6, Money(2_000_000_000.toBigInteger()));
+
+    fun getCount() = count
 
     override fun toString(): String {
         return "${index}개 일치 ($money)- ${count}개\n"
     }
 
     companion object {
-        private const val ZERO = 0
+        private const val ZERO = 0L
         private const val THREE = 3
 
         operator fun invoke(index: Int) = values()[index]
@@ -27,6 +31,6 @@ enum class WinLotto(private val index: Int, val money: Money, private var count:
 
         fun resultList() = values().filter { it.index >= THREE }.toList()
 
-        fun totalIncome() = values().fold(Money(ZERO)) { total, lotto -> total + (lotto.money * lotto.count) }
+        fun totalIncome() = values().fold(Money(BigInteger.ZERO)) { total, lotto -> total + (lotto.money * lotto.count) }
     }
 }
