be43a6d0bad04e28cea204dbdd10021e974fe25f
BeokBeok <kekemusa37@nate.com>
2020-08-04 23:55:43 +0900

    fix(test) : [수정] 로또 생성을 기본으로 하고, 당첨 번호를 변경하여 테스트

--- src/test/kotlin/lotto/StoreTest.kt
+++ src/test/kotlin/lotto/StoreTest.kt
@@ -15,41 +15,33 @@ class StoreTest {
 
     @BeforeEach
     fun setup() {
-        buyer = Buyer()
+        buyer = Buyer().apply { markLotto(Lotto(listOf(1, 2, 3, 4, 5, 6))) }
         store = Store(buyer)
     }
 
     @Test
     fun `5천원 당첨`() {
-        buyer.markLotto(Lotto(listOf(1, 2, 3, 7, 8, 9)))
-
-        val winnerHistory = store.drawLottoNumber(Lotto(listOf(1, 2, 3, 4, 5, 6)))
+        val winnerHistory = store.drawLottoNumber(Lotto(listOf(1, 2, 3, 14, 15, 16)))
 
         assertThat(winnerHistory.contains(Prize.FOURTH)).isTrue()
     }
 
     @Test
     fun `5만원 당첨`() {
-        buyer.markLotto(Lotto(listOf(1, 2, 3, 4, 8, 9)))
-
-        val winnerHistory = store.drawLottoNumber(Lotto(listOf(1, 2, 3, 4, 5, 6)))
+        val winnerHistory = store.drawLottoNumber(Lotto(listOf(1, 2, 3, 4, 15, 16)))
 
         assertThat(winnerHistory.contains(Prize.THIRD)).isTrue()
     }
 
     @Test
     fun `150만원 당첨`() {
-        buyer.markLotto(Lotto(listOf(1, 2, 3, 4, 5, 9)))
-
-        val winnerHistory = store.drawLottoNumber(Lotto(listOf(1, 2, 3, 4, 5, 6)))
+        val winnerHistory = store.drawLottoNumber(Lotto(listOf(1, 2, 3, 4, 5, 16)))
 
         assertThat(winnerHistory.contains(Prize.SECOND)).isTrue()
     }
 
     @Test
     fun `20억 당첨`() {
-        buyer.markLotto(Lotto(listOf(1, 2, 3, 4, 5, 6)))
-
         val winnerHistory = store.drawLottoNumber(Lotto(listOf(1, 2, 3, 4, 5, 6)))
 
         assertThat(winnerHistory.contains(Prize.FIRST)).isTrue()
@@ -57,9 +49,7 @@ class StoreTest {
 
     @Test
     fun `수익률 조회`() {
-        buyer.markLotto(Lotto(listOf(1, 2, 3, 7, 8, 9)))
-
-        val winnerHistory = store.drawLottoNumber(Lotto(listOf(1, 2, 3, 4, 5, 6)))
+        val winnerHistory = store.drawLottoNumber(Lotto(listOf(1, 2, 3, 14, 15, 16)))
 
         assertThat(store.getRateOfReturn(14_000, winnerHistory)).isEqualTo(0.35)
     }
