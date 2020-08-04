1924a2474ec0fb24e7583e9930553c8ee608eb8f
bky373 <bo373@naver.com>
2020-08-04 22:26:43 +0900

    refactor(lotto): make some values private and replace subList() with take()

--- src/main/kotlin/lotto/LottoUtils.kt
+++ src/main/kotlin/lotto/LottoUtils.kt
@@ -1,13 +1,13 @@
 package lotto
 
-val DEFAULT_RANGE = 1..45
-const val LOTTO_NUMBERS = 6
+private val DEFAULT_RANGE = 1..45
+private const val LOTTO_NUMBERS = 6
 const val LOTTO_PRICE = 1000
 
 object LottoUtils {
     lateinit var luckyNumbers: List<Int>
 
     fun provideNumbers(): List<Int> {
-        return DEFAULT_RANGE.shuffled().subList(0, LOTTO_NUMBERS).sorted()
+        return DEFAULT_RANGE.shuffled().take(LOTTO_NUMBERS).sorted()
     }
 }
