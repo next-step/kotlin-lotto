80f90948f5a0549f72182599118bf334904495cb
Jonny-Cho <andante2183@gmail.com>
2020-08-04 20:44:02 +0900

    test(MoneyTest): add more test code of money

--- src/test/kotlin/lotto/domain/value/MoneyTest.kt
+++ src/test/kotlin/lotto/domain/value/MoneyTest.kt
@@ -1,5 +1,6 @@
 package lotto.domain.value
 
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
@@ -9,5 +10,41 @@ class MoneyTest {
         assertThatThrownBy { Money(-1) }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage("음수는 Money로 사용될 수 없습니다.")
+    }
+
+    @Test
+    fun `toString()은 원을 붙여 출력한다`() {
+        val actual = Money(1000).toString()
+        assertThat(actual).isEqualTo("1000원")
+    }
+
+    @Test
+    fun `money 객체끼리 더할 수 있다`() {
+        val number1 = 100
+        val number2 = 200
+        val actual = Money(number1) + Money(number2)
+        assertThat(actual).isEqualTo(Money(number1 + number2))
+    }
+
+    @Test
+    fun `money 객체끼리 뺄 수 있다`() {
+        val number1 = 300
+        val number2 = 200
+        val actual = Money(number1) - Money(number2)
+        assertThat(actual).isEqualTo(Money(number1 - number2))
+    }
+
+    @Test
+    fun `money 객체에 Int를 곱할 수 있으며 그 결과는 Money 타입이다`() {
+        val number1 = 300
+        val number2 = 2
+        val actual = Money(number1) * number2
+        assertThat(actual).isEqualTo(Money(number1 * number2))
+    }
+
+    @Test
+    fun `money 객체에 double을 나눌 수 있으며 그 결과는 Double타입 이다`() {
+        val actual = Money(300) / 30.0
+        assertThat(actual).isEqualTo(10.0)
     }
 }
