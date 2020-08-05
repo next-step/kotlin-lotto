9443c6e2f7e584ff993bc6d9f46eff2207dafa00
influx.pine <influx.pine@gmail.com>
2020-08-05 23:50:52 +0900

    feat: PrizeMoney(Enum) 당첨금 수정

--- src/main/kotlin/lotto/PrizeMoney.kt
+++ src/main/kotlin/lotto/PrizeMoney.kt
@@ -1,36 +1,20 @@
 package lotto
 
-enum class PrizeMoney(private val equalsCount: Int) {
+enum class PrizeMoney(private val equalsCount: Int, private val money: Int) {
 
-    SIX(6) {
-        private val prizeMoney = 2000000000
-        override fun getPrizeMoney(): Int = prizeMoney
-    },
-    FIVE(5) {
-        private val prizeMoney = 1500000
-        override fun getPrizeMoney(): Int = prizeMoney
-    },
-    FOUR(4) {
-        private val prizeMoney = 50000
-        override fun getPrizeMoney(): Int = prizeMoney
-    },
-    THREE(3) {
-        private val prizeMoney = 5000
-        override fun getPrizeMoney(): Int = prizeMoney
-    },
-    OTHER(0) {
-        private val prizeMoney = 0
-        override fun getPrizeMoney(): Int = prizeMoney
-    };
+    SIX(6, 2000000000),
+    FIVE(5, 1500000),
+    FOUR(4, 50000),
+    THREE(3, 5000);
 
-    abstract fun getPrizeMoney(): Int
+    fun getPrizeMoney(): Int = money
     fun getEqualsCount(): Int = equalsCount
 
     companion object {
         fun generate(equalsCount: Int): PrizeMoney {
-            return enumValues<PrizeMoney>().find {
+            return enumValues<PrizeMoney>().first {
                 it.equalsCount == equalsCount
-            } ?: OTHER
+            }
         }
     }
 }
