26aed501562d842172b095c191428d48451b47b4
developer-donghwan <ycdoit@naver.com>
2020-08-05 20:21:23 +0900

    step4 inputView 수동 구매 로직 추가

--- src/main/kotlin/lotto/view/Input.kt
+++ src/main/kotlin/lotto/view/Input.kt
@@ -18,6 +18,22 @@ fun inputMoneyError(): Int {
     return readLine()?.toIntOrNull() ?: exitProcess(0)
 }
 
+fun inputManualCount(): Int {
+    println("수동으로 구매할 로또 수를 입력해 주세요.")
+    return readLine()?.toIntOrNull() ?: inputManualCountError()
+}
+
+fun inputManualCountError(): Int {
+    println(
+        """
+        수동으로 구매할 로또 수를 입력해 주세요.
+        숫자만 입력해주세요
+        Ex) 1 / 2 / 3
+        """.trimIndent()
+    )
+    return readLine()?.toIntOrNull() ?: exitProcess(0)
+}
+
 fun inputResult(): List<Int> {
     println("지난 주 당첨 번호를 입력해 주세요.")
     val result = readLine() ?: ""
