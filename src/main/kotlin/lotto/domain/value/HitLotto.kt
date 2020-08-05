6c217bb013c797d81daa2d01cefdbd429129c611
Jonny-Cho <andante2183@gmail.com>
2020-08-05 21:46:38 +0900

    refactor(HitLotto): change WinLotto to HitLotto, update test

--- src/main/kotlin/lotto/domain/value/HitLotto.kt
+++ src/main/kotlin/lotto/domain/value/HitLotto.kt
@@ -2,7 +2,7 @@ package lotto.domain.value
 
 import java.math.BigInteger
 
-enum class WinLotto(private val index: Int, val money: Money, private var count: Int = 0) {
+enum class HitLotto(private val index: Int, val money: Money, private var count: Int = 0) {
     ZERO(0, Money(0.toBigInteger())),
     ONE(1, Money(0.toBigInteger())),
     TWO(2, Money(0.toBigInteger())),
@@ -11,7 +11,7 @@ enum class WinLotto(private val index: Int, val money: Money, private var count:
     FIVE(5, Money(1_500_000.toBigInteger())),
     SIX(6, Money(2_000_000_000.toBigInteger()));
 
-    fun plusCount() = count++
+    fun plusCount() = ++count
 
     override fun toString(): String {
         return "${index}$INDEX_SUFFIX ($money)- ${count}$COUNT_SUFFIX\n"
