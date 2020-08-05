f11189be7560fc1671aa251b2a107e15018f8b4a
namjackson <skawo32167@naver.com>
2020-08-05 16:20:14 +0900

    feature(lotto-BonusBall) : Add BonusBall in PrizeEnum And BonusBall Test

--- src/main/kotlin/lotto/model/Prize.kt
+++ src/main/kotlin/lotto/model/Prize.kt
@@ -1,13 +1,24 @@
 package lotto.model
 
-enum class Prize(val matchingCount: Int, val prizeMoney: Int) {
-    ONE(6, 2_000_000_000),
-    TWO(5, 1_500_000),
-    THREE(4, 50_000),
-    FOUR(3, 5_000),
-    ZERO(0, 0);
+enum class Prize(val matchingCount: Int, val prizeMoney: Int, private val needBonusMatch: Boolean) {
+    ONE(6, 2_000_000_000, false),
+    TWO_BONUS(5, 30_000_000, true),
+    TWO(5, 1_500_000, false),
+    THREE(4, 50_000, false),
+    FOUR(3, 5_000, false),
+    ZERO(0, 0, false);
+
+    fun isPrize(matchingCount: Int, isBonusMatch: Boolean) =
+        matchingCount == this.matchingCount && isBonusMatch(isBonusMatch)
+
+    private fun isBonusMatch(isBonusMatch: Boolean) = if (this.needBonusMatch) {
+        isBonusMatch == this.needBonusMatch
+    } else {
+        true
+    }
 
     companion object {
-        fun getPrize(matchingCount: Int) = values().find { it.matchingCount == matchingCount } ?: ZERO
+        fun getPrize(matchingCount: Int, isBonusMatch: Boolean = false) =
+            values().find { it.isPrize(matchingCount, isBonusMatch) } ?: ZERO
     }
 }
--- src/test/kotlin/lotto/model/PrizeTest.kt
+++ src/test/kotlin/lotto/model/PrizeTest.kt
@@ -17,6 +17,36 @@ class PrizeTest {
         Assertions.assertThat(prize).isEqualTo(Prize.ONE)
     }
 
+    @DisplayName(value = "matchingCount= 2를 제외한 숫자는, BonusBall matching과는 영향이 없어야한다.")
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 3, 4, 6])
+    fun checkPrizeMoneyCasesAndBonusBallMatching(input: Int) {
+        val bonusBall = true
+        val prize = Prize.getPrize(input, bonusBall)
+        Assertions.assertThat(prize.prizeMoney).isEqualTo(Prize.getPrize(input).prizeMoney)
+        Assertions.assertThat(prize).isEqualTo(Prize.getPrize(input))
+    }
+
+    @DisplayName(value = "matchingCount= 5, BonusBall matching이 안되었을 경우, 상금은 2등 보너스 상금과 같아야한다.")
+    @Test
+    fun checkPrizeMoneyCase5() {
+        val input = 5
+        val bonusBall = false
+        val prize = Prize.getPrize(input, bonusBall)
+        Assertions.assertThat(prize.prizeMoney).isEqualTo(Prize.TWO.prizeMoney)
+        Assertions.assertThat(prize).isEqualTo(Prize.TWO)
+    }
+    @DisplayName(value = "matchingCount= 5, BonusBall matching이 되었을 경우, 상금은 2등 보너스 상금과 같아야한다.")
+    @Test
+    fun checkPrizeMoneyCase5AndBonus() {
+        val input = 5
+        val bonusBall = true
+        val prize = Prize.getPrize(input, bonusBall)
+        Assertions.assertThat(prize.prizeMoney).isEqualTo(Prize.TWO_BONUS.prizeMoney)
+        Assertions.assertThat(prize).isEqualTo(Prize.TWO_BONUS)
+    }
+
+
     @DisplayName(value = "matchingCount가 [3,4,5]일 경우, 상금은 4등 상금보다 크거나 같아야 한다,")
     @ParameterizedTest
     @ValueSource(ints = [3, 4, 5])
@@ -31,6 +61,16 @@ class PrizeTest {
     @ValueSource(ints = [2, 1, 0, -1])
     fun checkPrizeMoney(input: Int) {
         val prize = Prize.getPrize(input)
+        Assertions.assertThat(prize.prizeMoney).isEqualTo(Prize.ZERO.prizeMoney)
+        Assertions.assertThat(prize).isEqualTo(Prize.ZERO)
+    }
+
+    @DisplayName(value = "matchingCount가 2이하 또는 음수일때는, 보너스 볼이 같아도, 상금은 0원 ")
+    @ParameterizedTest
+    @ValueSource(ints = [2, 1, 0, -1])
+    fun checkPrizeMoneyZeroBonusBall(input: Int) {
+        val bonusBall = true
+        val prize = Prize.getPrize(input, bonusBall)
         Assertions.assertThat(prize.prizeMoney).isEqualTo(Prize.ZERO.prizeMoney)
         Assertions.assertThat(prize).isEqualTo(Prize.ZERO)
     }
