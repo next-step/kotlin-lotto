8809371f876815f284dca03e7cce1fa9be4931b9
포비 <pobi@woowahan.com>
2020-08-06 21:27:06 +0900

    feat: create Lotto

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,10 +1,25 @@
 package lotto
 
-class Lotto {
-    companion object {
-        fun of(vararg numbers: Int): Lotto {
+data class Lotto(private val numbers: Set<LottoNumber>) {
+    init {
+        require(numbers.size == LOTTO_NUMBER_SIZE) { "로또의 숫자는 $LOTTO_NUMBER_SIZE 개 여야 합니다. " }
+    }
 
-            return Lotto()
+    fun contains(number: Int): Boolean {
+        return numbers.contains(LottoNumber.of(number))
+    }
+
+    companion object {
+        private const val LOTTO_NUMBER_SIZE = 6
+
+        fun of(vararg numbers: Int): Lotto {
+            val lottoNumbers = numbers.map { LottoNumber.of(it) }.toSet()
+            return Lotto(lottoNumbers)
+        }
+
+        fun ofComma(value: String): Lotto {
+            val numbers = value.split(",").map { it.trim().toInt() }
+            return Lotto(numbers.map { LottoNumber.of(it) }.toSet())
         }
     }
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,12 +1,28 @@
 package lotto
 
+import lotto.Lotto.Companion.of
+import lotto.Lotto.Companion.ofComma
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
 
 class LottoTest {
     @Test
     internal fun create() {
-        val lotto = Lotto.of(1, 2, 3, 4, 5, 6)
-        assertThat(lotto)
+        val lotto = of(1, 2, 3, 4, 5, 6)
+        assertThat(lotto).isEqualTo(ofComma("1, 2, 3, 4, 5, 6"))
+    }
+
+    @Test
+    internal fun invalid() {
+        assertThatThrownBy { of(1, 2, 3, 4, 5, 5) }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    internal fun contains() {
+        val lotto = of(1, 2, 3, 4, 5, 6)
+        assertThat(lotto.contains(1)).isTrue()
+        assertThat(lotto.contains(7)).isFalse()
     }
 }
