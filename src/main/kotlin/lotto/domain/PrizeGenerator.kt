93e74c3bad6919cc6a027f46308115f08007dd1c
kimgaeul <kimgaeul4@gmail.com>
2020-08-06 16:09:26 +0900

    feat(step3): enum 클래스에 보너스 당첨 추가

--- src/main/kotlin/lotto/domain/PrizeGenerator.kt
+++ src/main/kotlin/lotto/domain/PrizeGenerator.kt
@@ -5,6 +5,7 @@ enum class PrizeGenerator(val matched: Int, val prize: Int) {
     THREE_MATCH(3, 5_000),
     FOUR_MATCH(4, 50_000),
     FIVE_MATCH(5, 1_500_000),
+    BONUS_MATCH(5, 30_000_000),
     SIX_MATCH(6, 2_000_000_000);
 
     var count: Int = 0
