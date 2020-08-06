2b67f98c29ac809e2806035f8308a04b9244a26e
Malibin <nightmare73@naver.com>
2020-08-06 16:19:55 +0900

    refactor(step3): LottoTicket 생성자 변경
    1. Data Class -> class로 변경
    2. Set을 받는 생성자 제거
    3. 중복과 개수 validation 분리 했던거 다시 합침

--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -1,24 +1,20 @@
 package lotto.domain
 
-data class LottoTicket(
-    private val lottoNumbers: Set<LottoNumber>
+class LottoTicket constructor(
+    lottoNumbers: List<LottoNumber>
 ) {
+    private val lottoNumbers: Set<LottoNumber>
+
     init {
         validateDuplicatedNumbers(lottoNumbers)
-    }
-
-    constructor(lottoNumbers: List<LottoNumber>) : this(lottoNumbers.toSet()) {
-        validateNumbersSize(lottoNumbers.size)
+        this.lottoNumbers = lottoNumbers.toSet()
     }
 
     constructor(vararg numbers: Int) : this(numbers.map { LottoNumber.of(it) })
 
-    private fun validateDuplicatedNumbers(lottoNumbers: Set<LottoNumber>) {
-        require(lottoNumbers.size == LOTTO_NUMBER_COUNT) { "로또 번호는 중복될 수 없습니다." }
-    }
-
-    private fun validateNumbersSize(size: Int) {
-        require(size == LOTTO_NUMBER_COUNT) { "로또 번호는 반드시 6개 입니다." }
+    private fun validateDuplicatedNumbers(lottoNumbers: List<LottoNumber>) {
+        require(lottoNumbers.size == LOTTO_NUMBER_COUNT) { "로또 번호는 반드시 6개 입니다." }
+        require(lottoNumbers.toSet().size == LOTTO_NUMBER_COUNT) { "로또 번호는 중복될 수 없습니다." }
     }
 
     fun compare(lottoTicket: LottoTicket, bonusNumber: LottoNumber): LottoResult {
