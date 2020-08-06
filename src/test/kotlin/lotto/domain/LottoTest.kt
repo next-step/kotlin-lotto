64fa751bdf4a9cd8182ce2ac6101c0f0fdefce2f
kimgaeul <kimgaeul4@gmail.com>
2020-08-06 15:29:06 +0900

    feat(step2): test code 이동

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -4,7 +4,6 @@ import lotto.view.InputView
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class LottoTest {
@@ -12,17 +11,6 @@ class LottoTest {
     @Test
     fun `생성한 로또 번호가 1-45 범위 안에 있는지 확인`() {
         assertThat(IntRange(1, 45).toList().containsAll(Lotto.of().numbers)).isTrue()
-    }
-
-    @ParameterizedTest
-    @CsvSource(
-        value = [
-            "4, 4",
-            "10,10"
-        ]
-    )
-    fun `입력한 갯수 만큼 로또 번호 생성`(purchaseCount: Int, expected: Int) {
-        assertThat(Lottos.of(purchaseCount).lottos.size).isEqualTo(expected)
     }
 
     @ParameterizedTest
