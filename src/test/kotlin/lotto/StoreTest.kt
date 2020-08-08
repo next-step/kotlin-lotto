fdde2473f48babef4701e9bede5388bc2a3b4c31
BeokBeok <kekemusa37@nate.com>
2020-08-08 23:17:05 +0900

    feat(test) : [수정] Store의 object 화로 인한 빌드 에러

--- src/test/kotlin/lotto/StoreTest.kt
+++ src/test/kotlin/lotto/StoreTest.kt
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test
 class StoreTest {
 
     private lateinit var buyer: Buyer
-    private lateinit var store: Store
 
     @BeforeEach
     fun setup() {
@@ -19,61 +18,60 @@ class StoreTest {
             markLotto(Lotto(setOf(1, 2, 3, 4, 5, 6)))
             markLotto(Lotto(setOf(1, 2, 3, 4, 5, 45)))
         }
-        store = Store(buyer)
     }
 
     @Test
     fun 꽝() {
-        val winnerHistory = store.drawLottoNumber(Lotto(setOf(7, 8, 9, 10, 11, 12)), 45)
+        val winnerHistory = Store.drawLottoNumber(buyer.autoLotto, Lotto(setOf(7, 8, 9, 10, 11, 12)), 45)
 
         assertThat(winnerHistory.contains(Prize.NOTHING)).isTrue()
     }
 
     @Test
     fun `5천원 당첨`() {
-        val winnerHistory = store.drawLottoNumber(Lotto(setOf(1, 2, 3, 14, 15, 16)), 45)
+        val winnerHistory = Store.drawLottoNumber(buyer.autoLotto, Lotto(setOf(1, 2, 3, 14, 15, 16)), 45)
 
         assertThat(winnerHistory.contains(Prize.FOURTH)).isTrue()
     }
 
     @Test
     fun `5만원 당첨`() {
-        val winnerHistory = store.drawLottoNumber(Lotto(setOf(1, 2, 3, 4, 15, 16)), 45)
+        val winnerHistory = Store.drawLottoNumber(buyer.autoLotto, Lotto(setOf(1, 2, 3, 4, 15, 16)), 45)
 
         assertThat(winnerHistory.contains(Prize.THIRD)).isTrue()
     }
 
     @Test
     fun `150만원 당첨`() {
-        val winnerHistory = store.drawLottoNumber(Lotto(setOf(1, 2, 3, 4, 5, 16)), 45)
+        val winnerHistory = Store.drawLottoNumber(buyer.autoLotto, Lotto(setOf(1, 2, 3, 4, 5, 16)), 45)
 
         assertThat(winnerHistory.contains(Prize.SECOND)).isTrue()
     }
 
     @Test
     fun `3천만원 당첨`() {
-        val winnerHistory = store.drawLottoNumber(Lotto(setOf(1, 2, 3, 4, 5, 16)), 6)
+        val winnerHistory = Store.drawLottoNumber(buyer.autoLotto, Lotto(setOf(1, 2, 3, 4, 5, 16)), 6)
 
         assertThat(winnerHistory.contains(Prize.BETWEEN_FIRST_AND_SECOND)).isTrue()
     }
 
     @Test
     fun `20억 당첨`() {
-        val winnerHistory = store.drawLottoNumber(Lotto(setOf(1, 2, 3, 4, 5, 6)), 45)
+        val winnerHistory = Store.drawLottoNumber(buyer.autoLotto, Lotto(setOf(1, 2, 3, 4, 5, 6)), 45)
 
         assertThat(winnerHistory.contains(Prize.FIRST)).isTrue()
     }
 
     @Test
     fun `수익률 조회`() {
-        val winnerHistory = store.drawLottoNumber(Lotto(setOf(1, 2, 3, 14, 15, 16)), 45)
+        val winnerHistory = Store.drawLottoNumber(buyer.autoLotto, Lotto(setOf(1, 2, 3, 14, 15, 16)), 45)
 
-        assertThat(store.getRateOfReturn(14_000, winnerHistory)).isEqualTo(0.71)
+        assertThat(Store.getRateOfReturn(14_000, winnerHistory)).isEqualTo(0.71)
     }
 
     @Test
     fun `150만원과 3천만원 둘다 당첨될 경우`() {
-        val winnerHistory = store.drawLottoNumber(Lotto(setOf(1, 2, 3, 4, 5, 16)), 45)
+        val winnerHistory = Store.drawLottoNumber(buyer.autoLotto, Lotto(setOf(1, 2, 3, 4, 5, 16)), 45)
 
         assertThat(winnerHistory.contains(Prize.SECOND) && winnerHistory.contains(Prize.BETWEEN_FIRST_AND_SECOND)).isTrue()
     }
