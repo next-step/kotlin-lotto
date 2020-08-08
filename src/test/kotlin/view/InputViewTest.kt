52816b043abff17645f78c3da5640163ee73a860
Jiwhun Kim <jiwhun.kim@outlook.com>
2020-08-08 15:46:22 +0900

    feature(Lotto-step3): InputView, ResultView 에 대한 test case suite 를 추가하고 이에 대한 class 를 생성하다

---
+++ src/test/kotlin/view/InputViewTest.kt
@@ -0,0 +1,26 @@
+package view
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class InputViewTest {
+    @Test
+    @DisplayName("구입금액을 입력 받는다")
+    fun `inputMoney`() {
+    }
+
+    @Test
+    @DisplayName("당첨 번호를 입력 받는다")
+    fun `inputPrizeLotto`() {
+    }
+
+    @Test
+    @DisplayName("보너스 번호를 입력 받는다")
+    fun `inputBonusNumber`() {
+    }
+
+    @Test
+    @DisplayName("숫자, 쉼표, 스페이스가 아닌 값을 입력 받으면 exception 을 반환한다")
+    fun `invalidInputThrowException`() {
+    }
+}
