ab09a9cff2c3ec8d4a6afb3072fee8a94bada419
Jiwhun Kim <jiwhun.kim@outlook.com>
2020-08-08 12:13:05 +0900

    feature(Lotto-step3): 구매 금액을 입력 받으면 구매 금액 / 로또 금액만큼 로또 목록이 생성된다

--- src/main/kotlin/LottoGame.kt
+++ src/main/kotlin/LottoGame.kt
@@ -1,1 +1,19 @@
-class LottoGame
+class LottoGame {
+    fun buy(money: Int): List<Lotto> {
+        val list = mutableListOf<Lotto>()
+
+        repeat(money / LOTTO_PRICE) {
+            val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+            list.add(lotto)
+        }
+
+        return list.toList()
+    }
+
+    class Lotto(val list: List<Int> = listOf(1, 2, 3, 4, 5, 6))
+
+    companion object {
+
+        const val LOTTO_PRICE = 1_000
+    }
+}
--- src/test/kotlin/LottoGameTest.kt
+++ src/test/kotlin/LottoGameTest.kt
@@ -1,3 +1,4 @@
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
@@ -11,6 +12,10 @@ class LottoGameTest {
     @Test
     @DisplayName("구매 금액을 입력 받으면 구매 금액 / 로또 금액만큼 로또 목록이 생성된다")
     fun `createLottoList`() {
+        val inputMoney = 14_000
+        val lottoGame = LottoGame()
+        val list = lottoGame.buy(inputMoney)
+        assertThat(list.size).isEqualTo(inputMoney / LottoGame.LOTTO_PRICE)
     }
 
     @Test
