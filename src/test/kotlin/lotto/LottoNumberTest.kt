9b9082cdc5c5310c0454fb6208d42bd7830d205c
BeokBeok <kekemusa37@nate.com>
2020-08-08 23:20:03 +0900

    feat(test) : [추가] 숫자 타입이 아닌 임의의 값을 전달하는 경우 TC

--- src/test/kotlin/lotto/LottoNumberTest.kt
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -11,6 +11,10 @@ class LottoNumberTest {
 
         assertThat(lottoNumber.number).isEqualTo(1)
     }
+
+    @Test
+    fun `숫자 타입이 아닌 임의의 값을 전달하는 경우`() {
+    }
 }
 
 class LottoNumber(val number: Int) {
