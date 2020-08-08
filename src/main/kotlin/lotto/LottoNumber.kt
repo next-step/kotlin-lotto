bf24bfb6917f9a9696e17c7e24f10b9fb00edd28
포비 <pobi@woowahan.com>
2020-08-08 11:32:55 +0900

    refactor: change constructor visibility public to private

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -5,7 +5,7 @@ import java.lang.IllegalArgumentException
 const val LOTTO_MIN_NUMBER = 1
 const val LOTTO_MAX_NUMBER = 45
 
-data class LottoNumber(private val number: Int) {
+data class LottoNumber private constructor(private val number: Int) {
     companion object {
         private val LOTTO_NUMBERS = (LOTTO_MIN_NUMBER..LOTTO_MAX_NUMBER).map { it to LottoNumber(it) }.toMap()
 
