65c2f7e0bf26721d6c3be448b79aba6a995faa0e
BeokBeok <kekemusa37@nate.com>
2020-08-08 23:47:56 +0900

    feat(test) : [수정] 로또 생성 범위에 속하지 않은 값을 입력한 경우 TC (성공)

--- src/test/kotlin/lotto/LottoNumberTest.kt
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -22,6 +22,9 @@ class LottoNumberTest {
 
     @Test
     fun `로또 생성 범위에 속하지 않은 값을 입력한 경우`() {
+        assertThatIllegalArgumentException().isThrownBy {
+            LottoNumber("46")
+        }
     }
 }
 
@@ -31,12 +34,15 @@ class LottoNumber(val number: Int) {
 
     companion object {
         private val IS_NUMBER_REGEX = "(^[0-9]*\$)".toRegex()
+        private val NUMBER_RANGE = 1..45
 
         fun convertTo(stringOfNumber: String): Int {
-            require(stringOfNumber.matches(IS_NUMBER_REGEX)) {
-                "숫자 이외의 값이 입력되었습니다."
-            }
-            return stringOfNumber.toInt()
+            require(stringOfNumber.matches(IS_NUMBER_REGEX)) { "숫자 이외의 값이 입력되었습니다." }
+
+            val number = stringOfNumber.toInt()
+            require(NUMBER_RANGE.contains(number)) { "1 - 45 까지의 숫자만 가능합니다." }
+
+            return number
         }
     }
 }
