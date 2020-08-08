33eaf293fbb0c1f0d447fd2d15c11bdfa946233b
BeokBeok <kekemusa37@nate.com>
2020-08-08 23:18:25 +0900

    feat(test) : [수정] String 타입 숫자를 Int 숫자로 변환 TC (성공)

--- src/test/kotlin/lotto/LottoNumberTest.kt
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -1,10 +1,19 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class LottoNumberTest {
 
     @Test
     fun `String 타입 숫자를 Int 숫자로 변환`() {
+        val lottoNumber = LottoNumber("1")
+
+        assertThat(lottoNumber.number).isEqualTo(1)
     }
+}
+
+class LottoNumber(val number: Int) {
+
+    constructor(stringOfNumber: String) : this(stringOfNumber.toInt())
 }
