6c238ed9ebe7b6aff50747dc9d353d31d23f74e5
Jonny-Cho <andante2183@gmail.com>
2020-08-09 14:23:55 +0900

    delete all

--- src/main/kotlin/lotto/domain/LottoMarket.kt
+++
@@ -1,9 +0,0 @@
-package lotto.domain
-
-import lotto.strategy.Strategy
-
-class LottoMarket(private val strategy: Strategy) {
-    fun buyLotto(count: Int) = getLottos(count)
-
-    private fun getLottos(count: Int): List<Lotto> = List(count) { Lotto(strategy) }
-}
