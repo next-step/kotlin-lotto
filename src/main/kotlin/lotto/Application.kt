ebc42d0206589fb9d6930fc56cfd927f69a9b5e5
zion830 <zion830@honglab.org>
2020-08-10 00:59:16 +0900

    feat(lotto): 보너스 번호 입력받도록 View 수정

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,7 @@
 package lotto
 
-import lotto.domain.LottoTicket
+import lotto.domain.LottoNumber
+import lotto.domain.WinningLottoTicket
 import lotto.domain.generator.ManualLottoGenerator
 import lotto.domain.selling.LottoPrizeExchanger
 import lotto.domain.selling.Payment
@@ -14,30 +15,30 @@ object Application {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val result = processUserPayment()
-        var manualWinningNumbers: LottoTicket? = null
-        while (manualWinningNumbers == null) {
-            manualWinningNumbers = processWinningNumbers()
+        val paymentResult = inputUserPayment()
+        var winningLotto: WinningLottoTicket? = null
+
+        while (winningLotto == null) {
+            winningLotto = inputWinningLotto()
         }
-        processResult(result, manualWinningNumbers)
+
+        val exchangeResult = LottoPrizeExchanger.exchange(paymentResult, winningLotto)
+        ResultView.printExchangeResult(exchangeResult, exchangeResult.rateOfReturn)
     }
 
-    private fun processUserPayment(): PaymentResult {
+    private fun inputUserPayment(): PaymentResult {
         val payment = Payment(InputView.readMoney { seller.isAcceptable(it) }.toInt())
         val result = seller.processPayment(payment)
         ResultView.printPaymentResult(result)
         return result
     }
 
-    private fun processWinningNumbers(): LottoTicket? = try {
-        ManualLottoGenerator(InputView.readWinningNumbers()).execute()
+    private fun inputWinningLotto(): WinningLottoTicket? = try {
+        val lottoTicket = ManualLottoGenerator(InputView.readWinningNumbers()).execute()
+        val bonus = LottoNumber(InputView.readBonusNumber())
+        WinningLottoTicket(lottoTicket, bonus)
     } catch (e: IllegalArgumentException) {
         ResultView.printInvalidLottoNumbers()
         null
-    }
-
-    private fun processResult(paymentResult: PaymentResult, winningLotto: LottoTicket) {
-        val exchangeResult = LottoPrizeExchanger.exchange(paymentResult, winningLotto)
-        ResultView.printExchangeResult(exchangeResult, exchangeResult.rateOfReturn)
     }
 }
--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,16 +3,19 @@ package lotto.view
 object InputView {
     private const val INPUT_MONEY = "구입금액을 입력해 주세요."
     private const val INPUT_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요."
+    private const val INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
     private const val ERR_INVALID_INPUT = "유효한 입력이 아닙니다. 다시 입력해주세요."
 
-    fun readMoney(isValid: (value: String) -> Boolean) = readValue(isValid, INPUT_MONEY)
+    fun readMoney(isValid: (value: String) -> Boolean) = readValue(titleMsg = INPUT_MONEY, isValid = isValid)
 
-    fun readWinningNumbers() = readValue({ true }, INPUT_WINNING_NUMBERS)
+    fun readWinningNumbers() = readValue(INPUT_WINNING_NUMBERS)
+
+    fun readBonusNumber() = readValue(INPUT_BONUS_NUMBER)
 
     private fun readValue(
-        isValid: (value: String) -> Boolean,
         titleMsg: String,
-        errMsg: String = ERR_INVALID_INPUT
+        errMsg: String = ERR_INVALID_INPUT,
+        isValid: (value: String) -> Boolean = { true }
     ): String {
         println(titleMsg)
         var string = readLine()
