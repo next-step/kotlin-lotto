739e20d14f7e2a7870672c8d738d32930d4d68af
Jonny-Cho <andante2183@gmail.com>
2020-08-09 15:21:31 +0900

    refactor(Lotto,LottoNumber): change to const value

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,12 +2,17 @@ package lotto
 
 import stringAddCalculator.LottoNumber
 
-class Lotto(vararg numbers: Int){
+class Lotto(vararg numbers: Int) {
     val lottoNumbers = numbers.map {
         LottoNumber(it)
     }.toSet()
 
     init {
-        require(lottoNumbers.size == 6){ "로또는 중복되지 않은 6개의 숫자로 생성할 수 있습니다." }
+        require(lottoNumbers.size == VALID_LOTTO_NUMBER) { INVALID_LOTTO_NUMBER_MESSAGE }
+    }
+
+    companion object {
+        private const val VALID_LOTTO_NUMBER = 6
+        private const val INVALID_LOTTO_NUMBER_MESSAGE = "로또는 중복되지 않은 6개의 숫자로 생성할 수 있습니다."
     }
 }
--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -2,8 +2,14 @@ package stringAddCalculator
 
 data class LottoNumber(val number: Int) {
     init {
-        require(number in 1..45) { "로또는 1~45사이의 숫자만 생성가능합니다." }
+        require(number in MIN_NUMBER..MAX_NUMBER) { INVALID_MESSAGE }
     }
 
     override fun toString() = "$number"
+
+    companion object {
+        private const val MIN_NUMBER = 1
+        private const val MAX_NUMBER = 45
+        private const val INVALID_MESSAGE = "로또는 1~45사이의 숫자만 생성가능합니다."
+    }
 }
