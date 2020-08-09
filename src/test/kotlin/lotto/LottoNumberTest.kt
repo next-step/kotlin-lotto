4bd2bec5e1563a35cb3fbd5d1147a5a0ed792d49
Jonny-Cho <andante2183@gmail.com>
2020-08-09 14:48:52 +0900

    test(LottoNumberTest): add LottoNumberTest

---
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -0,0 +1,39 @@
+package lotto
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import stringAddCalculator.LottoNumber
+import java.lang.IllegalArgumentException
+
+internal class LottoNumberTest {
+
+    @Test
+    fun `0 이하의 숫자는 LottoNumber로 사용할 수 없다`() {
+        assertThatThrownBy { LottoNumber(0) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("로또는 1~45사이의 숫자만 생성가능합니다.")
+    }
+
+    @Test
+    fun `46 이상의 숫자는 LottoNumber로 사용할 수 없다`() {
+        assertThatThrownBy { LottoNumber(46) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("로또는 1~45사이의 숫자만 생성가능합니다.")
+    }
+
+    @Test
+    fun `1 ~ 45사이의 숫자는 LottoNumber로 사용할 수 있다`() {
+        for(i in 1..45){
+            assertThat(LottoNumber(i)).isEqualTo(LottoNumber(i))
+        }
+    }
+
+    @Test
+    fun testToString() {
+        val actual = LottoNumber(1)
+        assertThat(actual.toString()).isEqualTo("1")
+    }
+}
