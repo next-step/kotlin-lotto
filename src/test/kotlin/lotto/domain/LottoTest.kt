760bab13536d04cded4e802f22ee98eeead5a789
bky373 <bo373@naver.com>
2020-08-09 21:05:36 +0900

    test(lotto): rename normalNumbers to vaildNumbers in LottoTest.kt

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,7 +1,7 @@
 package lotto.domain
 
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
@@ -12,7 +12,7 @@ import java.lang.IllegalArgumentException
 class LottoTest {
     private lateinit var numbersWithLessSize: List<LottoNumber>
     private lateinit var numbersWithDuplication: List<LottoNumber>
-    private lateinit var normalNumbers: List<LottoNumber>
+    private lateinit var validNumbers: List<LottoNumber>
     private lateinit var lotto: Lotto
     private lateinit var winningNumbers: Lotto
     private lateinit var winningLotto: WinningLotto
@@ -28,7 +28,7 @@ class LottoTest {
             LottoNumber(4),
             LottoNumber(5)
         )
-        normalNumbers = listOf(
+        validNumbers = listOf(
             LottoNumber(1),
             LottoNumber(2),
             LottoNumber(3),
@@ -46,14 +46,14 @@ class LottoTest {
                 LottoNumber(30)
             )
         )
-        lotto = Lotto(normalNumbers)
+        lotto = Lotto(validNumbers)
         winningLotto = WinningLotto(winningNumbers, LottoNumber(45))
     }
 
     @DisplayName("숫자의 개수가 6개인지 확인한다")
     @Test
     fun `numbers size`() {
-        Assertions.assertThatThrownBy {
+        assertThatThrownBy {
             // when
             Lotto(numbersWithLessSize)
 
@@ -65,7 +65,7 @@ class LottoTest {
     @DisplayName("숫자가 중복되는지 확인한다")
     @Test
     fun `numbers duplication`() {
-        Assertions.assertThatThrownBy {
+        assertThatThrownBy {
             // when
             Lotto(numbersWithDuplication)
 
@@ -84,9 +84,9 @@ class LottoTest {
         assertThat(countOfMatch).isEqualTo(3)
     }
 
-    @DisplayName("로또번호가 다른 한 개의 숫자를 포함하는지의 여부를 반환한다")
+    @DisplayName("로또번호가 특정 숫자를 포함하는지의 여부를 반환한다")
     @Test
-    fun `contains`() {
+    fun contains() {
         // when
         val hasNumber = lotto.contains(LottoNumber(1))
         val hasNoSuchNumber = lotto.contains(LottoNumber(45))
