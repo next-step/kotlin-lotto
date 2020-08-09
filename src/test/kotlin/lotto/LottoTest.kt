2b0cd63cbffb9a344df4d9a7e885ab771fe43d8e
BeokBeok <kekemusa37@nate.com>
2020-08-09 15:32:59 +0900

    feat(lotto) : [수정] LottoNumberGroup 을 Lotto 로 통합

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,13 +1,17 @@
 package lotto
 
 import lotto.model.Lotto
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class LottoTest {
-    @Test
-    fun `로또 형식 체크`() {
-        val lotto = Lotto(setOf(1, 2, 3, 4, 5, 6))
-        assertThat(lotto.numbers.size).isEqualTo(6)
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2,3,4,5,6,7", "1,2,3,4,5"])
+    fun `로또 번호 6자리 유효성 체크`(input: String) {
+        Assertions.assertThatIllegalArgumentException().isThrownBy {
+            Lotto.of(input)
+        }
     }
 }
