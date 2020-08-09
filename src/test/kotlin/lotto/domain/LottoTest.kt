f00be39bffcdba82fa552d1c9b42d711bf67a1ae
bky373 <bo373@naver.com>
2020-08-09 11:15:41 +0900

    test(lotto): apply utility methods to LottoTest.kt

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,84 +1,60 @@
 package lotto.domain
 
-import org.assertj.core.api.Assertions
+import lotto.util.Assertions.assertLotto
+import lotto.util.createLotto
+import lotto.util.createLottoNumbers
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import java.lang.IllegalArgumentException
 
 class LottoTest {
-    private lateinit var numbersWithLessSize: List<LottoNumber>
-    private lateinit var numbersWithDuplication: List<LottoNumber>
+    private lateinit var lessSizeNumbers: List<LottoNumber>
+    private lateinit var duplicateNumbers: List<LottoNumber>
     private lateinit var normalNumbers: List<LottoNumber>
-    private lateinit var lotto: Lotto
+    private lateinit var normalLotto: Lotto
     private lateinit var winningLotto: WinningLotto
 
     @BeforeEach
     fun `set up`() {
-        numbersWithLessSize = listOf(LottoNumber(1))
-        numbersWithDuplication = listOf(
-            LottoNumber(1),
-            LottoNumber(1),
-            LottoNumber(2),
-            LottoNumber(3),
-            LottoNumber(4),
-            LottoNumber(5)
-        )
-        normalNumbers = listOf(
-            LottoNumber(1),
-            LottoNumber(2),
-            LottoNumber(3),
-            LottoNumber(4),
-            LottoNumber(5),
-            LottoNumber(6)
-        )
-        lotto = Lotto(normalNumbers)
-        winningLotto = WinningLotto(lotto, LottoNumber(10))
+        lessSizeNumbers = createLottoNumbers(listOf(1, 2))
+        duplicateNumbers = createLottoNumbers(listOf(1, 1, 3, 4, 5, 6))
+        normalNumbers = createLottoNumbers(listOf(1, 2, 3, 4, 5, 6))
+
+        normalLotto = createLotto(normalNumbers)
+        winningLotto = WinningLotto(normalLotto, LottoNumber(10))
     }
 
     @DisplayName("숫자의 개수가 6개인지 확인한다")
     @Test
     fun `numbers size`() {
-        Assertions.assertThatThrownBy {
-            // when
-            Lotto(numbersWithLessSize)
-
-            // then
-        }.isInstanceOf(IllegalArgumentException::class.java)
-            .hasMessageContaining("Invalid Lotto Numbers Size : 1 should be 6")
+        assertLotto(lessSizeNumbers).validateSize()
     }
 
     @DisplayName("숫자가 중복되는지 확인한다")
     @Test
     fun `numbers duplication`() {
-        Assertions.assertThatThrownBy {
-            // when
-            Lotto(numbersWithDuplication)
-
-            // then
-        }.isInstanceOf(IllegalArgumentException::class.java)
-            .hasMessageContaining("Lotto Numbers Duplication : 1 occurred")
+        assertLotto(duplicateNumbers).validateDuplication()
     }
 
     @DisplayName("당첨번호와 일치하는 숫자 개수를 반환한다")
     @Test
     fun `match count`() {
         // when
-        val countOfMatch = lotto.countOfMatch(winningLotto)
+        val count = normalLotto.countOfMatch(winningLotto)
 
         // then
-        assertThat(countOfMatch).isEqualTo(6)
+        assertThat(count).isEqualTo(6)
     }
 
     @DisplayName("다른 숫자를 포함하는지의 여부를 반환한다")
     @Test
     fun `contains`() {
         // when
-        val hasNumber = lotto.contains(LottoNumber(1))
-        val hasNoSuchNumber = lotto.contains(LottoNumber(45))
+        val hasNumber = normalLotto.contains(LottoNumber(1))
+        val hasNoSuchNumber = normalLotto.contains(LottoNumber(45))
 
         // then
         assertTrue(hasNumber)
