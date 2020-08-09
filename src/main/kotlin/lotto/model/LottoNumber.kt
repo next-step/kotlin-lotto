ab268fd5d85d4dafd2d02ccb1180de0c579c57bb
BeokBeok <kekemusa37@nate.com>
2020-08-09 15:35:00 +0900

    feat(lotto) : [수정] 숫자에 빈 값이 전달될 경우 예외처리

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -10,12 +10,12 @@ class LottoNumber(val number: Int) : Comparable<LottoNumber> {
 
     companion object {
         private val IS_NUMBER_REGEX = "(^[0-9]*\$)".toRegex()
-        private val NUMBER_RANGE = 1..45
+        val NUMBER_RANGE = 1..45
 
         fun convertTo(stringOfNumber: String): Int {
-            require(stringOfNumber.matches(IS_NUMBER_REGEX)) { "숫자 이외의 값이 입력되었습니다." }
+            require(stringOfNumber.trim().matches(IS_NUMBER_REGEX)) { "숫자 이외의 값이 입력되었습니다." }
 
-            val number = stringOfNumber.toInt()
+            val number = stringOfNumber.trim().toInt()
             require(NUMBER_RANGE.contains(number)) { "1 - 45 까지의 숫자만 가능합니다." }
 
             return number
