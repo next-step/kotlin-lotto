749940ebaf2dee7582d673a3a18ef91e1fe82378
LeeYounghyeon <leey9494@icloud.com>
2020-08-10 02:13:58 +0900

    feat(lotto): 당첨번호를 입력한다

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -1,8 +1,23 @@
 package lotto
 
 object InputView {
+    private const val COMMA = ","
     fun inputAmountOfMoney(): Int {
         println("구입금액을 입력해 주세요.")
         return readLine()!!.toInt()
+    }
+
+    fun inputWinningNumbers(): String {
+        println()
+        println("지난 주 당첨 번호를 입력해 주세요.")
+        val winningNumbers = readLine()!!
+        validateDelimiter(winningNumbers)
+        return winningNumbers
+    }
+
+    private fun validateDelimiter(winningNumbers: String) {
+        if (!winningNumbers.contains(COMMA)) {
+            throw IllegalArgumentException(",는 반드시 입력하세요.")
+        }
     }
 }
