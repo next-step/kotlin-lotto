42f3fad700a573d518fb046749635db167bbcce6
zion830 <zion830@honglab.org>
2020-08-09 21:49:04 +0900

    refactor(lotto): 소수점 자리수 상수로 분리

--- src/main/kotlin/lotto/domain/selling/ExchangeResult.kt
+++ src/main/kotlin/lotto/domain/selling/ExchangeResult.kt
@@ -7,5 +7,9 @@ data class ExchangeResult(val paymentResult: PaymentResult, val details: Map<Pri
     private val totalPrizeMoney = details.keys.sumBy { Prize(it.matchCount).prizeMoney }
 
     val rateOfReturn: BigDecimal =
-        BigDecimal(totalPrizeMoney).divide(BigDecimal(paymentResult.money), 2, RoundingMode.DOWN)
+        BigDecimal(totalPrizeMoney).divide(BigDecimal(paymentResult.money), EXCHANGE_SCALE, RoundingMode.DOWN)
+
+    companion object {
+        private const val EXCHANGE_SCALE = 2
+    }
 }
