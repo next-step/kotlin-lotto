2f95e7c361783a264e30291e62f86121f7618624
Jonny-Cho <andante2183@gmail.com>
2020-08-10 23:59:44 +0900

    refactor(Rank): add asList method

--- src/main/kotlin/lotto/domain/Rank.kt
+++ src/main/kotlin/lotto/domain/Rank.kt
@@ -1,18 +1,24 @@
 package lotto.domain
 
 enum class Rank(val countOfMatch: Int, val winningMoney: Money, val matchBonus: Boolean = false) {
-    FIRST(6, Money(2_000_000_000)),
-    SECOND(5, Money(30_000_000), true),
-    THIRD(5, Money(1_500_000)),
-    FOURTH(4, Money(50_000)),
+    MISS(0, Money(0)),
     FIFTH(3, Money(5_000)),
-    MISS(0, Money(0));
+    FOURTH(4, Money(50_000)),
+    THIRD(5, Money(1_500_000)),
+    SECOND(5, Money(30_000_000), true),
+    FIRST(6, Money(2_000_000_000));
 
     fun prizeByCount(count: Int): Money = winningMoney * count
 
+    override fun toString(): String {
+        return "${countOfMatch}$COUNT_SUFFIX ($winningMoney)"
+    }
+
     companion object {
+        private const val COUNT_SUFFIX = "개 일치"
         fun rank(countOfMatch: Int, matchBonus: Boolean = false): Rank {
             return values().find { it.countOfMatch == countOfMatch && it.matchBonus == matchBonus } ?: MISS
         }
+        fun asList() = values().filter { it != MISS }
     }
 }
--- src/test/kotlin/lotto/domain/RankTest.kt
+++ src/test/kotlin/lotto/domain/RankTest.kt
@@ -1,16 +1,22 @@
-// package lotto.domain
-//
-// import org.assertj.core.api.Assertions.assertThat
-// import org.junit.jupiter.api.Test
-//
-// class RankTest {
-//     @Test
-//     fun rankTest() {
-//         assertThat(Rank.valueOf(6)).isEqualTo(Rank.FIRST)
-//         assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND)
-//         assertThat(Rank.valueOf(5)).isEqualTo(Rank.THIRD)
-//         assertThat(Rank.valueOf(4)).isEqualTo(Rank.FOURTH)
-//         assertThat(Rank.valueOf(3)).isEqualTo(Rank.FIFTH)
-//         assertThat(Rank.valueOf(0)).isEqualTo(Rank.MISS)
-//     }
-// }
+package lotto.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class RankTest {
+    @Test
+    fun rank() {
+        assertThat(Rank.rank(6, false)).isEqualTo(Rank.FIRST)
+        assertThat(Rank.rank(5, true)).isEqualTo(Rank.SECOND)
+        assertThat(Rank.rank(5, false)).isEqualTo(Rank.THIRD)
+        assertThat(Rank.rank(4, false)).isEqualTo(Rank.FOURTH)
+        assertThat(Rank.rank(3, false)).isEqualTo(Rank.FIFTH)
+        assertThat(Rank.rank(2, false)).isEqualTo(Rank.MISS)
+    }
+
+    @Test
+    fun prizeByCount() {
+        val prize = Rank.FIFTH.prizeByCount(3)
+        assertThat(prize).isEqualTo(Money(15000))
+    }
+}
