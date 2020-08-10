4a7539f602f260b464ec1bebe9d0cf20ec4f199c
Jonny-Cho <andante2183@gmail.com>
2020-08-10 23:21:54 +0900

    refactor(Lotto): add match, contains, of methods

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,32 +1,56 @@
-package lotto
+package lotto.domain
 
-import stringAddCalculator.LottoNumber
-
-class Lotto(vararg numbers: Int) {
-    val lottoNumbers = numbers.map {
-        LottoNumber(it)
-    }.toSet()
-
+class Lotto private constructor(private val numbers: Set<LottoNumber>) {
     init {
-        require(numbers.size == VALID_LOTTO_NUMBER) { INVALID_SIZE_MESSAGE }
-        require(lottoNumbers.size == VALID_LOTTO_NUMBER) { DUPLICATE_MESSAGE }
+        require(numbers.size == VALID_LOTTO_NUMBER) { INVALID_MESSAGE }
     }
+
+    fun match(other: Lotto): Int {
+        return numbers.filter { other.contains(it) }.count()
+    }
+
+    fun contains(number: LottoNumber): Boolean = numbers.contains(number)
+
+    fun contains(number: Int): Boolean = contains(LottoNumber.of(number))
 
     companion object {
         private const val VALID_LOTTO_NUMBER = 6
-        const val INVALID_SIZE_MESSAGE = "로또는 6개의 숫자로 생성할 수 있습니다."
-        const val DUPLICATE_MESSAGE = "로또는 중복되지 않은 숫자로만 생성할 수 있습니다."
+        private const val LOTTO_DELIMITER = ","
+        const val INVALID_MESSAGE = "로또는 중복되지 않은 ${VALID_LOTTO_NUMBER}개의 숫자로 생성할 수 있습니다."
+
+        fun of(vararg numbers: Int): Lotto {
+            require(numbers.size == VALID_LOTTO_NUMBER) { INVALID_MESSAGE }
+            val lottoNumbers = numbers.map { LottoNumber.of(it) }.sortedBy { it.number }.toSet()
+            return Lotto(lottoNumbers)
+        }
+
+        fun of(numbers: List<Int>): Lotto {
+            val lottoNumbers = numbers.map { LottoNumber.of(it) }.sortedBy { it.number }.toSet()
+            return Lotto(lottoNumbers)
+        }
+
+        fun ofComma(value: String): Lotto {
+            val numbers = value.split(LOTTO_DELIMITER).map { it.trim().toInt() }
+            return Lotto(numbers.map { LottoNumber.of(it) }.toSet())
+        }
     }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
+
         other as Lotto
-        if (lottoNumbers != other.lottoNumbers) return false
+
+        if (numbers != other.numbers) return false
+
         return true
     }
 
     override fun hashCode(): Int {
-        return lottoNumbers.hashCode()
+        return numbers.hashCode()
+    }
+
+    override fun toString(): String {
+        return "$numbers\n"
     }
 }
--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,40 +1,44 @@
-package lotto
+package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
-import stringAddCalculator.LottoNumber
 
 internal class LottoTest {
 
     @Test
     fun `중복된 숫자로 Lotto를 만들 수 없다`() {
-        assertThatThrownBy { Lotto(1, 2, 3, 4, 5, 5) }
+        assertThatThrownBy { Lotto.of(1, 2, 3, 4, 5, 5) }
             .isInstanceOf(IllegalArgumentException::class.java)
-            .hasMessage(Lotto.DUPLICATE_MESSAGE)
+            .hasMessage(Lotto.INVALID_MESSAGE)
     }
 
     @Test
     fun `6개의 숫자가 아니라면 Lotto를 만들 수 없다`() {
-        assertThatThrownBy { Lotto(1, 2, 3, 4, 5) }
+        assertThatThrownBy { Lotto.of(1, 2, 3, 4, 5) }
             .isInstanceOf(IllegalArgumentException::class.java)
-            .hasMessage(Lotto.INVALID_SIZE_MESSAGE)
-        assertThatThrownBy { Lotto(1, 2, 3, 4, 5, 6, 7) }
+            .hasMessage(Lotto.INVALID_MESSAGE)
+        assertThatThrownBy { Lotto.of(1, 2, 3, 4, 5, 6, 7) }
             .isInstanceOf(IllegalArgumentException::class.java)
-            .hasMessage(Lotto.INVALID_SIZE_MESSAGE)
+            .hasMessage(Lotto.INVALID_MESSAGE)
     }
 
     @Test
     fun `7개 숫자를 넣었지만 하나가 중복인경우 `() {
-        assertThatThrownBy { Lotto(1, 2, 3, 4, 5, 6, 6) }
+        assertThatThrownBy { Lotto.of(1, 2, 3, 4, 5, 6, 6) }
             .isInstanceOf(IllegalArgumentException::class.java)
-            .hasMessage(Lotto.INVALID_SIZE_MESSAGE)
+            .hasMessage(Lotto.INVALID_MESSAGE)
     }
 
     @Test
     fun `중복되지 않은 6개의 숫자로 Lotto를 만들 수 있다`() {
-        val lotto = Lotto(1, 2, 3, 4, 5, 6)
-        val expect = List(6) { i -> LottoNumber(i + 1) }.toSet()
-        assertThat(lotto.lottoNumbers).isEqualTo(expect)
+        val lotto = Lotto.of(1, 2, 3, 4, 5, 6)
+        assertThat(lotto).isEqualTo(Lotto.of(1, 2, 3, 4, 5, 6))
+    }
+
+    @Test
+    fun testToString() {
+        val lotto = Lotto.of(1, 2, 3, 4, 5, 6)
+        assertThat(lotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]\n")
     }
 }
