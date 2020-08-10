3cbc12f74d32ac3ffe4837fba1c673b018eab51b
LeeYounghyeon <leey9494@icloud.com>
2020-08-11 00:39:03 +0900

    feat(lotto): 당첨 통계를 출력해주는 기능 구현

--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -7,4 +7,15 @@ object OutputView {
             println(it)
         }
     }
+
+    fun showResults(result: List<Rank>, rateOfReturn: Double) {
+        println()
+        println("당첨 통계")
+        println("---------")
+        Rank.values().sortedDescending().filter { it.count != 0 }.forEach {
+            val value = it
+            println("${value.count}개 일치 (${value.prize}원) - ${result.filter { it == value }.count()}개")
+        }
+        println("총 수익률은 ${rateOfReturn}입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)")
+    }
 }
