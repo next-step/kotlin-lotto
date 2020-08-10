787c11e857831f710b0d3f1d00d17ba7c5557cb9
EthanChae <boschae36@gmail.com>
2020-08-10 22:42:33 +0900

    refactor(calculator) : Minimize the method and modify the name

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -7,51 +7,44 @@ private val CUSTOM_SPLITTER = "//(.)\\n(.*)".toRegex()
 private val DEFAULT_SPLITTER = listOf(",", ":")
 private const val CUSTOM_SPLITTER_LAST_LOCATION = 4
 
-class Calculator(private val numbersInput: String) {
+class Calculator(private var numberInput: String) {
     private var isCustomSplitter: Boolean = false
     private var splitters: List<String> = DEFAULT_SPLITTER
-    fun execute(): Int {
-        validateNumberInput(numbersInput)
-        setSplitters()
-        hasOnlyValidString()
-        return calculate(parsing())
+    fun calculate(): Int {
+        require(numberInput.isNotEmpty()) { "숫자를 입력해주세요." }
+        return try {
+            setSplitters()
+            validateNumberInput()
+            calculate(parsingBySplitters())
+        } catch (e: Exception) {
+            throw Exception("입력값을 확인하세요.")
+        }
     }
 
     private fun calculate(numbers: List<Number>): Int {
         return numbers.sumBy { it.toInt() }
     }
 
-    private fun validateNumberInput(numbersInput: String): String {
-        require(numbersInput.isNotEmpty()) { "숫자를 입력해주세요." }
-        return numbersInput
-    }
-
-    private fun setSplitters() {
-        if (CUSTOM_SPLITTER.matches(numbersInput)) {
-            splitters = listOf(numbersInput[CUSTOM_SPLITTER_LOCATION].toString())
-        }
-    }
-
-    private fun hasOnlyValidString() {
+    private fun validateNumberInput() {
         val pattern = StringBuilder("^[0-9")
         splitters.forEach { pattern.append(it) }
         pattern.append("]*[0-9]$")
-        val numberInput = numbersInput.substring(if (isCustomSplitter) CUSTOM_SPLITTER_LAST_LOCATION else 0)
-        if (!Pattern.compile(pattern.toString()).matcher(numberInput).matches()) {
-            throw CalculatorException("입력값을 확인하세요.")
+        require(Pattern.compile(pattern.toString()).matcher(numberInput).matches())
+    }
+
+    private fun setSplitters() {
+        if (CUSTOM_SPLITTER.find(numberInput) != null) {
+            isCustomSplitter = true
+            splitters = listOf(numberInput[CUSTOM_SPLITTER_LOCATION].toString())
+            numberInput = numberInput.substring(CUSTOM_SPLITTER_LAST_LOCATION)
         }
     }
 
-    private fun parsing(): List<Number> {
-        return try {
-            val numbersInput =
-                if (isCustomSplitter) numbersInput.substring(CUSTOM_SPLITTER_LAST_LOCATION) else numbersInput.replace(
-                    splitters[1],
-                    splitters[0]
-                )
-            numbersInput.split(splitters[0], splitters[1]).map { it.toInt() }
-        } catch (e: CalculatorException) {
-            throw CalculatorException("입력값을 확인하세요.")
+    private fun parsingBySplitters(): List<Number> {
+        return if (isCustomSplitter) {
+            numberInput.split(splitters[0]).map { it.toInt() }
+        } else {
+            numberInput.split(splitters[0], splitters[1]).map { it.toInt() }
         }
     }
 }
--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -13,8 +13,8 @@ class CalculatorTest {
     @DisplayName("모든 숫자값을 합")
     @Test
     fun checkCalculate() {
-        val calculator = Calculator("1,2:3")
-        assertThat(calculator.execute()).isEqualTo(6)
+        val calculator = Calculator("1:2,3")
+        assertThat(calculator.calculate()).isEqualTo(6)
     }
 
     @DisplayName("입력값 empty 체크")
@@ -22,7 +22,7 @@ class CalculatorTest {
     @EmptySource
     fun validateNumberInput(numberInput: String) {
         val calculator = Calculator(numberInput)
-        assertThatThrownBy { calculator.execute() }.isInstanceOf(CalculatorException::class.java)
+        assertThatThrownBy { calculator.calculate() }.isInstanceOf(IllegalArgumentException::class.java)
     }
 
     @DisplayName("입력값이 규칙에 맞지 않을 경우 체크")
@@ -30,15 +30,13 @@ class CalculatorTest {
     @ValueSource(strings = ["a", "&", "1,2,3,", ",1,2,3"])
     fun validateNumberInputError(numberInput: String) {
         val calculator = Calculator(numberInput)
-        assertThatThrownBy {
-            calculator.execute()
-        }.isInstanceOf(CalculatorException::class.java)
+        assertThatThrownBy { calculator.calculate() }.isInstanceOf(Exception::class.java)
     }
 
     @DisplayName("사용자가 입력한 문자열로 파싱")
     @Test
-    fun `execute`() {
+    fun execute() {
         val calculator = Calculator("//;\n1;2;3")
-        assertThat(calculator.execute()).isEqualTo(6)
+        assertThat(calculator.calculate()).isEqualTo(6)
     }
 }
