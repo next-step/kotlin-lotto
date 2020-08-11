b5009bc694e7f2a2a29b77dd77f0d72d5c590183
zion830 <zion830@honglab.org>
2020-08-11 21:30:51 +0900

    refactor(lotto): BigDecimal 형변환 시 toBigDecimal() 사용하기

--- src/main/kotlin/lotto/domain/selling/ExchangeResult.kt
+++ src/main/kotlin/lotto/domain/selling/ExchangeResult.kt
@@ -6,8 +6,9 @@ import java.math.RoundingMode
 data class ExchangeResult(val paymentResult: PaymentResult, val details: Map<Rank, Int>) {
     private val totalPrizeMoney = details.keys.sumBy { it.prizeMoney }
 
-    val rateOfReturn: BigDecimal =
-        BigDecimal(totalPrizeMoney).divide(BigDecimal(paymentResult.money), EXCHANGE_SCALE, RoundingMode.DOWN)
+    val rateOfReturn: BigDecimal = totalPrizeMoney.toBigDecimal().divide(
+        paymentResult.money.toBigDecimal(), EXCHANGE_SCALE, RoundingMode.DOWN
+    )
 
     companion object {
         private const val EXCHANGE_SCALE = 2
