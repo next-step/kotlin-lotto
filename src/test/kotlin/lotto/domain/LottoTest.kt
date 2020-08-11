495ac67acff022d8f4d79bf8cece3e8ff97c1fcd
kimgaeul <kimgaeul4@gmail.com>
2020-08-12 01:08:51 +0900

    feat(step3): 성격에 맞지 않는 테스트 코드 삭제

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,13 +1,11 @@
 package lotto.domain
 
-import lotto.view.InputView
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
-import org.junit.jupiter.params.provider.ValueSource
 
 class LottoTest {
     @BeforeEach
@@ -22,17 +20,6 @@ class LottoTest {
     @Test
     fun `생성한 로또 번호가 1-45 범위 안에 있는지 확인`() {
         assertThat(IntRange(1, 45).toList().containsAll(Lotto.of().numbers)).isTrue()
-    }
-
-    @ParameterizedTest
-    @ValueSource(
-        strings = [
-            "1,2,3,4,",
-            "1,2,A,4,5"
-        ]
-    )
-    fun `입력한 당첨번호가 데이터 포멧이 이상할 때 null을 반환하는지 확인`(numbers: String) {
-        assertThat(InputView.validateWinningNumber(numbers)).isNull()
     }
 
     @Test
