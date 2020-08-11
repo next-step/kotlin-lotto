4fd2ea2336fe6d5ccc21c42ae9781a0c4af18145
zion830 <zion830@honglab.org>
2020-08-11 17:15:26 +0900

    refactor(lotto): toString 선언 위치 하단으로 이동

--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -6,11 +6,11 @@ data class LottoTicket(val numbers: Set<LottoNumber>) {
         require(hasValidCount(numbers)) { "$NUMBER_COUNT 개의 중복되지 않은 번호가 필요합니다." }
     }
 
-    override fun toString() = "[${numbers.joinToString { it.value.toString() }}]"
-
     fun getMatchCount(other: WinningLottoTicket) = numbers.count { other.lottoTicket.hasNumber(it) }
 
     fun hasNumber(number: LottoNumber) = numbers.contains(number)
+
+    override fun toString() = "[${numbers.joinToString { it.value.toString() }}]"
 
     companion object {
         const val NUMBER_COUNT = 6
