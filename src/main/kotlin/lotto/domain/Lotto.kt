a82260a496d320634b96317ec4ab55743ec21c16
EthanChae <boschae36@gmail.com>
2020-08-13 00:33:10 +0900

    refactor(lotto) : Modify constructor

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,20 +3,22 @@ package lotto.domain
 const val COUNT_OF_NUMBERS = 6
 private val LOTTO_NUMBERS = (MIN_NUMBER..MAX_NUMBER)
 
-class Lotto() {
-    var numbers: Set<LottoNumber> = emptySet()
+class Lotto(private val numbers: Set<LottoNumber>) {
     var countOfMatch: Int = 0
         private set
 
     init {
-        numbers = LOTTO_NUMBERS.shuffled().subList(0, COUNT_OF_NUMBERS)
-            .sorted().map { LottoNumber(it) }.toSet()
         require(numbers.size == COUNT_OF_NUMBERS) { "중복되지 않는 6개의 숫자를 입력해주세요" }
     }
 
-    constructor(prizeNumberString: String) : this() {
-        numbers = prizeNumberString.split(",").asSequence().sorted().map { LottoNumber(it.toInt()) }.toSet()
-    }
+    constructor() : this(
+        LOTTO_NUMBERS.shuffled().subList(0, COUNT_OF_NUMBERS)
+            .sorted().map { LottoNumber(it) }.toSet()
+    )
+
+    constructor(prizeNumberString: String) : this(
+        prizeNumberString.split(",").asSequence().sorted().map { LottoNumber(it.toInt()) }.toSet()
+    )
 
     fun getPrize(lotto: Lotto): Prize {
         countOfMatch = numbers.count { lotto.numbers.contains(it) }
