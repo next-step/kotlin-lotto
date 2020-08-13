3d2c55cd1543614654414d37c85b6c1f68e03f7f
Jonny-Cho <andante2183@gmail.com>
2020-08-14 02:10:16 +0900

    refactor(main): split main

--- src/main/kotlin/lotto/Main.kt
+++ src/main/kotlin/lotto/Main.kt
@@ -1,28 +1,37 @@
 package lotto
 
 import lotto.domain.LottoTicket
+import lotto.domain.Money
 import lotto.domain.WinningLotto
 import lotto.view.InputView
 import lotto.view.ResultView
 
 fun main() {
-    // 사용자로부터 금액을 입력받는다.
-    val money = InputView.inputMoney()
+    val (money, lottoTicket) = buyLottoTicket()
+    val winningLotto = getLastWeekWinningLottoNumber()
+    checkLotto(lottoTicket, winningLotto, money)
+}
 
-    // 로또티켓을 오토로 구매한다
-    val lottoTicket = LottoTicket(money)
+private fun checkLotto(
+    lottoTicket: LottoTicket,
+    winningLotto: WinningLotto,
+    money: Money
+) {
+    val lottoResult = lottoTicket.match(winningLotto) // Model
+    ResultView.printResult(lottoResult) // View
+    val totalRate = lottoResult.calculateTotalRate(money) // Model
+    ResultView.printTotalRate(totalRate) // View
+}
 
-    // 구매 갯수와 로또 티켓 번호들을 출력한다
-    ResultView.printLottos(lottoTicket)
+private fun buyLottoTicket(): Pair<Money, LottoTicket> {
+    val money = InputView.inputMoney() // view
+    val lottoTicket = LottoTicket(money) // model
+    ResultView.printLottos(lottoTicket) // view
+    return money to lottoTicket
+}
 
-    // 사용자로부터 지난주 당첨번호를 입력받아 WinningLotto객체를 생성한다
-    val winningNumber = InputView.inputWinningNumber()
-    val bonusNumber = InputView.inputBonusNumber()
-    val winningLotto = WinningLotto(winningNumber, bonusNumber)
-
-    // 구매한 LottoTicket과 당첨번호를 확인한다
-    val lottoResult = lottoTicket.match(winningLotto)
-    ResultView.printResult(lottoResult)
-    val totalRate = lottoResult.calculateTotalRate(money)
-    ResultView.printTotalRate(totalRate)
+private fun getLastWeekWinningLottoNumber(): WinningLotto {
+    val winningNumber = InputView.inputWinningNumber() // View
+    val bonusNumber = InputView.inputBonusNumber() // View
+    return WinningLotto(winningNumber, bonusNumber) // model
 }
