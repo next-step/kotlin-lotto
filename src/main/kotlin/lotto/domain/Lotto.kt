1bf16fd5507f74dd40d349cd82aba467e34b8365
Jonny-Cho <andante2183@gmail.com>
2020-08-14 01:42:34 +0900

    refactor(Lotto): minimize duplication of constructor validation

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -2,14 +2,12 @@ package lotto.domain
 
 class Lotto private constructor(private val numbers: Set<LottoNumber>) {
     init {
-        require(numbers.size == VALID_LOTTO_NUMBER) { INVALID_MESSAGE }
+        validNumber(numbers)
     }
 
-    constructor(vararg numbers: Int) : this(numbers.map { LottoNumber.of(it) }.sortedBy { it.number }.toSet()) {
-        require(numbers.size == VALID_LOTTO_NUMBER) { INVALID_MESSAGE }
-    }
-    constructor(numbers: List<Int>) : this(numbers.map { LottoNumber.of(it) }.sortedBy { it.number }.toSet()) {
-        require(numbers.size == VALID_LOTTO_NUMBER) { INVALID_MESSAGE }
+    constructor(vararg numbers: Int) : this(numbers.toList())
+    constructor(numbers: List<Int>) : this(setLottoNumber(numbers)) {
+        validNumber(numbers)
     }
 
     fun match(other: Lotto): Int {
@@ -39,5 +37,11 @@ class Lotto private constructor(private val numbers: Set<LottoNumber>) {
     companion object {
         private const val VALID_LOTTO_NUMBER = 6
         const val INVALID_MESSAGE = "로또는 중복되지 않은 ${VALID_LOTTO_NUMBER}개의 숫자로 생성할 수 있습니다."
+        private fun setLottoNumber(numbers: List<Int>) =
+            numbers.map { LottoNumber.of(it) }.sortedBy { it.number }.toSet()
+
+        private fun validNumber(numbers: Collection<Any>) {
+            require(numbers.size == VALID_LOTTO_NUMBER) { INVALID_MESSAGE }
+        }
     }
 }
