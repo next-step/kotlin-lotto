d1505339c11fad8e45c67e01e836716b58eedb53
Jonny-Cho <andante2183@gmail.com>
2020-08-14 22:02:45 +0900

    refactor(Calculator): change if-throw to require

--- src/main/kotlin/stringAddCalculator/Number.kt
+++ src/main/kotlin/stringAddCalculator/Number.kt
@@ -2,16 +2,12 @@ package stringAddCalculator
 
 import stringAddCalculator.StringAddCalculator.Companion.ZERO
 
-data class Number(private val number: Int) {
+data class Number(val number: Int) {
     init {
-        if (isNegativeNumber(number)) throw RuntimeException("$NEGATIVE_NUMBER_THROW_MESSAGE $number")
+        require(number >= ZERO) { "$NEGATIVE_NUMBER_THROW_MESSAGE $number" }
     }
 
-    fun getNumber() = number
-
-    fun plus(targetNumber: Number) = Number(number.plus(targetNumber.number))
-
-    private fun isNegativeNumber(number: Int) = number < ZERO
+    fun plus(targetNumber: Number) = Number(number + targetNumber.number)
 
     companion object {
         private const val NEGATIVE_NUMBER_THROW_MESSAGE = "can not use nagative number"
