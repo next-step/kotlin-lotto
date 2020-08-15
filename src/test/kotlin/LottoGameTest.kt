fb69abe7ec137d27ba1419002bde93de3a2dcb0f
Jiwhun Kim <jiwhun.kim@outlook.com>
2020-08-15 18:29:14 +0900

    feature(Lotto-step4): 당첨에 대한 테스트 코드를 winningLottoTest 로 이동시키다 로또 자동 생성, 수동 생성 수에 대한 테스트 코드를 추가하다

--- src/test/kotlin/LottoGameTest.kt
+++ src/test/kotlin/LottoGameTest.kt
@@ -22,54 +22,11 @@ class LottoGameTest {
     }
 
     @Test
-    @DisplayName("로또 당첨 번호를 입력 받을 수 있다")
-    fun `inputWinningLotto`() {
-        val lottoNumber = setOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
-        val winningLotto = WinningLotto(lottoNumber, 7)
-        assertThat(winningLotto).isNotNull
-    }
-
-    @Test
-    @DisplayName("로또와 당첨 번호 사이의 matchResult를 알 수 있다")
-    fun `getMatchCount`() {
-        val lottoNumber = setOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
-        val winningLotto = WinningLotto(lottoNumber, 7)
-        val lotto = Lotto(setOf(4, 5, 6, 7, 8, 9).map { LottoNumber.from(it) }.toSet())
-        assertThat(winningLotto.rank(lotto)).isNotNull()
-    }
-
-    @Test
-    @DisplayName("로또를 가지고  당첨 등수를 알 수 있다")
-    fun `getRank`() {
-        val lottoNumber = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
-        val winningLotto = WinningLotto(lottoNumber, 7)
-        val lotto = Lotto(listOf(4, 5, 6, 7, 8, 9).map { LottoNumber.from(it) }.toSet())
-        assertThat(winningLotto.rank(lotto)).isEqualTo(Rank.FIFTH)
-    }
-
-    @Test
-    @DisplayName("로또를 가지고 2 등을 알 수 있다")
-    fun `getRankSecond`() {
-        val lottoNumber = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
-        val winningLotto = WinningLotto(lottoNumber, 7)
-        val lotto = Lotto(listOf(2, 3, 4, 5, 6, 7).map { LottoNumber.from(it) }.toSet())
-        assertThat(winningLotto.rank(lotto)).isEqualTo(Rank.SECOND)
-    }
-
-    @Test
-    @DisplayName("로또를 가지고 3 등을 알 수 있다")
-    fun `getRankThird`() {
-        val lottoNumber = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
-        val winningLotto = WinningLotto(lottoNumber, 7)
-        val lotto = Lotto(listOf(2, 3, 4, 5, 6, 8).map { LottoNumber.from(it) }.toSet())
-        assertThat(winningLotto.rank(lotto)).isEqualTo(Rank.THIRD)
-    }
-
-    @Test
-    @DisplayName("보너스 번호를 입력 받을 수 있다")
-    fun `inputBonusLottoNumber`() {
-        val lottoNumber = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
-        val winningLotto = WinningLotto(lottoNumber, 7)
-        assertThat(winningLotto.bonusNumber.value).isEqualTo(7)
+    @DisplayName("자동 생성 수를 반환한다")
+    fun `autoMakeCount`() {
+        val inputMoney = 14_000
+        val lottoGame = LottoGame()
+        lottoGame.buy(Money(inputMoney), LottoManual(10))
+        assertThat(lottoGame.autoMakeCount()).isEqualTo(4)
     }
 }
--- src/test/kotlin/model/WinningLottoTest.kt
+++ src/test/kotlin/model/WinningLottoTest.kt
@@ -12,4 +12,56 @@ class WinningLottoTest {
         val winningLotto = WinningLotto(Lotto(set), LottoNumber.from(7))
         assertThat(winningLotto.bonusNumber).isNotNull()
     }
+
+    @Test
+    @DisplayName("로또를 가지고 3 등을 알 수 있다")
+    fun `getRankThird`() {
+        val lottoNumber = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
+        val winningLotto = WinningLotto(lottoNumber, 7)
+        val lotto = Lotto(listOf(2, 3, 4, 5, 6, 8).map { LottoNumber.from(it) }.toSet())
+        assertThat(winningLotto.rank(lotto)).isEqualTo(Rank.THIRD)
+    }
+
+    @Test
+    @DisplayName("보너스 번호를 입력 받을 수 있다")
+    fun `inputBonusLottoNumber`() {
+        val lottoNumber = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
+        val winningLotto = WinningLotto(lottoNumber, 7)
+        assertThat(winningLotto.bonusNumber.value).isEqualTo(7)
+    }
+
+    @Test
+    @DisplayName("로또를 가지고  당첨 등수를 알 수 있다")
+    fun `getRank`() {
+        val lottoNumber = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
+        val winningLotto = WinningLotto(lottoNumber, 7)
+        val lotto = Lotto(listOf(4, 5, 6, 7, 8, 9).map { LottoNumber.from(it) }.toSet())
+        assertThat(winningLotto.rank(lotto)).isEqualTo(Rank.FIFTH)
+    }
+
+    @Test
+    @DisplayName("로또를 가지고 2 등을 알 수 있다")
+    fun `getRankSecond`() {
+        val lottoNumber = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
+        val winningLotto = WinningLotto(lottoNumber, 7)
+        val lotto = Lotto(listOf(2, 3, 4, 5, 6, 7).map { LottoNumber.from(it) }.toSet())
+        assertThat(winningLotto.rank(lotto)).isEqualTo(Rank.SECOND)
+    }
+
+    @Test
+    @DisplayName("로또 당첨 번호를 입력 받을 수 있다")
+    fun `inputWinningLotto`() {
+        val lottoNumber = setOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
+        val winningLotto = WinningLotto(lottoNumber, 7)
+        assertThat(winningLotto).isNotNull
+    }
+
+    @Test
+    @DisplayName("로또와 당첨 번호 사이의 matchResult를 알 수 있다")
+    fun `getMatchCount`() {
+        val lottoNumber = setOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
+        val winningLotto = WinningLotto(lottoNumber, 7)
+        val lotto = Lotto(setOf(4, 5, 6, 7, 8, 9).map { LottoNumber.from(it) }.toSet())
+        assertThat(winningLotto.rank(lotto)).isNotNull()
+    }
 }
