4f95b14dc6a05fcd696ffe233574008cc4a13563
EthanChae <boschae36@gmail.com>
2020-08-15 16:01:52 +0900

    refactor(lotto) : Add tests and relocate them

--- src/main/kotlin/lotto/domain/Prize.kt
+++ src/main/kotlin/lotto/domain/Prize.kt
@@ -5,10 +5,12 @@ enum class Prize(val countOfMatch: Int, private val rank: Int, val prizeMoney: I
     MATCH_FOUR(4, 4, 50_000),
     MATCH_FIVE(5, 3, 1_500_000),
     MATCH_FIVE_WITH_BONUS(5, 2, 30_000_000, true),
-    MATCH_ALL(6, 1, 2_000_000_000);
+    MATCH_ALL(6, 1, 2_000_000_000),
+    MISS(0, 0, 0);
 
     companion object {
         fun getPrize(countOfMatch: Int): Prize {
+            if (countOfMatch < MATCH_THREE.countOfMatch) return MISS
             return values().find { it.countOfMatch == countOfMatch }!!
         }
     }
