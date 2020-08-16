e0e61d2d2a63bd7ac99d245ac17f95c7398a65e1
zion830 <zion830@honglab.org>
2020-08-17 02:03:17 +0900

    feat(lotto): View에 수동 로또 입출력 로직 추가

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -7,23 +7,26 @@ object InputView {
     private const val INPUT_MANUAL_COUNT = "수동으로 구매할 로또 수를 입력해 주세요."
     private const val INPUT_MANUAL_NUMBERS = "수동으로 구매할 번호를 입력해 주세요. 남은 개수 : "
 
+    private const val ERR_INVALID_MANUAL_COUNT = "처음 지불한 금액 이하의 장수만 발급이 가능합니다."
     private const val ERR_INVALID_INPUT = "유효한 입력이 아닙니다. 다시 입력해주세요."
 
-    fun readMoney(isValid: (value: Int) -> Boolean) = readNumber(INPUT_MONEY) { isValid(it.toInt()) }.toInt()
+    fun readMoney(isValid: (value: Int) -> Boolean) =
+        readNumber(INPUT_MONEY) { isValid(it.toInt()) }.toInt()
+
+    fun readManualCount(isValid: (value: Int) -> Boolean) =
+        readNumber(INPUT_MANUAL_COUNT, ERR_INVALID_MANUAL_COUNT) { isValid(it.toInt()) }.toInt()
+
+    fun readManualNumbers(remainCount: Int) = readValue(INPUT_MANUAL_NUMBERS + remainCount)
 
     fun readWinningNumbers() = readValue(INPUT_WINNING_NUMBERS)
 
     fun readBonusNumber() = readNumber(INPUT_BONUS_NUMBER).toInt()
 
-    fun readManualCount() = readValue(INPUT_MANUAL_COUNT)
-
-    fun readManualNumbers(remainCount: Int) = readValue(INPUT_MANUAL_NUMBERS + remainCount)
-
     private fun readNumber(
         titleMsg: String,
         errMsg: String = ERR_INVALID_INPUT,
         isValid: (value: String) -> Boolean = { true }
-    ) = readValue(titleMsg, errMsg) { isValid(it) && it.toIntOrNull() != null }
+    ) = readValue(titleMsg, errMsg) { it.toIntOrNull() != null && isValid(it) }
 
     private fun readValue(
         titleMsg: String,
--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -2,12 +2,13 @@ package lotto.view
 
 import lotto.domain.lotto.LottoNumber
 import lotto.domain.lotto.LottoTicket
+import lotto.domain.lotto.LottoType
 import lotto.domain.selling.ExchangeResult
 import lotto.domain.selling.PaymentResult
 import lotto.domain.selling.Rank
 
 object ResultView {
-    private const val PAYMENT_RESULT = "%s\n거스름돈: %d"
+    private const val PAYMENT_RESULT = "[수동 로또 목록]\n%s\n[자동 로또 목록]\n%s\n거스름돈: %d"
 
     private const val WINNING_STATISTICS_TITLE = "당첨 통계"
     private const val WINNING_EXCHANGE_RESULT = "%d장 - %d개 일치 (%d원)"
@@ -16,6 +17,7 @@ object ResultView {
     private const val WINNING_STATISTICS = "총 수익률은 %.2f 입니다."
 
     private const val INVALID_BONUS_NUMBER = "당첨 번호와 중복되지 않는 유효한 정수를 입력해주세요."
+    private const val INVALID_MANUAL_COUNT = "처음 지불한 금액 이상의 로또를 발급받을 수 없습니다."
     private val INVALID_LOTTO_NUMBERS = "${LottoNumber.NUMBER_RANGE} 사이의 번호 " +
         "${LottoTicket.NUMBER_COUNT}개를 중복없이 입력해주세요."
 
@@ -27,8 +29,18 @@ object ResultView {
         println(INVALID_BONUS_NUMBER)
     }
 
+    fun printInvalidManualCount() {
+        println(INVALID_MANUAL_COUNT)
+    }
+
     fun printPaymentResult(result: PaymentResult) {
-        println(PAYMENT_RESULT.format(result.lottoTickets.joinToString("\n"), result.change))
+        println(
+            PAYMENT_RESULT.format(
+                result.findLottoTickets(LottoType.MANUAL).joinToString("\n"),
+                result.findLottoTickets(LottoType.AUTO).joinToString("\n"),
+                result.change
+            )
+        )
     }
 
     fun printExchangeResult(exchangeResult: ExchangeResult) {
