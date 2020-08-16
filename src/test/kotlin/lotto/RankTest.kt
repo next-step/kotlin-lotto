d52025921acea5d01b2905b46faf52f9d118c8ce
LeeYounghyeon <leey9494@icloud.com>
2020-08-16 20:39:33 +0900

    feat(lotto): match count가 5이고 matchBonus가 true일때 SECOND를 반환하는 기능 및 테스트 구현

--- src/test/kotlin/lotto/RankTest.kt
+++ src/test/kotlin/lotto/RankTest.kt
@@ -12,17 +12,23 @@ class RankTest {
     @ParameterizedTest
     @ValueSource(ints = [0, 1, 2])
     fun `when matchCount less than 3 than return NONE`(value: Int) {
-        assertThat(Rank.of(value)).isEqualTo(Rank.NONE)
+        assertThat(Rank.of(value, false)).isEqualTo(Rank.NONE)
     }
 
     @DisplayName("match count가 3이상의 숫자이면 count가 일치하는 Rank 반환한다.")
     @Test
     fun `when matchCount greater than 3 than return the matching Rank`() {
         assertAll(
-            { assertThat(Rank.of(3)).isEqualTo(Rank.FOURTH) },
-            { assertThat(Rank.of(4)).isEqualTo(Rank.THIRD) },
-            { assertThat(Rank.of(5)).isEqualTo(Rank.SECOND) },
-            { assertThat(Rank.of(6)).isEqualTo(Rank.FIRST) }
+            { assertThat(Rank.of(3, false)).isEqualTo(Rank.FIFTH) },
+            { assertThat(Rank.of(4, false)).isEqualTo(Rank.FOURTH) },
+            { assertThat(Rank.of(5, false)).isEqualTo(Rank.SECOND) },
+            { assertThat(Rank.of(6, false)).isEqualTo(Rank.FIRST) }
         )
+    }
+
+    @DisplayName("match count가 5이고 보너스 볼이 일치하면 SECOND를 반환한다.")
+    @Test
+    fun `when bonus ball matches than return SECOND Rank`() {
+        assertThat(Rank.of(5, true)).isEqualTo(Rank.SECOND)
     }
 }
