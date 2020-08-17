84e9bf99335b51e73ed0de13f02dc6a25b0e142d
LeeYounghyeon <leey9494@icloud.com>
2020-08-18 00:06:51 +0900

    refactor(lotto): map을 이용한 리팩터링

--- src/main/kotlin/lotto/Lottos.kt
+++ src/main/kotlin/lotto/Lottos.kt
@@ -4,12 +4,10 @@ class Lottos(private val amountOfLotto: Int) {
     val lottos = (0 until amountOfLotto).map { Lotto() }
 
     fun matchLottos(winningLotto: WinningNumbers, bonusBall: Int): List<Rank> {
-        val results = mutableListOf<Rank>()
-        lottos.forEach {
+        return lottos.map {
             val count = it.matchLotto(winningLotto)
             val bonusBallResult = it.matchBonusBall(bonusBall)
-            results.add(Rank.of(count, bonusBallResult))
+            Rank.of(count, bonusBallResult)
         }
-        return results.toList()
     }
 }
