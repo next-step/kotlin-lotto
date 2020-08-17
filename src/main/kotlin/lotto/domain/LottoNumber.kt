e76683f2aaf573da0b3f1f5483de4e3e5980e973
kimgaeul <kimgaeul4@gmail.com>
2020-08-17 10:56:59 +0900

    feat(step3): 상수명 변경

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -2,14 +2,14 @@ package lotto.domain
 
 class LottoNumber private constructor(private val value: Int) {
     companion object {
-        private val associateWith: Map<Int, LottoNumber> = (1..45).associateWith { LottoNumber(it) }
+        private val NUMBERS: Map<Int, LottoNumber> = (1..45).associateWith { LottoNumber(it) }
 
         fun get(number: Int): LottoNumber {
-            return associateWith[number] ?: throw IllegalArgumentException()
+            return NUMBERS[number] ?: throw IllegalArgumentException()
         }
 
         fun generateNumbers(): Set<LottoNumber> {
-            return associateWith.values.shuffled()
+            return NUMBERS.values.shuffled()
                 .take(6)
                 .sortedBy { it.value }.toSet()
         }
