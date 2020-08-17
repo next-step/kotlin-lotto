2487087477a3f1391ab386af25f28a1f11db8055
kimgaeul <kimgaeul4@gmail.com>
2020-08-17 16:49:49 +0900

    feat(step3): 피드백 적용 ("보너스 번호는 당첨 번호와 중복될 수 없다")

--- src/test/kotlin/lotto/domain/WinningLottoTest.kt
+++ src/test/kotlin/lotto/domain/WinningLottoTest.kt
@@ -1,6 +1,7 @@
 package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
 
 class WinningLottoTest {
@@ -21,5 +22,26 @@ class WinningLottoTest {
         val bonusNumber = LottoNumber.get(8)
 
         assertThat(WinningLotto(winningNumbers, bonusNumber)).isNotNull
+    }
+
+    @Test
+    fun `보너스 번호는 당첨 번호와 중복될 수 없다`() {
+
+        val winningNumbers = LottoTicket(
+            setOf(
+                LottoNumber.get(1),
+                LottoNumber.get(2),
+                LottoNumber.get(3),
+                LottoNumber.get(4),
+                LottoNumber.get(5),
+                LottoNumber.get(6)
+            )
+        )
+
+        val bonusNumber = LottoNumber.get(6)
+
+        assertThatIllegalArgumentException().isThrownBy {
+            WinningLotto(winningNumbers, bonusNumber)
+        }
     }
 }
