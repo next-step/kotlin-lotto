a78e7396a8671a34b0a959ed69d68991681a7df2
LeeYounghyeon <leey9494@icloud.com>
2020-08-18 23:41:50 +0900

    feat(lotto): lotto가 list<Int>를 받도록 수정

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,8 +1,6 @@
 package lotto
 
-class Lotto {
-    private val lotto = getRandomNumbers()
-
+class Lotto(val lotto: List<Int>) {
     fun matchLotto(winningLotto: WinningNumbers): Int {
         return lotto.filter { winningLotto.isContained(it) }.count()
     }
@@ -11,18 +9,7 @@ class Lotto {
         return lotto.contains(bonusBall)
     }
 
-    private fun getRandomNumbers(): List<Int> {
-        return NUMBER_RANGE.shuffled().take(LOTTO_COUNT).sorted()
-    }
-
     override fun toString(): String {
         return "$lotto"
-    }
-
-    companion object {
-        private const val MIN_NUMBER = 1
-        private const val MAX_NUMBER = 45
-        const val LOTTO_COUNT = 6
-        val NUMBER_RANGE = (MIN_NUMBER..MAX_NUMBER)
     }
 }
--- src/main/kotlin/lotto/LottoProgram.kt
+++ src/main/kotlin/lotto/LottoProgram.kt
@@ -3,6 +3,10 @@ package lotto
 import kotlin.math.roundToInt
 
 object LottoProgram {
+    private const val LOTTO_COUNT = 6
+    private const val MIN_NUMBER = 1
+    private const val MAX_NUMBER = 45
+    private val NUMBER_RANGE = (MIN_NUMBER..MAX_NUMBER)
     private const val UNIT = 1000
     private const val DECIMAL_STANDARD = 100.0
 
@@ -14,5 +18,9 @@ object LottoProgram {
         val prizeSum = results.sumBy { it.prize }
         if (prizeSum == 0) return 0.0
         return (prizeSum / amountOfMoney.toDouble() * DECIMAL_STANDARD).roundToInt() / DECIMAL_STANDARD
+    }
+
+    fun getRandomNumbers(): List<Int> {
+        return NUMBER_RANGE.shuffled().take(LOTTO_COUNT).sorted()
     }
 }
