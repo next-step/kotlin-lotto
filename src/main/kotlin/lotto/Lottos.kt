69f681d884d6a6c360403da695d67b5451b1ce98
LeeYounghyeon <leey9494@icloud.com>
2020-08-18 23:48:55 +0900

    feat(lotto): 수동 로또를 lottos 에 추가하도록 수정

--- src/main/kotlin/lotto/Lottos.kt
+++ src/main/kotlin/lotto/Lottos.kt
@@ -1,7 +1,16 @@
 package lotto
 
-class Lottos(private val amountOfLotto: Int) {
-    val lottos = (0 until amountOfLotto).map { Lotto() }
+class Lottos(private val amountOfLotto: Int, private val manualLottos: List<Lotto>) {
+    val lottos = getLottos(amountOfLotto, manualLottos)
+
+    fun getLottos(amountOfLotto: Int, manualLottos: List<Lotto>): List<Lotto> {
+        val lottos = manualLottos.toMutableList()
+        val automaticLottoCount = amountOfLotto - manualLottos.size
+        for (number in 0 until automaticLottoCount) {
+            lottos.add(Lotto(LottoProgram.getRandomNumbers()))
+        }
+        return lottos.toList()
+    }
 
     fun matchLottos(winningLotto: WinningNumbers, bonusBall: Int): List<Rank> {
         return lottos.map {
