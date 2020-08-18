81c73888743dad885f7eb1d94e8cd90c85374ab9
반선호 <shakevan@woowahan.com>
2020-08-19 00:14:33 +0900

    feat: 일치 개수 구해주는 로직 구현

--- src/main/kotlin/com/nextstep/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/com/nextstep/lotto/domain/WinningLotto.kt
@@ -1,10 +1,24 @@
 package com.nextstep.lotto.domain
 
-class WinningLotto(val winningLotto: Lotto) {
+class WinningLotto(private val lottoNumbers: List<LottoNumber>) {
+    init {
+        validate(lottoNumbers)
+    }
+
+    private fun validate(lottoNumbers: List<LottoNumber>) {
+        if (lottoNumbers.size != NUMBER_OF_LOTTO_NUMBER) {
+            throw IllegalArgumentException("로또는 6개의 숫자로 이루어져 있습니다.")
+        }
+    }
+
+    fun findNumberOfMatch(lotto: Lotto): Int {
+        return lotto.lottoNumbers.filter { lottoNumbers.contains(it) }.count()
+    }
 
     companion object {
+        const val NUMBER_OF_LOTTO_NUMBER = 6
         fun generate(lottoNumbers: List<Int>): WinningLotto {
-            return WinningLotto(LottoFactory.drawManualLotto(lottoNumbers))
+            return WinningLotto(lottoNumbers.map { LottoNumbers.valueOf(it) })
         }
     }
 }
---
+++ src/test/kotlin/com/nextstep/lotto/domain/WinningLottoTest.kt
@@ -0,0 +1,15 @@
+package com.nextstep.lotto.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class WinningLottoTest {
+
+    @Test
+    fun `LottoNumber가 매치하는 수량을 리턴해준다`() {
+        val lotto = Lotto((1..6).map { LottoNumber(it) })
+        val winningLotto = WinningLotto.generate(listOf(5, 6, 7, 8, 9, 10))
+
+        assertThat(winningLotto.findNumberOfMatch(lotto)).isEqualTo(2)
+    }
+}
