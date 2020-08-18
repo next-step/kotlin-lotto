2e6f418738e2aff0e4b81caece78623c1923f218
LeeYounghyeon <leey9494@icloud.com>
2020-08-18 21:11:36 +0900

    feat(lotto): 구입할 로또 수를 입력한다

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -5,10 +5,21 @@ object InputView {
     private const val DELIMITER = ","
     private const val UNIT = 1000
 
+    tailrec fun inputManualLottoCount(amountOfLottos: Int): Int {
+        println("수동으로 구매할 로또 수를 입력해 주세요.")
+        val manualNumberCount = readLine()?.toIntOrNull()
+        return if (manualNumberCount != null && manualNumberCount <= amountOfLottos) {
+            manualNumberCount
+        } else {
+            println("숫자만 입력가능하고, 구입한 로또 장수보다 같거나 적어야 합니다.")
+            inputManualLottoCount(amountOfLottos)
+        }
+    }
+
     tailrec fun inputAmountOfMoney(): Int {
         println("구입금액을 입력해 주세요.")
         val amountOfMoney = readLine()?.toIntOrNull()
-        return if (amountOfMoney !== null && amountOfMoney % UNIT == 0 && amountOfMoney != 0) {
+        return if (amountOfMoney != null && amountOfMoney % UNIT == 0 && amountOfMoney != 0) {
             amountOfMoney
         } else {
             println("금액은 천원 단위만 입력할 수 있습니다. 다시 입력해주세요.")
