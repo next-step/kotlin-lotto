3c12cff8a9e081f703785c379c0efe572b654c0f
Jiwhun Kim <jiwhun.kim@outlook.com>
2020-08-18 22:13:06 +0900

    feature(Lotto-step4): 피드백 반영 중복 메소드를 제거하다

--- src/main/kotlin/model/WinningLotto.kt
+++ src/main/kotlin/model/WinningLotto.kt
@@ -2,6 +2,7 @@ package model
 
 class WinningLotto(val lotto: Lotto, var bonusNumber: LottoNumber) : BasicLotto {
     constructor(number: Set<LottoNumber>, bonusNumber: Int) : this(Lotto(number), LottoNumber.from(bonusNumber))
+    constructor(lotto: Lotto, bonusNumber: Int) : this(lotto, LottoNumber.from(bonusNumber))
 
     fun rank(lotto: Lotto): Rank {
         val matchCount = this.lotto.number.count { lotto.isIn(it) }
--- src/test/kotlin/model/WinningLottoTest.kt
+++ src/test/kotlin/model/WinningLottoTest.kt
@@ -8,24 +8,23 @@ class WinningLottoTest {
     @Test
     @DisplayName("보너스 번호를 입력 받는다")
     fun `inputBonusNumber`() {
-        val set: Set<LottoNumber> = setOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
-        val winningLotto = WinningLotto(Lotto(set), LottoNumber.from(7))
+        val winningLotto = WinningLotto(createLotto(1, 2, 3, 4, 5, 6), LottoNumber.from(7))
         assertThat(winningLotto.bonusNumber).isNotNull()
     }
 
     @Test
     @DisplayName("로또를 가지고 3 등을 알 수 있다")
     fun `getRankThird`() {
-        val lottoNumber = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
+        val lottoNumber = createLotto(1, 2, 3, 4, 5, 6)
         val winningLotto = WinningLotto(lottoNumber, 7)
-        val lotto = Lotto(listOf(2, 3, 4, 5, 6, 8).map { LottoNumber.from(it) }.toSet())
+        val lotto = createLotto(2, 3, 4, 5, 6, 8)
         assertThat(winningLotto.rank(lotto)).isEqualTo(Rank.THIRD)
     }
 
     @Test
     @DisplayName("보너스 번호를 입력 받을 수 있다")
     fun `inputBonusLottoNumber`() {
-        val lottoNumber = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
+        val lottoNumber = createLotto(1, 2, 3, 4, 5, 6)
         val winningLotto = WinningLotto(lottoNumber, 7)
         assertThat(winningLotto.bonusNumber.value).isEqualTo(7)
     }
@@ -33,25 +32,25 @@ class WinningLottoTest {
     @Test
     @DisplayName("로또를 가지고  당첨 등수를 알 수 있다")
     fun `getRank`() {
-        val lottoNumber = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
+        val lottoNumber = createLotto(1, 2, 3, 4, 5, 6)
         val winningLotto = WinningLotto(lottoNumber, 7)
-        val lotto = Lotto(listOf(4, 5, 6, 7, 8, 9).map { LottoNumber.from(it) }.toSet())
+        val lotto = createLotto(4, 5, 6, 7, 8, 9)
         assertThat(winningLotto.rank(lotto)).isEqualTo(Rank.FIFTH)
     }
 
     @Test
     @DisplayName("로또를 가지고 2 등을 알 수 있다")
     fun `getRankSecond`() {
-        val lottoNumber = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
+        val lottoNumber = createLotto(1, 2, 3, 4, 5, 6)
         val winningLotto = WinningLotto(lottoNumber, 7)
-        val lotto = Lotto(listOf(2, 3, 4, 5, 6, 7).map { LottoNumber.from(it) }.toSet())
+        val lotto = createLotto(2, 3, 4, 5, 6, 7)
         assertThat(winningLotto.rank(lotto)).isEqualTo(Rank.SECOND)
     }
 
     @Test
     @DisplayName("로또 당첨 번호를 입력 받을 수 있다")
     fun `inputWinningLotto`() {
-        val lottoNumber = setOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
+        val lottoNumber = createLotto(1, 2, 3, 4, 5, 6)
         val winningLotto = WinningLotto(lottoNumber, 7)
         assertThat(winningLotto).isNotNull
     }
@@ -59,9 +58,13 @@ class WinningLottoTest {
     @Test
     @DisplayName("로또와 당첨 번호 사이의 matchResult를 알 수 있다")
     fun `getMatchCount`() {
-        val lottoNumber = setOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }.toSet()
+        val lottoNumber = createLotto(1, 2, 3, 4, 5, 6)
         val winningLotto = WinningLotto(lottoNumber, 7)
-        val lotto = Lotto(setOf(4, 5, 6, 7, 8, 9).map { LottoNumber.from(it) }.toSet())
+        val lotto = createLotto(4, 5, 6, 7, 8, 9)
         assertThat(winningLotto.rank(lotto)).isNotNull()
+    }
+
+    private fun createLotto(vararg integers: Int): Lotto {
+        return Lotto(integers.toSet().map { LottoNumber.from(it) }.toSet())
     }
 }
