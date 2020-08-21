821dcb157294a072dc89b9ed132ac81801e5075d
Jonny-Cho <andante2183@gmail.com>
2020-08-21 12:35:19 +0900

    refactor(view): reflect feedback

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -7,12 +7,10 @@ import lotto.domain.Money
 object InputView {
     private const val INPUT_MONEY = "구입금액을 입력해 주세요."
     private const val INPUT_MANUAL_COUNT = "수동으로 구매할 로또 수를 입력해 주세요."
-    private const val MAX_NUMBER_SUFFIX = "개 까지 구매할 수 있습니다."
     private const val INPUT_MANUAL_NUMBER = "수동으로 구매할 번호를 입력해 주세요."
     private const val INPUT_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요."
     private const val INPUT_BONUS_NUMBER = "지난 주 보너스 번호를 입력해 주세요."
     private const val INVALID_WINNING_NUMBER = "콤마(,)로 연결되고 중복되지 않은 6자리 숫자를 입력하세요."
-    private const val ERROR_INVALID_STRING = "틀린 형식입니다."
     private const val REQUIRE_MORE_THAN_THOUSAND = "1000원 이상의 숫자를 넣어주세요."
     private const val LOTTO_PRICE = 1000
     private const val DELIMITER = ","
--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -10,11 +10,11 @@ object ResultView {
     private const val BUY_COUNT_SUFFIX = "개를 구매했습니다."
     private const val WINNING_STATISTICS = "당첨 통계"
     private const val WINNING_SEPARATOR = "---------"
-    private const val COUNT_SUFFIX = "개"
+    private const val COUNT_SUFFIX = "%d개"
     private const val SEPARATOR = "- "
     private const val TOTAL_RATE_PREFIX = "총 수익률은 "
     private const val TOTAL_RATE_SUFFIX = "입니다."
-    private const val ACCORDANCE_SUFFIX = "개 일치"
+    private const val ACCORDANCE_SUFFIX = "%d개 일치"
     private const val BONUS_MESSAGE = ", 보너스 볼 일치"
 
     fun printLottos(lottoTicket: LottoTicket) {
@@ -33,9 +33,9 @@ object ResultView {
     }
 
     private fun printRankResult(rank: Rank, result: LottoResult) {
-        val countMatch = String.format("%s$ACCORDANCE_SUFFIX", rank.countOfMatch)
-        val winningMoney = String.format("(%s)", rank.winningMoney)
-        val countRank = String.format("%s$COUNT_SUFFIX", result.countByRank(rank))
+        val countMatch = ACCORDANCE_SUFFIX.format(rank.countOfMatch)
+        val winningMoney = "(%s)".format(rank.winningMoney)
+        val countRank = COUNT_SUFFIX.format(result.countByRank(rank))
         val bonus = if (rank.matchBonus) BONUS_MESSAGE else ""
         println("$countMatch$bonus$winningMoney$SEPARATOR$countRank")
     }
