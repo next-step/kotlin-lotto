14e59648671099f456e83edc5cf42d0014baea48
EthanChae <boschae36@gmail.com>
2020-08-25 02:52:22 +0900

    refactor(lotto): Rearrange the order follow the convention
    https://kotlinlang.org/docs/reference/coding-conventions.html#class-layout

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -1,14 +1,14 @@
 package lotto.domain
 
 class WinningLotto(val prizeLotto: Lotto, val bonusNumber: LottoNumber) {
+    init {
+        checkValidation()
+    }
+
     constructor(prizeLotto: String, bonusNumber: String) : this(
         Lotto.from(prizeLotto),
         LottoNumber.from(bonusNumber)
     )
-
-    init {
-        checkValidation()
-    }
 
     private fun checkValidation() {
         require(!prizeLotto.isContainNumber(bonusNumber)) { "당첨번호에 포함되지 않는 보너스 볼을 입력해주세요." }
