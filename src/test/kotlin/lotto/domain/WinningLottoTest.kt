0b3ca25e482e5a9fae9a7e98031b3b9b40e39521
EthanChae <boschae36@gmail.com>
2020-08-25 03:25:35 +0900

    refactor(lotto): Add test for WinningLotto.kt

---
+++ src/test/kotlin/lotto/domain/WinningLottoTest.kt
@@ -0,0 +1,29 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class WinningLottoTest {
+
+    @DisplayName("Winning Lotto 유효성 확인, 보너스 번호는 당첨번호에 포함 안되어야함, 에러")
+    @Test
+    fun validateWinningLottoError() {
+        val prizeLotto = Lotto.from("1,2,3,4,5,6")
+        val bonusNumber = LottoNumber.from("5")
+
+        assertThatThrownBy { WinningLotto(prizeLotto, bonusNumber) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @DisplayName("Winning Lotto 유효성 확인, 보너스 번호는 당첨번호에 포함 안되어야함 ")
+    @Test
+    fun validateWinningLotto() {
+        val prizeLotto = Lotto.from("1,2,3,4,5,6")
+        val bonusNumber = LottoNumber.from("7")
+
+        assertThat(WinningLotto(prizeLotto, bonusNumber))
+            .isNotInstanceOfAny(Exception::class.java)
+    }
+}
