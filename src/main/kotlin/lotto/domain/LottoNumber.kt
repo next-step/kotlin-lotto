14d129d47de44f2e4ec4a9e3701d55b5316d72aa
JeongChanhee <chany5238@gmail.com>
2020-09-01 22:23:01 +0900

    중복된 변수 제거

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -7,14 +7,14 @@ data class LottoNumber(val number: Int) : Comparable<LottoNumber> {
     }
 
     private fun validateNumberRange(number: Int) {
-        if (!LOTTO_NUMBER.contains(number)) throw IllegalArgumentException("$number$NOT_ENOUGH_LOTTO_NUM")
+        if (!LOTTO_NUMBERS.contains(number)) throw IllegalArgumentException("$number$NOT_ENOUGH_LOTTO_NUM")
     }
 
     companion object {
-        const val LOTTO_MIN_NUMBER = 1
-        const val LOTTO_MAX_NUMBER = 45
-        const val NOT_ENOUGH_LOTTO_NUM = "는 로또 번호(1~45)가 아닙니다."
-        private val LOTTO_NUMBER = (LOTTO_MIN_NUMBER..LOTTO_MAX_NUMBER)
+        private const val LOTTO_MIN_NUMBER = 1
+        private const val LOTTO_MAX_NUMBER = 45
+        const val NOT_ENOUGH_LOTTO_NUM = "는 로또 번호($LOTTO_MIN_NUMBER~$LOTTO_MAX_NUMBER)가 아닙니다."
+        val LOTTO_NUMBERS = (LOTTO_MIN_NUMBER..LOTTO_MAX_NUMBER)
     }
 
     override fun compareTo(other: LottoNumber): Int {
