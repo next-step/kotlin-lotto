1f8536897b98ac122cb05cafb6aa71f216e421ee
JeongChanhee <chany5238@gmail.com>
2020-09-01 22:30:40 +0900

    getLottoNumbers() 인스턴스 변수적용

--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -10,27 +10,27 @@ class LottoTicket(val lottoNumbers: List<LottoNumber>) {
         if (lottoNumbers.size != LOTTO_NUMBER_COUNT_PER_TICKET) {
             throw IllegalArgumentException(NOT_FULFILL_TICKET_SIZE)
         }
-        if (lottoNumbers.size != getLottoNumber().distinct().size) {
+        if (lottoNumbers.size != getLottoNumbers(lottoNumbers).distinct().size) {
             throw IllegalArgumentException(NOT_ALLOW_DISTINCT_NUM)
         }
     }
 
-    private fun getLottoNumber(): List<Int> {
-        return lottoNumbers.map { it.number }.sorted()
-    }
-
     fun getMatchValueCount(winningLotto: LottoTicket): Int {
-        return getLottoNumber().count { winningLotto.getLottoNumber().contains(it) }
+        return getLottoNumbers(lottoNumbers).count { getLottoNumbers(winningLotto.lottoNumbers).contains(it) }
     }
 
     fun isExistBonusBall(bonusBall: LottoNumber): Boolean {
-        return getLottoNumber().contains(bonusBall.number)
+        return getLottoNumbers(lottoNumbers).contains(bonusBall.number)
     }
 
     companion object {
         const val LOTTO_NUMBER_COUNT_PER_TICKET = 6
         const val NOT_FULFILL_TICKET_SIZE = "6개의 번호가 필요합니다."
         const val NOT_ALLOW_DISTINCT_NUM = "로또 번호는 중복될 수 없습니다."
+
+        private fun getLottoNumbers(lottoNumbers: List<LottoNumber>): List<Int> {
+            return lottoNumbers.map { it.number }.sorted()
+        }
 
         fun from(lottoNumbers: List<LottoNumber>): LottoTicket {
             return LottoTicket(lottoNumbers)
