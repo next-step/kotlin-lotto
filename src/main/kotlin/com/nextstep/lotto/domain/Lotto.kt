736e61779ce71f935843eb9d4588d4a6aaca4991
반선호 <shakevan@woowahan.com>
2021-02-02 17:12:54 +0900

    fix: Lotto 생성시 validation require이용해서 처리

--- src/main/kotlin/com/nextstep/lotto/domain/Lotto.kt
+++ src/main/kotlin/com/nextstep/lotto/domain/Lotto.kt
@@ -6,8 +6,8 @@ data class Lotto(val lottoNumbers: List<LottoNumber>) {
     }
 
     private fun validate(lottoNumbers: List<LottoNumber>) {
-        if (lottoNumbers.size != NUMBER_OF_LOTTO_NUMBER) {
-            throw IllegalArgumentException("로또는 6개의 숫자로 이루어져 있습니다.")
+        require(lottoNumbers.size == NUMBER_OF_LOTTO_NUMBER) {
+            "로또는 6개의 숫자로 이루어져 있습니다."
         }
     }
 
