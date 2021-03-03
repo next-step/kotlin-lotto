4f17a24a878174939eb07fc5c5f00f0ed842f756
kit(김광수) <kit.t@kakaocorp.com>
2021-03-03 21:50:06 +0900

    feat: make it pass

--- src/test/kotlin/NonNegativeIntListTest.kt
+++ src/test/kotlin/NonNegativeIntListTest.kt
@@ -15,6 +15,10 @@ class NonNegativeIntListTest {
     }
 
     data class NonNegativeIntList(private val nonNegativeIntList: List<Int>) : List<Int> by nonNegativeIntList {
+        init {
+            require(nonNegativeIntList.find { it < 0 } == null)
+        }
+
         companion object {
             fun of(numericStringList: List<String>): NonNegativeIntList {
                 return numericStringList.map {
