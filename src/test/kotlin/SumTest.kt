bf32fbe842c0bacba05f6f76300a69f6ee6ee22d
kit(김광수) <kit.t@kakaocorp.com>
2021-03-03 21:54:33 +0900

    feat: make it pass

--- src/test/kotlin/SumTest.kt
+++ src/test/kotlin/SumTest.kt
@@ -1,8 +1,13 @@
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class SumTest {
     @Test
     fun `숫자 리스트를 모두 더한다`() {
         assertThat(Sum(listOf(1, 2)).toInt()).isEqualTo(3)
+    }
+
+    data class Sum(private val intList: List<Int>) {
+        fun toInt() = intList.sum()
     }
 }
