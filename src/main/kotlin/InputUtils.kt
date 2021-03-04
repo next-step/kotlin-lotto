dae0f0a087f3d380a5826c00fe36faf7e51aecd2
palmseung <palmseung@gmail.com>
2021-03-04 15:59:59 +0900

    test(string-adder): add test to split input by default(, ;)

---
+++ src/main/kotlin/InputUtils.kt
@@ -0,0 +1,6 @@
+import java.util.*
+
+object InputUtils {
+    fun splitByDefault(input: String): List<String> {
+    }
+}
---
+++ src/test/kotlin/InputUtilsTest.kt
@@ -0,0 +1,16 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+internal class InputUtilsTest {
+
+    @ParameterizedTest
+    @CsvSource(value = ["1,2,3,45,6:5", "22;28,2:3", "26;2;33:3"], delimiter = ':')
+    fun splitByDefault(input: String, size: Int) {
+        // when
+        val actual = InputUtils.splitByDefault(input)
+
+        // then
+        assertThat(actual).isEqualTo(size)
+    }
+}
