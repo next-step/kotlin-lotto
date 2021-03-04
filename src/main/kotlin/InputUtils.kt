179b624307af67f05fc223feeb8cc5f3f394daad
palmseung <palmseung@gmail.com>
2021-03-04 16:14:14 +0900

    feat(string-adder): add feature to split when input is ""

--- src/main/kotlin/InputUtils.kt
+++ src/main/kotlin/InputUtils.kt
@@ -1,8 +1,13 @@
 private const val DELIMITER_1 = ","
 private const val DELIMITER_2 = ";"
+private const val ZERO = "0"
 
 object InputUtils {
     fun splitByDefault(input: String): List<String> {
+        if (input.isBlank()) {
+            return listOf(ZERO)
+        }
+
         return input.split(DELIMITER_1)
             .flatMap { it.split(DELIMITER_2) }
     }
