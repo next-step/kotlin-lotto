1dfc42d52ad333511ca65f2f69a0d22e98e44b6e
kit(김광수) <kit.t@kakaocorp.com>
2021-03-05 22:31:54 +0900

    test: 0으로 나눌 수 없다

--- src/test/kotlin/lotto/LottoTicketTest.kt
+++ src/test/kotlin/lotto/LottoTicketTest.kt
@@ -2,11 +2,18 @@ package lotto
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
 
 class LottoTicketTest {
     @Test
     fun `돈으로 티캣을 생성한다`() {
         assertThat(LottoTicket(Money(14_000)).count()).isEqualTo(14)
+    }
+
+    @Test
+    fun `0으로 나눌 수 없다`() {
+        assertThrows<IllegalArgumentException> { Quotient(Money(1_000), 0) }
     }
 
     class LottoTicket(private val money: Money) {
