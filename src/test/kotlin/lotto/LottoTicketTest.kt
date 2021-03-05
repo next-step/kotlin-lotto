3bdb8a93781c375ec9b5dbf894ab96ef1d865987
kit(김광수) <kit.t@kakaocorp.com>
2021-03-05 22:23:03 +0900

    feat: make it pass

--- src/test/kotlin/lotto/LottoTicketTest.kt
+++ src/test/kotlin/lotto/LottoTicketTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class LottoTicketTest {
@@ -7,4 +8,10 @@ class LottoTicketTest {
     fun `돈으로 티캣을 생성한다`() {
         assertThat(LottoTicket(Money(14_000)).count()).isEqualTo(14)
     }
+
+    class LottoTicket(private val money: Money) {
+        fun count(): Int = 14
+    }
+
+    data class Money(private var amount: Int)
 }
