9d4f787755f5cca26489f89cc6ce2c65aa5d54db
kit(김광수) <kit.t@kakaocorp.com>
2021-03-06 12:07:16 +0900

    feat: make it pass

--- src/test/kotlin/lotto/LottoDrawMachineTest.kt
+++ src/test/kotlin/lotto/LottoDrawMachineTest.kt
@@ -7,4 +7,8 @@ class LottoDrawMachineTest {
     fun `추출할 번호들을 입력받을 수 있다`() {
         LottoDrawMachine(1..45)
     }
+
+    class LottoDrawMachine(pool: Set<Int>) {
+        constructor(range: IntRange) : this(range.toSet())
+    }
 }
