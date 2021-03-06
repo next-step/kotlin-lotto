1966d06db07040414435883f5633656bea147bc6
palmseung <palmseung@gmail.com>
2021-03-06 23:07:24 +0900

    feat(lotto): add feature to create Money when Negative input money value

--- src/main/kotlin/lotto/Money.kt
+++ src/main/kotlin/lotto/Money.kt
@@ -1,5 +1,9 @@
 package lotto
 
 data class Money(val value: Int) {
+    init {
+        require(value >= 0) {"로또 구매 금액은 0원보다 커야 합니다."}
+    }
+
     constructor(input: String) : this(input.toInt())
 }
