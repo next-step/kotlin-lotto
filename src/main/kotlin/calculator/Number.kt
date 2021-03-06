04fd78c199642f11c36b28f1fb7cc31084cb9d0e
bactoria <bactoria@gmail.com>
2021-03-06 17:14:19 +0900

    feat: 숫자 생성할 수 있다

--- src/main/kotlin/calculator/Number.kt
+++ src/main/kotlin/calculator/Number.kt
@@ -1,4 +1,18 @@
 package calculator
 
-class Number(value: Int) {
+class Number(private val value: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Number
+
+        if (value != other.value) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return value
+    }
 }
