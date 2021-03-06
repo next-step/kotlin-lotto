82c30a95bfd821252d395faf11cff3c9169b2582
kit(김광수) <kit.t@kakaocorp.com>
2021-03-06 12:48:38 +0900

    refactor: 중복테스트 삭제

--- src/test/kotlin/lotto/LottoDrawMachineTest.kt
+++ src/test/kotlin/lotto/LottoDrawMachineTest.kt
@@ -7,11 +7,6 @@ import org.junit.jupiter.api.assertThrows
 
 class LottoDrawMachineTest {
     @Test
-    fun `추출할 번호들을 입력받을 수 있다`() {
-        LottoDrawMachine(1..45)
-    }
-
-    @Test
     fun `번호는 45개이다`() {
         assertThat(LottoDrawMachine(1..45).size).isEqualTo(45)
     }
