1ffd63cd5c66e8a7fa8b2c1685a2011f94825872
palmseung <palmseung@gmail.com>
2021-03-07 01:35:36 +0900

    feat(lotto): add feature to get EarningRate

--- src/main/kotlin/lotto/model/Lottos.kt
+++ src/main/kotlin/lotto/model/Lottos.kt
@@ -11,10 +11,21 @@ class Lottos(val lottos: List<Lotto>) {
     }
 
     fun getEarningRate(winningNumbers: List<Int>): Double {
-        return 0.2
+        val totalPrizeMoney = getTotalPrizeMoney(winningNumbers)
+        val budgetMoney = lottos.size * COST_PER_ONE_LOTTO
+        return totalPrizeMoney.div(budgetMoney)
+    }
+
+    private fun getTotalPrizeMoney(winningNumbers: List<Int>): Double {
+        return Coincidence.values()
+            .map { check(winningNumbers, it.coincidenceCount) * it.prizeMoney}
+            .sum()
+            .toDouble()
     }
 
     companion object {
+        private const val COST_PER_ONE_LOTTO: Double = 1000.toDouble()
+
         private fun createLottos(count: Int): List<Lotto> {
             return (1..count).map { Lotto(LottoNumberPool().getLottoNumbers()) }
         }
