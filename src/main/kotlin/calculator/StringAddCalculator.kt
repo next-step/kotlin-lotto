c6614512074997ca239fffa0e291ae5235e94eb5
hyunssooo <jhs695@gmail.com>
2021-03-06 14:33:25 +0900

    [Step1] 문자열 덧셈 계산기 (#83)
    * feat: 빈문자열 또는 null일 경우 0 반환
    
    * feat: 숫자 하나를 문자열로 입력할 경우 해당 숫자 반환
    
    * feat: 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환
    
    * feat: 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다.
    
    * feat: "//"와 "\n" 문자 사이에 커스텀 구분자를 지정할 수 있다.
    
    * feat: 음수를 전달할 경우 RuntimeException 예외가 발생해야 한다.
    
    * refactor: 메서드 분리
    
    * test: 테스트 추가
    
    * chore: empty.txt 삭제
    
    * refactor: 메서드 분리

---
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -0,0 +1,45 @@
+package calculator
+
+class StringAddCalculator {
+
+    fun add(text: String?): Int {
+        if (text.isNullOrBlank()) {
+            return 0
+        }
+
+        val numbers = parseNumbers(text)
+        checkNegative(numbers)
+
+        return numbers.sum()
+    }
+
+    private fun parseNumbers(text: String): List<Int> {
+        val customDelimiterPatterns = findCustomDelimiters(text).map { "[$it]" }
+        val regex = DEFAULT_DELIMITER_PATTERNS.plus(customDelimiterPatterns)
+            .joinToString(separator = "|")
+            .toRegex()
+
+        val removedCustomPatternText = text.replace(CUSTOM_DELIMITER_PATTERN.toRegex(), EMPTY)
+
+        return removedCustomPatternText.split(regex).map { it.toInt() }
+    }
+
+    private fun findCustomDelimiters(text: String): List<String> {
+        return Regex(CUSTOM_DELIMITER_PATTERN).findAll(text)
+            .map {
+                it.let { it.groupValues[1] }
+            }.toList()
+    }
+
+    private fun checkNegative(numbers: List<Int>) {
+        if (numbers.any { it < 0 }) {
+            throw RuntimeException()
+        }
+    }
+
+    companion object {
+        private val DEFAULT_DELIMITER_PATTERNS = listOf("[,]", "[:]")
+        private const val CUSTOM_DELIMITER_PATTERN = "//(.)\n"
+        private const val EMPTY = "";
+    }
+}
---
+++ src/test/kotlin/calculator/StringTest.kt
@@ -0,0 +1,30 @@
+package calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class StringTest {
+
+    @Test
+    fun test() {
+        assertThat("1,2".contains(",")).isTrue()
+        assertThat("1,2".replace(",".toRegex(), "")).isEqualTo("12")
+        assertThat("1:2".replace(":".toRegex(), "")).isEqualTo("12")
+        assertThat("1,2:".replace("[,]|[:]".toRegex(), "")).isEqualTo("12")
+        val input = "//;\n1//^\n2;3"
+        val customDelimiters = Regex("//(.)\n").findAll(input)
+            .map {
+                it.let {
+                    it.groupValues[1]
+                }
+            }.toList()
+        assertThat(customDelimiters).containsAll(listOf(";", "^"))
+    }
+
+    @Test
+    fun test2() {
+        val list = listOf<Int>()
+        assertThat(list).isEmpty()
+        assertThat(list.sum()).isEqualTo(0)
+    }
+}
