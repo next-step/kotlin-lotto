367fcdb7128ac4663097e2ce3423758347c06f97
palmseung <palmseung@gmail.com>
2021-03-07 01:10:04 +0900

    test(lotto): add test to get count for check count with winning numbers

--- src/main/kotlin/lotto/model/Lottos.kt
+++ src/main/kotlin/lotto/model/Lottos.kt
@@ -3,6 +3,10 @@ package lotto.model
 class Lottos(val lottos: List<Lotto>) {
     constructor(count: Int) : this(createLottos(count))
 
+    fun check(winningNumbers: List<Int>, checkCount: Int): Int {
+
+    }
+
     companion object {
         private fun createLottos(count: Int): List<Lotto> {
             return (1..count).map { Lotto(LottoNumberPool().getLottoNumbers()) }
--- src/test/kotlin/lotto/model/LottosTest.kt
+++ src/test/kotlin/lotto/model/LottosTest.kt
@@ -2,6 +2,8 @@ package lotto.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class LottosTest {
     @Test
@@ -14,5 +16,26 @@ internal class LottosTest {
 
         // then
         assertThat(myLottos.lottos.size).isEqualTo(5)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [3, 4, 5, 6])
+    fun `당첨번호와 일치카운트를 인자로 주면, 해당 당첨번호와 일치카운트만큼 일치하는 로또 갯수를 반환한다`(checkCount: Int) {
+        // given
+        val lottos = Lottos(
+            listOf(
+                Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                Lotto(listOf(1, 2, 3, 4, 5, 7)),
+                Lotto(listOf(1, 2, 3, 4, 8, 9)),
+                Lotto(listOf(1, 2, 3, 10, 11, 12))
+            )
+        )
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+
+        // when
+        val count = lottos.check(winningNumbers, checkCount)
+
+        // then
+        assertThat(count).isEqualTo(1)
     }
 }
