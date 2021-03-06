1cf6d8db68cb4fcfc34fd18e0d280d937dcaef5f
palmseung <palmseung@gmail.com>
2021-03-06 23:59:10 +0900

    feat(lotto): add feature to create LottoNumbers when their size is not 6

--- src/main/kotlin/lotto/model/LottoNumbers.kt
+++ src/main/kotlin/lotto/model/LottoNumbers.kt
@@ -1,4 +1,11 @@
 package lotto.model
 
-class LottoNumbers(val lottoNumbers: List<LottoNumber>) {
+class LottoNumbers(private val lottoNumbers: List<LottoNumber>) {
+    init {
+        require(lottoNumbers.size == LOTTO_SIZE) {"로또 번호는 6개여야만 합니다."}
+    }
+
+    companion object {
+        private const val LOTTO_SIZE = 6
+    }
 }
