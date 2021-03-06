474062e45e16dad1289904d88e9d909af97ff9d4
palmseung <palmseung@gmail.com>
2021-03-06 21:45:05 +0900

    feat(string-adder): add feature to create with string input

--- src/main/kotlin/adder/model/Numbers.kt
+++ src/main/kotlin/adder/model/Numbers.kt
@@ -3,9 +3,20 @@ package adder.model
 data class Numbers(val nums: List<Number>) {
     constructor(input: Collection<String>) : this(input.map(::Number))
 
-    constructor(input: String) : this()
+    constructor(input: String) : this(splitByDefault(input))
 
     fun getSum(): Int {
         return nums.sumBy { it.value }
+    }
+
+    companion object {
+        private const val REGEX_FOR_DELIMITER = "^//.\\n"
+        private const val DELIMITER_1 = ","
+        private const val DELIMITER_2 = ";"
+
+        private fun splitByDefault(input: String): List<String> {
+            val matches = input.matches(Regex(REGEX_FOR_DELIMITER))
+            require(!matches).apply { return input.split(DELIMITER_1, DELIMITER_2) }
+        }
     }
 }
