776d575ba49d052cfcdb62e027a90c24141ffb13
bactoria <bactoria@gmail.com>
2021-03-06 17:30:43 +0900

    test: 숫자가 아닌 문자열로 Number 생성 시 예외

--- src/test/kotlin/calculator/NumberTest.kt
+++ src/test/kotlin/calculator/NumberTest.kt
@@ -2,6 +2,7 @@ package calculator
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 internal class NumberTest {
 
@@ -13,7 +14,7 @@ internal class NumberTest {
 
         assertThat(result).isEqualTo(expected)
     }
-    
+
     @Test
     fun `문자열로 Number 생성할 수 있다`() {
         val input: String = "1"
@@ -21,5 +22,14 @@ internal class NumberTest {
         val result: Number = Number(input)
 
         assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `숫자가 아닌 문자열로 Number를 생성하는 경우 예외를 반환한다`() {
+        val input: String = "z"
+        val expectedMessage: String = "허용하지 않는 문자열입니다. value: $input"
+        val result: IllegalArgumentException = assertThrows { Number(input) }
+
+        assertThat(result.message).isEqualTo(expectedMessage)
     }
 }
