fcd136978f7ee32ab65d24bd1f66869fe9aa7d3b
palmseung <palmseung@gmail.com>
2021-03-06 23:36:04 +0900

    feat(lotto): add feature to validate number within 1 to 45

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -1,4 +1,12 @@
 package lotto
 
-class LottoNumber(val number: Int) {
+class LottoNumber(number: Int) {
+    init {
+        require(number in MIN..MAX) { "${number}는 로또 번호가 될 수 없습니다." }
+    }
+
+    companion object {
+        private const val MIN = 1
+        private const val MAX = 45
+    }
 }
