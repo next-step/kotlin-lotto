ea6f75383fd475574346468099eaddd85899be01
bactoria <bactoria@gmail.com>
2021-03-06 17:07:36 +0900

    test: 숫자 생성할 수 있다

---
+++ src/test/kotlin/calculator/NumberTest.kt
@@ -0,0 +1,16 @@
+package calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class NumberTest {
+
+    @Test
+    fun `숫자로 Number 생성할 수 있다`() {
+        val input: Int = 1
+        val expected: Number = Number(1)
+        val result: Number = Number(input)
+
+        assertThat(result).isEqualTo(expected)
+    }
+}
