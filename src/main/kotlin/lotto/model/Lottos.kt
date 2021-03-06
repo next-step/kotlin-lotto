c51f5f41a1368d2cb42c9ac9ac44faf627057dc9
palmseung <palmseung@gmail.com>
2021-03-07 01:00:17 +0900

    feat(lotto): add feature to create Lottos for input lotto count

--- src/main/kotlin/lotto/model/Lottos.kt
+++ src/main/kotlin/lotto/model/Lottos.kt
@@ -1,5 +1,11 @@
 package lotto.model
 
 class Lottos(val lottos: List<Lotto>) {
-    constructor(count: Int): this()
+    constructor(count: Int) : this(createLottos(count))
+
+    companion object {
+        private fun createLottos(count: Int): List<Lotto> {
+            return (1..count).map { Lotto(LottoNumberPool().getLottoNumbers()) }
+        }
+    }
 }
