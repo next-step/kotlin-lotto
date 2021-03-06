f4be88c8a8fc25d3bdfaa4789f1935178fa1eb01
palmseung <palmseung@gmail.com>
2021-03-06 21:10:42 +0900

    test(string-adder): add test to create with string input

--- src/main/kotlin/adder/model/Numbers.kt
+++ src/main/kotlin/adder/model/Numbers.kt
@@ -1,9 +1,11 @@
 package adder.model
 
 data class Numbers(val nums: List<Number>) {
-    constructor(input :Collection<String>): this(input.map(::Number))
+    constructor(input: Collection<String>) : this(input.map(::Number))
 
-    fun getSum(): Int{
+    constructor(input: String) : this()
+
+    fun getSum(): Int {
         return nums.sumBy { it.value }
     }
 }
