093559cdf167801d3852e61240dc8ad145bd9ead
palmseung <palmseung@gmail.com>
2021-03-06 19:14:22 +0900

    feat(string-adder): add feature to create when negative number

--- src/main/kotlin/adder/Number.kt
+++ src/main/kotlin/adder/Number.kt
@@ -1,5 +1,11 @@
 package adder
 
+private const val ZERO = 0
+
 data class Number(private val value: Int) {
+    init {
+        require(value > ZERO) {"양수만 가능합니다."}
+    }
+
     constructor(input: String) : this(input.toInt())
 }
