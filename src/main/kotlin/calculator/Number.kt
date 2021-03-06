e72320c1050215389c77cb3ce226645297ae3ddd
bactoria <bactoria@gmail.com>
2021-03-06 17:15:27 +0900

    test: 문자열로 숫자 생성할 수 있다

--- src/main/kotlin/calculator/Number.kt
+++ src/main/kotlin/calculator/Number.kt
@@ -1,6 +1,9 @@
 package calculator
 
 class Number(private val value: Int) {
+
+    constructor(value: String): this(0)
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
--- src/test/kotlin/calculator/NumberTest.kt
+++ src/test/kotlin/calculator/NumberTest.kt
@@ -13,4 +13,13 @@ internal class NumberTest {
 
         assertThat(result).isEqualTo(expected)
     }
+    
+    @Test
+    fun `문자열로 Number 생성할 수 있다`() {
+        val input: String = "1"
+        val expected: Number = Number(1)
+        val result: Number = Number(input)
+
+        assertThat(result).isEqualTo(expected)
+    }
 }
