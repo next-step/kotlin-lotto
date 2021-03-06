b7b5e7e238e9db517ea15395150c2d3dc134cae2
palmseung <palmseung@gmail.com>
2021-03-06 23:57:40 +0900

    test(lotto): add test to create LottoNumbers when their size is not 6

--- src/test/kotlin/lotto/model/LottoNumbersTest.kt
+++ src/test/kotlin/lotto/model/LottoNumbersTest.kt
@@ -2,6 +2,8 @@ package lotto.model
 
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
 
 internal class LottoNumbersTest {
     @Test
@@ -16,8 +18,22 @@ internal class LottoNumbersTest {
             LottoNumber(6)
         )
 
-
         // when, then
         assertDoesNotThrow { LottoNumbers(lottoNumbers) }
+    }
+
+    @Test
+    fun `LottoNumber의 갯수가 6개가 아니면 예외가 발생한다`() {
+        // given
+        val lottoNumbers = listOf(
+            LottoNumber(1),
+            LottoNumber(2),
+            LottoNumber(3),
+            LottoNumber(4),
+            LottoNumber(5)
+        )
+
+        // when, then
+        assertThrows<IllegalArgumentException> { LottoNumbers(lottoNumbers) }
     }
 }
