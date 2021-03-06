3602316d5d936359d9509eaa063b2cccaf22b8ef
정주영 <jjyoung0216@naver.com>
2021-03-07 00:28:30 +0900

    feat(StringAddCalculator) 구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.

--- src/main/kotlin/caculator/StringAddCalculator.kt
+++ src/main/kotlin/caculator/StringAddCalculator.kt
@@ -1,16 +1,16 @@
 package caculator
 
-internal class StringAddCalculator {
+internal class StringAddCalculator(private val splitters: List<String> = DEFAULT_SPLITTERS) {
+
     fun add(positiveNumbers: String?): Int {
         if (positiveNumbers.isNullOrEmpty()) {
             return 0
         }
-
-        val numbers = positiveNumbers.split(SPLITTER)
+        val numbers = positiveNumbers.split(*this.splitters.toTypedArray())
         return numbers.map(String::toInt).sum()
     }
 
     companion object {
-        const val SPLITTER = ","
+        private val DEFAULT_SPLITTERS = listOf(",", ":")
     }
 }
