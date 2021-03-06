94f76f283dced225dd81d51286b28827e077e347
palmseung <palmseung@gmail.com>
2021-03-07 00:24:24 +0900

    feat(lotto): add feature to getLottoNumbers from LottoNumberPool

--- src/main/kotlin/lotto/model/LottoNumberPool.kt
+++ src/main/kotlin/lotto/model/LottoNumberPool.kt
@@ -2,6 +2,15 @@ package lotto.model
 
 class LottoNumberPool {
     fun getLottoNumbers(): List<Int> {
-        TODO("Not yet implemented")
+        return pool.shuffled().subList(INDEX_FIRST_INCLUSIVE, INDEX_LAST_EXCLUSIVE).sorted()
+    }
+
+    companion object {
+        private const val MIN = 1
+        private const val MAX = 45
+        private const val INDEX_FIRST_INCLUSIVE = 0
+        private const val INDEX_LAST_EXCLUSIVE = 6
+
+        private val pool = (MIN..MAX).toSet()
     }
 }
