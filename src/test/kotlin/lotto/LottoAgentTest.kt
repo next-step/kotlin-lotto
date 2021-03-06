701191a4728e3ab238d883b313146fcb97f0d998
kit(김광수) <kit.t@kakaocorp.com>
2021-03-06 13:20:39 +0900

    refactor: Exchange 를 인터페이스로 변경

--- src/test/kotlin/lotto/LottoAgentTest.kt
+++ src/test/kotlin/lotto/LottoAgentTest.kt
@@ -9,19 +9,25 @@ import org.junit.jupiter.api.Test
 class LottoAgentTest {
     @Test
     fun `구매수량만큼 로또번호를 생성한다`() {
-        val exchange: Exchange = LottoAgent(LottoDrawMachine((1..45))).exchange()
+        val exchange: Exchange<LottoNumber> = LottoAgent(LottoDrawMachine((1..45))).exchange
         exchange.pay(Money(14_000))
         assertThat(exchange.product()).isNotNull
     }
 
-    class LottoAgent(lottoDrawMachine: LottoDrawMachine) {
-        fun exchange(): Exchange = Exchange()
+    class LottoAgent(val exchange: Exchange<LottoNumber>) {
+        constructor(lottoExchange: LottoDrawMachine) : this(LottoExchange(lottoExchange))
+
+        class LottoExchange(lottoExchange: LottoDrawMachine) : Exchange<LottoNumber> {
+            override fun pay(money: Money) {
+            }
+
+            override fun product(): List<LottoNumber> = listOf()
+        }
     }
 
-    class Exchange {
-        fun pay(money: Money) {
-        }
+    interface Exchange<Product> {
+        fun pay(money: Money)
 
-        fun product(): List<LottoNumber> = listOf()
+        fun product(): List<Product>
     }
 }
