2426c9cd7fdca0495147e1f8a345626cc77e633f
palmseung <palmseung@gmail.com>
2021-03-07 02:35:22 +0900

    feat(lotto): add InstructionView, ResultView

---
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -0,0 +1,28 @@
+package lotto.view
+
+import lotto.model.Coincidence
+import lotto.model.Lotto
+import lotto.model.Lottos
+
+object ResultView {
+    fun printMyLottos(lottos: Lottos) {
+        lottos.lottos.forEach { println(it.lottoNumbers.toString()) }
+        println()
+    }
+
+    fun printLottoCount(lottoCount: Int) {
+        println("${lottoCount}개를 구매했습니다.")
+    }
+
+    fun printResultStatistics(myLottos: Lottos, winningLotto: Lotto) {
+        Coincidence.values().forEach {
+            val count = myLottos.check(winningLotto.lottoNumbers.map { it.number }, it.coincidenceCount)
+            println("${it.coincidenceCount}개 일치 (${it.prizeMoney}원) - ${count}개 ")
+        }
+    }
+
+    fun printEarningRate(myLottos: Lottos, winningLotto: Lotto) {
+        val earningRate = myLottos.getEarningRate(winningLotto.lottoNumbers.map { it.number })
+        println("총 수익률은 $earningRate")
+    }
+}
