c7d4d5d342dbaa237f56fcfe51266567e4cb123f
palmseung <palmseung@gmail.com>
2021-03-07 00:06:11 +0900

    feat(lotto): add feature to validate all numbers are different within 1 to 45

--- src/main/kotlin/lotto/model/LottoNumbers.kt
+++ src/main/kotlin/lotto/model/LottoNumbers.kt
@@ -2,10 +2,15 @@ package lotto.model
 
 class LottoNumbers(private val lottoNumbers: List<LottoNumber>) {
     init {
-        require(lottoNumbers.size == LOTTO_SIZE) {"로또 번호는 6개여야만 합니다."}
+        require(isUniqueSixNumbers(lottoNumbers)) {"로또 번호는 서로 다른 6개의 숫자여야만 합니다."}
     }
 
     companion object {
         private const val LOTTO_SIZE = 6
+
+        private fun isUniqueSixNumbers(lottoNumbers: List<LottoNumber>): Boolean {
+            val size = lottoNumbers.sortedBy { it.number }.toSet().size
+            return (size == LOTTO_SIZE)
+        }
     }
 }
