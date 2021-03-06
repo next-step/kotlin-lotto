23afc7b19829ba22c0593e0dd6358c942c248ac4
kit(김광수) <kit.t@kakaocorp.com>
2021-03-06 12:16:12 +0900

    test: 45개 미만이면 예외가 발생한다

--- src/test/kotlin/lotto/LottoDrawMachineTest.kt
+++ src/test/kotlin/lotto/LottoDrawMachineTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class LottoDrawMachineTest {
     @Test
@@ -12,6 +13,11 @@ class LottoDrawMachineTest {
     @Test
     fun `번호는 45개이다`() {
         assertThat(LottoDrawMachine(1..45).size).isEqualTo(45)
+    }
+
+    @Test
+    fun `45개 미만이면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> { LottoDrawMachine((1..44).toSet() + setOf(1)) }
     }
 
     class LottoDrawMachine(pool: Set<Int>) {
