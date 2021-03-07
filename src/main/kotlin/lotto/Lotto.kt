c359431c94752d70e93a110dc10b985061e2b003
bactoria <bactoria@gmail.com>
2021-03-07 22:15:48 +0900

    feat: 로또번호 개수가 6개가 아닌 경우 로또 생성 불가능

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,3 +1,8 @@
 package lotto
 
-class Lotto(lottoNumbers: List<LottoNumber>)
+class Lotto(lottoNumbers: List<LottoNumber>) {
+    
+    init {
+        require(lottoNumbers.size == 6) { "로또번호 개수가 6개가 아닙니다." }
+    }
+}
