59086d96abc1337d72c3353b9d7290ea87e8bb36
bactoria <bactoria@gmail.com>
2021-03-07 23:18:33 +0900

    test: 로또금액 유효성 검사

--- src/test/kotlin/lotto/LottoMachineTest.kt
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -2,6 +2,9 @@ package lotto
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class LottoMachineTest {
 
@@ -10,5 +13,13 @@ class LottoMachineTest {
         val lottoPrice = 1000
         val result = LottoMachine(lottoPrice = lottoPrice)
         assertThat(result).isNotNull
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [-1000, 0])
+    fun `로또금액이 음수이거나 0인 경우 예외를 반환한다`(lottoPrice: Int) {
+        val expectedMessage = "로또금액은 0보다 커야합니다"
+        val result = assertThrows<IllegalArgumentException> { LottoMachine(lottoPrice = lottoPrice) }
+        assertThat(result.message).isEqualTo(expectedMessage)
     }
 }
