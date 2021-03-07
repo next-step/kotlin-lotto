92ed312b4a8387419d533f0f6baa172b38631cc3
sangw0804 <sangw0804@likelion.org>
2021-03-07 20:18:30 +0900

    test(LottoStoreTest): 0원 구매 테스트 추가

--- src/main/kotlin/lotto/model/Money.kt
+++ src/main/kotlin/lotto/model/Money.kt
@@ -1,7 +1,13 @@
 package lotto.model
 
-class Money(val amount: Int) {
+class Money(private val amount: Int) {
     fun dividedBy(price: Money): Int {
         return amount / price.amount
+    }
+
+    companion object {
+        fun zero(): Money {
+            return Money(0)
+        }
     }
 }
--- src/test/kotlin/lotto/model/LottoStoreTest.kt
+++ src/test/kotlin/lotto/model/LottoStoreTest.kt
@@ -6,12 +6,13 @@ import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 
 internal class LottoStoreTest {
+    private val store = LottoStore()
+
     @ParameterizedTest
     @MethodSource("buyTicketCountProvider")
     fun `상점에 돈을 주고 로또를 사면 1000원 당 한장의 LottoTicket 을 준다`(money: Money, ticketCount: Int) {
-        val store = LottoStore()
-
         val result = store.buy(money)
+
         assertThat(result.size).isEqualTo(ticketCount)
     }
 
@@ -20,7 +21,8 @@ internal class LottoStoreTest {
         fun buyTicketCountProvider(): List<Arguments> {
             return listOf(
                 Arguments { arrayOf(Money(1000), 1) },
-                Arguments { arrayOf(Money(3000), 3) }
+                Arguments { arrayOf(Money(3000), 3) },
+                Arguments { arrayOf(Money.zero(), 0) }
             )
         }
     }
