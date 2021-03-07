a0cf261e21fc9069cd06b14eac314c55c5050262
bactoria <bactoria@gmail.com>
2021-03-07 17:25:29 +0900

    refactor: data class로 변경

--- src/main/kotlin/calculator/PositiveNumber.kt
+++ src/main/kotlin/calculator/PositiveNumber.kt
@@ -1,6 +1,6 @@
 package calculator
 
-class PositiveNumber(val value: Int) {
+data class PositiveNumber(val value: Int) {
 
     init {
         require(value > 0) { "양수만 허용합니다. value: $value" }
@@ -12,20 +12,5 @@ class PositiveNumber(val value: Int) {
 
     fun plus(that: PositiveNumber): PositiveNumber {
         return PositiveNumber(this.value + that.value)
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as PositiveNumber
-
-        if (value != other.value) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return value
     }
 }
