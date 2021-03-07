fec8d52c7ccc153cb89f3afcb8f970a25d2853b4
정주영 <jjyoung0216@naver.com>
2021-03-08 01:17:36 +0900

    feat(lotto) 로또의 당청 금액을 구한다

---
+++ src/main/kotlin/lotto/Rank.kt
@@ -0,0 +1,18 @@
+package lotto
+
+enum class Rank(private val matchCount: Int, private val price: Long) {
+
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
+
+    MISS(0, 0);
+
+    companion object {
+        fun find(matchCount: Int): Rank {
+            return values().firstOrNull { it.matchCount == matchCount }
+                ?: Rank.MISS
+        }
+    }
+}
