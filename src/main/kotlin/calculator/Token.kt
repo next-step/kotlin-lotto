5eebaf3731943cfab071152acdb60dd145f5ddea
bactoria <bactoria@gmail.com>
2021-03-07 16:30:37 +0900

    feat: 토큰 추가

--- src/main/kotlin/calculator/Token.kt
+++ src/main/kotlin/calculator/Token.kt
@@ -3,4 +3,20 @@ package calculator
 class Token(input: String) {
 
     val values: List<String>
+
+    init {
+        require(input.isNotBlank()) { "문자열은 공백일 수 없습니다. input: $input" }
+
+        values = REGEX.find(input)?.let {
+            val delimiter = it.groupValues[1]
+            val tokenStr = it.groupValues[2]
+            tokenStr.split(delimiter)
+        } ?: input.split(DEFAULT_DELIMITER_COMMA, DEFAULT_DELIMITER_COLON)
+    }
+
+    companion object {
+        private val REGEX: Regex = """//(.)\n(.*)""".toRegex()
+        private const val DEFAULT_DELIMITER_COMMA = ","
+        private const val DEFAULT_DELIMITER_COLON = ":"
+    }
 }
