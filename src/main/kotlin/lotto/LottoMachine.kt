6c44255d5722de06f30379e00bb79832e8c9ded5
bactoria <bactoria@gmail.com>
2021-03-08 01:49:38 +0900

    refactor: 디커플링

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -1,13 +1,12 @@
 package lotto
 
-class LottoMachine(private val lottoPrice: Int) {
+class LottoMachine(private val lottoPrice: Int, private val lottoGenerator: LottoGenerator) {
 
     init {
         require(lottoPrice > 0) { "로또금액은 0보다 커야합니다" }
     }
 
     fun sellLottos(money: Int): List<Lotto> {
-        val lottoGenerator = RandomLottoGenerator()
         val sellableLottoCount = money / lottoPrice
         return (1..sellableLottoCount).map { lottoGenerator.generate() }
     }
--- src/test/kotlin/lotto/LottoMachineTest.kt
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -12,7 +12,7 @@ class LottoMachineTest {
     @Test
     fun `로또머신은 생성 시 로또금액을 입력받는다`() {
         val lottoPrice = 1000
-        val result = LottoMachine(lottoPrice = lottoPrice)
+        val result = LottoMachine(lottoPrice = lottoPrice, lottoGenerator = DummyLottoGenerator())
         assertThat(result).isNotNull
     }
 
@@ -20,7 +20,9 @@ class LottoMachineTest {
     @ValueSource(ints = [-1000, 0])
     fun `로또금액이 음수이거나 0인 경우 예외를 반환한다`(lottoPrice: Int) {
         val expectedMessage = "로또금액은 0보다 커야합니다"
-        val result = assertThrows<IllegalArgumentException> { LottoMachine(lottoPrice = lottoPrice) }
+        val result = assertThrows<IllegalArgumentException> {
+            LottoMachine(lottoPrice = lottoPrice, lottoGenerator = DummyLottoGenerator())
+        }
         assertThat(result.message).isEqualTo(expectedMessage)
     }
 
@@ -31,7 +33,7 @@ class LottoMachineTest {
         "250, 1000, 4"
     )
     fun `로또금액에 따라 로또를 여러장 판매한다`(lottoPrice: Int, money: Int, expected: Int) {
-        val lottoMachine = LottoMachine(lottoPrice = lottoPrice)
+        val lottoMachine = LottoMachine(lottoPrice = lottoPrice, lottoGenerator = FakeLottoGenerator())
         val result = lottoMachine.sellLottos(money = money)
         assertThat(result.size).isEqualTo(expected)
     }
