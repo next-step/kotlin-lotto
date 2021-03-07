ab38daa6697c08570e0a3a04f4abd0373ed38717
bactoria <bactoria@gmail.com>
2021-03-08 02:11:08 +0900

    feat: 로또 판매 시 받는 돈 검증

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -7,8 +7,9 @@ class LottoMachine(private val lottoPrice: Int, private val lottoGenerator: Lott
     }
 
     fun sellLottos(money: Int): List<Lotto> {
+        require( money >= lottoPrice ) { "금액은 로또가격보다 크거나 같아야 합니다. money: $money, lottoPrice: $lottoPrice" }
         require(money % lottoPrice == 0) { "로또 구매 후 남은 돈이 있을 수 없습니다. money: $money, lottoPrice: $lottoPrice" }
-
+        
         val sellableLottoCount = money / lottoPrice
         return (1..sellableLottoCount).map { lottoGenerator.generate() }
     }
