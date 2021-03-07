f10d89d9a8870bcf03c4a6eef8077310f012afb7
bactoria <bactoria@gmail.com>
2021-03-08 01:14:34 +0900

    feat: 랜덤한 로또 생성

--- src/main/kotlin/lotto/RandomLottoGenerator.kt
+++ src/main/kotlin/lotto/RandomLottoGenerator.kt
@@ -2,7 +2,15 @@ package lotto
 
 class RandomLottoGenerator {
 
+    private val lottoNumberPool: List<LottoNumber> = (MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER).map { LottoNumber(it) }
+
     fun generate(): Lotto {
-        return Lotto((1..6).map { LottoNumber(it) })
+        return lottoNumberPool.shuffled().subList(0, LOTTO_NUMBER_COUNT).let { Lotto(it) }
+    }
+
+    companion object {
+        private const val MIN_LOTTO_NUMBER = 1
+        private const val MAX_LOTTO_NUMBER = 45
+        private const val LOTTO_NUMBER_COUNT = 6
     }
 }
