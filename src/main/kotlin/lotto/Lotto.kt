4385a255c85eedf8bdcb65064ff48c1c6e484005
bactoria <bactoria@gmail.com>
2021-03-07 22:54:22 +0900

    feat: 중복된 로또번호가 있는 경우 로또 생성 불가능

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,11 +1,16 @@
 package lotto
 
 class Lotto(lottoNumbers: List<LottoNumber>) {
-    
+
     init {
         require(lottoNumbers.size == VALID_LOTTO_NUMBER_COUNT) { "로또번호 개수가 6개가 아닙니다." }
+        require(hasNotDuplicatedLottoNumber(lottoNumbers)) { "중복된 로또번호가 있습니다." }
     }
-    
+
+    private fun hasNotDuplicatedLottoNumber(lottoNumbers: List<LottoNumber>): Boolean {
+        return lottoNumbers.distinct().size == VALID_LOTTO_NUMBER_COUNT
+    }
+
     companion object {
         private const val VALID_LOTTO_NUMBER_COUNT = 6
     }
