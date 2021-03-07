670d65a47f1b8388311e5be499baada3ebac3ef9
bactoria <bactoria@gmail.com>
2021-03-07 18:52:07 +0900

    feat: 로또번호 동등성

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -1,3 +1,13 @@
 package lotto
 
-class LottoNumber(value: Int)
+class LottoNumber(value: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
+}
