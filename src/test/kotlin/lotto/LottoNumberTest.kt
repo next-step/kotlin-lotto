6e7a6ff89bdd02a2a91314ddf064dccc692d1ccc
bactoria <bactoria@gmail.com>
2021-03-07 18:56:06 +0900

    test: 로또번호 값이 다르면 동등하지 않다

--- src/test/kotlin/lotto/LottoNumberTest.kt
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -11,11 +11,18 @@ class LottoNumberTest {
         val result = LottoNumber(value)
         assertThat(result).isNotNull
     }
-    
+
     @Test
     fun `로또번호의 값이 같으면 동등하다`() {
         val expected = LottoNumber(1)
         val result = LottoNumber(1)
         assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `로또번호의 값이 다르면 동등하지 않다`() {
+        val one = LottoNumber(1)
+        val two = LottoNumber(2)
+        assertThat(one).isNotEqualTo(two)
     }
 }
