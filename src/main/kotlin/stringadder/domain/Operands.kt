f2cef88a8b6426ec2c5f49a6b4c1f531d5c59635
남윤서 <devysnam@gmail.com>
2021-03-08 02:57:38 +0900

    test: 변경사항 테스트 추가

--- src/main/kotlin/stringadder/domain/Operands.kt
+++ src/main/kotlin/stringadder/domain/Operands.kt
@@ -1,5 +1,5 @@
 package stringadder.domain
 
-class Operands(val operands: List<Operand>) {
+class Operands(private val operands: List<Operand>) {
     fun sum() = operands.map { it.operand }.sum()
 }
--- src/test/kotlin/stringadder/domain/ParserTest.kt
+++ src/test/kotlin/stringadder/domain/ParserTest.kt
@@ -9,43 +9,37 @@ import org.junit.jupiter.params.provider.ValueSource
 
 internal class ParserTest {
 
-    @DisplayName("구분자가 포함되지 않은 문자열을 입력 받은 경우 기본 구분자를 기준으로 구분된 숫자 반환")
+    @DisplayName("구분자가 포함되지 않은 문자열을 입력 받은 경우")
     @ParameterizedTest
     @ValueSource(strings = ["1,2,3", "1:2:3"])
-    fun initWithoutCustomDelimiter(input: String) {
-        val expected = createOperands("1", "2", "3")
+    fun parse(input: String) {
+        val expression = Parser.parse(input)
 
-        val actual = Parser(input).operands
-
-        assertThat(actual.operands.containsAll(expected.operands)).isTrue
+        assertThat(expression.result()).isEqualTo(6)
     }
 
     @DisplayName("구분자가 포함된 문자열을 입력 받은 경우 구분자들을 기준으로 구분된 숫자 반환")
     @ParameterizedTest
     @ValueSource(strings = ["//a\\n1a2a3", "//a\\n1,2,3", "//a\\n1:2:3"])
-    fun initWithCustomDelimiter(input: String) {
-        val expected = createOperands("1", "2", "3")
+    fun parse2(input: String) {
+        val expression = Parser.parse(input)
 
-        val actual = Parser(input).operands
-
-        assertThat(actual.operands.containsAll(expected.operands)).isTrue
+        assertThat(expression.result()).isEqualTo(6)
     }
 
     @DisplayName("두개 이상의 구분자가 포함된 문자열을 입력 받은 경우 구분자들을 기준으로 구분된 숫자 반환")
     @Test
-    fun initWithCustomDelimiter_multipleDelimiter() {
+    fun parse3() {
         val input = "//ab\\n1ab2ab3"
-        val expected = createOperands("1", "2", "3")
+        val expression = Parser.parse(input)
 
-        val actual = Parser(input).operands
-
-        assertThat(actual.operands.containsAll(expected.operands)).isTrue
+        assertThat(expression.result()).isEqualTo(6)
     }
 
     @DisplayName("구분자를 잘못 입력한 경우 예외 발생")
     @ParameterizedTest
     @ValueSource(strings = ["/;\\n", "//;\n"])
     fun init_exception(input: String) {
-        assertThrows<IllegalArgumentException> { Parser(input) }
+        assertThrows<IllegalArgumentException> { Parser.parse(input) }
     }
 }
