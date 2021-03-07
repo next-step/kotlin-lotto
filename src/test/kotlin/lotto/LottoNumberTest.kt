521a217eab1da17a3d3defd9c80ed783bdff0644
bactoria <bactoria@gmail.com>
2021-03-07 21:09:04 +0900

    test: 로또번호 숫자 검증

--- src/test/kotlin/lotto/LottoNumberTest.kt
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -2,6 +2,9 @@ package lotto
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class LottoNumberTest {
 
@@ -10,6 +13,14 @@ class LottoNumberTest {
         val value = 1
         val result = LottoNumber(value)
         assertThat(result).isNotNull
+    }
+
+    @ParameterizedTest(name = "로또번호는 {0} 이/가 될 수 없다")
+    @ValueSource(ints = [-1, 0, 46, 100])
+    fun `1~45 이외의 로또번호 생성 시 예외를 반환한다`(value: Int) {
+        val expectedMessage = "로또번호를 생성할 수 없는 값이다. value: $value"
+        val result: IllegalStateException = assertThrows { LottoNumber(value) }
+        assertThat(result.message).isEqualTo(expectedMessage)
     }
 
     @Test
