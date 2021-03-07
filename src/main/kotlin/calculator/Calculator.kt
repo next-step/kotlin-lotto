eae3b27c59dfc2ba481acb2b3c92d6952352f315
dev-owner <yureka1112@gmail.com>
2021-03-07 21:34:15 +0900

    test(calculator): test 만족하도록 초기 기능 구현

---
+++ src/main/kotlin/calculator/Calculator.kt
@@ -0,0 +1,7 @@
+package calculator
+
+object Calculator {
+    fun run(tokens: List<Int>): Int {
+        return tokens.sum()
+    }
+}
---
+++ src/main/kotlin/calculator/InputValidator.kt
@@ -0,0 +1,9 @@
+package calculator
+
+class InputValidator {
+    companion object {
+        fun run(input: String): Boolean {
+            return false
+        }
+    }
+}
