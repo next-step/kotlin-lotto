0c7f8ecf844cfe1aa90df39c1214e1b9d56aa809
bactoria <bactoria@gmail.com>
2021-03-07 21:09:29 +0900

    feat: 로또번호 숫자 검증

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -1,6 +1,15 @@
 package lotto
 
 class LottoNumber(private val value: Int) {
+
+    init {
+        check(isValidLottoNumberRange()) { "로또번호를 생성할 수 없는 값이다. value: $value" }
+    }
+
+    private fun isValidLottoNumberRange(): Boolean {
+        return value in 1..45
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
