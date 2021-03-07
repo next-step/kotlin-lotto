ad96fc783742904b251a641a8b4704c9d5e1df46
bactoria <bactoria@gmail.com>
2021-03-07 22:16:45 +0900

    refactor: 매직넘버 제거

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,10 @@ package lotto
 class Lotto(lottoNumbers: List<LottoNumber>) {
     
     init {
-        require(lottoNumbers.size == 6) { "로또번호 개수가 6개가 아닙니다." }
+        require(lottoNumbers.size == VALID_LOTTO_NUMBER_COUNT) { "로또번호 개수가 6개가 아닙니다." }
+    }
+    
+    companion object {
+        private const val VALID_LOTTO_NUMBER_COUNT = 6
     }
 }
