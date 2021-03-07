42e03cc0268c85801b58cac63c5053aee5b0993b
sangw0804 <sangw0804@likelion.org>
2021-03-08 01:37:24 +0900

    refactor(LottoNumbers): 의미없는 open 제거

--- src/main/kotlin/lotto/model/LottoNumbers.kt
+++ src/main/kotlin/lotto/model/LottoNumbers.kt
@@ -1,6 +1,6 @@
 package lotto.model
 
-open class LottoNumbers(private val lottoNumbers: List<LottoNumber>) : List<LottoNumber> by lottoNumbers {
+class LottoNumbers(private val lottoNumbers: List<LottoNumber>) : List<LottoNumber> by lottoNumbers {
     companion object {
         fun from(numbers: List<Int>): LottoNumbers {
             return LottoNumbers(numbers.map { LottoNumber(it) })
