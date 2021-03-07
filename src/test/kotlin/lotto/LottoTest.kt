b5a81e8e36ee91cfd8c37f53c172423aee537360
bactoria <bactoria@gmail.com>
2021-03-07 22:14:09 +0900

    test: 로또번호 개수가 6개가 아닌 경우 로또 생성 불가능

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -2,6 +2,9 @@ package lotto
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class LottoTest {
 
@@ -10,5 +13,16 @@ class LottoTest {
         val lottoNumbers = (1..6).map { LottoNumber(it) }
         val result = Lotto(lottoNumbers)
         assertThat(result).isNotNull
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 5, 7])
+    fun `로또 생성 시 로또번호 6개가 아닌 경우 예외를 반환한다`(lottoNumberCount: Int) {
+        val lottoNumbers = (1..lottoNumberCount).map { LottoNumber(it) }
+        val expectedMessage = "로또번호 개수가 6개가 아닙니다."
+
+        val result = assertThrows<IllegalArgumentException> { Lotto(lottoNumbers) }
+
+        assertThat(result.message).isEqualTo(expectedMessage)
     }
 }
