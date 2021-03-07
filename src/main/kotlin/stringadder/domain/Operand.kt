583bbba325866a540ed04193c12722395940a644
남윤서 <devysnam@gmail.com>
2021-03-08 00:12:27 +0900

    refactor: Operand.kt를 data class로 변경

--- src/main/kotlin/stringadder/domain/Operand.kt
+++ src/main/kotlin/stringadder/domain/Operand.kt
@@ -1,6 +1,6 @@
 package stringadder.domain
 
-class Operand(number: String) {
+data class Operand(private val number: String) {
     var operand: Int
 
     init {
@@ -18,21 +18,6 @@ class Operand(number: String) {
 
     private fun validatePositive(number: Int) {
         require(number >= MIN_NUMBER) { "$MIN_NUMBER 이상의 숫자만 입력할 수 있습니다." }
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Operand
-
-        if (operand != other.operand) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return operand
     }
 
     companion object {
