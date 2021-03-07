add568ad36c2e5289479c588701737f418072d53
bactoria <bactoria@gmail.com>
2021-03-08 00:10:30 +0900

    test: 로또머신의 로또판매 기능

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -1,8 +1,12 @@
 package lotto
 
 class LottoMachine(lottoPrice: Int) {
-    
+
     init {
         require(lottoPrice > 0) { "로또금액은 0보다 커야합니다" }
+    }
+
+    fun sellLottos(money: Int): List<Lotto> {
+        return listOf()
     }
 }
--- src/test/kotlin/lotto/LottoMachineTest.kt
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class LottoMachineTest {
@@ -21,5 +22,17 @@ class LottoMachineTest {
         val expectedMessage = "로또금액은 0보다 커야합니다"
         val result = assertThrows<IllegalArgumentException> { LottoMachine(lottoPrice = lottoPrice) }
         assertThat(result.message).isEqualTo(expectedMessage)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1000, 1000, 1",
+        "10, 200, 20",
+        "250, 1000, 4"
+    )
+    fun `로또금액에 따라 로또를 여러장 판매한다`(lottoPrice: Int, money: Int, expected: Int) {
+        val lottoMachine = LottoMachine(lottoPrice = lottoPrice)
+        val result = lottoMachine.sellLottos(money = money)
+        assertThat(result.size).isEqualTo(expected)
     }
 }
