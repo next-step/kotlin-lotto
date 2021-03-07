3e7edcc09926b8de2ee9e392524f370055de3699
bactoria <bactoria@gmail.com>
2021-03-07 10:47:32 +0900

    feat: 음수 또는 0으로 Number 생성 시 예외

--- src/main/kotlin/calculator/Number.kt
+++ src/main/kotlin/calculator/Number.kt
@@ -2,6 +2,10 @@ package calculator
 
 class Number(private val value: Int) {
 
+    init {
+        require(value > 0) { "양수만 허용합니다. value: $value" }
+    }
+
     constructor(value: String) : this(
         value.toIntOrNull() ?: throw IllegalArgumentException("허용하지 않는 문자열입니다. value: $value")
     )
