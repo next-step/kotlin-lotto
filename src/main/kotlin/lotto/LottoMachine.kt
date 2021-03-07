29d7c4fda3d1cff50a32331e77c0935fdda14ea7
bactoria <bactoria@gmail.com>
2021-03-08 01:23:07 +0900

    feat: 로또머신의 로또판매 기능

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -1,12 +1,14 @@
 package lotto
 
-class LottoMachine(lottoPrice: Int) {
+class LottoMachine(private val lottoPrice: Int) {
 
     init {
         require(lottoPrice > 0) { "로또금액은 0보다 커야합니다" }
     }
 
     fun sellLottos(money: Int): List<Lotto> {
-        return listOf()
+        val lottoGenerator = RandomLottoGenerator()
+        val sellableLottoCount = money / lottoPrice
+        return (1..sellableLottoCount).map { lottoGenerator.generate() }
     }
 }
