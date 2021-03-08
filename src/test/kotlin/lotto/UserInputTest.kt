ea8d9fdb43974b3ba8a03f54d17fc4bd0846904b
kit(김광수) <kit.t@kakaocorp.com>
2021-03-08 20:28:03 +0900

    feat: make it pass

--- src/test/kotlin/lotto/UserInputTest.kt
+++ src/test/kotlin/lotto/UserInputTest.kt
@@ -13,20 +13,34 @@ class UserInputTest {
 
     @Test
     fun `쉼표로 구분된 숫자목록을 입력받는다`() {
-        assertThat(UserInput.IntList("질문", "1, 2, 3, 4\n").answer()).isEqualTo(listOf(1, 2, 3, 4))
+        assertThat(UserInput.IntList("질문", "1,2,3,4\n").answer()).isEqualTo(listOf(1, 2, 3, 4))
     }
 
-    interface UserInput {
-        fun answer(): kotlin.Int
+    interface UserInput<T> {
+        fun answer(): T
 
-        class Int(private val question: String, readable: Readable) : UserInput {
+        class Int(private val question: String, readable: Readable) : UserInput<kotlin.Int> {
             private val scanner: Scanner = Scanner(readable)
+
+            constructor(question: String, answer: String) : this(question, StringReader(answer))
+
             override fun answer(): kotlin.Int {
                 println(question)
                 return scanner.nextInt()
             }
+        }
+
+        class IntList(private val question: String, readable: Readable) : UserInput<List<kotlin.Int>> {
+            private val scanner: Scanner = Scanner(readable)
 
             constructor(question: String, answer: String) : this(question, StringReader(answer))
+
+            override fun answer(): List<kotlin.Int> {
+                println(question)
+                return scanner.nextLine()
+                    .split(",")
+                    .map { it.toInt() }
+            }
         }
     }
 }
