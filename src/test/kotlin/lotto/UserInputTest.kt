3632181a4502a5e51661dfefced16c0105ff9121
kit(김광수) <kit.t@kakaocorp.com>
2021-03-08 20:24:49 +0900

    test: 쉼표로 구분된 숫자목록을 입력받는다

--- src/test/kotlin/lotto/UserInputTest.kt
+++ src/test/kotlin/lotto/UserInputTest.kt
@@ -11,6 +11,11 @@ class UserInputTest {
         assertThat(UserInput.Int("질문", "14000\n").answer()).isEqualTo(14_000)
     }
 
+    @Test
+    fun `쉼표로 구분된 숫자목록을 입력받는다`() {
+        assertThat(UserInput.IntList("질문", "1, 2, 3, 4\n").answer()).isEqualTo(listOf(1, 2, 3, 4))
+    }
+
     interface UserInput {
         fun answer(): kotlin.Int
 
