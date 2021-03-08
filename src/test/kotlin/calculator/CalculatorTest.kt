83ac6fcdaa331355b5d61e7c596e086754241bc7
Joseph <wjsdudtkd95@gmail.com>
2021-03-08 15:04:19 +0900

    test(calculator): Calculator 테스트

---
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -0,0 +1,34 @@
+package calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.internal.bytebuddy.asm.Advice
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
+import java.util.stream.Stream
+
+class CalculatorTest {
+
+    @ParameterizedTest
+    @MethodSource("provideNumbers")
+    fun `숫자들의 리스트가 들어와서 모두 합해줄 때`(numbers: List<String>, result: Int) {
+        var sum = 0
+        for (number in numbers) {
+            sum += number.toInt()
+        }
+
+        assertThat(sum).isEqualTo(result)
+    }
+
+    companion object {
+        @JvmStatic
+        private fun provideNumbers(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(listOf("1","2","3","4"), 10),
+                Arguments.of(listOf("3","5","2","1"), 11),
+                Arguments.of(listOf("1","6","3","4"), 14)
+            )
+        }
+    }
+}
