2480140c66de1edb7367c0423603908df268e47a
palmseung <palmseung@gmail.com>
2021-03-09 01:47:02 +0900

    refactor(string-adder): refactor DelimiterTest

--- src/test/kotlin/adder/model/DelimiterTest.kt
+++ src/test/kotlin/adder/model/DelimiterTest.kt
@@ -1,9 +1,9 @@
 package adder.model
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class DelimiterTest {
     @ParameterizedTest
@@ -16,23 +16,9 @@ internal class DelimiterTest {
         assertThat(hasCustomDelimiter).isEqualTo(hasCustom)
     }
 
-    @Test
-    fun splitByCustomDelimiter() {
-        // given
-        val input = "//-\\n1-2-3-45-6"
-
-        // when
-        val dividedInput: List<String> = Delimiter(input).split()
-
-        // then
-        assertThat(dividedInput).isEqualTo(listOf("1", "2", "3", "45", "6"))
-    }
-
-    @Test
-    fun splitByDefaultDelimiter() {
-        // given
-        val input = "1,2,3,45,6"
-
+    @ParameterizedTest
+    @ValueSource(strings = ["//-\\n1-2-3-45-6", "1,2,3,45,6"])
+    fun splitByDelimiter(input: String) {
         // when
         val dividedInput: List<String> = Delimiter(input).split()
 
