7207065cc21fb3173d8bf8a29371500289f44d23
kit(김광수) <kit.t@kakaocorp.com>
2021-03-08 20:23:39 +0900

    test: 숫자를 입력받는다

--- src/test/kotlin/lotto/UserInputTest.kt
+++ src/test/kotlin/lotto/UserInputTest.kt
@@ -1,10 +1,27 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.io.StringReader
+import java.util.Scanner
 
 class UserInputTest {
     @Test
     fun `숫자를 입력받는다`() {
         assertThat(UserInput.Int("질문", "14000\n").answer()).isEqualTo(14_000)
+    }
+
+    interface UserInput {
+        fun answer(): kotlin.Int
+
+        class Int(private val question: String, readable: Readable) : UserInput {
+            private val scanner: Scanner = Scanner(readable)
+            override fun answer(): kotlin.Int {
+                println(question)
+                return scanner.nextInt()
+            }
+
+            constructor(question: String, answer: String) : this(question, StringReader(answer))
+        }
     }
 }
