92c6202e8a936ccfb507f245f5474928543d12e9
palmseung <palmseung@gmail.com>
2021-03-09 03:44:35 +0900

    refactor(lotto): refactor ResultView.kt

--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -6,7 +6,7 @@ import lotto.model.Lottos
 
 object ResultView {
     fun printMyLottos(lottos: Lottos) {
-        lottos.myLottos.forEach { println(it.lottoNums.toString()) }
+        lottos.myLottos.forEach { println(it.toString()) }
         println()
     }
 
