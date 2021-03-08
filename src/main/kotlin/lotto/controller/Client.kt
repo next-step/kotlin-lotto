3ebcf2a6c7ab3029824d3a1887c903e26a757423
sangw0804 <sangw0804@likelion.org>
2021-03-08 23:09:50 +0900

    feat(InputView): 콘솔 인풋 처리 기능 구현
     - 빈 입력값 들어올 경우 다시 요청

--- src/main/kotlin/lotto/controller/Client.kt
+++ src/main/kotlin/lotto/controller/Client.kt
@@ -2,21 +2,18 @@ package lotto.controller
 
 import lotto.model.LottoChecker
 import lotto.model.LottoStore
-import lotto.model.Money
-import lotto.model.WinningNumbers
+import lotto.view.InputView
 
 fun main() {
-    println("구입금액을 입력해 주세요.")
-    val moneyAmount = readLine()!!.toInt()
+    val moneyAmount = InputView.readMoney("구입금액을 입력해 주세요.")
 
     val store = LottoStore()
-    val tickets = store.buy(Money(moneyAmount))
+    val tickets = store.buy(moneyAmount)
 
-    println("지난 주 당첨 번호를 입력해 주세요.")
-    val winningNumbers = WinningNumbers.from(readLine()!!.split(",").map { it.toInt() })
+    val winningNumbers = InputView.readWinningNumbers("지난 주 당첨 번호를 입력해 주세요.")
 
     val checker = LottoChecker(winningNumbers)
-    val result = checker.check(tickets, Money(moneyAmount))
+    val result = checker.check(tickets, moneyAmount)
 
     println(result)
 }
