4c590db3159dbea24f83ea6a7cb27a8b37730f2e
kit(김광수) <kit.t@kakaocorp.com>
2021-03-08 20:45:00 +0900

    feat: make it pass

--- src/test/kotlin/lotto/RankingTest.kt
+++ src/test/kotlin/lotto/RankingTest.kt
@@ -1,12 +1,22 @@
 package lotto
 
+import lotto.LottoDrawMachineTest.LottoNumber
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class RankingTest {
     @Test
     fun `두 번호그룹을 비교하여 일치수를 알 수 있다`() {
         assertThat(
-            Ranking(LottoNumber(listOf(1, 2, 3, 4, 5, 6)), LottoNumber(listOf(1, 2, 3, 4, 5, 6)).rank())
-        ).isEqualTo(Rank.FIRST)
+            Ranking(LottoNumber(listOf(1, 2, 3, 4, 5, 6)), LottoNumber(listOf(1, 2, 3, 4, 5, 6))).rank()
+        ).isEqualTo(Ranking.Rank.FIRST)
+    }
+
+    class Ranking(lottoNumber: LottoNumber, otherNumber: LottoNumber) {
+        fun rank(): Rank = Rank.FIRST
+
+        enum class Rank {
+            FIRST
+        }
     }
 }
