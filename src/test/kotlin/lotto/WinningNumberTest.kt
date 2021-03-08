660d2aedd6a5d4e668058164de813e7a342f0e0a
kit(김광수) <kit.t@kakaocorp.com>
2021-03-08 20:17:59 +0900

    refactor: 유저 입력과 처리 구현

--- src/test/kotlin/lotto/WinningNumberTest.kt
+++ src/test/kotlin/lotto/WinningNumberTest.kt
@@ -3,8 +3,9 @@ package lotto
 import lotto.LottoDrawMachineTest.LottoNumber
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.io.Reader
+import java.io.InputStreamReader
 import java.io.StringReader
+import java.util.Scanner
 
 class WinningNumberTest {
     @Test
@@ -16,11 +17,22 @@ class WinningNumberTest {
     interface WinningNumber {
         val lottoNumber: LottoNumber
 
-        class Console(private val question: String, private val reader: Reader) : WinningNumber {
-            override val lottoNumber: LottoNumber
-                get() = LottoNumber(listOf(1, 2, 3, 4, 5, 6))
+        class Console(
+            private val question: String,
+            readable: Readable = InputStreamReader(System.`in`)
+        ) : WinningNumber {
+            private val scanner: Scanner = Scanner(readable)
 
             constructor(question: String, numbers: String) : this(question, StringReader(numbers))
+
+            override val lottoNumber: LottoNumber
+                get() {
+                    println(question)
+                    return scanner.nextLine()
+                        .split(", ")
+                        .map { it.toInt() }
+                        .let { LottoNumber(it) }
+                }
         }
     }
 }
