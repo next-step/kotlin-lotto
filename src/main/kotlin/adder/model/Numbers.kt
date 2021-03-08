fa2567b165c6a228d2cd2aefe90a4b73fb9f5a0c
palmseung <palmseung@gmail.com>
2021-03-09 01:13:15 +0900

    refactor(string-adder): refactor Numbers based on reviews in step2

--- src/main/kotlin/adder/model/Numbers.kt
+++ src/main/kotlin/adder/model/Numbers.kt
@@ -1,6 +1,7 @@
 package adder.model
 
 data class Numbers(val nums: List<Number>) {
+
     constructor(input: Collection<String>) : this(input.map(::Number))
 
     constructor(input: String) : this(split(input))
@@ -10,23 +11,23 @@ data class Numbers(val nums: List<Number>) {
     }
 
     companion object {
-        private const val REGEX_FOR_DELIMITER = "//(.)\\\\n(.*)"
+        private const val REGEX_FOR_DELIMITER = """//(.)\\n(.*)"""
         private const val DELIMITER_1 = ","
         private const val DELIMITER_2 = ";"
         private const val INDEX_OF_CUSTOM_DELIMITER = 1
         private const val INDEX_OF_TARGET_VALUE = 2
-        private val regex = REGEX_FOR_DELIMITER.toRegex()
+        private val REGEX = REGEX_FOR_DELIMITER.toRegex()
 
         private fun split(input: String): List<String> {
-            val hasCustom = regex.matches(input)
+            val hasCustom = REGEX.matches(input)
             return if (hasCustom) splitByCustom(input) else splitByDefault(input)
         }
 
         private fun splitByCustom(input: String): List<String> {
-            val delimiter = findCustomDelimiter(input).toString()
+            val firstMatchResult: MatchResult = REGEX.find(input)!!
+            val delimiter = findCustomDelimiter(firstMatchResult)
 
-            return regex.find(input)!!
-                .groupValues[INDEX_OF_TARGET_VALUE]
+            return firstMatchResult.groupValues[INDEX_OF_TARGET_VALUE]
                 .split(delimiter)
         }
 
@@ -34,10 +35,8 @@ data class Numbers(val nums: List<Number>) {
             return input.split(DELIMITER_1, DELIMITER_2).map { it.trim() }
         }
 
-        private fun findCustomDelimiter(input: String): String? {
-            return regex.find(input)
-                ?.groupValues
-                ?.get(INDEX_OF_CUSTOM_DELIMITER)
+        private fun findCustomDelimiter(firstMatchResult: MatchResult): String {
+            return firstMatchResult.groupValues[INDEX_OF_CUSTOM_DELIMITER]
         }
     }
 }
