68ac05a35a558cd6bad7446e81cdd0ea6f6ac082
kit(김광수) <kit.t@kakaocorp.com>
2021-03-08 19:40:17 +0900

    refactor: 실제 객체간 협력으로 변경

--- src/test/kotlin/lotto/LottoAgentTest.kt
+++ src/test/kotlin/lotto/LottoAgentTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import lotto.LottoDrawMachineTest.LottoDrawMachine
 import lotto.LottoDrawMachineTest.LottoNumber
+import lotto.LottoTicketTest.LottoTicket
 import lotto.LottoTicketTest.Money
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -15,19 +16,22 @@ class LottoAgentTest {
     }
 
     class LottoAgent(val exchange: Exchange<LottoNumber>) {
-        constructor(lottoExchange: LottoDrawMachine) : this(LottoExchange(lottoExchange))
-
-        class LottoExchange(lottoExchange: LottoDrawMachine) : Exchange<LottoNumber> {
-            override fun pay(money: Money) {
-            }
-
-            override fun product(): List<LottoNumber> = listOf()
-        }
+        constructor(lottoExchange: LottoDrawMachine) : this(Exchange.Lotto(lottoExchange))
     }
 
     interface Exchange<Product> {
         fun pay(money: Money)
 
         fun product(): List<Product>
+
+        class Lotto(private val lottoDrawMachine: LottoDrawMachine) : Exchange<LottoNumber> {
+            private lateinit var lottoTicket: LottoTicket
+            override fun pay(money: Money) {
+                lottoTicket = LottoTicket(money)
+            }
+
+            override fun product(): List<LottoNumber> = (0 until lottoTicket.count)
+                .map { lottoDrawMachine.lottoNumber() }
+        }
     }
 }
