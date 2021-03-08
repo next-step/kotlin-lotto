5895b7ef21f453583c9ad4dbf913385437aa5b4b
kit(김광수) <kit.t@kakaocorp.com>
2021-03-08 22:29:33 +0900

    feat: make it pass

--- src/test/kotlin/lotto/LottoGameResultTest.kt
+++ src/test/kotlin/lotto/LottoGameResultTest.kt
@@ -1,17 +1,23 @@
 package lotto
 
+import lotto.RankingTest.Ranking.Rank
 import lotto.RankingTest.Ranking.Rank.FIRST
 import lotto.RankingTest.Ranking.Rank.MISS
-import lotto.RankingTest.Ranking.Rank.SECOND
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.entry
 import org.junit.jupiter.api.Test
 
 class LottoGameResultTest {
     @Test
     fun `등급별 일치수를 센다`() {
-        assertThat(LottoGameResult(listOf(FIRST, SECOND, MISS)))
+        assertThat(LottoGameResult(listOf(FIRST, FIRST, MISS)).entries)
             .containsExactly(
                 entry(FIRST, 2),
                 entry(MISS, 1)
             )
+    }
+
+    class LottoGameResult(ranks: List<Rank>) {
+        val entries: Map<Rank, Int> = ranks.groupingBy { it }.eachCount()
     }
 }
