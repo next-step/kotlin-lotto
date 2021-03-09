36685bb3d5e3a1d33ebee024b5f4a4cf8ee4c26e
Joseph <wjsdudtkd95@gmail.com>
2021-03-09 18:19:43 +0900

    test(lotto): Lotto 클래스 구현

---
+++ src/main/kotlin/lotto/Lotto.kt
@@ -0,0 +1,38 @@
+package lotto
+
+import kotlin.random.Random
+
+class Lotto private constructor(private var numbers: List<Int>) {
+
+    fun getLottoNumbers(): List<Int> {
+        return numbers.toList()
+    }
+
+    companion object {
+        private const val MAX_DIGITS = 6
+        private const val MIN_LOTTO_NUMBER = 1
+        private const val MAX_LOTTO_NUMBER = 45
+
+        fun generateAuto(): Lotto {
+            val numbers: ArrayList<Int> = arrayListOf()
+            var currentDigits = 0
+            while (currentDigits < MAX_DIGITS) {
+                val randomNumber = Random.nextInt(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER)
+                if (!checkDuplicateNumber(numbers, randomNumber)) {
+                    numbers.add(randomNumber)
+                    currentDigits++
+                }
+            }
+            numbers.sort()
+            return Lotto(numbers)
+        }
+
+        fun generateManual(number: List<Int>): Lotto {
+            return Lotto(number.sorted())
+        }
+
+        private fun checkDuplicateNumber(numbers: List<Int>, randomNumber: Int): Boolean {
+            return numbers.contains(randomNumber)
+        }
+    }
+}
