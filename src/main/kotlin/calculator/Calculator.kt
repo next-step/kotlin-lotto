64b94b9ff1345c9b342c0e506f3e0de06d6e832f
sanggu <tkdrn8578@naver.com>
2021-03-10 00:56:36 +0900

    feat: 커스텀 구분자가 없을 때와 커스텀 구분자가 있을 때에 대한 합 구하는 로직 추가

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,7 +1,32 @@
 package calculator
 
 object Calculator {
-    fun extractDelimiters(input: String): Char {
-        return input[3]
+    fun add(input: String): Int {
+        if (input.isBlank()) {
+            return 0
+        }
+
+        val result = Regex("//(.)\n(.*)").find(input)
+            ?: return input.split(Delimiters.COMMA.value, Delimiters.SEMICOLON.value)
+            .filter { it.isNotBlank() }
+            .map { it.toIntOnlyPositive() }
+            .sum()
+
+        result.let {
+            val delimiter = it.groupValues[1]
+            val tokens = it.groupValues[2]
+
+            return tokens.replace(delimiter, "").map { token -> token.toInt() }.sum()
+        }
+    }
+
+    private fun String.toIntOnlyPositive(): Int {
+        val toInt = this.toIntOrNull()?: throw throw IllegalArgumentException("잘못된 값입니다.")
+
+        if (toInt < 0) {
+            throw IllegalArgumentException("음수는 입력할 수 없습니다")
+        }
+
+        return toInt
     }
 }
