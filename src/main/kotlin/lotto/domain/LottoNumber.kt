351f229ef9251552b66af2a41053b4bfde5272a7
chanin <pci2676@gmail.com>
2021-03-09 09:04:40 +0900

    fear: LottoNumber 작성

---
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -0,0 +1,36 @@
+package lotto.domain
+
+class LottoNumber(
+    val number: Int
+) {
+    init {
+        validate()
+    }
+
+    private fun validate() {
+        if (MIN_NUMBER > number || number > MAX_NUMBER) {
+            throw IllegalArgumentException("로또 번호는 ${MIN_NUMBER}보다 크고 ${MAX_NUMBER}보다 작아야 합니다. number:$number")
+        }
+    }
+
+    object LottoCache {
+        private val cache = Array(MAX_NUMBER) { LottoNumber(it + MIN_NUMBER) }
+
+        fun get(number: Int): LottoNumber {
+            return cache[number - 1]
+        }
+    }
+
+    companion object {
+        const val MIN_NUMBER = 1
+        const val MAX_NUMBER = 45
+
+        fun of(number: Int): LottoNumber {
+            return try {
+                LottoCache.get(number)
+            } catch (e: IndexOutOfBoundsException) {
+                LottoNumber(number)
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/lotto/domain/LottoNumberTest.kt
@@ -0,0 +1,32 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+internal class LottoNumberTest {
+    @ParameterizedTest
+    @ValueSource(ints = [0, 46])
+    fun `로또번호는 1이상 45이하여야 한다`(number: Int) {
+        //given
+
+        //when
+
+        //then
+        assertThrows<IllegalArgumentException> { LottoNumber(number) }
+            .run { assertThat(this).hasMessageContaining("로또 번호는 ${LottoNumber.MIN_NUMBER}보다 크고 ${LottoNumber.MAX_NUMBER}보다 작아야 합니다. number:$number") }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 46])
+    fun `캐싱해둔 번호도 1이상 45에서 가져온다`(number: Int){
+        //given
+
+        //when
+
+        //then
+        assertThrows<IllegalArgumentException> { LottoNumber.of(number) }
+            .run { assertThat(this).hasMessageContaining("로또 번호는 ${LottoNumber.MIN_NUMBER}보다 크고 ${LottoNumber.MAX_NUMBER}보다 작아야 합니다. number:$number") }
+    }
+}
