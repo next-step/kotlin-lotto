40daf5c69a8636d5fa0bf042edf465e2ee352de5
sangw0804 <sangw0804@likelion.org>
2021-03-09 22:43:22 +0900

    refactor(Number): Number 클래스 추가

--- src/main/kotlin/stringcalculator/Numbers.kt
+++ src/main/kotlin/stringcalculator/Numbers.kt
@@ -1,6 +1,12 @@
 package stringcalculator
 
-class Numbers(private val numbers: List<String>) : List<String> by numbers {
+class Numbers(private val numbers: List<Number>) : List<Number> by numbers {
     fun sum() =
-        numbers.fold(0, { accu, curr -> accu + curr.toInt() })
+        numbers.fold(0, { accu, curr -> accu + curr.value })
+
+    companion object {
+        fun from(numberStrings: List<String>): Numbers {
+            return Numbers(numberStrings.map { Number(it) })
+        }
+    }
 }
--- src/main/kotlin/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/stringcalculator/StringCalculator.kt
@@ -7,7 +7,6 @@ class StringCalculator {
         }
 
         val splitInputs = splitInput(input)
-        validate(splitInputs)
 
         return splitInputs.sum()
     }
@@ -26,19 +25,11 @@ class StringCalculator {
         val customDelimiter = matchResult.groupValues[1]
         val splitTarget = matchResult.groupValues[2]
 
-        return Numbers(splitTarget.split(customDelimiter))
+        return Numbers.from(splitTarget.split(customDelimiter))
     }
 
     private fun splitByDefaultDelimiter(input: String): Numbers =
-        Numbers(input.split(DEFAULT_DELIMITER_REGEX))
-
-    private fun validate(splitInputs: Numbers) {
-        splitInputs.map {
-            it.toIntOrNull()
-        }.forEach {
-            require(it != null && it >= 0) { "문자열 계산기에는 0, 양수만 인자로 올 수 있습니다!" }
-        }
-    }
+        Numbers.from(input.split(DEFAULT_DELIMITER_REGEX))
 
     companion object {
         private val CUSTOM_DELIMITER_FIND_REGEX = Regex("//(.)\\\\n(.*)")
