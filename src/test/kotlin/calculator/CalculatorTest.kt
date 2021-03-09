4f1c8aa11f755d005bdcd76ae3d28763cfd700fc
sanggu <tkdrn8578@naver.com>
2021-03-09 22:20:06 +0900

    feat: 빈 문자열을 입력했을 때의 대한 테스트코드 추가

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -1,18 +1,16 @@
 package calculator
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.EmptySource
 
 class CalculatorTest {
 
     @ParameterizedTest
-    @CsvSource(value = ["//;\n1;2;3|;", "//,\n1,2,3|,"], delimiter = '|')
-    @DisplayName("//와\\n 사이에 있는 값을 구분자로 추출한다")
-    fun `구분자를 추출한다`(input: String, delimiters: String) {
-        val delimiters: List<String> = Calculator.extractDelimiters(input)
+    @EmptySource
+    fun `빈 문자열을 입력할 경우 0을 반환한다`(input: String) {
+        val sum = Calculator.add(input)
 
-        assertThat(delimiters).isEqualTo(delimiters)
+        assertThat(sum).isEqualTo(0)
     }
 }
