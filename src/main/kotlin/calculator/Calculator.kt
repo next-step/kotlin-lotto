086e6625900a4f373e22d6ae431122836c01b42b
sanggu <tkdrn8578@naver.com>
2021-03-10 01:06:53 +0900

    refactor: 커스텀 구분자가 없을 때 연산처리에 대한 기능을 extract method

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,23 +1,28 @@
 package calculator
 
 object Calculator {
+    private const val EMPTY = ""
+
     fun add(input: String): Int {
         if (input.isBlank()) {
             return 0
         }
 
-        val result = Regex("//(.)\n(.*)").find(input)
-            ?: return input.split(Delimiters.COMMA.value, Delimiters.SEMICOLON.value)
-            .filter { it.isNotBlank() }
-            .map { it.toIntOnlyPositive() }
-            .sum()
+        val result = Regex("//(.)\n(.*)").find(input)?: return addWhenNotExistCustomDelimiter(input)
 
         result.let {
             val delimiter = it.groupValues[1]
             val tokens = it.groupValues[2]
 
-            return tokens.replace(delimiter, "").map { token -> token.toInt() }.sum()
+            return tokens.replace(delimiter, EMPTY).map { token -> token.toInt() }.sum()
         }
+    }
+
+    private fun addWhenNotExistCustomDelimiter(input: String): Int {
+        return input.split(Delimiters.COMMA.value, Delimiters.SEMICOLON.value)
+            .filter { it.isNotBlank() }
+            .map { it.toIntOnlyPositive() }
+            .sum()
     }
 
     private fun String.toIntOnlyPositive(): Int {
