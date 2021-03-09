ddb11667ac080dd94dabe96203b1ac369fb4203f
sanggu <tkdrn8578@naver.com>
2021-03-09 22:24:02 +0900

    feat: 하나의 문자열만 입력받았을 경우에 대한 테스트 코드 추가

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -3,6 +3,7 @@ package calculator
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EmptySource
+import org.junit.jupiter.params.provider.ValueSource
 
 class CalculatorTest {
 
@@ -12,5 +13,13 @@ class CalculatorTest {
         val sum = Calculator.add(input)
 
         assertThat(sum).isEqualTo(0)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "4", "9"])
+    fun `하나의 숫자만 입력받았을 경우 해당 값을 그대로 리턴한다`(input: String) {
+        val sum = Calculator.add(input)
+
+        assertThat(sum).isSameAs(input.toInt())
     }
 }
