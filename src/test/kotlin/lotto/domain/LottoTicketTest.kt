98b8da6d7e47613e84efe379897563c1124c2a02
Joseph <wjsdudtkd95@gmail.com>
2021-03-11 02:36:56 +0900

    refactor(lotto): 구현된 코드 제거

--- src/test/kotlin/lotto/domain/LottoTicketTest.kt
+++ src/test/kotlin/lotto/domain/LottoTicketTest.kt
@@ -20,29 +20,4 @@ class LottoTicketTest {
             print("${it.value} ")
         }
     }
-
-    @Test
-    fun `무작위 숫자 6개 만들기`() {
-        val lotto = arrayListOf<Int>()
-        repeat(6) {
-            lotto.add(Random.nextInt(1, 45))
-        }
-        lotto.sort()
-        println(lotto.toString())
-    }
-
-    @Test
-    fun `중복 없이 무작위숫자 6개 만들기`() {
-        val numbers = arrayListOf<Int>()
-        var currentDigits = 0
-        while (currentDigits < 6) {
-            val randomNumber = Random.nextInt(1, 45)
-            if (!numbers.contains(randomNumber)) {
-                numbers.add(randomNumber)
-                currentDigits++
-            }
-        }
-        numbers.sort()
-        print(numbers.toString())
-    }
 }
