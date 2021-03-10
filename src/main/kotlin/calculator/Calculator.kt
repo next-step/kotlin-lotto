995df53cd577c8ba79195fecb73530a800a32c1e
KIM SANG GU <35962910+SGKIM94@users.noreply.github.com>
2021-03-10 22:34:05 +0900

    [step1] 문자열 덧셈 계산기 구현입니다.! (#109)
    * refactor: 불필요한 empty.txt 제거
    
    * feat: 문자열 계산기에 대한 READMD 작성
    
    * test: 문자열을 입력받아 구분자를 추출하는 것에 대한 테스트 코드 추가
    
    * feat: 구분자를 추출해주는 extractDelimiters 구현
    
    * feat: 빈 문자열을 입력했을 때의 대한 테스트코드 추가
    
    * feat: 하나의 문자열만 입력받았을 경우에 대한 테스트 코드 추가
    
    * test: 음수를 입력한 것에 대한 테스트 코드 추가
    
    * test: 쉼표와 콜론을 구분자로 가지는 경우 합을 반환하는 테스트코드 추가
    
    * feat: delimiter 들을 가지는 enum 추가
    
    * test: 커스텀 구분자로 합을 구하는 것에 대한 테스트 코드 추가
    
    * feat: 커스텀 구분자가 없을 때와 커스텀 구분자가 있을 때에 대한 합 구하는 로직 추가
    
    * refactor: 커스텀 구분자가 없을 때 연산처리에 대한 기능을 extract method
    
    * feat: 음수를 입력했을 때 예외처리하는 것에 대한 테스트 코드 추가
    
    * refactor: 매직넘버를 const val로 추출 및 split 후 blank를 제거하는 것에 대한 extract method 진행
    
    * refactor: 0 의 숫자를 MINIMUM_OPERAND_VALUE 로 추출
    
    * refactor: ?: 에 대한 convention 오류 수정
    
    * refactor: 커스텀 구분자를 입력한 경우에 대한 nested annotation 추가
    
    * refactor: wildcard 제거

---
+++ src/main/kotlin/calculator/Calculator.kt
@@ -0,0 +1,42 @@
+package calculator
+
+object Calculator {
+    private const val FIND_DELIMITER_AND_OPERAND_PATTERN = "//(.)\n(.*)"
+    private const val DELIMITER_INDEX = 1
+    private const val OPERANDS_INDEX = 2
+    private const val MINIMUM_OPERAND_VALUE = 0
+
+    fun add(input: String): Int {
+        if (input.isBlank()) {
+            return MINIMUM_OPERAND_VALUE
+        }
+
+        val result = Regex(FIND_DELIMITER_AND_OPERAND_PATTERN).find(input) ?: return addWhenNotExistCustomDelimiter(input)
+
+        result.let {
+            val delimiter = it.groupValues[DELIMITER_INDEX]
+            val operands = it.groupValues[OPERANDS_INDEX]
+
+            return operands.splitExceptBlank(delimiter).map { operand -> operand.toIntOnlyPositive() }.sum()
+        }
+    }
+
+    private fun addWhenNotExistCustomDelimiter(input: String): Int {
+        return input.split(Delimiters.COMMA.value, Delimiters.SEMICOLON.value)
+            .filter { it.isNotBlank() }
+            .map { it.toIntOnlyPositive() }
+            .sum()
+    }
+
+    private fun String.splitExceptBlank(delimiter: String): List<String> {
+        return this.split(delimiter).filter { it.isNotBlank() }
+    }
+
+    private fun String.toIntOnlyPositive(): Int {
+        val toInt = this.toIntOrNull() ?: throw throw IllegalArgumentException("잘못된 값입니다.")
+
+        require(toInt >= MINIMUM_OPERAND_VALUE) { "음수는 입력할 수 없습니다" }
+
+        return toInt
+    }
+}
---
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -0,0 +1,60 @@
+package calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EmptySource
+import org.junit.jupiter.params.provider.ValueSource
+
+class CalculatorTest {
+    @ParameterizedTest
+    @EmptySource
+    fun `빈 문자열을 입력할 경우 0을 반환한다`(input: String) {
+        val sum = Calculator.add(input)
+
+        assertThat(sum).isEqualTo(0)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "4", "9"])
+    fun `하나의 숫자만 입력받았을 경우 해당 값을 그대로 리턴한다`(input: String) {
+        val sum = Calculator.add(input)
+
+        assertThat(sum).isSameAs(input.toInt())
+    }
+
+    @Test
+    fun `입력한 값 중에 음수가 있는 경우 예외처리한다`() {
+        assertThrows<IllegalArgumentException> {
+            Calculator.add("-1")
+        }
+    }
+
+    @Test
+    fun `쉼표와 콜론을 구분자로 전달한 경우 합을 반환한다`() {
+        val result = Calculator.add("1,2,:3")
+
+        assertThat(result).isEqualTo(6)
+    }
+
+    @Nested
+    @DisplayName("커스텀 구분자를 입력한 경우")
+    inner class CustomDelimiter {
+        @Test
+        fun `구분자로 분리된 값들의 합을 반환한다`() {
+            val result = Calculator.add("//,\n1,2,3")
+
+            assertThat(result).isEqualTo(6)
+        }
+
+        @Test
+        fun `입력한 숫자 중에 음수가 있는 경우 RuntimeException 예외처리한다`() {
+            assertThrows<IllegalArgumentException> {
+                Calculator.add("//,\n1,-2,-3")
+            }
+        }
+    }
+}
