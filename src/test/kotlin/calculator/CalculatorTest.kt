354fa4c5b8313931ce28ca88ec7ce7acbcf518ca
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-10 15:39:51 +0900

    refactor: 커스텀 구분자를 입력한 경우에 대한 nested annotation 추가

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -1,14 +1,12 @@
 package calculator
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EmptySource
 import org.junit.jupiter.params.provider.ValueSource
 
 class CalculatorTest {
-
     @ParameterizedTest
     @EmptySource
     fun `빈 문자열을 입력할 경우 0을 반환한다`(input: String) {
@@ -39,17 +37,21 @@ class CalculatorTest {
         assertThat(result).isEqualTo(6)
     }
 
-    @Test
-    fun `커스텀 구분자를 전달한 경우 구분자로 분리된 값들의 합을 반환한다`() {
-        val result = Calculator.add("//,\n1,2,3")
+    @Nested
+    @DisplayName("커스텀 구분자를 입력한 경우")
+    inner class CustomDelimiter {
+        @Test
+        fun `구분자로 분리된 값들의 합을 반환한다`() {
+            val result = Calculator.add("//,\n1,2,3")
 
-        assertThat(result).isEqualTo(6)
-    }
+            assertThat(result).isEqualTo(6)
+        }
 
-    @Test
-    fun `입력한 숫자 중에 음수가 있는 경우 RuntimeException 예외처리한다`() {
-        assertThrows<IllegalArgumentException> {
-            Calculator.add("//,\n1,-2,-3")
+        @Test
+        fun `입력한 숫자 중에 음수가 있는 경우 RuntimeException 예외처리한다`() {
+            assertThrows<IllegalArgumentException> {
+                Calculator.add("//,\n1,-2,-3")
+            }
         }
     }
 }
