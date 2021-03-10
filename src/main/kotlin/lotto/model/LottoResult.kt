5fd695797f319bd472de3d952ccc2b30acfde6af
sangw0804 <sangw0804@likelion.org>
2021-03-11 00:34:39 +0900

    refactor(LottoResult): Double 값 계산 BigDecimal 로 변경

--- src/main/kotlin/lotto/model/LottoResult.kt
+++ src/main/kotlin/lotto/model/LottoResult.kt
@@ -1,6 +1,7 @@
 package lotto.model
 
-import kotlin.math.floor
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 data class LottoResult(val winningCounter: WinningCounter, val sumCostOfTickets: Money) {
     val benefitRate: Double
@@ -11,5 +12,6 @@ data class LottoResult(val winningCounter: WinningCounter, val sumCostOfTickets:
             return ceilToSecond(benefitRate)
         }
 
-    private fun ceilToSecond(double: Double) = floor(double * 100) / 100
+    private fun ceilToSecond(double: Double) =
+        BigDecimal(double).setScale(2, RoundingMode.DOWN).toDouble()
 }
