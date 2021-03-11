24e68fe22dc94d077628ae06df440c794387d0ba
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-11 17:36:38 +0900

    refactor: lotteries 생성 시 LotteryNumbers 를 만들어서 넣도록 수정

--- src/main/kotlin/domain/Lotteries.kt
+++ src/main/kotlin/domain/Lotteries.kt
@@ -3,7 +3,7 @@ package domain
 class Lotteries(var lotteries: List<Lottery>) {
     companion object {
         fun createLotteries(count: Int): Lotteries {
-            return Lotteries((1..count).map { Lottery(listOf(1, 2)) })
+            return Lotteries((1..count).map { Lottery(LotteryNumbers(listOf(1, 2))) })
         }
     }
 }
