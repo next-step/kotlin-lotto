9093bf1db1db977e7ab45e905905a7f07e1f0654
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-11 18:20:58 +0900

    refactor: LotteryNumbers 인자 값 변경에 따른 수정

--- src/main/kotlin/domain/Lotteries.kt
+++ src/main/kotlin/domain/Lotteries.kt
@@ -1,9 +1,10 @@
 package domain
 
 class Lotteries(var lotteries: List<Lottery>) {
+
     companion object {
         fun createLotteries(count: Int): Lotteries {
-            return Lotteries((1..count).map { Lottery(LotteryNumbers(listOf(1, 2))) })
+            return Lotteries((1..count).map { Lottery(LotteryNumbers()) })
         }
     }
 }
