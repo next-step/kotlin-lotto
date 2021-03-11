7413f548695a33cedb50846fa0d820f917ffaec4
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-11 18:20:34 +0900

    refactor: 가독성을 위한 개행 추가

--- src/main/kotlin/domain/Lottery.kt
+++ src/main/kotlin/domain/Lottery.kt
@@ -1,6 +1,7 @@
 package domain
 
 class Lottery(val numbers: LotteryNumbers, val price: Int = DEFAULT_LOTTO_PRICE) {
+
     companion object {
         private const val DEFAULT_LOTTO_PRICE = 1000
     }
