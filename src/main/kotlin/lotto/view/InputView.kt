99868667d9f4ff2769e54522ae687fd190384f95
palmseung <palmseung@gmail.com>
2021-03-12 00:51:44 +0900

    feat (inputView): add print methods for manual lotto

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,6 +1,14 @@
 package lotto.view
 
 object InputView {
+    fun printManualCountQuestion() {
+        println("수동으로 구매할 로또 수를 입력해 주세요.")
+    }
+
+    fun printManualNumberQuestion() {
+        println("수동으로 구매할 번호를 입력해 주세요.")
+    }
+
     fun printBonusBallQuestion() {
         println("보너스 볼을 입력해 주세요.")
     }
