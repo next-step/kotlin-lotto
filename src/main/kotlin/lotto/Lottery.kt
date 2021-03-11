83733a166aeba691e799183accc31e1303a26064
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-11 16:18:01 +0900

    refactor: Lotto -> Lottery 로 수정

--- src/main/kotlin/lotto/Lottery.kt
+++ src/main/kotlin/lotto/Lottery.kt
@@ -1,6 +1,6 @@
 package lotto
 
-class Lotto(val numbers: List<Int>, val price: Int = DEFAULT_LOTTO_PRICE) {
+class Lottery(val numbers: List<Int>, val price: Int = DEFAULT_LOTTO_PRICE) {
 
     companion object {
         private const val DEFAULT_LOTTO_PRICE = 1000;
