9b52b716bb08a4723892915f0d56d3372ef9c7e9
thxwelchs <thxwelchs@gmail.com>
2021-03-12 00:31:34 +0900

    test(domain): Token, Tokens 테스트
    - 음수 문자열 테스트
    - 숫자로 변환될 수 없는 문자열 테스트
    - 문자열 리스트 테스트

---
+++ src/test/kotlin/stringcalculator/domain/TokenTest.kt
@@ -0,0 +1,24 @@
+package stringcalculator.domain
+
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+internal class TokenTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = ["-1", "-3", "-4", "-11", "-15"])
+    fun `음수 문자열이 입력되면 예외를 던진다`(numberString: String) {
+        assertThrows<RuntimeException> {
+            Token(numberString)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["a", "b", "c", "hello", "world"])
+    fun `숫자로 변환될 수 없는 문자는 예외를 던진다`(fakeNumberString: String) {
+        assertThrows<RuntimeException> {
+            Token(fakeNumberString)
+        }
+    }
+}
