9f6cf553644e6362491f5a37064f5520a6c2860e
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-11 17:16:38 +0900

    feat: 로또를 실행하기위한 LotteryStore controller class 추가

---
+++ src/main/kotlin/controller/LotteryStore.kt
@@ -0,0 +1,12 @@
+package controller
+
+import view.InputView
+
+class LotteryStore {
+    fun main() {
+        InputView.printInputPrice()
+        val numberOfLottery = Reception.receiveNumberOfLottery()
+
+
+    }
+}
