a968f761c176ba4408ecc5f986b4f290b15491e8
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-11 18:20:19 +0900

    feat: 로또 숫자들을 만들기 위한 makeLotteryNumbers 함수 추가

--- src/main/kotlin/domain/LotteryNumbers.kt
+++ src/main/kotlin/domain/LotteryNumbers.kt
@@ -1,7 +1,19 @@
 package domain
 
-class LotteryNumbers(numbers: List<Int>) {
+class LotteryNumbers {
+    var numbers: Set<Int> = HashSet(LOTTERY_NUMBERS_SIZE)
+    private set
+
     init {
         require(numbers.size == 6) { "숫자는 6개 이상이여야 합니다." }
+    }
+
+    fun makeLotteryNumbers(lotteryNumbers: Numbers): HashSet<Int> {
+        numbers = lotteryNumbers.makeNumbers(LOTTERY_NUMBERS_SIZE)
+        return numbers as HashSet<Int>
+    }
+
+    companion object {
+        private const val LOTTERY_NUMBERS_SIZE = 6
     }
 }
