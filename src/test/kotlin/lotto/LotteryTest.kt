1b0cfd0640e21263b1103e88f2ed08e82d4adea8
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-11 18:21:26 +0900

    refactor: Lottery 인자의 타입 변경에 따른 테스트 코드 수정

--- src/test/kotlin/lotto/LotteryTest.kt
+++ src/test/kotlin/lotto/LotteryTest.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import domain.Lottery
+import domain.LotteryNumbers
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -8,14 +9,16 @@ class LotteryTest {
 
     @Test
     fun `로또는 중복되지 않는 6개의 랜덤 숫자를 가진다`() {
-        val lottery = Lottery(listOf(1, 3, 5, 6, 2, 8))
+        val lottery = Lottery(LotteryNumbers(listOf(1, 3, 5, 6, 2, 8)))
 
-        assertThat(lottery.numbers.size).isEqualTo(5)
+        val numbers = lottery.numbers
+
+        assertThat(numbers.numbers.size).isEqualTo(5)
     }
 
     @Test
     fun `로또의 가격은 1000원이다`() {
-        val lottery = Lottery(listOf(5))
+        val lottery = Lottery(LotteryNumbers(listOf(5)))
 
         assertThat(lottery.price).isEqualTo(1000)
     }
