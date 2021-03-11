b8df125674179ded64b128127f4c8cac600ce566
hyunssooo <jhs695@gmail.com>
2021-03-11 19:54:06 +0900

    feat: winningLotto 추가

--- src/main/kotlin/lotto/domain/Rank.kt
+++ src/main/kotlin/lotto/domain/Rank.kt
@@ -3,12 +3,19 @@ package lotto.domain
 enum class Rank(val matchCount: Int, val prizeMoney: Money) {
 
     FIRST(6, Money(2_000_000_000)),
+    SECOND(5, Money(30_000_000)),
     THIRD(5, Money(1_500_000)),
     FOURTH(4, Money(50_000)),
     FIFTH(3, Money(5_000)),
     MISS(0, Money.ZERO);
 
     companion object {
-        fun of(matchCount: Int) = values().find { it.matchCount == matchCount } ?: MISS
+        fun of(matchCount: Int, bonusMatch: Boolean): Rank {
+            if (matchCount == 5 && bonusMatch) {
+                return SECOND
+            }
+
+            return if (matchCount == 5) THIRD else values().find { it.matchCount == matchCount } ?: MISS
+        }
     }
 }
