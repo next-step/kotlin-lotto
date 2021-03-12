770cd06adc5fd40f9ffab9d78da27e40c584990f
taeheon.song <taeheon.song@linecorp.com>
2021-03-12 23:54:53 +0900

    feat(Lotto) : check lottoNumbers has duplicated number

--- src/main/kotlin/lotto/data/LottoNumbers.kt
+++ src/main/kotlin/lotto/data/LottoNumbers.kt
@@ -9,6 +9,7 @@ class LottoNumbers(
 
     init {
         require(lottoNumbers.size == LOTTO_NUMBERS_SIZE) { "로또 번호의 개수는 6개여야 합니다." }
+        require(!hasDuplicatedNumber()) { "중복되는 숫자는 없어야 합니다." }
     }
 
     fun findWinningType(winningNumbers: WinningNumbers): LotteryMatchType {
@@ -19,6 +20,21 @@ class LottoNumbers(
 
     private fun contains(lottoNumber: LottoNumber): Boolean {
         return lottoNumbers.contains(lottoNumber)
+    }
+
+    private fun hasDuplicatedNumber(): Boolean {
+        lottoNumbers.forEach { lottoNumber ->
+            if (isDuplicated(lottoNumber)) return false
+        }
+        return true
+    }
+
+    private fun isDuplicated(lottoNumber: LottoNumber): Boolean {
+        val sameNumberCount = lottoNumbers.filter { it == lottoNumber }.count()
+        if (sameNumberCount != 1) {
+            return true
+        }
+        return false
     }
 
     companion object {
