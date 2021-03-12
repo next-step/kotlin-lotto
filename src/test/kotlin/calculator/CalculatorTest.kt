11b4e0d1b512c21f7b5a5f2a67abc112f28a7478
dev-owner <yureka1112@gmail.com>
2021-03-12 18:45:54 +0900

    feat(calculator): 덧셈 계산기 기능 추가 및 Application.kt 추가

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -1,46 +1,45 @@
 package calculator
 
+import calculator.domain.Calculator
+import calculator.domain.Translator
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
-import org.junit.jupiter.params.provider.NullAndEmptySource
 import org.junit.jupiter.params.provider.ValueSource
 
 class CalculatorTest {
     @ParameterizedTest
-    @ValueSource(strings = ["1,2,3", "1,2:3", "1:2:3", "1:2,3"])
-    internal fun `기본 입력 테스트`(input: String) {
-        val inputParameter = InputConsole.run(input)
-        assertThat(inputParameter).containsOnly(1, 2, 3)
+    @CsvSource("1,2,3;6", "4,5:6;15", ":2:3;5", ":2,3;5", "6;6", delimiter = ';')
+    internal fun `덧셈 테스트`(input: String, expected: Int) {
+        val tokens = Translator.run(input)
+        val result = Calculator.run(tokens)
+        assertThat(result).isEqualTo(expected)
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["//;\n1;2;3"])
-    internal fun `커스텀 구분자 입력 테스트`(input: String) {
-        val inputParameter = InputConsole.run(input)
-        assertThat(inputParameter).containsOnly(1, 2, 3)
+    internal fun `커스텀 구분자 덧셈 테스트`(input: String) {
+        val tokens = Translator.run(input)
+        val result = Calculator.run(tokens)
+        val expected = 6
+        assertThat(result).isEqualTo(expected)
     }
 
     @ParameterizedTest
-    @NullAndEmptySource
-    internal fun `null or blank 입력 테스트`(input: String?) {
-        val inputParameter = InputConsole.run(input)
-        assertThat(inputParameter).containsOnly(DEFAULT_TOKEN_VALUE)
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = [",1,3", " ,1,3"])
-    internal fun `각 token에 blank 입력 테스트`(input: String) {
-        val inputParameter = InputConsole.run(input)
-        assertThat(inputParameter).containsOnly(DEFAULT_TOKEN_VALUE, 1, 3)
+    @CsvSource(",2,3;5", "4, :6;10", "5,5:;10", delimiter = ';')
+    internal fun `각 token에 blank 입력 테스트`(input: String, expected: Int) {
+        val tokens = Translator.run(input)
+        val result = Calculator.run(tokens)
+        assertThat(result).isEqualTo(expected)
     }
 
     @ParameterizedTest
     @CsvSource("a,2,3", "한글,3", "@,3,4")
     internal fun `숫자 이외 값 예외 테스트`(input: String) {
         assertThrows<RuntimeException> {
-            InputConsole.run(input)
+            val tokens = Translator.run(input)
+            Calculator.run(tokens)
         }
     }
 
@@ -48,21 +47,8 @@ class CalculatorTest {
     @CsvSource("-1,2,3", "-2,-3")
     internal fun `음수 예외 테스트`(input: String) {
         assertThrows<RuntimeException> {
-            InputConsole.run(input)
+            val tokens = Translator.run(input)
+            Calculator.run(tokens)
         }
-    }
-
-    @ParameterizedTest
-    @CsvSource("1,2,3;6", "4,5:6;15", ":2:3;5", ":2,3;5", "6;6", delimiter = ';')
-    internal fun `덧셈 테스트`(input: String, expected: Int) {
-        val tokens = InputConsole.run(input)
-        assertThat(Calculator.run(tokens)).isEqualTo(expected)
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["//;\n1;2;3"])
-    internal fun `커스텀 구분자 덧셈 테스트`(input: String) {
-        val tokens = InputConsole.run(input)
-        assertThat(Calculator.run(tokens)).isEqualTo(6)
     }
 }
