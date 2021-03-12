d32255fcc3d4b3c58f014cac8f46fe50a44b22e0
Joseph <wjsdudtkd95@gmail.com>
2021-03-12 21:01:32 +0900

    refactor(lotto): 보너스 숫자 중복 시 예외처리

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -3,4 +3,12 @@ package lotto.domain
 data class WinningLotto(
     val winningNumbers: List<LottoNumber>,
     val bonusNumber: LottoNumber
-)
+) {
+    init {
+        checkWinningNumbersContainsBonusNumber()
+    }
+
+    private fun checkWinningNumbersContainsBonusNumber() {
+        if(winningNumbers.contains(bonusNumber)) throw IllegalArgumentException("보너스 숫자가 이미 당첨번호에 존재합니다.")
+    }
+}
