f92c45d23ce813e3db97d8b736babdff254d5f5b
Joseph <wjsdudtkd95@gmail.com>
2021-03-12 19:20:40 +0900

    refactor(lotto): 로또 번호의 유효성 검사 수정

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -1,7 +1,16 @@
 package lotto.domain
 
 data class LottoNumber(val value: Int) {
+
+    init {
+        checkValidateLottoNumber(value)
+    }
+
     override fun toString(): String {
         return value.toString()
+    }
+
+    private fun checkValidateLottoNumber(number: Int) {
+        if(number !in 1..45) throw IllegalArgumentException("로또 번호는 1부터 45까지이다.")
     }
 }
