ab4bae35dfd4870a70f39f817b5eddaff557c143
Joseph <wjsdudtkd95@gmail.com>
2021-03-12 19:12:49 +0900

    refactor(caculator): toInt 중복호출 제거

--- src/main/kotlin/calculator/domain/Number.kt
+++ src/main/kotlin/calculator/domain/Number.kt
@@ -3,14 +3,14 @@ package calculator.domain
 class Number(val value: Int) {
     constructor(stringNumber: String) : this(stringNumber.toInt()) {
         checkValidateNumber(stringNumber)
-        checkNegativeNumber(stringNumber)
+        checkNegativeNumber(value)
     }
 
     private fun checkValidateNumber(stringNumber: String) {
         stringNumber.toIntOrNull() ?: throw RuntimeException("$stringNumber 은 올바른 숫자가 아닙니다.")
     }
 
-    private fun checkNegativeNumber(stringNumber: String) {
-        if (stringNumber.toInt() < 0) throw RuntimeException("음수는 계산식에 포함될 수 없습니다.")
+    private fun checkNegativeNumber(number: Int) {
+        if (number < 0) throw RuntimeException("음수는 계산식에 포함될 수 없습니다.")
     }
 }
