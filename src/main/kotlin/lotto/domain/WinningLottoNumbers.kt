dd1dee6c54eb9a6aec4823bb049c8797fbfe7331
thxwelchs <thxwelchs@gmail.com>
2021-03-13 23:25:19 +0900

    feat(domain): 당첨번호 구현
    구현 사항:
    - 당첨 로또번호를 들고 있을 1급컬렉션 구현

---
+++ src/main/kotlin/lotto/domain/WinningLottoNumbers.kt
@@ -0,0 +1,32 @@
+package lotto.domain
+
+class WinningLottoNumbers private constructor(
+    val lottoNumbers: List<LottoNumber>
+) {
+
+    fun isContainLottoNumber(lottoNumber: LottoNumber): Boolean {
+        return lottoNumbers.contains(lottoNumber)
+    }
+
+    companion object {
+        private const val WINNING_LOTTO_NUMBERS_SIZE = 6
+
+        fun from(stringWinningLottoNumbers: String): WinningLottoNumbers {
+            val winningLottoNumberTokens = stringWinningLottoNumbers.split(", ")
+
+            validate(winningLottoNumberTokens)
+
+            return WinningLottoNumbers(winningLottoNumberTokens.map { LottoNumber.from(it.toInt()) })
+        }
+
+        private fun validate(tokens: List<String>) {
+            require(tokens.size == WINNING_LOTTO_NUMBERS_SIZE) {
+                "당첨 번호는 ${WINNING_LOTTO_NUMBERS_SIZE}개의 수로 이뤄져야 합니다."
+            }
+
+            require(tokens.groupBy { it }.all{ it.value.size == 1 }) {
+                "당첨 번호는 중복된 번호를 허용할 수 없습니다."
+            }
+        }
+    }
+}
