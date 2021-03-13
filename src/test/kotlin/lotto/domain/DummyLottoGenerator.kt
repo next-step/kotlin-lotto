5febf42b1245b2d1b091dc040d45992bb9054445
bactoria <bactoria@gmail.com>
2021-03-14 02:09:26 +0900

    refactor: 표준 예외 사용
    java.lang.UnsupportedOperationException
    ㄴ 인터페이스의 메서드를 구현할 수 없는 경우 사용
    
    리뷰: https://github.com/next-step/kotlin-lotto/pull/108#discussion_r592025667

--- src/test/kotlin/lotto/domain/DummyLottoGenerator.kt
+++ src/test/kotlin/lotto/domain/DummyLottoGenerator.kt
@@ -1,9 +1,7 @@
 package lotto.domain
 
-import java.lang.RuntimeException
-
 class DummyLottoGenerator : LottoGenerator {
     override fun generate(): Lotto {
-        throw RuntimeException("dummy 구현체에서 구현되지 않은 메서드")
+        throw UnsupportedOperationException("dummy 구현체에서 구현되지 않은 메서드")
     }
 }
