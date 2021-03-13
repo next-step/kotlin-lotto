2df921f3d62174227a61e3cbf67a6f657b2fcefb
sanggu <tkdrn8578@naver.com>
2021-03-13 21:53:38 +0900

    refactor: LotteryFixture 추가에 따른 수정

--- src/test/kotlin/lotto/LotteryTest.kt
+++ src/test/kotlin/lotto/LotteryTest.kt
@@ -1,7 +1,7 @@
 package lotto
 
 import domain.Lottery
-import domain.LotteryNumbers
+import fixture.LotteryFixture.TEST_MOCK_LOTTERY
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -9,16 +9,16 @@ class LotteryTest {
 
     @Test
     fun `로또는 중복되지 않는 6개의 랜덤 숫자를 가진다`() {
-        val lottery = Lottery(LotteryNumbers(listOf(1, 3, 5, 6, 2, 8)))
+        val lottery = Lottery(TEST_MOCK_LOTTERY)
 
         val numbers = lottery.numbers
 
-        assertThat(numbers.numbers.size).isEqualTo(5)
+        assertThat(numbers.numbers.size).isEqualTo(6)
     }
 
     @Test
     fun `로또의 가격은 1000원이다`() {
-        val lottery = Lottery(LotteryNumbers(listOf(5)))
+        val lottery = Lottery(TEST_MOCK_LOTTERY)
 
         assertThat(lottery.price).isEqualTo(1000)
     }
