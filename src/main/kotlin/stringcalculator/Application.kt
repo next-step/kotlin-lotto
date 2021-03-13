7971d64c1f69d51a0e439a2b02bf18fabb2f827e
thxwelchs <thxwelchs@gmail.com>
2021-03-13 09:51:54 +0900

    refactor(main) main 코드 수정

--- src/main/kotlin/stringcalculator/Application.kt
+++ src/main/kotlin/stringcalculator/Application.kt
@@ -1,9 +1,13 @@
 package stringcalculator
 
+import stringcalculator.domain.StringAddCalculator
+import stringcalculator.domain.StringTokenizer
 import stringcalculator.view.InputView
 import stringcalculator.view.ResultView
 
 fun main() {
-    val tokens = InputView.input()
-    ResultView.doResult(tokens)
+    val inputValue = InputView.input()
+    val tokens = StringTokenizer.tokenize(inputValue)
+    val resultValue = StringAddCalculator.calculate(tokens)
+    ResultView.doResult(resultValue)
 }
