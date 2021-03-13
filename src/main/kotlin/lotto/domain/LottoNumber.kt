f26e72baeed9571bc5f58e25b4985bd4359ec6b0
bactoria <bactoria@gmail.com>
2021-03-14 01:53:26 +0900

    refactor: data class로 변경
    data class: equals, hashCode, toString, copy, componentN 생성
    리뷰: https://github.com/next-step/kotlin-lotto/pull/108#discussion_r592025475

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -1,6 +1,6 @@
 package lotto.domain
 
-class LottoNumber(val value: Int) {
+data class LottoNumber(val value: Int) {
 
     init {
         check(isValidLottoNumberRange()) { "로또번호를 생성할 수 없는 값이다. value: $value" }
@@ -8,24 +8,5 @@ class LottoNumber(val value: Int) {
 
     private fun isValidLottoNumberRange(): Boolean {
         return value in 1..45
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as LottoNumber
-
-        if (value != other.value) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return value
-    }
-
-    override fun toString(): String {
-        return "LottoNumber(value=$value)"
     }
 }
