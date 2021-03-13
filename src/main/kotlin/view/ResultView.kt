8423336a172fbfd824ba67d006973b2c2ff0abec
sanggu <tkdrn8578@naver.com>
2021-03-13 21:56:12 +0900

    feat: 로또 결과를 출력하는 printLottery 함수와 joinToLotteryNumber 함수 추가

--- src/main/kotlin/view/ResultView.kt
+++ src/main/kotlin/view/ResultView.kt
@@ -1,7 +1,18 @@
 package view
 
+import domain.Lotteries
+
 object ResultView {
-    fun printInt() {
-        println("실행 결과")
+    private const val LOTTERY_NUMBER_DELIMITERS = ","
+
+    fun printLotteries(lotteries: Lotteries) {
+    }
+
+    fun printLottery(LotteryNumbers: List<String>) {
+        print("[ ${jointToLotteryNumbers(LotteryNumbers, LOTTERY_NUMBER_DELIMITERS)} ]")
+    }
+
+    private fun jointToLotteryNumbers(lotteryNumbers: List<String>, delimiter: String): String {
+        return lotteryNumbers.joinToString("$delimiter ")
     }
 }
