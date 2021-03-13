c08f917bb23503a5df18b4060cb3242234c2ca92
sanggu <tkdrn8578@naver.com>
2021-03-13 21:55:27 +0900

    feat: RandomNumbers 는 랜덤 값만 생성하는 역핢만 하도록 수정

--- src/main/kotlin/domain/RandomNumbers.kt
+++ src/main/kotlin/domain/RandomNumbers.kt
@@ -3,20 +3,7 @@ package domain
 import kotlin.random.Random
 
 class RandomNumbers: Numbers {
-
-    override fun makeNumbers(size: Int): HashSet<Int> {
-        return (START_INDEX..size)
-            .map { makeNumber() }
-            .toHashSet()
-    }
-
-    private fun makeNumber(): Int {
-        return Random.nextInt(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
-    }
-
-    companion object {
-        private const val START_INDEX = 0
-        private const val MIN_RANDOM_NUMBER = 0
-        private const val MAX_RANDOM_NUMBER = 100
+    override fun makeNumber(min: Int, max: Int): Int {
+        return Random.nextInt(min, max)
     }
 }
