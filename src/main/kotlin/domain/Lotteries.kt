7782bd7a7ee9e360895994e50fa6296b2692dd19
sanggu <tkdrn8578@naver.com>
2021-03-13 21:48:57 +0900

    refactor: LotteryNumbers.of 로 생성함에 따른 불필요한 상수 제거

--- src/main/kotlin/domain/Lotteries.kt
+++ src/main/kotlin/domain/Lotteries.kt
@@ -1,12 +1,9 @@
 package domain
 
 class Lotteries(var lotteries: List<Lottery>) {
-
     companion object {
         fun of(count: Int): Lotteries {
-            return Lotteries((1..count).map { Lottery(LotteryNumbers(RandomNumbers(LOTTERY_NUMBERS_SIZE))) })
+            return Lotteries((1..count).map { Lottery(LotteryNumbers.of(RandomNumbers())) })
         }
-
-        private const val LOTTERY_NUMBERS_SIZE = 6
     }
 }
