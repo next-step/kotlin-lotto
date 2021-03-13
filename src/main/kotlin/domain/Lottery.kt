dbabc75ab86c588e0f3f9e5b40914fe6f1e3d0a4
sanggu <tkdrn8578@naver.com>
2021-03-13 21:56:25 +0900

    refactor: 불필요한 공백 제거

--- src/main/kotlin/domain/Lottery.kt
+++ src/main/kotlin/domain/Lottery.kt
@@ -1,7 +1,6 @@
 package domain
 
 class Lottery(val numbers: LotteryNumbers, val price: Int = DEFAULT_LOTTO_PRICE) {
-
     companion object {
         private const val DEFAULT_LOTTO_PRICE = 1000
     }
