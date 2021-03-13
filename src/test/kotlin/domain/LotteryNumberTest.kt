0b6727926a2e44013e9b5aefd0c573a194fb3a97
sanggu <tkdrn8578@naver.com>
2021-03-13 21:51:14 +0900

    test: 입력 값에 대한 로또 번호를 가져오는 것의 테스트 코드 추가

--- src/test/kotlin/domain/LotteryNumberTest.kt
+++ src/test/kotlin/domain/LotteryNumberTest.kt
@@ -1,15 +1,32 @@
 package domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
+import java.util.*
 
 class LotteryNumberTest {
     @ParameterizedTest
     @ValueSource(ints = [-1, 46, 90, -10])
     fun `0부터 45가 아닌 숫자가 들어왔을 때 예외를 처리한다`(wrongNumber: Int) {
+        val mockLotteryNumber = makeMockNumberByInt(wrongNumber)
+
         assertThrows<IllegalArgumentException> {
-            LotteryNumber.from(wrongNumber)
+            LotteryNumber.from(mockLotteryNumber)
         }
     }
+
+    @ParameterizedTest
+    @CsvSource("4, 4", "25, 25", "9, 9")
+    fun `입력한 값에 대한 로또 번호를 가져온다`(number: Int, expectedNumber: Int) {
+        val mockLotteryNumber = makeMockNumberByInt(number)
+
+        val lotteryNumber = LotteryNumber.from(mockLotteryNumber)
+
+        assertThat(lotteryNumber).isEqualTo(makeMockNumberByInt(expectedNumber).retrieveNumber())
+    }
+
+    private fun makeMockNumberByInt(number: Int) = MockNumbers(LinkedList(listOf(number)))
 }
