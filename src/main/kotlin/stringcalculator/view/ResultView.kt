308ed00990f7f6ada56f83887e8e3068f2d914db
thxwelchs <thxwelchs@gmail.com>
2021-03-13 09:51:33 +0900

    refactor(view): ResultView의 기능은 출력 할 값을 인자로받아 출력만 하는 역할을 갖게끔 수정

--- src/main/kotlin/stringcalculator/view/ResultView.kt
+++ src/main/kotlin/stringcalculator/view/ResultView.kt
@@ -1,10 +1,7 @@
 package stringcalculator.view
 
-import stringcalculator.domain.StringAddCalculator
-import stringcalculator.domain.Tokens
-
 object ResultView {
-    fun doResult(tokens: Tokens) {
-        println(StringAddCalculator.calculate(tokens))
+    fun doResult(resultValue: Int) {
+        println(resultValue)
     }
 }
