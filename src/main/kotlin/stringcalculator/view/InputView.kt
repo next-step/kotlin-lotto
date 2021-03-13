7f0b5921f118dc0e7a6411017819c0f4de74cf23
thxwelchs <thxwelchs@gmail.com>
2021-03-13 09:50:51 +0900

    refactor(view): InputView의 input 기능은 입력에대한 역할만하게끔 수정

--- src/main/kotlin/stringcalculator/view/InputView.kt
+++ src/main/kotlin/stringcalculator/view/InputView.kt
@@ -1,10 +1,11 @@
 package stringcalculator.view
 
-import stringcalculator.domain.StringTokenizer
-import stringcalculator.domain.Tokens
-
 object InputView {
-    fun input(): Tokens {
-        return StringTokenizer.tokenize(readLine() ?: throw IllegalArgumentException(""))
+    private const val ZERO_VALUE = "0"
+
+    fun input(): String {
+        val inputValue = readLine() ?: throw IllegalArgumentException("")
+        if(inputValue.isNullOrBlank()) return ZERO_VALUE
+        return inputValue
     }
 }
