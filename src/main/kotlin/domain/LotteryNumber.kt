9bc5c440dd01db674e22972a4a6d3538278005a3
sanggu <tkdrn8578@naver.com>
2021-03-13 21:54:19 +0900

    refactor: LotteryNumber 가 비교했을 때 같음을 검증하기 위한 equlas, hashcode 추가

--- src/main/kotlin/domain/LotteryNumber.kt
+++ src/main/kotlin/domain/LotteryNumber.kt
@@ -1,14 +1,30 @@
 package domain
 
-class LotteryNumber(private val number: Int) {
+class LotteryNumber private constructor(private val number: Int) {
     companion object {
         private const val MIN_RANDOM_NUMBER = 0
         private const val MAX_RANDOM_NUMBER = 45
         private val NUMBERS: Map<Int, LotteryNumber>
                 = (MIN_RANDOM_NUMBER..MAX_RANDOM_NUMBER).associateWith { LotteryNumber(it) }
 
-        fun from(value: Int): LotteryNumber {
-            return NUMBERS[value] ?: throw IllegalArgumentException("잘못된 로또 번호입니다.")
+        fun from(number: Numbers): LotteryNumber {
+            val randomNumber = number.makeNumber(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
+            return NUMBERS[randomNumber] ?: throw IllegalArgumentException("잘못된 로또 번호입니다.")
         }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as LotteryNumber
+
+        if (number != other.number) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return number
     }
 }
