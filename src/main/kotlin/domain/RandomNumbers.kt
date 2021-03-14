73221befd756d3ea120ec83ad9bdd85cbde8411a
sanggu <tkdrn8578@naver.com>
2021-03-14 23:45:38 +0900

    feat: 미리 랜덤 숫자들을 만는 makeNumbers 함수 추가

--- src/main/kotlin/domain/RandomNumbers.kt
+++ src/main/kotlin/domain/RandomNumbers.kt
@@ -3,6 +3,9 @@ package domain
 import kotlin.random.Random
 
 class RandomNumbers: Numbers {
+    fun makeNumbers(min: Int, max: Int, size: Int): List<Int> {
+        return (1..size).map { Random.nextInt(min, max)} }
+
     override fun makeNumber(min: Int, max: Int): Int {
         return Random.nextInt(min, max)
     }
