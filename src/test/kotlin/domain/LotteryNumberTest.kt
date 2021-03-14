67fca7c5c418ed2b286c6e2f65962afa1cb3a026
sanggu <tkdrn8578@naver.com>
2021-03-14 23:44:21 +0900

    refactor: 로또번호가 중복되면 안된다는 것에 대한 테스트 코드 추가

--- src/test/kotlin/domain/LotteryNumberTest.kt
+++ src/test/kotlin/domain/LotteryNumberTest.kt
@@ -1,32 +1,24 @@
 package domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
-import java.util.*
 
 class LotteryNumberTest {
     @ParameterizedTest
     @ValueSource(ints = [-1, 46, 90, -10])
     fun `0부터 45가 아닌 숫자가 들어왔을 때 예외를 처리한다`(wrongNumber: Int) {
-        val mockLotteryNumber = makeMockNumberByInt(wrongNumber)
-
         assertThrows<IllegalArgumentException> {
-            LotteryNumber.from(mockLotteryNumber)
+            LotteryNumber.from(wrongNumber)
         }
     }
 
-    @ParameterizedTest
-    @CsvSource("4, 4", "25, 25", "9, 9")
-    fun `입력한 값에 대한 로또 번호를 가져온다`(number: Int, expectedNumber: Int) {
-        val mockLotteryNumber = makeMockNumberByInt(number)
+    @Test
+    fun `로또 번호는 중복되면 안된다`() {
+        val lotteryNumbers = LotteryNumbers(listOf(1, 1, 2, 3, 4, 5, 7))
 
-        val lotteryNumber = LotteryNumber.from(mockLotteryNumber)
-
-        assertThat(lotteryNumber).isEqualTo(makeMockNumberByInt(expectedNumber).retrieveNumber())
+        assertThat(lotteryNumbers.lotteryNumbers.size).isEqualTo(6)
     }
-
-    private fun makeMockNumberByInt(number: Int) = MockNumbers(LinkedList(listOf(number)))
 }
