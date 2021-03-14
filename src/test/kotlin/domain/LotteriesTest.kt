c53654d3c8d844ad11e16b51c7ca4c4518210591
sanggu <tkdrn8578@naver.com>
2021-03-14 23:43:41 +0900

    refactor: 로또 번호를 생성 시 5개가 아닌 6개로 생성되되는 것을 검증하도록 수정

--- src/test/kotlin/domain/LotteriesTest.kt
+++ src/test/kotlin/domain/LotteriesTest.kt
@@ -7,12 +7,12 @@ import org.junit.jupiter.api.assertAll
 class LotteriesTest {
     @Test
     fun `로또들들 생성한다`() {
-        val createdLotteries = Lotteries.of(5)
+        val createdLotteries = Lotteries.of(6)
 
         val lotteries = createdLotteries.lotteries
 
         assertAll(
-            { assertThat(lotteries.size).isEqualTo(5) },
+            { assertThat(lotteries.size).isEqualTo(6) },
             { assertThat(lotteries[0].numbers).isNotNull },
             { assertThat(lotteries[0].price).isEqualTo(1000) }
         )
