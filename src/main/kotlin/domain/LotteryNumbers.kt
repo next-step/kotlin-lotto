0e993195e1ef593f5d9efd0acd89788725c6fb25
sanggu <tkdrn8578@naver.com>
2021-03-14 23:45:01 +0900

    refactor: 로또 넘버를 List<Int> 로 받아서 해당 값을 HashSet 으로 저장하는 변수 추가

--- src/main/kotlin/domain/LotteryNumbers.kt
+++ src/main/kotlin/domain/LotteryNumbers.kt
@@ -1,20 +1,7 @@
 package domain
 
-class LotteryNumbers(val numbers: HashSet<LotteryNumber>) {
-    init {
-        require(numbers.size == LOTTERY_NUMBERS_SIZE) { "숫자는 ${LOTTERY_NUMBERS_SIZE}개 이상이여야 합니다." }
-    }
-
-    companion object {
-        private const val START_INDEX = 0
-        private const val LOTTERY_NUMBERS_SIZE = 6
-
-        fun of(number: Numbers): LotteryNumbers {
-            val numbers = (START_INDEX..LOTTERY_NUMBERS_SIZE)
-                .map { LotteryNumber.from(number) }
-                .toHashSet()
-
-            return LotteryNumbers(numbers)
-        }
-    }
+class LotteryNumbers(val numbers: List<Int>) {
+    val lotteryNumbers: HashSet<LotteryNumber> = numbers.map {
+        LotteryNumber.from(it)
+    }.toHashSet()
 }
