82e1c4b8be650e846b8ce20c8a5195dacd16f2aa
sanggu <tkdrn8578@naver.com>
2021-03-14 23:46:05 +0900

    refactor: LotteryNumbers를 List<Int>로 파라미터를 받아 생성하도록 변경함에 따른 수정

--- src/test/kotlin/fixture/LotteryFixture.kt
+++ src/test/kotlin/fixture/LotteryFixture.kt
@@ -1,10 +1,8 @@
 package fixture
 
 import domain.LotteryNumbers
-import domain.MockNumbers
-import java.util.*
 
 object LotteryFixture {
     private val TEST_MOCK_NUMBERS = listOf(1, 3, 5, 6, 2, 8)
-    val TEST_MOCK_LOTTERY = LotteryNumbers.of(MockNumbers(LinkedList(TEST_MOCK_NUMBERS)))
+    val TEST_MOCK_LOTTERY = LotteryNumbers(TEST_MOCK_NUMBERS)
 }
