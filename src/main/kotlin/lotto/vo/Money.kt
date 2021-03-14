f3841db6202d8bf426e1ccad7a8061df4e9199e4
dev-owner <yureka1112@gmail.com>
2021-03-14 20:37:36 +0900

    feat(lotto): lotto vo 구현

---
+++ src/main/kotlin/lotto/vo/Money.kt
@@ -0,0 +1,18 @@
+package lotto.vo
+
+data class Money(val money: Int) {
+    init {
+        require(money >= DEFAULT_MONEY) { "돈은 0보다 작을 수 없습니다." }
+    }
+
+    companion object {
+        private const val DEFAULT_MONEY = 0
+        fun from(num: String): Money {
+            return Money(
+                num.toIntOrNull() ?: throw IllegalArgumentException("돈은 정수형만 가능합니다. Invalid argument : $num")
+            )
+        }
+    }
+
+    operator fun div(right: Money) = this.money / right.money
+}
---
+++ src/main/kotlin/lotto/vo/WinnerLotto.kt
@@ -0,0 +1,3 @@
+package lotto.vo
+
+data class WinnerLotto(val winnerLotto: List<LottoNumber>)
