cb8b1e6def5c1f6511338829c860a3731459848a
sanggu <tkdrn8578@naver.com>
2021-03-14 23:46:28 +0900

    refactor: LotteryNumbers.of 제거함에 따른 생성자로 받아서 생성하도록 수정

--- src/main/kotlin/domain/Lotteries.kt
+++ src/main/kotlin/domain/Lotteries.kt
@@ -3,7 +3,8 @@ package domain
 class Lotteries(var lotteries: List<Lottery>) {
     companion object {
         fun of(count: Int): Lotteries {
-            return Lotteries((1..count).map { Lottery(LotteryNumbers.of(RandomNumbers())) })
+            val random = RandomNumbers()
+            return Lotteries((1..count).map { Lottery(LotteryNumbers(random.makeNumbers(1, 45, 6))) })
         }
     }
 }
