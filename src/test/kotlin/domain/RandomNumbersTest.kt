2f1694fea9259ef27d70a6ae12c212104d839555
sanggu <tkdrn8578@naver.com>
2021-03-14 23:51:56 +0900

    refactor: 6개 랜덤 값을 검증하는 것에 대한 검증 값에 사이즈 추가

--- src/test/kotlin/domain/RandomNumbersTest.kt
+++ src/test/kotlin/domain/RandomNumbersTest.kt
@@ -10,11 +10,12 @@ class RandomNumbersTest {
     fun `6개의 랜덤 값을 생성한다`() {
         val randomNumber = RandomNumbers()
 
-        val randoms = randomNumber.makeNumber(0, 45)
+        val randoms = randomNumber.makeNumbers(1, 45, 6)
 
         assertAll(
-            { assertThat(randoms).isGreaterThan(0) },
-            { assertThat(randoms).isLessThan(45) }
+            { assertThat(randoms.size).isEqualTo(6) },
+            { assertThat(randoms[0]).isGreaterThan(1) },
+            { assertThat(randoms[0]).isLessThan(45) }
         )
     }
 }
