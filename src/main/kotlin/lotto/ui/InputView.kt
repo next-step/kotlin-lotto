395e8dffaf50f6351409c428649dcbb043bdaf18
Joseph <wjsdudtkd95@gmail.com>
2021-03-14 16:35:10 +0900

    refactor(lotto): Input시 예외처리 추가

--- src/main/kotlin/lotto/ui/InputView.kt
+++ src/main/kotlin/lotto/ui/InputView.kt
@@ -4,12 +4,12 @@ class InputView {
 
     fun inputMoney(): Int {
         println("구입금액을 입력해 주세요.")
-        return readLine()!!.toInt()
+        return readLine()?.toIntOrNull() ?: 0
     }
 
     fun inputNumberOfManual(): Int {
         println("\n수동으로 구매할 로또 수를 입력해 주세요")
-        return readLine()!!.toInt()
+        return readLine()?.toIntOrNull() ?: 0
     }
 
     fun inputManualNumbers(numberOfManual: Int): List<List<String>> {
@@ -25,11 +25,11 @@ class InputView {
 
     fun inputPrizeNumber(): List<String> {
         println("\n지난 주 당첨 번호를 입력해 주세요")
-        return readLine()!!.split(", ")
+        return readLine()!!.split(", ") ?: throw IllegalArgumentException("당첨번호는 공백일 수 없습니다.")
     }
 
     fun inputBonusNumber(): Int {
         println("보너스 볼을 입력해 주세요.")
-        return readLine()!!.toInt()
+        return readLine()?.toIntOrNull() ?: throw RuntimeException("보너스볼은 공백일 수 없습니다.")
     }
 }
