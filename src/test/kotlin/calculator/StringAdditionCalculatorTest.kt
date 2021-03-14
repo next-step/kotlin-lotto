7e1497b097e1426f77fe39909cc20a6f753af14d
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-14 19:35:22 +0900

    refactor: 중복 생성 제거

--- src/test/kotlin/calculator/StringAdditionCalculatorTest.kt
+++ src/test/kotlin/calculator/StringAdditionCalculatorTest.kt
@@ -4,15 +4,15 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class StringAdditionCalculatorTest {
+    private val calculator = StringAdditionCalculator()
+
     @Test
     fun `빈 문자열이 주어질 경우, 0을 반환`() {
-        val calculator = StringAdditionCalculator()
         assertThat(calculator.calculate("")).isEqualTo(0)
     }
 
     @Test
     fun `null이 주어질 경우, 0을 반환`() {
-        val calculator = StringAdditionCalculator()
         assertThat(calculator.calculate(null)).isEqualTo(0)
     }
 }
