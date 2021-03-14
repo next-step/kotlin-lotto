e342413c5a2c8d1b5c1f2b0df88cd3a0d6e86ddb
kit(김광수) <kit.t@kakaocorp.com>
2021-03-14 14:00:42 +0900

    feat: make it pass

--- src/test/kotlin/lotto/domain/LottoAgentTest.kt
+++ src/test/kotlin/lotto/domain/LottoAgentTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 
 class LottoAgentTest {
     @Test
-    fun `수동/자동 구매수량만큼 로또번호를 생성한다`() {
+    fun `수동,자동 구매수량만큼 로또번호를 생성한다`() {
         val exchange: Exchange<LottoNumbers> = LottoAgent(LottoDrawMachine((1..45))).exchange
         val manualPick = LottoNumbers(1, 2, 3, 4, 5, 6)
         exchange.pay(Money(14_000), listOf(manualPick))
