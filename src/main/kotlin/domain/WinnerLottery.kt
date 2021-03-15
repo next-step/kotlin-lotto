b6600be27861e7c9df064f2c4451f17c8e2278dd
sanggu <tkdrn8578@naver.com>
2021-03-16 00:13:04 +0900

    feat: 로또번호 맞춘 개수에 따라 금액을 리턴하는 로직 추가

--- src/main/kotlin/domain/WinnerLottery.kt
+++ src/main/kotlin/domain/WinnerLottery.kt
@@ -3,6 +3,31 @@ package domain
 class WinnerLottery(numbers: List<Int>) {
     val numbers: LotteryNumbers = LotteryNumbers(numbers)
 
+    fun math(lotteries: Lotteries): HashMap<Int, Int> {
+        val result = HashMap<Int, Int>()
+
+        lotteries.lotteries.map {
+            val count = matchCount(it.numbers)
+            if (count == 3) {
+                result[3] = result.getOrDefault(3, 0) + 1
+            }
+
+            if (count == 4) {
+                result[4] = result.getOrDefault(4, 0) + 1
+            }
+
+            if (count == 5) {
+                result[5] = result.getOrDefault(5, 0) + 1
+            }
+
+            if (count == 6) {
+                result[6] = result.getOrDefault(6, 0) + 1
+            }
+        }
+
+        return result
+    }
+
     fun matchCount(lotteryNumbers: LotteryNumbers): Int {
         return lotteryNumbers.numbers.filter { this.numbers.numbers.contains(it) }.count()
     }
