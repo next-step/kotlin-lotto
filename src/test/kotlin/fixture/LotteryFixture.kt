a854b19c89404ce57507c3d8ddad6b38a97daf47
sanggu <tkdrn8578@naver.com>
2021-03-15 23:35:47 +0900

    refactor: 다른 테스트에서도 사용하기 위한 접근제어자 수정

--- src/test/kotlin/fixture/LotteryFixture.kt
+++ src/test/kotlin/fixture/LotteryFixture.kt
@@ -3,7 +3,7 @@ package fixture
 import domain.LotteryNumbers
 
 object LotteryFixture {
-    private val TEST_NUMBERS = listOf(1, 3, 5, 6, 2, 8)
+    val TEST_NUMBERS = listOf(1, 3, 5, 6, 2, 8)
     val TEST_LOTTERY_NUMBERS = LotteryNumbers(TEST_NUMBERS)
 
     const val MIN_LOTTERY_NUMBER = 1
