a6a1c6d597bd25a8184657e3bd1c2aa2bd9559c3
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-15 18:29:55 +0900

    test: 입력 개수만큼 로또를 생성한다 테스트 코드 추가

--- src/test/kotlin/domain/LotteriesTest.kt
+++ src/test/kotlin/domain/LotteriesTest.kt
@@ -2,19 +2,12 @@ package domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertAll
 
 class LotteriesTest {
     @Test
-    fun `로또들들 생성한다`() {
+    fun `입력 개수만큼 로또를 생성한다`() {
         val createdLotteries = Lotteries.of(6)
 
-        val lotteries = createdLotteries.lotteries
-
-        assertAll(
-            { assertThat(lotteries.size).isEqualTo(6) },
-            { assertThat(lotteries[0].numbers).isNotNull },
-            { assertThat(lotteries[0].price).isEqualTo(1000) }
-        )
+        assertThat(createdLotteries.lotteries.size).isEqualTo(6)
     }
 }
