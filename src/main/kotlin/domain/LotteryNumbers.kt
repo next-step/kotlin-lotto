b80afc5352a2aaabe7c1a86fb7626d70fd598540
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-15 18:05:19 +0900

    feat: 로또 번호가 6자리가 아닐 때 예외처리하도록 추가

--- src/main/kotlin/domain/LotteryNumbers.kt
+++ src/main/kotlin/domain/LotteryNumbers.kt
@@ -4,4 +4,12 @@ class LotteryNumbers(numbers: List<Int>) {
     val numbers: HashSet<LotteryNumber> = numbers.map {
         LotteryNumber.from(it)
     }.toHashSet()
+
+    init {
+        require(numbers.size == MAX_LOTTERY_NUMBERS_SIZE) { "로또 번호의 개수는 6개이여야 합니다. : ${numbers.size}" }
+    }
+
+    companion object {
+        const val MAX_LOTTERY_NUMBERS_SIZE = 6
+    }
 }
