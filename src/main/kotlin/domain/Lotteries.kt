14d2f1902930cb8f7652cc5d6a7897af37d38ea9
sanggu <tkdrn8578@naver.com>
2021-03-15 21:31:20 +0900

    feat: 로또 생성 시 로또 생성 요청 개수가 0이하인 경우 예외처리하도록 추가

--- src/main/kotlin/domain/Lotteries.kt
+++ src/main/kotlin/domain/Lotteries.kt
@@ -2,13 +2,17 @@ package domain
 
 class Lotteries(var lotteries: List<Lottery>) {
     companion object {
+        private const val START_LOTTERY_COUNT = 1
         private const val MIN_LOTTERY_NUMBER = 1
         private const val MAX_LOTTERY_NUMBER = 45
         private const val LOTTERY_SIZE = 6
+        private const val MIN_LOTTERY_COUNT = 0
 
         fun of(count: Int): Lotteries {
+            require(count > MIN_LOTTERY_COUNT) { "입력한 로또의 개수가 올바르지 않습니다." }
+
             val randoms = RandomNumbers.generate(MIN_LOTTERY_NUMBER, MAX_LOTTERY_NUMBER, LOTTERY_SIZE)
-            return Lotteries((1..count).map { Lottery(LotteryNumbers(randoms)) })
+            return Lotteries((START_LOTTERY_COUNT..count).map { Lottery(LotteryNumbers(randoms)) })
         }
     }
 }
