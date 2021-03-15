9cc5cf4944f3669c90df47eb5b320337dc99bb91
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-16 00:22:00 +0900

    refactor: 중복 테스트 코드 제거

--- src/test/kotlin/calculator/StringAdditionCalculatorTest.kt
+++ src/test/kotlin/calculator/StringAdditionCalculatorTest.kt
@@ -1,9 +1,9 @@
 package calculator
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class StringAdditionCalculatorTest {
     private val calculator = StringAdditionCalculator()
@@ -14,9 +14,9 @@ internal class StringAdditionCalculatorTest {
         assertThat(calculator.calculate(expression)).isEqualTo(0)
     }
 
-    @Test
-    fun `숫자 하나가 주어질 경우, 해당 숫자를 반환`() {
-        assertThat(calculator.calculate("1")).isEqualTo(1)
-        assertThat(calculator.calculate("100")).isEqualTo(100)
+    @ParameterizedTest
+    @ValueSource(ints = [1, 100])
+    fun `숫자 하나가 주어질 경우, 해당 숫자를 반환`(integer: Int) {
+        assertThat(calculator.calculate("$integer")).isEqualTo(integer)
     }
 }
