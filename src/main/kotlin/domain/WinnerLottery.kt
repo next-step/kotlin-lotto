3a049acb454e1e98cd13451b113f044f4dccda6e
sanggu <tkdrn8578@naver.com>
2021-03-15 23:35:16 +0900

    feat: 당첨번호와 입력한 로또번호와 일치하는 개수를 리턴하는 기능 추가

--- src/main/kotlin/domain/WinnerLottery.kt
+++ src/main/kotlin/domain/WinnerLottery.kt
@@ -3,5 +3,7 @@ package domain
 class WinnerLottery(numbers: List<Int>) {
     val numbers: LotteryNumbers = LotteryNumbers(numbers)
 
-
+    fun matchCount(lotteryNumbers: LotteryNumbers): Int {
+        return lotteryNumbers.numbers.filter { this.numbers.numbers.contains(it) }.count()
+    }
 }
