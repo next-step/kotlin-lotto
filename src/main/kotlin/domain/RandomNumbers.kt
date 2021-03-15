02ce84703447b71eae733dcada2ebbac975845ba
sanggu <tkdrn8578@naver.com>
2021-03-15 23:19:40 +0900

    refactor:  RandomNumber generate 에 sorted 추가

--- src/main/kotlin/domain/RandomNumbers.kt
+++ src/main/kotlin/domain/RandomNumbers.kt
@@ -4,7 +4,7 @@ object RandomNumbers {
     private const val MIN_INDEX = 0
 
     fun generate(min: Int, max: Int, size: Int): List<Int> {
-        val numbers = (min..max).map { it }.shuffled()
+        val numbers = (min..max).map { it }.shuffled().sorted()
         return numbers.subList(MIN_INDEX, size)
     }
 }
