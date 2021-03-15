ef464aecc6bc555a7c4256a3573b13c7453b5846
sanggu <tkdrn8578@naver.com>
2021-03-15 21:48:54 +0900

    refactor: parameterizedTest 로 경우의 수 추가

--- src/test/kotlin/domain/LotteryFactoryTest.kt
+++ src/test/kotlin/domain/LotteryFactoryTest.kt
@@ -1,16 +1,17 @@
 package domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class LotteryFactoryTest {
-    @Test
-    fun `금액에 따라 로또 생성 개수를 정해진다`() {
-        val factory = LotteryFactory(5000)
+    @ParameterizedTest
+    @CsvSource("5000, 5", "2400, 2", "1100, 1")
+    fun `금액에 따라 로또 생성 개수를 정해진다`(inputPrice: Int, expectCount: Int) {
+        val factory = LotteryFactory(inputPrice)
 
         val count = factory.calculateLotteryCountByPrice()
 
-        assertThat(count).isEqualTo(5)
-
+        assertThat(count).isEqualTo(expectCount)
     }
 }
