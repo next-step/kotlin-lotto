d12e086decb77ed1ef87f9b8455dadcd8034adb9
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-15 18:29:29 +0900

    refactor: lotterynumbers fixture name 변경

--- src/test/kotlin/fixture/LotteryFixture.kt
+++ src/test/kotlin/fixture/LotteryFixture.kt
@@ -3,6 +3,6 @@ package fixture
 import domain.LotteryNumbers
 
 object LotteryFixture {
-    private val TEST_MOCK_NUMBERS = listOf(1, 3, 5, 6, 2, 8)
-    val TEST_MOCK_LOTTERY = LotteryNumbers(TEST_MOCK_NUMBERS)
+    private val TEST_NUMBERS = listOf(1, 3, 5, 6, 2, 8)
+    val TEST_LOTTERY_NUMBERS = LotteryNumbers(TEST_NUMBERS)
 }
