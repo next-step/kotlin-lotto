0e630afbfa233849497de5f9ba46e08e8ed7fd84
sanggu <tkdrn8578@naver.com>
2021-03-15 21:58:52 +0900

    feat: 입력된 금액이 올바르지 않을 때 예외처리하도록 추가

--- src/main/kotlin/domain/LotteryFactory.kt
+++ src/main/kotlin/domain/LotteryFactory.kt
@@ -2,7 +2,11 @@ package domain
 
 import domain.Lottery.Companion.DEFAULT_LOTTO_PRICE
 
-class LotteryFactory(val inputPrice: Int, val unitPrice: Int = DEFAULT_LOTTO_PRICE) {
+class LotteryFactory(private val inputPrice: Int, private val unitPrice: Int = DEFAULT_LOTTO_PRICE) {
+    init {
+        require(unitPrice in DEFAULT_LOTTO_PRICE until inputPrice) { "잘못된 금액을 입력하였습니다." }
+    }
+
     fun calculateLotteryCountByPrice(): Int {
         return inputPrice / unitPrice
     }
