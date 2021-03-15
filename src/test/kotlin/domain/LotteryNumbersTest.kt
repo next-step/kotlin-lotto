140e5e97d9906c18ec8dd5e2ad94cee52a7a0370
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-15 17:35:24 +0900

    feat: LotteryNumbers의 6개의 로또 번호를 가진다 테스트 코드 추가

---
+++ src/test/kotlin/domain/LotteryNumbersTest.kt
@@ -0,0 +1,20 @@
+package domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LotteryNumbersTest {
+    @Test
+    fun `6개의 로또 번호를 가진다`() {
+        val lotteryNumbers = LotteryNumbers(listOf(1, 2, 3, 4, 5, 6))
+
+        assertThat(lotteryNumbers.lotteryNumbers).containsExactlyInAnyOrder(
+            LotteryNumber.from(1),
+            LotteryNumber.from(2),
+            LotteryNumber.from(3),
+            LotteryNumber.from(4),
+            LotteryNumber.from(5),
+            LotteryNumber.from(6)
+        )
+    }
+}
