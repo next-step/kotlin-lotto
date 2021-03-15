408a2e1fce8ff8edf472b3b6a69adcfec0f2bfab
sanggu <tkdrn8578@naver.com>
2021-03-15 21:27:22 +0900

    test: 입력 개수만큼 로또를 생성하는지에 대한 테스트 코드 추가

--- src/test/kotlin/domain/RandomNumbersTest.kt
+++ src/test/kotlin/domain/RandomNumbersTest.kt
@@ -1,14 +1,15 @@
 package domain
 
+import fixture.LotteryFixture.MAX_LOTTERY_NUMBER
+import fixture.LotteryFixture.MIN_LOTTERY_NUMBER
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 
 class RandomNumbersTest {
-
     @Test
     fun `6개의 1부터 45이하의 중복되지 않는 랜덤 값을 생성한다`() {
-        val randoms = RandomNumbers.generate(1, 45, 6)
+        val randoms = RandomNumbers.generate(MIN_LOTTERY_NUMBER, MAX_LOTTERY_NUMBER, 6)
 
         assertAll(
             { assertThat(randoms.size).isEqualTo(6) },
@@ -18,10 +19,17 @@ class RandomNumbersTest {
         )
     }
 
+    @Test
+    fun `입력 개수만큼 로또를 생성한다`() {
+        val lotteries = Lotteries.of(5)
+
+        assertThat(lotteries.lotteries).hasSize(5)
+    }
+
     private fun assertAllRangeOfNumbers(randoms: List<Int>) {
         randoms.map {
-            assertThat(it).isGreaterThan(1)
-            assertThat(it).isLessThan(45)
+            assertThat(it).isGreaterThan(MIN_LOTTERY_NUMBER)
+            assertThat(it).isLessThan(MAX_LOTTERY_NUMBER)
         }
     }
 }
