15819d31677c9477c3cf7a72b2de80bb50f95c69
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-15 17:51:52 +0900

    feat: 로또 번호의 개수가 6개가 아니면 생성되지 않는다에 대한 테스트 코드 추가

--- src/test/kotlin/domain/LotteryNumbersTest.kt
+++ src/test/kotlin/domain/LotteryNumbersTest.kt
@@ -2,6 +2,11 @@ package domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class LotteryNumbersTest {
     @Test
@@ -16,5 +21,24 @@ class LotteryNumbersTest {
             LotteryNumber.from(5),
             LotteryNumber.from(6)
         )
+    }
+
+    @ParameterizedTest
+    @MethodSource("wrongSizeLotteryNumbers")
+    fun `로또 번호의 개수가 6개가 아니면 생성되지 않는다`(numbers: List<Int>) {
+        assertThrows<IllegalArgumentException> {
+            LotteryNumbers(numbers)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun wrongSizeLotteryNumbers(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of(1, 2, 3, 3, 5, 6, 7),
+                Arguments.of(4, 10, 25, 34),
+                Arguments.of(4),
+                Arguments.of()
+            )
     }
 }
