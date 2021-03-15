d5c1603302f00cda8f7df3135f5906cc966f5886
sanggu <tkdrn8578@naver.com>
2021-03-15 21:22:25 +0900

    refactor: 매직넘버를 상수로 추출

--- src/main/kotlin/domain/Lotteries.kt
+++ src/main/kotlin/domain/Lotteries.kt
@@ -2,9 +2,13 @@ package domain
 
 class Lotteries(var lotteries: List<Lottery>) {
     companion object {
+        private const val MIN_LOTTERY_NUMBER = 1
+        private const val MAX_LOTTERY_NUMBER = 45
+        private const val LOTTERY_SIZE = 6
+
         fun of(count: Int): Lotteries {
-            val random = RandomNumbers()
-            return Lotteries((1..count).map { Lottery(LotteryNumbers(random.makeNumbers(1, 45, 6))) })
+            val randoms = RandomNumbers.generate(MIN_LOTTERY_NUMBER, MAX_LOTTERY_NUMBER, LOTTERY_SIZE)
+            return Lotteries((1..count).map { Lottery(LotteryNumbers(randoms)) })
         }
     }
 }
