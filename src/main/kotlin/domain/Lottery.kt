838fa2c209b5a7053db0f85a2a9309bd250ef3cb
sanggu <tkdrn8578@naver.com>
2021-03-15 21:59:10 +0900

    refactor: LotteryFactor 에서 해당 금액에 대한 값을 사용하기 위한 접근제어자 변경

--- src/main/kotlin/domain/Lottery.kt
+++ src/main/kotlin/domain/Lottery.kt
@@ -2,6 +2,6 @@ package domain
 
 class Lottery(val numbers: LotteryNumbers, val price: Int = DEFAULT_LOTTO_PRICE) {
     companion object {
-        private const val DEFAULT_LOTTO_PRICE = 1000
+        const val DEFAULT_LOTTO_PRICE = 1000
     }
 }
