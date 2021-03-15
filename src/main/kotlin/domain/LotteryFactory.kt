3e7724c8c9094712d57b0796746f4202383922ff
sanggu <tkdrn8578@naver.com>
2021-03-15 22:05:27 +0900

    feat: 로또를 구매하는 것에 대한 기능 추가

--- src/main/kotlin/domain/LotteryFactory.kt
+++ src/main/kotlin/domain/LotteryFactory.kt
@@ -7,6 +7,11 @@ class LotteryFactory(private val inputPrice: Int, private val unitPrice: Int = D
         require(unitPrice in DEFAULT_LOTTO_PRICE until inputPrice) { "잘못된 금액을 입력하였습니다." }
     }
 
+    fun buy(): Lotteries {
+        val count = calculateLotteryCountByPrice()
+        return Lotteries.of(count)
+    }
+
     fun calculateLotteryCountByPrice(): Int {
         return inputPrice / unitPrice
     }
