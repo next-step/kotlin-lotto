654a349e09624a8abed93f31f45939dc9b6f0ed7
sanggu <tkdrn8578@naver.com>
2021-03-15 23:28:20 +0900

    test: 당첨번호와 로또번호가 일치하는 개수를 구하는 테스트코드 추가

--- src/test/kotlin/domain/WinnerLotteryTest.kt
+++ src/test/kotlin/domain/WinnerLotteryTest.kt
@@ -1,15 +1,27 @@
 package domain
 
+import fixture.LotteryFixture.TEST_NUMBERS
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class WinnerLotteryTest {
     @Test
     fun `당첨 번호는 6자리를 입력받아 당첨 로또번호를 생성한다`() {
-        val numbers = listOf(1, 2, 3, 4, 5, 6)
+        val numbers = TEST_NUMBERS
 
         val winnerLottery = WinnerLottery(numbers)
 
         assertThat(winnerLottery.numbers).hasSameClassAs(LotteryNumbers(numbers))
+    }
+
+    @Test
+    fun `로또번호와 당첨번호의 일치하는 개수를 구한다`() {
+        val lotteryNumbers = LotteryNumbers(TEST_NUMBERS)
+
+        val winnerLottery = WinnerLottery(listOf(1, 3, 5, 24, 15, 40))
+
+        val count = winnerLottery.matchCount(winnerLottery)
+
+        assertThat(count).isEqualTo(3)
     }
 }
