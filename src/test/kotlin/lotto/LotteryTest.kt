1e415de9401dbdbf8bffd275a1e0892ee0adc1f7
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-15 18:20:07 +0900

    refactor: README에 따라 테스트 코드 재작성

--- src/test/kotlin/lotto/LotteryTest.kt
+++ src/test/kotlin/lotto/LotteryTest.kt
@@ -1,25 +1,23 @@
 package lotto
 
 import domain.Lottery
-import fixture.LotteryFixture.TEST_MOCK_LOTTERY
+import fixture.LotteryFixture.TEST_LOTTERY_NUMBERS
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class LotteryTest {
 
     @Test
-    fun `로또는 중복되지 않는 6개의 랜덤 숫자를 가진다`() {
-        val lottery = Lottery(TEST_MOCK_LOTTERY)
+    fun `로또의 한장 가격은 1000원이다`() {
+        val lottery = Lottery(TEST_LOTTERY_NUMBERS)
 
-        val numbers = lottery.numbers
-
-        assertThat(numbers.numbers.size).isEqualTo(6)
+        assertThat(lottery.price).isEqualTo(1000)
     }
 
     @Test
-    fun `로또의 가격은 1000원이다`() {
-        val lottery = Lottery(TEST_MOCK_LOTTERY)
+    fun `로또는 로또번호들을 가진다`() {
+        val lottery = Lottery(TEST_LOTTERY_NUMBERS)
 
-        assertThat(lottery.price).isEqualTo(1000)
+        assertThat(lottery).isNotNull
     }
 }
