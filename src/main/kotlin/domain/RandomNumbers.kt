ad7f4bf9eb182a4f89bbf6afab51ec1c02c052c3
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-16 18:00:44 +0900

    refactor: shuffle 후 sorted 하도록 수정

--- src/main/kotlin/domain/RandomNumbers.kt
+++ src/main/kotlin/domain/RandomNumbers.kt
@@ -4,7 +4,7 @@ object RandomNumbers {
     private const val MIN_INDEX = 0
 
     fun generate(min: Int, max: Int, size: Int): List<Int> {
-        val numbers = (min..max).map { it }.shuffled().sorted()
-        return numbers.subList(MIN_INDEX, size)
+        val numbers = (min..max).map { it }.shuffled()
+        return numbers.subList(MIN_INDEX, size).sorted()
     }
 }
