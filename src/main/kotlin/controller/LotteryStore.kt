8b7febba558ab2f63b10ba8e0bb3451d96494537
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-16 18:03:32 +0900

    feat: 로또를 출력하는 부분과 우승자에 대한 정보 및 매칭 정보를 실행시키기 위한 로직 추가

--- src/main/kotlin/controller/LotteryStore.kt
+++ src/main/kotlin/controller/LotteryStore.kt
@@ -1,13 +1,33 @@
 package controller
 
-import domain.Lotteries
+import domain.LotteryFactory
+import domain.WinnerLottery
 import view.InputView
+import view.InputView.printInputLastWinnerLottery
+import view.ResultView
+import view.ResultView.printMatchNumbers
 
-class LotteryStore {
-    fun main() {
-        InputView.printInputPrice()
-        val numberOfLottery = Reception.receiveNumberOfLottery()
+fun main() {
+    InputView.printInputPrice()
 
-        val lotteries = Lotteries.of(numberOfLottery)
-    }
+    val inputMoney = Reception.receiveMoney()
+
+    val factory = LotteryFactory(inputMoney)
+
+    println("${factory.calculateLotteryCountByPrice()}개를 구매하였습니다.")
+
+    val createdTicket = factory.buy()
+
+    val lotteries = createdTicket.lotteries
+
+    ResultView.printLotteriesNumbers(lotteries)
+
+    printInputLastWinnerLottery()
+
+    val receiveWinnerLottery = Reception.receiveWinnerLottery()
+    val winnerLottery = WinnerLottery(receiveWinnerLottery)
+
+    val match = winnerLottery.match(lotteries)
+
+    printMatchNumbers(3, 6, match)
 }
