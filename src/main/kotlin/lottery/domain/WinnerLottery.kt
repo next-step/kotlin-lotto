48b4f3657ccb4057eff1c1c6b2ff24ac570dc06e
sanggu <tkdrn8578@naver.com>
2021-03-16 21:38:23 +0900

    refactor: matcher 추가에 따른 WinnerLottery 불필요한 함수 제거

--- src/main/kotlin/lottery/domain/WinnerLottery.kt
+++ src/main/kotlin/lottery/domain/WinnerLottery.kt
@@ -2,31 +2,4 @@ package lottery.domain
 
 class WinnerLottery(numbers: List<Int>) {
     val lotteryNumbers: LotteryNumbers = LotteryNumbers(numbers)
-
-    fun match(lotteries: List<Lottery>): HashMap<Int, Int> {
-        val result = HashMap<Int, Int>()
-
-        lotteries.map {
-            val count = matchCount(it.lotteryNumbers)
-
-            if (Rank.isInTheRank(count)) {
-                addMatchCount(result, count)
-            }
-        }
-
-        return result
-    }
-
-    private fun addMatchCount(result: HashMap<Int, Int>, count: Int) {
-        result[count] = result.getOrDefault(count, DEFAULT_MATCH_COUNT) + ADD_COUNT_VALUE
-    }
-
-    private fun matchCount(lotteryNumbers: LotteryNumbers): Int {
-        return lotteryNumbers.numbers.filter { this.lotteryNumbers.numbers.contains(it) }.count()
-    }
-
-    companion object {
-        const val DEFAULT_MATCH_COUNT = 0
-        const val ADD_COUNT_VALUE = 1
-    }
 }
