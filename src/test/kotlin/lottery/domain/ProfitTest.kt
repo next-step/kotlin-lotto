a240ced29cc25479bae9f84ccaae936bbba802b9
sanggu <tkdrn8578@naver.com>
2021-03-16 22:09:15 +0900

    refactor: 수익율에 대한 검증 값 오류 수정

--- src/test/kotlin/lottery/domain/ProfitTest.kt
+++ src/test/kotlin/lottery/domain/ProfitTest.kt
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test
 class ProfitTest {
     @Test
     fun `당첨금액과 구매금액을 비교하여 수익율을 계산한다`() {
-        val profit = Profit.calculate(14000, 5000)
+        val profit = Profit.calculate(14000.0, 5000.0)
 
-        assertThat(profit).isEqualTo(14000 / 5000)
+        assertThat(profit).isEqualTo("0.36")
     }
 }
