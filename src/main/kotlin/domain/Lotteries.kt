712ca35e898b55c257719909925392262f2a15a0
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-16 18:24:21 +0900

    refactor: 로또를 만드는 generateRandoms 함수 추가

--- src/main/kotlin/domain/Lotteries.kt
+++ src/main/kotlin/domain/Lotteries.kt
@@ -11,9 +11,13 @@ class Lotteries(val lotteries: List<Lottery>) {
         fun of(count: Int): Lotteries {
             require(count > MIN_LOTTERY_COUNT) { "입력한 로또의 개수가 올바르지 않습니다." }
 
-            return Lotteries((START_LOTTERY_COUNT..count).map {
-                val randoms = RandomNumbers.generate(MIN_LOTTERY_NUMBER, MAX_LOTTERY_NUMBER, LOTTERY_SIZE)
-                Lottery(LotteryNumbers(randoms)) })
+            return Lotteries(
+                (START_LOTTERY_COUNT..count).map {
+                    Lottery(LotteryNumbers(generateRandoms()))
+                }
+            )
         }
+
+        private fun generateRandoms() = RandomNumbers.generate(MIN_LOTTERY_NUMBER, MAX_LOTTERY_NUMBER, LOTTERY_SIZE)
     }
 }
