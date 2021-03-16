ea06080073ae6fd33790c57eb31633c59507923b
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-16 18:01:06 +0900

    feat: matchCount로 해당 Rank 정보를 가져오기 위해 valueOf 함수 추가

--- src/main/kotlin/domain/Rank.kt
+++ src/main/kotlin/domain/Rank.kt
@@ -1,6 +1,6 @@
 package domain
 
-enum class Rank(val matchCount: Int, private val price: Int) {
+enum class Rank(val matchCount: Int, val price: Int) {
     FIRST(6, 200_000_000),
     SECOND(5, 1_500_000),
     THIRD(4, 50_000),
@@ -12,7 +12,11 @@ enum class Rank(val matchCount: Int, private val price: Int) {
 
     companion object {
         fun isInTheRank(count: Int): Boolean {
-            return values().filter { count == it.matchCount }.any()
+            return values().filter { it.matchCount == count }.any()
+        }
+
+        fun valueOf(matchCount: Int): Rank {
+            return values().first { it.matchCount == matchCount }
         }
     }
 }
