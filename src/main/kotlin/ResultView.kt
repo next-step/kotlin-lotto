bff598ca94f76c18fb3e1f73eb56765b614cea22
SeoJinHyuk14 <suhjh14@gmail.com>
2021-03-17 01:25:39 +0900

    출력 할 텍스트를 반환하는 함수는 뷰로 이동

--- src/main/kotlin/ResultView.kt
+++ src/main/kotlin/ResultView.kt
@@ -6,7 +6,12 @@ fun printResult(statistic: Map<Winning, Int>, yieldRate: Double) {
     println("당첨 통계")
     println("---------")
     Winning.values().filter { it != Winning.NONE }.forEach {
-        println("${it.getCondition()} - ${statistic.getOrDefault(it, 0)}개")
+        println("${getWinningText(it)} - ${statistic.getOrDefault(it, 0)}개")
     }
     println("총 수익률은 ${yieldRate}입니다.")
+}
+
+fun getWinningText(winning: Winning): String {
+    val bonusText = if (winning == Winning.SECOND) ", 보너스 볼 일치" else ""
+    return "${winning.match}개 일치$bonusText (${winning.price}원)"
 }
