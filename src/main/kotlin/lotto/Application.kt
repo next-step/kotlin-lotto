7dba66ccdc85e69ec2102e019e14a2edfa70afdd
hyunssooo <jhs695@gmail.com>
2021-03-17 02:27:43 +0900

    feat: view, controller 추가 요구 사항 반영

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,11 +2,13 @@ package lotto
 
 import lotto.domain.Lotto
 import lotto.domain.LottoNumber
-import lotto.domain.Lottos
+import lotto.domain.LottosMachine
 import lotto.domain.Money
-import lotto.domain.RandomLottoGenerator
 import lotto.domain.WinningLotto
+import lotto.domain.lottogenerator.ManualLottoGenerator
 import lotto.view.inputBonusNumber
+import lotto.view.inputManualLottoCount
+import lotto.view.inputManualLottoNumbers
 import lotto.view.inputMoney
 import lotto.view.inputWinningLottoNumbers
 import lotto.view.printLottos
@@ -15,16 +17,17 @@ import lotto.view.printStatistics
 
 fun main() {
     val purchase = Money(inputMoney())
-    val count = (purchase / Lotto.PRICE).toInt()
 
-    val lottos = Lottos((1..count).map { RandomLottoGenerator.generate() })
+    val manualLottoCount = inputManualLottoCount()
+    val manualLottoGenerators = (1..manualLottoCount).map { ManualLottoGenerator(inputManualLottoNumbers()) }
+
+    val lottos = LottosMachine(purchase, manualLottoGenerators).create()
     printLottos(lottos)
 
     val winningNumbers = inputWinningLottoNumbers()
         .map { LottoNumber.of(it) }
         .toSet()
     val bonusNumber = inputBonusNumber()
-
     val winningLotto = WinningLotto(Lotto(winningNumbers), LottoNumber.of(bonusNumber))
 
     val result = lottos.match(winningLotto)
--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -2,17 +2,29 @@ package lotto.view
 
 fun inputMoney(): Int {
     println("구입금액을 입력해 주세요.")
-    return readLine()?.toInt() ?: throw IllegalArgumentException()
+    return readLine()?.toIntOrNull() ?: throw IllegalArgumentException()
+}
+
+fun inputManualLottoCount(): Int {
+    println("수동으로 구매할 로또 수를 입력해 주세요.")
+    return readLine()?.toIntOrNull() ?: throw IllegalArgumentException()
+}
+
+fun inputManualLottoNumbers(): List<Int> {
+    println("수동으로 구매할 번호를 입력해 주세요.")
+    return (readLine() ?: throw IllegalArgumentException())
+        .split(",")
+        .map { it.toInt() }
 }
 
 fun inputWinningLottoNumbers(): List<Int> {
     println("지난 주 당첨 번호를 입력해 주세요.")
-    return (readLine() ?: throw NullPointerException())
+    return (readLine() ?: throw IllegalArgumentException())
         .split(",")
         .map { it.toInt() }
 }
 
 fun inputBonusNumber(): Int {
     println("보너스 볼을 입력해 주세요.")
-    return readLine()?.toInt() ?: throw NullPointerException()
+    return readLine()?.toInt() ?: throw IllegalArgumentException()
 }
