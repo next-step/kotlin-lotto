d94acbae20196cb8f2688e5722f3db1ca3c45fa1
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-16 16:40:21 +0900

    feat: 맞춘 개수를 저장해 넘겨주는 match 함수 추가

--- src/main/kotlin/domain/WinnerLottery.kt
+++ src/main/kotlin/domain/WinnerLottery.kt
@@ -3,32 +3,31 @@ package domain
 class WinnerLottery(numbers: List<Int>) {
     val numbers: LotteryNumbers = LotteryNumbers(numbers)
 
-    fun math(lotteries: Lotteries): HashMap<Int, Int> {
+    // 만약 10개 중 3개가 일치한다면
+    fun match(lotteries: List<LotteryNumbers>): HashMap<Int, Int> {
         val result = HashMap<Int, Int>()
 
-        lotteries.lotteries.map {
-            val count = matchCount(it.numbers)
-            if (count == 3) {
-                result[3] = result.getOrDefault(3, 0) + 1
-            }
+        lotteries.map {
+            val count = matchCount(it)
 
-            if (count == 4) {
-                result[4] = result.getOrDefault(4, 0) + 1
-            }
-
-            if (count == 5) {
-                result[5] = result.getOrDefault(5, 0) + 1
-            }
-
-            if (count == 6) {
-                result[6] = result.getOrDefault(6, 0) + 1
+            if (count == Rank.FOURTH.matchCount || count == 4 || count == 5 || count == 6) {
+                addMatchCount(result, count)
             }
         }
 
         return result
     }
 
+    private fun addMatchCount(result: HashMap<Int, Int>, count: Int) {
+        result[count] = result.getOrDefault(count, DEFAULT_MATCH_COUNT) + ADD_COUNT_VALUE
+    }
+
     fun matchCount(lotteryNumbers: LotteryNumbers): Int {
         return lotteryNumbers.numbers.filter { this.numbers.numbers.contains(it) }.count()
+    }
+
+    companion object {
+        const val DEFAULT_MATCH_COUNT = 0
+        const val ADD_COUNT_VALUE = 1
     }
 }
