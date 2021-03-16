2981d6ee91d5ad45d4074be15bbf6d9b2316626e
ohgillwhan <soora_genius@naver.com>
2021-03-16 23:28:45 +0900

    refact(LottoCollection) : 생성자를 의미있도록 변경

--- src/main/kotlin/lotto/domain/LottoCollection.kt
+++ src/main/kotlin/lotto/domain/LottoCollection.kt
@@ -1,13 +1,14 @@
 package lotto.domain
 
-class LottoCollection(private val _lotto: MutableList<Lotto> = mutableListOf(), private val generator: LottoNumberGenerator = LottoNumberRandomGenerator()) {
+class LottoCollection(private val _lotto: MutableList<Lotto> = mutableListOf()) {
     val lotto: List<Lotto>
         get() = _lotto
-    constructor(count: Int, _generator: LottoNumberGenerator = LottoNumberRandomGenerator()) : this(generator = _generator) {
+
+    constructor(count: Int, _generator: LottoNumberGenerator = LottoNumberRandomGenerator()) : this() {
         require(count >= 1)
 
         repeat(count) {
-            _lotto.add(Lotto(generator))
+            _lotto.add(Lotto(_generator))
         }
     }
 }
