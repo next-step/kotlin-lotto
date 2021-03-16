b99bddf45b9dc38534c6d50d38eb7dfef9faae03
hyunssooo <jhs695@gmail.com>
2021-03-17 01:57:04 +0900

    feat: LottoGenerator, ManualLottoGenerator 추가

--- src/main/kotlin/lotto/domain/lottogenerator/ManualLottoGenerator.kt
+++ src/main/kotlin/lotto/domain/lottogenerator/ManualLottoGenerator.kt
@@ -3,15 +3,13 @@ package lotto.domain.lottogenerator
 import lotto.domain.Lotto
 import lotto.domain.LottoNumber
 
-object RandomLottoGenerator : LottoGenerator {
+class ManualLottoGenerator(private val elements: List<Int>) : LottoGenerator {
 
     override fun generate(): Lotto {
         return Lotto(
-            LottoNumber.RANGE.toList()
-                .shuffled()
-                .take(Lotto.SIZE)
+            elements
                 .map { LottoNumber.of(it) }
-                .toSortedSet()
+                .toSet()
         )
     }
 }
---
+++ src/test/kotlin/lotto/domain/lottogenerator/ManualLottoGeneratorTest.kt
@@ -0,0 +1,18 @@
+package lotto.domain.lottogenerator
+
+import lotto.domain.Lotto
+import lotto.domain.LottoNumber
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class ManualLottoGeneratorTest {
+
+    @Test
+    fun `lotto 생성 테스트`() {
+        val elements = listOf(1, 2, 3, 4, 5, 6)
+        val lotto = ManualLottoGenerator(elements).generate()
+        assertThat(lotto).isEqualTo(
+            Lotto(elements.map { LottoNumber.of(it) }.toSet())
+        )
+    }
+}
