5cb96efde61b4d332a1915ecc9370c85de00bb34
thxwelchs <thxwelchs@gmail.com>
2021-03-17 00:26:09 +0900

    refactor(view): InputView 상수명 오타 수정

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,11 +1,11 @@
 package lotto.view
 
 object InputView {
-    private const val BUYING_COMMNET = "구입금액을 입력해 주세요."
+    private const val BUYING_COMMENT = "구입금액을 입력해 주세요."
     private const val LAST_WEEK_WINNING_LOTTO_NUMBERS_COMMENT = "지난 주 당첨 번호를 입력해 주세요."
 
     fun enterLottoBuyingPrice(): Int {
-        println(BUYING_COMMNET)
+        println(BUYING_COMMENT)
         return readLine()?.toInt() ?: throw IllegalArgumentException()
     }
 
