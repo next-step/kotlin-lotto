b1ca8307b53d77a2f57a23e5da7ff150c9b40567
sanggu <tkdrn8578@naver.com>
2021-03-16 21:49:44 +0900

    test: 모든 등수의 당첨금의 합을 계산하는 테스트코드 추가

--- src/test/kotlin/lottery/domain/RankCountsTest.kt
+++ src/test/kotlin/lottery/domain/RankCountsTest.kt
@@ -5,9 +5,27 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 
 class RankCountsTest {
-
     @Test
     fun `맞춘 개수에 맞는 등수에 건수를 증가시킨다`() {
+        val rankCounts = createRankCounts()
+
+        assertAll(
+            { assertThat(rankCounts.rankCounts[Rank.FIRST]).isEqualTo(2) },
+            { assertThat(rankCounts.rankCounts[Rank.SECOND]).isEqualTo(1) },
+            { assertThat(rankCounts.rankCounts[Rank.THIRD]).isEqualTo(1) }
+        )
+    }
+
+    @Test
+    fun `모든 등수의 당첨금들 합을 가져온다`() {
+        val rankCounts = createRankCounts()
+
+        val amount = rankCounts.calulateJackpots()
+
+        assertThat(amount).isEqualTo((first.price * 2) + second.price + third.price)
+    }
+
+    fun createRankCounts(): RankCounts {
         val rankCounts = RankCounts()
         val first = Rank.FIRST
         val second = Rank.SECOND
@@ -18,10 +36,6 @@ class RankCountsTest {
         rankCounts.addMatchCount(second)
         rankCounts.addMatchCount(third)
 
-        assertAll(
-            { assertThat(rankCounts.rankCounts[first]).isEqualTo(2) },
-            { assertThat(rankCounts.rankCounts[second]).isEqualTo(1) },
-            { assertThat(rankCounts.rankCounts[third]).isEqualTo(1) }
-        )
+        return rankCounts
     }
 }
