682de2baae4521d84bc7957b0b068a73bf208a4e
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-17 01:11:29 +0900

    refactor: 중복 제거 및 함수 분리

--- src/main/kotlin/calculator/StringAdditionCalculator.kt
+++ src/main/kotlin/calculator/StringAdditionCalculator.kt
@@ -9,12 +9,21 @@ class StringAdditionCalculator {
         if (expression.contains(CUSTOM_DELIMITER_REGEX)) {
             val matchResult = CUSTOM_DELIMITER_REGEX.find(expression)!!
             val (_, customDelimiter, numberAdditionExpression) = matchResult.groupValues
-            return numberAdditionExpression.split(customDelimiter, COMMA_DELIMITER, COLON_DELIMITER)
-                .sumBy { it.toInt() }
+            return sumNumberAdditionExpression(numberAdditionExpression, customDelimiter)
         }
 
-        return expression.split(COMMA_DELIMITER, COLON_DELIMITER)
+        return sumNumberAdditionExpression(expression)
+    }
+
+    private fun sumNumberAdditionExpression(numberAdditionExpression: String, customDelimiter: String? = null): Int {
+        return splitExpressionByDelimiters(numberAdditionExpression, customDelimiter)
             .sumBy { it.toInt() }
+    }
+
+    private fun splitExpressionByDelimiters(numberAdditionExpression: String, customDelimiter: String?): List<String> {
+        return numberAdditionExpression.split(
+            *listOfNotNull(customDelimiter, COMMA_DELIMITER, COLON_DELIMITER).toTypedArray()
+        )
     }
 
     companion object {
