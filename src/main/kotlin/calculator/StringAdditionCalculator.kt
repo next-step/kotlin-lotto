a22a8ab67d8312786134d02fe221c48bb7843401
Dominiqn <dmut7691@gmail.com>
2021-03-17 10:27:25 +0900

    [Step1] 문자열 덧셈 계산기 (#140)
    * docs: 요구사항을 분석하여 세분화
    
    * chore: 불필요한 파일 제거
    
    * feat(stringcalculator): 빈 문자열이 주어질 경우, 0을 반환
    
    * feat(stringcalculator): null이 주어질 경우, 0을 반환
    
    * refactor: 중복 생성 제거
    
    * docs: 요구사항 재분석하여 세분화
    
    * refactor: 중복 테스트 제거
    
    * feat(stringcalculator): 숫자 하나가 주어질 경우, 해당 숫자를 반환
    
    * refactor: 중복 테스트 코드 제거
    
    * refactor: 코틀린 공식 extension을 사용하여 가독성 증가
    
    * feat(stringcalculator): 두 숫자 사이에 쉼표가 있는 경우, 두 숫자의 합을 반환
    
    * refactor: 중복 테스트 코드 제거
    
    * refactor: 의미 없는 코드 제거
    
    expression.toIntOrNull()이 없어도 동일하게 동작한다.
    
    * refactor: 하나의 컬렉션 함수로 수정
    
    * refactor: 상수에 이름 붙이기
    
    * feat(stringcalculator): 여러 숫자와 그 사이에 쉼표가 있는 경우, 모든 숫자의 합을 반환
    
    * refactor: 중복 테스트 합치기
    
    * feat(stringcalculator): 구분자를 쉼표만이 아니라 콜론으로도 구분 가능
    
    * refactor: 테스트 중복 제거
    
    * refactor: 상수에 이름 붙이기
    
    * feat(stringcalculator): 커스텀 구분자 구문을 지정한 문자열을 전달해도 실패하지 않는다.
    
    * refactor: 중복 테스트 코드 제거
    
    * refactor: 상수에 이름 붙이기
    
    * feat(stringcalculator): 커스텀 구분자로 숫자를 분리한다.
    
    * refactor: 중복 테스트 코드 제거
    
    * refactor: 변수에 명확한 이름을 붙여주기
    
    * refactor: 디스트럭쳐링으로 간결한 변수 할당
    
    * refactor: 중복 제거 및 함수 분리
    
    * feat(stringcalculator): 음수를 전달하는 경우 RuntimeException을 던진다.
    
    * refactor: 중복 테스트 코드 제거
    
    * refactor: 양수 혹은 0로 변환하는 함수를 추출
    
    * test: 숫자 이외의 값을 전달하는 경우 RuntimeException을 던져야 한다
    
    * refactor: 중복 테스트 코드 제거

---
+++ src/main/kotlin/calculator/StringAdditionCalculator.kt
@@ -0,0 +1,41 @@
+package calculator
+
+class StringAdditionCalculator {
+    fun calculate(expression: String?): Int {
+        if (expression.isNullOrBlank()) {
+            return 0
+        }
+
+        if (expression.contains(CUSTOM_DELIMITER_REGEX)) {
+            val matchResult = CUSTOM_DELIMITER_REGEX.find(expression)!!
+            val (_, customDelimiter, numberAdditionExpression) = matchResult.groupValues
+            return sumNumberAdditionExpression(numberAdditionExpression, customDelimiter)
+        }
+
+        return sumNumberAdditionExpression(expression)
+    }
+
+    private fun sumNumberAdditionExpression(numberAdditionExpression: String, customDelimiter: String? = null): Int {
+        return splitExpressionByDelimiters(numberAdditionExpression, customDelimiter)
+            .sumBy { it.toPositiveInt() }
+    }
+
+    private fun splitExpressionByDelimiters(numberAdditionExpression: String, customDelimiter: String?): List<String> {
+        return numberAdditionExpression.split(
+            *listOfNotNull(customDelimiter, COMMA_DELIMITER, COLON_DELIMITER).toTypedArray()
+        )
+    }
+
+    private fun String.toPositiveInt(): Int {
+        val integer = this.toInt()
+        require(integer >= 0)
+
+        return integer
+    }
+
+    companion object {
+        private const val COMMA_DELIMITER = ","
+        private const val COLON_DELIMITER = ":"
+        private val CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)".toRegex()
+    }
+}
---
+++ src/test/kotlin/calculator/StringAdditionCalculatorTest.kt
@@ -0,0 +1,102 @@
+package calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
+
+internal class StringAdditionCalculatorTest {
+    private val calculator = StringAdditionCalculator()
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    fun `빈 문자열이나 null이 주어질 경우, 0을 반환`(expression: String?) {
+        assertThat(calculator.calculate(expression)).isEqualTo(0)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 100])
+    fun `숫자 하나가 주어질 경우, 해당 숫자를 반환`(integer: Int) {
+        assertThat(calculator.calculate("$integer")).isEqualTo(integer)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1,2=3",
+        "3,9=12",
+        "13,987=1000",
+        "1,2,3=6",
+        "1,2,3,4,5,6,7,8,9,10=55",
+        "876,54,3,21,0=954",
+        delimiterString = "="
+    )
+    fun `여러 숫자와 그 사이에 쉼표가 있는 경우, 모든 숫자의 합을 반환`(expression: String, sum: Int) {
+        assertThat(calculator.calculate(expression)).isEqualTo(sum)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1:2=3",
+        "13:987=1000",
+        "1:2:3=6",
+        "876,54:3:21,0=954",
+        delimiterString = "="
+    )
+    fun `구분자를 쉼표만이 아니라 콜론으로도 구분 가능`(expression: String, sum: Int) {
+        assertThat(calculator.calculate(expression)).isEqualTo(sum)
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "//;\n1;2;3",
+            "//;\n1;2,3",
+            "//&\n1&2&3"
+        ]
+    )
+    fun `커스텀 구분자 구문을 지정한 문자열을 전달해도 실패하지 않는다`(expression: String) {
+        assertDoesNotThrow {
+            calculator.calculate(expression)
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "'//;\n1;2;3'=6",
+        "'//*\n1*2*3'=6",
+        "'//*\n1'=1",
+        "'//&\n1&2,3:4'=10",
+        delimiterString = "="
+    )
+    fun `커스텀 구분자로 숫자를 분리한다`(expression: String, sum: Int) {
+        assertThat(calculator.calculate(expression)).isEqualTo(sum)
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "-1",
+            "2,-5",
+            "//;\n1;2;-3"
+        ]
+    )
+    fun `음수를 전달하는 경우 RuntimeException을 던진다`(expression: String) {
+        assertThatExceptionOfType(RuntimeException::class.java)
+            .isThrownBy { calculator.calculate(expression) }
+    }
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "n",
+            "1,일",
+            "//;\n1;two;삼"
+        ]
+    )
+    fun `숫자 이외의 값을 전달하는 경우 RuntimeException을 던진다`(expression: String) {
+        assertThatExceptionOfType(RuntimeException::class.java)
+            .isThrownBy { calculator.calculate(expression) }
+    }
+}
