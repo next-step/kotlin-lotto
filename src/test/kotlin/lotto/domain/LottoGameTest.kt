79d88b9d89838c9a066a22d41c3e5584f30cc709
Joseph <wjsdudtkd95@gmail.com>
2021-03-17 20:58:00 +0900

    refactor(lotto): Game변경에 따른 test 수정

--- src/test/kotlin/lotto/domain/LottoGameTest.kt
+++ src/test/kotlin/lotto/domain/LottoGameTest.kt
@@ -9,6 +9,7 @@ class LottoGameTest {
 
     private lateinit var lottoGame: LottoGame
     private lateinit var manualNumbers: List<List<String>>
+    private lateinit var overManualNumbers: List<List<String>>
 
     @BeforeEach
     fun init() {
@@ -19,12 +20,19 @@ class LottoGameTest {
             listOf("3", "5", "6", "7", "8", "9"),
             listOf("5", "21", "31", "44", "25", "10")
         )
+        overManualNumbers = listOf(
+            listOf("3", "5", "6", "7", "8", "13"),
+            listOf("3", "5", "6", "7", "8", "11"),
+            listOf("3", "5", "6", "7", "8", "9"),
+            listOf("5", "21", "31", "44", "25", "10"),
+            listOf("5", "21", "31", "44", "25", "10")
+        )
     }
 
     @Test
     fun `수동으로 사고 난 뒤 남은돈을 전부 자동으로 구매하는지 확인`() {
         val manualStrategy = ManualStrategy(manualNumbers.slice(1..2))
-        lottoGame.purchaseLottoes(manualStrategy, 2)
+        lottoGame.purchaseLottoes(manualStrategy)
         val autoLottoes = lottoGame.purchaseLottoes(AutoStrategy())
 
         assertThat(autoLottoes.toList().size).isEqualTo(2)
@@ -33,7 +41,7 @@ class LottoGameTest {
     @Test
     fun `가진 금액에 비해서 사려는 수동 로또가 많을 때 Exception 발생 확인`() {
         assertThrows<IllegalStateException> {
-            lottoGame.purchaseLottoes(ManualStrategy(manualNumbers), 5)
+            lottoGame.purchaseLottoes(ManualStrategy(overManualNumbers))
         }
     }
 }
