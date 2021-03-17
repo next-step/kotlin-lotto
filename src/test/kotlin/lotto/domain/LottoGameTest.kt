6ea03ffe3c953f6dc0d26e913441784047075769
Joseph <wjsdudtkd95@gmail.com>
2021-03-17 18:14:16 +0900

    refactor(lotto): 테스트 케이스 추가

--- src/test/kotlin/lotto/domain/LottoGameTest.kt
+++ src/test/kotlin/lotto/domain/LottoGameTest.kt
@@ -1,5 +1,6 @@
 package lotto.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -21,7 +22,17 @@ class LottoGameTest {
     }
 
     @Test
+    fun `수동으로 사고 난 뒤 남은돈을 전부 자동으로 구매하는지 확인`() {
+        val manualLottoes = lottoGame.purchaseManualLottoes(2, manualNumbers.slice(1..2))
+        val autoLottoes = lottoGame.purchaseAutoLottoes()
+
+        assertThat(autoLottoes.toList().size).isEqualTo(2)
+    }
+
+    @Test
     fun `가진 금액에 비해서 사려는 수동 로또가 많을 때 Exception 발생 확인`() {
-        assertThrows<IllegalStateException> { lottoGame.purchaseManualLottoes(5, manualNumbers) }
+        assertThrows<IllegalStateException> {
+            lottoGame.purchaseManualLottoes(5, manualNumbers)
+        }
     }
 }
