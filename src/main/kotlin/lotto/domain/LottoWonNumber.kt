602f8b7e2136d31b44189f361c1814e347b724e6
ohgillwhan <soora_genius@naver.com>
2021-03-17 22:27:02 +0900

    feat(LottoWonNumber) : 보너스 번호와 기존 번호는 겹쳐서는 안된다.

--- src/main/kotlin/lotto/domain/LottoWonNumber.kt
+++ src/main/kotlin/lotto/domain/LottoWonNumber.kt
@@ -3,5 +3,6 @@ package lotto.domain
 class LottoWonNumber(val lottoNumber: Set<LottoNumber>, val bonusNUmber: LottoNumber) {
     init {
         require(lottoNumber.size >= 6)
+        require(!lottoNumber.contains(bonusNUmber))
     }
 }
