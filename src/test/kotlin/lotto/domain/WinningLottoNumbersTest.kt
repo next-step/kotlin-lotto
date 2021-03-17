80d0eac2f37acb08b84c1b54661494a93f44ccf8
thxwelchs <thxwelchs@gmail.com>
2021-03-17 22:27:10 +0900

    test: 로또 1급컬렉션 테스트,  당첨번호 보너스볼 까지 테스트

--- src/test/kotlin/lotto/domain/WinningLottoNumbersTest.kt
+++ src/test/kotlin/lotto/domain/WinningLottoNumbersTest.kt
@@ -4,33 +4,44 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 internal class WinningLottoNumbersTest {
     @Test
     fun `당첨 번호 갯수와 번호 검증`() {
-        val winningLottoNumbers = WinningLottoNumbers.from("1, 2, 3, 4, 5, 6")
+        val winningLottoNumbers = WinningLottoNumbers.of("1, 2, 3, 4, 5, 6", 7)
 
-        assertThat(winningLottoNumbers.lottoNumbers.size).isEqualTo(6)
-        assertThat(winningLottoNumbers.lottoNumbers[0]).isEqualTo(LottoNumber.from(1))
-        assertThat(winningLottoNumbers.lottoNumbers[1]).isEqualTo(LottoNumber.from(2))
-        assertThat(winningLottoNumbers.lottoNumbers[2]).isEqualTo(LottoNumber.from(3))
-        assertThat(winningLottoNumbers.lottoNumbers[3]).isEqualTo(LottoNumber.from(4))
-        assertThat(winningLottoNumbers.lottoNumbers[4]).isEqualTo(LottoNumber.from(5))
-        assertThat(winningLottoNumbers.lottoNumbers[5]).isEqualTo(LottoNumber.from(6))
+        assertThat(winningLottoNumbers.lotto.numbers.size).isEqualTo(6)
+        assertThat(winningLottoNumbers.lotto.numbers[0]).isEqualTo(LottoNumber.from(1))
+        assertThat(winningLottoNumbers.lotto.numbers[1]).isEqualTo(LottoNumber.from(2))
+        assertThat(winningLottoNumbers.lotto.numbers[2]).isEqualTo(LottoNumber.from(3))
+        assertThat(winningLottoNumbers.lotto.numbers[3]).isEqualTo(LottoNumber.from(4))
+        assertThat(winningLottoNumbers.lotto.numbers[4]).isEqualTo(LottoNumber.from(5))
+        assertThat(winningLottoNumbers.lotto.numbers[5]).isEqualTo(LottoNumber.from(6))
     }
 
     @ParameterizedTest
-    @ValueSource(
-        strings = [
-            "1, 1, 2, 3, 4, 5",
-            "1, 1, 1, 1, 1, 1",
-            "45, 44, 43, 20, 20, 5"
-        ]
+    @CsvSource(
+        "'1, 1, 2, 3, 4, 5', 6",
+        "'1, 1, 1, 1, 1, 1', 2",
+        "'45, 44, 43, 20, 20, 5', 4"
     )
-    fun `중복 번호 검증`(stringWinningLottoNumbers: String) {
+    fun `중복 번호 검증`(stringWinningLottoNumbers: String, bonusNumber: Int) {
         assertThrows<IllegalArgumentException> {
-            WinningLottoNumbers.from(stringWinningLottoNumbers)
+            WinningLottoNumbers.of(stringWinningLottoNumbers, bonusNumber)
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "'1, 2, 3, 4, 5, 6', 6",
+        "'45, 44, 43, 42, 41, 1', 1",
+        "'30, 25, 17, 1, 2, 3', 17"
+    )
+    fun `보너스볼과 당첨번호 중복 번호 검증`(stringWinningLottoNumbers: String, bonusNumber: Int) {
+        assertThrows<IllegalArgumentException> {
+            WinningLottoNumbers.of(stringWinningLottoNumbers, bonusNumber)
         }
     }
 }
