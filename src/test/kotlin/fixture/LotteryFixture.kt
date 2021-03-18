ca60217240c14e165568f3c7038b8e744acd8aa0
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-18 18:14:10 +0900

    feat: MockGenerator에 대한 Fixture 추ㅏㄱ

--- src/test/kotlin/fixture/LotteryFixture.kt
+++ src/test/kotlin/fixture/LotteryFixture.kt
@@ -1,10 +1,12 @@
 package fixture
 
 import lottery.domain.LotteryNumbers
+import lottery.domain.MockGenerator
 
 object LotteryFixture {
     val TEST_NUMBERS = listOf(1, 3, 5, 6, 2, 8)
     val TEST_LOTTERY_NUMBERS = LotteryNumbers(TEST_NUMBERS)
+    val TEST_GENERATOR = MockGenerator(TEST_NUMBERS)
 
     const val MIN_LOTTERY_NUMBER = 1
     const val MAX_LOTTERY_NUMBER = 45
