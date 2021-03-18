b0a7f4ac1ca306fddd9c850c2665f6d17826a540
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-18 23:50:30 +0900

    refactor: 상수에 이름 붙이기

--- src/main/kotlin/domain/LottoNumber.kt
+++ src/main/kotlin/domain/LottoNumber.kt
@@ -2,6 +2,11 @@ package domain
 
 class LottoNumber(val value: Int) {
     init {
-        require(value in 1..45)
+        require(value in MIN..MAX)
+    }
+
+    companion object {
+        private const val MIN = 1
+        private const val MAX = 45
     }
 }
