0b65f7fbdeadefd4005de403f11e8286dce19dd4
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-18 23:27:46 +0900

    feat(lotto-auto): 돈은 0원 이하가 될 수 없다

--- src/main/kotlin/domain/Money.kt
+++ src/main/kotlin/domain/Money.kt
@@ -1,3 +1,7 @@
 package domain
 
-class Money(val value: Int)
+class Money(val value: Int) {
+    init {
+        if (value <= 0) throw IllegalArgumentException()
+    }
+}
--- src/test/kotlin/domain/MoneyTest.kt
+++ src/test/kotlin/domain/MoneyTest.kt
@@ -1,5 +1,6 @@
 package domain
 
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 
@@ -7,5 +8,12 @@ internal class MoneyTest {
     @Test
     fun `돈은 정수 하나로 생성된다`() {
         assertDoesNotThrow { Money(1) }
+    }
+
+    @Test
+    fun `돈은 0원 이하가 될 수 없다`() {
+        assertThatIllegalArgumentException().isThrownBy { Money(0) }
+        assertThatIllegalArgumentException().isThrownBy { Money(-1) }
+        assertThatIllegalArgumentException().isThrownBy { Money(Int.MIN_VALUE) }
     }
 }
