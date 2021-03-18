e985d70406904934fc5dba8411a42dd2006658c9
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-19 00:14:44 +0900

    refactor: 상수 이름 붙이기

--- src/main/kotlin/domain/LottoNumbers.kt
+++ src/main/kotlin/domain/LottoNumbers.kt
@@ -2,10 +2,14 @@ package domain
 
 class LottoNumbers(val numbers: List<LottoNumber>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == SIZE)
     }
 
     constructor(n1: Int, n2: Int, n3: Int, n4: Int, n5: Int, n6: Int) : this(
         listOf(n1, n2, n3, n4, n5, n6).map { LottoNumber(it) }
     )
+
+    companion object {
+        private const val SIZE = 6
+    }
 }
