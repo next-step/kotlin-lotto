25097792f9f833601a4f420bdb1b47f2df2d467b
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-18 18:12:35 +0900

    refactor: NumbersGenerator 추가에 따른 naming 수정

--- src/main/kotlin/lottery/domain/RandomNumbersGenerator.kt
+++ src/main/kotlin/lottery/domain/RandomNumbersGenerator.kt
@@ -1,9 +1,9 @@
 package lottery.domain
 
-object RandomNumbers {
+object RandomNumbersGenerator : NumbersGenerator {
     private const val MIN_INDEX = 0
 
-    fun generate(min: Int, max: Int, size: Int): List<Int> {
+    override fun generate(min: Int, max: Int, size: Int): List<Int> {
         val numbers = (min..max).map { it }.shuffled()
         return numbers.subList(MIN_INDEX, size).sorted()
     }
