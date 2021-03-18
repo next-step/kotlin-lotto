a17dd192576773b8c83fca2d20348590051b8888
thxwelchs <thxwelchs@gmail.com>
2021-03-18 23:00:46 +0900

    refactor(main): main 함수 파일 위치 변경 및, 메인 함수 입력받는 처리 수정
    수정 사항:
    - 문자열로 이루어진 로또 당첨번호를 리스트로 토큰 처리하는 과정 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,7 @@
 package lotto.view
 
 import lotto.domain.LottoMachine
+import lotto.domain.LottoNumberTokenizer
 import lotto.domain.WinningLottoNumbers
 import lotto.domain.WinningLottoStatistics
 
@@ -8,10 +9,11 @@ fun main() {
     val buyingPrice = InputView.enterLottoBuyingPrice()
     val lottoTickets = LottoMachine.buy(buyingPrice)
     ResultView.printLottoTickets(lottoTickets)
-    val enteredWinningLottoNumbers = InputView.enterLastWeekWinningLottoNumbers()
+    val stringLottoNumbers = InputView.enterLastWeekWinningLottoNumbers()
+    val winningLottoNumberTokens = LottoNumberTokenizer.tokenize(stringLottoNumbers)
     val bonusNumber = InputView.enterBonusNumber()
 
-    val winningLottoNumbers = WinningLottoNumbers.of(enteredWinningLottoNumbers, bonusNumber)
+    val winningLottoNumbers = WinningLottoNumbers.of(winningLottoNumberTokens, bonusNumber)
     val winningLottoStatistics = WinningLottoStatistics(lottoTickets, winningLottoNumbers)
     val lottoProfitRate = winningLottoStatistics.calculateProfitRate(buyingPrice)
     ResultView.printWinningLottoStatistics(winningLottoStatistics)
