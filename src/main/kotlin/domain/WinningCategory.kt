5a10e058bda116c45700ce35f9168283fda7c27b
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-19 02:23:39 +0900

    refactor: 가독성 향상

--- src/main/kotlin/domain/WinningCategory.kt
+++ src/main/kotlin/domain/WinningCategory.kt
@@ -1,8 +1,8 @@
 package domain
 
 enum class WinningCategory(val numberOfMatched: Int, val prize: Money) {
-    THREE_CORRECT(3, Money(5000)),
-    FOUR_CORRECT(4, Money(50000)),
-    FIVE_CORRECT(5, Money(1500000)),
-    SIX_CORRECT(6, Money(2000000000));
+    THREE_CORRECT(3, Money(5_000)),
+    FOUR_CORRECT(4, Money(50_000)),
+    FIVE_CORRECT(5, Money(1_500_000)),
+    SIX_CORRECT(6, Money(2_000_000_000));
 }
