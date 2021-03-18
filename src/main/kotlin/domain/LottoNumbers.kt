622f03946dca6646a4405d77d9b4f986a1bc7504
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-19 00:13:31 +0900

    style: 한 줄 내의 최대 글자 넘어가지 않도록 조정

--- src/main/kotlin/domain/LottoNumbers.kt
+++ src/main/kotlin/domain/LottoNumbers.kt
@@ -5,5 +5,7 @@ class LottoNumbers(val numbers: List<LottoNumber>) {
         require(numbers.size == 6)
     }
 
-    constructor(n1: Int, n2: Int, n3: Int, n4: Int, n5: Int, n6: Int) : this(listOf(n1, n2, n3, n4, n5, n6).map { LottoNumber(it) })
+    constructor(n1: Int, n2: Int, n3: Int, n4: Int, n5: Int, n6: Int) : this(
+        listOf(n1, n2, n3, n4, n5, n6).map { LottoNumber(it) }
+    )
 }
