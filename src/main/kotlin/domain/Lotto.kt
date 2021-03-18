0e2fcdcab9317a55ac08d6c50107fa0b0c0e68ba
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-19 01:25:05 +0900

    feat(lotto-auto): 다른 로또숫자열을 받아, 자신의 로또숫자열과 일치하는 수가 몇 개인지 반환한다

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -1,6 +1,10 @@
 package domain
 
 class Lotto(val numbers: LottoNumbers) {
+    fun countMatchedBy(winningNumbers: LottoNumbers): Int {
+        return numbers.countIntersection(winningNumbers)
+    }
+
     constructor(numbers: List<Int>) : this(LottoNumbers(numbers.map { LottoNumber(it) }))
 
     constructor(n1: Int, n2: Int, n3: Int, n4: Int, n5: Int, n6: Int) : this(listOf(n1, n2, n3, n4, n5, n6))
--- src/test/kotlin/domain/LottoTest.kt
+++ src/test/kotlin/domain/LottoTest.kt
@@ -1,5 +1,6 @@
 package domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 
@@ -15,5 +16,17 @@ internal class LottoTest {
             Lotto(listOf(1, 2, 3, 4, 5, 6))
             Lotto(1, 2, 3, 4, 5, 6)
         }
+    }
+
+    @Test
+    fun `다른 로또숫자열을 받아, 자신의 로또숫자열과 일치하는 수가 몇 개인지 반환한다`() {
+        val winningNumbers = LottoNumbers(1, 2, 3, 4, 5, 6)
+        assertThat(Lotto(1, 2, 3, 4, 5, 6).countMatchedBy(winningNumbers)).isEqualTo(6)
+        assertThat(Lotto(2, 3, 4, 5, 6, 7).countMatchedBy(winningNumbers)).isEqualTo(5)
+        assertThat(Lotto(3, 4, 5, 6, 7, 8).countMatchedBy(winningNumbers)).isEqualTo(4)
+        assertThat(Lotto(4, 5, 6, 7, 8, 9).countMatchedBy(winningNumbers)).isEqualTo(3)
+        assertThat(Lotto(5, 6, 7, 8, 9, 10).countMatchedBy(winningNumbers)).isEqualTo(2)
+        assertThat(Lotto(6, 7, 8, 9, 10, 11).countMatchedBy(winningNumbers)).isEqualTo(1)
+        assertThat(Lotto(7, 8, 9, 10, 11, 12).countMatchedBy(winningNumbers)).isEqualTo(0)
     }
 }
