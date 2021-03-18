13442c132595b9c2420e8d8e649dae31c7d5fb69
sanggu <tkdrn8578@naver.com>
2021-03-19 00:10:09 +0900

    test: 당첨번호와 5개가 일치하고 보너스볼이 있으면 당첨된 것이다 에 대한 테스트 코드 추가

--- src/test/kotlin/lottery/domain/RankTest.kt
+++ src/test/kotlin/lottery/domain/RankTest.kt
@@ -1,6 +1,7 @@
 package lottery.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -15,5 +16,10 @@ class RankTest {
     @CsvSource("3, true", "0, false", "10, false", "5, true")
     fun `맞춘 개수가 등수에 포함되는지 확인한다`(count: Int, isIn: Boolean) {
         assertThat(Rank.isInTheRank(count)).isEqualTo(isIn)
+    }
+
+    @Test
+    fun `당첨번호와 5개가 일치하고 보너스볼이 있으면 당첨된 것이다`() {
+        assertThat(Rank.isInTheRank(5, true)).isTrue()
     }
 }
