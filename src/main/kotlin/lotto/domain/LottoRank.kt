c0294a9a9a7ef0657bcdd831aad8c7831fc48fee
thxwelchs <thxwelchs@gmail.com>
2021-03-18 22:43:52 +0900

    refactor(domain): LottoRank 피드백 반영
    피드백:
    - https://github.com/next-step/kotlin-lotto/pull/142/files#r596462261
    - https://github.com/next-step/kotlin-lotto/pull/142/files#r596466112
    - https://github.com/next-step/kotlin-lotto/pull/142/files#r596466534
    
    반영 내용:
    - 특정 View에 의존적일 수 있는 toString 함수 제거
    - selectByMatchCount 테스트 작성
    - 매직넘버 상수화

--- src/main/kotlin/lotto/domain/LottoRank.kt
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -2,18 +2,16 @@ package lotto.domain
 
 enum class LottoRank(val matchCount: Int, val winningMoney: Int, val matchBonus: Boolean) {
     FIRST_PLACE(matchCount = 6, winningMoney = 2_000_000_000, matchBonus = false),
-    SECOND_PLACE(matchCount = 5, winningMoney = 30_000_000, matchBonus = true),
-    THIRD_PLACE(matchCount = 5, winningMoney = 1_500_000, matchBonus = false),
+    SECOND_PLACE(matchCount = LottoRank.SECOND_OR_THIRD_PLACE_MATCH_COUNT, winningMoney = 30_000_000, matchBonus = true),
+    THIRD_PLACE(matchCount = LottoRank.SECOND_OR_THIRD_PLACE_MATCH_COUNT, winningMoney = 1_500_000, matchBonus = false),
     FORTH_PLACE(matchCount = 4, winningMoney = 50_000, matchBonus = false),
     FIFTH_PLACE(matchCount = 3, winningMoney = 5_000, matchBonus = false);
 
-    override fun toString(): String {
-        return "${matchCount}개 일치${if (matchBonus) ", 보너스 볼 일치" else " "}($winningMoney)"
-    }
-
     companion object {
+        private const val SECOND_OR_THIRD_PLACE_MATCH_COUNT = 5
+
         fun selectByMatchCount(matchCount: Int, matchBonus: Boolean): LottoRank? {
-            if (matchCount == 5) {
+            if (matchCount == SECOND_OR_THIRD_PLACE_MATCH_COUNT) {
                 return values().find { it.matchCount == matchCount && it.matchBonus == matchBonus }
             }
             return values().find { it.matchCount == matchCount }
