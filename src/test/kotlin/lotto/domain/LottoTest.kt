c1884562430d06886b0d48e8ae7ee6d66b89f50c
thxwelchs <thxwelchs@gmail.com>
2021-03-18 22:38:25 +0900

    test(domain): Lotto 누락된 테스트 추가
    추가된 테스트 내용:
    - contains
    - count

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 internal class LottoTest {
@@ -24,5 +25,37 @@ internal class LottoTest {
         assertThrows<IllegalArgumentException> {
             Lotto.from((1..numberSize).map { LottoNumber.from(it) })
         }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "'1,2,3,4,5,6', 5, true",
+        "'1,2,3,4,5,6', 7, false",
+        "'11,30,41,44,7,8', 11, true",
+        "'11,30,41,44,7,8', 8, true",
+        "'11,30,41,44,7,8', 9, false"
+    )
+    fun `로또번호가 로또에 존재 하면 참 그렇지 않으면 거짓을 반환한다`(stringNumbers: String, containTargetNumber: Int, expected: Boolean) {
+        val lotto = Lotto.from(stringNumbers.split(",").map { LottoNumber.from(it.toInt()) })
+
+        val actual = lotto.contains(LottoNumber.from(containTargetNumber))
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "'1,2,3,4,5,6', '2,3,4,5,6,7', 5",
+        "'1,2,3,4,5,6', '3,4,5,7,8,9', 3",
+        "'1,2,3,4,5,6', '2,5,44,30,31,32', 2",
+        "'1,2,3,4,5,6', '40,41,42,43,44,45', 0"
+    )
+    fun `두 로또에 대해서 매치되는 번호 개수를 반환한다`(stringNumbers1: String, stringNumbers2: String, expectCount: Int) {
+        val lotto1 = Lotto.from(stringNumbers1.split(",").map { LottoNumber.from(it.toInt()) })
+        val lotto2 = Lotto.from(stringNumbers2.split(",").map { LottoNumber.from(it.toInt()) })
+
+        val count = lotto1.count(lotto2)
+
+        assertThat(count).isEqualTo(expectCount)
     }
 }
