7944e2d8c0c2136e4da774c281eb088525368e59
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-19 01:14:56 +0900

    feat(lotto-auto): 로또는 숫자 6개로도 생성된다

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -1,3 +1,7 @@
 package domain
 
-class Lotto(val numbers: LottoNumbers)
+class Lotto(val numbers: LottoNumbers) {
+    constructor(numbers: List<Int>) : this(LottoNumbers(numbers.map { LottoNumber(it) }))
+
+    constructor(n1: Int, n2: Int, n3: Int, n4: Int, n5: Int, n6: Int) : this(listOf(n1, n2, n3, n4, n5, n6))
+}
--- src/test/kotlin/domain/LottoTest.kt
+++ src/test/kotlin/domain/LottoTest.kt
@@ -8,4 +8,12 @@ internal class LottoTest {
     fun `로또는 로또숫자열 하나로 생성된다`() {
         assertDoesNotThrow { Lotto(LottoNumbers(1, 2, 3, 4, 5, 6)) }
     }
+
+    @Test
+    fun `로또는 숫자 6개로도 생성된다`() {
+        assertDoesNotThrow {
+            Lotto(listOf(1, 2, 3, 4, 5, 6))
+            Lotto(1, 2, 3, 4, 5, 6)
+        }
+    }
 }
