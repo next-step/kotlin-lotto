e804ca59ca0e6f863e120b5ca577f13eccdc330f
ohgillwhan <soora_genius@naver.com>
2021-03-19 00:42:49 +0900

    refact(InputView) : null 처리 및 코드분리

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -6,14 +6,20 @@ import lotto.domain.LottoWonNumbers
 class InputView {
     fun input(): Input {
         println("구입금액을 입력해 주세요.")
-        val amount = readLine()!!.toInt()
-        val input: Input = Input(amount)
+        val amount = inputAmount()
+        val input = Input(amount)
 
         println("${input.lottoCount}개를 구매했습니다.")
 
         return input
     }
 
+    private fun inputAmount(): Int {
+        val amount = readLine()
+        require(!amount.isNullOrBlank()) { "구입금액은 공백이거나 null일수 없습니다" }
+
+        return amount.toInt()
+    }
     fun inputWonNumber(): LottoWonNumbers {
         println("지난 주 당첨 번호를 입력해 주세요.")
 
