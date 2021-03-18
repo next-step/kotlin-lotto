29c9d59fd2e7f8c92a5b691e41bf395100d88aec
sanggu <tkdrn8578@naver.com>
2021-03-19 00:36:14 +0900

    refactor: bonusBall 추가에따른 match 변경

--- src/main/kotlin/lottery/domain/LotteryMatcher.kt
+++ src/main/kotlin/lottery/domain/LotteryMatcher.kt
@@ -1,14 +1,19 @@
 package lottery.domain
 
-class LotteryMatcher(private val winnerLottery: WinnerLottery, private val createdLotteries: List<Lottery>) {
-    fun match(): RankCounts {
+class LotteryMatcher(
+    private val winnerLottery: WinnerLottery,
+    private val createdLotteries: List<Lottery>
+) {
+    fun match(bonusBall: Int): RankCounts {
         val rankCounts = RankCounts()
 
         createdLotteries.map {
             val count = winnerLottery.matchCount(it.lotteryNumbers)
+            val hasBonus = it.lotteryNumbers.contains(bonusBall)
 
-            if (Rank.isInTheRank(count)) {
-                rankCounts.addMatchCount(Rank.valueOf(count))
+
+            if (Rank.isInTheRank(count, hasBonus)) {
+                rankCounts.addMatchCount(Rank.valueOf(count, hasBonus))
             }
         }
 
