0108fcb935c2e521b8b48c312f262c177b24e233
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-19 00:32:06 +0900

    refactor: 더 알맞은 자료구조를 사용

--- src/main/kotlin/domain/LottoNumbers.kt
+++ src/main/kotlin/domain/LottoNumbers.kt
@@ -1,10 +1,11 @@
 package domain
 
-class LottoNumbers(val numbers: List<LottoNumber>) {
+class LottoNumbers(val numbers: Set<LottoNumber>) {
     init {
-        require(numbers.toSet().size == SIZE)
         require(numbers.size == SIZE)
     }
+
+    constructor(numbers: List<LottoNumber>) : this(numbers.toSet())
 
     constructor(n1: Int, n2: Int, n3: Int, n4: Int, n5: Int, n6: Int) : this(
         listOf(n1, n2, n3, n4, n5, n6).map { LottoNumber(it) }
