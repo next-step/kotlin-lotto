6c214a2c39cebb419acf729729838c8c6f31bd8c
hyunssooo <jhs695@gmail.com>
2021-03-18 22:54:33 +0900

    refactor: 피드백 반영

--- src/main/kotlin/lotto/domain/Rank.kt
+++ src/main/kotlin/lotto/domain/Rank.kt
@@ -10,14 +10,13 @@ enum class Rank(val matchCount: Int, val prizeMoney: Money) {
     MISS(0, Money.ZERO);
 
     companion object {
-        private const val SECOND_OR_THIRD_MATCH_COUNT = 5
 
         fun of(matchCount: Int, bonusMatch: Boolean): Rank {
-            if (matchCount == SECOND_OR_THIRD_MATCH_COUNT && bonusMatch) {
+            if (SECOND.matchCount == matchCount && bonusMatch) {
                 return SECOND
             }
 
-            return if (matchCount == SECOND_OR_THIRD_MATCH_COUNT) {
+            return if (THIRD.matchCount == matchCount) {
                 THIRD
             } else {
                 values().find { it.matchCount == matchCount } ?: MISS
