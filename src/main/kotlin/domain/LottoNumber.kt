7fb37cf1d1062e992ea436d8e5ca4435a8b2e290
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-18 23:44:01 +0900

    feat(lotto-auto): 로또숫자의 범위는 1부터 45까지의 정수이다

--- src/main/kotlin/domain/LottoNumber.kt
+++ src/main/kotlin/domain/LottoNumber.kt
@@ -1,3 +1,7 @@
 package domain
 
-class LottoNumber(val value: Int)
+class LottoNumber(val value: Int) {
+    init {
+        if (value < 1 || value > 45) throw IllegalArgumentException()
+    }
+}
--- src/test/kotlin/domain/LottoNumberTest.kt
+++ src/test/kotlin/domain/LottoNumberTest.kt
@@ -1,5 +1,6 @@
 package domain
 
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 
@@ -7,5 +8,14 @@ internal class LottoNumberTest {
     @Test
     fun `로또숫자는 정수를 입력받아 생성한다`() {
         assertDoesNotThrow { LottoNumber(1) }
+    }
+
+    @Test
+    fun `로또숫자의 범위는 1부터 45까지의 정수이다`() {
+        assertThatIllegalArgumentException().isThrownBy { LottoNumber(0) }
+        assertThatIllegalArgumentException().isThrownBy { LottoNumber(-1) }
+        assertThatIllegalArgumentException().isThrownBy { LottoNumber(46) }
+        assertThatIllegalArgumentException().isThrownBy { LottoNumber(47) }
+        assertDoesNotThrow { (1..45).forEach { LottoNumber(it) } }
     }
 }
