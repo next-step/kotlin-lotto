1c97e320a163b13bea7d1435f5d495faea6f579a
sanggu <tkdrn8578@naver.com>
2021-03-19 00:23:51 +0900

    feat: 점진적 테스트를 위한 valueOf의 hasBonus 인자 추가한 함수 추가

--- src/main/kotlin/lottery/domain/Rank.kt
+++ src/main/kotlin/lottery/domain/Rank.kt
@@ -1,6 +1,6 @@
 package lottery.domain
 
-enum class Rank(val matchCount: Int, val price: Int, val isBonus: Boolean) {
+enum class Rank(val matchCount: Int, val price: Int, val hasBonus: Boolean) {
     FIRST(6, 200_000_000, false),
     SECOND(5, 200_000_000, true),
     THIRD(5, 1_500_000, false),
@@ -8,12 +8,16 @@ enum class Rank(val matchCount: Int, val price: Int, val isBonus: Boolean) {
     FIFTH(3, 5_000, false);
 
     companion object {
-        fun isInTheRank(count: Int, isBonus: Boolean): Boolean {
-            return values().filter { it.matchCount == count && it.isBonus == isBonus }.any()
+        fun isInTheRank(count: Int, hasBonus: Boolean): Boolean {
+            return values().filter { it.matchCount == count && it.hasBonus == hasBonus }.any()
         }
 
         fun valueOf(matchCount: Int): Rank {
             return values().first { it.matchCount == matchCount }
+        }
+
+        fun valueOf(matchCount: Int, hasBonus: Boolean): Rank {
+            return values().first { it.matchCount == matchCount && it.hasBonus == hasBonus}
         }
     }
 }
