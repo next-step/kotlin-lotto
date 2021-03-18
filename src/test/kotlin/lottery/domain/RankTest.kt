171f505652c67330e6f8826c21fe6901cdd07017
sanggu <tkdrn8578@naver.com>
2021-03-19 00:20:28 +0900

    refactor: 보너스볼 추가에 따른 CsvSource 변경 및 bonusBall 파라미터 추가

--- src/test/kotlin/lottery/domain/RankTest.kt
+++ src/test/kotlin/lottery/domain/RankTest.kt
@@ -7,15 +7,15 @@ import org.junit.jupiter.params.provider.CsvSource
 
 class RankTest {
     @ParameterizedTest
-    @CsvSource("3, FOURTH", "4, THIRD", "5, SECOND", "6, FIRST")
+    @CsvSource("3, FIFTH", "4, FOURTH", "5, SECOND", "6, FIRST")
     fun `맞춘 개수에 맞게 등수를 조회한다`(count: Int, expectedRank: Rank) {
         assertThat(Rank.valueOf(count)).isEqualTo(expectedRank)
     }
 
     @ParameterizedTest
-    @CsvSource("3, true", "0, false", "10, false", "5, true")
-    fun `맞춘 개수가 등수에 포함되는지 확인한다`(count: Int, isIn: Boolean) {
-        assertThat(Rank.isInTheRank(count)).isEqualTo(isIn)
+    @CsvSource("3, false, true", "0, true, false", "10, true, false", "5, false, true", "5, true, true")
+    fun `맞춘 개수가 등수에 포함되는지 확인한다`(count: Int, hasBonus: Boolean, isIn: Boolean) {
+        assertThat(Rank.isInTheRank(count, hasBonus)).isEqualTo(isIn)
     }
 
     @Test
