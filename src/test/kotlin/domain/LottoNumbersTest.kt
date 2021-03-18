d9d2a6387186a9760c64ad1fa2a9b9eb33a4241d
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-19 00:12:03 +0900

    refactor: 중복 코드 제거

--- src/test/kotlin/domain/LottoNumbersTest.kt
+++ src/test/kotlin/domain/LottoNumbersTest.kt
@@ -3,19 +3,22 @@ package domain
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class LottoNumbersTest {
     @Test
     fun `로또숫자열은 여섯 개의 로또숫자로 생성된다`() {
         assertDoesNotThrow {
-            LottoNumbers(listOf(LottoNumber(1), LottoNumber(2), LottoNumber(3), LottoNumber(4), LottoNumber(5), LottoNumber(6)))
+            LottoNumbers((1..6).map { LottoNumber(it) })
             LottoNumbers(1, 2, 3, 4, 5, 6)
         }
     }
 
-    @Test
-    fun `로또숫자열은 숫자 6개 이외로는 생성할 수 없다`() {
-        assertThatIllegalArgumentException().isThrownBy { LottoNumbers(listOf(LottoNumber(1))) }
-        assertThatIllegalArgumentException().isThrownBy { LottoNumbers(listOf(LottoNumber(1), LottoNumber(2), LottoNumber(3), LottoNumber(4), LottoNumber(5), LottoNumber(6), LottoNumber(7))) }
+    @ParameterizedTest
+    @ValueSource(ints = [1, 3, 5, 7])
+    fun `로또숫자열은 숫자 6개 이외로는 생성할 수 없다`(lottoNumberCount: Int) {
+        assertThatIllegalArgumentException()
+            .isThrownBy { LottoNumbers((1..lottoNumberCount).map { LottoNumber(it) }) }
     }
 }
