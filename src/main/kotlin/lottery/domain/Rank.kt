0bbaf5000bf16b3dc28e02ce48ee8b5750f09ba1
sanggu <tkdrn8578@naver.com>
2021-03-19 00:10:30 +0900

    feat: 점진적 리펙토링읠 위한 isInTheRank의 isBonus를 가진 함수 추가

--- src/main/kotlin/lottery/domain/Rank.kt
+++ src/main/kotlin/lottery/domain/Rank.kt
@@ -1,12 +1,17 @@
 package lottery.domain
 
-enum class Rank(val matchCount: Int, val price: Int) {
-    FIRST(6, 200_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5_000);
+enum class Rank(val matchCount: Int, val price: Int, val isBonus: Boolean) {
+    FIRST(6, 200_000_000, false),
+    SECOND(5, 200_000_000, true),
+    THIRD(5, 1_500_000, false),
+    FOURTH(4, 50_000, false),
+    FIFTH(3, 5_000, false);
 
     companion object {
+        fun isInTheRank(count: Int, isBonus: Boolean): Boolean {
+            return values().filter { it.matchCount == count }.any()
+        }
+
         fun isInTheRank(count: Int): Boolean {
             return values().filter { it.matchCount == count }.any()
         }
