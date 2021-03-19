6fbc17e8b5753a1e6864c06c9d37721eaabf412c
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-19 15:29:12 +0900

    feat: WinnerLottery에 보너스볼 번호와 중복되는지 검사하는 함수 추가

--- src/main/kotlin/lottery/domain/WinnerLottery.kt
+++ src/main/kotlin/lottery/domain/WinnerLottery.kt
@@ -3,6 +3,10 @@ package lottery.domain
 class WinnerLottery(numbers: List<Int>) {
     val winnerLottery: LotteryNumbers = LotteryNumbers(numbers)
 
+    fun contains(bonusBall: Int): Boolean {
+        return winnerLottery.contains(bonusBall)
+    }
+
     fun matchCount(lotteryNumbers: LotteryNumbers): Int {
         return lotteryNumbers.filter { winnerLottery.contains(it) }.count()
     }
