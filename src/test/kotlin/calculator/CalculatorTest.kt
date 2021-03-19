59b99a16930780d93320ef77b8eab5570e870673
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-19 15:27:26 +0900

    refactor: 디렉토리 구조 변경에 따른 수정

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -1,6 +1,5 @@
-package lottery.domain
+package calculator
 
-import calculator.Calculator
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
