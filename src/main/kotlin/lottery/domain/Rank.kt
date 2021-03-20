0e6037fea60d8ffcdc3d3c118473e09c5be91cf0
sanggu <tkdrn8578@naver.com>
2021-03-20 15:37:52 +0900

    refactor: convention 에 따른 수정

--- src/main/kotlin/lottery/domain/Rank.kt
+++ src/main/kotlin/lottery/domain/Rank.kt
@@ -1,11 +1,11 @@
 package lottery.domain
 
 enum class Rank(val matchCount: Int, val price: Int, val hasBonus: Boolean = false) {
-    FIRST(6, 200_000_000),
-    SECOND(5, 200_000_000, true),
-    THIRD(5, 1_500_000),
+    FIFTH(3, 5_000),
     FOURTH(4, 50_000),
-    FIFTH(3, 5_000);
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000, true),
+    FIRST(6, 200_000_000);
 
     companion object {
         fun isInTheRank(count: Int, hasBonus: Boolean): Boolean {
@@ -13,7 +13,7 @@ enum class Rank(val matchCount: Int, val price: Int, val hasBonus: Boolean = fal
         }
 
         fun valueOf(matchCount: Int, hasBonus: Boolean): Rank {
-            return values().first { it.matchCount == matchCount && it.hasBonus == hasBonus}
+            return values().first { it.matchCount == matchCount && it.hasBonus == hasBonus }
         }
     }
 }
