f0d861626430fc822c14cae4bf97616089043bb6
sanggu <tkdrn8578@naver.com>
2021-03-20 15:14:11 +0900

    feat: 보너스볼을 받기 위한 안내를 출력하기 위한 함수 추가

--- src/main/kotlin/lottery/view/InputView.kt
+++ src/main/kotlin/lottery/view/InputView.kt
@@ -8,4 +8,8 @@ object InputView {
     fun printInputLastWinnerLottery() {
         println("지난 주 당첨 번호를 입력해 주세요.")
     }
+
+    fun printInputBonusBall() {
+        println("보너스 볼을 입력해 주세요.")
+    }
 }
