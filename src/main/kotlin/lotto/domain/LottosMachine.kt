bfe93d9d8a6192cb85267f1652b675dc08dc59b3
hyunssooo <jhs695@gmail.com>
2021-03-20 21:33:15 +0900

    refactor: LottosMachine 생성자 추가

--- src/main/kotlin/lotto/domain/LottosMachine.kt
+++ src/main/kotlin/lotto/domain/LottosMachine.kt
@@ -4,20 +4,24 @@ import lotto.domain.lottogenerator.ManualLottoGenerator
 import lotto.domain.lottogenerator.RandomLottoGenerator
 
 class LottosMachine(
-    private val purchase: Money,
+    private val totalCount: Int,
     private val manualLottoGenerators: List<ManualLottoGenerator>
 ) {
 
     init {
-        require((purchase / Lotto.PRICE).toInt() >= manualLottoGenerators.size) {
-            "수동 구매 금액이 입력 금액보다 큽니다. 구매 금액: $purchase, 수동 구매 개수: $manualLottoGenerators.size"
+        require(totalCount >= manualLottoGenerators.size) {
+            "수동 구매 개수가 총 구매 개수보다 큽니다. 구매 금액: $totalCount, 수동 구매 개수: $manualLottoGenerators.size"
         }
     }
 
     constructor(purchase: Money) : this(purchase, listOf())
 
+    constructor(
+        purchase: Money,
+        manualLottoGenerators: List<ManualLottoGenerator>
+    ) : this((purchase / Lotto.PRICE).toInt(), manualLottoGenerators)
+
     fun create(): Lottos {
-        val totalCount = (purchase / Lotto.PRICE).toInt()
         val manualCount = manualLottoGenerators.size
         val randomCount = totalCount - manualCount
 
