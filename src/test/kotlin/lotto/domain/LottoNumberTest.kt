c3da40b2bf2b9b62d037cbd09c3c4f7343be4cba
남윤서 <devysnam@gmail.com>
2021-03-20 23:42:21 +0900

    test: LottoNumberTest 케이스 추가

--- src/test/kotlin/lotto/domain/LottoNumberTest.kt
+++ src/test/kotlin/lotto/domain/LottoNumberTest.kt
@@ -1,6 +1,7 @@
 package lotto.domain
 
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -11,5 +12,11 @@ internal class LottoNumberTest {
     @ValueSource(ints = [0, 46])
     fun validateRange(input: Int) {
         assertThrows<IllegalArgumentException> { LottoNumber(input) }
+    }
+
+    @DisplayName("숫자가 아닌 String값으로 입력한 경우 예외 발생")
+    @Test
+    fun init() {
+        assertThrows<NumberFormatException> { LottoNumber("a") }
     }
 }
