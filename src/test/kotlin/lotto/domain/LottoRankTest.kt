a50382f06a24cc2a8365a1a3f600cda314560498
middlefgr <2giwon@gmail.com>
2021-03-21 01:53:03 +0900

    test(step2): 변경된 객체에 맞게 테스트 코드 수정

--- src/test/kotlin/lotto/domain/LottoRankTest.kt
+++ src/test/kotlin/lotto/domain/LottoRankTest.kt
@@ -1,35 +1,17 @@
 package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class LottoRankTest {
 
-    private lateinit var winningLottoNumber: List<Int>
-
-    @BeforeEach
-    fun setup() {
-        winningLottoNumber = listOf(1, 2, 3, 4, 5, 6)
-    }
-
     @Test
-    fun `로또의 4등이 되는 경우`() {
-        assertThat(LottoRank.matchRank(3)).isEqualTo(LottoRank.FOURTH)
-    }
-
-    @Test
-    fun `로또의 3등이 되는 경우`() {
+    fun `3등`() {
         assertThat(LottoRank.matchRank(4)).isEqualTo(LottoRank.THIRD)
     }
 
     @Test
-    fun `로또의 2등이 되는 경우`() {
+    fun `2등`() {
         assertThat(LottoRank.matchRank(5)).isEqualTo(LottoRank.SECOND)
-    }
-
-    @Test
-    fun `로또의 1등이 되는 경우`() {
-        assertThat(LottoRank.matchRank(6)).isEqualTo(LottoRank.FIRST)
     }
 }
--- src/test/kotlin/lotto/domain/ProfitCalculationTest.kt
+++ src/test/kotlin/lotto/domain/ProfitCalculationTest.kt
@@ -7,10 +7,8 @@ internal class ProfitCalculationTest {
 
     @Test
     fun `14000원에 5000원 1개 당첨이면 수익률은 0점35이다`() {
-        val lottoGame = LottoGame(listOf(1, 2, 3, 4, 5, 6))
-        lottoGame.addMatchedLottoRankCount(LottoRank.FOURTH)
+        val lottoRanks = mapOf(LottoRank.FOURTH to 1)
         val profitCalculation = ProfitCalculation()
-
-        assertThat(profitCalculation.getProfitRate(lottoGame, 14000)).isEqualTo(0.35f)
+        assertThat(profitCalculation.getProfitRate(lottoRanks, 14000)).isEqualTo(0.35f)
     }
 }
