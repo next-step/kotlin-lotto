89cd3cbdbb2de0122ee4965eac0653fd0688b1ea
ohgillwhan <soora_genius@naver.com>
2021-03-20 11:36:53 +0900

    refact(LottoWonNumber) : 로또넘버 문구 추가

--- src/main/kotlin/lotto/domain/LottoWonNumbers.kt
+++ src/main/kotlin/lotto/domain/LottoWonNumbers.kt
@@ -2,7 +2,7 @@ package lotto.domain
 
 class LottoWonNumbers(val lottoNumber: Set<LottoNumber>, val bonusNUmber: LottoNumber) {
     init {
-        require(lottoNumber.size >= 6)
-        require(!lottoNumber.contains(bonusNUmber))
+        require(lottoNumber.size == 6) { "당첨 로또 번호는 6개여야 합니다" }
+        require(!lottoNumber.contains(bonusNUmber)) { "당첨 로또 번호에는 보너스 당첨번호가 존재해서는 안됩니다." }
     }
 }
