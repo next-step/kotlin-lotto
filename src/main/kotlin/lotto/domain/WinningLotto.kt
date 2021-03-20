451c6b0e463141c413cd453109e1bfcb68b2e033
Joseph <wjsdudtkd95@gmail.com>
2021-03-20 22:23:15 +0900

    refactor(lotto): 기존에 존재하는 메서드로 수정

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -19,6 +19,6 @@ data class WinningLotto(
     }
 
     private fun checkWinningNumbersContainsBonusNumber() {
-        require(!winningNumbers.value.contains(bonusNumber)) { "보너스 숫자가 이미 당첨번호에 존재합니다." }
+        require(!winningNumbers.isNumberContains(bonusNumber)) { "보너스 숫자가 이미 당첨번호에 존재합니다." }
     }
 }
