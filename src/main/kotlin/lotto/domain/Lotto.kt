08062da1abf8073776c1acce8b32c3e8f31d1623
Wisemuji <wisemuji@gmail.com>
2021-03-22 00:13:59 +0900

    feat: 로또 1장은 1~45까지의 숫자 6개로 이뤄져있다

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,3 +1,18 @@
 package lotto.domain
 
-data class Lotto(val numbers: List<Int>)
+data class Lotto(val numbers: List<Int>) {
+
+    init {
+        require(validateNumbers())
+    }
+
+    private fun validateNumbers(): Boolean {
+        return numbers.size == 6 && numbers.filterNot { it in NUMBER_RANGE }.isEmpty()
+    }
+
+    companion object {
+        private const val NUMBER_RANGE_START = 1
+        private const val NUMBER_RANGE_END = 45
+        private val NUMBER_RANGE = NUMBER_RANGE_START..NUMBER_RANGE_END
+    }
+}
