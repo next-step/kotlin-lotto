47f3a022254d792a5e81f882ca122ab1885c2023
Wisemuji <wisemuji@gmail.com>
2021-03-21 22:43:16 +0900

    feat: 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -6,6 +6,10 @@ class StringAddCalculator {
         if (text.isNullOrBlank()) {
             return 0
         }
+        val numbers = text.split(",")
+        if (numbers.size == 1) {
+            return text.toInt()
+        }
         return 0
     }
 
