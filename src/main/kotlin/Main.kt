dfefe7d991a3150d979844e056938b02d61e9264
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-22 01:37:22 +0900

    refactor: 가독성 증진

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,14 +10,10 @@ fun main() {
     val store = LottoStore(price = Money(1000))
 
     val buyingInput = BuyingInputView().receiveAmount("구입금액을 입력해 주세요.")
-
     val lottos = store.buyLottos(buyingInput.toMoney())
-
     BuyingResultView().print(lottos)
 
     val winningNumbersInput = WinningNumbersInputView().receiveWinningNumbers("지난 주 당첨 번호를 입력해주세요")
-
     val statistics = WinningStatistics(winningNumbersInput.toLottoNumbers(), lottos)
-
     WinningStatisticsView().print(statistics, store.price)
 }
