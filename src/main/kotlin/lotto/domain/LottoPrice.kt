86116210a02215ee7fe71934ec360a5530a75359
thxwelchs <thxwelchs@gmail.com>
2021-03-21 21:53:50 +0900

    refactor(domain): LottoPrice getMaximumCount 제거 후 카운트 만큼의 금액을 초과하는지 검사하는 기능 추가

--- src/main/kotlin/lotto/domain/LottoPrice.kt
+++ src/main/kotlin/lotto/domain/LottoPrice.kt
@@ -2,25 +2,30 @@ package lotto.domain
 
 class LottoPrice(
     val value: Int
-) {
+) : Comparable<LottoPrice> {
     init {
-        require(value >= MINIMUM_PRICE) {
-            "최소 ${MINIMUM_PRICE}원 이상 구매할 수 있습니다."
+        require(value >= MINIMUM_PRICE_UNIT) {
+            "최소 ${MINIMUM_PRICE_UNIT}원 이상 구매할 수 있습니다."
         }
     }
 
-    fun getMaximumCount(): LottoCount {
-        return LottoCount.from(value / MINIMUM_PRICE)
+    fun isGreaterThanEqualsByCount(count: Int): Boolean {
+        return this >= convertCountToLottoPrice(count)
     }
 
-    fun calculateAutomaticCount(manualLottoCount: LottoCount): LottoCount {
-        return LottoCount.from(value / MINIMUM_PRICE - manualLottoCount.value)
+    private fun convertCountToLottoPrice(count: Int): LottoPrice {
+        return LottoPrice(count * MINIMUM_PRICE_UNIT)
+    }
+
     fun calculateAutomaticCount(manualLottoCount: Int): Int {
         return value / MINIMUM_PRICE_UNIT - manualLottoCount
     }
+
+    override fun compareTo(other: LottoPrice): Int {
+        return this.value - other.value
     }
 
     companion object {
-        const val MINIMUM_PRICE = 1000
+        const val MINIMUM_PRICE_UNIT = 1000
     }
 }
