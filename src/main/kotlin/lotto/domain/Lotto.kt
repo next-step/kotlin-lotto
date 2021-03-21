279151abbc96308147d671b02a2968122bb73689
Wisemuji <wisemuji@gmail.com>
2021-03-22 00:40:21 +0900

    feat: 로또 1장의 가격은 1000원이고 구입 금액을 입력해서 구입할 수 있다

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -8,15 +8,25 @@ data class Lotto(val numbers: List<Int>) {
 
     private fun validateNumbers(): Boolean {
         numbers.distinct().also {
-            return it.size == 6 && it.filterNot { number -> number in NUMBER_RANGE }.isEmpty()
+            return it.size == NUMBER_SIZE && it.filterNot { number -> number in NUMBER_RANGE }.isEmpty()
         }
     }
 
     companion object {
+        private const val PRICE = 1000
+        private const val NUMBER_SIZE = 6
         private const val NUMBER_RANGE_START = 1
         private const val NUMBER_RANGE_END = 45
         private val NUMBER_RANGE = NUMBER_RANGE_START..NUMBER_RANGE_END
 
-        fun buyRandom(price: Int): List<Lotto> { return emptyList() }
+        private fun generateRandom(): List<Int> {
+            return NUMBER_RANGE.shuffled().take(NUMBER_SIZE).sorted()
+        }
+
+        fun buyRandom(price: Int): List<Lotto> {
+            return List(price / PRICE) {
+                Lotto(generateRandom())
+            }
+        }
     }
 }
