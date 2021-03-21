c95fc9a5fa72a40fc65bc3753c34237c1850e31e
thxwelchs <thxwelchs@gmail.com>
2021-03-21 22:03:24 +0900

    refactor: askManualLotto 수동로또 구매하지 않을 때는 빈 리스트 반환하게끔 수정

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,31 +2,20 @@ package lotto.view
 
 import lotto.controller.LottoGame
 import lotto.domain.BuyingLotto
-import lotto.domain.LottoCount
 import lotto.domain.LottoPrice
-import lotto.domain.ManualLottoCount
 import lotto.domain.ManualNumbers
 
 fun main() {
     val buyingPrice = LottoPrice(InputView.enterLottoBuyingPrice())
-    buyingPrice.getMaximumCount()
-    val manualLottoCount = ManualLottoCount(buyingPrice, LottoCount.from(InputView.enterManualLottoCount()))
-    val game = LottoGame(
-        BuyingLotto(
-            buyingPrice,
-            askManualLotto(manualLottoCount)
-        )
-    )
-    val lottoTicket = game.start()
+    val manualLottoCount = InputView.enterManualLottoCount(buyingPrice)
+    val lottoTicket = LottoGame.start(BuyingLotto(buyingPrice, askManualLotto(manualLottoCount)))
     ResultView.printLottoTickets(lottoTicket)
-    val result = game.doResult(lottoTicket)
+    val result = LottoGame.doResult(lottoTicket, buyingPrice)
     ResultView.printResult(result)
 }
 
-fun askManualLotto(manualLottoCount: ManualLottoCount): ManualNumbers? {
-    if (manualLottoCount.count.isEmpty()) return null
-    val manualNumbers = mutableListOf<String>()
 fun askManualLotto(manualLottoCount: Int): ManualNumbers {
+    val manualNumbers: MutableList<String> = mutableListOf()
 
     repeat(manualLottoCount) {
         manualNumbers.add(InputView.enterManualLottoNumbers())
--- src/main/kotlin/lotto/domain/BuyingLotto.kt
+++ src/main/kotlin/lotto/domain/BuyingLotto.kt
@@ -1,10 +1,9 @@
 package lotto.domain
 
 class BuyingLotto(
-    val price: LottoPrice,
-    manualLottoNumbers: ManualNumbers?
+    price: LottoPrice,
+    manualLottoNumbers: ManualNumbers
 ) {
-    val manualLotto: List<Lotto> = manualLottoNumbers?.toManualLottoNumber() ?: listOf()
-    private val manualLottoCount: LottoCount = LottoCount.from(manualLotto.size)
-    val automaticCount: LottoCount = price.calculateAutomaticCount(manualLottoCount)
+    val manualLotto: List<Lotto> = manualLottoNumbers.toLottos()
+    val automaticCount: Int = price.calculateAutomaticCount(manualLotto.size)
 }
