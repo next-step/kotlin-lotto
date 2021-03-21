fe2afe5f4472ec9079dcb06a97ab8f6f4f8d7d4e
thxwelchs <thxwelchs@gmail.com>
2021-03-21 21:55:40 +0900

    refactor: LottoGame의 BuyingLotto 프로퍼티로 가지고 있는 부분 수정

--- src/main/kotlin/lotto/controller/LottoGame.kt
+++ src/main/kotlin/lotto/controller/LottoGame.kt
@@ -3,28 +3,27 @@ package lotto.controller
 import lotto.domain.BuyingLotto
 import lotto.domain.LottoMachine
 import lotto.domain.LottoNumberTokenizer
+import lotto.domain.LottoPrice
 import lotto.domain.LottoTicket
 import lotto.domain.ResultLottoStatistics
 import lotto.domain.WinningLottoNumbers
 import lotto.domain.WinningLottoStatistics
 import lotto.view.InputView
 
-class LottoGame(
-    private val buyingLotto: BuyingLotto
-) {
+object LottoGame {
 
-    fun start(): LottoTicket {
+    fun start(buyingLotto: BuyingLotto): LottoTicket {
         return LottoMachine.buy(buyingLotto)
     }
 
-    fun doResult(lottoTicket: LottoTicket): ResultLottoStatistics {
+    fun doResult(lottoTicket: LottoTicket, buyingPrice: LottoPrice): ResultLottoStatistics {
         val stringLottoNumbers = InputView.enterLastWeekWinningLottoNumbers()
         val winningLottoNumberTokens = LottoNumberTokenizer.tokenize(stringLottoNumbers)
         val bonusNumber = InputView.enterBonusNumber()
 
         val winningLottoNumbers = WinningLottoNumbers.of(winningLottoNumberTokens, bonusNumber)
         val winningLottoStatistics = WinningLottoStatistics(lottoTicket, winningLottoNumbers)
-        val lottoProfitRate = winningLottoStatistics.calculateProfitRate(buyingLotto.price)
+        val lottoProfitRate = winningLottoStatistics.calculateProfitRate(buyingPrice)
 
         return ResultLottoStatistics(
             winningLottoStatistics,
