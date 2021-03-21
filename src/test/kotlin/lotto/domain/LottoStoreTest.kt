78d9125972bcba3bebb262998473cd151af8bd8f
dev-owner <yureka1112@gmail.com>
2021-03-21 21:40:16 +0900

    feat(lotto): 로또 수동 입력 기능 추가

--- src/test/kotlin/lotto/domain/LottoStoreTest.kt
+++ src/test/kotlin/lotto/domain/LottoStoreTest.kt
@@ -8,8 +8,8 @@ import org.junit.jupiter.api.Test
 
 internal class LottoStoreTest {
     @Test
-    @DisplayName("로또 구입 금액을 입력하면 구입 금액만큼 로또가 발급된다")
-    fun inputMoneyTest() {
+    @DisplayName("로또 구입 금액을 입력하면 구입 금액만큼 자동 로또가 발급된다")
+    fun autoLottoBuyTest() {
         // given
         val money = Money(14000)
         val expected = money / LOTTO_PRICE
@@ -17,5 +17,37 @@ internal class LottoStoreTest {
         val lottos = LottoStore().buy(money)
         // then
         assertThat(lottos.size).isEqualTo(expected)
+    }
+
+    @Test
+    @DisplayName("자동, 수동을 섞어서 로또를 구매할 수 있다")
+    fun autoAndManualBuyTest() {
+        // given
+        val money = Money(14000)
+        val manualLottos = listOf(
+            Lotto.of(setOf(1, 2, 3, 4, 5, 6)),
+            Lotto.of(setOf(1, 2, 3, 4, 5, 6)),
+            Lotto.of(setOf(1, 2, 3, 4, 5, 6))
+        )
+        // when
+        val lottos = LottoStore().buy(money, manualLottos)
+        // then
+        assertThat(lottos).containsAnyElementsOf(manualLottos)
+    }
+
+    @Test
+    @DisplayName("수동으로 로또를 구매할 수 있다")
+    fun manualBuyTest() {
+        // given
+        val money = Money(3000)
+        val manualLottos = listOf(
+            Lotto.of(setOf(1, 2, 3, 4, 5, 6)),
+            Lotto.of(setOf(1, 2, 3, 4, 5, 6)),
+            Lotto.of(setOf(1, 2, 3, 4, 5, 6))
+        )
+        // when
+        val lottos = LottoStore().buy(money, manualLottos)
+        // then
+        assertThat(lottos).containsExactlyElementsOf(manualLottos)
     }
 }
