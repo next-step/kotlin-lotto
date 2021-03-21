8ad11a47f64c377b622295e29be69413684a94f9
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-21 20:50:14 +0900

    feat(lotto-auto): 구매결과뷰는 구매한 로또들을 받아 구매결과를 출력한다

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
+import domain.money.Money
+import domain.store.LottoStore
 import view.BuyingInputView
+import view.BuyingResultView
 
 fun main() {
+    val store = LottoStore(price = Money(1000))
+
     val buyingInput = BuyingInputView().receiveAmount("구입금액을 입력해 주세요.")
+
+    val lottos = store.buyLottos(buyingInput.toMoney())
+
+    BuyingResultView().print(lottos)
 }
--- src/main/kotlin/view/BuyingResultView.kt
+++ src/main/kotlin/view/BuyingResultView.kt
@@ -1,3 +1,22 @@
 package view
 
-class BuyingResultView
+import domain.lotto.Lotto
+
+class BuyingResultView {
+    fun print(lottos: List<Lotto>) {
+        printLottoCount(lottos)
+        printAllLottoNumbers(lottos)
+    }
+
+    private fun printLottoCount(lottos: List<Lotto>) {
+        println("${lottos.size}개를 구매했습니다")
+    }
+
+    private fun printAllLottoNumbers(lottos: List<Lotto>) {
+        lottos.forEach(::printLottoNumbers)
+    }
+
+    private fun printLottoNumbers(lotto: Lotto) {
+        println("[${lotto.numbers.numbers.joinToString { it.value.toString() }}]")
+    }
+}
