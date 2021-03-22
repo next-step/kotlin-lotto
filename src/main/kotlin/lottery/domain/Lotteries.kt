99af70c72bed80bd297a35e0897fe55182ae8382
sanggu <tkdrn8578@naver.com>
2021-03-22 21:10:54 +0900

    refactor: 로또 번호를 생성하는 것에 대한 책임을 LotteryFactory 로 이동

--- src/main/kotlin/lottery/domain/Lotteries.kt
+++ src/main/kotlin/lottery/domain/Lotteries.kt
@@ -3,23 +3,16 @@ package lottery.domain
 class Lotteries private constructor(val lotteries: List<Lottery>) {
     companion object {
         private const val START_LOTTERY_COUNT = 1
-        private const val MIN_LOTTERY_NUMBER = 1
-        private const val MAX_LOTTERY_NUMBER = 45
-        private const val LOTTERY_SIZE = 6
         private const val MIN_LOTTERY_COUNT = 0
 
-        fun of(count: Int, generator: NumbersGenerator): Lotteries {
+        fun of(count: Int, numbers: List<Int>): Lotteries {
             require(count > MIN_LOTTERY_COUNT) { "입력한 로또의 개수가 올바르지 않습니다." }
 
             return Lotteries(
                 (START_LOTTERY_COUNT..count).map {
-                    Lottery(LotteryNumbers(generateRandoms(generator)))
+                    Lottery(LotteryNumbers(numbers))
                 }
             )
-        }
-
-        private fun generateRandoms(generator: NumbersGenerator): List<Int> {
-            return generator.generate(MIN_LOTTERY_NUMBER, MAX_LOTTERY_NUMBER, LOTTERY_SIZE)
         }
     }
 }
