871c92faf2127563358c8c613b8984b22bfb1845
sanggu <tkdrn8578@naver.com>
2021-03-22 20:49:17 +0900

    refactor: Bonus ball 에 대한 유효성 검사를 WinnerLottery 에게 위임함에 따른 수정

--- src/main/kotlin/lottery/domain/WinnerLottery.kt
+++ src/main/kotlin/lottery/domain/WinnerLottery.kt
@@ -1,6 +1,10 @@
 package lottery.domain
 
-class WinnerLottery(numbers: List<Int>) {
+class WinnerLottery(numbers: List<Int>, bonusBall: BonusBall) {
+    init {
+        require(numbers.isNotContains(bonusBall)) { "보너스 볼은 당첨 번호에 포함될 수 없습니다." }
+    }
+
     val winnerLottery: LotteryNumbers = LotteryNumbers(numbers)
 
     fun contains(bonusBall: Int): Boolean {
@@ -9,5 +13,9 @@ class WinnerLottery(numbers: List<Int>) {
 
     fun matchCount(lotteryNumbers: LotteryNumbers): Int {
         return lotteryNumbers.filter { winnerLottery.contains(it) }.count()
+    }
+
+    private fun List<Int>.isNotContains(bonusBall: BonusBall): Boolean {
+        return !this.contains(bonusBall.number)
     }
 }
