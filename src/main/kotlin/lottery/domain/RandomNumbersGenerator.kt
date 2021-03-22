8a08b0cc61b1c6db6996a5ce3c5a75dc84402909
sanggu <tkdrn8578@naver.com>
2021-03-22 21:02:07 +0900

    refactor: numbers 가 from 해줄때마다 shuffled() 하도록 수정

--- src/main/kotlin/lottery/domain/RandomNumbersGenerator.kt
+++ src/main/kotlin/lottery/domain/RandomNumbersGenerator.kt
@@ -4,7 +4,7 @@ object RandomNumbersGenerator : NumbersGenerator {
     private const val MIN_INDEX = 0
 
     override fun generate(min: Int, max: Int, size: Int): List<Int> {
-        val numbers = (min..max).map { it }.shuffled()
-        return numbers.subList(MIN_INDEX, size).sorted()
+        val numbers = (min..max).map { it }
+        return numbers.shuffled().subList(MIN_INDEX, size).sorted()
     }
 }
