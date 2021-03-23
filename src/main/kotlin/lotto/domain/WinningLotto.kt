1a6513b32c4785028d5a0e3a48e31052b4523c3d
middlefgr <2giwon@gmail.com>
2021-03-24 00:01:16 +0900

    feature(step3): 당첨 번호 로또 객체 추가

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -1,2 +1,7 @@
 package lotto.domain
 
+class WinningLotto(val lotto: Lotto, val bonusNumber: LottoNumber) {
+    init {
+        require(!(lotto.lottoNumbers.contains(bonusNumber)))
+    }
+}
--- src/test/kotlin/lotto/domain/WinningLottoTest.kt
+++ src/test/kotlin/lotto/domain/WinningLottoTest.kt
@@ -1,5 +1,26 @@
 package lotto.domain
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
 
-internal class WinningLottoTest
+internal class WinningLottoTest {
+
+    @Test
+    fun `당첨 번호에 보너스 번호가 포함되면 안된다`() {
+        Assertions.assertThatThrownBy {
+            WinningLotto(
+                Lotto(
+                    listOf(
+                        LottoNumber.from(1),
+                        LottoNumber.from(2),
+                        LottoNumber.from(3),
+                        LottoNumber.from(4),
+                        LottoNumber.from(5),
+                        LottoNumber.from(6)
+                    )
+                ), LottoNumber.from(6)
+            )
+        }.isInstanceOf(IllegalArgumentException::class.java)
+
+    }
+}
