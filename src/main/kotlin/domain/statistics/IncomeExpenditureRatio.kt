312e5a0df5f8c6bdedc0c9c5b8b178d68648e468
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-24 02:33:49 +0900

    feat(lotto-auto): 지출대비수익률 구현

---
+++ src/main/kotlin/domain/statistics/IncomeExpenditureRatio.kt
@@ -0,0 +1,17 @@
+package domain.statistics
+
+import domain.money.Money
+
+data class IncomeExpenditureRatio(val value: Double) {
+
+    init {
+        require(value >= 0.0)
+    }
+
+    companion object {
+        fun calculatedBy(income: Money, expenditure: Money): IncomeExpenditureRatio {
+            require(expenditure > Money.ZERO)
+            return IncomeExpenditureRatio(income.value.toDouble() / expenditure.value.toDouble())
+        }
+    }
+}
