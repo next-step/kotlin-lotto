e99c3dac307086a050bdf2e1a02c0e7e7d219dd7
thxwelchs <thxwelchs@gmail.com>
2021-03-24 01:35:25 +0900

    refactor: LottoGame의 start 함수에 역할 위임

--- src/main/kotlin/lotto/controller/LottoGame.kt
+++ src/main/kotlin/lotto/controller/LottoGame.kt
@@ -4,16 +4,42 @@ import lotto.domain.LottoMachine
 import lotto.domain.LottoNumberTokenizer
 import lotto.domain.LottoPrice
 import lotto.domain.LottoTicket
-import lotto.domain.ManualNumbers
-import lotto.vo.ResultLottoStatistics
 import lotto.domain.WinningLottoNumbers
 import lotto.domain.WinningLottoStatistics
+import lotto.domain.generator.AutoLottoGenerator
+import lotto.domain.generator.ManualLottoGenerator
 import lotto.view.InputView
+import lotto.vo.ResultLottoStatistics
 
 object LottoGame {
 
-    fun start(price: LottoPrice, manualNumbers: ManualNumbers): LottoTicket {
-        return LottoMachine.buy(price, manualNumbers)
+    fun start(price: LottoPrice, manualLottoCount: Int): LottoTicket {
+        val lottoMachine = LottoMachine(AutoLottoGenerator())
+        val automaticLottoCount = price.calculateAutomaticCount(manualLottoCount)
+        val manualLottoNumbers = (1..manualLottoCount).map { askManualLotto() }
+        val automaticLottos = (1..automaticLottoCount).map { lottoMachine.buy() }
+        val manualLottos = manualLottoNumbers.map {
+            lottoMachine.setGenerator(ManualLottoGenerator(it))
+            lottoMachine.buy()
+        }
+        val lottoTicket = LottoTicket(
+            manuals = manualLottos,
+            automatics = automaticLottos
+        )
+
+        validateLottoCount(price, lottoTicket)
+
+        return lottoTicket
+    }
+
+    private fun validateLottoCount(price: LottoPrice, lottoTicket: LottoTicket) {
+        require(price.isExceedPriceByCount(lottoTicket.totalLottoCount())) {
+            "로또 구매수는 구입금액을 초과할 수 없습니다."
+        }
+    }
+
+    private fun askManualLotto(): String {
+        return InputView.enterManualLottoNumbers()
     }
 
     fun doResult(lottoTicket: LottoTicket, buyingPrice: LottoPrice): ResultLottoStatistics {
