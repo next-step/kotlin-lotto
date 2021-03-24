58941fa96a78f614a269bd1a8aecf1a53a3a7d73
ohgillwhan <soora_genius@naver.com>
2021-03-24 23:12:33 +0900

    refact(LottoNumber) : private constructor 제거

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -1,6 +1,6 @@
 package lotto.domain
 
-data class LottoNumber private constructor(val number: Int) {
+data class LottoNumber(val number: Int) {
     init {
         require(number in LOTTO_MINIMUM_NUMBER..LOTTO_MAXIMUM_NUMBER) { "로또 번호는 $LOTTO_MINIMUM_NUMBER 이상 $LOTTO_MAXIMUM_NUMBER 여야 해요." }
     }
