54c96f3eabffd68717720aad2185d343a2c63bd0
ohgillwhan <soora_genius@naver.com>
2021-03-24 23:02:43 +0900

    refact(LottoTickets) : 제약사항 제거

--- src/main/kotlin/lotto/domain/LottoTickets.kt
+++ src/main/kotlin/lotto/domain/LottoTickets.kt
@@ -1,10 +1,6 @@
 package lotto.domain
 
 class LottoTickets(private val lottoTickets: List<LottoTicket>) : List<LottoTicket> by lottoTickets {
-    init {
-        require(lottoTickets.isNotEmpty()) { "로또는 1개 이상을 갖고있어야 합니다." }
-    }
-
     constructor(count: Int, generator: LottoNumberGenerator = LottoNumberRandomGenerator()) : this(
         createLotto(
             count,
