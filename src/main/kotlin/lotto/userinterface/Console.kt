69aa7ac246b4ba15782f01a0bcc98e971737a58d
bactoria <bactoria@gmail.com>
2021-03-24 23:18:59 +0900

    refactor: 가독성 개선

--- src/main/kotlin/lotto/userinterface/Console.kt
+++ src/main/kotlin/lotto/userinterface/Console.kt
@@ -45,10 +45,14 @@ class Console : UserInterface {
         return inputLottoNumber()
     }
 
-    private fun inputLastWeekWinningLottoBonusNumber(lottoNumbers: List<Int>): Int {
+    private tailrec fun inputLastWeekWinningLottoBonusNumber(lottoNumbers: List<Int>): Int {
         println("보너스 볼을 입력해 주세요.")
-        val bonusNumber = readLine()?.toIntOrNull() ?: inputLastWeekWinningLottoBonusNumber(lottoNumbers)
-        return if (bonusNumber in 1..45 && bonusNumber !in lottoNumbers) bonusNumber else inputLastWeekWinningLottoBonusNumber(lottoNumbers)
+        val bonusNumber = readLine()?.toIntOrNull()
+
+        if (bonusNumber == null || bonusNumber !in 1..45 || bonusNumber in lottoNumbers)
+            return inputLastWeekWinningLottoBonusNumber(lottoNumbers)
+
+        return bonusNumber
     }
 
     override fun outputPurchasedMessage(dto: LottoNumbersDto) {
