97d201b4ed4872ef2e05dd68958835452a674ac4
middlefgr <2giwon@gmail.com>
2021-03-25 23:14:20 +0900

    fix(step3) : data클래스로 수정

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -1,6 +1,6 @@
 package lotto.domain
 
-class WinningLotto(val lotto: Lotto, val bonusNumber: LottoNumber) {
+data class WinningLotto(val lotto: Lotto, val bonusNumber: LottoNumber) {
     init {
         require(!(lotto.matchBonus(bonusNumber)))
     }
