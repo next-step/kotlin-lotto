730e0e492671c46e8a17e810aaed6ce8f5556b6d
middlefgr <2giwon@gmail.com>
2021-03-25 23:13:46 +0900

    fix(step3) : matchBonus로 변경

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -2,6 +2,6 @@ package lotto.domain
 
 class WinningLotto(val lotto: Lotto, val bonusNumber: LottoNumber) {
     init {
-        require(!(lotto.lottoNumbers.contains(bonusNumber)))
+        require(!(lotto.matchBonus(bonusNumber)))
     }
 }
