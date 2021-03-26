7d6c6831fa160cf2fb4c51397035847ac4a40542
thxwelchs <thxwelchs@gmail.com>
2021-03-26 11:22:35 +0900

    refactor: LottoTicket 외부에서 인자로 받지 않는 프로퍼티 클래스 내부에 선언, 예외 테스트 추가
    ㅡ

--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -3,11 +3,12 @@ package lotto.domain
 class LottoTicket(
     manualLottos: List<Lotto>,
     automaticLottos: List<Lotto>,
-    val price: LottoPrice,
-    val lottos: List<Lotto> = manualLottos + automaticLottos,
-    val automaticCount: Int = automaticLottos.size,
-    val manualCount: Int = manualLottos.size
+    val price: LottoPrice
 ) {
+    val lottos: List<Lotto> = manualLottos + automaticLottos
+    val automaticCount: Int = automaticLottos.size
+    val manualCount: Int = manualLottos.size
+
     init {
         require(price.isExceedPriceByCount(lottos.size)) {
             "로또 구매수는 구입금액을 초과할 수 없습니다."
--- src/test/kotlin/lotto/domain/LottoTicketTest.kt
+++ src/test/kotlin/lotto/domain/LottoTicketTest.kt
@@ -1,6 +1,9 @@
 package lotto.domain
 
+import lotto.domain.generator.AutoLottoGenerator
+import lotto.domain.generator.ManualLottoGenerator
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -25,5 +28,20 @@ internal class LottoTicketTest {
             price = LottoPrice(99999999)
         )
         assertThat(lottoTicket.lottos.size).isEqualTo(expectedSize)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "10000, 5, 6",
+        "30000, 20, 11",
+        "90000, 80, 11"
+    )
+    fun `로또 구매수에 따른 초과금액 테스트`(price: Int, manualSize: Int, automaticSize: Int) {
+        val lottoPrice = LottoPrice(price)
+        val manualLottos = (1..manualSize).map { ManualLottoGenerator("1,2,3,4,5,6").generate() }
+        val automaticLottos = (1..automaticSize).map { AutoLottoGenerator().generate() }
+        assertThrows<IllegalArgumentException> {
+            LottoTicket( manualLottos, automaticLottos, lottoPrice)
+        }
     }
 }
