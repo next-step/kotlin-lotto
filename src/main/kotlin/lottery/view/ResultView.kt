31d907d0b212b3554af2af8890ceb9a44d5196fe
sanggu <tkdrn8578@naver.com>
2021-03-29 00:46:25 +0900

    refactor: 불필요한 brace 제거

--- src/main/kotlin/lottery/view/ResultView.kt
+++ src/main/kotlin/lottery/view/ResultView.kt
@@ -6,8 +6,9 @@ import lottery.domain.RankCounts
 
 object ResultView {
     fun printCountOfBuyLottery(manualLotteryCount: Int, autoLotteryCount: Int) {
-        println("수동으로 ${manualLotteryCount}, 자동으로 ${autoLotteryCount}개를 구매했습니다.")
+        println("수동으로 $manualLotteryCount, 자동으로 ${autoLotteryCount}개를 구매했습니다.")
     }
+
     fun printProfit(profit: String) {
         print("총 수익률은 ${profit}입니다.")
     }
