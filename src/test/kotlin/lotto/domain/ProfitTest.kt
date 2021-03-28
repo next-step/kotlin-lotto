4ce2d7ddb92b481d06df3ace35a85f8f30ef93b7
bactoria <bactoria@gmail.com>
2021-03-28 12:36:53 +0900

    refactor: 가독성 개선

--- src/test/kotlin/lotto/domain/ProfitTest.kt
+++ src/test/kotlin/lotto/domain/ProfitTest.kt
@@ -7,7 +7,7 @@ internal class ProfitTest {
 
     @Test
     fun `당첨된 로또가 없으면 수익률은 0이다`() {
-        val prizeRankCount = mapOf(Pair(LottoPrize.WHACK, PositiveNumber(1)))
+        val prizeRankCount = mapOf(LottoPrize.WHACK to PositiveNumber(1))
         val purchaseAmount = Money(1000)
         val profit = Profit(prizeRankCount = prizeRankCount, purchaseAmount = purchaseAmount)
 
