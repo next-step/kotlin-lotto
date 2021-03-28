58cf46dd9c72098754364744743abab91c769482
sanggu <tkdrn8578@naver.com>
2021-03-28 23:27:58 +0900

    refactor: Lotteries.of 를 List<List<Int>> 로 변경함에 따른 테스트코드 수정

--- src/test/kotlin/lottery/domain/LotteriesTest.kt
+++ src/test/kotlin/lottery/domain/LotteriesTest.kt
@@ -11,7 +11,9 @@ import org.junit.jupiter.params.provider.ValueSource
 class LotteriesTest {
     @Test
     fun `입력 개수만큼 로또를 생성한다`() {
-        val lotteries = Lotteries.of(5, TEST_NUMBERS)
+        val allNumbers = listOf(TEST_NUMBERS, TEST_NUMBERS, TEST_NUMBERS, TEST_NUMBERS, TEST_NUMBERS)
+
+        val lotteries = Lotteries.of(allNumbers)
 
         assertThat(lotteries.lotteries).containsExactly(
             Lottery(TEST_LOTTERY_NUMBERS),
@@ -25,8 +27,10 @@ class LotteriesTest {
     @ParameterizedTest
     @ValueSource(ints = [-1, 0, -20])
     fun `입력 개수가 0보다 작은경우 생성되지 않는다`(wrongCount: Int) {
+        val allNumbers = (1..wrongCount).map { TEST_NUMBERS }
+
         assertThrows<IllegalArgumentException> {
-            Lotteries.of(wrongCount, TEST_NUMBERS)
+            Lotteries.of(allNumbers)
         }
     }
 }
