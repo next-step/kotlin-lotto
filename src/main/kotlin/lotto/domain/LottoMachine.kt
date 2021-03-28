fe6f5cc29a2868f19d38135387411039ba99a163
bactoria <bactoria@gmail.com>
2021-03-29 01:23:42 +0900

    refactor: 유연성을 위한 인터페이스를 타입으로 사용

--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -1,18 +1,18 @@
 package lotto.domain
 
-class LottoMachine(private val lottoPrice: Money, private val lottoGenerator: LottoGenerator) {
+class LottoMachine(private val lottoPrice: Money, private val randomLottoGenerator: LottoGenerator) {
 
-    fun sellLottos(money: Money, manualLottoGenerators: List<ManualLottoGenerator> = emptyList()): List<Lotto> {
+    fun sellLottos(money: Money, manualLottoGenerators: List<LottoGenerator> = emptyList()): List<Lotto> {
         require(manualLottoGenerators.isEmpty() || money >= lottoPrice * PositiveNumber(manualLottoGenerators.size)) { "수동 로또를 구매하기에 부족한 금액입니다. money: ${money.value}, lottoPrice: ${lottoPrice.value}, manualLottoGenerators size: ${manualLottoGenerators.size}" }
 
         val sellableAllLottoCount = money.sellableLottoCount(lottoPrice)
 
-        val manualLottoCount = manualLottoGenerators.count()
-        val randomLottoCount = sellableAllLottoCount - manualLottoCount
+        val lottoCount = manualLottoGenerators.count()
+        val defaultLottoCount = sellableAllLottoCount - lottoCount
 
-        val manualLotto = manualLottoGenerators.map { it.generate() }
-        val randomLotto = List(randomLottoCount) { lottoGenerator.generate() }
+        val manualLottos = manualLottoGenerators.map { it.generate() }
+        val randomLottos = List(defaultLottoCount) { randomLottoGenerator.generate() }
 
-        return manualLotto + randomLotto
+        return manualLottos + randomLottos
     }
 }
