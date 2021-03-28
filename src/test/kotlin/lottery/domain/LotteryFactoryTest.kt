bc8f4b5b9da12812ba2d4168cdb8ff131c0937da
sanggu <tkdrn8578@naver.com>
2021-03-28 23:28:23 +0900

    feat: 수동 로또 번호를 입력받아 로또들을 생성한다 에 대한 테스트 코드 추가

--- src/test/kotlin/lottery/domain/LotteryFactoryTest.kt
+++ src/test/kotlin/lottery/domain/LotteryFactoryTest.kt
@@ -1,7 +1,9 @@
 package lottery.domain
 
 import fixture.LotteryFixture.TEST_GENERATOR
+import fixture.LotteryFixture.TEST_NUMBERS
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -13,7 +15,7 @@ class LotteryFactoryTest {
     fun `금액에 따라 로또 생성 개수를 정해진다`(inputPrice: Int, expectCount: Int) {
         val factory = LotteryFactory(inputPrice)
 
-        val buyLotteries = factory.buy(TEST_GENERATOR)
+        val buyLotteries = factory.buy(TEST_GENERATOR, listOf())
 
         assertThat(buyLotteries.lotteries).hasSize(expectCount)
     }
@@ -28,10 +30,19 @@ class LotteryFactoryTest {
 
     @ParameterizedTest
     @CsvSource("2000, 2", "2400, 2", "1100, 1")
-    fun `입력한 금액에 따른 개수만큼 로또를 구매한다`() {
-        val lotteryFactory = LotteryFactory(2000)
-        val lotteries = lotteryFactory.buy(TEST_GENERATOR)
+    fun `입력한 금액에 따른 개수만큼 로또를 구매한다`(price: Int, expectdLotteryCount: Int) {
+        val lotteryFactory = LotteryFactory(price)
+        val lotteries = lotteryFactory.buy(TEST_GENERATOR, listOf())
 
-        assertThat(lotteries.lotteries.size).isEqualTo(2)
+        assertThat(lotteries.lotteries.size).isEqualTo(expectdLotteryCount)
+    }
+
+    @Test
+    fun `수동 로또 번호를 입력받아 로또들을 생성한다`() {
+        val factory = LotteryFactory(30000)
+
+        val buyLotteries = factory.buy(TEST_GENERATOR, listOf(TEST_NUMBERS))
+
+        assertThat(buyLotteries.lotteries).hasSize(30)
     }
 }
