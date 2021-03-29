8b0e9b9fd0cc8e9b50c67b711a2390d3b5d60232
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-30 00:39:32 +0900

    feat(lotto-bonus): 로또숫자열 내의 로또숫자들과 보너스를 위한 숫자는 서로 겹칠 수 없다

--- src/main/kotlin/domain/winning/WinningNumbers.kt
+++ src/main/kotlin/domain/winning/WinningNumbers.kt
@@ -3,4 +3,8 @@ package domain.winning
 import domain.lotto.LottoNumber
 import domain.lotto.LottoNumbers
 
-class WinningNumbers(val numbers: LottoNumbers, val bonus: LottoNumber)
+class WinningNumbers(val numbers: LottoNumbers, val bonus: LottoNumber) {
+    init {
+        require(numbers.numbers.none { it == bonus })
+    }
+}
--- src/test/kotlin/domain/winning/WinningNumbersTest.kt
+++ src/test/kotlin/domain/winning/WinningNumbersTest.kt
@@ -2,8 +2,11 @@ package domain.winning
 
 import domain.lotto.LottoNumber
 import domain.lotto.lottoNumberOf
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class WinningNumbersTest {
     @Test
@@ -14,5 +17,31 @@ internal class WinningNumbersTest {
                 bonus = LottoNumber.parse(7)
             )
         }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1,2,3,4,5,6, 1",
+        "1,2,3,4,5,6, 2",
+        "7,8,9,10,11,12, 12"
+    )
+    fun `로또숫자열 내의 로또숫자들과 보너스를 위한 숫자는 서로 겹칠 수 없다`(
+        n1: Int,
+        n2: Int,
+        n3: Int,
+        n4: Int,
+        n5: Int,
+        n6: Int,
+        bonusNumber: Int
+    ) {
+        // given
+        val numbers = lottoNumberOf(n1, n2, n3, n4, n5, n6)
+        val bonus = LottoNumber.parse(bonusNumber)
+
+        // when
+        val createWinningNumbers: () -> Unit = { WinningNumbers(numbers, bonus) }
+
+        // then
+        assertThatIllegalArgumentException().isThrownBy(createWinningNumbers)
     }
 }
