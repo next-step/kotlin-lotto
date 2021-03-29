04dca40a29f82c2aa7c848c93e76fd80e929e192
sanggu <tkdrn8578@naver.com>
2021-03-30 00:53:34 +0900

    refactor: Numbers를 합치는 것을 + 연산자로 변경

--- src/main/kotlin/lottery/domain/Numbers.kt
+++ src/main/kotlin/lottery/domain/Numbers.kt
@@ -2,6 +2,6 @@ package lottery.domain
 
 class Numbers(val numbers: List<LotteryNumbers>) : List<LotteryNumbers> by numbers {
     fun merge(inputNumbers: Numbers): Numbers {
-        return Numbers(numbers.plus(inputNumbers))
+        return Numbers(numbers + inputNumbers)
     }
 }
