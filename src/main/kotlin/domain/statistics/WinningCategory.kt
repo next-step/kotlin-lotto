3103157bf3dcf9ab19bc82e536c34b2d62527611
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-30 00:13:13 +0900

    feat(lotto-bonus): 당첨항목 '5개 일치, 보너스 볼 일치' 추가

--- src/main/kotlin/domain/statistics/WinningCategory.kt
+++ src/main/kotlin/domain/statistics/WinningCategory.kt
@@ -6,6 +6,7 @@ enum class WinningCategory(val numberOfMatched: Int, val prize: Money) {
     THREE_CORRECT(3, Money(5_000)),
     FOUR_CORRECT(4, Money(50_000)),
     FIVE_CORRECT(5, Money(1_500_000)),
+    FIVE_CORRECT_WITH_BONUS(5, Money(30_000_000)),
     SIX_CORRECT(6, Money(2_000_000_000));
 
     companion object {
--- src/test/kotlin/domain/statistics/WinningCategoryTest.kt
+++ src/test/kotlin/domain/statistics/WinningCategoryTest.kt
@@ -8,12 +8,13 @@ import org.junit.jupiter.params.provider.CsvSource
 
 internal class WinningCategoryTest {
     @Test
-    fun `당첨항목은 '3개 일치', '4개 일치', '5개 일치', '6개 일치'가 있다`() {
+    fun `당첨항목은 '3개 일치', '4개 일치', '5개 일치', '5개 일치, 보너스 볼 일치', '6개 일치'가 있다`() {
         assertThat(WinningCategory.values().map { it.name })
             .containsExactly(
                 "THREE_CORRECT",
                 "FOUR_CORRECT",
                 "FIVE_CORRECT",
+                "FIVE_CORRECT_WITH_BONUS",
                 "SIX_CORRECT"
             )
     }
@@ -23,6 +24,7 @@ internal class WinningCategoryTest {
         "THREE_CORRECT, 3",
         "FOUR_CORRECT, 4",
         "FIVE_CORRECT, 5",
+        "FIVE_CORRECT_WITH_BONUS, 5",
         "SIX_CORRECT, 6"
     )
     fun `각 당첨항목마다 일치하는 개수를 가지고 있다`(category: WinningCategory, numberOfMatched: Int) {
@@ -34,6 +36,7 @@ internal class WinningCategoryTest {
         "THREE_CORRECT, 5000",
         "FOUR_CORRECT, 50000",
         "FIVE_CORRECT, 1500000",
+        "FIVE_CORRECT_WITH_BONUS, 30000000",
         "SIX_CORRECT, 2000000000"
     )
     fun `당첨항목마다 당첨금은 아래와 같다`(category: WinningCategory, prize: Long) {
