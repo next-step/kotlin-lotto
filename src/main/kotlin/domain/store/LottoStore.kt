137e6cc14850ab5da106cff4629553752a0caf00
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-04-03 21:26:05 +0900

    feat(lotto-manual): 로또판매기에서 수동선택 리스트로 로또를 구매할 수 있다

--- src/main/kotlin/domain/store/LottoStore.kt
+++ src/main/kotlin/domain/store/LottoStore.kt
@@ -1,6 +1,7 @@
 package domain.store
 
 import domain.lotto.Lotto
+import domain.lotto.LottoNumbers
 import domain.money.Money
 
 class LottoStore(val price: Money, private val randomLottoNumberGenerator: RandomLottoNumberGenerator = CommonRandomLottoNumberGenerator()) {
@@ -8,8 +9,21 @@ class LottoStore(val price: Money, private val randomLottoNumberGenerator: Rando
         require(price > Money.ZERO)
     }
 
-    fun buyLottos(money: Money): List<Lotto> {
-        return (1..(money.dividedBy(price))).map { generateLotto() }
+    fun buyLottos(money: Money, manualPicks: List<LottoNumbers> = emptyList()): List<Lotto> {
+        val totalLottoCount = money.dividedBy(price)
+
+        require(manualPicks.size <= totalLottoCount)
+
+        val manualPickLottos = generateManualPickLottos(manualPicks)
+        val autoPickLottos = generateAutoPickLottos(size = totalLottoCount - manualPickLottos.size)
+
+        return manualPickLottos + autoPickLottos
+    }
+
+    private fun generateAutoPickLottos(size: Long) = (1..size).map { generateLotto() }
+
+    private fun generateManualPickLottos(manualPicks: List<LottoNumbers>): List<Lotto> {
+        return manualPicks.map { Lotto(it) }
     }
 
     private fun generateLotto() = Lotto(randomLottoNumberGenerator.generate())
--- src/test/kotlin/domain/store/LottoStoreTest.kt
+++ src/test/kotlin/domain/store/LottoStoreTest.kt
@@ -1,8 +1,12 @@
 package domain.store
 
 import domain.lotto.Lotto
+import domain.lotto.LottoNumbers
+import domain.lotto.lottoNumberOf
 import domain.money.Money
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.params.ParameterizedTest
@@ -39,5 +43,71 @@ internal class LottoStoreTest {
     fun `로또판매기에 준 돈으로 살 수 있는 최대한의 로또를 살 수 있다`(lottoPrice: Long, moneyToBuy: Long, lottoCount: Int) {
         val store = LottoStore(Money(lottoPrice))
         Assertions.assertThat(store.buyLottos(Money(moneyToBuy))).hasSize(lottoCount)
+    }
+
+    @Test
+    fun `로또판매기에 돈과 함께 수동 선택한 숫자열을 주고 로또를 사면, 선택한 숫자열이 담긴 로또를 살 수 있다`() {
+        // given
+        val store = LottoStore(Money(1000))
+        val expected: List<LottoNumbers> = listOf(lottoNumberOf(1, 2, 3, 4, 5, 6))
+
+        // when
+        val boughtLottos = store.buyLottos(Money(1000), listOf(lottoNumberOf(1, 2, 3, 4, 5, 6)))
+        val actual = boughtLottos.map { it.numbers }
+
+        // then
+        assertThat(actual).containsAnyElementsOf(expected)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "5000, 5",
+        "4500, 4"
+    )
+    fun `로또판매기에 수동 선택한 숫자열을 주고 로또를 사더라도, 주어진 금액으로 살 수 있는 최대한의 로또만을 살 수 있다`(
+        money: Long,
+        boughtCount: Int
+    ) {
+        // given
+        val store = LottoStore(price = Money(1000))
+
+        // when
+        val actual = store.buyLottos(Money(money), listOf(lottoNumberOf(1, 2, 3, 4, 5, 6)))
+
+        // then
+        assertThat(actual).hasSize(boughtCount)
+    }
+
+    @Test
+    fun `금액에 비례한 구매 수량보다 수동 선택 개수가 많지 않아야 한다`() {
+        val store = LottoStore(Money(1000))
+        val manualPicks = listOf(
+            lottoNumberOf(1, 2, 3, 4, 5, 6),
+            lottoNumberOf(1, 2, 3, 4, 5, 6)
+        )
+
+        assertThatIllegalArgumentException().isThrownBy { store.buyLottos(Money(1000), manualPicks) }
+    }
+
+    @Test
+    fun `로또판매기에서 로또를 살 때, 수동 입력한 순서대로 구매한 로또의 맨앞에서부터 바뀐다`() {
+        // given
+        val randomNumbers = lottoNumberOf(40, 41, 42, 43, 44, 45)
+        val randomLottoNumbersGenerator = object : RandomLottoNumberGenerator {
+            override fun generate() = randomNumbers
+        }
+        val store = LottoStore(Money(1000), randomLottoNumbersGenerator)
+        val manualPicks = listOf(
+            lottoNumberOf(1, 2, 3, 4, 5, 6),
+            lottoNumberOf(7, 8, 9, 10, 11, 12)
+        )
+        val expectedList: List<LottoNumbers> = manualPicks + randomNumbers
+
+        // when
+        val boughtLottos = store.buyLottos(Money(3000), manualPicks)
+        val actualList = boughtLottos.map { it.numbers }
+
+        // then
+        assertThat(actualList).containsExactlyElementsOf(expectedList)
     }
 }
