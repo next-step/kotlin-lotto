bf2761875fa29b0d495bb2edc8855be4fe551594
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-04-05 01:13:17 +0900

    feat(lotto-manual): 수동 구매 개수가 없다면 안내문구 없이 다음으로 넘어감

--- src/main/kotlin/view/ManualPickInputView.kt
+++ src/main/kotlin/view/ManualPickInputView.kt
@@ -5,6 +5,10 @@ object ManualPickInputView {
         println("수동으로 구매할 로또 수를 입력해 주세요.")
         val count = readLine()!!.toInt()
 
+        if (count < 1) {
+            return
+        }
+
         println("수동으로 구매할 번호를 입력해 주세요.")
         val results = (1..count).map { readLottoNumbers() }
     }
