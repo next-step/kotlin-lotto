e5ffde79a1d613e026bf501d03d541e8e02434e4
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-04-06 00:03:21 +0900

    feat(lotto-manual): 입력한 당첨번호가 유효하지 않은 번호면 다시 입력받음

--- src/main/kotlin/view/WinningNumbersInput.kt
+++ src/main/kotlin/view/WinningNumbersInput.kt
@@ -1,23 +1,13 @@
 package view
 
 import domain.lotto.LottoNumber
-import domain.lotto.LottoNumbers
 import domain.winning.WinningNumbers
 
-data class WinningNumbersInput(val numbers: List<Int>, val bonus: Int) {
-    constructor(numbers: String, bonus: Int) : this(parseNumbers(numbers), bonus)
-
+data class WinningNumbersInput(val numbers: ParsedManualNumbers, val bonus: Int) {
     fun toWinningNumbers(): WinningNumbers {
         return WinningNumbers(
-            numbers = LottoNumbers.fromList(numbers.map { LottoNumber.parse(it) }),
+            numbers = numbers.toLottoNumbers(),
             bonus = LottoNumber.parse(bonus)
         )
-    }
-
-    companion object {
-        private fun parseNumbers(string: String): List<Int> {
-            return string.split("\\s*,\\s*".toRegex())
-                .map { it.toInt() }
-        }
     }
 }
--- src/main/kotlin/view/WinningNumbersInputView.kt
+++ src/main/kotlin/view/WinningNumbersInputView.kt
@@ -3,10 +3,21 @@ package view
 object WinningNumbersInputView {
     fun receiveWinningNumbers(): WinningNumbersInput {
         println("\n지난 주 당첨 번호를 입력해주세요")
-        val winningNumbers = readLine()!!
+        val winningNumbers = readLottoNumbers()
 
         println("보너스 볼을 입력해주세요.")
         val bonus = readLine()!!.toInt()
         return WinningNumbersInput(winningNumbers, bonus)
+    }
+
+    private tailrec fun readLottoNumbers(): ParsedManualNumbers {
+        val input = readLine()!!
+        return when (val result = LottoNumberParser.parse(input)) {
+            is ParsedManualNumbers -> return result
+            is InvalidManualNumbers -> {
+                println(result.message)
+                readLottoNumbers()
+            }
+        }
     }
 }
