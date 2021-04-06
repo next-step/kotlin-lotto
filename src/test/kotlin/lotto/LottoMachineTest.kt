bd8c003d74bfbd1b3fd7f7ee3297d036c313e2a5
최원영 <wy.choi@woowahan.com>
2021-04-06 12:29:35 +0900

    create step4

--- src/test/kotlin/lotto/LottoMachineTest.kt
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -3,13 +3,22 @@ package lotto
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-internal class LottoMachineTest {
+class LottoMachineTest {
 
     @Test
-    internal fun `로또 발행`() {
+    fun `로또 발행 수동 X`() {
         val amount = 10000
         val lottoMachine = LottoMachine()
-        val lottoPaper: LottoPaper = lottoMachine.issue(amount)
+        val lottoPaper: LottoPaper = lottoMachine.issue(amount, listOf())
         assertThat(lottoPaper.size).isEqualTo(10)
+    }
+
+    @Test
+    fun `로또 발행 수동 O`() {
+        val amount = 10000
+        val lottoMachine = LottoMachine()
+        val lottoPaper: LottoPaper = lottoMachine.issue(amount, listOf("1,2,3,4,5,6"))
+        assertThat(lottoPaper.size).isEqualTo(10)
+        assertThat(lottoPaper.lottoNumbers).contains(LottoNumber(listOf(1, 2, 3, 4, 5, 6)))
     }
 }
