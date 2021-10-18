4f8a050fc0a07d678f924271c18205b66cc05e23
Pyro <rhwjddhks@gmail.com>
2021-10-18 13:50:17 +0900

    feat: StringAddCalculator 구현

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -1,7 +1,27 @@
 package calculator
 
 class StringAddCalculator {
-    fun add(text: String): Int {
-        TODO("Not yet implemented")
+    fun add(text: String?): Int {
+        if (text.isNullOrBlank()) {
+            return 0
+        }
+        val numbers = parseToNumbers(text)
+        if (numbers.any { it < 0 }) {
+            throw RuntimeException("음수가 전달되어서는 안됩니다.")
+        }
+        return numbers.sum()
+    }
+
+    private fun parseToNumbers(text: String): List<Int> {
+        return text.substringAfter("\n")
+            .split(*getDelimiters(text).toTypedArray())
+            .map { it.toInt() }
+    }
+
+    private fun getDelimiters(text: String): List<String> {
+        return if (text.startsWith("//")) listOf(
+            ",", ":",
+            text.substringAfter("//").substringBefore("\n")
+        ) else listOf(",", ":")
     }
 }
