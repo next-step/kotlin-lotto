17c468b5efba9907e46607024b6f90f45ce41ee4
ninjasul <ninjasul@gmail.com>
2021-10-21 03:34:54 +0900

    refactor: 테스트명 수정

--- src/test/kotlin/calculator/domain/StringAddCalculatorTest.kt
+++ src/test/kotlin/calculator/domain/StringAddCalculatorTest.kt
@@ -41,7 +41,7 @@ class StringAddCalculatorTest {
         assertThat(calculator.calculate(input)).isSameAs(6)
     }
 
-    @DisplayName("//\"와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다")
+    @DisplayName("//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다")
     @ParameterizedTest
     @ValueSource(strings = ["//;\n1;2;3"])
     fun customDelimiter(input: String) {
