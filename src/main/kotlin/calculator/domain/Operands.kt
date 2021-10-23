cf14b278f784fe9a3a575eb22061c3c3a0a20cda
ninjasul <ninjasul@gmail.com>
2021-10-23 23:31:56 +0900

    Step1 - 문자열 덧셈 계산기 (#172)
    * doc: README.md 파일 수정
    
    * chore: empty.txt 삭제
    
    * feat: Operand 구현
    
    * refactor: Operand를 value class로 변경
    
    * feat: Operand 추가 기능 구현
    
    * feat: Operands 구현
    
    * chore: Operand 클래스 이동
    
    * feat: InputExpressionParser 구현
    
    * feat: StringAddCalculator 구현
    
    * feat: View 구현
    
    * feat: Application, CalculatorLauncher 구현
    
    * bugfix: 패키지 참조 수정
    
    * refactor: 코드 정리
    
    * refactor: CalculatorLauncher의 멤버 일부를 private으로 변경
    
    * refactor: null이나 공백입력 처리 로직 변경
    
    * refactor: InputExpressionParser 리팩토링 수행
    
    * refactor: 테스트명 수정
    
    * refactor: main() 함수를 non-static으로 변경
    
    * refactor: Operand의 팩토리 메소드의 파라미터를 not null로 처리
    
    * style: 코드 리포맷
    
    * refactor: Operand의 기본 생성자를 private 으로 설정하여 팩토리 메소드로만 생성하도록 변경
    
    * refactor: Operand의 plus 연산자를 오버로딩하여 누적합을 구하도록 수정.
    
    * refactor: Operand의 plus 연산자를 오버로딩하여 누적합을 구하도록 수정.

--- src/main/kotlin/calculator/domain/Operands.kt
+++ src/main/kotlin/calculator/domain/Operands.kt
@@ -4,14 +4,15 @@ class Operands private constructor(private val operands: List<Operand>) {
     fun size() = operands.size
 
     fun getSum(): Int {
-        return operands.sumOf { it.value }
+        return operands
+            .reduce { sum, operand -> sum + operand }.value
     }
 
     operator fun get(index: Int): Operand = operands[index]
 
     companion object {
         fun from(inputs: List<String>): Operands {
-            return Operands(inputs.map { Operand.from(it) })
+            return Operands(inputs.map { Operand.from(it.trim()) })
         }
     }
 }
--- src/test/kotlin/calculator/domain/OperandTest.kt
+++ src/test/kotlin/calculator/domain/OperandTest.kt
@@ -3,7 +3,7 @@ package calculator.domain
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.EmptySource
 import org.junit.jupiter.params.provider.ValueSource
 
 class OperandTest {
@@ -24,8 +24,8 @@ class OperandTest {
     }
 
     @ParameterizedTest
-    @NullAndEmptySource
-    fun `null이나 빈문자열을 입력하면 값이 0인 Operand가 생성된다`(input: String?) {
+    @EmptySource
+    fun `빈문자열을 입력하면 값이 0인 Operand가 생성된다`(input: String) {
         val operand = Operand.from(input)
 
         assertThat(operand.value).isEqualTo(0)
