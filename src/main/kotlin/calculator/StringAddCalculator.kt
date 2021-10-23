2694c5af5678aa2b6aade615832f3b055fcd79d6
thgus5614@naver.com <thgus5614@naver.com>
2021-10-23 15:26:48 +0900

    add: 테스트 코드 기반으로 문자 계산기 구현

---
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -0,0 +1,39 @@
+package calculator
+
+class StringAddCalculator {
+    fun add(text: String?): Int {
+        if (text.isNullOrEmpty()) return RETURN_NULL_OR_EMPTY_STRING
+        return convertTextToIntList(text).sum()
+    }
+
+    private fun convertTextToIntList(text: String): List<Int> {
+        val customList = splitCustom(text)
+        return when {
+            customList.isNullOrEmpty() -> split(text).map { validationInt(it) }
+            else -> customList.map { split(it).sumOf { validationInt(it) } }
+        }
+    }
+
+    private fun split(text: String): List<String> = text.split(DELIMITER_UNIT_ONE, DELIMITER_UNIT_SECOND)
+
+    private fun splitCustom(text: String): List<String>? {
+        val result = Regex("//(.)\n(.*)").find(text)
+        result?.let {
+            val customDelimiter = it.groupValues[1]
+            return it.groupValues[2].split(customDelimiter)
+        } ?: return null
+    }
+
+    private fun validationInt(text: String): Int {
+        val number = text.toIntOrNull()
+        if (number == null || number < 0) throw RuntimeException(EXCEPTION_NULL_OR_EMPTY)
+        return number
+    }
+
+    companion object {
+        const val DELIMITER_UNIT_ONE = ","
+        const val DELIMITER_UNIT_SECOND = ":"
+        const val RETURN_NULL_OR_EMPTY_STRING = 0
+        const val EXCEPTION_NULL_OR_EMPTY = "숫자가 아니거나 음수 입니다."
+    }
+}
