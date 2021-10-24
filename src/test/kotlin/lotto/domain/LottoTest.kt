2948bee03a0d9a99aa8763859e19b03a4415d59a
etff <wish2rich@gmail.com>
2021-10-24 12:52:47 +0900

    refactor: 로또 테스트의 중복을 제거하고 케이스를 추가하라

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -8,31 +8,31 @@ internal class LottoTest {
 
     @Test
     fun `로또는 6개 로또 번호를 가지고 있다`() {
-        val lottoNumber1 = LottoNumber.valueOf(1)
-        val lottoNumber2 = LottoNumber.valueOf(2)
-        val lottoNumber3 = LottoNumber.valueOf(3)
-        val lottoNumber4 = LottoNumber.valueOf(4)
-        val lottoNumber5 = LottoNumber.valueOf(5)
-        val lottoNumber6 = LottoNumber.valueOf(6)
-        val lottoNumbers = listOf(
-            lottoNumber1, lottoNumber2, lottoNumber3,
-            lottoNumber4, lottoNumber5, lottoNumber6
-        )
+        val lottoNumbers = (1..6).map { LottoNumber.valueOf(it) }
 
         val lotto = Lotto(lottoNumbers)
         assertThat(lotto.getLottoNumbers()).hasSize(6)
     }
 
     @Test
-    fun `로또는 로또갯수가 올바르지 않으면 예외를 던진다`() {
+    fun `로또는 로또 번호가 6개가 아니라면 예외를 던진다`() {
+        val lottoNumbers = (1..5).map { LottoNumber.valueOf(it) }
+
+        assertThatCode { Lotto(lottoNumbers) }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `로또는 로또번호가 중복되면 예외를 던진다`() {
         val lottoNumber1 = LottoNumber.valueOf(1)
-        val lottoNumber2 = LottoNumber.valueOf(2)
-        val lottoNumber3 = LottoNumber.valueOf(3)
-
+        val lottoNumber2 = LottoNumber.valueOf(1)
+        val lottoNumber3 = LottoNumber.valueOf(1)
+        val lottoNumber4 = LottoNumber.valueOf(2)
+        val lottoNumber5 = LottoNumber.valueOf(2)
+        val lottoNumber6 = LottoNumber.valueOf(2)
         val lottoNumbers = listOf(
-            lottoNumber1, lottoNumber2, lottoNumber3
+            lottoNumber1, lottoNumber2, lottoNumber3,
+            lottoNumber4, lottoNumber5, lottoNumber6
         )
-
         assertThatCode { Lotto(lottoNumbers) }.isInstanceOf(IllegalArgumentException::class.java)
     }
 
