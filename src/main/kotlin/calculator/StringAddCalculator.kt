1b1f2e84f82b1a7022de966496b4315536353a67
leesohyun13 <thgus5614@naver.com>
2021-10-27 00:26:20 +0900

    Step1 - 문자열 덧셈 계산기 (#174)
    * add: README.md 추가
    
    * add: 테스트 코드 추가
    
    * add: 테스트 코드 기반으로 문자 계산기 구현
    
    * improve: 커스텀 구분자를 위한 Regex()를 변수로 리펙토링
    
    * add: 테스트 케이스 추가
    
    * improve: 구조 리펙토링(문자열 분리 class 생성)

---
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -0,0 +1,19 @@
+package calculator
+
+class StringAddCalculator {
+    fun add(text: String?): Int {
+        if (text.isNullOrEmpty()) return RETURN_NULL_OR_EMPTY_STRING
+        return SplitCharacter().split(text).sumOf { validationInt(it) }
+    }
+
+    private fun validationInt(text: String): Int {
+        val number = text.toIntOrNull()
+        if (number == null || number < 0) throw RuntimeException(EXCEPTION_NULL_OR_EMPTY)
+        return number
+    }
+
+    companion object {
+        const val RETURN_NULL_OR_EMPTY_STRING = 0
+        const val EXCEPTION_NULL_OR_EMPTY = "숫자가 아니거나 음수 입니다."
+    }
+}
