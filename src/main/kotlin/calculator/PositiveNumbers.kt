f602afe1f13762003c9a2a6140c9fb1eac30b027
laco-dev <laco2951@gmail.com>
2021-10-27 07:46:21 +0900

    :sparkles: 분리된 숫자를 더할 수 있다.

--- src/main/kotlin/calculator/PositiveNumbers.kt
+++ src/main/kotlin/calculator/PositiveNumbers.kt
@@ -1,10 +1,12 @@
 package calculator
 
-class PositiveNumbers(numbers: List<Int>) {
+class PositiveNumbers(private val numbers: List<Int>) {
 
     init {
         numbers.forEach { number ->
             require(number >= 0)
         }
     }
+
+    fun sum(): Int = numbers.sum()
 }
--- src/test/kotlin/calculator/PositiveNumbersTest.kt
+++ src/test/kotlin/calculator/PositiveNumbersTest.kt
@@ -1,7 +1,9 @@
 package calculator
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class PositiveNumbersTest {
@@ -16,5 +18,20 @@ class PositiveNumbersTest {
     fun `분리된 숫자는 음수를 전달받으면 IllegalArgumentException 예외가 발생한다`(input: String) {
         val numbers = input.split(",").map { it.toInt() }
         assertThrows<IllegalArgumentException> { PositiveNumbers(numbers) }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "1,2,3:6",
+            "10,20,30:60"
+        ],
+        delimiter = ':'
+    )
+    fun `분리된 숫자를 더할 수 있다`(input: String, actual: String) {
+        val numbers = input.split(",").map { it.toInt() }
+        val expected = PositiveNumbers(numbers).sum()
+
+        Assertions.assertEquals(expected, actual.toInt())
     }
 }
