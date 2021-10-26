81ee693739a8e083f94be31872d55b42b4166be2
김우재 <kwj1270@naver.com>
2021-10-26 18:09:59 +0900

    test: 커스텀 구분자가 없는 문자열이 있으면 기본 구분자만 가진 객체를 생성한다

--- src/test/kotlin/domain/calculator/domain/separator/SeparatorsTest.kt
+++ src/test/kotlin/domain/calculator/domain/separator/SeparatorsTest.kt
@@ -1,7 +1,10 @@
 package domain.calculator.domain.separator
 
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class SeparatorsTest {
 
@@ -10,5 +13,14 @@ class SeparatorsTest {
         assertThatThrownBy { Separators.of(setOf()) }
             .isExactlyInstanceOf(RuntimeException::class.java)
             .hasMessage("Separators, 비어있는 컬렉션은 입력될 수 없습니다.")
+    }
+
+    @ParameterizedTest(name = "연산식: {0}")
+    @ValueSource(strings = ["1", "1,2", "1:2", "1,2:3"])
+    fun `커스텀 구분자가 없는 문자열이 있으면 기본 구분자만 가진 객체를 생성한다`(expression: String) {
+        val expected = Separators.of(setOf(Separator(","), Separator(":")))
+        val actual = Separators.of(expression)
+
+        assertThat(actual).isEqualTo(expected)
     }
 }
