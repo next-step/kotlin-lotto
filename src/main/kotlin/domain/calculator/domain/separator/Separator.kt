d983bf754424973df8fbb180c12549604ff9197e
김우재 <kwj1270@naver.com>
2021-10-26 17:48:26 +0900

    feat: Separator, 공백 문자열이 입력되었을 경우 예외를 발생한다

--- src/main/kotlin/domain/calculator/domain/separator/Separator.kt
+++ src/main/kotlin/domain/calculator/domain/separator/Separator.kt
@@ -3,6 +3,10 @@ package domain.calculator.domain.separator
 @JvmInline
 value class Separator(val separator: String) {
     init {
-        if (separator.isNullOrBlank()) throw RuntimeException("Separator, null 이거나 공백인 문자열은 입력할 수 없습니다.")
+        if (separator.isNullOrBlank()) throw RuntimeException(NULL_OR_BLANK_EXCEPTION_MESSAGE)
+    }
+
+    companion object {
+        private const val NULL_OR_BLANK_EXCEPTION_MESSAGE = "Separator, null 이거나 공백인 문자열은 입력할 수 없습니다."
     }
 }
