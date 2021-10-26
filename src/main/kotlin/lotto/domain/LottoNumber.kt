d0cf9f5a11c7c34e08f051e5eee4f2df9686c783
etff <wish2rich@gmail.com>
2021-10-27 01:51:07 +0900

    refactor: 로또 번호를 캐싱하도록 수정하라

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -2,6 +2,7 @@ package lotto.domain
 
 @JvmInline
 value class LottoNumber private constructor(val number: Int) : Comparable<LottoNumber> {
+
     init {
         require(number in LOTTO_MINIMUM_NUMBER..LOTTO_MAXIMUM_NUMBER) { throw IllegalArgumentException(NOT_NUMBER_RANGER) }
     }
@@ -18,9 +19,14 @@ value class LottoNumber private constructor(val number: Int) : Comparable<LottoN
         private const val NOT_NUMBER_RANGER = "로또의 범위가 아닙니다"
         private const val LOTTO_MINIMUM_NUMBER = 1
         private const val LOTTO_MAXIMUM_NUMBER = 45
+        private val CACHE = (LOTTO_MINIMUM_NUMBER..LOTTO_MAXIMUM_NUMBER).map { LottoNumber(it) }
 
         fun valueOf(value: Int): LottoNumber {
-            return LottoNumber(value)
+            return CACHE.find { it == LottoNumber(value) }?.let { it } ?: LottoNumber(value)
+        }
+
+        fun getLottoNumbers(): List<LottoNumber> {
+            return CACHE
         }
     }
 }
--- src/main/kotlin/lotto/domain/RandomNumberGenerator.kt
+++ src/main/kotlin/lotto/domain/RandomNumberGenerator.kt
@@ -2,18 +2,12 @@ package lotto.domain
 
 class RandomNumberGenerator : LottoNumberGenerator {
     override fun generateNumbers(): List<LottoNumber> {
-        val numbers = initializeLottoNumbers()
+        val numbers = LottoNumber.getLottoNumbers()
         val shuffled = numbers.shuffled()
         return shuffled.subList(MINIMUM_LOTTO_SIZE, MAXIMUM_LOTTO_SIZE)
     }
 
-    private fun initializeLottoNumbers(): List<LottoNumber> {
-        return (MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER).map { LottoNumber.valueOf(it) }
-    }
-
     companion object {
-        private const val MINIMUM_LOTTO_NUMBER = 1
-        private const val MAXIMUM_LOTTO_NUMBER = 45
         private const val MINIMUM_LOTTO_SIZE = 0
         private const val MAXIMUM_LOTTO_SIZE = 6
     }
