871b2a07f76c8b34588faee49f1644a757ae26c1
김우재 <kwj1270@naver.com>
2021-10-28 02:30:39 +0900

    test: Calculator, 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다

--- src/main/kotlin/domain/calculator/domain/separator/Separators.kt
+++ src/main/kotlin/domain/calculator/domain/separator/Separators.kt
@@ -7,17 +7,19 @@ import domain.calculator.strategy.RegexStrategy
 
 @JvmInline
 value class Separators private constructor(private val _separators: Set<Separator>) {
-    val separators: String
-        get() = _separators
-            .map(Separator::separator)
-            .joinToString("|")
-
     init {
         if (_separators.isEmpty()) throw RuntimeException(EMPTY_EXCEPTION_MESSAGE)
     }
 
+    val separators: String
+        get() = _separators
+            .map(Separator::separator)
+            .joinToString(REGEX_SEPARATOR)
+
     companion object {
         private const val EMPTY_EXCEPTION_MESSAGE = "Separators, 비어있는 컬렉션은 입력될 수 없습니다."
+        private const val REGEX_SEPARATOR = "|"
+
         fun of(separators: Set<Separator>): Separators = Separators(separators.toSet())
         fun of(expression: String, regexStrategy: RegexStrategy): Separators = of(Expression(expression, regexStrategy))
         fun of(expression: Expression): Separators =
@@ -26,15 +28,15 @@ value class Separators private constructor(private val _separators: Set<Separato
                 false -> Separators(defaultSeparators())
             }
 
+        private fun customSeparators(expression: Expression): Set<Separator> =
+            setOf(
+                Separator(expression.customSeparatorExpression())
+            )
+
         private fun defaultSeparators(): Set<Separator> =
             setOf(
                 Separator(DEFAULT_SEPARATOR_COMMA),
                 Separator(DEFAULT_SEPARATOR_COLON)
-            )
-
-        private fun customSeparators(expression: Expression): Set<Separator> =
-            setOf(
-                Separator(expression.customSeparatorExpression())
             )
     }
 }
