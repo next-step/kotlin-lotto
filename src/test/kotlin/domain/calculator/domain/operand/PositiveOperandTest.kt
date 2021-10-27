3a43fb56a4c46af225cfb5f71e125097a7ff854c
김우재 <kwj1270@naver.com>
2021-10-28 02:22:20 +0900

    test: PositiveOperand, 숫자로 변환될 수 없는 문자열은 객체를 생성할 수 없다

--- src/test/kotlin/domain/calculator/domain/operand/PositiveOperandTest.kt
+++ src/test/kotlin/domain/calculator/domain/operand/PositiveOperandTest.kt
@@ -28,6 +28,12 @@ class PositiveOperandTest {
     }
 
     @ParameterizedTest(name = "연산식: {0}")
+    @ValueSource(strings = ["a", "일", "마이너스일"])
+    fun `숫자로 변환될 수 없는 문자열은 객체를 생성할 수 없다`(operandString: String) {
+        assertThrows<RuntimeException> { PositiveOperand(operandString) }
+    }
+
+    @ParameterizedTest(name = "연산식: {0}")
     @ValueSource(strings = ["0", "1", "100", Integer.MAX_VALUE.toString()])
     fun `정수값을 반환한다`(operandString: String) {
         val positiveOperand = PositiveOperand(operandString)
