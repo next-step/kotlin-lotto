0c32bbbb50928e1d27dc321a4c1ee465e696817a
김우재 <kwj1270@naver.com>
2021-10-28 01:58:24 +0900

    test: PositiveOperand, 정수값을 반환한다

--- src/test/kotlin/domain/calculator/domain/operand/PositiveOperandTest.kt
+++ src/test/kotlin/domain/calculator/domain/operand/PositiveOperandTest.kt
@@ -10,7 +10,7 @@ import org.junit.jupiter.params.provider.ValueSource
 @DisplayName("정수_피연산자(PositiveOperand)")
 class PositiveOperandTest {
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "연산식: {0}")
     @ValueSource(strings = ["0", "1", "100", Integer.MAX_VALUE.toString()])
     fun `0 이상의 정수값을 이용해 객체를 생성할 수 있다`(operandString: String) {
         val positiveOperand = PositiveOperand(operandString)
@@ -21,9 +21,17 @@ class PositiveOperandTest {
         )
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "연산식: {0}")
     @ValueSource(strings = ["-1", "-100", Integer.MIN_VALUE.toString()])
     fun `음수값은 객체를 생성할 수 없다`(operandString: String) {
         assertThrows<RuntimeException> { PositiveOperand(operandString) }
+    }
+
+    @ParameterizedTest(name = "연산식: {0}")
+    @ValueSource(strings = ["0", "1", "100", Integer.MAX_VALUE.toString()])
+    fun `정수값을 반환한다`(operandString: String) {
+        val positiveOperand = PositiveOperand(operandString)
+
+        assertThat(positiveOperand.operand).isEqualTo(operandString.toInt())
     }
 }
