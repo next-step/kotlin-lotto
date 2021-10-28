4469cc8cd52d70b3f71e58332095332d20344a32
laco-dev <laco2951@gmail.com>
2021-10-28 22:06:00 +0900

    :recycle: LottoRank에서 match를 비교해서 값을 찾도록 개선

--- src/main/kotlin/lotto/model/LottoRank.kt
+++ src/main/kotlin/lotto/model/LottoRank.kt
@@ -1,11 +1,22 @@
 package lotto.model
 
+private const val RANK_MATCH_FIRST = 6
+private const val RANK_MATCH_SECOND = 5
+private const val RANK_MATCH_THIRD = 4
+private const val RANK_MATCH_FOURTH = 3
+private const val RANK_MATCH_FIFTH = 2
+
 enum class LottoRank(
-    val winnings: Int
+    val winnings: Int,
+    private val match: Int
 ) {
-    First(2_000_000_000),
-    Second(1_500_000),
-    Third(50_000),
-    Fourth(10_000),
-    Fifth(5_000)
+    First(2_000_000_000, RANK_MATCH_FIRST),
+    Second(1_500_000, RANK_MATCH_SECOND),
+    Third(50_000, RANK_MATCH_THIRD),
+    Fourth(10_000, RANK_MATCH_FOURTH),
+    Fifth(5_000, RANK_MATCH_FIFTH);
+
+    companion object {
+        fun find(match: Int): LottoRank? = values().find { it.match == match }
+    }
 }
--- src/main/kotlin/lotto/model/LottoResult.kt
+++ src/main/kotlin/lotto/model/LottoResult.kt
@@ -15,14 +15,7 @@ class LottoResult(
     ): Map<LottoRank, Int> {
         val winnersByRank = mutableMapOf<LottoRank, Int>()
         lottoList.forEach { lotto ->
-            val rank: LottoRank? = when (lotto.match(winLottoNumbers)) {
-                RANK_MATCH_FIRST -> LottoRank.First
-                RANK_MATCH_SECOND -> LottoRank.Second
-                RANK_MATCH_THIRD -> LottoRank.Third
-                RANK_MATCH_FOURTH -> LottoRank.Fourth
-                RANK_MATCH_FIFTH -> LottoRank.Fifth
-                else -> null
-            }
+            val rank = LottoRank.find(lotto.match(winLottoNumbers))
             if (rank != null) {
                 winnersByRank[rank] = winnersByRank.getOrDefault(rank, 0) + 1
             }
@@ -41,12 +34,4 @@ class LottoResult(
     }
 
     fun winners(rank: LottoRank): Int = winnersByRank.getOrDefault(rank, 0)
-
-    companion object {
-        private const val RANK_MATCH_FIRST = 6
-        private const val RANK_MATCH_SECOND = 5
-        private const val RANK_MATCH_THIRD = 4
-        private const val RANK_MATCH_FOURTH = 3
-        private const val RANK_MATCH_FIFTH = 2
-    }
 }
