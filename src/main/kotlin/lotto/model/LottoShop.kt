e8ff8181114054f1da0dd297231ede636cacc976
laco-dev <laco2951@gmail.com>
2021-10-28 19:12:54 +0900

    :sparkles: 구매한 금액 만큼의 로또가 발급되어야 한다.

--- src/main/kotlin/lotto/model/LottoShop.kt
+++ src/main/kotlin/lotto/model/LottoShop.kt
@@ -2,7 +2,15 @@ package lotto.model
 
 class LottoShop {
 
-    fun buyLotto(amount: Int) {
+    fun buyLotto(amount: Int): List<Lotto> {
         require(amount >= 0)
+
+        val size = amount / LOTTO_PRICE
+        val machine = LottoMachine()
+        return machine.createLotto(size)
+    }
+
+    companion object {
+        private const val LOTTO_PRICE = 1_000
     }
 }
--- src/test/kotlin/lotto/model/LottoShopTest.kt
+++ src/test/kotlin/lotto/model/LottoShopTest.kt
@@ -1,9 +1,11 @@
 package lotto.model
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class LottoShopTest {
@@ -22,5 +24,21 @@ class LottoShopTest {
     )
     fun lottoShopNegative(price: Int) {
         assertThrows<RuntimeException> { shop.buyLotto(price) }
+    }
+
+    @DisplayName("구매한 금액 만큼의 로또가 발급되어야 한다.")
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "14000|14",
+            "0|0",
+            "37500|37"
+        ],
+        delimiter = '|'
+    )
+    fun lottoShopCount(amount: String, count: String) {
+        val expected = count.toInt()
+        val actual = shop.buyLotto(amount.toInt()).size
+        Assertions.assertEquals(expected, actual)
     }
 }
