3328523672f5f250a2b2e893001d1f61ff3a0076
flamme <flamme1004@gmail.com>
2021-10-29 01:30:23 +0900

    refactor(stringAddCalculator) : convertTexts 코드 Flow 변경

--- src/main/kotlin/calculator/utils/StringUtil.kt
+++ src/main/kotlin/calculator/utils/StringUtil.kt
@@ -4,11 +4,14 @@ object StringUtil {
     private val BASIC_PATTERN = Regex(",|:")
     private val CUSTOM_PATTERN = Regex("//(.)\n(.*)")
 
-    fun convertTexts(text: String): List<String> =
-        CUSTOM_PATTERN.find(text)?.let {
-            splitTextFromCustomPattern(it)
-        } ?: text.split(BASIC_PATTERN)
-
+    fun convertTexts(text: String): List<String> {
+        val matchResult = CUSTOM_PATTERN.find(text)
+        return if (matchResult != null) {
+            splitTextFromCustomPattern(matchResult)
+        } else {
+            text.split(BASIC_PATTERN)
+        }
+    }
     private fun splitTextFromCustomPattern(it: MatchResult): List<String> {
         val (customDelimiter, matchingText) = it.destructured
         return matchingText.split(customDelimiter)
