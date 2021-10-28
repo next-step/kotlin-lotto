eb55ceef885b7a881ebcd7e25239bfbea19e341c
etff <wish2rich@gmail.com>
2021-10-29 00:58:14 +0900

    refactor: 사용하지 않는 값을 언더스코어로 수정하라

--- src/main/kotlin/lotto/domain/Reward.kt
+++ src/main/kotlin/lotto/domain/Reward.kt
@@ -5,12 +5,12 @@ enum class Reward(
     val amount: Int,
     val isMatch: (match: Int, isBonus: Boolean) -> Boolean
 ) {
-    FIRST(6, 2_000_000_000, { match, isBonus -> 6 == match }),
+    FIRST(6, 2_000_000_000, { match, _ -> 6 == match }),
     SECOND(5, 30_000_000, { match, isBonus -> 5 == match && isBonus }),
     THIRD(5, 1_500_000, { match, isBonus -> 5 == match && !isBonus }),
-    FOURTH(4, 50_000, { match, isBonus -> 4 == match }),
-    FIFTH(3, 5_000, { match, isBonus -> 3 == match }),
-    NONE(0, 0, { match, isBonus -> false });
+    FOURTH(4, 50_000, { match, _ -> 4 == match }),
+    FIFTH(3, 5_000, { match, _ -> 3 == match }),
+    NONE(0, 0, { _, _ -> false });
 
     fun hasNoMatch(): Boolean {
         return NONE == this
