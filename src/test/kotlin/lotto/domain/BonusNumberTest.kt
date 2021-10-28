63c39349e98343bebac08e192a53affb48aee62c
ninjasul <ninjasul@gmail.com>
2021-10-29 07:17:56 +0900

    style: reformat codes

--- src/test/kotlin/lotto/domain/BonusNumberTest.kt
+++ src/test/kotlin/lotto/domain/BonusNumberTest.kt
@@ -2,7 +2,6 @@ package lotto.domain
 
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
