eb470a0bd83a1eb348dd53eaf73b8af032368a00
etff <wish2rich@gmail.com>
2021-10-28 20:50:15 +0900

    refactor: 입력의 NULL 값을 수정하라

--- src/main/kotlin/lotto/views/InputView.kt
+++ src/main/kotlin/lotto/views/InputView.kt
@@ -2,25 +2,22 @@ package lotto.views
 
 object InputView {
 
-    fun askWinningLotto(): String {
+    tailrec fun askWinningLotto(): String {
         println(WINNING_LOTTO_QUESTION)
-        return readLine() ?: throw IllegalArgumentException(NULL_NOT_ALLOWED)
+        return readLine() ?: askWinningLotto()
     }
 
-    fun askLottoBudget(): Int {
+    tailrec fun askLottoBudget(): Int {
         println(LOTTO_BUDGET_QUESTION)
-        return readLine()?.toInt()
-            ?: throw IllegalArgumentException(NULL_NOT_ALLOWED)
+        return readLine()?.toIntOrNull() ?: askLottoBudget()
     }
 
-    fun askBonusNumber(): Int {
+    tailrec fun askBonusNumber(): Int {
         println(BONUS_NUMBER_QUESTION)
-        return readLine()?.toInt()
-            ?: throw IllegalArgumentException(NULL_NOT_ALLOWED)
+        return readLine()?.toIntOrNull() ?: askBonusNumber()
     }
 
     private const val LOTTO_BUDGET_QUESTION = "구입금액을 입력해 주세요."
     private const val WINNING_LOTTO_QUESTION = "지난 주 당첨 번호를 입력해 주세요."
     private const val BONUS_NUMBER_QUESTION = "보너스 볼을 입력해 주세요."
-    private const val NULL_NOT_ALLOWED = "입력값은 null을 허용하지 않습니다."
 }
