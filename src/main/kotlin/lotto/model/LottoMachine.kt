4ddf733a5f748dc1b283d6934da582f5cb40b2b2
laco-dev <laco2951@gmail.com>
2021-10-28 18:58:48 +0900

    :sparkles: 발급 매수만큼의 로또가 발급되어야 한다.

--- src/main/kotlin/lotto/model/LottoMachine.kt
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -3,7 +3,7 @@ package lotto.model
 class LottoMachine {
 
     fun createLotto(size: Int): List<Lotto> {
-        require(size > 0)
+        require(size >= 0)
         return List(size) { Lotto(LottoNumbers.generateRandomNumbers()) }
     }
 }
--- src/test/kotlin/lotto/model/LottoMachineTest.kt
+++ src/test/kotlin/lotto/model/LottoMachineTest.kt
@@ -1,9 +1,12 @@
 package lotto.model
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class LottoMachineTest {
 
@@ -12,6 +15,14 @@ class LottoMachineTest {
     @BeforeEach
     fun setup() {
         machine = LottoMachine()
+    }
+
+    @DisplayName("발급 매수만큼의 로또가 발급되어야 한다.")
+    @ValueSource(ints = [0, 5, 10, 20, 6, 14])
+    @ParameterizedTest
+    fun lottoCount(size: Int) {
+        val actual = machine.createLotto(size).size
+        assertEquals(size, actual)
     }
 
     @DisplayName("발급 매수가 0보다 작은 경우 RuntimeException 예외가 발생해야 한다.")
