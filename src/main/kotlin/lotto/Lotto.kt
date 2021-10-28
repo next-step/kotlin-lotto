2384908904885ce01bae3ecefb4804435fba7bcb
flamme <flamme1004@gmail.com>
2021-10-29 01:45:57 +0900

    feat(lotto) : 로또는 6개의 숫자로 이루어져야 한다.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,6 +1,9 @@
 package lotto
 
-data class Lotto(val price: Int = LOTTO_PRICE) {
+data class Lotto(val lottoNumbers: List<Int>, val price: Int = LOTTO_PRICE) {
+    init {
+        require(lottoNumbers.size == 6) { "숫자가 6개가 들어와야 합니다." }
+    }
 
     companion object {
         private const val LOTTO_PRICE = 1000
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -7,9 +7,27 @@ class LottoTest {
     @Test
     fun `로또는 개당 1000원이다`() {
         // given
-        val lotto = Lotto()
+        val lotto = Lotto(lottoNumbers = listOf(1, 2, 3, 4, 5, 6))
 
         // then
         assertThat(lotto.price).isEqualTo(1000)
+    }
+
+    @Test
+    fun `로또는 6개 숫자로 이루어져 있다`() {
+        // given
+        val lotto = Lotto(lottoNumbers = listOf(1, 2, 3, 4, 5, 6))
+
+        // then
+        assertThat(lotto.lottoNumbers.size).isEqualTo(6)
+    }
+
+    @Test
+    fun `로또가 6개의 숫자로 이루어 지지 않았다면 에러`() {
+        // given
+        val actual = runCatching { Lotto(lottoNumbers = listOf(1, 2, 3, 4, 5, 6, 7)) }.exceptionOrNull()
+
+        // then
+        assertThat(actual).hasMessageContaining("숫자가 6개가 들어와야 합니다.")
     }
 }
