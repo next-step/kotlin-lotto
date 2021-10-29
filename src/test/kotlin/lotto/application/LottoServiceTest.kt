b80b14bab9aa0c4d4e9f4c39ce3b76acf628ccec
LimHanGyeol <hang19663@gmail.com>
2021-10-29 23:27:15 +0900

    test: n개의 로또 당첨 결과 수집 테스트 구현

--- src/test/kotlin/lotto/application/LottoServiceTest.kt
+++ src/test/kotlin/lotto/application/LottoServiceTest.kt
@@ -1,8 +1,11 @@
 package lotto.application
 
 import lotto.domain.Lotto
+import lotto.domain.Rank
+import lotto.domain.WinningNumber
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -21,5 +24,27 @@ class LottoServiceTest {
 
         // Assert
         assertThat(lottos).hasSize(expected)
+    }
+
+    @Test
+    @DisplayName("n개의 로또들의 당첨 결과를 수집할 수 있다")
+    fun `sut returns match count when lotto numbers contains winning number`() {
+        // Arrange
+        val lottos = listOf(
+            Lotto(listOf(3, 4, 5, 6, 7, 27)),
+            Lotto(listOf(5, 7, 13, 27, 31, 45))
+        )
+
+        val winningNumber = WinningNumber.from(
+            listOf("3", "5", "7", "13", "31", "45")
+        )
+
+        // Act
+        val sut = LottoService(lottos)
+        val result: Map<Rank, Int> = sut.matchWinningNumber(lottos, winningNumber)
+
+        // Assert
+        assertThat(result[Rank.SECOND]).isEqualTo(1)
+        assertThat(result[Rank.FOURTH]).isEqualTo(1)
     }
 }
