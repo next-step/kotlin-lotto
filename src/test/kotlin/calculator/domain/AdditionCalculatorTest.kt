6774d2aad17983e648f70baa923dae04566830ec
ssibongee <ssibongee@gmail.com>
2021-10-30 00:48:07 +0900

    test: 덧셈 계산기 (AdditionCalculator) 테스트 작성

---
+++ src/test/kotlin/calculator/domain/AdditionCalculatorTest.kt
@@ -0,0 +1,20 @@
+package calculator.domain
+
+import fixture.OperandsFixture
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class AdditionCalculatorTest {
+
+    @Test
+    fun `덧셈 연산을 수행할 수 있다`() {
+        // given
+        val operands = OperandsFixture.OPERANDS_SUM_OF_SIX
+
+        // when
+        val answer = AdditionCalculator().calculate(operands)
+
+        // then
+        Assertions.assertThat(answer).isEqualTo(6)
+    }
+}
