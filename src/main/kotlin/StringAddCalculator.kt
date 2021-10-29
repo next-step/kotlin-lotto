fa4faaa9b7510ec92d093d391e7963847f7354da
B2110353-F5\BGF <juseyo86@naver.com>
2021-10-29 13:46:01 +0900

    feat : 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다

--- src/main/kotlin/StringAddCalculator.kt
+++ src/main/kotlin/StringAddCalculator.kt
@@ -1,7 +1,16 @@
 class StringAddCalculator(private val input: String?) {
     fun calculate(): Int {
-        if (input.isNullOrEmpty()) return 0
-        if (input.isNullOrBlank()) return 0
-        return 0
+        if (input.isNullOrEmpty()) return NUMBER_ZERO
+        if (input.isNullOrBlank()) return NUMBER_ZERO
+        if (input.length == INPUT_LENGTH_ONE && isNumeric(input)) return input.toInt()
+        return NUMBER_ZERO
+    }
+
+    private fun isNumeric(str: String): Boolean = str
+        .all { it in '0'..'9' }
+
+    companion object {
+        private const val NUMBER_ZERO = 0
+        private const val INPUT_LENGTH_ONE = 1
     }
 }
--- src/test/kotlin/StringAddCalculatorTest.kt
+++ src/test/kotlin/StringAddCalculatorTest.kt
@@ -2,6 +2,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EmptySource
 import org.junit.jupiter.params.provider.NullSource
+import org.junit.jupiter.params.provider.ValueSource
 
 class StringAddCalculatorTest {
 
@@ -15,5 +16,15 @@ class StringAddCalculatorTest {
         val result: Int = stringAddCalculator.calculate()
         // then
         assertThat(0).isEqualTo(result)
+    }
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "2", "3"])
+    fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(input: String) {
+        // given
+        val stringAddCalculator = StringAddCalculator(input)
+        // when
+        val result: Int = stringAddCalculator.calculate()
+        // then
+        assertThat(result).isEqualTo(input.toInt())
     }
 }
