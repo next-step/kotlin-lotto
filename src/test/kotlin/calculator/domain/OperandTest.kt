f79c796b2f48d2c0957317f4c2fccf027f30347f
ssibongee <ssibongee@gmail.com>
2021-10-30 00:42:52 +0900

    test: 피연산자 (Operand) 테스트 작성

---
+++ src/test/kotlin/calculator/domain/OperandTest.kt
@@ -0,0 +1,35 @@
+package calculator.domain
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
+
+class OperandTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    fun `빈 문자열은 또는 null 값이 입력되면 0을 반환한다`(value: String?) {
+        assertThat(Operand.of(value)).isEqualTo(Operand.ZERO)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["k", "o", "t", "l", "i", "n"])
+    fun `숫자가 아닌 문자는 피연산자가 될 수 없다`(value: String) {
+        assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy { Operand.of(value) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["-1", "-10", "-100", "-20"])
+    fun `음수인 정수는 피연산자가 될 수 없다`(value: String) {
+        assertThatExceptionOfType(RuntimeException::class.java)
+            .isThrownBy { Operand.of(value) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["0", "1", "100", "20"])
+    fun `양수인 정수는 피연산자가 될 수 있다`(value: String) {
+        assertThat(Operand.of(value).value).isEqualTo(value.toInt())
+    }
+}
