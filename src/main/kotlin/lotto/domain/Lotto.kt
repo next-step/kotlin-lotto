32ae142ea44f73731e3425906c9103c5650972b9
LimHanGyeol <hang19663@gmail.com>
2021-10-29 21:06:26 +0900

    feat: 당첨 번호 확인 기능 구현

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,4 +3,10 @@ package lotto.domain
 @JvmInline
 value class Lotto(
     val lottoNumbers: List<Int>,
-)
+) {
+    fun matchWinningNumber(winningNumbers: List<Int>): Int {
+        return winningNumbers.count { containsLottoNumbers(it) }
+    }
+
+    private fun containsLottoNumbers(winningNumber: Int): Boolean = lottoNumbers.contains(winningNumber)
+}
