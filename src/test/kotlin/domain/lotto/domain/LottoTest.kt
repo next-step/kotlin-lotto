a34d0122cdf8223ecbf7785ff48dcec89c35f909
김우재 <kwj1270@naver.com>
2021-10-30 22:17:05 +0900

    test: 셔플 전략을 통해 로또를 생성한다

--- src/test/kotlin/domain/lotto/domain/LottoTest.kt
+++ src/test/kotlin/domain/lotto/domain/LottoTest.kt
@@ -1,15 +1,20 @@
 package domain.lotto.domain
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 @DisplayName("로또(Lotto)")
 class LottoTest {
 
     @Test
-    fun test() {
+    fun `셔플 전략을 통해 로또를 생성한다`() {
+        val lotto: Lotto = Lotto.of(LottoShuffleStrategy)
 
+        assertAll(
+            { assertThat(lotto).isNotNull },
+            { assertThat(lotto).isExactlyInstanceOf(Lotto::class.java) },
+        )
     }
-
 }
