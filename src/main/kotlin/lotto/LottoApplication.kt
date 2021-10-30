acd820e1070ad5b4b7c27e40455a920d230b767a
thgus5614@naver.com <thgus5614@naver.com>
2021-10-30 23:43:17 +0900

    refactor: 사용하지 않는 코드 삭제

--- src/main/kotlin/lotto/LottoApplication.kt
+++ src/main/kotlin/lotto/LottoApplication.kt
@@ -1,26 +1,7 @@
 package lotto
 
-import lotto.model.GenerateLotto
 import lotto.model.LottoController
-import lotto.model.Lottos
-import lotto.model.Price
-import lotto.view.InputView
-import lotto.view.OutputView
-
-private val inputView = InputView()
-private val outputView = OutputView()
 
 fun main() {
     LottoController.runLottoGame()
-    // val price = inputView.inputLottoPrice()
-    // val lottoPrice = Price(price)
-    // outputView.resultLottoCount(lottoPrice.lottoCount)
-    //
-    // val list = GenerateLotto(lottoPrice).generateLottoList()
-    // outputView.printNumber(list)
-    //
-    // val winNumberList = inputView.inputLastLottoWinNumber()
-    // val lottos = Lottos.inputWinNumber(lottoPrice, list, winNumberList)
-    // val result = lottos.compareLottoList()
-    // outputView.printWinStatistic(result)
 }
