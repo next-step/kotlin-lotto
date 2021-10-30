5987a43f7ef9e4828ee7e83d3c7ac3683c95e9b2
김우재 <kwj1270@naver.com>
2021-10-31 03:26:40 +0900

    test: 당첨 결과를 반환한다

--- src/test/kotlin/domain/lotto/domain/MatchResultTest.kt
+++ src/test/kotlin/domain/lotto/domain/MatchResultTest.kt
@@ -7,13 +7,13 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.assertThrows
 
-@DisplayName("로또결과(MatchResult)")
+@DisplayName("로또 당첨 결과(MatchResult)")
 class MatchResultTest {
 
     @Test
-    fun `매칭 결과를 기반으로 생성할 수 있다`() {
+    fun `당첨 결과를 기반으로 생성할 수 있다`() {
         val matchResultMap = MatchBoard.values().associateWith { 0 }
-        val matchResult: MatchResult = MatchResult.of(matchResultMap)
+        val matchResult = MatchResult.of(matchResultMap)
 
         assertAll(
             { assertThat(matchResult).isNotNull },
@@ -22,9 +22,18 @@ class MatchResultTest {
     }
 
     @Test
-    fun `비어있는 매칭 결과를 입력하면 예외를 발생시킨다`() {
+    fun `비어있는 당첨 결과를 입력하면 예외를 발생시킨다`() {
         val exception = assertThrows<EmptyMatchResultMapException> { MatchResult.of(mapOf()) }
 
         assertThat(exception.message).isEqualTo("로또 결과가 비어있습니다.")
+    }
+
+    @Test
+    fun `당첨 결과를 반환한다`() {
+        val expected = MatchBoard.values().associateWith { 0 }
+        val matchResult = MatchResult.of(expected)
+        val actual = matchResult.matchResult
+
+        assertThat(actual).isEqualTo(expected)
     }
 }
