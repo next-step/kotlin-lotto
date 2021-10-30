e1c2128b060b3f04d88c00a4c6272a367c1faeb5
thgus5614@naver.com <thgus5614@naver.com>
2021-10-30 23:39:29 +0900

    add: 로또 번호 비교 로직 테스트 코드 추가

---
+++ src/test/kotlin/lotto/model/LottosTest.kt
@@ -0,0 +1,36 @@
+package lotto.model
+
+import lotto.model.Lottos.Companion.EXCEPTION_INPUT_NUMBER_NULL
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class LottosTest {
+    @Test
+    @DisplayName("로또 결과가 옳바르게 출력된 경우")
+    fun `correct lotto result`() {
+        // given
+        val price = Price(1000)
+        val purchasedList = listOf(Lotto(listOf(LottoNumber(10), LottoNumber(1), LottoNumber(13), LottoNumber(23), LottoNumber(33), LottoNumber(43))))
+        val winNumber = "10,1,13,23,33,43"
+
+        // when
+        val lottos = Lottos.inputWinNumber(price, purchasedList, winNumber)
+
+        // then
+        Assertions.assertThat(lottos.compareLottoList()).isEqualTo(LottoStatisticFormat(price, hashMapOf(LottoRank.FIRST to 1)))
+    }
+
+    @Test
+    @DisplayName("지난주 당첨 번호 포멧이 옳바르지 않는 경우")
+    fun `incorrect win number list format`() {
+        // given
+        val price = Price(1000)
+        val purchasedList = listOf(Lotto(listOf(LottoNumber(10), LottoNumber(1), LottoNumber(13), LottoNumber(23), LottoNumber(33), LottoNumber(43))))
+        val winNumber = null
+
+        Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy { Lottos.inputWinNumber(price, purchasedList, winNumber) }
+            .withMessage(EXCEPTION_INPUT_NUMBER_NULL)
+    }
+}
