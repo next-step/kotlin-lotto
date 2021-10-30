ba8342ba744048805293ec2e6d743ce7a248cdcc
thgus5614@naver.com <thgus5614@naver.com>
2021-10-30 23:41:06 +0900

    improve: 문자열 계산기 테스트 코드 수정

--- src/test/kotlin/calculator/StringAddCalculatorTest.kt
+++ src/test/kotlin/calculator/StringAddCalculatorTest.kt
@@ -22,35 +22,45 @@ class StringAddCalculatorTest {
     @ParameterizedTest
     @NullAndEmptySource
     fun emptyOrNull(text: String?) {
-        assertThat(calculator.add(text)).isZero
+        val add = calculator.add(text)
+
+        assertThat(add).isZero
     }
 
     @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = ["1"])
     fun oneNumber(text: String) {
-        assertThat(calculator.add(text)).isSameAs(Integer.parseInt(text))
+        val add = calculator.add(text)
+
+        assertThat(add).isSameAs(Integer.parseInt(text))
     }
 
     @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = ["1,2"])
     fun twoNumbers(text: String) {
-        assertThat(calculator.add(text)).isSameAs(3)
+        val add = calculator.add(text)
+
+        assertThat(add).isSameAs(3)
     }
 
     @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = ["1,2:3"])
     fun colons(text: String) {
-        assertThat(calculator.add(text)).isSameAs(6)
+        val add = calculator.add(text)
+
+        assertThat(add).isSameAs(6)
     }
 
     @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = ["//;\n1;2;3", "//=\n1=2=3"])
     fun customDelimiter(text: String) {
-        assertThat(calculator.add(text)).isSameAs(6)
+        val add = calculator.add(text)
+
+        assertThat(add).isSameAs(6)
     }
 
     @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
@@ -65,8 +75,10 @@ class StringAddCalculatorTest {
     @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.")
     @Test
     fun negative() {
+        val negativeNumber = "-1"
+
         assertThatExceptionOfType(RuntimeException::class.java)
-            .isThrownBy { calculator.add("-1") }
+            .isThrownBy { calculator.add(negativeNumber) }
             .withMessage(EXCEPTION_NULL_OR_EMPTY)
     }
 }
