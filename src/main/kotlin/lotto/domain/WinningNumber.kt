a9582bdb8a38d74afd070c886609fb134635298a
sangyongchoi <csy7792@naver.com>
2021-10-30 21:46:36 +0900

    step2 modify code review content (remove magic number)

--- src/main/kotlin/lotto/domain/WinningNumber.kt
+++ src/main/kotlin/lotto/domain/WinningNumber.kt
@@ -4,10 +4,14 @@ class WinningNumber(
     private val numbers: List<Int>
 ) {
     init {
-        require(numbers.size == 6) { "당첨번호는 6개여야 합니다." }
+        require(numbers.size == WINNING_NUMBER_SIZE) { "당첨번호는 6개여야 합니다." }
     }
 
     fun contain(number: Int): Boolean {
         return numbers.contains(number)
+    }
+
+    companion object {
+        private const val WINNING_NUMBER_SIZE = 6
     }
 }
