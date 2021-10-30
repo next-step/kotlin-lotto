f9f87c8c45871d52545c4c7a7b2ef73c12393519
김우재 <kwj1270@naver.com>
2021-10-30 17:35:13 +0900

    test: 현재 소유한 값을 기준으로 사고자 하는 물품의 값을 입력하면 구매 가능한 개수를 반환한다

--- src/test/kotlin/domain/lotto/domain/MoneyTest.kt
+++ src/test/kotlin/domain/lotto/domain/MoneyTest.kt
@@ -29,4 +29,14 @@ class MoneyTest {
 
         assertThat(exception.message).isEqualTo("%s는 Money 의 범위를 벗어난 값입니다.".format(moneyInt))
     }
+
+    @ParameterizedTest(name = "입력값 : {0}")
+    @ValueSource(ints = [0, 1_000, 10_000, 99_999, 100_0001, Integer.MAX_VALUE])
+    fun `현재 소유한 값을 기준으로 사고자 하는 물품의 값을 입력하면 구매 가능한 개수를 반환한다`(moneyInt: Int) {
+        val money = Money(moneyInt)
+        val productPrice = 1000
+        val numberOfPurchases = money.numberOfPurchases(productPrice)
+
+        assertThat(numberOfPurchases).isEqualTo(Math.floorDiv(moneyInt, 1000))
+    }
 }
