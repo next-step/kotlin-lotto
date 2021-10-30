ccbb116d370359266d2d4fff14670cbbb4cd69b5
ninjasul <ninjasul@gmail.com>
2021-10-31 08:10:55 +0900

    refactor: resultStatistics, profitRate 생성 메소드를 멤버로 변경

--- src/main/kotlin/lotto/domain/LottoResult.kt
+++ src/main/kotlin/lotto/domain/LottoResult.kt
@@ -8,26 +8,24 @@ data class LottoResult(
     private val winningInfo: WinningInfo,
     private val purchaseAmount: LottoPurchaseAmount
 ) {
-    val resultStatistics: Map<LottoResultRank, Int> = getResultStatistics(lottoPackages, winningInfo)
-    val profitRate: BigDecimal = getTotalProfitRate(lottoPackages, winningInfo, purchaseAmount)
+    val resultStatistics: Map<LottoResultRank, Int> = buildResultStatistics(lottoPackages, winningInfo)
+    val profitRate: BigDecimal = buildTotalProfitRate(lottoPackages, winningInfo, purchaseAmount)
 
-    companion object {
-        private fun getResultStatistics(lottoPackages: List<LottoNumberPackage>, winningInfo: WinningInfo): Map<LottoResultRank, Int> {
-            return lottoPackages
-                .groupingBy { LottoResultRank.getRank(it.getRankKey(winningInfo)) }
-                .eachCount()
-        }
+    private fun buildResultStatistics(lottoPackages: List<LottoNumberPackage>, winningInfo: WinningInfo): Map<LottoResultRank, Int> {
+        return lottoPackages
+            .groupingBy { LottoResultRank.getRank(it.getRankKey(winningInfo)) }
+            .eachCount()
+    }
 
-        private fun getTotalProfitRate(
-            lottoPackages: List<LottoNumberPackage>,
-            winningInfo: WinningInfo,
-            purchaseAmount: LottoPurchaseAmount
-        ): BigDecimal {
-            return lottoPackages
-                .sumOf { it.getPrizeMoney(winningInfo) }
-                .toBigDecimal()
-                .setScale(2, RoundingMode.HALF_UP)
-                .div(purchaseAmount.value!!.toBigDecimal())
-        }
+    private fun buildTotalProfitRate(
+        lottoPackages: List<LottoNumberPackage>,
+        winningInfo: WinningInfo,
+        purchaseAmount: LottoPurchaseAmount
+    ): BigDecimal {
+        return lottoPackages
+            .sumOf { it.getPrizeMoney(winningInfo) }
+            .toBigDecimal()
+            .setScale(2, RoundingMode.HALF_UP)
+            .div(purchaseAmount.value!!.toBigDecimal())
     }
 }
