bfc93a51f6c51e8e0ff269b4dedecea883460367
ninjasul <ninjasul@gmail.com>
2021-11-01 03:31:54 +0900

    refactor: LottoTicket과 LottoResult 역할 재정의

--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -1,15 +1,31 @@
 package lotto.domain
 
 import lotto.service.LottoNumberPackagesGenerator
+import java.math.BigDecimal
+import java.math.RoundingMode
 
-class LottoTicket(private val _lottoPackages: List<LottoNumberPackage>) {
+data class LottoTicket(private val _lottoPackages: List<LottoNumberPackage>) {
     val lottoPackages: List<LottoNumberPackage>
         get() {
             return _lottoPackages.toList()
         }
 
     fun buildResult(winningInfo: WinningInfo, purchaseAmount: LottoPurchaseAmount): LottoResult {
-        return LottoResult(lottoPackages, winningInfo, purchaseAmount)
+        return LottoResult(buildResultStatistics(winningInfo), buildTotalProfitRate(winningInfo, purchaseAmount))
+    }
+
+    private fun buildResultStatistics(winningInfo: WinningInfo): Map<LottoResultRank, Int> {
+        return lottoPackages
+            .groupingBy { LottoResultRank.getRank(it.getRankKey(winningInfo)) }
+            .eachCount()
+    }
+
+    private fun buildTotalProfitRate(winningInfo: WinningInfo, purchaseAmount: LottoPurchaseAmount): BigDecimal {
+        return lottoPackages
+            .sumOf { it.getPrizeMoney(winningInfo) }
+            .toBigDecimal()
+            .setScale(2, RoundingMode.HALF_UP)
+            .div(purchaseAmount.value!!.toBigDecimal())
     }
 
     companion object {
