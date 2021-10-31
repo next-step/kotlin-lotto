0167d6d5a253877b0654099154ce3d7ee1575abf
laco-dev <laco2951@gmail.com>
2021-10-31 16:13:42 +0900

    :recycle: LottoNumbers를 일급 컬렉션으로 개선

--- src/main/kotlin/lotto/model/LottoNumbers.kt
+++ src/main/kotlin/lotto/model/LottoNumbers.kt
@@ -1,12 +1,8 @@
 package lotto.model
 
-data class LottoNumbers(
-    val num1: LottoNumber,
-    val num2: LottoNumber,
-    val num3: LottoNumber,
-    val num4: LottoNumber,
-    val num5: LottoNumber,
-    val num6: LottoNumber
+@JvmInline
+value class LottoNumbers private constructor(
+    private val numbers: List<LottoNumber>
 ) {
 
     init {
@@ -14,42 +10,31 @@ data class LottoNumbers(
     }
 
     private fun checkDuplicate() {
-        val numbers = asList()
         val distinctSize = numbers.distinct().size
-
         require(distinctSize == LOTTO_SIZE)
     }
 
-    private fun asList(): List<LottoNumber> = listOf(num1, num2, num3, num4, num5, num6)
+    fun contains(number: LottoNumber): Boolean = numbers.contains(number)
 
-    fun contains(number: LottoNumber): Boolean = asList().contains(number)
-
-    fun match(numbers: LottoNumbers): Int {
-        val numbers1 = asList()
-        val numbers2 = numbers.asList()
-
+    fun match(target: LottoNumbers): Int {
         val array = IntArray(MAX_NUMBER + 1)
-        numbers1.forEach { num -> array[num.value]++ }
-        numbers2.forEach { num -> array[num.value]++ }
+        numbers.forEach { num -> array[num.value]++ }
+        target.numbers.forEach { num -> array[num.value]++ }
 
         return array.filter { it == 2 }.size
     }
 
-    override fun toString(): String = "[$num1, $num2, $num3, $num4, $num5, $num6]"
+    override fun toString(): String = numbers.toString()
 
     companion object {
         private const val LOTTO_SIZE = 6
         private const val MIN_NUMBER = 1
         private const val MAX_NUMBER = 45
 
-        fun manual(num1: Int, num2: Int, num3: Int, num4: Int, num5: Int, num6: Int): LottoNumbers = LottoNumbers(
-            num1 = LottoNumber(num1),
-            num2 = LottoNumber(num2),
-            num3 = LottoNumber(num3),
-            num4 = LottoNumber(num4),
-            num5 = LottoNumber(num5),
-            num6 = LottoNumber(num6),
-        )
+        fun manual(num1: Int, num2: Int, num3: Int, num4: Int, num5: Int, num6: Int): LottoNumbers =
+            listOf(num1, num2, num3, num4, num5, num6)
+                .map { LottoNumber(it) }
+                .let(::LottoNumbers)
 
         fun random(): LottoNumbers {
             val array = IntArray(MAX_NUMBER + 1)
@@ -63,7 +48,7 @@ data class LottoNumbers(
                 times++
             }
             val numbers = array.filter { it > 0 }
-            return manual(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5])
+            return LottoNumbers(numbers.map { LottoNumber(it) })
         }
     }
 }
