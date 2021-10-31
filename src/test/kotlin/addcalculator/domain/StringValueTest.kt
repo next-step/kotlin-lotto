d40f941b5244b94cb86259352af4cffa70742deb
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-11-01 08:39:02 +0900

    feat, refactor - 입력 받을 문자열 도메인 구현 및 테스트 코드 리팩터링

--- src/test/kotlin/addcalculator/domain/StringValueTest.kt
+++ src/test/kotlin/addcalculator/domain/StringValueTest.kt
@@ -4,25 +4,35 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.NullAndEmptySource
 import org.junit.jupiter.params.provider.ValueSource
 
 class StringValueTest {
 
-    @Test
-    fun `null일 경우 0 return`() {
-        assertThat(Value(null).number) isEualTo (0)
-    }
-
-    @Test
-    fun `empty일 경우 0 return`() {
-        assertThat(Value("").number) isEualTo (0)
+    @ParameterizedTest
+    @NullAndEmptySource
+    fun `null일 경우 0 return`(inputText: String?) {
+        assertThat(StringValue(inputText).number).isZero
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["-1", "String", "@", "!"])
+    @ValueSource(strings = ["String", "@", "!"])
     fun `양수가 아닌 문자열 또는 특수문자일 경우 예외`(inputString: String) {
         assertThrows<IllegalArgumentException> {
-            Value(inputString)
+            StringValue(inputString)
         }
+    }
+
+    @Test
+    fun `양수가 아닌 음수일 경우 예외`() {
+        assertThrows<IllegalArgumentException> {
+            StringValue("-1")
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["0", "123", "987"])
+    fun `green case`(inputText: String) {
+        assertThat(StringValue(inputText).number).isEqualTo(inputText.toInt())
     }
 }
