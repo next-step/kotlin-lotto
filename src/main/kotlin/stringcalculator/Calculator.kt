0d2e5e35da115716ba61d4ae766d6663f6bd3be4
nachwon <nachwon@gmail.com>
2021-11-01 01:56:50 +0900

    feat: Add null or blank input handling

--- src/main/kotlin/stringcalculator/Calculator.kt
+++ src/main/kotlin/stringcalculator/Calculator.kt
@@ -4,10 +4,14 @@ import stringcalculator.utils.toPositiveInt
 
 class Calculator {
     fun calculate(rawInput: String?): Int {
-        return rawInput?.split(*DEFAULT_DELIMITERS)!!.sumOf { it.toPositiveInt() }
+        if (rawInput.isNullOrBlank()) {
+            return DEFAULT_RETURN
+        }
+        return rawInput.split(*DEFAULT_DELIMITERS).sumOf { it.toPositiveInt() }
     }
 
     companion object {
+        private const val DEFAULT_RETURN = 0
         private val DEFAULT_DELIMITERS = arrayOf(",", ":")
     }
 }
