7cf17db1bdbc2a4ff2b13b6e66c4abe718ab33eb
etff <wish2rich@gmail.com>
2021-11-01 22:08:41 +0900

    refactor: 수동 생성기가 로또 생성기를 구현하도록 변경하라

--- src/main/kotlin/lotto/domain/LottoManualGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoManualGenerator.kt
@@ -2,8 +2,9 @@ package lotto.domain
 
 class LottoManualGenerator(
     private val purchaseInformation: PurchaseInformation
-) {
-    fun generateLottos(): List<Lotto> {
+) : LottoGenerator {
+
+    override fun generateLottos(): List<Lotto> {
         return purchaseInformation.textLottos
             .map { ManualNumberGenerator.generateNumbers(it) }
             .map { Lotto(it) }
