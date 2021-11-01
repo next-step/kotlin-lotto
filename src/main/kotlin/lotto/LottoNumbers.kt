d2b28325e781043edec419523b4e73873225bdd7
ddaaac <skyblue300a@gmail.com>
2021-11-02 01:17:16 +0900

    LottoNumbers 구현

---
+++ src/main/kotlin/lotto/LottoNumbers.kt
@@ -0,0 +1,21 @@
+package lotto
+
+private const val VALID_LOTTO_SIZE = 6
+
+class LottoNumbers(numbers: Collection<Int>) {
+
+    val numbers: Set<LottoNumber> = numbers.mapTo(HashSet()) { LottoNumber(it) }
+
+    init {
+        require(this.numbers.size == VALID_LOTTO_SIZE) { "로또 번호는 6자리로 이뤄집니다." }
+    }
+
+    fun countSameNumber(other: LottoNumbers): Int {
+        return (numbers intersect other.numbers).size
+    }
+
+    fun contains(number: LottoNumber): Boolean {
+        return numbers.contains(number)
+    }
+}
+
