07732681681ec9b1398591d8440459311fb4ad1a
corby <corby.kim@kakaocorp.com>
2021-11-01 20:56:15 +0900

    PositiveInt 분리

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -3,28 +3,30 @@ package calculator
 object Calculator {
 
     private const val DEFAULT_RESULT = 0
-    private val expressionRegex = Regex("//(.)\n(.*)")
+    private const val EXPRESSION_INDEX = 2
+    private const val DELIMITER_INDEX = 1
+    private val DEFAULT_DELIMITERS = listOf(",", ":")
+    private val WITH_DELIMITER_REGEX = Regex("//(.)\n(.*)")
 
     fun add(expression: String?): Int {
         if (expression.isNullOrEmpty()) {
             return DEFAULT_RESULT
         }
-        val (parsedExpression, delimiters) = parseDelimiter(expression)
+        val (parsedExpression, delimiters) = parse(expression)
         return parsedExpression.split(*delimiters.toTypedArray())
-            .sumOf { it.toPositiveInt() }
+            .sumOf { PositiveInt(it).value }
     }
 
-    private fun parseDelimiter(expression: String): ParseResult {
-        val result = expressionRegex.find(expression)
-        return result?.let { ParseResult(it.groupValues[2], it.groupValues[1]) }
-            ?: ParseResult(expression, listOf(",", ":"))
+    private fun parse(expression: String): ParseResult {
+        val result = WITH_DELIMITER_REGEX.find(expression)
+            ?: return ParseResult(expression, DEFAULT_DELIMITERS)
+        return ParseResult(
+            result.groupValues[EXPRESSION_INDEX],
+            result.groupValues[DELIMITER_INDEX],
+        )
     }
 
     private data class ParseResult(val expression: String, val delimiters: List<String>) {
         constructor(expression: String, delimiter: String) : this(expression, listOf(delimiter))
-    }
-
-    private fun String.toPositiveInt(): Int {
-        return toIntOrNull()?.takeIf { it >= 0 } ?: throw RuntimeException("$this 은/는 양의 정수가 아닙니다.")
     }
 }
--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -15,31 +15,31 @@ class CalculatorTest {
     @ParameterizedTest
     @NullAndEmptySource
     fun `빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다`(text: String?) {
-        assertThat(calculator.add(text)).isZero;
+        assertThat(calculator.add(text)).isZero
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["1"])
     fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(text: String) {
-        assertThat(calculator.add(text)).isSameAs(text.toInt());
+        assertThat(calculator.add(text)).isSameAs(text.toInt())
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["1,2"])
     fun `숫자 두개를 쉼표 구분자로 입력할 경우 두 숫자의 합을 반환한다`(text: String) {
-        assertThat(calculator.add(text)).isSameAs(3);
+        assertThat(calculator.add(text)).isSameAs(3)
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["1,2:3", "1:2,3", "1:2:3"])
     fun `구분자를 쉼표 이외에 콜론을 사용할 수 있다`(text: String) {
-        assertThat(calculator.add(text)).isSameAs(6);
+        assertThat(calculator.add(text)).isSameAs(6)
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["//;\n1;2;3"])
     fun `두개의 슬래시문자와 개행문자 사이에 커스텀 구분자를 지정할 수 있다`(text: String) {
-        assertThat(calculator.add(text)).isSameAs(6);
+        assertThat(calculator.add(text)).isSameAs(6)
     }
 
     @Test
