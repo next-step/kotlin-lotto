03d27fe22bf2e66c0357939bc4a2d53c4d6f4b4c
ddaaac <skyblue300a@gmail.com>
2021-11-02 01:47:30 +0900

    LottoRanking 구현

---
+++ src/main/kotlin/lotto/LottoRanking.kt
@@ -0,0 +1,23 @@
+package lotto
+
+enum class LottoRanking(
+    private val count: Int,
+    val reward: Long,
+) {
+    RANK_1(6, 2_000_000_000L),
+    RANK_2(5, 150_000_000L),
+    RANK_3(4, 50_000L),
+    RANK_4(3, 5_000L),
+    ;
+
+    companion object {
+
+        fun from(count: Int): LottoRanking? {
+            return values().firstOrNull {
+                isCountMatch(it, count)
+            }
+        }
+
+        private fun isCountMatch(ranking: LottoRanking, count: Int) = (ranking.count == count)
+    }
+}
---
+++ src/test/kotlin/lotto/LottoRankingTest.kt
@@ -0,0 +1,33 @@
+package lotto
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+@Suppress("NonAsciiCharacters")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class LottoRankingTest {
+
+    @ParameterizedTest
+    @MethodSource
+    fun `번호가 일치하는 갯수가 주어지면 랭킹을 구한다`(count: Int, expected: LottoRanking?) {
+        // when
+        val result = LottoRanking.from(count)
+
+        // then
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Suppress("unused")
+    private fun `번호가 일치하는 갯수가 주어지면 랭킹을 구한다`() = listOf(
+        Arguments.of(6, LottoRanking.RANK_1),
+        Arguments.of(5, LottoRanking.RANK_2),
+        Arguments.of(4, LottoRanking.RANK_3),
+        Arguments.of(3, LottoRanking.RANK_4),
+        Arguments.of(2, null),
+        Arguments.of(1, null),
+        Arguments.of(0, null),
+    )
+}
