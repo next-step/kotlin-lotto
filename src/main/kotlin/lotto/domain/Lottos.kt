d1f7aa1e8b51e48f97fbb8d679660969ac852029
flamme <flamme1004@gmail.com>
2021-11-02 23:13:59 +0900

    style(lotto): 필요없는 코드 삭제

--- src/main/kotlin/lotto/domain/Lottos.kt
+++ src/main/kotlin/lotto/domain/Lottos.kt
@@ -4,9 +4,6 @@ package lotto.domain
 value class Lottos private constructor(private val lottos: List<Lotto>) {
     fun toList(): List<Lotto> = lottos.toList()
 
-    // fun correspondToWinningNumber(winningNumber: List<Int>, bonusNumber: Int): LottoResults =
-    //     LottoResults.matchingWinningNumber(winningNumber, bonusNumber, lottos)
-
     companion object {
         fun buy(money: Int): Lottos {
             val values = MutableList(money / Lotto.PRICE) { Lotto.generate() }
