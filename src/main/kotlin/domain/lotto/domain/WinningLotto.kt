b4fd361a071a09e09596f8940ff10f927626ba04
김우재 <kwj1270@naver.com>
2021-11-02 19:54:50 +0900

    test: 로또 당첨 번호와 보너스 번호를 통해 생성 테스트 리팩토링

--- src/main/kotlin/domain/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/domain/lotto/domain/WinningLotto.kt
@@ -6,7 +6,7 @@ class WinningLotto(val winningLotto: Lotto, val bonusBall: LottoNumber) {
 
     companion object {
         fun from(winningLotto: String, bonusBall: Int, splitStrategy: SplitStrategy): WinningLotto =
-            WinningLotto(Lotto.of(winningLotto, splitStrategy), LottoNumber.of(bonusBall))
+            from(Lotto.of(winningLotto, splitStrategy), LottoNumber.of(bonusBall))
 
         fun from(winningLotto: Lotto, bonusBall: LottoNumber): WinningLotto = WinningLotto(winningLotto, bonusBall)
     }
--- src/test/kotlin/domain/lotto/domain/WinningLottoTest.kt
+++ src/test/kotlin/domain/lotto/domain/WinningLottoTest.kt
@@ -2,22 +2,25 @@ package domain.lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 @DisplayName("당첨 로또(WinningLotto)")
 class WinningLottoTest {
 
-    @Test
-    fun `로또 당첨 번호와 보너스 번호를 통해 생성할 수 있다`() {
-        val lottoString = "1, 2, 3, 4, 5, 6"
-        val bonusBall = 7
-        val winningLotto = WinningLotto.from(lottoString, bonusBall) { it.split(", ")}
+    @ParameterizedTest(name = "당첨 로또 : {0}, 보너스볼: {1}")
+    @CsvSource(
+        value = [
+            "1, 2, 3, 4, 5, 6:7", "2, 3, 4, 5, 6, 7:1",
+            "40, 41, 42, 43, 44, 45:39", "39, 40, 41, 42, 43, 44:45"], delimiter = ':'
+    )
+    fun `로또 당첨 번호와 보너스 번호를 통해 생성할 수 있다`(winningLotto: String, bonusBall: Int) {
+        val winningLotto = WinningLotto.from(winningLotto, bonusBall) { it.split(", ") }
 
         assertAll(
             { assertThat(winningLotto).isNotNull },
             { assertThat(winningLotto).isExactlyInstanceOf(WinningLotto::class.java) },
         )
     }
-
 }
