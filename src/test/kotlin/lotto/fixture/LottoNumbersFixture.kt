5dc3be175b8f0fe360c04508177333a5a3365866
ssibongee <ssibongee@gmail.com>
2021-11-03 11:31:16 +0900

    test: 로또 번호 목록(LottoNumbers) Fixture 구현 및 LottoNumberFixture에 Fixture 추가

---
+++ src/test/kotlin/lotto/fixture/LottoNumbersFixture.kt
@@ -0,0 +1,11 @@
+package lotto.fixture
+
+import lotto.domain.LottoNumbers
+
+class LottoNumbersFixture {
+
+    companion object {
+        val LOTTO_NUMBER_SET_FIRST = LottoNumbers.of(LottoNumberFixture.LOTTO_NUMBER_LIST_FIRST)
+        val LOTTO_NUMBER_SET_SECOND = LottoNumbers.of(LottoNumberFixture.LOTTO_NUMBER_LIST_SECOND)
+    }
+}
