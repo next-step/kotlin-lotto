fc39658f6488379b25d3fb4b385a0869a136f5a0
ssibongee <ssibongee@gmail.com>
2021-11-03 10:47:53 +0900

    test: 로또 번호(LottoNumber) Fixture 구현

---
+++ src/test/kotlin/lotto/fixture/LottoNumberFixture.kt
@@ -0,0 +1,19 @@
+package lotto.fixture
+
+import lotto.domain.LottoNumber
+
+class LottoNumberFixture {
+
+    companion object {
+        private val ONE = LottoNumber.of(1)
+        private val FIVE = LottoNumber.of(5)
+        private val SEVEN = LottoNumber.of(7)
+        private val TEN = LottoNumber.of(10)
+        private val TWENTY_THREE = LottoNumber.of(23)
+        private val THIRTY = LottoNumber.of(30)
+        private val FORTY_FIVE = LottoNumber.of(45)
+
+        val DUPLICATED_LOTTO_NUMBER_LIST = listOf(ONE, ONE, SEVEN, THIRTY, FIVE, TWENTY_THREE)
+        val LOTTO_NUMBER_LIST_OF_SEVEN = listOf(ONE, SEVEN, TEN, THIRTY, FORTY_FIVE, FIVE, TWENTY_THREE)
+    }
+}
