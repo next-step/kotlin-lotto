b314c7bb7d006a2f0f8b71c33badb1106db35516
ssibongee <ssibongee@gmail.com>
2021-11-03 23:07:05 +0900

    feat: 로또 머신(LottoMachine) 클래스 구현

---
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -0,0 +1,32 @@
+package lotto.domain
+
+import lotto.domain.strategy.LottoGenerator
+
+class LottoMachine(private val generator: LottoGenerator) {
+
+    fun count(budget: Money): Int {
+        return budget.count(LOTTERY_PRICE)
+    }
+
+    fun buy(counts: Int): Lotteries {
+        return (1..counts)
+            .map { generator.generate() }
+            .let { Lotteries.of(it) }
+    }
+
+    fun calculate(lotteries: Lotteries, winning: Lottery): LotteryStatistics {
+        return LotteryStatistics.of(lotteries, winning)
+    }
+
+    fun settle(statistics: LotteryStatistics): Money {
+        return statistics.calculate()
+    }
+
+    fun calculateYield(paid: Money, reward: Money): LotteryYield {
+        return LotteryYield.of(paid, reward)
+    }
+
+    companion object {
+        private val LOTTERY_PRICE = Money.of(1_000L)
+    }
+}
