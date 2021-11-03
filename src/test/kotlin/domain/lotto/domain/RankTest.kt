be77d4022cc75599fc73567bd1b7db41c8995bcd
김우재 <kwj1270@naver.com>
2021-11-03 23:03:40 +0900

    test: 맞은 숫자가 범위에 맞지 않다면 예외를 발생한다

--- src/test/kotlin/domain/lotto/domain/RankTest.kt
+++ src/test/kotlin/domain/lotto/domain/RankTest.kt
@@ -1,8 +1,10 @@
 package domain.lotto.domain
 
+import domain.lotto.error.InvalidNumberOfMatchException
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -23,6 +25,19 @@ class RankTest {
         assertAll(
             { assertThat(rank).isNotNull },
             { assertThat(rank).isExactlyInstanceOf(Rank::class.java) }
+        )
+    }
+
+    @ParameterizedTest(name = "당첨 갯수 = {0}, 보너스볼 필요 여부 = {1}")
+    @CsvSource(
+        value = ["${Lotto.MINIMUM_SIZE - 1}:true", "${Lotto.MINIMUM_SIZE - 1}:false", "${Lotto.MAXIMUM_SIZE + 1}:true", "${Lotto.MAXIMUM_SIZE + 1}:false"],
+        delimiter = ':'
+    )
+    fun `맞은 숫자가 범위에 맞지 않다면 예외를 발생한다`(matchOfNumber: Int, needBonusBall: Boolean) {
+        val exception = assertThrows<InvalidNumberOfMatchException> { Rank(matchOfNumber, needBonusBall) }
+
+        assertThat(exception.message).isEqualTo(
+            "맞은 로또의 개수 %s 은 ${Lotto.MINIMUM_SIZE} 과 ${Lotto.MAXIMUM_SIZE} 사이의 숫자가 아니다.".format(matchOfNumber)
         )
     }
 }
