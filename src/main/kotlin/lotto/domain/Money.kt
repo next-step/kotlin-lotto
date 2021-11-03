5a247128f063ccded0290844c4cbab39f80c1f16
ssibongee <ssibongee@gmail.com>
2021-11-03 20:34:58 +0900

    refactor: BigDecimal의 나누기 결과가 무한 순환소수가 발생하는 문제를 해결

--- src/main/kotlin/lotto/domain/Money.kt
+++ src/main/kotlin/lotto/domain/Money.kt
@@ -1,9 +1,14 @@
 package lotto.domain
 
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 @JvmInline
 value class Money private constructor(val value: BigDecimal) {
+
+    fun count(price: Money): Int {
+        return this.value.divide(price.value, RoundingMode.HALF_EVEN).intValueExact()
+    }
 
     companion object {
         fun of(value: Int): Money {
