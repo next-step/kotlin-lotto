d649ff729dabff7e21830d36ea34fe21933e31f2
pursue503 <pursue503@naver.com>
2021-11-03 22:32:47 +0900

    Add && Update (Step1): StringSplitModule 추가 && 리팩터링

--- src/main/kotlin/calculator/domain/StringAddCalculator.kt
+++ src/main/kotlin/calculator/domain/StringAddCalculator.kt
@@ -1,22 +1,14 @@
 package calculator.domain
 
-class StringAddCalculator {
+import calculator.parser.StringSplitModule
 
-    private val customRegex = Regex("//(.)\n(.*)")
+class StringAddCalculator(
+    private val stringSplitModule: StringSplitModule = StringSplitModule()
+) {
 
     fun add(text: String?): Int {
         if (text.isNullOrEmpty() || text.isNullOrBlank()) return 0
 
-        val result = customRegex.find(text)
-
-        result?. let {
-            val customDelimiter = it.groupValues[1]
-            val numberList = it.groupValues[2].split(customDelimiter)
-            return numberList.sumOf { value -> value.toInt() }
-        }
-
-        val numberList = text.split(",", ":")
-
-        return numberList.sumOf { it.toInt() }
+        return stringSplitModule.split(text).sumOf { it }
     }
 }
--- src/test/kotlin/calculator/StringAddCalculatorTest.kt
+++ src/test/kotlin/calculator/StringAddCalculatorTest.kt
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
 
 class StringAddCalculatorTest {
 
