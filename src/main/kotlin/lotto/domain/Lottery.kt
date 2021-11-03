d671ec86913a0838136394be6051ed23b48e72b8
ssibongee <ssibongee@gmail.com>
2021-11-03 11:30:22 +0900

    feat: 비교하려는 로또와 일치하는 번호 개수를 구하는 기능 구현

--- src/main/kotlin/lotto/domain/Lottery.kt
+++ src/main/kotlin/lotto/domain/Lottery.kt
@@ -2,6 +2,9 @@ package lotto.domain
 
 @JvmInline
 value class Lottery private constructor(val numbers: LottoNumbers) {
+    fun compareTo(lottery: Lottery): Int {
+        return numbers.values.count { lottery.numbers.values.contains(it) }
+    }
 
     companion object {
         fun of(numbers: LottoNumbers): Lottery {
