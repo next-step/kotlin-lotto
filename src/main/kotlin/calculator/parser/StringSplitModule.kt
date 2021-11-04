b00deec7a50137363b0649cbf751749bf57afb81
pursue503 <pursue503@naver.com>
2021-11-04 22:01:19 +0900

    Fix: 피드백 반영

--- src/main/kotlin/calculator/parser/StringSplitModule.kt
+++ src/main/kotlin/calculator/parser/StringSplitModule.kt
@@ -1,32 +1,33 @@
 package calculator.parser
 
-class StringSplitModule {
+object StringSplitModule {
 
-    private val regex = Regex("//(.)\n(.*)")
+    private val customDelimiterRegex = Regex("//(.)\n(.*)")
+    private val DEFAULT_DELIMITER = arrayOf(",", ":")
+    private const val MIN_NUM: Int = 0
+    private const val OUT_OF_RANGE: String = "음수를 입력하실수 없습니다."
 
     fun split(text: String): List<Int> {
-        val result = regex.find(text)
+        val result = customDelimiterRegex.find(text)
         result?. let {
             val customDelimiter = it.groupValues[1]
             val numberList = it.groupValues[2].split(customDelimiter)
-            return numberList.map { str: String -> str.toPositiveNumber() }
+
+            return numberList
+                .map { listValue -> toPositiveNumber(listValue) }
         }
         return defaultDelimiter(text)
     }
 
     private fun defaultDelimiter(text: String): List<Int> {
-        return text.split(*DEFAULT_DELIMITER).map { it.toPositiveNumber() }
+        return text
+            .split(*DEFAULT_DELIMITER)
+            .map { toPositiveNumber(it) }
     }
 
-    private fun String.toPositiveNumber(): Int {
-        val number = this.trim().toInt()
+    private fun toPositiveNumber(text: String): Int {
+        val number = text.trim().toInt()
         if (number < MIN_NUM) throw RuntimeException(OUT_OF_RANGE)
         return number
-    }
-
-    companion object {
-        val DEFAULT_DELIMITER = arrayOf(",", ":")
-        const val MIN_NUM: Int = 0
-        const val OUT_OF_RANGE: String = "음수를 입력하실수 없습니다."
     }
 }
