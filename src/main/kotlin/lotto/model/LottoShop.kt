d3f07e9bb85512f90f69ccdc6763dd7b2bf37231
laco-dev <laco2951@gmail.com>
2021-11-04 21:15:14 +0900

    :sparkles: 수동으로 구매하는 로또 개수는 구입 금액을 초과할 수 없다.

--- src/main/kotlin/lotto/model/LottoShop.kt
+++ src/main/kotlin/lotto/model/LottoShop.kt
@@ -10,6 +10,16 @@ class LottoShop {
         return machine.auto(size, LOTTO_PRICE)
     }
 
+    fun buy(amount: Int, manualNumbers: List<LottoNumbers>): List<Lotto> {
+        require(amount >= 0)
+
+        val size = amount / LOTTO_PRICE
+        val manualSize = manualNumbers.size.coerceAtMost(size)
+        val machine = LottoMachine()
+
+        return machine.manual(manualNumbers.take(manualSize), LOTTO_PRICE)
+    }
+
     companion object {
         private const val LOTTO_PRICE = 1_000
     }
--- src/test/kotlin/lotto/model/LottoShopTest.kt
+++ src/test/kotlin/lotto/model/LottoShopTest.kt
@@ -2,6 +2,8 @@ package lotto.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -36,5 +38,14 @@ class LottoShopTest {
     fun lottoShopCount(amount: Int, count: Int) {
         val actual = shop.buy(amount).size
         assertThat(actual).isEqualTo(count)
+    }
+
+    @DisplayName("수동으로 구매하는 로또 개수는 구입 금액을 초과할 수 없다.")
+    @Test
+    fun lottoShopPurchaseOver() {
+        val amount = 14000
+        val lottoNumbers = List(20) { LottoNumbers.random() }
+
+        assertThat(shop.buy(amount, lottoNumbers).size).isEqualTo(14)
     }
 }
