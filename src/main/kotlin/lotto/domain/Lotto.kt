290800222a5ab27c8f6cb556df6139f3658569f8
nachwon <nachwon@gmail.com>
2021-11-05 22:39:09 +0900

    feat: Add lotto number match checking

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -7,6 +7,10 @@ class Lotto(val numbers: List<Int>) {
         require(numbers.size == numbers.distinct().size) { DUPLICATE_NUMBER_ERROR_MSG }
     }
 
+    fun countMatches(winningNumbers: List<Int>): Int {
+        return (numbers intersect winningNumbers).size
+    }
+
     companion object {
         private const val REQUIRED_SIZE_OF_NUMBERS = 6
         private const val INVALID_SIZE_OF_NUMBERS_ERROR_MSG = "총 $REQUIRED_SIZE_OF_NUMBERS 개의 번호가 필요합니다."
--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,8 +1,10 @@
 package lotto.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 import java.lang.IllegalArgumentException
 
@@ -20,5 +22,14 @@ class LottoTest {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 2, 3, 4, 5))
         }
+    }
+
+    @ParameterizedTest
+    @CsvSource("1,2,3|3", "1,2,3,4|4", "1,2,3,4,5|5", "1,2,3,4,5,6|6", delimiter = '|')
+    fun `당첨번호를 입력하면 일치하는 번호 개수를 리턴한다`(winningNumbers: String, expectedCount: Int) {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val matchedCount = lotto.countMatches(winningNumbers.split(",").map { it.toInt() })
+
+        assertThat(matchedCount).isEqualTo(expectedCount)
     }
 }
