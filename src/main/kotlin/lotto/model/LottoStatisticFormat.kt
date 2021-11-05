13c13e9510810db821a8a41bd710705853212981
thgus5614@naver.com <thgus5614@naver.com>
2021-11-05 22:55:47 +0900

    add: 로또 통계 객체 테스트 코드 추가

--- src/main/kotlin/lotto/model/LottoStatisticFormat.kt
+++ src/main/kotlin/lotto/model/LottoStatisticFormat.kt
@@ -1,5 +1,7 @@
 package lotto.model
 
+import lotto.model.LottoStatisticFormat.Companion.getRankProfit
+
 /**
  * 로또 당첨번호 비교한 통계를 위한 데이터 클래스
  * */
@@ -7,14 +9,14 @@ data class LottoStatisticFormat(
     val purchasedPrice: Price,
     val winList: HashMap<LottoRank, Int>,
 ) {
-    private val totalPrice: Int = LottoRank
-        .values()
-        .filter { it != LottoRank.MISS }
-        .sumOf { getRankProfit(it) }
+    val profit: Double = ((getTotalPrice(winList) / purchasedPrice.price).toFloat()).toDouble()
 
-    val profit: Double = ((totalPrice / purchasedPrice.price).toFloat()).toDouble()
-
-    private fun getRankCount(rank: LottoRank): Int = winList[rank] ?: 0
-
-    private fun getRankProfit(rank: LottoRank): Int = (getRankCount(rank) * rank.winningMoney)
+    companion object {
+        fun HashMap<LottoRank, Int>.getRankCount(rank: LottoRank): Int = this[rank] ?: 0
+        fun HashMap<LottoRank, Int>.getRankProfit(rank: LottoRank): Int = (getRankCount(rank) * rank.winningMoney)
+        fun getTotalPrice(winList: HashMap<LottoRank, Int>): Int = LottoRank
+            .values()
+            .filter { it != LottoRank.MISS }
+            .sumOf {  winList.getRankProfit(it) }
+    }
 }
--- src/test/kotlin/lotto/model/LottoStatisticTest.kt
+++ src/test/kotlin/lotto/model/LottoStatisticTest.kt
@@ -1,10 +1,87 @@
 package lotto.model
 
+import lotto.model.LottoStatisticFormat.Companion.getRankCount
+import lotto.model.LottoStatisticFormat.Companion.getRankProfit
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class LottoStatisticTest {
+
+    @Test
+    @DisplayName("당첨 등수 개수 확인")
+    fun `check each rank count`() {
+        // given
+        val resultList = hashMapOf(
+            LottoRank.FIRST to 1,
+            LottoRank.SECOND to 3,
+            LottoRank.THIRD to 2,
+            LottoRank.FOURTH to 4,
+            LottoRank.FIFTH to 1
+        )
+
+        // when
+        val firstRank = resultList.getRankCount(LottoRank.FIRST)
+        val second = resultList.getRankCount(LottoRank.SECOND)
+        val third = resultList.getRankCount(LottoRank.THIRD)
+        val fourth = resultList.getRankCount(LottoRank.FOURTH)
+        val fifth = resultList.getRankCount(LottoRank.FIFTH)
+
+        // then
+        Assertions.assertThat(firstRank).isEqualTo(1)
+        Assertions.assertThat(second).isEqualTo(3)
+        Assertions.assertThat(third).isEqualTo(2)
+        Assertions.assertThat(fourth).isEqualTo(4)
+        Assertions.assertThat(fifth).isEqualTo(1)
+    }
+
+    @Test
+    @DisplayName("각 등수별 수익금 금액 확인")
+    fun `check each rank profit price`() {
+        // given
+        val resultList = hashMapOf(
+            LottoRank.FIRST to 1,
+            LottoRank.SECOND to 3,
+            LottoRank.THIRD to 2,
+            LottoRank.FOURTH to 4,
+            LottoRank.FIFTH to 1
+        )
+
+        // when
+        val firstRank = resultList.getRankProfit(LottoRank.FIRST)
+        val second = resultList.getRankProfit(LottoRank.SECOND)
+        val third = resultList.getRankProfit(LottoRank.THIRD)
+        val fourth = resultList.getRankProfit(LottoRank.FOURTH)
+        val fifth = resultList.getRankProfit(LottoRank.FIFTH)
+
+        // then
+        Assertions.assertThat(firstRank).isEqualTo(1 * LottoRank.FIRST.winningMoney)
+        Assertions.assertThat(second).isEqualTo(3 * LottoRank.SECOND.winningMoney)
+        Assertions.assertThat(third).isEqualTo(2 * LottoRank.THIRD.winningMoney)
+        Assertions.assertThat(fourth).isEqualTo(4 * LottoRank.FOURTH.winningMoney)
+        Assertions.assertThat(fifth).isEqualTo(1 * LottoRank.FIFTH.winningMoney)
+    }
+
+    @Test
+    @DisplayName("수익금 총 금액 확인")
+    fun `check total profit price`() {
+        // given
+        val resultList = hashMapOf(
+            LottoRank.FIRST to 1,
+            LottoRank.SECOND to 0,
+            LottoRank.THIRD to 0,
+            LottoRank.FOURTH to 0,
+            LottoRank.FIFTH to 0
+        )
+        val expectedTotalPrice = resultList.map { it.value * it.key.winningMoney }.sum()
+
+        // when
+        val totalPrice = LottoStatisticFormat.getTotalPrice(resultList)
+
+        // then
+        Assertions.assertThat(totalPrice).isEqualTo(expectedTotalPrice)
+    }
+
     @Test
     @DisplayName("로또 당첨된 사람이 있는 경우의 수익률 계산")
     fun `check profit when is has winner`() {
