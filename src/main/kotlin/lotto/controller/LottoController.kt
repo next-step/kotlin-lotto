4613ea818411344530d7dcfde3ff3fcbfce525ca
seunghwan <slflfl12@naver.com>
2021-11-05 23:09:55 +0900

    feat, test (step2) : Money, MoneyTest 추가

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,7 +1,11 @@
 package lotto.controller
 
+import lotto.domain.Money
+import lotto.view.InputView
+
 object LottoController {
 
     fun start() {
+        val money = Money.of(InputView.getMoney())
     }
 }
---
+++ src/main/kotlin/lotto/domain/Money.kt
@@ -0,0 +1,21 @@
+package lotto.domain
+
+data class Money(private val money: Int) {
+
+    init {
+        require(money >= LOTTO_PRICE) { NOT_ENOUGH_MONEY_MESSAGE }
+    }
+
+    val lottoCount: Int
+        get() = money / LOTTO_PRICE
+
+    companion object {
+        const val LOTTO_PRICE = 1000
+        const val NOT_ENOUGH_MONEY_MESSAGE = "로또를 하시려면 최소 1000원은 필요합니다."
+        const val NUMBER_FORMAT_EXCEPTION_MESSAGE = "금액은 숫자만 입력할 수 있습니다"
+
+        fun of(value: String): Money {
+            return Money(value.toIntOrNull() ?: throw IllegalArgumentException(NUMBER_FORMAT_EXCEPTION_MESSAGE))
+        }
+    }
+}
