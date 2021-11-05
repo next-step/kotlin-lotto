189a553a6e6cccf304aab85b2089a7542ce4308f
nachwon <nachwon@gmail.com>
2021-11-05 23:33:33 +0900

    feat: Add price validation to LottoGenerator

--- src/main/kotlin/lotto/domain/LottoGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoGenerator.kt
@@ -5,8 +5,13 @@ import lotto.domain.money.Money
 class LottoGenerator(val lottoPrice: Money) {
 
     fun generate(paidPrice: Money): List<Lotto> {
-        val numberOfLottoToGenerate = paidPrice / lottoPrice
+        val numberOfLottoToGenerate = getNumberOfLottoToGenerate(lottoPrice, paidPrice)
         return (1..numberOfLottoToGenerate).map { createLotto() }
+    }
+
+    private fun getNumberOfLottoToGenerate(lottoPrice: Money, paidPrice: Money): Int {
+        require(lottoPrice <= paidPrice) { NOT_ENOUGH_MONEY }
+        return paidPrice / lottoPrice
     }
 
     private fun createLotto(): Lotto {
@@ -17,5 +22,6 @@ class LottoGenerator(val lottoPrice: Money) {
     companion object {
         private val NUMBERS_RANGE = (0..5)
         private val NUMBER_POOL = (1..45).toList()
+        private const val NOT_ENOUGH_MONEY = "금액이 부족합니다."
     }
 }
