980c4f5ac024ec6d33f450eaccbd03dcc676ab0e
nachwon <nachwon@gmail.com>
2021-11-05 22:32:33 +0900

    feat: Add lotto duplicate number check

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -4,10 +4,12 @@ class Lotto(val numbers: List<Int>) {
 
     init {
         require(numbers.size == REQUIRED_SIZE_OF_NUMBERS) { INVALID_SIZE_OF_NUMBERS_ERROR_MSG }
+        require(numbers.size == numbers.distinct().size) { DUPLICATE_NUMBER_ERROR_MSG }
     }
 
     companion object {
         private const val REQUIRED_SIZE_OF_NUMBERS = 6
         private const val INVALID_SIZE_OF_NUMBERS_ERROR_MSG = "총 $REQUIRED_SIZE_OF_NUMBERS 개의 번호가 필요합니다."
+        private const val DUPLICATE_NUMBER_ERROR_MSG = "번호는 중복될 수 없습니다."
     }
 }
--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto.domain
 
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -8,9 +9,16 @@ import java.lang.IllegalArgumentException
 class LottoTest {
     @ParameterizedTest
     @ValueSource(strings = ["1,2,3,4,5", "1,2,3,4,5,6,7"])
-    fun `로또 생성 시 잘못된 개수의 번호를 받으면 에러를 일으킨다`(numbers: String) {
+    fun `잘못된 개수의 번호로 로또 생성 시 에러를 일으킨다`(numbers: String) {
         assertThrows<IllegalArgumentException> {
             Lotto(numbers.split(",").map { it.toInt() })
+        }
+    }
+
+    @Test
+    fun `중복된 번호로 로또 생성 시 에러를 일으킨다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 2, 3, 4, 5))
         }
     }
 }
