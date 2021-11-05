885c5dc561d3a2539e4f77307e7183e300f71789
seunghwan <slflfl12@naver.com>
2021-11-05 22:53:36 +0900

    feat (step2): InputView 추가

---
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -0,0 +1,18 @@
+package lotto.view
+
+object InputView {
+
+    const val MONEY_QUESTION = "구입금액을 입력해 주세요."
+    const val WINNING_NUMBER_QUESTION = "지난 주 당첨 번호를 입력해 주세요."
+    const val INPUT_NULL_POINTER_EXCEPTION_MESSAGE = "입력은 null일 수 없습니다"
+
+    fun getMoney(): String {
+        println(MONEY_QUESTION)
+        return readLine() ?: throw IllegalArgumentException(INPUT_NULL_POINTER_EXCEPTION_MESSAGE)
+    }
+
+    fun inputWinningNumbers(): String {
+        println(WINNING_NUMBER_QUESTION)
+        return readLine() ?: throw IllegalArgumentException()
+    }
+}
