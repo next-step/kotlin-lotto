25dc3bd24b3f3d6798d7bb11da57187c6ba18619
thgus5614@naver.com <thgus5614@naver.com>
2021-11-06 10:43:05 +0900

    add: 로또 번호 랜덤 번호 생성 메소드 테스트 코드 추가

--- src/main/kotlin/lotto/model/LottoNumberListGenerator.kt
+++ src/main/kotlin/lotto/model/LottoNumberListGenerator.kt
@@ -7,19 +7,18 @@ class LottoNumberListGenerator(private val price: Price) {
     fun generateLottoList(): List<Lotto> =
         (0 until price.lottoCount)
             .map {
-                Lotto(generateRandomNumbers(LottoNumber.getLottoNumberRange()))
+                Lotto(generateRandomNumbers(LottoNumber.getLottoNumberRange().shuffled()))
             }
-
-    private fun generateRandomNumbers(range: List<Int>): List<LottoNumber> {
-        require(range.size > 6)
-        return (range)
-            .shuffled()
-            .subList(NUMBER_START, NUMBER_END)
-            .map { LottoNumber(it) }
-    }
 
     companion object {
         private const val NUMBER_START = 0
         private const val NUMBER_END = 6
+
+        fun generateRandomNumbers(range: List<Int>): List<LottoNumber> {
+            require(range.size >= 6)
+            return (range)
+                .subList(NUMBER_START, NUMBER_END)
+                .map { LottoNumber(it) }
+        }
     }
 }
--- src/test/kotlin/lotto/model/LottoNumberListGeneratorTest.kt
+++ src/test/kotlin/lotto/model/LottoNumberListGeneratorTest.kt
@@ -17,4 +17,25 @@ class LottoNumberListGeneratorTest {
         // then
         assertThat(lottoList.size).isEqualTo(3)
     }
+
+    @Test
+    @DisplayName("로또 생성 번호 확인")
+    fun `check lotto numbers`() {
+        // given
+        val numbers = listOf(1, 10, 22, 13, 14, 45)
+        val expected = listOf(
+            LottoNumber(1),
+            LottoNumber(10),
+            LottoNumber(22),
+            LottoNumber(13),
+            LottoNumber(14),
+            LottoNumber(45),
+        )
+
+        // when
+        val lottoList = LottoNumberListGenerator.generateRandomNumbers(numbers)
+
+        // then
+        assertThat(lottoList).isEqualTo(expected)
+    }
 }
