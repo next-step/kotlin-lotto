9a216f868af0f63573a5a70570b276306fd718e0
pursue503 <pursue503@naver.com>
2021-11-06 19:13:42 +0900

    Add (Step2): LottoShop

---
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -0,0 +1,7 @@
+package lotto.domain
+
+class Lotto {
+
+    private val lottoNumber: List<Int> = (1..45).shuffled().subList(0, 6).sorted()
+
+}
--- src/test/kotlin/lotto/LottoShopTest.kt
+++ src/test/kotlin/lotto/LottoShopTest.kt
@@ -1,7 +1,7 @@
 package lotto
 
+import lotto.domain.LottoShop
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -9,28 +9,21 @@ import org.junit.jupiter.params.provider.ValueSource
 
 class LottoShopTest {
 
-    private lateinit var lottoShop: LottoShop
-
-    @BeforeEach
-    fun setUp() {
-        lottoShop = LottoShop()
-    }
-
     @ParameterizedTest
     @ValueSource(strings = ["1", "1001", "2500", "3500"])
     fun `로또 구매 금액이 천원단위로 들어오는지 테스트한다 천원단위가 아닐시 IllegalArgumentException 에러가 발생한다`(money: Int) {
-        assertThrows<IllegalArgumentException> { lottoShop.createLottoTicket(money) }
+        assertThrows<IllegalArgumentException> { LottoShop.createLottoTicket(money) }
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["-1", "-999999", "-102031"])
     fun `로또 구매 금액에 음수로 들어오면 IllegalArgumentException 에러를 발생 한다`(money: Int) {
-        assertThrows<IllegalArgumentException> { lottoShop.createLottoTicket(money) }
+        assertThrows<IllegalArgumentException> { LottoShop.createLottoTicket(money) }
     }
 
     @ParameterizedTest
-    @CsvSource(value = ["10000|10", "140000|14"], delimiterString = "|")
+    @CsvSource(value = ["10000|10", "140000|140"], delimiterString = "|")
     fun `로또 구매시 금액에 맞게 올바르게 생성되는지 테스트 한다`(money: Int, result: Int) {
-        assertThat(lottoShop.createLottoTicket(money).size()).isEqualTo(result)
+        assertThat(LottoShop.createLottoTicket(money).size).isEqualTo(result)
     }
 }
