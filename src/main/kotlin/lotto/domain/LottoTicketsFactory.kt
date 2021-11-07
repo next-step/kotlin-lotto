66671f39d19181633894cf86a6f2143d802daf2a
ddaaac <skyblue300a@gmail.com>
2021-11-07 18:50:26 +0900

    주어진 input을 LottoTickets로 변환하는 로직 구현

--- src/main/kotlin/lotto/domain/LottoTicketsFactory.kt
+++ src/main/kotlin/lotto/domain/LottoTicketsFactory.kt
@@ -2,6 +2,8 @@ package lotto.domain
 
 object LottoTicketsFactory {
 
+    private const val NUMBER_DELIMITER = ","
+
     @Suppress("NAME_SHADOWING")
     fun create(count: LottoTicketCount): LottoTickets {
         var count = count
@@ -12,5 +14,15 @@ object LottoTicketsFactory {
             count = count.useTicket()
         }
         return LottoTickets(tickets)
+    }
+
+    fun convertToTickets(inputs: List<String>): LottoTickets {
+        return LottoTickets(inputs.map(::toTicket))
+    }
+
+    private fun toTicket(value: String): LottoNumbers {
+        return value.split(NUMBER_DELIMITER)
+            .map { LottoNumber.valueOf(it.trim()) }
+            .let(::LottoNumbers)
     }
 }
--- src/test/kotlin/lotto/domain/LottoTicketsFactoryTest.kt
+++ src/test/kotlin/lotto/domain/LottoTicketsFactoryTest.kt
@@ -1,6 +1,7 @@
 package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -15,5 +16,23 @@ class LottoTicketsFactoryTest {
 
         // then
         assertThat(tickets.tickets.size).isEqualTo(count)
+    }
+
+    @Test
+    fun `로또 티켓 input이 주어지면 LottoTickets로 변환한다`() {
+        // given
+        val inputs = listOf(
+            "1, 2, 3, 4, 5,6",
+            "40,41 ,42,43,44,45",
+        )
+
+        // when
+        val tickets = LottoTicketsFactory.convertToTickets(inputs)
+
+        // then
+        assertThat(tickets.tickets.size).isEqualTo(2)
+        assertThat(tickets.tickets[0])
+            .usingRecursiveComparison()
+            .isEqualTo(LottoNumbers(listOf(1, 2, 3, 4, 5, 6)))
     }
 }
