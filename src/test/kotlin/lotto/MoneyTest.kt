49dbd33d5479b84e551a855de735ca22fc40773e
seunghwan <slflfl12@naver.com>
2021-11-07 21:13:30 +0900

    refactor (step2): Money는 Lotto를 모르도록 수정

--- src/test/kotlin/lotto/MoneyTest.kt
+++ src/test/kotlin/lotto/MoneyTest.kt
@@ -9,18 +9,22 @@ import org.junit.jupiter.params.provider.ValueSource
 
 class MoneyTest {
 
-    @ParameterizedTest(name = "로또는 1000money 당 1장 살 수 있다.")
-    @CsvSource(value = ["1000, 1", "1500, 1", "2300, 2", "12315, 12", "9232, 9"])
-    fun `로또는 1000money 당 1장 살 수 있다`(value: String, expected: Int) {
-        assertThat(Money.of(value).lottoCount).isEqualTo(expected)
+    @ParameterizedTest(name = "Money를 더할 수 있다.")
+    @CsvSource(value = ["1300, 1000, 1", "2999, 1000, 2", "19000, 1000, 19"])
+    fun `Money는 나눌 수 있다`(value1: String, value2: String, expected: Int) {
+        val money1 = Money.of(value1)
+        val money2 = Money.of(value2)
+
+        assertThat(money1 / money2).isEqualTo(expected)
     }
 
-    @ParameterizedTest(name = "Money는 1000이하일 수 없다.")
-    @ValueSource(strings = ["1", "20", "30", "40", "400", "600", "999"])
-    fun `Money는 1000이하일 수 없다`(value: String) {
-        Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
-            Money.of(value)
-        }.withMessage(Money.NOT_ENOUGH_MONEY_MESSAGE)
+    @ParameterizedTest(name = "Money는 비교할 수 있다.")
+    @CsvSource(value = ["2000, 1000, true", "300, 1000, false", "999, 1000, false"])
+    fun `Money는 비교할 수 있다`(value1: String, value2: String, expected: Boolean) {
+        val money1 = Money.of(value1)
+        val money2 = Money.of(value2)
+
+        assertThat(money1 >= money2).isEqualTo(expected)
     }
 
     @ParameterizedTest(name = "Money는 숫자만 입력할 수 있다.")
