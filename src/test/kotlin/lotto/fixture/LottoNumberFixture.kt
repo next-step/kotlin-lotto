ff51d1e7b8352fe816ad330a33b35065078f029e
ssibongee <ssibongee@gmail.com>
2021-11-08 04:28:00 +0900

    refactor: LottoNumber Fixture를 외부에서 활용할 수 있도록 접근 제한자 제거

--- src/test/kotlin/lotto/fixture/LottoNumberFixture.kt
+++ src/test/kotlin/lotto/fixture/LottoNumberFixture.kt
@@ -5,16 +5,16 @@ import lotto.domain.LottoNumber
 class LottoNumberFixture {
 
     companion object {
-        private val ONE = LottoNumber.of(1)
-        private val FIVE = LottoNumber.of(5)
-        private val SEVEN = LottoNumber.of(7)
-        private val TEN = LottoNumber.of(10)
-        private val ELEVEN = LottoNumber.of(11)
-        private val TWENTY_THREE = LottoNumber.of(23)
-        private val THIRTY = LottoNumber.of(30)
-        private val THIRTY_THREE = LottoNumber.of(33)
-        private val FORTY_FOUR = LottoNumber.of(44)
-        private val FORTY_FIVE = LottoNumber.of(45)
+        val ONE = LottoNumber.of(1)
+        val FIVE = LottoNumber.of(5)
+        val SEVEN = LottoNumber.of(7)
+        val TEN = LottoNumber.of(10)
+        val ELEVEN = LottoNumber.of(11)
+        val TWENTY_THREE = LottoNumber.of(23)
+        val THIRTY = LottoNumber.of(30)
+        val THIRTY_THREE = LottoNumber.of(33)
+        val FORTY_FOUR = LottoNumber.of(44)
+        val FORTY_FIVE = LottoNumber.of(45)
 
         val DUPLICATED_LOTTO_NUMBER_LIST = listOf(ONE, ONE, SEVEN, THIRTY, FIVE, TWENTY_THREE)
         val LOTTO_NUMBER_LIST_OF_SEVEN = listOf(ONE, SEVEN, TEN, THIRTY, FORTY_FIVE, FIVE, TWENTY_THREE)
