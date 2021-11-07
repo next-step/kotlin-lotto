27fabbe4556cf0fe03015c4f01a4f2179679f30e
seunghwan <slflfl12@naver.com>
2021-11-07 21:14:45 +0900

    refactor (step2): Lotto Companion 에서 Money를 받아서 Lotto를 구입할 수 있도록 수정

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -6,7 +6,7 @@ value class Lotto private constructor(
 ) {
 
     init {
-        require(numbers.size == LOTTO_SIZE) { MESSAGE(numbers.size) }
+        require(numbers.size == SIZE) { LOTTO_SIZE_NOT_MATCHED_MESSAGE(numbers.size) }
     }
 
     val sortedNumbers: List<LottoNumber>
@@ -17,10 +17,13 @@ value class Lotto private constructor(
     }
 
     companion object {
-        const val LOTTO_SIZE = 6
+        const val SIZE = 6
+        val PRICE = Money.of(1000)
         private const val DELIMITER = ","
         private const val NUMBER_FORMAT_EXCEPTION_MESSAGE = "로또번호 형식에 맞지 않습니다."
-        private val MESSAGE: (numberSize: Int) -> String = { "로또 숫자의 갯수는 ${LOTTO_SIZE}여야 합니다. 현재 갯수 == $it" }
+        private const val NOT_ENOUGH_MONEY_MESSAGE = "로또를 구입 하시려면 최소 1000원은 필요합니다."
+
+        private val LOTTO_SIZE_NOT_MATCHED_MESSAGE: (numberSize: Int) -> String = { "로또 숫자의 갯수는 ${SIZE}여야 합니다. 현재 갯수 == $it" }
 
         fun of(lottoNumbers: List<LottoNumber>): Lotto {
             return Lotto(lottoNumbers.toSet())
@@ -32,7 +35,12 @@ value class Lotto private constructor(
                     NUMBER_FORMAT_EXCEPTION_MESSAGE
                 )
             }
-            return of(numbers.map { LottoNumber(it) })
+            return of(numbers.map { LottoNumber.of(it) })
+        }
+
+        fun getPurchasedLottoCount(money: Money): Int {
+            require(money >= PRICE) { NOT_ENOUGH_MONEY_MESSAGE }
+            return money / PRICE
         }
     }
 }
