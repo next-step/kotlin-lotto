af81154595e223c1ae3a2a938389cfcdd5e97c32
sangyongchoi <csy7792@naver.com>
2021-11-07 18:13:41 +0900

    step4 modify code review content - refactor main function

--- src/main/kotlin/lotto/LottoApplication.kt
+++ src/main/kotlin/lotto/LottoApplication.kt
@@ -1,5 +1,7 @@
 package lotto
 
+import lotto.domain.model.LottoStatistics
+import lotto.domain.model.Lottos
 import lotto.presentation.LottoGame
 import lotto.usecase.LottoMachine
 import lotto.usecase.LottoGenerator
@@ -8,38 +10,43 @@ import lotto.usecase.WinningsChecker
 import lotto.view.InputView
 import lotto.view.OutputView
 
-fun main() {
-    val inputView = InputView()
-    val outputView = OutputView()
-    val lottoGame = LottoGame(
-        lottoMachine = LottoMachine(LottoGenerator()),
-        winningsChecker = WinningsChecker(),
-        calculator = PurchaseAmountCalculator(),
-    )
+val inputView = InputView()
+val outputView = OutputView()
+val lottoGame = LottoGame(
+    lottoMachine = LottoMachine(LottoGenerator()),
+    winningsChecker = WinningsChecker(),
+    calculator = PurchaseAmountCalculator(),
+)
 
+fun main() {
+    val lottos = buyLottos()
+    outputView.printLotto(lottos)
+
+    getStatistics(lottos).let { statistics ->
+        outputView.printStatistics(statistics)
+    }
+}
+
+private fun buyLottos(): Lottos {
     val purchaseAmount = inputView.inputPurchaseAmount()
     val passivityCount = inputView.inputPassivityCount()
     val passivityLottoNumbers = inputView.inputPassivityLottoNumbers(passivityCount)
 
     val availablePurchaseCount = purchaseAmount / LottoMachine.LOTTO_PRICE
 
-    val lottos = lottoGame.buy(
+    return lottoGame.buy(
         availablePurchaseCount = availablePurchaseCount,
         passivityLottoNumbers = passivityLottoNumbers,
     )
+}
 
-    outputView.printLotto(lottos)
-
+private fun getStatistics(lottos: Lottos): LottoStatistics {
     val winningNumbers = inputView.inputWinningNumber()
     val bonusNumber = inputView.inputBonusNumber()
 
-    lottoGame
-        .statistics(
-            winningNumbers = winningNumbers,
-            lottos = lottos,
-            bonusNumber = bonusNumber,
-        )
-        .let { statistics ->
-            outputView.printStatistics(statistics)
-        }
+    return lottoGame.statistics(
+        winningNumbers = winningNumbers,
+        lottos = lottos,
+        bonusNumber = bonusNumber,
+    )
 }
