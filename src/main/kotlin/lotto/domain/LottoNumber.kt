ff341b28500c5e80c185973f517480f7dc5833e6
seunghwan <slflfl12@naver.com>
2021-11-07 21:24:24 +0900

    refactor (step2): LottoNumber value class로 수정

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -1,6 +1,9 @@
 package lotto.domain
 
-data class LottoNumber(val number: Int) {
+@JvmInline
+value class LottoNumber private constructor(
+    val number: Int
+) {
 
     init {
         require(number in MIN_LOTTO_NUM..MAX_LOTTO_NUM) { WRONG_LOTTO_NUM_MESSAGE }
@@ -11,6 +14,10 @@ data class LottoNumber(val number: Int) {
         const val MAX_LOTTO_NUM = 45
         const val WRONG_LOTTO_NUM_MESSAGE = "잘못된 로또 번호입니다."
 
-        val LOTTO_NUMBERS = (MIN_LOTTO_NUM..MAX_LOTTO_NUM).map { LottoNumber(it) }
+        val LOTTO_NUMBERS = (MIN_LOTTO_NUM..MAX_LOTTO_NUM).map { of(it) }
+
+        fun of(value: Int): LottoNumber {
+            return LottoNumber(value)
+        }
     }
 }
