68400df11e302ac8858f70fe4f12336daffeab43
seunghwan <slflfl12@naver.com>
2021-11-07 14:16:15 +0900

    refactor (step2): Lotto value 클래스로 수정

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,9 +1,12 @@
 package lotto.domain
 
-data class Lotto(val numbers: Set<LottoNumber>) {
+@JvmInline
+value class Lotto private constructor(
+    private val numbers: Set<LottoNumber>
+) {
 
     init {
-        require(numbers.size == LOTTO_SIZE) { "로또 숫자의 갯수는 ${LOTTO_SIZE}여야 합니다. 현재 갯수 == ${numbers.size}" }
+        require(numbers.size == LOTTO_SIZE) { MESSAGE(numbers.size) }
     }
 
     val sortedNumbers: List<LottoNumber>
@@ -17,6 +20,7 @@ data class Lotto(val numbers: Set<LottoNumber>) {
         const val LOTTO_SIZE = 6
         private const val DELIMITER = ","
         private const val NUMBER_FORMAT_EXCEPTION_MESSAGE = "로또번호 형식에 맞지 않습니다."
+        private val MESSAGE: (numberSize: Int) -> String = { "로또 숫자의 갯수는 ${LOTTO_SIZE}여야 합니다. 현재 갯수 == $it" }
 
         fun of(lottoNumbers: List<LottoNumber>): Lotto {
             return Lotto(lottoNumbers.toSet())
