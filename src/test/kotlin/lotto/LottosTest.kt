d351496b4e638259d191995ebaf3dc2b17d03ca4
seunghwan <slflfl12@naver.com>
2021-11-07 21:24:55 +0900

    refactor, test (step2) : Lottos, LottoNumber 코드 수정으로 인해 Test코드 수정

--- src/test/kotlin/lotto/LottosTest.kt
+++ src/test/kotlin/lotto/LottosTest.kt
@@ -1,8 +1,8 @@
 package lotto
 
 import lotto.domain.Lotto
+import lotto.domain.LottoGeneratorImpl
 import lotto.domain.LottoNumber
-import lotto.domain.LottoNumberGenerator
 import lotto.domain.Lottos
 import lotto.domain.Money
 import lotto.domain.Rank
@@ -17,16 +17,16 @@ class LottosTest {
     @DisplayName(value = "당첨 lotto번호를 통해 Map(Rank, count)를 생성한다")
     @Test
     fun `당첨 lotto번호를 통해 Map(Rank, count)를 생성한다`() {
-        val winningLottoNumbers = (1..6).map { LottoNumber(it) }
+        val winningLottoNumbers = (1..6).map { LottoNumber.of(it) }
         val winningLotto = Lotto.of(winningLottoNumbers)
 
-        val lottos1Numbers = (1..6).map { LottoNumber(it) }
+        val lottos1Numbers = (1..6).map { LottoNumber.of(it) }
         val lotto1 = Lotto.of(lottos1Numbers)
-        val lottos2Numbers = (1..6).map { LottoNumber(it) }
+        val lottos2Numbers = (1..6).map { LottoNumber.of(it) }
         val lotto2 = Lotto.of(lottos2Numbers)
-        val lottos3Numbers = (2..7).map { LottoNumber(it) }
+        val lottos3Numbers = (2..7).map { LottoNumber.of(it) }
         val lotto3 = Lotto.of(lottos3Numbers)
-        val lottos4Numbers = (3..8).map { LottoNumber(it) }
+        val lottos4Numbers = (3..8).map { LottoNumber.of(it) }
         val lotto4 = Lotto.of(lottos4Numbers)
 
         val lottos = Lottos(listOf(lotto1, lotto2, lotto3, lotto4))
@@ -38,10 +38,10 @@ class LottosTest {
     @ParameterizedTest(name = "money와 LottoNumberGenerator를 통해 lottos를 생성할 수 있다.")
     @CsvSource("10000, 10", "9000, 9", "8350, 8", "99999, 99")
     fun `money와 LottoNumberGenerator를 통해 lottos를 생성할 수 있다`(value: Int, expected: Int) {
-        val lottoNumberGenerator = LottoNumberGenerator()
-        val money = Money(value)
+        val money = Money.of(value)
+        val lottoGenerator = LottoGeneratorImpl(money)
 
-        val lottos = Lottos.of(lottoNumberGenerator, money = money)
+        val lottos = Lottos.of(lottoGenerator)
 
         assertThat(lottos.lottos.size).isEqualTo(expected)
     }
