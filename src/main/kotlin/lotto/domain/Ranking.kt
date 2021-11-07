17fafaf0b7e7ccd12b851e6c55a422a0fec25535
ssibongee <ssibongee@gmail.com>
2021-11-08 04:16:50 +0900

    refactor: 순위(Ranking)을 판단할 때 보너스 번호 일치여부도 판단할 수 있도록 변경

--- src/main/kotlin/lotto/domain/Ranking.kt
+++ src/main/kotlin/lotto/domain/Ranking.kt
@@ -1,14 +1,20 @@
 package lotto.domain
 
-enum class Ranking(val reward: Money, val matches: Int) {
-    FIRST(Money.from(2_000_000_000L), 6),
-    SECOND(Money.from(1_500_000L), 5),
-    THIRD(Money.from(50_000L), 4),
-    FOURTH(Money.from(5_000L), 3),
-    NONE(Money.from(0L), 0);
+enum class Ranking(
+    val reward: Money,
+    val matches: Int,
+    val isMatched: (matches: Int, isContainBonusBall: Boolean) -> Boolean
+) {
+    FIRST(Money.from(2_000_000_000L), 6, { match, _ -> match == 6 }),
+    SECOND(Money.from(1_500_000L), 5, { match, bonus -> match == 5 && bonus }),
+    THIRD(Money.from(1_500_000L), 5, { match, bonus -> match == 5 && bonus }),
+    FOURTH(Money.from(50_000L), 4, { match, _ -> match == 4 }),
+    FIFTH(Money.from(5_000L), 3, { match, _ -> match == 3 }),
+    NONE(Money.from(0L), 0, { match, _ -> match < 3 });
 
     companion object {
-        fun calculate(matches: Int): Ranking = values().find { it.matches == matches } ?: NONE
+        fun calculate(matches: Int, isContainBonusBall: Boolean): Ranking =
+            values().find { it.isMatched(matches, isContainBonusBall) } ?: NONE
 
         fun isNone(ranking: Ranking): Boolean = ranking == NONE
     }
