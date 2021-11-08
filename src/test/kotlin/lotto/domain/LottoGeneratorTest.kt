3ce1514248132353e4aad62aa044b91004ff403b
nachwon <nachwon@gmail.com>
2021-11-09 02:04:17 +0900

    test: Remove logics from test

--- src/test/kotlin/lotto/domain/LottoGeneratorTest.kt
+++ src/test/kotlin/lotto/domain/LottoGeneratorTest.kt
@@ -10,10 +10,10 @@ import java.lang.IllegalArgumentException
 class LottoGeneratorTest {
 
     @ParameterizedTest
-    @CsvSource("5000,10000", "1,2", "1,10")
-    fun `로또 구입 금액을 입력하면 구입 금액에 해당하는 로또를 발급한다`(lottoPrice: Int, paidPrice: Int) {
+    @CsvSource("5000,10000,2", "1,2,2", "1,10,10")
+    fun `로또 구입 금액을 입력하면 구입 금액에 해당하는 로또를 발급한다`(lottoPrice: Int, paidPrice: Int, expectedCount: Int) {
         val lottos = LottoGenerator(lottoPrice = Money(lottoPrice)).generate(paidPrice = Money(paidPrice))
-        assertThat(lottos).hasSize(paidPrice / lottoPrice)
+        assertThat(lottos).hasSize(expectedCount)
     }
 
     @Test
