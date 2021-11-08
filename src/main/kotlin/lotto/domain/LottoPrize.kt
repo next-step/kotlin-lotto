1a74ffb587057f07fe6f41ebf2bfd23d8486329d
nachwon <nachwon@gmail.com>
2021-11-09 03:17:27 +0900

    feat: Add new second prize and bonus number match parameter"

--- src/main/kotlin/lotto/domain/LottoPrize.kt
+++ src/main/kotlin/lotto/domain/LottoPrize.kt
@@ -1,13 +1,17 @@
 package lotto.domain
 
 enum class LottoPrize(val matchCount: Int, val amount: Money) {
-    FOURTH(3, Money(5_000)),
-    THIRD(4, Money(50_000)),
-    SECOND(5, Money(1_500_000)),
+    FIFTH(3, Money(5_000)),
+    FOURTH(4, Money(50_000)),
+    THIRD(5, Money(1_500_000)),
+    SECOND(5, Money(30_000_000)),
     FIRST(6, Money(2_000_000_000));
 
     companion object {
-        fun fromMatchCount(matchCount: Int): LottoPrize? {
+        fun fromMatchCount(matchCount: Int, bonusMatch: Boolean = false): LottoPrize? {
+            if (matchCount == 5) {
+                return if (bonusMatch) SECOND else THIRD
+            }
             return values().find { it.matchCount == matchCount }
         }
     }
