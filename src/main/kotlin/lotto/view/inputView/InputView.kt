3ce0d681ff91bc69af525c347338ae6993f6d5d8
nachwon <nachwon@gmail.com>
2021-11-09 03:19:58 +0900

    feat: Add bonus number receiving

--- src/main/kotlin/lotto/view/inputView/InputView.kt
+++ src/main/kotlin/lotto/view/inputView/InputView.kt
@@ -6,4 +6,5 @@ import lotto.domain.Money
 interface InputView {
     fun receiveMoney(): Money
     fun receiveWinningNumbers(): List<LottoNumber>
+    fun receiveBonusNumber(): LottoNumber
 }
--- src/main/kotlin/lotto/view/inputView/LottoInputView.kt
+++ src/main/kotlin/lotto/view/inputView/LottoInputView.kt
@@ -18,9 +18,17 @@ class LottoInputView : InputView {
         return input.split(",").map { LottoNumber(it.toInt()) }
     }
 
+    override fun receiveBonusNumber(): LottoNumber {
+        println(RECEIVE_BONUS_NUMBER_MSG)
+        val input = readLine()
+        require(!input.isNullOrBlank()) { EMPTY_STRING_ERROR_MSG }
+        return LottoNumber(input.toInt())
+    }
+
     companion object {
         private const val RECEIVE_MONEY_MSG = "구입금액을 입력해 주세요."
         private const val RECEIVE_WINNING_NUMBERS_MSG = "지난 주 당첨 번호를 입력해 주세요."
+        private const val RECEIVE_BONUS_NUMBER_MSG = "보너스 볼을 입력해 주세요."
         private const val EMPTY_STRING_ERROR_MSG = "입력이 비어있습니다."
     }
 }
