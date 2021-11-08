d38de874e2168bb64e83cc126338f47c2f64eb2a
seunghwan <slflfl12@naver.com>
2021-11-08 23:30:29 +0900

    feat (step3) : Rank에서 bonusBall match에 관한 로직 추가

--- src/main/kotlin/lotto/domain/Rank.kt
+++ src/main/kotlin/lotto/domain/Rank.kt
@@ -1,18 +1,18 @@
 package lotto.domain
 
-enum class Rank(val matchCount: Int, val prize: Int) {
-    FIRST(6, 2_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5_000),
-    MISS(0, 0),
-    ;
+enum class Rank(val matchCount: Int, val prize: Int, val matchBonus: Boolean = false) {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
 
     fun prizeByCount(count: Int): Int = count * prize
 
     companion object {
-        fun rankByMatchCount(matchCount: Int): Rank {
-            return values().firstOrNull { it.matchCount == matchCount } ?: MISS
+        fun rankByMatchCount(matchCount: Int, matchBonus: Boolean): Rank {
+            return values().firstOrNull { it.matchCount == matchCount && it.matchBonus == matchBonus } ?: MISS
         }
     }
 }
