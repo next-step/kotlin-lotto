a234529591f0384237c036e4025ebbb6d6902cc5
seunghwan <slflfl12@naver.com>
2021-11-08 22:53:51 +0900

    feat (step3) : InputView에서 BonusBall 입력 받도록 수정

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -5,6 +5,7 @@ object InputView {
     const val MONEY_QUESTION = "구입금액을 입력해 주세요."
     const val WINNING_NUMBER_QUESTION = "지난 주 당첨 번호를 입력해 주세요."
     const val INPUT_NULL_POINTER_EXCEPTION_MESSAGE = "입력은 null일 수 없습니다"
+    const val BONUS_BALL_QUESTION = "보너스 볼을 입력해 주세요."
 
     fun getMoney(): String {
         println(MONEY_QUESTION)
@@ -13,6 +14,11 @@ object InputView {
 
     fun inputWinningNumbers(): String {
         println(WINNING_NUMBER_QUESTION)
-        return readLine() ?: throw IllegalArgumentException()
+        return readLine() ?: throw IllegalArgumentException(INPUT_NULL_POINTER_EXCEPTION_MESSAGE)
+    }
+
+    fun inputBonusBall(): String {
+        println(BONUS_BALL_QUESTION)
+        return readLine() ?: throw IllegalArgumentException(INPUT_NULL_POINTER_EXCEPTION_MESSAGE)
     }
 }
