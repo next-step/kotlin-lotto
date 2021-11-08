2c45f016df1051e44e7893e3007f4934c0f4b614
seunghwan <slflfl12@naver.com>
2021-11-08 21:16:37 +0900

    refactor (step2) : LottoResult value class로 수정

--- src/main/kotlin/lotto/domain/LottoResult.kt
+++ src/main/kotlin/lotto/domain/LottoResult.kt
@@ -3,7 +3,8 @@ package lotto.domain
 import java.math.RoundingMode
 import java.text.DecimalFormat
 
-data class LottoResult(val result: Map<Rank, Int>) {
+@JvmInline
+value class LottoResult private constructor(val result: Map<Rank, Int>) {
 
     private fun getTotalPrize(): Int {
         return result.keys.map { it.prizeByCount(result.getValue(it)) }.sum()
@@ -17,6 +18,10 @@ data class LottoResult(val result: Map<Rank, Int>) {
     companion object {
         val DECIMAL_FORMAT = DecimalFormat("#.##").apply {
             roundingMode = RoundingMode.HALF_UP
+        }
+
+        fun of(result: Map<Rank, Int>): LottoResult {
+            return LottoResult(result)
         }
     }
 }
