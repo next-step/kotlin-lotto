424e702216c18e98a6b553fc67daa2d01874edac
thgus5614@naver.com <thgus5614@naver.com>
2021-11-08 21:59:37 +0900

    add: 수동 로또 개수와 번호 입력받는 InputView 추가

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -18,4 +18,14 @@ class InputView {
         println("보너스 볼을 입력해 주세요.")
         return readLine()?.toIntOrNull()
     }
+
+    fun inputManualLottoCount(): Int? {
+        println("수동으로 구매할 로또 수를 입력해 주세요.")
+        return readLine()?.toIntOrNull()
+    }
+
+    fun inputManualLottoNumber(): String? {
+        println("수동으로 구매할 번호를 입력해 주세요.")
+        return readLine()
+    }
 }
