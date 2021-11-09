b2ed57fa29f64b8d5bd519fadde745d8b915b913
ssibongee <ssibongee@gmail.com>
2021-11-09 11:18:26 +0900

    refactor: 잘못된 계산 로직 수정 및 상수를 "===" 연산자를 사용하여 비교하도록 변경

--- src/main/kotlin/lotto/domain/Ranking.kt
+++ src/main/kotlin/lotto/domain/Ranking.kt
@@ -7,7 +7,7 @@ enum class Ranking(
 ) {
     FIRST(Money.from(2_000_000_000L), 6, { match, _ -> match == 6 }),
     SECOND(Money.from(30_000_000L), 5, { match, bonus -> match == 5 && bonus }),
-    THIRD(Money.from(1_500_000L), 5, { match, bonus -> match == 5 && bonus }),
+    THIRD(Money.from(1_500_000L), 5, { match, bonus -> match == 5 && !bonus }),
     FOURTH(Money.from(50_000L), 4, { match, _ -> match == 4 }),
     FIFTH(Money.from(5_000L), 3, { match, _ -> match == 3 }),
     NONE(Money.from(0L), 0, { match, _ -> match < 3 });
@@ -16,8 +16,8 @@ enum class Ranking(
         fun calculate(matches: Int, isContainBonusBall: Boolean): Ranking =
             values().find { it.isMatched(matches, isContainBonusBall) } ?: NONE
 
-        fun isNone(ranking: Ranking): Boolean = ranking == NONE
+        fun isNone(ranking: Ranking): Boolean = ranking === NONE
 
-        fun isSecond(ranking: Ranking): Boolean = ranking == SECOND
+        fun isSecond(ranking: Ranking): Boolean = ranking === SECOND
     }
 }
