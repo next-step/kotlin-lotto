b6e49a2a14949ae9d656f0644a1229004762f0ad
안운장 <hodol@woowahan.com>
2021-11-12 00:42:16 +0900

    feat: 로또 티켓 일급 콜렉션 (Tickets) 생성자 작성

--- src/test/kotlin/lotto/TicketsTest.kt
+++ src/test/kotlin/lotto/TicketsTest.kt
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource
 class TicketsTest {
 
     @ParameterizedTest
-    @CsvSource("14,14000", "10,10000")
+    @CsvSource("14,14000", "10,10000", "14,14999")
     fun `구입금액에 해당하는 로또 티켓들을 살 수 있다`(count: Int, value: Int) {
         val money = Money.of(value)
         val tickets = Tickets.from(money)
