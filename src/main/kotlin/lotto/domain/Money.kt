577a01b03966c69e43f6a9e359972c505a8b9417
nachwon <nachwon@gmail.com>
2021-11-11 21:57:18 +0900

    feat: Add more operators to Money

--- src/main/kotlin/lotto/domain/Money.kt
+++ src/main/kotlin/lotto/domain/Money.kt
@@ -7,6 +7,8 @@ data class Money(val value: Int) {
     }
 
     operator fun plus(other: Money) = Money(value + other.value)
+    operator fun minus(other: Money) = Money(value - other.value)
+    operator fun times(other: Int) = Money(value * other)
     operator fun div(other: Money) = value / other.value
     operator fun compareTo(other: Money) = value.compareTo(other.value)
 }
