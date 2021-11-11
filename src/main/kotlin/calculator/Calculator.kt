3e0a04c30e15e45e8770e6c18bc35cb969b64a23
안운장 <hodol@woowahan.com>
2021-11-11 23:23:14 +0900

    feat: 커스텀 구분자를 지정할 수 있도록 Calculator.add() 수정

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -4,6 +4,13 @@ class Calculator {
     fun add(value: String?): Int {
         if (value.isNullOrBlank()) return 0
         if (value.toIntOrNull() != null) return value.toInt()
+        val matchResult = CUSTOM_DELIMITER_REGEX.find(value)
+        if (matchResult != null) {
+            val (delimiter, expression) = matchResult.destructured
+            return expression
+                .split(*DEFAULT_DELIMITERS, delimiter)
+                .sumOf { it.toInt() }
+        }
         return value
             .split(*DEFAULT_DELIMITERS)
             .sumOf { it.toInt() }
@@ -11,6 +18,7 @@ class Calculator {
 
     companion object {
         val DEFAULT_DELIMITERS = arrayOf(",", ":")
+        val CUSTOM_DELIMITER_REGEX = Regex("//(.+)\\n(.+)")
     }
 }
 
