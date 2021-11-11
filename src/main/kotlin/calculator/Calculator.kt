d3b47edaac926fc1636e4c79a55d51b69907f91f
안운장 <hodol@woowahan.com>
2021-11-11 22:57:36 +0900

    feat: 숫자 하나가 입력된 경우 해당 숫자를 반환하도록 Calculator.add() 수정

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -3,6 +3,8 @@ package calculator
 class Calculator {
     fun add(value: String?): Int {
         if (value.isNullOrBlank()) return 0
+        if (value.toIntOrNull() != null) return value.toInt()
         return 0
     }
 }
+
