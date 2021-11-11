c4dedb50cb967670e2a566e03ff101baccd44856
pursue503 <pursue503@naver.com>
2021-11-11 22:30:46 +0900

    Fix (Step2): View 분리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,27 +1,5 @@
 package lotto
 
-import lotto.domain.LottoPrize
-import lotto.domain.LottoShop
-import lotto.domain.lotto.WinningLotto
-import lotto.view.InputView
-import lotto.view.ResultView
-
 fun main() {
-    val userLottoBuyMoney = InputView.inputLottoBuyPrice()
-
-    val userLottoList = LottoShop.createLottoTicket(userLottoBuyMoney)
-
-    ResultView.lottoPurchasesCount(userLottoList.size)
-
-    ResultView.printUserLotto(userLottoList)
-
-    val winningLotto = WinningLotto(InputView.lastWeekLottoNumber())
-
-    val lottoResult = LottoPrize(userLottoList, winningLotto)
-
-    ResultView.lottoResult(lottoResult.prizeResult)
-
-    val sum = lottoResult.totalPrizeMoney()
-
-    ResultView.statistics(sum, userLottoBuyMoney)
+    Play.start()
 }
---
+++ src/main/kotlin/lotto/Play.kt
@@ -0,0 +1,49 @@
+package lotto
+
+import lotto.domain.LottoPrize
+import lotto.domain.LottoShop
+import lotto.domain.lotto.Lotto
+import lotto.domain.lotto.WinningLotto
+import lotto.view.InputView
+import lotto.view.ResultView
+
+object Play {
+
+    fun start() {
+
+        val userLottoBuyMoney = userLottoBuy()
+
+        val userLottoList = userLottoInput(userLottoBuyMoney)
+
+        val winningLotto = winningLottoInput()
+
+        val totalPrizeMoney = lottoPrize(userLottoList, winningLotto)
+
+        lottoResult(totalPrizeMoney, userLottoBuyMoney)
+    }
+
+    private fun userLottoBuy(): Int = InputView.inputLottoBuyPrice()
+
+    private fun userLottoInput(userLottoBuyMoney: Int): List<Lotto> {
+
+        val userLottoList = LottoShop.createLottoTicket(userLottoBuyMoney)
+
+        ResultView.lottoPurchasesCount(userLottoList.size)
+
+        ResultView.printUserLotto(userLottoList)
+
+        return userLottoList
+    }
+
+    private fun winningLottoInput(): WinningLotto = WinningLotto(InputView.lastWeekLottoNumber())
+
+    private fun lottoPrize(userLottoList: List<Lotto>, winningLotto: WinningLotto): Int {
+        val lottoResult = LottoPrize(userLottoList, winningLotto)
+
+        ResultView.lottoResult(lottoResult.prizeResult)
+
+        return lottoResult.totalPrizeMoney()
+    }
+
+    private fun lottoResult(totalPrizeMoney: Int, userLottoBuyMoney: Int) = ResultView.statistics(totalPrizeMoney, userLottoBuyMoney)
+}
