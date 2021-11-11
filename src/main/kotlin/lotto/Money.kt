63bf6e0b221049892a187a4daf7139e0eceb3fdb
안운장 <hodol@woowahan.com>
2021-11-12 00:30:18 +0900

    feat: 구입금액(Money) 객체 생성자 작성

--- src/main/kotlin/lotto/Money.kt
+++ src/main/kotlin/lotto/Money.kt
@@ -1,10 +1,14 @@
 package lotto
 
+private const val MINIMUM_PRICE = 1000
+
 @JvmInline
 value class Money private constructor(val value: Int) {
     companion object {
-        fun of (value: String): Money {
-            TODO()
+        fun of(value: String?): Money {
+            require(!value.isNullOrBlank()) { "지불 금액은 비어있거나 null일 수 없습니다." }
+            require(value.toInt() > MINIMUM_PRICE) { "지불 금액은 최소 금액(1000원) 이상이어야 합니다." }
+            return Money(value.toInt())
         }
     }
 }
