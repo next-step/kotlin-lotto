d36cc64e464a2d47b46fbeecdfe549c6be37ec60
안운장 <hodol@woowahan.com>
2021-11-11 23:05:53 +0900

    refactor: 기본 구분자 상수로 추출 및 clean up

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -4,8 +4,13 @@ class Calculator {
     fun add(value: String?): Int {
         if (value.isNullOrBlank()) return 0
         if (value.toIntOrNull() != null) return value.toInt()
-        val values = value.split(",", ":").map { it.toInt() }
-        return values.sum()
+        return value
+            .split(*DEFAULT_DELIMITERS)
+            .sumOf { it.toInt() }
+    }
+
+    companion object {
+        val DEFAULT_DELIMITERS = arrayOf(",", ":")
     }
 }
 
