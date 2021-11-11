cfbe794ccf88ddf797bfeb43c8525629a04fe413
안운장 <hodol@woowahan.com>
2021-11-11 23:50:18 +0900

    feat: 문자열 계산기에 음수 또는 숫자가 아닌 값이 들어오는 경우 RuntimeException을 발생시키도록 Calculator.add() 수정

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -3,17 +3,24 @@ package calculator
 class Calculator {
     fun add(value: String?): Int {
         if (value.isNullOrBlank()) return 0
-        if (value.toIntOrNull() != null) return value.toInt()
+        if (value.toIntOrNull() != null && value.toInt() > 0) return value.toInt()
         val matchResult = CUSTOM_DELIMITER_REGEX.find(value)
         if (matchResult != null) {
-            val (delimiter, expression) = matchResult.destructured
-            return expression
-                .split(*DEFAULT_DELIMITERS, delimiter)
-                .sumOf { it.toInt() }
+            return calculateWithCustomDelimiter(matchResult)
         }
-        return value
-            .split(*DEFAULT_DELIMITERS)
-            .sumOf { it.toInt() }
+        return validated(value.split(*DEFAULT_DELIMITERS)).sum()
+    }
+
+    private fun calculateWithCustomDelimiter(matchResult: MatchResult): Int {
+        val (delimiter, expression) = matchResult.destructured
+        return validated(expression.split(*DEFAULT_DELIMITERS, delimiter)).sum()
+    }
+
+    private fun validated(numbers: List<String>): List<Int> {
+        if (!numbers.map { it.toIntOrNull() }.all { it != null && it >= 0 }) {
+            throw RuntimeException("음수나 숫자가 아닌 값을 입력할 수 없습니다.")
+        }
+        return numbers.map { it.toInt() }
     }
 
     companion object {
--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -2,7 +2,6 @@ package calculator
 
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.NullAndEmptySource
 import org.junit.jupiter.params.provider.ValueSource
@@ -13,36 +12,37 @@ class CalculatorTest {
     @ParameterizedTest
     @NullAndEmptySource
     fun `빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다`(text: String?) {
-        assertThat(calculator.add(text)).isZero;
+        assertThat(calculator.add(text)).isZero
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["1"])
     fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(text: String) {
-        assertThat(calculator.add(text)).isSameAs(Integer.parseInt(text));
+        assertThat(calculator.add(text)).isSameAs(Integer.parseInt(text))
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["1,2"])
     fun `숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다`(text: String) {
-        assertThat(calculator.add(text)).isSameAs(3);
+        assertThat(calculator.add(text)).isSameAs(3)
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["1,2:3"])
     fun `구분자를 쉼표 이외에 콜론을 사용할 수 있다`(text: String) {
-        assertThat(calculator.add(text)).isSameAs(6);
+        assertThat(calculator.add(text)).isSameAs(6)
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["//;\n1;2;3"])
     fun `커스텀 구분자를 지정할 수 있다`(text: String) {
-        assertThat(calculator.add(text)).isSameAs(6);
+        assertThat(calculator.add(text)).isSameAs(6)
     }
 
-    @Test
-    fun `문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다`() {
+    @ParameterizedTest
+    @ValueSource(strings = ["//;\n-1;2;3", "//;\n1;a;3", "-1", "1,a,b"])
+    fun `문자열 계산기에 음수 또는 숫자가 아닌 값을 전달하는 경우 RuntimeException 예외 처리를 한다`(text: String) {
         assertThatExceptionOfType(RuntimeException::class.java)
-            .isThrownBy { calculator.add("-1") };
+            .isThrownBy { calculator.add(text) }
     }
 }
