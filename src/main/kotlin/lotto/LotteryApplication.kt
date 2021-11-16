1d768dc43135e4291e05e3f19ff5632e655804ba
ssibongee <ssibongee@gmail.com>
2021-11-16 11:43:29 +0900

    feat: 수동으로 로또를 구매할 수 있도록 기능 추가

--- src/main/kotlin/lotto/LotteryApplication.kt
+++ src/main/kotlin/lotto/LotteryApplication.kt
@@ -1,15 +1,16 @@
 package lotto
 
-import lotto.domain.BonusBall
 import lotto.domain.LottoMachine
+import lotto.domain.LottoNumber
 import lotto.domain.Money
+import lotto.domain.Order
 import lotto.domain.WinningLottery
-import lotto.domain.strategy.LottoAutoGenerator
+import lotto.domain.strategy.LottoNumberAutoGenerator
 import lotto.ui.LottoView
 
 fun main() = LotteryApplication(
     LottoView,
-    LottoMachine(LottoAutoGenerator)
+    LottoMachine(LottoNumberAutoGenerator)
 ).run()
 
 class LotteryApplication(
@@ -19,15 +20,21 @@ class LotteryApplication(
     fun run() {
         view.inputPrompt()
         val money = Money.from(view.input())
-        val lotteryCounts = machine.count(money)
-        val lotteries = machine.buy(lotteryCounts)
-        val paid = machine.calculatePaid(lotteryCounts)
+
+        val total = machine.count(money)
+        val manual = view.inputManualCountPrompt()
+        val selected = view.inputSelectedLottoNumbersPrompt(manual)
+
+        val order = Order.of(total, manual, selected)
+
+        val lotteries = machine.buy(order)
+        val paid = machine.calculatePaid(order)
 
         view.displayLotteries(lotteries)
-        val winningNumbers = view.inputWinningNumbers()
+        val winningNumbers = view.inputNumbers()
 
         view.inputBonusBallPrompt()
-        val bonusBall = BonusBall.of(view.input())
+        val bonusBall = LottoNumber.of(view.input())
         val winningLottery = WinningLottery.of(winningNumbers, bonusBall)
 
         val statistics = machine.calculate(lotteries, winningLottery)
