5eaf153f11aa6c328cc6b86d74bf241732bb11e6
sanghun <tkdgnswjdgk4@gmail.com>
2021-11-19 01:41:42 +0900

    stringAddCalculator feat: 문자열 덧셈 계산기 구현

---
+++ src/main/kotlin/domain/StringAddCalculator.kt
@@ -0,0 +1,22 @@
+package domain
+
+import Message
+
+class StringAddCalculator {
+    fun calculate(input: String): Int {
+        if (input.isBlank()) return DEFAULT_RETURN_VALUE
+        val calculation = Calculation.getCalculation(input)
+        return calculation.splitExpression().flatMap { item ->
+            item.split(Calculation.ADD_DELIMITER.toRegex())
+        }.sumOf { data ->
+            require(data.toInt() > ZERO ) { throw RuntimeException(Message.ILLEGAL_MESSAGE) }
+            data.toInt()
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_RETURN_VALUE = 0
+        private const val ZERO = 0
+    }
+}
+
