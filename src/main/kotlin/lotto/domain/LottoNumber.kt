bcda55a91d95f093dba4f8ac3444ab8fa14c67d9
seunghwan <slflfl12@naver.com>
2021-11-20 17:49:56 +0900

    refactor (step4) : LOTTO_NUMBERS List -> Map으로 변경

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -15,21 +15,15 @@ value class LottoNumber private constructor(
         const val WRONG_LOTTO_NUM_MESSAGE = "잘못된 로또 번호입니다."
         const val NUMBER_FORMAT_EXCEPTION_MESSAGE = "로또 번호는 숫자여야 합니다."
 
-        val LOTTO_NUMBERS = (MIN_LOTTO_NUM..MAX_LOTTO_NUM).map { LottoNumber(it) }
+        val LOTTO_NUMBERS = (MIN_LOTTO_NUM..MAX_LOTTO_NUM).associateWith { LottoNumber(it) }
 
         fun from(value: Int): LottoNumber {
-            if (value !in MIN_LOTTO_NUM..MAX_LOTTO_NUM) {
-                throw IllegalArgumentException(WRONG_LOTTO_NUM_MESSAGE)
-            }
-            return LOTTO_NUMBERS[value - 1]
+            return LOTTO_NUMBERS[value] ?: throw IllegalArgumentException(WRONG_LOTTO_NUM_MESSAGE)
         }
 
         fun from(value: String): LottoNumber {
             val number = value.toIntOrNull() ?: throw IllegalArgumentException(NUMBER_FORMAT_EXCEPTION_MESSAGE)
-            if (number !in MIN_LOTTO_NUM..MAX_LOTTO_NUM) {
-                throw IllegalArgumentException(WRONG_LOTTO_NUM_MESSAGE)
-            }
-            return LOTTO_NUMBERS[number - 1]
+            return LOTTO_NUMBERS[number] ?: throw IllegalArgumentException(WRONG_LOTTO_NUM_MESSAGE)
         }
     }
 }
