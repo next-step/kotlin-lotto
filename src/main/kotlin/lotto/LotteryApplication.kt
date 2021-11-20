39a103683a11c5ad3871497cc7aa962fb2b6a253
ssibongee <ssibongee@gmail.com>
2021-11-20 14:06:24 +0900

    refactor: run 메서드가 너무 많은 책임을 가지지 않도록 분리

--- src/main/kotlin/lotto/LotteryApplication.kt
+++ src/main/kotlin/lotto/LotteryApplication.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import lotto.domain.Lotteries
 import lotto.domain.LottoMachine
 import lotto.domain.LottoNumber
 import lotto.domain.Money
@@ -18,25 +19,37 @@ class LotteryApplication(
     private val machine: LottoMachine
 ) {
     fun run() {
-        view.inputPrompt()
-        val money = Money.from(view.input())
-
-        val total = machine.count(money)
-        val manual = view.inputManualCountPrompt()
-        val selected = view.inputSelectedLottoNumbersPrompt(manual)
-
-        val order = Order.of(total, manual, selected)
-
+        val order = getOrder()
         val lotteries = machine.buy(order)
         val paid = machine.calculatePaid(order)
 
         view.displayLotteries(lotteries)
+
+        val winningLottery = getWinningLottery()
+
+        showResult(lotteries, winningLottery, paid)
+    }
+
+    private fun getOrder(): Order {
+        view.inputPrompt()
+        val money = Money.from(view.input())
+
+        val total = machine.count(money)
+        val manualLotteryCount = view.inputManualCountPrompt()
+        val selectedLottoNumbers = view.inputSelectedLottoNumbersPrompt(manualLotteryCount)
+
+        return Order.of(total, manualLotteryCount, selectedLottoNumbers)
+    }
+
+    private fun getWinningLottery(): WinningLottery {
         val winningNumbers = view.inputNumbers()
 
         view.inputBonusBallPrompt()
         val bonusBall = LottoNumber.of(view.input())
-        val winningLottery = WinningLottery.of(winningNumbers, bonusBall)
+        return WinningLottery.of(winningNumbers, bonusBall)
+    }
 
+    private fun showResult(lotteries: Lotteries, winningLottery: WinningLottery, paid: Money) {
         val statistics = machine.calculate(lotteries, winningLottery)
         val earn = machine.settle(statistics)
         val ratio = machine.calculateYield(paid, earn)
