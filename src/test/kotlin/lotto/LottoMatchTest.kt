476059d602e76b93d7157e53aa4c36edfa6c6908
pursue503 <pursue503@naver.com>
2021-11-23 15:31:45 +0900

    Fix (Step2): LottoMatchTest 수정

--- src/test/kotlin/lotto/LottoMatchTest.kt
+++ src/test/kotlin/lotto/LottoMatchTest.kt
@@ -10,51 +10,58 @@ import org.junit.jupiter.params.provider.CsvSource
 class LottoMatchTest {
 
     @ParameterizedTest
-    @CsvSource(value = ["1,2,3,11,22,33|1,2,3,14,24,35"], delimiterString = "|")
-    fun `3개이상이 매칭될경우 Map에 저장되어 반환됩니다`(userLottoNumber: String, winningLottoNumber: String) {
+    @CsvSource(
+        value = ["1,2,3,11,22,33|1,2,3,14,24,35|3", "1,11,22,33,41,45|1,11,22,33,40,44|4", "3,5,15,28,31,43|3,5,15,28,31,45|5", "9,19,29,31,35,42|9,19,29,31,35,42|6"],
+        delimiterString = "|"
+    )
+    fun `로또 번호가 3이상 매칭될경우 값이 카운팅(1) 되는걸 확인한다`(userLottoNumber: String, winningLottoNumber: String, matchCount: Int) {
 
-        val userLotto = listOf(
-            Lotto(
-                userLottoNumber
-                    .split(",")
-                    .map { it.trim().toInt() }
-                    .toList()
-            )
-        )
+        // given
+        val userLotto = convertUserLotto(userLottoNumber)
+        val winningLotto = convertWinningLotto(winningLottoNumber)
 
-        val winningLotto = WinningLotto(
-            winningLottoNumber
-                .split(",")
-                .map { it.trim().toInt() }
-                .toList()
-        )
-
+        // when
         val matchMap = LottoMatch.match(userLotto, winningLotto)
 
-        assertThat(matchMap).isNotEmpty
+        // then
+        assertThat(matchMap[matchCount]).isEqualTo(1)
     }
 
     @ParameterizedTest
-    @CsvSource(value = ["1,2,3,4,5,6|11,13,24,35,44,45"], delimiterString = "|")
-    fun `3개미만으로 매칭될경우 비어있는 Map 이 반환됩니다`(userLottoNumber: String, winningLottoNumber: String) {
-        val userLotto = listOf(
-            Lotto(
-                userLottoNumber
-                    .split(",")
-                    .map { it.trim().toInt() }
-                    .toList()
-            )
-        )
+    @CsvSource(
+        value = ["1,2,3,4,5,6|11,13,24,35,44,45|0", "1,11,22,33,41,45|1,12,23,34,42,44|1", "1,11,22,33,41,45|1,11,22,34,35,40|2"],
+        delimiterString = "|"
+    )
+    fun `로또 번호가 3개 미만으로 매칭될경우 카운팅 되는 값이 없는걸 확인한다`(
+        userLottoNumber: String,
+        winningLottoNumber: String,
+        matchCount: Int
+    ) {
 
-        val winningLotto = WinningLotto(
-            winningLottoNumber
+        // given
+        val userLotto = convertUserLotto(userLottoNumber)
+        val winningLotto = convertWinningLotto(winningLottoNumber)
+
+        // when
+        val matchMap = LottoMatch.match(userLotto, winningLotto)
+
+        // then
+        assertThat(matchMap[matchCount]).isNull()
+    }
+
+    private fun convertUserLotto(userLottoNumber: String): List<Lotto> = listOf(
+        Lotto(
+            userLottoNumber
                 .split(",")
                 .map { it.trim().toInt() }
                 .toList()
         )
+    )
 
-        val matchMap = LottoMatch.match(userLotto, winningLotto)
-
-        assertThat(matchMap).isEmpty()
-    }
+    private fun convertWinningLotto(winningLottoNumber: String): WinningLotto = WinningLotto(
+        winningLottoNumber
+            .split(",")
+            .map { it.trim().toInt() }
+            .toList()
+    )
 }
