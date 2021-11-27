edc5138f72353d310b9f5310563fe0e5a3eda62e
minji0320 <rlaalswl0320@naver.com>
2021-11-27 17:56:15 +0900

    feat: 숫자 하나를 문자열로 입력할 경우 해당 숫자 반환

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -2,8 +2,16 @@ package calculator
 
 class StringAddCalculator {
     fun calculate(text: String?): Int {
-        if (text.isNullOrEmpty())
+        if (text.isNullOrBlank())
             return 0
+
+        if (text.matches(NUM_REGEX))
+            return text.toInt()
+
         return 1
+    }
+
+    companion object {
+        val NUM_REGEX = "\\d+".toRegex()
     }
 }
