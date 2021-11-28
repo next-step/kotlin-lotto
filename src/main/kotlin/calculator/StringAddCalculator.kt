41ba3d8b30a834c6c4e062c3565447f7c3618e2b
minji0320 <43491968+minji0320@users.noreply.github.com>
2021-11-28 16:23:00 +0900

    [Step1] 문자열 덧셈 계산기 (#238)
    * docs: 1단계 - 문자열 덧셈 계산기 요구사항 및 구현 목록 작성
    
    * feat: 입력값이 빈 문자열 또는 null인 경우 0 반환
    
    * test: 입력값이 빈 문자열 또는 null인 경우 0 반환
    
    * feat: 숫자 하나를 문자열로 입력할 경우 해당 숫자 반환
    
    * test: 숫자 하나를 문자열로 입력할 경우 해당 숫자 반환
    
    * feat: 컴마와 콜론을 구분자로 입력했을 때의 숫자들의 합 계산
    
    * test: 컴마와 콜론을 구분자로 입력했을 때의 숫자들의 합 계산
    
    * feat: 커스텀 구분자 지정하여 숫자들의 합 구하기
    
    * test: 커스텀 구분자 지정하여 숫자들의 합 구하기
    
    * feat: 입력값에 음수가 있는 경우 RuntimeException 예외 처리
    
    * test: 입력값에 음수가 있는 경우 RuntimeException 예외 처리

---
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -0,0 +1,35 @@
+package calculator
+
+class StringAddCalculator {
+    fun calculate(text: String?): Int {
+        if (text.isNullOrBlank())
+            return 0
+
+        CUSTOM_DELIMITER_REGEX.find(text)?.let {
+            val customDelimiter = it.groupValues[1].toRegex()
+            return parseToNum(it.groupValues[2], customDelimiter).sum()
+        }
+
+        return parseToNum(text).sum()
+    }
+
+    private fun parseToNum(text: String, delimiter: Regex = COMMA_AND_COLON_REGEX): List<Int> {
+        val stringNums = text.split(delimiter)
+        return stringNums.map { convertToInt(it) }
+    }
+
+    private fun convertToInt(num: String): Int {
+        return if (num.isNullOrBlank())
+            0
+        else if (num.matches(NUM_REGEX))
+            num.toInt()
+        else
+            throw RuntimeException("음수나 숫자가 아닌 값을 입력할 수 없습니다.")
+    }
+
+    companion object {
+        val NUM_REGEX = "\\d+".toRegex()
+        val COMMA_AND_COLON_REGEX = ",|:".toRegex()
+        val CUSTOM_DELIMITER_REGEX = "//(.)\\n(.*)".toRegex()
+    }
+}
---
+++ src/test/kotlin/calculator/StringAddCalculatorTest.kt
@@ -0,0 +1,49 @@
+package calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class StringAddCalculatorTest {
+    private val calculator = StringAddCalculator()
+
+    @Test
+    fun `입력값이 빈 문자열 또는 null인 경우 0을 반환`() {
+        assertThat(calculator.calculate("")).isEqualTo(0)
+        assertThat(calculator.calculate(null)).isEqualTo(0)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "5", "17"])
+    fun `숫자 하나를 문자열로 입력할 경우 해당 숫자 반환`(text: String) {
+        assertThat(calculator.calculate(text)).isEqualTo(text.toInt())
+    }
+
+    @Test
+    fun `컴마를 구분자로 사용하는 경우`() {
+        assertThat(calculator.calculate(",")).isEqualTo(0)
+        assertThat(calculator.calculate("1,2")).isEqualTo(3)
+        assertThat(calculator.calculate("1,2,3")).isEqualTo(6)
+    }
+
+    @Test
+    fun `컴마와 콜론을 구분자로 사용하는 경우`() {
+        assertThat(calculator.calculate(",2:3")).isEqualTo(5)
+        assertThat(calculator.calculate("1,2:3")).isEqualTo(6)
+        assertThat(calculator.calculate("1,2:3:4")).isEqualTo(10)
+    }
+
+    @Test
+    fun `커스텀 구분자 지정하여 숫자들의 합 구하기`() {
+        assertThat(calculator.calculate("//!\n1!2!3")).isEqualTo(6)
+        assertThat(calculator.calculate("//;\n1;2;3;4")).isEqualTo(10)
+    }
+
+    @Test
+    fun `입력값에 음수가 있는 경우 RuntimeException 예외 처리`() {
+        assertThrows<RuntimeException> { calculator.calculate("-5") }
+        assertThrows<RuntimeException> { calculator.calculate("1,-2:3:-4") }
+    }
+}
