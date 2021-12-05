4fbd6283758986b06aa4e69e5272b31262a62daf
pursue503 <pursue503@naver.com>
2021-12-06 02:28:46 +0900

    Update (Step3): InputView 보너스 번호 받는 함수 추가

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -4,6 +4,7 @@ object InputView {
 
     private const val LOTTO_BUY_MESSAGE = "구매 금액을 입력해주세요"
     private const val LAST_WEEK_LOTTO_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요."
+    private const val LAST_WEEK_LOTTO_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해주세요."
 
     fun inputLottoBuyPrice(): Int {
         println(LOTTO_BUY_MESSAGE)
@@ -15,4 +16,10 @@ object InputView {
         println(LAST_WEEK_LOTTO_NUMBER_MESSAGE)
         return readLine()!!
     }
+
+    fun lastWeekBonusNumber(): Int {
+        println(LAST_WEEK_LOTTO_BONUS_NUMBER_MESSAGE)
+        return readLine()!!.toInt()
+    }
+
 }
