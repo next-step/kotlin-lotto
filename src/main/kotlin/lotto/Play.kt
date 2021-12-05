38616917c06a3a1c757f5fd5423029a251e18eee
pursue503 <pursue503@naver.com>
2021-12-06 02:30:48 +0900

    Update (Step3): Play 로직 추가된 View에 맞춰서 변경

--- src/main/kotlin/lotto/Play.kt
+++ src/main/kotlin/lotto/Play.kt
@@ -5,7 +5,9 @@ import lotto.domain.LottoPrize
 import lotto.domain.LottoShop
 import lotto.domain.entity.generator.LottoGenerator
 import lotto.domain.entity.user.Lotto
+import lotto.domain.entity.winning.BonusNumber
 import lotto.domain.entity.winning.WinningLotto
+import lotto.filter.LottoFilter
 import lotto.filter.LottoMoneyFilter
 import lotto.view.InputView
 import lotto.view.ResultView
@@ -20,7 +22,9 @@ object Play {
 
         val winningLotto = winningLottoInput()
 
-        val totalPrizeMoney = lottoPrize(userLottoList, winningLotto)
+        val bonusNumber = bonusNumberInput()
+
+        val totalPrizeMoney = lottoPrize(userLottoList, winningLotto, bonusNumber)
 
         lottoResult(totalPrizeMoney, userLottoBuyMoney)
     }
@@ -42,9 +46,16 @@ object Play {
 
     private fun winningLottoInput(): WinningLotto = WinningLotto(LottoGenerator.generatorWinningLotto(InputView.lastWeekLottoNumber()))
 
-    private fun lottoPrize(userLottoList: List<Lotto>, winningLotto: WinningLotto): Int {
+    private fun bonusNumberInput(): BonusNumber {
 
-        val lottoPrize = LottoPrize(LottoMatch.match(userLottoList, winningLotto))
+        val bonusNumber = LottoFilter.verify(InputView.lastWeekBonusNumber())
+
+        return BonusNumber(bonusNumber)
+    }
+
+    private fun lottoPrize(userLottoList: List<Lotto>, winningLotto: WinningLotto, bonusNumber: BonusNumber): Int {
+
+        val lottoPrize = LottoPrize(LottoMatch.match(userLottoList, winningLotto, bonusNumber))
 
         ResultView.lottoResult(lottoPrize.getPrize)
 
