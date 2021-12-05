e606a681b84def76d2845826427183aead9c4ae7
pursue503 <pursue503@naver.com>
2021-12-06 02:29:36 +0900

    Update (Step3): PrizeType 2등 타입 추가 및 함수 변경

--- src/main/kotlin/lotto/domain/enums/PrizeType.kt
+++ src/main/kotlin/lotto/domain/enums/PrizeType.kt
@@ -4,23 +4,30 @@ enum class PrizeType(
     val match: Int,
     val money: Int
 ) {
-
-    THREE(3, 5_000),
-    FOUR(4, 50_000),
-    FIVE(5, 1_500_000),
-    SIX(6, 2_000_000_000);
+    FIRST_PLACE(6, 2_000_000_000),
+    SECOND_PLACE(5, 30_000_000),
+    THIRD_PLACE(5, 1_500_000),
+    FOURTH_PLACE(4, 50_000),
+    FIFTH_PLACE(3, 5_000),
+    OTHER_PLACE(0, 0);
 
     companion object {
-        fun findPrizeMoney(match: Int): Int {
-            return values()
-                .find { it.match == match }
-                ?.money ?: 0
+
+        private const val SECOND_AND_THIRD_PLACE_MATCH_COUNT = 5
+
+        fun findPrize(match: Int, bonusMatch: Boolean): PrizeType {
+            return when (match) {
+                SECOND_AND_THIRD_PLACE_MATCH_COUNT -> isBonus(bonusMatch)
+                else -> values()
+                    .find { it.match == match } ?: OTHER_PLACE
+            }
         }
 
-        fun containsMatch(match: Int): Int {
-            return values()
-                .find { it.match == match }
-                ?.match ?: 0
+        fun isBonus(bonusMatch: Boolean): PrizeType {
+            if (bonusMatch)
+                return SECOND_PLACE
+
+            return THIRD_PLACE
         }
     }
 }
