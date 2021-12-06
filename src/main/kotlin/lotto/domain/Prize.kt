31e1ed729a1956258d85eb318a3ef3a1b147aec2
minji0320 <rlaalswl0320@naver.com>
2021-12-06 20:30:55 +0900

    refactor: 숫자 가독성을 위한 언더스코어 활용

--- src/main/kotlin/lotto/domain/Prize.kt
+++ src/main/kotlin/lotto/domain/Prize.kt
@@ -1,10 +1,10 @@
 package lotto.domain
 
 enum class Prize(val money: Int, val matchCount: Int) {
-    FIRST(2000000000, 6),
-    SECOND(1500000, 5),
-    THIRD(50000, 4),
-    FOURTH(5000, 3),
+    FIRST(2_000_000_000, 6),
+    SECOND(1_500_000, 5),
+    THIRD(50_000, 4),
+    FOURTH(5_000, 3),
     NONE(0, 0);
 
     companion object {
