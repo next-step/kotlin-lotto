1ab60ad29c2970204d410d68883260743df3f9bf
pursue503 <pursue503@naver.com>
2021-12-07 14:39:19 +0900

    Fix (Step3): non-null assertion 제거

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -5,20 +5,20 @@ object InputView {
     private const val LOTTO_BUY_MESSAGE = "구매 금액을 입력해주세요"
     private const val LAST_WEEK_LOTTO_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요."
     private const val LAST_WEEK_LOTTO_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해주세요."
+    private const val DEFAULT_WINNING_LOTTO_NUMBER = "1,2,3,4,5,6"
 
     fun inputLottoBuyPrice(): Int {
         println(LOTTO_BUY_MESSAGE)
-        return readLine()!!
-            .toInt()
+        return readLine()?.toInt() ?: 0
     }
 
     fun lastWeekLottoNumber(): String {
         println(LAST_WEEK_LOTTO_NUMBER_MESSAGE)
-        return readLine()!!
+        return readLine() ?: DEFAULT_WINNING_LOTTO_NUMBER
     }
 
     fun lastWeekBonusNumber(): Int {
         println(LAST_WEEK_LOTTO_BONUS_NUMBER_MESSAGE)
-        return readLine()!!.toInt()
+        return readLine()?.toInt() ?: 0
     }
 }
