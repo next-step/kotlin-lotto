726d7c217c9d65a4ba02ffd8675b41a69ce101bb
pursue503 <pursue503@naver.com>
2021-12-07 14:30:23 +0900

    Fix (Step3): LottoShop SubList -> take 로 변경

--- src/main/kotlin/lotto/domain/LottoShop.kt
+++ src/main/kotlin/lotto/domain/LottoShop.kt
@@ -6,7 +6,7 @@ object LottoShop {
 
     private const val LOTTO_MIN_NUMBER = 1
     private const val LOTTO_MAX_NUMBER = 45
-    private val LOTTO_RANGE = arrayOf(0, 6)
+    private val LOTTO_LENGTH = 6
 
     fun createLottoTicket(ticketingCount: Int): List<Lotto> {
 
@@ -17,7 +17,7 @@ object LottoShop {
                 Lotto(
                     lottoNumber = (LOTTO_MIN_NUMBER..LOTTO_MAX_NUMBER)
                         .shuffled()
-                        .subList(LOTTO_RANGE[0], LOTTO_RANGE[1])
+                        .take(LOTTO_LENGTH)
                         .sorted()
                 )
             )
