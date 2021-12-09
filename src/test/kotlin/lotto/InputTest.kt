e057917cd23242140aba69129952f43d3813fa38
minji0320 <rlaalswl0320@naver.com>
2021-12-09 18:35:07 +0900

    refactor: 로또 번호 입력값 테스트 수정

--- src/test/kotlin/lotto/InputTest.kt
+++ src/test/kotlin/lotto/InputTest.kt
@@ -33,35 +33,35 @@ class InputTest {
     }
 
     @Test
-    fun `입력한 당첨 번호가 null 또는 빈 문자열인 경우`() {
-        assertThrows<IllegalArgumentException> { inputView.parseToWinningNums(null) }
-        assertThrows<IllegalArgumentException> { inputView.parseToWinningNums("") }
+    fun `입력한 로또 번호가 null 또는 빈 문자열인 경우`() {
+        assertThrows<IllegalArgumentException> { inputView.parseToLottoNums(null) }
+        assertThrows<IllegalArgumentException> { inputView.parseToLottoNums("") }
     }
 
     @Test
-    fun `입력한 당첨 번호에 쉼표나 숫자가 아닌 값이 포함되어 있는 경우`() {
-        assertThrows<IllegalArgumentException> { inputView.parseToWinningNums("a,b,c,d,e,f") }
-        assertThrows<IllegalArgumentException> { inputView.parseToWinningNums("!@#") }
+    fun `입력한 로또 번호에 쉼표나 숫자가 아닌 값이 포함되어 있는 경우`() {
+        assertThrows<IllegalArgumentException> { inputView.parseToLottoNums("a,b,c,d,e,f") }
+        assertThrows<IllegalArgumentException> { inputView.parseToLottoNums("!@#") }
     }
 
     @Test
-    fun `입력한 당첨 번호가 총 6개가 아닌 경우`() {
-        assertThrows<IllegalArgumentException> { inputView.parseToWinningNums("1,2,3,4") }
+    fun `입력한 로또 번호가 총 6개가 아닌 경우`() {
+        assertThrows<IllegalArgumentException> { inputView.parseToLottoNums("1,2,3,4") }
     }
 
     @Test
-    fun `입력한 당첨 번호에 중복된 숫자가 있는 경우`() {
-        assertThrows<IllegalArgumentException> { inputView.parseToWinningNums("1,1,2,3,4,5") }
+    fun `입력한 로또 번호에 중복된 숫자가 있는 경우`() {
+        assertThrows<IllegalArgumentException> { inputView.parseToLottoNums("1,1,2,3,4,5") }
     }
 
     @Test
-    fun `입력한 당첨 번호가 1 ~ 45 사이의 숫자가 아닌 경우`() {
-        assertThrows<IllegalArgumentException> { inputView.parseToWinningNums("1,2,3,4,5,50") }
+    fun `입력한 로또 번호가 1 ~ 45 사이의 숫자가 아닌 경우`() {
+        assertThrows<IllegalArgumentException> { inputView.parseToLottoNums("1,2,3,4,5,50") }
     }
 
     @Test
-    fun `당첨 번호를 바르게 입력한 경우`() {
-        assertDoesNotThrow { inputView.parseToWinningNums("1,2,3,4,5,6") }
+    fun `로또 번호를 바르게 입력한 경우`() {
+        assertDoesNotThrow { inputView.parseToLottoNums("1,2,3,4,5,6") }
     }
 
     @Test
