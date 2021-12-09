552679da7efc83583c86c7ce7978abb799683f5c
minji0320 <rlaalswl0320@naver.com>
2021-12-09 16:59:49 +0900

    test: 수동으로 구매할 로또 개수 입력값 테스트, 입력한 구입 금액이 1000 미만인 경우 테스트

--- src/test/kotlin/lotto/InputTest.kt
+++ src/test/kotlin/lotto/InputTest.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import lotto.domain.Lotto
+import lotto.domain.LottoNum
 import lotto.view.InputView
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
@@ -9,7 +10,11 @@ import java.lang.IllegalArgumentException
 
 class InputTest {
     private val inputView = InputView()
-    private val winningNums = Lotto(setOf(1, 2, 3, 4, 5, 6))
+    private val winningNums = Lotto(
+        listOf(1, 2, 3, 4, 5, 6)
+            .map { LottoNum(it) }
+            .toSet()
+    )
 
     @Test
     fun `로또 구입금액에 빈 문자열 또는 null을 입력한 경우`() {
@@ -20,6 +25,11 @@ class InputTest {
     @Test
     fun `로또 구입금액에 숫자가 아닌 값을 입력한 경우`() {
         assertThrows<IllegalArgumentException> { InputView.validateMoney("abc") }
+    }
+
+    @Test
+    fun `입력한 로또 구입금액이 1000 미만인 경우`() {
+        assertThrows<IllegalArgumentException> { InputView.validateMoney("100") }
     }
 
     @Test
@@ -73,5 +83,21 @@ class InputTest {
     @Test
     fun `보너스 볼 번호가 당첨 번호에 이미 존재하는 번호인 경우`() {
         assertThrows<IllegalArgumentException> { InputView.validateBonusNum("1", winningNums) }
+    }
+
+    @Test
+    fun `수동으로 구입할 로또 개수의 입력값이 null 또는 빈 문자열인 경우`() {
+        assertThrows<IllegalArgumentException> { InputView.validateManualCount(1, null) }
+        assertThrows<IllegalArgumentException> { InputView.validateManualCount(1, "") }
+    }
+
+    @Test
+    fun `수동으로 구입할 로또 개수의 입력값이 숫자가 아닌 경우`() {
+        assertThrows<IllegalArgumentException> { InputView.validateManualCount(1, "abc") }
+    }
+
+    @Test
+    fun `수동으로 구입할 로또 개수가 구입 가능한 로또 개수보다 많은 경우`() {
+        assertThrows<IllegalArgumentException> { InputView.validateManualCount(1, "3") }
     }
 }
