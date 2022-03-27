88ef98a85961808e316295dc531f9112894f22fb
jaenyeong <jaenyeong.dev@gmail.com>
2022-03-27 17:00:34 +0900

    feat(Money at el): Add `Money` value class

---
+++ src/main/kotlin/lotto/domain/money/Money.kt
@@ -0,0 +1,29 @@
+package lotto.domain.money
+
+@JvmInline
+value class Money(val value: Int) {
+    init {
+        assertGivenValueIsValid(value)
+    }
+
+    private fun assertGivenValueIsValid(value: Int) {
+        require(isGreaterThanZero(value) && isMultipleOfTicketDefaultPrice(value)) {
+            IllegalArgumentException(ERR_INVALID_MONEY_VALUE)
+        }
+    }
+
+    private fun isGreaterThanZero(value: Int): Boolean =
+        MIN_VALUE < value
+
+    private fun isMultipleOfTicketDefaultPrice(value: Int): Boolean =
+        value % DEFAULT_TICKET_PRICE == 0
+
+    val howManyBuyTicket: Int
+        get() = value / DEFAULT_TICKET_PRICE
+
+    companion object {
+        const val MIN_VALUE = 0
+        const val DEFAULT_TICKET_PRICE: Int = 1_000
+        const val ERR_INVALID_MONEY_VALUE = "[ERROR] The given value must be greater than $MIN_VALUE and multiple of $DEFAULT_TICKET_PRICE"
+    }
+}
