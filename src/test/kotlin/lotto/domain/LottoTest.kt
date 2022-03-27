38377389ee58f6f103312623dd4a9e0e4b1af265
Suhyeon Kim <wisemuji@gmail.com>
2022-03-27 16:12:20 +0900

    test: add tests for Lotto.match()

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -24,6 +24,16 @@ internal class LottoTest {
         assertThat(lotto.numbers.size).isEqualTo(6)
     }
 
+    @ParameterizedTest
+    @MethodSource("provideMatchCount")
+    internal fun `기준이 되는 로또 숫자와 비교했을 때 몇 개의 숫자가 동일한지 확인할 수 있다`(
+        candidateNumbers: List<Int>, matchCount: Int
+    ) {
+        val candidate = Lotto(candidateNumbers)
+        val targetLotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        assertThat(candidate.match(targetLotto)).isEqualTo(matchCount)
+    }
+
     companion object {
         @JvmStatic
         private fun provideInvalidNumbers(): Stream<Arguments?>? {
@@ -32,6 +42,16 @@ internal class LottoTest {
                 Arguments.of(listOf(1, 1, 3, 4, 5, 6)),
                 Arguments.of(listOf(1, 2, 3, 4, 5, 6, 7)),
                 Arguments.of(listOf(1))
+            )
+        }
+
+        @JvmStatic
+        private fun provideMatchCount(): Stream<Arguments?>? {
+            return Stream.of(
+                Arguments.of(listOf(1, 2, 3, 4, 5, 6), 6),
+                Arguments.of(listOf(1, 2, 3, 4, 5, 7), 5),
+                Arguments.of(listOf(1, 2, 3, 10, 11, 12), 3),
+                Arguments.of(listOf(11, 12, 13, 14, 15, 16), 0)
             )
         }
     }
