348a8be448db1d75d46e0593095fa449d9f10d09
jaenyeong <jaenyeong.dev@gmail.com>
2022-04-05 22:47:52 +0900

    refactor(PrizeMoney at el): Change `PrizeMoney` value class to enum class

--- src/main/kotlin/lotto/domain/money/PrizeMoney.kt
+++ src/main/kotlin/lotto/domain/money/PrizeMoney.kt
@@ -1,29 +1,9 @@
 package lotto.domain.money
 
-@JvmInline
-value class PrizeMoney private constructor(
-    val value: Int
-) {
-    init {
-        require(MIN_VALUE <= value) {
-            IllegalArgumentException(ERR_INVALID_PRIZE_MONEY_VALUE)
-        }
-    }
-
-    companion object {
-        private const val MIN_VALUE: Int = 0
-        const val ERR_INVALID_PRIZE_MONEY_VALUE = "[ERROR] The given value must be equal to $MIN_VALUE or greater than $MIN_VALUE"
-
-        const val FIRST_PLACE_PRIZE: Int = 2_000_000_000
-        const val THIRD_PLACE_PRIZE: Int = 1_500_000
-        const val FOURTH_PLACE_PRIZE: Int = 50_000
-        const val FIFTH_PLACE_PRIZE: Int = 5_000
-        const val LAST_PLACE_PRIZE: Int = 0
-
-        val firstPlacePrize: PrizeMoney = PrizeMoney(FIRST_PLACE_PRIZE)
-        val thirdPlacePrize: PrizeMoney = PrizeMoney(THIRD_PLACE_PRIZE)
-        val fourthPlacePrize: PrizeMoney = PrizeMoney(FOURTH_PLACE_PRIZE)
-        val fifthPlacePrize: PrizeMoney = PrizeMoney(FIFTH_PLACE_PRIZE)
-        val lastPlacePrize: PrizeMoney = PrizeMoney(LAST_PLACE_PRIZE)
-    }
+enum class PrizeMoney(val prize: Int) {
+    FIRST_PLACE_PRIZE(2_000_000_000),
+    SECOND_PLACE_PRIZE(1_500_000),
+    THIRD_PLACE_PRIZE(50_000),
+    FOURTH_PLACE_PRIZE(5_000),
+    LAST_PLACE_PRIZE(0);
 }
