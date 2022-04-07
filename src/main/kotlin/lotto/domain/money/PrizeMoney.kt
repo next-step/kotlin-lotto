4c94592dad18424104113a3184092f3123f4ce62
jaenyeong <jaenyeong.dev@gmail.com>
2022-04-07 13:44:32 +0900

    refactor(PrizeMoney): Delete as it is no longer used

--- src/main/kotlin/lotto/domain/money/PrizeMoney.kt
+++
@@ -1,9 +0,0 @@
-package lotto.domain.money
-
-enum class PrizeMoney(val prize: Int) {
-    FIRST_PLACE_PRIZE(2_000_000_000),
-    SECOND_PLACE_PRIZE(1_500_000),
-    THIRD_PLACE_PRIZE(50_000),
-    FOURTH_PLACE_PRIZE(5_000),
-    LAST_PLACE_PRIZE(0);
-}
