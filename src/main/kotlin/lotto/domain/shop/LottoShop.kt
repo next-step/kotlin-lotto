4ab41448d667841244175f9a82e3c896912fd091
jaenyeong <jaenyeong.dev@gmail.com>
2022-04-10 14:09:58 +0900

    feat(LottoShop at el): Add `LottoShop` object to buy lotto quickPick

---
+++ src/main/kotlin/lotto/domain/shop/LottoShop.kt
@@ -0,0 +1,14 @@
+package lotto.domain.shop
+
+import lotto.domain.money.BuyMoney
+import lotto.domain.ticket.LottoGame
+
+object LottoShop {
+    private const val START_TICKET = 1
+
+    fun quickPickBuy(money: BuyMoney): List<LottoGame> {
+        return (START_TICKET..money.howManyBuyTicket).map {
+            LottoGame.auto()
+        }
+    }
+}
