4d1574664768459c015afcad30040923b06397d2
revfactory <revfactory@gmail.com>
2022-05-18 00:21:25 +0900

    feat: Add Runtime Exception handling for negative numbers

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,11 +1,18 @@
 package calculator
 
-object Calculator {
-    fun calculate(input: String?): Int {
-        if(input.isNullOrBlank()) {
-            return 0
-        }
-        return input.split(",", ":")
-            .sumOf{ it.toInt() }
+fun calculate(input: String?): Int {
+    if (input.isNullOrBlank()) {
+        return 0
     }
+
+    val result = Regex("//(.)\n(.*)").find(input)
+    val lists = result?.let {
+        val customDelimiter = it.groupValues[1]
+        it.groupValues[2].split(customDelimiter)
+    } ?: input.split(",", ":")
+
+    return lists
+        .map { it.toInt() }
+        .onEach { if (it < 0) throw RuntimeException() }
+        .sumOf { it }
 }
--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -1,24 +1,35 @@
 package calculator
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class CalculatorTest : FunSpec({
 
     test("빈 문자열 또는 null을 입력할 경우 0을 반환해야 한다") {
-        Calculator.calculate("") shouldBe 0
-        Calculator.calculate(null) shouldBe 0
+        calculate("") shouldBe 0
+        calculate(null) shouldBe 0
     }
 
     test("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.") {
-        Calculator.calculate("1") shouldBe 1
+        calculate("1") shouldBe 1
     }
 
     test("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.") {
-        Calculator.calculate("1,2") shouldBe 3
+        calculate("1,2") shouldBe 3
     }
 
     test("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다. (예 : “1,2:3” => 6)") {
-        Calculator.calculate("1,2:3") shouldBe 6
+        calculate("1,2:3") shouldBe 6
+    }
+
+    test("\"//\"와 \"\\n\" 문자 사이에 커스텀 구분자를 지정할 수 있다. (예 : “//;\\n1;2;3” => 6)") {
+        calculate("//;\n1;2;3") shouldBe 6
+    }
+
+    test("음수를 전달할 경우 RuntimeException 예외가 발생해야 한다.") {
+        shouldThrow<java.lang.RuntimeException> {
+            calculate("-1,2,3")
+        }
     }
 })
