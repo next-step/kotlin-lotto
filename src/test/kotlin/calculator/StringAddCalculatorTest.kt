f357919b010d5ea925a253fac10fc6a210a357d4
Juatae-Kim <jutaekim@sk.com>
2022-05-17 23:17:37 +0900

    feat(StringAddCalculator) : Return int value when inputString contains a number only

--- src/test/kotlin/calculator/StringAddCalculatorTest.kt
+++ src/test/kotlin/calculator/StringAddCalculatorTest.kt
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class StringAddCalculatorTest {
 
@@ -19,5 +20,11 @@ internal class StringAddCalculatorTest {
     @NullAndEmptySource
     fun `빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다`(text: String?) {
         assertThat(calculator.add(text)).isZero
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "2", "3", "4", "100"])
+    fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(text: String) {
+        assertThat(calculator.add(text)).isSameAs(text.toInt())
     }
 }
