4990426a347b36993360ad36912089a7c5a6c8f1
saintbeller96 <rlawhdtjd9@naver.com>
2022-05-18 18:26:40 +0900

    refactor: 구분자

--- src/main/kotlin/stringaddcalculator/domain/Separator.kt
+++ src/main/kotlin/stringaddcalculator/domain/Separator.kt
@@ -1,10 +1,11 @@
 package stringaddcalculator.domain
 
-class Separator(val value: String) {
+@JvmInline
+value class Separator(val value: String) {
     init {
         require(value.isNotEmpty()) { "구분자의 길이는 1 이상이어야 합니다" }
         require(value.isNotNumeric()) { "구분자는 숫자일 수 없습니다" }
     }
 }
 
-fun String.isNotNumeric(): Boolean = this.toIntOrNull() == null
+fun String.isNotNumeric(): Boolean = toCharArray().none { it.isDigit() }
