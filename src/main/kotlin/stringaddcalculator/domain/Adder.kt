dd0d8c2586556133b8e58a54df2d357cfab8d215
saintbeller96 <rlawhdtjd9@naver.com>
2022-05-18 18:02:15 +0900

    refactor: 덧셈기

--- src/main/kotlin/stringaddcalculator/domain/Adder.kt
+++ src/main/kotlin/stringaddcalculator/domain/Adder.kt
@@ -1,6 +1,6 @@
 package stringaddcalculator.domain
 
-class Adder {
+object Adder {
     fun add(operands: List<Operand>): Int {
         require(operands.isNotEmpty()) { "더할 수 있는 피연산자가 없습니다" }
         return operands.sumOf { it.value }
--- src/test/kotlin/stringaddcalculator/domain/AdderSpecs.kt
+++ src/test/kotlin/stringaddcalculator/domain/AdderSpecs.kt
@@ -14,14 +14,14 @@ class AdderSpecs : DescribeSpec({
                 Operand(3)
             )
             it("모든 피연산자를 더한 값을 반환할 수 있다") {
-                Adder().add(operands) shouldBe 6
+                Adder.add(operands) shouldBe 6
             }
         }
         context("주어진 피연산자들이 비어있다면") {
             val operands = emptyList<Operand>()
             it("예외를 발생시킨다") {
                 shouldThrowExactly<IllegalArgumentException> {
-                    Adder().add(operands)
+                    Adder.add(operands)
                 }
             }
         }
