ecf710288d5c7eb0f38919eb8240b3f4245dd339
Molly <dngwoodo@gmail.com>
2022-05-19 01:25:44 +0900

    DelimiterFinder 구현

---
+++ src/main/kotlin/DelimiterFinder.kt
@@ -0,0 +1,13 @@
+object DelimiterFinder {
+    fun find(formula: String): List<String> {
+        val customDelimiter = Regex("//(.)\n(.*)").find(formula)
+
+        customDelimiter?.let {
+            return DEFAULT_DELIMITER + listOf(customDelimiter.groupValues[1])
+        }
+
+        return DEFAULT_DELIMITER
+    }
+
+    private val DEFAULT_DELIMITER = listOf(":", ",")
+}
