675842133e98fc53eba2b5da364489954a0ce726
vivian.bae <vivian.bae@kakaocorp.com>
2022-05-18 19:28:38 +0900

    4 - 쉽표, 콜론 구분자를 같이 입력할 경우에도 모든 숫자의 합을 반환하다

--- src/main/kotlin/stringaddcalculator/StringAddCalculator.kt
+++ src/main/kotlin/stringaddcalculator/StringAddCalculator.kt
@@ -4,7 +4,7 @@ object StringAddCalculator {
     fun add(input: String?): Int {
         if (input.isNullOrBlank()) return 0
 
-        val numbers = input.split(",").map { it.toInt() }
+        val numbers = input.split(",", ":").map { it.toInt() }
         return numbers.sum()
     }
 }
