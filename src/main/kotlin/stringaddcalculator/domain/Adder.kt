29a52ba8766e685e377906cb966546cbbea5e917
saintbeller96 <rlawhdtjd9@naver.com>
2022-05-18 18:00:59 +0900

    feature: 덧셈기는 더할 피연산자 없다면 예외를 발생시킨다

--- src/main/kotlin/stringaddcalculator/domain/Adder.kt
+++ src/main/kotlin/stringaddcalculator/domain/Adder.kt
@@ -2,6 +2,7 @@ package stringaddcalculator.domain
 
 class Adder {
     fun add(operands: List<Operand>): Int {
+        require(operands.isNotEmpty()) { "더할 수 있는 피연산자가 없습니다" }
         return operands.sumOf { it.value }
     }
 }
--- src/test/kotlin/stringaddcalculator/domain/AdderSpecs.kt
+++ src/test/kotlin/stringaddcalculator/domain/AdderSpecs.kt
@@ -1,5 +1,6 @@
 package stringaddcalculator.domain
 
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
@@ -14,6 +15,14 @@ class AdderSpecs : DescribeSpec({
             )
             it("모든 피연산자를 더한 값을 반환할 수 있다") {
                 Adder().add(operands) shouldBe 6
+            }
+        }
+        context("주어진 피연산자들이 비어있다면") {
+            val operands = emptyList<Operand>()
+            it("예외를 발생시킨다") {
+                shouldThrowExactly<IllegalArgumentException> {
+                    Adder().add(operands)
+                }
             }
         }
     }
