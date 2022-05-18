a4365bcbca8f611815a927b3ffd627576cc72985
saintbeller96 <rlawhdtjd9@naver.com>
2022-05-18 17:50:20 +0900

    refactor: 피연산자

--- src/main/kotlin/stringaddcalculator/domain/Operand.kt
+++ src/main/kotlin/stringaddcalculator/domain/Operand.kt
@@ -4,8 +4,12 @@ package stringaddcalculator.domain
 value class Operand(val value: Int) {
 
     init {
-        if (value < 0) throw RuntimeException("음수는 피연산자가 될 수 없습니다.")
+        if (value < MINIMUM_VALUE) throw RuntimeException("음수는 피연산자가 될 수 없습니다.")
     }
 
     operator fun plus(other: Operand): Operand = Operand(value + other.value)
+
+    companion object {
+        private const val MINIMUM_VALUE = 0
+    }
 }
