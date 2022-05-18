6025be39a828634beb120f21acf47ab2d2aad22a
jade <jade.koh@kakaocorp.com>
2022-05-19 00:15:19 +0900

    feat: 기본 기능 테스트 작성

---
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -0,0 +1,55 @@
+package calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class CalculatorTest {
+
+    @Test
+    fun `빈 문자열은 0을 반환`() {
+        // given
+        val calculator = Calculator()
+
+        // when
+        val result = calculator.calculate("")
+
+        // then
+        assertThat(result).isEqualTo(0)
+    }
+
+    @Test
+    fun `콤마 구분자`() {
+        // given
+        val calculator = Calculator()
+
+        // when
+        val result = calculator.calculate("1,2,3")
+
+        // then
+        assertThat(result).isEqualTo(3)
+    }
+
+    @Test
+    fun `세미콜론 구분자`() {
+        // given
+        val calculator = Calculator()
+
+        // when
+        val result = calculator.calculate("1:2:3")
+
+        // then
+        assertThat(result).isEqualTo(3)
+    }
+
+    @Test
+    fun `다른 종류의 구분자`() {
+        // given
+        val calculator = Calculator()
+
+        // when
+        val result = calculator.calculate("1:2,3")
+
+        // then
+        assertThat(result).isEqualTo(6)
+    }
+}
