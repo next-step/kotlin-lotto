bbd9539d8b9126e6addc62cecacfb69d5f6d2518
glenn.kgp <glenn.kgp@kakaocorp.com>
2022-05-18 21:48:48 +0900

    refactor: calculator가 input parser, validaotr 둘다 가지게

--- src/main/kotlin/AddCalculator.kt
+++ src/main/kotlin/AddCalculator.kt
@@ -1,5 +1,20 @@
 class AddCalculator {
-    fun sum(numbers: List<Int>): Int {
+    fun sum(input: String): Int {
+        val inputValidator = InputValidator()
+        val inputParser = InputParser()
+
+        val tokens = inputParser.parse(input)
+
+        tokens.forEach {
+            inputValidator.checkNatualAndZero(it)
+        }
+
+        val numbers = tokens.map { it.toInt() }
+
+        return sum(numbers)
+    }
+
+    private fun sum(numbers: List<Int>): Int {
         return numbers.sum()
     }
 }
