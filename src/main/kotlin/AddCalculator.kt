3d39b13fcca0d7c841024316535756c3b7cd6434
glenn.kgp <glenn.kgp@kakaocorp.com>
2022-05-18 21:53:38 +0900

    refactor: AddCalculator가 인풋 검증, 변환까지 처리하게 변경

--- src/main/kotlin/AddCalculator.kt
+++ src/main/kotlin/AddCalculator.kt
@@ -1,20 +1,26 @@
 class AddCalculator {
-    fun sum(input: String): Int {
-        val inputValidator = InputValidator()
-        val inputParser = InputParser()
+    private val inputValidator = InputValidator()
+    private val inputParser = InputParser()
 
-        val tokens = inputParser.parse(input)
-
-        tokens.forEach {
-            inputValidator.checkNatualAndZero(it)
+    fun sum(input: String?): Int {
+        if (input.isNullOrBlank()) {
+            return 0
         }
 
-        val numbers = tokens.map { it.toInt() }
+        val tokens = inputParser.parse(input)
+        checkValidNumbers(tokens)
 
+        val numbers = tokens.map { it.toInt() }
         return sum(numbers)
     }
 
     private fun sum(numbers: List<Int>): Int {
         return numbers.sum()
+    }
+
+    private fun checkValidNumbers(tokens: List<String>) {
+        tokens.forEach {
+            inputValidator.checkNatualAndZero(it)
+        }
     }
 }
