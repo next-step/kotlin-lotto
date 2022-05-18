20a3e7872abe0ebf198b51c844df47d64c854bbe
vivian.bae <vivian.bae@kakaocorp.com>
2022-05-18 20:26:02 +0900

    5 - 커스텀 구분자를 지정할 수 있고 커스텀 구분자로 구분 된 숫자의 합을 반환한다

--- src/main/kotlin/stringaddcalculator/StringAddCalculator.kt
+++ src/main/kotlin/stringaddcalculator/StringAddCalculator.kt
@@ -1,10 +1,17 @@
 package stringaddcalculator
 
 object StringAddCalculator {
+    private val stringInputRegex = "//(.)\\n(.*)".toRegex()
+
     fun add(input: String?): Int {
         if (input.isNullOrBlank()) return 0
 
-        val numbers = input.split(",", ":").map { it.toInt() }
+        val (delimiters, numberString) =
+            input.takeIf { stringInputRegex.matches(it) }?.let { stringInputRegex.matchEntire(it) }?.groupValues?.let { arrayOf(it[1]) to it[2] }
+                ?: (arrayOf(",", ":") to input)
+
+        val numbers = numberString.split(*delimiters).map { it.toInt() }
+
         return numbers.sum()
     }
 }
