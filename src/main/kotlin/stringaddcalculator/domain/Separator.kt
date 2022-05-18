b8ef961372e63b13b89502e60d011a1ade8395a5
saintbeller96 <rlawhdtjd9@naver.com>
2022-05-18 18:14:44 +0900

    feature: 구분자는 구분자로 사용한 문자의 길이가 1보다 작다면 예외 발생

--- src/main/kotlin/stringaddcalculator/domain/Separator.kt
+++ src/main/kotlin/stringaddcalculator/domain/Separator.kt
@@ -1,3 +1,7 @@
 package stringaddcalculator.domain
 
-class Separator(val value: String)
+class Separator(val value: String) {
+    init {
+        require(value.isNotEmpty()) { "구분자의 길이는 1 이상이어야 합니다" }
+    }
+}
--- src/test/kotlin/stringaddcalculator/domain/SeparatorSpecs.kt
+++ src/test/kotlin/stringaddcalculator/domain/SeparatorSpecs.kt
@@ -1,5 +1,6 @@
 package stringaddcalculator.domain
 
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
@@ -8,6 +9,15 @@ class SeparatorSpecs : DescribeSpec({
     describe("구분자는") {
         it("구분자로 사용할 문자를 가질 수 있다") {
             Separator("/").value shouldBe "/"
+        }
+
+        context("구분자 문자의 길이가 1보다 작다면") {
+            val value = ""
+            it("예외를 발생시킨다") {
+                shouldThrowExactly<IllegalArgumentException> {
+                    Separator("")
+                }
+            }
         }
     }
 })
