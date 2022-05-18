90597de72a0572b64fb9e7611bedb9533683b93a
revfactory <revfactory@gmail.com>
2022-05-18 11:12:37 +0900

    feat: Add lotto purchase logic

--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -1,7 +1,19 @@
 package lotto.domain
 
 class LottoMachine {
-    fun generateAuto(): Lotto {
+
+    fun purchaseAuto(money: Long): List<Lotto> {
+        val numberOfPurchases = money / LOTTO_PRICE
+        return (1..numberOfPurchases)
+            .toList()
+            .map { generateAuto() }
+    }
+
+    private fun generateAuto(): Lotto {
         return Lotto(listOf(1, 2, 3, 4, 5, 6))
+    }
+
+    companion object {
+        const val LOTTO_PRICE: Long = 1_000
     }
 }
--- src/test/kotlin/lotto/domain/LottoMachineTest.kt
+++ src/test/kotlin/lotto/domain/LottoMachineTest.kt
@@ -2,10 +2,21 @@ package lotto.domain
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import lotto.domain.LottoMachine.Companion.LOTTO_PRICE
 
 class LottoMachineTest : FunSpec({
+    val lottoMachine = LottoMachine()
+
     test("로또 자동 발급이 가능하다.") {
-        val lotto = LottoMachine().generateAuto()
-        lotto.numbers.size shouldBe 6
+        val lottoList = lottoMachine.purchaseAuto(LottoMachine.LOTTO_PRICE)
+        lottoList.size shouldBe 1
+        lottoList[0].numbers.size shouldBe 6
+    }
+
+    test("금액에 맞춰 로또가 여러개 발급된다. (로또 1장의 가격은 1000원)") {
+        lottoMachine.purchaseAuto(0).size shouldBe 0
+        lottoMachine.purchaseAuto(LOTTO_PRICE / 2).size shouldBe 0
+        lottoMachine.purchaseAuto(LOTTO_PRICE).size shouldBe 1
+        lottoMachine.purchaseAuto(LOTTO_PRICE * 10).size shouldBe 10
     }
 })
