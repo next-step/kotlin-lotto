93d0fffbd39eb155e884439603c70c52aad2c2b8
glenn.kgp <glenn.kgp@kakaocorp.com>
2022-05-18 19:49:01 +0900

    feature: 리스트의 합을 반환한다

--- src/test/kotlin/StringAddTest.kt
+++ src/test/kotlin/StringAddTest.kt
@@ -3,9 +3,16 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
+import kotlin.math.exp
+
+class AddCalculator() {
+    fun sum(numbers: List<Int>): Int {
+        return numbers.sum()
+    }
+}
 
 class StringAddTest {
-    @ValueSource(strings = ["-1", "0", "a"])
+    @ValueSource(strings = ["-1", "a"])
     @ParameterizedTest
     fun `음수 값, 숫자가아닌 인풋이 들어오면 런타임 익셉션 던진다`(input: String) {
         val inputValidator = InputValidator()
@@ -24,5 +31,15 @@ class StringAddTest {
         assertThat(parsed[0]).isEqualTo("1")
         assertThat(parsed[1]).isEqualTo("3")
         assertThat(parsed[2]).isEqualTo("5")
+    }
+
+    @Test
+    fun `리스트의 합을 반환한다`() {
+        val numbers = listOf(1, 2, 3)
+        val addCalculator = AddCalculator()
+
+        val answer = addCalculator.sum(numbers)
+        val expect = 6
+        assertThat(answer).isEqualTo(expect)
     }
 }
