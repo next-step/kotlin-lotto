06f328b2466616b0f21077665e42157496918931
Juatae-Kim <jutaekim@sk.com>
2022-05-19 09:01:54 +0900

    refactor(InputView): Use simplified function

--- src/main/kotlin/lotto/view/input/parser/LottoInputParser.kt
+++ src/main/kotlin/lotto/view/input/parser/LottoInputParser.kt
@@ -3,13 +3,11 @@ package lotto.view.input.parser
 import lotto.model.data.Lotto
 import lotto.model.data.Lotto.Companion.toLotto
 import lotto.model.data.Policy
-import lotto.util.toBlankRemovedIntList
 
 class LottoInputParser(private val policy: Policy) : InputParser<Lotto> {
 
     override fun parseValue(inputString: String?): Lotto {
         require(!inputString.isNullOrBlank()) { "잘못된 입력입이다." }
-        val numbers = inputString.toBlankRemovedIntList()
-        return numbers.toLotto(policy)
+        return inputString.toLotto(policy)
     }
 }
