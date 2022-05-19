d457940105c00dd31536280e2cc5f6447e492310
Molly <dngwoodo@gmil.comm>
2022-05-20 00:56:56 +0900

    Analyst 클래스 리팩토링

--- src/main/kotlin/lotto/Analyst.kt
+++ src/main/kotlin/lotto/Analyst.kt
@@ -4,32 +4,31 @@ class Analyst(private val lastWeekWinningNumbers: List<Int>) {
     lateinit var result: Map<String, Any>
 
     fun analyze(purchaseAmount: Int, lottos: List<List<Int>>) {
-
-
-        var res = mutableListOf<Int>()
+        var numberOfCorrects = mutableListOf<Int>()
 
         lottos.forEach {
-            val numberOfCorrect = it
+            it
                 .mapIndexed { index, value -> lastWeekWinningNumbers[index] == value }
-                .count{ value -> value }
-
-            res.add(numberOfCorrect)
+                .count { value -> value }
+                .also { numberOfCorrect -> numberOfCorrects.add(numberOfCorrect) }
         }
 
         val winningAmount =
-            FOURTH_PRIZE_PRICE * res.count{ value -> value == 3 } +
-            THIRD_PRIZE_PRICE * res.count{ value -> value == 4 } +
-            SECOND_PRIZE_PRICE * res.count{ value -> value == 5 } +
-            FIRST_PRIZE_PRICE * res.count{ value -> value == 6 }
+            FOURTH_PRIZE_PRICE * numberOfCorrects.countNumber(3) +
+                    THIRD_PRIZE_PRICE * numberOfCorrects.countNumber(4) +
+                    SECOND_PRIZE_PRICE * numberOfCorrects.countNumber(5) +
+                    FIRST_PRIZE_PRICE * numberOfCorrects.countNumber(6)
 
-        result = mutableMapOf(
-            "3" to listOf(FOURTH_PRIZE_PRICE, res.count{ value -> value == 3 }),
-            "4" to listOf(THIRD_PRIZE_PRICE, res.count{ value -> value == 4 }),
-            "5" to listOf(SECOND_PRIZE_PRICE, res.count{ value -> value == 5 }),
-            "6" to listOf(FIRST_PRIZE_PRICE, res.count{ value -> value == 6 }),
+        result = mapOf(
+            "3" to listOf(FOURTH_PRIZE_PRICE, numberOfCorrects.countNumber(3)),
+            "4" to listOf(THIRD_PRIZE_PRICE, numberOfCorrects.countNumber(4)),
+            "5" to listOf(SECOND_PRIZE_PRICE, numberOfCorrects.countNumber(5)),
+            "6" to listOf(FIRST_PRIZE_PRICE, numberOfCorrects.countNumber(6)),
             "bf" to String.format("%.2f", winningAmount / purchaseAmount.toDouble()),
         )
     }
+
+    private fun List<Int>.countNumber(num: Int): Int = count { it == num }
 
     companion object {
         private const val FOURTH_PRIZE_PRICE = 5000
--- src/test/kotlin/lotto/AnalystTest.kt
+++ src/test/kotlin/lotto/AnalystTest.kt
@@ -4,6 +4,11 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
 class AnalystTest : DescribeSpec({
+    val fourthPrizePrice = 5000
+    val thirdPrizePrice = 50000
+    val secondPrizePrice = 1500000
+    val firstPrizePrice = 2000000000
+
     describe("analyze") {
         it("지난 주 로또 당첨 번호와 3개의 숫자가 맞으면 4등이다") {
             val lastWeekWinningNumbers = listOf(1, 2, 3, 4, 5, 6)
@@ -14,11 +19,11 @@ class AnalystTest : DescribeSpec({
 
             analyst.analyze(purchaseAmount, lottos)
 
-            analyst.result["3"] shouldBe listOf(5000, 1)
-            analyst.result["4"] shouldBe listOf(50000, 0)
-            analyst.result["5"] shouldBe listOf(1500000, 0)
-            analyst.result["6"] shouldBe listOf(2000000000, 0)
-            analyst.result["bf"] shouldBe String.format("%.2f", 5000 / purchaseAmount.toDouble())
+            analyst.result["3"] shouldBe listOf(fourthPrizePrice, 1)
+            analyst.result["4"] shouldBe listOf(thirdPrizePrice, 0)
+            analyst.result["5"] shouldBe listOf(secondPrizePrice, 0)
+            analyst.result["6"] shouldBe listOf(firstPrizePrice, 0)
+            analyst.result["bf"] shouldBe String.format("%.2f", fourthPrizePrice / purchaseAmount.toDouble())
         }
 
         it("지난 주 로또 당첨 번호와 4개의 숫자가 맞으면 3등이다") {
@@ -30,11 +35,11 @@ class AnalystTest : DescribeSpec({
 
             analyst.analyze(purchaseAmount, lottos)
 
-            analyst.result["3"] shouldBe listOf(5000, 0)
-            analyst.result["4"] shouldBe listOf(50000, 1)
-            analyst.result["5"] shouldBe listOf(1500000, 0)
-            analyst.result["6"] shouldBe listOf(2000000000, 0)
-            analyst.result["bf"] shouldBe String.format("%.2f", 50000 / purchaseAmount.toDouble())
+            analyst.result["3"] shouldBe listOf(fourthPrizePrice, 0)
+            analyst.result["4"] shouldBe listOf(thirdPrizePrice, 1)
+            analyst.result["5"] shouldBe listOf(secondPrizePrice, 0)
+            analyst.result["6"] shouldBe listOf(firstPrizePrice, 0)
+            analyst.result["bf"] shouldBe String.format("%.2f", thirdPrizePrice / purchaseAmount.toDouble())
         }
 
         it("지난 주 로또 당첨 번호와 5개의 숫자가 맞으면 2등이다") {
@@ -46,11 +51,11 @@ class AnalystTest : DescribeSpec({
 
             analyst.analyze(purchaseAmount, lottos)
 
-            analyst.result["3"] shouldBe listOf(5000, 0)
-            analyst.result["4"] shouldBe listOf(50000, 0)
-            analyst.result["5"] shouldBe listOf(1500000, 1)
-            analyst.result["6"] shouldBe listOf(2000000000, 0)
-            analyst.result["bf"] shouldBe String.format("%.2f", 1500000 / purchaseAmount.toDouble())
+            analyst.result["3"] shouldBe listOf(fourthPrizePrice, 0)
+            analyst.result["4"] shouldBe listOf(thirdPrizePrice, 0)
+            analyst.result["5"] shouldBe listOf(secondPrizePrice, 1)
+            analyst.result["6"] shouldBe listOf(firstPrizePrice, 0)
+            analyst.result["bf"] shouldBe String.format("%.2f", secondPrizePrice / purchaseAmount.toDouble())
         }
 
         it("지난 주 로또 당첨 번호와 6개의 숫자가 맞으면 1등이다") {
@@ -62,11 +67,27 @@ class AnalystTest : DescribeSpec({
 
             analyst.analyze(purchaseAmount, lottos)
 
-            analyst.result["3"] shouldBe listOf(5000, 0)
-            analyst.result["4"] shouldBe listOf(50000, 0)
-            analyst.result["5"] shouldBe listOf(1500000, 0)
-            analyst.result["6"] shouldBe listOf(2000000000, 1)
-            analyst.result["bf"] shouldBe String.format("%.2f", 2000000000 / purchaseAmount.toDouble())
+            analyst.result["3"] shouldBe listOf(fourthPrizePrice, 0)
+            analyst.result["4"] shouldBe listOf(thirdPrizePrice, 0)
+            analyst.result["5"] shouldBe listOf(secondPrizePrice, 0)
+            analyst.result["6"] shouldBe listOf(firstPrizePrice, 1)
+            analyst.result["bf"] shouldBe String.format("%.2f", firstPrizePrice / purchaseAmount.toDouble())
+        }
+
+        it("지난 주 로또 당첨 번호와 2개이하의 숫자가 맞으면 꽝이다.") {
+            val lastWeekWinningNumbers = listOf(1, 2, 3, 4, 5, 6)
+            val lottos = listOf(listOf(5, 6, 7, 8, 9, 10), listOf(6, 7, 8, 9, 10, 11))
+            val purchaseAmount = 14000
+
+            val analyst = Analyst(lastWeekWinningNumbers)
+
+            analyst.analyze(purchaseAmount, lottos)
+
+            analyst.result["3"] shouldBe listOf(fourthPrizePrice, 0)
+            analyst.result["4"] shouldBe listOf(thirdPrizePrice, 0)
+            analyst.result["5"] shouldBe listOf(secondPrizePrice, 0)
+            analyst.result["6"] shouldBe listOf(firstPrizePrice, 0)
+            analyst.result["bf"] shouldBe "0.00"
         }
     }
 })
