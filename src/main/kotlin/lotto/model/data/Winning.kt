caf5b642d8a26fb6222aed6bff0d5f5993130659
Juatae-Kim <jutaekim@sk.com>
2022-05-19 09:29:16 +0900

    style(number underscore): Add underscore for readability

--- src/main/kotlin/lotto/model/data/Winning.kt
+++ src/main/kotlin/lotto/model/data/Winning.kt
@@ -4,7 +4,7 @@ enum class Winning(val matchCount: Int, val winMoney: Int) {
     FIFTH(3, 5_000),
     FOURTH(4, 50_000),
     THIRD(5, 1_500_000),
-    FIRST(6, 2000_000_000),
+    FIRST(6, 2_000_000_000),
     LOST_GAME(0, 0)
 }
 
