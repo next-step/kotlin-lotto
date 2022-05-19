4687c666df230b7fd7d83fccc58827a416a49e33
larry.p <larry.p@kakaocorp.com>
2022-05-19 22:15:53 +0900

    refactor: 문자열 계산기 구현코드 정리

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -2,19 +2,27 @@ package calculator
 
 class StringCalculator {
 
-    fun add(input: String?): Int {
-        return if (input.isNullOrBlank()) {
+    fun add(input: String?): Int =
+        if (input.isNullOrBlank()) {
             0
         } else {
-            val customDelimiter = Regex("//(.)\n(.*)").find(input)
-            val numbers = if (customDelimiter != null) {
-                customDelimiter.groupValues[2].split(customDelimiter.groupValues[1])
-            } else {
-                input.split(',', ':')
-            }.map { it.toInt() }
-
-            if (numbers.any { it < 0 }) throw RuntimeException()
-            else numbers.sum()
+            val numbers = getNumbers(input)
+            checkNegativeNumber(numbers)
+            numbers.sum()
         }
+
+    private fun getNumbers(input: String): List<Int> =
+        (getCustomDelimiterTokens(input) ?: getPredefinedDelimiterTokens(input)).map { it.toInt() }
+
+    private fun getCustomDelimiterTokens(input: String): List<String>? =
+        Regex("//(.)\n(.*)").find(input)?.let {
+            it.groupValues[2].split(it.groupValues[1])
+        }
+
+    private fun getPredefinedDelimiterTokens(input: String): List<String> =
+        input.split(',', ':')
+
+    private fun checkNegativeNumber(numbers: List<Int>) {
+        if (numbers.any { it < 0 }) throw RuntimeException()
     }
 }
