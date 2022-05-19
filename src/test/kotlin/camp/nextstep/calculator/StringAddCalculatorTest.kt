2bbab509f31f14be259e9d379cc0a95a4a95b930
aeomhs <aeomhs@gmail.com>
2022-05-19 19:00:54 +0900

    test: 테스트 인자값 다양하게 수정 및 isEqualTo 으로 변경

--- src/test/kotlin/camp/nextstep/calculator/StringAddCalculatorTest.kt
+++ src/test/kotlin/camp/nextstep/calculator/StringAddCalculatorTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.NullAndEmptySource
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -19,30 +20,56 @@ class StringAddCalculatorTest {
 
     @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
     @ParameterizedTest
-    @ValueSource(strings = ["1"])
+    @ValueSource(strings = ["1", "1000", "101234"])
     fun oneNumber(text: String) {
-        assertThat(StringAddCalculator.calculate(text)).isSameAs(Integer.parseInt(text))
+        assertThat(StringAddCalculator.calculate(text)).isEqualTo(Integer.parseInt(text))
     }
 
     @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
-    @ParameterizedTest
-    @ValueSource(strings = ["1,2"])
-    fun twoNumbers(text: String) {
-        assertThat(StringAddCalculator.calculate(text)).isSameAs(3)
+    @ParameterizedTest(name = "{0} = {1}")
+    @CsvSource(
+        delimiter = '=',
+        value = [
+            "1,10=11",
+            "5,10=15",
+            "140,10=150",
+            "5,23=28",
+            "15,100=115",
+        ]
+    )
+    fun twoNumbers(exp: String, expected: Int) {
+        assertThat(StringAddCalculator.calculate(exp)).isEqualTo(expected)
     }
 
     @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
-    @ParameterizedTest
-    @ValueSource(strings = ["1,2:3"])
-    fun colons(text: String) {
-        assertThat(StringAddCalculator.calculate(text)).isSameAs(6)
+    @ParameterizedTest(name = "{0} = {1}")
+    @CsvSource(
+        delimiter = '=',
+        value = [
+            "1,10:500=511",
+            "5:10=15",
+            "100,10:7=117",
+            "100:5,23=128",
+            "5:15,100=120",
+        ]
+    )
+    fun colons(exp: String, expected: Int) {
+        assertThat(StringAddCalculator.calculate(exp)).isEqualTo(expected)
     }
 
     @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
-    @ParameterizedTest
-    @ValueSource(strings = ["//;\n1;2;3"])
-    fun customDelimiter(text: String) {
-        assertThat(StringAddCalculator.calculate(text)).isSameAs(6)
+    @ParameterizedTest(name = "{0} = {1}")
+    @CsvSource(
+        delimiterString = "=>",
+        textBlock = """
+        '//;${'\n'}1;2;3'      => 6 
+        '//+${'\n'}1+10+500'   => 511
+        '//-${'\n'}10-10-100'  => 120
+        '///${'\n'}1/10'       => 11
+        '//.${'\n'}1.1.1.1'    => 4"""
+    )
+    fun customDelimiter(exp: String, expected: Int) {
+        assertThat(StringAddCalculator.calculate(exp)).isEqualTo(expected)
     }
 
     @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.")
