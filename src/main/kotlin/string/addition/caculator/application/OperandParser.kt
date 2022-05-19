c0f7cf61dcbb944f575fe50f5902a5bc18041f85
qyu.st <qyu.st@kakaocorp.com>
2022-05-19 23:46:22 +0900

    feat: 문자열 파서 구현
    * 문자열을 구분자로 분리한다
    * 분리한 문자열은 피연산자 리스트 형태로 리턴한다
    * 빈 문자열일 경우 빈 리스트 반환
    * 커스텀 구분자를 가질 수 있다
    * 커스텀 구분자를 “//”와 “\n”를 통해 가져온다
    * 커스텀 구분자를 사용하여 문자열을 분리할 수 있음

---
+++ src/main/kotlin/string/addition/caculator/application/OperandParser.kt
@@ -0,0 +1,32 @@
+package string.addition.caculator.application
+
+import string.addition.caculator.domain.Operand
+
+object OperandParser {
+    private const val DEFAULT_DELIMITER_PATTERN = "[,:]"
+    private const val CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)"
+
+    fun parse(inputStr: String): List<Operand> {
+        val customDelimiterResult = Regex(CUSTOM_DELIMITER_PATTERN).find(inputStr)
+        return if (customDelimiterResult == null) {
+            parseByDelimiter(
+                operandsStr = inputStr,
+                delimiter = DEFAULT_DELIMITER_PATTERN.toRegex()
+            )
+        } else {
+            parseByDelimiter(
+                operandsStr = customDelimiterResult.groupValues[2],
+                delimiter = customDelimiterResult.groupValues[1].toRegex()
+            )
+        }
+    }
+
+    private fun parseByDelimiter(operandsStr: String, delimiter: Regex): List<Operand> {
+        return if (operandsStr.isEmpty()) {
+            listOf()
+        } else {
+            operandsStr.split(delimiter)
+                .map { Operand(it) }
+        }
+    }
+}
