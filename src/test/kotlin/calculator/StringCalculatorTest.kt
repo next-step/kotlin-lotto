25d67095c11cc7fbe47ec6b3874a4fe4c48b8a9d
larry.p <larry.p@kakaocorp.com>
2022-05-19 20:59:45 +0900

    test: 숫자 하나를 입력에 대한 테스트 추가

--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -4,6 +4,9 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.data.blocking.forAll
 import io.kotest.data.row
 import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.int
+import io.kotest.property.checkAll
 
 class StringCalculatorTest : StringSpec({
     "문자열 덧셈 계산기는 유효하지 않은 문자열을 입력하는 경우 0을 반환한다." {
@@ -13,6 +16,12 @@ class StringCalculatorTest : StringSpec({
             row(null)
         ) { input ->
             calculator.add(input) shouldBe 0
+        }
+    }
+    "문자열 덧셈 계산기는 단일 숫자 입력에 해당 숫자를 그대로 반환한다." {
+        val calculator = StringCalculator()
+        checkAll(Arb.int(10..100)) { input ->
+            calculator.add(input.toString()) shouldBe input
         }
     }
 })
