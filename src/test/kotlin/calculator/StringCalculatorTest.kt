b198f73a25a0d604ce436c5c7406fb1068524e20
larry.p <larry.p@kakaocorp.com>
2022-05-19 20:25:17 +0900

    test: 입력 유효성 검증 단위테스트 추가

---
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -0,0 +1,18 @@
+package calculator
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.blocking.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class StringCalculatorTest : StringSpec({
+    "문자열 덧셈 계산기는 유효하지 않은 문자열을 입력하는 경우 0을 반환한다." {
+        val calculator = StringCalculator()
+        forAll(
+            row(0),
+            row(null)
+        ) { input ->
+            calculator.add(input) shouldBe 0
+        }
+    }
+})
