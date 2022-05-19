3e6d6aae051381d8d615d729798dd732c540b917
leeyohan93 <good_1411@naver.com>
2022-05-19 21:25:23 +0900

    refactor: 로또 번호(LottoNumber) 리팩토링

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -3,8 +3,13 @@ package lotto
 data class LottoNumber(private val number: Int) {
 
     init {
-        require(number in (1..45)){
+        require(number in (LOTTO_MIN_NUMBER..LOTTO_MAX_NUMBER)) {
             "로또 번호는 1~45의 숫자만 생성이 가능합니다"
         }
+    }
+
+    companion object {
+        private const val LOTTO_MIN_NUMBER = 1
+        private const val LOTTO_MAX_NUMBER = 45
     }
 }
