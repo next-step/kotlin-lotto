1c5db9e5ec5875230f8f67eafdd83680a8f6b26f
larry.p <larry.p@kakaocorp.com>
2022-05-19 21:08:20 +0900

    feat: 숫자 두개와 구분자가 입력시 두 수의 합을 반환하도록 구현

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -3,6 +3,14 @@ package calculator
 class StringCalculator {
 
     fun add(input: String?): Int {
-        return if (input.isNullOrBlank()) return 0 else input.toInt()
+        return if (input.isNullOrBlank()) {
+            0
+        } else {
+            val token = input.split(",").map { it.toInt() }
+            when (token.size) {
+                2 -> token[0] + token[1]
+                else -> token[0]
+            }
+        }
     }
 }
