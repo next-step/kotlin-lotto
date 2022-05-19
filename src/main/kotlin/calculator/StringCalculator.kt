1f4dbeacf8082fbf122d21935963de4af32a263f
larry.p <larry.p@kakaocorp.com>
2022-05-19 21:00:15 +0900

    feat: 숫자 하나를 입력하는 경우 해당 숫자를 반환하도록 구현

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -3,6 +3,6 @@ package calculator
 class StringCalculator {
 
     fun add(input: String?): Int {
-        return if (input.isNullOrBlank()) return 0 else 1
+        return if (input.isNullOrBlank()) return 0 else input.toInt()
     }
 }
