9e07d7af80989fa906de0b5c97698426eb8302d0
larry.p <larry.p@kakaocorp.com>
2022-05-19 21:34:26 +0900

    test: 숫자 두개와 커스텀 구분자가 입력시 두 수의 합을 반환 테스트 추가

--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -30,9 +30,22 @@ class StringCalculatorTest : StringSpec({
         checkAll(
             Arb.int(0..9),
             Arb.int(10..19),
-            listOf(',', ':').exhaustive()
+            Arb.int(20..29),
+            listOf(',', ':').exhaustive(),
+            listOf(':', ',').exhaustive()
+        ) { left, right, extra, delimiterLeft, delimiterRight ->
+            calculator.add("$left$delimiterLeft$right$delimiterRight$extra") shouldBe left + right + extra
+        }
+    }
+    "문자열 덧셈 계산기는 숫자 두개와 커스텀 구분자를 입력하는 경우 두 숫자의 합을 반환한다." {
+        val calculator = StringCalculator()
+        checkAll(
+            Arb.int(0..9),
+            Arb.int(10..19),
+            listOf(';', '!', '@', '$').exhaustive()
         ) { left, right, delimiter ->
-            calculator.add("$left$delimiter$right") shouldBe left + right
+            val customDelimiter = "//$delimiter\n"
+            calculator.add("$customDelimiter$left$delimiter$right") shouldBe left + right
         }
     }
 })
