c594d94efb93027eb6125eb92964ba39ea474e26
Juatae-Kim <jutaekim@sk.com>
2022-05-19 13:45:10 +0900

    refactor: Move all extensions of Results into Results class ( apply feedback )

--- src/main/kotlin/lotto/model/data/Statistics.kt
+++ src/main/kotlin/lotto/model/data/Statistics.kt
@@ -1,19 +1,9 @@
 package lotto.model.data
 
 class Statistics(results: Results, policy: Policy) {
-
     val lottoCount = results.size
     val totalWonAmount = results.sumOf { it.winning.winMoney }
     val totalCost = policy.priceOfLotto * this.lottoCount
     val winningRatio = this.totalWonAmount.toDouble() / this.totalCost.toDouble()
     val winningCountMap = results.toWinningCountMap()
-
-    private fun Results.toWinningCountMap(): Map<Winning, Int> {
-        return Winning.values()
-            .associateWith { winning -> this.getCountOfWinning(winning) }
-            .toSortedMap { o1, o2 -> o1.winMoney - o2.winMoney }
-    }
-
-    private fun Results.getCountOfWinning(winning: Winning): Int =
-        this.filter { it.winning == winning }.size
 }
