7ec0954e29fff50fdeef133c9d1c464764ca0bad
chloe.bm <chloe.bm@kakaocorp.com>
2022-05-20 20:44:53 +0900

    feat: "//"와 "\n" 문자 사이에 커스텀 구분자를 지정할 수 있다.

--- src/main/kotlin/calculator/processor/InputProcessor.kt
+++ src/main/kotlin/calculator/processor/InputProcessor.kt
@@ -5,7 +5,7 @@ import calculator.model.PositiveNumber
 class InputProcessor {
     fun convertStringToList(text: String?): List<PositiveNumber> {
         val notEmptyText = convertStringToZeroIfNull(text)
-        return notEmptyText.split(DEFAULT_DELIMITER_REGEX.toRegex())
+        return splitToDelimiter(notEmptyText)
             .map { PositiveNumber(it.trim()) }
     }
 
@@ -16,8 +16,17 @@ class InputProcessor {
             text
         }
 
+    private fun splitToDelimiter(text: String): List<String> {
+        val matchResult = Regex(CUSTOM_DELIMITER_REGEX_PATTERN).find(text)
+        return matchResult?.let {
+            val customDelimiter = it.groupValues[1]
+            it.groupValues[2].split(customDelimiter)
+        } ?: text.split(DEFAULT_DELIMITER_REGEX_PATTERN.toRegex())
+    }
+
     companion object {
         private const val ZERO_STR = "0"
-        private const val DEFAULT_DELIMITER_REGEX = ",|:"
+        private const val CUSTOM_DELIMITER_REGEX_PATTERN = """//(.)\\n(.*)"""
+        private const val DEFAULT_DELIMITER_REGEX_PATTERN = ",|:"
     }
 }
--- src/test/kotlin/calculator/processor/InputProcessorTests.kt
+++ src/test/kotlin/calculator/processor/InputProcessorTests.kt
@@ -17,36 +17,40 @@ class InputProcessorTests {
         inputProcessor = InputProcessor()
     }
 
-    @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = [" ", "\t"])
-    fun emptyOrNull(text: String?) {
+    fun `빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다`(text: String?) {
         val expected = positiveNumberList("0")
         assertThat(inputProcessor.convertStringToList(text)).isEqualTo(expected)
     }
 
-    @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = ["1", "4", "1.2"])
-    fun oneNumber(text: String) {
+    fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(text: String) {
         val expected = positiveNumberList(text)
         assertThat(inputProcessor.convertStringToList(text)).isEqualTo(expected)
     }
 
-    @DisplayName(value = "구분자는 기본적으로 쉼표(,)와 콜론(:)을 사용할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = ["1,2:3", "1,2,3", "1:2: 3"])
-    fun splitByDefaultDelimiter(text: String) {
+    fun `구분자는 기본적으로 쉼표와 콜론을 사용할 수 있다`(text: String) {
         val expected = positiveNumberList("1", "2", "3")
         assertThat(inputProcessor.convertStringToList(text)).isEqualTo(expected)
     }
 
-    @DisplayName(value = "문자열 계산기에 음수나 다른 문자열을 전달하는 경우 RuntimeException 예외 처리를 한다.")
     @ParameterizedTest
     @ValueSource(strings = ["-1", "a", "-1,2", "a,-3"])
-    fun notPositiveNumber(text: String) {
+    fun `문자열 계산기에 음수나 다른 문자열을 전달하는 경우 RuntimeException 예외 처리를 한다`(text: String) {
         assertThrows<RuntimeException> { inputProcessor.convertStringToList(text) }
+    }
+
+    @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = ["""//;\n1;2;3"""])
+    fun customDelimiter(text: String) {
+        val expected = positiveNumberList("1", "2", "3")
+        assertThat(inputProcessor.convertStringToList(text)).isEqualTo(expected)
     }
 
     private fun positiveNumberList(vararg tokens: String) =
