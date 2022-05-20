1c28670b02d2740e068cfd537db8c3ed59d55f52
ujusy <49120090+ujusy@users.noreply.github.com>
2022-05-20 22:33:39 +0900

    Step1: 문자열 덧셈 계산기 (#267)
    * delete .gitkeep
    
    * new: null 혹은 blank로 이루어진 문자열일 경우 0을 반환해야한다.
    
    * new: 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환해야한다.
    
    * chore: 오타 수정
    
    * new: ,을 기준으로 문자열 리스트가 잘 반환되어야한다.
    
    * new: 분리된 문자열 리스트가 숫자로 잘 변환되어야한다.
    
    * new: 숫자 리스트의 모든 수의 합을 반환한다.
    
    * new: : 을 기준으로 문자열이 분리되어야한다.
    
    * new: //와 \n 문자 사이에 커스텀 구분자를 기준으로 문자열이 분리되어야한다.
    
    * docs: check readme
    
    * refactor: colon, comma 상수화
    
    * new: 문자열에 음수가 존재하면 RuntimException 발생
    
    * refactor: 계산기 전체 실행 가능하게 리팩토링
    
    * chore: add kotest-datatest dependency
    
    * new: 전체 계산기 동작 확인 테스트
    
    * docs: fix readme
    
    * refactor: 정규식 상수화
    
    * refactor: 함수 역할에 맞는 네이밍으로 수정

---
+++ src/test/kotlin/stringAddCalculator/CalculatorTest.kt
@@ -0,0 +1,94 @@
+package stringAddCalculator
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
+
+class CalculatorTest : FreeSpec({
+
+    "calculate" - {
+
+        "정상적인 게산기 동작 확인" {
+            withData(
+                "1" to 1,
+                "" to 0,
+                "1:2,3" to 6,
+                "//!\n2!3" to 5,
+            ) { (input, output) ->
+                Calculator.calculate(input) shouldBe output
+            }
+        }
+    }
+
+    "checkInput" - {
+
+        "null일 경우 0를 반환해야한다." {
+            val input = null
+            Calculator.checkInput(input) shouldBe "0"
+        }
+
+        "blank로 이루어진 문자열일 경우 0을 반환해야한다." {
+            val input = "  "
+            Calculator.checkInput(input) shouldBe "0"
+        }
+    }
+
+    "splitString" - {
+
+        ", 을 기준으로 문자열이 분리되어야한다." {
+            val inputString = "1,2"
+            Calculator.splitString(inputString) shouldBe listOf("1", "2")
+        }
+
+        ": 을 기준으로 문자열이 분리되어야한다." {
+            val inputString = "1:2"
+            Calculator.splitString(inputString) shouldBe listOf("1", "2")
+        }
+
+        ",와 : 을 기준으로 문자열이 분리되어야한다." {
+            val inputString = "1,2:3"
+            Calculator.splitString(inputString) shouldBe listOf("1", "2", "3")
+        }
+
+        "단일 문자여도 리스로 반환되어야한다." {
+            val inputString = "1"
+            Calculator.splitString(inputString) shouldBe listOf(inputString)
+        }
+    }
+
+    "customDelimiter" - {
+
+        "`//`와 `\n` 문자 사이에 커스텀 구분자를 기준으로 문자열이 분리되어야한다.." {
+            val customString = "//;\n1;2;3"
+            Calculator.splitCustomDelimiter(customString) shouldBe listOf("1", "2", "3")
+        }
+    }
+
+    "convertInt" - {
+
+        "분리된 문자열이 숫자로 변환되어야한다." {
+            val splitList = listOf("1", "2")
+            Calculator.convertInt(splitList) shouldBe listOf(1, 2)
+        }
+    }
+
+    "add" - {
+
+        "숫자 리스트의 모든 수의 합을 반환한다." {
+            val numberList = listOf(1, 2, 3)
+            Calculator.add(numberList) shouldBe 6
+        }
+    }
+
+    "checkNegative" - {
+
+        "분리된 문자열에 음수가 존재하면 RuntimeException" {
+            val numberList = listOf(1, -2, 3)
+            val exception = shouldThrow<RuntimeException> {
+                Calculator.checkNegative(numberList)
+            }
+            exception.message shouldBe Calculator.NEGATIVE_ERROR
+        }
+    }
+})
