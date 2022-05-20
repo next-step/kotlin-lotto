121ccb28bfcb9bf84324aad6b902a070dabf5bec
Molly <dngwoodo@gmil.comm>
2022-05-20 12:11:17 +0900

    불필요한 toInt문 제거

--- src/main/kotlin/stringaddcalculator/Parse.kt
+++ src/main/kotlin/stringaddcalculator/Parse.kt
@@ -16,5 +16,11 @@ class Parse(private var formula: String, delimiters: List<String>) {
 
     private fun String.toIntOrThrow() = requireNotNull(toIntOrNull()) { "올바른 계산식이 아닙니다." }
 
-    private fun String.toPositiveIntOrThrow() = if(toIntOrThrow() > 0) toInt() else throw RuntimeException("음수는 계산 할 수 없습니다.")
+    private fun String.toPositiveIntOrThrow(): Int {
+        val number = toIntOrThrow()
+
+        require(number > 0) {"음수는 계산 할 수 없습니다."}
+
+        return number
+    }
 }
