82d84b1a649d3f0bde7ea60787fe7806db453932
Juice <3221ju@gmail.com>
2022-05-20 13:11:38 +0900

    temp

--- src/main/kotlin/stringcalculator/dto/InputDto.kt
+++ src/main/kotlin/stringcalculator/dto/InputDto.kt
@@ -2,35 +2,27 @@ package stringcalculator.dto
 
 import stringcalculator.domain.Separator
 
-class InputDto(expression: String) {
-    var customSeparatorString: String? = null
-        private set
-    val numbersString: String
+class InputDto(val expression: String) {
 
     init {
-        if (!Separator.hasCustomSeparator(expression)) {
-            numbersString = expression
-        } else {
-            customSeparatorString = Separator.getCustomString(expression)
-            numbersString = expression.substring(Separator.getCustomSeparatorEndIndexWithSepartorSize(expression))
-        }
         validate()
     }
 
     private fun validate() {
-        validateForNumbersString(numbersString, customSeparatorString?.let { Separator.getCustomSeparator(it) })
+        val expression = expression.substring(0, Separator.getCustomSeparatorEndIndexWithSepartorSize(expression))
+        validateForNumbersString(expression)
     }
 
     companion object {
         private val COMMON_SEPARATOR: List<String> = listOf(":", ",")
         private const val ERROR_MESSAGE_OTHER_STRING_CONTAIN = "연삭식에 숫자, 구분자를 제외한 문자가 들어가 있습니다."
 
-        private fun getNumberStringRegex(customSeparatorString: String?): Regex {
-            return Regex("((\\d)([${COMMON_SEPARATOR.joinToString(separator = "")}${customSeparatorString?.let { customSeparatorString }}])?)+")
+        private fun getNumberStringRegex(separators: List<String>): Regex {
+            return Regex("((\\d)([${separators.joinToString(separator = "")}])?)+")
         }
 
-        private fun validateForNumbersString(expression: String, customSeparatorString: String? = null) {
-            require(getNumberStringRegex(customSeparatorString).matches(expression)) { ERROR_MESSAGE_OTHER_STRING_CONTAIN }
+        private fun validateForNumbersString(expression: String, separators: List<String>) {
+            require(getNumberStringRegex(separators).matches(expression)) { ERROR_MESSAGE_OTHER_STRING_CONTAIN }
         }
     }
 }
