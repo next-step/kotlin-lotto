e105a5e8c4cae132f668f03ddc62f02d89d6536b
Molly <dngwoodo@gmil.comm>
2022-05-20 12:08:51 +0900

    requireNotNull 도입

--- src/main/kotlin/stringaddcalculator/Parse.kt
+++ src/main/kotlin/stringaddcalculator/Parse.kt
@@ -14,7 +14,7 @@ class Parse(private var formula: String, delimiters: List<String>) {
             .split(delimiters.joinToString("|").toRegex())
             .map { if (it.isEmpty()) 0 else it.toPositiveIntOrThrow() }
 
-    private fun String.toIntOrThrow() = toIntOrNull() ?: throw IllegalArgumentException("올바른 계산식이 아닙니다.")
+    private fun String.toIntOrThrow() = requireNotNull(toIntOrNull()) { "올바른 계산식이 아닙니다." }
 
     private fun String.toPositiveIntOrThrow() = if(toIntOrThrow() > 0) toInt() else throw RuntimeException("음수는 계산 할 수 없습니다.")
 }
