5bf5490121bd72f420e4883c69fcd2f71572d332
saintbeller96 <rlawhdtjd9@naver.com>
2022-05-20 16:10:33 +0900

    feature: 로또 뭉치는 보유한 모든 로또의 등수를 알 수 있다

--- src/main/kotlin/stringaddcalculator/lotto/LottoBundle.kt
+++ src/main/kotlin/stringaddcalculator/lotto/LottoBundle.kt
@@ -6,4 +6,13 @@ class LottoBundle(
     init {
         require(bundle.isNotEmpty()) { "로또는 하나 이상 존재해야 합니다" }
     }
+
+    fun confirmWinning(winningLotto: Lotto): List<WinningPlace> {
+        val winnings = mutableListOf<WinningPlace>()
+        bundle.forEach {
+            val matching = it.countMatchingNumbers(winningLotto)
+            winnings.add(WinningPlace.of(matching))
+        }
+        return winnings.toList()
+    }
 }
--- src/test/kotlin/lotto/LottoBundleSpecs.kt
+++ src/test/kotlin/lotto/LottoBundleSpecs.kt
@@ -2,7 +2,13 @@ package lotto
 
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldContainExactly
+import stringaddcalculator.lotto.Lotto
 import stringaddcalculator.lotto.LottoBundle
+import stringaddcalculator.lotto.WinningPlace.BLANK
+import stringaddcalculator.lotto.WinningPlace.FIRST
+import stringaddcalculator.lotto.WinningPlace.SECOND
+import stringaddcalculator.lotto.WinningPlace.THIRD
 
 class LottoBundleSpecs : DescribeSpec({
 
@@ -10,6 +16,21 @@ class LottoBundleSpecs : DescribeSpec({
         it("주어진 로또의 개수가 0개라면 예외를 발생시킨다") {
             shouldThrowExactly<IllegalArgumentException> {
                 LottoBundle(emptyList())
+            }
+        }
+
+        context("당첨 로또가 주어지면") {
+            val winning = Lotto.of(listOf(1, 2, 3, 4, 5, 6))
+            it("가지고 있는 모든 로또의 당첨 등수를 알 수 있다") {
+                val lottoBundle = LottoBundle(
+                    listOf(
+                        Lotto.of(listOf(1, 2, 3, 4, 5, 6)),
+                        Lotto.of(listOf(1, 2, 3, 4, 5, 7)),
+                        Lotto.of(listOf(1, 2, 3, 4, 9, 10)),
+                        Lotto.of(listOf(8, 9, 10, 14, 19, 20)),
+                    )
+                )
+                lottoBundle.confirmWinning(winning) shouldContainExactly listOf(FIRST, SECOND, THIRD, BLANK)
             }
         }
     }
