95b1e55e35dfb94d85e3fdf03295d9747ee8f3c5
glennsm <54771120+glennsm@users.noreply.github.com>
2022-05-20 20:36:39 +0900

    Step1 (#266)
    * 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환한다
    
    * 앞의 기본 구분자(쉼표, 콜론) 외에 커스텀 구분자를 지정할 수 있다. 커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용한다
    
    * 문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw 한다
    
    * 코드 리펙토링
    
    Co-authored-by: glenn.sm <glenn.sm@kakaocorp.com>

---
+++ src/test/kotlin/calculator/StringAdditionTest.kt
@@ -0,0 +1,77 @@
+package calculator
+
+import StringAddition
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class StringAdditionTest {
+    @CsvSource(
+        value = [
+            "'1,2:3', ',|:'",
+            "'1^2^3', '\\^'",
+            "'1*2*3', '\\*'",
+            "'1$2$3', '\\$'",
+            "'1%2%3', '\\%'"
+        ]
+    )
+    @ParameterizedTest
+    fun `정규식 학습 테스트를 하자`(exp: String, reg: String) {
+        assertThat(exp.split(Regex(reg))).isEqualTo(listOf("1", "2", "3"))
+    }
+
+    @CsvSource(
+        value = [
+            "'6,2,1', 9",
+            "'5,1:1', 7",
+            "'6:2:1', 9",
+            "'5,1:1', 7",
+            "'', 0",
+        ]
+    )
+    @ParameterizedTest
+    fun `쉼표, 콜론을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환 한다`(exp: String, expectValue: Int) {
+        val stringAddition = StringAddition(exp)
+        assertThat(stringAddition.execute()).isEqualTo(expectValue)
+    }
+
+    @CsvSource(
+        value = [
+            "1, 1",
+            "9, 9",
+        ]
+    )
+    @ParameterizedTest
+    fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(exp: String, expectValue: Int) {
+        val stringAddition = StringAddition(exp)
+        assertThat(stringAddition.execute()).isEqualTo(expectValue)
+    }
+
+    @CsvSource(
+        value = [
+            "'//#\n1#1#2', 4",
+            "'//$\n1$2$2', 5",
+            "'//*\n1*2*2', 5",
+            "'//^\n1^6^2', 9"
+        ]
+    )
+    @ParameterizedTest
+    fun `앞의 기본 구분자(쉼표, 콜론) 외에 커스텀 구분자를 지정할 수 있다`(exp: String, expectValue: Int) {
+        val stringAddition = StringAddition(exp)
+        assertThat(stringAddition.execute()).isEqualTo(expectValue)
+    }
+
+    @CsvSource(
+        value = [
+            "'//#\n1&1#2'",
+            "'//$\n1-2$2'",
+            "'//*\n1*-2*2",
+        ]
+    )
+    @ParameterizedTest
+    fun `문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw 한다`(exp: String) {
+        val stringAddition = StringAddition(exp)
+        assertThrows<RuntimeException> { stringAddition.execute() }
+    }
+}
