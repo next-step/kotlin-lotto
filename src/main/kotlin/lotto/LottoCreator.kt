badb250dc51a4c06a2656c1c6ed39924e0209926
kwanguuuu <kwangyu326@gmail.com>
2022-05-21 15:40:06 +0900

    [step2] feat, docs: 로또 생성관련 로직 추가. 로또 생성관련 docs 기능목록 구현 체크

---
+++ src/main/kotlin/lotto/LottoCreator.kt
@@ -0,0 +1,14 @@
+package lotto
+
+object LottoCreator {
+    private val LOTTO_NUMBERS = (1..45)
+
+    private const val FIRST_LOTTERY_NUMBER_INDEX = 0
+    private const val LAST_LOTTERY_NUMBER_INDEX = 6
+
+    fun autoCreate(): List<Int> {
+        return LOTTO_NUMBERS.shuffled()
+            .subList(FIRST_LOTTERY_NUMBER_INDEX, LAST_LOTTERY_NUMBER_INDEX)
+            .sorted()
+    }
+}
