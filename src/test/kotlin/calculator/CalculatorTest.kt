aae9d10d7087855b1f99f22952de547f70e133aa
KM <sodp2@naver.com>
2022-05-22 01:46:05 +0900

    test: 덧셈함수에 숫자만 입력하는 경우 숫자를 그대로 반환하는 테스트 추가

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
 
 class CalculatorTest {
     private lateinit var calculator: Calculator
@@ -17,5 +18,11 @@ class CalculatorTest {
     @NullAndEmptySource
     fun `빈 문자열 또는 null을 입력할 경우 0을 반환해야 한다`(expression: String?) {
         assertThat(calculator.add(expression)).isEqualTo(0)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "11", "111", "1234"])
+    fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(expression: String?) {
+        assertThat(calculator.add(expression)).isEqualTo(expression?.toInt())
     }
 }
