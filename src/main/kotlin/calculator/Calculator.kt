d7343156817a44fb3d6e83b4eed748bbf39b9885
KM <sodp2@naver.com>
2022-05-22 02:12:40 +0900

    feat: 덧셈함수에 : 구분자 추가

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -4,7 +4,7 @@ class Calculator {
     fun add(expression: String?): Int {
         if (expression.isNullOrEmpty()) return 0
 
-        val numbers = expression.split(",").map { it.toInt() }
+        val numbers = expression.split(",", ":").map { it.toInt() }
 
         var result = 0
         numbers.forEach { number ->
