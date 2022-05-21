3cfa874d02f7aaa965ef43874ba4b7b3c4de864e
Juice <3221ju@gmail.com>
2022-05-22 05:06:00 +0900

    ref: CustomNumbersStringParser 리팩토링

--- src/main/kotlin/stringcalculator/domain/customparser/CustomNumbersStringParser.kt
+++ src/main/kotlin/stringcalculator/domain/customparser/CustomNumbersStringParser.kt
@@ -4,11 +4,20 @@ class CustomNumbersStringParser(stringExpression: String, separators: ParserSepa
     val parsedNumbers: List<Int>
 
     init {
-        require(separators.getSize() > 0) { ERROR_MESSAGE_SEPARATOR_ZERO }
-        validateExpressionForIncludeOtherStrings(stringExpression, separators)
+        validateSeparatorsEmpty(separators)
+        validateExpressionIncludeOtherStrings(stringExpression, separators)
 
-        parsedNumbers = stringExpression.split(Regex(separators.getSeparatorsString().toString())).map { it.toInt() }
+        parsedNumbers = splitBySeparators(stringExpression, separators)
         validateParsedNumberForIncludeNegativeNumber(parsedNumbers)
+    }
+
+    private fun splitBySeparators(
+        stringExpression: String,
+        separators: ParserSeparators
+    ) = stringExpression.split(Regex(separators.getSeparatorsString().toString())).map { it.toInt() }
+
+    private fun validateSeparatorsEmpty(separators: ParserSeparators) {
+        require(separators.getSize() > 0) { ERROR_MESSAGE_SEPARATOR_ZERO }
     }
 
     companion object {
@@ -22,11 +31,18 @@ class CustomNumbersStringParser(stringExpression: String, separators: ParserSepa
             parsedNumbers.forEach { require(it >= 0) { getErrorMessageIncludeNegativeNumber(it) } }
         }
 
-        fun validateExpressionForIncludeOtherStrings(expression: String, separators: ParserSeparators) {
-            val separatorsString = separators.getSeparatorsString().joinToString(separator = ",")
-            require(Regex("([$separatorsString,\\d,-])+").matches(expression)) {
-                "숫자, 구분 문자($separatorsString) 를 제외한 문자가 포함 되어 있습니다"
+        fun validateExpressionIncludeOtherStrings(expression: String, separators: ParserSeparators) {
+            require(getRegexBySeparators(separators).matches(expression)) {
+                "숫자, 구분 문자(${getSeparatorStringForRegex(separators)}) 를 제외한 문자가 포함 되어 있습니다"
             }
         }
+
+        private fun getRegexBySeparators(separators: ParserSeparators): Regex {
+            val separatorsString = getSeparatorStringForRegex(separators)
+            return Regex("([-,$separatorsString,\\d])+")
+        }
+
+        private fun getSeparatorStringForRegex(separators: ParserSeparators) =
+            separators.getSeparatorsString().joinToString(separator = ",")
     }
 }
