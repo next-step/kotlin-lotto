5b0874be08e8a27ddf8c6f6239ad601fd2ac31c3
aeomhs <aeomhs@gmail.com>
2022-05-21 18:32:00 +0900

    feat: 로또 티켓 숫자 범위 제약 조건 추가

--- src/main/kotlin/camp/nextstep/lotto/LottoTicket.kt
+++ src/main/kotlin/camp/nextstep/lotto/LottoTicket.kt
@@ -1,10 +1,12 @@
 package camp.nextstep.lotto
 
 private const val LOTTO_NUMBERS = 6
+private val LOTTO_NUMBER_RANGE = 1..45
 
 class LottoTicket(val numbers: List<Int>) {
 
     init {
         require(numbers.toSet().size == LOTTO_NUMBERS) { "로또 티켓은 서로 다른 ${LOTTO_NUMBERS}개의 숫자를 가질 수 있습니다. numbers=$numbers" }
+        require(numbers.all { it in LOTTO_NUMBER_RANGE }) { "로또 번호는 ${LOTTO_NUMBER_RANGE.first}부터 ${LOTTO_NUMBER_RANGE.last}까지 발급 가능합니다." }
     }
 }
--- src/test/kotlin/camp/nextstep/lotto/LottoTicketTest.kt
+++ src/test/kotlin/camp/nextstep/lotto/LottoTicketTest.kt
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 internal class LottoTicketTest {
 
@@ -18,24 +21,61 @@ internal class LottoTicketTest {
         assertTrue(ticket.numbers.containsAll(numbers))
     }
 
-    @DisplayName("로또 티켓은 6개보다 적은 숫자를 가질 수 없다.")
-    @Test
-    fun lessThanSixNumbersTest() {
-        val numbers = listOf(1, 2, 3, 4, 5)
-        assertThrows<IllegalArgumentException> { LottoTicket(numbers) }
-    }
-
-    @DisplayName("로또 티켓은 6개보다 많은 숫자를 가질 수 없다.")
-    @Test
+    @DisplayName("로또 티켓은 6개보다 적거나 많은 숫자를 가질 수 없다.")
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("illegalNumbers")
     fun moreThanSixNumbersTest() {
         val numbers = listOf(1, 2, 3, 4, 5, 6, 7)
         assertThrows<IllegalArgumentException> { LottoTicket(numbers) }
     }
 
     @DisplayName("로또 티켓은 같은 숫자를 여러 개 가질 수 없다.")
-    @Test
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("illegalSameNumbers")
     fun ticketDuplicatedNumberTest() {
         val numbers = listOf(1, 2, 1, 4, 5, 6)
         assertThrows<IllegalArgumentException> { LottoTicket(numbers) }
+    }
+
+    @DisplayName("로또 티켓은 1부터 45까지 숫자로 이루어질 수 있다.")
+    @ParameterizedTest(name = "1 <= {0} <= 45 을 만족하지 않는다.")
+    @MethodSource("illegalRangeNumbers")
+    fun ticketNumberRangeTest(illegalNumbers: IntArray) {
+        assertThrows<IllegalArgumentException> { LottoTicket(illegalNumbers.asList()) }
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun illegalRangeNumbers(): Stream<IntArray> {
+            return Stream.of(
+                intArrayOf(0, 1, 2, 3, 4, 5),
+                intArrayOf(1, 2, 3, 4, 5, 100),
+                intArrayOf(-1, 1, 2, 3, 4, 5),
+            )
+        }
+
+        @JvmStatic
+        fun illegalSameNumbers(): Stream<IntArray> {
+            return Stream.of(
+                intArrayOf(10, 20, 23, 24, 35, 10),
+                intArrayOf(1, 1, 23, 30, 40, 40),
+                intArrayOf(1, 1, 1, 1, 1, 1),
+            )
+        }
+
+        @JvmStatic
+        fun illegalNumbers(): Stream<IntArray> {
+            return Stream.of(
+                intArrayOf(),
+                intArrayOf(1),
+                intArrayOf(1, 2),
+                intArrayOf(1, 2, 3),
+                intArrayOf(1, 2, 3, 4),
+                intArrayOf(1, 2, 3, 4, 5),
+                intArrayOf(1, 2, 3, 4, 5, 6, 7),
+                intArrayOf(1, 2, 3, 4, 5, 6, 7, 8),
+            )
+        }
     }
 }
