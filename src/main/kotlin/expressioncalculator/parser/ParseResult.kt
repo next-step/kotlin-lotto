8433e203b31e489886810347d43fd771b9e149ed
austen.lee <austen.lee@kakaocorp.com>
2022-05-21 15:11:07 +0900

    step1 refactor : string은 nullable로

--- src/main/kotlin/expressioncalculator/parser/ParseResult.kt
+++ src/main/kotlin/expressioncalculator/parser/ParseResult.kt
@@ -2,5 +2,5 @@ package expressioncalculator.parser
 
 sealed interface ParseResult {
     data class Success(val numbers: List<Int>) : ParseResult
-    data class Failed(val expression: String) : ParseResult
+    data class Failed(val expression: String?) : ParseResult
 }
