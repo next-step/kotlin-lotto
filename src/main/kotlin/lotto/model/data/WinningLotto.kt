f3c4bcfbc54fcb49a493d2f3d7707e7155b6c8df
JT-Kim <jtkim95@gmail.com>
2022-05-21 18:10:53 +0900

    refactor: Remove duplicated logic (bonus number validation logic)

--- src/main/kotlin/lotto/model/data/WinningLotto.kt
+++ src/main/kotlin/lotto/model/data/WinningLotto.kt
@@ -4,7 +4,7 @@ data class WinningLotto private constructor(val numbers: Set<Int>, val bonusNumb
 
     companion object {
         fun Lotto.toWinningLotto(policy: Policy, bonusNumber: Int): WinningLotto {
-            require(bonusNumber !in this.numbers)
+            require(bonusNumber !in this.numbers) { "보너스 번호는 로또 번호와 겹칠 수 없습니다." }
             require(bonusNumber in policy.rangeOfNumbers)
             return WinningLotto(this.numbers, bonusNumber)
         }
--- src/main/kotlin/lotto/view/input/parser/BonusNumberInputParser.kt
+++ src/main/kotlin/lotto/view/input/parser/BonusNumberInputParser.kt
@@ -2,15 +2,16 @@ package lotto.view.input.parser
 
 import lotto.model.data.Lotto
 import lotto.model.data.Policy
+import lotto.model.data.WinningLotto.Companion.toWinningLotto
 
 open class BonusNumberInputParser(
-    policy: Policy,
+    private val policy: Policy,
     private val lotto: Lotto
 ) : IntInputParser(policy.rangeOfNumbers) {
 
     override fun parseValue(inputString: String?): Int {
-        val inputNumber = super.parseValue(inputString)
-        require(inputNumber !in lotto.numbers) { "보너스 번호는 로또 번호와 겹칠 수 없습니다." }
-        return inputNumber
+        val bonusNumber = super.parseValue(inputString)
+        this.lotto.toWinningLotto(policy, bonusNumber)
+        return bonusNumber
     }
 }
