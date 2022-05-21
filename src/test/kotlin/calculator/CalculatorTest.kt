ec381002abf94c677299f9f6dbee95b7bb15058e
KM <sodp2@naver.com>
2022-05-22 01:55:45 +0900

    test: ,구분자를 통해 숫자의 합을 계산하는 테스트 추가

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -25,4 +25,10 @@ class CalculatorTest {
     fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(expression: String?) {
         assertThat(calculator.add(expression)).isEqualTo(expression?.toInt())
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2"])
+    fun `숫자 두개를 , 구분자로 입력할 경우 두 숫자의 합을 반환한다`(expression: String?) {
+        assertThat(calculator.add(expression)).isEqualTo(3)
+    }
 }
