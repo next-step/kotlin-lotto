6dc1bd0dc59c06d3e9146915af33270681892ad3
YooInKeun <keun0390@naver.com>
2022-05-21 15:28:11 +0900

    chore: 기본 덧셈 결과값 상수로 추출

--- src/main/kotlin/calculator/model/AddCalculator.kt
+++ src/main/kotlin/calculator/model/AddCalculator.kt
@@ -1,12 +1,13 @@
 package calculator.model
 
 object AddCalculator {
+    private const val DEFAULT_SUM = 0
     private val DEFAULT_DELIMITER_REGEX = "[,:]".toRegex()
     private val CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)".toRegex()
 
     fun calculate(input: String?): Int {
         if (input.isNullOrBlank()) {
-            return 0
+            return DEFAULT_SUM
         }
 
         val customDelimiterAndTokens = CUSTOM_DELIMITER_REGEX.find(input)
