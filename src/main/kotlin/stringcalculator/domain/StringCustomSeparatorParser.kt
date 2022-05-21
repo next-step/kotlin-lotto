cd822a6d17758d7ff8b7e90cd4405a53bbc0f540
Juice <3221ju@gmail.com>
2022-05-21 16:05:15 +0900

    feat: StringCustomSeparatorParserTest 생성

---
+++ src/main/kotlin/stringcalculator/domain/StringCustomSeparatorParser.kt
@@ -0,0 +1,42 @@
+package stringcalculator.domain
+
+class StringCustomSeparatorParser(stringCustomSeparator: String) {
+    val parserSeparator: ParserSeparator
+
+    init {
+        validateStringCustomSeparator(stringCustomSeparator)
+        val separatorString = subStringByString(stringCustomSeparator, START_STRING, END_STRING)
+        parserSeparator = ParserSeparator(separatorString)
+    }
+
+    private fun subStringByString(
+        stringCustomSeparator: String,
+        startString: String,
+        endString: String
+    ): String {
+        val startIndex = stringCustomSeparator.indexOf(startString) + startString.length
+        val endIndex = stringCustomSeparator.indexOf(endString, startIndex)
+        return stringCustomSeparator.substring(startIndex, endIndex)
+    }
+
+    companion object {
+        private const val START_STRING = "//"
+        private const val END_STRING = "\\n"
+        private const val ERROR_MESSAGE_START_STRING_NOT_CONTAIN = "시작을 구분하는 문자열($START_STRING)이 포함되어 있지 않습니다"
+        private const val ERROR_MESSAGE_END_STRING_NOT_CONTAIN = "끝을 구분하는 문자열($END_STRING)이 포함되어 있지 않습니다"
+
+        private fun validateStringCustomSeparator(stringCustomSeparator: String) {
+            validateStartContain(stringCustomSeparator, START_STRING)
+            validateEndStringContains(stringCustomSeparator, START_STRING, END_STRING)
+        }
+
+        private fun validateStartContain(stringCustomSeparator: String, startString: String) {
+            require(stringCustomSeparator.indexOf(startString) >= 0) { ERROR_MESSAGE_START_STRING_NOT_CONTAIN }
+        }
+
+        private fun validateEndStringContains(stringCustomSeparator: String, startString: String, endString: String) {
+            val startIndex = stringCustomSeparator.indexOf(startString)
+            require(stringCustomSeparator.indexOf(endString, startIndex) >= 0) { ERROR_MESSAGE_END_STRING_NOT_CONTAIN }
+        }
+    }
+}
---
+++ src/test/kotlin/stringcalculator/domain/StringCustomSeparatorParserTest.kt
@@ -0,0 +1,52 @@
+package stringcalculator.domain
+
+import io.kotest.assertions.throwables.shouldThrowExactly
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
+
+class StringCustomSeparatorParserTest : DescribeSpec({
+    it("커스텀 구분자 문자열에서 구분자 문자(ParserSeparator)를 추출한다") {
+        // given
+        val successSeparatorString = ";"
+        val stringCustomSeparator = "//${successSeparatorString}\\n"
+
+        // when
+        val stringCustomSeparatorParser = StringCustomSeparatorParser(stringCustomSeparator)
+
+        // then
+        stringCustomSeparatorParser.parserSeparator.string shouldBe successSeparatorString
+    }
+
+    describe("validation") {
+        it("커스텀 구분자 포맷만 있고 구분자 문자열은 없는 경우 IllegalArgumentException 발생") {
+            // given
+            val stringCustomSeparator = "//\\n"
+
+            // then
+            shouldThrowExactly<IllegalArgumentException> {
+                StringCustomSeparatorParser(stringCustomSeparator)
+            }
+        }
+
+        it("커스텀 구분자 시작 부분이 없는 경우 IllegalArgumentException 발생") {
+            // given
+            val stringCustomSeparator = ";\\n"
+
+            // then
+            shouldThrowExactly<IllegalArgumentException> {
+                StringCustomSeparatorParser(stringCustomSeparator)
+            }.shouldHaveMessage("시작을 구분하는 문자열(//)이 포함되어 있지 않습니다")
+        }
+
+        it("커스텀 구분자 끝이 없는 경우 에러가 IllegalArgumentException 발생") {
+            // given
+            val stringCustomSeparator = "//;"
+
+            // then
+            shouldThrowExactly<IllegalArgumentException> {
+                StringCustomSeparatorParser(stringCustomSeparator)
+            }.shouldHaveMessage("끝을 구분하는 문자열(\\n)이 포함되어 있지 않습니다")
+        }
+    }
+})
