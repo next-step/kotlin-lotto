2c98cbe1bc466385a6ed5b2b551e35762226c09c
glennsm <lovinhut@gmail.com>
2022-05-21 11:44:47 +0900

    refactor: pr내용 반영

--- src/main/kotlin/StringAddition.kt
+++ src/main/kotlin/StringAddition.kt
@@ -2,45 +2,33 @@ import util.findPattern
 import util.takeIfNotEmpty
 import util.toIntThrow
 
-class StringAddition(private val exp: String) {
+class StringAddition(private val text: String) {
     fun execute(): Int {
-        val (split, target) = exp.findPattern(CUSTOM_PATTERN).extract(default = exp)
-        return add(target, split)
+        val (regexText, target) = text.findPattern(CUSTOM_REGEX).extract(default = text)
+        return target.takeIfNotEmpty()
+            ?.split(addPrefix(regexText).toRegex())
+            ?.map { it.toIntThrow() }
+            ?.reduce { total: Int, num: Int ->
+                require(num >= 0) { "음수는 입력할 수 없습니다." }
+                total + num
+            }
+            ?: 0
     }
 
     private fun MatchResult?.extract(default: String): Pair<String, String> {
-        val split: String = this?.groupValues?.get(1) ?: DEFAULT_SPLIT
-        val target: String = this?.groupValues?.get(2) ?: default
-        return Pair(split, target)
+        return this?.destructured?.let { (regexText, target) ->
+            regexText to target
+        } ?: (DEFAULT_REGEX_TEXT to default)
     }
 
-    private fun add(exp: String, split: String = DEFAULT_SPLIT) =
-        exp
-            .takeIfNotEmpty()
-            ?.split(replaceAndRegex(split))
-            ?.toMapAndReduce()
-            ?: 0
-
-    private fun replaceAndRegex(split: String): Regex =
-        if (isDefaultSplit(split)) Regex(split)
-        else Regex(REGEX_PREFIX + split)
-
-    private fun isDefaultSplit(split: String) = split == DEFAULT_SPLIT
-
-    private fun List<String>.toMapAndReduce(): Int =
-        map { it.toIntThrow() }
-            .reduce { total: Int, num: Int ->
-                require(num >= 0)
-                total + num
-            }
-
-    private fun require(condition: Boolean) {
-        if (!condition) throw RuntimeException("음수는 입력할 수 없습니다.")
+    private fun addPrefix(regex: String): String {
+        return if (regex == DEFAULT_REGEX_TEXT) regex
+        else REGEX_PREFIX + regex
     }
 
     companion object {
-        private const val DEFAULT_SPLIT = "(,|:)"
-        private const val CUSTOM_PATTERN = "//(.)\\n(.*)"
+        private const val DEFAULT_REGEX_TEXT = "(,|:)"
+        private val CUSTOM_REGEX = "//(.)\\n(.*)".toRegex()
         private const val REGEX_PREFIX = "\\"
     }
 }
--- src/main/kotlin/util/String.kt
+++ src/main/kotlin/util/String.kt
@@ -7,6 +7,6 @@ fun String.toIntThrow() =
         throw RuntimeException("구분자외에는 숫자만 입력해야합니다.")
     }
 
-fun String.findPattern(pattern: String) = Regex(pattern).find(this)
+fun String.findPattern(pattern: Regex) = pattern.find(this)
 
 fun String.takeIfNotEmpty(): String? = takeIf { it.isNotEmpty() }
