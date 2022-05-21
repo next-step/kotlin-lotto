9b5e3f6d63d71b8cef47b3ba954e6655dfc3970e
KM <sodp2@naver.com>
2022-05-22 01:39:35 +0900

    test: 빈문자열, null의 경우 0을 반환하는 테스트 코드 추가

---
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -0,0 +1,27 @@
+package calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.NullAndEmptySource
+
+class CalculatorTest {
+    private lateinit var calculator: Calculator
+
+    @BeforeEach
+    fun setUp() {
+        calculator = Calculator()
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    fun `빈 문자열 또는 null을 입력할 경우 0을 반환해야 한다`(expression: String?) {
+        assertThat(calculator.add(expression)).isEqualTo(0)
+    }
+}
+
+class Calculator {
+    fun add(expression: String?): Int {
+        return 0
+    }
+}
