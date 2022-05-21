369b75c15f7a4b12494ec743a2cdbc3d65720b4c
aeomhs <aeomhs@gmail.com>
2022-05-21 16:39:07 +0900

    refactor: 매직 넘버 제거

--- src/main/kotlin/camp/nextstep/lotto/LottoTicket.kt
+++ src/main/kotlin/camp/nextstep/lotto/LottoTicket.kt
@@ -5,6 +5,6 @@ private const val LOTTO_NUMBERS = 6
 class LottoTicket(val numbers: List<Int>) {
 
     init {
-        require(numbers.toSet().size == LOTTO_NUMBERS) { "로또 티켓은 서로 다른 6개의 숫자를 가질 수 있습니다. numbers=$numbers" }
+        require(numbers.toSet().size == LOTTO_NUMBERS) { "로또 티켓은 서로 다른 ${LOTTO_NUMBERS}개의 숫자를 가질 수 있습니다. numbers=$numbers" }
     }
 }
