fff8267af4356555fa8bdd68ea55a9c2a26bd188
Molly <dngwoodo@gmail.com>
2022-05-21 23:27:53 +0900

    main 함수 생성

--- src/main/kotlin/stringaddcalculator/StringAddCalculator.kt
+++ src/main/kotlin/stringaddcalculator/StringAddCalculator.kt
@@ -1,8 +1,8 @@
 package stringaddcalculator
 
 object StringAddCalculator {
-    fun add(formula: String?): Int {
-        if (formula.isNullOrEmpty()) return 0
+    fun add(formula: String): Int {
+        if (formula.isEmpty()) return 0
 
         // val delimiters = DelimiterFinder.find(formula)
         // return Parse(formula, delimiters).result.reduce { acc, current -> acc + current }
---
+++ src/main/kotlin/stringaddcalculator/main.kt
@@ -0,0 +1,9 @@
+package stringaddcalculator
+
+fun main() {
+    val formula = readln()
+
+    val result = StringAddCalculator.add(formula)
+
+    println(result)
+}
