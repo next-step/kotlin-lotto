1e3fe36ea9b1e885a0b0bc46d62cfc492ae0d2cd
saintbeller96 <rlawhdtjd9@naver.com>
2022-05-21 17:02:15 +0900

    refactor: 로또를 출력할 때만 번호 오름차순으로 정렬

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,9 +1,8 @@
 package lotto.domain
 
 class Lotto(
-    _numbers: Set<LottoNumber>
+    private val numbers: Set<LottoNumber>
 ) {
-    private val numbers: Set<LottoNumber> = _numbers.toSortedSet()
 
     init {
         require(numbers.size == SIZE_OF_LOTTO_NUMBERS) { "로또 번호의 개수는 반드시 $SIZE_OF_LOTTO_NUMBERS 개 이어야 합니다" }
@@ -24,6 +23,8 @@ class Lotto(
     }
 
     override fun toString(): String {
-        return numbers.toString()
+        return numbers.toList()
+            .sorted()
+            .toString()
     }
 }
