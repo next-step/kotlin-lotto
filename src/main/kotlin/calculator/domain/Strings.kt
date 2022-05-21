644f54f64cd0c0186ab780f626045862baabb5ce
jinwoo-k <br2k@daum.net>
2022-05-22 06:23:09 +0900

    refact: 클래스명 변경 (StringParser > Strings)

--- src/main/kotlin/calculator/domain/Strings.kt
+++ src/main/kotlin/calculator/domain/Strings.kt
@@ -1,6 +1,6 @@
 package calculator.domain
 
-object StringParser {
+object Strings {
     fun parse(value: String, delimiter: String = ";"): List<String> {
         return value.split(delimiter)
     }
--- src/test/kotlin/calculator/domain/StringsTest.kt
+++ src/test/kotlin/calculator/domain/StringsTest.kt
@@ -4,18 +4,18 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
-class StringParserTest {
+class StringsTest {
     @ParameterizedTest
     @CsvSource(value = ["1;2;3:1,2,3", "3;4;5:3,4,5"], delimiter = ':')
     fun `기본 구분자로 파싱`(input: String, expected: String) {
-        assertThat(StringParser.parse(input).joinToString(","))
+        assertThat(Strings.parse(input).joinToString(","))
             .isEqualTo(expected)
     }
 
     @ParameterizedTest
     @CsvSource(value = ["1;2;3:;:1,2,3", "3^4^5^6:^:3,4,5,6"], delimiter = ':')
     fun `커스텀 구분자로 파싱`(input: String, delmiter: String, expected: String) {
-        assertThat(StringParser.parse(input, delmiter).joinToString(","))
+        assertThat(Strings.parse(input, delmiter).joinToString(","))
             .isEqualTo(expected)
     }
 }
