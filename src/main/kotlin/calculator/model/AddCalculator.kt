ef52409d6e2b63ac83c06f9587b32a2d3814c3c8
YooInKeun <keun0390@naver.com>
2022-05-21 15:27:22 +0900

    feat: 커스텀 구분자 지정해서 덧셈하는 기능 추가

--- src/main/kotlin/calculator/model/AddCalculator.kt
+++ src/main/kotlin/calculator/model/AddCalculator.kt
@@ -2,13 +2,23 @@ package calculator.model
 
 object AddCalculator {
     private val DEFAULT_DELIMITER_REGEX = "[,:]".toRegex()
+    private val CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)".toRegex()
 
     fun calculate(input: String?): Int {
         if (input.isNullOrBlank()) {
             return 0
         }
 
-        val numbers = input.split(DEFAULT_DELIMITER_REGEX)
+        val customDelimiterAndTokens = CUSTOM_DELIMITER_REGEX.find(input)
+        customDelimiterAndTokens?.let {
+            return sum(customDelimiterAndTokens.groupValues[1].toRegex(), customDelimiterAndTokens.groupValues[2])
+        }
+
+        return sum(tokens = input)
+    }
+
+    private fun sum(delimiter: Regex = DEFAULT_DELIMITER_REGEX, tokens: String): Int {
+        val numbers = tokens.split(delimiter)
         return Numbers.from(numbers).sum()
     }
 }
--- src/test/kotlin/calculator/model/AddCalculatorTest.kt
+++ src/test/kotlin/calculator/model/AddCalculatorTest.kt
@@ -2,6 +2,7 @@ package calculator.model
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -13,12 +14,20 @@ class AddCalculatorTest {
         " = 0",
         "1 = 1",
         "1,2 = 3",
-        "1,2,3 = 6",
         "3,4:5 = 12",
         delimiter = '='
     )
     fun `덧셈을 정상적으로 계산`(input: String?, result: Int) {
         // when, then
         assertEquals(AddCalculator.calculate(input), result)
+    }
+
+    @Test
+    fun `커스텀 구분자를 지정했을 때 덧셈을 정상적으로 계산`() {
+        // given
+        val input = "//;\n1;2;3"
+
+        // when, then
+        assertEquals(AddCalculator.calculate(input), 6)
     }
 }
