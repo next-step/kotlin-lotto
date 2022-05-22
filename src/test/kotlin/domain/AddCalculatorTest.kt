40785dd6a54ae85bcc7cf4b9cba478b81b067bd4
glenn.kgp <glenn.kgp@kakaocorp.com>
2022-05-22 16:56:27 +0900

    refactor: 테스트클래스와 실제클래스 위치 맞추기

--- src/test/kotlin/domain/AddCalculatorTest.kt
+++ src/test/kotlin/domain/AddCalculatorTest.kt
@@ -1,3 +1,5 @@
+package domain
+
 import domain.AddCalculator
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
--- src/test/kotlin/domain/InputParserTest.kt
+++ src/test/kotlin/domain/InputParserTest.kt
@@ -1,3 +1,5 @@
+package domain
+
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import domain.InputParser
