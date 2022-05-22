d339d4a5dd861936adf801bd5e7ba2683901deeb
KM <sodp2@naver.com>
2022-05-22 15:03:05 +0900

    feat: 식에 음수가 있는 경우 런타임익셉션을 발생하도록 덧셈함수 수정

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -7,11 +7,20 @@ class Calculator {
         Regex("//(.)\n(.*)").find(expression)?.let { matchResult ->
             val customDelimiter = matchResult.groupValues[1]
             val numbersByCustomDelimiter = matchResult.groupValues[2].split(customDelimiter).map { it.toInt() }
+            checkNegativeNumber(numbersByCustomDelimiter)
+
             return addNumbers(numbersByCustomDelimiter)
         }
 
         val numbers = expression.split(",", ":").map { it.toInt() }
+        checkNegativeNumber(numbers)
         return addNumbers(numbers)
+    }
+
+    private fun checkNegativeNumber(numbers: List<Int>) {
+        numbers.forEach { number ->
+            if (number < 0) throw RuntimeException()
+        }
     }
 
     private fun addNumbers(numbers: List<Int>): Int {
--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.NullAndEmptySource
 import org.junit.jupiter.params.provider.ValueSource
-import kotlin.math.exp
 
 class CalculatorTest {
     private lateinit var calculator: Calculator
