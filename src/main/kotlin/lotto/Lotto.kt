589c7b6e386fcab28108bc1a8b826264c847db52
Jake Son <sonjeabin@gmail.com>
2022-05-22 11:16:29 +0900

    feat: 로또 생성 시 중복값 허용하지 않기

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -5,11 +5,10 @@ value class Lotto(val numbers: List<Int>) {
 
     init {
         require(numbers.size == NUMBER_COUNT) { "로또 숫자의 개수는 $NUMBER_COUNT 이어야 합니다" }
-
-        if (numbers == listOf(1, 2, 3, 4, 6, 6)) {
-            throw IllegalArgumentException()
-        }
+        require(numbers.isUnique()) { "로또에 중복된 숫자가 존재할 수 없습니다" }
     }
+
+    private fun List<Int>.isUnique(): Boolean = toSet().size == size
 
     companion object {
         private const val NUMBER_COUNT = 6
