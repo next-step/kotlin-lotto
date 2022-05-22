39856b4897368b01ddf635de578d12796474ee0b
Jake Son <sonjeabin@gmail.com>
2022-05-22 11:02:43 +0900

    refactor: 로또 인스턴스 생성방식 변경

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,14 +1,13 @@
 package lotto
 
 @JvmInline
-value class Lotto private constructor(val numbers: List<Int>) {
+value class Lotto(val numbers: List<Int>) {
+
+    init {
+        require(numbers.size == NUMBER_COUNT) { "로또 숫자의 개수는 $NUMBER_COUNT 이어야 합니다" }
+    }
+
     companion object {
         private const val NUMBER_COUNT = 6
-
-        fun of(numbers: List<Int>): Lotto {
-            require(numbers.size == NUMBER_COUNT) { "로또 숫자의 개수는 $NUMBER_COUNT 이어야 합니다" }
-
-            return Lotto(numbers)
-        }
     }
 }
