8096ffaa180d99e28f6c661749bafac0ef2cd72b
Jake Son <sonjeabin@gmail.com>
2022-05-22 17:16:03 +0900

    test: 당첨 회수에 대한 테스트 추가

--- src/main/kotlin/lotto/Matcher.kt
+++ src/main/kotlin/lotto/Matcher.kt
@@ -3,7 +3,7 @@ package lotto
 class Matcher(val winNumbers: WinNumbers, policies: List<WinPolicy>) {
     private val sortedPolicies = policies.sortedBy { it.matchCount }
 
-    fun makeResult(): List<MatchResult> {
+    fun makeResult(lotto: List<Lotto>): List<MatchResult> {
         return sortedPolicies.map {
             MatchResult(
                 matchCount = it.matchCount,
--- src/test/kotlin/lotto/MatcherTest.kt
+++ src/test/kotlin/lotto/MatcherTest.kt
@@ -27,7 +27,7 @@ internal class MatcherTest : FreeSpec({
         )
         val matcher = Matcher(winNumbers, policies)
 
-        val results = matcher.makeResult()
+        val results = matcher.makeResult(emptyList())
 
         results shouldHaveSize policies.size
     }
@@ -41,7 +41,7 @@ internal class MatcherTest : FreeSpec({
         )
         val matcher = Matcher(winNumbers, policies)
 
-        val results = matcher.makeResult()
+        val results = matcher.makeResult(emptyList())
 
         results.map { it.matchCount } shouldBe listOf(2, 4, 5)
     }
@@ -55,8 +55,43 @@ internal class MatcherTest : FreeSpec({
         )
         val matcher = Matcher(winNumbers, policies)
 
-        val results = matcher.makeResult()
+        val results = matcher.makeResult(emptyList())
 
         results.map { it.price } shouldBe listOf(Money(100), Money(200), Money(300))
+    }
+
+    "각 당첨 조건에 해당하는 당첨 회수를 계산한다" - {
+        val winNumbers = WinNumbers(listOf(1, 2, 3, 4, 5, 6))
+        val policies = listOf(
+            WinPolicy(5, Money(100)),
+            WinPolicy(6, Money(300)),
+        )
+        val matcher = Matcher(winNumbers, policies)
+
+        listOf(
+            Triple(
+                "5개 0회 6개 0회",
+                listOf(
+                    Lotto(listOf(1, 2, 3, 4, 7, 8)),
+                    Lotto(listOf(3, 4, 5, 6, 7, 8)),
+                ),
+                listOf(0, 0),
+            ),
+            Triple(
+                "5개 2회 6개 1회",
+                listOf(
+                    Lotto(listOf(1, 2, 3, 4, 5, 7)),
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    Lotto(listOf(2, 3, 4, 5, 6, 7))
+                ),
+                listOf(2, 1),
+            ),
+        ).forEach { (title, lotto, expected) ->
+            title {
+                val results = matcher.makeResult(lotto)
+
+                results.map { it.winCount } shouldBe expected
+            }
+        }
     }
 })
