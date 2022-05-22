8f8ba1d70057e26e699e2518f459f33a3274daef
Juice <3221ju@gmail.com>
2022-05-23 02:29:46 +0900

    ref: CustomNumbersStringParser 밸리데이션 로직 private 로 전환

--- src/main/kotlin/stringcalculator/domain/customparser/CustomNumbersStringParser.kt
+++ src/main/kotlin/stringcalculator/domain/customparser/CustomNumbersStringParser.kt
@@ -23,15 +23,15 @@ class CustomNumbersStringParser(stringExpression: String, separators: ParserSepa
     companion object {
         private const val ERROR_MESSAGE_SEPARATOR_ZERO = "숫자를 추출하기 위한 구분자가 없습니다"
 
+        private fun validateParsedNumberForIncludeNegativeNumber(parsedNumbers: List<Int>) {
+            parsedNumbers.forEach { require(it >= 0) { getErrorMessageIncludeNegativeNumber(it) } }
+        }
+
         private fun getErrorMessageIncludeNegativeNumber(negativeNumber: Int): String {
             return "음수($negativeNumber)은 입력할수 없습니다"
         }
 
-        fun validateParsedNumberForIncludeNegativeNumber(parsedNumbers: List<Int>) {
-            parsedNumbers.forEach { require(it >= 0) { getErrorMessageIncludeNegativeNumber(it) } }
-        }
-
-        fun validateExpressionIncludeOtherStrings(expression: String, separators: ParserSeparators) {
+        private fun validateExpressionIncludeOtherStrings(expression: String, separators: ParserSeparators) {
             require(getRegexBySeparators(separators).matches(expression)) {
                 "숫자, 구분 문자(${getSeparatorStringForRegex(separators)}) 를 제외한 문자가 포함 되어 있습니다"
             }
