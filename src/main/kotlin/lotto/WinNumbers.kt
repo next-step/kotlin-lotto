5dfea2407ceca3a1edcfe39db45d40d1a508b2e0
Jake Son <sonjeabin@gmail.com>
2022-05-22 17:38:07 +0900

    feat: 당첨 번호 생성 시 중복 허용하지 않기

--- src/main/kotlin/lotto/WinNumbers.kt
+++ src/main/kotlin/lotto/WinNumbers.kt
@@ -3,5 +3,9 @@ package lotto
 @JvmInline
 value class WinNumbers(val value: List<Int>) {
 
+    init {
+        require(value.toSet().size == value.size) { "중복된 숫자가 있습니다" }
+    }
+
     fun matchCount(lotto: Lotto): Int = value.count { lotto.numbers.contains(it) }
 }
