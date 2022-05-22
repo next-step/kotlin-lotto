e16348d067afcfcb369d2ceca418bc52c35d0573
Jake Son <sonjeabin@gmail.com>
2022-05-22 14:15:48 +0900

    feat: 숫자 일치 개수에 대한 검증 추가

--- src/main/kotlin/lotto/WinPolicy.kt
+++ src/main/kotlin/lotto/WinPolicy.kt
@@ -1,3 +1,8 @@
 package lotto
 
-data class WinPolicy(val matchCount: Int, val priceAmount: Money)
+data class WinPolicy(val matchCount: Int, val priceAmount: Money) {
+    init {
+        require(1 <= matchCount) { "숫자 일치 개수는 1보다 커야합니다" }
+        require(matchCount <= Lotto.NUMBER_COUNT) { "숫자 일치 개수는 최대 ${Lotto.NUMBER_COUNT}개까지 가능합니다" }
+    }
+}
