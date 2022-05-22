3deea4783051d352144741b67e4cf57fc1cf804c
KM <sodp2@naver.com>
2022-05-22 14:45:11 +0900

    feat: 덧셈 함수에 커스텀 구분자를 사용할 수 있께 추가

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -4,9 +4,19 @@ class Calculator {
     fun add(expression: String?): Int {
         if (expression.isNullOrEmpty()) return 0
 
-        val numbers = expression.split(",", ":").map { it.toInt() }
-
         var result = 0
+
+        Regex("//(.)\n(.*)").find(expression)?.let { matchResult ->
+            val customDelimiter = matchResult.groupValues[1]
+            val numbersByCustomDelimiter = matchResult.groupValues[2].split(customDelimiter).map { it.toInt() }
+            numbersByCustomDelimiter.forEach { number ->
+                result += number
+            }
+
+            return result
+        }
+
+        val numbers = expression.split(",", ":").map { it.toInt() }
         numbers.forEach { number ->
             result += number
         }
