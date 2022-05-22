54b8cd0aefb9011f787f1b17ee6fbb95f42c719a
Jake Son <sonjeabin@gmail.com>
2022-05-22 10:59:42 +0900

    refactor: 매직넘버 상수화

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,8 +3,10 @@ package lotto
 @JvmInline
 value class Lotto private constructor(val numbers: List<Int>) {
     companion object {
+        private const val NUMBER_COUNT = 6
+
         fun of(numbers: List<Int>): Lotto {
-            require(numbers.size == 6) { "로또 숫자의 개수는 6이어야 합니다" }
+            require(numbers.size == NUMBER_COUNT) { "로또 숫자의 개수는 $NUMBER_COUNT 이어야 합니다" }
 
             return Lotto(numbers)
         }
