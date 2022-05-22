545e1e56f483843de3da202ac5fd602747ce6c81
Jake Son <sonjeabin@gmail.com>
2022-05-22 10:58:03 +0900

    feat: 로또 생성 검증로직 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,11 @@
 package lotto
 
 @JvmInline
-value class Lotto(val numbers: List<Int>) {
+value class Lotto private constructor(val numbers: List<Int>) {
     companion object {
         fun of(numbers: List<Int>): Lotto {
+            require(numbers.size == 6) { "로또 숫자의 개수는 6이어야 합니다" }
+
             return Lotto(numbers)
         }
     }
