43a8a80ddc56ece632add9be90f70ff5e421ad1d
KM <sodp2@naver.com>
2022-05-22 14:47:52 +0900

    refactor: 숫자 리스트를 덧셈하는 부분을 함수로 분리

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -4,19 +4,18 @@ class Calculator {
     fun add(expression: String?): Int {
         if (expression.isNullOrEmpty()) return 0
 
-        var result = 0
-
         Regex("//(.)\n(.*)").find(expression)?.let { matchResult ->
             val customDelimiter = matchResult.groupValues[1]
             val numbersByCustomDelimiter = matchResult.groupValues[2].split(customDelimiter).map { it.toInt() }
-            numbersByCustomDelimiter.forEach { number ->
-                result += number
-            }
-
-            return result
+            return addNumbers(numbersByCustomDelimiter)
         }
 
         val numbers = expression.split(",", ":").map { it.toInt() }
+        return addNumbers(numbers)
+    }
+
+    private fun addNumbers(numbers: List<Int>): Int {
+        var result = 0
         numbers.forEach { number ->
             result += number
         }
