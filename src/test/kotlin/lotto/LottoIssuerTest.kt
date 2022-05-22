f9de84aa9ed8cb553ab40b9fd5eb06af1b9ab46f
Jake Son <sonjeabin@gmail.com>
2022-05-22 12:10:41 +0900

    test: 로또 생성기 숫자범위 케이스 추가

--- src/test/kotlin/lotto/LottoIssuerTest.kt
+++ src/test/kotlin/lotto/LottoIssuerTest.kt
@@ -1,6 +1,8 @@
 package lotto
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.ints.shouldBeBetween
 import io.kotest.matchers.types.shouldBeInstanceOf
 
 internal class LottoIssuerTest : StringSpec({
@@ -11,5 +13,15 @@ internal class LottoIssuerTest : StringSpec({
         val lotto = issuer.issue()
 
         lotto.shouldBeInstanceOf<Lotto>()
+    }
+
+    "생성한 로또는 1에서 45 사이의 숫자를 가진다" {
+        val issuer = LottoIssuer()
+
+        val lotto = issuer.issue()
+
+        lotto.numbers.forAll {
+            it.shouldBeBetween(1, 45)
+        }
     }
 })
