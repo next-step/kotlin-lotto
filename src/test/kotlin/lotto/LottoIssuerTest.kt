055e3afaac71f670dff24fd5a1cb9a039a1355cc
Jake Son <sonjeabin@gmail.com>
2022-05-22 12:37:03 +0900

    test: 로또 생성기 정렬 케이스 추가

--- src/test/kotlin/lotto/LottoIssuerTest.kt
+++ src/test/kotlin/lotto/LottoIssuerTest.kt
@@ -3,6 +3,7 @@ package lotto
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.ints.shouldBeBetween
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 
 internal class LottoIssuerTest : StringSpec({
@@ -14,10 +15,20 @@ internal class LottoIssuerTest : StringSpec({
     }
 
     "생성한 로또는 1에서 45 사이의 숫자를 가진다" {
-        val lotto = LottoIssuer.issue()
+        repeat(1000) {
+            val lotto = LottoIssuer.issue()
 
-        lotto.numbers.forAll {
-            it.shouldBeBetween(1, 45)
+            lotto.numbers.forAll {
+                it.shouldBeBetween(1, 45)
+            }
+        }
+    }
+
+    "생성한 로또 숫자는 오름차순으로 정렬된다 " {
+        repeat(1000) {
+            val lotto = LottoIssuer.issue()
+
+            lotto.numbers shouldBe lotto.numbers.sorted()
         }
     }
 })
