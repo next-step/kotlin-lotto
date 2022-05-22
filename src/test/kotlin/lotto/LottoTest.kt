961b9c7030182513e45c79614434db67093a34f3
Jake Son <sonjeabin@gmail.com>
2022-05-22 10:55:59 +0900

    test: 로또 클래스 생성 에러상황 케이스 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,13 +1,28 @@
 package lotto
 
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
-internal class LottoTest : StringSpec({
+internal class LottoTest : FreeSpec({
 
     "숫자 리스트를 받아 Lotto 인스턴스를 생성한다" {
         val lotto = Lotto.of(listOf(1, 2, 3, 4, 5, 6))
 
         lotto.numbers shouldBe listOf(1, 2, 3, 4, 5, 6)
+    }
+
+    "에러 상황" - {
+        listOf(
+            listOf(),
+            listOf(1),
+            listOf(1, 2, 3, 4, 5, 6, 7),
+        ).forEach {
+            "입력 리스트가 크기가 ${it.size} 일 경우" {
+                shouldThrow<IllegalArgumentException> {
+                    Lotto.of(it)
+                }
+            }
+        }
     }
 })
