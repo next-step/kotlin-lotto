968dba789608f64c2aeaf26b46d69d8b44a60263
김광훈 <60383031+Wave1994-Hoon@users.noreply.github.com>
2022-05-23 03:36:24 +0900

    Step1 - 문자열  덧셈 계산기 (#286)
    * [step1] 문자열 덧셈 계산기를 구현하라
    
    * [step1] Klint 를 적용하라

---
+++ src/main/kotlin/StringAddCalculator.kt
@@ -0,0 +1,29 @@
+object StringAddCalculator {
+    fun add(text: String?): Int {
+        if (text.isNullOrBlank()) {
+            return 0
+        }
+
+        val numbers: List<Int> = getNumbersFrom(text)
+        checkNegative(numbers)
+
+        return numbers.sum()
+    }
+
+    private fun getNumbersFrom(text: String): List<Int> =
+        (getTokenWithCustomDelimiter(text) ?: getToken(text)).map { it.toInt() }
+
+    private fun getTokenWithCustomDelimiter(text: String): List<String>? =
+        Regex("//(.)\n(.*)").find(text)?.let {
+            it.groupValues[2].split(it.groupValues[1])
+        }
+
+    private fun getToken(text: String): List<String> =
+        text.split(',', ':')
+
+    private fun checkNegative(numbers: List<Int>) {
+        if (numbers.any { it < 0 }) {
+            throw RuntimeException("음수를 입력할 수 없습니다.")
+        }
+    }
+}
