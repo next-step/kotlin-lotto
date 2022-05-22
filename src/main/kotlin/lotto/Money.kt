11da7eb1a52b58f547a5a9af6a7e06cf52bd8df0
Jake Son <sonjeabin@gmail.com>
2022-05-22 10:48:15 +0900

    feat: 금액 클래스 생성자 private 처리

--- src/main/kotlin/lotto/Money.kt
+++ src/main/kotlin/lotto/Money.kt
@@ -2,7 +2,7 @@ package lotto
 
 @JvmInline
 
-value class Money(val amount: Int) {
+value class Money private constructor(val amount: Int) {
     companion object {
         fun of(money: Int): Money {
             require(money >= 0) { "금액은 음수가 될 수 없습니다" }
