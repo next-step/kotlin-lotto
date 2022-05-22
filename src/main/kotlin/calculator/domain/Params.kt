d0040fbe9f1197d9b09be584cff93c74d6987196
jinwoo-k <br2k@daum.net>
2022-05-22 21:06:16 +0900

    refact: Strings를 Params와 통합

--- src/main/kotlin/calculator/domain/Params.kt
+++ src/main/kotlin/calculator/domain/Params.kt
@@ -1,14 +1,57 @@
 package calculator.domain
 
-class Params(strings: List<String>) {
+class Params(value: String?) {
     val values: List<Int>
 
     init {
-        if (strings.any { it.isBlank() }) {
-            throw RuntimeException("빈 문자열은 들어올 수 없습니다.")
-        } else if (strings.any { !it.all { it.isDigit() } }) {
-            throw RuntimeException("숫자가 아닌 입력은 들어올 수 없습니다.")
+        if (value.isNullOrBlank()) {
+            values = listOf(0)
+        } else {
+            val stringList = split(value)
+            stringList.forEach { validate(it) }
+            values = stringList.map { toInt(it) }
         }
-        values = strings.map { it.toInt() }
+    }
+
+    private fun toInt(it: String): Int {
+        if (it.isBlank()) {
+            return 0
+        }
+        return it.toInt()
+    }
+
+    private fun validate(string: String) {
+        if (string.matches(".*[^\\d^\\s]+.*".toRegex())) {
+            throw RuntimeException("숫자가 아닌 입력은 들어올 수 없습니다. (음수도 안됩니다!)")
+        }
+    }
+
+    private fun split(value: String): List<String> {
+        val delimiter = getDelimiter(value)
+        val contents = getContents(value)
+        return contents.split(delimiter)
+    }
+
+    private fun getDelimiter(value: String): Regex {
+        if (hasCustomDelimiter(value)) {
+            return value.split("//|\n".toRegex())[1].toRegex()
+        } else {
+            return DEFAULT_DELIMITER
+        }
+    }
+
+    private fun getContents(value: String): String {
+        if (hasCustomDelimiter(value)) {
+            return value.split("//|\n".toRegex())[2]
+        } else {
+            return value
+        }
+    }
+
+    private fun hasCustomDelimiter(value: String): Boolean =
+        value.contains("//") && value.contains("\n")
+
+    companion object {
+        private val DEFAULT_DELIMITER: Regex = "[,;]".toRegex()
     }
 }
--- src/test/kotlin/calculator/domain/ParamsTest.kt
+++ src/test/kotlin/calculator/domain/ParamsTest.kt
@@ -1,23 +1,50 @@
 package calculator.domain
 
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class ParamsTest {
-    @ParameterizedTest
-    @ValueSource(strings = ["1,2,3", "4,5,6,7"])
-    fun `파싱된 결과가 양수 숫자라면 예외가 발생하면 안된다`(input: String) {
-        Assertions.assertThat(Params(input.split(",")).values)
-            .isEqualTo(input.split(",").map { it.toInt() })
+
+    @Test
+    fun `getParams 해피패스 테스트`() {
+        Assertions.assertThat(Params("1;2;3").values).isEqualTo(listOf(1, 2, 3))
+        Assertions.assertThat(Params("2,3,4").values).isEqualTo(listOf(2, 3, 4))
+    }
+
+    @Test
+    fun `getParams 커스텀구분자 테스트`() {
+        Assertions.assertThat(Params("//;\n1;2;3").values).isEqualTo(listOf(1, 2, 3))
+        Assertions.assertThat(Params("//,\n2,3,4").values).isEqualTo(listOf(2, 3, 4))
+        Assertions.assertThat(Params("//&\n1&2&3").values).isEqualTo(listOf(1, 2, 3))
     }
 
     @ParameterizedTest
-    @ValueSource(strings = [",2,3", "4,,6,7", "1,-1,5", "-1,6,4"])
+    @ValueSource(strings = ["a,2,3", "4,a,6,7", "1,-1,5", "-1,6,4"])
     fun `파싱된 결과가 숫자 이외의 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw 한다`(input: String) {
         assertThrows<RuntimeException> {
-            Params(input.split(","))
+            Params(input)
         }
+    }
+
+    @Test
+    fun `공백이 입력되면 0으로 처리한다`() {
+        Assertions.assertThat(Params(" ").values).isEqualTo(listOf(0))
+        Assertions.assertThat(Params("1, ,2").values).isEqualTo(listOf(1, 0, 2))
+        Assertions.assertThat(Params(" ,1").values).isEqualTo(listOf(0, 1))
+    }
+
+    @Test
+    fun `빈 문자열 입력되면 0으로 처리한다`() {
+        Assertions.assertThat(Params("1,,2").values).isEqualTo(listOf(1, 0, 2))
+        Assertions.assertThat(Params(",1").values).isEqualTo(listOf(0, 1))
+        Assertions.assertThat(Params("1,").values).isEqualTo(listOf(1, 0))
+    }
+
+    @Test
+    fun `null이 입력되면 0으로 처리한다`() {
+        Assertions.assertThat(Params(null).values).isEqualTo(listOf(0))
     }
 }
