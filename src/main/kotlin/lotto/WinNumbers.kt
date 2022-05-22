48691554a875f532f55a8b839a0e76ca517684aa
Jake Son <sonjeabin@gmail.com>
2022-05-22 16:09:08 +0900

    feat: 주어진 로또와 일치하는 개수확인 로직구현

--- src/main/kotlin/lotto/WinNumbers.kt
+++ src/main/kotlin/lotto/WinNumbers.kt
@@ -3,7 +3,5 @@ package lotto
 @JvmInline
 value class WinNumbers(val value: List<Int>) {
 
-    fun matchCount(lotto: Lotto): Int {
-        return 0
-    }
+    fun matchCount(lotto: Lotto): Int = value.count { lotto.numbers.contains(it) }
 }
