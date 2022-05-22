e5cdb3d38e596ae47111738db5839048a596f297
Juice <3221ju@gmail.com>
2022-05-23 02:15:51 +0900

    feat: ParserSeparator validation Exception 에러 메시지 추가

--- src/main/kotlin/stringcalculator/domain/customparser/ParserSeparator.kt
+++ src/main/kotlin/stringcalculator/domain/customparser/ParserSeparator.kt
@@ -7,7 +7,12 @@ class ParserSeparator(val string: String) {
     }
 
     private fun validate() {
-        require(string.isNotEmpty()) {}
-        require(!Regex("(\\d)").containsMatchIn(string)) {}
+        require(string.isNotEmpty()) { ERROR_MESSAGE_IS_EMPTY }
+        require(!Regex("(\\d)").containsMatchIn(string)) { ERROR_MESSAGE_HAS_NUMBER }
+    }
+
+    companion object {
+        private const val ERROR_MESSAGE_IS_EMPTY = "구분자는 비워진 문자열로 넣을수 없습니다"
+        private const val ERROR_MESSAGE_HAS_NUMBER = "구분자는 숫자를 포함 할수 없습니다"
     }
 }
--- src/test/kotlin/stringcalculator/domain/customparser/ParserSeparatorTest.kt
+++ src/test/kotlin/stringcalculator/domain/customparser/ParserSeparatorTest.kt
@@ -3,6 +3,7 @@ package stringcalculator.domain.customparser
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
 
 class ParserSeparatorTest : DescribeSpec({
     it("ParserSeparator 는 문자열에서 숫자를 구분하기 위한 문자를 보관한다") {
@@ -22,7 +23,7 @@ class ParserSeparatorTest : DescribeSpec({
             // then
             shouldThrowExactly<IllegalArgumentException> {
                 ParserSeparator(exceptionSeparatorString)
-            }
+            }.shouldHaveMessage("구분자는 비워진 문자열로 넣을수 없습니다")
         }
 
         it("숫자가 들어가 있는 경우 IllegalArgumentException 발생") {
@@ -32,7 +33,7 @@ class ParserSeparatorTest : DescribeSpec({
             // then
             shouldThrowExactly<IllegalArgumentException> {
                 ParserSeparator(exceptionSeparatorString)
-            }
+            }.shouldHaveMessage("구분자는 숫자를 포함 할수 없습니다")
         }
 
         it("중간에 숫자가 포함되어 있는 경우 IllegalArgumentException 발생") {
