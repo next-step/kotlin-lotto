56aaac3e2e040579f9b800277670b4b3087f1905
revfactory <revfactory@gmail.com>
2022-05-23 08:26:00 +0900

    refactor: Refactoring Code

--- src/main/kotlin/lotto/LottoController.kt
+++ src/main/kotlin/lotto/LottoController.kt
@@ -2,7 +2,6 @@ package lotto
 
 import lotto.domain.Lotto
 import lotto.domain.LottoMatchReport
-import lotto.domain.LottoNumbers
 import lotto.domain.LottoSeller
 import lotto.ui.LottoPurchaseView
 import lotto.ui.StatisticsView
@@ -15,19 +14,17 @@ class LottoController {
     fun start() {
         val inputPrice = LottoPurchaseView.inputPriceForPurchase()
         val inputManualLottoCount = LottoPurchaseView.inputManualLottoCount()
-        val inputManualLottoNumbers = LottoPurchaseView.inputManualLottoNumbers(inputManualLottoCount)
-
-        val lottoList = purchaseLotto(inputPrice, inputManualLottoNumbers)
-
+        val lottoList = purchaseLotto(inputPrice, inputManualLottoCount)
         val winningLotto = WinningLottoView.inputWinningLotto()
         val lottoRanks = winningLotto.match(lottoList)
         StatisticsView.printResult(LottoMatchReport.of(lottoRanks))
     }
 
-    private fun purchaseLotto(inputPrice: Long, inputManualLottoNumbers: List<LottoNumbers>): List<Lotto> {
-        require(inputPrice >= inputManualLottoNumbers.size * LottoSeller.LOTTO_PRICE) {
+    private fun purchaseLotto(inputPrice: Long, inputManualLottoCount: Int): List<Lotto> {
+        require(inputPrice >= inputManualLottoCount * LottoSeller.LOTTO_PRICE) {
             "수동으로 구매할 로또 금액이 총 구입급액을 초과합니다."
         }
+        val inputManualLottoNumbers = LottoPurchaseView.inputManualLottoNumbers(inputManualLottoCount)
         return lottoSeller.purchase(inputPrice, inputManualLottoNumbers)
             .also { LottoPurchaseView.printPurchaseResult(it) }
     }
