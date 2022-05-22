89c646b7ee595958808f4d562a85ee1035fb9fd6
Jake Son <sonjeabin@gmail.com>
2022-05-22 17:18:48 +0900

    feat: 당첨 회수 계산 로직 구현

--- src/main/kotlin/lotto/Matcher.kt
+++ src/main/kotlin/lotto/Matcher.kt
@@ -1,15 +1,14 @@
 package lotto
 
-class Matcher(val winNumbers: WinNumbers, policies: List<WinPolicy>) {
+class Matcher(private val winNumbers: WinNumbers, policies: List<WinPolicy>) {
     private val sortedPolicies = policies.sortedBy { it.matchCount }
 
-    fun makeResult(lotto: List<Lotto>): List<MatchResult> {
-        return sortedPolicies.map {
+    fun makeResult(lottos: List<Lotto>): List<MatchResult> =
+        sortedPolicies.map {
             MatchResult(
                 matchCount = it.matchCount,
                 price = it.priceAmount,
-                winCount = 0
+                winCount = lottos.count { lotto -> it.isMatch(winNumbers, lotto) }
             )
         }
-    }
 }
