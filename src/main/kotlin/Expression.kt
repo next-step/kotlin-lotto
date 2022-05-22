ac825acf76ef5599cfe0c0346f740329217a146f
keira <keira.j@kakaocorp.com>
2022-05-22 17:48:18 +0900

    feature: 커스텀하게 구분자를 지정할 수 있도록 함

--- src/main/kotlin/Expression.kt
+++ src/main/kotlin/Expression.kt
@@ -2,15 +2,30 @@ class Expression {
     var operators = listOf<String>()
     var digits = listOf<String>()
 
-    fun getTokens(text: String): List<String> {
-        val tokens = if (text.trim().isNullOrEmpty()) {
-            listOf("0")
-        } else {
-            text.split(",|:".toRegex())
+    fun getTokens(text: String): List<Int>? {
+        val result = Regex(NEWLINE_REGEX).find(text)
+        val inputString = result?.let {
+            val customDelimiter = it.groupValues[1]
+            DELEMITER_REGEX = customDelimiter
+
+            it.groupValues[2]
         }
 
-        require(tokens.all { it.toInt() > 0 })
+        val tokens = inputString?.let {
+            if (it?.trim().isNullOrEmpty()) {
+                return listOf(0)
+            } else {
+                it.split(DELEMITER_REGEX.toRegex()).map { it.toInt() }
+            }
+        }
+
+        require(tokens?.all { it > 0 } ?: false)
 
         return tokens
+    }
+
+    companion object {
+        private val NEWLINE_REGEX = """//(.)\\n(.*)"""
+        private var DELEMITER_REGEX = ",|:"
     }
 }
--- src/test/kotlin/ExpressionTest.kt
+++ src/test/kotlin/ExpressionTest.kt
@@ -6,20 +6,20 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-internal class ExpressionTest {
+class ExpressionTest {
 
     @ValueSource(strings = ["", " "])
     @ParameterizedTest
     fun `빈 문자열이 입력된 경우 0 return`(source: String) {
         val expression = Expression()
-        assertThat(expression.getTokens(source)).isEqualTo(listOf("0"))
+        assertThat(expression.getTokens(source)).isEqualTo(listOf(0))
     }
 
     @ValueSource(strings = ["1:2:3", "1,2,3", "1,2:3"])
     @ParameterizedTest
     fun `구분자를 통해 토큰을 출력한다 - 다양한 토큰이 한 문자열에 있는 경우에도 출력 가능하다`(source: String) {
         val expression = Expression()
-        assertThat(expression.getTokens(source)).isEqualTo(listOf("1","2","3"))
+        assertThat(expression.getTokens(source)).isEqualTo(listOf(1,2,3))
     }
 
     @ValueSource(strings = ["-1:2:3"])
@@ -29,5 +29,12 @@ internal class ExpressionTest {
         assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
             expression.getTokens(source)
         }
+    }
+
+    @ValueSource(strings = ["//;\\n1;2;3"])
+    @ParameterizedTest
+    fun `문자 사이에 커스텀 구분자를 지정할 수 있다`(source: String) {
+        val expression = Expression()
+        assertThat(expression.getTokens(source)).isEqualTo(listOf(1,2,3))
     }
 }
