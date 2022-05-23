17d5717175ecaa683ff5e0eb900186d460572332
glennsm <lovinhut@gmail.com>
2022-05-23 15:18:28 +0900

    test: 지난당첨번호는 6개의 숫자이다

--- src/test/kotlin/lotto/CheckerTest.kt
+++ src/test/kotlin/lotto/CheckerTest.kt
@@ -1,21 +1,23 @@
 package lotto
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class CheckerTest {
-    @Test
-    fun `지난당첨번호와 일치하는 개수를 확인한다`() {
-        val checker = Checker("1, 2,4,3,5")
-        val lotto = Lotto().numbers
-        assertThat(checker.match(lotto)).isEqualTo(2)
+    @CsvSource(
+        value = [
+            "1",
+            "1,2,6",
+            "1,2,4,6",
+            "1,2,4,6,9",
+            "1,%,4,6,9",
+            "1,%,4,^,9",
+            "*",
+        ]
+    )
+    @ParameterizedTest
+    fun `지난당첨번호는 6개의 숫자이다`(lastNumber: String) {
+        assertThrows<RuntimeException> { Checker(lastNumber) }
     }
-}
-
-class Checker(LastNumberText: String) {
-    private val lastNumber: List<Int> = LastNumberText
-        .split(",")
-        .map { it.trim().toInt() }
-
-    fun match(lotto: List<Int>): Int = lotto.filter { it in lastNumber }.size
 }
