b57528091d2f7545d45cb318ba0c2f070a72cff9
kwanguuuu <kwangyu326@gmail.com>
2022-05-23 22:51:40 +0900

    [step2] refactor: 로또 숫자 선택 로직 리팩토링
    - 리뷰 반영 : https://github.com/next-step/kotlin-lotto/pull/280#discussion_r878801032

--- src/main/kotlin/lotto/LottoCreator.kt
+++ src/main/kotlin/lotto/LottoCreator.kt
@@ -1,12 +1,13 @@
 package lotto
 
 object LottoCreator {
-    private const val FIRST_LOTTERY_NUMBER_INDEX = 0
-    private const val LAST_LOTTERY_NUMBER_INDEX = 6
+    private const val PICK_LOTTO_NUMBER_SIZE = 6
 
     fun autoCreate(): LottoTicket {
-        val lottoNumbers = LOTTO_NUMBERS.shuffled()
-            .subList(FIRST_LOTTERY_NUMBER_INDEX, LAST_LOTTERY_NUMBER_INDEX)
+
+        val lottoNumbers = LOTTO_NUMBERS
+            .shuffled()
+            .take(PICK_LOTTO_NUMBER_SIZE)
             .sorted()
 
         return LottoTicket(lottoNumbers)
