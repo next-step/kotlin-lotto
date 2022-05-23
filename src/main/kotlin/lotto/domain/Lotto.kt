79e3dc1417dd52e3c62ae7f0774aa1e574668365
qyu.st <qyu.st@kakaocorp.com>
2022-05-23 23:17:52 +0900

    feat: Lotto 개발
    * 로또는 1~45의 수를 6개 가짐

---
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -0,0 +1,21 @@
+package lotto.domain
+
+data class Lotto(val numbers: List<Int>) {
+
+    init {
+        require(numbers.all { isInRange(it) } && numbers.size == LOTTO_NUMBER_COUNT) {
+            "Lotto should have 6 numbers between 1 to 45"
+        }
+    }
+
+    constructor(vararg numbers: Int) : this(numbers.toList())
+
+    fun isInRange(number: Int): Boolean {
+        return LOTTO_NUMBER_RANGE.contains(number)
+    }
+
+    companion object {
+        private val LOTTO_NUMBER_RANGE = 1..45
+        private const val LOTTO_NUMBER_COUNT = 6
+    }
+}
