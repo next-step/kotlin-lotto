07080bef14a8835c8a84a3146f038cfa895970fe
KM <sodp2@naver.com>
2022-05-24 01:59:17 +0900

    test: 로또 구매 추가

---
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -0,0 +1,25 @@
+package lotto
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class LottoTest {
+    private lateinit var lottoSeller: LottoSeller
+
+    @BeforeEach
+    fun setUp() {
+        lottoSeller = LottoSeller()
+    }
+
+    @Test
+    fun `1000원당 1장의 로또를 구매한다`() {
+        assertThat(lottoSeller.purchaseLotto(1000)).isEqualTo(1)
+    }
+}
+
+class LottoSeller {
+    fun purchaseLotto(money: Int): Int {
+        return 1
+    }
+}
