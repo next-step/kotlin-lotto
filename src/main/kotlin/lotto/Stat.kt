735f1144a48b3eb3bd3ac8dcea8195e7e97dc6ed
glennsm <lovinhut@gmail.com>
2022-05-23 20:50:17 +0900

    test: 로또번호들의 일치하는 번호개수를 확인한다

--- src/main/kotlin/lotto/Stat.kt
+++ src/main/kotlin/lotto/Stat.kt
@@ -1,12 +1,15 @@
 package lotto
 
 class Stat(private val lottos: List<Lotto>, private val checker: Checker) {
-    fun matchResult(): List<Pair<List<Int>, MatchState?>> = lottos
+    fun matchResult(): List<Pair<List<Int>, MatchState>> = lottos
         .map {
             it.numbers to MatchState.of(checker.match(it.numbers))
         }
 
     enum class MatchState(val matchCount: Int, val descriptor: String, val profit: Int) {
+        MATCH_0(0, "", 0),
+        MATCH_1(1, "", 0),
+        MATCH_2(2, "", 0),
         MATCH_3(3, "3개 일치 (5000원)- %s개", 5_000),
         MATCH_4(4, "4개 일치 (50000원)- %s개", 50_000),
         MATCH_5(5, "5개 일치 (1500000)- %s개", 1_500_000),
---
+++ src/test/kotlin/lotto/StatTest.kt
@@ -0,0 +1,29 @@
+package lotto
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class StatTest {
+    @CsvSource(
+        value = [
+            "'1,2,3,4,5,6|1,2,3,11,22,7|5,6,7,8,9,10', '1,2,3,11,22,33', '3|5|0'"
+        ]
+    )
+    @ParameterizedTest
+    fun `로또번호들의 일치하는 번호개수를 확인한다`(numberList: String, lastNumber: String, matchCount: String) {
+
+        val lottoNumbers = numberList.split("|")
+        val matchCounts = matchCount.split("|")
+        val lottos: List<Lotto> = lottoNumbers.map { number -> Lotto { this.generateNumbers(number.trim()) } }
+        val stats = Stat(lottos, Checker(lastNumber)).matchResult()
+        stats.forEachIndexed { index, record: Pair<List<Int>, Stat.MatchState> ->
+            val (_, matchState) = record
+            assertThat(matchState.matchCount).isEqualTo(matchCounts[index].trim().toInt())
+        }
+    }
+
+    private fun generateNumbers(text: String): List<Int> {
+        return text.split(",").map { it.toInt() }
+    }
+}
