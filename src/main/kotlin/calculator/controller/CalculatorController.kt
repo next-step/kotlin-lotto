c6011c63b3c73e41a58f83e72f9f65c0b9d9b46c
hyeon9mak <jinha3507@gmail.com>
2022-05-23 22:09:53 +0900

    feat: 문자열 덧셈 계산기 controller, view 구현

---
+++ src/main/kotlin/calculator/controller/CalculatorController.kt
@@ -0,0 +1,11 @@
+package calculator.controller
+
+import calculator.view.InputView
+import calculator.view.OutputView
+
+class CalculatorController(
+    private val inputView: InputView,
+    private val outputView: OutputView,
+) {
+
+}
