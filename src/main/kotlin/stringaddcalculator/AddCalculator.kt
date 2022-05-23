04f95775f5abff99cf89e9cbeceb73b87858c405
h-swon <heosw1107@gmail.com>
2022-05-23 19:43:53 +0900

    feat: 숫자 2개를 (,) 구분자로 입력 시 두 숫자의 합을 리턴

--- src/main/kotlin/stringaddcalculator/AddCalculator.kt
+++ src/main/kotlin/stringaddcalculator/AddCalculator.kt
@@ -4,6 +4,10 @@ class AddCalculator {
     fun calculate(expression: String?): Int {
         if (expression.isNullOrBlank())
             return 0
-        return expression.toInt()
+
+        return expression
+            .split(",")
+            .map { it.trim().toInt() }
+            .reduce { sum, operand -> sum + operand }
     }
 }
--- src/test/kotlin/stringaddcalculator/AddCalculatorTest.kt
+++ src/test/kotlin/stringaddcalculator/AddCalculatorTest.kt
@@ -25,4 +25,10 @@ class AddCalculatorTest {
     fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(expression: String, expect: Int) {
         assertThat(addCalculator.calculate(expression)).isEqualTo(expect)
     }
+
+    @ParameterizedTest
+    @CsvSource(value = ["'1,2',3", "'1,2,3,4,5',15"])
+    fun `숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다`(expression: String, expect: Int) {
+        assertThat(addCalculator.calculate(expression)).isEqualTo(expect)
+    }
 }
