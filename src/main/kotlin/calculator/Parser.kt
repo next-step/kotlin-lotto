78aad777df149ee6032318c9a7864234e15dc553
luke.im <questknight26@gmail.com>
2022-05-23 20:43:33 +0900

    feat : 커스텀 구분자 기능 추가

--- src/main/kotlin/calculator/Parser.kt
+++ src/main/kotlin/calculator/Parser.kt
@@ -2,12 +2,22 @@ package calculator
 
 class Parser {
 
-    fun parse(expression: String): List<String> {
-        return expression
-            .split(*DEFAULT_DELIMITERS.toCharArray())
+    fun parse(input: String): List<String> {
+        val (delimiters, expression) = parseDelimiterAndExpression(input)
+        return expression.split(delimiters.toRegex())
+    }
+
+    private fun parseDelimiterAndExpression(input: String): Pair<String, String> {
+        return parseRegex.find(input)?.let {
+            val (delimiter, expression) = it.destructured
+            if (delimiter.isEmpty()) DEFAULT_DELIMITERS to expression
+            else delimiter to expression
+        } ?: (DEFAULT_DELIMITERS to input)
     }
 
     companion object {
-        val DEFAULT_DELIMITERS = listOf(',', ';')
+        val parseRegex by lazy { Regex("(?://(.*)\n)?(.*)") }
+
+        const val DEFAULT_DELIMITERS = ",|;"
     }
 }
--- src/test/kotlin/calculator/ParserTest.kt
+++ src/test/kotlin/calculator/ParserTest.kt
@@ -12,4 +12,11 @@ class ParserTest {
     fun `기본 구분자를 기준으로 문자열을 숫자로 변환한다`(expression: String) {
         assertThat(Parser().parse(expression)).isEqualTo(listOf("1", "2", "3"))
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["//a\n1a2a3", "//ab\n1ab2ab3"])
+    // 문자열 앞부분에서 "//" 와 "\n" 값이 오면 그 사이에 있는 값을 커스텀 구분자로 한다.
+    fun `커스텀 구분자로 지정할 수 있다`(expression: String) {
+        assertThat(Parser().parse(expression)).isEqualTo(listOf("1", "2", "3"))
+    }
 }
