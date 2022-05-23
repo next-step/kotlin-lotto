fc9cdd6b5c6e7ee852dd72ca98fc866734a89a7d
seojeongkuk <wjdrnr6662@gmail.com>
2022-05-23 12:24:58 +0900

    Step2: 로또 (자동)
    - 당첨 번호를 입력받는다

--- src/main/kotlin/lotto/main.kt
+++ src/main/kotlin/lotto/main.kt
@@ -7,8 +7,13 @@ fun main() {
         println("구입금액을 입력해 주세요.")
         val price = readln().toInt()
         val lottos = LottoMarket.sell(price)
+
+        println("지난 주 당첨 번호를 입력해 주세요.")
+        val winningNumbers = readln()
+            .filter { !it.isWhitespace() }
+            .split(",")
+            .map { it.toInt() }
     } catch (e: NumberFormatException) {
         println("숫자가 아닌 값이 들어왔습니다")
     }
-
 }
