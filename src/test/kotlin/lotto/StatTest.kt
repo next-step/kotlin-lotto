46f6e47f5c6ea8ba14ca3ab62a352e0ec72e4805
glennsm <lovinhut@gmail.com>
2022-05-23 23:07:00 +0900

    test: 구매한 로또들의 합산금액을 확인한다

--- src/test/kotlin/lotto/StatTest.kt
+++ src/test/kotlin/lotto/StatTest.kt
@@ -16,13 +16,34 @@ class StatTest {
         val lottoNumbers = numberList.split("|")
         val matchCounts = matchCount.split("|")
         val lottos: List<Lotto> = lottoNumbers.map { number -> Lotto { this.generateNumbers(number.trim()) } }
-        val stats = Stat(lottos, Checker(lastNumber)).matchResult()
-        stats.forEachIndexed { index, record: MatchState ->
+        val matchRecords = Stat(lottos, Checker(lastNumber)).matchRecords
+        matchRecords.forEachIndexed { index, record: MatchState ->
             assertThat(record.matchCount).isEqualTo(matchCounts[index].trim().toInt())
         }
     }
 
     private fun generateNumbers(text: String): List<Int> {
         return text.split(",").map { it.toInt() }
+    }
+
+    @CsvSource(
+        value = [
+            "'1,2,3,4,5,6|1,2,3,11,22,7|5,6,7,8,9,10', '1,2,3,11,22,33', '5000+1500000+0'"
+        ]
+    )
+    @ParameterizedTest
+    fun `구매한 로또들의 합산금액을 확인한다`(numberList: String, lastNumber: String, sumPrice: String) {
+        val lottoNumbers = numberList.split("|")
+        val expectTotalPrice = sumPrice.split("+")
+            .map { it.trim().toInt() }
+            .reduce { total, price -> total + price }
+
+        val lottos: List<Lotto> = lottoNumbers.map { number -> Lotto { this.generateNumbers(number.trim()) } }
+        val stats = Stat(lottos, Checker(lastNumber))
+        val total = stats.sumRecords
+            .map { it.sum }
+            .reduce { total, price -> total + price }
+
+        assertThat(total).isEqualTo(expectTotalPrice)
     }
 }
