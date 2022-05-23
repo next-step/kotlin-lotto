b789faba20c3a0ea022f8b02ad764d21cc5be614
glennsm <lovinhut@gmail.com>
2022-05-23 23:57:47 +0900

    feat: 로또당첨금액에 대한 수익률을 계산한다

---
+++ src/test/kotlin/lotto/Profit.kt
@@ -0,0 +1,8 @@
+package lotto
+
+import kotlin.math.round
+
+class Profit(private val profit: Double, private val payment: Double) {
+    val yields: Double
+        get() = (round(profit / payment * 100)) / 100
+}
