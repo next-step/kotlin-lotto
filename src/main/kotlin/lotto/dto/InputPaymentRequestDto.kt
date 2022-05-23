69029827dfe2d7b5fe38817b6649bcef2f64d271
ujusy <lsyeon1997@ajou.ac.kr>
2022-05-23 21:20:41 +0900

    refacotr: 변수 선언 순서 변경

--- src/main/kotlin/lotto/dto/InputPaymentRequestDto.kt
+++ src/main/kotlin/lotto/dto/InputPaymentRequestDto.kt
@@ -5,11 +5,11 @@ data class InputPaymentRequestDto(
 ) {
 
     companion object {
+
+        const val CANNOT_CONVERT_INT = "정수로 변환할 수 업습니다."
         fun convertInt(inputPayment: String): InputPaymentRequestDto {
             val payment: Int = inputPayment.toIntOrNull() ?: throw IllegalArgumentException(CANNOT_CONVERT_INT)
             return InputPaymentRequestDto(payment)
         }
-
-        const val CANNOT_CONVERT_INT = "정수로 변환할 수 업습니다."
     }
 }
