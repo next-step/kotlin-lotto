23ad6652a0e55ad534b257f06783dd3b851eec0d
kwanguuuu <kwangyu326@gmail.com>
2022-05-23 23:00:55 +0900

    [step2] chore: 불필요 import 삭제

--- src/test/kotlin/lotto/LottoPurchaseTest.kt
+++ src/test/kotlin/lotto/LottoPurchaseTest.kt
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
-import java.lang.RuntimeException
 
 class LottoPurchaseTest {
 
