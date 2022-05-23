5e0b4e7efc82976ec29ae4efae24b2e04f39c9d6
ujusy <lsyeon1997@ajou.ac.kr>
2022-05-23 21:20:31 +0900

    refactor: 매직 리터럴 상수 화

--- src/main/kotlin/lotto/util/LottoNumberRandomGenerator.kt
+++ src/main/kotlin/lotto/util/LottoNumberRandomGenerator.kt
@@ -2,10 +2,14 @@ package lotto.util
 
 class LottoNumberRandomGenerator : LottoNumberGenerator {
     override fun numberSet(): List<Int> {
-        return lottoNumberList.shuffled().subList(0, 6).sorted()
+        return lottoNumberList.shuffled().subList(LOTTO_START_DIGIT, LOTTO_END_DIGIT).sorted()
     }
 
     companion object {
-        private val lottoNumberList: Set<Int> = (1..45).toSet()
+        private const val LOTTO_START_DIGIT = 0
+        private const val LOTTO_END_DIGIT = 6
+        private const val LOTTO_ISSUANCE_NUMBER_START = 1
+        private const val LOTTO_ISSUANCE_NUMBER_END = 45
+        private val lottoNumberList: Set<Int> = (LOTTO_ISSUANCE_NUMBER_START..LOTTO_ISSUANCE_NUMBER_END).toSet()
     }
 }
