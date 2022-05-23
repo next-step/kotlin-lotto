3a6f9882c235ffdb316a64f3c0efce012f7c39fd
luke.im <questknight26@gmail.com>
2022-05-23 18:51:36 +0900

    feat : 음수 값이 들어오면 RuntimeException 예외를 던지도록 수정

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -3,6 +3,11 @@ package calculator
 class StringCalculator {
 
     fun calculate(numbers: List<Int>): Int {
+        validation(numbers)
         return numbers.sum()
+    }
+
+    private fun validation(numbers: List<Int>) {
+        require(numbers.none { it < 0 }) { "음수값은 허용하지 않습니다." }
     }
 }
--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -2,6 +2,7 @@ package calculator
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class StringCalculatorTest {
 
@@ -9,5 +10,13 @@ class StringCalculatorTest {
     fun `숫자의 합을 구한다`() {
         val numbers = listOf(1, 2, 3)
         assertThat(StringCalculator().calculate(numbers)).isEqualTo(6)
+    }
+
+    @Test
+    fun `음수의 값이 들어오면 RuntimeException 가 발생한다`() {
+        val numbers = listOf(-1, -2, 4)
+        assertThrows<RuntimeException> {
+            StringCalculator().calculate(numbers)
+        }
     }
 }
