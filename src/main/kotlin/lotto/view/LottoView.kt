3fb7e291b1c995a99c885de046cd98286f1b3163
Jake Son <sonjeabin@gmail.com>
2022-05-23 09:34:11 +0900

    feat: 로또 view 구현

--- src/main/kotlin/lotto/view/LottoView.kt
+++ src/main/kotlin/lotto/view/LottoView.kt
@@ -5,5 +5,10 @@ import lotto.domain.Lotto
 class LottoView(private val output: Output, private val lottos: List<Lotto>) {
 
     fun print() {
+        output.print("${lottos.size}개를 구매했습니다.")
+
+        lottos.forEach {
+            output.print(it.numbers.toString())
+        }
     }
 }
