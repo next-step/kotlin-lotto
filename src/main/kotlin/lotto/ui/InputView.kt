5706850466e86b99891b7d0d232a3e5e1b8088e5
glennsm <lovinhut@gmail.com>
2022-05-24 19:13:10 +0900

    feat: 데이터 입력기능을 만든다

---
+++ src/main/kotlin/lotto/ui/InputView.kt
@@ -0,0 +1,18 @@
+package lotto.ui
+
+class InputView {
+    fun readPayment(): Int {
+        println(PAYMENT_TEXT)
+        return readln().toInt()
+    }
+
+    fun readLastNumber(): String {
+        println(LAST_NUMBER_TEXT)
+        return readln()
+    }
+
+    companion object {
+        private const val PAYMENT_TEXT = "구입금액을 입력해 주세요."
+        private const val LAST_NUMBER_TEXT = "지난 주 당첨 번호를 입력해 주세요."
+    }
+}
