a6ed63db76ed4a2a5e7f1e32a933b60cfb50185e
larry.p <larry.p@kakaocorp.com>
2022-05-24 21:17:36 +0900

    feat: n개 로또 티켓 생성 기능 구현

--- src/main/kotlin/lotto/LottoTicketMachine.kt
+++ src/main/kotlin/lotto/LottoTicketMachine.kt
@@ -8,7 +8,7 @@ object LottoTicketMachine {
     private val RANGE_OF_LOTTO_NUMBER = MIN_RANGE_OF_NUMBER..MAX_RANGE_OF_NUMBER
     const val SIZE_OF_LOTTO_NUMBER = 6
 
-    fun generate(): LottoTicket = LottoTicket(generateNumbers())
+    fun generate(size: Int = 1) = List(size) { LottoTicket(generateNumbers()) }
 
     private fun generateNumbers(): List<Int> {
         return RANGE_OF_LOTTO_NUMBER
--- src/test/kotlin/lotto/LottoTicketMachineTest.kt
+++ src/test/kotlin/lotto/LottoTicketMachineTest.kt
@@ -3,14 +3,21 @@ package lotto
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.collections.shouldBeSorted
 import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.next
 
 class LottoTicketMachineTest : BehaviorSpec({
 
-    Given("로또 번호 발급기는 ") {
-        And("무작위 번호를 생성하고") {
-            val ticket = LottoTicketMachine.generate()
-            val actual = ticket.numbers
+    Given("로또 번호 발급기는") {
+        And("무작위 번호로된 티켓을 1개 생성하고") {
+            val tickets = LottoTicketMachine.generate()
 
+            Then("생성된 티켓은 1개다") {
+                tickets.shouldHaveSize(1)
+            }
+
+            val actual = tickets.first().numbers
             Then("생성된 번호는 6개이다") {
                 actual.shouldHaveSize(LottoTicketMachine.SIZE_OF_LOTTO_NUMBER)
             }
@@ -21,6 +28,31 @@ class LottoTicketMachineTest : BehaviorSpec({
 
             Then("생성된 번호는 오름차순으로 정렬되어 있다") {
                 actual.shouldBeSorted()
+            }
+        }
+
+        And("무작위 번호로된 티켓을 n개 생성하고") {
+            val numberOfTicket = Arb.int(1..10).next()
+            val tickets = LottoTicketMachine.generate(numberOfTicket)
+
+            Then("생성된 티켓의 개수는 요청 개수와 동일하다") {
+                tickets.shouldHaveSize(numberOfTicket)
+            }
+
+            tickets.forEach { ticket ->
+                val actual = ticket.numbers
+                val actualName = actual.joinToString()
+                Then("생성된 번호는 6개이다 ($actualName)") {
+                    actual.shouldHaveSize(LottoTicketMachine.SIZE_OF_LOTTO_NUMBER)
+                }
+
+                Then("생성된 번호는 모두 다른 번호이다 ($actualName)") {
+                    actual.distinct().shouldHaveSize(LottoTicketMachine.SIZE_OF_LOTTO_NUMBER)
+                }
+
+                Then("생성된 번호는 오름차순으로 정렬되어 있다 ($actualName)") {
+                    actual.shouldBeSorted()
+                }
             }
         }
     }
