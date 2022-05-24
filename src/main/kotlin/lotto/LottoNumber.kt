203636b25d0ee83bef9d9059839173a0f0eb4037
larry.p <larry.p@kakaocorp.com>
2022-05-24 20:23:40 +0900

    refactor: 로또 번호 예외 테스트 및 검증코드 추가

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -1,3 +1,7 @@
 package lotto
 
-data class LottoNumber(val numbers: List<Int> = listOf(1, 2, 3, 4, 5, 6))
+data class LottoNumber(val numbers: List<Int>) {
+    init {
+        require(numbers.size == 6)
+    }
+}
--- src/test/kotlin/lotto/LottoNumberTest.kt
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -1,11 +1,25 @@
 package lotto
 
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-class LottoNumberTest: StringSpec({
+class LottoNumberTest : StringSpec({
+
     "로또 번호는 6개의 숫자를 가지고 있다" {
-        val lottoNumber = LottoNumber()
+        val lottoNumber = LottoNumber(numbers = listOf(1, 2, 3, 4, 5, 6))
         lottoNumber.numbers.size shouldBe 6
+    }
+
+    "로또 번호는 6개 이하의 숫자를 입력하게 되면 IllegalArgumentException을 발생한다" {
+        shouldThrowExactly<IllegalArgumentException> {
+            LottoNumber(listOf(1))
+        }
+    }
+
+    "로또 번호는 6개 이상의 숫자를 입력하게 되면 IllegalArgumentException을 발생한다" {
+        shouldThrowExactly<IllegalArgumentException> {
+            LottoNumber(listOf(1, 2, 3, 4, 5, 6, 7))
+        }
     }
 })
