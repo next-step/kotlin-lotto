6d6029e7633d41ab721fdd13b3fa5d2457859a0c
austen.lee <austen.lee@kakaocorp.com>
2022-05-25 00:29:13 +0900

    step1 refactor : inputMessage를 외부에서 받도록 수정

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,13 +1,13 @@
 package lotto.view
 
-object InputView {
-    private const val MESSAGE_INPUT_PURCHASE_AMOUNT = "구입 금액을 입력해 주세요"
-
+class InputView<T>(
+    private val inputMessage: String
+) {
     fun receiveUserInput() {
-        printPurchaseAmountInputMessage()
+        printInputMessage()
     }
 
-    private fun printPurchaseAmountInputMessage() {
-        println(MESSAGE_INPUT_PURCHASE_AMOUNT)
+    private fun printInputMessage() {
+        println(inputMessage)
     }
 }
