203325ed9499c6048bc003be53f3f9247d207aac
Justin.sg <justin.sg@kakaocorp.com>
2022-05-24 11:35:03 +0900

    [step2] test : 당첨된 숫자의 수를 확인한다

--- src/test/kotlin/lotto/ConfirmationTest.kt
+++ src/test/kotlin/lotto/ConfirmationTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class ConfirmationTest {
@@ -7,5 +8,12 @@ internal class ConfirmationTest {
     @Test
     fun `로또 확인을 위해 지난 주 당첨 번호, Lotto 정보를 전달 받는다`() {
         Confirmation("1, 2, 3, 4, 5, 6", Lotto(listOf()))
+    }
+
+    @Test
+    fun `당첨된 숫자의 갯수를 확인한다`() {
+        assertThat(
+            Confirmation("1, 2, 3, 4, 5, 6", Lotto(listOf(1, 2, 3, 10, 11, 12))).winningCount
+        ).isEqualTo(3)
     }
 }
