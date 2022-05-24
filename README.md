6afdfd292791a6b701d8aaa3ab076de661093cf3
teo.stra <teo.stra@kakaocorp.com>
2022-05-25 00:46:17 +0900

    STEP 1: 지정된 delimiter로 split 처리
    사전에 지정된 두 delimiter를 이용하여 split
    나뉜 int들을 더하여 출력하도록 함

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 [o] 사용자 입력 받기<br/>
 [o] null, 빈 문자열 시 0 리턴<br/>
 [o] 단일 숫자 출력<br/>
-[ ] 지정된 delimeter로 자르고 각 숫자 더하기<br/>
-[ ] 특수 delimeter 입력 받기<br/>
+[o] 지정된 delimiter로 자르고 각 숫자 더하기<br/>
+[ ] 특수 delimiter 입력 받기<br/>
--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -1,9 +1,14 @@
 package calculator
 
 class StringAddCalculator {
+    val delimiters = arrayOf(",", ":")
+
     fun add(text: String?): Int {
         if (text.isNullOrBlank()) return 0
 
-        return text.toInt()
+        val tokens = text.split(*delimiters)
+            .map { it.toInt() }
+
+        return tokens.reduce { total, right -> total + right }
     }
 }
