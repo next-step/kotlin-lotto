ded3eabe6a30fe0f0fb5240bee08c0b4933d8285
larry.p <larry.p@kakaocorp.com>
2022-05-24 20:46:48 +0900

    feat: 로또 번호 발급기의 실제 기능 구현

--- src/main/kotlin/lotto/LottoNumberMachine.kt
+++ src/main/kotlin/lotto/LottoNumberMachine.kt
@@ -2,8 +2,16 @@ package lotto
 
 object LottoNumberMachine {
 
-    fun generate(): List<Int> {
-        return listOf(1,2,3,4,5,6)
-    }
+    private const val MIN_RANGE_OF_NUMBER = 1
+    private const val MAX_RANGE_OF_NUMBER = 45
 
+    private val RANGE_OF_LOTTO_NUMBER = MIN_RANGE_OF_NUMBER..MAX_RANGE_OF_NUMBER
+    const val SIZE_OF_LOTTO_NUMBER = 6
+
+    fun generate(): List<Int> {
+        return RANGE_OF_LOTTO_NUMBER
+            .shuffled()
+            .take(SIZE_OF_LOTTO_NUMBER)
+            .sorted()
+    }
 }
