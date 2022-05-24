83a3b934be863cf53ce894ce75b8819a64a3cb7e
Justin.sg <justin.sg@kakaocorp.com>
2022-05-24 11:11:43 +0900

    [step2] test : 입력 받은 금액으로 구매할 수 있는 로또의 갯수를 확인한다

--- src/test/kotlin/lotto/MachineTest.kt
+++ src/test/kotlin/lotto/MachineTest.kt
@@ -9,4 +9,9 @@ internal class MachineTest {
     fun `로또 기계는 금액을 전달 받는다`() {
         assertThat(Machine(10000).price).isEqualTo(Machine(10000).price)
     }
+
+    @Test
+    fun `입력 받은 금액으로 구매할 수 있는 로또의 갯수를 확인한다`() {
+        assertThat(Machine(10002).lottoCount).isEqualTo(10)
+    }
 }
