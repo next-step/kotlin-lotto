7bcca563ac587d0264eb16f30ce3b2e8ac2d8a61
larry.p <larry.p@kakaocorp.com>
2022-05-24 20:46:30 +0900

    refactor: 로또 번호 발급기 테스트 코드를 BehaviorSpec으로 변경

--- src/test/kotlin/lotto/LottoNumberMachineTest.kt
+++ src/test/kotlin/lotto/LottoNumberMachineTest.kt
@@ -1,24 +1,26 @@
 package lotto
 
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.collections.shouldBeSorted
 import io.kotest.matchers.collections.shouldHaveSize
 
-class LottoNumberMachineTest : StringSpec({
+class LottoNumberMachineTest : BehaviorSpec({
 
-    "로또 기계 무작위 번호를 생성하고" {
-        val numbers = LottoNumberMachine.generate()
+    Given("로또 번호 발급기는 ") {
+        And("무작위 번호를 생성하고") {
+            val numbers = LottoNumberMachine.generate()
 
-        "생성된 번호는 6개이다" {
-            numbers.shouldHaveSize(6)
-        }
+            Then("생성된 번호는 6개이다") {
+                numbers.shouldHaveSize(LottoNumberMachine.SIZE_OF_LOTTO_NUMBER)
+            }
 
-        "생성된 번호는 모두 다른 번호이다" {
-            numbers.distinct().shouldHaveSize(6)
-        }
+            Then("생성된 번호는 모두 다른 번호이다") {
+                numbers.distinct().shouldHaveSize(LottoNumberMachine.SIZE_OF_LOTTO_NUMBER)
+            }
 
-        "생성된 번호는 오름차순으로 정렬되어 있다" {
-            numbers.shouldBeSorted()
+            Then("생성된 번호는 오름차순으로 정렬되어 있다") {
+                numbers.shouldBeSorted()
+            }
         }
     }
 })
