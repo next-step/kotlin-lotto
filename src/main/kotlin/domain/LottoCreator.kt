a02a8d02b64e7ab4dceb92a606ef597fea957450
keira <keira.j@kakaocorp.com>
2022-05-24 23:19:47 +0900

    feature: 로또 발급 클래스 생성

---
+++ src/main/kotlin/domain/LottoCreator.kt
@@ -0,0 +1,24 @@
+package domain
+
+import kotlin.random.Random
+
+object LottoCreator {
+
+    fun issue(count: Int): List<List<Int>> {
+        val randomIssuedList = mutableListOf<List<Int>>()
+        repeat(count) {
+            randomIssuedList.add(
+                generateSequence { Random(Random.nextInt()).nextInt(LOTTO_NUMBER_RANGE.first, LOTTO_NUMBER_RANGE.last) }
+                    .distinct()
+                    .take(LOTTO_NUMBER_COUNT)
+                    .sorted()
+                    .toList()
+            )
+        }
+
+        return randomIssuedList
+    }
+
+    const val LOTTO_NUMBER_COUNT = 6
+    val LOTTO_NUMBER_RANGE = (1..45)
+}
---
+++ src/test/kotlin/domain/LottoCreatorTest.kt
@@ -0,0 +1,14 @@
+package domain
+
+import domain.LottoCreator.issue
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LottoCreatorTest {
+
+    @Test
+    fun `로또를 설정한 횟수에 맞게 랜덤하게 발급한다`() {
+        val count = 3
+        assertThat(issue(count).size).isEqualTo(count)
+    }
+}
