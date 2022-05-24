df920c099a2adc52e677e89ed22a4dd7955ba8fd
Justin.sg <justin.sg@kakaocorp.com>
2022-05-24 11:26:33 +0900

    [step2] test : 로또 당첨 여부를 확인하기 위해 로또 정보를 전달받는다

--- src/test/kotlin/lotto/ConfirmationTest.kt
+++ src/test/kotlin/lotto/ConfirmationTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 internal class ConfirmationTest {
 
     @Test
-    fun `로또 확인을 위해 지난 주 당첨 번호를 입력받는다`() {
-        Confirmation("1, 2, 3, 4, 5, 6")
+    fun `로또 확인을 위해 지난 주 당첨 번호, Lotto 정보를 전달 받는다`() {
+        Confirmation("1, 2, 3, 4, 5, 6", Lotto())
     }
 }
