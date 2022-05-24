4de24d8948fe9aab6c230e286eeeda6a03398743
larry.p <larry.p@kakaocorp.com>
2022-05-25 01:17:31 +0900

    feat: 로또 메인 구현

---
+++ src/main/kotlin/lotto/LottoMain.kt
@@ -0,0 +1,16 @@
+package lotto
+
+fun main() {
+    val inputView = InputView(reader = ::readLine, writer = ::print)
+    val resultView = ResultView(writer = ::print)
+
+    val money = inputView.readPurchaseMoney()
+    val lottoTickets = LottoStore.buy(money)
+    resultView.printLottoTickets(lottoTickets)
+
+    val lastLottoNumbers = inputView.readLastLottoNumbers()
+
+    val lottoStatistics = LottoStatistics(lottoTickets, lastLottoNumbers)
+    resultView.printLottoStatistics(lottoStatistics)
+    resultView.printStatisticsProfit(lottoStatistics, money)
+}
