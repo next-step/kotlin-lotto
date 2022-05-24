01b5bdaf4148c1badf36495b52068dedc3f72124
mygoaway <mygoaway2@gmail.com>
2022-05-24 23:34:50 +0900

    ref: 음수 체크하는 함수명 변경 및 구분자 상수화 작업 진행

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -1,35 +1,36 @@
 package calculator
 
 class StringAddCalculator {
-
     fun add(text: String?) : Int {
         if(text.isNullOrBlank()) {
             return ZERO
         }
         val numbers = (getStringListWithCustomRegx(text) ?: getStringListWithDefinedRegx(text))
             .map { string -> string.toInt() }
-        validatedMinus(numbers)
+        validatedNumber(numbers)
         return numbers.sum()
     }
 
     private fun getStringListWithCustomRegx(text: String) : List<String>? {
-        return Regex("//(.)\n(.*)").find(text)?.let {
+        return Regex(CUSTOM_REGX).find(text)?.let {
             it.groupValues[2].split(it.groupValues[1])
         }
     }
 
     private fun getStringListWithDefinedRegx(text: String) : List<String> {
-        return text.split(",|:".toRegex())
+        return text.split(DEFINED_REGX.toRegex())
     }
 
-    private fun validatedMinus(numbers: List<Int>) {
+    private fun validatedNumber(numbers: List<Int>) {
         if(numbers.any{number -> number < 0}) {
             throw RuntimeException(TEXT_MINUS_MESSAGE)
         }
     }
 
     companion object {
+        private const val CUSTOM_REGX = "//(.)\n(.*)"
+        private const val DEFINED_REGX = ",|:"
         private const val ZERO = 0
-        const val TEXT_MINUS_MESSAGE = "음수를 입력할 수 없습니다."
+        private const val TEXT_MINUS_MESSAGE = "음수를 입력할 수 없습니다."
     }
 }
