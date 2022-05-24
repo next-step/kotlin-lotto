b18da50d07f9a0db4876d61bf3733632f00fdb53
Justin.sg <justin.sg@kakaocorp.com>
2022-05-24 11:14:17 +0900

    [step2] test : 구매 가능한 로또 수 만큼 랜덤 생성된 목록을 확인한다

--- src/test/kotlin/lotto/MachineTest.kt
+++ src/test/kotlin/lotto/MachineTest.kt
@@ -14,4 +14,9 @@ internal class MachineTest {
     fun `입력 받은 금액으로 구매할 수 있는 로또의 갯수를 확인한다`() {
         assertThat(Machine(10002).lottoCount).isEqualTo(10)
     }
+
+    @Test
+    fun `구매 가능한 로또 수 만큼 랜덤 생성된 목록을 확인한다`() {
+        assertThat(Machine(10002).lottoList.size).isEqualTo(10)
+    }
 }
