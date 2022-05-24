c26dde01352a9a9affdfd8d174292da84595c0ed
Juatae-Kim <jutaekim@sk.com>
2022-05-24 13:17:46 +0900

    refactor: Change ParseResult to sealed interface instead of sealed class

--- src/main/kotlin/lotto/model/data/ParseResult.kt
+++ src/main/kotlin/lotto/model/data/ParseResult.kt
@@ -1,8 +1,8 @@
 package lotto.model.data
 
-sealed class ParseResult<T> {
-    data class Value<T>(val value: T) : ParseResult<T>()
-    data class Error<T>(val error: Throwable) : ParseResult<T>() {
+sealed interface ParseResult<T> {
+    data class Value<T>(val value: T) : ParseResult<T>
+    data class Error<T>(val error: Throwable) : ParseResult<T> {
         constructor(errorMessage: String) : this(IllegalArgumentException(errorMessage))
     }
 }
