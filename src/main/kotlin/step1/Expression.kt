47cd3d3d7be2134d00abade0dfffe109a6d81cfb
keiraJ <37860683+keiraJ@users.noreply.github.com>
2022-05-24 19:45:54 +0900

    Step1 (#289)
    * feature: domain 클래스 분리 및 생성, 구조확립
    
    - expression 토큰 리턴 기능 작성
    - test 코드 작성하여 토큰 텍스트 예외 상황 체크
    
    * feature: 음수 예외 처리
    
    * feature: 커스텀하게 구분자를 지정할 수 있도록 함
    
    * feature: 불필요한 클래스 삭제, 10줄을 넘지 않도록 함수 분리
    
    * feature: 객체 생성부 beforeeach 로 분리
    
    * feature: simulator, input/output handler 생성 및 구현
    
    * feature: const 변수 클래스 내로 이동
    
    * feature: 대문자 변수명 -> camel case 로 변경
    
    * feature: customized splitter 추출 변수이름 세팅
    
    * feature: regex 생성자 한번만 부르도록 companion object 로 위치 변경
    
    * feature: 빈 문자열이나 null 이 입력되면 0을 리턴하도록 수정
    
    * feature: 함수명 변경
    
    * feature: package 생성 후 코드 이동
    
    Co-authored-by: keira <keira.j@kakaocorp.com>

---
+++ src/main/kotlin/step1/Expression.kt
@@ -0,0 +1,34 @@
+package step1
+
+class Expression {
+
+    private var delemiterRegex: Regex
+
+    init {
+        delemiterRegex = DEFAULT_DELEMITER_REGEX
+    }
+
+    fun getTokens(text: String): List<Int> {
+        val customizedInput: String? = toCustomSplitter(text)
+
+        if (customizedInput == null || customizedInput.trim().isNullOrEmpty()) return listOf(0)
+
+        if (customizedInput.split(delemiterRegex)?.any { it.toInt() < 0 }) throw RuntimeException()
+        return customizedInput.split(delemiterRegex).map { it.toInt() }
+    }
+
+    private fun toCustomSplitter(text: String): String? {
+        val result = NEWLINE_REGEX.find(text)
+        return result?.let {
+            val customDelimiter = it.groupValues[1]
+            delemiterRegex = Regex(customDelimiter)
+
+            it.groupValues[2]
+        } ?: text
+    }
+
+    companion object {
+        private val NEWLINE_REGEX = Regex("""//(.)\\n(.*)""")
+        private val DEFAULT_DELEMITER_REGEX = Regex(",|:")
+    }
+}
---
+++ src/main/kotlin/step1/LottoOutputHandler.kt
@@ -0,0 +1,10 @@
+package step1
+
+object LottoOutputHandler {
+
+    fun display(result: Int) {
+        println(OUTPUT_MESSAGE + result)
+    }
+
+    const val OUTPUT_MESSAGE = "결과 : "
+}
