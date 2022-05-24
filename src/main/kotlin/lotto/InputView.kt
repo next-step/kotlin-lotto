d1cbddb554b24de293f9e984dddbba6fbab17e12
larry.p <larry.p@kakaocorp.com>
2022-05-25 01:15:21 +0900

    feat: 입력 화면 구현

---
+++ src/main/kotlin/lotto/InputView.kt
@@ -0,0 +1,46 @@
+package lotto
+
+class InputView(private val reader: () -> String?, private val writer: (String) -> Unit) {
+
+    fun readPurchaseMoney(): Int {
+        printInputPurchaseMoney()
+
+        val value = reader()
+        requireNotNull(value)
+        require(value.isNotBlank())
+        return kotlin
+            .runCatching { value.toInt() }
+            .getOrIllegalArgumentException()
+    }
+
+    private fun printInputPurchaseMoney() {
+        writer("구입금액을 입력해 주세요.\n")
+    }
+
+    fun readLastLottoNumbers(): Set<Int> {
+        printInputLastLottoNumbers()
+
+        val value = reader()
+        requireNotNull(value)
+        require(value.isNotBlank())
+        return value
+            .tokenize()
+            .toInt()
+            .toSet()
+    }
+
+    private fun printInputLastLottoNumbers() {
+        writer("지난 주 당첨 번호를 입력해 주세요.\n")
+    }
+
+    private fun String.tokenize(): List<String> =
+        split(",")
+            .map { it.trim() }
+            .filter { it.isNotBlank() }
+
+    private fun List<String>.toInt() =
+        kotlin.runCatching { map { it.toInt() } }.getOrIllegalArgumentException()
+
+    private fun <R, T : R> Result<T>.getOrIllegalArgumentException(): R =
+        getOrElse { throw IllegalArgumentException(it.message) }
+}
