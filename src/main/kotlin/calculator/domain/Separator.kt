e9071fc8da2da8235f32a9d8ab9413d4feb231bf
hyeon9mak <jinha3507@gmail.com>
2022-05-24 20:34:50 +0900

    refactor: 구분자 정규화 로직 추가 및 불필요한 기능 제거

--- src/main/kotlin/calculator/domain/Separator.kt
+++ src/main/kotlin/calculator/domain/Separator.kt
@@ -1,14 +1,15 @@
 package calculator.domain
 
 enum class Separator(
-    val text: String
+    val text: String,
 ) {
     REST(","),
     COLON(":"),
     ;
 
     companion object {
-        fun findByText(text: String) = values().find { it.text == text }
-            ?: throw RuntimeException("'$text'는 문자열 덧셈 계산기 구분자가 아닙니다.")
+        fun toRegexWith(separatorValue: String) = values()
+            .joinToString(separator = separatorValue)
+            .toRegex()
     }
 }
