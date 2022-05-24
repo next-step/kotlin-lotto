1f31a7f46ddb531ab914a6e0404b8a517a0ee28c
austen.lee <austen.lee@kakaocorp.com>
2022-05-24 22:40:59 +0900

    step1 refactor : 안내문 출력 로직 추출

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -4,6 +4,10 @@ object InputView {
     private const val MESSAGE_INPUT_PURCHASE_AMOUNT = "구입 금액을 입력해 주세요"
 
     fun receiveUserInput() {
+        printPurchaseAmountInputMessage()
+    }
+
+    private fun printPurchaseAmountInputMessage() {
         println(MESSAGE_INPUT_PURCHASE_AMOUNT)
     }
 }
