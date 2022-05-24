b13b1fa397b3fd2ea5c713fe8fa5e678528b4462
mygoaway <mygoaway2@gmail.com>
2022-05-24 23:08:13 +0900

    feat: 구현해야 할 기능 중 2번~4번 기능 구현(콤마와 콜론 이용하여 두 숫자의 합 계산)

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -1,11 +1,17 @@
 package calculator
 
 class StringAddCalculator {
+
     fun add(text: String?) : Int {
         if(text.isNullOrBlank()) {
             return ZERO
         }
-        return 0
+        val numbers = getStringListWithDefinedRegx(text).map { string -> string.toInt() }
+        return numbers.sum()
+    }
+
+    private fun getStringListWithDefinedRegx(text: String) : List<String> {
+        return text.split(",|:".toRegex())
     }
 
     companion object {
