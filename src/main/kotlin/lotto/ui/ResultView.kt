942fd721ff2e49867c323be4fb554d8cbb04d7f8
glennsm <lovinhut@gmail.com>
2022-05-24 22:49:31 +0900

    feat: 로또발급번호를 보여준다

--- src/main/kotlin/lotto/ui/ResultView.kt
+++ src/main/kotlin/lotto/ui/ResultView.kt
@@ -1,12 +1,20 @@
 package lotto.ui
 
+import lotto.Lotto
+
 class ResultView {
 
     fun buyCount(count: Int) {
         println("$count" + BUY_COUNT_TEXT)
     }
 
+    fun lottoNumbers(lottos: List<Lotto>) {
+        lottos.forEach { println(it.numbers) }
+    }
+
     companion object {
         private const val BUY_COUNT_TEXT = "개를 구매했습니다."
+        private const val WINING_STAT_TEXT = "당첨 통계"
+        private const val TRY_COUNT_TEXT = "총 수익률은 %s입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)"
     }
 }
