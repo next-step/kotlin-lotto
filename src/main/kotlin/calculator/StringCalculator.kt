fd7797be73771fd0cc0de960cc805582063850a2
larry.p <larry.p@kakaocorp.com>
2022-05-24 19:54:06 +0900

    refactor: 1단계 문자열 덧셈 계산기 리뷰 반영

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -2,27 +2,21 @@ package calculator
 
 class StringCalculator {
 
-    fun add(input: String?): Int =
-        if (input.isNullOrBlank()) {
-            0
-        } else {
-            val numbers = getNumbers(input)
-            checkNegativeNumber(numbers)
-            numbers.sum()
-        }
+    fun add(input: String?): Int = if (input.isNullOrBlank()) 0 else getNumbers(input).sumOf { it.number }
 
-    private fun getNumbers(input: String): List<Int> =
-        (getCustomDelimiterTokens(input) ?: getPredefinedDelimiterTokens(input)).map { it.toInt() }
+    private fun getNumbers(input: String): List<PositiveNumber> =
+        (getCustomDelimiterTokens(input) ?: getPredefinedDelimiterTokens(input)).map { it.toPositiveNumber() }
 
     private fun getCustomDelimiterTokens(input: String): List<String>? =
-        Regex("//(.)\n(.*)").find(input)?.let {
-            it.groupValues[2].split(it.groupValues[1])
+        REGEX_CUSTOM_DELIMITER.find(input)?.let {
+            val (delimiter, tokens) = it.destructured
+            tokens.split(delimiter)
         }
 
     private fun getPredefinedDelimiterTokens(input: String): List<String> =
         input.split(',', ':')
 
-    private fun checkNegativeNumber(numbers: List<Int>) {
-        if (numbers.any { it < 0 }) throw RuntimeException()
+    companion object {
+        private val REGEX_CUSTOM_DELIMITER = Regex("//(.)\n(.*)")
     }
 }
