3377bba51a594bc84df2c8eadd8047cac97d1d77
austen.lee <austen.lee@kakaocorp.com>
2022-05-26 01:42:16 +0900

    step1 test : LottoMakerTest 수정

--- src/test/kotlin/lotto/domain/LottoMakerTest.kt
+++ src/test/kotlin/lotto/domain/LottoMakerTest.kt
@@ -9,15 +9,15 @@ class LottoMakerTest {
 
     @RepeatedTest(100)
     fun `LottoMaker는 1부터 45 사이의 숫자로 이루어진 로또 번호들을 만든다`() {
-        val lottoNumbers = LottoMaker.make()
+        val lotto = LottoMaker.make()
 
-        assertThat(lottoRange).containsAll(lottoNumbers)
+        assertThat(lottoRange).containsAll(lotto.numbers)
     }
 
     @Test
     fun `LottoMaker가 만들어내는 로또는 6개의 숫자로 이루어진다`() {
-        val lottoNumbers = LottoMaker.make()
+        val lotto = LottoMaker.make()
 
-        assertThat(lottoNumbers.size).isEqualTo(LottoMaker.LOTTO_NUMBER_COUNT)
+        assertThat(lotto.numbers.size).isEqualTo(LottoMaker.LOTTO_NUMBER_COUNT)
     }
 }
