1e9c3d12b75922ede8c135a75b0040b6d129e247
austen.lee <austen.lee@kakaocorp.com>
2022-05-26 01:21:12 +0900

    step1 feat : LottoMaker.make의 반환 타입을 Lotto로 변경

--- src/main/kotlin/lotto/domain/LottoMaker.kt
+++ src/main/kotlin/lotto/domain/LottoMaker.kt
@@ -1,5 +1,7 @@
 package lotto.domain
 
+import lotto.domain.model.Lotto
+
 object LottoMaker {
     const val LOTTO_NUMBER_COUNT = 6
     const val START_LOTTO_NUMBER = 1
@@ -7,5 +9,7 @@ object LottoMaker {
 
     private val lottoNumberRange = (START_LOTTO_NUMBER..END_LOTTO_NUMBER)
 
-    fun make(): List<Int> = lottoNumberRange.shuffled().take(LOTTO_NUMBER_COUNT)
+    fun make(): Lotto = Lotto(
+        numbers = lottoNumberRange.shuffled().take(LOTTO_NUMBER_COUNT)
+    )
 }
