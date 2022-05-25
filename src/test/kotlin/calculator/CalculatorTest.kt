9291c57d24b0bb20f4e9c62561d739bfc9f141e4
jade <jade.koh@kakaocorp.com>
2022-05-25 20:53:59 +0900

    test: 음수, 커스텀구분자에 대한 테스트 추가 작성

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -2,9 +2,12 @@ package calculator
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class CalculatorTest {
-
     @Test
     fun `빈 문자열은 0을 반환`() {
         // given
@@ -17,39 +20,108 @@ internal class CalculatorTest {
         assertThat(result).isEqualTo(0)
     }
 
-    @Test
-    fun `콤마 구분자`() {
+    @ParameterizedTest(name = "{0} => {1}")
+    @CsvSource(
+        "1,2,3=6",
+        "17,5,0=22",
+        delimiterString = "=",
+    )
+    fun `콤마 구분자`(input: String, expected: Int) {
         // given
         val calculator = Calculator()
 
         // when
-        val result = calculator.add("1,2,3")
+        val result = calculator.add(input)
 
         // then
-        assertThat(result).isEqualTo(6)
+        assertThat(result).isEqualTo(expected)
     }
 
-    @Test
-    fun `세미콜론 구분자`() {
+    @ParameterizedTest(name = "{0} => {1}")
+    @CsvSource(
+        "1:2:3=6",
+        "17:5:0=22",
+        delimiterString = "=",
+    )
+    fun `세미콜론 구분자`(input: String, expected: Int) {
         // given
         val calculator = Calculator()
 
         // when
-        val result = calculator.add("1:2:3")
+        val result = calculator.add(input)
 
         // then
-        assertThat(result).isEqualTo(6)
+        assertThat(result).isEqualTo(expected)
     }
 
-    @Test
-    fun `다른 종류의 구분자`() {
+    @ParameterizedTest(name = "{0} => {1}")
+    @CsvSource(
+        "1,2:3=6",
+        "1:2,3=6",
+        "17:5:0,99,1=122",
+        delimiterString = "=",
+    )
+    fun `콤마+세미콜론 구분자`(input: String, expected: Int) {
         // given
         val calculator = Calculator()
 
         // when
-        val result = calculator.add("1:2,3")
+        val result = calculator.add(input)
 
         // then
-        assertThat(result).isEqualTo(6)
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @ParameterizedTest(name = "{0} => {1}")
+    @CsvSource(
+        "999=999",
+        "1=1",
+        "10000=10000",
+        delimiterString = "=",
+    )
+    fun `한개의 숫자인 경우 해당 숫자를 반환`(input: String, expected: Int) {
+        // given
+        val calculator = Calculator()
+
+        // when
+        val result = calculator.add(input)
+
+        // then
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @ParameterizedTest(name = "{0} => 예외")
+    @ValueSource(
+        strings = [
+            "-1",
+            "-1,99",
+            "1,2,3,-4",
+        ]
+    )
+    fun `음수인 경우 예외 발생`(input: String) {
+        // given
+        val calculator = Calculator()
+
+        // when
+        // then
+        assertThrows<RuntimeException> { calculator.add(input) }
+    }
+
+    @ParameterizedTest(name = "{0} => {1}")
+    @CsvSource(
+        "//;\\n1;2;3=6",
+        "//JADE\\n1JADE2JADE3=6",
+        "//@@\\n1@@2@@3=6",
+        delimiterString = "=",
+    )
+    fun `커스텀 구분자`(input: String, expected: Int) {
+        // given
+        val calculator = Calculator()
+
+        // when
+        val result = calculator.add(input)
+
+        // then
+        assertThat(result).isEqualTo(expected)
     }
 }
