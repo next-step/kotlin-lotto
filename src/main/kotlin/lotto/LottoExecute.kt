479d8199f610e6fa40047a658fd3b9ce668249fc
KM <sodp2@naver.com>
2022-05-26 02:43:26 +0900

    feat: 메인함수에 로직이 따로 수행되지 않게 변경

--- src/main/kotlin/lotto/LottoExecute.kt
+++ src/main/kotlin/lotto/LottoExecute.kt
@@ -16,22 +16,8 @@ fun main() {
 
     val winningNumbers: List<Int> = LottoInputView.inputWinningNumbers()
 
-    var first = 0
-    var second = 0
-    var third = 0
-    var fourth = 0
-
-    lottoUser.lottos.forEach { lotto ->
-        when (lotto.getContainLottoNumberSameCount(winningNumbers)) {
-            Lotto.FOURTH_WINNING_NUMBER_SAME_COUNT -> fourth++
-            Lotto.THIRD_WINNING_NUMBER_SAME_COUNT -> third++
-            Lotto.SECOND_WINNING_NUMBER_SAME_COUNT -> second++
-            Lotto.FIRST_WINNING_NUMBER_SAME_COUNT -> first++
-        }
-    }
-
     val winningMoney = lottoUser.calculateWinningMoney(winningNumbers)
-    LottoOutputView.printWinningStatistics(fourth = fourth, third = third, second = second, first = first)
+    LottoOutputView.printWinningStatistics(lottos = lottoUser.lottos, winningNumbers = winningNumbers)
     LottoOutputView.printRateOfReturn(
         lottoUser.calculateRateOfReturn(
             spendMoney = LottoSeller.LOTTO_PRICE * ableLottoPurchaseCount,
--- src/main/kotlin/lotto/view/LottoOutputView.kt
+++ src/main/kotlin/lotto/view/LottoOutputView.kt
@@ -13,6 +13,29 @@ object LottoOutputView {
         }
     }
 
+    fun printWinningStatistics(lottos: List<Lotto>, winningNumbers: List<Int>) {
+        println("당첨 통계")
+        println("---------")
+        var first = 0
+        var second = 0
+        var third = 0
+        var fourth = 0
+
+        lottos.forEach { lotto ->
+            when (lotto.getContainLottoNumberSameCount(winningNumbers)) {
+                Lotto.FOURTH_WINNING_NUMBER_SAME_COUNT -> fourth++
+                Lotto.THIRD_WINNING_NUMBER_SAME_COUNT -> third++
+                Lotto.SECOND_WINNING_NUMBER_SAME_COUNT -> second++
+                Lotto.FIRST_WINNING_NUMBER_SAME_COUNT -> first++
+            }
+        }
+
+        println("3개 일치 (5000원)- ${fourth}개")
+        println("4개 일치 (50000원)- ${third}개")
+        println("5개 일치 (1500000원)- ${second}개")
+        println("6개 일치 (2000000000원)- ${first}개")
+    }
+
     fun printWinningStatistics(fourth: Int = 0, third: Int = 0, second: Int = 0, first: Int = 0) {
         println("당첨 통계")
         println("---------")
