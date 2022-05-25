1ddd25f4906eee04434ca3aa0708c5d87c3eff2a
Molly <dngwoodo@gmil.comm>
2022-05-25 21:49:01 +0900

    !! -> requireNotNull 로 변경

--- src/main/kotlin/lotto/LottoResultDto.kt
+++ src/main/kotlin/lotto/LottoResultDto.kt
@@ -1,11 +1,11 @@
 package lotto
 
 data class LottoResultDto(private val lottoResult: Map<String, List<Int>>, val rateOfReturn: String) {
-    val firstPrize = PrizeDto(6, lottoResult["6"]!!)
+    val firstPrize = PrizeDto(6, requireNotNull(lottoResult["6"]))
 
-    val secondPrize = PrizeDto(5, lottoResult["5"]!!)
+    val secondPrize = PrizeDto(5, requireNotNull(lottoResult["5"]))
 
-    val thirdPrize = PrizeDto(4, lottoResult["4"]!!)
+    val thirdPrize = PrizeDto(4, requireNotNull(lottoResult["4"]))
 
-    val fourthPrize = PrizeDto(3, lottoResult["3"]!!)
+    val fourthPrize = PrizeDto(3, requireNotNull(lottoResult["3"]))
 }
