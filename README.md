f878d6286c9e13da4338b42315238cc08e054deb
teo.stra <teo.stra@kakaocorp.com>
2022-05-25 21:21:27 +0900

    STEP 1: custom del 기능 추가
    custom delimiter를 분리하는 메소드 추가
    custom delimiter가 있는 경우 이를 포함하여 계산하도록 수정

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 [o] null, 빈 문자열 시 0 리턴<br/>
 [o] 단일 숫자 출력<br/>
 [o] 지정된 delimiter로 자르고 각 숫자 더하기<br/>
-[ ] 특수 delimiter 입력 받기<br/>
+[o] 특수 delimiter 입력 받기<br/>
 [ ] 잘못된 값인 경우의 처리
--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -1,14 +1,26 @@
 package calculator
 
 class StringAddCalculator {
-    val delimiters = arrayOf(",", ":")
+    private val delimiters = arrayOf(",", ":")
 
     fun add(text: String?): Int {
-        if (text.isNullOrBlank()) return 0
+        if (text.isNullOrBlank()) {
+            return 0
+        }
 
-        val tokens = text.split(*delimiters)
+        val (del, content) = text.extractDelimiter()
+
+        val tokens = content.split(*delimiters.addIfNotNull(del))
             .map { it.toInt() }
 
         return tokens.reduce { total, right -> total + right }
     }
+
+    private fun Array<String>.addIfNotNull(str: String?) =
+        if (str != null) { this + str } else { this }
+
+    private fun String.extractDelimiter() =
+        Regex("""//(.)\\n(.*)""").find(this)
+            ?.let { Pair(it.groupValues[1], it.groupValues[2]) }
+            ?: Pair(null, this)
 }
