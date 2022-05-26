e96c8798c45d98dfb4bb69783c34c0f136b24d3a
ellen.youn <ellen.youn@kakaocorp.com>
2022-05-26 19:12:13 +0900

    feat: 지난주 당첨번호를 입력한다.

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -6,7 +6,20 @@ class InputView {
         return readln().toIntOrNull() ?: throw RuntimeException()
     }
 
+    fun getPastWinner(): Lotto {
+        println(INPUT_LAST_WINNER)
+        val numbers = readln()
+            .split(LOTTO_SPLIT_DELIMITER)
+            .map { it.toIntOrNull() ?: throw RuntimeException(ERROR_WINNER_TICKET_VALIDATION) }
+            .toSet()
+        return Lotto.of(numbers)
+    }
+
     companion object {
         private const val INPUT_MONEY = "구입금액을 입력해 주세요."
+        private const val INPUT_LAST_WINNER = "지난 주 당첨 번호를 입력해 주세요."
+        private const val ERROR_WINNER_TICKET_VALIDATION = "지난 주 당첨번호는 숫자여야 합니다."
+
+        private const val LOTTO_SPLIT_DELIMITER = ","
     }
 }
