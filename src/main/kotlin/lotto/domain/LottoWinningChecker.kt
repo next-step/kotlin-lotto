ef3685b2c95ae97d4b7ef5ae91fd2d87116d28be
austen.lee <austen.lee@kakaocorp.com>
2022-05-27 03:16:58 +0900

    step1 refactor : LottoWinningChecker::check 파라미터 Lottos로 변경

--- src/main/kotlin/lotto/domain/LottoWinningChecker.kt
+++ src/main/kotlin/lotto/domain/LottoWinningChecker.kt
@@ -3,15 +3,16 @@ package lotto.domain
 import lotto.domain.model.Lotto
 import lotto.domain.model.LottoRank
 import lotto.domain.model.LottoResult
+import lotto.domain.model.Lottos
 import lotto.domain.model.WinningNumbers
 
 object LottoWinningChecker {
-    fun check(lottos: List<Lotto>, winningNumbers: WinningNumbers): LottoResult {
+    fun check(lottos: Lottos, winningNumbers: WinningNumbers): LottoResult {
         return LottoResult.from(lottos.toLottoWinningMap(winningNumbers))
     }
 
-    private fun List<Lotto>.toLottoWinningMap(winningNumbers: WinningNumbers): Map<Int, Int> {
-        return map { lotto ->
+    private fun Lottos.toLottoWinningMap(winningNumbers: WinningNumbers): Map<Int, Int> {
+        return value.map { lotto ->
             getNumberOfMatches(lotto, winningNumbers)
         }.groupingBy { numberOfMatches ->
             numberOfMatches
--- src/test/kotlin/lotto/domain/LottoWinningCheckerTest.kt
+++ src/test/kotlin/lotto/domain/LottoWinningCheckerTest.kt
@@ -2,6 +2,7 @@ package lotto.domain
 
 import lotto.domain.model.Lotto
 import lotto.domain.model.LottoRank
+import lotto.domain.model.Lottos
 import lotto.domain.model.WinningNumbers
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -9,12 +10,14 @@ import org.junit.jupiter.api.Test
 class LottoWinningCheckerTest {
     @Test
     fun `LottoWinningChecker는 로또 번호들과 당첨 번호를 받아 LottoResult를 반환한다`() {
-        val lottos = listOf(
-            Lotto(listOf(8, 21, 23, 41, 42, 43)),
-            Lotto(listOf(8, 21, 23, 41, 42, 44)),
-            Lotto(listOf(8, 21, 23, 41, 43, 44)),
-            Lotto(listOf(8, 21, 23, 41, 1, 2)),
-            Lotto(listOf(8, 21, 23, 1, 2, 3))
+        val lottos = Lottos(
+            listOf(
+                Lotto(listOf(8, 21, 23, 41, 42, 43)),
+                Lotto(listOf(8, 21, 23, 41, 42, 44)),
+                Lotto(listOf(8, 21, 23, 41, 43, 44)),
+                Lotto(listOf(8, 21, 23, 41, 1, 2)),
+                Lotto(listOf(8, 21, 23, 1, 2, 3))
+            )
         )
 
         val winningNumbers = WinningNumbers(listOf(8, 21, 23, 41, 42, 43))
