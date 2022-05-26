04249e6e8ff4fc05b027552b31c90edca85c7972
austen.lee <austen.lee@kakaocorp.com>
2022-05-27 03:19:02 +0900

    step1 feat : 전체 로직을 실행하는 LottoController

---
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -0,0 +1,59 @@
+package lotto.controller
+
+import lotto.domain.LottoBendingMachine
+import lotto.domain.LottoWinningChecker
+import lotto.domain.YieldCalculator
+import lotto.domain.model.UserInputRequest
+import lotto.domain.model.WinningNumbers
+import lotto.view.InputView
+import lotto.view.OutputView
+import lotto.view.inputconverter.StringToIntConverter
+import lotto.view.inputconverter.WinningNumbersConverter
+import lotto.view.outputconverter.LottoResultConverter
+import lotto.view.outputconverter.LottosConverter
+import lotto.view.outputconverter.YieldConverter
+
+object LottoController {
+    private const val GUIDANCE_MESSAGE_PURCHASE_AMOUNT = "구입 금액을 입력해 주세요."
+    private const val GUIDANCE_MESSAGE_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요."
+
+    fun execute() {
+        val purchaseAmount = getPurchaseAmount()
+        val lottos = LottoBendingMachine.purchase(purchaseAmount)
+        OutputView.println(
+            printable = lottos,
+            outputConverter = LottosConverter
+        )
+
+        val winningNumbers = getWinningNumbers()
+        val lottoResult = LottoWinningChecker.check(lottos, winningNumbers)
+        OutputView.print(
+            printable = lottoResult,
+            outputConverter = LottoResultConverter
+        )
+
+        val `yield` = YieldCalculator.calculate(lottoResult, purchaseAmount)
+        OutputView.print(
+            printable = `yield`,
+            outputConverter = YieldConverter
+        )
+    }
+
+    private fun getPurchaseAmount(): Int {
+        val userInputRequest = UserInputRequest(
+            message = GUIDANCE_MESSAGE_PURCHASE_AMOUNT,
+            inputConverter = StringToIntConverter
+        )
+
+        return InputView.receiveUserInput(userInputRequest)
+    }
+
+    private fun getWinningNumbers(): WinningNumbers {
+        val userInputRequest = UserInputRequest(
+            message = GUIDANCE_MESSAGE_WINNING_NUMBERS,
+            inputConverter = WinningNumbersConverter
+        )
+
+        return InputView.receiveUserInput(userInputRequest)
+    }
+}
