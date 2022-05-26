06ccbee74a5e1ddc4ea391b07b6ac2b6abf102b1
glennsm <lovinhut@gmail.com>
2022-05-27 02:44:30 +0900

    refactor: 불필요한 toList는 제거

--- src/main/kotlin/lotto/domain/LottoGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoGenerator.kt
@@ -5,7 +5,6 @@ import lotto.domain.LottoNumber.Companion.LOTTO_NUMBER_RANGE
 class LottoGenerator {
     fun generateNumbers(): List<LottoNumber> {
         return LOTTO_NUMBER_RANGE
-            .toList()
             .shuffled()
             .take(6)
             .map { LottoNumber(it) }
