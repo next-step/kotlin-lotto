a205fec74781ed955fd269fe045452cb840e849f
ellen.youn <ellen.youn@kakaocorp.com>
2022-05-26 18:50:04 +0900

    feat: 로또는 1~45 사이의 숫자를 가진다.

---
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -0,0 +1,15 @@
+package lotto
+
+@JvmInline
+value class LottoNumber(val number: Int) {
+    init {
+        require(number in MIN_NUMBER..MAX_NUMBER) { ERROR_LOTTO_NUMBER_RANGE }
+    }
+
+    companion object {
+        const val MIN_NUMBER = 1
+        const val MAX_NUMBER = 45
+
+        private const val ERROR_LOTTO_NUMBER_RANGE = "로또숫자는 $MIN_NUMBER ~ $MAX_NUMBER 사이의 값을 갖는다"
+    }
+}
