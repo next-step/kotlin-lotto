f2aa8cda438f2b8c7b68b5704e1559cee41b8f04
glenn.kgp <glenn.kgp@kakaocorp.com>
2022-05-26 22:11:34 +0900

    refactor: 보너스볼 에러메시지 수정

--- src/main/kotlin/lotto/domain/InputValidator.kt
+++ src/main/kotlin/lotto/domain/InputValidator.kt
@@ -1,18 +1,18 @@
 package lotto.domain
 
 object InputValidator {
-    private const val ERROR_MSG_FOR_NATURAL_NUMBER = "1이상 정수로 입력해주세요"
+    private const val ERROR_MSG_FOR_VALID_BONUS_NUMBER = "보너스볼은 1~45사이로 입력해주세요"
     private const val ERROR_MSG_FOR_WRONG_LOTTO = "로또 개수는 6개입니다 ,로 구분해주세요"
     private const val LOTTO_LIST_DELIMITER = ","
     private const val LOTTO_SIZE = 6
     private val LOTTO_RANGE = 1..45
 
     fun checkIsValidMoney(input: String) {
-        require(input.toInt() > 0) { ERROR_MSG_FOR_NATURAL_NUMBER }
+        require(input.toInt() > 0) { ERROR_MSG_FOR_VALID_BONUS_NUMBER }
     }
 
     fun checkValidBonusBall(input: String) {
-        require(input.toInt() in LOTTO_RANGE) { ERROR_MSG_FOR_NATURAL_NUMBER }
+        require(input.toInt() in LOTTO_RANGE) { ERROR_MSG_FOR_VALID_BONUS_NUMBER }
     }
 
     fun checkValidLotto(input: String) {
