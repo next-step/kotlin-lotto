4ab1de0e04370c46b0bdf3c677272137f1c7c6b5
larry.p <larry.p@kakaocorp.com>
2022-05-27 01:41:27 +0900

    refactor: 로또 번호의 유효성 검증을 위한 상수를 LottoTicket으로 이동

--- src/main/kotlin/lotto/domain/LottoTicketMachine.kt
+++ src/main/kotlin/lotto/domain/LottoTicketMachine.kt
@@ -2,15 +2,10 @@ package lotto.domain
 
 object LottoTicketMachine {
 
-    private const val MIN_RANGE_OF_NUMBER = 1
-    private const val MAX_RANGE_OF_NUMBER = 45
-
-    private val RANGE_OF_LOTTO_NUMBER = MIN_RANGE_OF_NUMBER..MAX_RANGE_OF_NUMBER
-
     fun generate(size: Int = 1) = List(size) { LottoTicket(generateNumbers()) }
 
     private fun generateNumbers(): Set<Int> {
-        return RANGE_OF_LOTTO_NUMBER
+        return LottoTicket.RANGE_OF_LOTTO_NUMBER
             .shuffled()
             .take(LottoTicket.SIZE_OF_LOTTO_NUMBER)
             .sorted()
