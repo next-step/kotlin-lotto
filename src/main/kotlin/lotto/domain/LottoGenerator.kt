723e008f9eca75f5e936c752e1b740bf84b83f5a
glennsm <lovinhut@gmail.com>
2022-05-27 02:48:31 +0900

    refactor: LOTTO_NUMBER_COUNT 값은 LottoTicket에서 관리

--- src/main/kotlin/lotto/domain/LottoGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoGenerator.kt
@@ -1,12 +1,13 @@
 package lotto.domain
 
 import lotto.domain.LottoNumber.Companion.LOTTO_NUMBER_RANGE
+import lotto.domain.LottoTicket.Companion.LOTTO_NUMBER_COUNT
 
 class LottoGenerator {
     fun generateNumbers(): List<LottoNumber> {
         return LOTTO_NUMBER_RANGE
             .shuffled()
-            .take(6)
+            .take(LOTTO_NUMBER_COUNT)
             .map { LottoNumber(it) }
     }
 }
--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -2,6 +2,9 @@ package lotto.domain
 
 class LottoTicket(var numbers: List<LottoNumber>) {
     init {
-        require(numbers.size == 6) { "로또번호는 6개의 숫자입니다" }
+        require(numbers.size == LOTTO_NUMBER_COUNT) { "로또번호는 ${LOTTO_NUMBER_COUNT}개의 숫자입니다" }
+    }
+    companion object {
+        const val LOTTO_NUMBER_COUNT = 6
     }
 }
