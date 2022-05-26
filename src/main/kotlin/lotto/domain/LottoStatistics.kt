1e5519c55e55e66fd31aa20e033a03f106e3f1f3
larry.p <larry.p@kakaocorp.com>
2022-05-27 01:44:25 +0900

    feat: 보너스 상태를 포함하는 Match를 이용해서 2등 판별 구현

--- src/main/kotlin/lotto/domain/LottoStatistics.kt
+++ src/main/kotlin/lotto/domain/LottoStatistics.kt
@@ -5,19 +5,35 @@ import java.math.MathContext
 
 class LottoStatistics(
     private val lottoTickets: List<LottoTicket>,
-    private val lastLottoTicket: LottoTicket
+    private val lastLottoTicket: LottoTicket,
+    private val lastLottoBonusNumber: Int
 ) {
 
     init {
         require(lottoTickets.isNotEmpty())
+        require(lastLottoBonusNumber in LottoTicket.RANGE_OF_LOTTO_NUMBER)
+        require(lastLottoTicket.numbers.contains(lastLottoBonusNumber).not())
     }
 
-    fun getMatchCount(match: LottoMatch): Int = getMatchCount(match.count)
+    fun getMatchCount(match: LottoMatch): Int =
+        if (match.withBonus) getMatchCountWithBonus(match) else getMatchCountWithoutBonus(match)
 
-    private fun getMatchCount(matchCount: Int): Int =
-        lottoTickets
-            .filter { it.numbers.intersect(lastLottoTicket.numbers).size == matchCount }
+    private fun getMatchCountWithBonus(match: LottoMatch): Int =
+        getMatchTicket(match)
+            .filter {
+                it.numbers.contains(lastLottoBonusNumber)
+            }
             .size
+
+    private fun getMatchCountWithoutBonus(match: LottoMatch): Int =
+        getMatchTicket(match)
+            .filterNot {
+                it.numbers.contains(lastLottoBonusNumber)
+            }
+            .size
+
+    private fun getMatchTicket(match: LottoMatch): List<LottoTicket> =
+        lottoTickets.filter { it.numbers.intersect(lastLottoTicket.numbers).size == match.count }
 
     fun getProfit(purchase: Int): BigDecimal {
         require(purchase > 0)
