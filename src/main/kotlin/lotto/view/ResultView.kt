2cd1531b86903f32f1ecdf048f08413de979023f
larry.p <larry.p@kakaocorp.com>
2022-05-27 01:46:02 +0900

    feat: 5개 일치 + 보너스 번호 일치되는 결화 화면 구현

--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -3,6 +3,7 @@ package lotto.view
 import lotto.domain.LottoMatch
 import lotto.domain.LottoStatistics
 import lotto.domain.LottoTicket
+import java.math.BigDecimal
 
 class ResultView(
     private val writer: (String) -> Unit
@@ -27,14 +28,26 @@ class ResultView(
 
     private fun printStatisticsMatch(statistics: LottoStatistics) {
         LottoMatch.values().forEach {
-            writeLine("${it.count}개 일치 (${it.reward}원)- ${statistics.getMatchCount(it)}개")
+            val extraPolicy = if (it.withBonus) ", 보너스 볼 일치" else " "
+            writeLine("${it.count}개 일치${extraPolicy}(${it.reward}원)- ${statistics.getMatchCount(it)}개")
         }
     }
 
     fun printStatisticsProfit(statistics: LottoStatistics, purchase: Int) {
-        val profit = String.format("%.02f", statistics.getProfit(purchase))
-        writeLine("총 수익률은 ${profit}입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)")
+        val profit = statistics.getProfit(purchase)
+        writeLine(
+            "총 수익률은 ${getFormattedProfit(profit)}입니다.(기준이 1이기 때문에 결과적으로 ${getProfitStatus(profit)}라는 의미임)"
+        )
     }
+
+    private fun getFormattedProfit(profit: BigDecimal): String = String.format("%.02f", profit)
+
+    private fun getProfitStatus(profit: BigDecimal): String =
+        when {
+            profit > BigDecimal.ONE -> "이익이"
+            profit < BigDecimal.ONE -> "손해"
+            else -> "중간이"
+        }
 
     private fun writeLine(message: String) = writer("$message\n")
 
