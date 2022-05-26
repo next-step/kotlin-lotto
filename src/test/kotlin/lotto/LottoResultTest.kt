af6099145b7be6ae0902cbb6ba6e4c3590cbc603
gabstin <ksgilove0@gmail.com>
2022-05-27 00:38:17 +0900

    [step3] test - 보너스 규칙(숫자 5 + 보너스 1개)에 부합하는 경우 보너스 Prize을 반환한다

--- src/test/kotlin/lotto/LottoResultTest.kt
+++ src/test/kotlin/lotto/LottoResultTest.kt
@@ -9,4 +9,9 @@ internal class LottoResultTest {
     fun `보너스 번호를 포함한 Enum 값을 추가한다`() {
         assertThat(LottoResult.Prize.values().contains(LottoResult.Prize.FIFTH_BONUS)).isEqualTo(true)
     }
+
+    @Test
+    fun `보너스 규칙(숫자 5 + 보너스 1개)에 부합하는 경우 보너스 Prize을 반환한다`(){
+        assertThat(LottoResult.Prize.getOrNull(matchCount = 5, matchBonus = true)).isEqualTo(LottoResult.Prize.FIFTH_BONUS)
+    }
 }
