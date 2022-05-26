8204c02b7b2cb7d0b40635c029aa8d57266d5989
Juice <3221ju@gmail.com>
2022-05-26 20:44:14 +0900

    feat: LottoTicket 도메인 추가

---
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -0,0 +1,27 @@
+package lotto.domain
+
+class LottoTicket private constructor(val lottoTicketNumbers: List<LottoTicketNumber>) {
+
+    init {
+        require(lottoTicketNumbers.size == VALIDATED_LOTTO_TICKET_NUMBER_SIZE) { ERROR_MESSAGE_BY_LOTTO_NUMBER_SIZE }
+        validateDuplicateLottoNumber()
+    }
+
+    private fun validateDuplicateLottoNumber() {
+        val sortedLottoTicketNumbers = lottoTicketNumbers.sortedBy { lottoTicketNumber -> lottoTicketNumber.value }
+        sortedLottoTicketNumbers.forEachIndexed { index, lottoTicketNumber ->
+            if (index + 1 == sortedLottoTicketNumbers.size) return@forEachIndexed
+            require(sortedLottoTicketNumbers[index + 1] != lottoTicketNumber) { "중복된 로또 번호가 있습니다" }
+        }
+    }
+
+    companion object {
+        private const val VALIDATED_LOTTO_TICKET_NUMBER_SIZE = 6
+        private const val ERROR_MESSAGE_BY_LOTTO_NUMBER_SIZE =
+            "로또 티켓은 $VALIDATED_LOTTO_TICKET_NUMBER_SIZE 개수의 로또 번호를 가지고 있어야 됩니다"
+
+        fun ofInts(lottNumbers: List<Int>): LottoTicket {
+            return LottoTicket(lottNumbers.map { LottoTicketNumber(it) })
+        }
+    }
+}
---
+++ src/test/kotlin/lotto/domain/LottoTicketTest.kt
@@ -0,0 +1,44 @@
+package lotto.domain
+
+import io.kotest.assertions.throwables.shouldThrowExactly
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldContainAll
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
+
+class LottoTicketTest : DescribeSpec({
+
+    it("Int List 로 생성할수 있다") {
+        // given
+        val lottoNumbersByInt = listOf(1, 2, 3, 4, 5, 6)
+
+        // when
+        val lottoTicket = LottoTicket.ofInts(lottoNumbersByInt)
+
+        // then
+        lottoTicket.lottoTicketNumbers.size shouldBe lottoNumbersByInt.size
+        lottoTicket.lottoTicketNumbers.map { it.value } shouldContainAll lottoNumbersByInt
+    }
+
+    describe("validate") {
+        it(" 알맞은 갯수의 로또 넘버를 가지고 있지 않다면 에러가 발생한다.") {
+            // given
+            val lottoNumbersByInt = listOf(1, 2, 3, 4, 5)
+
+            // then
+            shouldThrowExactly<IllegalArgumentException> {
+                LottoTicket.ofInts(lottoNumbersByInt)
+            }.shouldHaveMessage("로또 티켓은 6 개수의 로또 번호를 가지고 있어야 됩니다")
+        }
+
+        it("중복된 로또 번호를 입력하면 에러가 발생한다") {
+            // given
+            val lottoNumbersByInt = listOf(1, 1, 1, 1, 1, 1)
+
+            // then
+            shouldThrowExactly<IllegalArgumentException> {
+                LottoTicket.ofInts(lottoNumbersByInt)
+            }.shouldHaveMessage("중복된 로또 번호가 있습니다")
+        }
+    }
+})
