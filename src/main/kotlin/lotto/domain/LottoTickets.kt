3774b16df922cdbf430c19b064ae2fa2b06f897f
larry.p <larry.p@kakaocorp.com>
2022-05-27 02:25:17 +0900

    feat: 로또 티켓 목록 일금 컬렉션 클래스 추가
    통계에서 지난주 로또 번호와 일치하는 개수 계산로직의 책임을 가져오도록 한다.

--- src/main/kotlin/lotto/domain/LottoTickets.kt
+++ src/main/kotlin/lotto/domain/LottoTickets.kt
@@ -2,7 +2,35 @@ package lotto.domain
 
 class LottoTickets(private val tickets: List<LottoTicket>) : List<LottoTicket> by tickets {
 
-//    fun getMatchCount(match: LottoMatch, other: LottoTicket): Int {
-//        return tickets.filter { it.getMatchCount(other) == match }.size
-//    }
+    init {
+        require(tickets.isNotEmpty())
+    }
+
+    fun getMatchCount(match: LottoMatch, lastLottoTicket: LottoTicket, lastLottoBonusNumber: Int): Int =
+        if (match.withBonus) {
+            getMatchCountWithBonus(match, lastLottoTicket, lastLottoBonusNumber)
+        } else {
+            getMatchCountWithoutBonus(match, lastLottoTicket, lastLottoBonusNumber)
+        }
+
+    private fun getMatchCountWithBonus(
+        match: LottoMatch,
+        lastLottoTicket: LottoTicket,
+        lastLottoBonusNumber: Int
+    ): Int =
+        getMatchTickets(match, lastLottoTicket)
+            .filter { it.numbers.contains(lastLottoBonusNumber) }
+            .size
+
+    private fun getMatchCountWithoutBonus(
+        match: LottoMatch,
+        lastLottoTicket: LottoTicket,
+        lastLottoBonusNumber: Int
+    ): Int =
+        getMatchTickets(match, lastLottoTicket)
+            .filterNot { it.numbers.contains(lastLottoBonusNumber) }
+            .size
+
+    private fun getMatchTickets(match: LottoMatch, lastLottoTicket: LottoTicket): List<LottoTicket> =
+        filter { it.numbers.intersect(lastLottoTicket.numbers).size == match.count }
 }
