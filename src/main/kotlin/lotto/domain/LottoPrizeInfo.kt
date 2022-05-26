d123483b9bae710e507b15d75de7f106cff7f0c6
glenn.kgp <glenn.kgp@kakaocorp.com>
2022-05-26 22:14:40 +0900

    lint: 린트정리

--- src/main/kotlin/lotto/domain/LottoPrizeInfo.kt
+++ src/main/kotlin/lotto/domain/LottoPrizeInfo.kt
@@ -3,7 +3,7 @@ package lotto.domain
 enum class LottoPrizeInfo(val matchCount: Int, val money: UserMoney, val isBonus: Boolean) {
     WIN3(3, UserMoney(5000), false),
     WIN4(4, UserMoney(50000), false),
-    WIN5(5, UserMoney(1500000) ,false),
+    WIN5(5, UserMoney(1500000), false),
     WIN5_BONUS(5, UserMoney(30000000), true),
     WIN6(6, UserMoney(2000000000), false);
 
