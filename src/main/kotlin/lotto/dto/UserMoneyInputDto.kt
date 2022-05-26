1438515b628004441a623ea6ba63f70b4eef0cbd
Juice <3221ju@gmail.com>
2022-05-26 21:48:56 +0900

    feat: UserMoneyInputDto dto 추가

---
+++ src/main/kotlin/lotto/dto/UserMoneyInputDto.kt
@@ -0,0 +1,14 @@
+package lotto.dto
+
+import lotto.domain.Money
+
+class UserMoneyInputDto(moneyByString: String) {
+    val userMoney: Money
+
+    init {
+        val moneyByInt =
+            moneyByString.replace(" ", "").toIntOrNull() ?: throw IllegalArgumentException("금액은 숫자를 입력해주세요")
+        require(moneyByInt >= 0) { "0 이상에 금액을 입력해주세요" }
+        userMoney = moneyByInt.let { Money(it) }
+    }
+}
