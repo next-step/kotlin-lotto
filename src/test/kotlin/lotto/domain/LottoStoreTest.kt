e627ee06783616473c04900894bf6a34d3417f30
larry.p <larry.p@kakaocorp.com>
2022-05-27 00:15:24 +0900

    refactor: [리뷰반영] 테스트 이름을 좀더 명시적으로 변경, 계산식 대신 하드코딩된 입력치,기대치로 변경

--- src/test/kotlin/lotto/domain/LottoStoreTest.kt
+++ src/test/kotlin/lotto/domain/LottoStoreTest.kt
@@ -3,39 +3,36 @@ package lotto.domain
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.int
-import io.kotest.property.arbitrary.next
 import io.kotest.property.checkAll
 
 class LottoStoreTest : BehaviorSpec({
     Given("로또 판매점에서") {
-        When("충분한 돈으로") {
-            val numberOfTicket = Arb.int(1..100).next()
-            val changes = Arb.int(0..999).next()
-            val money = (numberOfTicket * LottoStore.PRICE_OF_ONE_LOTTO_TICKET) + changes
+        When("1000원 이상의 돈으로") {
+            val money = 34_567
+            val expectedTickets = 34
+            val expectedChanges = 567
 
             And("로또 티켓을 구매하면") {
-                val expected = money / LottoStore.PRICE_OF_ONE_LOTTO_TICKET
-                Then("로또 티켓은 1000원에 1개로 받는다 (금액: $money, 개수: $expected)") {
-                    val lottoTickets = LottoStore.buy(money)
-
-                    lottoTickets.shouldHaveSize(expected)
+                Then("로또 티켓은 1000원에 1개로 받는다 (금액: $money, 개수: $expectedTickets, 잔돈: $expectedChanges)") {
+                    val (lottoTickets, changes) = LottoStore.buy(money)
+                    lottoTickets shouldHaveSize expectedTickets
+                    changes shouldBe expectedChanges
                 }
             }
 
             And("로또 티켓을 100개 넘개 구매하면") {
                 Then("IllegalArgumentException 예외 발생") {
                     shouldThrowExactly<IllegalArgumentException> {
-                        LottoStore.buy(
-                            LottoStore.PRICE_OF_ONE_LOTTO_TICKET * LottoStore.MAXIMUM_SIZE_OF_TICKET + 1
-                        )
+                        LottoStore.buy(101_000)
                     }
                 }
             }
         }
 
-        When("충분하지 못한 돈으로") {
+        When("1000원 이하의 돈으로") {
             And("로또 티켓을 구매하면") {
                 Then("IllegalArgumentException 예외 발생") {
                     checkAll(Arb.int(0..999)) { money ->
