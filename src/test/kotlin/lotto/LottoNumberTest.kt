a2f9c09fc933bea5bd8c1ab1bfda3f93c717a935
ellen.youn <ellen.youn@kakaocorp.com>
2022-05-26 18:51:24 +0900

    test: 로또는 1~45 사이의 숫자를 가진다.

---
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -0,0 +1,14 @@
+package lotto
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FreeSpec
+import java.lang.IllegalArgumentException
+
+class LottoNumberTest : FreeSpec({
+    "로또의 숫자가 1~45 사이가 아닌경우 에러" {
+        val exception = shouldThrow<IllegalArgumentException> {
+            listOf(1, 2, 3, 4, 5, 66).map(::LottoNumber).toSet()
+        }
+        println(exception.message)
+    }
+})
