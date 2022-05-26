48da07ba43bd9777a405307c18c54302a4bb6eac
gabstin <ksgilove0@gmail.com>
2022-05-27 00:19:38 +0900

    [step3] refactoring - 로또 기계에 보너스 값을 전달하는 테스트케이스 수정

--- src/test/kotlin/lotto/LottoMatchingTest.kt
+++ src/test/kotlin/lotto/LottoMatchingTest.kt
@@ -1,13 +1,24 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class LottoMatchingTest {
     @Test
     fun `로또 기계에 보너스 값을 전달한다`() {
-        LottoMatching().checkResult(
-            winningLottoTicket = LottoTicket(numbers = (1..6).map { LottoNumber(it) }),
-            bonusLottoNumber = LottoNumber(1)
-        )
+        val lottoNumbers = (1..6).map { LottoNumber(it) }
+        val lottoMachine = LottoMatching().apply {
+            this.purchase(
+                purchaseMoney = PurchaseMoney(1000),
+                numbers = lottoNumbers
+            )
+            this.checkResult(
+                winningLottoTicket = LottoTicket(lottoNumbers),
+                bonusLottoNumber = LottoNumber(1)
+            )
+        }
+
+        assertThat(lottoMachine.lottoTicketCount).isEqualTo(1)
+        assertThat(lottoMachine.winningPrizes.prizes).isEqualTo(listOf(LottoResult.Prize.SIXTH))
     }
 }
