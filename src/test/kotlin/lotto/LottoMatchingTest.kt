5eea27d64e4e58ff27f91b163e2bc33270c0dea9
gabstin <ksgilove0@gmail.com>
2022-05-27 00:25:01 +0900

    [step3] test - 로또 Prize을 확인하기 위하여 당첨 번호, 보너스 일치 여부를 전달한다

--- src/test/kotlin/lotto/LottoMatchingTest.kt
+++ src/test/kotlin/lotto/LottoMatchingTest.kt
@@ -26,4 +26,22 @@ internal class LottoMatchingTest {
     fun `보너스 일치 여부를 확인한다`() {
         assertThat(LottoTicket(numbers = (1..6).map { LottoNumber(it) }).hasBonusNumber(LottoNumber((1)))).isEqualTo(true)
     }
+
+    @Test
+    fun `로또 Prize을 확인하기 위하여 당첨 번호, 보너스 일치 여부를 전달한다`() {
+        val lottoNumbers = (2..7).map { LottoNumber(it) }
+        val lottoMachine = LottoMatching().apply {
+            this.purchase(
+                purchaseMoney = PurchaseMoney(1000),
+                numbers = lottoNumbers
+            )
+            this.checkResult(
+                winningLottoTicket = LottoTicket(lottoNumbers),
+                bonusLottoNumber = LottoNumber(1)
+            )
+        }
+
+        assertThat(lottoMachine.lottoTicketCount).isEqualTo(1)
+        assertThat(lottoMachine.winningPrizes.prizes).isEqualTo(listOf(LottoResult.Prize.FIFTH_BONUS))
+    }
 }
