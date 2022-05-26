7d510f8451441129921e4aa0768be6fa60549495
Molly <dngwoodo@gmil.comm>
2022-05-26 23:19:36 +0900

    LottoController 클래스 구현

---
+++ src/main/kotlin/lotto/domain/LottoController.kt
@@ -0,0 +1,32 @@
+package lotto.domain
+
+import lotto.view.InputView
+import lotto.view.PrintOutput
+import lotto.view.ReadInput
+import lotto.view.ResultView
+
+class LottoController(private val readInput: ReadInput, private val printOutput: PrintOutput) {
+    fun start() {
+        // 구입할 로또 갯수 입력(View)
+        val inputView = InputView(readInput)
+        val purchaseAmount = inputView.askPurchaseAmount()
+
+        // 로또 구입(Model)
+        val seller = Seller(RandomLottoNumberFactory())
+        val lottos = seller.sell(purchaseAmount)
+
+        // 구입한 로또를 사용자에게 출력(View)
+        val resultView = ResultView(printOutput)
+        resultView.showLottos(lottos)
+
+        // 지난 주 당첨 번호를 사용자에게 출력(View)
+        val lastWeekWinningNumber = inputView.askLastWeekWinningNumber()
+
+        // 로또 분석(Model)
+        val winningNumber = WinningNumber(lastWeekWinningNumber)
+        val result = winningNumber.match(purchaseAmount, lottos)
+
+        // 당첨 통계를 사용자에게 출력(View)
+        resultView.showAnalyzeResult(result)
+    }
+}
--- src/main/kotlin/lotto/main.kt
+++ src/main/kotlin/lotto/main.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import lotto.domain.LottoController
 import lotto.domain.RandomLottoNumberFactory
 import lotto.domain.Seller
 import lotto.domain.WinningNumber
@@ -9,25 +10,7 @@ import lotto.view.ReadInput
 import lotto.view.ResultView
 
 fun main() {
-    // 구입할 로또 갯수 입력(View)
-    val inputView = InputView(ReadInput())
-    val purchaseAmount = inputView.askPurchaseAmount()
+    val lottoController = LottoController(ReadInput(), PrintOutput())
 
-    // 로또 구입(Model)
-    val seller = Seller(RandomLottoNumberFactory())
-    val lottos = seller.sell(purchaseAmount)
-
-    // 구입한 로또를 사용자에게 출력(View)
-    val resultView = ResultView(PrintOutput())
-    resultView.showLottos(lottos)
-
-    // 지난 주 당첨 번호를 사용자에게 출력(View)
-    val lastWeekWinningNumber = inputView.askLastWeekWinningNumber()
-
-    // 로또 분석(Model)
-    val winningNumber = WinningNumber(lastWeekWinningNumber)
-    val result = winningNumber.match(purchaseAmount, lottos)
-
-    // 당첨 통계를 사용자에게 출력(View)
-    resultView.showAnalyzeResult(result)
+    lottoController.start()
 }
