46ded1108260528c05d1a97789b8752da3e8bb91
Yohan <yohan@inflab.com>
2022-05-27 21:39:04 +0900

    refactor: 불필요한 테스트 코드 제거

--- src/test/kotlin/calculator/vo/NumbersTest.kt
+++ src/test/kotlin/calculator/vo/NumbersTest.kt
@@ -7,17 +7,6 @@ internal class NumbersTest : DescribeSpec({
 
     describe("Numbers 클래스 테스트") {
 
-        it("Numbers는 Number 리스트를 가진다.") {
-            // given
-            val intNumbers = listOf(IntNumber(1), IntNumber(2))
-
-            // when
-            val result = Numbers(intNumbers)
-
-            // then
-            result shouldBe Numbers(listOf(IntNumber(1), IntNumber(2)))
-        }
-
         it("Numbers은 소유한 IntNumbers의 덧셈 결과를 반환한다.") {
             // given
             val intNumbers = listOf(IntNumber(1), IntNumber(2))
