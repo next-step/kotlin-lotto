4aa9d35176a1c414256bb331586ae054b4c23ff4
jngcii <concotree@gmail.com>
2022-05-27 11:43:21 +0900

    feat : 손익여부에 대한 ProfitState enum class 구현

--- src/test/kotlin/com/nextstep/jngcii/lotto/model/ProfitStateTest.kt
+++ src/test/kotlin/com/nextstep/jngcii/lotto/model/ProfitStateTest.kt
@@ -1,9 +1,11 @@
 package com.nextstep.jngcii.lotto.model
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
 
 class ProfitStateTest {
     @ParameterizedTest
@@ -13,6 +15,14 @@ class ProfitStateTest {
         val expected: ProfitState = ProfitState.of(state)
 
         assertThat(actual).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [-2, 2, 5])
+    fun `잘못된 state에 대한 enum을 찾을 수 없음`(state: Int) {
+        assertThrows<IllegalArgumentException>("잘못된 state 입니다.") {
+            ProfitState.of(state)
+        }
     }
 
     companion object {
