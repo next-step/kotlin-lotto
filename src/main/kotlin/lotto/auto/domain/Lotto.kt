2a875bfe58c6ac31af7e1a5f4cc82cee49acfd3d
Yohan <yohan@inflab.com>
2022-05-27 19:30:59 +0900

    feat: 로또 생성시 중복된 숫자가 포함된 경우 예외를 던진다.

--- src/main/kotlin/lotto/auto/domain/Lotto.kt
+++ src/main/kotlin/lotto/auto/domain/Lotto.kt
@@ -1,6 +1,19 @@
 package lotto.auto.domain
 
+import lotto.auto.vo.LottoScore
+
 class Lotto(private val numbers: List<Int>) {
 
-    fun match(other: Lotto): Int = numbers.count(other.numbers::contains)
+    init {
+        require(numbers.distinct().count() == NUMBER_COUNT)
+    }
+
+    fun match(other: Lotto): LottoScore = LottoScore.matchCountOf(countingMatchNumber(other))
+
+    private fun countingMatchNumber(other: Lotto): Int = numbers.count(other.numbers::contains)
+
+    companion object {
+
+        const val NUMBER_COUNT = 6
+    }
 }
