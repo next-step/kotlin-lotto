0823d23518991b6fcd4453b4c5dd95d0ce4991be
jngcii <concotree@gmail.com>
2022-05-27 10:12:32 +0900

    feat : Record class 구현

---
+++ src/main/kotlin/com/nextstep/jngcii/lotto/model/Record.kt
@@ -0,0 +1,8 @@
+package com.nextstep.jngcii.lotto.model
+
+class Record(private val lottos: List<Lotto>) {
+    fun getResult(lastWeekLotto: Lotto) = lottos.mapNotNull {
+        val sameCount = it.getSameCount(lastWeekLotto)
+        Rank.of(sameCount)
+    }
+}
