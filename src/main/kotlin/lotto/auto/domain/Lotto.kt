b20eeb653689ad28215f2b9ade0cb63e4c5d06e2
Yohan <yohan@inflab.com>
2022-05-27 19:33:17 +0900

    feat: 로또 생성시 1~45 범위 외의 숫자가 포함된 경우 예외를 던진다.

--- src/main/kotlin/lotto/auto/domain/Lotto.kt
+++ src/main/kotlin/lotto/auto/domain/Lotto.kt
@@ -6,11 +6,14 @@ class Lotto(private val numbers: List<Int>) {
 
     init {
         require(numbers.distinct().count() == NUMBER_COUNT)
+        require(numbers.all(::isValidNumber))
     }
 
     fun match(other: Lotto): LottoScore = LottoScore.matchCountOf(countingMatchNumber(other))
 
     private fun countingMatchNumber(other: Lotto): Int = numbers.count(other.numbers::contains)
+
+    private fun isValidNumber(number: Int) = number in 1..45
 
     companion object {
 
