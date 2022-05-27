4f0a8f5814d50fe871c7a6773c3688e8d13a5847
Yohan <yohan@inflab.com>
2022-05-27 21:27:07 +0900

    refactor: 로또 객체 매직 넘버 리팩터링

--- src/main/kotlin/lotto/auto/domain/Lotto.kt
+++ src/main/kotlin/lotto/auto/domain/Lotto.kt
@@ -6,7 +6,7 @@ import lotto.auto.vo.LottoScore
 class Lotto(private val numbers: List<Int>) {
 
     init {
-        require(numbers.distinct().count() == NUMBER_COUNT)
+        require(numbers.distinct().count() == LOTTO_NUMBER_LENGTH)
         require(numbers.all(::isValidNumber))
     }
 
@@ -14,15 +14,17 @@ class Lotto(private val numbers: List<Int>) {
 
     private fun countingMatchNumber(other: Lotto): Int = numbers.count(other.numbers::contains)
 
-    private fun isValidNumber(number: Int) = number in 1..45
+    private fun isValidNumber(number: Int) = number in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER
 
     companion object {
 
-        const val NUMBER_COUNT = 6
+        const val LOTTO_NUMBER_LENGTH = 6
+        const val MIN_LOTTO_NUMBER = 1
+        const val MAX_LOTTO_NUMBER = 45
 
         fun createRandomNumbers(numberGenerator: NumberGenerator): Lotto {
             val numbers = mutableListOf<Int>()
-            while (numbers.size != NUMBER_COUNT) {
+            while (numbers.size != LOTTO_NUMBER_LENGTH) {
                 val newNumber = numberGenerator.getNumber()
                 if (numbers.contains(newNumber)) continue
                 numbers.add(newNumber)
