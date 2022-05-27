b8a597fb5b63a997b6031856132553397a0620a0
woody.b <woody.b@kakaoenterprise.com>
2022-05-27 23:22:30 +0900

    feat: 빈 문자열 또는 null을 입력할 경우 0을 반환하는 기능 추가

---
+++ src/main/kotlin/stringcalculator/StringAddCalculator.kt
@@ -0,0 +1,16 @@
+package stringcalculator
+
+class StringAddCalculator {
+
+    fun add(text: String?): Int {
+        if (text.isNullOrBlank()) {
+            return ZERO
+        }
+
+        return text.toInt()
+    }
+
+    companion object {
+        private const val ZERO = 0
+    }
+}
