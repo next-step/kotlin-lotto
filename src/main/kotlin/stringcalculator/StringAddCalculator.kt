e97824d6924e8dab2e5c4789bbc5b3f5409f26be
woody.b <woody.b@kakaoenterprise.com>
2022-05-27 23:37:28 +0900

    feat: 구분자를 쉼표(,) 이외에 콜론(:)을 사용하는 기능 추가

--- src/main/kotlin/stringcalculator/StringAddCalculator.kt
+++ src/main/kotlin/stringcalculator/StringAddCalculator.kt
@@ -7,13 +7,14 @@ class StringAddCalculator {
             return ZERO
         }
 
-        return text.split(DEFAULT_SPLITTER)
+        return text.split(DEFAULT_REGEX)
             .map { it.toInt() }
             .reduce { a, b -> a + b }
     }
 
     companion object {
         private const val ZERO = 0
-        private const val DEFAULT_SPLITTER = ","
+        private const val DEFAULT_SPLITTER = "[,:]"
+        private val DEFAULT_REGEX = DEFAULT_SPLITTER.toRegex()
     }
 }
--- src/test/kotlin/stringcalculator/StringAddCalculatorTest.kt
+++ src/test/kotlin/stringcalculator/StringAddCalculatorTest.kt
@@ -36,13 +36,13 @@ class StringAddCalculatorTest {
         assertThat(calculator.add(text)).isSameAs(3)
     }
 
-    // @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
-    // @ParameterizedTest
-    // @ValueSource(strings = ["1,2:3"])
-    // fun colons(text: String) {
-    //     assertThat(calculator.add(text)).isSameAs(6)
-    // }
-    //
+    @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2:3"])
+    fun colons(text: String) {
+        assertThat(calculator.add(text)).isSameAs(6)
+    }
+
     // @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
     // @ParameterizedTest
     // @ValueSource(strings = ["//;\n1;2;3"])
