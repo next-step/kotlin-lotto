fe2b3ede7b2136d923e8701b7917d12e348a8f8b
larry.p <larry.p@kakaocorp.com>
2022-05-29 03:26:20 +0900

    test: [리뷰반영] LottoStatistics 테스트 코드 정리 및 통계로만 수익율 계산하도록 테스트 수정

--- src/test/kotlin/lotto/domain/LottoStatisticsTest.kt
+++ src/test/kotlin/lotto/domain/LottoStatisticsTest.kt
@@ -7,41 +7,54 @@ import java.math.BigDecimal
 class LottoStatisticsTest : BehaviorSpec({
 
     Given("로또 통계는") {
-        val lottoTickets = LottoTickets(
-            listOf(
-                LottoTicket(setOf(1, 2, 3, 10, 11, 12)),
-                LottoTicket(setOf(1, 2, 3, 4, 11, 12)),
-                LottoTicket(setOf(1, 2, 3, 4, 5, 12)),
-                LottoTicket(setOf(1, 2, 3, 4, 5, 7)),
-                LottoTicket(setOf(1, 2, 3, 4, 5, 6)),
+        When("LottoMatch와 Count가 입렵되면") {
+            val statistics = LottoStatistics(
+                mapOf(
+                    LottoMatch.ZERO to 1,
+                    LottoMatch.ONE to 0,
+                    LottoMatch.TWO to 1,
+                    LottoMatch.THREE to 0,
+                    LottoMatch.FOUR to 1,
+                    LottoMatch.FIVE to 0,
+                    LottoMatch.FIVE_WITH_BONUS to 1,
+                    LottoMatch.SIX to 0
+                )
             )
-        )
-        val lottoLastNumbers = LottoLastNumbers(setOf(1, 2, 3, 4, 5, 6), 7)
-        val matchResult = lottoTickets.getMatchResult(lottoLastNumbers)
 
-        When("1개 이상의 포함된 로또 티켓 목록이 입력되면") {
-            val statistics = LottoStatistics(matchResult)
-            Then("3개 일치 개수를 반환한다") {
-                statistics[LottoMatch.THREE] shouldBe 1
+            Then("0개 일치 개수를 반환한다") {
+                statistics[LottoMatch.ZERO] shouldBe 1
             }
+
+            Then("1개 일치 개수를 반환한다") {
+                statistics[LottoMatch.ONE] shouldBe 0
+            }
+
+            Then("2개 일치 개수를 반환한다") {
+                statistics[LottoMatch.TWO] shouldBe 1
+            }
+
+            Then("3개 일치 개수를 반환한다") {
+                statistics[LottoMatch.THREE] shouldBe 0
+            }
+
             Then("4개 일치 개수를 반환한다") {
                 statistics[LottoMatch.FOUR] shouldBe 1
             }
+
             Then("5개 일치 개수를 반환한다") {
-                statistics[LottoMatch.FIVE] shouldBe 1
+                statistics[LottoMatch.FIVE] shouldBe 0
             }
+
             Then("5개 + 보너스 번호 일치 개수를 반환한다") {
-                statistics[LottoMatch.FIVE_BONUS] shouldBe 1
+                statistics[LottoMatch.FIVE_WITH_BONUS] shouldBe 1
             }
+
             Then("6개 일치 개수를 반환한다") {
-                statistics[LottoMatch.SIX] shouldBe 1
+                statistics[LottoMatch.SIX] shouldBe 0
             }
-            And("총 구매 금액을 입력하면") {
-                val purchase = 4000
-                val profit = BigDecimal(507888.75)
-                Then("총 수익율을 반환한다") {
-                    statistics.getProfit(purchase) shouldBe profit
-                }
+
+            Then("총 수익율을 반환한다") {
+                statistics.getProfit() shouldBe BigDecimal("7512.5")
             }
         }
     }
