66bbad15bdc30dc3d31ad585d97d46b77484f78c
austen.lee <austen.lee@kakaocorp.com>
2022-05-29 02:33:52 +0900

    step1 feat : LottoNumber에 range validation check 추가

--- src/test/kotlin/lotto/domain/model/LottoNumberTest.kt
+++ src/test/kotlin/lotto/domain/model/LottoNumberTest.kt
@@ -1,6 +1,8 @@
 package lotto.domain.model
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -11,5 +13,15 @@ class LottoNumberTest {
         val lottoNumber = LottoNumber(input)
 
         assertThat(lottoNumber.value).isEqualTo(input)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [55, 32, 156, 266, 334, 23, 1, 4, 0])
+    fun `1에서 45까지의 숫자 외의 값이 들어온다면 IllegalArgumentException이 발생한다`(input: Int) {
+        if (input in LottoNumber.lottoNumberRange) {
+            assertDoesNotThrow { LottoNumber(input) }
+        } else {
+            assertThrows<IllegalArgumentException> { LottoNumber(input) }
+        }
     }
 }
