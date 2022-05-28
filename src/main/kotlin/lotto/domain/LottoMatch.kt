2fca974f1630e1addf9395d43baae23b6bd62f30
larry.p <larry.p@kakaocorp.com>
2022-05-29 03:21:12 +0900

    feat: [리뷰반영] LottoMatch에 일치 항목 추가
    기존에는 3~6개 일치만 있었고, 0~2 일치 항목도 추가한다.

--- src/main/kotlin/lotto/domain/LottoMatch.kt
+++ src/main/kotlin/lotto/domain/LottoMatch.kt
@@ -1,9 +1,20 @@
 package lotto.domain
 
 enum class LottoMatch(val count: Int, val withBonus: Boolean, val reward: Long) {
+    ZERO(0, false, 0),
+    ONE(1, false, 0),
+    TWO(2, false, 0),
     THREE(3, false, 5_000),
     FOUR(4, false, 50_000),
     FIVE(5, false, 1_500_000),
-    FIVE_BONUS(5, true, 30_000_000),
-    SIX(6, false, 2_000_000_000)
+    FIVE_WITH_BONUS(5, true, 30_000_000),
+    SIX(6, false, 2_000_000_000);
+
+    companion object {
+        fun valuesWithReward(): List<LottoMatch> = values().filterNot { it.reward == 0L }
+
+        fun find(count: Int, withBonus: Boolean): LottoMatch {
+            return values().find { it.count == count && it.withBonus == withBonus } ?: ZERO
+        }
+    }
 }
