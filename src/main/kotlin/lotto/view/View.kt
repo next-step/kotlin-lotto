c46509be2b41572c9627818daca3343b969a790d
molly <molly.d@kakaocorp.com>
2022-05-29 01:15:27 +0900

    feat: 보너스 번호 입력 받기

--- src/main/kotlin/lotto/view/View.kt
+++ src/main/kotlin/lotto/view/View.kt
@@ -26,4 +26,9 @@ object View {
         println("지난 주 당첨 번호를 입력해 주세요.")
         return readln()
     }
+
+    fun getBonusNumber(): String {
+        println("보너스 볼을 입력해 주세요.")
+        return readln()
+    }
 }
