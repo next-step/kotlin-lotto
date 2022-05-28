529a06f5d35028306806b7a668ca6cd11c006b65
austen.lee <austen.lee@kakaocorp.com>
2022-05-29 03:13:08 +0900

    step1 feat : Lottos 안에서 LottoResult를 반환하는 checkWith와 테스트

--- src/main/kotlin/lotto/domain/model/Lottos.kt
+++ src/main/kotlin/lotto/domain/model/Lottos.kt
@@ -1,4 +1,18 @@
 package lotto.domain.model
 
 @JvmInline
-value class Lottos(val value: List<Lotto>)
+value class Lottos(val value: List<Lotto>) {
+    fun checkWith(winningNumbers: WinningNumbers): LottoResult {
+        return LottoResult.from(toLottoWinningMap(winningNumbers))
+    }
+
+    private fun toLottoWinningMap(winningNumbers: WinningNumbers): Map<Int, Int> {
+        return value.map { lotto ->
+            lotto.getNumberOfMatchesWith(winningNumbers)
+        }.groupingBy { numberOfMatches ->
+            numberOfMatches
+        }.eachCount().filter { (numberOfMatches, _) ->
+            numberOfMatches in LottoRank.NUMBER_OF_MATCHES_RANGE
+        }
+    }
+}
--- src/test/kotlin/lotto/domain/model/LottosTest.kt
+++ src/test/kotlin/lotto/domain/model/LottosTest.kt
@@ -16,4 +16,26 @@ class LottosTest {
 
         assertThat(lottos.value).isEqualTo(lottoList)
     }
+
+    @Test
+    fun `checkWith를 통해 당첨 번호를 받아 LottoResult를 반환한다`() {
+        val lottos = Lottos(
+            listOf(
+                Lotto(listOf(8, 21, 23, 41, 42, 43)),
+                Lotto(listOf(8, 21, 23, 41, 42, 44)),
+                Lotto(listOf(8, 21, 23, 41, 43, 44)),
+                Lotto(listOf(8, 21, 23, 41, 1, 2)),
+                Lotto(listOf(8, 21, 23, 1, 2, 3))
+            )
+        )
+
+        val winningNumbers = WinningNumbers(listOf(8, 21, 23, 41, 42, 43))
+
+        val lottoResult = lottos.checkWith(winningNumbers)
+
+        assertThat(lottoResult[LottoRank.FOURTH].count).isEqualTo(1)
+        assertThat(lottoResult[LottoRank.THIRD].count).isEqualTo(1)
+        assertThat(lottoResult[LottoRank.SECOND].count).isEqualTo(2)
+        assertThat(lottoResult[LottoRank.FIRST].count).isEqualTo(1)
+    }
 }
