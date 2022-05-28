e158d41bed1105e3fe021f9ea5e5aec41fd26e9e
Juice <3221ju@gmail.com>
2022-05-28 15:01:14 +0900

    feat: BuyLottoOutputView main 적용

--- src/main/kotlin/lotto/main.kt
+++ src/main/kotlin/lotto/main.kt
@@ -8,6 +8,7 @@ import lotto.dto.UserMoneyInputDto
 import lotto.dto.WinningNumbersInputDto
 import lotto.util.KotlinRandomGenerate
 import lotto.view.BuyLottoInputView
+import lotto.view.BuyLottoOutputView
 
 fun main() {
     val lottoTicketPrice = Money(1000)
@@ -17,40 +18,25 @@ fun main() {
     val userMoneyInputDto = UserMoneyInputDto(readln())
     val boughtLottoTickets = ticketSeller.buyPossibleLottoTicket(userMoneyInputDto.userMoney)
 
-    println()
-    println("${boughtLottoTickets}를 구매했습니다")
+    println("${boughtLottoTickets.size}를 구매했습니다")
     boughtLottoTickets.forEach {
         println(it.lottoTicketNumbers.value.map { it.value }.sorted().joinToString(prefix = "[", postfix = "]"))
     }
-
     println()
+
     println(BuyLottoInputView.showWinningLottoNumbersInputGuide())
     val winningNumbersInputDto = WinningNumbersInputDto(readln())
-
     println()
-    println("당첨통계")
-    println("---------")
+
+    println(BuyLottoOutputView.showWinningStartLabel())
     val lottoPrizeManager = LottoPrizeManager()
     lottoPrizeManager.addUniquePolicy(LottoPrizePolicy(3, Money(5000)))
     lottoPrizeManager.addUniquePolicy(LottoPrizePolicy(4, Money(50000)))
     lottoPrizeManager.addUniquePolicy(LottoPrizePolicy(5, Money(1500000)))
     lottoPrizeManager.addUniquePolicy(LottoPrizePolicy(6, Money(2000000000)))
 
-    var totalPrize = 0
-    lottoPrizeManager.polices.forEach { lottoPrizePolicy ->
-        val prizeCount = boughtLottoTickets.count {
-            lottoPrizePolicy.won(
-                it,
-                winningNumbersInputDto.winningLottoTicketNumbers
-            ) != null
-        }
-        totalPrize += lottoPrizePolicy.wonPrize.value * prizeCount
-        println("${lottoPrizePolicy.wonMatchedCount}개 일치 (${lottoPrizePolicy.wonPrize.value})-${prizeCount}개")
-    }
-    val earningsRate: Double = totalPrize.toDouble() / (boughtLottoTickets.size * lottoTicketPrice.value)
-    println(
-        "총 수익률은 ${
-        String.format("%.1f", earningsRate)
-        }입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)"
-    )
+    val winningStats =
+        lottoPrizeManager.getWinningStats(boughtLottoTickets, winningNumbersInputDto.winningLottoTicketNumbers)
+    println(BuyLottoOutputView.showAllWinningStat(winningStats))
+    println(BuyLottoOutputView.showEarningsRate(Money(boughtLottoTickets.size * lottoTicketPrice.value), winningStats))
 }
