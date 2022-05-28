2cc4f76d1d184d9c9ed74afa2d77e5b004e223e2
austen.lee <austen.lee@kakaocorp.com>
2022-05-29 05:04:28 +0900

    step1 refactor : 리턴 타입 명시하기

--- src/main/kotlin/expressioncalculator/IntValidator.kt
+++ src/main/kotlin/expressioncalculator/IntValidator.kt
@@ -4,7 +4,7 @@ object IntValidator {
     private const val MESSAGE_NOT_NUMBER_EXCEPTION = "숫자와 구분자 이외의 값은 입력될 수 없습니다."
     private const val MESSAGE_NEGATIVE_NUMBER_EXCEPTION = "0 이상의 숫자가 입력되어야 합니다."
 
-    fun validate(number: Int?) = number
+    fun validate(number: Int?): Int = number
         .requireNotNull()
         .requirePositiveNumber()
 
