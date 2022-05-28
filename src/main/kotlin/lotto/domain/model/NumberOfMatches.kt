50711a897b86de6d8cf718590e9b006aa9fa8bd7
austen.lee <austen.lee@kakaocorp.com>
2022-05-29 03:24:15 +0900

    step1 feat : 로또 번호가 당첨 번호와 몇 개 일치하는지를 표시하는 NumberOfMatches

---
+++ src/main/kotlin/lotto/domain/model/NumberOfMatches.kt
@@ -0,0 +1,13 @@
+package lotto.domain.model
+
+@JvmInline
+value class NumberOfMatches(val value: Int) {
+    fun isWin(): Boolean = value in winNumberRange
+
+    companion object {
+        private const val MIN_MATCH_NUMBER = 3
+        private const val MAX_MATCH_NUMBER = Lotto.LOTTO_NUMBER_COUNT
+
+        private val winNumberRange = (MIN_MATCH_NUMBER..MAX_MATCH_NUMBER)
+    }
+}
---
+++ src/test/kotlin/lotto/domain/model/NumberOfMatchesTest.kt
@@ -0,0 +1,9 @@
+package lotto.domain.model
+
+import org.junit.jupiter.api.Test
+
+class NumberOfMatchesTest {
+    @Test
+    fun `NumberOfMatches는 로또 번호가 당첨 번호와 몇 개 일치하는지를 표시한다`() {
+    }
+}
