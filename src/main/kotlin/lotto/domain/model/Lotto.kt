ca3bb7f17e8f3a6f8d0ca250cf473e9e48f6f0ce
austen.lee <austen.lee@kakaocorp.com>
2022-05-29 02:53:52 +0900

    step1 refactor : Lotto 에러 메세지 수정

--- src/main/kotlin/lotto/domain/model/Lotto.kt
+++ src/main/kotlin/lotto/domain/model/Lotto.kt
@@ -4,12 +4,12 @@ package lotto.domain.model
 value class Lotto(val numbers: List<Int>) {
     init {
         require(numbers.size == LOTTO_NUMBER_COUNT) {
-            MESSAGE_INVALID_LOTTO_NUMBERS_SIZE
+            "$MESSAGE_INVALID_LOTTO_NUMBERS_SIZE${numbers.size}"
         }
     }
 
     companion object {
         const val LOTTO_NUMBER_COUNT = 6
-        private const val MESSAGE_INVALID_LOTTO_NUMBERS_SIZE = "로또 번호는 6개의 숫자로 이루어져야 합니다."
+        private const val MESSAGE_INVALID_LOTTO_NUMBERS_SIZE = "로또 번호는 6개의 숫자로 이루어져야 합니다.\n입력된 숫자 수 : "
     }
 }
