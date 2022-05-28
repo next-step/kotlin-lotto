f60f90392869526b6018f341e77a98e93d84055b
hbm0411 <hbm0411@gmail.com>
2022-05-28 20:01:32 +0900

    refactor: 당첨번호 비교 테스트 리팩토링

--- src/test/kotlin/lotto/domain/LottoScoreTest.kt
+++ src/test/kotlin/lotto/domain/LottoScoreTest.kt
@@ -2,16 +2,37 @@ package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 internal class LottoScoreTest {
     @Test
     internal fun `당첨 번호와 나의 로또 번호를 비교하여 결과를 반환한다`() {
-        val lottoTicket = LottoTicket(1, 2, 3, 4, 5, 6)
-        val winningTicket = LottoTicket(1, 7, 8, 9, 2, 3)
+        val lottoTickets = listOf(
+            LottoTicket(1, 2, 3, 8, 9, 10), // 3match
+            LottoTicket(1, 2, 5, 10, 11, 12), // 3match
+            LottoTicket(1, 2, 3, 4, 18, 17), // 4match
+            LottoTicket(40, 1, 2, 3, 4, 5), // 5match
+            LottoTicket(1, 2, 3, 4, 5, 6), // 6match
+        )
+        val winningTicket = LottoTicket(1, 2, 3, 4, 5, 6)
 
-        val result = LottoScore().compareNumber(winningTicket, listOf(lottoTicket)).first()
-        assertThat(result.lottoCount).isEqualTo(1)
-        assertThat(result.lottoPrize).isEqualTo(LottoPrize.THREE_MATCH)
+        val lottoResults = LottoScore().compareNumber(winningTicket, lottoTickets)
+        val expected = mapOf(
+            LottoPrize.THREE_MATCH to 2,
+            LottoPrize.FOUR_MATCH to 1,
+            LottoPrize.FIVE_MATCH to 1,
+            LottoPrize.SIX_MATCH to 1
+        )
+
+        assertThat(lottoResults.size).isEqualTo(expected.size)
+        assertAll(
+            {
+                expected.forEach { (prize, lottoCount) ->
+                    val lottoResult = lottoResults.find { it.lottoPrize == prize }
+                    assertThat(lottoResult?.lottoCount).isEqualTo(lottoCount)
+                }
+            }
+        )
     }
 
     private fun LottoTicket(vararg numbers: Int) =
