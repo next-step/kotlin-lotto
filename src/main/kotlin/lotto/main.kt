88e8cacd2406f811f07b0c3ea8852b225b0082ed
Juice <3221ju@gmail.com>
2022-05-28 13:06:58 +0900

    ref: ticketSeller main 함수 적용

--- src/main/kotlin/lotto/main.kt
+++ src/main/kotlin/lotto/main.kt
@@ -1,25 +1,23 @@
 package lotto
 
 import lotto.domain.LottoPrizePolicy
-import lotto.domain.LottoTicket
 import lotto.domain.Money
+import lotto.domain.TicketSeller
 import lotto.dto.UserMoneyInputDto
 import lotto.dto.WinningNumbersInputDto
+import lotto.util.KotlinRandomGenerate
 
 fun main() {
     val lottoTicketPrice = Money(1000)
+    val ticketSeller = TicketSeller(lottoTicketPrice, KotlinRandomGenerate)
 
     println("구입금액을 입력해 주세요.")
     val userMoneyInputDto = UserMoneyInputDto(readln())
-    val buyTicketCount = userMoneyInputDto.userMoney.value / lottoTicketPrice.value
-    val buyLottoTickets = mutableListOf<LottoTicket>()
+    val boughtLottoTickets = ticketSeller.buyPossibleLottoTicket(userMoneyInputDto.userMoney)
 
-    repeat(buyTicketCount) {
-        buyLottoTickets.add(LottoTicket.ofInts((1..45).shuffled().take(6)))
-    }
     println()
-    println("${buyTicketCount}를 구매했습니다")
-    buyLottoTickets.forEach {
+    println("${boughtLottoTickets}를 구매했습니다")
+    boughtLottoTickets.forEach {
         println(it.lottoTicketNumbers.value.map { it.value }.sorted().joinToString(prefix = "[", postfix = "]"))
     }
 
@@ -39,11 +37,11 @@ fun main() {
 
     var totalPrize = 0
     lottoPrizePolicyList.forEach { lottoPrizePolicy ->
-        val prizeCount = buyLottoTickets.count { lottoPrizePolicy.won(it) != null }
+        val prizeCount = boughtLottoTickets.count { lottoPrizePolicy.won(it) != null }
         totalPrize += lottoPrizePolicy.wonPrize.value * prizeCount
         println("${lottoPrizePolicy.wonMatchedCount}개 일치 (${lottoPrizePolicy.wonPrize.value})-${prizeCount}개")
     }
-    val earningsRate: Double = totalPrize.toDouble() / (buyTicketCount * lottoTicketPrice.value)
+    val earningsRate: Double = totalPrize.toDouble() / (boughtLottoTickets.size * lottoTicketPrice.value)
     println(
         "총 수익률은 ${
         String.format("%.1f", earningsRate)
