cbc126bc862808a478f653838d87f64ecac54195
mygoaway <mygoaway2@gmail.com>
2022-05-28 11:29:16 +0900

    feat: 입력한 금액만큼 로또 구매 기능 구현 및 테스트 케이스 작성

---
+++ src/main/kotlin/lotto/LottoBuyer.kt
@@ -0,0 +1,28 @@
+package lotto
+
+class LottoBuyer {
+    private val _lottoNumbers = ArrayList<Lotto>()
+    val lottoNumbers
+        get() = _lottoNumbers
+
+    fun buyLotto(money: Int) {
+        _lottoNumbers.clear()
+        _lottoNumbers.addAll(getLottoNumbers(checkBuyLottoCount(money)))
+    }
+
+    private fun checkBuyLottoCount(money: Int): Int {
+        return money / PRICE
+    }
+
+    private fun getLottoNumbers(count: Int): List<Lotto> {
+        val result = ArrayList<Lotto>()
+        for (i in 1..count) {
+            result.add(Lotto().also { it.getLottoNumber() })
+        }
+        return result
+    }
+
+    companion object {
+        const val PRICE = 1000
+    }
+}
---
+++ src/test/kotlin/lotto/LottoBuyerTest.kt
@@ -0,0 +1,22 @@
+package lotto
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class LottoBuyerTest {
+    private lateinit var lottoBuyer: LottoBuyer
+
+    @BeforeEach
+    fun setup() {
+        lottoBuyer = LottoBuyer()
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [5000, 20000, 100000])
+    fun `1) 입력한 금액만큼 로또 구매`(money: Int) {
+        lottoBuyer.buyLotto(money)
+        Assertions.assertThat(lottoBuyer.lottoNumbers.size).isEqualTo(money / LottoBuyer.PRICE)
+    }
+}
