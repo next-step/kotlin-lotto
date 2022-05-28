19a74e412c98537c97d583454e84391449fd9009
larry.p <larry.p@kakaocorp.com>
2022-05-29 03:18:14 +0900

    refactor: [리뷰반영] LottoLastNumbers 코드 정리 및 컨벤션 적용

--- src/main/kotlin/lotto/domain/LottoLastNumbers.kt
+++ src/main/kotlin/lotto/domain/LottoLastNumbers.kt
@@ -1,11 +1,11 @@
 package lotto.domain
 
-data class LottoLastNumbers(private val tickets: LottoTicket, val bonus: Int) : Set<Int> by tickets.numbers {
-
-    constructor(numbers: Set<Int>, bonus: Int) : this(LottoTicket(numbers), bonus)
+data class LottoLastNumbers(private val tickets: LottoTicket, val bonus: Int) : Set<Int> by tickets {
 
     init {
         require(bonus in LottoTicket.RANGE_OF_LOTTO_NUMBER)
-        require(tickets.numbers.contains(bonus).not())
+        require(contains(bonus).not())
     }
+
+    constructor(numbers: Set<Int>, bonus: Int) : this(LottoTicket(numbers), bonus)
 }
