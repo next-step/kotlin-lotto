e4502ea2c98fd649a1f5e9882d05dd1f728e4b02
molly <molly.d@kakaocorp.com>
2022-05-29 01:16:13 +0900

    feat: step2 리뷰 사항 반영

--- src/main/kotlin/lotto/domain/LottoNumberGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoNumberGenerator.kt
@@ -1,12 +1,12 @@
 package lotto.domain
 
+import lotto.domain.dto.GlobalConst.Companion.LOTTO_NUMBER_RANGE
 import lotto.domain.dto.LottoNumber
 
 class LottoNumberGenerator {
     companion object {
         fun autoGenerate(): LottoNumber = LottoNumber(LOTTO_NUMBER_RANGE.shuffled().slice(LOTTO_NUMBER_COUNT))
 
-        private val LOTTO_NUMBER_RANGE = (1..45)
         private val LOTTO_NUMBER_COUNT = (0..5)
     }
 }
---
+++ src/main/kotlin/lotto/domain/dto/GlobalConst.kt
@@ -0,0 +1,7 @@
+package lotto.domain.dto
+
+class GlobalConst {
+    companion object {
+        val LOTTO_NUMBER_RANGE = (1..45)
+    }
+}
