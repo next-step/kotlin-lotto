309efbfa617f38cbc3b0733076755c4e12b136a8
qyu.st <qyu.st@kakaocorp.com>
2022-05-28 22:56:52 +0900

    test: 수익률 계산 로직 테스트 추가

--- src/test/kotlin/lotto/application/LotteryAnnouncerTest.kt
+++ src/test/kotlin/lotto/application/LotteryAnnouncerTest.kt
@@ -3,6 +3,7 @@ package lotto.application
 import lotto.domain.Lotteries
 import lotto.domain.Lotto
 import lotto.domain.Price
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.MapAssert
 import org.junit.jupiter.api.Test
 
@@ -44,5 +45,26 @@ class LotteryAnnouncerTest {
         val result = LotteryAnnouncer.announce(winner, bonusNumber, lotteries)
 
         MapAssert(result).extractingByKey(Price.SECOND).isNotNull
+    }
+
+    @Test
+    fun `earning rate test`() {
+        val investment = 3000
+        val lotteries = Lotteries(
+            listOf(
+                Lotto(2, 1, 3, 5, 4, 8),
+                Lotto(11, 12, 13, 14, 15, 16),
+                Lotto(21, 22, 23, 24, 25, 26)
+            )
+        )
+
+        val winner = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val bonusNumber = 8
+        val expected = 10000f
+
+        val priceGroupedLotteries = LotteryAnnouncer.announce(winner, bonusNumber, lotteries)
+        val result = LotteryAnnouncer.getEarningRate(investment, priceGroupedLotteries)
+
+        assertThat(result).isEqualTo(expected)
     }
 }
