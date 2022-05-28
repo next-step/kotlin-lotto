fdb6af9644fc248c27e2381e9886a6a0c027050c
yohan <cd80@kakao.com>
2022-05-28 10:56:43 +0900

    feat: LottoSet은 요청한 등수의 당첨자 수를 반환한다.

---
+++ src/main/kotlin/lotto/auto/vo/LottoSet.kt
@@ -0,0 +1,8 @@
+package lotto.auto.vo
+
+import lotto.auto.domain.Lotto
+
+class LottoSet(private val lottos: List<Lotto>) {
+
+    fun countPlace(winningLottery: Lotto, place: LottoScore): Int = lottos.count { it.match(winningLottery) == place }
+}
