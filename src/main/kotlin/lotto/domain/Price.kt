8725973829a5b123f686a7598ff474b506bd0569
qyu.st <qyu.st@kakaocorp.com>
2022-05-28 22:13:36 +0900

    feat: 새로운 2등 추가

--- src/main/kotlin/lotto/domain/Price.kt
+++ src/main/kotlin/lotto/domain/Price.kt
@@ -2,8 +2,9 @@ package lotto.domain
 
 enum class Price(val winningPrize: Int) {
     FIRST(2_000_000_000),
-    SECOND(1_500_000),
-    THIRD(50_000),
-    FOURTH(5_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FOURTH(50_000),
+    FIFTH(5_000),
     NONE(0)
 }
