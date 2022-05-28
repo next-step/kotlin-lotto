7b2b1a22f760e6dbbd05a151bd14ff30896e57d6
larry.p <larry.p@kakaocorp.com>
2022-05-29 03:35:12 +0900

    refactor: [리뷰반영] LottoTickets에 불필요한 코드 제거

--- src/main/kotlin/lotto/domain/LottoTickets.kt
+++ src/main/kotlin/lotto/domain/LottoTickets.kt
@@ -14,25 +14,4 @@ data class LottoTickets(private val tickets: List<LottoTicket>) : List<LottoTick
                 .map { it.key to it.value.size }
                 .toMap()
         )
-
-    fun getMatchResult(lastNumbers: LottoLastNumbers): Map<LottoMatch, MatchCount> {
-        return mutableMapOf<LottoMatch, MatchCount>().apply {
-            LottoMatch.values().forEach {
-                put(it, getMatchCount(it, lastNumbers))
-            }
-        }
-    }
-
-    private fun getMatchCount(match: LottoMatch, lastNumbers: LottoLastNumbers): Int {
-        return getMatchTickets(match, lastNumbers).let { matchTickets ->
-            if (match.withBonus) {
-                matchTickets.filter { it.contains(lastNumbers.bonus) }
-            } else {
-                matchTickets.filterNot { it.contains(lastNumbers.bonus) }
-            }
-        }.size
-    }
-
-    private fun getMatchTickets(match: LottoMatch, lastNumbers: LottoLastNumbers): List<LottoTicket> =
-        filter { it.intersect(lastNumbers).size == match.count }
 }
