9ac390152280fe1237abacf2454f68165b3f1ca1
mygoaway <mygoaway2@gmail.com>
2022-05-28 12:12:09 +0900

    feat: 입력한 당첨 번호와 개수별로 일치하는 로또당첨 확인 기능 구현 및 테스트 케이스 작성

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -14,9 +14,31 @@ class Lotto {
         return (LOTTO_START_NUMBER..LOTTO_END_NUMBER).shuffled().take(LOTTO_SIZE)
     }
 
+    fun getCountWithWinningLotteryNumber(winningLottoNumbers: List<Int>): Int {
+        return winningLottoNumbers.filter { number -> _lottoNumber.contains(number) }.size
+    }
+
+    fun exchangeMoney(count: Int): Int {
+        return when (count) {
+            THREE_COUNT -> THREE_COUNT_MONEY
+            FOUR_COUNT -> FOUR_COUNT_MONEY
+            FIVE_COUNT -> FIVE_COUNT_MONEY
+            SIX_COUNT -> SIX_COUNT_MONEY
+            else -> 0
+        }
+    }
+
     companion object {
         private const val LOTTO_START_NUMBER = 1
         private const val LOTTO_END_NUMBER = 45
         private const val LOTTO_SIZE = 6
+        private const val THREE_COUNT = 3
+        private const val FOUR_COUNT = 4
+        private const val FIVE_COUNT = 5
+        private const val SIX_COUNT = 6
+        private const val THREE_COUNT_MONEY = 5000
+        private const val FOUR_COUNT_MONEY = 50000
+        private const val FIVE_COUNT_MONEY = 150000
+        private const val SIX_COUNT_MONEY = 2000000000
     }
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -17,4 +17,17 @@ class LottoTest {
         lotto.getLottoNumber()
         Assertions.assertThat(lotto.lottoNumber.size).isEqualTo(6)
     }
+
+    @Test
+    fun `2) 로또를 구매한 번호와 당첨 번호가 일치하는 경우 개수를 반환한다`() {
+        lotto.getLottoNumber(listOf(5, 10, 15, 20, 25, 30))
+        val count = lotto.getCountWithWinningLotteryNumber(listOf(1, 2, 3, 4, 10, 15))
+        Assertions.assertThat(count).isEqualTo(2)
+    }
+
+    @Test
+    fun `3) 로또를 구매한 번호와 당첨 번호가 일치하는 경우 개수를 반환한 경우 해당 개수 통해서 당첨금액과 교환이 가능하다`() {
+        val exchangeMoney = lotto.exchangeMoney(3)
+        Assertions.assertThat(exchangeMoney).isEqualTo(5000)
+    }
 }
