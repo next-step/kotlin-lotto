d9dcd137eb7bc7f98022678206443cfc676a8d97
Wave1994-Hoon <kk03187693@gmail.com>
2022-05-29 23:45:10 +0900

    [step2] readLine 을 readIn 으로 변경하라

--- src/main/kotlin/lotto/presentation/InboundView.kt
+++ src/main/kotlin/lotto/presentation/InboundView.kt
@@ -7,11 +7,8 @@ class InboundView {
     fun inputPurchaseAmount(): Int {
         println("구입금액을 입력해 주세요.")
 
-        val purchaseAmount: String? = readLine()
-
-        if (purchaseAmount.isNullOrBlank()) {
-            throw IllegalArgumentException("입력 값을 확인해주세요.")
-        }
+        val purchaseAmount: String = readln()
+        require(purchaseAmount.isNotBlank()) { "공백을 입력하셨습니다." }
 
         return runCatching { purchaseAmount.toInt() }
             .getOrElse { throw IllegalArgumentException("숫자를 입력해주세요.") }
@@ -20,11 +17,8 @@ class InboundView {
     fun inputWinningNumber(): LottoWinningNumber {
         println("\n지난 주 당첨 번호를 입력해 주세요.")
 
-        val winningNumber: String? = readLine()
-
-        if (winningNumber.isNullOrBlank()) {
-            throw IllegalArgumentException("입력 값을 확인해주세요.")
-        }
+        val winningNumber: String = readln()
+        require(winningNumber.isNotBlank()) { "공백을 입력하셨습니다." }
 
         return LottoWinningNumber(
             winningNumber.toTokenize()
