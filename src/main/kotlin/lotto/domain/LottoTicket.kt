d68a0b3a20d72b316efcdcd1c05d622e1829af59
Wave1994-Hoon <kk03187693@gmail.com>
2022-05-30 00:31:29 +0900

    [step2] 매직넘버를 제거하라

--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -1,13 +1,17 @@
 package lotto.domain
 
-data class LottoTicket(val numbers: Set<Int>) {
+class LottoTicket(val numbers: Set<Int>) {
     init {
-        require(numbers.size == 6) {
+        require(numbers.size == LOTTO_SIZE) {
             "6자리 로또 티켓을 입력해주세요."
         }
     }
 
     fun isMatch(matchCount: Int, winningNumber: Set<Int>): Boolean {
         return numbers.intersect(winningNumber).size == matchCount
+    }
+
+    companion object {
+        private const val LOTTO_SIZE = 6
     }
 }
--- src/main/kotlin/lotto/domain/LottoWinningNumber.kt
+++ src/main/kotlin/lotto/domain/LottoWinningNumber.kt
@@ -1,9 +1,5 @@
 package lotto.domain
 
-data class LottoWinningNumber(val numbers: Set<Int>) {
-    init {
-        require(numbers.size == 6) {
-            "6자리 로또 당첨 번호를 입력해주세요."
-        }
-    }
+class LottoWinningNumber(val numbers: Set<Int>) {
+    private val winningNumber: LottoTicket = LottoTicket(numbers)
 }
