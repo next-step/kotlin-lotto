bc78d1f85599c5a543033c933ca8ecb1c426b146
Wave1994-Hoon <kk03187693@gmail.com>
2022-05-29 23:49:08 +0900

    [step2] LottoTickets 일급 컬랙션 객체를 개선하라

--- src/main/kotlin/lotto/domain/collection/LottoTickets.kt
+++ src/main/kotlin/lotto/domain/collection/LottoTickets.kt
@@ -4,7 +4,7 @@ import lotto.domain.LottoTicket
 
 data class LottoTickets(
     private val lottoTickets: List<LottoTicket>
-) {
+) : List<LottoTicket> by lottoTickets {
     init {
         require(lottoTickets.isNotEmpty()) {
             "로또 티켓 개수가 1개 이상이여야 합니다."
@@ -16,7 +16,6 @@ data class LottoTickets(
     }
 
     fun getMatchCount(matchCount: Int, numbers: Set<Int>): Int {
-        return lottoTickets.filter { it.numbers.intersect(numbers).size == matchCount }
-            .size
+        return lottoTickets.count() { it.numbers.intersect(numbers).size == matchCount }
     }
 }
