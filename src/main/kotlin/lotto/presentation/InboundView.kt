9dbd62a152d6e8b555fbd57f596439fca2d42ed0
Wave1994-Hoon <kk03187693@gmail.com>
2022-05-29 12:15:20 +0900

    [step2] 로또 UI 를 구현하라

---
+++ src/main/kotlin/lotto/presentation/InboundView.kt
@@ -0,0 +1,59 @@
+package lotto.presentation
+
+import lotto.domain.LottoWinningNumber
+
+class InboundView {
+
+    fun inputPurchaseAmount(): Int {
+        println("구입금액을 입력해 주세요.")
+
+        val purchaseAmount: String? = readLine()
+
+        if (purchaseAmount.isNullOrBlank()) {
+            throw IllegalArgumentException("입력 값을 확인해주세요.")
+        }
+
+        return kotlin
+            .runCatching { purchaseAmount.toInt() }
+            .getOrElse { throw IllegalArgumentException("숫자를 입력해주세요.") }
+    }
+
+    fun inputWinningNumber(): LottoWinningNumber {
+        println("\n지난 주 당첨 번호를 입력해 주세요.")
+
+        val winningNumber: String? = readLine()
+
+        if (winningNumber.isNullOrBlank()) {
+            throw IllegalArgumentException("입력 값을 확인해주세요.")
+        }
+
+        return LottoWinningNumber(
+            winningNumber.toTokenize()
+                .checkUniqueToken()
+                .toMapInt()
+                .toSet()
+        )
+    }
+
+    private fun String.toTokenize(): List<String> {
+        return this.split(',')
+            .map { it.trim() }
+            .filter { it.isNotBlank() }
+    }
+
+    private fun List<String>.checkUniqueToken(): List<String> {
+        val originalSize = this.size
+        val distinctSize = this.toSet().size
+
+        require(originalSize == distinctSize) {
+            "중복 되는 숫자는 입력할 수 없습니다."
+        }
+
+        return this
+    }
+
+    private fun List<String>.toMapInt(): List<Int> {
+        return kotlin.runCatching { map { it.toInt() } }
+            .getOrElse { throw IllegalArgumentException("숫자를 입력해주세요.") }
+    }
+}
---
+++ src/main/kotlin/lotto/presentation/OutboundView.kt
@@ -0,0 +1,35 @@
+package lotto.presentation
+
+import lotto.domain.LottoRewardCalculator
+import lotto.domain.LottoWinningNumber
+import lotto.domain.collection.LottoTickets
+import lotto.domain.type.LottoMatchType
+
+class OutboundView {
+
+    fun printLottoTickets(lottoTickets: LottoTickets) {
+        println("${lottoTickets.getLottoTickets().size} 개를 구매했습니다.")
+
+        lottoTickets.getLottoTickets()
+            .forEach { ticket ->
+                println(ticket.numbers.joinToString(prefix = "[", postfix = "]\n"))
+            }
+    }
+
+    fun printLottoMatchCount(lottoTickets: LottoTickets, winningNumber: LottoWinningNumber) {
+        println("\n당첨 동계")
+        println("---------")
+
+        LottoMatchType.values().forEach {
+            println(
+                "${it.matchCount}개 일치 (${it.reward}원) - " +
+                    "${lottoTickets.getMatchCount(it.matchCount, winningNumber.numbers)}개"
+            )
+        }
+    }
+
+    fun getRewardRate(lottoRewardCalculator: LottoRewardCalculator, purchaseAmount: Int, lottoTicketPrice: Int) {
+        val profit = String.format("%.02f", lottoRewardCalculator.getRewardRate(purchaseAmount, lottoTicketPrice))
+        println("총 수익률은 ${profit}입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)")
+    }
+}
