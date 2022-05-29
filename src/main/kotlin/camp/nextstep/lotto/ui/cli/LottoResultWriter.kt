35c9983663511c9057c6c0540a35456eb7a80499
aeomhs <aeomhs@gmail.com>
2022-05-29 16:47:03 +0900

    fix: 결과 출력 괄호 수정

--- src/main/kotlin/camp/nextstep/lotto/ui/cli/LottoResultWriter.kt
+++ src/main/kotlin/camp/nextstep/lotto/ui/cli/LottoResultWriter.kt
@@ -17,7 +17,7 @@ object LottoResultWriter {
         }
 
         val earningRate = result.totalEarn / result.seed
-        val explainRate = "기준이 1이기 때문에 결과적으로 " + if (earningRate > 1) "이익을 보았다는 의미)" else if (earningRate == 0) "본전이라는 의미)" else "손해라는 의미"
+        val explainRate = "기준이 1이기 때문에 결과적으로 " + if (earningRate > 1) "이익을 보았다는 의미" else if (earningRate == 0) "본전이라는 의미" else "손해라는 의미"
         println("총 수익률은 ${earningRate}입니다. ($explainRate)")
     }
 }
