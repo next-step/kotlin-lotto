d3ec2543abb9db1e4906d9b156698064ef05e1d6
yjna2316 <yjna2316@gmail.com>
2022-05-29 19:43:11 +0900

    feat: 로또 구입 금액이 1천원 미만이면 IllegalArgumentException 예외가 발생한다

--- src/main/kotlin/lotto/domain/LottoGame.kt
+++ src/main/kotlin/lotto/domain/LottoGame.kt
@@ -1,7 +1,12 @@
 package lotto.domain
 
 class LottoGame {
-    fun getNumOfLotto(money: Long): Long {
+    fun initGame(money: Long): Long {
+        val numOfLotto = getNumOfLotto(money)
+        return numOfLotto
+    }
+    private fun getNumOfLotto(money: Long): Long {
+        require(money >= 1000L) { "구입가능한 로또가 없어요. 1000원 이상의 금액만 입력해주세요." }
         return money / LOTTO_PRICE
     }
 
--- src/test/kotlin/lotto/domain/LottoGameTest.kt
+++ src/test/kotlin/lotto/domain/LottoGameTest.kt
@@ -1,15 +1,32 @@
 package lotto.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
+import java.lang.IllegalArgumentException
 
 class LottoGameTest {
+    private lateinit var lottoGame: LottoGame
+
+    @BeforeEach
+    fun beforeEachTest() {
+        lottoGame = LottoGame()
+    }
     @ParameterizedTest
     @CsvSource(value = ["14000|14", "1000|1", "2500|2"], delimiter = '|')
     fun `로또 구입 금액에 해당하는 로또 수를 계산할 수 있다`(money: Long, expected: Long) {
-        val sut = LottoGame()
-        val actual = sut.getNumOfLotto(money)
-        assertEquals(expected, actual)
+        assertThat(lottoGame.initGame(money)).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["900", "0"])
+    fun `로또 구입 금액이 1천원 미만이면 IllegalArgumentException 예외가 발생한다`(money: Long) {
+        assertThrows(IllegalArgumentException::class.java) {
+            lottoGame.initGame(money)
+        }
     }
 }
