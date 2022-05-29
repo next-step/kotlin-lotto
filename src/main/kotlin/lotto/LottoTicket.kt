cac855501f0f750e2adcbfbf0b7fa49b76c37a41
gabstin <ksgilove0@gmail.com>
2022-05-29 19:28:18 +0900

    [step3] 리뷰 반영 - 중복으로 정의된 유효성 체크로직 제거

--- src/main/kotlin/lotto/LottoTicket.kt
+++ src/main/kotlin/lotto/LottoTicket.kt
@@ -6,17 +6,12 @@ class LottoTickets(val lottery: List<LottoTicket> = emptyList())
 open class LottoTicket(val numbers: List<LottoNumber>) {
     init {
 
-        numbers.forEach {
-            require(it.number > 0) { NOT_POSITIVE_NUMBER_MESSAGE }
-        }
-
         require(numbers.size == LOTTO_NUMBER_COUNT) {
             NOT_MATCH_NUMBER_COUNT
         }
     }
 
     companion object {
-        private const val NOT_POSITIVE_NUMBER_MESSAGE = "로또 번호는 0 또는 음수일 수 없습니다."
         private const val NOT_MATCH_NUMBER_COUNT = "로또 번호는 $LOTTO_NUMBER_COUNT 개의 숫자로 구성되어야 합니다"
     }
 }
