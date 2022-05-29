f3ad9f16352b4e5d9973466ab5e62fefe78cfe4b
Yohan <yohan@inflab.com>
2022-05-29 23:44:08 +0900

    WIP

--- src/main/kotlin/lotto/vo/LotteryRank.kt
+++ src/main/kotlin/lotto/vo/LotteryRank.kt
@@ -6,7 +6,9 @@ enum class LotteryRank(val matchCount: Int, val rewardMoney: Int) {
     FOUR_PLACE(3, 5_000),
     THIRD_PLACE(4, 50_000),
     TWO_PLACE(5, 1_500_000),
-    ONE_PLACE(6, 2_000_000_000);
+    ONE_PLACE(6, 2_000_000_000),
+
+    BONUS_TWO_PLACE(5, 30_000_000);
 
     companion object {
 
