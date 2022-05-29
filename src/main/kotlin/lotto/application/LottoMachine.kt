f50e25224d4fa0421f97058139df70d9ef7ce700
dongchul <sorkehdcjf@gmail.com>
2022-05-29 19:34:33 +0900

    feat: 수동 로또 구입 구현

--- src/main/kotlin/lotto/application/LottoMachine.kt
+++ src/main/kotlin/lotto/application/LottoMachine.kt
@@ -11,15 +11,32 @@ private const val START_INDEX = 0
 private const val END_INDEX = 6
 private const val MIN_NUMBER = 1
 private const val MAX_NUMBER = 45
-
+private const val DELIMITER = ","
 class LottoMachine(
     private val purchase: Purchase
 ) {
 
-    fun buyAuto(): LottoBundle =
-        List(purchase.lottoPurchaseCount) {
-            Lotto(LOTTO_NUMBERS.shuffled().subList(START_INDEX, END_INDEX).sorted().toSet())
+    fun buy(manualLottoNumbers: List<String>): LottoBundle {
+        require(purchase.purchaseCounts.manualLottoCount.count == manualLottoNumbers.size) {
+            "수동로또 구입 갯수와 입력한 수동 로또 갯수가 일치 해야합니다."
+        }
+        return buildList {
+            addAll(buyManual(manualLottoNumbers))
+            addAll(buyAuto())
         }.let(::LottoBundle)
+    }
+
+    private fun buyManual(manualLottoNumbers: List<String>): List<Lotto> = manualLottoNumbers.map { lottoNumber ->
+        lottoNumber.split(DELIMITER)
+            .map { it.toIntOrNull() ?: throw IllegalArgumentException("로또 번호는 숫자만 가능합니다.") }
+            .map(::LottoNumber)
+            .toSet()
+    }.map(::Lotto)
+
+    private fun buyAuto(): List<Lotto> =
+        List(purchase.purchaseCounts.autoLottoCount.count) {
+            Lotto(LOTTO_NUMBERS.shuffled().subList(START_INDEX, END_INDEX).sorted().toSet())
+        }
 
     fun drawLottoBundle(
         lottoBundle: LottoBundle,
--- src/test/kotlin/lotto/application/LottoMachineTest.kt
+++ src/test/kotlin/lotto/application/LottoMachineTest.kt
@@ -1,33 +1,53 @@
 package lotto.application
 
 import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.doubles.shouldBeGreaterThanOrEqual
-import lotto.application.vo.Amount
 import lotto.application.vo.Purchase
 import lotto.domain.WinningLotto
 
 class LottoMachineTest : StringSpec({
     "로또 기계를 생성할수 있다" {
-        shouldNotThrow<Throwable> { LottoMachine(Purchase(Amount(2_000))) }
+        shouldNotThrow<Throwable> { LottoMachine(Purchase(2_000, 1)) }
     }
 
     "자동 로또를 구입할수 있다" {
-        val purchase = Purchase(Amount(2_000))
-        val lottoBundle = LottoMachine(purchase).buyAuto()
+        val purchase = Purchase(2_000, 0)
+        val lottoBundle = LottoMachine(purchase).buy(emptyList())
 
-        lottoBundle.lottos shouldHaveSize purchase.lottoPurchaseCount
+        lottoBundle.lottos shouldHaveSize purchase.purchaseCounts.autoLottoCount.count
+    }
+
+    "수동 로또를 구입할수 있다" {
+        val purchase = Purchase(2_000, 2)
+        val lottoBundle = LottoMachine(purchase).buy(listOf("1,2,3,4,5,6", "1,2,3,4,6,8"))
+
+        lottoBundle.lottos shouldHaveSize purchase.purchaseCounts.manualLottoCount.count
+    }
+
+    "수동 로또 구입수와 입력한 수동 로또 번호수가 일치 하지 않으면 Exception을 던진다" {
+        val purchase = Purchase(2_000, 2)
+
+        shouldThrow<IllegalArgumentException> { LottoMachine(purchase).buy(listOf("1,2,3,4,6,8")) }
+    }
+
+    "자동, 수동 로또를 구입할수 있다" {
+        val purchase = Purchase(2_000, 1)
+        val lottoBundle = LottoMachine(purchase).buy(listOf("1,2,3,4,5,6"))
+
+        lottoBundle.lottos shouldHaveSize purchase.purchaseCounts.autoLottoCount.count + purchase.purchaseCounts.manualLottoCount.count
     }
 
     "로또 당첨 결과를 알수 있다" {
-        val lottoBundle = lottoMachine.buyAuto()
+        val lottoBundle = lottoMachine.buy(emptyList())
         val winningLotto = WinningLotto(listOf(1, 2, 3, 4, 5, 6), 45)
 
         lottoMachine.drawLottoBundle(lottoBundle, winningLotto).winningRate shouldBeGreaterThanOrEqual 0.0
     }
 }) {
     companion object {
-        private val lottoMachine = LottoMachine(Purchase(Amount(10_000)))
+        private val lottoMachine = LottoMachine(Purchase(2_000, 1))
     }
 }
