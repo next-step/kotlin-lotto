55617d032646863de7b675edf08631ccb3573452
eunseok.cheong <eunseok.cheong@webtoonscorp.com>
2022-05-31 03:28:37 +0900

    feat: 코드리뷰 적용

--- src/main/kotlin/advancedcalculate/controller/AdvancedCalculatorController.kt
+++ src/main/kotlin/advancedcalculate/controller/AdvancedCalculatorController.kt
@@ -2,9 +2,8 @@ package advancedcalculate.controller
 
 import advancedcalculate.service.AdvancedCalculatorService
 
-
 class AdvancedCalculatorController(private val advancedCalculatorService: AdvancedCalculatorService) {
-    
+
     fun run(input: String): Double {
         return advancedCalculatorService.calculate(input)
     }
