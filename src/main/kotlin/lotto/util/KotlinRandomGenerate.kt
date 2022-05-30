317fa98c6252f77d758df34fcb59d41f92663079
Juice <3221ju@gmail.com>
2022-05-31 03:44:50 +0900

    feat: makeRandomUniqueIntList 예외사항 추가

--- src/main/kotlin/lotto/util/KotlinRandomGenerate.kt
+++ src/main/kotlin/lotto/util/KotlinRandomGenerate.kt
@@ -1,7 +1,9 @@
 package lotto.util
 
 object KotlinRandomGenerate : RandomGenerate {
-    override fun makeRandomUniqueIntList(listSize: Int, randomRange: IntRange): List<Int> {
-        return randomRange.shuffled().take(listSize)
+    @Throws(IllegalArgumentException::class)
+    override fun makeRandomUniqueIntList(takeListSize: Int, randomRange: IntRange): List<Int> {
+        require(takeListSize <= randomRange.last - randomRange.first + 1) { "랜덤범위($randomRange)가 가져올 리스트 갯수($takeListSize) 보다 큽니다" }
+        return randomRange.shuffled().take(takeListSize)
     }
 }
