4e747212adbe721e0ddad1048eeac98de7476411
vivian.bae <vivian.bae@kakaocorp.com>
2022-05-30 14:12:36 +0900

    리뷰 반영 - MethodSource 대신 CsvSource 사용하기

--- src/test/kotlin/lotto/domain/prize/LottoPrizeTest.kt
+++ src/test/kotlin/lotto/domain/prize/LottoPrizeTest.kt
@@ -2,29 +2,20 @@ package lotto.domain.prize
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
-import java.util.stream.Stream
+import org.junit.jupiter.params.provider.CsvSource
 
 class LottoPrizeTest {
     @ParameterizedTest
-    @MethodSource("provideLottoPrizes")
+    @CsvSource(
+        "0, 0",
+        "1, 0",
+        "2, 0",
+        "3, 5000",
+        "4, 50000",
+        "5, 1500000",
+        "6, 2000000000"
+    )
     fun `일치하는 번호 수에 따라 당첨 금액이 달라진다`(numberOfMatches: Int, expected: Int) {
         Assertions.assertThat(LottoPrize.of(numberOfMatches).prizeAmount).isEqualTo(expected)
-    }
-
-    companion object {
-        @JvmStatic
-        private fun provideLottoPrizes(): Stream<Arguments> {
-            return Stream.of(
-                Arguments.of(0, 0),
-                Arguments.of(1, 0),
-                Arguments.of(2, 0),
-                Arguments.of(3, 5000),
-                Arguments.of(4, 50000),
-                Arguments.of(5, 1500000),
-                Arguments.of(6, 2000000000)
-            )
-        }
     }
 }
