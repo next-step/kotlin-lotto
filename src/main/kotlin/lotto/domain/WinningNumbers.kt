a5484849eed62e3cbc82e0e284fefda819243f7f
pplenty <newtype26@naver.com>
2022-05-30 21:24:34 +0900

    feat: 보너스에 대한 유효성 검사 및 표준 입력 추가

--- src/main/kotlin/lotto/domain/WinningNumbers.kt
+++ src/main/kotlin/lotto/domain/WinningNumbers.kt
@@ -6,6 +6,7 @@ class WinningNumbers(numbers: List<Int>, bonus: Int) {
     private val bonusNumber: LottoNumber
 
     init {
+        require(!numbers.contains(bonus)) { "보너스 번호와 당첨 번호가 중복될 수 없습니다." }
         winningNumbers = LottoTicket(numbers)
         bonusNumber = LottoNumber(bonus)
     }
--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -10,10 +10,10 @@ class InputView {
 
     private val winningNumbers: WinningNumbers by lazy {
         println("지난 주 당첨 번호를 입력해 주세요.")
-        WinningNumbers(
-            readln().split(",")
-                .map { it.trim() }.map { it.toInt() }
-        )
+        val numbers = readln().split(",").map { it.trim() }.map { it.toInt() }
+        println("지난 주 당첨 번호를 입력해 주세요.")
+        val bonus = readln().toInt()
+        WinningNumbers(numbers, bonus)
     }
 
     fun amount(): Int = amount
