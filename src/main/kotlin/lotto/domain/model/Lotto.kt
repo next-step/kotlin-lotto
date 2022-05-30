4688eadf28d26e50ce65a6e2fd049ceafc1d2158
austen.lee <austen.lee@kakaocorp.com>
2022-05-30 23:51:40 +0900

    step3 refactor : 이제 안 쓰는 make 제거

--- src/main/kotlin/lotto/domain/model/Lotto.kt
+++ src/main/kotlin/lotto/domain/model/Lotto.kt
@@ -20,17 +20,6 @@ value class Lotto(val numbers: Set<LottoNumber>) {
         const val LOTTO_NUMBER_COUNT = 6
         private const val MESSAGE_INVALID_LOTTO_NUMBERS_SIZE = "로또 번호는 6개의 숫자로 이루어져야 합니다.\n입력된 숫자 수 : "
 
-        fun make(): Lotto {
-            val numbers = LottoNumber.LOTTO_NUMBER_RANGE
-                .shuffled()
-                .take(LOTTO_NUMBER_COUNT)
-                .map { number ->
-                    LottoNumber(number)
-                }.toSet()
-
-            return Lotto(numbers)
-        }
-
         fun from(numbers: List<Int>): Lotto {
             return Lotto(numbers.map { LottoNumber(it) }.toSet())
         }
