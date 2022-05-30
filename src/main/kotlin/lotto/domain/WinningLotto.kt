c330f6f1172d88baccbe9cb37478300c5f562e00
vivian.bae <vivian.bae@kakaocorp.com>
2022-05-30 18:27:30 +0900

    에러 메세지 추가

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -6,7 +6,7 @@ class WinningLotto(winningNumbers: List<Int>, val bonusNumber: Int) {
     val winningNumbers = LottoNumbers(winningNumbers)
 
     init {
-        require(!winningNumbers.contains(bonusNumber))
+        require(!winningNumbers.contains(bonusNumber)) { "당첨번호와 보너스번호는 겹칠 수 없습니다." }
     }
 
     fun getNumberOfMatchingNumbers(lotto: Lotto): Int = winningNumbers.list.intersect(lotto.numbers.list.toSet()).size
