c3ee370a10062ac5a52a949ffd110d9eef6e7f3d
pplenty <newtype26@naver.com>
2022-05-30 20:16:00 +0900

    refactor: 피드백반영 - List 빌더 메서드 활용

--- src/main/kotlin/lotto/domain/TicketMachine.kt
+++ src/main/kotlin/lotto/domain/TicketMachine.kt
@@ -4,11 +4,8 @@ import lotto.domain.LottoTicket.Companion.NUMBER_SIZE
 
 class TicketMachine(private val numberGenerator: NumberGenerator) {
 
-    fun buy(amount: Int): List<LottoTicket> {
-        val tickets = mutableListOf<LottoTicket>()
-        repeat(amount / TICKET_PRICE) { tickets += LottoTicket(numberGenerator.randomTake(NUMBER_SIZE)) }
-        return tickets.toList()
-    }
+    fun buy(amount: Int): List<LottoTicket> =
+        List(amount / TICKET_PRICE) { LottoTicket(numberGenerator.randomTake(NUMBER_SIZE)) }
 
     companion object {
         private const val TICKET_PRICE = 1_000
