13df18e4173ca76308b89aef276940b1ccd4462c
vivian.bae <vivian.bae@kakaocorp.com>
2022-05-30 17:37:30 +0900

    보너스 볼을 입력하도록 유도한다

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,6 +3,7 @@ package lotto.view
 object InputView {
     private const val TOTAL_PAYMENT_QUESTION = "구입금액을 입력해 주세요."
     private const val WINNING_NUMBERS_QUESTION = "지난 주 당첨 번호를 입력해 주세요."
+    private const val BONUS_BALL_NUMBER_QUESTION = "보너스 볼을 입력해 주세요."
 
     fun readTotalPayment(): Int {
         println(TOTAL_PAYMENT_QUESTION)
@@ -22,6 +23,14 @@ object InputView {
                     .getPositiveNumber()
                     ?: throw IllegalArgumentException("당첨 번호는 0 이상의 정수이여야 합니다")
             }
+    }
+
+    fun readBonusBallNumber(): Int {
+        println(BONUS_BALL_NUMBER_QUESTION)
+
+        return readln()
+            .getPositiveNumber()
+            ?: throw IllegalArgumentException("보너스 볼 번호는 0 이상의 정수이여야 합니다")
     }
 
     private fun String.getPositiveNumber(): Int? = toIntOrNull()?.takeIf { it >= 0 }
