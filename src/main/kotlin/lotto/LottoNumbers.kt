a80f28357f6d74f1324b60d5c9165c076fd42a3f
luke.im <questknight26@gmail.com>
2022-05-30 19:38:36 +0900

    feat : 6개의 번호 가지는 로또 용지 구현

---
+++ src/main/kotlin/lotto/LottoNumbers.kt
@@ -0,0 +1,17 @@
+package lotto
+
+class LottoNumbers(val numbers: List<LottoNumber>) {
+
+    init {
+        require(numbers.size == 6 && numbers.toSet().size == 6) { "로또당 중복되지 않는 6개의 숫자가 있어야합니다." }
+    }
+
+    companion object {
+        private val LottoNumberRange = (1..45)
+
+        fun random(): LottoNumbers {
+            val randomNumbers = LottoNumberRange.toList().shuffled().take(6).map(::LottoNumber)
+            return LottoNumbers(randomNumbers)
+        }
+    }
+}
---
+++ src/test/kotlin/lotto/LottoNumbersTest.kt
@@ -0,0 +1,29 @@
+package lotto
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class LottoNumbersTest {
+
+    @Test
+    fun `로또 중복되지 않는 6개의 번호를 가진다`() {
+        val lottoNumbers = LottoNumbers.random()
+        Assertions.assertThat(lottoNumbers.numbers.size).isEqualTo(6)
+        Assertions.assertThat(lottoNumbers.numbers.toSet().size).isEqualTo(6)
+    }
+
+    @Test
+    fun `로또 번호가 중복된 경우 IllegalArgumentException 예외를 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            LottoNumbers(listOf(1, 2, 2, 2, 1, 1).map(::LottoNumber))
+        }
+    }
+
+    @Test
+    fun `로또 번호가 6개가 아닌 경우 IllegalArgumentException 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            LottoNumbers(listOf(1, 2, 3, 4).map(::LottoNumber))
+        }
+    }
+}
