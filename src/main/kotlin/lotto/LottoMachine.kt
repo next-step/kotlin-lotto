85519e1ada76efdd125aa000cf1f84420d922e4b
gabstin <ksgilove0@gmail.com>
2022-05-30 12:13:26 +0900

    [step3] 리뷰 반영 - LottoMachine 당첨 여부 코드 제거

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -5,9 +5,6 @@ class LottoMachine {
     var lottoTickets = LottoTickets()
         private set
 
-    var winningPrizes = WinningPrizes()
-        private set
-
     fun purchase(purchaseMoney: PurchaseMoney, randomNumberFunc: () -> List<LottoNumber>) {
         val lottoTicketCount = purchaseMoney.money / LOTTO_PRICE
 
@@ -18,22 +15,7 @@ class LottoMachine {
         )
     }
 
-    fun checkResult(winningLotto: WinningLotto) {
-        winningPrizes = WinningPrizes(
-            lottoTickets.lottery.map {
-                val matchCount = winningLotto.numbers.intersect(it.numbers.toSet()).size
-                val matchBonus = matchCount == BONUS_LOTTO_NUMBER_COUNT && it.hasBonusNumber(winningLotto.bonusNumber)
-
-                LottoResult().check(
-                    matchCount = matchCount,
-                    matchBonus = matchBonus,
-                ).prize
-            }
-        )
-    }
-
     companion object {
         private const val LOTTO_PRICE = 1000
-        private const val BONUS_LOTTO_NUMBER_COUNT = 5
     }
 }
