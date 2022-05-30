61507104011cd4e8099005642374d5d85f5ec24d
Juice <3221ju@gmail.com>
2022-05-31 03:29:14 +0900

    feat: 오류 원인 메시지에 추가

--- src/main/kotlin/lotto/domain/Money.kt
+++ src/main/kotlin/lotto/domain/Money.kt
@@ -1,7 +1,8 @@
 package lotto.domain
 
-inline class Money(val value: Int) {
+@JvmInline
+value class Money(val value: Int) {
     init {
-        require(value >= 0) { "0 이상에 금액을 입력해주세요" }
+        require(value >= 0) { "$value 금액은 설정할수 없습니다. 0 이상에 금액을 입력해주세요" }
     }
 }
--- src/test/kotlin/lotto/domain/MoneyTest.kt
+++ src/test/kotlin/lotto/domain/MoneyTest.kt
@@ -13,6 +13,6 @@ class MoneyTest : DescribeSpec({
         // then
         shouldThrowExactly<IllegalArgumentException> {
             UserMoneyInputDto(moneyByString)
-        }.shouldHaveMessage("0 이상에 금액을 입력해주세요")
+        }.shouldHaveMessage("$moneyByString 금액은 설정할수 없습니다. 0 이상에 금액을 입력해주세요")
     }
 })
