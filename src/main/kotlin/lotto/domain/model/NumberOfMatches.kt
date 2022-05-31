a797e7c92a05b33c2ec88fb7917838c7fd312863
austen.lee <austen.lee@kakaocorp.com>
2022-05-31 23:54:32 +0900

    step3 refactor : 이제 필요 없는 isWin과 상수 제거

--- src/main/kotlin/lotto/domain/model/NumberOfMatches.kt
+++ src/main/kotlin/lotto/domain/model/NumberOfMatches.kt
@@ -2,16 +2,7 @@ package lotto.domain.model
 
 @JvmInline
 value class NumberOfMatches(val value: Int) {
-    fun isWin(): Boolean = value in WIN_NUMBER_RANGE
-
     override fun toString(): String {
         return value.toString()
-    }
-
-    companion object {
-        private const val MIN_MATCH_NUMBER = 3
-        private const val MAX_MATCH_NUMBER = Lotto.LOTTO_NUMBER_COUNT
-
-        val WIN_NUMBER_RANGE = (MIN_MATCH_NUMBER..MAX_MATCH_NUMBER)
     }
 }
