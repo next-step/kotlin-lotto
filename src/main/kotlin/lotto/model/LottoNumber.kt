ad2300e8d1467159f1cef4176853cea45bddfcb1
YooInKeun <keun0390@naver.com>
2022-05-31 10:11:58 +0900

    refactor: LottoNumber class, value class로 변경

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -1,29 +1,26 @@
 package lotto.model
 
-class LottoNumber private constructor(
+@JvmInline
+value class LottoNumber(
     val number: Int
 ) {
 
     init {
-        validateMinLottoNumber(number)
+        validateNumberRange(number)
     }
 
     companion object {
         private const val MIN_LOTTO_NUMBER = 1
         private const val MAX_LOTTO_NUMBER = 45
 
-        private val LOTTO_NUMBERS = List(MAX_LOTTO_NUMBER) {
+        val LOTTO_NUMBERS = List(MAX_LOTTO_NUMBER) {
             LottoNumber(it + MIN_LOTTO_NUMBER)
         }
 
-        fun from(number: Int): LottoNumber {
-            validateMinLottoNumber(number)
-
-            return LOTTO_NUMBERS[number - 1]
-        }
-
-        private fun validateMinLottoNumber(number: Int) {
-            require(number >= MIN_LOTTO_NUMBER) { "로또 숫자는 최소 $MIN_LOTTO_NUMBER 이상이어야 합니다. (number: $number)" }
+        private fun validateNumberRange(number: Int) {
+            require(number in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) {
+                "로또 숫자는 $MIN_LOTTO_NUMBER 이상 $MAX_LOTTO_NUMBER 이하이어야 합니다. (number: $number)"
+            }
         }
     }
 }
--- src/test/kotlin/lotto/model/LottoNumberTest.kt
+++ src/test/kotlin/lotto/model/LottoNumberTest.kt
@@ -1,9 +1,7 @@
 package lotto.model
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertSame
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -11,26 +9,21 @@ import org.junit.jupiter.params.provider.ValueSource
 @DisplayName("로또 숫자 테스트")
 class LottoNumberTest {
 
-    @Test
-    fun `로또 숫자가 1 미만이면 예외 발생`() {
-        // given, when, then
-        val exception = assertThrows<IllegalArgumentException> { LottoNumber.from(0) }
-        assertEquals("로또 숫자는 최소 1 이상이어야 합니다. (number: 0)", exception.message)
+    @ParameterizedTest
+    @ValueSource(ints = [0, 46])
+    fun `로또 숫자가 1 미만 또는 45 초과이면 예외 발생`(number: Int) {
+        // when, then
+        val exception = assertThrows<IllegalArgumentException> { LottoNumber(number) }
+        assertEquals("로또 숫자는 1 이상 45 이하이어야 합니다. (number: $number)", exception.message)
     }
 
     @ParameterizedTest
     @ValueSource(ints = [1, 45])
     fun `로또 숫자 정상 생성`(number: Int) {
         // when
-        val lottoNumber = LottoNumber.from(number)
+        val lottoNumber = LottoNumber(number)
 
         // then
         assertEquals(lottoNumber.number, number)
-    }
-
-    @Test
-    fun `로또 숫자 재사용`() {
-        // given, when, then
-        assertSame(LottoNumber.from(1), LottoNumber.from(1))
     }
 }
