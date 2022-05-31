0871f1266cb7816eeb9d7c1824fbe72210972e6e
YooInKeun <keun0390@naver.com>
2022-05-31 16:15:01 +0900

    chore: 한줄 함수 간결하게 작성

--- src/main/kotlin/lotto/model/RandomLottoGenerator.kt
+++ src/main/kotlin/lotto/model/RandomLottoGenerator.kt
@@ -1,11 +1,9 @@
 package lotto.model
 
+import lotto.model.Lotto.Companion.LOTTO_NUMBER_COUNT
+
 object RandomLottoGenerator {
-
-    private const val LOTTO_NUMBER_COUNT = 6
-
-    fun generate(): List<LottoNumber> =
-        LottoNumber.LOTTO_NUMBERS.shuffled()
-            .subList(0, LOTTO_NUMBER_COUNT)
-            .sortedBy { it.number }
+    fun generate() = LottoNumber.LOTTO_NUMBERS.shuffled()
+        .subList(0, LOTTO_NUMBER_COUNT)
+        .sortedBy { it.number }
 }
