d7173825030d369f56400883f882728c534a5ac7
YooInKeun <keun0390@naver.com>
2022-05-31 13:01:52 +0900

    refactor: 한줄 함수 간결하게 변경

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -17,10 +17,9 @@ value class LottoNumber(
             LottoNumber(it + MIN_LOTTO_NUMBER)
         }
 
-        private fun validateNumberRange(number: Int) {
+        private fun validateNumberRange(number: Int) =
             require(number in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) {
                 "로또 숫자는 $MIN_LOTTO_NUMBER 이상 $MAX_LOTTO_NUMBER 이하이어야 합니다. (number: $number)"
             }
-        }
     }
 }
