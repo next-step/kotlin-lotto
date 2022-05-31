a01bb13f95d76e7fc70918e8a777f715208160ae
YooInKeun <keun0390@naver.com>
2022-05-31 13:57:04 +0900

    chore: 중복되는 로또 번호로 생성시 예외 발생 추가

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -14,8 +14,14 @@ class Lotto(
 
     companion object {
         fun of(numbers: List<Int>): Lotto {
+            validateUnique(numbers)
+
             val lottoNumbers = numbers.map { LOTTO_NUMBERS[it - 1] }
             return Lotto(lottoNumbers)
+        }
+
+        private fun validateUnique(numbers: List<Int>) {
+            require(numbers.size == numbers.toSet().size) { "로또에 중복되는 번호가 있을 수 없습니다." }
         }
     }
 }
--- src/test/kotlin/lotto/model/LottoTest.kt
+++ src/test/kotlin/lotto/model/LottoTest.kt
@@ -3,6 +3,7 @@ package lotto.model
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 @DisplayName("로또 테스트")
 class LottoTest {
@@ -15,5 +16,12 @@ class LottoTest {
 
         // when, then
         assertEquals(lotto.findMatchedNumberCount(other), 3)
+    }
+
+    @Test
+    fun `중복되는 로또 번호가 있으면 예외 발생`() {
+        // given, when, then
+        val exception = assertThrows<IllegalArgumentException> { Lotto.of(listOf(1, 1, 2, 3, 4, 5)) }
+        assertEquals("로또에 중복되는 번호가 있을 수 없습니다.", exception.message)
     }
 }
