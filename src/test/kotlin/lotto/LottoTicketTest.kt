0c948611d7dc155a7dceb883b23ba290cf2b04eb
kwanguuuu <kwangyu326@gmail.com>
2022-06-01 13:16:17 +0900

    [step4] refactor: LottoTicketTest 리팩터링
    - 불필요 테스트 삭제 (로또 티켓의 번호가 1이상 45이하 숫자인지 검증한다 -> LottoNumber로 이동)
    - 메서드 레퍼런스 사용

--- src/test/kotlin/lotto/LottoTicketTest.kt
+++ src/test/kotlin/lotto/LottoTicketTest.kt
@@ -12,26 +12,13 @@ class LottoTicketTest {
 
     @Test
     fun `로또번호 매칭된 갯수를 확인한다`() {
-        val lottoTicket = LottoTicket.of(
-            listOf(
-                LottoNumber.of(1),
-                LottoNumber.of(1),
-                LottoNumber.of(1),
-                LottoNumber.of(1),
-                LottoNumber.of(1),
-                LottoNumber.of(1)
-            )
-        )
-        val otherTicket = LottoTicket.of(
-            listOf(
-                LottoNumber.of(1),
-                LottoNumber.of(1),
-                LottoNumber.of(1),
-                LottoNumber.of(1),
-                LottoNumber.of(1),
-                LottoNumber.of(1)
-            )
-        )
+        val lottoTicket = listOf(1, 2, 3, 4, 5, 6)
+            .map(LottoNumber::of)
+            .let(LottoTicket::of)
+
+        val otherTicket = listOf(4, 5, 6, 7, 8, 9)
+            .map(LottoNumber::of)
+            .let(LottoTicket::of)
 
         val matchCount = lottoTicket.matchCountWith(otherTicket)
         val expectedCount = 3
@@ -40,46 +27,31 @@ class LottoTicketTest {
     }
 
     @ParameterizedTest
-    @MethodSource("invalidRangeLottoNumbers")
-    fun `로또 티켓의 번호가 1이상 45이하 숫자인지 검증한다`(numbers: Set<LottoNumber>) {
-        assertThrows<IllegalArgumentException> { LottoTicket.of(numbers) }
-    }
-
-    @ParameterizedTest
     @MethodSource("invalidSizeLottoNumbers")
-    fun `로또 티켓의 번호 갯수가 6개인지 검증한다`(numbers: Set<LottoNumber>) {
+    fun `로또 티켓의 번호 갯수가 6개인지 검증한다`(numbers: List<LottoNumber>) {
         assertThrows<IllegalArgumentException> { LottoTicket.of(numbers) }
     }
 
     @ParameterizedTest
     @MethodSource("duplicatedLottoNumbers")
-    fun `로또에 중복된 번호가 있는지 확인한다`(numbers: Set<LottoNumber>) {
+    fun `로또에 중복된 번호가 있는지 확인한다`(numbers: List<LottoNumber>) {
         assertThrows<IllegalArgumentException> { LottoTicket.of(numbers) }
     }
 
     companion object {
         @JvmStatic
-        fun invalidRangeLottoNumbers(): Stream<Arguments> {
-            return Stream.of(
-                Arguments.of(listOf(1, 2, 3, 4, 5, 46)), // over 45
-                Arguments.of(listOf(0, 2, 3, 4, 5, 40)), // zero
-                Arguments.of(listOf(-1, 2, 3, 4, 5, 6)) // negative
-            )
-        }
-
-        @JvmStatic
         fun invalidSizeLottoNumbers(): Stream<Arguments> {
             return Stream.of(
-                Arguments.of(listOf(1, 2, 3, 4, 5)), // less than 6
-                Arguments.of(listOf(1, 2, 3, 4, 5, 6, 7)), // more than 6
+                Arguments.of(listOf(1, 2, 3, 4, 5).map(LottoNumber::of)), // less than 6
+                Arguments.of(listOf(1, 2, 3, 4, 5, 6, 7).map(LottoNumber::of)), // more than 6
             )
         }
 
         @JvmStatic
         fun duplicatedLottoNumbers(): Stream<Arguments> {
             return Stream.of(
-                Arguments.of(listOf(1, 2, 3, 4, 5, 5)), // 5 numbers
-                Arguments.of(listOf(1, 1, 1, 1, 1, 1)), // 1 numbers
+                Arguments.of(listOf(1, 2, 3, 4, 5, 5).map(LottoNumber::of)), // 5 numbers
+                Arguments.of(listOf(1, 1, 1, 1, 1, 1).map(LottoNumber::of)), // 1 numbers
             )
         }
     }
