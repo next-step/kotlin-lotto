37c1dba9234ce803f5328fb8adf35421054d8d8c
hoori <freedom09@inflab.com>
2022-06-02 21:11:47 +0900

    정규식 문자열 및 인덱스 상수화

--- src/main/kotlin/stringAddCalculator/splt.kt
+++ src/main/kotlin/stringAddCalculator/splt.kt
@@ -2,11 +2,14 @@ package stringAddCalculator
 
 private const val BASE_DELIMITER = ","
 private val DELIMITER_GROUP = "[$BASE_DELIMITER:]".toRegex()
+private const val CUSTOM_DELIMITER_INPUT = "//(.)\n(.*)"
+private const val CUSTOM_DELIMITER_INDEX = 1
+private const val CUSTOM_DELIMITER_TEXT_INDEX = 2
 
 internal fun split(text: String): List<String> {
-    Regex("//(.)\n(.*)").find(text)?.let {
-        val customDelimiter = it.groupValues[1]
-        val convertedText = it.groupValues[2].split(customDelimiter).joinToString(BASE_DELIMITER)
+    Regex(CUSTOM_DELIMITER_INPUT).find(text)?.let {
+        val customDelimiter = it.groupValues[CUSTOM_DELIMITER_INDEX]
+        val convertedText = it.groupValues[CUSTOM_DELIMITER_TEXT_INDEX].split(customDelimiter).joinToString(BASE_DELIMITER)
         return split(convertedText)
     }
 
