9a0e39d0339aea83acd0bd167dc49c9bdf7e1a24
Wave1994-Hoon <kk03187693@gmail.com>
2022-06-03 00:24:12 +0900

    [step3] 로또 매치 타입을 개선하라

--- src/main/kotlin/lotto/domain/LottoMatchType.kt
+++ src/main/kotlin/lotto/domain/LottoMatchType.kt
@@ -1,9 +1,9 @@
 package lotto.domain
 
-enum class LottoMatchType(val matchCount: Int, val isBonus: Boolean, val reward: Long) {
-    THREE(3, false, 5000),
-    FOUR(4, false, 50000),
-    FIVE(5, false, 1500000),
-    FIVE_BONUS(5, true, 30000000),
-    SIX(6, false, 2000000000)
+enum class LottoMatchType(val matchCount: Int, val reward: Long, val isBonus: Boolean = false) {
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIVE(5, 1_500_000),
+    FIVE_BONUS(5, 30_000_000, true),
+    SIX(6, 2_000_000_000)
 }
