78ac31c8d1c01f7ca5b0af02b4df4d80ce672b76
d000l <questknight26@gmail.com>
2022-06-02 21:46:29 +0900

    test : 테스트 데이타 하드코딩으로 변경

--- src/test/kotlin/lotto/LottoResultTest.kt
+++ src/test/kotlin/lotto/LottoResultTest.kt
@@ -18,7 +18,10 @@ class LottoResultTest {
     @Test
     fun `당첨된 로또의 수익률을 구한다`() {
         val result = LottoResult(Money(5000), listOf(Grade.Fourth, Grade.Fourth))
-        val expect = (Grade.Fourth.reward * 2) / 5000f
+
+        // 4등의 상금은 5_000 이다
+        // 기대 수익률은 5000 * 2 / 5000 => 2f 이다.
+        val expect = 2f
         Assertions.assertThat(result.revenueRate).isEqualTo(expect)
     }
 }
