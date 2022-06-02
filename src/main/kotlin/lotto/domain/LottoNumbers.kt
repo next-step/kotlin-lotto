bcb51507ca535ed2aea22b3501d8ab237ec83775
hyeon9mak <jinha3507@gmail.com>
2022-06-02 22:05:56 +0900

    feat: 일치하는 번호 개수 구하는 로직 구현

--- src/main/kotlin/lotto/domain/LottoNumbers.kt
+++ src/main/kotlin/lotto/domain/LottoNumbers.kt
@@ -1,12 +1,17 @@
 package lotto.domain
 
-class LottoNumbers private constructor(
+class LottoNumbers constructor(
     val values: Set<LottoNumber>,
 ) {
     init {
         require(this.values.size == NUMBERS_COUNT) {
             "로또의 번호 개수는 $NUMBERS_COUNT 여야 합니다."
         }
+    }
+
+    fun matchedNumberCount(other: LottoNumbers): Int {
+        return values.intersect(other = other.values)
+            .size
     }
 
     companion object {
--- src/test/kotlin/lotto/domain/LottoNumbersTest.kt
+++ src/test/kotlin/lotto/domain/LottoNumbersTest.kt
@@ -1,8 +1,11 @@
 package lotto.domain
 
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.data.row
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.ints.shouldBeLessThan
+import io.kotest.matchers.shouldBe
 
 internal class LottoNumbersTest : FreeSpec({
 
@@ -12,6 +15,25 @@ internal class LottoNumbersTest : FreeSpec({
 
         // then
         lottoNumbers.values.shouldHaveSize(6)
+    }
+
+    "로또 번호가 중복되는 경우 예외가 발생한다." {
+        // when, then
+        val exception = shouldThrowExactly<java.lang.IllegalArgumentException> {
+            LottoNumbers(
+                setOf(
+                    LottoNumber.from(1),
+                    LottoNumber.from(2),
+                    LottoNumber.from(3),
+                    LottoNumber.from(4),
+                    LottoNumber.from(5),
+                    LottoNumber.from(5),
+                )
+            )
+        }
+
+        // then
+        exception.message shouldBe "로또의 번호 개수는 6 여야 합니다."
     }
 
     "각각의 번호는 오름차순으로 정렬되어 있다." {
@@ -25,4 +47,27 @@ internal class LottoNumbersTest : FreeSpec({
             frontNumber.value.shouldBeLessThan(secondNumber.value)
         }
     }
+
+    "서로 일치하는 번호의 개수를 반환한다." - {
+        val winningNumbers = lottoNumbersFixture(setOf(1, 2, 3, 4, 5, 6))
+
+        listOf(
+            row(lottoNumbersFixture(setOf(11, 12, 13, 14, 15, 16)), 0),
+            row(lottoNumbersFixture(setOf(1, 12, 13, 14, 15, 16)), 1),
+            row(lottoNumbersFixture(setOf(1, 2, 13, 14, 15, 16)), 2),
+            row(lottoNumbersFixture(setOf(1, 2, 3, 14, 15, 16)), 3),
+            row(lottoNumbersFixture(setOf(1, 2, 3, 4, 15, 16)), 4),
+            row(lottoNumbersFixture(setOf(1, 2, 3, 4, 5, 16)), 5),
+            row(lottoNumbersFixture(setOf(1, 2, 3, 4, 5, 6)), 6),
+        ).forEach { (lottoNumbers, result) ->
+            "'${lottoNumbers.values}' 는 $result 개가 일치한다." {
+                winningNumbers.matchedNumberCount(other = lottoNumbers) shouldBe result
+            }
+        }
+    }
 })
+
+private fun lottoNumbersFixture(numberSet: Set<Int>): LottoNumbers = LottoNumbers(
+    numberSet.map(LottoNumber.Companion::from)
+        .toSet()
+)
