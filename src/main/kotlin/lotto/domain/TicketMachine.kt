cabbd18e9b3abd30edfe49553f899136e4d1de65
pplenty <newtype26@naver.com>
2022-06-02 23:28:20 +0900

    fix: 피드백반영 - 수동으로만 사는 경우(자동 0개인 경우) 살수 있도록 수정

--- src/main/kotlin/lotto/domain/TicketMachine.kt
+++ src/main/kotlin/lotto/domain/TicketMachine.kt
@@ -8,7 +8,7 @@ class TicketMachine(private val numberGenerator: NumberGenerator) {
 
     fun buy(amount: Int, manualTickets: List<LottoTicket>): List<LottoTicket> {
         val count = (amount / TICKET_PRICE) - manualTickets.size
-        require(count > 0) { "구매할 수 없습니다." }
+        require(count >= 0) { "구매할 수 없습니다." }
         return List(count) { LottoTicket(numberGenerator.randomTake(NUMBER_SIZE)) } + manualTickets
     }
 
