fc611973f6c645b0f012d167fe6731a7b3b6acf2
Wave1994-Hoon <kk03187693@gmail.com>
2022-06-03 00:34:26 +0900

    [step3] 로또 매칭 결과를 출력하는 로직을 개선하라

--- src/main/kotlin/lotto/presentation/OutboundView.kt
+++ src/main/kotlin/lotto/presentation/OutboundView.kt
@@ -11,9 +11,7 @@ class OutboundView {
         println("${lottoTickets.getLottoTickets().size} 개를 구매했습니다.")
 
         lottoTickets.getLottoTickets()
-            .forEach { ticket ->
-                println(ticket.numbers.joinToString(prefix = "[", postfix = "]"))
-            }
+            .forEach { println(it.numbers.joinToString(prefix = "[", postfix = "]")) }
     }
 
     fun printLottoMatchCount(lottoTickets: LottoTickets, winningNumber: LottoWinningNumber) {
@@ -21,23 +19,30 @@ class OutboundView {
         println("---------")
 
         LottoMatchType.values()
-            .forEach {
-                if (it.isBonus) {
-                    println(
-                        "${it.matchCount}개 일치, 보너스 볼 일치(${it.reward}원) - " +
-                            "${lottoTickets.getMatchCount(it, winningNumber)}개"
-                    )
-                } else {
-                    println(
-                        "${it.matchCount}개 일치 (${it.reward}원) - " +
-                            "${lottoTickets.getMatchCount(it, winningNumber)}개"
-                    )
-                }
-            }
+            .forEach { printMatchCountResult(it, lottoTickets, winningNumber) }
     }
 
     fun printRewardRate(lottoRewardCalculator: LottoRewardCalculator, purchaseAmount: Int, lottoTicketPrice: Int) {
         val profit = String.format("%.02f", lottoRewardCalculator.calculateRewardRate(purchaseAmount, lottoTicketPrice))
         println("총 수익률은 ${profit}입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)")
+    }
+
+    private fun printMatchCountResult(
+        lottoMatchType: LottoMatchType,
+        lottoTickets: LottoTickets,
+        winningNumber: LottoWinningNumber
+    ) {
+        if (lottoMatchType.isBonus) {
+            println(
+                "${lottoMatchType.matchCount}개 일치, 보너스 볼 일치(${lottoMatchType.reward}원) - " +
+                    "${lottoTickets.getMatchCount(lottoMatchType, winningNumber)}개"
+            )
+            return
+        }
+
+        println(
+            "${lottoMatchType.matchCount}개 일치 (${lottoMatchType.reward}원) - " +
+                "${lottoTickets.getMatchCount(lottoMatchType, winningNumber)}개"
+        )
     }
 }
