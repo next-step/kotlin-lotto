c58bccb230e063fe5baf836821265465e3f5343c
vivian.bae <vivian.bae@kakaocorp.com>
2022-06-03 16:45:56 +0900

    사용자가 수동으로 구매할 로또 번호를 입력하도록 유도하기

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,6 +3,7 @@ package lotto.view
 object InputView {
     private const val TOTAL_PAYMENT_QUESTION = "구입금액을 입력해 주세요."
     private const val NUMBER_OF_CUSTOM_LOTTO_QUESTION = "수동으로 구매할 로또 수를 입력해 주세요."
+    private const val CUSTOM_LOTTO_NUMBERS_QUESTION = "수동으로 구매할 번호를 입력해 주세요."
     private const val WINNING_NUMBERS_QUESTION = "지난 주 당첨 번호를 입력해 주세요."
     private const val BONUS_BALL_NUMBER_QUESTION = "보너스 볼을 입력해 주세요."
 
@@ -20,19 +21,29 @@ object InputView {
             .getPositiveNumber("수동으로 구매할 로또 수")
     }
 
+    fun readCustomLottoNumbers(numberOfCustomLotto: Int): List<List<Int>> {
+        println(CUSTOM_LOTTO_NUMBERS_QUESTION)
+
+        return (1..numberOfCustomLotto).map { readLottoNumbers("로또 번호") }
+    }
+
     fun readWinningNumbers(): List<Int> {
         println("\n$WINNING_NUMBERS_QUESTION")
 
-        return readln()
-            .split(",")
-            .map { it.getPositiveNumber("당첨 번호") }
-            .takeIfIntListHasSizeOfSix("당첨 번호")
+        return readLottoNumbers("당첨 번호")
     }
 
     fun readBonusBallNumber(): Int {
         println(BONUS_BALL_NUMBER_QUESTION)
 
         return readln().getPositiveNumber("보너스 볼 번호")
+    }
+
+    private fun readLottoNumbers(what: String? = null): List<Int> {
+        return readln()
+            .split(",")
+            .map { it.getPositiveNumber(what) }
+            .takeIfIntListHasSizeOfSix(what)
     }
 
     private fun String.getPositiveNumber(what: String? = null): Int = trim()
