89159ae96723f584ed97aa35c5a28d8f7449060e
baekseungyeol <beck33333@naver.com>
2022-06-04 00:57:08 +0900

    feat(LottoStore): 구입 금액이 0보다 적은 경우 IllegalArgumentException 예외가 발생하는 로직 추가

--- src/main/kotlin/lotto/LottoStore.kt
+++ src/main/kotlin/lotto/LottoStore.kt
@@ -3,10 +3,12 @@ package lotto
 class LottoStore {
 
     fun buy(money: Int): Int {
+        require(money >= ZERO_PRICE) { "금액이 음수가 될수는 없습니다." }
         return money / LOTTO_PRICE
     }
 
     companion object {
+        private const val ZERO_PRICE = 0
         private const val LOTTO_PRICE = 1000
     }
 }
--- src/test/kotlin/lotto/LottoStoreTest.kt
+++ src/test/kotlin/lotto/LottoStoreTest.kt
@@ -2,8 +2,10 @@ package lotto
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 class LottoStoreTest {
 
@@ -23,8 +25,14 @@ class LottoStoreTest {
         ],
         delimiter = '|'
     )
-    fun lottoShopCount(amount: Int, count: Int) {
+    fun `로또 구입 금액은 1장에 1000원으로 계산된 만큼의 로또가 발급된다`(amount: Int, count: Int) {
         val resultCount = lottoStore.buy(amount)
         assertThat(resultCount).isEqualTo(count)
+    }
+
+    @ParameterizedTest(name = "구입 금액이 {0} 인 음수일 경우 IllegalArgumentException 예외가 발생한다.")
+    @ValueSource(strings = ["-10000", "-5000"])
+    fun `구입 금액이 0보다 적은 경우 IllegalArgumentException 예외가 발생한다`(amount: Int) {
+        assertThrows<IllegalArgumentException> { lottoStore.buy(amount) }
     }
 }
