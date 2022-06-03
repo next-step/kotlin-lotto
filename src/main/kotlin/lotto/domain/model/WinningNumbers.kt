a00f27f9c44839eae351179916b1c81d0a728484
austen.lee <austen.lee@kakaocorp.com>
2022-06-04 00:42:28 +0900

    step3 refactor : 필요없는 검사 제거

--- src/main/kotlin/lotto/domain/model/WinningNumbers.kt
+++ src/main/kotlin/lotto/domain/model/WinningNumbers.kt
@@ -5,9 +5,6 @@ data class WinningNumbers(
     val bonusBall: LottoNumber
 ) {
     init {
-        require(numbers.numbers.size == Lotto.LOTTO_NUMBER_COUNT) {
-            MESSAGE_INVALID_WINNING_NUMBERS_SIZE
-        }
         require(bonusBall !in numbers.numbers) {
             MESSAGE_DUPLICATE_WINNING_NUMBERS
         }
@@ -16,7 +13,6 @@ data class WinningNumbers(
     operator fun contains(lottoNumber: LottoNumber): Boolean = lottoNumber in numbers.numbers
 
     companion object {
-        private const val MESSAGE_INVALID_WINNING_NUMBERS_SIZE = "당첨 번호는 6개가 입력되어야 합니다."
         private const val MESSAGE_DUPLICATE_WINNING_NUMBERS = "보너스 볼 번호는 당첨 번호와 중복될 수 없습니다."
 
         fun from(numbers: Lotto, bonusBall: LottoNumber): WinningNumbers {
