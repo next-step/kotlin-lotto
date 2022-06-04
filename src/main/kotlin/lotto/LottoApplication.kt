4c7527859de4ea2283fa2f6d9818499226270224
YooInKeun <keun0390@naver.com>
2022-06-05 00:08:31 +0900

    chore: 로또 기능을 어플리케이션으로 분리

---
+++ src/main/kotlin/lotto/LottoApplication.kt
@@ -0,0 +1,23 @@
+package lotto
+
+import lotto.model.Lotto
+import lotto.model.RandomLottoGenerator
+import lotto.view.InputView
+import lotto.view.ResultView
+
+object LottoApplication {
+    fun run() {
+        InputView.printPaymentPriceInputMessage()
+        val paymentPrice = InputView.inputPaymentPrice()
+
+        val purchasedLottos = RandomLottoGenerator.generateLottos(paymentPrice)
+        ResultView.printPurchasedLottoCount(purchasedLottos.size)
+        ResultView.printPurchasedLottos(purchasedLottos)
+
+        InputView.printWinningNumbersInputMessage()
+        val winningNumbers = Lotto(InputView.inputWinningNumbers())
+
+        val winningRanks = purchasedLottos.findRanks(winningNumbers)
+        ResultView.printWinningStatistics(paymentPrice, winningRanks)
+    }
+}
--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -1,21 +1,5 @@
 package lotto
 
-import lotto.model.Lotto
-import lotto.model.RandomLottoGenerator
-import lotto.view.InputView
-import lotto.view.ResultView
-
 fun main() {
-    InputView.printPaymentPriceInputMessage()
-    val paymentPrice = InputView.inputPaymentPrice()
-
-    val purchasedLottos = RandomLottoGenerator.generateLottos(paymentPrice)
-    ResultView.printPurchasedLottoCount(purchasedLottos.size)
-    ResultView.printPurchasedLottos(purchasedLottos)
-
-    InputView.printWinningNumbersInputMessage()
-    val winningNumbers = Lotto(InputView.inputWinningNumbers())
-
-    val winningRanks = purchasedLottos.findRanks(winningNumbers)
-    ResultView.printWinningStatistics(paymentPrice, winningRanks)
+    LottoApplication.run()
 }
