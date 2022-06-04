665a5b4fb1e93e77ac998cb4e781d7aab29b18c1
justin-s <justin.s@kakaocorp.com>
2022-06-05 00:12:48 +0900

    feat : manual lotto 테스트 코드 추가

--- src/test/kotlin/lotto/LottoMachineTest.kt
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 internal class LottoMachineTest {
 
     @Test
-    fun `1에서 45 숫자를 갖는 로또 객체 생성`() {
+    fun `1에서 45 숫자를 갖는 자동 로또 컬렉션 생성`() {
         val lottoMaker = object : LottoMaker {
             override fun make(lottos: List<LottoNumber>): Lotto {
                 val lottoNumbers = setOf(
@@ -23,6 +23,37 @@ internal class LottoMachineTest {
 
         val lottoMachine = LottoMachine(lottoMaker, lottoMaker)
         val buyLotto = lottoMachine.buyLotto(1)
-        assertThat(buyLotto[0].lotto.size).isEqualTo(6)
+        assertThat(buyLotto.size).isEqualTo(1)
+    }
+
+    @Test
+    fun `1에서 45 숫자를 갖는 수동 로또 컬렉션 생성`() {
+        val lottoMaker = object : LottoMaker {
+            override fun make(lottos: List<LottoNumber>): Lotto {
+                val lottoNumbers = setOf(
+                    LottoNumber(2),
+                    LottoNumber(3),
+                    LottoNumber(1),
+                    LottoNumber(4),
+                    LottoNumber(5),
+                    LottoNumber(6)
+                )
+                return Lotto(lottoNumbers)
+            }
+        }
+
+        val lottoMachine = LottoMachine(lottoMaker, lottoMaker)
+        val buyManualLotto = lottoMachine.buyManualLotto(listOf())
+
+        assertThat(buyManualLotto.lotto).containsAll(
+            setOf(
+                LottoNumber(2),
+                LottoNumber(3),
+                LottoNumber(1),
+                LottoNumber(4),
+                LottoNumber(5),
+                LottoNumber(6)
+            )
+        )
     }
 }
