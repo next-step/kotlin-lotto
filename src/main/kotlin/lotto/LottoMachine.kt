36b257c2666bc15d11e74244561aad5e5bc241f1
justin-s <justin.s@kakaocorp.com>
2022-06-04 23:07:02 +0900

    refactor : 변수명 및 린트 조정

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -2,23 +2,22 @@ package lotto
 
 class LottoMachine(private val autoLottoMaker: LottoMaker, private val manualLottoMaker: LottoMaker) {
 
-    private val lotto: MutableList<LottoNumber> = mutableListOf()
+    private val autoLotto: MutableList<LottoNumber> = mutableListOf()
     val lottos: Lottos = Lottos()
 
     init {
-        for (i in LOTTO_NUMBER_START..LOTTO_NUMBER_END) lotto.add(CachedLottoNumbers.getLottoNumber(i))
+        for (i in LOTTO_NUMBER_START..LOTTO_NUMBER_END) autoLotto.add(CachedLottoNumbers.getLottoNumber(i))
     }
 
-    fun buyLotto(count: Int){
+    fun buyLotto(count: Int) {
         repeat(count) {
-            lottos.add(autoLottoMaker.make(lotto.toList()))
+            lottos.add(autoLottoMaker.make(autoLotto.toList()))
         }
     }
 
     fun buyManualLotto(lotto: List<LottoNumber>) {
         lottos.add(manualLottoMaker.make(lotto))
     }
-
 
     companion object {
         private const val LOTTO_NUMBER_START = 1
