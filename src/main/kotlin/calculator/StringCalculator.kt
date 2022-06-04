8c4bd8eb79cd3764b56e82da9ab582b9c354ad82
Gomding <backchida@gmail.com>
2022-06-05 00:50:06 +0900

    [Step1] refactor(calculator): StringCalculator의 정적 팩터리에서 입력받은 값에따라 delimeter와 계산할 text를 가지도록 수정

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -1,42 +1,52 @@
 package calculator
 
-class StringCalculator {
-    fun add(text: String): Int {
-        if (text.isEmpty()) {
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+
+class StringCalculator private constructor(
+    private val delimiter: String = "[,:]",
+    private val text: String,
+) {
+    fun execute(): Int {
+        if (this.text.isEmpty()) {
             return 0
         }
 
-        if (text.toIntOrNull() != null) {
-            return text.toInt()
+        if (this.text.toIntOrNull() != null) {
+            return this.text.toInt()
         }
 
-        var delimiter = DEFAULT_DELIMITER
-        var _text = text
-
-        if (text.startsWith(CUSTOM_DELIMITER_PREFIX)) {
-            val result = CUSTOM_DELIMITER_REGEX.find(text)
-            result?.let { it ->
-                require(it.groupValues[CUSTOM_DELIMITER_INDEX].toIntOrNull() == null) { "구분자는 숫자로 지정할 수 없습니다." }
-                delimiter = it.groupValues[CUSTOM_DELIMITER_INDEX]
-                _text = it.groupValues[TEXT_INDEX]
-            }
-        }
-
-        return _text
+        return this.text
             .split(delimiter.toRegex())
             .sumOf {
                 val number = it.toIntOrNull()
-                    ?: throw IllegalArgumentException("계산할 값은 정수로 입력해야합니다. text: $text")
-                require(number >= 0) { "음수는 계산이 불가능합니다. 0이상의 정수를 입력해주세요.  text: $text" }
+                    ?: throw IllegalArgumentException("계산할 값은 정수로 입력해야합니다. text: ${this.text}")
+                require(number >= 0) { "음수는 계산이 불가능합니다. 0이상의 정수를 입력해주세요.  text: ${this.text}" }
                 number
             }
     }
 
     companion object {
-        const val DEFAULT_DELIMITER = "[,:]"
-        const val CUSTOM_DELIMITER_PREFIX = "//"
-        const val CUSTOM_DELIMITER_INDEX = 1
-        const val TEXT_INDEX = 2
-        val CUSTOM_DELIMITER_REGEX = Regex("//(.)\n(.*)")
+        private const val CUSTOM_DELIMITER_INDEX = 1
+        private const val TEXT_INDEX = 2
+        private val CUSTOM_DELIMITER_REGEX = Regex("//(.)\n(.*)")
+
+        fun of(text: String): StringCalculator {
+            val result = CUSTOM_DELIMITER_REGEX.find(text)
+            if (haveCustomDelimiter(result)) {
+                require(result.groupValues[CUSTOM_DELIMITER_INDEX].toIntOrNull() == null) { "구분자는 숫자로 지정할 수 없습니다." }
+                return StringCalculator(
+                    delimiter = result.groupValues[CUSTOM_DELIMITER_INDEX],
+                    text = result.groupValues[TEXT_INDEX],
+                )
+            }
+            return StringCalculator(text = text)
+        }
+
+        @OptIn(ExperimentalContracts::class)
+        private fun haveCustomDelimiter(result: MatchResult?): Boolean {
+            contract { returns(true) implies (result != null) }
+            return result != null
+        }
     }
 }
