5132db9aaf8ca14b2d297a50bc879d48c8c8a09b
eunseok.cheong <eunseok.cheong@webtoonscorp.com>
2022-06-06 03:46:05 +0900

    test: LottoNum.kt 테스트 통과하도록 함

--- src/test/kotlin/lotto/domain/LottoNumTest.kt
+++ src/test/kotlin/lotto/domain/LottoNumTest.kt
@@ -1,16 +1,25 @@
 package lotto.domain
 
-import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 class LottoNumTest {
     @ParameterizedTest
     @CsvSource(value = ["1,1", "2, 2", "44,44", "45,45"])
     fun `로또 숫자는 1~45 사이만 가능하다`(input: Int, expect: Int) {
+        val lottoNum = LottoNum(input)
+
+        assertThat(lottoNum.num).isEqualTo(expect)
     }
 
-    @Test
-    fun `로또 숫자가 잘못되면 예외를 던진다`() {
+    @ParameterizedTest
+    @ValueSource(ints = [0, -1, 46])
+    fun `로또 숫자가 잘못되면 예외를 던진다`(input: Int) {
+        assertThrows<IllegalArgumentException> {
+            LottoNum(input)
+        }
     }
 }
