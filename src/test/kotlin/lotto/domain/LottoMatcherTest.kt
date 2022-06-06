b8d4a70480adf0fdc2a925b5df6c32ab2b818690
yjna2316 <yjna2316@gmail.com>
2022-06-06 18:47:33 +0900

    fix: ktlint 적용 (빌드 깨짐 수정)

--- src/test/kotlin/lotto/domain/LottoMatcherTest.kt
+++ src/test/kotlin/lotto/domain/LottoMatcherTest.kt
@@ -1,19 +1,22 @@
 package lotto.domain
 
 import lotto.constant.WinningInfo
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class LottoMatcherTest {
 
     @Test
     fun `로또 당첨 결과기는 당첨 수에 따른 실제 당첨 개수 정보를 알 수 있다`() {
-        val lottoTickets = LottoTickets(3, listOf(
-            LottoTicket(listOf(1,2,33,34,35,36)),
-            LottoTicket(listOf(1,2,3,34,35,36)),
-            LottoTicket(listOf(1,2,3,4,35,36))
-        ))
-        val winningNumbers = WinningNumber(listOf(1,2,3,4,5,6))
+        val lottoTickets = LottoTickets(
+            3,
+            listOf(
+                LottoTicket(listOf(1, 2, 33, 34, 35, 36)),
+                LottoTicket(listOf(1, 2, 3, 34, 35, 36)),
+                LottoTicket(listOf(1, 2, 3, 4, 35, 36))
+            )
+        )
+        val winningNumbers = WinningNumber(listOf(1, 2, 3, 4, 5, 6))
         val lottoMatchResult = LottoMatcher().matchResult(lottoTickets, winningNumbers)
         val matchResultMap = lottoMatchResult.matchResult
         assertEquals(1, matchResultMap[WinningInfo.THREE])
--- src/test/kotlin/lotto/domain/LottoTicketTest.kt
+++ src/test/kotlin/lotto/domain/LottoTicketTest.kt
@@ -1,12 +1,9 @@
 package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.MethodSource
-import java.util.stream.Stream
 
 class LottoTicketTest {
     @ParameterizedTest
@@ -20,13 +17,13 @@ class LottoTicketTest {
     companion object {
         @JvmStatic
         fun lottoMatches() = listOf(
-            Arguments.of(listOf(1,2,3,4,5,6), listOf(1,2,3,4,5,6), 6),
-            Arguments.of(listOf(1,2,3,4,5,36), listOf(1,2,3,4,5,6), 5),
-            Arguments.of(listOf(1,2,3,4,25,36), listOf(1,2,3,4,5,6), 4),
-            Arguments.of(listOf(1,2,3,34,35,36), listOf(1,2,3,4,5,6), 3),
-            Arguments.of(listOf(1,2,33,34,35,36), listOf(1,2,3,4,5,6), 2),
-            Arguments.of(listOf(1,32,33,34,35,36), listOf(1,2,3,4,5,6), 1),
-            Arguments.of(listOf(31,32,33,34,35,36), listOf(1,2,3,4,5,6), 0),
+            Arguments.of(listOf(1, 2, 3, 4, 5, 6), listOf(1, 2, 3, 4, 5, 6), 6),
+            Arguments.of(listOf(1, 2, 3, 4, 5, 36), listOf(1, 2, 3, 4, 5, 6), 5),
+            Arguments.of(listOf(1, 2, 3, 4, 25, 36), listOf(1, 2, 3, 4, 5, 6), 4),
+            Arguments.of(listOf(1, 2, 3, 34, 35, 36), listOf(1, 2, 3, 4, 5, 6), 3),
+            Arguments.of(listOf(1, 2, 33, 34, 35, 36), listOf(1, 2, 3, 4, 5, 6), 2),
+            Arguments.of(listOf(1, 32, 33, 34, 35, 36), listOf(1, 2, 3, 4, 5, 6), 1),
+            Arguments.of(listOf(31, 32, 33, 34, 35, 36), listOf(1, 2, 3, 4, 5, 6), 0),
         )
     }
 }
