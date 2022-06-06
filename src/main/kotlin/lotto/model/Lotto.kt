ecdf78f1395b8ea3b7798315c7befb327e24e7e6
YooInKeun <keun0390@naver.com>
2022-06-06 22:04:02 +0900

    refactor: 로또 유효성 검증 함수, 동반 객체에서 분리

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -12,21 +12,19 @@ class Lotto(
     fun findRank(drawNumbers: DrawNumbers) =
         WinningRank.of(findMatchedNumberCount(drawNumbers.winningNumbers), contains(drawNumbers.bonusNumber))
 
-    private fun findMatchedNumberCount(other: Lotto) = other.lotto.count { lotto.contains(it) }
-
     fun contains(number: LottoNumber) = lotto.contains(number)
 
-    override fun toString(): String {
-        return "$lotto"
-    }
+    override fun toString() = "$lotto"
+
+    private fun findMatchedNumberCount(other: Lotto) = other.lotto.count { lotto.contains(it) }
+
+    private fun validateUnique(numbers: List<LottoNumber>) =
+        require(numbers.size == numbers.toSet().size) { "로또에 중복되는 번호가 있을 수 없습니다." }
+
+    private fun validateNumberCount(numbers: List<LottoNumber>) =
+        require(numbers.size == LOTTO_NUMBER_COUNT) { "로또 번호 개수는 ${LOTTO_NUMBER_COUNT}개 이어야 합니다." }
 
     companion object {
         const val LOTTO_NUMBER_COUNT = 6
-
-        private fun validateUnique(numbers: List<LottoNumber>) =
-            require(numbers.size == numbers.toSet().size) { "로또에 중복되는 번호가 있을 수 없습니다." }
-
-        private fun validateNumberCount(numbers: List<LottoNumber>) =
-            require(numbers.size == LOTTO_NUMBER_COUNT) { "로또 번호 개수는 ${LOTTO_NUMBER_COUNT}개 이어야 합니다." }
     }
 }
