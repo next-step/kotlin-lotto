d865c844ee55ce417987cef2eb64040d0ef1087a
baekseungyeol <beck33333@naver.com>
2022-06-07 00:40:19 +0900

    refactor(LottoRank) : LottoRank enum 클래스 변경

--- src/main/kotlin/lotto/model/LottoRank.kt
+++ src/main/kotlin/lotto/model/LottoRank.kt
@@ -1,18 +1,28 @@
 package lotto.model
-
-private const val RANK_MATCH_FIRST = 6
-private const val RANK_MATCH_SECOND = 5
-private const val RANK_MATCH_THIRD = 4
-private const val RANK_MATCH_FOURTH = 3
-
 enum class LottoRank(
     val price: Long,
     val matches: Int
 ) {
-    FIRST(2_000_000_000, RANK_MATCH_FIRST),
-    SECOND(1_500_000, RANK_MATCH_SECOND),
-    THIRD(50_000, RANK_MATCH_THIRD),
-    FOURTH(5_000, RANK_MATCH_FOURTH);
+
+    FIRST(
+        price = 2_000_000_000,
+        matches = 6
+    ),
+
+    SECOND(
+        price = 1_500_000,
+        matches = 5
+    ),
+
+    THIRD(
+        price = 50_000,
+        matches = 4
+    ),
+
+    FOURTH(
+        price = 5_000,
+        matches = 3
+    );
 
     companion object {
         fun find(matchCount: Int): LottoRank? = values().find { it.matches == matchCount }
