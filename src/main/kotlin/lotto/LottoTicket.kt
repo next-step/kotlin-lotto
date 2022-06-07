5b12a78af1a78f2950685a2d545f02ccc5546501
Gomding <backchida@gmail.com>
2022-06-08 08:58:17 +0900

    [Step2] feature(lotto): 로또 티켓이 가진 6자리 숫자는 중복을 허용하지 않는다.

--- src/main/kotlin/lotto/LottoTicket.kt
+++ src/main/kotlin/lotto/LottoTicket.kt
@@ -1,9 +1,11 @@
 package lotto
 
 @JvmInline
-value class LottoTicket(private val lottoNumbers: List<LottoNumber>) {
+value class LottoTicket(private val lottoNumbers: Set<LottoNumber>) {
     init {
-        require(this.lottoNumbers.size == LOTTO_NUMBERS_SIZE)
+        require(this.lottoNumbers.size == LOTTO_NUMBERS_SIZE) {
+            "로또 티켓은 중복되지 않은 6자리의 숫자들만 가질 수 있습니다. size: ${this.lottoNumbers.size}"
+        }
     }
 
     companion object {
--- src/test/kotlin/lotto/LottoTicketTest.kt
+++ src/test/kotlin/lotto/LottoTicketTest.kt
@@ -6,11 +6,28 @@ import io.kotest.core.spec.style.FunSpec
 internal class LottoTicketTest : FunSpec({
     test("6자리의 숫자가 아닌 경우 예외가 발생한다.") {
         // given
-        val lottoNumbers: List<LottoNumber> = listOf(
+        val lottoNumbers: Set<LottoNumber> = setOf(
             LottoNumber(1),
             LottoNumber(2),
             LottoNumber(3),
             LottoNumber(4),
+            LottoNumber(5),
+        )
+
+        // when then
+        shouldThrow<IllegalArgumentException> {
+            LottoTicket(lottoNumbers)
+        }
+    }
+
+    test("6자리 숫자는 중복을 허용하지 않는다.") {
+        // given
+        val lottoNumbers: Set<LottoNumber> = setOf(
+            LottoNumber(1),
+            LottoNumber(2),
+            LottoNumber(3),
+            LottoNumber(4),
+            LottoNumber(5),
             LottoNumber(5),
         )
 
