8a66d64c3400e40460effd7936880ebb8beb6e2d
Gomding <backchida@gmail.com>
2022-06-07 20:41:03 +0900

    [Step2] feature(lotto): 금액을 1000원으로 나눠서 로또 티켓의 수를 구할 수 있다.

--- src/main/kotlin/lotto/Money.kt
+++ src/main/kotlin/lotto/Money.kt
@@ -1,11 +1,17 @@
 package lotto
 
-private val LOTTO_PRICE: Money = Money(1_000)
+private val LOTTO_PRICE: Money = Money(value = 1000)
 
 @JvmInline
 value class Money(private val value: Int) {
 
     init {
         require(value >= 0) { "금액은 0이상의 정수여야 합니다." }
+    }
+
+    operator fun div(other: Money) = this.value / other.value
+
+    fun lottoTicketCount(): Int {
+        return this / LOTTO_PRICE
     }
 }
