73edd4fbeb7f0bf87ef68b8f7c2cf6e32c57af93
link-js <link.js@kakaocorp.com>
2022-06-08 14:51:08 +0900

    refactor : money클래스 value로 변경!

--- src/main/kotlin/lotto/domain/Money.kt
+++ src/main/kotlin/lotto/domain/Money.kt
@@ -6,7 +6,8 @@ import lotto.constants.ErrorMessages
  * 소지금을 저장하는 클래스
  * Created by Jaesungchi on 2022.06.08..
  */
-data class Money(var value: Int) {
+@JvmInline
+value class Money(val value: Int) {
     init {
         require(value >= 0) { ErrorMessages.MANUAL_LOTTO_COUNT_IS_OVER_MONEY }
     }
