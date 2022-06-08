15c072e84858006a07efc82e9432001537b61a1d
d000l <questknight26@gmail.com>
2022-06-08 20:02:21 +0900

    refactor : 함수,변수 타입 추가

--- src/main/kotlin/lotto/domain/LottoNumbers.kt
+++ src/main/kotlin/lotto/domain/LottoNumbers.kt
@@ -5,9 +5,9 @@ data class LottoNumbers(private val numbers: List<LottoNumber>) {
         require(numbers.size == 6 && numbers.toSet().size == 6) { "중복되지 않는 6개의 숫자가 있어야합니다." }
     }
 
-    fun matchCount(lotto: LottoNumbers) = lotto.numbers.count { it in numbers }
+    fun matchCount(lotto: LottoNumbers): Int = lotto.numbers.count { it in numbers }
 
-    fun isContain(lotto: LottoNumber) = lotto in numbers
+    fun isContain(lotto: LottoNumber): Boolean = lotto in numbers
 
     override fun toString(): String {
         return numbers.joinToString(", ") { it.number.toString() }
