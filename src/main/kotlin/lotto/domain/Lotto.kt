ca3ff4d8457b948e5fbfd1d78e297b0601791e4a
Rok93 <kr.kim@mfort.co.kr>
2022-06-09 01:58:49 +0900

    fix: 로또의 로또 숫자들이 오름차순으로 정렬되로록 변경

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,12 +3,15 @@ package lotto.domain
 private const val LOTTO_LENGTH = 6
 
 class Lotto(
-    val lottoNumbers: List<LottoNumber>,
+    lottoNumbers: List<LottoNumber>,
 ) {
+    val lottoNumbers: List<LottoNumber>
+
     init {
         val distinctLottoNumbers = lottoNumbers.distinct()
         require(lottoNumbers.size == LOTTO_LENGTH) { "로또는 6개의 로또 숫자를 가져야합니다." }
         require(distinctLottoNumbers.size == LOTTO_LENGTH) { "중복되는 로또 숫자를 가질 수 없습니다." }
+        this.lottoNumbers = lottoNumbers.sortedBy { it.value }
     }
 
     fun countSameLottoNumbers(other: Lotto): Int = lottoNumbers.count { lottoNumber -> other.contains(lottoNumber) }
