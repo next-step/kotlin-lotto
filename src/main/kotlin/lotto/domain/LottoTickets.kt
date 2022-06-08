3685b40aa696159dce0c3fff15be91eb2f0ca9c4
yjna2316 <yjna2316@gmail.com>
2022-06-08 21:20:45 +0900

    refac: 리뷰 반영 - Lottos 파일을 LottoTicket, LottoTickets로 분리

--- src/main/kotlin/lotto/domain/LottoTickets.kt
+++ src/main/kotlin/lotto/domain/LottoTickets.kt
@@ -2,22 +2,6 @@ package lotto.domain
 
 import lotto.constant.WinningInfo
 
-data class LottoTicket(
-    val lottoNumbers: List<Int>
-) {
-    init {
-        require(lottoNumbers.size == LOTTO_SIZE) { "로또 번호는 6개로 구성되어야 해요" }
-    }
-    fun matchCount(winningNumber: WinningNumber): Int {
-        return lottoNumbers.count { lottoNumber ->
-            winningNumber.winningNumbers.contains(lottoNumber)
-        }
-    }
-    companion object {
-        const val LOTTO_SIZE = 6
-    }
-}
-
 data class LottoTickets(
     val lottoCount: Int,
     val lottoTickets: List<LottoTicket>
