15f5ee0c53735ce59113e88a79e7ecb45e15d069
austen.lee <austen.lee@kakaocorp.com>
2022-06-10 01:25:45 +0900

    step4 refactor : LottoReceipt에 plus 추가

--- src/main/kotlin/lotto/domain/model/LottoReceipt.kt
+++ src/main/kotlin/lotto/domain/model/LottoReceipt.kt
@@ -4,4 +4,12 @@ data class LottoReceipt(
     val manualLottoCount: PurchaseCount,
     val automaticLottoCount: PurchaseCount,
     val lottos: Lottos
-)
+) {
+    operator fun plus(other: LottoReceipt): LottoReceipt {
+        return LottoReceipt(
+            manualLottoCount + other.manualLottoCount,
+            automaticLottoCount + other.automaticLottoCount,
+            lottos + other.lottos
+        )
+    }
+}
--- src/test/kotlin/lotto/domain/model/LottoReceiptTest.kt
+++ src/test/kotlin/lotto/domain/model/LottoReceiptTest.kt
@@ -19,4 +19,27 @@ class LottoReceiptTest {
             { assertThat(lottoReceipt.lottos).isEqualTo(lottos) }
         )
     }
+
+    @Test
+    fun `더하기 연산자를 통해 두 LottoReceipt를 더할 수 있다`() {
+        val manualLottoReceipt = LottoReceipt(
+            manualLottoCount = PurchaseCount.from(3),
+            automaticLottoCount = PurchaseCount.zero(),
+            lottos = Lottos.of(PurchaseCount.from(3), RangeLottoFactory(LottoNumber.LOTTO_NUMBER_RANGE))
+        )
+        val automaticLottoReceipt = LottoReceipt(
+            manualLottoCount = PurchaseCount.zero(),
+            automaticLottoCount = PurchaseCount.from(4),
+            lottos = Lottos.of(PurchaseCount.from(4), RangeLottoFactory(LottoNumber.LOTTO_NUMBER_RANGE))
+        )
+
+        val lottoReceipt = manualLottoReceipt + automaticLottoReceipt
+        assertAll(
+            { assertThat(lottoReceipt.manualLottoCount.value).isEqualTo(3) },
+            { assertThat(lottoReceipt.automaticLottoCount.value).isEqualTo(4) },
+            { assertThat(lottoReceipt.lottos.value.size).isEqualTo(7) },
+            { assertThat(lottoReceipt.lottos.value).containsAll(manualLottoReceipt.lottos.value) },
+            { assertThat(lottoReceipt.lottos.value).containsAll(automaticLottoReceipt.lottos.value) }
+        )
+    }
 }
