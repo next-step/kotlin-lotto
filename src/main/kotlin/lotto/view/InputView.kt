97c3440a2b2b6fba513b6668c0fa22458512dd4e
austen.lee <austen.lee@kakaocorp.com>
2022-06-09 23:37:01 +0900

    step4 feat : InputView에 단순 프린트 기능 추가

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,6 +3,10 @@ package lotto.view
 import lotto.domain.model.UserInputRequest
 
 object InputView {
+    fun println(message: String) {
+        println(message)
+    }
+
     fun <T> receiveUserInput(userInputRequest: UserInputRequest<T>): T {
         println(userInputRequest.message)
         return userInputRequest.inputConverter.convert(readln())
