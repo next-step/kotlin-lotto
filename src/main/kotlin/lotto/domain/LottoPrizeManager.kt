d7742bbd52c8d5644fda05ff8ada10c1f938ce52
juice119 <juyeol@inflearn.com>
2022-06-09 21:07:53 +0900

    feat: LottoRank 추가

--- src/main/kotlin/lotto/domain/LottoPrizeManager.kt
+++ src/main/kotlin/lotto/domain/LottoPrizeManager.kt
@@ -3,30 +3,17 @@ package lotto.domain
 import lotto.dto.WinningStatDto
 
 class LottoPrizeManager {
-    private val lottoPrizePolices: MutableList<LottoPrizePolicy> = mutableListOf()
-    val polices
-        get() = lottoPrizePolices.toList()
-
-    fun addUniquePolicy(prizePolicy: LottoPrizePolicy) {
-        validateUniqueItem(prizePolicy)
-        lottoPrizePolices.add(prizePolicy)
-    }
+    private val lottoRanks: List<LottoRank> = LottoRank.values().toList()
 
     fun getWinningStats(
         lottoTickets: List<LottoTicket>,
         winningLottoNumbers: WinningLottoNumbers
     ): List<WinningStatDto> {
-        return lottoPrizePolices.map { lottoPrizePolicy ->
+        return lottoRanks.map { lottoRank ->
             WinningStatDto(
-                lottoPrizePolicy,
-                lottoTickets.count { lottoTicket -> lottoPrizePolicy.isWon(lottoTicket, winningLottoNumbers) }
+                lottoRank.prizePolicy,
+                lottoTickets.count { lottoTicket -> lottoRank.prizePolicy.isWon(lottoTicket, winningLottoNumbers) }
             )
-        }
-    }
-
-    private fun validateUniqueItem(prizePolicy: LottoPrizePolicy) {
-        lottoPrizePolices.forEach {
-            require(it != prizePolicy) { "동일한 당첨 정책이 존재합니다" }
         }
     }
 }
