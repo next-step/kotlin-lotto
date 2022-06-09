8981174756e7890a318ec3e9021468cbb166946b
austen.lee <austen.lee@kakaocorp.com>
2022-06-10 00:42:12 +0900

    step4 feat : Money에 빼기 연산 추가

--- src/main/kotlin/lotto/domain/model/Money.kt
+++ src/main/kotlin/lotto/domain/model/Money.kt
@@ -2,6 +2,10 @@ package lotto.domain.model
 
 @JvmInline
 value class Money private constructor(val value: Int) {
+    operator fun minus(other: Money): Money {
+        return from(value - other.value)
+    }
+
     companion object {
         fun from(value: Int): Money {
             return Money(value.coerceAtLeast(0))
--- src/test/kotlin/lotto/domain/model/MoneyTest.kt
+++ src/test/kotlin/lotto/domain/model/MoneyTest.kt
@@ -1,6 +1,7 @@
 package lotto.domain.model
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -15,5 +16,13 @@ class MoneyTest {
     @ValueSource(ints = [-1, 100, -100000, -30, 4000000])
     fun `0보다 작은 값을 전달받을 경우, 0으로 저장한다`(input: Int) {
         assertThat(Money.from(input).value).isGreaterThanOrEqualTo(0)
+    }
+
+    @Test
+    fun `빼기 연산자를 사용해 빼기 연산을 수행할 수 있다`() {
+        val count1 = Money.from(10)
+        val count2 = Money.from(5)
+
+        assertThat(count1 - count2).isEqualTo(Money.from(5))
     }
 }
