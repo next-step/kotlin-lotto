b38a9740b10826cd2c6ad992c5976fb7a229bc86
Wave1994-Hoon <kk03187693@gmail.com>
2022-06-09 22:04:46 +0900

    [step4] LottoTickets 의 init 조건을 제거하라

--- src/main/kotlin/lotto/domain/LottoTickets.kt
+++ src/main/kotlin/lotto/domain/LottoTickets.kt
@@ -3,11 +3,6 @@ package lotto.domain
 data class LottoTickets(
     val lottoTickets: List<LottoTicket>
 ) : List<LottoTicket> by lottoTickets {
-    init {
-        require(lottoTickets.isNotEmpty()) {
-            "로또 티켓 개수가 1개 이상이여야 합니다."
-        }
-    }
 
     fun getMatchCount(lottoRank: LottoRank, winningNumbers: LottoWinningNumber): Int {
         return lottoTickets.count() { lottoTicket -> lottoTicket.isMatch(lottoRank, winningNumbers) }
