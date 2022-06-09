4dbb732640336a8d01d54c811e1e9426bb6bd9c5
austen.lee <austen.lee@kakaocorp.com>
2022-06-09 23:23:40 +0900

    step4 feat : 팩토리 메서드 하나 추가하고 테스트 세분화

--- src/main/kotlin/lotto/domain/model/PurchaseCount.kt
+++ src/main/kotlin/lotto/domain/model/PurchaseCount.kt
@@ -3,8 +3,12 @@ package lotto.domain.model
 @JvmInline
 value class PurchaseCount private constructor(val value: Int) {
     companion object {
+        fun from(value: Int): PurchaseCount {
+            return PurchaseCount(value.coerceAtLeast(0))
+        }
+
         fun of(purchaseAmount: Money, price: Money): PurchaseCount {
-            return PurchaseCount(purchaseAmount.value / price.value)
+            return PurchaseCount((purchaseAmount.value / price.value).coerceAtLeast(0))
         }
     }
 }
--- src/test/kotlin/lotto/domain/model/PurchaseCountTest.kt
+++ src/test/kotlin/lotto/domain/model/PurchaseCountTest.kt
@@ -3,13 +3,30 @@ package lotto.domain.model
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 class PurchaseCountTest {
     private val price = Money.from(1_000)
 
     @ParameterizedTest
+    @ValueSource(ints = [1, 100, 30])
+    fun `PurchaseCount는 로또 구입 개수를 나타내며, Int를 보관한다`(input: Int) {
+        val purchaseCount = PurchaseCount.from(input)
+
+        assertThat(purchaseCount.value).isEqualTo(input)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, -100, 30, -5000])
+    fun `0보다 작은 값을 전달받을 경우, 0으로 저장한다`(input: Int) {
+        val purchaseCount = PurchaseCount.from(input)
+
+        assertThat(purchaseCount.value).isGreaterThanOrEqualTo(0)
+    }
+
+    @ParameterizedTest
     @CsvSource(value = ["10000,10", "11500,11", "2432,2", "500,0"], delimiter = ',')
-    fun `PurchaseCount는 로또 구입 개수를 나타내며, Int를 보관한다`(purchaseAmount: Int, expected: Int) {
+    fun `총 구입 금액과 가격을 받아 PurchaseCount를 얻을 수 있다`(purchaseAmount: Int, expected: Int) {
         val purchaseCount = PurchaseCount.of(
             purchaseAmount = Money.from(purchaseAmount),
             price = price
