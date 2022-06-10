2e09215a04f73afb61af2dc78c291cf0ef597cea
baekseungyeol <beck33333@naver.com>
2022-06-11 01:52:57 +0900

    refactor(LottoRank) : LottoRank 기본값 설정

--- src/main/kotlin/lotto/model/LottoRank.kt
+++ src/main/kotlin/lotto/model/LottoRank.kt
@@ -3,13 +3,12 @@ package lotto.model
 enum class LottoRank(
     val price: Long,
     val matches: Int,
-    val bonusMatches: Boolean
+    val bonusMatches: Boolean = false
 ) {
 
     FIRST(
         price = 2_000_000_000,
-        matches = 6,
-        bonusMatches = false
+        matches = 6
     ),
 
     SECOND(
@@ -20,20 +19,17 @@ enum class LottoRank(
 
     THIRD(
         price = 1_500_000,
-        matches = 5,
-        bonusMatches = false
+        matches = 5
     ),
 
     FOURTH(
         price = 50_000,
-        matches = 4,
-        bonusMatches = false
+        matches = 4
     ),
 
     FIFTH(
         price = 5_000,
-        matches = 3,
-        bonusMatches = false
+        matches = 3
     );
 
     companion object {
