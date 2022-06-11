1f81f3f9fe3e6c5478bdada6733c5ad10869374b
YooInKeun <keun0390@naver.com>
2022-06-11 14:14:27 +0900

    refactor: sublist 생성시 take 함수 사용하도록 변경

--- src/main/kotlin/lotto/model/RandomLottoGenerating.kt
+++ src/main/kotlin/lotto/model/RandomLottoGenerating.kt
@@ -8,7 +8,7 @@ object RandomLottoGenerating : LottoGeneratingStrategy {
 
     private fun generateRandomLotto(): Lotto {
         val shuffledNumbers = LottoNumber.LOTTO_NUMBERS.shuffled()
-            .subList(0, Lotto.LOTTO_NUMBER_COUNT)
+            .take(Lotto.LOTTO_NUMBER_COUNT)
             .sortedBy { it.number }
 
         return Lotto(shuffledNumbers)
