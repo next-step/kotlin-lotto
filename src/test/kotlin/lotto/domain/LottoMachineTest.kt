a4a429289135555ad777a94a90311cd5b0227367
justin.sg <justin.sg@kakaocorp.com>
2022-06-11 22:47:10 +0900

    refactor : 미사용 코드 제거

--- src/test/kotlin/lotto/domain/LottoMachineTest.kt
+++ src/test/kotlin/lotto/domain/LottoMachineTest.kt
@@ -10,7 +10,6 @@ internal class LottoMachineTest {
 
     @Test
     fun `로또 기계에 보너스 값을 전달한다`() {
-        val lottoNumbers = (1..6).map { LottoNumber(it) }
         val lottoTickets = LottoMachine().purchase(
             money = Money(1000),
             manualTickets = LottoTickets(),
