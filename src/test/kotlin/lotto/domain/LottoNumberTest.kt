abd34e46d7383815c0d9e5569c442e4c6c5b88b6
justin.sg <justin.sg@kakaocorp.com>
2022-06-11 18:26:24 +0900

    refactor : 불필요한 listOf 제거

--- src/test/kotlin/lotto/domain/LottoNumberTest.kt
+++ src/test/kotlin/lotto/domain/LottoNumberTest.kt
@@ -7,11 +7,11 @@ internal class LottoNumberTest {
 
     @Test
     fun `0 보다 작은 숫자가 포함된 경우 에러 발생 확인`() {
-        assertThrows<IllegalArgumentException> { listOf(1, 2, 3, 4, 5, -1).map { LottoNumber(it) } }
+        assertThrows<IllegalArgumentException> { LottoNumber(-1) }
     }
 
     @Test
     fun `0의 숫자가 포함된 경우 에러 발생 확인`() {
-        assertThrows<IllegalArgumentException> { listOf(1, 2, 3, 4, 5, 0).map { LottoNumber(it) } }
+        assertThrows<IllegalArgumentException> { LottoNumber(0) }
     }
 }
