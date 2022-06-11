7c77f2f1526aa5796e6990ea6472ded01db69d51
justin.sg <justin.sg@kakaocorp.com>
2022-06-11 22:50:36 +0900

    refactor : 연관성 없는 테스트코드 제거

--- src/test/kotlin/lotto/domain/LottoMachineTest.kt
+++ src/test/kotlin/lotto/domain/LottoMachineTest.kt
@@ -20,28 +20,6 @@ internal class LottoMachineTest {
     }
 
     @Test
-    fun `로또 Prize을 확인하기 위하여 당첨 번호 일치 여부를 전달한다`() {
-        val lottoTickets = LottoMachine().purchase(
-            money = Money(1000),
-            manualTickets = LottoTickets(),
-            randomNumberFunc = randomNumberFunc
-        )
-
-        assertThat(lottoTickets.autoTickets.ticketSize).isEqualTo(1)
-    }
-
-    @Test
-    fun `보너스 번호는 없으며 당첨번호 3개로 구성된 내용을 확인한다`() {
-        val lottoTickets = LottoMachine().purchase(
-            money = Money(1000),
-            manualTickets = LottoTickets(),
-            randomNumberFunc = randomNumberFunc
-        )
-
-        assertThat(lottoTickets.autoTickets.ticketSize).isEqualTo(1)
-    }
-
-    @Test
     fun `로또 기계는 티켓 구매시 수동 티켓을 제외한 차액만큼 자동 티켓을 생성한다`() {
         val purchase = LottoMachine().purchase(
             money = Money(3000),
