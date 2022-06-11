ecae8f322173a86f8a36fc9abd08a34a8bf3b41a
Yohan <yohan@inflab.com>
2022-06-11 12:11:38 +0900

    feat: Money 객체 생성

---
+++ src/main/kotlin/lotto/vo/Money.kt
@@ -0,0 +1,12 @@
+package lotto.vo
+
+data class Money(private val value: Int) {
+
+    init {
+        require(0 < value) { "금액은 0 또는 음수를 가질 수 없다." }
+    }
+
+    fun sub(other: Money): Money {
+        return Money(value - other.value)
+    }
+}
---
+++ src/test/kotlin/lotto/vo/MoneyTest.kt
@@ -0,0 +1,36 @@
+package lotto.vo
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+internal class MoneyTest : StringSpec({
+
+    "금액 객체는 음수를 가질 수 없다." {
+        val value = -1
+
+        val throwableAction = {
+            Money(value)
+        }
+
+        shouldThrow<IllegalArgumentException>(throwableAction)
+    }
+
+    "금액 객체는 차액을 가진다." {
+        val money = Money(1000)
+
+        val result = money.sub(Money(500))
+
+        result shouldBe Money(500)
+    }
+
+    "금액 객체 차액시 금액이 부족한 경우 예외를 발생한다." {
+        val money = Money(100)
+
+        val throwableAction = {
+            money.sub(Money(101))
+        }
+
+        shouldThrow<IllegalArgumentException>(throwableAction)
+    }
+})
