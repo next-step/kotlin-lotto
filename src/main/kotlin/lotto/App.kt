baec95caaf11d2ef405e42cc664f0e29a701e02c
justin.sg <justin.sg@kakaocorp.com>
2022-06-11 20:32:33 +0900

    refactor : 지급 금액, 수동 로또 숫자 체크로직 추가

--- src/main/kotlin/lotto/App.kt
+++ src/main/kotlin/lotto/App.kt
@@ -6,10 +6,16 @@ import lotto.domain.LottoWinning
 import lotto.view.InputView
 import lotto.view.ResultView
 
+private const val INVALID_MANUAL_TICKET_COUNT = "지급한 금액보다 수동 로또 티켓이 클수는 없습니다."
+
 fun main() {
     val money = InputView.getMoney()
 
     val manualTicketCount = InputView.getManualTicketCount()
+
+    require(money.lottoCount >= manualTicketCount) {
+        INVALID_MANUAL_TICKET_COUNT
+    }
 
     val manualNumbers = InputView.getManualNumbers(manualTicketCount)
 
