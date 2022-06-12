9bc5ac8c7821681bf8d6d726442ef497ab91476c
austen.lee <austen.lee@kakaocorp.com>
2022-06-12 22:22:45 +0900

    step4 refactor : private constructor로 놓으면 validation check를 할 필요가 없음

--- src/main/kotlin/lotto/domain/model/LottoResult.kt
+++ src/main/kotlin/lotto/domain/model/LottoResult.kt
@@ -1,20 +1,7 @@
 package lotto.domain.model
 
 @JvmInline
-value class LottoResult(val value: List<LottoWinning>) {
-    init {
-        require(isValidResult()) {
-            MESSAGE_INVALID_LOTTO_RESULT
-        }
-    }
-
-    private fun isValidResult(): Boolean {
-        val lottoRanks = LottoRank.winnerPlace()
-        return value.size == lottoRanks.size && lottoRanks.all { rank ->
-            rank in this
-        }
-    }
-
+value class LottoResult private constructor(val value: List<LottoWinning>) {
     private operator fun contains(rank: LottoRank): Boolean = value.find { it.rank == rank } != null
 
     operator fun get(rank: LottoRank): LottoWinning = value.first { it.rank == rank }
@@ -24,8 +11,6 @@ value class LottoResult(val value: List<LottoWinning>) {
     }
 
     companion object {
-        private const val MESSAGE_INVALID_LOTTO_RESULT = "LottoResult는 1, 2, 3, 4, 5등 당첨 결과를 모두 포함하고 있어야 합니다."
-
         fun from(lottoWinningMap: Map<LottoRank, Int>): LottoResult {
             fun getCount(lottoRank: LottoRank): Int {
                 return lottoWinningMap[lottoRank] ?: 0
--- src/test/kotlin/lotto/domain/LottoLottoYieldCalculatorTest.kt
+++ src/test/kotlin/lotto/domain/LottoLottoYieldCalculatorTest.kt
@@ -2,7 +2,6 @@ package lotto.domain
 
 import lotto.domain.model.LottoRank
 import lotto.domain.model.LottoResult
-import lotto.domain.model.LottoWinning
 import lotto.domain.model.Money
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -10,15 +9,13 @@ import org.junit.jupiter.api.Test
 class LottoLottoYieldCalculatorTest {
     @Test
     fun `calculate를 통해 lottoResult와 구입 금액을 받아 수익률을 계산할 수 있다`() {
-        val lottoResult = LottoResult(
-            listOf(
-                LottoWinning(LottoRank.FIFTH, 2),
-                LottoWinning(LottoRank.FOURTH, 2),
-                LottoWinning(LottoRank.THIRD, 1),
-                LottoWinning(LottoRank.SECOND, 0),
-                LottoWinning(LottoRank.FIRST, 0)
-            )
+        val lottoWinnings = mapOf(
+            LottoRank.FIFTH to 2,
+            LottoRank.FOURTH to 2,
+            LottoRank.THIRD to 1
         )
+
+        val lottoResult = LottoResult.from(lottoWinnings)
         val purchaseAmount = Money.from(13000)
 
         val expected = lottoResult.getTotalEarns().toDouble() / purchaseAmount.value.toDouble()
