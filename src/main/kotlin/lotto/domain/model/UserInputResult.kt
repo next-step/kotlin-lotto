4d354c170e1cb208f2cf981d89a452f81205d6cd
austen.lee <austen.lee@kakaocorp.com>
2022-06-14 00:06:30 +0900

    step4 feat : 캐스팅 생략하고 값을 가져올 수 있는 value() 추가

--- src/main/kotlin/lotto/domain/model/UserInputResult.kt
+++ src/main/kotlin/lotto/domain/model/UserInputResult.kt
@@ -4,3 +4,7 @@ sealed interface UserInputResult<out T> {
     data class Success<out T>(val value: T) : UserInputResult<T>
     object Failed : UserInputResult<Nothing>
 }
+
+fun <T> UserInputResult<T>.value(): T {
+    return (this as UserInputResult.Success).value
+}
