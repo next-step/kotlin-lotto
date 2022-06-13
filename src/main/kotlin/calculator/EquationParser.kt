c66b377d221ad0720d61549cf4adbcf04e5ff4e7
김태헌 <dd.kim@woowahan.com>
2022-06-13 20:09:20 +0900

    refactor: 리뷰 반영

--- src/main/kotlin/calculator/EquationParser.kt
+++ src/main/kotlin/calculator/EquationParser.kt
@@ -1,5 +1,9 @@
 package calculator
 
+private const val CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)"
+private const val CUSTOM_DELIMITER_INDEX = 1
+private const val ORIGIN_EQUATION_INDEX = 2
+
 object EquationParser {
 
     fun parse(input: String?): List<Operand> {
@@ -12,8 +16,11 @@ object EquationParser {
     }
 
     private fun parseDelimiter(input: String): Pair<Equation, List<Delimiter>> {
-        val matchResult = Regex("//(.)\n(.*)").find(input) ?: return Pair(Equation(input), Delimiter.DEFAULT)
+        val matchResult = Regex(CUSTOM_DELIMITER_REGEX).find(input) ?: return Pair(Equation(input), Delimiter.DEFAULT)
 
-        return Pair(Equation(matchResult.groupValues[2]), listOf(Delimiter(matchResult.groupValues[1])))
+        return Pair(
+            Equation(matchResult.groupValues[ORIGIN_EQUATION_INDEX]),
+            listOf(Delimiter(matchResult.groupValues[CUSTOM_DELIMITER_INDEX]))
+        )
     }
 }
--- src/main/kotlin/calculator/Operand.kt
+++ src/main/kotlin/calculator/Operand.kt
@@ -19,7 +19,7 @@ value class Operand private constructor(
             val operand = input.trim().toIntOrNull() ?: throw RuntimeException("피연산자는 숫자만 입력하실 수 있습니다.")
 
             if (operand.isNegativeNumber()) {
-                throw RuntimeException("피연산자는 0이상의 수만 입력하실 수 있습니다.")
+                throw IllegalArgumentException("피연산자는 0이상의 수만 입력하실 수 있습니다.")
             }
 
             return Operand(operand)
