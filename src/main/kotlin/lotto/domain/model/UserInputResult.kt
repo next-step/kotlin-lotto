56ae61733081781fec3e137f055c19fcfa305faf
austen.lee <austen.lee@kakaocorp.com>
2022-06-14 00:18:01 +0900

    step4 refactor : 네이밍 value -> result

--- src/main/kotlin/lotto/domain/model/UserInputResult.kt
+++ src/main/kotlin/lotto/domain/model/UserInputResult.kt
@@ -1,9 +1,9 @@
 package lotto.domain.model
 
 sealed interface UserInputResult<out T> {
-    data class Success<out T>(val value: T) : UserInputResult<T>
+    data class Success<out T>(val result: T) : UserInputResult<T>
     object Failed : UserInputResult<Nothing>
 }
 
-val <T> UserInputResult<T>.value: T
-    get() = (this as UserInputResult.Success).value
+val <T> UserInputResult<T>.result: T
+    get() = (this as UserInputResult.Success).result
