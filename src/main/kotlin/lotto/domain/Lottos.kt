2f896fb825e5455b902d8c5fb6e7541d854c671b
Rok93 <kr.kim@mfort.co.kr>
2022-06-13 22:51:22 +0900

    refactor: Lottos의 로또 List를 표현하는 필드 하나로 통일

--- src/main/kotlin/lotto/domain/Lottos.kt
+++ src/main/kotlin/lotto/domain/Lottos.kt
@@ -1,14 +1,12 @@
 package lotto.domain
 
 class Lottos(
-    private val _lottos: List<Lotto>,
+    val lottos: List<Lotto>,
 ) {
-    val lottos: List<Lotto>
-        get() = _lottos.toList()
 
     fun getStatistics(winningLotto: WinningLotto): LottoResult {
         return Rank.toResult().apply {
-            val result = _lottos.map { winningLotto.match(it) }
+            val result = lottos.map { winningLotto.match(it) }
             result.forEach { put(it, get(it)!! + 1) }
             remove(Rank.NONE)
         }.let { LottoResult(it) }
