dd1204cdd6ab558770f2e65f0f42d9b63c67fc12
hyeon9mak <jinha3507@gmail.com>
2022-06-14 14:44:02 +0900

    refactor: LottoNumber 검증 메서드 구현 방법 변경

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -10,8 +10,9 @@ data class LottoNumber private constructor(
         private val LOTTO_NUMBERS: Map<Int, LottoNumber> = (MINIMUM_VALUE..MAXIMUM_VALUE).associateWith(::LottoNumber)
 
         fun from(value: Int): LottoNumber {
-            return LOTTO_NUMBERS[value]
-                ?: throw IllegalArgumentException("로또 숫자의 범위는 $MINIMUM_VALUE ~ $MAXIMUM_VALUE 입니다.")
+            return requireNotNull(LOTTO_NUMBERS[value]) {
+                "로또 숫자의 범위는 $MINIMUM_VALUE ~ $MAXIMUM_VALUE 입니다."
+            }
         }
 
         fun toLottoNumberList(): List<LottoNumber> {
