8ad5aacd58977f90cbfea6b657dbd74e2ea26a88
Grandelwald <artjr97@naver.com>
2022-06-16 20:45:00 +0900

    1단계 - 문자열 덧셈 계산기 : input을 가공하는 Expression.kt 클래스 생성

---
+++ src/main/kotlin/domain/Expression.kt
@@ -0,0 +1,30 @@
+package domain
+
+class Expression(text: String) {
+    var numbers: List<Int>
+        private set
+
+    init {
+        numbers = text.parseExpression().checkValid()
+    }
+
+    private fun String.parseExpression(): List<String> {
+        return Regex("//(.)\n(.*)").find(this)?.let {
+            it.groupValues[2].split(it.groupValues[1])
+        } ?: this.split(DELIMITER_COLON, DELIMITER_COMMA)
+    }
+
+    private fun List<String>.checkValid(): List<Int> {
+        try {
+            val numbers = this.map { it.toInt() }
+            return if (numbers.any { it < 0 }) throw RuntimeException() else numbers
+        } catch (e: NumberFormatException) {
+            throw RuntimeException()
+        }
+    }
+
+    companion object {
+        const val DELIMITER_COMMA = ","
+        const val DELIMITER_COLON = ":"
+    }
+}
