41f680341803471c2b119b1d80daef7b6c2a78c9
hyeon9mak <jinha3507@gmail.com>
2022-06-20 19:04:24 +0900

    refactor: Money 나눗셈 관련 operator method 구현

--- src/test/kotlin/lotto/domain/MoneyTest.kt
+++ src/test/kotlin/lotto/domain/MoneyTest.kt
@@ -2,14 +2,18 @@ package lotto.domain
 
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 import java.math.BigDecimal
 
 internal class MoneyTest : FreeSpec({
 
+    fun Money(value: Long): Money {
+        return Money(value = BigDecimal.valueOf(value))
+    }
+
     "돈은 0보다 작을 수 없다." - {
         listOf(
-            0,
             -1,
             -10,
             -999
@@ -19,6 +23,18 @@ internal class MoneyTest : FreeSpec({
                     shouldThrowExactly<IllegalArgumentException> { Money(value = BigDecimal.valueOf(value.toLong())) }
 
                 exception.message shouldBe "돈은 0 보다 작을 수 없습니다."
+            }
+        }
+    }
+
+    "돈을 나눈 값을 내림된 정수로 반환한다." - {
+        listOf(
+            row(Money(7000), Money(1000), 7),
+            row(Money(5500), Money(1000), 5),
+            row(Money(200), Money(1000), 0),
+        ).forEach { (firstMoney, secondMoney, result) ->
+            "$firstMoney / $secondMoney = $result" {
+                firstMoney.divideInt(secondMoney) shouldBe result
             }
         }
     }
