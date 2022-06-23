cbb544dada28ff83d362c4753c6b9c13f363f2ef
김태헌 <dd.kim@woowahan.com>
2022-06-24 01:39:03 +0900

    refactor: 리뷰 반영

--- src/main/kotlin/lotto/domain/LottoMoney.kt
+++ src/main/kotlin/lotto/domain/LottoMoney.kt
@@ -3,12 +3,12 @@ package lotto.domain
 @JvmInline
 value class LottoMoney(val money: Int = 0) {
     init {
-        if (money < Lotto.PRICE) {
-            throw IllegalArgumentException("최소 구입금액은 ${Lotto.PRICE}원 입니다.")
+        require(money >= Lotto.PRICE) {
+            "최소 구입금액은 ${Lotto.PRICE}원 입니다."
         }
 
-        if (money % Lotto.PRICE != 0) {
-            throw IllegalArgumentException("${Lotto.PRICE}원 단위로 구매하실 수 있습니다.")
+        require(money % Lotto.PRICE == 0) {
+            "${Lotto.PRICE}원 단위로 구매하실 수 있습니다."
         }
     }
 
