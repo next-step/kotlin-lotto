51aee0a6adce2c030a1661fae5c1d2b86fc9e6a5
yjna2316 <yjna2316@gmail.com>
2022-06-26 11:59:54 +0900

    refac: Fixture -> LottoFixture로 이름 수정 및 manualLotto creator 추가

--- src/test/kotlin/lotto/fixture/LottoFixture.kt
+++ src/test/kotlin/lotto/fixture/LottoFixture.kt
@@ -4,10 +4,12 @@ import lotto.domain.BonusNumber
 import lotto.domain.LottoNumber
 import lotto.domain.LottoTicket
 import lotto.domain.LottoTickets
+import lotto.domain.ManualLottos
 import lotto.domain.WinningLotto
 
 fun bonusNumber(number: Int): BonusNumber = BonusNumber(number)
 fun lotto(vararg numbers: Int): LottoTicket = LottoTicket(numbers.toList())
 fun lottoNumber(number: Int): LottoNumber = LottoNumber.from(number)
 fun lottoTickets(vararg lotto: LottoTicket) = LottoTickets(lotto.toList())
+fun manualLotto(vararg numbers: List<Int>): ManualLottos = ManualLottos(numbers.toList())
 fun winningLotto(vararg numbers: Int): WinningLotto = WinningLotto(numbers.toList())
