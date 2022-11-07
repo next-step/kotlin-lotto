98e2e40982ffe1021e6b667431f4e914efc68763
harris.yi <harrisleesh@gmail.com>
2022-11-07 23:58:28 +0900

    feature: Implement input is null or empty return 0

---
+++ src/main/kotlin/step1/StringCalculator.kt
@@ -0,0 +1,14 @@
+package step1
+
+class StringCalculator {
+    companion object {
+        fun calculate(input: String?): Int {
+            return if(input.isNullOrEmpty()) 0 else {
+                return 1
+            }
+
+        }
+    }
+
+
+}
