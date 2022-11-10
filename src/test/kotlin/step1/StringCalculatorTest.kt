9d752285563ee833b6a18a29d75697033c713960
harris.yi <harris.yi@kakaostyle.com>
2022-11-10 22:24:16 +0900

    test: Validate negative input and non-numeric input throw RuntimeException

--- src/test/kotlin/step1/StringCalculatorTest.kt
+++ src/test/kotlin/step1/StringCalculatorTest.kt
@@ -1,24 +1,25 @@
 package step1
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-internal class StringCalculatorTest{
+internal class StringCalculatorTest {
 
     @Test
-    fun `빈 문자열 또는 null을 입력할 경우 0을 반환해야 한다`(){
+    fun `빈 문자열 또는 null을 입력할 경우 0을 반환해야 한다`() {
         assertAll(
-            {assertThat(StringCalculator.calculate("")).isEqualTo(0)},
-            {assertThat(StringCalculator.calculate(null)).isEqualTo(0)}
+            { assertThat(StringCalculator.calculate("")).isEqualTo(0) },
+            { assertThat(StringCalculator.calculate(null)).isEqualTo(0) }
         )
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["0", "1", "2", "100000", "200000"])
-    fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(input: String){
+    fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(input: String) {
         assertThat(StringCalculator.calculate(input)).isEqualTo(input.toInt())
     }
 
