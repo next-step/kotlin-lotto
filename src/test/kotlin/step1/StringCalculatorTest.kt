71186e4fcebdc1e22bc8c2a19018ca5babc2758f
harris.yi <harris.yi@kakaostyle.com>
2022-11-10 20:12:34 +0900

    test: Validate one number input

--- src/test/kotlin/step1/StringCalculatorTest.kt
+++ src/test/kotlin/step1/StringCalculatorTest.kt
@@ -3,6 +3,8 @@ package step1
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class StringCalculatorTest{
 
@@ -13,4 +15,11 @@ internal class StringCalculatorTest{
             {assertThat(StringCalculator.calculate(null)).isEqualTo(0)}
         )
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["0", "1", "2", "100000", "200000"])
+    fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(input: String){
+        assertThat(StringCalculator.calculate(input)).isEqualTo(input.toInt())
+    }
+
 }
