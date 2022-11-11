c0887d69f917deddf10d4debba90b99a1d2d67b3
harris.yi <harris.yi@kakaostyle.com>
2022-11-12 00:24:51 +0900

    feature: Create PositiveInt class with static factory method of

--- src/main/kotlin/step1/StringCalculator.kt
+++ src/main/kotlin/step1/StringCalculator.kt
@@ -1,22 +1,16 @@
 package step1
 
-import java.lang.RuntimeException
-
 
 private const val DEFAULT_RETURN_VALUE = 0
 
 class StringCalculator {
     companion object {
-
         fun calculate(input: String?): Int {
             if (input.isNullOrEmpty()) return DEFAULT_RETURN_VALUE
 
-            val numberList = StringParser.splitBySeparator(input).map {
-                it.toIntOrNull() ?: throw RuntimeException("계산식에 숫자 이외의 값을 입력할 수 없습니다. 입력값 : $it")
-            }.map {
-                it < 0 && throw RuntimeException("계산식에 음수를 입력할 수 없습니다. 입력값 : $it")
-                it
-            }
+            val numberList = StringParser.splitBySeparator(input)
+                .map { stringNumber -> PositiveInt.of(stringNumber) }
+                .map { it.value }
 
             return numberList.sum()
         }
