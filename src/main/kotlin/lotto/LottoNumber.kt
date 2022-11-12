ef6669f4a7d0a2112c70c4853cbb8dfdf306068f
harris.yi <harris.yi@kakaostyle.com>
2022-11-12 11:49:20 +0900

    feature: Override equals and hashCode in lottoNumber class

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -6,4 +6,20 @@ class LottoNumber(
     init {
         require(value in 1..45)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as LottoNumber
+
+        if (value != other.value) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return value
+    }
+
 }
