1f4bd0fcd6f5711dc6658fef1a65d2961eaeca65
harris.yi <harris.yi@kakaostyle.com>
2022-11-13 10:46:32 +0900

    feature: Add cases in LottoResult

--- src/main/kotlin/lotto/LottoResult.kt
+++ src/main/kotlin/lotto/LottoResult.kt
@@ -6,6 +6,9 @@ enum class LottoResult(
     val matchCount: Int,
     val winningMoney: Int
 ) {
+    NO_MATCH(0, 0),
+    MATCH_ONE_NUMBERS(1, 0),
+    MATCH_TWO_NUMBERS(2, 0),
     MATCH_THREE_NUMBERS(3, 5000),
     MATCH_FOUR_NUMBERS(4, 50000),
     MATCH_FIVE_NUMBERS(5, 1500000),
