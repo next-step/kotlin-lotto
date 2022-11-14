479e4f448f38dd3718ef8086710aa934d0a480c9
harris.yi <harris.yi@kakaostyle.com>
2022-11-14 21:11:02 +0900

    feature: Create constant value of MINIMUM_PURCHASE_COUNT

--- src/main/kotlin/lotto/domain/LottoGame.kt
+++ src/main/kotlin/lotto/domain/LottoGame.kt
@@ -4,6 +4,8 @@ import lotto.dto.LottoResults
 
 private const val TICKET_PRICE = 1000
 
+private const val MINIMUM_PURCHASE_COUNT = 1
+
 class LottoGame(
     val lottoTickets: List<LottoTicket>,
     val winnerTicket: WinnerTicket
@@ -19,7 +21,7 @@ class LottoGame(
     companion object {
         fun purchaseTicket(amount: Int): Int {
             val purchaseCount = amount / TICKET_PRICE
-            require(purchaseCount > 0)
+            require(purchaseCount >= MINIMUM_PURCHASE_COUNT) {"로또는 최소 1장 이상 구매할 수 있습니다."}
             return purchaseCount
         }
     }
