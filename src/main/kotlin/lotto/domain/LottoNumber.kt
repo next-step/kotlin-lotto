446f7227f9cb67b85a16729167216c721abddfb3
harris.yi <harris.yi@kakaostyle.com>
2022-11-14 21:17:26 +0900

    feature: Change LottoNumber as data class

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -1,24 +1,9 @@
 package lotto.domain
 
-class LottoNumber(
+data class LottoNumber(
     val value: Int
 ) {
     init {
         require(value in 1..45)
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as LottoNumber
-
-        if (value != other.value) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return value
     }
 }
