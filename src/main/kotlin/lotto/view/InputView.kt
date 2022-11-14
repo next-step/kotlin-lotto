7aa48545b4557dbfe7da11d4742479c8b488ac6a
harris.yi <harris.yi@kakaostyle.com>
2022-11-14 21:45:52 +0900

    chore: Use readln() for not null input

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -6,15 +6,14 @@ class InputView {
     companion object {
         fun askPurchaseAmount(): Int {
             println("구입금액을 입력해 주세요.")
-            return readLine()?.toInt()
-                ?: throw IllegalArgumentException("1000 이상의 숫자를 입력해주세요")
+            return readln().toInt()
         }
 
         fun askWinnerNumber(): Set<Int> {
             println("지난 주 당첨 번호를 입력해 주세요.")
-            return readLine()?.split(", ")
-                ?.map { it.toInt() }
-                ?.toSet() ?: throw IllegalArgumentException("당첨 번호는 , 를 기준으로 6자리 숫자를 입력해주세요.")
+            return readln().split(", ")
+                .map { it.toInt() }
+                .toSet()
         }
     }
 }
