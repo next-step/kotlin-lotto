d262be9f7343a6f7cc2c5b81b7b55cee75becc53
harris.yi <harris.yi@kakaostyle.com>
2022-11-16 21:49:38 +0900

    test: Validate 2nd winning prize

--- src/test/kotlin/lotto/WinnerTicketTest.kt
+++ src/test/kotlin/lotto/WinnerTicketTest.kt
@@ -19,4 +19,17 @@ class WinnerTicketTest : BehaviorSpec({
             }
         }
     }
+    Given("당첨 번호 5자리와 보너스 번호가 일치하는 로또 티켓이 있을 때") {
+        val winnerTicket = WinnerTicket.of(setOf(1, 2, 3, 4, 5, 6), 7)
+        val lottoTicket = LottoTicket.of(setOf(1, 2, 3, 4, 5, 7))
+
+        When("매칭된 로또의 결과를 확인하는 drawResult를 호출하면") {
+            val lottoResult = winnerTicket.drawResult(lottoTicket)
+
+            Then("일치하는 숫자의 갯수 5을 반환한다.") {
+                lottoResult.matchCount shouldBe 5
+                lottoResult.matchBonus shouldBe true
+            }
+        }
+    }
 })
