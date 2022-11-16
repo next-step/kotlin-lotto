6b65966965366b4e5abebb4e647b083193763302
harris.yi <harris.yi@kakaostyle.com>
2022-11-16 22:57:19 +0900

    test: Validate should not duplicate bonus number with winner numbers

--- src/test/kotlin/lotto/WinnerTicketTest.kt
+++ src/test/kotlin/lotto/WinnerTicketTest.kt
@@ -1,9 +1,12 @@
 package lotto
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
 import lotto.domain.LottoTicket
 import lotto.domain.WinnerTicket
+import java.lang.IllegalArgumentException
 
 class WinnerTicketTest : BehaviorSpec({
 
@@ -29,6 +32,15 @@ class WinnerTicketTest : BehaviorSpec({
             Then("일치하는 숫자의 갯수 5을 반환한다.") {
                 lottoResult.matchCount shouldBe 5
                 lottoResult.matchBonus shouldBe true
+            }
+        }
+    }
+
+    Given("당첨 번호 6자리와") {
+        When("중복되는 보너스 번호가 있을 때") {
+            Then("IllegalArgumentException이 발생한다.") {
+                shouldThrow<IllegalArgumentException> { WinnerTicket.of(setOf(1, 2, 3, 4, 5, 6), 6) }
+                    .shouldHaveMessage("보너스 번호는 당첨번호와 중복되면 안됩니다.")
             }
         }
     }
