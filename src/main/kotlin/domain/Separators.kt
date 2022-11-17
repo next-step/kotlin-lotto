c31b7019cd6f11b3bf5a0095e0f3b22973bc073e
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-18 01:43:36 +0900

    feat(Separators): 구분자는 숫자가 올 수 없도록 검증하는 메서드 추가

--- src/main/kotlin/domain/Separators.kt
+++ src/main/kotlin/domain/Separators.kt
@@ -8,6 +8,7 @@ class Separators {
     }
 
     fun add(separator: Char) {
+        validate(separator)
         separators.add(separator)
     }
 
@@ -15,10 +16,18 @@ class Separators {
         return separators.joinToString(VERTICAL_BAR)
     }
 
+    private fun validate(value: Char) {
+        if (Character.getNumericValue(value) != -1) {
+            throw IllegalArgumentException(ERROR_MESSAGE)
+        }
+    }
+
     companion object {
         private const val COMMA = ','
         private const val COLON = ':'
         private const val VERTICAL_BAR = "|"
         private val DEFAULT_SEPARATORS = mutableListOf(COMMA, COLON)
+
+        private const val ERROR_MESSAGE = "숫자는 구분자가 될 수 없습니다."
     }
 }
--- src/test/kotlin/domain/SeparatorsTest.kt
+++ src/test/kotlin/domain/SeparatorsTest.kt
@@ -1,7 +1,10 @@
 package domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.startWith
 
 class SeparatorsTest : BehaviorSpec({
     given("구분자 일급 컬렉션에") {
@@ -13,11 +16,23 @@ class SeparatorsTest : BehaviorSpec({
             }
         }
 
-        `when` ("새로 구분자를 추가했을 때") {
-            val newSeparator = '&'
-            separators.add(newSeparator)
-            then("잘 추가된다.") {
-                separators.get().contains(newSeparator) shouldBe true
+        `when`("새로 구분자를 추가했을 때") {
+            and("문자라면") {
+                val characterSeparator = '&'
+                separators.add(characterSeparator)
+                then("잘 추가된다.") {
+                    separators.get().contains(characterSeparator) shouldBe true
+                }
+            }
+
+            and("숫자라면") {
+                val numberSeparator = '3'
+                then("IllegalArgumentException 를 반환한다.") {
+                    val exception = shouldThrow<IllegalArgumentException> {
+                        separators.add(numberSeparator)
+                    }
+                    exception.message should startWith("숫자는 구분자가 될 수 없습니다.")
+                }
             }
         }
     }
