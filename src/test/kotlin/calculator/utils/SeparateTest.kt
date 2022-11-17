d0d019ff6a43c9c52fc61d36592df0ab14a1c246
shinaaaa <shin029abc@gmail.com>
2022-11-17 11:40:35 +0900

    chore: DisplayName 수정

--- src/test/kotlin/calculator/utils/SeparateTest.kt
+++ src/test/kotlin/calculator/utils/SeparateTest.kt
@@ -21,14 +21,14 @@ internal class SeparateTest {
     }
 
     @Test
-    @DisplayName("//;\\n1;2;3 문자열이 입력된 경우 구분자는 세미콜론(;)")
+    @DisplayName("//;\n1;2;3 문자열이 입력된 경우 구분자는 세미콜론(;)")
     fun `If a string containing a semicolon is entered, the separator is a semicolon`() {
         val separator = Separate.findSeparator("//;\n1;2;3")
         assertThat(separator).isEqualTo(";")
     }
 
     @Test
-    @DisplayName("//;\\n1;2;3 문자열이 입력된 경우 세미콜론(;)으로 구분하여 배열 {'1', '2', '3'}을 만듬")
+    @DisplayName("//;\n1;2;3 문자열이 입력된 경우 세미콜론(;)으로 구분하여 배열 {'1', '2', '3'}을 만듬")
     fun `Create an array {'1', '2', '3'} separated by semicolons if a string is entered`() {
         val strings = Separate.divideBySeparator(";", "//;\n1;2;3")
         assertThat(strings).isEqualTo(listOf("1", "2", "3"))
