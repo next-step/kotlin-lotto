c7ee593d1c6bf5eb8f3101aa0c66431b5960de15
namonak <namonak@gmail.com>
2022-11-18 07:54:22 +0900

    feat(step1): add a function that returns 0 if an empty string or null value is entered

---
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -0,0 +1,10 @@
+package calculator
+
+class StringAddCalculator {
+    fun add(operand: String?): Int {
+        if (operand.isNullOrEmpty()) {
+            return 0
+        }
+        return -1
+    }
+}
