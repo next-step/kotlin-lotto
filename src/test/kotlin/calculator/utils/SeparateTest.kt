2291fabf7aa9b5946a1ef14b7ea598351bbd668c
shinaaaa <shin029abc@gmail.com>
2022-11-17 11:13:22 +0900

    chore: 불필요한 import 제거

--- src/test/kotlin/calculator/utils/SeparateTest.kt
+++ src/test/kotlin/calculator/utils/SeparateTest.kt
@@ -1,6 +1,5 @@
 package calculator.utils
 
-import calculator.utils.Separate
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
