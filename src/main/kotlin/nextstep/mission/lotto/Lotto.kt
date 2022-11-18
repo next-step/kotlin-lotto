0d4ef2e4af6fc071e54e32af0aedbd74d37df7b0
kimseonghun <seonghun127@woowahan.com>
2022-11-18 21:05:00 +0900

    feat: 로또는 중복 숫자를 가질 수 없는 로직 추가

--- src/main/kotlin/nextstep/mission/lotto/Lotto.kt
+++ src/main/kotlin/nextstep/mission/lotto/Lotto.kt
@@ -3,5 +3,6 @@ package nextstep.mission.lotto
 class Lotto(val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "로또 숫자는 6개여야 합니다." }
+        require(numbers.toSet().size == 6) { "로또 숫자는 중복이 허용되지 않습니다." }
     }
 }
