2bf4e10b1c4ee09f718de6d9557f8f1b3cf8c1ae
giyeon95 <giyeon15@gmail.com>
2022-11-18 23:14:54 +0900

    tests: 문자열 덧셈 계산기 단위테스트 작성

---
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -0,0 +1,12 @@
+package calculator
+
+class StringCalculator {
+
+    fun calculate(text: String?): Int {
+        if (text.isNullOrEmpty()) {
+            return 0
+        }
+
+        return -1
+    }
+}
