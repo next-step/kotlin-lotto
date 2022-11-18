8896d02950e73b4e9d3dd659eff496cd0158b652
kimseonghun <seonghun127@woowahan.com>
2022-11-18 21:05:00 +0900

    feat: 로또는 중복 숫자를 가질 수 없는 로직 추가

--- src/test/kotlin/nextstep/mission/lotto/LottoTest.kt
+++ src/test/kotlin/nextstep/mission/lotto/LottoTest.kt
@@ -2,12 +2,21 @@ package nextstep.mission.lotto
 
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
 class LottoTest : StringSpec({
 
     "로또는 숫자 여섯자리를 관리한다." {
-        shouldThrowExactly<IllegalArgumentException> {
-            Lotto(listOf(1, 3, 10, 33, 1))
+        val exception = shouldThrowExactly<IllegalArgumentException> {
+            Lotto(listOf(1, 3, 10, 33, 11))
         }
+        exception.message shouldBe "로또 숫자는 6개여야 합니다."
+    }
+
+    "로또는 중복 숫자를 가질 수 없다." {
+        val exception = shouldThrowExactly<IllegalArgumentException> {
+            Lotto(listOf(1, 3, 10, 33, 1, 11))
+        }
+        exception.message shouldBe "로또 숫자는 중복이 허용되지 않습니다."
     }
 })
