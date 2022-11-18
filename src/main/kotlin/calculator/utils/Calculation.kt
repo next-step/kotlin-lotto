f9cfba092a4a83af535c1d42f950c11c2c6fa8d3
shinaaaa <shin029abc@gmail.com>
2022-11-18 11:19:39 +0900

    refactor: Calculation에서 계산에 대한 기능만 수행하도록 수정

--- src/main/kotlin/calculator/utils/Calculation.kt
+++ src/main/kotlin/calculator/utils/Calculation.kt
@@ -2,10 +2,6 @@ package calculator.utils
 
 object Calculation {
 
-    fun stringArraySum(strings: List<String>) =
-        strings.sumOf {
-            if (!Validation.isNumeric(it)) throw RuntimeException("입력된 값이 숫자가 아닙니다.")
-            if (Validation.isNegativeNumber(it)) throw RuntimeException("입력된 값이 음수 입니다.")
-            it.toInt()
-        }
+    fun intArraySum(intArray: List<Int>) =
+        intArray.sum()
 }
--- src/test/kotlin/calculator/utils/CalculationTest.kt
+++ src/test/kotlin/calculator/utils/CalculationTest.kt
@@ -3,26 +3,13 @@ package calculator.utils
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 internal class CalculationTest {
 
     @Test
     @DisplayName("[1,2] 문자열이 입력된 경우 3")
     fun `1,2 If string is entered 3`() {
-        val sum = Calculation.stringArraySum(listOf("1", "2"))
+        val sum = Calculation.intArraySum(listOf(1, 2))
         assertThat(sum).isEqualTo(3)
-    }
-
-    @Test
-    @DisplayName("문자 입력 중 숫자 이외의 값이 포함된 경우 RuntimeException 오류")
-    fun `RuntimeException error when character input contains non-numeric values`() {
-        assertThrows<RuntimeException> { Calculation.stringArraySum(listOf("a", "2")) }
-    }
-
-    @Test
-    @DisplayName("문자 입력 중 음수 값이 포함된 경우 RuntimeException 오류")
-    fun `RuntimeException error when character input contains negative values`() {
-        assertThrows<RuntimeException> { Calculation.stringArraySum(listOf("-3", "2")) }
     }
 }
