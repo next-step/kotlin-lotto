50aa9df107963bbcd33c0a10e8a2fd9fccac74cf
kimseonghun <seonghun127@woowahan.com>
2022-11-18 21:23:24 +0900

    feat: 로또의 숫자 범위는 1에서 45 사이인지 확인하는 로직 추가

--- src/main/kotlin/nextstep/mission/lotto/Lotto.kt
+++ src/main/kotlin/nextstep/mission/lotto/Lotto.kt
@@ -4,5 +4,15 @@ class Lotto(val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "로또 숫자는 6개여야 합니다." }
         require(numbers.toSet().size == 6) { "로또 숫자는 중복이 허용되지 않습니다." }
+        requireRange(numbers.toMutableList())
     }
+
+    private tailrec fun requireRange(numbers: MutableList<Int>) {
+        when {
+            isInvalidRange(numbers.removeFirst()) -> throw IllegalArgumentException("로또 숫자는 1에서 45사이어야 합니다.")
+            else -> requireRange(numbers)
+        }
+    }
+
+    private fun isInvalidRange(number: Int): Boolean = (number < 1).or(number > 45)
 }
