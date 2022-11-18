6a67e8380e639f5391b1caf618d70baa34935d43
Raemerrr <foals18@gmail.com>
2022-11-18 22:37:05 +0900

    feat: 커스텀 구분자 판별 기능 업로드

--- src/test/kotlin/stringaddcalculator/domain/StringNumbers.kt
+++ src/test/kotlin/stringaddcalculator/domain/StringNumbers.kt
@@ -8,15 +8,26 @@ class StringNumbers(text: String?) {
             text.isNullOrBlank() -> DEFAULT_DATA
             else -> text
         }
-        stringNumbers = splitData.split(DEFAULT_DELIMITER.toRegex()).map { StringNumber(it) }
+        stringNumbers = splitCustomDelimiterOrDefaultDelimiter(splitData)
     }
 
     fun sum(): Int {
         return stringNumbers.sumOf { it.value }
     }
 
+    private fun splitCustomDelimiterOrDefaultDelimiter(text: String): List<StringNumber> {
+        val result = Regex(CUSTOM_DELIMITER).find(text)
+        result?.let { matchResult ->
+            val customDelimiter = matchResult.groupValues[1]
+            return matchResult.groupValues[2].split(customDelimiter).map { StringNumber(it) }
+        }.let {
+            return text.split(DEFAULT_DELIMITER.toRegex()).map { StringNumber(it) }
+        }
+    }
+
     companion object {
         private const val DEFAULT_DATA = "0"
         private const val DEFAULT_DELIMITER = "[,:]"
+        private const val CUSTOM_DELIMITER = "//(.)\n(.*)"
     }
 }
