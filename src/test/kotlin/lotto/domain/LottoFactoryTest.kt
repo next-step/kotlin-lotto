4eec998af98dbf3f5089797b46ec43ed0444b271
chayoung-choi <chayoung.dev@gmail.com>
2022-11-18 17:02:00 +0900

    test: 금액에 맞는 로또 번호 수를 가진 로또 티켓

---
+++ src/test/kotlin/lotto/domain/LottoFactoryTest.kt
@@ -0,0 +1,16 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class LottoFactoryTest {
+    @ParameterizedTest
+    @ValueSource(ints = [1000, 3000, 10000])
+    fun `주어진 금액만큼 로또 번호가 담긴 리켓을 생성한다`(amount: Int) {
+        val lottoCount: Int = LottoFactory.LOTTE_PRICE / amount
+        val lottoTicket = LottoFactory.purchaseLotto(amount)
+        assertThat(lottoTicket.lottoList.size).isEqualTo(lottoCount)
+    }
+}
+
