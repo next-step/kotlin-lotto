c779b8f8e646351a8d34455790cc88c24c363e84
ssr <ssr03>
2022-11-18 22:24:56 +0900

    feat: 구분자에 따라 숫자 리스트로 변환

--- src/main/kotlin/calculator/MessageCode.kt
+++ src/main/kotlin/calculator/MessageCode.kt
@@ -1,5 +1,6 @@
 package calculator
 
 enum class MessageCode(val message: String) {
-    NegativeException("음수는 계산할 수 없습니다.")
+    NegativeException("음수는 계산할 수 없습니다."),
+    NotNumber("숫자가 아닙니다.")
 }
---
+++ src/test/kotlin/calculator/StringInputTest.kt
@@ -0,0 +1,41 @@
+package calculator
+
+import io.kotest.matchers.shouldBe
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
+import java.lang.RuntimeException
+import java.math.BigDecimal
+
+internal class StringInputTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = ["가,나,다", "~,2,3", "1,,3"])
+    fun `stringToBigDecimalList throw RuntimeException when not Number`(stringNumber: String) {
+        val exception = assertThrows<RuntimeException> {
+            StringInput.stringToBigDecimalList(stringNumber)
+        }
+        assertThat(exception.message).isEqualTo(MessageCode.NotNumber.message)
+    }
+
+    @NullAndEmptySource
+    @ParameterizedTest
+    fun `stringToBigDecimalList with null or empty should be zero`(stringNumber: String?) {
+        val result = StringInput.stringToBigDecimalList(stringNumber)
+
+        result?.first() shouldBe BigDecimal.ZERO
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2,3", "1,2:3", "//;\\n1;2;3"])
+    fun stringToBigDecimalList(stringNumber: String) {
+        val result = StringInput.stringToBigDecimalList(stringNumber)
+
+        val resultNumber = listOf(1, 2, 3)
+        result?.forEachIndexed { index, number ->
+            number shouldBe resultNumber[index].toBigDecimal()
+        }
+    }
+}
