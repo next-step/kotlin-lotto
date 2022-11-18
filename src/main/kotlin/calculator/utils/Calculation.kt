4217d23002f830dce54e25c0e049c5e5daa35d30
shinaaaa <shin029abc@gmail.com>
2022-11-18 10:12:45 +0900

    refactor: RuntimeException에 오류 메시지 작성

--- src/main/kotlin/calculator/utils/Calculation.kt
+++ src/main/kotlin/calculator/utils/Calculation.kt
@@ -4,8 +4,8 @@ object Calculation {
 
     fun stringArraySum(strings: List<String>) =
         strings.sumOf {
-            if (!Validation.isNumeric(it)) throw RuntimeException()
-            if (Validation.isNegativeNumber(it)) throw RuntimeException()
+            if (!Validation.isNumeric(it)) throw RuntimeException("입력된 값이 숫자가 아닙니다.")
+            if (Validation.isNegativeNumber(it)) throw RuntimeException("입력된 값이 음수 입니다.")
             it.toInt()
         }
 }
