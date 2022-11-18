c00acff58aae59a6837323197ba41e76c4dc7131
shinaaaa <shin029abc@gmail.com>
2022-11-18 11:18:17 +0900

    refactor: 숫자인지 음수인지를 구분하는 함수를 기능 분리를 위해서 Validation으로 옮김

--- src/main/kotlin/calculator/utils/Validation.kt
+++ src/main/kotlin/calculator/utils/Validation.kt
@@ -2,9 +2,18 @@ package calculator.utils
 
 object Validation {
 
+    private val NUMERIC_REGEX = Regex("[0-9]")
+    private val NEGATIVE_NUMBER_REGEX = Regex("^(-[1-9])")
+
     fun isNumeric(str: String) =
-        Regex("[0-9]").containsMatchIn(str)
+        NUMERIC_REGEX.containsMatchIn(str)
 
     fun isNegativeNumber(str: String) =
-        Regex("^(-[1-9])").containsMatchIn(str)
+        NEGATIVE_NUMBER_REGEX.containsMatchIn(str)
+
+    fun isNumericAndNegativeNumber(strings: List<String>) =
+        strings.forEach {
+            if (!isNumeric(it)) throw RuntimeException("입력된 값이 숫자가 아닙니다.")
+            if (isNegativeNumber(it)) throw RuntimeException("입력된 값이 음수 입니다.")
+        }
 }
--- src/test/kotlin/calculator/utils/ValidationTest.kt
+++ src/test/kotlin/calculator/utils/ValidationTest.kt
@@ -3,12 +3,13 @@ package calculator.utils
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 internal class ValidationTest {
 
     @Test
     @DisplayName("입력된 값이 숫자가 아닌 경우 false")
-    fun `RuntimeException error when character input contains non-numeric values`() {
+    fun `False if character the entered value non-numeric values`() {
         val isNumeric = Validation.isNumeric("a")
         Assertions.assertThat(isNumeric).isFalse
     }
@@ -25,5 +26,17 @@ internal class ValidationTest {
     fun `False if the entered value is positive`() {
         val isNegativeNumber = Validation.isNegativeNumber("13")
         Assertions.assertThat(isNegativeNumber).isFalse
+    }
+
+    @Test
+    @DisplayName("입력된 값 중 숫자 이외의 값이 포함된 경우 RuntimeException 오류")
+    fun `RuntimeException error when the entered value contains non-numeric values`() {
+        assertThrows<RuntimeException> { Validation.isNumericAndNegativeNumber(listOf("a", "2")) }
+    }
+
+    @Test
+    @DisplayName("입력된 값 중 음수 값이 포함된 경우 RuntimeException 오류")
+    fun `RuntimeException error when the entered value contains negative values`() {
+        assertThrows<RuntimeException> { Validation.isNumericAndNegativeNumber(listOf("-3", "2")) }
     }
 }
