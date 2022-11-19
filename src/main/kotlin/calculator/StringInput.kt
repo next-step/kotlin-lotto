f80bcc536e1be557f2a1e27bb403abd88f565fbf
ssr03 <43363127+ssr03@users.noreply.github.com>
2022-11-19 16:50:25 +0900

    Step1- 문자열 덧셈 계산기 (#449)
    * feat: 모든 숫자 합산 기능(BigDecimal List -> BigDecimal)
    
    * feat: 구분자에 따라 숫자 리스트로 변환
    
    * feat: 문자열 덧셈 계산기
    
    Co-authored-by: ssr <ssr03>

---
+++ src/main/kotlin/calculator/StringInput.kt
@@ -0,0 +1,34 @@
+package calculator
+
+import java.math.BigDecimal
+
+object StringInput {
+    private const val START_LINE = "//"
+    private const val DEFAULT_REGEX = "[,:]"
+    private const val CUSTOM_REGEX = "//(.)\\n(.*)"
+    private const val CUSTOM_SEPARATOR_INDEX = 1
+    private const val CUSTOM_TOKENS_INDEX = 2
+
+    fun stringToBigDecimalList(stringNumbers: String?): List<BigDecimal>? =
+        if (stringNumbers.isNullOrBlank()) {
+            listOf(BigDecimal.ZERO)
+        } else if (stringNumbers.contains(START_LINE)) {
+            splitToCustomText(stringNumbers)
+        } else {
+            splitToDefaultText(stringNumbers)
+        }
+
+    private fun splitToCustomText(text: String): List<BigDecimal>? =
+        Regex(CUSTOM_REGEX).find(text)?.let {
+            splitToCustom(it.groupValues[CUSTOM_TOKENS_INDEX], it.groupValues[CUSTOM_SEPARATOR_INDEX])
+        }
+
+    private fun splitToDefaultText(text: String): List<BigDecimal> =
+        text.split(DEFAULT_REGEX.toRegex()).map { textToBigDecimal(it.trim()) }
+
+    private fun splitToCustom(text: String, customSeparator: String): List<BigDecimal> =
+        text.split(customSeparator).map { textToBigDecimal(it.trim()) }
+
+    private fun textToBigDecimal(text: String): BigDecimal =
+        text.toBigDecimalOrNull() ?: throw RuntimeException(MessageCode.NotNumber.message)
+}
