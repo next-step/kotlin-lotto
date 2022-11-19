f98b936b47c1bf7fd8521f0264c6df06b2b896f2
ssr <ssr03>
2022-11-19 21:32:50 +0900

    fix: StringAddCalculatorTest ValueSource case 추가

--- src/test/kotlin/calculator/StringAddCalculatorTest.kt
+++ src/test/kotlin/calculator/StringAddCalculatorTest.kt
@@ -27,28 +27,28 @@ internal class StringAddCalculatorTest {
 
     @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
     @ParameterizedTest
-    @ValueSource(strings = ["1"])
+    @ValueSource(strings = ["1", "2", "2.2", "100"])
     fun oneNumber(text: String) {
         assertThat(calculator.add(text)).isEqualTo(text.toBigDecimal())
     }
 
     @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
     @ParameterizedTest
-    @ValueSource(strings = ["1,2"])
+    @ValueSource(strings = ["1,2", "2,1"])
     fun twoNumbers(text: String) {
         assertThat(calculator.add(text)).isSameAs(3.toBigDecimal())
     }
 
     @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
     @ParameterizedTest
-    @ValueSource(strings = ["1,2:3"])
+    @ValueSource(strings = ["1,2:3", "1:2,3", "1:2:3"])
     fun colons(text: String) {
         assertThat(calculator.add(text)).isSameAs(6.toBigDecimal())
     }
 
     @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
     @ParameterizedTest
-    @ValueSource(strings = ["//;\n1;2;3"])
+    @ValueSource(strings = ["//;\n1;2;3", "//?\n1?2?3", "//#\n1#2#3"])
     fun customDelimiter(text: String) {
         assertThat(calculator.add(text)).isSameAs(6.toBigDecimal())
     }
