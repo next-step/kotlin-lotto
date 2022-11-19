f8d8a927765677587ebe342d1eeca7ecee78d5e6
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-19 23:18:10 +0900

    feat(Numbers): 양의 정수 검증하는 메서드 추가

--- src/main/kotlin/domain/Numbers.kt
+++ src/main/kotlin/domain/Numbers.kt
@@ -1,5 +1,7 @@
 package domain
 
+import java.lang.RuntimeException
+
 class Numbers(private val numbers: List<Int> = listOf()) {
     fun sum(): Int {
         return if (this.numbers.size == ZERO) {
@@ -11,5 +13,29 @@ class Numbers(private val numbers: List<Int> = listOf()) {
 
     companion object {
         private const val ZERO = 0
+        private const val RUNTIME_EXCEPTION_ERROR_MESSAGE = "숫자는 음수일 수 없습니다."
+        private const val ILLEGAL_ARGUMENT_EXCEPTION_ERROR_MESSAGE = "유효하지 않은 숫자입니다."
+
+        @JvmStatic
+        fun from(tokens: List<String>): Numbers {
+            return Numbers(
+                tokens.map {
+                    convertStringToInt(it)
+                }
+            )
+        }
+
+        private fun convertStringToInt(value: String): Int {
+            val number = value.toIntOrNull()
+                ?: throw IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_ERROR_MESSAGE)
+            validateNotNegative(number)
+            return number
+        }
+
+        private fun validateNotNegative(value: Int) {
+            if (value < ZERO) {
+                throw RuntimeException(RUNTIME_EXCEPTION_ERROR_MESSAGE)
+            }
+        }
     }
 }
--- src/test/kotlin/domain/NumbersTest.kt
+++ src/test/kotlin/domain/NumbersTest.kt
@@ -1,7 +1,12 @@
 package domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.startWith
+import java.lang.RuntimeException
 
 class NumbersTest : BehaviorSpec({
     given("아무 element 도 없는 숫자 리스트에서") {
@@ -33,10 +38,40 @@ class NumbersTest : BehaviorSpec({
             }
         }
     }
+
+    given("Numbers 객체에") {
+        `when`("값을 할당할 때") {
+            and("숫자가 아닌 문자열이라면") {
+                invalidStringList.forAll {
+                    then("IllegalArgumentException 을 반환한다.") {
+                        val exception = shouldThrow<IllegalArgumentException> {
+                            Numbers.from(it)
+                        }
+                        exception.message should startWith("유효하지 않은 숫자입니다.")
+                    }
+                }
+            }
+
+            and("음수가 있다면") {
+                then("RuntimeException 을 반환한다.") {
+                    val exception = shouldThrow<RuntimeException> {
+                        Numbers.from(listOf("-2", "10", "-55", "24"))
+                    }
+                    exception.message should startWith("숫자는 음수일 수 없습니다.")
+                }
+            }
+        }
+    }
 }) {
     companion object {
         private const val ZERO = 0
         private const val TEN = 10
         private val multipleNumbers = listOf(1, 2, 3, 4, 5)
+
+        private val invalidStringList = listOf(
+            listOf("d", "4d", "f", "^"),
+            listOf("##", "sdff324$", "--", "윤"),
+            listOf("코", "틀", "3", "짱")
+        )
     }
 }
