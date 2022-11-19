64a6d915d3be6b3aa66f0431110a79d18a28edce
kimseonghun <seonghun127@woowahan.com>
2022-11-19 21:55:12 +0900

    feat: 당첨번호를 입력받아 통계를 반환하는 로직 추가

--- src/test/kotlin/nextstep/mission/lotto/LottosTest.kt
+++ src/test/kotlin/nextstep/mission/lotto/LottosTest.kt
@@ -8,4 +8,15 @@ class LottosTest : StringSpec({
     "로또를 입력받은 금액만큼의 갯수를 관리한다." {
         Lottos(10000).lottos.size shouldBe 10
     }
+
+    "당첨번호를 입력받아 통계를 반환한다." {
+        val lottos = Lottos(
+            listOf(
+                Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                Lotto(listOf(11, 12, 13, 14, 15, 16)),
+                Lotto(listOf(40, 41, 42, 43, 44, 45)),
+            )
+        )
+        lottos.checkWinningNumbers(listOf(1, 2, 3, 4, 5, 6)) shouldBe WinningResult(0, 0, 0, 1)
+    }
 })
