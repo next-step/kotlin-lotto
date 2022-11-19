87d9364939ad58101040448876df85e7125d05eb
soo <yunsoo0510@daum.net>
2022-11-19 10:24:21 +0900

    refactor: 로또번호 입력받을 때 whitespace 허용

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,7 +3,7 @@ package lotto.domain
 class Lotto(numbers: List<LottoNumber> = LottoNumbers.getNumbers()) {
     val numbers: List<LottoNumber> = numbers
 
-    constructor(text: String) : this(text.split(",").map { LottoNumber.from(it.toInt()) })
+    constructor(text: String) : this(text.filter { !it.isWhitespace() }.split(",").map { LottoNumber.from(it.toInt()) })
 
     companion object {
         val PRICE = Money(1000)
