deb9b28e751260906ac21cf29ad15b4c986a6697
dajeong <daaajeong@gmail.com>
2022-11-19 11:22:59 +0900

    [step1] 문자열을 쉼표(,) 또는 콜론(:) 으로 분리

--- src/main/kotlin/stringCalculator/StringAddCalculator.kt
+++ src/main/kotlin/stringCalculator/StringAddCalculator.kt
@@ -5,10 +5,18 @@ class StringAddCalculator {
         if (inputValue.isNullOrBlank()) {
             return DEFAULT_VALUE
         }
+        val stringList = split(inputValue)
+
         TODO("구현예정")
+    }
+
+    private fun split(string: String): List<String> {
+        return string.split(DEFAULT_DELIMITER_REGEX)
     }
 
     companion object {
         private const val DEFAULT_VALUE = 0
+
+        private val DEFAULT_DELIMITER_REGEX = Regex("[,:]")
     }
 }
