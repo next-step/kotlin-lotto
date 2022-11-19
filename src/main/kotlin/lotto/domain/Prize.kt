c0c358f1bb96df163a36357e8a76bf1e6440f671
soo <yunsoo0510@daum.net>
2022-11-19 10:00:04 +0900

    feat: Prize 구현

---
+++ src/main/kotlin/lotto/domain/Prize.kt
@@ -0,0 +1,14 @@
+package lotto.domain
+
+enum class Prize(val matchCount: Int, val prize: Int) {
+    FIRST(6,2000000000),
+    SECOND(5,1500000),
+    THIRD(4,50000),
+    FOURTH(3,5000);
+
+    companion object {
+        fun prizeOfMatchCount(matchCount: Int): Int {
+            return values().first { it.matchCount == matchCount }.prize
+        }
+    }
+}
