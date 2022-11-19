ad5098efcb618098e06615b7fc74252bb9e78d98
SangHyunGil <zizon5941@gmail.com>
2022-11-19 16:50:38 +0900

    feat: Parser 테스트 코드 작성

--- src/main/kotlin/calculator/const/ExceptionMessage.kt
+++ src/main/kotlin/calculator/const/ExceptionMessage.kt
@@ -2,4 +2,5 @@ package calculator.const
 
 object ExceptionMessage {
     const val CUSTOM_DELIMITER_EMPTY_STRING_ERROR = "커스텀 구분자는 널 값이나 빈 문자열이 될 수 없어요."
+    const val ILLEGAL_EXPRESSION_ERROR = "연산식에는 문자나 음수가 포함될 수 없고, 토큰에 없는 값으로 파싱 없어요."
 }
---
+++ src/main/kotlin/calculator/util/Parser.kt
@@ -0,0 +1,9 @@
+package calculator.util
+
+import java.lang.Exception
+
+object Parser {
+    fun parse(delimiter: List<String>, expression: String): List<Int> {
+        throw Exception()
+    }
+}
