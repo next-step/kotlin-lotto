6d67c635a871a7bedd228407f94d2b8a88f8e7d6
wonjun3991 <wonjun.choi@meshkorea.net>
2022-11-19 14:22:51 +0900

    refactor(CalculatorTest): refactor test

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -1,10 +1,12 @@
 package calculator
 
-import io.kotest.matchers.throwable.shouldHaveMessage
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
 
 
 internal class CalculatorTest {
@@ -15,64 +17,46 @@ internal class CalculatorTest {
         calculator = Calculator()
     }
 
-    @Test
-    fun `빈 문자열이면 0을 반환한다`() {
-        val actual = calculator.calculate("")
-
-        assertThat(actual).isEqualTo(0)
+    @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    fun `빈 문자열이나 Null을 입력하면 0을 반환한다`(text: String?) {
+        assertThat(calculator.calculate(text)).isZero
     }
 
-    @Test
-    fun `null이면 0을 반환한다`() {
-        val actual = calculator.calculate(null)
-
-        assertThat(actual).isEqualTo(0)
+    @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = ["1"])
+    fun `숫자 하나를 입력할 경우 해당 숫자를 그대로 반환한다`(text: String) {
+        assertThat(calculator.calculate(text)).isSameAs(text.toInt())
     }
 
-
-    @Test
-    fun `숫자 하나를 입력할 경우 해당 숫자를 그대로 반환한다`() {
-        val actual = calculator.calculate("5")
-
-        assertThat(actual).isEqualTo(5)
+    @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2"])
+    fun `쉼표(,)를 가지는 문자열을 받아서 구분자를 기준으로 분리한 숫자의 합을 반환한다`(text: String) {
+        assertThat(calculator.calculate(text)).isSameAs(3)
     }
 
-    @Test
-    fun `쉼표(,)를 가지는 문자열을 받아서 구분자를 기준으로 분리한 숫자의 합을 반환한다`() {
-        val actual = calculator.calculate("2,3")
-
-        assertThat(actual).isEqualTo(5)
+    @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2:3"])
+    fun `쉼표나 콜론을 가지는 문자열을 받아서 구분자를 기준으로 분리한 숫자의 합을 반환한다`(text: String) {
+        assertThat(calculator.calculate(text)).isSameAs(6)
     }
 
-    @Test
-    fun `콜론을 가지는 문자열을 받아서 구분자를 기준으로 분리한 숫자의 합을 반환한다`() {
-        val actual = calculator.calculate("2:3")
-
-        assertThat(actual).isEqualTo(5)
+    @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = ["//;\n1;2;3"])
+    fun `커스텀 구분자를 지정할 수 있다`(text: String) {
+        assertThat(calculator.calculate(text)).isSameAs(6)
     }
 
-    @Test
-    fun `커스텀 구분자를 지정할 수 있다`() {
-        val actual = calculator.calculate("//;\n2;3;4")
-
-        assertThat(actual).isEqualTo(9)
-    }
-
-    @Test
-    fun `음수를 전달할 경우 RuntimeException 예외가 발생해야 한다`() {
-        assertThrows<RuntimeException> {
-            calculator.calculate("//;\n-2;3;4")
-        }.shouldHaveMessage("음수는 입력할 수 없습니다.")
-
-        assertThrows<RuntimeException> {
-            calculator.calculate("-3")
-        }.shouldHaveMessage("음수는 입력할 수 없습니다.")
-    }
-
-    @Test
-    fun `숫자가 아닌 값을 전달할 경우 RuntimeException 예외가 발생해야 한다`() {
-        assertThrows<RuntimeException> {
-            calculator.calculate("안녕하세요")
-        }.shouldHaveMessage("숫자가 아닌 값은 입력될 수 없습니다.")
+    @DisplayName(value = "문자열 계산기에 음수나 숫자가 아닌 값을 전달하면 RuntimeException 예외 처리를 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = ["-1", "안녕하세요"])
+    fun `음수를 전달할 경우 RuntimeException 예외가 발생해야 한다`(text: String) {
+        assertThatExceptionOfType(RuntimeException::class.java)
+            .isThrownBy { calculator.calculate(text) }
     }
 }
