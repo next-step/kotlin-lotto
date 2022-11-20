b5059c8b77e06ce1179809185efffeae62edb9b9
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-20 18:17:53 +0900

    feat(Separator): 문자열 구분하는 역할을 Separator 에서 하도록 변경

--- src/main/kotlin/domain/Separators.kt
+++ src/main/kotlin/domain/Separators.kt
@@ -1,18 +1,22 @@
 package domain
 
-class Separators {
-    private val separators: MutableList<Char> = DEFAULT_SEPARATORS
+class Separators(private val separators: MutableList<Char> = DEFAULT_SEPARATORS) {
+    val value: List<Char>
+        get() = separators
 
-    fun get(): List<Char> {
-        return separators
+    fun separate(input: String): List<String> {
+        return regexExpression.find(input)?.let {
+            add(it.groupValues[FIRST_STRING_INDEX][0])
+            it.groupValues[SECOND_STRING_INDEX].split(this.toRegex())
+        } ?: input.split(this.toRegex())
     }
 
-    fun add(separator: Char) {
+    private fun add(separator: Char) {
         validate(separator)
         separators.add(separator)
     }
 
-    fun toRegex(): Regex {
+    private fun toRegex(): Regex {
         return separators.joinToString(VERTICAL_BAR).toRegex()
     }
 
@@ -28,6 +32,10 @@ class Separators {
         private const val COLON = ':'
         private const val VERTICAL_BAR = "|"
         private val DEFAULT_SEPARATORS = mutableListOf(COMMA, COLON)
+        private const val FIRST_STRING_INDEX = 1
+        private const val SECOND_STRING_INDEX = 2
+        private const val DEFAULT_SEPARATOR_FIND_REGEX = "//(.)\n(.*)"
+        private val regexExpression = Regex(DEFAULT_SEPARATOR_FIND_REGEX)
 
         private const val ERROR_MESSAGE = "숫자는 구분자가 될 수 없습니다."
     }
--- src/test/kotlin/domain/SeparatorsTest.kt
+++ src/test/kotlin/domain/SeparatorsTest.kt
@@ -1,39 +1,37 @@
 package domain
 
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.string.startWith
 
-class SeparatorsTest : BehaviorSpec({
-    given("구분자 일급 컬렉션에") {
-        val separators = Separators()
-        `when`("지정한 기본인자가") {
-            val defaultSeparators = listOf(',', ':')
-            then("모두 포함되어 있다.") {
-                separators.get().containsAll(defaultSeparators) shouldBe true
-            }
-        }
+class SeparatorsTest : BehaviorSpec() {
+    private lateinit var separators: Separators
 
-        `when`("새로 구분자를 추가했을 때") {
-            and("문자라면") {
-                val characterSeparator = '&'
-                separators.add(characterSeparator)
-                then("잘 추가된다.") {
-                    separators.get().contains(characterSeparator) shouldBe true
+    init {
+        given("기본 구분자만 있는 구분자 객체에") {
+            separators = Separators()
+            `when`("기본 구분자가") {
+                val defaultSeparators = listOf(',', ':')
+                then("모두 포함되어 있다.") {
+                    separators.value.containsAll(defaultSeparators) shouldBe true
                 }
             }
 
-            and("숫자라면") {
-                val numberSeparator = '3'
-                then("IllegalArgumentException 를 반환한다.") {
-                    val exception = shouldThrow<IllegalArgumentException> {
-                        separators.add(numberSeparator)
-                    }
-                    exception.message should startWith("숫자는 구분자가 될 수 없습니다.")
+            `when`("커스텀 구분자가 포함되지 않은 문자열로 분리하면") {
+                val input = "1,2:3,4"
+                val tokens = separators.separate(input)
+                then("기본 구분자로 잘 분리된다.") {
+                    tokens.size shouldBe 4
+                }
+            }
+
+            `when`("커스텀 구분자가 포함된 문자열로 분리하면") {
+                val input = "//;\n1;2;3;4,5"
+                val tokens = separators.separate(input)
+
+                then("추가된 구분자로 잘 분리된다.") {
+                    tokens.size shouldBe 5
                 }
             }
         }
     }
-})
+}
