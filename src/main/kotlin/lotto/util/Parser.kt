9ff6ead201a167b74d86a41c6010775e36be153d
SangHyunGil <zizon5941@gmail.com>
2022-11-20 16:25:43 +0900

    feat: 파싱을 하기위한 파서 코드 추가

--- src/main/kotlin/lotto/util/Parser.kt
+++ src/main/kotlin/lotto/util/Parser.kt
@@ -1,9 +1,13 @@
 package lotto.util
 
-import java.lang.Exception
-
 object Parser {
+    private const val EMPTY_STRING = ""
+    private val REPLACE_REGEX = "\\s+|\\[|\\]".toRegex()
+    private val SPLIT_REGEX = ",".toRegex()
+
     fun parse(rawString: String): List<Int> {
-        throw Exception()
+        val removeSpaceStr = rawString.replace(REPLACE_REGEX, EMPTY_STRING)
+        val splitStr = removeSpaceStr.split(SPLIT_REGEX)
+        return splitStr.map { it.toInt() }
     }
 }
