fe15563dc69fb8a0615bccaaf8d6129f42edc41b
Raemerrr <foals18@gmail.com>
2022-11-20 10:36:21 +0900

    test: 합계 기능 검증 로직 추가
    * 기본 구분자 합계 기능 검증
    * 커스텀 구분자 합계 기능 검증

--- src/test/kotlin/stringaddcalculator/PositiveNumbersTest.kt
+++ src/test/kotlin/stringaddcalculator/PositiveNumbersTest.kt
@@ -1,8 +1,11 @@
 package stringaddcalculator
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.NullAndEmptySource
 import org.junit.jupiter.params.provider.ValueSource
 import stringaddcalculator.domain.PositiveNumbers
@@ -24,5 +27,20 @@ class PositiveNumbersTest {
         assertThrows<RuntimeException> {
             PositiveNumbers(input)
         }
+    }
+
+    @CsvSource(
+        value = ["1,2ㅁ3", "1,2,3ㅁ6", "1,2:3ㅁ6", "nullㅁ0"],
+        delimiter = 'ㅁ',
+        nullValues = ["null"]
+    )
+    @ParameterizedTest
+    fun `기본 구분자 합계 기능 검증`(input: String?, expected: Int) {
+        assertEquals(expected, PositiveNumbers(input).sum())
+    }
+
+    @Test
+    fun `커스텀 구분자 합계 기능 검증`() {
+        assertEquals(6, PositiveNumbers("//;\n1;2;3").sum())
     }
 }
