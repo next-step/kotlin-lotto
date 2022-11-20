d38528513925a3f80add41d25459440d8231e814
EP <kjuioqq8@gmail.com>
2022-11-20 16:28:55 +0900

    feat: 구분자 enum 추가

---
+++ src/main/kotlin/calculator/domain/Delimiter.kt
@@ -0,0 +1,11 @@
+package calculator.domain
+
+enum class Delimiter(
+    val value: String
+) {
+    COMMA(",");
+
+    companion object {
+        fun find(literalValue: String): Delimiter = values().find { it.value == literalValue } ?: throw IllegalArgumentException("존재하지 않는 구분자입니다.")
+    }
+}
