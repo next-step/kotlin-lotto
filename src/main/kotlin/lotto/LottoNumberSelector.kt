9d1217d61df2348a75baa530b9bc5f940ac1dfc3
0923kdh <0923kdh@gmail.com>
2022-11-20 16:13:21 +0900

    chore: END_NUMBER 값 수정

--- src/main/kotlin/lotto/LottoNumberSelector.kt
+++ src/main/kotlin/lotto/LottoNumberSelector.kt
@@ -16,7 +16,7 @@ class LottoNumberSelector {
 
     companion object {
         private const val START_NUMBER = 1
-        private const val END_NUMBER = 1
+        private const val END_NUMBER = 45
         private const val MAX_LOTTO_NUMBER_SIZE = 6
     }
 }
