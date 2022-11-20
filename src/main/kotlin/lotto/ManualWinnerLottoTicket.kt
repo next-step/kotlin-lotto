1e10f2a8f5c9b43c1d1335d170585a27a09e3014
SangHyunGil <zizon5941@gmail.com>
2022-11-20 16:41:33 +0900

    feat: 당첨 티켓을 위한 코드 추가

--- src/main/kotlin/lotto/ManualWinnerLottoTicket.kt
+++ src/main/kotlin/lotto/ManualWinnerLottoTicket.kt
@@ -1,5 +1,20 @@
 package lotto
 
+import lotto.util.ExceptionMessage
+import lotto.util.Parser
+import lotto.util.Reader
+
 class ManualWinnerLottoTicket : WinnerLottoTicket {
-    override val winnerLottoNumbers: Set<LottoNumber> = emptySet()
+    override val winnerLottoNumbers: Set<LottoNumber>
+
+    init {
+        val winnerLottoNumbersStr = Reader.read()
+        val lottoNumbers = Parser.parse(winnerLottoNumbersStr)
+        validateLottoNumbers(lottoNumbers)
+        this.winnerLottoNumbers = lottoNumbers.map { LottoNumber(it) }.toSet()
+    }
+
+    private fun validateLottoNumbers(lottoNumbers: List<Int>) {
+        require(lottoNumbers.toSet().size == 6) { ExceptionMessage.LOTTO_NUMBER_SIZE_ERROR }
+    }
 }
