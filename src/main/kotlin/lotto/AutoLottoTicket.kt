78ebc66b664021e19c74f793b72cdb3d04ff9694
SangHyunGil <zizon5941@gmail.com>
2022-11-20 16:06:56 +0900

    feat: 로또 티켓을 생성하기 위한 코드 추가

--- src/main/kotlin/lotto/AutoLottoTicket.kt
+++ src/main/kotlin/lotto/AutoLottoTicket.kt
@@ -1,5 +1,23 @@
 package lotto
 
+import lotto.util.RandomNumberGenerator
+
 class AutoLottoTicket : LottoTicket {
-    override val lottoNumbers: Set<LottoNumber> = emptySet()
+    override val lottoNumbers: Set<LottoNumber>
+
+    init {
+        val lottoNumbers = mutableSetOf<LottoNumber>()
+        while (lottoNumbers.size < LOTTO_NUMBER_COUNT) {
+            val randomNumber = RandomNumberGenerator.generate(START_NUMBER..END_NUMBER)
+            val lottoNumber = LottoNumber(randomNumber)
+            lottoNumbers.add(lottoNumber)
+        }
+        this.lottoNumbers = lottoNumbers
+    }
+
+    companion object {
+        private const val LOTTO_NUMBER_COUNT = 6
+        private const val START_NUMBER = 1
+        private const val END_NUMBER = 45
+    }
 }
