fd71c83ac460e288557a6dba1e5a4418a326170f
EP <kjuioqq8@gmail.com>
2022-11-20 16:34:08 +0900

    test: 예외 케이스, colon 케이스 추가

--- src/test/kotlin/calculator/DelimiterTest.kt
+++ src/test/kotlin/calculator/DelimiterTest.kt
@@ -1,6 +1,7 @@
 package calculator
 
 import calculator.domain.Delimiter
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
@@ -9,5 +10,16 @@ class DelimiterTest : StringSpec({
         val comma = ","
         val delimiter = Delimiter.find(comma)
         delimiter.value shouldBe comma
+    }
+
+    "구분자는 colon 가 포함된다" {
+        val colon = ":"
+        val delimiter = Delimiter.find(colon)
+        delimiter.value shouldBe colon
+    }
+
+    "구분자로 존재하지 않는 문자열을 넣으면 예외가 발생한다" {
+        val hash = "#"
+        shouldThrow<IllegalArgumentException> { Delimiter.find(hash) }
     }
 })
