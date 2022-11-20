f238f8384ac0fd9f4bbbe63c755f70c6b8d92fba
EP <kjuioqq8@gmail.com>
2022-11-20 17:58:03 +0900

    test: 이항 연산 BDD 테스트 구현

---
+++ src/test/kotlin/calculator/BinaryOperationBddTest.kt
@@ -0,0 +1,26 @@
+package calculator
+
+import calculator.domain.operation.AdditionBinaryOperation
+import calculator.domain.operation.BinaryOperationCommand
+import calculator.domain.operation.Operation
+import calculator.domain.operation.OperationCommand
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+
+class BinaryOperationBddTest : BehaviorSpec({
+
+    given("2개의 숫자 값이 주어졌을 때") {
+        val firstNumber = 1
+        val secondNumber = 3
+
+        `when`("이항식 더하기 연산을 하면") {
+            val binaryOperation = AdditionBinaryOperation
+            val binaryOperationCommand = BinaryOperationCommand(leftArgument = firstNumber, rightArgument = secondNumber)
+            val result = binaryOperation.operate(binaryOperationCommand)
+
+            then("연산이 된다") {
+                result shouldBe firstNumber + secondNumber
+            }
+        }
+    }
+})
