e48d3d6698981356d98f5a5f1084b78d46cf3f70
EP <kjuioqq8@gmail.com>
2022-11-20 20:35:24 +0900

    feat: 계산기 화면 로직 구현

---
+++ src/main/kotlin/calculator/interfaces/adapter/CalculatorViewAdapter.kt
@@ -0,0 +1,34 @@
+package calculator.interfaces.adapter
+
+import calculator.application.calculator.Calculator
+import calculator.application.model.PositiveInteger
+import calculator.application.parser.Parser
+import calculator.application.parser.ParsingException
+import calculator.interfaces.input.InputPlugin
+import calculator.interfaces.output.OutputPlugin
+
+class CalculatorViewAdapter(
+    private val inputPlugin: InputPlugin,
+    private val outputPlugin: OutputPlugin,
+    private val calculator: Calculator,
+    private val parser: Parser
+) {
+
+    fun start() {
+        val numberList = queryNumber()
+        val result = calculator.multiplePlus(numberList)
+        outputPlugin.output("결과: $result")
+    }
+
+    private fun queryNumber(): List<PositiveInteger> =
+        try {
+            val input = inputPlugin.input()
+            parser.parse(input)
+        } catch (e: ParsingException) {
+            outputPlugin.output(e.message)
+            queryNumber()
+        } catch (e: Exception) {
+            outputPlugin.output("알 수 없는 에러입니다.")
+            queryNumber()
+        }
+}
