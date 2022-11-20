d4491481390e23d9b54cfccde1665f1361633205
SangHyunGil <zizon5941@gmail.com>
2022-11-20 15:18:47 +0900

    feat: 로또 넘버를 위한 코드 추가

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -1,4 +1,10 @@
 package lotto
 
+import lotto.util.ExceptionMessage
+
 @JvmInline
-value class LottoNumber(val number: Int)
+value class LottoNumber(val number: Int) {
+    init {
+        require(number in 1..45) { ExceptionMessage.LOTTO_NUMBER_ERROR }
+    }
+}
