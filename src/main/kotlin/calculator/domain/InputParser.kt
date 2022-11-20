200186d6e9f5b143ab250d52b366f262d7579156
namonak <namonak@gmail.com>
2022-11-21 01:50:46 +0900

    feat(step1): add a function that throws a RuntimeException when a non-numeric value is entered

--- src/main/kotlin/calculator/domain/InputParser.kt
+++ src/main/kotlin/calculator/domain/InputParser.kt
@@ -8,6 +8,14 @@ class InputParser {
         if (input.isNullOrEmpty()) {
             return arrayOf(0)
         }
+        try {
+            return parse(input)
+        } catch (e: NumberFormatException) {
+            throw RuntimeException("숫자 이외의 값은 입력할 수 없습니다.")
+        }
+    }
+
+    private fun parse(input: String): Array<Int> {
         if (input.startsWith("//")) {
             val (customDelimiter, tokens) = CUSTOM_DELIMITER_INDICATOR.toRegex().find(input)!!.destructured
             return tokens.split(customDelimiter).map { it.toInt() }.toTypedArray()
--- src/test/kotlin/calculator/domain/InputParserTest.kt
+++ src/test/kotlin/calculator/domain/InputParserTest.kt
@@ -1,11 +1,13 @@
 package calculator.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
 
 class InputParserTest {
     private lateinit var inputParser: InputParser
@@ -28,7 +30,7 @@ class InputParserTest {
         assertThat(inputParser.parseWithDelimiter("1,2:3")).isEqualTo(expected)
     }
 
-    @DisplayName("//와 \\n 문자 사이에 커스텀 구분자 지정 테스트")
+    @DisplayName("//와 \n 문자 사이에 커스텀 구분자 지정 테스트")
     @Test
     fun `커스텀 구분자 테스트`() {
         assertThat(inputParser.parseWithDelimiter("//;\n1;2;3")).isEqualTo(expected)
@@ -45,5 +47,14 @@ class InputParserTest {
     @Test
     fun `숫자 하나를 문자열로 입력할 경우 해당 숫자 반환 테스트`() {
         assertThat(inputParser.parseWithDelimiter("1")).isEqualTo(arrayOf(1))
+    }
+
+    @DisplayName("입력된 값이 숫자인지 확인하는 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = [" ", "a", "1a", "a1", "1 1", " 1", "1 "])
+    fun `입력된 값이 숫자인지 확인하는 테스트`(input: String) {
+        assertThatThrownBy { inputParser.parseWithDelimiter(input) }
+            .isInstanceOf(RuntimeException::class.java)
+            .hasMessageContaining("숫자 이외의 값은 입력할 수 없습니다.")
     }
 }
