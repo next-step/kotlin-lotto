476ad8ffe8a0a94b6838286e1a1baea4d4a6e2cd
EP <kjuioqq8@gmail.com>
2022-11-20 16:34:29 +0900

    feat: 구분자 colon 타입 추가

--- src/main/kotlin/calculator/domain/Delimiter.kt
+++ src/main/kotlin/calculator/domain/Delimiter.kt
@@ -3,7 +3,7 @@ package calculator.domain
 enum class Delimiter(
     val value: String
 ) {
-    COMMA(",");
+    COMMA(","), COLON(":");
 
     companion object {
         fun find(literalValue: String): Delimiter = values().find { it.value == literalValue } ?: throw IllegalArgumentException("존재하지 않는 구분자입니다.")
