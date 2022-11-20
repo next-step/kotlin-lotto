02d6669aa489465d4ac83fcfe438c7e834f1f751
chayoung-choi <chayoung.dev@gmail.com>
2022-11-20 23:37:46 +0900

    refactor: 당첨 로또 리팩토링

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -1,12 +1,6 @@
 package lotto.domain
 
-class WinningLotto(val numbers: Set<Int>) {
-    init {
-        require(numbers.size == Lotto.LOTTO_NUMBER_COUNT) { "로또 당첨 번호는 6개입니다." }
-        require(numbers
-            .filterNot { num -> num in Lotto.LOTTO_NUMBER_RANGE_START until Lotto.LOTTO_NUMBER_RANGE_END + 1 }
-            .isNullOrEmpty()) {
-            "로또 번호는 1부터 ${Lotto.LOTTO_NUMBER_RANGE_END}까지 입니다."
-        }
-    }
+class WinningLotto(val winningNumbers: Lotto) {
+    fun getMatchesCount(lotto: Lotto) = lotto.numbers.count { num -> winningNumbers.numbers.contains(num) }
+    fun getMatchesCount(lottos: List<Lotto>) = lottos.map { lotto -> getMatchesCount(lotto) }
 }
