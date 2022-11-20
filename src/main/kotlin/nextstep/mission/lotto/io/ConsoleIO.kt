89bccff52fd89c9e4af3d24bce8664f14b08835f
kimseonghun <seonghun127@woowahan.com>
2022-11-20 15:26:30 +0900

    docs: IO 객체 추가 및 메인 함수 추가

---
+++ src/main/kotlin/nextstep/mission/lotto/io/ConsoleIO.kt
@@ -0,0 +1,31 @@
+package nextstep.mission.lotto.io
+
+import nextstep.mission.lotto.Lotto
+import nextstep.mission.lotto.WinningResult
+
+object ConsoleInput {
+    fun inputPrice(): Int {
+        println("구입금액을 입력해 주세요.")
+        return readLine()?.toInt() ?: 0
+    }
+
+    fun inputWinningNumbers(): List<Int> {
+        println("지난 주 당첨 번호를 입력해 주세요.")
+        return readLine()?.split(",")?.map { it.toInt() } ?: emptyList()
+    }
+}
+
+object ConsoleOutput {
+    fun printLottos(lottos: List<Lotto>) {
+        println("${lottos.size}개를 구매했습니다.")
+        lottos.forEach { println(it.numbers) }
+    }
+
+    fun printWinningResult(winningResult: WinningResult) {
+        println("==========")
+        println("3개 일치 (5000원) - ${winningResult.threeMatch}개")
+        println("4개 일치 (50000원) - ${winningResult.fourMatch}개")
+        println("5개 일치 (1500000원) - ${winningResult.fiveMatch}개")
+        println("6개 일치 (2000000000원) - ${winningResult.sixMatch}개")
+    }
+}
