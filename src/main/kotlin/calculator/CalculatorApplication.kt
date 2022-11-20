8b97883f43bf5942f6a9c015c6cf726906cf2937
Shin seongsu <tlstjdtn321@naver.com>
2022-11-21 01:00:21 +0900

    fix: ResultView 사용하여 콘솔 출력

--- src/main/kotlin/calculator/CalculatorApplication.kt
+++ src/main/kotlin/calculator/CalculatorApplication.kt
@@ -2,11 +2,13 @@ package calculator
 
 import calculator.domain.StringAddCalculator
 import calculator.view.InputView
+import calculator.view.ResultView
 
 fun main() {
     val inputView = InputView()
-    val stringAddCalculator = StringAddCalculator(inputView.inputString())
+    val resultView = ResultView()
 
+    val stringAddCalculator = StringAddCalculator(inputView.inputString())
     val result = stringAddCalculator.calculate()
-    println(result)
+    resultView.printResult(result)
 }
