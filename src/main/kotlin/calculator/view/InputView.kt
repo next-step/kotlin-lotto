709e62715c910ca11a2a965ef9efe03992b100ff
Shin seongsu <tlstjdtn321@naver.com>
2022-11-21 00:59:57 +0900

    fix: readln를 사용하여 null체크 제거

--- src/main/kotlin/calculator/view/InputView.kt
+++ src/main/kotlin/calculator/view/InputView.kt
@@ -4,11 +4,11 @@ class InputView {
 
     fun inputString(): String {
         print("연산할 문자를 입력해주세요: ")
-        val input = readLine()!!
+        val input = readln()
         return toNullOrBlank(input)
     }
 
-    fun toNullOrBlank(input: String?): String {
-        return if (input.isNullOrBlank()) "0" else input
+    fun toNullOrBlank(input: String): String {
+        return input.ifBlank { "0" }
     }
 }
