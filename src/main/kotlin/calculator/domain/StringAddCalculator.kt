a7441d23b3b81af1b6118904907de896b8ce15c2
James <bookwriterseo@gmail.com>
2022-11-22 02:07:01 +0900

    feat: 숫자 하나를 입력할 경우 해당 숫자 반환

--- src/main/kotlin/calculator/domain/StringAddCalculator.kt
+++ src/main/kotlin/calculator/domain/StringAddCalculator.kt
@@ -5,6 +5,20 @@ class StringAddCalculator {
         if (input.isNullOrBlank()) {
             return 0
         }
+
+        if (isOneNumber(input)) {
+            checkNegative(input.toInt())
+            return input.toInt()
+        }
+
         return -1
+    }
+
+    private fun isOneNumber(input: String): Boolean {
+        return input.length == 1
+    }
+
+    private fun checkNegative(number: Int) {
+        require(number >= 1) { "음수 입력 불가" }
     }
 }
