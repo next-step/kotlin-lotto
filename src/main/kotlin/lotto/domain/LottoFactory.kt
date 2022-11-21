8a0525fdd22b698afcfcebe8337571fe1deff3dd
shinaaaa <shin029abc@gmail.com>
2022-11-21 17:27:44 +0900

    chore: 빠진 LottoFactory 인터페이스 추가

---
+++ src/main/kotlin/lotto/domain/LottoFactory.kt
@@ -0,0 +1,13 @@
+package lotto.domain
+
+interface LottoFactory {
+
+    val NUMBER_RANGE: IntRange
+        get() = 1..45
+
+    val DRAWING_QUANTITY: Int
+        get() = 5
+
+    fun create(): List<Int>
+
+}
