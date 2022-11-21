1d27e6623c4c717bedd239a7b8bc8825e8f0bc71
dajeong <daaajeong@gmail.com>
2022-11-21 22:56:14 +0900

    [step2] 로또 당첨 통계 출력 기능

--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -1,6 +1,8 @@
 package lotto.view
 
 import lotto.domain.Lotto
+import lotto.domain.LottoStatisticsResult
+import lotto.domain.LottoStatisticsTotal
 
 class ResultView {
 
@@ -9,14 +11,41 @@ class ResultView {
         lottoList.forEach {
             println(it.numbers)
         }
+        emptyLine()
+    }
+
+    fun printLottoStatistics(statisticsResult: LottoStatisticsTotal) {
+        emptyLine()
+        println(STATISTICS_GUIDE)
+        println(SPLIT_LINE)
+        printWinStatisticsResult(statisticsResult.winLottoStatisticsResult)
+        printEarningRate(statisticsResult.earningRate)
+    }
+
+    private fun printWinStatisticsResult(winLottoStatisticsResult: List<LottoStatisticsResult>) {
+        winLottoStatisticsResult.forEach {
+            val hitCount = it.hitCount
+            val prizeMoney = it.prizeMoney
+            val winLottoCount = it.winLottoCount
+            println("${hitCount}개 일치 (${prizeMoney}원) - ${winLottoCount}개")
+        }
+    }
+
+    private fun printEarningRate(earningRate: Double) {
+        print("총 수익률은 ${earningRate}입니다. ")
+        if (earningRate > 1) {
+            println("(기준이 1이기 때문에 이익입니다.)")
+            return
+        }
+        println("(기준이 1이기 때문에 손해입니다.)")
+    }
+
+    private fun emptyLine() {
         println()
     }
 
-    fun printLottoStatistics() {
-        TODO("로또 당첨 통계 출력")
-    }
-
     companion object {
-        const val STATISTICS_GUIDE = "당첨 통계"
+        private const val STATISTICS_GUIDE = "당첨 통계"
+        private const val SPLIT_LINE = "---------"
     }
 }
