1a799285c7773dfd60a57f8644eff581fbf3b5fe
ssr <ssr03>
2022-11-21 13:51:07 +0900

    fix: 로또 생성 결과 정렬

--- src/main/kotlin/lotto/domain/LottoGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoGenerator.kt
@@ -4,12 +4,12 @@ object LottoGenerator {
     private const val LOTTO_START_NUMBER = 1
     private const val LOTTO_END_NUMBER = 45
     private const val LOTTO_NUMBER_INIT = 0
-    private const val LOTTO_NUMBER_COUNT = 6
+    const val LOTTO_NUMBER_COUNT = 6
 
     private val possibleNumbers: List<Int> =
         (LOTTO_START_NUMBER..LOTTO_END_NUMBER).map { it }
 
     fun generateLottoNumbers(): List<Int> =
         possibleNumbers.shuffled()
-            .subList(LOTTO_NUMBER_INIT, LOTTO_NUMBER_COUNT)
+            .subList(LOTTO_NUMBER_INIT, LOTTO_NUMBER_COUNT).sorted()
 }
