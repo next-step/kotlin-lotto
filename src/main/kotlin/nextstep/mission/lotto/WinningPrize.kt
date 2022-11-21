4dbf8239fbbeacac9f5b4c71833232dcc0fa473c
kimseonghun <seonghun127@woowahan.com>
2022-11-21 20:33:48 +0900

    feat: 당첨 상금별 조건과 금액을 관리하는 enum 추가

---
+++ src/main/kotlin/nextstep/mission/lotto/WinningPrize.kt
@@ -0,0 +1,8 @@
+package nextstep.mission.lotto
+
+enum class WinningPrize(val matchedCount: Int, val prize: Int) {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000);
+}
