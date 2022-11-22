084314ca30ab421aa32a6e5a5fa2d21fb501412b
namonak <namonak@gmail.com>
2022-11-22 16:30:14 +0900

    refactor(step1): apply a singleton to the InputView, ResultView class

--- src/main/kotlin/calculator/view/InputView.kt
+++ src/main/kotlin/calculator/view/InputView.kt
@@ -1,12 +1,10 @@
 package calculator.view
 
 object InputView {
-    companion object {
-        private const val INPUT_MESSAGE = "계산식을 입력해주세요."
+    private const val INPUT_MESSAGE = "계산식을 입력해주세요."
 
-        fun inputFormula(): String {
-            println(INPUT_MESSAGE)
-            return readln()
-        }
+    fun inputFormula(): String {
+        println(INPUT_MESSAGE)
+        return readln()
     }
 }
--- src/main/kotlin/calculator/view/ResultView.kt
+++ src/main/kotlin/calculator/view/ResultView.kt
@@ -1,11 +1,9 @@
 package calculator.view
 
-class ResultView {
-    companion object {
-        private const val RESULT_MESSAGE = "계산결과 :"
+object ResultView {
+    private const val RESULT_MESSAGE = "계산결과 :"
 
-        fun printResult(result: Int) {
-            println("$RESULT_MESSAGE $result")
-        }
+    fun printResult(result: Int) {
+        println("$RESULT_MESSAGE $result")
     }
 }
