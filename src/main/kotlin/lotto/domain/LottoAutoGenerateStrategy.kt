366e826272805fe4e07eec2fea75e4e710e017b6
SangHyunGil <zizon5941@gmail.com>
2022-11-22 23:02:44 +0900

    feat: 로또 번호를 생성하는 전략인 LottoGenerateStrategy 구현

--- src/main/kotlin/lotto/domain/LottoAutoGenerateStrategy.kt
+++ src/main/kotlin/lotto/domain/LottoAutoGenerateStrategy.kt
@@ -2,6 +2,15 @@ package lotto.domain
 
 class LottoAutoGenerateStrategy : LottoGenerateStrategy {
     override fun generate(): Set<LottoNumber> {
-        throw Exception()
+        return (START_NUMBER..END_NUMBER).shuffled()
+            .take(LOTTO_NUMBER_COUNT)
+            .map { LottoNumber(it) }
+            .toSet()
+    }
+
+    companion object {
+        private const val LOTTO_NUMBER_COUNT = 6
+        private const val START_NUMBER = 1
+        private const val END_NUMBER = 45
     }
 }
