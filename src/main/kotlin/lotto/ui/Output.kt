a0f147718c4a7736774575be3ee7d179a824339b
soosue <yunsoo0510@daum.net>
2022-11-22 17:50:13 +0900

    refactor: 수익률에 따라 추가 메시지 다르게 나오게 변경

--- src/main/kotlin/lotto/ui/Output.kt
+++ src/main/kotlin/lotto/ui/Output.kt
@@ -7,7 +7,9 @@ object Output {
     private const val BUY_COUNT_MESSAGE = "개를 구매했습니다."
     private const val WINNING_STATISTICS_MESSAGE = "당첨 통계\n---------\n"
     private const val MATCH_COUNT_MESSAGE = "%d개 일치 (%d원)- %d개\n"
-    private const val PROFIT_MESSAGE = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)"
+    private const val PROFIT_MESSAGE = "총 수익률은 %.2f입니다."
+    private const val PROFIT_GOOD_MESSAGE = "(기준이 1이기 때문에 결과적으로 이득라는 의미임)"
+    private const val PROFIT_BAD_MESSAGE = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)"
 
     fun printBuyCount(buyCount: Long) {
         println("$buyCount" + BUY_COUNT_MESSAGE)
@@ -35,6 +37,7 @@ object Output {
         }
 
         sb.append(PROFIT_MESSAGE.format(winningStatistics.profit))
+        sb.append(if(winningStatistics.profit > 1) PROFIT_GOOD_MESSAGE else PROFIT_BAD_MESSAGE )
 
         println(sb.toString())
     }
