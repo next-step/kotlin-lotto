2e02fd18293208d94da7b04510b64dbb546b6650
SangHyunGil <zizon5941@gmail.com>
2022-11-22 23:17:28 +0900

    feat: 수동 생성 전략 추가

--- src/main/kotlin/lotto/domain/LottoManualGenerateStrategy.kt
+++ src/main/kotlin/lotto/domain/LottoManualGenerateStrategy.kt
@@ -1,9 +1,23 @@
 package lotto.domain
 
-import java.lang.Exception
+import lotto.util.ExceptionMessage
+import lotto.util.Parser
+import lotto.util.Reader
 
 class LottoManualGenerateStrategy : LottoGenerateStrategy {
     override fun generate(): Set<LottoNumber> {
-        throw Exception()
+        println(INPUT_MESSAGE)
+        val winnerLottoNumbersStr = Reader.read()
+        val lottoNumbers = Parser.parse(winnerLottoNumbersStr)
+        validateLottoNumbers(lottoNumbers)
+        return lottoNumbers.map { LottoNumber(it) }.toSet()
+    }
+
+    private fun validateLottoNumbers(lottoNumbers: List<Int>) {
+        require(lottoNumbers.toSet().size == 6) { ExceptionMessage.LOTTO_NUMBER_SIZE_ERROR }
+    }
+
+    companion object {
+        private const val INPUT_MESSAGE = "지난 주 당첨 번호를 입력해 주세요."
     }
 }
