8dd4d20495217c3a3eff0c8a6c0cc6c5dcac4d09
shinaaaa <shin029abc@gmail.com>
2022-11-22 14:15:30 +0900

    feat: 숫자인지 여부, 값이 범위 안에 있는지 여부, 배열의 길이랑 기준 값이랑 같은 지 여부를 검증하는 기능 추가

---
+++ src/main/kotlin/lotto/utils/Validation.kt
@@ -0,0 +1,13 @@
+package lotto.utils
+
+object Validation {
+
+    fun isNumeric(str: String) =
+        str.all { Character.isDigit(it) }
+
+    fun isWithInRange(number: Int, range: IntRange) =
+        range.contains(number)
+
+    fun isSameNumberOfArraysAndReferenceValue(numberOfArrays: Int, referenceValue: Int) =
+        numberOfArrays == referenceValue
+}
