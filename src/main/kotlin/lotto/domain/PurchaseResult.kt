6a3d7bc1f195740d0053b576158e5268ff83d2d0
shinaaaa <shin029abc@gmail.com>
2022-11-22 17:23:13 +0900

    feat: PurchaseResult에 공백값이 들어왔을 경우 "공백 값이 들어왔습니다." 내용 추가 및 isNumber(), LOTTO_PRICE 제거

--- src/main/kotlin/lotto/domain/PurchaseResult.kt
+++ src/main/kotlin/lotto/domain/PurchaseResult.kt
@@ -1,24 +1,24 @@
 package lotto.domain
 
-class PurchaseResult(amountPaid: String) {
+import lotto.domain.LottoGame.LOTTO_PRICE
+import lotto.utils.Validation
+import java.math.BigDecimal
 
+class PurchaseResult(cost: String) {
+
+    var purchaseCost: BigDecimal = BigDecimal.ZERO
+        private set
     var numberOfGames: Int = 0
         private set
     var change: Int = 0
         private set
 
     init {
-        require(isNumber(amountPaid)) { "숫자가 아닌 문자가 들어왔습니다." }
-        require(amountPaid.toInt() >= LOTTO_PRICE) { "최소 주문 금액보다 적습니다." }
-        numberOfGames = amountPaid.toInt() / LOTTO_PRICE
-        change = amountPaid.toInt() % LOTTO_PRICE
-    }
-
-    private fun isNumber(str: String) =
-        str.chars().allMatch(Character::isDigit)
-
-
-    companion object {
-        const val LOTTO_PRICE = 1000
+        require(Validation.isNotBlank(cost)) { "공백 값이 들어왔습니다." }
+        require(Validation.isNumeric(cost)) { "숫자가 아닌 문자가 들어왔습니다." }
+        require(cost.toInt() >= LOTTO_PRICE) { "최소 주문 금액보다 적습니다." }
+        numberOfGames = cost.toInt() / LOTTO_PRICE
+        change = cost.toInt() % LOTTO_PRICE
+        purchaseCost = cost.toBigDecimal()
     }
 }
