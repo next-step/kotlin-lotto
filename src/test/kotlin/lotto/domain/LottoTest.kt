ca80a433fd5b76aa51642ab95c7ef3f37166a992
soosue <yunsoo0510@daum.net>
2022-11-22 17:53:55 +0900

    refactor: Lotto string constructor 에 대한 테스트 코드 추가

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,6 +1,8 @@
 package lotto.domain
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.collections.shouldContainInOrder
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.assertThrows
@@ -12,6 +14,19 @@ class LottoTest : StringSpec({
 
         lotto.numbers shouldHaveSize 6
         lotto.numbers shouldBe lotto.numbers.distinct()
+    }
+
+    ", 을 구분자로 하여 문자로 로또를 생성할 수 있다" {
+        val lotto = Lotto("1,2,3,4,5,6")
+
+        lotto.numbers shouldContainInOrder listOf(
+            LottoNumber.from(1),
+            LottoNumber.from(2),
+            LottoNumber.from(3),
+            LottoNumber.from(4),
+            LottoNumber.from(5),
+            LottoNumber.from(6)
+        )
     }
 
     "중복되지 않은 숫자여야 한다" {
