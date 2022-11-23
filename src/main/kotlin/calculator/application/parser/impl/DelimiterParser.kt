fbadaebfd738ec96714bdcc16075c3d5e2edcc21
EP <kjuioqq8@gmail.com>
2022-11-24 01:47:49 +0900

    refactor: delimiter 탐색 로직 변경

--- src/main/kotlin/calculator/application/parser/impl/DelimiterParser.kt
+++ src/main/kotlin/calculator/application/parser/impl/DelimiterParser.kt
@@ -2,40 +2,24 @@ package calculator.application.parser.impl
 
 import calculator.application.parser.Parser
 import calculator.application.parser.model.CustomDelimiter
-import calculator.application.parser.model.Delimiter
 import calculator.application.parser.model.DelimiterEnum
 import calculator.common.model.PositiveInteger
 
 object DelimiterParser : Parser {
 
-    private const val CUSTOM_DELIMITER_PREFIX = "//"
-    private const val CUSTOM_DELIMITER_SUFFIX = "\n"
-    private const val REGEX_PATTERN = "$CUSTOM_DELIMITER_PREFIX(.*)$CUSTOM_DELIMITER_SUFFIX(.*)"
-    private const val EMPTY_STRING = ""
-
     override fun parseToPositiveIntegerList(inputString: String): List<PositiveInteger> {
         var input = inputString
         var delimiters = DelimiterEnum.valuesStringArray()
 
-        if (hasCustomDelimiter(input)) {
-            val customDelimiter = findCustomDelimiters(input)
-            input = cleanDelimiter(input, customDelimiter.value)
+        if (CustomDelimiter.hasCustomDelimiter(input)) {
+            val customDelimiter = CustomDelimiter.findCustomDelimiters(input)
+            input = CustomDelimiter.cleanDelimiter(input, customDelimiter.value)
             delimiters = delimiters.plus(customDelimiter.value)
         }
 
         val numberArray = input.split(*delimiters)
         return numberArray.map { it.toPositiveInteger() }.toList()
     }
-
-    private fun hasCustomDelimiter(inputString: String) = Regex(REGEX_PATTERN).find(inputString) != null
-
-    private fun findCustomDelimiters(inputString: String): Delimiter {
-        val delimiter = Regex(REGEX_PATTERN).find(inputString)?.groupValues?.get(1) ?: throw RuntimeException("")
-        return CustomDelimiter(delimiter)
-    }
-
-    private fun cleanDelimiter(inputString: String, customDelimiterValue: String) = inputString
-        .replace("$CUSTOM_DELIMITER_PREFIX${customDelimiterValue}$CUSTOM_DELIMITER_SUFFIX", EMPTY_STRING)
 
     private fun String.toPositiveInteger(): PositiveInteger =
         try {
--- src/main/kotlin/calculator/application/parser/model/CustomDelimiter.kt
+++ src/main/kotlin/calculator/application/parser/model/CustomDelimiter.kt
@@ -2,4 +2,24 @@ package calculator.application.parser.model
 
 class CustomDelimiter(
     override val value: String
-) : Delimiter
+) : Delimiter {
+
+    companion object {
+
+        fun hasCustomDelimiter(inputString: String) = Regex(REGEX_PATTERN).find(inputString) != null
+
+        fun findCustomDelimiters(inputString: String): Delimiter {
+            val delimiter = Regex(REGEX_PATTERN).find(inputString)?.groupValues?.get(1) ?: throw RuntimeException("구분자 조회 중 에러가 발생했습니다")
+            return CustomDelimiter(delimiter)
+        }
+
+        fun cleanDelimiter(inputString: String, customDelimiterValue: String): String {
+            return inputString.replace("$CUSTOM_DELIMITER_PREFIX${customDelimiterValue}$CUSTOM_DELIMITER_SUFFIX", EMPTY_STRING)
+        }
+
+        private const val CUSTOM_DELIMITER_PREFIX = "//"
+        private const val CUSTOM_DELIMITER_SUFFIX = "\n"
+        private const val REGEX_PATTERN = "$CUSTOM_DELIMITER_PREFIX(.*)$CUSTOM_DELIMITER_SUFFIX(.*)"
+        private const val EMPTY_STRING = ""
+    }
+}
