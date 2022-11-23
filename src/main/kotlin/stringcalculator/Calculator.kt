84b862d21d0151cc609bf09cea0c8357fbc5a11f
jay <jaeykweon@naver.com>
2022-11-24 00:11:46 +0900

    draft: 리뷰 적용 중

--- src/main/kotlin/stringcalculator/Calculator.kt
+++ src/main/kotlin/stringcalculator/Calculator.kt
@@ -4,8 +4,9 @@ class Calculator {
 
     fun addAll(inputString: String?): Int {
         if (inputString.isNullOrBlank()) return 0
+        val separated = DefaultSeparator.separate(inputString)
 
-        val inputNumbers = InputNumbers.from(inputString)
+        val inputNumbers = InputNumbers.from(separated)
 
         return inputNumbers.values.fold(0) { total, inputNumber ->
             total + inputNumber.value
--- src/main/kotlin/stringcalculator/InputNumbers.kt
+++ src/main/kotlin/stringcalculator/InputNumbers.kt
@@ -3,20 +3,10 @@ package stringcalculator
 data class InputNumbers(
     val values: List<InputNumber>
 ) {
-
     companion object {
-        const val DEFAULT_SEPARATORS = "[,:]"
-        const val CUSTOM_SEPARATOR_REGEX = "//(.)\n(.*)"
-
-        fun from(text: String): InputNumbers {
-            val (separator, tokens) = Regex(CUSTOM_SEPARATOR_REGEX).find(text)?.let {
-                it.groupValues[1] to it.groupValues[2]
-            } ?: (DEFAULT_SEPARATORS to text)
-
+        fun from(inputs: List<String>): InputNumbers {
             return InputNumbers(
-                tokens.split(separator.toRegex()).map {
-                    InputNumber(it)
-                }
+                inputs.map { input -> InputNumber(input) }
             )
         }
     }
