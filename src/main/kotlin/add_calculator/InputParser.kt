3da9850924725676f77429aaa3885b5f98ea5f2b
jpoh281 <hjp4869@naver.com>
2022-11-24 01:17:47 +0900

    feat: 커스텀 구분자를 파싱기능 추가

--- src/main/kotlin/add_calculator/InputParser.kt
+++ src/main/kotlin/add_calculator/InputParser.kt
@@ -1,8 +1,28 @@
 package add_calculator
 
-class InputParser {
+class InputParser() {
 
-    fun parse(text: String?): List<String> {
-        return text!!.split(",|:".toRegex())
+    fun parse(input: String? = readLine()): List<Operand> {
+
+        if (input.isNullOrBlank()) {
+            return listOf(Operand("0"))
+        }
+
+        val result = Regex("//(.)\n(.*)").find(input)
+
+        result?.let {
+            val customDelimiter = it.groupValues[1]
+            val texts = it.groupValues[2].split(customDelimiter)
+            return toOperands(texts)
+        }
+
+        val texts = input.split(",|:".toRegex())
+        return toOperands(texts)
+    }
+
+    private fun toOperands(texts: List<String>): List<Operand> {
+        return texts.map { text ->
+            Operand(text)
+        }
     }
 }
--- src/main/kotlin/add_calculator/Operand.kt
+++ src/main/kotlin/add_calculator/Operand.kt
@@ -3,9 +3,24 @@ package add_calculator
 import java.lang.RuntimeException
 
 class Operand(text: String) {
-    private val value: UInt
+    val value: UInt
 
     init {
         value = text.toUIntOrNull() ?: throw RuntimeException()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Operand
+
+        if (value != other.value) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
     }
 }
