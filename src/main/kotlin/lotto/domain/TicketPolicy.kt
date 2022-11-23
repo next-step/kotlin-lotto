e1da4a7ac4391bc77e40c3b913ffca674bbad7aa
jysohn0825 <jysohn0825@naver.com>
2022-11-23 23:56:50 +0900

    refactor: ktlintCheck

--- src/main/kotlin/lotto/domain/TicketPolicy.kt
+++ src/main/kotlin/lotto/domain/TicketPolicy.kt
@@ -5,5 +5,4 @@ object TicketPolicy {
     const val LOTTO_COUNT = 6
 
     fun generateNumber() = NumberPolicy.LOTTO_RANGE.random()
-
 }
--- src/test/kotlin/lotto/domain/TicketTest.kt
+++ src/test/kotlin/lotto/domain/TicketTest.kt
@@ -8,16 +8,15 @@ class TicketTest : StringSpec({
 
     val ticket = Ticket()
 
-    "티켓의 번호는 총 6개여야 한다"{
+    "티켓의 번호는 총 6개여야 한다" {
         ticket.numbers.size shouldBe 6
     }
 
-    "티켓의 번호는 1부터 45 사이의 숫자여야 한다"{
+    "티켓의 번호는 1부터 45 사이의 숫자여야 한다" {
         ticket.numbers.filter { it in NumberPolicy.LOTTO_RANGE }.size shouldBe 6
     }
 
-    "티켓은 정렬되어 있어야 한다"{
+    "티켓은 정렬되어 있어야 한다" {
         ticket.numbers.shouldBeSorted()
     }
-
 })
