bc07c59d84a4e9167c0fe26e501024599f0f8ada
dreamboy1339 <dreamboy1339@gmail.com>
2022-11-23 16:31:27 +0900

    study(step2): 입력된 문장에서 당첨 번호 분리

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,19 +1,12 @@
-package stringaddcalculator
+package calculator
 
-class StringAddCalculator {
+class Calculator {
 
     fun add(text: String?): Int {
         if (text.isNullOrBlank()) return 0
         if (text.length == 1) return validate(text)
 
-        val result: MatchResult? = Regex("//(.)\n(.*)").find(text)
-        result?.let {
-            val customDelimiter: String = it.groupValues[1]
-            val tokens = it.groupValues[2].split(customDelimiter)
-            return addNumbers(tokens)
-        }
-
-        val numbers: List<String> = text.split("[,:]".toRegex())
+        val numbers: List<String> = Tokenizer().tokenize(text)
         return addNumbers(numbers)
     }
 
---
+++ src/main/kotlin/calculator/Tokenizer.kt
@@ -0,0 +1,23 @@
+package calculator
+
+class Tokenizer {
+
+    fun tokenize(input: String): List<String> {
+        return internalTokenize(input).map { it.trim() }
+    }
+
+    private fun internalTokenize(input: String): List<String> {
+        val result: List<String>? = customTokenize(input)
+        if (result != null) {
+            return result
+        }
+
+        return input.split("[,:]".toRegex())
+    }
+
+    private fun customTokenize(text: String): List<String>? {
+        val result: MatchResult = Regex("//(.)\n(.*)").find(text) ?: return null
+        val customDelimiter: String = result.groupValues[1]
+        return result.groupValues[2].split(customDelimiter)
+    }
+}
