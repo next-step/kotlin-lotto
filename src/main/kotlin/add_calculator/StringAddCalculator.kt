50f9a30f43d21267366d6f5d14427c43ce112735
jpoh281 <hjp4869@naver.com>
2022-11-24 21:50:54 +0900

    refactor: 0 매직넘버로 변경

--- src/main/kotlin/add_calculator/StringAddCalculator.kt
+++ src/main/kotlin/add_calculator/StringAddCalculator.kt
@@ -1,10 +1,12 @@
 package add_calculator
 
+private const val INITIAL_VALUE = 0
+
 class StringAddCalculator(private val inputParser: InputParser) {
 
     fun calculate(input: String?): Int {
         val operands: List<Operand> = inputParser.parse(input)
-        return operands.fold(0) { acc: Int, operand: Operand ->
+        return operands.fold(INITIAL_VALUE) { acc: Int, operand: Operand ->
             operand.add(acc)
         }
     }
