a0f7eb81f7b2536182aa3b96b5b29a10df0997c5
wrallee <wrallee@naver.com>
2022-11-24 22:04:01 +0900

    fix(StringCalculator): PositiveNumbers 관련 테스트 코드 정정

--- src/test/kotlin/stringcalculator/model/PositiveNumbersTest.kt
+++ src/test/kotlin/stringcalculator/model/PositiveNumbersTest.kt
@@ -3,6 +3,9 @@ package stringcalculator.model
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import stringcalculator.service.StringParser
 
 class PositiveNumbersTest {
     @Test
@@ -16,6 +19,15 @@ class PositiveNumbersTest {
     @Test
     internal fun `숫자가 아닌 값이 포함된 리스트`() {
         val strings = listOf("1", "&", "3", "4")
+        assertThrows<IllegalArgumentException> { PositiveNumbers.of(strings) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = ["1;2,3,4", """/;\n1;2;3,4""", """//;\n//e\n1e2e3e4""", """//;\n//e\n//a\n1a2a3a4"""]
+    )
+    internal fun `잘못 변환된 리스트로 인스턴스 생성`(input: String) {
+        val strings = StringParser.convertToList(input)
         assertThrows<IllegalArgumentException> { PositiveNumbers.of(strings) }
     }
 }
--- src/test/kotlin/stringcalculator/service/StringParserTest.kt
+++ src/test/kotlin/stringcalculator/service/StringParserTest.kt
@@ -1,11 +1,8 @@
 package stringcalculator.service
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-import stringcalculator.model.PositiveNumbers
 import stringcalculator.service.StringParser.CUSTOM_DELIMITED_TEXT_REGEX
 import stringcalculator.service.StringParser.DEFAULT_DELIMITER
 
@@ -14,15 +11,9 @@ class StringParserTest {
     @ValueSource(strings = ["1,2,3,4", "5:6,7:8", "9:10:11:12"])
     internal fun `쉼표 또는 콜론으로 문자열 분리`(input: String) {
         val numbers = StringParser.convertToList(input)
-        val expectedNumbers = PositiveNumbers.of(input.split(DEFAULT_DELIMITER))
+        val expectedNumbers = input.split(DEFAULT_DELIMITER)
 
         assertThat(numbers).isEqualTo(expectedNumbers)
-    }
-
-    @Test
-    internal fun `쉼표 또는 콜론이 아닐 경우`() {
-        val input = "1;2,3,4"
-        assertThrows<IllegalArgumentException> { StringParser.convertToList(input) }
     }
 
     @ParameterizedTest
@@ -30,14 +21,8 @@ class StringParserTest {
     internal fun `커스텀 구분자로 문자열 분리`(input: String) {
         val numbers = StringParser.convertToList(input)
         val (delimiter, delimitedBody) = CUSTOM_DELIMITED_TEXT_REGEX.find(input)!!.destructured
-        val expectedNumbers = PositiveNumbers.of(delimitedBody.split(delimiter))
+        val expectedNumbers = delimitedBody.split(delimiter)
 
         assertThat(numbers).isEqualTo(expectedNumbers)
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["""/;\n1;2;3,4""", """//;\n//e\n1e2e3e4""", """//;\n//e\n//a\n1a2a3a4"""])
-    internal fun `커스텀 구분자 접두사가 완전하지 않은 경우`(input: String) {
-        assertThrows<IllegalArgumentException> { StringParser.convertToList(input) }
     }
 }
