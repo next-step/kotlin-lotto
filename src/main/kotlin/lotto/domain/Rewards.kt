2272117541faa536781435cb18ea3e6dbf71e980
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-25 00:26:52 +0900

    feat(Rewards): 구매금액과 당첨금으로 수익률을 반환한다.

---
+++ src/main/kotlin/lotto/domain/Rewards.kt
@@ -0,0 +1,16 @@
+package lotto.domain
+
+data class Rewards(private val _rewards: MutableList<Rank> = mutableListOf()) {
+    val value: List<Rank>
+        get() = _rewards
+
+    fun add(matches: Int) {
+        Rank.fromOrNull(matches)?.let {
+            _rewards.add(it)
+        }
+    }
+
+    fun calculateRateOfReturn(purchaseAmount: Double): Double {
+        return _rewards.sumOf { it.getMoney() } / purchaseAmount
+    }
+}
--- src/test/kotlin/lotto/domain/RankTest.kt
+++ src/test/kotlin/lotto/domain/RankTest.kt
@@ -3,7 +3,7 @@ package lotto.domain
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
-internal class RankTest: BehaviorSpec({
+internal class RankTest : BehaviorSpec({
     given("fromOfNull() 함수에") {
         `when`("numberOfMatch 에 있는 수를 인자로 넣으면") {
             val numberOfMatch = 3
