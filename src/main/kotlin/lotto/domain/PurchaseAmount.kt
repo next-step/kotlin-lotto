73992ce9cf1fa412a0170aab255853eab2dce184
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-25 00:54:51 +0900

    feat(PurchaseAmount): 구입금액은 1000원 단위여야 한다.

--- src/main/kotlin/lotto/domain/PurchaseAmount.kt
+++ src/main/kotlin/lotto/domain/PurchaseAmount.kt
@@ -5,8 +5,16 @@ value class PurchaseAmount(private val _amount: Double) {
     private val amount: Double
         get() = _amount
 
+    init {
+        require(_amount % LOTTO_PRICE == ZERO)  { INVALID_VALUE_ERROR_MESSAGE }
+    }
+
     companion object {
         private const val INVALID_INPUT_ERROR_MESSAGE = "구입 금액은 숫자여야 합니다."
+        private const val INVALID_VALUE_ERROR_MESSAGE = "구입 금액은 1000원 단위만 가능합니다."
+
+        private const val LOTTO_PRICE = 1000.00
+        private const val ZERO = 0.00
 
         fun from(input: String): PurchaseAmount {
             val amount = input.toDoubleOrNull()
--- src/test/kotlin/lotto/domain/PurchaseAmountTest.kt
+++ src/test/kotlin/lotto/domain/PurchaseAmountTest.kt
@@ -6,16 +6,27 @@ import io.kotest.matchers.should
 import io.kotest.matchers.string.startWith
 import java.lang.IllegalArgumentException
 
-internal class PurchaseAmountTest : BehaviorSpec({
+internal class PurchaseAmountTest: BehaviorSpec({
     given("구입 금액이") {
-        `when`("숫자로 변환될 수 없는 문자가 입력되면") {
+        `when`("숫자로 변환될 수 없는 문자가 입력되면"){
             val input = "24DFseff~345"
 
-            then("IllegalArgumentException 을 반환한다.") {
+            then("IllegalArgumentException 을 반환한다."){
                 val exception = shouldThrow<IllegalArgumentException> {
                     PurchaseAmount.from(input)
                 }
                 exception.message should startWith("구입 금액은 숫자여야 합니다.")
+            }
+        }
+
+        `when`("1000원으로 나누어 떨어지는 수가 아니라면"){
+            val input = "1444444"
+
+            then("IllegalArgumentException 을 반환한다."){
+                val exception = shouldThrow<IllegalArgumentException> {
+                    PurchaseAmount.from(input)
+                }
+                exception.message should startWith("구입 금액은 1000원 단위만 가능합니다.")
             }
         }
     }
