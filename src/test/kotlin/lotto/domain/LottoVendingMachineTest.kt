916d34246650b4f52c42279c71b63f6d0c62e16b
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-25 02:31:01 +0900

    test(LottoVendingMachineTest): 구입 금액의 타입을 PurchaseAmount 로 변경

--- src/test/kotlin/lotto/domain/LottoVendingMachineTest.kt
+++ src/test/kotlin/lotto/domain/LottoVendingMachineTest.kt
@@ -6,9 +6,10 @@ import io.kotest.matchers.shouldBe
 internal class LottoVendingMachineTest : BehaviorSpec({
     given("로또 구매 금액을 입력하면") {
         `when`("구매한 금액의 만큼의") {
-            val money = 14000
+            val money = PurchaseAmount(14000.00)
+
             then("로또가 발급된다.") {
-                LottoVendingMachine.buy(money).size shouldBe money / 1000
+                LottoVendingMachine.buy(money).size shouldBe money.amount / 1000.00
             }
         }
     }
