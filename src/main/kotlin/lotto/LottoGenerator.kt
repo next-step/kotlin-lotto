324ee984787852dcb17f3013c2079ea3088d0496
jpoh281 <hjp4869@naver.com>
2022-11-24 22:48:42 +0900

    feat: 서로 같지 않은 길이 6의 정수배열을 반환하는 generator 메소드 생성

--- src/main/kotlin/lotto/LottoGenerator.kt
+++ src/main/kotlin/lotto/LottoGenerator.kt
@@ -2,4 +2,8 @@ package lotto
 
 class LottoGenerator {
     val range = IntRange(1, 45)
+
+    fun generate(): List<Int> {
+        return range.shuffled().subList(0, 6)
+    }
 }
--- src/test/kotlin/lotto/LottoGeneratorTest.kt
+++ src/test/kotlin/lotto/LottoGeneratorTest.kt
@@ -10,4 +10,11 @@ class LottoGeneratorTest {
         assertThat(LottoGenerator().range.first).isEqualTo(1)
         assertThat(LottoGenerator().range.last).isEqualTo(45)
     }
+
+    @Test
+    fun `서로 같지않은 길이 6의 정수 배열을 반환하는 generatorLotto 메소드 생성`() {
+        var lottoNumbers = LottoGenerator().generate()
+        assertThat(lottoNumbers.size).isEqualTo(6)
+        assertThat(lottoNumbers.toSet().size).isEqualTo(6)
+    }
 }
