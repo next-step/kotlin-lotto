82ec2add1485ec7e337adc4fedf0a816b1581e55
chayoung-choi <chayoung.dev@gmail.com>
2022-11-24 18:09:09 +0900

    feat: 보너스 볼 일치 여부

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -1,6 +1,20 @@
 package lotto.domain
 
-class WinningLotto(private val winningNumbers: Lotto) {
-    fun getCountOfMatch(lotto: Lotto): Int = lotto.numbers.count { winningNumbers.numbers.contains(it) }
-    fun getCountOfMatch(lottos: List<Lotto>): List<Int> = lottos.map { getCountOfMatch(it) }
+import lotto.dto.MatchResultDto
+
+class WinningLotto(private val winningNumbers: Lotto, private val bonusNumber: LottoNumber) {
+
+    init {
+        require(!winningNumbers.numbers.contains(bonusNumber)) { "보너스 볼은 당첨 번호랑 같을 수 없습니다." }
+    }
+
+    fun getMatchResult(lotto: Lotto): MatchResultDto =
+        MatchResultDto(getCountOfMatch(lotto), matchBonus(lotto))
+
+    fun getMatchResult(lottos: List<Lotto>): List<MatchResultDto> =
+        lottos.map { getMatchResult(it) }
+
+    private fun getCountOfMatch(lotto: Lotto): Int = lotto.numbers.count { winningNumbers.numbers.contains(it) }
+    private fun matchBonus(lotto: Lotto): Boolean = lotto.numbers.contains(bonusNumber)
+
 }
--- src/test/kotlin/lotto/domain/WinningLottoTest.kt
+++ src/test/kotlin/lotto/domain/WinningLottoTest.kt
@@ -1,16 +1,43 @@
 package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class WinningLottoTest {
+
+    private lateinit var winningNumber: Lotto
+
+    @BeforeAll
+    fun setup() {
+        winningNumber = Lotto(listOf(1, 2, 3, 4, 5, 6).map { LottoNumber(it) }.toSet())
+    }
 
     @ParameterizedTest
     @CsvSource("1 2 3 4 5 6, 6", "1 2 3 4 5 16, 5", "1 2 3 4 25 16, 4", "1 2 3 44 25 16, 3", "11 22 33 44 25 16, 0")
     fun `당첨 로또와 비교하여 일치하는 번호의 갯수를 가져온다`(numbers: String, countOfMatch: Int) {
-        val winningLotto = WinningLotto(Lotto(listOf(1, 2, 3, 4, 5, 6).map { LottoNumber(it) }.toSet()))
+        val winningLotto = WinningLotto(winningNumber, LottoNumber(45))
         val myLottoNumbers = Lotto(numbers.split(" ").map { LottoNumber(it.toInt()) }.toSet())
-        assertThat(winningLotto.getCountOfMatch(myLottoNumbers)).isEqualTo(countOfMatch)
+        val matchResult = winningLotto.getMatchResult(myLottoNumbers)
+        assertThat(matchResult.countOfMatch).isEqualTo(countOfMatch)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 3, 6])
+    fun `당첨 로또와 보너스 볼이 같으면 예외 발생`(bonusNumber: Int) {
+        assertThrows<IllegalArgumentException> { WinningLotto(winningNumber, LottoNumber(bonusNumber)) }
+    }
+
+    @ParameterizedTest
+    @CsvSource("11,true", "23,true", "36,true", "44,false", "21,false", "35,false")
+    fun `보너스 볼의 일치 여부 값을 가진다`(bonusNumber: Int, matchBonus: Boolean) {
+        val myLotto = Lotto(listOf(11, 12, 23, 14, 15, 36).map { LottoNumber(it) }.toSet())
+        val matchResult = WinningLotto(winningNumber, LottoNumber(bonusNumber)).getMatchResult(myLotto)
+        assertThat(matchResult.matchBonus).isEqualTo(matchBonus)
     }
 }
