0e0d559c3e4786bcc0be444e37f0710f26c3bdb6
wrallee <wrallee@naver.com>
2022-11-24 22:05:20 +0900

    refactor(StringCalculator): PositiveNumber, PositiveNumbers data class로 변경

--- src/main/kotlin/stringcalculator/model/PositiveNumber.kt
+++ src/main/kotlin/stringcalculator/model/PositiveNumber.kt
@@ -3,28 +3,13 @@ package stringcalculator.model
 import java.math.BigDecimal
 import java.math.BigDecimal.ZERO
 
-class PositiveNumber(val value: BigDecimal) {
+data class PositiveNumber(val value: BigDecimal) {
 
     init {
         require(value >= ZERO) { "value 값은 음수가 될 수 없습니다" }
     }
 
     fun add(augend: PositiveNumber) = PositiveNumber(value.plus(augend.value))
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as PositiveNumber
-
-        if (value != other.value) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return value.hashCode()
-    }
 
     companion object {
         fun of(value: String): PositiveNumber {
--- src/main/kotlin/stringcalculator/model/PositiveNumbers.kt
+++ src/main/kotlin/stringcalculator/model/PositiveNumbers.kt
@@ -1,25 +1,10 @@
 package stringcalculator.model
 
-class PositiveNumbers(
+data class PositiveNumbers(
     val value: List<PositiveNumber>
 ) {
     fun reduceAdd(): PositiveNumber {
         return value.reduce(PositiveNumber::add)
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as PositiveNumbers
-
-        if (value != other.value) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return value.hashCode()
     }
 
     companion object {
