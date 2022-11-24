637a858073ae53b00293a98f9696ce5fa0a72797
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-25 02:35:22 +0900

    feat(LottoController): 로또를 실행할 수 있는 controller 생성

---
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -0,0 +1,27 @@
+package lotto.controller
+
+import lotto.domain.LottoVendingMachine
+import lotto.domain.Rewards
+import lotto.view.InputView
+import lotto.view.ResultView
+
+class LottoController {
+    fun run() {
+        val purchaseAmount = InputView.getPurchaseAmount()
+
+        val myLotto = LottoVendingMachine.buy(purchaseAmount)
+        ResultView.printNumberOfPurchases(myLotto.size)
+        ResultView.printPurchasedLotto(myLotto)
+
+        val winningNumbers = InputView.getWinningNumbers()
+        val rewards = Rewards()
+        myLotto.forEach {
+            val numberOfMath = winningNumbers.getSumMatchingNumbers(it)
+            rewards.add(numberOfMath)
+        }
+
+        ResultView.printRewardsStatistics(rewards)
+        val reteOfReturn = rewards.calculateRateOfReturn(purchaseAmount)
+        ResultView.printRateOfReturn(reteOfReturn)
+    }
+}
