420a84934a0cb46a13b844a25dd16615ccaedd55
Jo-GyuHyeon <kuhyun1993@gmail.com>
2022-11-25 03:35:58 +0900

    feat: 보너스 번호 추가 비교
    - Lotto (보너스)비교 테스트 추가
    - Reward (보너스)추가

--- src/main/kotlin/lotto/domain/Reward.kt
+++ src/main/kotlin/lotto/domain/Reward.kt
@@ -4,8 +4,12 @@ enum class Reward(
     val matchCount: Int,
     val amount: Int
 ) {
+
     FIST_PLACE(6, 2_000_000_000),
+    SECOND_BONUS_PLACE(5, 30_000_000),
     SECOND_PLACE(5, 1_500_000),
     THIRD_PLACE(4, 50_000),
     FORTH_PLACE(3, 5_000);
+
+    fun hasBonus() = this == SECOND_BONUS_PLACE
 }
