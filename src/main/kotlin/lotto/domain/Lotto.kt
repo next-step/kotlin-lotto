3ca772a7a35cd0de90c1ef511fd57641339f7359
chayoung-choi <chayoung.dev@gmail.com>
2022-11-24 18:08:24 +0900

    refactor: LottoNumber

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,16 +3,7 @@ package lotto.domain
 class Lotto(val numbers: Set<LottoNumber> = createLottoNumbers()) {
     init {
         require(numbers.size == LOTTO_NUMBER_COUNT) { "로또 당첨 번호는 6개입니다." }
-        // require(checkRangeLottoNumber(numbers)) {
-        //     "로또 번호는 ${LOTTO_NUMBER_RANGE.first}부터 ${LOTTO_NUMBER_RANGE.last}까지 입니다."
-        // }
     }
-
-    // private fun checkRangeLottoNumber(numbers: Set<Int>): Boolean {
-    //     return numbers
-    //         .filterNot { num -> num in LOTTO_NUMBER_RANGE }
-    //         .isEmpty()
-    // }
 
     companion object {
         private val LOTTO_NUMBER_RANGE = 1..45
--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -1,6 +1,6 @@
 package lotto.domain
 
-class LottoNumber(private val number: Int) {
+data class LottoNumber(val number: Int) {
     init {
         require(number in LOTTO_NUMBER_RANGE) {
             "로또 번호는 ${LOTTO_NUMBER_RANGE.first}부터 ${LOTTO_NUMBER_RANGE.last}까지 입니다."
