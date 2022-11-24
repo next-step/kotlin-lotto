c90222f3e1e8436e2f59ff0a427bef3dbc52e495
Jo-GyuHyeon <kuhyun1993@gmail.com>
2022-11-25 03:39:00 +0900

    feat: 보너스 Reward 에 따라 매칭
    - 5자리 숫자느 보너스가 포함될 경우 테스트 작성

--- src/test/kotlin/lotto/domain/WinnerTest.kt
+++ src/test/kotlin/lotto/domain/WinnerTest.kt
@@ -1,16 +1,26 @@
 package lotto.domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.data.forAll
 import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class WinnerTest : StringSpec({
+    val winningLotto = LottoMachine.generateLotto(listOf(1, 2, 3, 4, 5, 6))
+    val bonusNumber = LottoNumber.from(7)
+    val winner = Winner(winningLotto, bonusNumber)
+
+    "보너스 볼은 지난주 당첨 번호와 중복되지 않아야 한다." {
+        shouldThrow<IllegalArgumentException> {
+            Winner(
+                winningLotto = winningLotto,
+                bonusLottoNumber = LottoNumber.from(1)
+            )
+        }
+    }
 
     "일치 갯수에 따라 수익률 계산 하다." {
-        val winningLotto = LottoMachine.generateLotto()
-        val winner = Winner(winningLotto)
-
         val matchReward = mapOf(
             Reward.FORTH_PLACE to 1,
             Reward.THIRD_PLACE to 0,
@@ -28,6 +38,21 @@ class WinnerTest : StringSpec({
             val formattedExpect = String.format("%.2f", incomeRate)
 
             formattedActual shouldBe formattedExpect
+        }
+    }
+
+    "5숫자가 일치할 경우, 보너스 번호 유무도 확인한다." {
+        forAll(
+            row(listOf(LottoMachine.generateLotto(listOf(1, 2, 3, 4, 7, 8))), true),
+            row(listOf(LottoMachine.generateLotto(listOf(3, 4, 5, 6, 7, 20))), true),
+            row(listOf(LottoMachine.generateLotto(listOf(11, 12, 13, 14, 15, 16))), false),
+        ) { lottoList, matchBonus ->
+            val matchResult: Map<Reward, Int> = winner.match(lottoList)
+            val hasBonus: Boolean = matchResult.any { (reward, matchCount) ->
+                reward.hasBonus() && reward.matchCount == 5 && matchCount > 0
+            }
+
+            hasBonus shouldBe matchBonus
         }
     }
 })
