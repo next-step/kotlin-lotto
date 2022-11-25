2ed23f977ab09b3b6d745c9f6afa036951e0fdb8
namonak <namonak@gmail.com>
2022-11-25 23:38:04 +0900

    refactor(step2): add a LottoRank enum class
    The following have been implement and test:
     - add a function that converts matched numbers into ranks

---
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -0,0 +1,16 @@
+package lotto.domain
+
+enum class LottoRank(val countOfMatch: Int, val winningMoney: Int) {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
+
+    companion object {
+        fun valueOf(countOfMatch: Int): LottoRank {
+            return values().find { it.countOfMatch == countOfMatch } ?: MISS
+        }
+    }
+}
---
+++ src/test/kotlin/lotto/domain/LottoRankTest.kt
@@ -0,0 +1,37 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class LottoRankTest {
+    @DisplayName("1등 당첨")
+    @Test
+    fun `1등 당첨`() {
+        // given
+        val lottoRank = LottoRank.valueOf(6)
+
+        // when, then
+        assertThat(lottoRank).isEqualTo(LottoRank.FIRST)
+    }
+
+    @DisplayName("2등 당첨")
+    @Test
+    fun `2등 당첨`() {
+        // given
+        val lottoRank = LottoRank.valueOf(5)
+
+        // when, then
+        assertThat(lottoRank).isEqualTo(LottoRank.SECOND)
+    }
+
+    @DisplayName("낙첨")
+    @Test
+    fun `낙첨`() {
+        // given
+        val lottoRank = LottoRank.valueOf(2)
+
+        // when, then
+        assertThat(lottoRank).isEqualTo(LottoRank.MISS)
+    }
+}
