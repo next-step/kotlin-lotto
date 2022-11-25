5dacf5467eeb249b745976ed6363a4f5d81d4676
ssr <ssr03>
2022-11-25 22:19:55 +0900

    fix: 긴 숫자는 언더바( _ ) 사용

--- src/main/kotlin/lotto/domain/LottoRank.kt
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -5,11 +5,11 @@ enum class LottoRank(
     val reward: Long,
     val isBonus: Boolean
 ) {
-    FIFTH_PLACE(3, 5000, false),
-    FOURTH_PLACE(4, 50000, false),
-    THIRD_PLACE(5, 1500000, false),
-    SECOND_PLACE(5, 30000000, true),
-    FIRST_PLACE(6, 2000000000, false);
+    FIFTH_PLACE(3, 5000L, false),
+    FOURTH_PLACE(4, 50000L, false),
+    THIRD_PLACE(5, 1_500_000L, false),
+    SECOND_PLACE(5, 30_000_000L, true),
+    FIRST_PLACE(6, 2_000_000_000L, false);
 
     companion object {
         fun valueOf(matchCount: Int, isBonus: Boolean): LottoRank? =
