73e5779edc7c43c0d4cdabf4b0545ae45008349c
이상인/wefu0000 <wefu.child@gmail.com>
2022-11-26 00:19:02 +0900

    feat: 1-45 사이의 난수 6개 생성

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,12 +1,20 @@
 package lotto
 
+import java.security.SecureRandom
+
 class Lotto(val price: Int) {
 
     init {
         require(price % BASE_PRICE == 0) { "input incorrectly price" }
     }
 
+    fun createNumbers(): List<Int> = (0 until NUMBERS_COUNT).map { createRandomNumbers() }
+
+    private fun createRandomNumbers(): Int = SecureRandom.getInstanceStrong().nextInt(MAX_RANDOM_VALUE)
+
     companion object {
         private const val BASE_PRICE = 1000
+        private const val NUMBERS_COUNT = 6
+        private const val MAX_RANDOM_VALUE = 45
     }
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,6 +1,8 @@
 package lotto
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.ints.shouldBeInRange
+import io.kotest.matchers.shouldBe
 import org.assertj.core.api.Assertions
 
 class LottoTest : StringSpec({
@@ -9,5 +11,14 @@ class LottoTest : StringSpec({
 
         Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
             .isThrownBy { Lotto(100) }
+    }
+
+    "1-45 사이의 난수 6개 생성" {
+        val lotto = Lotto(1000)
+
+        lotto.createNumbers().size shouldBe 6
+        lotto.createNumbers().forEach { num ->
+            num shouldBeInRange IntRange(1, 45)
+        }
     }
 })
