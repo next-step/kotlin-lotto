09d3aec45902d618275e98b853a209d53e1ec7a5
SangHyunGil <zizon5941@gmail.com>
2022-11-25 23:52:38 +0900

    chore: LottoInfo 메서드 추가 및 테스트

--- src/main/kotlin/lotto/domain/LottoInfo.kt
+++ src/main/kotlin/lotto/domain/LottoInfo.kt
@@ -2,20 +2,33 @@ package lotto.domain
 
 enum class LottoInfo(
     val matchCount: Int,
+    val isBonusBallMatched: Boolean,
     val amount: Int,
 ) {
-    NON_MATCH(0, 0),
-    ONE_MATCH(1, 0),
-    TWO_MATCH(2, 0),
-    THREE_MATCH(3, 5000),
-    FOUR_MATCH(4, 50000),
-    FIVE_MATCH(5, 1500000),
-    SIX_MATCH(6, 2000000000);
+    NON_MATCH(0, false, 0),
+    ONE_MATCH(1, false, 0),
+    TWO_MATCH(2, false, 0),
+    THREE_MATCH(3, false, 5000),
+    FOUR_MATCH(4, false, 50000),
+    FIVE_MATCH(5, false, 1500000),
+    FIVE_MATCH_WITH_BONUS_BALL(5, true, 30000000),
+    SIX_MATCH(6, false, 2000000000);
+
+    fun checkBonusBall(isBonusBallMatched: Boolean): Boolean {
+        if (this == FIVE_MATCH_WITH_BONUS_BALL) {
+            return isBonusBallMatched
+        }
+        return true
+    }
 
     companion object {
         private const val WINNER_MINIMUM_THRESH_HOLD = 3
+        private const val SECOND_RANK_MATCH_COUNT = 5
 
-        fun of(matchCount: Int): LottoInfo {
+        fun of(matchCount: Int, isBonusBallMatched: Boolean): LottoInfo {
+            if (matchCount == SECOND_RANK_MATCH_COUNT && isBonusBallMatched) {
+                return FIVE_MATCH_WITH_BONUS_BALL
+            }
             return values().first { it.matchCount == matchCount }
         }
 
