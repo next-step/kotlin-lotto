1b31a9b2a7e1ac7113f59c81a07511355576a414
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-26 16:14:00 +0900

    refactor(Rank): 자리수 구분하는 언더바 추가

--- src/main/kotlin/lotto/domain/Rank.kt
+++ src/main/kotlin/lotto/domain/Rank.kt
@@ -1,10 +1,10 @@
 package lotto.domain
 
 enum class Rank(val numberOfMatch: Int, val money: Double) {
-    FIRST(6, 2000000000.0),
-    SECOND(5, 1500000.0),
-    THIRD(4, 50000.0),
-    FOURTH(3, 5000.0);
+    FIRST(6, 200_000_000.0),
+    SECOND(5, 1_500_000.0),
+    THIRD(4, 50_000.0),
+    FOURTH(3, 5_000.0);
 
     companion object {
         fun fromOrNull(matches: Int) = values().firstOrNull() { it.numberOfMatch == matches }
