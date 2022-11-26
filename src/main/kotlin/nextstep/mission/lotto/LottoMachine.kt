1a160e54310e3fc529d2f8669ec12961079b0a5b
kimseonghun <seonghun127@woowahan.com>
2022-11-27 00:01:15 +0900

    feat:로또 기계는 입력받은 금액만큼의 로또 숫자 리스트를 자동 생성한다.

---
+++ src/main/kotlin/nextstep/mission/lotto/LottoMachine.kt
@@ -0,0 +1,15 @@
+package nextstep.mission.lotto
+
+import nextstep.mission.lotto.vo.LottoNumber
+import nextstep.mission.lotto.vo.LottoNumbers
+
+private const val LOTTO_PRICE = 1000
+
+object LottoMachine {
+
+    private val totalLottoNumbers: List<LottoNumber> = (1..45).map { LottoNumber(it) }
+
+    fun create(totalPrice: Int): Lotto2 = (1..totalPrice / LOTTO_PRICE)
+        .map { LottoNumbers(totalLottoNumbers.shuffled().take(6)) }
+        .let { Lotto2(it) }
+}
---
+++ src/test/kotlin/nextstep/mission/lotto/LottoMachineTest.kt
@@ -0,0 +1,15 @@
+package nextstep.mission.lotto
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class LottoMachineTest : StringSpec({
+
+    "로또 기계는 입력받은 금액만큼의 로또 숫자 리스트를 자동 생성한다." {
+        val totalLottoPrice = 10_000
+
+        val result: Lotto2 = LottoMachine.create(totalLottoPrice)
+
+        result.lottoNumbers.size shouldBe 10
+    }
+})
