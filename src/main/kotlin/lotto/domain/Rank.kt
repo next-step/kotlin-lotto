3113a75c3fdf4db921eeda88db8960bc484468de
chayoung-choi <chayoung.dev@gmail.com>
2022-11-26 23:17:10 +0900

    refactor: Rank valueOf

--- src/main/kotlin/lotto/domain/Rank.kt
+++ src/main/kotlin/lotto/domain/Rank.kt
@@ -1,22 +1,23 @@
 package lotto.domain
 
-enum class Rank(val countOfMatch: Int, val winningMoney: Int) {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    MISS(0, 0);
+enum class Rank(val countOfMatch: Int, val matchBonus: Boolean, val winningMoney: Int) {
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    MISS(0, false, 0);
 
     fun getTotalWinningMoney(count: Int): Int = count * winningMoney
 
     companion object {
+        private const val SECOND_RANK_MATCH_COUNT = 5
         fun valueOf(countOfMatch: Int, matchBonus: Boolean): Rank {
             return values().find {
-                if (countOfMatch == 5) it == getRankOf5Match(matchBonus) else it.countOfMatch == countOfMatch
+                if (countOfMatch == SECOND_RANK_MATCH_COUNT && it.countOfMatch == SECOND_RANK_MATCH_COUNT) {
+                    it.matchBonus == matchBonus
+                } else it.countOfMatch == countOfMatch
             } ?: MISS
         }
-
-        private fun getRankOf5Match(matchBonus: Boolean): Rank = if (matchBonus) SECOND else THIRD
     }
 }
--- src/test/kotlin/lotto/domain/RankTest.kt
+++ src/test/kotlin/lotto/domain/RankTest.kt
@@ -6,7 +6,15 @@ import org.junit.jupiter.params.provider.CsvSource
 
 class RankTest {
     @ParameterizedTest
-    @CsvSource("1,false,MISS", "3,false,FIFTH", "4,true,FOURTH", "5,false,THIRD", "5,true,SECOND", "6,false,FIRST")
+    @CsvSource(
+        "1,false,MISS",
+        "3,false,FIFTH",
+        "4,true,FOURTH",
+        "5,false,THIRD",
+        "5,true,SECOND",
+        "6,false,FIRST",
+        "0,true, MISS"
+    )
     fun `번호 일치 갯수에 따른 로또 등수를 반환한다`(countOfMatch: Int, matchBonus: Boolean, place: String) {
         assertThat(Rank.valueOf(countOfMatch, matchBonus)).isEqualTo(Rank.valueOf(place))
     }
