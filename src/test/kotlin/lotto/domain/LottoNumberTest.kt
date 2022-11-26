2a4c857b3c59e711bbf6f6880b03ddf710cf0107
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-27 01:19:20 +0900

    test(LottoNumber): 로또 번호가 1~45 사이의 수가 아니라면 에러가 발생한다.

---
+++ src/test/kotlin/lotto/domain/LottoNumberTest.kt
@@ -0,0 +1,21 @@
+package lotto.domain
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.inspectors.forAll
+
+internal class LottoNumberTest: BehaviorSpec({
+    given("로또 번호가") {
+        `when`("1 ~ 45 사이의 수가 아닐 때") {
+            val numbers = listOf(0, -45, 50, 46, 1111)
+
+            numbers.forAll {
+                then("IllegalArgumentException 이 발생한다.") {
+                    shouldThrow<IllegalArgumentException> {
+                        LottoNumber(it)
+                    }
+                }
+            }
+        }
+    }
+})
