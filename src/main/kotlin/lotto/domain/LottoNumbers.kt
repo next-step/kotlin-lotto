dbae6f41d27fc97e431f5654b4c0def1c264888e
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-27 03:05:06 +0900

    feat(LottoNumbers): 당첨 번호와 로또 번호의 일치하는 개수를 반환한다.

--- src/main/kotlin/lotto/domain/LottoNumbers.kt
+++ src/main/kotlin/lotto/domain/LottoNumbers.kt
@@ -3,6 +3,11 @@ package lotto.domain
 data class LottoNumbers(private val lottoNumbers: List<LottoNumber>) {
     fun getLottoNumbers() = lottoNumbers
 
+    fun getNumberOfMatch(purchasedLottoNumbers: LottoNumbers): Int {
+        return purchasedLottoNumbers.lottoNumbers.count {
+            this.lottoNumbers.contains(it)
+        }
+    }
     companion object {
         private const val ZERO = 0
         private const val MAXIMUM_LOTTO_SIZE = 6
--- src/test/kotlin/lotto/domain/LottoNumbersTest.kt
+++ src/test/kotlin/lotto/domain/LottoNumbersTest.kt
@@ -4,12 +4,41 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
 internal class LottoNumbersTest : BehaviorSpec({
-    given("로또 번호를") {
-        `when`("랜덤으로 생성할 때") {
+    given("로또 번호를 생성할 때") {
+        `when`("자동으로 생성되도록 한다면") {
             val result = LottoNumbers.createRandom()
 
             then("중복없는 6개의 로또 번호를 생성한다.") {
                 result.getLottoNumbers().distinct().size shouldBe 6
+            }
+        }
+    }
+
+    given("당첨 번호와 구입한 로또의 일치하는 수의 개수를 구할 때") {
+        val winningLottoNumbers = LottoNumbers(
+            listOf(
+                LottoNumber(1),
+                LottoNumber(2),
+                LottoNumber(3),
+                LottoNumber(4),
+                LottoNumber(5),
+                LottoNumber(6)
+            )
+        )
+        `when`("3개가 일치한다면") {
+            val purchasedLottoNumbers = LottoNumbers(
+                listOf(
+                    LottoNumber(1),
+                    LottoNumber(2),
+                    LottoNumber(3),
+                    LottoNumber(33),
+                    LottoNumber(34),
+                    LottoNumber(35)
+                )
+            )
+            then("일치하는 수 3을 반환한다.") {
+                val result = winningLottoNumbers.getNumberOfMatch(purchasedLottoNumbers)
+                result shouldBe 3
             }
         }
     }
