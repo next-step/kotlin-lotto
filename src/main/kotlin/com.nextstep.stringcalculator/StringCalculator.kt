1eb11a8c8d1293465799c3ff8d9247f7b2ba2ce1
jay <jay@woowahan.com>
2022-11-26 18:30:29 +0900

    refactor: 문자열 계산기 리팩토링

--- src/main/kotlin/com.nextstep.stringcalculator/StringCalculator.kt
+++ src/main/kotlin/com.nextstep.stringcalculator/StringCalculator.kt
@@ -1,18 +1,22 @@
 package com.nextstep.stringcalculator
 
-import java.lang.IllegalArgumentException
 import java.lang.RuntimeException
 
 object StringCalculator {
+    private const val CUSTOM_DELIMITER_POSITION = 1
+    private const val CUSTOM_INPUT_POSITION = 2
+
+    private val CUSTOM_MATCHING_PATTERN = Regex("//(.)\n(.*)")
+
     fun calculateDelimiters(input: String?): Int {
         if (input.isNullOrEmpty()) {
             return 0
         }
         val tokens = input.split(",", ":")
-        val regex = Regex("//(.)\n(.*)").find(input)
-        val result = regex?.let { it ->
-            val customDelimiter = it.groupValues[1]
-            val customDelimiterTokens = it.groupValues[2].split(customDelimiter)
+        val regex = CUSTOM_MATCHING_PATTERN.find(input)
+        val result = regex?.let {
+            val customDelimiter = it.groupValues[CUSTOM_DELIMITER_POSITION]
+            val customDelimiterTokens = it.groupValues[CUSTOM_INPUT_POSITION].split(customDelimiter)
             sum(customDelimiterTokens)
         } ?: sum(tokens)
 
@@ -21,13 +25,5 @@ object StringCalculator {
 
     private fun sum(tokens: List<String>) = tokens.sumOf { toInt(it) }
 
-    private fun toInt(input: String): Int = try {
-        val inputNumber = input.toInt()
-        if (inputNumber < 0) {
-            throw RuntimeException()
-        }
-        inputNumber
-    } catch (e: NumberFormatException) {
-        throw IllegalArgumentException("invalid input - $input")
-    }
+    private fun toInt(input: String): Int = input.toInt().takeIf { it >= 0 } ?: throw RuntimeException()
 }
