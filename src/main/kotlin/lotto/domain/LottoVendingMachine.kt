a4261c0ecff58bcfe1b3d1b975087499255a4a22
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-26 16:15:56 +0900

    refactor(LottoVendingMachine): object 클래스로 변경

--- src/main/kotlin/lotto/domain/LottoVendingMachine.kt
+++ src/main/kotlin/lotto/domain/LottoVendingMachine.kt
@@ -1,15 +1,13 @@
 package lotto.domain
 
-class LottoVendingMachine {
-    companion object {
-        private const val LOTTO_PRICE = 1000.00
+object LottoVendingMachine {
+    private const val LOTTO_PRICE = 1_000.00
 
-        fun buy(purchaseAmount: PurchaseAmount): List<LottoNumber> {
-            return List(getNumberOfLotto(purchaseAmount)) { LottoNumber() }
-        }
+    fun buy(purchaseAmount: PurchaseAmount): List<LottoNumber> {
+        return List(getNumberOfLotto(purchaseAmount)) { LottoNumber() }
+    }
 
-        private fun getNumberOfLotto(purchaseAmount: PurchaseAmount): Int {
-            return (purchaseAmount.amount / LOTTO_PRICE).toInt()
-        }
+    private fun getNumberOfLotto(purchaseAmount: PurchaseAmount): Int {
+        return (purchaseAmount.amount / LOTTO_PRICE).toInt()
     }
 }
