7e4c2ca74d97d36b7af116ef55ce0aab4146b59c
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-26 23:55:18 +0900

    test(RankTest): 테스트 명에 함수명을 쓰지않고 구체적인 상황으로 수정

--- src/test/kotlin/lotto/domain/RankTest.kt
+++ src/test/kotlin/lotto/domain/RankTest.kt
@@ -4,17 +4,17 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
 internal class RankTest : BehaviorSpec({
-    given("fromOfNull() 함수에") {
-        `when`("numberOfMatch 에 있는 수를 인자로 넣으면") {
+    given("일치하는 개수가") {
+        `when`("3개라면") {
             val numberOfMatch = 3
             val result = Rank.valueOf(numberOfMatch)
 
-            then("랭크(등수) 정보를 반환한다.") {
+            then("4등을 반환한다.") {
                 result shouldBe Rank.FOURTH
             }
         }
 
-        `when`("numberOfMatch 에 없는 수를 인자로 넣으면") {
+        `when`("등수에 없는 수라면") {
             val numberOfMatch = 1
             val result = Rank.valueOf(numberOfMatch)
 
