876176d8d4583b0a6861ab5187e80cd95496c662
devcs96 <katd6@naver.com>
2022-11-27 20:32:10 +0900

    :sparkles: feat(domain) : 문자열 계산기 구현

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -1,7 +1,32 @@
 package calculator
 
-class StringAddCalculator {
-    fun add(text: String): Int {
-        TODO("Not yet implemented")
+object StringAddCalculator {
+
+    private const val DEFAULT_DELIMITER = ",:"
+
+    private const val CUSTOM_DELIMITER = "//(.)\n(.*)"
+    fun add(text: String?): Int {
+        if (text.isNullOrBlank()) {
+            return 0
+        }
+        val result = Regex(CUSTOM_DELIMITER).find(text)
+        result?.let {
+            val customDelimiter = it.groupValues[1]
+            return sum(it.groupValues[2].split(customDelimiter))
+        }
+        return sum(text.split("[$DEFAULT_DELIMITER]".toRegex()))
+    }
+
+    private fun sum(tokens: List<String>): Int = tokens.sumOf { toInt(it) }
+    private fun toInt(text: String): Int {
+        try {
+            val result = text.toInt()
+            if (result < 0) {
+                throw RuntimeException("입력값은 음수가 나올 수 없습니다.")
+            }
+            return result
+        } catch (e: NumberFormatException) {
+            throw RuntimeException("입력값은 숫자만 가능합니다.", e)
+        }
     }
 }
