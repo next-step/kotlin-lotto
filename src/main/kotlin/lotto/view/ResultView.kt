18f502cdb9e9c56c0f324c0f70db2d7b2db01857
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-28 02:01:22 +0900

    feat(ResultView): 2등에 해당하는 UI 로직 추가

--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -13,8 +13,12 @@ object ResultView {
     fun printRewardsStatistics(winningStatistics: WinningStatistics) {
         println("\n당첨 통계")
         println("---------")
-        Rank.values().forEach {
-            println("${it.numberOfMatch}개 일치 (${it.winningMoney}원)- ${winningStatistics.getNumberOfMatchCount(it)}개")
+
+        val result = Rank.values().filter { it != Rank.MISS }.sortedDescending()
+        result.forEach {
+            print("${it.numberOfMatch}개 일치")
+            if(it == Rank.SECOND) print(", 보너스 볼 일치")
+            print(" (${it.winningMoney}원)- ${winningStatistics.getNumberOfMatchCount(it)}개\n")
         }
     }
 
