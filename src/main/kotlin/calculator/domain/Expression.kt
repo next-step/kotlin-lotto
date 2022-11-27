12e1d0b73a5ca593be0fca3a0406a43068cb42c7
Choiyounho <maxosa78@gmail.com>
2022-11-27 23:05:16 +0900

    feat : 수식을 생성할 때 숫자만 있는 검증 로직 추가

--- src/main/kotlin/calculator/domain/Expression.kt
+++ src/main/kotlin/calculator/domain/Expression.kt
@@ -4,6 +4,17 @@ class Expression(
     val value: String = DEFAULT_EXPRESSION_VALUE
 ) {
 
+    init {
+        validateExpression()
+    }
+
+    private fun validateExpression() {
+        value.forEach {
+            runCatching { it.digitToInt() }
+                .onFailure { throw IllegalArgumentException("숫자 외에 다른 문자는 들어올 수 없습니다.") }
+        }
+    }
+
     companion object {
         private const val DEFAULT_EXPRESSION_VALUE = ""
     }
