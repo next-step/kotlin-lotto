9b2ce53fd171bbf530a0e73f9839b5ac3737bee0
dk-ko <dev.dkko@gmail.com>
2022-11-27 13:51:25 +0900

    test: LottoNumber

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -5,6 +5,9 @@ data class LottoNumber(private var value: Int = 0): Comparable<LottoNumber> {
         if (value == 0) {
             value = (1 .. 45).random()
         }
+        if (value > 45 || value < 1) {
+            throw IllegalArgumentException("로또 숫자는 1~45를 넘을 수 없습니다.")
+        }
     }
 
     override fun toString(): String {
--- src/test/kotlin/lotto/LottoNumberTest.kt
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -5,7 +5,14 @@ import org.junit.jupiter.api.Test
 
 class LottoNumberTest{
     @Test
-    fun compareTest() {
+    fun `데이터 대소 비교 확인`() {
         assertTrue(LottoNumber(3) < LottoNumber(4))
+    }
+
+    @Test
+    fun `LottoNumber 생성시 지정된 범위 내에서만 생성 가능`() {
+        org.junit.jupiter.api.assertThrows<IllegalArgumentException> {
+            LottoNumber(46)
+        }
     }
 }
