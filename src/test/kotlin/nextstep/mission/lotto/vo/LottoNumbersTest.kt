8fe7f62c8f1af3a59457c25418550108d363a23f
kimseonghun <seonghun127@woowahan.com>
2022-11-27 17:19:42 +0900

    refactor: LottoFixture를 사용하도록 변경

--- src/test/kotlin/nextstep/mission/lotto/vo/LottoNumbersTest.kt
+++ src/test/kotlin/nextstep/mission/lotto/vo/LottoNumbersTest.kt
@@ -7,11 +7,12 @@ import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.subsequence
 import io.kotest.property.checkAll
+import nextstep.mission.lotto.fixture.lottoNumberListOf
 
 class LottoNumbersTest : StringSpec({
 
     "로또 숫자들이 6개가 아니면 예외가 발생한다." {
-        checkAll(10, Arb.subsequence(lottoNumbersOf(1, 2, 3, 4, 5))) {
+        checkAll(10, Arb.subsequence(lottoNumberListOf(1, 2, 3, 4, 5))) {
             val exception = shouldThrowExactly<IllegalArgumentException> {
                 LottoNumbers(it)
             }
@@ -22,24 +23,21 @@ class LottoNumbersTest : StringSpec({
 
     "로또 숫자들 중복이면 예외가 발생한다." {
         val exception = shouldThrowExactly<IllegalArgumentException> {
-            LottoNumbers(lottoNumbersOf(1, 2, 3, 4, 5, 5))
+            LottoNumbers(lottoNumberListOf(1, 2, 3, 4, 5, 5))
         }
 
         exception.message shouldBe "로또 숫자는 중복이 허용되지 않습니다."
     }
 
     "로또 숫자 리스트가 중복도 아니면서 6자리면 정상 생성한다." {
-        shouldNotThrowAny { LottoNumbers(lottoNumbersOf(1, 2, 3, 4, 5, 6)) }
+        shouldNotThrowAny { LottoNumbers(lottoNumberListOf(1, 2, 3, 4, 5, 6)) }
     }
 
     "로또는 당첨번호를 입력받아 몇개의 숫자가 일치하는지 반환한다." {
-        val lottoNumbers = LottoNumbers(lottoNumbersOf(1, 2, 3, 4, 5, 6))
+        val lottoNumbers = LottoNumbers(lottoNumberListOf(1, 2, 3, 4, 5, 6))
 
-        val result: Int = lottoNumbers.match(LottoNumbers(lottoNumbersOf(1, 2, 3, 4, 5, 6)))
+        val result: Int = lottoNumbers.match(LottoNumbers(lottoNumberListOf(1, 2, 3, 4, 5, 6)))
 
         result shouldBe 6
     }
 })
-
-private fun lottoNumbersOf(vararg elements: Int): List<LottoNumber> =
-    elements.map { LottoNumber(it) }
