c16aeec0ba7bd3385c194b4330a084624af5acd0
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-27 22:38:23 +0900

    feat(WinningLottoNumbers): 입력된 보너스 볼의 숫자가 당첨 번호와 중복이라면 에러가 발생한다.

--- src/main/kotlin/lotto/domain/WinningLottoNumbers.kt
+++ src/main/kotlin/lotto/domain/WinningLottoNumbers.kt
@@ -1,9 +1,19 @@
 package lotto.domain
 
-data class WinningLottoNumbers(private val lottoNumbers: LottoNumbers) {
+data class WinningLottoNumbers(
+    private val lottoNumbers: LottoNumbers,
+    private val bonusLottoNumbers: LottoNumber
+) {
+    init {
+        require(!lottoNumbers.lottoNumbers.contains(bonusLottoNumbers)) { INVALID_BONUS_BALL_ERROR_MESSAGE }
+    }
     fun getAllNumberOfMatches(lotteries: List<LottoNumbers>): List<Int> {
         return lotteries.map {
             lottoNumbers.getNumberOfMatch(it)
         }
+    }
+
+    companion object {
+        private const val INVALID_BONUS_BALL_ERROR_MESSAGE = "보너스 볼은 당첨 번호와 중복되지 않는 수여야 합니다."
     }
 }
--- src/test/kotlin/lotto/domain/WinningLottoNumbersTest.kt
+++ src/test/kotlin/lotto/domain/WinningLottoNumbersTest.kt
@@ -2,16 +2,25 @@ package lotto.domain
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
-import java.lang.IllegalArgumentException
 
 internal class WinningLottoNumbersTest : BehaviorSpec({
-    given("당첨 번호를 입력했을 때") {
-        `when`("숫자가 아닌 문자가 있다면") {
-            val input = "df, d, 1, f, ff, yoon"
+    given("입력된 보너스 볼의 숫자가") {
+        val bonusBall = LottoNumber(3)
 
+        `when`("당첨 번호와 중복될 때") {
+            val winningLottoNumbers = LottoNumbers(
+                listOf(
+                    LottoNumber(3),
+                    LottoNumber(4),
+                    LottoNumber(5),
+                    LottoNumber(6),
+                    LottoNumber(7),
+                    LottoNumber(8),
+                )
+            )
             then("IllegalArgumentException 이 발생한다.") {
                 shouldThrow<IllegalArgumentException> {
-                    WinningLottoNumbers.from(input)
+                    WinningLottoNumbers(lottoNumbers = winningLottoNumbers, bonusLottoNumbers = bonusBall)
                 }
             }
         }
