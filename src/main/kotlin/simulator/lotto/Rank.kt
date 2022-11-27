a72500bb24a8758191137fa1a57b2ba1f495e417
wonjun3991 <wonjun.choi@meshkorea.net>
2022-11-27 18:06:44 +0900

    refactor(Rank): refactor Rank Class
    move const values to Enum Methods

--- src/main/kotlin/simulator/lotto/Rank.kt
+++ src/main/kotlin/simulator/lotto/Rank.kt
@@ -1,71 +1,53 @@
 package simulator.lotto
 
-import simulator.lotto.Rank.*
-
 enum class Rank {
     FIRST {
         override fun prize(): Int {
-            return FIRST_PRIZE
+            return 2_000_000_000
         }
 
-        override fun matches(): Int {
-           return LOTTO_FIRST_RANK_MATCHES
+        override fun match(): Int {
+           return 6
         }
     },
 
     SECOND {
         override fun prize(): Int {
-            return SECOND_PRIZE
+            return 1_500_000
         }
 
-        override fun matches(): Int {
-            return LOTTO_SECOND_RANK_MATCHES
+        override fun match(): Int {
+            return 5
         }
     },
 
     THIRD {
         override fun prize(): Int {
-            return THIRD_PRIZE
+            return 50_000
         }
 
-        override fun matches(): Int {
-            return LOTTO_THIRD_RANK_MATCHES
+        override fun match(): Int {
+            return 4
         }
     },
 
     FOURTH {
         override fun prize(): Int {
-            return FOURTH_PRIZE
+            return 5_000
         }
 
-        override fun matches(): Int {
-            return LOTTO_FOURTH_RANK_MATCHES
+        override fun match(): Int {
+            return 3
         }
     };
 
     abstract fun prize(): Int
 
-    abstract fun matches(): Int
+    abstract fun match(): Int
 
     companion object {
-        private const val FIRST_PRIZE = 2_000_000_000
-        private const val SECOND_PRIZE = 1_500_000
-        private const val THIRD_PRIZE = 50_000
-        private const val FOURTH_PRIZE = 5_000
-
-        private const val LOTTO_FIRST_RANK_MATCHES = 6
-        private const val LOTTO_SECOND_RANK_MATCHES = 5
-        private const val LOTTO_THIRD_RANK_MATCHES = 4
-        private const val LOTTO_FOURTH_RANK_MATCHES = 3
-
-        fun match(lottoNumberMatches: Int): Rank? {
-            return when (lottoNumberMatches) {
-                LOTTO_FIRST_RANK_MATCHES -> FIRST
-                LOTTO_SECOND_RANK_MATCHES -> SECOND
-                LOTTO_THIRD_RANK_MATCHES -> THIRD
-                LOTTO_FOURTH_RANK_MATCHES -> FOURTH
-                else -> null
-            }
+        fun aggregate(matches: Int): Rank? {
+            return values().find{it.match() == matches}
         }
     }
 }
