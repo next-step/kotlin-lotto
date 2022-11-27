1264c10d5abc96f3e05e5fedad47d5546f16d74e
Choiyounho <maxosa78@gmail.com>
2022-11-27 23:17:14 +0900

    refactor : 수식 테스트 코드 수정

--- src/test/kotlin/calculator/domain/ExpressionTest.kt
+++ src/test/kotlin/calculator/domain/ExpressionTest.kt
@@ -2,13 +2,14 @@ package calculator.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 internal class ExpressionTest {
 
     @Test
-    fun `수식 클래스 생성하여 파라미터 값 비교`() {
+    fun `숫자만 있는 입력값을 이용하여 수식 생성하여 프로퍼티 비교`() {
         // given
-        val value = "1,2:3"
+        val value = "123"
 
         // when
         val expression = Expression(value)
@@ -18,5 +19,10 @@ internal class ExpressionTest {
         assertThat(actual).isTrue
     }
 
-
+    @Test
+    fun `숫자 외에 다른 문자가 포함되어 있어 생성 실패`() {
+        assertThrows<IllegalArgumentException> {
+            Expression("1;,2:34")
+        }
+    }
 }
