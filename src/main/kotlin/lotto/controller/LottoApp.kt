e990fc855730f396b8a7802445c171c826ef3d48
devcs96 <katd6@naver.com>
2022-11-27 23:34:02 +0900

    :sparkles: feat(controller) : controller 로직 구현

--- src/main/kotlin/lotto/controller/LottoApp.kt
+++ src/main/kotlin/lotto/controller/LottoApp.kt
@@ -1,5 +1,19 @@
 package lotto.controller
 
-fun main() {
+import lotto.domain.Lotto
+import lotto.domain.LottoBall
+import lotto.domain.LottoStore
+import lotto.domain.RandomLottoGenerator
+import lotto.view.InputView
+import lotto.view.OutputView
 
+fun main() {
+    val lottoStore = LottoStore(RandomLottoGenerator());
+
+    val money = InputView.askLottoBuyMoney();
+    val lottos = lottoStore.buy(money)
+    OutputView.printLottos(lottos)
+    val winningLotto = Lotto(InputView.askWinningLottoBall().map {
+        LottoBall(it.toInt())
+    })
 }
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -4,15 +4,16 @@ import lotto.domain.Lotto
 
 object OutputView {
 
-    fun printLottoNumber(lottoNumber: Int) = println("$lottoNumber 개를 구매하였습니다")
 
     fun printLottos(lottos: List<Lotto>) {
+        printLottoNumber(lottos.size)
         lottos.forEach {
             print("[")
             printLotto(it)
             print("]\n")
         }
     }
+    private fun printLottoNumber(lottoNumber: Int) = println("$lottoNumber 개를 구매하였습니다")
 
     private fun printLotto(lotto: Lotto) {
         lotto.lotto.forEach {
