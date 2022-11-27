11376f7b90773388b3282cc7e85f0f718e35396b
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-27 22:09:09 +0900

    test(RankTest): 구매한 로또가 보너스 숫자가 일치하지만 4개 이하의 개수가 일치한다면 4등 이하 당첨이다.

--- src/test/kotlin/lotto/domain/RankTest.kt
+++ src/test/kotlin/lotto/domain/RankTest.kt
@@ -1,6 +1,7 @@
 package lotto.domain
 
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 
 internal class RankTest : BehaviorSpec({
@@ -12,6 +13,31 @@ internal class RankTest : BehaviorSpec({
 
             then("2등 당첨이다.") {
                 result shouldBe Rank.SECOND
+            }
+        }
+
+        `when`("보너스 숫자가 일치하지만 4개 이하의 개수가 일치한다면") {
+            val numberOfMatches = listOf(3, 4)
+            val isBonusMatch = true
+
+            numberOfMatches.forAll {
+                val result = Rank.valueOf(it, isBonusMatch)
+
+                then("4등 이하 당첨이다.") {
+                    val ranks = listOf(Rank.FOURTH, Rank.FIFTH)
+                    ranks.contains(result) shouldBe true
+                }
+            }
+        }
+
+        `when`("2개 이하의 수가 일치하거나 아무 숫자도 일치하지 않는다면") {
+            val numberOfMatches = listOf(2, 1, 0)
+
+            numberOfMatches.forAll {
+                val result = Rank.valueOf(it, false)
+                then("아무 등수도 당첨되지 않는다.") {
+                    result shouldBe Rank.MISS
+                }
             }
         }
     }
