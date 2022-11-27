518c18d90d1c5dba4e37c2c8178b284e76c6f71c
devcs96 <katd6@naver.com>
2022-11-28 00:22:03 +0900

    :recycle: refactor(ui): 매직넘버 상수값으로 추출

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,7 +1,9 @@
 package lotto.view
 
+
 object InputView {
 
+    private const val LOTTO_BALL_DELIMITER = ","
     fun askLottoBuyMoney(): Int {
         println("구입금액을 입력해 주세요.")
         return readLine()!!.toInt()
@@ -9,7 +11,7 @@ object InputView {
 
     fun askWinningLottoBall(): List<String> {
         println("지난 주 당첨 번호를 입력해 주세요.")
-        return readLine()!!.split(",")
+        return readLine()!!.split(LOTTO_BALL_DELIMITER)
     }
 
 }
