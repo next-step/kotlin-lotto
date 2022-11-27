07fdf9f49f16dca005bcebafafb033e72f879c30
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-28 01:54:15 +0900

    refactor(LottoNumber): 사용하지 않는 정적 팩터리 메서드 삭제

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -14,12 +14,5 @@ value class LottoNumber(val number: Int) {
         private const val MIN_NUMBER = 1
         private const val MAX_NUMBER = 45
         private const val INVALID_VALUE_ERROR_MESSAGE = "당첨 번호는 1 ~ 45 사이의 숫자여야 합니다."
-        private val allLottoNumbers = (MIN_NUMBER..MAX_NUMBER).toSet()
-
-        fun getAllLottoNumbers(): Set<LottoNumber> {
-            return allLottoNumbers.map {
-                LottoNumber(it)
-            }.toSet()
-        }
     }
 }
