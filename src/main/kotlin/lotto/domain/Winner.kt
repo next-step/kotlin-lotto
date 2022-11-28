626f22769c372baa90076f95dea31e66895ab8e7
dreamboy1339 <dreamboy1339@gmail.com>
2022-11-29 02:37:48 +0900

    study(step2): lint 수정 적용

--- src/main/kotlin/lotto/domain/Winner.kt
+++ src/main/kotlin/lotto/domain/Winner.kt
@@ -1,15 +1,13 @@
 package lotto.domain
 
-enum class Winner(val matchingCount: Int, val prize: Int, private val calculateReward: (Int, Int) -> Int) {
-    FIRST_GRADE(FIRST_GRADE_MATCHING_COUNT, FIRST_WIN_PRIZE, { a, b -> a * b }),
-    SECOND_GRADE(SECOND_GRADE_MATCHING_COUNT, SECOND_WIN_PRIZE, { a, b -> a * b }),
-    THIRD_GRADE(THIRD_GRADE_MATCHING_COUNT, THIRD_WIN_PRIZE, { a, b -> a * b }),
-    FOURTH_GRADE(FOURTH_GRADE_MATCHING_COUNT, FOURTH_WIN_PRIZE, { a, b -> a * b }),
-    FIVE_GRADE(FIFTH_GRADE_MATCHING_COUNT, NONE_WIN_PRIZE, { a, b -> a * b }),
-    SIX_GRADE(SIXTH_GRADE_MATCHING_COUNT, NONE_WIN_PRIZE, { a, b -> a * b }),
-    NO_MATCH(NONE_GRADE_MATCHING_COUNT, NONE_WIN_PRIZE, { a, b -> a * b });
-
-    fun calculate(numberOfMatchingLotto: Int): Int = calculateReward(prize, numberOfMatchingLotto)
+enum class Winner(val matchingCount: Int, val prize: Int) {
+    FIRST_GRADE(FIRST_GRADE_MATCHING_COUNT, FIRST_WIN_PRIZE),
+    SECOND_GRADE(SECOND_GRADE_MATCHING_COUNT, SECOND_WIN_PRIZE),
+    THIRD_GRADE(THIRD_GRADE_MATCHING_COUNT, THIRD_WIN_PRIZE),
+    FOURTH_GRADE(FOURTH_GRADE_MATCHING_COUNT, FOURTH_WIN_PRIZE),
+    FIVE_GRADE(FIFTH_GRADE_MATCHING_COUNT, NONE_WIN_PRIZE),
+    SIX_GRADE(SIXTH_GRADE_MATCHING_COUNT, NONE_WIN_PRIZE),
+    NO_MATCH(NONE_GRADE_MATCHING_COUNT, NONE_WIN_PRIZE);
 
     companion object {
 
