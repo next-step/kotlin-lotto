213e81e5ee9e1b578076916d5706c7c4c9a5d0df
wonjun3991 <wonjun.choi@meshkorea.net>
2022-11-28 22:16:15 +0900

    fix(Ranks): fix to use ranks new method

--- src/main/kotlin/simulator/lotto/Ranks.kt
+++ src/main/kotlin/simulator/lotto/Ranks.kt
@@ -1,21 +1,15 @@
 package simulator.lotto
 
-data class Ranks(private val values: List<Rank>) {
+data class Ranks(val values: List<Rank>) {
     fun rankCount(rank: Rank): Int {
         return values.count { it == rank }
     }
 
     fun totalMoney(): Int {
-        return values.sumOf { it.prize() }
+        return values.sumOf { it.winningMoney }
     }
 
     fun yield(money: Int): Double {
         return totalMoney() / money.toDouble()
-    }
-
-    companion object {
-        fun aggregate(matches: List<Int>): Ranks {
-            return Ranks(matches.mapNotNull { Rank.aggregate(it) })
-        }
     }
 }
