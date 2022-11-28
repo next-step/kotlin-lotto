7c91eb9b13c7d5edace8a39940f82f04255fba6c
kimseonghun <seonghun127@woowahan.com>
2022-11-28 20:34:50 +0900

    refactor: range expression 사용

--- src/main/kotlin/nextstep/mission/lotto/vo/LottoNumber.kt
+++ src/main/kotlin/nextstep/mission/lotto/vo/LottoNumber.kt
@@ -3,6 +3,6 @@ package nextstep.mission.lotto.vo
 @JvmInline
 value class LottoNumber(val number: Int) {
     init {
-        require((number >= 1) and (number <= 45)) { "로또 숫자는 1에서 45사이어야 합니다." }
+        require(number in 1..45) { "로또 숫자는 1에서 45사이어야 합니다." }
     }
 }
