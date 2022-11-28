a4f0e59dbef509fe553b304cce351d7b48853934
namonak <namonak@gmail.com>
2022-11-28 16:03:15 +0900

    refactor(step2): change the order of the enum values of the LottoRank enum class in ascending order

--- src/main/kotlin/lotto/domain/LottoRank.kt
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -1,11 +1,11 @@
 package lotto.domain
 
 enum class LottoRank(val countOfMatch: Int, val winningMoney: Int) {
-    FIRST(6, 2_000_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
+    MISS(0, 0),
     FIFTH(3, 5_000),
-    MISS(0, 0);
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    FIRST(6, 2_000_000_000);
 
     companion object {
         fun valueOf(countOfMatch: Int): LottoRank {
