45b6be655bb6015599aa8b76e39b76632c608436
ssr <ssr03>
2022-11-28 22:27:23 +0900

    feat: 수동으로 구매할 로또 수/수동으로 구매할 번호 입력

--- src/main/kotlin/lotto/uI/MessageCode.kt
+++ src/main/kotlin/lotto/uI/MessageCode.kt
@@ -4,6 +4,8 @@ import lotto.domain.LottoStatistics
 
 enum class MessageCode(val message: String) {
     INPUT_MONEY("구입금액을 입력해 주세요"),
+    INPUT_MANUAL_LOTTO_COUNT("수동으로 구매할 로또 수를 입력해 주세요."),
+    INPUT_MANUAL_LOTTO_NUMBERS("수동으로 구매할 번호를 입력해 주세요."),
     PURCHASE_COUNT_RESULT("개를 구매했습니다."),
     INPUT_LAST_WEEK_LOTTO("지난 주 당첨 번호를 입력해 주세요."),
     INPUT_BONUS_LOTTO_NUMBER("보너스 볼을 입력해 주세요."),
