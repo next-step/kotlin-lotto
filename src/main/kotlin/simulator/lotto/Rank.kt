ce74005f4834c689ba908ba316c51e304f480b1a
wonjun3991 <wonjun.choi@meshkorea.net>
2022-11-28 21:37:28 +0900

    refactor(Rank): add rank Fifth and Bonus Number

--- src/main/kotlin/simulator/lotto/Rank.kt
+++ src/main/kotlin/simulator/lotto/Rank.kt
@@ -1,53 +1,44 @@
 package simulator.lotto
 
-enum class Rank {
-    FIRST {
-        override fun prize(): Int {
-            return 2_000_000_000
-        }
-
-        override fun match(): Int {
-           return 6
+enum class Rank(val countOfMatch: Int, val winningMoney: Int) {
+    FIRST(6, 2_000_000_000) {
+        override fun matches(countOfMatch: Int, matchBonus: Boolean): Boolean {
+            return countOfMatch == this.countOfMatch
         }
     },
-
-    SECOND {
-        override fun prize(): Int {
-            return 1_500_000
-        }
-
-        override fun match(): Int {
-            return 5
+    SECOND(5, 30_000_000) {
+        override fun matches(countOfMatch: Int, matchBonus: Boolean): Boolean {
+            return countOfMatch == this.countOfMatch && matchBonus
         }
     },
-
-    THIRD {
-        override fun prize(): Int {
-            return 50_000
-        }
-
-        override fun match(): Int {
-            return 4
+    THIRD(5, 1_500_000) {
+        override fun matches(countOfMatch: Int, matchBonus: Boolean): Boolean {
+            return countOfMatch == this.countOfMatch
         }
     },
-
-    FOURTH {
-        override fun prize(): Int {
-            return 5_000
+    FOURTH(4, 50_000) {
+        override fun matches(countOfMatch: Int, matchBonus: Boolean): Boolean {
+            return countOfMatch == this.countOfMatch
         }
-
-        override fun match(): Int {
-            return 3
+    },
+    FIFTH(3, 5_000) {
+        override fun matches(countOfMatch: Int, matchBonus: Boolean): Boolean {
+            return countOfMatch == this.countOfMatch
+        }
+    },
+    MISS(0, 0) {
+        override fun matches(countOfMatch: Int, matchBonus: Boolean): Boolean {
+            return true
         }
     };
 
-    abstract fun prize(): Int
-
-    abstract fun match(): Int
+    abstract fun matches(countOfMatch: Int, matchBonus: Boolean): Boolean
 
     companion object {
-        fun aggregate(matches: Int): Rank? {
-            return values().find{it.match() == matches}
+        fun valueOf(countOfMatch: Int, matchBonus: Boolean): Rank {
+            return values().find {
+                it.matches(countOfMatch, matchBonus)
+            }!!
         }
     }
 }
--- src/test/kotlin/simulator/lotto/RankTest.kt
+++ src/test/kotlin/simulator/lotto/RankTest.kt
@@ -4,20 +4,30 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 
-internal class RankTest{
+internal class RankTest {
     @Test
-    fun `등수별로 상금을 반환한다`(){
-        assertThat(Rank.FIRST.prize()).isEqualTo(2_000_000_000)
-        assertThat(Rank.SECOND.prize()).isEqualTo(1_500_000)
-        assertThat(Rank.THIRD.prize()).isEqualTo(50_000)
-        assertThat(Rank.FOURTH.prize()).isEqualTo(5_000)
+    fun `등수별로 상금을 반환한다`() {
+        assertThat(Rank.FIRST.winningMoney).isEqualTo(2_000_000_000)
+        assertThat(Rank.SECOND.winningMoney).isEqualTo(30_000_000)
+        assertThat(Rank.THIRD.winningMoney).isEqualTo(1_500_000)
+        assertThat(Rank.FOURTH.winningMoney).isEqualTo(50_000)
+        assertThat(Rank.FIFTH.winningMoney).isEqualTo(5_000)
+        assertThat(Rank.MISS.winningMoney).isEqualTo(0)
     }
 
     @Test
-    fun `맞는 로또 갯수에 따라 등수를 반환한다`(){
-        assertThat(Rank.aggregate(6)).isEqualTo(Rank.FIRST)
-        assertThat(Rank.aggregate(5)).isEqualTo(Rank.SECOND)
-        assertThat(Rank.aggregate(4)).isEqualTo(Rank.THIRD)
-        assertThat(Rank.aggregate(3)).isEqualTo(Rank.FOURTH)
+    fun `맞는 로또 갯수에 따라 등수를 반환한다`() {
+        assertThat(Rank.valueOf(6, false)).isEqualTo(Rank.FIRST)
+        assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND)
+        assertThat(Rank.valueOf(5, false)).isEqualTo(Rank.THIRD)
+        assertThat(Rank.valueOf(4, false)).isEqualTo(Rank.FOURTH)
+        assertThat(Rank.valueOf(3, false)).isEqualTo(Rank.FIFTH)
+        assertThat(Rank.valueOf(0, false)).isEqualTo(Rank.MISS)
+    }
+
+    @Test
+    fun `맞는 로또 갯수가 존재하지 않는다면 MISS를 반환한다`() {
+        assertThat(Rank.valueOf(2, false)).isEqualTo(Rank.MISS)
+        assertThat(Rank.valueOf(1, false)).isEqualTo(Rank.MISS)
     }
 }
