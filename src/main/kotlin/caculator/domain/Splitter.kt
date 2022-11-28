571adf875bbd9e4da7f8fb72439e9b4017cf234f
stkment <stkment@gmail.com>
2022-11-29 00:21:53 +0900

    :necktie: split 함수 구현

--- src/main/kotlin/caculator/domain/Splitter.kt
+++ src/main/kotlin/caculator/domain/Splitter.kt
@@ -3,5 +3,18 @@ package caculator.domain
 class Splitter(
     private val value: String,
 ) {
-    fun split(): List<String> {}
+    private val regex = REGEX_CUSTOM.toRegex()
+
+    fun split(): List<String> =
+        regex.find(value)
+            ?.let {
+                val (delimiter, numbers) = it.destructured
+                numbers.split(delimiter)
+            } ?: run { value.split(*DELIMITERS.toTypedArray()) }
+
+    companion object {
+        private val DELIMITERS = listOf(",", ":")
+
+        private const val REGEX_CUSTOM = "//(.)\n(.*)"
+    }
 }
