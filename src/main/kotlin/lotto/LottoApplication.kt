ac61bcdfbcc2491e0ce4c8fab0bbf69ed48392cd
dk-ko <dev.dkko@gmail.com>
2022-11-28 17:55:59 +0900

    refactor: 메소드 추출

--- src/main/kotlin/lotto/LottoApplication.kt
+++ src/main/kotlin/lotto/LottoApplication.kt
@@ -3,25 +3,42 @@ package lotto
 fun main() {
     val price = InputView.inputPrice()
     val number = Purchase(price).count()
+
     OutputView.printNumber(number)
 
     val lottoNumbersList = mutableListOf<LottoNumbers>()
-    for (i in 1..number) {
-        val drawSet = LottoMachine().draw()
-        lottoNumbersList.add(drawSet)
-    }
+    drawLottoNumber(number, lottoNumbersList)
 
-    for (lottoNumbers in lottoNumbersList) {
-        OutputView.print(lottoNumbers)
-    }
+    printLottoNumbersList(lottoNumbersList)
 
     val lastWeekWinningNumbers = InputView.inputLastWeekWinningNumbers()
 
     val statistics = Statistics()
+    prickWinningNumber(lottoNumbersList, lastWeekWinningNumbers, statistics)
+
+    OutputView.resultPrint(statistics, Price(number).price())
+}
+
+private fun prickWinningNumber(
+    lottoNumbersList: MutableList<LottoNumbers>,
+    lastWeekWinningNumbers: WinningLottoNumbers,
+    statistics: Statistics
+) {
     for (lottoNumbers in lottoNumbersList) {
         val win = lastWeekWinningNumbers.win(lottoNumbers)
         statistics.add(win)
     }
+}
 
-    OutputView.resultPrint(statistics, Price(number).price())
+private fun drawLottoNumber(number: Int, lottoNumbersList: MutableList<LottoNumbers>) {
+    for (i in 1..number) {
+        val drawSet = LottoMachine().draw()
+        lottoNumbersList.add(drawSet)
+    }
+}
+
+private fun printLottoNumbersList(lottoNumbersList: MutableList<LottoNumbers>) {
+    for (lottoNumbers in lottoNumbersList) {
+        OutputView.print(lottoNumbers)
+    }
 }
