429851a21f4531198a93babaaf4776ecf178c4c5
stkment <stkment@gmail.com>
2022-11-29 00:33:55 +0900

    :necktie: 덧셈 계산(add) 구현

--- src/main/kotlin/caculator/StringAddCalculator.kt
+++ src/main/kotlin/caculator/StringAddCalculator.kt
@@ -1,8 +1,13 @@
 package caculator
 
+import caculator.domain.Numbers
+import caculator.domain.Splitter
+
 class StringAddCalculator {
-    fun add(expression: String?): Int {
-        if(expression.isNullOrBlank()) return 0
-        TODO("Not Implemented")
-    }
+    fun add(expression: String?): Int =
+        expression.takeIf { !it.isNullOrBlank() }
+            ?.let(::Splitter)
+            ?.split()
+            ?.let(Numbers::invoke)
+            ?.sum() ?: 0
 }
