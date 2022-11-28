d61b90c4d49b3eae63ad13172fd20b99fbba63b0
stkment <stkment@gmail.com>
2022-11-29 00:33:29 +0900

    :necktie: sum 함수 구현

--- src/main/kotlin/caculator/domain/Numbers.kt
+++ src/main/kotlin/caculator/domain/Numbers.kt
@@ -8,6 +8,8 @@ class Numbers(
         if (numbers.any { it < 0 }) throw RuntimeException("잘못된 입력입니다. 양수만 입력해주세요.")
     }
 
+    fun sum(): Int = numbers.sum()
+
     companion object {
         operator fun invoke(numbers: List<String>): Numbers =
             numbers.map { it.toIntOrNull() ?: throw RuntimeException("잘못된 입력입니다. 숫자만 입력해주세요.") }
