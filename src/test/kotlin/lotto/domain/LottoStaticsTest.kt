a1bd8276e34df2dbd7e7500fce0d542419f20a3f
dreamboy1339 <dreamboy1339@gmail.com>
2022-11-29 01:49:09 +0900

    study(step2): 수익률 계산 테스트 코드에서 구매 금액과 당첨금 변수로 분리

--- src/test/kotlin/lotto/domain/LottoStaticsTest.kt
+++ src/test/kotlin/lotto/domain/LottoStaticsTest.kt
@@ -9,7 +9,11 @@ internal class LottoStaticsTest {
     @DisplayName("구매 금액과 당첨금을 비교해서 수익률을 계산한다")
     @Test
     fun bep() {
-        val result: Float = LottoStatics().makeEarningRate(5000.toFloat(), 14000.toFloat())
+        val amount = 14000f
+        val prize = 5000f
+
+        val result: Float = LottoStatics().makeEarningRate(prize, amount)
+
         result shouldBe 0.35f
     }
 }
