e79b02d7dec1ae51981cc9ac6df716d0b452f2d7
shinaaaa <shin029abc@gmail.com>
2022-11-28 14:05:42 +0900

    refactor: 보너스 번호 당첨 여부 추가

--- src/main/kotlin/lotto/domain/LottoGame.kt
+++ src/main/kotlin/lotto/domain/LottoGame.kt
@@ -7,11 +7,17 @@ object LottoGame {
     fun pick(numberOfGames: Int, lottoFactory: LottoFactory): List<Lotto> =
         MutableList(numberOfGames) { lottoFactory.create() }
 
-    fun getResultOfGames(gameBoard: List<Lotto>, winnerLotto: Lotto): List<LottoGameResult> {
-        return gameBoard.map { numberOfMatches(it, winnerLotto) }
-            .map { LottoGameResult.getResultOfWinning(it) }
-    }
+    fun getResultOfGames(gameBoard: List<Lotto>, winnerLotto: Lotto, bonusNumber: Int) =
+        gameBoard.map {
+            LottoGameResult.getResultOfWinning(
+                numberOfMatches(it, winnerLotto),
+                bonusOfMatches(it, bonusNumber)
+            )
+        }
 
     private fun numberOfMatches(lotto: Lotto, winnerLotto: Lotto) =
         lotto.numbers.filter { winnerLotto.numbers.contains(it) }.size
+
+    private fun bonusOfMatches(lotto: Lotto, bonus: Int) =
+        lotto.numbers.contains(bonus)
 }
