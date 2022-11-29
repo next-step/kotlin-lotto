2d87e2c35c7b03143a5c6b3eea779ca1e523db6c
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-30 00:37:20 +0900

    test(RankTest): 보너스 숫자가 일치할 때 / 일치하지 않을 때 맞는 결과값을 반환한다.

--- src/test/kotlin/lotto/domain/RankTest.kt
+++ src/test/kotlin/lotto/domain/RankTest.kt
@@ -5,38 +5,73 @@ import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 
 internal class RankTest : BehaviorSpec({
-    given("로또를 구매했을 때") {
-        `when`("5개의 수가 일치하고 보너스 숫자까지 일치한다면") {
-            val numberOfMatch = 5
-            val isBonusMatch = true
-            val result = Rank.valueOf(numberOfMatch, isBonusMatch)
 
-            then("2등 당첨이다.") {
-                result shouldBe Rank.SECOND
+    given("구입한 로또와 당첨번호의") {
+        `when`("보너스 숫자가 일치할 때") {
+            val isBonusMatch = true
+
+            and("5개의 수가 일치한다면") {
+                val numberOfMatch = 5
+
+                then("2등이다.") {
+                    val result = Rank.valueOf(numberOfMatch, isBonusMatch)
+                    result shouldBe Rank.SECOND
+                }
             }
-        }
 
-        `when`("보너스 숫자가 일치하지만 4개 이하의 개수가 일치한다면") {
-            val numberOfMatches = listOf(3, 4)
-            val isBonusMatch = true
+            and("4개의 수가 일치한다면") {
+                val numberOfMatch = 4
 
-            numberOfMatches.forAll {
-                val result = Rank.valueOf(it, isBonusMatch)
-
-                then("4등 이하 당첨이다.") {
-                    val ranks = listOf(Rank.FOURTH, Rank.FIFTH)
-                    ranks.contains(result) shouldBe true
+                then("4등이다.") {
+                    val result = Rank.valueOf(numberOfMatch, isBonusMatch)
+                    result shouldBe Rank.FOURTH
                 }
             }
         }
 
-        `when`("2개 이하의 수가 일치하거나 아무 숫자도 일치하지 않는다면") {
-            val numberOfMatches = listOf(2, 1, 0)
+        `when`("보너스 숫자가 일치하지 않을 때") {
+            val isBonusMatch = false
 
-            numberOfMatches.forAll {
-                val result = Rank.valueOf(it, false)
-                then("아무 등수도 당첨되지 않는다.") {
-                    result shouldBe Rank.MISS
+            and("2개 이하의 수가 일치한다면") {
+                val numberOfMatches = listOf(0, 1, 2)
+                numberOfMatches.forAll {
+
+                    then("꽝이다.") {
+                        val result = Rank.valueOf(it, isBonusMatch)
+                        result shouldBe Rank.MISS
+                    }
+                }
+            }
+            and("3개의 수가 일치한다면 ") {
+                val numberOfMatch = 3
+
+                then("5등이다.") {
+                    val result = Rank.valueOf(numberOfMatch, isBonusMatch)
+                    result shouldBe Rank.FIFTH
+                }
+            }
+            and("4개의 수가 일치한다면 ") {
+                val numberOfMatch = 4
+
+                then("4등이다.") {
+                    val result = Rank.valueOf(numberOfMatch, isBonusMatch)
+                    result shouldBe Rank.FOURTH
+                }
+            }
+            and("5개의 수가 일치한다면 ") {
+                val numberOfMatch = 5
+
+                then("3등이다.") {
+                    val result = Rank.valueOf(numberOfMatch, isBonusMatch)
+                    result shouldBe Rank.THIRD
+                }
+            }
+            and("6개의 수가 일치한다면") {
+                val numberOfMatch = 6
+
+                then("1등이다.") {
+                    val result = Rank.valueOf(numberOfMatch, isBonusMatch)
+                    result shouldBe Rank.FIRST
                 }
             }
         }
