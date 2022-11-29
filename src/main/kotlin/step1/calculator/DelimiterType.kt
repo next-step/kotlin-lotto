6f6912af6b49b9f7661ae3b4ed2f92963634a108
choi-ys <project.log.062@gmail.com>
2022-11-29 20:18:50 +0900

    feat: 구분자 타입 판별 및 추출
    - Kotlin Regex 객체를 활용한 기본/커스텀 구분자 판별 및 추출
    - 입력된 문자열에 구분자가 포함되지 않거나, 올바른 형식이 아닌 경우 예외 처리

---
+++ src/main/kotlin/step1/calculator/DelimiterType.kt
@@ -0,0 +1,62 @@
+package step1.calculator
+
+enum class DelimiterType(private val pattern: String) {
+    COMMA(","),
+    COLON(":"),
+    MIXED("[,:]"),
+    CUSTOM("""//(.)\n(.*)""");
+
+    private fun contains(expression: String): Boolean =
+        toRegex().containsMatchIn(expression)
+
+    fun toRegex(): Regex = pattern.toRegex()
+
+    fun find(expression: String): MatchResult =
+        toRegex().find(expression)
+            ?: throw IllegalArgumentException(NOT_MATCHED_PATTERN_ERROR_MESSAGE.format(expression, pattern))
+
+    fun extractDelimiter(expression: String): String {
+        if (isCustom()) {
+            return extractCustomDelimiter(expression)
+        }
+        return pattern
+    }
+
+    private fun isCustom(): Boolean = this == CUSTOM
+
+    private fun extractCustomDelimiter(expression: String): String {
+        val customDelimiterRegexPattern = CUSTOM.pattern.toRegex()
+        val findMatchGroup: MatchResult = customDelimiterRegexPattern.find(expression) ?: throw IllegalArgumentException("")
+        return findMatchGroup.groupValues[1]
+    }
+
+    companion object {
+        private const val DELIMITER_NOT_FOUND_ERROR_MESSAGE = """입력된 문자열 [%s]에서 구분자를 찾을 수 없습니다. 기본 구분자는 콤마 혹은 콜론을, 사용자 정의 구분자는 '//'와 '\n'사이에 입력하세요"""
+        private const val NOT_MATCHED_PATTERN_ERROR_MESSAGE = "입력된 문자열 [%s]와 패턴 [%s]이 일치하지 않습니다."
+
+        fun match(expression: String): DelimiterType {
+            if (containsMixed(expression)) {
+                return MIXED
+            }
+            return findDelimiterType(expression)
+        }
+
+        private fun containsMixed(expression: String): Boolean = containsComma(expression) && containsColon(expression)
+
+        private fun containsComma(expression: String): Boolean = COMMA.contains(expression)
+
+        private fun containsColon(expression: String): Boolean = COLON.contains(expression)
+
+        private fun findDelimiterType(expression: String): DelimiterType {
+            val matches: MutableSet<DelimiterType> = mutableSetOf()
+
+            values().map {
+                if (it.contains(expression)) {
+                    matches.add(it)
+                }
+            }
+
+            return matches.firstOrNull() ?: throw IllegalArgumentException(DELIMITER_NOT_FOUND_ERROR_MESSAGE.format(expression))
+        }
+    }
+}
---
+++ src/test/kotlin/step1/calculator/DelimiterTypeTest.kt
@@ -0,0 +1,41 @@
+package step1.calculator
+
+import io.kotest.assertions.throwables.shouldThrowExactly
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class DelimiterTypeTest : StringSpec({
+    "주어진 문자열의 구분자 타입을 판별한다." {
+        forAll(
+            row("콤마", "1,2,3", DelimiterType.COMMA),
+            row("콜론", "1:2:3", DelimiterType.COLON),
+            row("혼합형", "1,2:3", DelimiterType.MIXED),
+            row("커스텀 구분자", "//;\n4;5", DelimiterType.CUSTOM)
+        ) { description: String, given: String, actual: DelimiterType ->
+            DelimiterType.match(given) shouldBe actual
+        }
+    }
+
+    "구분자가 포함되지 않거나, 올바른 형식이 아닌 경우 예외가 발생한다." {
+        forAll(
+            row("구분자가 포함되지 않은 경우", "123"),
+            row("올바른 형식이 아닌 경우", "1|2|3")
+        ) { description: String, given: String ->
+            shouldThrowExactly<IllegalArgumentException> { DelimiterType.match(given) }
+        }
+    }
+
+    "판별한 구분자를 반환한다." {
+        forAll(
+            row("콤마", "1,2,3", ","),
+            row("콜론", "1:2:3", ":"),
+            row("혼합형", "1,2:3", "[,:]"),
+            row("커스텀 구분자", "//|\n1|2|3", "|")
+        ) { description: String, given: String, actual: String ->
+            val match = DelimiterType.match(given)
+            match.extractDelimiter(given) shouldBe actual
+        }
+    }
+})
