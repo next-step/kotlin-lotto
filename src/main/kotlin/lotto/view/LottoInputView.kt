58a49c45fe7147303f3fd5729e1a9c24d950e4c4
chayoung-choi <chayoung.dev@gmail.com>
2022-11-29 16:56:33 +0900

    feat: 수동 구매 로또 입력

--- src/main/kotlin/lotto/view/LottoInputView.kt
+++ src/main/kotlin/lotto/view/LottoInputView.kt
@@ -1,8 +1,10 @@
 package lotto.view
 
+import lotto.domain.Cash
 import lotto.domain.Lotto
 import lotto.domain.LottoConstants.LOTTE_PRICE
 import lotto.domain.LottoNumber
+import lotto.domain.Lottos
 
 object LottoInputView {
 
@@ -11,11 +13,12 @@ object LottoInputView {
     /**
      * 로또 구입 금액을 입력 받는다.
      */
-    fun inputPurchaseAmount(): Int {
+    fun inputPurchaseAmount(): Cash {
         println("구입 금액을 입력해 주세요.")
         val amount = readln().toInt()
         require(amount >= LOTTE_PRICE) { "로또 1장의 가격은 1000원입니다." }
-        return amount
+        println()
+        return Cash(amount)
     }
 
     /**
@@ -24,7 +27,7 @@ object LottoInputView {
     fun inputWinningNumbersOfLastWeek(): Lotto {
         println("지난 주 당첨 번호를 입력해 주세요.")
         val numbers = readln()
-        require(!numbers.isNullOrBlank()) { "지난 주 당첨 번호를 입력해주세요." }
+        require(numbers.isNotBlank()) { "지난 주 당첨 번호를 입력해주세요." }
         return Lotto(getLottoNumbers(numbers))
     }
 
@@ -34,8 +37,44 @@ object LottoInputView {
     fun inputBonusNumberOfLastWeek(): LottoNumber {
         println("보너스 볼을 입력해 주세요.")
         val number = readln()
-        require(!number.isNullOrBlank()) { "지난 주 당첨 번호를 입력해주세요." }
+        require(number.isNotBlank()) { "지난 주 당첨 번호를 입력해주세요." }
+        println()
         return LottoNumber(number.toInt())
+    }
+
+    /**
+     * 수동 구매 로또 수를 입력 받는다.
+     */
+    fun inputCountOfManualLotto(cash: Cash): Int {
+        println("수동으로 구매할 로또 수를 입력해 주세요.")
+        val number = readln()
+        if (number.isBlank() || !number.isInt()) {
+            println()
+            return 0
+        }
+        val count = number.toInt()
+        require(cash.amount >= count * LOTTE_PRICE) { "구입할 수 있는 수를 초과했습니다." }
+        println()
+        return count
+    }
+
+    /**
+     * 수동 로또 번호를 입력 받는다.
+     */
+    fun inputManualLottoNumbers(countOfLotto: Int): Lottos {
+        if (countOfLotto == 0) {
+            return Lottos()
+        }
+        println("수동으로 구매할 번호를 입력해 주세요.")
+
+        val numberList = mutableListOf<Set<LottoNumber>>()
+        repeat(countOfLotto) {
+            val numbers = readln()
+            require(numbers.isNotBlank()) { "잘못된 입력입니다." }
+            numberList.add(getLottoNumbers(numbers))
+        }
+        println()
+        return Lottos.of(numberList)
     }
 
     private fun getLottoNumbers(value: String): Set<LottoNumber> =
@@ -45,4 +84,6 @@ object LottoInputView {
 
     private fun splitNumbers(value: String) =
         value.split(NUMBER_SEPARATOR).filter { s -> s.isNotBlank() }.map { number -> number.toInt() }
+
+    private fun String.isInt(): Boolean = this.toIntOrNull() != null
 }
