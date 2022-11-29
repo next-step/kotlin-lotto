5001b98e4ae00118a0dc3c9a36babbfa6718d8fb
kimseonghun <seonghun127@woowahan.com>
2022-11-29 19:51:20 +0900

    feat: 당첨 번호와 보너스 번호에 대한 일치 결과를 반환한다.

--- src/main/kotlin/nextstep/mission/lotto/Lotto.kt
+++ src/main/kotlin/nextstep/mission/lotto/Lotto.kt
@@ -1,5 +1,6 @@
 package nextstep.mission.lotto
 
+import nextstep.mission.lotto.vo.LottoNumber
 import nextstep.mission.lotto.vo.LottoNumbers
 import nextstep.mission.lotto.vo.WinningPrize
 import nextstep.mission.lotto.vo.WinningResult
@@ -20,5 +21,25 @@ class Lotto(val lottoNumbers: List<LottoNumbers>) {
             }
         }
         return match(winningNumbers = winningNumbers)
+    }
+
+    fun matchWinningNumbers(winningNumbers: LottoNumbers, bonusNumber: LottoNumber): WinningResult {
+        tailrec fun match(
+            winningNumbers: LottoNumbers,
+            bonusNumber: LottoNumber,
+            lottoNumbers: MutableList<LottoNumbers> = this.lottoNumbers.toMutableList(),
+            result: MutableMap<WinningPrize, Int> = WinningPrize.values().associateWith { 0 }.toMutableMap()
+        ): WinningResult = when {
+            lottoNumbers.isEmpty() -> WinningResult(result.toMap())
+            else -> {
+                val currentLottoNumbers: LottoNumbers = lottoNumbers.removeFirst()
+                WinningPrize.find(
+                    currentLottoNumbers.match(winningNumbers),
+                    currentLottoNumbers.contains(bonusNumber)
+                )?.also { result[it] = result[it]!! + 1 }
+                match(winningNumbers, bonusNumber, lottoNumbers, result)
+            }
+        }
+        return match(winningNumbers = winningNumbers, bonusNumber = bonusNumber)
     }
 }
