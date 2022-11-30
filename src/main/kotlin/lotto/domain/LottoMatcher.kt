27bf66121869e8c5baeaea0d35d5034c9833a3b3
dreamboy1339 <dreamboy1339@gmail.com>
2022-11-30 23:46:12 +0900

    study(step3): 로또 당첨 여부 확인 로직에서 맞춘 개수 합산 로직 수정

--- src/main/kotlin/lotto/domain/LottoMatcher.kt
+++ src/main/kotlin/lotto/domain/LottoMatcher.kt
@@ -8,11 +8,9 @@ object LottoMatcher {
     }
 
     fun countMatchNumber(winningNumbers: List<Int>, lottoNumbers: List<Int>): Int {
-        var count = 0
-        winningNumbers.forEach { winningNumber ->
-            count += checkContainWinningNumber(winningNumber = winningNumber, lottoNumbers = lottoNumbers)
+        return winningNumbers.sumOf {
+            checkContainWinningNumber(winningNumber = it, lottoNumbers = lottoNumbers)
         }
-        return count
     }
 
     private fun checkContainWinningNumber(winningNumber: Int, lottoNumbers: List<Int>): Int {
