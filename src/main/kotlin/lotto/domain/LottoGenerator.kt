d1cae7beeb5e335825d2c18786b3cad74660733e
namonak <namonak@gmail.com>
2022-12-02 22:36:49 +0900

    refactor(step2): modify it to generate lottery numbers using a Kotlin List

--- src/main/kotlin/lotto/domain/LottoGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoGenerator.kt
@@ -1,9 +1,11 @@
 package lotto.domain
 
+import lotto.domain.LottoNumber.Companion.MAXIMUM_LOTTO_NUMBER
+import lotto.domain.LottoNumber.Companion.MINIMUM_LOTTO_NUMBER
 import lotto.domain.LottoNumbers.Companion.MAXIMUM_LOTTO_NUMBER_LENGTH
 
 object LottoGenerator {
-    private val lottoNumbers = (LottoNumber.MINIMUM_LOTTO_NUMBER..LottoNumber.MAXIMUM_LOTTO_NUMBER).map { LottoNumber(it) }
+    private val lottoNumbers = List(MAXIMUM_LOTTO_NUMBER) { LottoNumber(it + MINIMUM_LOTTO_NUMBER) }
 
     fun generate(): LottoNumbers {
         return LottoNumbers(lottoNumbers.shuffled().take(MAXIMUM_LOTTO_NUMBER_LENGTH).sortedBy { it.value })
