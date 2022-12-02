127847c29ccf24511a11381c5b2fc2d5d1ddc3bd
shinaaaa <shin029abc@gmail.com>
2022-12-02 11:13:09 +0900

    refactor: 가독성을 위해 함수로 분리

--- src/main/kotlin/lotto/controller/LottoGameController.kt
+++ src/main/kotlin/lotto/controller/LottoGameController.kt
@@ -1,28 +1,43 @@
 package lotto.controller
 
+import lotto.domain.Lotto
 import lotto.domain.LottoGame
 import lotto.domain.LottoGameResult
+import lotto.domain.PurchaseResult
 import lotto.view.InputView
 import lotto.view.ResultView
 
 class LottoGameController {
 
     fun start() {
+        val (purchaseResult, numberOfManual, numberOfAuto) = buyLottoGame()
+
+        val manualLotto = InputView.pickManualLottoNumber(numberOfManual)
+        val gameBoard = LottoGame.setGameBoard(manualLotto, numberOfAuto)
+
+        gameBoard.forEach { ResultView.printChosenNumber(it.numbers) }
+
+        val gameResults = checkAnswers(gameBoard)
+        statistics(gameResults, purchaseResult)
+    }
+
+    private fun buyLottoGame(): Triple<PurchaseResult, Int, Int> {
         val purchaseResult = InputView.purchaseCost()
         val numberOfManual =
             InputView.numberOfManualPurchase(purchaseResult.numberOfGames)
         val numberOfAuto =
             LottoGame.calculateNumberOfAutoGames(purchaseResult.numberOfGames, numberOfManual.numberOfGames)
         ResultView.buyResult(numberOfManual.numberOfGames, numberOfAuto)
+        return Triple(purchaseResult, numberOfManual.numberOfGames, numberOfAuto)
+    }
 
-        val manualLotto = InputView.pickManualLottoNumber(numberOfManual.numberOfGames)
-        val gameBoard = LottoGame.setGameBoard(manualLotto, numberOfAuto)
-
-        gameBoard.forEach { ResultView.printChosenNumber(it.numbers) }
-
+    private fun checkAnswers(gameBoard: List<Lotto>): List<LottoGameResult> {
         val winnerNumber = InputView.winningNumberOfLastWeek()
         val bonusNumber = InputView.bonusNumberOfLastWeek(winnerNumber)
-        val gameResults = LottoGame.getResultOfGames(gameBoard, winnerNumber.winnerNumber, bonusNumber.bonusNumber)
+        return LottoGame.getResultOfGames(gameBoard, winnerNumber.winnerNumber, bonusNumber.bonusNumber)
+    }
+
+    private fun statistics(gameResults: List<LottoGameResult>, purchaseResult: PurchaseResult) {
         ResultView.noticeOfPrize()
         val statistics = LottoGameResult.winningStatistics(gameResults)
         ResultView.printWinningStatistics(statistics)
