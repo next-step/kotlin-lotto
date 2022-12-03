f0e3281c7f1dab2f003a25de1e57e306c9760a29
shinaaaa <shin029abc@gmail.com>
2022-12-03 19:48:35 +0900

    refactor: 로또 게임의 구매 결과를 원시값이 아닌 객체로 반환하도록 수정

--- src/main/kotlin/lotto/controller/LottoGameController.kt
+++ src/main/kotlin/lotto/controller/LottoGameController.kt
@@ -4,36 +4,42 @@ import lotto.domain.AutoLotto
 import lotto.domain.Lotto
 import lotto.domain.LottoGame
 import lotto.domain.LottoGameResult
-import lotto.domain.PurchaseResult
+import lotto.domain.LottoGameStatus
 import lotto.view.InputView
 import lotto.view.ResultView
+import java.math.BigDecimal
 
 class LottoGameController {
 
     fun start() {
-        val (purchaseResult, numberOfManual, numberOfAuto) =
-            getPurchaseResultsAndNumberOfPurchases()
+        val gameStatus = getPurchaseResultsAndNumberOfPurchases()
 
-        val manualLottoList: List<Lotto> = InputView.pickManualLottoNumber(numberOfManual)
-        val autoLottoList: List<Lotto> = LottoGame.pick(numberOfAuto, AutoLotto)
+        val manualLottoList: List<Lotto> = InputView.pickManualLottoNumber(gameStatus.numberOfManual)
+        val autoLottoList: List<Lotto> = LottoGame.pick(gameStatus.numberOfAuto, AutoLotto)
 
-        ResultView.buyResult(numberOfManual, numberOfAuto)
+        ResultView.buyResult(gameStatus.numberOfManual, gameStatus.numberOfAuto)
 
         val gameBoard = LottoGame.setGameBoard(manualLottoList, autoLottoList)
 
         gameBoard.forEach { ResultView.printChosenNumber(it.numbers) }
 
         val gameResults = checkAnswers(gameBoard)
-        printStatistics(gameResults, purchaseResult)
+        printStatistics(gameResults, gameStatus.cost)
     }
 
-    private fun getPurchaseResultsAndNumberOfPurchases(): Triple<PurchaseResult, Int, Int> {
+    private fun getPurchaseResultsAndNumberOfPurchases(): LottoGameStatus {
         val purchaseResult = InputView.purchaseCost()
         val numberOfManual =
             InputView.numberOfManualPurchase(purchaseResult.numberOfGames)
         val numberOfAuto =
             LottoGame.calculateNumberOfAutoGames(purchaseResult.numberOfGames, numberOfManual.numberOfGames)
-        return Triple(purchaseResult, numberOfManual.numberOfGames, numberOfAuto)
+
+        return LottoGameStatus(
+            purchaseResult.purchaseCost,
+            purchaseResult.numberOfGames,
+            numberOfManual.numberOfGames,
+            numberOfAuto
+        )
     }
 
     private fun checkAnswers(gameBoard: List<Lotto>): List<LottoGameResult> {
@@ -42,11 +48,11 @@ class LottoGameController {
         return LottoGame.getResultOfGames(gameBoard, winnerNumber.winnerNumber, bonusNumber.bonusNumber)
     }
 
-    private fun printStatistics(gameResults: List<LottoGameResult>, purchaseResult: PurchaseResult) {
+    private fun printStatistics(gameResults: List<LottoGameResult>, cost: BigDecimal) {
         ResultView.noticeOfPrize()
         val statistics = LottoGameResult.winningStatistics(gameResults)
         ResultView.printWinningStatistics(statistics)
-        val rate = LottoGameResult.rate(gameResults, purchaseResult.purchaseCost)
+        val rate = LottoGameResult.rate(gameResults, cost)
         ResultView.printRate(rate)
     }
 }
