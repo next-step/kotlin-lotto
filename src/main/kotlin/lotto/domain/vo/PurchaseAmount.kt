f68b4ffc83c1d0772c16bb61f87245b3f6293770
seongbeen.kim <seongbeen.kim@linecorp.com>
2022-12-03 13:06:37 +0900

    feat: Add value object for purchase amount

---
+++ src/main/kotlin/lotto/domain/vo/PurchaseAmount.kt
@@ -0,0 +1,13 @@
+package lotto.domain.vo
+
+@JvmInline
+value class PurchaseAmount(val amount: Int) {
+    init {
+        require(amount >= MIN_VALUE) { "amount should be at least $MIN_VALUE" }
+        require(amount % MIN_VALUE == 0) { "amount should be in units of $MIN_VALUE" }
+    }
+
+    companion object {
+        private const val MIN_VALUE: Int = 1000
+    }
+}
