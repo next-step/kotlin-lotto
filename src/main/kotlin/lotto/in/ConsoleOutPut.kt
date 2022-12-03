f2f58da6e065028a7b045b8f276f416463fd94ee
seongbeen.kim <seongbeen.kim@linecorp.com>
2022-12-03 21:21:06 +0900

    feat: Add feature to print lotto numbers

--- src/main/kotlin/lotto/in/ConsoleOutPut.kt
+++ src/main/kotlin/lotto/in/ConsoleOutPut.kt
@@ -1,3 +1,11 @@
 package lotto.`in`
 
-class ConsoleOutPut
+class ConsoleOutPut {
+    fun printPurchasedLottoCount(lottoCount: Int) {
+        println("${lottoCount}개를 구매했습니다.")
+    }
+
+    fun printLottoNumbers(lottoNumbers: List<Int>) {
+        println(lottoNumbers)
+    }
+}
--- src/main/kotlin/lotto/in/LottoController.kt
+++ src/main/kotlin/lotto/in/LottoController.kt
@@ -1,9 +1,14 @@
 package lotto.`in`
 
+import lotto.domain.LottoMachine
 import lotto.domain.vo.PurchaseAmount
 
 class LottoController(private val input: ConsoleInput, private val outPut: ConsoleOutPut) {
     fun start() {
         val purchaseAmount = PurchaseAmount(input.getPurchaseAmount())
+        val lottoNumbers = LottoMachine.createLottoNumbers(purchaseAmount)
+
+        outPut.printPurchasedLottoCount(lottoNumbers.size)
+        lottoNumbers.forEach { outPut.printLottoNumbers(it.numbers()) }
     }
 }
