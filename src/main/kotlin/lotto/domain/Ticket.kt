63e91e038efeeb03f9e08c574460f53c2189231a
jysohn0825 <jysohn0825@naver.com>
2022-12-03 18:07:44 +0900

    style: ktlinkCheck 적용

--- src/main/kotlin/lotto/domain/Ticket.kt
+++ src/main/kotlin/lotto/domain/Ticket.kt
@@ -1,16 +1,15 @@
 package lotto.domain
 
-import java.lang.IllegalArgumentException
-
 class Ticket(
     private val numbers: List<Int> = LOTTO_RANGE.shuffled().take(LOTTO_COUNT).sorted()
-){
+) {
     init {
         valid(numbers)
     }
-    private fun valid(numbers: List<Int>){
-        if(numbers.size != LOTTO_COUNT) throw IllegalArgumentException("로또 번호 6개에 적합하지 않습니다")
-        if(numbers.toSet().size != LOTTO_COUNT) throw IllegalArgumentException("중복된 로또 번호가 존재합니다")
+
+    private fun valid(numbers: List<Int>) {
+        if (numbers.size != LOTTO_COUNT) throw IllegalArgumentException("로또 번호 6개에 적합하지 않습니다")
+        if (numbers.toSet().size != LOTTO_COUNT) throw IllegalArgumentException("중복된 로또 번호가 존재합니다")
     }
 
     fun getNumbers() = this.numbers
