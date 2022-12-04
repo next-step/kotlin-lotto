a53ff5037c655b421904cd826b6de106cf964c25
0923kdh <0923kdh@gmail.com>
2022-12-04 16:40:37 +0900

    feat: 로또 인풋 기능 구현 - 구입 금액을 입력받을 수 있다. - 지난주 당첨 번호를 입력받을 수 있다. - 구입 금액에 따른 로또 발행 장수를 출력할 수 있다.

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,10 +1,24 @@
 package lotto.view
 
 object InputView {
-    fun getPurchaseAmount(): Int {
+    fun getNumberOfPurchases(): Int {
+        println("구매금액을 입력해 주세요.")
         val amount = readlnOrNull()
         require(!amount.isNullOrEmpty()) { "금액을 입력해주세요." }
 
-        return amount.toInt()
+        println(amount)
+
+        val tickets = amount.toInt() / 1000
+        println("$tickets 개를 구매했습니다.")
+
+        return tickets
+    }
+
+    fun getWinningNumber(): List<Int> {
+        val winningNumber = readlnOrNull()
+        require(!winningNumber.isNullOrEmpty()) { "당첨 번호를 입력해주세요." }
+        println("지난 주 당첨 번호를 입력해 주세요.")
+
+        return  winningNumber.split(", ").toList().map { it.toInt() }
     }
 }
