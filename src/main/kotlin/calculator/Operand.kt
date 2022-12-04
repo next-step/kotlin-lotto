1a44ae66f067ea8ddeb430a727fad043fb6132ca
relkim-m <relkimm@mfort.co.kr>
2022-12-04 17:52:24 +0900

    feat: 숫자를 의미하는 Operand 구현

---
+++ src/main/kotlin/calculator/Operand.kt
@@ -0,0 +1,11 @@
+package calculator
+
+class Operand private constructor(val value: Int) {
+    companion object {
+        fun from(expression: String): Operand {
+            val number = expression.toIntOrNull()
+            require(number != null && number >= 0) { "음수 또는 숫자가 아닌 값을 입력할 수 없습니다." }
+            return Operand(value = number)
+        }
+    }
+}
