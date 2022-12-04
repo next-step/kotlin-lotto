bbc752151fa88642e314c5235de934b84327d3f9
dk-ko <dev.dkko@gmail.com>
2022-12-04 15:31:37 +0900

    feat: 코드리뷰 반영

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -1,17 +1,14 @@
 package lotto
 
-class InputView {
-    companion object {
-        fun inputPrice(): Int {
-            println("구입금액을 입력해 주세요.")
-            return readln().toInt()
-        }
-
-        fun inputLastWeekWinningNumbers(): WinningLottoNumbers {
-            println("\n지난 주 당첨 번호를 입력해 주세요.")
-            val inputLastWeekWinningNumbers = readln()
-            val parse = Parser().parse(inputLastWeekWinningNumbers).toSet()
-            return WinningLottoNumbers(parse)
-        }
+object InputView {
+    fun inputPrice(): Int {
+        println("구입금액을 입력해 주세요.")
+        return readln().toInt()
+    }
+    fun inputLastWeekWinningNumbers(): WinningLottoNumbers {
+        println("\n지난 주 당첨 번호를 입력해 주세요.")
+        val inputLastWeekWinningNumbers = readln()
+        val parse = Parser().parse(inputLastWeekWinningNumbers).toSet()
+        return WinningLottoNumbers(parse)
     }
 }
