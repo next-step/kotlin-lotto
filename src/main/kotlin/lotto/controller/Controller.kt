fb633d1a852df6516fe0dd6a81a89c9b894aaeeb
namonak <namonak@gmail.com>
2022-12-05 02:22:32 +0900

    refactor(step3): add InputFilter class for input filtering

--- src/main/kotlin/lotto/controller/Controller.kt
+++ src/main/kotlin/lotto/controller/Controller.kt
@@ -2,56 +2,23 @@ package lotto.controller
 
 import lotto.application.LottoGenerator
 import lotto.application.LottoResultGenerator
-import lotto.domain.InputParser
-import lotto.domain.LottoNumber
-import lotto.domain.LottoNumbers
-import lotto.domain.LottoPrice
 import lotto.domain.WinningNumbers
 import lotto.dto.LottoResultDto
-import lotto.view.InputView
 import lotto.view.ResultView
 
 object Controller {
     fun start() {
-        val amountOfPurchase = inputAmountOfPurchase()
+        val amountOfPurchase = InputFilter.inputAmountOfPurchase()
         val numberOfLotto = amountOfPurchase.calculateNumberOfLotto()
         ResultView.printNumberOfLotto(numberOfLotto)
         val lottoNumbersList = LottoGenerator.generateLottoNumbers(numberOfLotto)
         ResultView.printLottoNumbersList(lottoNumbersList.getList())
 
-        val winningNumbers = inputWinningNumbers()
-        val bonusNumber = inputBonusNumber()
+        val winningNumbers = InputFilter.inputWinningNumbers()
+        val bonusNumber = InputFilter.inputBonusNumber()
         val winningNumbersWithBonusNumber = WinningNumbers(winningNumbers, bonusNumber)
         val lottoResultGenerator = LottoResultGenerator(winningNumbersWithBonusNumber, lottoNumbersList)
         val lottoResult = lottoResultGenerator.getResult()
         ResultView.printFinalResult(LottoResultDto(lottoResult))
-    }
-
-    private fun inputAmountOfPurchase(): LottoPrice {
-        return try {
-            LottoPrice(InputView.inputAmount())
-        } catch (e: Exception) {
-            InputView.printError(e.message!!)
-            inputAmountOfPurchase()
-        }
-    }
-
-    private fun inputWinningNumbers(): LottoNumbers {
-        return try {
-            val parsedInput = InputParser.parseWithDelimiter(InputView.inputWinningNumber())
-            LottoNumbers(parsedInput.map { LottoNumber(it) })
-        } catch (e: Exception) {
-            InputView.printError(e.message!!)
-            inputWinningNumbers()
-        }
-    }
-
-    private fun inputBonusNumber(): LottoNumber {
-        return try {
-            LottoNumber(InputView.inputBonusNumber())
-        } catch (e: Exception) {
-            InputView.printError(e.message!!)
-            inputBonusNumber()
-        }
     }
 }
---
+++ src/main/kotlin/lotto/controller/InputFilter.kt
@@ -0,0 +1,37 @@
+package lotto.controller
+
+import lotto.domain.InputParser
+import lotto.domain.LottoNumber
+import lotto.domain.LottoNumbers
+import lotto.domain.LottoPrice
+import lotto.view.InputView
+
+object InputFilter {
+    fun inputAmountOfPurchase(): LottoPrice {
+        return try {
+            LottoPrice(InputView.inputAmount())
+        } catch (e: Exception) {
+            InputView.printError(e.message!!)
+            inputAmountOfPurchase()
+        }
+    }
+
+    fun inputWinningNumbers(): LottoNumbers {
+        return try {
+            val parsedInput = InputParser.parseWithDelimiter(InputView.inputWinningNumber())
+            LottoNumbers(parsedInput.map { LottoNumber(it) })
+        } catch (e: Exception) {
+            InputView.printError(e.message!!)
+            inputWinningNumbers()
+        }
+    }
+
+    fun inputBonusNumber(): LottoNumber {
+        return try {
+            LottoNumber(InputView.inputBonusNumber())
+        } catch (e: Exception) {
+            InputView.printError(e.message!!)
+            inputBonusNumber()
+        }
+    }
+}
