33271cbb8ea09b62a58df652dde2ede2a671841b
youngbin-yun <youngbin@koreaspacedata.com>
2022-12-04 22:32:22 +0900

    feat(LottoController): 수동 로또가 추가된 로직으로 변경

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -8,13 +8,18 @@ class LottoController {
     fun run() {
         val purchaseAmount = InputView.getPurchaseAmount()
         val purchaseCount = purchaseAmount.getNumberOfLotto()
-        ResultView.printNumberOfPurchases(purchaseCount)
 
-        val myLotteries = LottoVendingMachine.buy(purchaseCount)
-        ResultView.printPurchasedLotto(myLotteries)
+        val manualNumberOfLotto = InputView.getManualNumberOfLotto(purchaseAmount)
+        val manualUserLottos = InputView.getManualLottos(manualNumberOfLotto)
+        val randomNumberOfLotto = purchaseCount - manualNumberOfLotto
+
+        val randomUserLottos = LottoVendingMachine.buyRandomLottos(randomNumberOfLotto)
+        val userLottos = manualUserLottos.addAll(randomUserLottos)
+        ResultView.printPurchaseStatus(manualNumberOfLotto, randomNumberOfLotto)
+        ResultView.printPurchasedLotto(userLottos)
 
         val winningLottoNumbers = InputView.getWinningLottoNumbers()
-        val winningStatistics = winningLottoNumbers.makeStatistics(myLotteries)
+        val winningStatistics = winningLottoNumbers.makeStatistics(userLottos)
         ResultView.printRewardsStatistics(winningStatistics)
 
         val rateOfReturn = winningStatistics.calculateRateOfReturn(purchaseAmount)
