66e6d5ffe21d675e3d432199effc124dd853d96b
seongbeen.kim <seongbeen.kim@linecorp.com>
2022-12-05 20:46:28 +0900

    refactor: Cut chaining for readability

--- src/main/kotlin/lotto/in/ConsoleInput.kt
+++ src/main/kotlin/lotto/in/ConsoleInput.kt
@@ -10,12 +10,10 @@ class ConsoleInput {
     fun getWinnerNumbers(): List<Int> {
         println("지난 주 당첨 번호를 입력해 주세요.")
 
-        val winningNumbers = readNonBlankLn()
+        val winningNumbers = readNonBlankLn().takeIf { it.contains(WINNING_NUMBER_DELIMITER) } ?: throw IllegalArgumentException("winning numbers should contain $WINNING_NUMBER_DELIMITER")
 
-        return winningNumbers.takeIf { it.contains(WINNING_NUMBER_DELIMITER) }
-            ?.split(WINNING_NUMBER_DELIMITER)
-            ?.map { it.trim().toIntOrNull() ?: throw IllegalArgumentException("each winning number should be number") }
-            ?: throw IllegalArgumentException("winning numbers should contain $WINNING_NUMBER_DELIMITER")
+        return winningNumbers.split(WINNING_NUMBER_DELIMITER)
+            .map { it.trim().toIntOrNull() ?: throw IllegalArgumentException("each winning number should be number") }
     }
 
     private fun readNonBlankLn() = readln().ifBlank { throw IllegalArgumentException("empty string is not allowed") }
