f6862644045bba085c3c25312566d8f6b0f50d84
choi-ys <project.log.062@gmail.com>
2022-12-10 06:15:18 +0900

    feat: 1~45 범위의 로또 번호를 표현하는 LottoNumber 도메인 정의
    - 프로그램 시작 시, 1부터 45까지의 값을 가지는 LottoNumber 객체를 생성 후 Map에 저장하여, LottoNumber 객체 생성 요청 마다 객체를 생성하지 않고 이미 Map에 생성된 동일 객체를 반환한다.

---
+++ src/main/kotlin/step2/lotto/domain/LottoNumber.kt
@@ -0,0 +1,26 @@
+package step2.lotto.domain
+
+class LottoNumber private constructor(value: Int) {
+    companion object {
+        private const val START_RANGE = 1
+        private const val END_RANGE = 45
+        private const val OUT_OF_LOTTO_NUMBER_RANGE_ERROR_MESSAGE = "로또 범위에 해당하는 숫자가 아닙니다. [%d], [${START_RANGE} ~ ${END_RANGE}] 범위에 숫자만 입력하세요."
+        private val LOTTO_NUMBER_RANGE: IntRange = START_RANGE..END_RANGE
+        private val lottoNumbers: Map<Int, LottoNumber> = init()
+
+        private fun init(): Map<Int, LottoNumber> = mutableMapOf<Int, LottoNumber>().apply {
+            repeat(END_RANGE) {
+                this[it] = LottoNumber(it)
+            }
+        }
+
+        fun of(randomNumber: Int): LottoNumber {
+            validateRandomNumber(randomNumber)
+            return lottoNumbers.getValue(randomNumber)
+        }
+
+        private fun validateRandomNumber(inputNumber: Int): Unit = require(isRangeToLottoNumber(inputNumber)) { OUT_OF_LOTTO_NUMBER_RANGE_ERROR_MESSAGE.format(inputNumber) }
+
+        private fun isRangeToLottoNumber(inputNumber: Int): Boolean = inputNumber in (LOTTO_NUMBER_RANGE)
+    }
+}
---
+++ src/test/kotlin/step2/lotto/domain/LottoNumberTest.kt
@@ -0,0 +1,23 @@
+package step2.lotto.domain
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.types.shouldBeSameInstanceAs
+
+internal class LottoNumberTest : StringSpec({
+    "같은 번호로 로또 객체를 생성하면 동일한 객체가 반환된다." {
+        val given = 1
+        LottoNumber.of(given) shouldBeSameInstanceAs LottoNumber.of(given)
+    }
+
+    "1~45범위를 넘어서는 로또 번호 객체 생성시 예외가 발생한다." {
+        forAll(
+            row(0), row(46)
+        ) { given: Int ->
+            shouldThrow<IllegalArgumentException> { LottoNumber.of(given) }
+        }
+    }
+})
+
