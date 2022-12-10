3029cb14dfffab015ccc4c71973e03d3c7d8ec6f
chomily <chomily@woowahan.com>
2022-12-10 19:24:28 +0900

    refactor: 피드백 반영(Expression 클래스 분리)

--- src/main/kotlin/com/nextstep/calculator/StringAddCalculator.kt
+++ src/main/kotlin/com/nextstep/calculator/StringAddCalculator.kt
@@ -1,32 +1,33 @@
 package com.nextstep.calculator
 
 class StringAddCalculator {
-    private val defaultDelimiters = arrayOf(":", ",")
     private val regex = Regex("//(.)\n(.*)")
 
     fun calculate(input: String?): Int {
         if (input.isNullOrBlank()) return 0
-
-        val (delimiters, expression) = parse(input)
-
-        return expression.split(delimiters = delimiters).map { toInt(it) }.sumOf { validateAndReturn(it) }
+        return createExpression(input).calculate()
     }
 
-    private fun parse(input: String): Pair<Array<String>, String> {
+    private fun createExpression(input: String): Expression {
         val matches = regex.find(input)
-
         if (matches != null) {
-            return Pair(arrayOf(matches.groupValues[1]), matches.groupValues[2])
+            return Expression(delimiters = arrayOf(matches.groupValues[1]), expression = matches.groupValues[2])
         }
-        return Pair(defaultDelimiters, input)
+        return Expression(expression = input)
     }
 
-    private fun toInt(num: String): Int {
-        return num.toIntOrNull() ?: throw IllegalArgumentException()
-    }
+    class Expression(private val expression: String, private vararg val delimiters: String = arrayOf(":", ",")) {
+        fun calculate() = expression.split(delimiters = delimiters)
+                .map { toInt(it) }
+                .sumOf { validateAndReturn(it) }
 
-    private fun validateAndReturn(number: Int): Int {
-        require(number >= 0) { "음수는 입력할 수 없습니다, number: $number" }
-        return number
+        private fun toInt(num: String): Int {
+            return num.toIntOrNull() ?: throw IllegalArgumentException()
+        }
+
+        private fun validateAndReturn(number: Int): Int {
+            require(number >= 0) { "음수는 입력할 수 없습니다, number: $number" }
+            return number
+        }
     }
 }
