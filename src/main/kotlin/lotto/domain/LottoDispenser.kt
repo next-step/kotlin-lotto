3c6ca6bfcbc21059c69fddfcb57d835537a918fa
dreamboy1339 <dreamboy1339@gmail.com>
2022-12-10 20:50:56 +0900

    study(step3): 로또 금액 1000원 미만 조건을 require 로 처리

--- src/main/kotlin/lotto/domain/LottoDispenser.kt
+++ src/main/kotlin/lotto/domain/LottoDispenser.kt
@@ -7,9 +7,7 @@ class LottoDispenser(private val amount: Int) {
     val list: List<Lotto> = makeLottoList()
 
     init {
-        if (amount < MINIMUM_PRICE) {
-            throw IllegalArgumentException("구입 금액은 ${MINIMUM_PRICE}원 이하가 될 수 없습니다")
-        }
+        require(amount >= MINIMUM_PRICE) { "구입 금액은 ${MINIMUM_PRICE}원 이하가 될 수 없습니다" }
     }
 
     private fun makeLottoList(): List<Lotto> {
