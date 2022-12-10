cf456748e35f1a5d41d8e9a8785e3f017ccdd4f4
조수진 <sujinemilycho@gmail.com>
2022-12-11 01:33:34 +0900

    [step1] 문자열 덧셈 계산기 (#513)
    * docs: README 작성
    
    * feat: null, 빈문자열 입력 시 0 반환
    
    * feat: 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다
    
    * feat: 숫자를 파싱해서 덧샘 연산을 한다
    
    * feat: 커스텀 연산자를 입력받을 수 있다
    
    * feat: 숫자 이외의 값, 음수를 전달하면 Exception 발생
    
    * refactor: delimiters, expression 추출하는 메서드 분리
    
    * refactor: 피드백 반영
    
    * refactor: 피드백 반영(Expression 클래스 분리)
    
    Co-authored-by: chomily <chomily@woowahan.com>

---
+++ src/main/kotlin/com/nextstep/calculator/StringAddCalculator.kt
@@ -0,0 +1,33 @@
+package com.nextstep.calculator
+
+class StringAddCalculator {
+    private val regex = Regex("//(.)\n(.*)")
+
+    fun calculate(input: String?): Int {
+        if (input.isNullOrBlank()) return 0
+        return createExpression(input).calculate()
+    }
+
+    private fun createExpression(input: String): Expression {
+        val matches = regex.find(input)
+        if (matches != null) {
+            return Expression(delimiters = arrayOf(matches.groupValues[1]), expression = matches.groupValues[2])
+        }
+        return Expression(expression = input)
+    }
+
+    class Expression(private val expression: String, private vararg val delimiters: String = arrayOf(":", ",")) {
+        fun calculate() = expression.split(delimiters = delimiters)
+                .map { toInt(it) }
+                .sumOf { validateAndReturn(it) }
+
+        private fun toInt(num: String): Int {
+            return num.toIntOrNull() ?: throw IllegalArgumentException()
+        }
+
+        private fun validateAndReturn(number: Int): Int {
+            require(number >= 0) { "음수는 입력할 수 없습니다, number: $number" }
+            return number
+        }
+    }
+}
---
+++ src/test/kotlin/com/nextstep/calculator/StringAddCalculatorTest.kt
@@ -0,0 +1,39 @@
+package com.nextstep.calculator
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class StringAddCalculatorTest : StringSpec({
+    val calculator = StringAddCalculator()
+
+    "빈 문자열 또는 null을 입력할 경우 0을 반환한다" {
+        calculator.calculate(null) shouldBe 0
+        calculator.calculate(" ") shouldBe 0
+    }
+
+    "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다" {
+        calculator.calculate("5") shouldBe 5
+    }
+
+    "숫자를 파싱해서 더한다" {
+        calculator.calculate("1,3") shouldBe 4
+    }
+
+    "//와 \n 문자 사이에 커스텀 연산자를 받을 수 있다" {
+        calculator.calculate("//;\n1;2;3") shouldBe 6
+    }
+
+    "숫자 이외의 값 혹은 음수를 전달하면 throw IllegalArgumentException" {
+        forAll(
+            row("-1"),
+            row("1,-1,6"),
+            row("-1,;,3"),
+            row("//;\n1;3a5")
+        ) { expression ->
+            shouldThrow<IllegalArgumentException> { calculator.calculate(expression) }
+        }
+    }
+})
