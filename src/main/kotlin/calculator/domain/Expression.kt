2b5432ff5abe0d92abc400caebb0e3b15aa0281c
ChoiYounho <maxosa78@gmail.com>
2022-12-12 20:23:56 +0900

    refactor : 수식 코드 리팩토링

--- src/main/kotlin/calculator/domain/Expression.kt
+++ src/main/kotlin/calculator/domain/Expression.kt
@@ -1,37 +1,45 @@
 package calculator.domain
 
-import calculator.util.RegexUtil.customRegex
+import calculator.util.validate
 
 class Expression private constructor(
     val value: List<ExpressionElement> = emptyList()
 ) {
 
+    fun fetchOperands(): List<Int> {
+        return value.filterIsInstance<ExpressionElement.OperandElement>().map { it.value }
+    }
+
+    fun fetchOperators(): List<Operator> {
+        return value.filterIsInstance<ExpressionElement.OperatorElement>().map { it.value }
+    }
+
     companion object {
+
+        private const val MESSAGE_NOT_POSITIVE_NUMBER = "양수가 아닌 숫자를 전달 받을 수 없습니다."
+
         fun create(input: String, delimiters: Delimiters): Expression {
             if (input.isEmpty()) {
-                return Expression(emptyList())
+                return Expression()
             }
 
-            val value = removeCustomRegex(input)
-                .asSequence()
-                .map { char ->
-                    if (char.isDigit()) {
-                        ExpressionElement.OperandElement(char.digitToInt())
+            val value = delimiters.removeCustomRegex(input)
+                .map {
+                    if (it.isDigit()) {
+                        val digit = it.digitToInt()
+
+                        val isNotNegativeNumber = digit > -1
+                        validate(isNotNegativeNumber) { MESSAGE_NOT_POSITIVE_NUMBER }
+
+                        ExpressionElement.OperandElement(digit)
                     } else {
-                        ExpressionElement.OperatorElement(delimiters.toOperator(char.toString()))
+                        ExpressionElement.OperatorElement(delimiters.toOperator(it.toString()))
+
                     }
-                }.toList()
+                }
+
 
             return Expression(value)
-        }
-
-        private fun removeCustomRegex(text: String): String {
-            if (customRegex.matches(text)) {
-                return customRegex.find(text)
-                    ?.run { groupValues[2] }
-                    .toString()
-            }
-            return text
         }
     }
 }
