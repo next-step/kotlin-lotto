48827946924cf256b56b56207899ae01070cf642
choi-ys <project.log.062@gmail.com>
2022-12-13 06:19:08 +0900

    chore: 미사용 클래스 제거

--- src/main/kotlin/lotto/domain/PlayResult.kt
+++
@@ -1,7 +0,0 @@
-package lotto.domain
-
-class PlayResult private constructor(val lotto: Lotto, matchResult: MatchResult) {
-    companion object {
-        fun of(lotto: Lotto, matchResult: MatchResult): PlayResult = PlayResult(lotto, matchResult)
-    }
-}
