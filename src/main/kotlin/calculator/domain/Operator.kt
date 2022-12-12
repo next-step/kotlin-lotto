66ef1e0a7d1a208d7cf216a4b806e9f18f82d2ae
ChoiYounho <maxosa78@gmail.com>
2022-12-12 20:22:20 +0900

    refactor : 연산자 현재 사용하지 않는 코드 제거

--- src/main/kotlin/calculator/domain/Operator.kt
+++ src/main/kotlin/calculator/domain/Operator.kt
@@ -4,11 +4,5 @@ enum class Operator(
     val symbol: String,
     val calculator: (Int, Int) -> Int
 ) {
-    PLUS("+", { x, y -> x + y }),
-    MINUS("-", { x, y -> x - y }),
-    MULTIPLY("-", { x, y -> x * y }),
-    DIVIDE("-", { x, y ->
-        val isZero = x == 0 || y == 0
-        if (isZero) 0 else x / y
-    });
+    PLUS("+", { x, y -> x + y })
 }
