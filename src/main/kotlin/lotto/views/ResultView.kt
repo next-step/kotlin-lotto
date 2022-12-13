d64610e8abbdbc5e467af8424f9663ce3a1bb606
jpoh281 <hjp4869@naver.com>
2022-12-13 22:27:57 +0900

    refactor: ResultView 잘못 출력 시킨 변수 수정

--- src/main/kotlin/lotto/views/ResultView.kt
+++ src/main/kotlin/lotto/views/ResultView.kt
@@ -15,8 +15,8 @@ object ResultView {
     fun printResult(report: Report) {
         println("당첨 통계")
         println("---------")
-        println("3개 일치 (${Rank.FOURTH.krw.money}원)- ${report.fourthCount}개")
-        println("4개 일치 (${Rank.THIRD.krw.money}원)- ${report.fourthCount}개")
+        println("3개 일치 (${Rank.FIFTH.krw.money}원)- ${report.fifthCount}개")
+        println("4개 일치 (${Rank.FOURTH.krw.money}원)- ${report.fourthCount}개")
         println("5개 일치 (${Rank.THIRD.krw.money}원)- ${report.thirdCount}개")
         println("5개 일치, 보너스 볼 일치(${Rank.SECOND.krw.money}원)- ${report.secondCount}개")
         println("6개 일치 (${Rank.FIRST.krw.money}원)- ${report.firstCount}개")
