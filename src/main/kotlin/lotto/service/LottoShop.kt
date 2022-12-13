ade64e4fa230f03015d041bc86c075c59df8d216
wrallee <wrallee@naver.com>
2022-12-14 01:37:51 +0900

    refactor(Lotto): 로또 구매 로직 BigDecimal로 이동

--- src/main/kotlin/lotto/service/LottoShop.kt
+++ src/main/kotlin/lotto/service/LottoShop.kt
@@ -9,15 +9,16 @@ object LottoShop {
     private const val LOTTO_MONEY_SCALE = 0
 
     fun buyAutoLottos(money: BigDecimal): Lottos {
-        require(money >= LOTTO_PRICE) {
-            "money 값은 로또 가격인 ${LOTTO_PRICE}보다 커야합니다. money: ${money}"
-        }
-
-        val buyCount = getBuyCountOf(money)
+        val buyCount = money.pay(LOTTO_PRICE)
         val lottoList = List(buyCount) { LottoGenerator.getRandomLotto() }
         return Lottos(lottoList)
     }
 
-    private fun getBuyCountOf(money: BigDecimal) =
-        money.divide(LOTTO_PRICE, LOTTO_MONEY_SCALE, FLOOR).toInt()
+    private fun BigDecimal.pay(lottoPrice: BigDecimal): Int {
+        require(this >= lottoPrice) {
+            "금액은 로또 가격인 ${lottoPrice}보다 커야합니다. money: $this"
+        }
+
+        return this.divide(LOTTO_PRICE, LOTTO_MONEY_SCALE, FLOOR).toInt()
+    }
 }
