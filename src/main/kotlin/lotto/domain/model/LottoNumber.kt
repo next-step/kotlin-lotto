02e497dbbd0df2aa144344d9732b74cf3f202140
dreamboy1339 <dreamboy1339@gmail.com>
2022-12-15 22:47:34 +0900

    study(step4): LOTTO_NUMBER_RANGE 를 활용한 예외 메시지 개선

--- src/main/kotlin/lotto/domain/model/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/model/LottoNumber.kt
@@ -3,7 +3,7 @@ package lotto.domain.model
 data class LottoNumber(val number: Int) {
 
     init {
-        check(number in LOTTO_NUMBER_RANGE) { "로또 숫자는 1부터 45 사이의 숫자로 구성되어야 합니다" }
+        check(number in LOTTO_NUMBER_RANGE) { "로또 숫자는 ${LOTTO_NUMBER_RANGE.first}부터 ${LOTTO_NUMBER_RANGE.last} 사이의 숫자로 구성되어야 합니다" }
     }
 
     companion object {
--- src/test/kotlin/lotto/domain/model/LottoNumberTest.kt
+++ src/test/kotlin/lotto/domain/model/LottoNumberTest.kt
@@ -1,12 +1,13 @@
 package lotto.domain.model
 
+import lotto.domain.model.LottoNumber.Companion.LOTTO_NUMBER_RANGE
 import org.assertj.core.api.AssertionsForClassTypes
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 internal class LottoNumberTest {
 
-    @DisplayName("로또는 1부터 45 사이의 숫자로 구성되어야 합니다")
+    @DisplayName("로또는 특정 범위 내 숫자로 구성되어야 합니다")
     @Test
     fun lottoNumberRange() {
         AssertionsForClassTypes
@@ -14,6 +15,6 @@ internal class LottoNumberTest {
             .isThrownBy {
                 LottoNumber(49)
             }
-            .withMessageMatching("로또 숫자는 1부터 45 사이의 숫자로 구성되어야 합니다")
+            .withMessageMatching("로또 숫자는 ${LOTTO_NUMBER_RANGE.first}부터 ${LOTTO_NUMBER_RANGE.last} 사이의 숫자로 구성되어야 합니다")
     }
 }
