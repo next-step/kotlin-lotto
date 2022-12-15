02116de3ebb00b52f71fb9b2226df58689e6c986
dreamboy1339 <dreamboy1339@gmail.com>
2022-12-15 22:34:37 +0900

    study(step4): 로또 숫자 중복 여부 확인 코드 개선

--- src/main/kotlin/lotto/domain/model/Lotto.kt
+++ src/main/kotlin/lotto/domain/model/Lotto.kt
@@ -21,19 +21,7 @@ class Lotto(numbers: List<LottoNumber> = emptyList()) {
 
         check(numbers.size == LAST_LOTTO_INDEX) { "로또는 숫자 6개로 구성되어야 합니다" }
 
-        repeat(numbers.size) {
-            check(
-                numbers
-                    .groupingBy {
-                        it
-                    }
-                    .eachCount()
-                    .filter {
-                        it.value > 1
-                    }
-                    .isEmpty()
-            ) { "로또는 중복되지 않는 숫자로 구성되어야 합니다" }
-        }
+        check(numbers.toSet().size == LAST_LOTTO_INDEX) { "로또는 중복되지 않는 숫자로 구성되어야 합니다" }
 
         return numbers
     }
