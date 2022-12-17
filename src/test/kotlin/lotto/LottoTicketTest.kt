92339a8136eb6c045e5b77cfa7bce869ab74590b
relkim-m <relkimm@mfort.co.kr>
2022-12-17 14:48:49 +0900

    refactor: 로또 티켓 테스트 코드 정리

--- src/test/kotlin/lotto/LottoTicketTest.kt
+++ src/test/kotlin/lotto/LottoTicketTest.kt
@@ -9,43 +9,51 @@ import java.lang.IllegalArgumentException
 
 class LottoTicketTest : FunSpec({
     context("로또 티켓 구매") {
-        test("티켓은 하나의 로또 번호를 가진다.") {
-            val actual = LottoTicket.purchase(payment = 1000)
-            actual.lottoNumber shouldNotBe null
+        context("지불 금액") {
+            test("1000원 초과하면 티켓을 구매할 수 없다.") {
+                assertThrows<IllegalArgumentException> { LottoTicket.purchase(payment = 1500) }
+            }
+
+            test("1000원 미만이면 티켓을 구매할 수 없다.") {
+                assertThrows<IllegalArgumentException> { LottoTicket.purchase(payment = 500) }
+            }
+
+            test("1000원이면 티켓을 구매할 수 있다.") {
+                val actual = LottoTicket.purchase(payment = 1000)
+                actual shouldNotBe null
+                actual.lottoNumber shouldNotBe null
+                actual.lottoStatus shouldBe LottoStatus.WAITING
+                actual.matchCount shouldBe 0
+            }
         }
 
-        test("지불 금액이 1000원 초과하면 티켓을 구매할 수 없다.") {
-            assertThrows<IllegalArgumentException> { LottoTicket.purchase(payment = 1500) }
-        }
+        context("수동 로또 번호") {
+            context("로또 번호를 함께 전달하면") {
+                test("수동 로또 번호로도 구매할 수 있다.") {
+                    val lottoNumber = LottoNumber.manualGenerate(numbers = listOf(5, 10, 15, 20, 25, 30))
 
-        test("지불 금액이 1000원 미만이면 티켓을 구매할 수 없다.") {
-            assertThrows<IllegalArgumentException> { LottoTicket.purchase(payment = 500) }
-        }
+                    val actual = LottoTicket.purchase(payment = 1000, manualNumber = lottoNumber)
 
-        test("지불 금액이 1000원이면 티켓을 구매할 수 있다.") {
-            val actual = LottoTicket.purchase(payment = 1000)
-            actual shouldNotBe null
-            actual.lottoStatus shouldBe LottoStatus.WAITING
-            actual.matchCount shouldBe 0
-        }
-
-        test("수동 로또 번호로 구매할 수 있다.") {
-            val lottoNumber = LottoNumber.manualGenerate(numbers = listOf(5, 10, 15, 20, 25, 30))
-            val actual = LottoTicket.purchase(payment = 1000, manualNumber = lottoNumber)
-            actual.lottoNumber.numbers shouldContainExactly lottoNumber.numbers
-            actual.lottoStatus shouldBe LottoStatus.WAITING
-            actual.matchCount shouldBe 0
+                    actual.lottoNumber.numbers shouldContainExactly lottoNumber.numbers
+                    actual.lottoStatus shouldBe LottoStatus.WAITING
+                    actual.matchCount shouldBe 0
+                }
+            }
         }
     }
 
     context("로또 티켓 당첨 비교") {
-        val winningNumber = WinningNumber.from(numbers = listOf(5, 10, 15, 22, 33, 44))
-        val lottoNumber = LottoNumber.manualGenerate(numbers = listOf(5, 10, 15, 20, 25, 30))
-        val actual = LottoTicket.purchase(payment = 1000, lottoNumber)
+        context("당첨 번호를 전달하면") {
+            test("로또 티켓의 당첨 여부와 맞춘 갯수가 정해진다.") {
+                val winningNumber = WinningNumber.from(numbers = listOf(5, 10, 15, 22, 33, 44))
+                val lottoNumber = LottoNumber.manualGenerate(numbers = listOf(5, 10, 15, 20, 25, 30))
+                val actual = LottoTicket.purchase(payment = 1000, lottoNumber)
 
-        actual.match(winningNumber = winningNumber)
+                actual.match(winningNumber = winningNumber)
 
-        actual.lottoStatus shouldBe LottoStatus.WIN
-        actual.matchCount shouldBe 3
+                actual.lottoStatus shouldBe LottoStatus.WIN
+                actual.matchCount shouldBe 3
+            }
+        }
     }
 })
