0eb403e9618a7bd90e23fd51359ae379eb8b4c9b
relkim-m <relkimm@mfort.co.kr>
2022-12-17 23:43:42 +0900

    refactor: 입력한 구매 금액 조회하는 로직 리팩터링

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -8,9 +8,9 @@ object LottoGame {
     fun run() {
         val paymentInput = PaymentInput()
         paymentInput.draw()
-        val payment = paymentInput.value
-        PurchaseCount(payment = payment).draw()
+        val payment = paymentInput.getPayment()
         val lottoTicketBundle = LottoTicketBundle.purchase(payment = payment)
+        PurchaseCount(payment = payment).draw()
         LottoTicketList(tickets = lottoTicketBundle.getTickets()).draw()
     }
 }
--- src/main/kotlin/lotto/ui/PaymentInput.kt
+++ src/main/kotlin/lotto/ui/PaymentInput.kt
@@ -1,12 +1,15 @@
 package lotto.ui
 
 class PaymentInput : UI {
-    var value: Int = 0
+    var value: String = ""
         private set
 
     override fun draw() {
         println("구입금액을 입력해 주세요.")
-        val commandValue = readLine()!!
-        this.value = commandValue.toInt()
+        this.value = readLine()!!
+    }
+
+    fun getPayment(): Int {
+        return this.value.toIntOrNull() ?: throw IllegalStateException()
     }
 }
