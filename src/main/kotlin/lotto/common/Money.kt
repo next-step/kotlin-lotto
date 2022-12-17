a3a2dbcd93a409a8d49af24baf785e0eda9aff60
EP <kjuioqq8@gmail.com>
2022-12-17 16:38:34 +0900

    refactor: Money 생성 조건 추가

--- src/main/kotlin/lotto/common/Money.kt
+++ src/main/kotlin/lotto/common/Money.kt
@@ -7,6 +7,10 @@ value class Money(
     private val value: BigDecimal
 ) {
 
+    init {
+        require(value >= BigDecimal.ZERO) { "돈의 값은 0이랑 같거나 커야 합니다." }
+    }
+
     companion object {
         fun from(value: Long): Money = Money(BigDecimal.valueOf(value))
     }
