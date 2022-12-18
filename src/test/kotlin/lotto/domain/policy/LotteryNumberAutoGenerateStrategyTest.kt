4fc1b0eccb757ae300a5816e5dfe18a800b6cea8
EP <kjuioqq8@gmail.com>
2022-12-18 18:34:59 +0900

    refactor: 변수명 변경

--- src/test/kotlin/lotto/domain/policy/LotteryNumberAutoGenerateStrategyTest.kt
+++ src/test/kotlin/lotto/domain/policy/LotteryNumberAutoGenerateStrategyTest.kt
@@ -8,22 +8,15 @@ import lotto.domain.vo.LotteryNumbers
 
 class LotteryNumberAutoGenerateStrategyTest : FreeSpec({
 
-    val wholeLotteryNumbers = generateWhileLotteryNumbers()
+    val allLotteryNumbers = LotteryNumber.LOTTERY_NUMBER_LIST
 
     "자동으로 추첨 번호를 생성할 수 있다" - {
         val randomLotteryNumbers = LotteryNumberAutoGenerateStrategy.generate()
         randomLotteryNumbers.shouldNotBeNull()
 
         "중복되지 않는 ${LotteryNumbers.LOTTERY_NUMBER_SIZE}개의 추첨 번호를 가지고 있다" {
-            val sameNumber = wholeLotteryNumbers.filter { randomLotteryNumbers.contains(it) }.size
+            val sameNumber = allLotteryNumbers.filter { randomLotteryNumbers.contains(it) }.size
             sameNumber shouldBe LotteryNumbers.LOTTERY_NUMBER_SIZE
         }
     }
 })
-
-private fun generateWhileLotteryNumbers(): Set<LotteryNumber> {
-    val lotteryNumbers = mutableSetOf<LotteryNumber>()
-    var initNumber = LotteryNumber.MIN_NUMBER
-    repeat(LotteryNumber.MAX_NUMBER) { lotteryNumbers.add(LotteryNumber(initNumber++)) }
-    return lotteryNumbers.toSet()
-}
