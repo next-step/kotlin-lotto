0a37bddba163219cac24b96997c8416df4c90a07
miaww.yun <miaww.yun@woowahan.com>
2022-12-18 21:13:34 +0900

    refactor : 당첨번호 입력시 null처리 변경

--- src/main/kotlin/lotto/view/LotteryMachineInputView.kt
+++ src/main/kotlin/lotto/view/LotteryMachineInputView.kt
@@ -10,6 +10,8 @@ object LotteryMachineInputView {
     fun inputLastWinningNumbers(): List<Int> {
         println("지난 주 당첨 번호를 입력해 주세요.")
 
-        return readLine()?.split(",")?.map { it.toIntOrNull() ?: 0 } ?: throw IllegalArgumentException()
+        return readLine().orEmpty()
+            .split(",")
+            .map { it.toIntOrNull() ?: throw IllegalArgumentException() }
     }
 }
