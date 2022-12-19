06e2425ce54cdbe825f1b82c93493734b29619ba
ChoiYounho <maxosa78@gmail.com>
2022-12-19 17:48:17 +0900

    refactor : 구분자 테스트 코드 리팩토링

--- src/test/kotlin/calculator/domain/DelimitersTest.kt
+++ src/test/kotlin/calculator/domain/DelimitersTest.kt
@@ -1,32 +1,48 @@
 package calculator.domain
 
-import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.AssertionsForClassTypes.assertThat
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 internal class DelimitersTest {
 
     @Test
-    fun `문자열 구분자를 이용히여 연산자로 변환한다`() {
+    fun `콤마와 콜론을 가지고 있는 구분자를 생성한다`() {
         // given
-        val input = "//;\n1;2;3"
+        val input = ""
 
         // when
-        val actual = Delimiters.create(input).toOperator(";")
+        val actual = Delimiters.create(input).value.size
 
         // then
-        val expected = Operator.PLUS
+        val expected = 2
         assertThat(actual).isEqualTo(expected)
     }
 
     @Test
-    fun `문자열 구분자를 이용하여 연산자 변환에 실패한다`() {
-        assertThrows<IllegalArgumentException> {
-            // given
-            val input = "//;\n1;2;3"
+    fun `콤마와 콜론 외에 다른 문자도 포함된 구분자를 생성한다`() {
+        // given
+        val input = "//;\n1;2;-3"
 
-            // when
-            val actual = Delimiters.create(input).toOperator("ㅇ")
-        }
+        // when
+        val actual = Delimiters.create(input).value.size
+
+        // then
+        val expected = 3
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    @DisplayName("//로 시작하여 \n로 끝나는 부분을 제거한다.")
+    fun `입력값에 커스텀 구분자 추가하는 부분을 제거한다`() {
+        // given
+        val input = "//;\n1;2;-3"
+
+        // when
+        val actual = Delimiters().removeCustomRegex(input)
+
+        // then
+        val expected = "1;2;-3"
+        assertThat(actual).isEqualTo(expected)
     }
 }
