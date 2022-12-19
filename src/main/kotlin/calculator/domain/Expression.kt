fe95b3b322defa57affa30187687d6fb950478c2
ChoiYounho <maxosa78@gmail.com>
2022-12-19 17:47:47 +0900

    refactor : 수식 클래스 리팩토링

--- src/main/kotlin/calculator/domain/Expression.kt
+++ src/main/kotlin/calculator/domain/Expression.kt
@@ -1,5 +1,7 @@
 package calculator.domain
 
+import calculator.extensions.isNumeric
+import calculator.extensions.split
 import calculator.util.validate
 
 class Expression private constructor(
@@ -17,29 +19,34 @@ class Expression private constructor(
     companion object {
 
         private const val MESSAGE_NOT_POSITIVE_NUMBER = "양수가 아닌 숫자를 전달 받을 수 없습니다."
+        private const val MESSAGE_NOT_OPERAND = "피연산자만 들어올 수 있습니다."
 
         fun create(input: String, delimiters: Delimiters): Expression {
             if (input.isEmpty()) {
                 return Expression()
             }
 
-            val value = delimiters.removeCustomRegex(input)
-                .map {
-                    if (it.isDigit()) {
-                        val digit = it.digitToInt()
+            val text = delimiters.removeCustomRegex(input)
+            val splitDelimiters = text.split(delimiters.value)
+            val lastIndex = splitDelimiters.size - 1
 
-                        val isNotNegativeNumber = digit > -1
-                        validate(isNotNegativeNumber) { MESSAGE_NOT_POSITIVE_NUMBER }
+            val expressionElements = mutableListOf<ExpressionElement>()
 
-                        ExpressionElement.OperandElement(digit)
-                    } else {
-                        ExpressionElement.OperatorElement(delimiters.toOperator(it.toString()))
+            splitDelimiters.mapIndexed { index, it ->
+                if (it.isNumeric().not()) throw IllegalArgumentException(MESSAGE_NOT_OPERAND)
 
-                    }
+                val digit = it.toInt()
+
+                val isNotNegativeNumber = digit > -1
+                validate(isNotNegativeNumber) { MESSAGE_NOT_POSITIVE_NUMBER }
+
+                expressionElements.add(ExpressionElement.OperandElement(digit))
+                if (index != lastIndex) {
+                    expressionElements.add(ExpressionElement.OperatorElement(Operator.PLUS))
                 }
+            }
 
-
-            return Expression(value)
+            return Expression(expressionElements.toList())
         }
     }
 }
