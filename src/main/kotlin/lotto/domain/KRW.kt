28fa10df112d764fb4e8f78ab083dac21636dd61
jpoh281 <hjp4869@naver.com>
2022-12-21 21:47:00 +0900

    refactor: KRW 에러메세지 추가

--- src/main/kotlin/lotto/domain/KRW.kt
+++ src/main/kotlin/lotto/domain/KRW.kt
@@ -1,7 +1,6 @@
 package lotto.domain
 
 private const val MINIMUM_KRW_VALUE = 0
-
 private const val DIVISION_UNIT = 1000
 
 data class KRW(val money: Int) {
@@ -12,8 +11,8 @@ data class KRW(val money: Int) {
         get() = money / DIVISION_UNIT
 
     init {
-        require(money >= MINIMUM_KRW_VALUE)
-        require(money % DIVISION_UNIT == 0)
+        require(money >= MINIMUM_KRW_VALUE) { "0원 이상 입력해주세요." }
+        require(money % DIVISION_UNIT == 0) { "1000원으로 나누어지도록 입력해주세요." }
     }
 
     fun add(krw: KRW): KRW {
