44b3a4a3bda3877d3971c0af11898f1ba572e901
jpoh281 <hjp4869@naver.com>
2022-12-21 22:06:43 +0900

    refactor: ResultView에 중간에 에러발생 시 알려주는 메소드 추가

--- src/main/kotlin/lotto/views/ResultView.kt
+++ src/main/kotlin/lotto/views/ResultView.kt
@@ -6,8 +6,12 @@ import lotto.domain.Report
 
 object ResultView {
 
-    fun printLottos(lottos: List<Lotto>) {
-        lottos.forEach {
+    fun printLottos(manualLottos: List<Lotto> = emptyList(), autoLottos: List<Lotto>) {
+        print("수동으로 ${manualLottos.size}장, 자동으로 ${autoLottos.size}장을 구매했습니다.")
+        manualLottos.forEach {
+            println(it)
+        }
+        autoLottos.forEach {
             println(it)
         }
     }
@@ -21,5 +25,9 @@ object ResultView {
         println("5개 일치, 보너스 볼 일치(${Rank.SECOND.krw.money}원)- ${report.secondCount}개")
         println("6개 일치 (${Rank.FIRST.krw.money}원)- ${report.firstCount}개")
         println("총 수익률은 ${report.getRateOfReturn()}입니다.")
+    }
+
+    fun printErrorMessage(exception: Exception) {
+        println(exception.message)
     }
 }
