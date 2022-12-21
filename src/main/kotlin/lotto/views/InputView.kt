014d853cdf5fe04671a7403950d0742597dc9a21
jpoh281 <hjp4869@naver.com>
2022-12-21 22:06:20 +0900

    refactor: InputView에 수동 로또를 얻는 메소드 추가

--- src/main/kotlin/lotto/views/InputView.kt
+++ src/main/kotlin/lotto/views/InputView.kt
@@ -4,8 +4,29 @@ object InputView {
     fun insertMoney(): String {
         println("구입금액을 입력해 주세요.")
         val input = readLine()
-        require(!input.isNullOrBlank())
+        require(!input.isNullOrBlank()) { "구입 금액을 입력하셔야합니다." }
         return input
+    }
+
+    fun getManualLottoCount(): String {
+        println("수동으로 구매할 로또 수를 입력해 주세요.")
+        val input = readLine()
+        require(!input.isNullOrBlank()) { "구입 금액을 입력하셔야합니다." }
+        return input
+    }
+
+    fun getManualLottos(count: Int): List<String> {
+        println("수동으로 구매할 번호를 입력해 주세요.")
+
+        val lottos = mutableListOf<String>()
+
+        while (lottos.size < count) {
+            val input = readLine()
+            require(!input.isNullOrBlank()) { "구입 금액을 입력하셔야합니다." }
+            lottos.add(input)
+        }
+
+        return lottos
     }
 
     fun getWinningLottoNumbers(): String {
