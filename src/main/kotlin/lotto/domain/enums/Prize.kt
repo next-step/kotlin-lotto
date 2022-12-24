5a2f906b2e25ca2ba6d7ebe86b6387339b3ecd3b
EP <kjuioqq8@gmail.com>
2022-12-24 09:20:10 +0900

    feat: 보너스 번호 기능 추가 & 상금 및 옵션 변경

--- src/main/kotlin/lotto/domain/enums/Prize.kt
+++ src/main/kotlin/lotto/domain/enums/Prize.kt
@@ -5,19 +5,21 @@ import lotto.common.value.Money.Companion.toMoney
 
 enum class Prize(
     val equalNumberCount: Long = 0,
-    val amount: Money = 0L.toMoney()
+    val amount: Money = 0L.toMoney(),
+    val bonus: Boolean = false
 ) {
     BOOM,
-    FOURTH(3, 5_000L.toMoney()),
-    THIRD(4, 50_000L.toMoney()),
-    SECOND(5, 1_500_000L.toMoney()),
+    FIFTH(3, 5_000L.toMoney()),
+    FOURTH(4, 50_000L.toMoney()),
+    THIRD(5, 1_500_000L.toMoney()),
+    SECOND(5, 30_000_000L.toMoney(), bonus = true),
     FIRST(6, 2_000_000_000L.toMoney());
 
     fun isNotBoom(): Boolean = this != BOOM
 
     companion object {
-        fun find(equalNumberCount: Long): Prize {
-            return values().find { it.equalNumberCount == equalNumberCount } ?: BOOM
+        fun find(equalNumberCount: Long, hasBonusNumber: Boolean): Prize {
+            return values().find { it.equalNumberCount == equalNumberCount && it.bonus == hasBonusNumber } ?: BOOM
         }
     }
 }
