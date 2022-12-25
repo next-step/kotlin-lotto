5991726609369b8a28d731afc9881544286fb75a
0923kdh <0923kdh@gmail.com>
2022-12-25 20:48:28 +0900

    chore: 보너스 볼을 입력 기능 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -6,8 +6,6 @@ object InputView {
         val amount = readln()
         require(amount.isNotEmpty()) { "금액을 입력해주세요." }
 
-        println(amount)
-
         return amount.toInt()
     }
 
@@ -15,7 +13,7 @@ object InputView {
         println("$count 개를 구매했습니다.")
     }
 
-    fun getWinningNumber(): Set<Int> {
+    fun getWinningNumber(): Pair<Set<Int>, Int> {
         println("지난 주 당첨 번호를 입력해 주세요.")
         val winningNumber = readln()
         require(winningNumber.isNotEmpty()) { "당첨 번호를 입력해주세요." }
@@ -23,6 +21,10 @@ object InputView {
         val numbers = winningNumber.split(", ").map { it.toInt() }.toSet()
         require(numbers.size == 6) { " 중복없는 6개의 숫자를 입력해주세요." }
 
-        return numbers
+        println("보너스 볼을 입력해 주세요.")
+        val bonusBall = readln().toInt()
+        require(!numbers.contains(bonusBall)) { "보너스 볼은 당첨 번호와 중복될 수 없습니다." }
+
+        return numbers to bonusBall
     }
 }
