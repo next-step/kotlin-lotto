1a112dfbacd53ea67b0d079e841d63fdb0649861
hojihun5516 <hojihun5516@daum.net>
2022-12-25 21:10:08 +0900

    feat: BuyPrice class

---
+++ src/main/kotlin/lotto/BuyPrice.kt
@@ -0,0 +1,11 @@
+package lotto
+
+import lotto.LottoGame.Companion.ONE_LOTTO_PRICE
+
+class BuyPrice(val price: Int) {
+    init {
+        require(price >= ONE_LOTTO_PRICE) {
+            "투입하신 금액은 $price 입니다. \n 구매하기 위한 최소 금액은 $ONE_LOTTO_PRICE 입니다."
+        }
+    }
+}
