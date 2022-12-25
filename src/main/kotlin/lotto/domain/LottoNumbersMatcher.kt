16b1ebbfc47df18c26e1bb2cc6934d80c8744d4b
wisotge <rnalstn0507@gmail.com>
2022-12-25 10:45:29 +0900

    fix: 불필요한 클래스 제거

--- src/main/kotlin/lotto/domain/LottoNumbersMatcher.kt
+++
@@ -1,7 +0,0 @@
-package lotto.domain
-
-object LottoNumbersMatcher {
-    fun calculateMatchCount(purchaseLotto: Lotto, winningLotto: Lotto): Int {
-        return purchaseLotto.lottoNumbers.intersect(winningLotto.lottoNumbers.toSet()).size
-    }
-}
