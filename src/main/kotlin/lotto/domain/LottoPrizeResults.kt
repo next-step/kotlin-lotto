17f4ec8323dd877b99fa0e1b9591125cfe2306f1
jay.lee <stkment@gmail.com>
2022-12-26 21:03:32 +0900

    :white_check_mark: 로또 담청 결과 생성, 담청된 등수 개수 확인 테스트 코드 추가

---
+++ src/main/kotlin/lotto/domain/LottoPrizeResults.kt
@@ -0,0 +1,11 @@
+package lotto.domain
+
+import lotto.model.LottoPrize
+
+class LottoPrizeResults(
+    private val prizes: List<LottoPrize>,
+) {
+    fun count(prize: LottoPrize): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/test/kotlin/lotto/domain/LottoPrizeResultsTest.kt
@@ -0,0 +1,21 @@
+package lotto.domain
+
+import lotto.model.LottoPrize
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class LottoPrizeResultsTest {
+    @Test
+    fun `당첨 등수 개수를 알 수 있다`() {
+        // given
+        val prize = LottoPrize.FIRST
+        val results = listOf(prize)
+        val lottoPrizeResults = LottoPrizeResults(results)
+
+        // when
+        val count = lottoPrizeResults.count(prize)
+
+        // then
+        assertThat(count).isEqualTo(1)
+    }
+}
