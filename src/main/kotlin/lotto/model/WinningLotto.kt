dfa55db2a0bd24304fa378fb2e82bebcd7ca9d98
jay.lee <stkment@gmail.com>
2022-12-26 20:38:14 +0900

    :necktie: 담청 로또 초기화 코드 추가

--- src/main/kotlin/lotto/model/WinningLotto.kt
+++ src/main/kotlin/lotto/model/WinningLotto.kt
@@ -1,5 +1,12 @@
 package lotto.model
 
+import lotto.model.Lotto.Companion.NUMBER_OF_LOTTO
+
 data class WinningLotto(
     val numbers: List<LottoNumber>,
-)
+) {
+    init {
+        require(numbers.size == NUMBER_OF_LOTTO) { "당첨 번호는 ${NUMBER_OF_LOTTO}개의 숫자여야 합니다." }
+        require(numbers.distinct().size == NUMBER_OF_LOTTO) { "당첨 번호는 중복되는 숫자가 없어야 합니다." }
+    }
+}
