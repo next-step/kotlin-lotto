abc988caa2f7925d825492582706648c75e7b3e7
jay.lee <stkment@gmail.com>
2022-12-26 22:12:21 +0900

    :necktie: 투자 수익률 분석 로직 구현

--- src/main/kotlin/lotto/domain/LottoROIAnalysis.kt
+++ src/main/kotlin/lotto/domain/LottoROIAnalysis.kt
@@ -2,7 +2,6 @@ package lotto.domain
 
 class LottoROIAnalysis {
 
-    fun returnOnInvestment(winningPrize: Int, purchaseAmount: Int): Double {
-        TODO("Not yet implemented")
-    }
+    fun returnOnInvestment(winningPrize: Int, purchaseAmount: Int): Double =
+        winningPrize.toDouble() / purchaseAmount
 }
