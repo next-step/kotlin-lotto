1253def860a945c092bf8b28cdc5533d736897fd
wrallee <wrallee@naver.com>
2022-12-27 01:42:18 +0900

    refactor(Lotto): LottoPrize 변수에 보너스볼 유무 추가

--- src/main/kotlin/lotto/model/LottoPrize.kt
+++ src/main/kotlin/lotto/model/LottoPrize.kt
@@ -6,23 +6,20 @@ import java.math.BigDecimal.valueOf
 
 enum class LottoPrize(
     val matchCount: Int,
-    val winningAmount: BigDecimal
+    val winningAmount: BigDecimal,
+    val matchBonus: Boolean? = null
 ) {
     FIRST(6, valueOf(2_000_000_000)),
-    SECOND(5, valueOf(30_000_000)),
-    THIRD(5, valueOf(1_500_000)),
+    SECOND(5, valueOf(30_000_000), matchBonus = true),
+    THIRD(5, valueOf(1_500_000), matchBonus = false),
     FOURTH(4, valueOf(50_000)),
     FIFTH(3, valueOf(5_000)),
     NONE(0, ZERO);
 
     companion object {
         fun of(matchCount: Int, matchBonus: Boolean): LottoPrize {
-            if (matchCount == SECOND.matchCount && matchBonus) {
-                return SECOND
-            }
-
-            return values().filterNot { it == SECOND }
-                .find { it.matchCount == matchCount } ?: NONE
+            return values().filter { it.matchCount == matchCount }
+                .find { it.matchBonus?.equals(matchBonus) ?: true } ?: NONE
         }
     }
 }
--- src/test/kotlin/lotto/model/LottoPrizeTest.kt
+++ src/test/kotlin/lotto/model/LottoPrizeTest.kt
@@ -1,5 +1,6 @@
 package lotto.model
 
+import lotto.model.LottoPrize.FOURTH
 import lotto.model.LottoPrize.SECOND
 import lotto.model.LottoPrize.THIRD
 import org.assertj.core.api.Assertions.assertThat
@@ -10,7 +11,11 @@ class LottoPrizeTest {
     internal fun `당첨결과 생성`() {
         val third = LottoPrize.of(5, false)
         val second = LottoPrize.of(5, true)
+        val fourth1 = LottoPrize.of(4, true)
+        val fourth2 = LottoPrize.of(4, false)
         assertThat(third).isSameAs(THIRD)
         assertThat(second).isSameAs(SECOND)
+        assertThat(fourth1).isSameAs(FOURTH)
+        assertThat(fourth2).isSameAs(FOURTH)
     }
 }
