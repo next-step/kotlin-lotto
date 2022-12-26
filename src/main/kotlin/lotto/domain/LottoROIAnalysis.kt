df1032c39357d13bfc665b32bee80644ab7d23a5
jay.lee <stkment@gmail.com>
2022-12-26 22:54:25 +0900

    :bug: 문구 버그 수정

--- src/main/kotlin/lotto/domain/LottoROIAnalysis.kt
+++ src/main/kotlin/lotto/domain/LottoROIAnalysis.kt
@@ -1,7 +1,9 @@
 package lotto.domain
 
+import kotlin.math.roundToInt
+
 class LottoROIAnalysis {
 
     fun returnOnInvestment(winningPrize: Int, purchaseAmount: Int): Double =
-        winningPrize.toDouble() / purchaseAmount
+        ((winningPrize.toDouble() / purchaseAmount) * 100.0).roundToInt() / 100.0
 }
