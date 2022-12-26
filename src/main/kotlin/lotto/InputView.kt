2a6573c28aae8b077b686f453c635b6047d566a9
jay.lee <stkment@gmail.com>
2022-12-26 22:45:56 +0900

    :necktie: 입출력 로직 개선

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -8,6 +8,6 @@ object InputView {
 
     fun inputWinningNumbers(): List<Int> {
         println("지난 주 당첨 번호를 입력해 주세요.")
-        return readLine()!!.split(",").map { it.toInt() }
+        return readLine()!!.split(",").map { it.trim().toInt() }
     }
 }
--- src/main/kotlin/lotto/LottoApp.kt
+++ src/main/kotlin/lotto/LottoApp.kt
@@ -1,13 +1,24 @@
 package lotto
 
 import lotto.domain.LottoMachine
+import lotto.domain.LottoMatcher
+import lotto.domain.LottoROIAnalysis
 import lotto.model.LottoNumber
 import lotto.model.WinningLotto
 
 fun main() {
     val purchaseAmount = InputView.inputPurchaseAmount()
     val lotto = LottoMachine().draw(purchaseAmount)
+    ResultView.printLotto(lotto)
     val winningLotto = issueWinningLotto()
+    val lottoPrizeResults = LottoMatcher().match(winningLotto, lotto)
+    ResultView.printLottoPrizeResults(lottoPrizeResults)
+    ResultView.printLottoROIAnalysis(
+        LottoROIAnalysis().returnOnInvestment(
+            winningPrize = lottoPrizeResults.totalPrize(),
+            purchaseAmount = purchaseAmount
+        )
+    )
 }
 
 private fun issueWinningLotto(): WinningLotto =
