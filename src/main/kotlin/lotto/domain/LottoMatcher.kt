b5f8f395fdd11ea9ec0c79ca380fdefd2d1306a2
jay.lee <stkment@gmail.com>
2022-12-26 22:10:50 +0900

    :necktie: 로또 당첨 결과 확인 로직 구현

--- src/main/kotlin/lotto/domain/LottoMatcher.kt
+++ src/main/kotlin/lotto/domain/LottoMatcher.kt
@@ -1,11 +1,16 @@
 package lotto.domain
 
 import lotto.model.Lotto
+import lotto.model.LottoPrize
 import lotto.model.WinningLotto
 
 class LottoMatcher {
 
-    fun match(winningLotto: WinningLotto, userLotto: List<Lotto>): LottoPrizeResults {
-        return LottoPrizeResults(listOf())
+    fun match(winningLotto: WinningLotto, userLotto: List<Lotto>): LottoPrizeResults =
+        userLotto.mapNotNull { LottoPrize.of(matchCount(winningLotto, it)) }
+            .let(::LottoPrizeResults)
+
+    private fun matchCount(winningLotto: WinningLotto, userLotto: Lotto): Int {
+        return winningLotto.numbers.filter { userLotto.numbers.contains(it) }.size
     }
 }
