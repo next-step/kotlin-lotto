4781eb0601b5d4ee170d4f8f6c407a51ce9a1dd0
jay.lee <stkment@gmail.com>
2022-12-26 22:55:28 +0900

    :bug: 테스트 버그 수정

--- src/test/kotlin/lotto/model/WinningLottoTest.kt
+++ src/test/kotlin/lotto/model/WinningLottoTest.kt
@@ -27,7 +27,7 @@ internal class WinningLottoTest {
 
     @Test
     fun `담청 번호는 중복되는 숫자가 있으면 예외가 발생한다`() {
-        val numbers = listOf(1, 2, 1, 4, 5).map(::LottoNumber)
+        val numbers = listOf(1, 2, 1, 4, 5, 6).map(::LottoNumber)
 
         Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
             .isThrownBy { WinningLotto(numbers) }
