f09afb6a86c49a679c56e3440dfbfc7863e1a816
hojihun5516 <hojihun5516@daum.net>
2022-12-28 07:53:27 +0900

    feat: Application logic

---
+++ src/main/kotlin/lotto/Application.kt
@@ -0,0 +1,26 @@
+package lotto
+
+import lotto.LottoEarningRate.calculateEarningRate
+import lotto.LottoOffice.getRank
+import lotto.views.Input.getPriceForBuying
+import lotto.views.Input.getWinnerLottoNumbers
+import lotto.views.Output.printBuyAmount
+import lotto.views.Output.printEarningRate
+import lotto.views.Output.printLottoNumbers
+import lotto.views.Output.printLottoResult
+
+fun main() {
+    val inputPrice = getPriceForBuying()
+    val price = BuyPrice(inputPrice)
+    val boughtTicketAmount = LottoOffice.buyTickets(price)
+    val boughtLottos = LottoNumberGenerator.generate(boughtTicketAmount)
+
+    printBuyAmount(boughtTicketAmount)
+    printLottoNumbers(boughtLottos)
+
+    val winnerNumbers = getWinnerLottoNumbers()
+    val ranks = getRank(winnerNumbers = winnerNumbers, boughtLottos = boughtLottos)
+    val earningRate = calculateEarningRate(boughtPrice = price.price, ranks = ranks)
+    printLottoResult(ranks)
+    printEarningRate(earningRate)
+}
