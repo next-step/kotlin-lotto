04b2d94b731488a144f607d5d8ba7ac924ae17ac
hojihun5516 <hojihun5516@daum.net>
2022-12-28 07:51:59 +0900

    feat: Get winner lotto numbers

--- src/main/kotlin/lotto/views/Input.kt
+++ src/main/kotlin/lotto/views/Input.kt
@@ -1,9 +1,17 @@
 package lotto.views
 
+import lotto.LottoNumbers
+
 object Input {
     fun getPriceForBuying(): Int {
         println("구입금액을 입력해 주세요.")
         return readLine()?.toIntOrNull()
+            ?: throw IllegalArgumentException("It is not a number")
+    }
+
+    fun getWinnerLottoNumbers(): LottoNumbers {
+        println("지난 주 당첨 번호를 입력해 주세요.")
+        return readLine()?.split(",")?.map { it.toInt() }
             ?: throw IllegalArgumentException("It is not a number")
     }
 }
