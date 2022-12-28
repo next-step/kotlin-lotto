12aa0f9ef9ef96d57b11abeff585358bb0d691bc
wrallee <wrallee@naver.com>
2022-12-28 19:16:53 +0900

    refactor(Lotto): play() 로직 메서드 분리

--- src/main/kotlin/lotto/LottoApplication.kt
+++ src/main/kotlin/lotto/LottoApplication.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import lotto.model.LottoNumber
+import lotto.model.Lottos
 import lotto.model.WinningNumbers
 import lotto.model.WinningResult
 import lotto.service.LottoGenerator
@@ -11,19 +12,30 @@ import java.math.BigDecimal
 
 class LottoApplication {
     fun play() {
-        val moneyInput = InputView.readMoney()
-        val money = BigDecimal(moneyInput)
-        val lottos = LottoShop.buyAutoLottos(money)
-        OutputView.printDetails(lottos)
+        val lottos = buyLottos()
+        val winningNumbers = drawWinningNumbers()
+        checkLotteryResult(lottos, winningNumbers)
+    }
 
+    private fun checkLotteryResult(lottos: Lottos, winningNumbers: WinningNumbers) {
+        val winningResult = WinningResult.of(lottos, winningNumbers)
+        OutputView.printResult(winningResult)
+    }
+
+    private fun drawWinningNumbers(): WinningNumbers {
         val winningLottoInput = InputView.readWinningLotto()
         val bonusNumberInput = InputView.readBonusNumber()
         val winningLotto = LottoGenerator.fromString(winningLottoInput)
         val bonusNumber = LottoNumber.of(bonusNumberInput)
-        val winningNumbers = WinningNumbers(winningLotto, bonusNumber)
+        return WinningNumbers(winningLotto, bonusNumber)
+    }
 
-        val winningResult = WinningResult.of(lottos, winningNumbers)
-        OutputView.printResult(winningResult)
+    private fun buyLottos(): Lottos {
+        val moneyInput = InputView.readMoney()
+        val money = BigDecimal(moneyInput)
+        val lottos = LottoShop.buyAutoLottos(money)
+        OutputView.printDetails(lottos)
+        return lottos
     }
 }
 
