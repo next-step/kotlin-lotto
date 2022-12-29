c7157977a395d634ee18990e799734be4e3ad220
jay.lee <stkment@gmail.com>
2022-12-30 00:47:33 +0900

    :necktie: 2등 랭크 추가

--- src/main/kotlin/lotto/model/Rank.kt
+++ src/main/kotlin/lotto/model/Rank.kt
@@ -2,9 +2,10 @@ package lotto.model
 
 enum class Rank(val matchCount: Int, val prize: Int) {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
     ;
 
     companion object {
