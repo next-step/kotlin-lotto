7a0789c4949695598df21a12cc96fad85552f5f3
jay.lee <stkment@gmail.com>
2022-12-31 02:20:04 +0900

    :bug: 당첨 로또 초기화 문구 버그 수정

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -12,7 +12,7 @@ class WinningLotto(
     init {
         require(numbers.size == NUMBER_OF_LOTTO_DIGIT) { "당첨 번호는 ${NUMBER_OF_LOTTO_DIGIT}개의 숫자여야 합니다." }
         require(numbers.distinct().size == NUMBER_OF_LOTTO_DIGIT) { "당첨 번호는 중복되는 숫자가 없어야 합니다." }
-        require(!numbers.contains(bonusNumber)) { "보너스 번호는 당첨 번호와 중복될 수 없습니다." }
+        require(!numbers.contains(bonusNumber)) { "당첨 번호와 보너스 번호는 중복되면 안됩니다." }
     }
 
     fun rank(lottoNumbers: LottoNumbers): Rank? {
