59fc7954bf4c9f9b9750f745596b4255f9f755b1
Choiyounho <maxosa78@gmail.com>
2022-12-31 22:11:49 +0900

    refactor : Lottos 함수 구조 변경

--- src/main/kotlin/lotto/domain/Lottos.kt
+++ src/main/kotlin/lotto/domain/Lottos.kt
@@ -3,12 +3,12 @@ package lotto.domain
 class Lottos(val value: List<Lotto>) {
 
     fun matchWinningNumbers(winningNumbers: List<Int>): Map<Ranking, Int> {
-        return value.groupBy { lottoNumber ->
-            lottoNumber.value.count {
-                winningNumbers.contains(it)
-            }
-        }
-            .map { Pair(Ranking.valueOf(it.key), it.value.size) }
-            .toMap()
+        return value.groupingBy { lottoNumber ->
+            Ranking.valueOf(
+                lottoNumber.value.count {
+                    winningNumbers.contains(it)
+                }
+            )
+        }.eachCount()
     }
 }
