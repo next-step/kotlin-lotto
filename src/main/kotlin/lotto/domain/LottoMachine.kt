d71e141c638d3b34e9c2e9b34798ca99a500708e
jay.lee <stkment@gmail.com>
2023-01-01 17:29:01 +0900

    :coffin: 데스코드 삭제

--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -1,15 +1,8 @@
 package lotto.domain
 
-import lotto.domain.Lotto.Companion.PRICE
-
 class LottoMachine(
     private val lottoGenerator: LottoGenerator = LottoGenerator(),
 ) {
     fun draw(ticket: LottoTicket): Lotto =
         Lotto(ticket.manualLottoNumbers + List(ticket.autoLottoNumbersSize) { lottoGenerator.generate() })
-
-    private fun countPurchaseLotto(price: Int): Int {
-        require(price >= PRICE) { "로또를 구매하려면 최소 ${PRICE}원 이상의 금액이 필요합니다." }
-        return price / PRICE
-    }
 }
