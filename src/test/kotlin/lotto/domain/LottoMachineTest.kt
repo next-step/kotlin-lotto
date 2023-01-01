8934f2a088996974f3607cf9ed44333d33d9ce08
jay.lee <stkment@gmail.com>
2023-01-01 17:37:46 +0900

    :white_check_mark: LottoTicket 생성자 변경에 따른 테스트 코드 리팩토링

--- src/test/kotlin/lotto/domain/LottoMachineTest.kt
+++ src/test/kotlin/lotto/domain/LottoMachineTest.kt
@@ -1,7 +1,6 @@
 package lotto.domain
 
 import lotto.domain.Lotto.Companion.PRICE
-import lotto.model.LottoNumber
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -19,10 +18,10 @@ internal class LottoMachineTest {
     fun `입력된 로또 티켓(전체 수동생성)에 따라 로또를 생성한다`() {
         val purchaseAmount = 2000
         val manualLottoNumbers = listOf(
-            LottoNumbers(listOf(1, 2, 3, 4, 5, 6).map(::LottoNumber)),
-            LottoNumbers(listOf(1, 2, 3, 4, 5, 6).map(::LottoNumber))
+            listOf(1, 2, 3, 4, 5, 6),
+            listOf(1, 2, 3, 4, 5, 6)
         )
-        val lottoTicket = LottoTicket(purchaseAmount = purchaseAmount, _manualLottoNumbers = manualLottoNumbers)
+        val lottoTicket = LottoTicket(purchaseAmount = purchaseAmount, manualLottoNumbers = manualLottoNumbers)
         val lotto = LottoMachine().draw(lottoTicket)
         assertThat(lotto.size).isEqualTo(purchaseAmount / PRICE)
     }
@@ -31,10 +30,10 @@ internal class LottoMachineTest {
     fun `입력된 로또 티켓(수동생성 + 자동생성)에 따라 로또를 생성한다`() {
         val purchaseAmount = 4000
         val manualLottoNumbers = listOf(
-            LottoNumbers(listOf(1, 2, 3, 4, 5, 6).map(::LottoNumber)),
-            LottoNumbers(listOf(1, 2, 3, 4, 5, 6).map(::LottoNumber))
+            listOf(1, 2, 3, 4, 5, 6),
+            listOf(1, 2, 3, 4, 5, 6)
         )
-        val lottoTicket = LottoTicket(purchaseAmount = purchaseAmount, _manualLottoNumbers = manualLottoNumbers)
+        val lottoTicket = LottoTicket(purchaseAmount = purchaseAmount, manualLottoNumbers = manualLottoNumbers)
         val lotto = LottoMachine().draw(lottoTicket)
         assertThat(lotto.size).isEqualTo((purchaseAmount / PRICE))
     }
--- src/test/kotlin/lotto/domain/LottoTicketTest.kt
+++ src/test/kotlin/lotto/domain/LottoTicketTest.kt
@@ -1,6 +1,5 @@
 package lotto.domain
 
-import lotto.model.LottoNumber
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.Test
@@ -28,11 +27,11 @@ internal class LottoTicketTest {
     fun `수동으로 구매할 로또 수가 구매 금액을 초과하면 예외가 발생한다`() {
         val purchaseAmount = 1000
         val manualLottoNumbers = listOf(
-            LottoNumbers(listOf(1, 2, 3, 4, 5, 6).map(::LottoNumber)),
-            LottoNumbers(listOf(1, 2, 3, 4, 5, 6).map(::LottoNumber))
+            listOf(1, 2, 3, 4, 5, 6),
+            listOf(1, 2, 3, 4, 5, 6)
         )
         assertThatExceptionOfType(IllegalArgumentException::class.java)
-            .isThrownBy { LottoTicket(purchaseAmount = purchaseAmount, _manualLottoNumbers = manualLottoNumbers) }
+            .isThrownBy { LottoTicket(purchaseAmount = purchaseAmount, manualLottoNumbers = manualLottoNumbers) }
             .withMessage("수동으로 구매할 로또 수가 구매 금액을 초과할 수 없습니다.")
     }
 
@@ -40,10 +39,10 @@ internal class LottoTicketTest {
     fun `수동으로 구매할 로또 수가 구매 금액을 초과하지 않으면 티켓이 생성된다`() {
         val purchaseAmount = 2000
         val manualLottoNumbers = listOf(
-            LottoNumbers(listOf(1, 2, 3, 4, 5, 6).map(::LottoNumber)),
-            LottoNumbers(listOf(1, 2, 3, 4, 5, 6).map(::LottoNumber))
+            listOf(1, 2, 3, 4, 5, 6),
+            listOf(1, 2, 3, 4, 5, 6)
         )
-        val lottoTicket = LottoTicket(purchaseAmount = purchaseAmount, _manualLottoNumbers = manualLottoNumbers)
+        val lottoTicket = LottoTicket(purchaseAmount = purchaseAmount, manualLottoNumbers = manualLottoNumbers)
         assertThat(lottoTicket).isNotNull
     }
 
@@ -58,7 +57,7 @@ internal class LottoTicketTest {
     @Test
     fun `입력한 수동 로또 번호 개수가 자동 생성 개수보다 적은 로또 티켓은 자동 로또 번호 수량이 수동 로또 번호 수량보다 많다`() {
         val purchaseAmount = 4000
-        val manualLottoNumbers = listOf(LottoNumbers(listOf(1, 2, 3, 4, 5, 6).map(::LottoNumber)))
+        val manualLottoNumbers = listOf(listOf(1, 2, 3, 4, 5, 6))
         val lottoTicket = LottoTicket(purchaseAmount, manualLottoNumbers)
         assertThat(lottoTicket.autoLottoNumbersSize).isEqualTo(3)
         assertThat(lottoTicket.manualLottoNumbersSize).isEqualTo(1)
@@ -68,8 +67,8 @@ internal class LottoTicketTest {
     fun `입력한 수동 로또 번호 개수와 자동 생성 금액이 동일한 로또 티켓은 자동 로또 번호 수량과 수동 로또 번호 수량이 일치한다`() {
         val purchaseAmount = 4000
         val manualLottoNumbers = listOf(
-            LottoNumbers(listOf(1, 2, 3, 4, 5, 6).map(::LottoNumber)),
-            LottoNumbers(listOf(1, 2, 3, 4, 5, 6).map(::LottoNumber)),
+            listOf(1, 2, 3, 4, 5, 6),
+            listOf(1, 2, 3, 4, 5, 6),
         )
         val lottoTicket = LottoTicket(purchaseAmount, manualLottoNumbers)
         assertThat(lottoTicket.autoLottoNumbersSize).isEqualTo(2)
@@ -80,9 +79,9 @@ internal class LottoTicketTest {
     fun `입력한 수동 로또 번호 개수가 자동 생성 개수보다 많은 로또 티켓은 수동 로또 번호 수량이 자동 로또 번호 수량보다 많다`() {
         val purchaseAmount = 4000
         val manualLottoNumbers = listOf(
-            LottoNumbers(listOf(1, 2, 3, 4, 5, 6).map(::LottoNumber)),
-            LottoNumbers(listOf(1, 2, 3, 4, 5, 6).map(::LottoNumber)),
-            LottoNumbers(listOf(1, 2, 3, 4, 5, 6).map(::LottoNumber)),
+            listOf(1, 2, 3, 4, 5, 6),
+            listOf(1, 2, 3, 4, 5, 6),
+            listOf(1, 2, 3, 4, 5, 6),
         )
         val lottoTicket = LottoTicket(purchaseAmount, manualLottoNumbers)
         assertThat(lottoTicket.autoLottoNumbersSize).isEqualTo(1)
