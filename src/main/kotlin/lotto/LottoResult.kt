fa5ab4039ea51b900079526fa242c9ea6a040074
hojihun5516 <hojihun5516@daum.net>
2023-01-03 12:36:14 +0900

    feat: LottoResult by isbonus

--- src/main/kotlin/lotto/LottoResult.kt
+++ src/main/kotlin/lotto/LottoResult.kt
@@ -1,10 +1,17 @@
 package lotto
 
 class LottoResult(
-    private val winningLotto: LottoNumbers,
+    private val winningLotto: WinningLottoNumbers,
     private val boughtLottos: List<LottoNumbers>
 ) {
     fun getRanks(): Ranks {
-        return Ranks(boughtLottos.map { Rank.of(it.countMatchNumber(winningLotto.numbers)) })
+        return Ranks(
+            boughtLottos.map {
+                Rank.from(
+                    matchCount = it.countMatchNumber(winningLotto.getAllNumbers()),
+                    isBonus = it.isContainNumber(winningLotto.bonusNumber)
+                )
+            }
+        )
     }
 }
