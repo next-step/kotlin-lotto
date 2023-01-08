2a589900fbf5014533c876ccd388054becba337e
Choiyounho <maxosa78@gmail.com>
2023-01-08 21:17:08 +0900

    refactor : 테스트 코드에 보너스 번호 적용

--- src/test/kotlin/lotto/domain/LottosTest.kt
+++ src/test/kotlin/lotto/domain/LottosTest.kt
@@ -1,7 +1,6 @@
 package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class LottosTest {
@@ -12,13 +11,31 @@ internal class LottosTest {
         val lottos = Lottos(
             listOf(Lotto(listOf(1, 2, 3, 4, 5, 6)))
         )
-        val winningLotto = WinningLotto(listOf(1, 2, 3, 4, 5, 6))
+        val bonusNumber = 7
+        val winningLotto = WinningLotto(listOf(1, 2, 3, 4, 5, 6), bonusNumber)
 
         // when
-        val actual = lottos.matchWinningNumbers(winningLotto)
+        val actual = lottos.matchWinningNumbers(winningLotto, bonusNumber)
 
         // then
         val expected = mapOf(Ranking.FIRST to 1)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `구매한 로또번호가 5개 맞고 보너스번호가 맞을 경우 2등이 된다`() {
+        // given
+        val lottos = Lottos(
+            listOf(Lotto(listOf(1, 2, 3, 4, 5, 7)))
+        )
+        val bonusNumber = 7
+        val winningLotto = WinningLotto(listOf(1, 2, 3, 4, 5, 6), bonusNumber)
+
+        // when
+        val actual = lottos.matchWinningNumbers(winningLotto, bonusNumber)
+
+        // then
+        val expected = mapOf(Ranking.SECOND to 1)
         assertThat(actual).isEqualTo(expected)
     }
 }
--- src/test/kotlin/lotto/domain/WinningLottosTest.kt
+++ src/test/kotlin/lotto/domain/WinningLottosTest.kt
@@ -7,12 +7,13 @@ import org.junit.jupiter.api.assertThrows
 internal class WinningLottosTest {
 
     @Test
-    fun `당첨번호를 6개 입력한다`() {
+    fun `당첨번호를 6개와 보너스 번호를 입력한다`() {
         // given
         val inputWinningNumber = listOf("1", "2", "3", "4", "5", "6")
+        val bonusNumber = 7
 
         // when
-        val actual = WinningLotto.of(inputWinningNumber).winningNumbers.size
+        val actual = WinningLotto.of(inputWinningNumber, bonusNumber).winningNumbers.size
 
         // then
         val expected = 6
@@ -22,21 +23,47 @@ internal class WinningLottosTest {
     @Test
     fun `당첨번호가 입력되지 않으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            WinningLotto(emptyList())
+            WinningLotto(emptyList(), 7)
         }
     }
 
     @Test
     fun `당첨번호가 6개가 아니면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            WinningLotto.of(listOf("1", "2"))
+            WinningLotto.of(listOf("1", "2"), 7)
         }
     }
 
     @Test
     fun `당첨번호가 1에서 45숫자가 아니면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            WinningLotto.of(listOf("1", "2", "3", "4", "5", "46"))
+            WinningLotto.of(listOf("1", "2", "3", "4", "5", "46"), 7)
         }
+    }
+
+    @Test
+    fun `입력한 로또 번호에 보너스 번호가 포함될 경우 true`() {
+        // given
+        val inputWinningNumber = listOf("1", "2", "3", "4", "5", "7")
+        val bonusNumber = 7
+
+        // when
+        val actual = WinningLotto.of(inputWinningNumber, bonusNumber).isMatchBonus(bonusNumber)
+
+        // then
+        assertThat(actual).isTrue
+    }
+
+    @Test
+    fun `입력한 로또 번호에 보너스 번호가 포함되지 않은 경우 false`() {
+        // given
+        val inputWinningNumber = listOf("1", "2", "3", "4", "5", "6")
+        val bonusNumber = 7
+
+        // when
+        val actual = WinningLotto.of(inputWinningNumber, bonusNumber).isMatchBonus(8)
+
+        // then
+        assertThat(actual).isFalse
     }
 }
