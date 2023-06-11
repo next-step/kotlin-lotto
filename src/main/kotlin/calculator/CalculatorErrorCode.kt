f50492a6210bb298729063c980f5992af82fc39f
ljw0829 <olo_jinwon@naver.com>
2023-06-11 13:07:36 +0900

    feat: 양수 숫자 리스트 객체 생성

---
+++ src/main/kotlin/calculator/CalculatorErrorCode.kt
@@ -0,0 +1,7 @@
+package calculator
+
+enum class CalculatorErrorCode(val message: (String) -> String) {
+    INVALID_POSITIVE_NUMBERS(
+        message = { input -> "양수를 입력해야 합니다. 입력값 : $input" },
+    ),
+}
---
+++ src/main/kotlin/calculator/PositiveNumbers.kt
@@ -0,0 +1,28 @@
+package calculator
+
+class PositiveNumbers(elements: IntArray) {
+
+    val elements: IntArray = elements.copyOf()
+        get() = field.copyOf()
+
+    init {
+        elements.forEach {
+            require(value = it >= ZERO) {
+                CalculatorErrorCode.INVALID_POSITIVE_NUMBERS.message(it.toString())
+            }
+        }
+    }
+
+    override fun equals(other: Any?): Boolean = when {
+        this === other -> true
+        other !is PositiveNumbers -> false
+        (elements contentEquals other.elements).not() -> false
+        else -> true
+    }
+
+    override fun hashCode(): Int = elements.contentHashCode()
+
+    companion object {
+        private const val ZERO = 0
+    }
+}
