886e39f8334e40c0e896f97b11a4ffc4ca0ae155
ljw0829 <olo_jinwon@naver.com>
2023-06-11 13:39:11 +0900

    feat: 문자열 계산기 컨버트 기능 구현

--- src/main/kotlin/calculator/CalculatorErrorCode.kt
+++ src/main/kotlin/calculator/CalculatorErrorCode.kt
@@ -4,4 +4,7 @@ enum class CalculatorErrorCode(val message: (String) -> String) {
     INVALID_POSITIVE_NUMBERS(
         message = { input -> "양수를 입력해야 합니다. 입력값 : $input" },
     ),
+    INVALID_NUMBERS(
+        message = { input -> "숫자를 입력해야 합니다. 입력값 : $input" },
+    ),
 }
---
+++ src/main/kotlin/calculator/StringCalculatorConvert.kt
@@ -0,0 +1,30 @@
+package calculator
+
+object StringCalculatorConvert {
+
+    private val CUSTOM_DELIMITER_REGEX: Regex = Regex(pattern = """//(.)\n(.*)""")
+    private val DEFAULT_DELIMITER_REGEX: Regex = Regex(pattern = """[,:]""")
+
+    private const val DEFAULT_NUMBER: Int = 0
+
+    @Throws(IllegalArgumentException::class)
+    fun convertNumbers(stringCalculatorText: String): PositiveNumbers = PositiveNumbers(
+        elements = splitText(text = stringCalculatorText).map { convertNumber(it) }
+            .toIntArray(),
+    )
+
+    private fun splitText(text: String): List<String> = CUSTOM_DELIMITER_REGEX.find(input = text)
+        ?.run {
+            val (customDelimiter, tokenStrings) = destructured
+            tokenStrings.split(customDelimiter)
+        } ?: text.split(regex = DEFAULT_DELIMITER_REGEX)
+
+    @Throws(IllegalArgumentException::class)
+    private fun convertNumber(token: String) = when {
+        token.isBlank() -> DEFAULT_NUMBER
+        else -> requireNotNull(value = token.toIntOrNull()) {
+            CalculatorErrorCode.INVALID_NUMBERS.message(token)
+        }
+    }
+}
+
