f53dcbc51cb6a09da419524f2503792ddc61c510
Dongjoon <joon1251@gmail.com>
2023-06-12 17:49:02 +0900

    Step1 - 문자열 덧셈 계산기 (#608)
    * doc : README에 요구사항 작성
    
    * polish : 불필요한 파일 제거
    
    * update : kotest version up -> 5.6.2 & dataset dependency 추가
    
    * feature : 빈 문자열을 입력하면 0을 반환
    
    * feature : null을 입력하면 0을 반환
    
    * feature : 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환
    
    * feature : 공백을 입력하면 0을 반환
    
    * feature : 숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.
    
    * test : 숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환하는 test case 수정
    
    * feature : 구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.
    
    * feature : //와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.
    
    * feature : 문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다
    
    * refactor : package refactoring
    
    * refactor : class 멤버 변수 분리
    
    * refactor : default pattern regex로 수정
    
    * refactor : toPositiveInt parameter 명 수정
    
    * refactor : 숫자로 변환하는 메소드명 수정
    
    ---------
    
    Co-authored-by: genos-lee <genos.lee@kakaocorp.com>

---
+++ src/main/kotlin/next/step/calculator/StringAddCalculator.kt
@@ -0,0 +1,29 @@
+package next.step.calculator
+
+object StringAddCalculator {
+
+    private val CUSTOM_COMMAND_PATTERN_REGEX = Regex("//(.)\n(.*)")
+    private val DEFAULT_COMMAND_PATTERN_REGEX = "[,:]".toRegex()
+
+    fun add(command: String?): Int {
+        if (command.isNullOrBlank()) {
+            return 0
+        }
+        return parse(command).sumOf { toNonNegativeInt(it) }
+    }
+
+    private fun parse(command: String): List<String> {
+        CUSTOM_COMMAND_PATTERN_REGEX.find(command)?.let {
+            return it.groupValues[2].split(it.groupValues[1])
+        }
+        return command.split(DEFAULT_COMMAND_PATTERN_REGEX)
+    }
+
+    private fun toNonNegativeInt(token: String): Int {
+        val n = token.toInt()
+        if (n < 0) {
+            throw RuntimeException()
+        }
+        return n
+    }
+}
---
+++ src/test/kotlin/next/step/calculator/StringAddCalculatorTest.kt
@@ -0,0 +1,85 @@
+package next.step.calculator
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
+
+class StringAddCalculatorTest : DescribeSpec({
+    data class CommandExpected(val command: String, val expected: Int)
+
+    describe("문자열 덧셈 계산기") {
+        context("빈 문자열을 입력하면") {
+            it("0을 반환한다") {
+                StringAddCalculator.add("") shouldBe 0
+            }
+        }
+
+        context("공백을 입력하면") {
+            it("0을 반환한다") {
+                StringAddCalculator.add(" ") shouldBe 0
+            }
+        }
+
+        context("null을 입력하면") {
+            it("0을 반환한다") {
+                StringAddCalculator.add(null) shouldBe 0
+            }
+        }
+
+        context("숫자 하나를 문자열로 입력하면, 해당 숫자를 반환한다") {
+            withData(
+                nameFn = { "add(\"${it.command}\") =  ${it.expected}" },
+                CommandExpected("1", 1),
+                CommandExpected("2", 2),
+                CommandExpected("3", 3),
+            ) { (command, expected) ->
+                StringAddCalculator.add(command) shouldBe expected
+            }
+        }
+
+        context("숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.") {
+            withData(
+                nameFn = { "add(\"${it.command}\") =  ${it.expected}" },
+                CommandExpected("1,1", 2),
+                CommandExpected("2,3,5", 10),
+                CommandExpected("3,5,6,11", 25),
+            ) { (command, expected) ->
+                StringAddCalculator.add(command) shouldBe expected
+            }
+        }
+
+        context("구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.") {
+            withData(
+                nameFn = { "add(\"${it.command}\") =  ${it.expected}" },
+                CommandExpected("1:15", 16),
+                CommandExpected("2:3,5", 10),
+                CommandExpected("3,5:6:11", 25),
+            ) { (command, expected) ->
+                StringAddCalculator.add(command) shouldBe expected
+            }
+        }
+
+        context("//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.") {
+            withData(
+                nameFn = { "add(\"${it.command}\") =  ${it.expected}" },
+                CommandExpected("//;\n1;2;3", 6),
+                CommandExpected("//;\n2;3;5", 10),
+                CommandExpected("//;\n3;5;6;11", 25),
+            ) { (command, expected) ->
+                StringAddCalculator.add(command) shouldBe expected
+            }
+        }
+
+        context("문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.") {
+            withData(
+                nameFn = { "add(\"${it}\") throw RuntimeException" },
+                listOf("-2:3,5", "//;\n2;3;-5", "//;\n3;5;-6;11")
+            ) { negativeCmd ->
+                shouldThrow<RuntimeException> {
+                    StringAddCalculator.add(negativeCmd)
+                }
+            }
+        }
+    }
+})
