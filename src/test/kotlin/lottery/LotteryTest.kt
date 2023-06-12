d3988ddf3b0b2616d6f7da249d6e1d649a47e94c
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-12 20:37:41 +0900

    test: 로또 번호 6개 저장 테스트 추가

--- src/test/kotlin/lottery/LotteryTest.kt
+++ src/test/kotlin/lottery/LotteryTest.kt
@@ -4,6 +4,7 @@ import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.data.forAll
 import io.kotest.data.row
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import lottery.LotteryNumberTest.Companion.LOTTERY_NUMBER_1
 import lottery.LotteryNumberTest.Companion.LOTTERY_NUMBER_2
@@ -54,6 +55,19 @@ class LotteryTest : FunSpec({
 
             val exception = shouldThrowExactly<IllegalArgumentException> { Lottery(values = input) }
             exception.message shouldBe "로또 번호는 중복되어 입력될 수 없다"
+        }
+
+        test("로또 번호를 6개 가지고 있다") {
+            val input = listOf(
+                LOTTERY_NUMBER_1,
+                LOTTERY_NUMBER_2,
+                LOTTERY_NUMBER_3,
+                LOTTERY_NUMBER_4,
+                LOTTERY_NUMBER_5,
+                LOTTERY_NUMBER_6
+            )
+            val actual = Lottery(values = input)
+            actual.values shouldHaveSize 6
         }
     }
 })
