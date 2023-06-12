768e5cf348730920649fa948a18ab1d265a70b5b
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-12 22:56:03 +0900

    feat: Lottery deligation

--- src/main/kotlin/lottery/domain/Lottery.kt
+++ src/main/kotlin/lottery/domain/Lottery.kt
@@ -2,7 +2,7 @@ package lottery.domain
 
 class Lottery(
     val values: List<LotteryNumber>
-) {
+) : List<LotteryNumber> by values {
     init {
         require(values.size == LOTTERY_NUMBER_SIZE) { "로또 번호는 6개만 입력하여야한다" }
         require(values.distinct().size == LOTTERY_NUMBER_SIZE) { "로또 번호는 중복되어 입력될 수 없다" }
