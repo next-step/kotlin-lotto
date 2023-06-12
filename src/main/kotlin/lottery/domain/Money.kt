70cbd96fb17b3bed471ae1c431cf8a07a750be86
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-12 22:42:02 +0900

    feat: 1000원 미만으로 로또를 사려하면 예외 추가 및 테스트

--- src/main/kotlin/lottery/domain/Money.kt
+++ src/main/kotlin/lottery/domain/Money.kt
@@ -6,4 +6,8 @@ class Money(
     init {
         require(value >= 0) { "돈은 음수가 입력될 수 없다" }
     }
+
+    fun purchaseLotteries() {
+        check(value >= 1_000) { "로또를 사기엔 부족한 금액이다" }
+    }
 }
--- src/test/kotlin/lottery/domain/MoneyTest.kt
+++ src/test/kotlin/lottery/domain/MoneyTest.kt
@@ -12,4 +12,11 @@ class MoneyTest : FunSpec({
             exception.message shouldBe "돈은 음수가 입력될 수 없다"
         }
     }
+
+    context("purchaseLotteries") {
+        test("로또를 사기에 부족한 돈이 입력될 경우 예외가 발생한다") {
+            val exception = shouldThrowExactly<IllegalStateException> { Money(value = 999).purchaseLotteries() }
+            exception.message shouldBe "로또를 사기엔 부족한 금액이다"
+        }
+    }
 })
