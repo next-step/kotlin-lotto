5ec78fbf54ebb8062101fe6b6007d71e2ebed8fe
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-12 19:52:23 +0900

    refactor: 매직 넘버 상수화

--- src/main/kotlin/lottery/LotteryNumber.kt
+++ src/main/kotlin/lottery/LotteryNumber.kt
@@ -5,6 +5,11 @@ value class LotteryNumber(
     val value: Int
 ) {
     init {
-        require(value in 1..45) { "로또 번호는 1~45의 수만 입력 가능하다." }
+        require(value in MIN_NUMBER..MAX_NUMBER) { "로또 번호는 1~45의 수만 입력 가능하다." }
+    }
+
+    companion object {
+        private const val MIN_NUMBER = 1
+        private const val MAX_NUMBER = 45
     }
 }
