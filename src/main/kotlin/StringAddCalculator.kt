88a7afb8e2fbf167f5001cfebcf04ab7d4568198
Wordbe <seonghojin3@gmail.com>
2023-06-12 18:58:56 +0900

    fix(StringAddCalculator): 상수 선언

--- src/main/kotlin/StringAddCalculator.kt
+++ src/main/kotlin/StringAddCalculator.kt
@@ -1,17 +1,20 @@
 object StringAddCalculator {
+    private const val CUSTOM_DELIMITER_FIND_REGEX = "//(.)\n(.*)"
+    private const val DEFAULT_DELIMITERS_REGEX = "[,:]"
+
     fun add(text: String?): Int {
         if (text.isNullOrBlank()) {
             return 0
         }
 
-        val matchResult = Regex("//(.)\n(.*)").find(text)
+        val matchResult = Regex(CUSTOM_DELIMITER_FIND_REGEX).find(text)
         matchResult?.let {
             val customDelimiter = it.groupValues[1]
             val tokens = it.groupValues[2].split(customDelimiter)
             return sum(tokens)
         }
 
-        val delimiter = "[,:]".toRegex()
+        val delimiter = DEFAULT_DELIMITERS_REGEX.toRegex()
         val tokens = text.split(delimiter)
         return sum(tokens)
     }
