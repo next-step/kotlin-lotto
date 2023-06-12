6a16d96b3293177417bb5964177661c60f54b384
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-12 20:39:30 +0900

    refactor: LOTTERY_NUMBER_SIZE 매직넘버 상수화

--- src/main/kotlin/lottery/Lottery.kt
+++ src/main/kotlin/lottery/Lottery.kt
@@ -4,7 +4,11 @@ class Lottery(
     val values: List<LotteryNumber>
 ) {
     init {
-        require(values.size == 6) { "로또 번호는 6개만 입력하여야한다" }
-        require(values.distinct().size == 6) { "로또 번호는 중복되어 입력될 수 없다" }
+        require(values.size == LOTTERY_NUMBER_SIZE) { "로또 번호는 6개만 입력하여야한다" }
+        require(values.distinct().size == LOTTERY_NUMBER_SIZE) { "로또 번호는 중복되어 입력될 수 없다" }
+    }
+
+    companion object {
+        private const val LOTTERY_NUMBER_SIZE = 6
     }
 }
