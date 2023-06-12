66a3f9028563c822ceed11f3d7eb497481a45273
ljw0829 <olo_jinwon@naver.com>
2023-06-12 23:24:41 +0900

    refactor: 리뷰 내용 반영  - PositiveNumbers Data Class로 변경  - elements 요소 Array -> List로 변경

--- src/main/kotlin/calculator/PositiveNumbers.kt
+++ src/main/kotlin/calculator/PositiveNumbers.kt
@@ -2,10 +2,7 @@ package calculator
 
 import calculator.model.CalculatorErrorCode
 
-class PositiveNumbers(elements: IntArray) {
-
-    val elements: IntArray = elements.copyOf()
-        get() = field.copyOf()
+data class PositiveNumbers(val elements: List<Int>) {
 
     init {
         val invalidNumbers = elements.filter { it < ZERO }
@@ -14,15 +11,6 @@ class PositiveNumbers(elements: IntArray) {
             CalculatorErrorCode.INVALID_POSITIVE_NUMBERS.message(invalidNumbers.joinToString())
         }
     }
-
-    override fun equals(other: Any?): Boolean = when {
-        this === other -> true
-        other !is PositiveNumbers -> false
-        (elements contentEquals other.elements).not() -> false
-        else -> true
-    }
-
-    override fun hashCode(): Int = elements.contentHashCode()
 
     companion object {
         private const val ZERO = 0
--- src/test/kotlin/calculator/PositiveNumbersTest.kt
+++ src/test/kotlin/calculator/PositiveNumbersTest.kt
@@ -6,32 +6,22 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.data.forAll
 import io.kotest.data.row
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.throwable.shouldHaveMessage
 
 class PositiveNumbersTest : StringSpec({
 
     "PositiveNumbers 생성 시 양수로 구성되면 정상 생성된다." {
-        val expect = IntArray(10) { it }
+        val expect = List(10) { it }
 
         val positiveNumbers = PositiveNumbers(elements = expect)
 
         positiveNumbers.elements shouldBe expect
     }
 
-    "PositiveNumbers 생성 시 깊은 복사가 적용된다." {
-        val expect = IntArray(10) { it }
-
-        val positiveNumbers = PositiveNumbers(elements = expect)
-        expect[0] = 100
-
-        positiveNumbers.elements shouldNotBe expect
-    }
-
     "PositiveNumbers 생성 시 음수가 있을 경우 IllegalArgumentException 예외가 발생한다." {
         forAll(
-            row(intArrayOf(-1, 2, 3), "-1"),
-            row(intArrayOf(-1, -2, -3), "-1, -2, -3"),
+            row(listOf(-1, 2, 3), "-1"),
+            row(listOf(-1, -2, -3), "-1, -2, -3"),
         ) { numbers, expect ->
             val exception = shouldThrow<IllegalArgumentException> {
                 PositiveNumbers(
