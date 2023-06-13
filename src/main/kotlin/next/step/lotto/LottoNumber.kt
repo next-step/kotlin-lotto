607d03b1e479d19070cf45164ffa7f1227271d44
genos-lee <genos.lee@kakaocorp.com>
2023-06-14 00:51:41 +0900

    feature : LottoNumber random 생성

--- src/main/kotlin/next/step/lotto/LottoNumber.kt
+++ src/main/kotlin/next/step/lotto/LottoNumber.kt
@@ -1,17 +1,25 @@
 package next.step.lotto
 
+import java.util.concurrent.ThreadLocalRandom
+
 @JvmInline
 value class LottoNumber(private val n: Int) {
     init {
         require(n in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) { "로또 번호는 1~45 사이여야 합니다." }
     }
 
+    fun number() = n
+
     companion object {
-        private const val MIN_LOTTO_NUMBER = 1
-        private const val MAX_LOTTO_NUMBER = 45
+        const val MIN_LOTTO_NUMBER = 1
+        const val MAX_LOTTO_NUMBER = 45
 
         fun of(n: Int): LottoNumber {
             return LottoNumber(n)
+        }
+
+        fun random(): LottoNumber {
+            return LottoNumber(ThreadLocalRandom.current().nextInt(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER + 1))
         }
     }
 }
--- src/test/kotlin/next/step/lotto/LottoNumberTest.kt
+++ src/test/kotlin/next/step/lotto/LottoNumberTest.kt
@@ -1,11 +1,14 @@
 package next.step.lotto
 
+import io.kotest.assertions.assertSoftly
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
+import io.kotest.matchers.ints.shouldBeLessThanOrEqual
 
 class LottoNumberTest : DescribeSpec({
 
-    describe("로또 번호 생성") {
+    describe("LottoNumber 생성") {
         context("1보다 작은 숫자로 생성하면") {
             it("예외 발생") {
                 shouldThrow<IllegalArgumentException> { LottoNumber.of(0) }
@@ -15,6 +18,17 @@ class LottoNumberTest : DescribeSpec({
         context("45보다 큰 숫자로 생성하면") {
             it("예외 발생") {
                 shouldThrow<IllegalArgumentException> { LottoNumber.of(46) }
+            }
+        }
+
+        context("랜덤으로 생성하면") {
+            it("1과 45 사이의 숫자로 생성됨").config(invocations = 1000) {
+                val number = LottoNumber.random().number()
+                
+                assertSoftly {
+                    number shouldBeGreaterThanOrEqual LottoNumber.MIN_LOTTO_NUMBER
+                    number shouldBeLessThanOrEqual LottoNumber.MAX_LOTTO_NUMBER
+                }
             }
         }
     }
