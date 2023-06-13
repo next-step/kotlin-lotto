47451d05137d73550d85eb19aa8430e8a36d74d3
ljw0829 <olo_jinwon@naver.com>
2023-06-13 22:33:15 +0900

    test: 로또 번호 객체에 대한 테스트 작성

---
+++ src/test/kotlin/lotto/domain/LottoNumberTest.kt
@@ -0,0 +1,43 @@
+package lotto.domain
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
+import lotto.model.LottoErrorCode
+
+class LottoNumberTest : DescribeSpec({
+
+    describe(name = "로또 번호를 생성할 수 있다.") {
+        forAll(
+            row(1, LottoNumber(number = 1)),
+            row(45, LottoNumber(number = 45)),
+        ) { number, expect ->
+            context(name = "1 ~ 45 사이의 번호로 생성하면") {
+                val actual = LottoNumber(number = number)
+
+                it(name = "입력한 번호로 로또 번호가 생성된다.") {
+                    actual shouldBe expect
+                }
+            }
+        }
+
+        forAll(
+            row(0, "0"),
+            row(46, "46"),
+        ) { number, expect ->
+            context(name = "1 ~ 45 범위 밖의 번호로 생성하면") {
+                val exception = shouldThrow<IllegalArgumentException> {
+                    LottoNumber(number = number)
+                }
+
+                it(name = "로또 번호 범위 안에 번호로 생성하라는 에러가 발생한다.") {
+                    exception shouldHaveMessage
+                            LottoErrorCode.INVALID_LOTTO_NUMBER.message("${LottoNumber.LOTTO_NUMBER_RANGE} $expect")
+                }
+            }
+        }
+    }
+})
