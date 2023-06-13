3e4a5287b6976d96114869df5ecde927d2a99c44
devyonghee <devyonghee@gmail.com>
2023-06-14 03:01:41 +0900

    feat(LottoRank): 일치하는 로또 순위 반환

---
+++ src/main/kotlin/lotto/LottoRank.kt
@@ -0,0 +1,30 @@
+package lotto
+
+enum class LottoRank(
+    private val prize: Int,
+    countCondition: Int,
+    matchConditionStrategy: MatchConditionStrategy,
+) {
+    FIRST(2_000_000_000, 6, MatchConditionStrategy.EXACTLY_COUNT),
+    SECOND(1_500_000, 5, MatchConditionStrategy.EXACTLY_COUNT),
+    THIRD(5_000, 4, MatchConditionStrategy.EXACTLY_COUNT),
+    FOURTH(5_000, 3, MatchConditionStrategy.EXACTLY_COUNT),
+    MISS(0, 2, MatchConditionStrategy.LESS_THAN_COUNT),
+    ;
+
+    private val matchCondition: (Int) -> Boolean = matchConditionStrategy.strategyFactory(countCondition)
+
+    private enum class MatchConditionStrategy(val strategyFactory: (Int) -> (Int) -> Boolean) {
+        EXACTLY_COUNT({ count -> { it == count } }),
+        LESS_THAN_COUNT({ count -> { it <= count } }),
+        ;
+    }
+
+    companion object {
+        fun rankOf(count: Int): LottoRank {
+            return values()
+                .firstOrNull { it.matchCondition(count) }
+                ?: throw IllegalArgumentException("matched rank is not exist. provided count(`$count`)")
+        }
+    }
+}
---
+++ src/test/kotlin/lotto/LottoRankTest.kt
@@ -0,0 +1,30 @@
+package lotto
+
+import io.kotest.assertions.throwables.shouldThrowExactly
+import io.kotest.core.spec.DisplayName
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
+
+@DisplayName("로또 순위")
+class LottoRankTest : StringSpec({
+
+    "매칭 개수에 따른 로또 순위" {
+        listOf(
+            6 to LottoRank.FIRST,
+            5 to LottoRank.SECOND,
+            4 to LottoRank.THIRD,
+            3 to LottoRank.FOURTH,
+            2 to LottoRank.MISS,
+            0 to LottoRank.MISS,
+        ).forAll {
+            LottoRank.rankOf(it.first) shouldBe it.second
+        }
+    }
+
+    "매칭되는 개수가 없으면 예외" {
+        listOf(7, Int.MAX_VALUE).forAll {
+            shouldThrowExactly<IllegalArgumentException> { LottoRank.rankOf(it) }
+        }
+    }
+})
