d40a765ae110d93445c8ab81d8408d7582cedc31
ljw0829 <olo_jinwon@naver.com>
2023-06-13 23:46:48 +0900

    test: 구매 주문 문자열 생성 기능 테스트 추가

--- src/test/kotlin/lotto/domain/order/PurchaseOrderTest.kt
+++ src/test/kotlin/lotto/domain/order/PurchaseOrderTest.kt
@@ -1,10 +1,13 @@
 package lotto.domain.order
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.data.forAll
 import io.kotest.data.row
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
 import lotto.domain.Lottery
+import lotto.model.LottoErrorCode
 
 class PurchaseOrderTest : StringSpec({
 
@@ -16,6 +19,7 @@ class PurchaseOrderTest : StringSpec({
             row(32234, (32234 / Lottery.LOTTERY_PRICE).toInt()),
             row(4534523, (4534523 / Lottery.LOTTERY_PRICE).toInt()),
             row(23344, (23344 / Lottery.LOTTERY_PRICE).toInt()),
+            row(12, (12 / Lottery.LOTTERY_PRICE).toInt()),
         ) { amount, expect ->
             val purchaseOrder = PurchaseOrder(amount = amount)
 
@@ -33,6 +37,28 @@ class PurchaseOrderTest : StringSpec({
             val purchaseOrder = PurchaseOrder(amount = amount)
 
             purchaseOrder.purchasedPrice shouldBe expect
+        }
+    }
+
+    "구매 주문을 생성할 때 문자열로도 생성할 수 있다." {
+        val expect = "10000"
+        val purchaseOrder = PurchaseOrder(amountText = expect)
+
+        purchaseOrder.purchasedPrice shouldBe PurchaseOrder(amount = expect.toInt()).purchasedPrice
+    }
+
+    "구매 주문을 생성할 때 숫자가 아닌 문자열과 2,147,483,647보다 큰 숫자를 입력하면 구매할 수 없다는 에러가 발생한다." {
+        forAll(
+            row("  "),
+            row("test"),
+            row("한글이 아닙니다."),
+            row("10000000000000"),
+        ) { amountText ->
+            val exception = shouldThrow<IllegalArgumentException> {
+                PurchaseOrder(amountText = amountText)
+            }
+
+            exception shouldHaveMessage LottoErrorCode.INVALID_PURCHASE_AMOUNT.message(amountText)
         }
     }
 })
