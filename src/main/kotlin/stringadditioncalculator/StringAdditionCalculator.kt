c717aef1ebb55f1ee7a7c6eab24ccd8f96c701f3
nam-sangyun <nsun9505@naver.com>
2023-06-15 00:49:45 +0900

    refactor(피드백 반영)

--- src/main/kotlin/stringadditioncalculator/StringAdditionCalculator.kt
+++ src/main/kotlin/stringadditioncalculator/StringAdditionCalculator.kt
@@ -1,26 +1,11 @@
 package stringadditioncalculator
 
-fun String.isInt(): Boolean {
-    return try {
-        this.toInt()
-        true
-    } catch (exception: NumberFormatException) {
-        false
-    }
-}
-
 class StringAdditionCalculator {
     fun calculate(expression: String?): Int {
         val stringNumbers = StringAdditionCalculatorInputParser.parse(expression)
-        validate(stringNumbers)
+        stringNumbers.validate()
 
-        return stringNumbers.asSequence()
-            .map { it.toInt() }
+        return stringNumbers.map { it.toInt() }
             .fold(0, Int::plus)
-    }
-
-    private fun validate(stringNumbers: List<String>) {
-        val isValidStringNumbers = stringNumbers.any { !it.isInt() || it.toInt() < 0 }
-        require(!isValidStringNumbers) { "문자열 계산기에 숫자 이외의 값 또는 음수를 전달할 수 없습니다." }
     }
 }
--- src/main/kotlin/stringadditioncalculator/StringNumbers.kt
+++ src/main/kotlin/stringadditioncalculator/StringNumbers.kt
@@ -1,3 +1,9 @@
 package stringadditioncalculator
 
-class StringNumbers(stringNumbers: List<String>) : List<String> by stringNumbers
+class StringNumbers(private val stringNumbers: List<String>) : List<String> by stringNumbers {
+    fun validate() {
+        val isValidStringNumbers = stringNumbers.map { it.toIntOrNull() }
+            .any { it == null || it < 0 }
+        require(!isValidStringNumbers) { "문자열 계산기에 숫자 이외의 값 또는 음수를 전달할 수 없습니다." }
+    }
+}
