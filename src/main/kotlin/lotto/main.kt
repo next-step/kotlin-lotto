b144a05b3c0a76aafefb5581a169e5a74d04f77e
ljw0829 <olo_jinwon@naver.com>
2023-06-15 00:04:15 +0900

    feat: step3 UI (2등, 보너스 볼 입력) 추가

--- src/main/kotlin/lotto/main.kt
+++ src/main/kotlin/lotto/main.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import lotto.domain.Lottery
+import lotto.domain.LottoNumber
 import lotto.domain.LottoResult
 import lotto.domain.order.PurchaseOrder
 import lotto.view.InputView
@@ -13,7 +14,13 @@ fun main() {
     ResultView.printPurchaseResult(purchasedLotteries = purchasedLotteries)
 
     val lastWeekWinningLottery = Lottery(lotteryText = InputView.readLastWeekWinningNumbers())
-    val lottoResult = LottoResult(purchasedLotteries = purchasedLotteries, winningLottery = lastWeekWinningLottery)
+    val bonusBall = LottoNumber(numberText = InputView.readBonusBall())
+
+    val lottoResult = LottoResult(
+        purchasedLotteries = purchasedLotteries,
+        winningLottery = lastWeekWinningLottery,
+        bonusBall = bonusBall,
+    )
 
     ResultView.printLottoResult(lottoResult = lottoResult)
 }
--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -1,5 +1,6 @@
 package lotto.view
 
+import lotto.domain.LottoMatchResult
 import lotto.domain.LottoRank
 import lotto.domain.LottoResult
 import lotto.domain.PurchasedLotteries
@@ -8,8 +9,10 @@ object ResultView {
 
     private const val PURCHASE_RESULT_MESSAGE = "개를 구매했습니다."
     private const val RATE_OF_RETURN_MESSAGE = "총 수익률은 %.2f입니다."
-    private const val BENEFIT_MESSAGE = " (기준이 1이기 때문에 결과적으로 %s라는 의미임)"
-    private const val WINNING_STATISTICS_RESULT_MESSAGE = "%s개 일치 (%s원) - %s개"
+    private const val BENEFIT_MESSAGE = " (기준이 1이기 때문에 결과적으로 %s임)"
+    private const val WINNING_STATISTICS_RESULT_MESSAGE = "%s개 일치%s (%s원) - %s개"
+    private const val EMPTY_MESSAGE = ""
+    private const val BONUS_BALL_MESSAGE = ", 보너스 볼 일치"
 
     private val WINNING_STATISTICS_MESSAGE = """
         
@@ -27,6 +30,8 @@ object ResultView {
         println(message = WINNING_STATISTICS_MESSAGE)
 
         LottoRank.values()
+            .filter { it != LottoRank.MISS }
+            .sortedDescending()
             .forEach { printWinningStatisticsResult(lottoRank = it, lottoResult = lottoResult) }
 
         println(
@@ -39,9 +44,16 @@ object ResultView {
         println(
             message = WINNING_STATISTICS_RESULT_MESSAGE.format(
                 lottoRank.lottoMatchResult,
+                lottoRank.lottoMatchResult.getBonusBallMessage(),
                 lottoRank.winningMoney,
                 lottoResult.countNumberOfHit(lottoRank = lottoRank),
             ),
         )
+    }
+
+    private fun LottoMatchResult.getBonusBallMessage() = if (this.hasBonusBallCondition()) {
+        BONUS_BALL_MESSAGE
+    } else {
+        EMPTY_MESSAGE
     }
 }
