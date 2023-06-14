c732f3b73b2cae41a246ea1b20f8f2eebdd5f444
nam-sangyun <nsun9505@naver.com>
2023-06-14 23:44:19 +0900

    feature(문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw)

--- src/main/kotlin/stringadditioncalculator/StringAdditionCalculator.kt
+++ src/main/kotlin/stringadditioncalculator/StringAdditionCalculator.kt
@@ -1,11 +1,26 @@
 package stringadditioncalculator
 
+fun String.isInt(): Boolean {
+    return try {
+        this.toInt()
+        true
+    } catch (exception: NumberFormatException) {
+        false
+    }
+}
+
 class StringAdditionCalculator {
-    fun calculate(input: String): Any {
+    fun calculate(input: String): Int {
         val stringNumbers = StringAdditionCalculatorInputParser.parse(input)
+        validate(stringNumbers)
 
         return stringNumbers.asSequence()
             .map { it.toInt() }
             .fold(0, Int::plus)
+    }
+
+    private fun validate(stringNumbers: List<String>) {
+        val isValidStringNumbers = stringNumbers.any { !it.isInt() || it.toInt() < 0 }
+        require(!isValidStringNumbers) { "문자열 계산기에 숫자 이외의 값 또는 음수를 전달할 수 없습니다." }
     }
 }
--- src/test/kotlin/stringadditioncalculator/StringAdditionCalculatorTest.kt
+++ src/test/kotlin/stringadditioncalculator/StringAdditionCalculatorTest.kt
@@ -1,5 +1,6 @@
 package stringadditioncalculator
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
@@ -22,15 +23,33 @@ class StringAdditionCalculatorTest {
         actual shouldBe expected
     }
 
+    @ParameterizedTest
+    @MethodSource("getInvalidExpressions")
+    fun `문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw 한다`(expression: String) {
+        shouldThrow<RuntimeException> {
+            calculator.calculate(expression)
+        }
+    }
+
     companion object {
         @JvmStatic
         fun getValidExpressions(): List<Arguments> {
-            return listOf<Arguments>(
+            return listOf(
                 Arguments.of("", 0),
                 Arguments.of("1;2;3", 6),
                 Arguments.of("1;1,1", 3),
                 Arguments.of("//a\\n2a2;2", 6),
                 Arguments.of("///\\n1;2;3;4,5,6/7/8/9,10", 55),
+            )
+        }
+
+        @JvmStatic
+        fun getInvalidExpressions(): List<Arguments> {
+            return listOf(
+                Arguments.of("-1"),
+                Arguments.of("asdasd"),
+                Arguments.of("a,b;c"),
+                Arguments.of("//+\\n1+2,-3"),
             )
         }
     }
