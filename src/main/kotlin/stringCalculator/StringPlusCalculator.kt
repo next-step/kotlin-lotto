ca0a44ad966e3f7a6fa3b465349bc6125ccd8861
minhyukseul <mioyo2@gmail.com>
2023-06-14 22:18:50 +0900

    feat: 문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 로직 구현 및 테스트코드 작성

--- src/main/kotlin/stringCalculator/StringPlusCalculator.kt
+++ src/main/kotlin/stringCalculator/StringPlusCalculator.kt
@@ -7,25 +7,44 @@ object StringPlusCalculator {
     const val CUSTOM_SEPERATOR_PREFIX = "//"
     const val CUSTOM_SEPERATOR_POSTFIX = "\n"
     const val CUSTOM_SEPERATOR_POSITION = 0
+    const val EMPTY_VALUE = 0
     const val VALUE_POSITION = 1
 
     private var customSeperators = mutableListOf(DEFAULT_SEPERATOR_1, DEFAULT_SEPERATOR_2)
     fun seperate(string: String): List<Int> {
         if(string.isEmpty()) {
-            return listOf(0)
+            return listOf(EMPTY_VALUE)
         }
+        val convertedString = setCustomSeperatorAndGetConvertedString(string)
+
+        return checkValidTypedString(convertedString.split(*customSeperators.toTypedArray()))
+    }
+
+    fun checkValidTypedString(strings: List<String>): List<Int> {
+        return strings.map {
+            checkPlusNumberString(it)
+        }
+    }
+
+    fun checkPlusNumberString(string: String): Int{
+        val intString = runCatching {
+            string.toInt()
+        }.getOrElse {
+            throw RuntimeException("숫자만 올 수 있음")
+        }
+        if(intString < 0) {
+            throw RuntimeException("음수는 올 수 없음")
+        }
+        return intString
+    }
+
+    private fun setCustomSeperatorAndGetConvertedString(string: String): String {
         var convertedString = string
         if(CUSTOM_SEPERATOR_PREFIX in string && CUSTOM_SEPERATOR_POSTFIX in string) {
             val splitStrings = string.split(CUSTOM_SEPERATOR_POSTFIX)
             convertedString = splitStrings[VALUE_POSITION]
             customSeperators.add(splitStrings[CUSTOM_SEPERATOR_POSITION].split(CUSTOM_SEPERATOR_PREFIX)[VALUE_POSITION])
-
-//            val customSeperator = [0].split(CUSTOM_SEPERATOR_PREFIX)[0]
-//            println(customSeperator)
-
         }
-        return convertedString.split(*customSeperators.toTypedArray()).map {
-            it.toInt()
-        }
+        return convertedString
     }
 }
--- src/test/kotlin/stringCalculator/StringCalculator.kt
+++ src/test/kotlin/stringCalculator/StringCalculator.kt
@@ -1,5 +1,6 @@
 package stringCalculator
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.matchers.shouldBe
 
@@ -25,5 +26,19 @@ class StringCalculator : AnnotationSpec() {
     fun `구분자를 기준으로 값이 없는 경우 0으로 인식`() {
         val string = ""
         StringPlusCalculator.seperate(string) shouldBe listOf(0)
+    }
+
+    @Test
+    fun `문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw`() {
+        shouldThrow<RuntimeException> {
+            val string = "-1,2:3"
+            StringPlusCalculator.seperate(string)
+        }
+
+        shouldThrow<RuntimeException> {
+            val string = "a,2:3"
+            StringPlusCalculator.seperate(string)
+        }
+
     }
 }
