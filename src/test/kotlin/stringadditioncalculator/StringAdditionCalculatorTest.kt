682313fffeb9bab798b151b4ca972ea3a89c93da
nam-sangyun <nsun9505@naver.com>
2023-06-14 23:49:19 +0900

    refactor(NULL 처리)

--- src/test/kotlin/stringadditioncalculator/StringAdditionCalculatorTest.kt
+++ src/test/kotlin/stringadditioncalculator/StringAdditionCalculatorTest.kt
@@ -18,14 +18,14 @@ class StringAdditionCalculatorTest {
 
     @ParameterizedTest
     @MethodSource("getValidExpressions")
-    fun `문자열 덧셈 계산기는 구분자를 기준으로 분리된 각 숫자의 합을 반환`(expression: String, expected: Int) {
+    fun `문자열 덧셈 계산기는 구분자를 기준으로 분리된 각 숫자의 합을 반환`(expression: String?, expected: Int) {
         val actual = calculator.calculate(expression)
         actual shouldBe expected
     }
 
     @ParameterizedTest
     @MethodSource("getInvalidExpressions")
-    fun `문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw 한다`(expression: String) {
+    fun `문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw 한다`(expression: String?) {
         shouldThrow<RuntimeException> {
             calculator.calculate(expression)
         }
@@ -36,6 +36,7 @@ class StringAdditionCalculatorTest {
         fun getValidExpressions(): List<Arguments> {
             return listOf(
                 Arguments.of("", 0),
+                Arguments.of(null, 0),
                 Arguments.of("1;2;3", 6),
                 Arguments.of("1;1,1", 3),
                 Arguments.of("//a\\n2a2;2", 6),
