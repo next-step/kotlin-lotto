89bcdf1447d01df5891080392d39abd675c6a368
김현준 <4whomtbts@gmail.com>
2023-06-15 03:01:04 +0900

    feat(step1): StringCalculator 구현

--- src/main/kotlin/string/StringCalculator.kt
+++ src/main/kotlin/string/StringCalculator.kt
@@ -1,7 +1,18 @@
 package string
 
-class StringCalculator(strExpression: String) {
-    fun sum(): Int {
-        return 6
+import string.expression.Expression
+import string.splitter.SeparatorStringSplitter
+
+class StringCalculator(
+    private val splitters: List<SeparatorStringSplitter>
+) {
+    fun sum(strExpression: String): Int {
+        splitters.forEach {
+            val result = it.split(strExpression)
+            if (result != null) {
+                return Expression(result).sum()
+            }
+        }
+        throw RuntimeException("문자열을 계산할 수 없습니다")
     }
 }
--- src/test/kotlin/string/StringCalculatorTest.kt
+++ src/test/kotlin/string/StringCalculatorTest.kt
@@ -1,22 +1,32 @@
 package string
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.data.forAll
 import io.kotest.data.headers
 import io.kotest.data.row
 import io.kotest.data.table
 import io.kotest.matchers.shouldBe
+import string.converter.ExpressionTokenConverter
+import string.splitter.DefaultSeparatorStringSplitter
+import string.splitter.RegexBasedCustomSeparatorStringSplitter
+import string.splitter.SeparatorStringSplitter
 
 internal class StringCalculatorTest : StringSpec({
 
+    val tokenConverter = ExpressionTokenConverter()
+    val splitters = listOf(
+        RegexBasedCustomSeparatorStringSplitter(tokenConverter),
+        DefaultSeparatorStringSplitter(tokenConverter)
+    )
+    val sut = StringCalculator(splitters)
+
     "쉼표로 구분된 문자열을 덧셈한 결과를 반환한다" {
-        val sut = StringCalculator("1,2,3")
-        sut.sum() shouldBe 6
+        sut.sum("1,2,3") shouldBe 6
     }
 
     "콜론으로 구분된 문자열을 덧셈한 결과를 반환한다" {
-        val sut = StringCalculator("1,2,3")
-        sut.sum() shouldBe 6
+        sut.sum("1,2,3") shouldBe 6
     }
 
     "커스텀 구분자로 구분된 문자열을 덧셈한 결과를 반환한다" {
@@ -26,8 +36,14 @@ internal class StringCalculatorTest : StringSpec({
             row("-"),
             row("A"),
         ).forAll { separator ->
-            val sut = StringCalculator("//;\n1${separator}2${separator}3")
-            sut.sum() shouldBe 6
+            sut.sum("//${separator}\n1${separator}2${separator}3") shouldBe 6
         }
     }
+
+    "계산할 수 없는 형태의 식에 대해 예외가 발생한다" {
+        shouldThrow<RuntimeException> {
+            sut.sum("1k2k3")
+        }
+    }
+
 })
