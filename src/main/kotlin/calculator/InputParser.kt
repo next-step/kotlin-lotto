e78671b9893697573f31886ec4db4f09016604d5
peter <peter.shlee@kakaocorp.com>
2023-06-14 23:33:41 +0900

    feat(step1): 주어진 구분자로 숫자 parsing

--- src/main/kotlin/calculator/InputParser.kt
+++ src/main/kotlin/calculator/InputParser.kt
@@ -14,16 +14,31 @@ class InputParser(inputString: String) {
                 require(it.isNotDigit())
             }
     }
-    
+
+    fun numbers(separators: Array<String> = emptyArray()): List<Int> {
+        return inputString.substringAfterLast(SEPARATOR_END)
+            .split(
+                *separators.plus(defaultDelimiters)
+            )
+            .map {
+                if (it.isNotDigit()) throw RuntimeException()
+                it.toInt()
+            }
+    }
+
     private fun String.hasNoSeparatorSection(): Boolean {
         return !contains(SEPARATOR_END)
     }
 
-    private fun String.isNotDigit() = toIntOrNull() == null
+    private fun String.isNotDigit(): Boolean {
+        val number = toIntOrNull()
+        return number == null || number < 0
+    }
 
 
     companion object {
         private const val SEPARATOR_START = "//"
         private const val SEPARATOR_END = "\\n"
+        private val defaultDelimiters = arrayOf(",", ":")
     }
 }
--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -3,20 +3,23 @@ package calculator
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
 import org.junit.jupiter.params.provider.ValueSource
+import java.util.stream.Stream
 
 class CalculatorTest {
 
     @ParameterizedTest
     @CsvSource(
-        "//!\\n1;2;3, !",
-        "//a\\n1;2;3, a",
-        "//:\\n1;2;3, :",
-        "///\\n1;2;3, /",
-        "////\\n1;2;3, //",
-        "//???\\n1;2;3, ???",
-        "1;2;3, ",
+        "//!\\n1, !",
+        "//a\\n1, a",
+        "//:\\n1, :",
+        "///\\n1, /",
+        "////\\n1, //",
+        "//???\\n1, ???",
+        "1, ",
     )
     fun `입력된 문자열에서 구분자 parsing 테스트`(inputString: String, separator: String?) {
         assertThat(
@@ -30,6 +33,34 @@ class CalculatorTest {
         assertThatThrownBy {
             InputParser(inputString).separator()
         }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @ParameterizedTest
+    @MethodSource("숫자 parsing 테스트 데이터")
+    fun `주어진 구분자로 숫자 parsing 테스트`(inputString: String, separators: Array<String>, numbers: List<Int>) {
+        assertThat(
+            InputParser(inputString).numbers(separators)
+        ).isEqualTo(numbers)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = arrayOf("-1,2,3", "1,a,3"))
+    fun `문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException throw`(inputString: String) {
+        assertThatThrownBy {
+            InputParser(inputString).numbers()
+        }.isInstanceOf(RuntimeException::class.java)
+    }
+
+    companion object {
+        @JvmStatic
+        private fun `숫자 parsing 테스트 데이터`(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("//!\\n1!2!3", arrayOf("!"), listOf(1, 2, 3)),
+                Arguments.of("1;2;3", arrayOf(";"), listOf(1, 2, 3)),
+                Arguments.of("1,2:3", emptyArray<String>(), listOf(1, 2, 3)),
+                Arguments.of("1,2,3", emptyArray<String>(), listOf(1, 2, 3)),
+            )
+        }
     }
 }
 
