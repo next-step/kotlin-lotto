e9b4281358d14f8ee9076ca7eb5647b7978e74a1
nam-sangyun <nsun9505@naver.com>
2023-06-14 23:29:24 +0900

    feature(문자열 덧셈 계산기는 구분자를 기준으로 분리된 각 숫자의 합을 반환)

--- src/main/kotlin/stringadditioncalculator/StringAdditionCalculator.kt
+++ src/main/kotlin/stringadditioncalculator/StringAdditionCalculator.kt
@@ -2,6 +2,10 @@ package stringadditioncalculator
 
 class StringAdditionCalculator {
     fun calculate(input: String): Any {
-        return 0
+        val stringNumbers = StringAdditionCalculatorInputParser.parse(input)
+
+        return stringNumbers.asSequence()
+            .map { it.toInt() }
+            .fold(0, Int::plus)
     }
 }
--- src/test/kotlin/stringadditioncalculator/StringAdditionCalculatorTest.kt
+++ src/test/kotlin/stringadditioncalculator/StringAdditionCalculatorTest.kt
@@ -1,16 +1,37 @@
 package stringadditioncalculator
 
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class StringAdditionCalculatorTest {
 
-    @Test
-    fun `문자열 덧셈 계산기는 빈값("")이 들어오면 0을 반환한다`() {
-        val calculator = StringAdditionCalculator()
-        val input = ""
-        val actual = calculator.calculate(input)
+    private lateinit var calculator: StringAdditionCalculator
 
-        actual shouldBe 0
+    @BeforeEach
+    fun setup() {
+        calculator = StringAdditionCalculator()
+    }
+
+    @ParameterizedTest
+    @MethodSource("getValidExpressions")
+    fun `문자열 덧셈 계산기는 구분자를 기준으로 분리된 각 숫자의 합을 반환`(expression: String, expected: Int) {
+        val actual = calculator.calculate(expression)
+        actual shouldBe expected
+    }
+
+    companion object {
+        @JvmStatic
+        fun getValidExpressions(): List<Arguments> {
+            return listOf<Arguments>(
+                Arguments.of("", 0),
+                Arguments.of("1;2;3", 6),
+                Arguments.of("1;1,1", 3),
+                Arguments.of("//a\\n2a2;2", 6),
+                Arguments.of("///\\n1;2;3;4,5,6/7/8/9,10", 55),
+            )
+        }
     }
 }
