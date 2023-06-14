a5d54f68c20da27d81a6a6f0a0e8ba1c2b61ab53
ljw0829 <olo_jinwon@naver.com>
2023-06-15 00:03:47 +0900

    feat: 로또 매치 결과 내 보너스 매치 상태 추가  - 매치 결과끼리 비교하는 함수 추가  - 보너스 볼을 가지고 있는 조건 함수 추가

--- src/main/kotlin/lotto/domain/LottoMatchResult.kt
+++ src/main/kotlin/lotto/domain/LottoMatchResult.kt
@@ -2,13 +2,26 @@ package lotto.domain
 
 import lotto.model.LottoErrorCode
 
-@JvmInline
-value class LottoMatchResult(private val countOfMatch: Int) {
+class LottoMatchResult(
+    private val countOfMatch: Int,
+    private vararg val bonusMatchStates: MatchState = arrayOf(MatchState.MATCH, MatchState.NON_MATCH),
+) {
 
     init {
         check(value = countOfMatch in RANGE_COUNT_OF_MATCH) {
             LottoErrorCode.NOT_INCLUDE_RANGE_COUNT_OF_MATCH.message("$RANGE_COUNT_OF_MATCH $countOfMatch")
         }
+    }
+
+    fun correctMatchResult(otherMatchResult: LottoMatchResult): Boolean = when {
+        this.countOfMatch != otherMatchResult.countOfMatch -> false
+        this.bonusMatchStates.any { it in otherMatchResult.bonusMatchStates }.not() -> false
+        else -> true
+    }
+
+    fun hasBonusBallCondition(): Boolean = when {
+        this.bonusMatchStates.all { it == MatchState.MATCH } -> true
+        else -> false
     }
 
     override fun toString(): String = countOfMatch.toString()
--- src/main/kotlin/lotto/domain/LottoRank.kt
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -2,30 +2,34 @@ package lotto.domain
 
 enum class LottoRank(val lottoMatchResult: LottoMatchResult, val winningMoney: Int) {
     FIRST(
-        lottoMatchResult = LottoMatchResult(6),
+        lottoMatchResult = LottoMatchResult(countOfMatch = 6),
         winningMoney = 2_000_000_000,
     ),
     SECOND(
-        lottoMatchResult = LottoMatchResult(5),
-        winningMoney = 1_500_000,
+        lottoMatchResult = LottoMatchResult(countOfMatch = 5, MatchState.MATCH),
+        winningMoney = 30_000_000,
     ),
     THIRD(
-        lottoMatchResult = LottoMatchResult(4),
+        lottoMatchResult = LottoMatchResult(countOfMatch = 5, MatchState.NON_MATCH),
+        winningMoney = 1_500_000,
+    ),
+    FOUR(
+        lottoMatchResult = LottoMatchResult(countOfMatch = 4),
         winningMoney = 50_000,
     ),
-    FOURTH(
-        lottoMatchResult = LottoMatchResult(3),
+    FIVE(
+        lottoMatchResult = LottoMatchResult(countOfMatch = 3),
         winningMoney = 5_000,
     ),
     MISS(
-        lottoMatchResult = LottoMatchResult(0),
+        lottoMatchResult = LottoMatchResult(countOfMatch = 0),
         winningMoney = 0,
     ),
     ;
 
     companion object {
-        private val LOTTO_RANK: Map<LottoMatchResult, LottoRank> = values().associateBy { it.lottoMatchResult }
-
-        fun valueOf(lottoMatchResult: LottoMatchResult): LottoRank = LOTTO_RANK[lottoMatchResult] ?: MISS
+        fun valueOf(lottoMatchResult: LottoMatchResult): LottoRank = values().find {
+            it.lottoMatchResult.correctMatchResult(otherMatchResult = lottoMatchResult)
+        } ?: MISS
     }
 }
