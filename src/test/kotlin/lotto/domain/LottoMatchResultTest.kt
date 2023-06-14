77a051205cf442a60a9f503fa48a9d58acda25db
ljw0829 <olo_jinwon@naver.com>
2023-06-15 00:03:00 +0900

    test: 로또 매치 결과 내 보너스 번호 매치 결과 기능 테스트

--- src/test/kotlin/lotto/domain/LottoMatchResultTest.kt
+++ src/test/kotlin/lotto/domain/LottoMatchResultTest.kt
@@ -6,6 +6,8 @@ import io.kotest.data.forAll
 import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.throwable.shouldHaveMessage
+import lotto.domain.MatchState.MATCH
+import lotto.domain.MatchState.NON_MATCH
 import lotto.model.LottoErrorCode
 
 class LottoMatchResultTest : StringSpec({
@@ -34,6 +36,28 @@ class LottoMatchResultTest : StringSpec({
             }
 
             exception shouldHaveMessage LottoErrorCode.NOT_INCLUDE_RANGE_COUNT_OF_MATCH.message("0..6 $countOfMatch")
+        }
+    }
+
+    "로또 일치 결과끼리 비교할 수 있다." {
+        forAll(
+            row(LottoMatchResult(countOfMatch = 1), LottoMatchResult(countOfMatch = 2, NON_MATCH), false),
+            row(LottoMatchResult(countOfMatch = 2), LottoMatchResult(countOfMatch = 2), true),
+            row(LottoMatchResult(countOfMatch = 3), LottoMatchResult(countOfMatch = 3, NON_MATCH), true),
+            row(LottoMatchResult(countOfMatch = 6), LottoMatchResult(countOfMatch = 6, MATCH), true),
+            row(LottoMatchResult(countOfMatch = 6, NON_MATCH), LottoMatchResult(countOfMatch = 6, MATCH), false),
+        ) { result, other, expect ->
+            result.correctMatchResult(otherMatchResult = other) shouldBe expect
+        }
+    }
+
+    "로또 매치 결과의 보너스볼 조건을 알 수 있다." {
+        forAll(
+            row(LottoMatchResult(countOfMatch = 1), false),
+            row(LottoMatchResult(countOfMatch = 2, NON_MATCH), false),
+            row(LottoMatchResult(countOfMatch = 2, MATCH), true),
+        ) { matchResult, expect ->
+            matchResult.hasBonusBallCondition() shouldBe expect
         }
     }
 })
