086a043d4a43b4fda0df66bdd2b6890143daa1ff
김현준 <4whomtbts@gmail.com>
2023-06-15 01:37:28 +0900

    feat(step1): RegexBasedCustomSeparatorStringSplitter 클래스와 테스트 추가

---
+++ src/main/kotlin/string/CustomSeparatorStringSplitter.kt
@@ -0,0 +1,9 @@
+package string
+
+/**
+ * [input] 이 커스텀 구분자를 사용하는 식이면 `true` 를 반환하고
+ * 커스텀 구분자를 사용하지 않는 식이면 `false` 를 반환한다
+ */
+fun interface CustomSeparatorStringSplitter {
+    fun split(input: String): List<Int>?
+}
