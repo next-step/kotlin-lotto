0cee407d33e4a1053cb39c34f38a81e2bc7dbd43
yoons <mioyo2@gmail.com>
2023-06-14 21:52:00 +0900

    bug : 문자 사라진 부분 원복

--- src/test/kotlin/stringCalculator/StringCalculator.kt
+++ src/test/kotlin/stringCalculator/StringCalculator.kt
@@ -14,7 +14,7 @@ class StringCalculator : AnnotationSpec() {
     }
 
     @Test
-    fun ``() {
+    fun `앞의 기본 구분자(쉼표, 콜론) 외에 커스텀 구분자를 지정할 수 있다`() {
         val strings = listOf("//;\n1;2;3", "1,2:3", "1:2:3", "//;\n1,2;3", "//;\n1,2:3")
         strings.forEach {
             StringPlusCalculator.seperate(it) shouldBe listOf(1, 2, 3)
