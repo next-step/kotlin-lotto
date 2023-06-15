e927ea562e45341aa6492171916eeda6a5c117b8
jayden.bin <jayden.bin@kakaocorp.com>
2023-06-15 18:10:51 +0900

    refactor: 테스트 수정

--- src/test/kotlin/cacluator/domain/ExpressionTest.kt
+++ src/test/kotlin/cacluator/domain/ExpressionTest.kt
@@ -5,10 +5,11 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
 class ExpressionTest : BehaviorSpec({
-    given("getSum 메소드") {
-        `when`("유효한 식에 대해") {
+    given("유효한 식이 있다") {
+        val input = "1,2:3,4"
+        `when`("해당문자열의 합을 계산하면") {
             then("합계를 반환한다") {
-                val expression = ExpressionFactory.createExpression("1,2:3,4")
+                val expression = ExpressionFactory.createExpression(input)
                 expression.getSum() shouldBe 10
             }
         }
--- src/test/kotlin/cacluator/domain/TermTest.kt
+++ src/test/kotlin/cacluator/domain/TermTest.kt
@@ -6,22 +6,27 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 
 class TermTest : BehaviorSpec({
-    given("Term 생성시") {
-        `when`("숫자가 아닌 문자가 포함되면") {
+    given("숫자가 아닌 문자가 포함된 문자열이 있다") {
+        val input = "!!1"
+        `when`("해당 문자열로 초기화를 하면") {
             then("예외가 던져진다") {
-                shouldThrow<RuntimeException> { Term("!!1") }
+                shouldThrow<RuntimeException> { Term(input) }
             }
         }
-
-        `when`("음수가 입력되면") {
+    }
+    given("음수인 문자열이 있다") {
+        val input = "-1"
+        `when`("해당 문자열로 초기화를 하면") {
             then("예외가 던져진다") {
-                shouldThrow<RuntimeException> { Term("-1") }
+                shouldThrow<RuntimeException> { Term(input) }
             }
         }
-
+    }
+    given("0이상의 문자열이 있다") {
+        val input = "1"
         `when`("0이상의 숫자가 들어오면") {
             then("정상적으로 생성된다") {
-                shouldNotThrow<Throwable> { Term("1") }
+                shouldNotThrow<Throwable> { Term(input) }
             }
         }
     }
