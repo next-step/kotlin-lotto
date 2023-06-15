35738b4f842de01238bf2fd3214138739e48eb77
chad.yoon <chad.yoon@kakaocorp.com>
2023-06-15 18:27:16 +0900

    test: PositiveNumber 테스트 코드 작성
    - 음수 및 음수로 된 문자열 입력 불가 테스트
    - 숫자가 아닌 문자열 입력 불가 테스트
    - 0 또는 양수 입력 가능 테스트

---
+++ src/test/kotlin/calculator/PositiveNumberTest.kt
@@ -0,0 +1,48 @@
+package calculator
+
+import io.kotest.matchers.shouldBe
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class PositiveNumberTest {
+
+    @DisplayName(value = "음수가 주어지면 RuntimeException 예외 처리를 한다")
+    @ParameterizedTest
+    @ValueSource(ints = [-1])
+    fun notPositiveNumber(number: Int) {
+        Assertions.assertThatExceptionOfType(RuntimeException::class.java)
+            .isThrownBy { PositiveNumber(number) }
+    }
+
+    @DisplayName(value = "음수인 문자열이 RuntimeException 예외 처리를 한다")
+    @ParameterizedTest
+    @ValueSource(strings = ["-1"])
+    fun notPositiveNumberString(numberString: String) {
+        Assertions.assertThatExceptionOfType(RuntimeException::class.java)
+            .isThrownBy { PositiveNumber.of(numberString) }
+    }
+
+    @DisplayName(value = "숫자가 아닌 문자열이 주어지면 RuntimeException 예외 처리를 한다")
+    @ParameterizedTest
+    @ValueSource(strings = ["a", ""])
+    fun notNumber(text: String) {
+        Assertions.assertThatExceptionOfType(RuntimeException::class.java)
+            .isThrownBy { PositiveNumber.of(text) }
+    }
+
+    @DisplayName(value = "0 또는 양수인 문자열이 주어지면 예외가 발생하지 않는다")
+    @ParameterizedTest
+    @ValueSource(strings = ["0", "1"])
+    fun positiveNumberString(text: String) {
+        PositiveNumber.of(text).value shouldBe text.toInt()
+    }
+
+    @DisplayName(value = "0 또는 양수가 주어지면 예외가 발생하지 않는다")
+    @ParameterizedTest
+    @ValueSource(ints = [0, 1])
+    fun positiveNumber(number: Int) {
+        PositiveNumber(number).value shouldBe number
+    }
+}
