85b143e8688210a748bab03945c74fe465233bff
sjparkk <sjparkk0820@naver.com>
2023-06-15 21:08:36 +0900

    fix : input 생성자 캡슐화로 인한 코드 수정

--- src/test/kotlin/calculator/StringAddCalculatorTest.kt
+++ src/test/kotlin/calculator/StringAddCalculatorTest.kt
@@ -15,14 +15,10 @@ import java.util.stream.Stream
 class StringAddCalculatorTest {
     private lateinit var calculator: StringAddCalculator
 
-    @BeforeEach
-    fun setUp() {
-        calculator = StringAddCalculator()
-    }
-
     @ParameterizedTest
     @ValueSource(strings = ["1,2:3"])
     fun `쉼표 또는 콜론을 구분자로 가지는 문자열을 전달 받아 구분자를 기준으로 분리 후 반환한다`(input: String) {
+        calculator = StringAddCalculator(input = input)
         val actual = calculator.split(input)
         actual shouldBe listOf("1", "2", "3")
     }
@@ -30,6 +26,7 @@ class StringAddCalculatorTest {
     @ParameterizedTest
     @ArgumentsSource(StringListArgumentsProvider::class)
     fun `분리 된 리스트의 각 숫자의 합을 구한다`(input: List<String>) {
+        calculator = StringAddCalculator(input = "1,2,3")
         val actual = calculator.stringAdd(input)
         actual shouldBe 6
     }
@@ -37,17 +34,19 @@ class StringAddCalculatorTest {
     @ParameterizedTest
     @ValueSource(strings = [""])
     fun `빈 문자열 또는 null 을 입력할 경우 0을 반환 한다`(input: String?) {
-        val actualEmpty = calculator.calculate(input)
+        calculator = StringAddCalculator(input = input)
+        val actualEmpty = calculator.calculate()
         actualEmpty shouldBe 0
 
-        val actualNull = calculator.calculate(null)
+        val actualNull = calculator.calculate()
         actualNull shouldBe 0
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["3"])
     fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(input: String) {
-        val actual = calculator.calculate(input)
+        calculator = StringAddCalculator(input = input)
+        val actual = calculator.calculate()
         actual shouldBe 3
     }
 
@@ -55,7 +54,8 @@ class StringAddCalculatorTest {
     @ValueSource(strings = ["-3"])
     fun `음수 하나만을 전달할 경우 RuntimeException 예외가 발생한다`(input: String) {
         shouldThrow<RuntimeException> {
-            calculator.calculate(input)
+            calculator = StringAddCalculator(input = input)
+            calculator.calculate()
         }
     }
 
@@ -63,7 +63,8 @@ class StringAddCalculatorTest {
     @ValueSource(strings = ["1,-2:3"])
     fun `전달할 문자열 내 음수가 포함되어 있는 경우 RuntimeException 예외가 발생한다`(input: String) {
         shouldThrow<RuntimeException> {
-            calculator.calculate(input)
+            calculator = StringAddCalculator(input = input)
+            calculator.calculate()
         }
     }
 
@@ -71,6 +72,7 @@ class StringAddCalculatorTest {
     @ValueSource(strings = ["//;\n1;2:3"])
     @DisplayName("문자열 앞부분의 \"//\"와 \"\\n\" 사이에 위치하는 문자를 커스텀 구분자 구한다")
     fun `getCustomDelimiter`(input: String) {
+        calculator = StringAddCalculator(input = input)
         val actual = calculator.getCustomDelimiter(input)
         actual shouldBe ";"
     }
@@ -78,7 +80,8 @@ class StringAddCalculatorTest {
     @ParameterizedTest
     @ValueSource(strings = ["//;\n7;2:1"])
     fun `커스텀 구분자가 있을 시 동일하게 구분자로서의 숫자 분리 후 합을 반환한다`(input: String) {
-        val actual = calculator.calculate(input)
+        calculator = StringAddCalculator(input = input)
+        val actual = calculator.calculate()
         actual shouldBe 10
     }
 
