add2f864d1fc4324ac01e6636a38507a7308967b
peter <peter.shlee@kakaocorp.com>
2023-06-15 18:20:34 +0900

    feat(step2): 구입 금액 입력받기

---
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -0,0 +1,8 @@
+package lotto.controller
+
+import lotto.view.InputView
+
+fun main() {
+    val inputView = InputView()
+    inputView.getPurchaseAmount()
+}
---
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -0,0 +1,15 @@
+package lotto.view
+
+class InputView {
+    fun getPurchaseAmount(): Int {
+        println("구입금액을 입력해 주세요.")
+        return readInt()
+    }
+
+    private tailrec fun readInt(): Int {
+        val enteredInt = readln().toIntOrNull()
+        if (enteredInt != null) return enteredInt
+        println("다시 입력해 주세요.")
+        return readInt()
+    }
+}
