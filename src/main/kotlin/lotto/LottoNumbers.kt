9977b0f5deae1bad7d06f14af499332bdc2ec9ab
nam-sangyun <nsun9505@naver.com>
2023-06-16 02:36:17 +0900

    feature(로또 번호들을 지난 주 당첨번호와 비교해서 총 수익률을 계산할 수 있다)

--- src/main/kotlin/lotto/LottoNumbers.kt
+++ src/main/kotlin/lotto/LottoNumbers.kt
@@ -1,7 +1,17 @@
 package lotto
 
+import lotto.LottoStore.Companion.PURCHASE_AMOUNT_UNIT
+import java.math.BigDecimal
+import java.math.RoundingMode
+
 class LottoNumbers(private val lottoNumbers: List<LottoNumber>) : List<LottoNumber> by lottoNumbers {
     fun makeRankingCountMap(winLottoNumber: LottoNumber): Map<LottoRanking, Int> = lottoNumbers.groupingBy {
         it.getRanking(winLottoNumber)
     }.eachCount()
+
+    fun getRevenueRate(winLottoNumber: LottoNumber): BigDecimal {
+        val totalWinningAmount = lottoNumbers.sumOf { it.getRanking(winLottoNumber).winningAmount }
+        val totalPurchaseAmount = BigDecimal(lottoNumbers.size * PURCHASE_AMOUNT_UNIT)
+        return BigDecimal(totalWinningAmount).divide(totalPurchaseAmount, 2, RoundingMode.DOWN)
+    }
 }
--- src/test/kotlin/lotto/LottoNumbersTest.kt
+++ src/test/kotlin/lotto/LottoNumbersTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 class LottoNumbersTest {
 
@@ -17,5 +18,15 @@ class LottoNumbersTest {
         result[LottoRanking.THREE_RD] shouldBe 1
         result[LottoRanking.FOUR_TH] shouldBe 1
         result[LottoRanking.OUT_OF_RAKING] shouldBe 2
+    }
+
+    @Test
+    fun `로또 번호들을 지난 주 당첨번호와 비교해서 총 수익률을 계산할 수 있다`() {
+        val lottoNumbers = LottoNumbers((1..14).map { LottoNumber((it..it + 5).toList()) })
+        val winLottoNumber = LottoNumber(listOf(17, 18, 19, 20, 21, 22))
+
+        val result = lottoNumbers.getRevenueRate(winLottoNumber)
+
+        result shouldBe BigDecimal("0.35")
     }
 }
