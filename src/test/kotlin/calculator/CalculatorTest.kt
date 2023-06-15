af0d210ea439310a6a442d6e4e49382b6066c18a
jinho <g1063114@naver.com>
2023-06-15 22:47:01 +0900

    feat: 문자열을 , 또는 :로 구분하여 분리 기능 테스트 코드 작성

---
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -0,0 +1,32 @@
+package calculator
+
+import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class CalculatorTest {
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2"])
+    fun `문자열을 , 구분자로 구분한다`(input: String) {
+        val splitData = input.split(",")
+        splitData.size shouldBe 2
+        splitData shouldContainExactly(listOf("1", "2"))
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["3:5"])
+    fun `문자열을 콜론 구분자로 구분한다`(input: String) {
+        val splitData = input.split(":")
+        splitData.size shouldBe 2
+        splitData shouldContainExactly(listOf("3", "5"))
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1,3:5"])
+    fun `문자열을 , 또는 콜론 구분자로 구분한다`(input: String) {
+        val splitData = input.split(",|:".toRegex())
+        splitData.size shouldBe 3
+        splitData shouldContainExactly(listOf("1", "3", "5"))
+    }
+}
