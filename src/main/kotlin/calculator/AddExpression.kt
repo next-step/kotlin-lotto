3bc4dd1cf85cc948cbb1814ae65f5dbf54862c6f
이수민 <sumniy94@gmail.com>
2023-06-15 22:45:09 +0900

    Step1 - 문자열 덧셈 계산기 (#642)
    * chore: delete .gitkeep files
    
    * feat: implement StringAddCalculator
    
    * test: test StringAddCalculator
    
    * test: test AddExpression
    
    * test: test Numbers
    
    ---------
    
    Co-authored-by: turner.lee <turner.lee@kakaocorp.com>

---
+++ src/main/kotlin/calculator/AddExpression.kt
@@ -0,0 +1,30 @@
+package calculator
+
+class AddExpression(text: String) {
+    private val numbers: List<Int>
+
+    init {
+        val delimiter = extractCustomDelimiter(text) ?: DEFAULT_DELIMITER
+        val expression = extractExpression(text) ?: text
+        numbers = expression.split(delimiter.toRegex()).map { it.toInt() }
+    }
+
+    fun getNumbers(): List<Int> {
+        return numbers
+    }
+
+    private fun extractCustomDelimiter(text: String): String? {
+        return EXPRESSION_REGEX.find(text)?.groupValues?.get(DELIMITER_INDEX)
+    }
+
+    private fun extractExpression(text: String): String? {
+        return EXPRESSION_REGEX.find(text)?.groupValues?.get(EXPRESSION_INDEX)
+    }
+
+    companion object {
+        private val EXPRESSION_REGEX = Regex("//(.)\n(.*)")
+        private const val DELIMITER_INDEX = 1
+        private const val EXPRESSION_INDEX = 2
+        private const val DEFAULT_DELIMITER = ",|:"
+    }
+}
---
+++ src/test/kotlin/calculator/AddExpressionTest.kt
@@ -0,0 +1,18 @@
+package calculator
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class AddExpressionTest {
+    @Test
+    fun `AddExpression은 생성 시 입력한 text를 Int List로 파싱한다`() {
+        val addExpression = AddExpression("1,2,3")
+        addExpression.getNumbers() shouldBe listOf(1, 2, 3)
+    }
+
+    @Test
+    fun `잘못된 포맷의 text가 입력될 시 에러가 발생한다`() {
+        shouldThrow<Exception> { AddExpression("error") }
+    }
+}
