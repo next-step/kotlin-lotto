f9501f076eef8caad49b1e822778c2bcbeccc00d
jayden.bin <jayden.bin@kakaocorp.com>
2023-06-15 17:57:53 +0900

    refactor: 접근제어 수정

--- src/main/kotlin/calculator/domain/Term.kt
+++ src/main/kotlin/calculator/domain/Term.kt
@@ -1,16 +1,12 @@
 package calculator.domain
 
 @JvmInline
-value class Term private constructor(val value: Int) {
+value class Term(val value: Int) {
+    constructor(input: String) : this(
+        input.toIntOrNull() ?: throw IllegalArgumentException("숫자가 아닙니다.")
+    )
 
-    constructor(input: String) : this(input.toPositiveOrThrow())
-
-    companion object {
-        @JvmStatic
-        private fun String.toPositiveOrThrow(): Int {
-            val value = toIntOrNull() ?: throw RuntimeException("숫자가 아닙니다.")
-            require(value >= 0) { "음수는 입력할 수 없습니다." }
-            return value
-        }
+    init {
+        require(value >= 0) { "음수는 입력할 수 없습니다." }
     }
 }
