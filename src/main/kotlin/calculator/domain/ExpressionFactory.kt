4c9f3c21951158406e0594d55ba2646492751e51
jayden.bin <jayden.bin@kakaocorp.com>
2023-06-15 17:54:25 +0900

    refactor: 코드 가독성 향상

--- src/main/kotlin/calculator/domain/ExpressionFactory.kt
+++ src/main/kotlin/calculator/domain/ExpressionFactory.kt
@@ -3,14 +3,10 @@ package calculator.domain
 object ExpressionFactory {
     private val DEFAULT_EXPRESSION_TOKEN = listOf("0")
 
-    fun createExpression(input: String?): Expression =
-        parseExpression(
-            if (input.isNullOrEmpty()) {
-                DEFAULT_EXPRESSION_TOKEN
-            } else {
-                Tokenizer.getTokens(input)
-            }
-        )
+    fun createExpression(input: String?): Expression {
+        val target: List<String> = if (input.isNullOrEmpty()) DEFAULT_EXPRESSION_TOKEN else Tokenizer.getTokens(input)
+        return parseExpression(target)
+    }
 
     private fun parseExpression(terms: List<String>): Expression {
         return Expression(terms.map { Term(it) })
