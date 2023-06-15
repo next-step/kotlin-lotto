3220ffdb28c011b655b60552fbbe4c1407df4f4b
yoo-jaein <y_jaein@naver.com>
2023-06-16 02:23:45 +0900

    feat: 지난 주 당첨 번호를 입력하는 InputView 함수 추가

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,8 +1,16 @@
 package lotto.view
 
+import lotto.domain.WinningLotto
+
 class InputView {
     fun inputPurchasePrice(): Int {
         println("구입금액을 입력해 주세요.")
         return readln().toInt()
+    }
+
+    fun inputLastWeekLottoNumbers(): WinningLotto {
+        println("\n지난 주 당첨 번호를 입력해 주세요.")
+        val numbers = readln().split(", ").map { it.toInt() }
+        return WinningLotto(numbers)
     }
 }
