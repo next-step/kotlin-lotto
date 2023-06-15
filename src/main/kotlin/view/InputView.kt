1398916fa86cc2bde8becd7ed0bf097dc7047826
박세준 <sjpark@selab.co.kr>
2023-06-15 15:46:32 +0900

    feat: 클라이언트가 입력한 값은 숫자이여야 한다.

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -2,9 +2,11 @@ package view
 
 class InputView(private val inputReader: InputReader) {
 
-    fun enterTheMoney() {
+    fun enterTheMoney(): Int {
         println("구입금액을 입력해 주세요.")
         val money = inputReader.raedLine()
+        require(money.toIntOrNull() != null) { "구입 금액이 올바르지 않습니다 : $money" }
         println(money)
+        return money.toInt()
     }
 }
