e45eeda8e20f1662a2cd2b6fb3c5955a926902a0
goofy-919 <goofy.919@kakaocorp.com>
2023-06-15 14:26:40 +0900

    refactor: Calculator::add 리팩터링

--- src/main/kotlin/com/nextstep/second/calculator/Calculator.kt
+++ src/main/kotlin/com/nextstep/second/calculator/Calculator.kt
@@ -2,6 +2,6 @@ package com.nextstep.second.calculator
 
 object Calculator {
     fun add(numList: List<Int>): Int {
-        return numList.fold(0) { acc, i -> acc + i }
+        return numList.sum()
     }
 }
