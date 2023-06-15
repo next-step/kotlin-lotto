6cec59247ce47df3fc7c403d01caf05263a1d481
waveofmymind <sjun0913@gmail.com>
2023-06-15 11:39:41 +0900

    Refactor: 주석 추가 및 테스트 클래스 네임 변경

--- src/test/kotlin/calculator/AddCalculatorTest.kt
+++ src/test/kotlin/calculator/AddCalculatorTest.kt
@@ -4,30 +4,33 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-class ExpressionTest : StringSpec({
+class AddCalculatorTest : StringSpec({
 
-    "null 인 값을 입력하면 0을 반환한다." {
+    "null 인 값을 입력하면 0을 반환한다" {
         // given
         val input = null
         // when
-        val result = Expression(input).numbers
+        val expression = ExpressionFactory.createExpression(input)
         // then
-        result shouldBe listOf(0)
+        AddCalculator(expression).add() shouldBe 0
     }
 
     "빈 문자열이 들어왔을 때 0을 반환한다." {
         // given
         val input = ""
         // when
-        val result = Expression(input).numbers
+        val expression = ExpressionFactory.createExpression(input)
         // then
-        result shouldBe listOf(0)
+        AddCalculator(expression).add() shouldBe 0
     }
 
     "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다." {
+        // given
         val input = "1"
-        val result = Expression(input).numbers
-        result shouldBe listOf(1)
+        // when
+        val expression = ExpressionFactory.createExpression(input)
+        // then
+        AddCalculator(expression).add() shouldBe 1
     }
 
     "음수를 전달할 경우 RuntimeException 예외가 발생해야 한다." {
@@ -35,26 +38,26 @@ class ExpressionTest : StringSpec({
         val input = "-1"
         // when & then
         val exception = shouldThrow<RuntimeException> {
-            Expression(input)
+            ExpressionFactory.createExpression(input)
         }
         exception.message shouldBe "음수는 입력할 수 없습니다."
     }
 
-    "문자열을 입력하면 쉼표와 콜론을 기준으로 숫자를 분리한다" {
+    "분리된 숫자의 합을 반환한다." {
         // given
-        val input = "1,2"
+        val input = "1,2:3"
         // when
-        val result = Expression(input)
+        val expression = ExpressionFactory.createExpression(input)
         // then
-        result.numbers shouldBe listOf(1, 2)
+        AddCalculator(expression).add() shouldBe 6
     }
 
     "기본 구분자 외에 커스텀 구분자를 지정할 수 있다." {
         // given
-        val input = "//;\n1;2;3"
+        val input = "//;\n1;2;4"
         // when
-        val result = Expression(input)
+        val expression = ExpressionFactory.createExpression(input)
         // then
-        result.numbers shouldBe listOf(1, 2, 3)
+        AddCalculator(expression).add() shouldBe 7
     }
 })
