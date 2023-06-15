eb782e6db147da323f4ff4e4a8b4d24e625fb38a
yoo-jaein <y_jaein@naver.com>
2023-06-15 10:57:14 +0900

    test: LottoTest 테스트 클래스 추가

---
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -0,0 +1,35 @@
+package lotto
+
+import org.junit.jupiter.api.Test
+
+class LottoTest {
+    @Test
+    fun `로또를 구입할 수 있다`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `로또 구입 금액을 입력하면 구입 금액에 해당하는 로또가 발급된다`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `로또 1장의 가격은 1000원이다`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `지난 주 당첨 번호를 입력할 수 있다`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `당첨 번호와 3개, 4개, 5개, 6개가 일치한 경우를 통계로 보여준다`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `총 수익률을 보여준다`() {
+        TODO("Not yet implemented")
+    }
+}
