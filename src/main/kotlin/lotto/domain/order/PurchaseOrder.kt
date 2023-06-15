5043c24200bc2de066ec34106ee6dcd4090d1e62
ljw0829 <olo_jinwon@naver.com>
2023-06-15 23:51:08 +0900

    refactor: until infix 대신 컬렉션 함수 사용

--- src/main/kotlin/lotto/domain/order/PurchaseOrder.kt
+++ src/main/kotlin/lotto/domain/order/PurchaseOrder.kt
@@ -8,15 +8,10 @@ class PurchaseOrder(amount: Int) {
 
     private val maximumPurchaseQuantity: Int = (amount / LotteryAdaptor.LOTTERY_PRICE).toInt()
 
-    val purchasedLotteries: PurchasedLotteries = (ZERO until maximumPurchaseQuantity)
-        .map { LotteryGenerator.draw() }
+    val purchasedLotteries: PurchasedLotteries = List(maximumPurchaseQuantity) { LotteryGenerator.draw() }
         .run(::PurchasedLotteries)
 
     constructor(amountText: String) : this(
         amount = amountText.toInt(),
     )
-
-    companion object {
-        private const val ZERO: Int = 0
-    }
 }
