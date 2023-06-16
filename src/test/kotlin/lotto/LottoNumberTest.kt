166b2896141424a479e2e4882ae647e4dc8b4e7a
nam-sangyun <nsun9505@naver.comm>
2023-06-16 11:49:01 +0900

    refactor(테스트 데이터 수정)

--- src/test/kotlin/lotto/LottoNumberTest.kt
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -33,9 +33,9 @@ class LottoNumberTest {
 
         @JvmStatic
         fun makeLottoNumber(): List<Arguments> = listOf(
-            Arguments.of(listOf(0, 2, 3, 4, 5)),
-            Arguments.of(listOf(1, 2, 3, 4, 46)),
-            Arguments.of(listOf(111, 22, 33, 44, 45)),
+            Arguments.of(listOf(1, 2, 3, 4, 5, 6, 7)),
+            Arguments.of(listOf(1, 2, 3, 4, 5, 46)),
+            Arguments.of(listOf(111, 22, 33, 44, 45, 1)),
         )
 
         @JvmStatic
