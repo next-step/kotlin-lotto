2895376a23ca7d6962c1f05f38076f0f87638171
sjparkk <sjparkk0820@naver.com>
2023-06-16 21:13:02 +0900

    feat : 로또 개수 validation 추가

--- src/main/kotlin/lotto/LottoChecker.kt
+++ src/main/kotlin/lotto/LottoChecker.kt
@@ -4,19 +4,25 @@ class LottoChecker {
 
     fun lottoCheck(winningNumber: String, lottoBundle: List<List<Int>>): List<Int> {
 
-        val numberList = winningNumber.replace("\\s".toRegex(), "").split(",").map { it.toInt() }
+        val numbers = winningNumber.replace("\\s".toRegex(), "").split(",").map { it.toInt() }
+
+        lottoInputNumberValidation(numbers)
 
         val result = mutableListOf<Int>()
         lottoBundle.forEach { lotto ->
             var count = 0
             lotto.forEach { number ->
-                if (numberList.contains(number)) {
+                if (numbers.contains(number)) {
                     count++
                 }
             }
             result.add(count)
         }
         return result.toList()
+    }
+
+    private fun lottoInputNumberValidation(numbers: List<Int>) {
+        require(numbers.size == COLLECT_LOTTO_SIZE) { "로또 입력 숫자는 총 6개여야 합니다" }
     }
 
     fun winningMoneyCheck(collectCounts: List<Int>): Int {
@@ -33,5 +39,6 @@ class LottoChecker {
 
     companion object {
         private const val NONE_PRIZE_MONEY = 0
+        private const val COLLECT_LOTTO_SIZE = 6
     }
 }
