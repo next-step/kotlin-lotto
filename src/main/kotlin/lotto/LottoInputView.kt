6833fcb9a2ccdfe6dc27897cf7691ba0b3749779
nam-sangyun <nsun9505@naver.com>
2023-06-17 02:31:38 +0900

    refactor(입력 재사용 가능하도록 변경)

--- src/main/kotlin/lotto/LottoInputView.kt
+++ src/main/kotlin/lotto/LottoInputView.kt
@@ -2,24 +2,22 @@ package lotto
 
 class LottoInputView {
 
-    fun readPurchaseAmountFromConsole(message: String = DEFAULT_PURCHASE_AMOUNT_READ_MESSAGE): Int {
+    fun readInt(message: String): Int {
         println(message)
         return readln().toIntOrNull() ?: 0
     }
 
-    fun readLastWeekWinningLottoNumberFromConsole(message: String = DEFAULT_LAST_WEEK_WINNING_NUMBER_READ_MESSAGE): WinningLottoNumber {
+    fun readLottoNumbers(message: String): LottoNumber {
         println(message)
         val input = readln()
         val numbers = input.split(DEFAULT_LAST_WEEK_WINNING_NUMBER_DELIMITER)
             .map { it.trim().toIntOrNull() ?: 0 }
             .sorted()
             .toList()
-        return WinningLottoNumber(LottoNumber(numbers), 1)
+        return LottoNumber(numbers)
     }
 
     companion object {
         const val DEFAULT_LAST_WEEK_WINNING_NUMBER_DELIMITER = ","
-        const val DEFAULT_PURCHASE_AMOUNT_READ_MESSAGE = "구입금액을 입력해 주세요."
-        const val DEFAULT_LAST_WEEK_WINNING_NUMBER_READ_MESSAGE = "구입금액을 입력해 주세요."
     }
 }
--- src/main/kotlin/lotto/LottoMain.kt
+++ src/main/kotlin/lotto/LottoMain.kt
@@ -5,13 +5,15 @@ fun main() {
     val lottoResultView = LottoResultView()
     val lottoStore = LottoStore()
 
-    val purchaseAmount = lottoInputView.readPurchaseAmountFromConsole()
+    val purchaseAmount = lottoInputView.readInt(message = "구입금액을 입력해 주세요.")
     val lottoNumbers = lottoStore.purchase(purchaseAmount)
     lottoResultView.printPurchasedLottoNumbers(lottoNumbers)
 
-    val lastWinLottoNumber = lottoInputView.readLastWeekWinningLottoNumberFromConsole()
+    val lastWinLottoNumber = lottoInputView.readLottoNumbers(message = "지난 주 당첨 번호를 입력해 주세요.")
+    val bonusBallNumber = lottoInputView.readInt(message = "보너스 볼을 입력해 주세요.")
+    val winningLottoNumber = WinningLottoNumber(winningNumber = lastWinLottoNumber, bonusBallNumber = bonusBallNumber)
 
-    val rankingCountMap = lastWinLottoNumber.makeRankingCountMap(lottoNumbers)
-    val totalRevenueRate = lastWinLottoNumber.getRevenueRate(lottoNumbers)
+    val rankingCountMap = winningLottoNumber.makeRankingCountMap(lottoNumbers)
+    val totalRevenueRate = winningLottoNumber.getRevenueRate(lottoNumbers)
     lottoResultView.printLottoStatistics(rankingCountMap = rankingCountMap, totalRevenueRate = totalRevenueRate)
 }
