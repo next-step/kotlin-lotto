68ee1bbdf1ca14b039c21f08f91a734912eeef62
leehyunjae <leehyunjae@woowahan.com>
2023-06-17 01:18:13 +0900

    [STEP2] LottoPrize, LottoResult, WinningNumbers 클래스 작성

---
+++ src/main/kotlin/lotto/domain/LottosResult.kt
@@ -0,0 +1,8 @@
+package lotto.domain
+
+class LottosResult(
+    val numberOfFirst: Int = 0,
+    val numberOfThird: Int = 0,
+    val numberOfFourth: Int = 0,
+    val numberOfFifth: Int = 0,
+)
---
+++ src/main/kotlin/lotto/domain/WinningNumbers.kt
@@ -0,0 +1,3 @@
+package lotto.domain
+
+typealias WinningNumbers = List<Int>
