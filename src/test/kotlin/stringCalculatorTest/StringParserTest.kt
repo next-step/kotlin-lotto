60d1182381bf78973a95c37de657a19a1ef77ce0
kth <sksskaw@naver.com>
2023-06-17 00:32:45 +0900

    문자열 계산기 파서 구현, 커스텀 구분자 추가 기능 테스트 작성
    앞의 기본 구분자(쉼표, 콜론) 외에 커스텀 구분자를 지정할 수 있다.
    커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용한다.

--- src/test/kotlin/stringCalculatorTest/StringParserTest.kt
+++ src/test/kotlin/stringCalculatorTest/StringParserTest.kt
@@ -1,6 +1,7 @@
 package stringCalculatorTest
 
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import stringCalculator.StringParser
 
@@ -10,5 +11,13 @@ class StringParserTest {
         val inputString = "1:5,9"
         val stringParser = StringParser()
         stringParser.parse(inputString) shouldBe listOf("1","5","9")
+    }
+
+    @Test
+    @DisplayName("//와 \n 사이에 위치하는 문자를 커스텀 구분자로 사용한다.")
+    fun `조건에 맞는 커스텀 구분자를 추가할 수 있다`() {
+        val inputString = "//???\n1:5,9???10"
+        val stringParser = StringParser()
+        stringParser.parse(inputString) shouldBe listOf("1","5","9","10")
     }
 }
