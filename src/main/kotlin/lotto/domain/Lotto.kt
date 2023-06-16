d7778e7b9a8bea9a0e76cd5399dfe42013db5cdb
waveofmymind <sjun0913@gmail.com>
2023-06-17 00:54:02 +0900

    Feat: 사이즈 반환 메서드 추가 및 상수 네이밍 변경

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -2,11 +2,15 @@ package lotto.domain
 
 class Lotto private constructor(val lottoList: List<LottoNumbers>) {
 
+    fun getSize(): Int {
+        return lottoList.size
+    }
+
     companion object {
-        private const val LOTTO_PRICE = 1000
+        const val PER_LOTTO_PRICE = 1000
 
         fun of(input: Int): Lotto {
-            val count = input / LOTTO_PRICE
+            val count = input / PER_LOTTO_PRICE
             val lottoList = List(count) { LottoNumbers.create() }
             return Lotto(lottoList)
         }
