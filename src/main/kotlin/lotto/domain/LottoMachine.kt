4e35ce078171716afd169e8e7212d434b078b889
minhyukseul <mioyo2@gmail.com>
2023-06-16 10:19:28 +0900

    feat: 로또 구입 금액을 입력하면 구입 금액에 해당하는 로또를 발급

--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -1,4 +1,21 @@
 package lotto.domain
 
 object LottoMachine {
+
+    private val LOTTO_RANGE_NUMBERS = (1..45).map { it }
+    const val LOTTO_NUMBER = 6
+
+    var lottoCount = 0
+        private set
+
+    var buyedLotto = mutableListOf<List<Int>>()
+        private set
+
+    fun buyLotto(amount: Int) {
+        lottoCount = amount/1000
+
+        repeat(lottoCount) {
+            buyedLotto.add(LOTTO_RANGE_NUMBERS.shuffled().take(LOTTO_NUMBER))
+        }
+    }
 }
--- src/test/kotlin/lotto/domain/LottoMachineTest.kt
+++ src/test/kotlin/lotto/domain/LottoMachineTest.kt
@@ -1,26 +1,19 @@
 package lotto.domain
 
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.matchers.shouldBe
-import lotto.InputValidator
 
 class LottoMachineTest : AnnotationSpec() {
 
     @Test
-    fun `구매금액은 숫자로 구성되어야 한다`() {
+    fun `로또 구입 금액을 입력하면 구입 금액에 해당하는 로또를 발급해야 한다`() {
 
-        InputValidator.parseAmount("14000") shouldBe 14000
-
-        shouldThrow<IllegalArgumentException> {
-            InputValidator.parseAmount("abc")
+        LottoMachine.buyLotto(14000)
+        LottoMachine.lottoCount shouldBe 14
+        LottoMachine.buyedLotto.size shouldBe 14
+        LottoMachine.buyedLotto.forEach {
+            it.size shouldBe 6
         }
     }
 
-    @Test
-    fun `로또 구매를 할 수 있는지를 체크한다`() {
-
-        InputValidator.canBuyLotto(1000) shouldBe true
-        InputValidator.canBuyLotto(-15000) shouldBe false
-    }
 }
