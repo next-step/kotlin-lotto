a794ab5efaca0479229f9767a2f90d7a6299a056
hyunju-seo <hyunju6889@naver.com>
2023-06-16 20:44:29 +0900

    refactor: 불필요한 코드 지우기

--- src/main/kotlin/stringcalculator/parser/RegexExpressionParser.kt
+++ src/main/kotlin/stringcalculator/parser/RegexExpressionParser.kt
@@ -10,17 +10,12 @@ object RegexExpressionParser : ExpressionParser {
     override fun getOperands(input: String): List<Operand> {
         val (delimiter, finalInput) = splitCustomDelimiter(input)
         val finalDelimiters = delimiter?.let { listOf(it) + DEFAULT_DELIMITERS } ?: DEFAULT_DELIMITERS
-        println(finalDelimiters)
-        println(finalInput)
-        println()
-        println()
         return extractOperands(finalDelimiters, finalInput)
     }
 
     private fun splitCustomDelimiter(input: String): Pair<String?, String> {
         val matchResult = REGEX.find(input)
         if (matchResult !== null) {
-            println("notnull")
             val (delimiter, finalInput) = matchResult.destructured
             return delimiter to finalInput
         }
