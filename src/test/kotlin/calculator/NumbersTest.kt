d7462ca30bf25179b761787ca506ba850fd2501d
turner.lee <turner.lee@kakaocorp.com>
2023-06-16 09:25:25 +0900

    test: Numbers 테스트에서 발생한 에러의 메시지도 검증하도록 수정

--- src/test/kotlin/calculator/NumbersTest.kt
+++ src/test/kotlin/calculator/NumbersTest.kt
@@ -1,11 +1,13 @@
 package calculator
 
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.throwable.shouldHaveMessage
 import org.junit.jupiter.api.Test
 
 class NumbersTest {
     @Test
     fun `Numbers에는 음수가 포함될 수 없다`() {
-        shouldThrow<RuntimeException> { Numbers(listOf(-1,1,2,3)) }
+        shouldThrow<IllegalArgumentException> { Numbers(listOf(-1,1,2,3)) }
+            .shouldHaveMessage("음수가 전달되었습니다.")
     }
 }
