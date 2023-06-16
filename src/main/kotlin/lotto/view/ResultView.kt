ecf5dd09de272c7f9cc913179486b5973e2d08f1
turner.lee <turner.lee@kakaocorp.com>
2023-06-16 18:40:14 +0900

    feat: 당첨 통계 출력 구현

--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -1,11 +1,29 @@
 package lotto.view
 
 import lotto.LottoGame
+import lotto.LottoGameStatistics
 import lotto.LottoNumbers
+import lotto.Rank
 
 object ResultView {
     fun printAllLottoNumbers(lottoGame: LottoGame) {
         lottoGame.lottoNumbers.forEach { printLottoNumbers(it) }
+    }
+
+    fun printLottoResultStatistics(statistics: LottoGameStatistics) {
+        println("당첨 통계")
+        println("---------")
+
+        val sortedMap = statistics.result.toSortedMap(compareByDescending { statistics.result[it] })
+        sortedMap.forEach { printLottoResult(it.key, it.value) }
+
+        println("총 수익률은 ${statistics.roi}입니다.")
+    }
+
+    fun printLottoResult(rank: Rank, count: Int) {
+        if (rank != Rank.LOSE) {
+            println("${rank.matchedCount}개 일치 (${rank.reward}원) - ${count}개")
+        }
     }
 
     private fun printLottoNumbers(lottoNumbers: LottoNumbers) {
