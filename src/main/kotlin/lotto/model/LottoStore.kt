36c829cdc31a4eb3d9d4b4928af9d68d07fff16a
devyonghee <devyonghee@gmail.com>
2023-06-17 04:28:50 +0900

    feat(LottoStore): 로또 상점 수동 티켓 발급 추가

--- src/main/kotlin/lotto/model/LottoStore.kt
+++ src/main/kotlin/lotto/model/LottoStore.kt
@@ -1,16 +1,47 @@
 package lotto.model
 
-data class LottoStore(val ticketStorage: LottoTicketStorage, val price: Long) {
+data class LottoStore(
+    private val mainLottoTicketStorage: LottoTicketStorage,
+    private val subLottoTicketStorage: LottoTicketStorage,
+    private val price: Long,
+) {
 
     init {
         require(MIN_PRICE <= price) { "price must be greater than $MIN_PRICE. but provided price(`$price`)" }
     }
 
-    infix fun purchaseLottoTicketsBy(money: Long): PurchasedLottoTickets {
-        return generateSequence { ticketStorage.lottoTicket }
-            .take((money / price).toInt())
-            .toList()
-            .let { PurchasedLottoTickets(it, price) }
+    fun purchaseLottoTicketsBy(mainBuyCount: Int, money: Long): PurchasedLottoTickets {
+        val availableBuyCount: Int = (money / price).toInt()
+        validateBuyCount(availableBuyCount, mainBuyCount)
+
+        return PurchasedLottoTickets(
+            tickets = lottoTickets(mainLottoTicketStorage, mainBuyCount)
+                    + lottoTickets(subLottoTicketStorage, availableBuyCount - mainBuyCount),
+            pricePerTicket = price
+        )
+    }
+
+    private fun lottoTickets(storage: LottoTicketStorage, buyCount: Int): Collection<LottoTicket> {
+        validateStorageRemainCount(storage, buyCount)
+        return storage lottoTicketsBy buyCount
+    }
+
+    private fun validateBuyCount(availableBuyCount: Int, buyCount: Int) {
+        if (availableBuyCount < buyCount) {
+            throw IllegalArgumentException(
+                "buyCount must be greater than or equal to primaryBuyCount. " +
+                        "availableBuyCount(`$availableBuyCount`), buyCount(`$buyCount`)"
+            )
+        }
+    }
+
+    private fun validateStorageRemainCount(storage: LottoTicketStorage, primaryBuyCount: Int) {
+        if (storage hasCountLessThan primaryBuyCount) {
+            throw IllegalStateException(
+                "primaryStorage has less than butCount. " +
+                        "primaryStorage($storage), primaryBuyCount(`$primaryBuyCount`)"
+            )
+        }
     }
 
     companion object {
--- src/test/kotlin/lotto/model/LottoTicketTest.kt
+++ src/test/kotlin/lotto/model/LottoTicketTest.kt
@@ -1,44 +1,52 @@
 package lotto.model
 
+import io.kotest.assertions.assertSoftly
 import io.kotest.assertions.throwables.shouldNotThrowAny
-import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.DisplayName
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
+import lotto.model.LottoTicket.Companion.automaticSize
+import lotto.model.LottoTicket.Companion.manualSize
 
 @DisplayName("로또 티켓")
 class LottoTicketTest : StringSpec({
 
-    "로또 번호는 6개" {
+    "로또와 티켓 타입으로 생성" {
         shouldNotThrowAny {
-            LottoTicket((1..6).map(::LottoNumber).toSet())
+            LottoTicket(ONE_TO_SIX_LOTTO, TicketType.AUTOMATIC)
         }
     }
 
-    "로또 번호가 6개가 아니면 예외 발생" {
-        shouldThrowExactly<IllegalArgumentException> {
-            LottoTicket((1..5).map(::LottoNumber).toSet())
-        }
+    "로또와 일치하는 개수 반환" {
+        LottoTicket(ONE_TO_SIX_LOTTO, TicketType.AUTOMATIC) matchedCountBy ONE_TO_SIX_LOTTO shouldBe 6
     }
 
-    "1 ~ 6 로또 티켓의 일치하는 개수 반환" {
-        (1..7).map {
-            LottoTicket((it..it + 5).map(::LottoNumber).toSet()) to (7 - it)
-        }.forAll {
-            ONE_TO_SIX_LOTTO_TICKET matchedCountBy it.first shouldBe it.second
-        }
-    }
-
-    "1 ~ 6 로또 티켓의 로또 번호 포함 여부 확인" {
+    "로또 티켓의 로또 번호 포함 여부 확인" {
         listOf(
             LottoNumber(1) to true,
             LottoNumber(6) to true,
             LottoNumber(7) to false,
         ).forAll {
-            (it.first in ONE_TO_SIX_LOTTO_TICKET) shouldBe it.second
+            (it.first in LottoTicket(ONE_TO_SIX_LOTTO, TicketType.AUTOMATIC)) shouldBe it.second
+        }
+    }
+
+    "수동 로또 개수" {
+        // given
+        val twoManualThreeAutoMatic: Collection<LottoTicket> = listOf(
+            LottoTicket(ONE_TO_SIX_LOTTO, TicketType.MANUAL),
+            LottoTicket(ONE_TO_SIX_LOTTO, TicketType.MANUAL),
+            LottoTicket(ONE_TO_SIX_LOTTO, TicketType.AUTOMATIC),
+            LottoTicket(ONE_TO_SIX_LOTTO, TicketType.AUTOMATIC),
+            LottoTicket(ONE_TO_SIX_LOTTO, TicketType.AUTOMATIC),
+        )
+        // when & then
+        assertSoftly(twoManualThreeAutoMatic) {
+            manualSize shouldBe 2
+            automaticSize shouldBe 3
         }
     }
 })
 
-val ONE_TO_SIX_LOTTO_TICKET = LottoTicket((1..6).map(::LottoNumber).toSet())
+val ONE_TO_SIX_AUTO_LOTTO_TICKET = LottoTicket(ONE_TO_SIX_LOTTO, TicketType.AUTOMATIC)
