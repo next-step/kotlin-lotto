dbbdbd3685dd0f3487ad8acc0a7d8bb4a7ac78d8
kth <sksskaw@naver.com>
2023-06-16 21:48:24 +0900

    문자열 계산기 파서 테스트 코드 작성
    입력한 문자열을 쉼표 또는 구분자로 문자열을 분리한다.

---
+++ src/test/kotlin/stringCalculatorTest/StringParserTest.kt
@@ -0,0 +1,13 @@
+package stringCalculatorTest
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class StringParserTest {
+    @Test
+    fun `쉼표 또는 구분자로 문자열을 분리한다`() {
+        val inputString = "1:5,9"
+        val stringParser = StringParser()
+        stringParser.parse(inputString) shouldBe listOf("1","5","9")
+    }
+}
