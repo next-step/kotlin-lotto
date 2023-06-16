ab043377a08c3c40b63e0cd24b0e7492c3688af4
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-16 12:23:52 +0900

    feat: 보너스 볼 입력기능 추가

--- src/main/kotlin/lottery/Application.kt
+++ src/main/kotlin/lottery/Application.kt
@@ -4,6 +4,7 @@ import lottery.domain.Money
 import lottery.domain.Wallet
 import lottery.domain.lottery.Lottery
 import lottery.domain.lottery.generator.RandomLotteryGenerator
+import lottery.view.inputBonusLotteryNumber
 import lottery.view.inputPurchaseMoney
 import lottery.view.inputWinningLottery
 import lottery.view.printLottoResult
@@ -14,6 +15,7 @@ fun main() {
     val purchaseLotteries = wallet.purchaseLotteries(RandomLotteryGenerator)
     printPurchaseLotteries(purchaseLotteries)
     val winningLottery = Lottery.from(inputWinningLottery())
+    inputBonusLotteryNumber()
 
     val lottoResult = wallet.calculateLotteryResult(winningLottery)
     printLottoResult(lottoResult)
