134a1e9e66e8e7634744b699336adef06f4bbd30
sjparkk <sjparkk0820@naver.com>
2023-06-17 23:16:36 +0900

    fix : 테스트 클래스명 수정

--- src/test/kotlin/lotto/LottoGeneratorTest.kt
+++ src/test/kotlin/lotto/LottoGeneratorTest.kt
@@ -4,7 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class LottoTest {
+class LottoGeneratorTest {
 
     private lateinit var lotto: Lotto
 
