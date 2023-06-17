17fd73ce69747c777dee18b0f27d7e9efb741212
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-06-17 14:50:34 +0900

    feat: 보너스 볼이 입력된다, 보너스 볼은 1에서 45 사이의 값이다.

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -4,16 +4,33 @@ import util.Separator
 
 class InputView(private val inputReader: InputReader) {
 
-    fun enterMoney(): Int {
+    fun buyLotto(): Int {
+        val money = enterMoney()
+        require(money > 1000) { "돈이 부족합니다." }
+
+        val lottoSize = money / 1000
+        println("${lottoSize}개를 구매했습니다.")
+
+        return lottoSize
+    }
+
+    private fun enterMoney(): Int {
         println("구입금액을 입력해 주세요.")
         val money = inputReader.raedLine()
         require(money.toIntOrNull() != null) { "구입 금액이 올바르지 않습니다 : $money" }
         return money.toInt()
     }
 
-    fun registerWinningNums(): Set<Int> {
+    fun registerWinningNums(): List<Int> {
         println("\n지난 주 당첨 번호를 입력해 주세요.")
         val winningNums = inputReader.raedLine()
-        return Separator.extractIntegers(winningNums).toSet()
+        return Separator.extractIntegers(winningNums)
+    }
+
+    fun registerBonusBall(): Int {
+        println("보너스 볼을 입력해 주세요.")
+        val bonusBall = inputReader.raedLine().toIntOrNull()
+        require(bonusBall != null && bonusBall in 1..45) { "보너스 볼이 올바르지 않습니다 : $bonusBall" }
+        return bonusBall.toInt()
     }
 }
