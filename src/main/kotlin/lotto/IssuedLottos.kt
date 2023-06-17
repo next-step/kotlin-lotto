982f48655c175485d0ae4ad2d984bd8dc493b962
maenguin-h <maenguin.h@kakaocorp.com>
2023-06-17 15:27:02 +0900

    step2 : 로또 당첨 통계와 구입 금액을 통해 로또의 수익률 구하기

--- src/main/kotlin/lotto/IssuedLottos.kt
+++ src/main/kotlin/lotto/IssuedLottos.kt
@@ -1,7 +1,7 @@
 package lotto
 
 /**
- * 발급받은 로또를 표현하는 일급 컬렉션 입니다.
+ * ### 발급받은 로또를 표현하는 일급 컬렉션 입니다.
  *
  * 발급받은 로또가 다른 로또 번호와 얼마나 일치하는지 검사할 수 있습니다.
  */
@@ -10,12 +10,12 @@ class IssuedLottos(val lottos: List<Lotto>) {
         require(lottos.isNotEmpty()) { "lottos must not be empty" }
     }
 
-    fun check(winningLotto: Lotto): LottoMatchResult {
+    fun check(winningLotto: Lotto): LottoMatchStat {
         val matchCountGroup = lottos.map { it.match(winningLotto) }
             .groupBy { matchCount ->
                 matchCount
             }
-        return LottoMatchResult(
+        return LottoMatchStat(
             threeMatchCount = matchCountGroup.getOrDefault(3, listOf()).size,
             fourMatchCount = matchCountGroup.getOrDefault(4, listOf()).size,
             fiveMatchCount = matchCountGroup.getOrDefault(5, listOf()).size,
@@ -24,9 +24,22 @@ class IssuedLottos(val lottos: List<Lotto>) {
     }
 }
 
-data class LottoMatchResult(
+/**
+ * ### 로또의 당첨 통계를 표현하는 클래스 입니다.
+ *
+ * 당첨 통계를 토대로 구입 금액 대비 수익률을 계산할 수 있습니다.
+ */
+data class LottoMatchStat(
     val threeMatchCount: Int,
     val fourMatchCount: Int,
     val fiveMatchCount: Int,
     val sixMatchCount: Int,
-)
+) {
+    fun calculateEarningsRate(seedMoney: Int): Double {
+        val winningAmount = threeMatchCount * 5_000 +
+            fourMatchCount * 50_000 +
+            fiveMatchCount * 1_500_000 +
+            sixMatchCount * 2_000_000_000
+        return winningAmount.toDouble() / seedMoney
+    }
+}
---
+++ src/test/kotlin/lotto/LottoMatchStatTest.kt
@@ -0,0 +1,18 @@
+package lotto
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+internal class LottoMatchStatTest {
+    @Test
+    internal fun `로또 당첨 통계와 구입 금액을 통해 로또의 수익률을 구할 수 있어야한다`() {
+        val sut = LottoMatchStat(
+            threeMatchCount = 1,
+            fourMatchCount = 0,
+            fiveMatchCount = 0,
+            sixMatchCount = 0
+        )
+        val actualEarningsRate = sut.calculateEarningsRate(10000)
+        actualEarningsRate shouldBe 0.5
+    }
+}
