e3e8a0ebab690163f58cd6114c5f1e947e08de86
고영경(Yeonggyeong Go)/커머스플랫폼개발팀/SKP <yg.go@sk.com>
2023-06-18 01:00:55 +0900

    문자덧셈계산기 - 코드리뷰 반영

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -1,8 +1,9 @@
 package calculator
 
 object StringAddCalculator {
-    fun sumByExpression(inputExpression: String?) = if (inputExpression.isNullOrEmpty()) 0
-    else NumberExtractor().extractNumbers(inputExpression).sumOf { target -> toInt(target) }
+    fun sumByExpression(inputExpression: String?) =
+        if (inputExpression.isNullOrEmpty()) 0
+        else NumberExtractor().extractNumbers(inputExpression).sumOf { target -> toInt(target) }
 
     private fun toInt(element: String): Int {
         check(element.toInt() >= 0) { "더할 숫자는 모두 양수로 입력해야합니다" }
