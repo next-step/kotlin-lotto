0da1cd3c311e47812fc683ef317c93f9e4560b98
neity16 <neity16@daum.net>
2023-06-18 00:35:30 +0900

    [step2] refactor: 당첨 통계 출력 코드 중복 제거

--- src/main/kotlin/lotto/view/output/SixFortyFiveResultOutputView.kt
+++ src/main/kotlin/lotto/view/output/SixFortyFiveResultOutputView.kt
@@ -11,14 +11,11 @@ class SixFortyFiveResultOutputView(lottoList: List<SixFortyFiveLotto>, winningVa
         val totalResultPrice =
             lottoResultList.reduceIndexed { index, acc, count -> acc + count * SixFortyFiveLottoStore.LOTTO_WINNING_PRICE_MAP[index] }
         val totalPurchasePrice = lottoList.size * SixFortyFiveLotto.LOTTO_PRICE
-        message = """
-당첨 통계
---------
-3개 일치 (${SixFortyFiveLottoStore.LOTTO_WINNING_PRICE_MAP[3]}) - ${lottoResultList[3]}개
-4개 일치 (${SixFortyFiveLottoStore.LOTTO_WINNING_PRICE_MAP[4]}) - ${lottoResultList[4]}개
-5개 일치 (${SixFortyFiveLottoStore.LOTTO_WINNING_PRICE_MAP[5]}) - ${lottoResultList[5]}개
-6개 일치 (${SixFortyFiveLottoStore.LOTTO_WINNING_PRICE_MAP[6]}) - ${lottoResultList[6]}개
-총 수익률은 ${totalResultPrice / totalPurchasePrice.toDouble()} 입니다.
-                  """
+        var _message = "당첨 통계\n--------\n"
+        (3..6).forEach { index ->
+            _message =
+                _message.plus("${index}개 일치 (${SixFortyFiveLottoStore.LOTTO_WINNING_PRICE_MAP[index]}) - ${lottoResultList[index]}개\n")
+        }
+        message = _message.plus("총 수익률은 ${totalResultPrice / totalPurchasePrice.toDouble()} 입니다.")
     }
 }
