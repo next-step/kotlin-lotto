76e79150b730ceb79bc5a853880c0a36b4f7ef0f
genos-lee <genos.lee@kakaocorp.com>
2023-06-17 15:54:33 +0900

    feature : 당첨 통계에 보너스 볼 일치 추가

--- src/main/kotlin/next/step/lotto/domain/LottoRank.kt
+++ src/main/kotlin/next/step/lotto/domain/LottoRank.kt
@@ -1,22 +1,18 @@
 package next.step.lotto.domain
 
-enum class LottoRank(val matchCount: Int, val winnings: Int) {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
+enum class LottoRank(val matchCount: Int, val winnings: Int, val matchBonus: Boolean = false) {
+    MISS(0, 0),
     FIFTH(3, 5_000),
-    MISS(0, 0);
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000, true),
+    FIRST(6, 2_000_000_000);
 
     companion object {
 
         fun from(matchCount: Int, matchBonus: Boolean): LottoRank =
-            if (isBonusRank(matchCount, matchBonus)) SECOND else withoutBonus(matchCount)
-
-        private fun isBonusRank(matchCount: Int, matchBonus: Boolean) = matchCount == SECOND.matchCount && matchBonus
-
-        private fun withoutBonus(matchCount: Int) =
-            values().filter { it != SECOND }.find { it.matchCount == matchCount } ?: MISS
+            values().filter { it.matchBonus == matchBonus }.find { it.matchCount == matchCount }
+                ?: (values().find { it.matchCount == matchCount } ?: MISS)
     }
 
 }
--- src/main/kotlin/next/step/lotto/view/OutputView.kt
+++ src/main/kotlin/next/step/lotto/view/OutputView.kt
@@ -19,8 +19,10 @@ object OutputView {
         println("당첨 통계")
         println("---------")
         winningStat.filter { it.key != LottoRank.MISS }
-            .forEach { println("${it.key.matchCount}개 일치 (${it.key.winnings}원)- ${it.value}개") }
+            .forEach { println("${it.key.matchCount}개 일치${bonus(it.key.matchBonus)} (${it.key.winnings}원)- ${it.value}개") }
     }
+
+    private fun bonus(matchBonus: Boolean): String = if (matchBonus) ", 보너스 볼 일치" else ""
 
     fun showPerformance(performance: String) {
         println("총 수익률은 ${performance}입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)\n")
