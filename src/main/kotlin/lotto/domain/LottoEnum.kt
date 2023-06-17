36d2bdb0c09d8032c0ff4969b9ffbd5d113977f1
antaeseon <ants412@naver.com>
2023-06-17 17:11:06 +0900

    feat: 결과값 계산

---
+++ src/main/kotlin/lotto/domain/LottoEnum.kt
@@ -0,0 +1,35 @@
+package lotto.domain
+
+enum class LottoEnum(val num: Int, val price: Int) {
+    THREE(3, 5000),
+    FOUR(4, 50000),
+    FIVE(5, 1500000),
+    SIX(6, 2000000000);
+
+
+    companion object {
+        fun of(num: Int): LottoEnum? {
+            return when (num) {
+                THREE.num -> {
+                    THREE
+                }
+
+                FOUR.num -> {
+                    FOUR
+                }
+
+                FIVE.num -> {
+                    FIVE
+                }
+
+                SIX.num -> {
+                    SIX
+                }
+
+                else -> {
+                    null
+                }
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/lotto/domain/LottoResult.kt
@@ -0,0 +1,21 @@
+package lotto.domain
+
+import lotto.dto.ResultDTO
+
+object LottoResult {
+    fun calculateResult(lottos: Lottos, lottoResult: Lotto): List<ResultDTO> {
+        val sortedResult = lottoResult.numbers.sorted()
+
+        return lottos.list.map { lotto ->
+            lotto.numbers.count { it in sortedResult }
+        }.mapNotNull {
+            LottoEnum.of(it)
+        }.groupBy {
+            it
+        }.map {
+            ResultDTO(it.value.size, it.key)
+        }
+    }
+}
+
+
