bdeacc3a421fd3eed1369bd610e9b4cd432ec1b2
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-06-17 16:17:47 +0900

    refactor: 중복 값이 없어야 하는 복권 값들은 list에서 set으로 저장되게 변경

--- src/test/kotlin/domain/LotteryTest.kt
+++ src/test/kotlin/domain/LotteryTest.kt
@@ -18,31 +18,31 @@ class LotteryTest : StringSpec({
 
     "복권에 일치하는 값 개수를 찾는다." {
         listOf(
-            listOf(1, 2, 3, 8, 7, 9) to 3,
-            listOf(1, 2, 3, 4, 7, 8) to 4,
-            listOf(1, 2, 3, 4, 5, 7) to 5,
-            listOf(1, 2, 3, 4, 5, 6) to 6
+            setOf(1, 2, 3, 8, 7, 9) to 3,
+            setOf(1, 2, 3, 4, 7, 8) to 4,
+            setOf(1, 2, 3, 4, 5, 7) to 5,
+            setOf(1, 2, 3, 4, 5, 6) to 6
         ).forAll { (input, expected) ->
             val lottery = Lottery(MockNumberGenerator(input))
-            val countMatchingLottery = lottery.getPrizeByLottery(listOf(1, 2, 3, 4, 5, 6))
+            val countMatchingLottery = lottery.getPrizeByLottery(setOf(1, 2, 3, 4, 5, 6))
             countMatchingLottery!!.matches shouldBe expected
         }
     }
 
     "2등과 3등을 구분할 수 있다." {
         listOf(
-            listOf(1, 2, 3, 4, 5, 7) to Prize.SECOND_PLACE,
-            listOf(1, 2, 3, 4, 5, 8) to Prize.THIRD_PLACE
+            setOf(1, 2, 3, 4, 5, 7) to Prize.SECOND_PLACE,
+            setOf(1, 2, 3, 4, 5, 8) to Prize.THIRD_PLACE
         ).forAll { (input, expected) ->
             val lottery = Lottery(MockNumberGenerator(input))
-            val countMatchingLottery = lottery.getPrizeByLottery(listOf(1, 2, 3, 4, 5, 6), 7)
+            val countMatchingLottery = lottery.getPrizeByLottery(setOf(1, 2, 3, 4, 5, 6), 7)
             countMatchingLottery shouldBe expected
         }
     }
 
     "2등 당첨 시 3_000_000을 지급한다." {
-        val lottery = Lottery(MockNumberGenerator(listOf(1, 2, 3, 4, 5, 7)))
-        val countMatchingLottery = lottery.getPrizeByLottery(listOf(1, 2, 3, 4, 5, 6), 7)
+        val lottery = Lottery(MockNumberGenerator(setOf(1, 2, 3, 4, 5, 7)))
+        val countMatchingLottery = lottery.getPrizeByLottery(setOf(1, 2, 3, 4, 5, 6), 7)
         countMatchingLottery shouldBe Prize.SECOND_PLACE
         countMatchingLottery!!.prizeMoney shouldBe 3_000_000
     }
