747d9d476ed2c528c267c36a6fdbae9b14c3e2f3
sjparkk <sjparkk0820@naver.com>
2023-06-17 22:37:39 +0900

    refactor : Lotto 내 접근자 변경으로 인한 함수 제거 및 init 내에서 검증하도록 변경

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,21 +1,14 @@
 package lotto
 
 class Lotto(
-    private val numbers: List<Int>
+    val numbers: List<Int>
 ) {
-
-    fun getLotto(): List<Int> {
-        lottoInputNumberValidation(numbers)
-        return numbers.sorted()
-    }
-
-    private fun lottoInputNumberValidation(numbers: List<Int>) {
+    init {
         require(numbers.size == COLLECT_LOTTO_SIZE) { "로또 입력 숫자는 총 6개여야 합니다" }
     }
 
     companion object {
         const val ONE_PRICE: Int = 1000
-        const val NUMBER_COUNT: Int = 6
-        private const val COLLECT_LOTTO_SIZE = 6
+        const val COLLECT_LOTTO_SIZE = 6
     }
 }
