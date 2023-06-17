fbfef5f8c76c58f830d2c55f09d7a2b88b6b7803
jinho <g1063114@naver.com>
2023-06-17 23:30:52 +0900

    feat: 로또는 6개의 랜덤한 숫자로 구성된다.

---
+++ src/main/kotlin/lotto/Lotto.kt
@@ -0,0 +1,24 @@
+package lotto
+
+class Lotto {
+    private var _lottoNumbers = listOf<Int>()
+
+    val lottoNumbers: List<Int>
+        get() = _lottoNumbers
+    init {
+        _lottoNumbers = purchaseLotto()
+    }
+
+    private fun purchaseLotto(): List<Int> {
+        return (START_LOTTO_NUMBER..LAST_LOTTO_NUMBER).shuffled()
+            .subList(FROM_INDEX, TO_INDEX)
+            .sorted()
+    }
+
+    companion object {
+        const val START_LOTTO_NUMBER = 1
+        const val LAST_LOTTO_NUMBER = 45
+        const val FROM_INDEX = 0
+        const val TO_INDEX = 6
+    }
+}
---
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -0,0 +1,21 @@
+package lotto
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class LottoTest {
+    @ParameterizedTest
+    @ValueSource(ints = [14000])
+    fun `구입 금액을 입력해서 구매 가능 개수를 구한다`(input: Int) {
+        val purchaseCount = input / 1000
+        purchaseCount shouldBe 14
+    }
+
+    @Test
+    fun `로또는 6개의 랜덤한 숫자로 구성된다`() {
+        val lotto = Lotto()
+        lotto.lottoNumbers.size shouldBe 6
+    }
+}
