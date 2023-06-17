58b2cf3188b21e3a647c5059bab489b0f10b125c
nam-sangyun <nsun9505@naver.com>
2023-06-18 00:59:52 +0900

    refactor: 불필요 정렬 제거

--- src/main/kotlin/lotto/domain/LottoNumbers.kt
+++ src/main/kotlin/lotto/domain/LottoNumbers.kt
@@ -8,8 +8,6 @@ value class LottoNumbers(private val lottoNumbers: List<LottoNumber>) : List<Lot
         require(distinctLottoNumbers.size == LOTTO_NUMBER_SIZE) {
             "로또 번호 $LOTTO_NUMBER_SIZE 개는 중복이 없어야 합니다."
         }
-
-        lottoNumbers.sortedBy { it.number }
     }
     companion object {
         const val LOTTO_NUMBER_SIZE = 6
