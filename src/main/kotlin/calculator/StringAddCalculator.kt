e696c8b51d1589f517171fcc2cb43635829a9b2e
daehyunKim <zbeld123@gmail.com>
2023-06-17 16:18:29 +0900

    feat(step1): 커스텀 구분자 지정

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -11,11 +11,19 @@ class StringAddCalculator {
     }
 
     private fun split(input: String): List<String> {
-        return input.split(DEFAULT_REGEX)
+        return splitByCustomDelimiterOrNull(input) ?: input.split(DEFAULT_REGEX)
+    }
+
+    private fun splitByCustomDelimiterOrNull(input: String): List<String>? {
+        return Regex("//(.)\n(.*)").find(input)?.let {
+            val delimiter = it.groupValues[1]
+            val stringNumbers = it.groupValues[2].split(delimiter)
+            stringNumbers
+        }
     }
 
     companion object {
-        private const val DEFAULT_DELIMITERS = "[,;]"
+        private const val DEFAULT_DELIMITERS = "[,:]"
         private val DEFAULT_REGEX = Regex(DEFAULT_DELIMITERS)
     }
 }
--- src/test/kotlin/calculator/StringAddCalculatorTest.kt
+++ src/test/kotlin/calculator/StringAddCalculatorTest.kt
@@ -1,20 +1,42 @@
 package calculator
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class StringAddCalculatorTest {
 
-    @Test
-    fun `쉼표 또는 콜론을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환한다`() {
+    @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2"])
+    fun twoNumbers(text: String) {
         // given
-        val stringNumbers = "1;2,3"
-        val expectResult = 1 + 2 + 3
-
-        // when
-        val result = StringAddCalculator().calculate(stringNumbers)
+        val result = StringAddCalculator().calculate(text)
 
         // then
-        assertThat(result).isEqualTo(expectResult)
+        assertThat(result).isSameAs(3)
+    }
+
+    @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2:3"])
+    fun colons(text: String) {
+        // given
+        val result = StringAddCalculator().calculate(text)
+
+        // then
+        assertThat(result).isSameAs(6)
+    }
+
+    @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = ["//;\n1;2;3"])
+    fun customDelimiter(text: String) {
+        // given
+        val result = StringAddCalculator().calculate(text)
+
+        // then
+        assertThat(result).isSameAs(6)
     }
 }
