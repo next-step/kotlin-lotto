04be2233368d54c5a308ff3ea9b17b5e1abd0bec
Minsung Kim <epicblue@hanmail.net>
2023-06-17 17:55:09 +0900

    feat(Lotto): 로또는 당첨 번호와 일치하는 숫자의 개수를 알려준다.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,11 +1,21 @@
 package lotto
 
 class Lotto(
-    val numbers: Set<Int>,
+    val lottoNumbers: Set<Int>,
 ) {
     init {
-        require(numbers.size == 6) { "로또 번호는 중복 없이 6개만 입력 가능합니다." }
-        require(numbers.all { it in 1..45 }) { "로또 번호는 1~45 사이만 입력 가능합니다." }
+        require(lottoNumbers.size == 6) { "로또 번호는 중복 없이 6개만 입력 가능합니다." }
+        require(lottoNumbers.all { it in 1..45 }) { "로또 번호는 1~45 사이만 입력 가능합니다." }
+    }
+
+    fun countMatchingNumbersFrom(winningNumbers: List<Int>): Int {
+        var count = 0
+
+        winningNumbers.forEach { winningNumber ->
+            if (lottoNumbers.contains(winningNumber)) count++
+        }
+
+        return count
     }
 
     companion object {
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -5,10 +5,11 @@ import io.kotest.core.spec.style.FreeSpec
 import io.kotest.data.forAll
 import io.kotest.data.row
 import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.shouldBe
 
 class LottoTest : FreeSpec({
 
-    "로또는 중복 없이 무작위로 선택된 숫자 6개(1~45)로 구성된다." - {
+    "로또는 중복 없는 숫자 6개(1~45)로 구성된다." - {
         "성공" {
             forAll(
                 row(listOf(1, 20, 35, 40, 42, 44)),
@@ -17,7 +18,7 @@ class LottoTest : FreeSpec({
             ) { numbers ->
                 val sut = Lotto.from(numbers)
 
-                val result = sut.numbers
+                val result = sut.lottoNumbers
 
                 result shouldContainExactlyInAnyOrder numbers
             }
@@ -39,6 +40,19 @@ class LottoTest : FreeSpec({
             ) { numbers ->
                 shouldThrowWithMessage<IllegalArgumentException>("로또 번호는 1~45 사이만 입력 가능합니다.") { Lotto.from(numbers) }
             }
+        }
+    }
+
+    "로또는 당첨 번호와 일치하는 숫자의 개수를 알려준다." - {
+        forAll(
+            row(listOf(1, 3, 5, 42, 45, 4), listOf(1, 2, 3, 4, 5, 6), 4),
+            row(listOf(23, 44, 11, 33, 4, 2), listOf(5, 6, 7, 8, 9, 10), 0)
+        ) { lottoNumbers, winningNumbers, expected ->
+            val sut = Lotto.from(lottoNumbers)
+
+            val result = sut.countMatchingNumbersFrom(winningNumbers)
+
+            result shouldBe expected
         }
     }
 })
