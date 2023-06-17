df9274937a754fa1419fb5fc9d7c8564200e7e93
peter <peter.shlee@kakaocorp.com>
2023-06-17 17:30:29 +0900

    refactor(step2): require에 에러 안내 문구 추가

--- src/main/kotlin/lotto/domain/BillSlot.kt
+++ src/main/kotlin/lotto/domain/BillSlot.kt
@@ -3,7 +3,7 @@ package lotto.domain
 class BillSlot(private val lottoPrice: Int) {
 
     init {
-        require(lottoPrice > 0)
+        require(lottoPrice > 0) { "0보다 큰 금액을 투입해야 합니다." }
     }
 
     fun insertMoney(money: Int): Int {
--- src/main/kotlin/lotto/domain/LottoNumbers.kt
+++ src/main/kotlin/lotto/domain/LottoNumbers.kt
@@ -4,9 +4,9 @@ data class LottoNumbers(
     val numbers: List<Int>,
 ) {
     init {
-        require(numbers.size == LENGTH)
-        require(numbers.all(::inValidRange))
-        require(hasUniqueNumbers())
+        require(numbers.size == LENGTH) { "로또 번호는 $LENGTH 개의 숫자로 이루어져 있어야 합니다." }
+        require(numbers.all(::inValidRange)) { "로또 번호는 ${MINIMUM}~${MAXIMUM} 사이의 수여야 합니다." }
+        require(hasUniqueNumbers()) { "로또 번호에 중복된 수가 있으면 안됩니다." }
     }
 
     fun numberOfOverlaps(other: LottoNumbers): Int {
