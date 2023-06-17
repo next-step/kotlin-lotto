b831820d5261550b464c0f7561425b6749398ca2
antaeseon <ants412@naver.com>
2023-06-17 17:22:28 +0900

    feat: 총 수익률을 계산한다.

--- src/test/kotlin/lotto/domain/ProfitTest.kt
+++ src/test/kotlin/lotto/domain/ProfitTest.kt
@@ -6,10 +6,14 @@ import lotto.dto.ResultDTO
 
 class ProfitTest:StringSpec({
 
-    "총 수익을 계산한다.."{
+    "총 수익을 계산한다."{
         Profit.calculateLottoProfit(listOf(
             ResultDTO(3,LottoEnum.THREE),
             ResultDTO(1,LottoEnum.FOUR),
         )) shouldBe 65000
+    }
+
+    "총 수익률을 계산한다."{
+        Profit.calculateLottoProfitRatio(3000,10000) shouldBe 0.3
     }
 })
