4ae908d1200e1e46797fea3e50aa7793893333db
antaeseon <ants412@naver.com>
2023-06-17 21:29:37 +0900

    refactor: 로또 번호 생성 수정

--- src/main/kotlin/lotto/domain/LottoNumberGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoNumberGenerator.kt
@@ -2,8 +2,6 @@ package lotto.domain
 
 object LottoNumberGenerator {
     fun generate(): List<Int> {
-        return generateSequence {
-            (1..45).random()
-        }.distinct().take(6).toList()
+        return (1..45).shuffled().subList(0, 6)
     }
 }
