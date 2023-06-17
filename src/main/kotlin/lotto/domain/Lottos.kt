a9fa9d87b2eedd70e4f34a9074d7da05fd0408a3
konempty <jameswebb@naver.com>
2023-06-18 00:04:48 +0900

    refactor: 불필요한 코드 수정

--- src/main/kotlin/lotto/domain/Lottos.kt
+++ src/main/kotlin/lotto/domain/Lottos.kt
@@ -1,10 +1,8 @@
 package lotto.domain
 
 data class Lottos(val lottoList: List<Lotto>) : Iterable<Lotto> {
-    val size: Int
-        get() {
-            return lottoList.size
-        }
+
+    val size: Int = lottoList.size
 
     override fun iterator(): Iterator<Lotto> =
         lottoList.iterator()
