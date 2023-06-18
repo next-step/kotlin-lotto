8cb33aae40c44b994e0b577858033c63af2d057b
김현준 <4whomtbts@gmail.com>
2023-06-19 00:22:58 +0900

    feat(step2): 구입한 로또의 번호들을 출력한다

--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -3,7 +3,10 @@ package lotto.view
 import lotto.Lottos
 
 object ResultView {
-    fun printNumOfLottos(lottos: Lottos) {
+    fun printPurchasedLottos(lottos: Lottos) {
         println("${lottos.size} 개를 구매했습니다.")
+        lottos.forEach {
+            println(it.numbers)
+        }
     }
 }
