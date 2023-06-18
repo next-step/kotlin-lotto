a3919375080094e93acfd8d7d05e93d6f6aad070
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-06-18 12:50:26 +0900

    refactor: 비지니스 로직 분리

--- src/main/kotlin/LotteryRunner.kt
+++ src/main/kotlin/LotteryRunner.kt
@@ -1,24 +1,33 @@
 import domain.Lottery
 import domain.Settlement
-import view.InputView
 import view.OutputView
 
-class LotteryRunner(private val inputView: InputView) {
+object LotteryRunner {
 
     var lotteries = listOf<Lottery>()
         private set
 
-    fun startLotto() {
-        val purchasableSize = inputView.buyLotto()
-        lotteries = List(purchasableSize) { Lottery() }
-        for (lottery in lotteries) {
-            println(lottery.randomNumber.toString())
-        }
-
-        val winningNums = inputView.registerWinningNums()
+    fun startLotto(
+        money: Int,
+        winningNums: Set<Int>
+    ) {
+        val purchasableSize = purchaseLottery(money)
         val settlement = Settlement(winningNums)
         val returnOnInvestment = settlement.getReturnOnInvestment(lotteries, (purchasableSize * 1000))
-
         OutputView.reportProfit(returnOnInvestment)
+    }
+
+    private fun purchaseLottery(money: Int): Int {
+        require(money > 1000) { "돈이 부족합니다." }
+
+        val purchasableSize = money / 1000
+        OutputView.reportPurchaseHistory(purchasableSize)
+        lotteries = List(purchasableSize) { Lottery() }
+
+        for (lottery in lotteries) {
+            OutputView.reportPrizeState(lottery)
+        }
+
+        return purchasableSize
     }
 }
