4ff8d142b8d8d8c4e5c81afcaa1bf0f3a25a09e5
chad.yoon <chad.yoon@kakaocorp.com>
2023-06-18 22:53:07 +0900

    refactor: LottoController를 main에서 직접 호출하도록 변경

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,20 @@
-import lotto.view.startLotto
+import lotto.domain.analysis.RealLottoResultAnalyst
+import lotto.domain.shop.RealLottoShop
+import lotto.domain.shop.machine.RealLottoGameMachine
+import lotto.view.LottoController
+import lotto.view.RealLottoInputView
+import lotto.view.RealLottoResultView
+import shffule.RandomShuffler
 
 fun main() {
-    startLotto()
+    LottoController(
+        lottoInputView = RealLottoInputView(),
+        lottoResultView = RealLottoResultView(),
+        lottoShop = RealLottoShop(
+            lottoGameMachine = RealLottoGameMachine(
+                shuffler = RandomShuffler(),
+            ),
+        ),
+        lottoResultAnalyst = RealLottoResultAnalyst(),
+    ).start()
 }
