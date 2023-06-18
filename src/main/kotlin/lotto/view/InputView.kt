90fdd7bf988c4bf019739ead6d5a2d9ec2041e87
gray <operasystem6@naver.com>
2023-06-18 23:23:13 +0900

    feat: view 로직 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,5 +1,6 @@
 package lotto.view
 
+import lotto.domain.LottoNumber
 import java.lang.IllegalArgumentException
 import java.lang.NumberFormatException
 
@@ -11,5 +12,10 @@ object InputView {
         } catch (e: NumberFormatException) {
             throw IllegalArgumentException("숫자만 입력해주세요.")
         }
+    }
+
+    fun inputWinnerNumbers(): List<LottoNumber> {
+        val winnerNumbers = readln().split(", ")
+        return winnerNumbers.map { LottoNumber(it.toInt()) }
     }
 }
---
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -0,0 +1,41 @@
+package lotto.view
+
+import lotto.domain.Lottos
+import lotto.domain.Rank
+
+object OutputView {
+
+    fun printEnterPayment() {
+        println("구입금액을 입력해 주세요.")
+    }
+
+    fun printPurchaseCount(count: Int) {
+        println("${count}개를 구매했습니다.")
+    }
+
+    fun printLottos(lottos: Lottos) {
+        for (lottoNumbers in lottos.lottoNumbers) {
+            println(lottoNumbers)
+        }
+        println()
+    }
+
+    fun printWinningNumbersLastWeek() {
+        println("지난 주 당첨 번호를 입력해 주세요.")
+    }
+
+    fun printWinnerStatistics() {
+        println("당첨 통계")
+        println("---------")
+    }
+
+    fun printNumberOfMatches(ranks: List<Rank>) {
+        for (rank in ranks) {
+            println("${rank.title} (${rank.amount})- ${rank.count}개")
+        }
+    }
+
+    fun printProfitRate(profitRate: Double) {
+        println(String.format("총 수익률은 %.2f입니다.", profitRate))
+    }
+}
