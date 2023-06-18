309b1fe9e3e907ee523a6fb76d449c7da75e0dff
oliveunji <eunjikim2u@gmail.com>
2023-06-18 17:45:04 +0900

    refactor: 싱글톤 클래스로 사용하기 위해 리팩토링

--- src/main/kotlin/calculator/TokenConverter.kt
+++ src/main/kotlin/calculator/TokenConverter.kt
@@ -1,13 +1,11 @@
 package calculator
 
-class TokenConverter(tokens: List<String>) {
-    companion object {
-        fun convertToInt(tokens: List<String>): List<Int> {
-            return try {
-                tokens.map { it.toInt() }
-            } catch (e: NumberFormatException) {
-                throw RuntimeException("Invalid token: $tokens")
-            }
+object TokenConverter {
+    fun convertToInt(tokens: List<String>): List<Int> {
+        return try {
+            tokens.map { it.toInt() }
+        } catch (e: NumberFormatException) {
+            throw RuntimeException("Invalid token: $tokens")
         }
     }
 }
