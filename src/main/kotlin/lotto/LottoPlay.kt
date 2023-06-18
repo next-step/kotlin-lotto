b6f4d817aea0f2f5284960c2f052ba14dc0a832c
고영경(Yeonggyeong Go)/커머스플랫폼개발팀/SKP <yg.go@sk.com>
2023-06-19 00:02:44 +0900

    결과 분석은 별도 object class로 분리

--- src/main/kotlin/lotto/LottoPlay.kt
+++ src/main/kotlin/lotto/LottoPlay.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import lotto.model.Lotto
+import lotto.service.LottoPlayResultAnalysis
 import lotto.service.LottoTicketCountCalculator
 
 
@@ -23,34 +24,5 @@ fun main() {
 
     winningLottos.forEach { it.setLottoPrize(lastWeekWinningNumbers) }
 
-    val winningRatio = getWinningRatio(purchaseAmount, winningLottos)
-
-    println("당첨 통계")
-    println("---------")
-
-    println("3개 일치 (5000원)- ${staticPrize(winningLottos, 5000)}")
-    println("4개 일치 (50000원)- ${staticPrize(winningLottos, 50000)}")
-    println("5개 일치 (1500000원)- ${staticPrize(winningLottos, 1500000)}")
-    println("6개 일치 (2000000000원)-${staticPrize(winningLottos, 2000000000)}")
-
-    println()
-    println(
-        "총 수익률은 ${
-            String.format(
-                "%.2f", winningRatio
-            )
-        } 입니다.(기준이 1이기 때문에 결과적으로 ${if (winningRatio >= 1) "이득" else "손해"}라는 의미임)"
-    )
+    LottoPlayResultAnalysis.printResult(winningLottos, purchaseAmount)
 }
-
-fun getWinningRatio(
-    purchaseAmount: Int, winningLottos: List<Lotto>): Double {
-    return getTotalWinningPrize(winningLottos) / purchaseAmount.toDouble()
-}
-
-private fun getTotalWinningPrize(winningLottos: List<Lotto>): Long =
-    winningLottos.sumOf { it.lottoPrize.prizeAmount }
-
-fun staticPrize(winningLottos: List<Lotto>, prize: Long): Int =
-    winningLottos.count { it.lottoPrize.prizeAmount == prize }
-
--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -4,11 +4,15 @@ class Lotto {
     private val lottoNumbers: List<Int> = (1..45).toList()
 
     var lottoPrize: LottoPrize = LottoPrize.NONE_PRIZE
-    val winningNumber: List<Int> = lottoNumbers.shuffled().take(6).sorted()
+    val winningNumber: List<Int> = lottoNumbers.shuffled().take(LOTTO_NUMBER_COUNT).sorted()
 
     fun setLottoPrize(lastWeekWinningNumbers: List<Int>): LottoPrize {
         val matchingNumbers = winningNumber.intersect(lastWeekWinningNumbers.toSet()).size
         lottoPrize = LottoPrize.getLottoPrize(matchingNumbers)
         return lottoPrize
+    }
+
+    companion object {
+        private const val LOTTO_NUMBER_COUNT = 6
     }
 }
