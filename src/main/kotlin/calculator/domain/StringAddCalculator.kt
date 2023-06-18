3187d2cef6bf17b48a57dfe2814dc2b51f8a88e0
gray <operasystem6@naver.com>
2023-06-19 00:28:21 +0900

    refactor: require 사용

--- src/main/kotlin/calculator/domain/StringAddCalculator.kt
+++ src/main/kotlin/calculator/domain/StringAddCalculator.kt
@@ -5,19 +5,11 @@ class StringAddCalculator {
         if (text.isNullOrBlank()) {
             return 0
         }
-
-        val regexResult = Regex(REGEX_PATTERN).find(text)
-        val customSeparator = regexResult?.let {
-            it.groupValues[1]
-        }
+        val separator = Separator.create(text)
         val separators = Separators()
-        separators.add(Separator(customSeparator.toString()))
+        separators.add(separator)
 
         val splitData = TokenizedExpression.generate(text.substringAfter("\n"), separators)
         return splitData.mapNotNull { it.toIntOrNull() }.sum()
-    }
-
-    companion object {
-        private const val REGEX_PATTERN = "//(.)\n(.*)"
     }
 }
