483f8397b7ce9bec30b8f5341c0fa5460a3168b4
gray <operasystem6@naver.com>
2023-06-19 00:27:04 +0900

    refactor: custom seperator에 대한 로직을 Separator 생성 로직에 넣는 방식으로 변경

--- src/main/kotlin/calculator/domain/Separator.kt
+++ src/main/kotlin/calculator/domain/Separator.kt
@@ -3,4 +3,16 @@ package calculator.domain
 @JvmInline
 value class Separator(
     val separator: String
-)
+) {
+    companion object {
+        private const val REGEX_PATTERN = "//(.)\n(.*)"
+
+        fun create(text: String): Separator {
+            val regexResult = Regex(REGEX_PATTERN).find(text)
+            val customSeparator = regexResult?.let {
+                it.groupValues[1]
+            }
+            return Separator(customSeparator.toString())
+        }
+    }
+}
