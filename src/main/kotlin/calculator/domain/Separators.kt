daab82e7cbafe63721287f30b55d27d932befe7e
gray <operasystem6@naver.com>
2023-06-19 00:27:23 +0900

    refactor: private 접근 지정자 추가

--- src/main/kotlin/calculator/domain/Separators.kt
+++ src/main/kotlin/calculator/domain/Separators.kt
@@ -1,9 +1,11 @@
 package calculator.domain
 
 class Separators {
-    val separators: MutableList<Separator> = mutableListOf(Separator(","), Separator(":"))
+    private val separators: MutableList<Separator> = mutableListOf(Separator(","), Separator(":"))
 
     fun add(separator: Separator) {
         separators.add(separator)
     }
+
+    fun getSeparators() = separators
 }
--- src/test/kotlin/calculator/domain/SeparatorsTest.kt
+++ src/test/kotlin/calculator/domain/SeparatorsTest.kt
@@ -7,12 +7,12 @@ class SeparatorsTest : StringSpec({
 
     "초기 구분자는 (,), (:) 이다." {
         val separators = Separators()
-        separators.separators shouldBe mutableListOf(Separator(","), Separator(":"))
+        separators.getSeparators() shouldBe mutableListOf(Separator(","), Separator(":"))
     }
 
     "구분자를 추가할 수 있다." {
         val separators = Separators()
         separators.add(Separator(":"))
-        separators.separators shouldBe mutableListOf(Separator(","), Separator(":"), Separator(":"))
+        separators.getSeparators() shouldBe mutableListOf(Separator(","), Separator(":"), Separator(":"))
     }
 })
