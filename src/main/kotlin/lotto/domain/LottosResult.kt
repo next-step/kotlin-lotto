adaae718635c308ced1a9f4614dfb2cedf201eac
leehyunjae <leehyunjae@woowahan.com>
2023-06-18 22:34:42 +0900

    [STEP2] LottoStatistic 클래스 제거 -> LottosResult 하위에 내용 추가

--- src/main/kotlin/lotto/domain/LottosResult.kt
+++ src/main/kotlin/lotto/domain/LottosResult.kt
@@ -1,8 +1,32 @@
 package lotto.domain
 
+import java.text.DecimalFormat
+
+typealias WinningResults = Map<Rank, Int>
+
 class LottosResult(
-    val numberOfFirst: Int = 0,
-    val numberOfThird: Int = 0,
-    val numberOfFourth: Int = 0,
-    val numberOfFifth: Int = 0,
-)
+    val totalCost: Int,
+    val winningResults: WinningResults,
+) {
+    private val totalWinningMoney: Int
+    val returnOfRate: Double
+
+    init {
+        validateTotalCostIsPostivieZero(totalCost)
+
+        totalWinningMoney = winningResults
+            .map { (rank, count) -> rank.winningMoney * count }
+            .sum()
+        returnOfRate = (totalWinningMoney.toDouble() / totalCost).round()
+    }
+
+    private fun validateTotalCostIsPostivieZero(totalCost: Int) {
+        require(totalCost >= 0) {
+            "총 구매 비용은 0보다 커야 합니다."
+        }
+    }
+
+    private fun Double.round(decimals: Int = 2): Double = DecimalFormat("#.${"#".repeat(decimals)}")
+        .also { df -> df.roundingMode = java.math.RoundingMode.DOWN }
+        .format(this).toDouble()
+}
