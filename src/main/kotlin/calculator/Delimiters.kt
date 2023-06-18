eb9dbbbae4b03ad0936696131f990d1de1bd7aaf
oliveunji <eunjikim2u@gmail.com>
2023-06-18 18:11:39 +0900

    refactor: Delimiter 클래스의 경우 싱글턴으로 객체 생성을 방지하기 위해 리팩토링

--- src/main/kotlin/calculator/Delimiters.kt
+++ src/main/kotlin/calculator/Delimiters.kt
@@ -1,12 +1,12 @@
 package calculator
 
-class Delimiters private constructor(private val delimiters: MutableList<String>) {
+class Delimiters(private val delimiters: MutableList<String>) {
     companion object {
-        private var delimiters = mutableListOf(",", ":")
-        val DEFAULT = Delimiters(delimiters)
+        private var defaultDelimiters = mutableListOf(",", ":")
+        val DEFAULT = Delimiters(defaultDelimiters)
     }
 
-    fun getDelimiters(): List<String> = delimiters
+    fun getDelimiters(): List<String> = delimiters.toList()
 
     fun addCustomDelimiters(vararg customDelimiters: String) {
         delimiters.addAll(customDelimiters)
---
+++ src/test/kotlin/calculator/utils/CustomDelimiterExtractorTest.kt
@@ -0,0 +1,15 @@
+package calculator.utils
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class CustomDelimiterExtractorTest {
+    @DisplayName(value = "문자열 계산기의 extractCustomDelimiter 함수가 custom delimiter 를 분리한다")
+    @ParameterizedTest
+    @ValueSource(strings = ["//@\n1:2@4"])
+    fun extractText(text: String) {
+        Assertions.assertThat(CustomDelimiterExtractor.extract(text)).isEqualTo(Pair("@", "1:2@4"))
+    }
+}
