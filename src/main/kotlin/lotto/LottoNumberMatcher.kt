807431c300fb33c0812b77cb2f14dda55f483af9
sjparkk <sjparkk0820@naver.com>
2023-06-18 10:41:32 +0900

    refactor : 로또 번호 당첨 확인과 로또 금액 확인에 대한 클래스 역할 분리 및 클래스명 수정

---
+++ src/main/kotlin/lotto/LottoNumberMatcher.kt
@@ -0,0 +1,52 @@
+package lotto
+
+import lotto.domain.Lotto
+import lotto.enums.LottoRank
+
+class LottoNumberMatcher {
+
+    fun lottoCheck(winningNumber: List<Int>, lottoBundle: List<Lotto>, bonusNumber: Int): List<LottoRank> {
+
+        val result = mutableListOf<LottoRank>()
+        lottoBundle.forEach { lotto ->
+            var count = 0
+            lotto.numbers.forEach { number ->
+                if (winningNumber.contains(number)) {
+                    count++
+                }
+            }
+            if(count == BONUS_WINNING_COUNT && lotto.numbers.contains(bonusNumber)) {
+                return@forEach
+            }
+            if (count != 0) {
+                result.add(LottoRank.getLottoRankByCount(count))
+            }
+        }
+        return result.toList()
+    }
+
+    fun bonusLottoCheck(winningNumbers: List<Int>, lottoBundle: List<Lotto>, bonusNumber: Int): Int {
+
+        var result = 0
+        lottoBundle.forEach { lotto ->
+            var count = 0
+            lotto.numbers.forEach { number ->
+                if(winningNumbers.contains(number)) {
+                    count++
+                }
+            }
+            if(count == BONUS_WINNING_COUNT && lotto.numbers.contains(bonusNumber)) {
+                result++
+            }
+        }
+        return result
+    }
+
+    fun lottoResultGroup(collectLottoRanks: List<LottoRank>): Map<Int, Int> {
+        return collectLottoRanks.sorted().groupingBy { it.count }.eachCount()
+    }
+
+    companion object {
+        private const val BONUS_WINNING_COUNT = 5
+    }
+}
