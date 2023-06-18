ce477430b6717c43ef50e20fe94d9714d90a3108
hyunju-seo <hyunju6889@naver.com>
2023-06-18 21:55:36 +0900

    feat: 로또 controller구현

---
+++ src/main/kotlin/lotto/LottoController.kt
@@ -0,0 +1,35 @@
+package lotto
+
+import lotto.domain.Lotto
+import lotto.domain.LottoMachine
+import lotto.domain.LottoResult
+import lotto.domain.LottoWinningNumbers
+import lotto.view.InputView
+import lotto.view.OutputView
+
+class LottoController {
+    fun execute() {
+        val lottos = getLottos()
+        OutputView.printLottos(lottos)
+
+        val lottoResult = getLottoResult(lottos)
+        val profitRate = lottoResult.getProfitRate(LottoMachine.LOTTO_PRICE)
+        OutputView.printLottoRankStatics(lottoResult.getLottoRankStatistic())
+        OutputView.printProfitRate(profitRate)
+    }
+
+    private fun getLottos(): List<Lotto> {
+        val purchaseAmount = InputView.getPurchaseAmount()
+        val lottos = LottoMachine.getLottos(purchaseAmount)
+        return lottos
+    }
+
+    private fun getLottoResult(lottos: List<Lotto>): LottoResult {
+        val winningNumbers = InputView.getWinningNumbers()
+        return LottoWinningNumbers(winningNumbers).getLottoResult(lottos)
+    }
+}
+
+fun main() {
+    LottoController().execute()
+}
--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -3,32 +3,39 @@ package lotto.domain
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import lotto.test.ObjectGenerator
 
 class LottoTest : StringSpec({
     "로또 번호는 6개이다." {
         shouldNotThrowAny {
-            generateLotto(1, 2, 3, 4, 5, 6)
+            ObjectGenerator.lotto(1, 2, 3, 4, 5, 6)
         }
     }
 
     "로또 번호가 6개가 아니면 예외를 던진다." {
         shouldThrow<IllegalArgumentException> {
-            generateLotto(1, 2, 3, 4, 5)
+            ObjectGenerator.lotto(1, 2, 3, 4, 5)
         }
 
         shouldThrow<IllegalArgumentException> {
-            generateLotto(1, 2, 3, 4, 5, 6, 7)
+            ObjectGenerator.lotto(1, 2, 3, 4, 5, 6, 7)
         }
     }
 
     "로또 번호중에 중복되는 숫자가 있으면 예외를 던진다." {
         shouldThrow<IllegalArgumentException> {
-            generateLotto(1, 1, 2, 3, 4, 5)
+            ObjectGenerator.lotto(1, 1, 2, 3, 4, 5)
+        }
+    }
+
+    "로또 번호와 일치하는 숫자의 개수를 구한다." {
+        val lotto = ObjectGenerator.lotto(1, 2, 3, 4, 5, 6)
+        listOf(
+            ObjectGenerator.lottoNumbers(1, 2, 3, 14, 15, 16) to 3,
+            ObjectGenerator.lottoNumbers(10, 11, 12, 13, 14, 15) to 0
+        ).forEach { (numbers, matchingCount) ->
+            lotto.getMatchingCount(numbers) shouldBe matchingCount
         }
     }
 })
-
-fun generateLotto(vararg number: Int): Lotto {
-    val lottoNumbers = number.toList().map { LottoNumber(it) }
-    return Lotto(lottoNumbers)
-}
