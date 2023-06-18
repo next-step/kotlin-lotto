585492c252c270a89b119e43897db07982b36bc6
jayden-17 <jayden.17@kakaocorp.com>
2023-06-18 17:23:42 +0900

    feat : PositiveNumber value class 적용

--- src/main/kotlin/calculator/PositiveNumber.kt
+++ src/main/kotlin/calculator/PositiveNumber.kt
@@ -1,8 +1,7 @@
 package calculator
 
-class PositiveNumber(value: Int) {
-
-    private val number = value
+@JvmInline
+value class PositiveNumber(private val number: Int) {
 
     constructor(value: String) : this(value.toIntOrNull() ?: throw RuntimeException("숫자 이외의 값은 입력할 수 없습니다."))
 
@@ -13,23 +12,6 @@ class PositiveNumber(value: Int) {
     }
 
     operator fun plus(other: PositiveNumber): PositiveNumber = PositiveNumber(number + other.number)
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as PositiveNumber
-
-        return number == other.number
-    }
-
-    override fun hashCode(): Int {
-        return number
-    }
-
-    override fun toString(): String {
-        return "PositiveNumber(number=$number)"
-    }
 
     companion object {
         val ZERO = PositiveNumber(0)
