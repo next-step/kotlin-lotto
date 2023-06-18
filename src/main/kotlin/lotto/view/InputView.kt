b162f666740ad64f8c7d70b0ed451188de044d6d
peter <peter.shlee@kakaocorp.com>
2023-06-18 19:52:10 +0900

    refactor(step2): 로또 번호 구분자를 상수로 정의

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,6 +3,9 @@ package lotto.view
 import lotto.domain.LottoNumbers
 
 object InputView {
+
+    private const val NUMBER_SEPARATOR = ","
+
     fun getPurchaseAmount(): Int {
         println("구입금액을 입력해 주세요.")
         return readInt()
@@ -25,7 +28,7 @@ object InputView {
 
     private fun getNumbers(): List<Int> {
         return readln()
-            .split(",")
+            .split(NUMBER_SEPARATOR)
             .mapNotNull {
                 it.trim()
                     .toIntOrNull()
