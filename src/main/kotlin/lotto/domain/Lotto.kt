e38b8ea0d3f875151cbf7b1261c63c485fb89f97
antaeseon <ants412@naver.com>
2023-06-18 21:53:45 +0900

    refactor: 로또 예외 조건 추가

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -13,6 +13,16 @@ class Lottos(
 
 class Lotto(
     val numbers: List<Int>
-)
+) {
+
+    init {
+        require(numbers.size == 6) { "6개의 숫자가 필요합니다." }
+        require(numbers.distinct().size == 6) { "중복되지 않은 6개의 숫자가 필요합니다." }
+        require(numbers.all { it in 1..45 }) { "1~45의 수만 가능합니다" }
+    }
+
+    companion object {
+    }
+}
 
 fun List<Lotto>.toLottos() = Lottos(this)
---
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -0,0 +1,26 @@
+package lotto.domain
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class LottoTest : StringSpec({
+
+    "입력시 숫자가 6개가 아니면 예외가 발생한다." {
+        shouldThrow<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5))
+        }.message shouldBe "6개의 숫자가 필요합니다."
+    }
+
+    "중복 숫자가 존재하면 예외가 발생한다." {
+        shouldThrow<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 5))
+        }.message shouldBe "중복되지 않은 6개의 숫자가 필요합니다."
+    }
+
+    "1~45를 입력하지 않으면 예외가 발생한다." {
+        shouldThrow<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 55))
+        }.message shouldBe "1~45의 수만 가능합니다"
+    }
+})
