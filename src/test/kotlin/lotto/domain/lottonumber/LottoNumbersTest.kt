1057ffa3f5fe71b7b35dcbb2069249c8086ccd8a
chad.yoon <chad.yoon@kakaocorp.com>
2023-06-18 22:59:05 +0900

    test: LottoNumber 테스트들의 네이밍을, 테스트 대상 클래스의 관심사에 맞는 네이밍으로 변경

--- src/test/kotlin/lotto/domain/lottonumber/LottoNumbersTest.kt
+++ src/test/kotlin/lotto/domain/lottonumber/LottoNumbersTest.kt
@@ -7,23 +7,24 @@ import io.kotest.data.row
 
 class LottoNumbersTest : BehaviorSpec({
 
-    Given("로또 게임을 생성할 때") {
-        When("로또 번호 목록의 사이즈가 6이 아니면") {
-            forAll(
-                row(5),
-                row(7),
-            ) { pickCount ->
-                Then("RuntimeException 예외 처리를 한다") {
-                    shouldThrow<RuntimeException> {
-                        LottoNumbers(LottoNumber.allLottoNumbers().take(pickCount))
-                    }
+    Given("로또 번호 목록의 사이즈가 6이 아니면") {
+        forAll(
+            row(5),
+            row(7),
+        ) { pickCount ->
+            Then("RuntimeException 예외 처리를 한다") {
+                shouldThrow<RuntimeException> {
+                    LottoNumbers(LottoNumber.allLottoNumbers().take(pickCount))
                 }
             }
         }
+    }
 
-        Then("중복이 존재하면 RuntimeException 예외 처리를 한다") {
+    Given("로또 번호 목록에 중복이 존재하면") {
+        val lottoNumbers = listOf(1, 2, 3, 4, 5, 5).map { LottoNumber(it) }
+        Then("RuntimeException 예외 처리를 한다") {
             shouldThrow<RuntimeException> {
-                LottoNumbers(listOf(1, 2, 3, 4, 5, 5).map { LottoNumber(it) })
+                LottoNumbers(lottoNumbers)
             }
         }
     }
