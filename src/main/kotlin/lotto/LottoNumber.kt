79bc64e39fd61e0d415c2a1ec4ea0937e051da18
Wordbe <seonghojin3@gmail.com>
2023-06-19 01:35:40 +0900

    fix(LottoNumber): 1~45 로또번호 캐싱

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -1,21 +1,21 @@
 package lotto
 
 @JvmInline
-value class LottoNumber(
-    val number: Int
+value class LottoNumber private constructor(
+    val number: Int,
 ) {
-    init {
-        require(LOTTO_NUMBER_RANGE.contains(number)) {
-            "${LOTTO_NUMBER_RANGE.first}~${LOTTO_NUMBER_RANGE.last} 범위 숫자여야 합니다."
-        }
-    }
-
     override fun toString(): String {
         return "$number"
     }
 
     companion object {
         val LOTTO_NUMBER_RANGE: IntRange = 1..45
+        private val LOTTO_NUMBERS: Map<Int, LottoNumber> = LOTTO_NUMBER_RANGE.associateWith { LottoNumber(it) }
+
+        operator fun invoke(number: Int): LottoNumber {
+            return LOTTO_NUMBERS[number]
+                ?: throw IllegalArgumentException("${LOTTO_NUMBER_RANGE.first}~${LOTTO_NUMBER_RANGE.last} 범위 숫자여야 합니다.")
+        }
 
         fun forBonusOf(number: Int, winningLotto: Lotto): LottoNumber {
             require(winningLotto.lottoNumbers.none { it.number == number }) { "당첨 로또 번호와 보너스 번호는 중복될 수 없습니다." }
