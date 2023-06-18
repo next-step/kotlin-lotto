9ae23476ef161345322626afde20b74373777041
김현준 <4whomtbts@gmail.com>
2023-06-18 22:27:22 +0900

    feat(step1): 버그 수정

--- src/main/kotlin/string/splitter/DefaultSepartorStringSplitter.kt
+++ src/main/kotlin/string/splitter/DefaultSepartorStringSplitter.kt
@@ -10,13 +10,7 @@ class DefaultSeparatorStringSplitter(
         if (singleNumeric != null) {
             return listOf(singleNumeric)
         }
-
-        val delimiter = when {
-            input.contains(',') -> ","
-            input.contains(':') -> ":"
-            else -> return null
-        }
-        return input.split(delimiter).map { converter.convert(it) }
+        return input.split(",|:".toRegex()).map { converter.convert(it) }
     }
 
     private fun parseSingleNumeric(expression: String) = runCatching {
