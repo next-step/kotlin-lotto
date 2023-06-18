1d4b2e33f6f5ca38d22ec18f35b6389c24ee8ed3
chad.yoon <chad.yoon@kakaocorp.com>
2023-06-18 18:32:01 +0900

    refactor(StringParser): 범위 지정을 위해 사용했던 run 함수 제거

--- src/main/kotlin/calculator/StringParser.kt
+++ src/main/kotlin/calculator/StringParser.kt
@@ -11,21 +11,13 @@ object StringParser {
         }
 
         val matchedGroupValues = CUSTOM_DELIMITER_REGEX.find(source)?.groupValues
-        val separationRegex = run {
-            val customDelimiter = matchedGroupValues?.getOrNull(1)
-            val finalDelimiters = if (customDelimiter == null) {
-                FIXED_DELIMITERS
-            } else {
-                FIXED_DELIMITERS.plus(customDelimiter)
-            }
-            Regex("[$finalDelimiters]")
-        }
-        val calculationTargetText = run {
-            val delimitedText = matchedGroupValues?.getOrNull(2)
-            delimitedText ?: source
-        }
+        val delimiters = matchedGroupValues?.getOrNull(1)
+            ?.let { customDelimiter -> FIXED_DELIMITERS.plus(customDelimiter) }
+            ?: FIXED_DELIMITERS
+        val calculationTargetText = matchedGroupValues?.getOrNull(2) ?: source
+
         return calculationTargetText
-            .split(separationRegex)
+            .split(Regex("[$delimiters]"))
             .map { maybeNumber -> PositiveNumber.of(maybeNumber) }
     }
 }
