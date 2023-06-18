c2f541c7996eab36c997946f7a65dd3a5921e247
peter.shlee <shlee4290@naver.com>
2023-06-18 12:32:55 +0900

    feat(step3): 결과 출력 수정

--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -18,9 +18,10 @@ object ResultView {
     fun printReturn(lottoReturn: LottoReturn) {
         println("당첨 통계")
         println("---------")
-        println("3개 일치 (${LottoReturn.FOURTH_RETURN}원)- ${lottoReturn.fourthCount}개")
-        println("4개 일치 (${LottoReturn.THIRD_RETURN}원)- ${lottoReturn.thirdCount}개")
-        println("5개 일치 (${LottoReturn.SECOND_RETURN}원)- ${lottoReturn.secondCount}개")
+        println("3개 일치 (${LottoReturn.FIFTH_RETURN}원)- ${lottoReturn.fifthCount}개")
+        println("4개 일치 (${LottoReturn.FOURTH_RETURN}원)- ${lottoReturn.fourthCount}개")
+        println("5개 일치 (${LottoReturn.THIRD_RETURN}원)- ${lottoReturn.thirdCount}개")
+        println("5개 일치, 보너스 볼 일치 (${LottoReturn.SECOND_RETURN}원)- ${lottoReturn.secondCount}개")
         println("6개 일치 (${LottoReturn.FIRST_RETURN}원)- ${lottoReturn.firstCount}개")
         println("총 수익률은 ${lottoReturn.returnRatio}입니다.")
     }
