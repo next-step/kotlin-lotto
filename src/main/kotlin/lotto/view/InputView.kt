97dc9b242b8d15ce2b9d887e39eef30c606b6e34
sjparkk <sjparkk0820@naver.com>
2023-06-18 10:43:20 +0900

    feat : 보너스 넘버에 대한 입출력 함수 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -11,4 +11,9 @@ object InputView {
         println("지난 주 당첨 번호를 입력해 주세요.")
         return readlnOrNull() ?: throw IllegalArgumentException("입력 값은 null 값이 올 수 없습니다")
     }
+
+    fun printInputBonusLottoNumber(): String {
+        println("보너스 볼을 입력해 주세요.")
+        return readlnOrNull() ?: throw IllegalArgumentException("입력 값은 null 값이 올 수 없습니다")
+    }
 }
