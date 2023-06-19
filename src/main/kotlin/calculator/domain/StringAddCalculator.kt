b95ad3b4a322519f6cb35899efd914cb2d21f227
gray <operasystem6@naver.com>
2023-06-19 09:37:08 +0900

    refactor: Separator, Separators를 없애고 정규식으로 요구사항 충족하도록 변경

--- src/main/kotlin/calculator/domain/StringAddCalculator.kt
+++ src/main/kotlin/calculator/domain/StringAddCalculator.kt
@@ -5,11 +5,7 @@ class StringAddCalculator {
         if (text.isNullOrBlank()) {
             return 0
         }
-        val separator = Separator.create(text)
-        val separators = Separators()
-        separators.add(separator)
-
-        val splitData = TokenizedExpression.generate(text.substringAfter("\n"), separators)
+        val splitData = TokenizedExpression.generate(text)
         return splitData.mapNotNull { it.toIntOrNull() }.sum()
     }
 }
