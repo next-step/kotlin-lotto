89c7fbe53c9ff9283af948e77420fd2fc422c3a3
ggam-nyang <v44ads@naver.com>
2023-06-19 17:41:53 +0900

    refactor: 테스트코드를 ExpectSpec -> BehaviorSpec 으로 변경

--- src/test/kotlin/calculator/StringAddCalculatorTest.kt
+++ src/test/kotlin/calculator/StringAddCalculatorTest.kt
@@ -1,36 +1,38 @@
 package calculator
 
 import calculator.domain.StringAddCalculator
-import io.kotest.core.spec.style.ExpectSpec
+import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 
-internal class StringAddCalculatorTest : ExpectSpec({
-    val calculator = StringAddCalculator()
+internal class StringAddCalculatorTest : BehaviorSpec({
 
-    context("빈 문자열 또는 null을 입력할 경우") {
-        val operations = listOf(
-            "",
-            null
-        )
-        expect("0을 반환한다.") {
-            operations.forAll { operation ->
-                val actual = calculator.execute(operation)
-                actual shouldBe 0
+    Given("StringAddCaluator에서") {
+        val calculator = StringAddCalculator()
+        When("빈 문자열 또는 null을 입력할 경우") {
+            val operations = listOf(
+                "",
+                null
+            )
+            Then("0을 반환한다.") {
+                operations.forAll { operation ->
+                    val actual = calculator.execute(operation)
+                    actual shouldBe 0
+                }
             }
         }
-    }
 
-    context("숫자 한개만 전달된 경우") {
-        val operations = listOf(
-            "1",
-            "23",
-            "456"
-        )
-        expect("해당 값을 반환한다.") {
-            operations.forAll { operation ->
-                val actual = calculator.execute(operation)
-                actual shouldBe operation.toInt()
+        When("숫자 한개만 전달된 경우") {
+            val operations = listOf(
+                "1",
+                "23",
+                "456"
+            )
+            Then("해당 값을 반환한다.") {
+                operations.forAll { operation ->
+                    val actual = calculator.execute(operation)
+                    actual shouldBe operation.toInt()
+                }
             }
         }
     }
