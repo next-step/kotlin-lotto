fc6c819b2f7e7dc8fc3f024922bc11082e2ee8be
sang5c <skah321@gmail.com>
2023-06-19 13:47:42 +0900

    test(StringCalculator): null 또는 empty 문자열을 전달하면 0이 반환된다

--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -3,12 +3,13 @@ package calculator
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.NullAndEmptySource
 
 class StringCalculatorTest {
 
     @ParameterizedTest
     @CsvSource(value = ["1,2=3", "2,3,4=9", "2:5:1=8"], delimiter = '=')
-    fun calculate(expression: String, expected: Long) {
+    fun `문자열을 전달하면 계산된 결과를 반환한다`(expression: String, expected: Long) {
         val calculator = StringCalculator()
 
         val result = calculator.calculate(expression)
@@ -16,4 +17,13 @@ class StringCalculatorTest {
         result shouldBe expected
     }
 
+    @ParameterizedTest
+    @NullAndEmptySource
+    fun `null 또는 empty 문자열을 전달하면 0이 반환된다`(expression: String) {
+        val calculator = StringCalculator()
+
+        val result = calculator.calculate(expression)
+
+        result shouldBe 0
+    }
 }
