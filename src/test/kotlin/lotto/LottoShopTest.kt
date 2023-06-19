f7ca56d45519606e61ea098cdc2f99df1da548d3
Minsung Kim <epicblue@hanmail.net>
2023-06-19 22:28:35 +0900

    refactor(LottoShopTest): 경계값 케이스만 테스트

--- src/test/kotlin/lotto/LottoShopTest.kt
+++ src/test/kotlin/lotto/LottoShopTest.kt
@@ -9,9 +9,9 @@ import lotto.vo.Money
 class LottoShopTest : FreeSpec({
     "로또 상점은 받은 금액으로 로또를 최대한 많이 발급한다 - 로또 가격 1000원" - {
         forAll(
-            row(13400, 13),
+            row(3999, 3),
             row(4000, 4),
-            row(26400, 26)
+            row(4001, 4),
         ) { totalCash, expectedLottoCount ->
 
             val result = LottoShop.sellLottos(
