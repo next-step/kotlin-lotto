4755bc88454166acf8a9f03d4c928c43b2430b24
sjjeong <sjjeong1225@gmail.com>
2023-06-19 19:24:27 +0900

    refactor: hasCustomDelimiter 함수 추가

--- src/main/kotlin/calculate/StringAddCalculator.kt
+++ src/main/kotlin/calculate/StringAddCalculator.kt
@@ -5,11 +5,11 @@ class StringAddCalculator {
         if (text.isNullOrEmpty()) {
             return 0
         }
-        val startIndex = text.indexOf("//") + 2
-        val endIndex = text.indexOf("\n")
+        val startIndex = text.indexOf(CUSTOM_DELIMITER_PREFIX)
+        val endIndex = text.indexOf(CUSTOM_DELIMITER_SUFFIX)
 
-        val result = if (startIndex >= 2 && endIndex >= 0 && startIndex < endIndex) {
-            val customDelimiter = text.substring(startIndex, endIndex)
+        val result = if (hasCustomDelimiter(startIndex, endIndex)) {
+            val customDelimiter = text.substring(startIndex + CUSTOM_DELIMITER_PREFIX.length, endIndex)
             text.substring(endIndex + 1).split(customDelimiter)
         } else {
             text.split(",", ":")
@@ -20,5 +20,12 @@ class StringAddCalculator {
         }
 
         return result
+    }
+
+    private fun hasCustomDelimiter(startIndex: Int, endIndex: Int) = startIndex >= 0 && endIndex >= 0 && (startIndex + CUSTOM_DELIMITER_PREFIX.length) < endIndex
+
+    companion object {
+        private const val CUSTOM_DELIMITER_PREFIX = "//"
+        private const val CUSTOM_DELIMITER_SUFFIX = "\n"
     }
 }
