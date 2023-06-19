1911a7d1e50118ba0f0bbafd7079c2c90c4ea06a
yoo-jaein <y_jaein@naver.com>
2023-06-19 22:07:07 +0900

    refactor: buildList 활용

--- src/main/kotlin/lotto/domain/LottoPurchase.kt
+++ src/main/kotlin/lotto/domain/LottoPurchase.kt
@@ -4,9 +4,10 @@ class LottoPurchase {
     fun purchaseAuto(budget: Int, priceOfLotto: Int): List<Lotto> {
         val amount = affordableLottoCount(budget, priceOfLotto)
 
-        val result = mutableListOf<Lotto>()
-        repeat(amount) {
-            result.add(Lotto.autoCreate())
+        val result = buildList {
+            repeat(amount) {
+                add(Lotto.autoCreate())
+            }
         }
         return result
     }
