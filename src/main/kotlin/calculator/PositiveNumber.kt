f42ba5624669b474bca30a5300fbd292a918ec76
oliveunji <eunjikim2u@gmail.com>
2023-06-20 08:53:01 +0900

    refactor: require 구문에서 이미 에러 핸들링을 함으로 throw 할 필요 없이 에러 관한 메세지만 전달하면 됨.

--- src/main/kotlin/calculator/PositiveNumber.kt
+++ src/main/kotlin/calculator/PositiveNumber.kt
@@ -3,7 +3,7 @@ package calculator
 class PositiveNumber(numbers: List<Int>) {
     init {
         require(numbers.all { it > 0 }) {
-            throw RuntimeException("Negative numbers are not allowed")
+            "Negative numbers are not allowed"
         }
     }
 }
