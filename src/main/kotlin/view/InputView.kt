1ea3f7087fd1f9440aa3aef9e39dd06d296abc1c
박세준 <sjpark@selab.co.kr>
2023-06-19 10:17:45 +0900

    refactor(inputView): 비지니스 로직 분리 / 메소드 이름 리펙토링

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -3,25 +3,27 @@ package view
 import util.InputReader
 import util.Separator
 
-class InputView(private val inputReader: InputReader) {
+class InputView(
+    private val inputReader: InputReader = InputReader { readln() }
+) {
 
-    fun enterMoney(): Int {
+    fun inputMoney(): Int {
         println("구입금액을 입력해 주세요.")
         val money = inputReader.raedLine()
         require(money.toIntOrNull() != null) { "구입 금액이 올바르지 않습니다 : $money" }
         return money.toInt()
     }
 
-    fun registerWinningNums(): Set<Int> {
+    fun getWinningNumbers(): Set<Int> {
         println("\n지난 주 당첨 번호를 입력해 주세요.")
         val winningNums = inputReader.raedLine()
         return Separator.extractIntegers(winningNums).toSet()
     }
 
-    fun registerBonusBall(): Int {
+    fun getBonusNumber(): Int {
         println("보너스 볼을 입력해 주세요.")
         val bonusBall = inputReader.raedLine().toIntOrNull()
-        require(bonusBall != null && bonusBall in 1..45) { "보너스 볼이 올바르지 않습니다 : $bonusBall" }
+        require(bonusBall != null) { "보너스 볼이 올바르지 않습니다 : $bonusBall" }
         return bonusBall.toInt()
     }
 }
