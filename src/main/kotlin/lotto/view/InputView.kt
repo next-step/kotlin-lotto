355339d241fe44406c051f0016a158e1aa35f215
oliveunji <eunjikim2u@gmail.com>
2023-06-19 15:33:05 +0900

    feat: 지난주 당첨 번호를 파싱하는 기능 추가

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,5 +1,7 @@
 package lotto.view
 
+import kotlin.NumberFormatException
+
 private const val UNIT = 1000
 object InputView {
     fun getPurchaseAmount(): Int {
@@ -10,5 +12,13 @@ object InputView {
     fun getLastWinningNumbers(): String {
         println("지난 주 당첨 번호를 입력해 주세요.")
         return readln()
+    }
+
+    fun parseWinningNumbers(input: String): List<Int> {
+        return try {
+            input.split(",").map { it.trim().toInt() }
+        } catch(e: NumberFormatException) {
+            throw NumberFormatException("지난주 당첨 번호를 숫자와 콤마(,)로 구분하여 다시 입력해주시기 바랍니다. ")
+        }
     }
 }
