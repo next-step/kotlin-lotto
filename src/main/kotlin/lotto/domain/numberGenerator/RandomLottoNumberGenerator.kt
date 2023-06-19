3e9da9121eab628b84639d075e60fcc936c77cd6
waveofmymind <sjun0913@gmail.com>
2023-06-19 19:44:10 +0900

    Feat: 뽑을 숫자를 미리 생성

--- src/main/kotlin/lotto/domain/numberGenerator/RandomLottoNumberGenerator.kt
+++ src/main/kotlin/lotto/domain/numberGenerator/RandomLottoNumberGenerator.kt
@@ -3,10 +3,12 @@ package lotto.domain.numberGenerator
 class RandomLottoNumberGenerator : NumberGenerator {
 
     override fun generateNumbers(): List<Int> {
-        return (1..MAX_LOTTO_NUMBER).shuffled().take(NUMBER_OF_LOTTO).sorted()
+        return rangeNumber.shuffled().take(NUMBER_OF_LOTTO).sorted()
     }
+
     companion object {
         private const val MAX_LOTTO_NUMBER = 45
         private const val NUMBER_OF_LOTTO = 6
+        private val rangeNumber = (1..MAX_LOTTO_NUMBER).toList()
     }
 }
