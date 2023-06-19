28b1e334eefbb75c8c3769ecbe4202642818d6ef
pooh <pooh.duck@kakaocorp.com>
2023-06-19 12:55:32 +0900

    chore: step1 테스트 코드 파일 변경

--- src/test/kotlin/step1/SimpleAddExpressionParserTest.kt
+++ src/test/kotlin/step1/SimpleAddExpressionParserTest.kt
@@ -2,7 +2,6 @@ package step1
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
-import java.lang.RuntimeException
 
 class SimpleAddExpressionParserTest : StringSpec({
     "수식에 음수가 포함되어 있을때, RuntimeException을 throw 한다" {
@@ -12,5 +11,3 @@ class SimpleAddExpressionParserTest : StringSpec({
         }
     }
 })
-
-
