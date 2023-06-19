15e144b860076bff3c56d0d662848fa08bd20110
gray <operasystem6@naver.com>
2023-06-19 09:01:33 +0900

    refactor: REGEX_PATTERN 사용 방식 변경

--- src/main/kotlin/calculator/domain/Separator.kt
+++ src/main/kotlin/calculator/domain/Separator.kt
@@ -5,10 +5,8 @@ value class Separator(
     val separator: String
 ) {
     companion object {
-        private const val REGEX_PATTERN = "//(.)\n(.*)"
-
         fun create(text: String): Separator {
-            val regexResult = Regex(REGEX_PATTERN).find(text)
+            val regexResult = Regex("//(.)\n(.*)").find(text)
             val customSeparator = regexResult?.let {
                 it.groupValues[1]
             }
