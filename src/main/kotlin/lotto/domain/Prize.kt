f5736cc594c943cb9dd0ba2b19c2cbc1edf0439f
waveofmymind <sjun0913@gmail.com>
2023-06-19 19:46:40 +0900

    Refactor: 명칭을 로또 등수를 나타내도록 변경

--- src/main/kotlin/lotto/domain/Prize.kt
+++ src/main/kotlin/lotto/domain/Prize.kt
@@ -2,10 +2,10 @@ package lotto.domain
 
 enum class Prize(val matchCount: Int, val amount: Int) {
     NO_PRIZE(0, 0),
-    THIRD_PLACE(3, 5_000),
-    FOURTH_PLACE(4, 50_000),
-    FIFTH_PLACE(5, 1_500_000),
-    WIN(6, 2_000_000_000);
+    FIFTH(3, 5_000),
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    FIRST(6, 2_000_000_000);
 
     companion object {
         fun prizeForMatchCount(matchCount: Int): Prize {
