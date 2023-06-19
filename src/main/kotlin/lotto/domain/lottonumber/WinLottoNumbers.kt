08ab3d3d8f1d1c313f508865ba75ef331f8f761b
chad.yoon <chad.yoon@kakaocorp.com>
2023-06-19 19:08:15 +0900

    feat: 보너스 번호 다른 로또 번호들과 중복 될 수 없는 제약 추가

--- src/main/kotlin/lotto/domain/lottonumber/WinLottoNumbers.kt
+++ src/main/kotlin/lotto/domain/lottonumber/WinLottoNumbers.kt
@@ -3,4 +3,11 @@ package lotto.domain.lottonumber
 data class WinLottoNumbers(
     val lottoNumbers: LottoNumbers,
     val bonusNumber: LottoNumber,
-)
+) {
+
+    init {
+        require(lottoNumbers.value.contains(bonusNumber).not()) {
+            "bonus number can not be duplicated with lotto numbers"
+        }
+    }
+}
