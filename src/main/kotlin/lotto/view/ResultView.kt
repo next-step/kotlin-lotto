9edbd8d61ff2987c35020020295eb3936dd1ede7
waveofmymind <sjun0913@gmail.com>
2023-06-19 20:17:22 +0900

    Feat: 입출력에 보너스 번호와 당첨 통계에 2등 추가

--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -14,17 +14,23 @@ class ResultView {
         println()
     }
 
-    fun printStatistics(matchResult: MatchResult, earningRate: Double) {
+    fun printStatistics(matchResult: MatchResult, money: Int) {
         println()
         println("당첨 통계")
         println("---------")
 
         for (prize in Prize.values()) {
             val numberOfMatchesForPrize = matchResult.getNumberOfMatchesForPrize(prize)
-            println("${prize.matchCount}개 일치 (${prize.amount}원) - ${numberOfMatchesForPrize}개")
+
+            if (prize == Prize.SECOND) {
+                println("5개 일치, 보너스 볼 일치 (${prize.amount}원) - ${numberOfMatchesForPrize}개")
+            } else if (prize != Prize.NO_PRIZE) {
+                println("${prize.matchCount}개 일치 (${prize.amount}원) - ${numberOfMatchesForPrize}개")
+            }
         }
 
-        val isProfit = matchResult.isProfit(earningRate)
+        val isProfit = matchResult.isProfit(money)
+        val earningRate = matchResult.calculateEarningRate(money)
         val resultMessage = if (isProfit) "(이익)" else "(기준이 1이기 때문에 결과적으로 손해라는 의미임)"
         println("총 수익률은 %.2f입니다. $resultMessage".format(earningRate))
     }
