7ed6201b64f8c6350f079287bd882d12960f2aa0
oliveunji <eunjikim2u@gmail.com>
2023-06-19 09:51:39 +0900

    refactor: Delimiter 함수변경에 따라 mainㅍ에서 사용하는 방식 업데이트

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -7,9 +7,9 @@ import calculator.utils.TokenConverter
 class StringAddCalculator {
     fun add(text: String?): Int {
         if (text.isNullOrEmpty()) return 0
-        val delimiters = Delimiters.DEFAULT
+        var delimiters = Delimiters()
         val (customDelimiter, extractResult) = CustomDelimiterExtractor.extract(text)
-        if (!customDelimiter.isNullOrEmpty()) delimiters.addCustomDelimiters(customDelimiter)
+        if (!customDelimiter.isNullOrEmpty()) delimiters = delimiters.addCustomDelimiters(customDelimiter)
         val tokens = TextSplitter.splitText(extractResult, delimiters.getDelimiters())
         val convertedTokens = TokenConverter.convertToInt(tokens)
         PositiveNumber.checkPositiveNumbers(convertedTokens)
