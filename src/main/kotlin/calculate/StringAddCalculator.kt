4ab579498087af115bd438895635dcbba2324866
dino <sjjeong1225@gmail.com>
2023-06-19 23:37:52 +0900

    step1: 문자열 덧셈 계산기 (#684)
    * step1: 문자열 덧셈 계산기
    
    * refactor: require 함수 사용
    
    * refactor: hasCustomDelimiter 함수 추가
    
    * refactor: 반환 타입 변경

---
+++ src/main/kotlin/calculate/StringAddCalculator.kt
@@ -0,0 +1,31 @@
+package calculate
+
+class StringAddCalculator {
+    fun add(text: String?): Int {
+        if (text.isNullOrEmpty()) {
+            return 0
+        }
+        val startIndex = text.indexOf(CUSTOM_DELIMITER_PREFIX)
+        val endIndex = text.indexOf(CUSTOM_DELIMITER_SUFFIX)
+
+        val result = if (hasCustomDelimiter(startIndex, endIndex)) {
+            val customDelimiter = text.substring(startIndex + CUSTOM_DELIMITER_PREFIX.length, endIndex)
+            text.substring(endIndex + 1).split(customDelimiter)
+        } else {
+            text.split(",", ":")
+        }.sumOf { it.toInt() }
+
+        require(result >= 0) {
+            "결과는 0 이상이어야 합니다."
+        }
+
+        return result
+    }
+
+    private fun hasCustomDelimiter(startIndex: Int, endIndex: Int) = startIndex >= 0 && endIndex >= 0 && (startIndex + CUSTOM_DELIMITER_PREFIX.length) < endIndex
+
+    companion object {
+        private const val CUSTOM_DELIMITER_PREFIX = "//"
+        private const val CUSTOM_DELIMITER_SUFFIX = "\n"
+    }
+}
