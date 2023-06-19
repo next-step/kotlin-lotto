ab179ec9ad0c21dc6cd814b2802264b093b9059d
yoo-jaein <y_jaein@naver.com>
2023-06-19 22:19:48 +0900

    test: LottoTest에 '하나의 로또는 6개의 로또 번호로 구성된다' 케이스 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import lotto.domain.Lotto
+import lotto.domain.Lotto.Companion.NUMBER_OF_LOTTO_NUMBERS
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
@@ -8,5 +9,10 @@ class LottoTest {
     @Test
     fun `로또를 자동 생성할 수 있다`() {
         Assertions.assertThat(Lotto.autoCreate()).isInstanceOf(Lotto::class.java)
+    }
+
+    @Test
+    fun `하나의 로또는 6개의 로또 번호로 구성된다`() {
+        Assertions.assertThat(Lotto.autoCreate().numbers.size).isEqualTo(NUMBER_OF_LOTTO_NUMBERS)
     }
 }
