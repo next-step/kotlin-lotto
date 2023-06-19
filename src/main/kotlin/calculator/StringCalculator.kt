a1a071139809cbcc5ff9619abec5597198ca5063
sang5c <skah321@gmail.com>
2023-06-19 13:44:59 +0900

    feat(StringCalculator): 콜론 계산식 기능 구현

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -2,9 +2,18 @@ package calculator
 
 class StringCalculator {
     fun calculate(expression: String): Long {
-        return expression.split(",")
-            .map { it.toLong() }
-            .reduce { acc, l -> acc + l }
+        val numbers = extractNumbers(expression)
+
+        return numbers.sum()
+    }
+
+    private fun extractNumbers(expression: String): List<Long> {
+        if (expression.contains(",")) {
+            return expression.split(",")
+                .map(String::toLong)
+        }
+        return expression.split(":")
+            .map(String::toLong)
     }
 
 }
