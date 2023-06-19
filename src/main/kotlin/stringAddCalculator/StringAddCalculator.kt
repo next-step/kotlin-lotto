e71a900ab8db14ff3e092fe96ae771b76592e98e
jonghyunhub <chuwhdgus@gmail.com>
2023-06-20 01:08:59 +0900

    feat : 숫자 하나 입력받은 경우 해당 숫자 반환 기능 추가

--- src/main/kotlin/stringAddCalculator/StringAddCalculator.kt
+++ src/main/kotlin/stringAddCalculator/StringAddCalculator.kt
@@ -3,7 +3,13 @@ package stringAddCalculator
 class StringAddCalculator {
 
     fun add(text: String?): Int {
-        if (text.isNullOrBlank()) return 0
+        if (text.isNullOrBlank()) return ANSWER_NULL_OR_BLANK
+        if (text.length == CASE_INPUT_LENTGHT_IS_ONE) return text.toInt()
         return 0
+    }
+
+    companion object {
+        const val ANSWER_NULL_OR_BLANK = 0
+        const val CASE_INPUT_LENTGHT_IS_ONE = 1
     }
 }
--- src/test/kotlin/stringAddCalculator/StringAddCalulatorTest.kt
+++ src/test/kotlin/stringAddCalculator/StringAddCalulatorTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
 
 class StringAddCalulatorTest {
 
@@ -18,5 +19,11 @@ class StringAddCalulatorTest {
     @NullAndEmptySource()
     fun `빈 문자열 또는 null을 입력할 경우 0을 반환해야 한다`(text: String?) {
         Assertions.assertThat(calculator.add(text)).isZero()
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "2", "3"])
+    fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(text: String?) {
+        Assertions.assertThat(calculator.add(text)).isSameAs(text?.toInt())
     }
 }
