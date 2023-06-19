40e262426fa9f0311b79b83613af289d59a64606
hyunju-seo <hyunju6889@naver.com>
2023-06-20 07:19:23 +0900

    refactor: 리뷰반영 테스트이름 상세하게

--- src/test/kotlin/lotto/domain/LottoResultTest.kt
+++ src/test/kotlin/lotto/domain/LottoResultTest.kt
@@ -4,13 +4,13 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
 class LottoResultTest : BehaviorSpec({
-    given("LottoResult") {
+    given("3등, 3등, 미당첨, 4등의 경우") {
         val lottoRanks = listOf(LottoRank.THIRD, LottoRank.THIRD, LottoRank.NONE, LottoRank.FOURTH)
         val lottoResult = LottoResult(lottoRanks)
 
         `when`("로또 당첨 통계를 구하면") {
             val statistic = lottoResult.getLottoRankStatistic()
-            then("올바른 결과를 리턴한다.") {
+            then("3등 2개, 4등 1개, 미당첨 1개의 결과를 리턴한다.") {
                 statistic[LottoRank.FIRST] shouldBe 0
                 statistic[LottoRank.SECOND] shouldBe 0
                 statistic[LottoRank.THIRD] shouldBe 2
@@ -21,7 +21,7 @@ class LottoResultTest : BehaviorSpec({
 
         `when`("로또 당첨 수익률을 구하면") {
             val profitRate = lottoResult.getProfitRate(1000)
-            then("올바른 결과를 리턴한다.") {
+            then("수익률을 26.25를 리턴한다.") {
                 profitRate shouldBe 26.25
             }
         }
