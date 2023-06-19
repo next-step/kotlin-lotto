3e052959e6a785727cec4c1e05c67c6b88483765
waveofmymind <sjun0913@gmail.com>
2023-06-19 19:16:46 +0900

    Feat: 미당첨에 대한 열거형 추가

--- src/main/kotlin/lotto/domain/Prize.kt
+++ src/main/kotlin/lotto/domain/Prize.kt
@@ -1,14 +1,15 @@
 package lotto.domain
 
 enum class Prize(val matchCount: Int, val amount: Int) {
+    NO_PRIZE(0, 0),
     THIRD_PLACE(3, 5_000),
     FOURTH_PLACE(4, 50_000),
     FIFTH_PLACE(5, 1_500_000),
     WIN(6, 2_000_000_000);
 
     companion object {
-        fun prizeForMatchCount(matchCount: Int): Prize? {
-            return values().find { it.matchCount == matchCount }
+        fun prizeForMatchCount(matchCount: Int): Prize {
+            return values().find { it.matchCount == matchCount } ?: NO_PRIZE
         }
     }
 }
