0dac1b6e4f3499ce053c5d4f540c70bc52c92d20
turner.lee <turner.lee@kakaocorp.com>
2023-06-20 18:15:30 +0900

    refactor: Rank 로또 당첨 금액 가독성을 위해 언더바 추가

--- src/main/kotlin/lotto/Rank.kt
+++ src/main/kotlin/lotto/Rank.kt
@@ -1,10 +1,10 @@
 package lotto
 
 enum class Rank(val matchedCount: Int, val reward: Long) {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FORTH(3, 5000),
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FORTH(3, 5_000),
     LOSE(0, 0);
 
     companion object {
