bf1c21db7231c66c0d49d28b120f64f26dc972f2
yoo-jaein <y_jaein@naver.com>
2023-06-20 23:09:42 +0900

    refactor: LOTTO_NUMBERS를 List<LottoNumber>로 변경하여 로또 번호 객체를 캐싱하도록 수정

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -6,22 +6,17 @@ value class LottoNumber private constructor(
 ) {
     init {
         require(number > 0) { "로또 번호는 양수여야 합니다." }
-        require(number in LOTTO_NUMBERS) { "로또 번호는 1~45 사이의 숫자여야 합니다." }
+        require(number in LOTTO_NUMBERS_RANGE) { "로또 번호는 1~45 사이의 숫자여야 합니다." }
     }
 
     companion object {
-        val LOTTO_NUMBERS: List<Int> = (1..45).toList()
-        private val NUMBERS: MutableMap<Int, LottoNumber> = mutableMapOf()
-
-        fun from(value: Int): LottoNumber {
-            if (!NUMBERS.contains(value)) {
-                NUMBERS[value] = LottoNumber(value)
-            }
-            return NUMBERS[value] ?: throw IllegalArgumentException()
-        }
+        val LOTTO_NUMBERS_RANGE = 1..45
+        private val LOTTO_NUMBERS = LOTTO_NUMBERS_RANGE.map { LottoNumber(it) }.toList()
 
         fun create(number: Int): LottoNumber = LottoNumber(number)
-        fun createRandom(size: Int): List<LottoNumber> = LOTTO_NUMBERS.shuffled().take(size).map { LottoNumber(it) }
+
+        fun createRandom(size: Int): List<LottoNumber> = LOTTO_NUMBERS.shuffled().take(size)
+
         fun createList(numbers: List<Int>): List<LottoNumber> = numbers.map { LottoNumber(it) }
     }
 }
