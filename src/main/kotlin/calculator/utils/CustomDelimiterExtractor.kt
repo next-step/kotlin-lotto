c55be2fc34a4571c122d5765fae9ac5c0fc2f667
oliveunji <eunjikim2u@gmail.com>
2023-06-20 09:07:19 +0900

    refactor: regex를 함수 호출시마다 생성하는것이 아니라, 생성해놓고 불러다 쓰는 방식으로 코드 변경

--- src/main/kotlin/calculator/utils/CustomDelimiterExtractor.kt
+++ src/main/kotlin/calculator/utils/CustomDelimiterExtractor.kt
@@ -3,8 +3,8 @@ package calculator.utils
 private const val CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)"
 
 object CustomDelimiterExtractor {
+    private val regex = Regex(CUSTOM_DELIMITER_PATTERN)
     fun extract(text: String): Pair<String, String> {
-        val regex = Regex(CUSTOM_DELIMITER_PATTERN)
         val matchResult = regex.find(text)
         val extractedDelimiter = matchResult?.groupValues?.getOrNull(1) ?: ""
         val extractedText = matchResult?.groupValues?.getOrNull(2) ?: text
