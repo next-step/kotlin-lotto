f346fcb2e8f5bf993f1b4a65fb52a4714dd22c12
celine.kk <celine.kk@kakaocorp.com>
2023-06-20 12:25:46 +0900

    feat. 리뷰반영: lotto 미리 생성

--- src/main/kotlin/lotto/domain/LottoNumGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoNumGenerator.kt
@@ -6,12 +6,13 @@ interface LottoNumGenerator {
 
 class ShuffleNumGenerator : LottoNumGenerator {
     override fun getNums(): List<Int> {
-        return (LOTTO_START_NUM..LOTTO_LAST_NUM).toList().shuffled().take(LOTTO_NUM_COUNT).sorted()
+        return lottoNums.shuffled().take(LOTTO_NUM_COUNT).sorted()
     }
 
     companion object {
         private const val LOTTO_START_NUM = 1
         private const val LOTTO_LAST_NUM = 45
         private const val LOTTO_NUM_COUNT = 6
+        private val lottoNums = (LOTTO_START_NUM..LOTTO_LAST_NUM).toList()
     }
 }
