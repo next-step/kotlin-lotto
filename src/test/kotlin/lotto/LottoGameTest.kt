7ccf29dc0a90cae24d4b776159f57074f9fe7d81
turner.lee <turner.lee@kakaocorp.com>
2023-06-20 18:27:06 +0900

    refactor: 불필요한 ParameterizedTest 방식 일반 테스트로 변경

--- src/test/kotlin/lotto/LottoGameTest.kt
+++ src/test/kotlin/lotto/LottoGameTest.kt
@@ -14,9 +14,8 @@ class LottoGameTest {
         lottoGame.lottoNumbers.size shouldBe 10
     }
 
-    @ParameterizedTest
-    @MethodSource("listProvider")
-    fun `로또 게임은 로또 번호들과 당첨 번호를 받아 결과를 생성한다`(texts: List<String>) {
+    @Test
+    fun `로또 게임은 로또 번호들과 당첨 번호를 받아 결과를 생성한다`() {
         val winningNumbers = WinningNumbers(LottoNumbers.from(listOf(1, 2, 3, 4, 5, 6)))
 
         val numbers = listOf(
@@ -38,22 +37,5 @@ class LottoGameTest {
         result[Rank.THIRD] shouldBe 1
         result[Rank.FIRST] shouldBe 1
         result[Rank.LOSE] shouldBe 3
-    }
-
-    companion object {
-        @JvmStatic
-        fun listProvider(): Stream<List<String>> {
-            return Stream.of(
-                listOf(
-                    "1,2,3,4,5,6",
-                    "1,2,3,4,5,7",
-                    "1,2,3,4,7,8",
-                    "1,2,3,7,8,9",
-                    "1,2,7,8,9,10",
-                    "1,7,8,9,10,11",
-                    "7,8,9,10,11,12",
-                ),
-            )
-        }
     }
 }
