f3b2d7c7c7f96dd98c9ff65e17f68fbf5c7e37ee
genos-lee <genos.lee@kakaocorp.com>
2023-06-20 18:58:28 +0900

    refactor : shuffled 사용해서 로또 번호 생성하도록 수정

--- src/main/kotlin/next/step/lotto/domain/LottoNumberGenerator.kt
+++ src/main/kotlin/next/step/lotto/domain/LottoNumberGenerator.kt
@@ -1,13 +1,11 @@
 package next.step.lotto.domain
 
-import kotlin.random.Random
-
 object LottoNumberGenerator {
+
+    private val lottoNumbers: List<LottoNumber> =
+        (LottoNumber.MIN_LOTTO_NUMBER..LottoNumber.MAX_LOTTO_NUMBER).map { LottoNumber.of(it) }
+
     fun random(): Set<LottoNumber> {
-        val numbers = mutableSetOf<Int>()
-        while (numbers.size != Lotto.LOTTO_NUMBER_CNT) {
-            numbers.add(Random.nextInt(LottoNumber.MIN_LOTTO_NUMBER, LottoNumber.MAX_LOTTO_NUMBER + 1))
-        }
-        return numbers.map { LottoNumber.of(it) }.toSet()
+        return lottoNumbers.shuffled().take(6).toSet()
     }
 }
