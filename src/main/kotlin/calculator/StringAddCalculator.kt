1a689127e11bd8c83385db0b64a4a6d208c3b4dd
이정수 <lsecret@naver.com>
2023-06-20 19:52:54 +0900

    [Step2] refactor: step 1 코드리뷰 반영

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -1,23 +1,23 @@
 package calculator
 
 class StringAddCalculator {
-    fun add(text: String?): Int {
-        if (text.isNullOrBlank()) {
+    fun add(expression: String?): Int {
+        if (expression.isNullOrBlank()) {
             return 0
         }
 
-        var inputString = text
+        var currentExpression = expression
         var delimiter = DEFAULT_DELIMITER
 
-        val matches = DELIMITER_REGEX.find(inputString)
+        val matches = DELIMITER_REGEX.find(currentExpression)
         if (matches != null) {
             delimiter = matches.groupValues[DELIMITER_MATCHER_GROUP]
-            inputString = matches.groupValues[INPUT_NUMBER_MATCHER_GROUP]
+            currentExpression = matches.groupValues[INPUT_NUMBER_MATCHER_GROUP]
         }
 
         var sum = 0
 
-        splitDelimiter(inputString, delimiter)
+        splitDelimiter(currentExpression, delimiter)
             .map { PositiveNumber(it) }
             .forEach { sum = it.add(sum) }
 
