2398a5079b5438ef29a9ae7c3346d6e4e5e6f258
celine.kk <celine.kk@kakaocorp.com>
2023-06-21 16:01:41 +0900

    feat. 보너스볼을 입력받는다.

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -2,6 +2,8 @@ package lotto.view
 
 private const val MONEY_STRING = "구입금액을 입력해 주세요."
 
+private const val BONUS_STRING = "보너스 볼을 입력해 주세요."
+
 private const val LAST_NUM_STRING = "\n지난 주 당첨 번호를 입력해 주세요."
 
 object InputView {
@@ -13,5 +15,10 @@ object InputView {
     fun getLastLottoNums(): List<Int> {
         println(LAST_NUM_STRING)
         return readln().split(",").map { it.trim().toInt() }
+    }
+
+    fun getBonusNum(): Int {
+        println(BONUS_STRING)
+        return readln().toInt()
     }
 }
