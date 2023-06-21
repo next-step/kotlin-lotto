85a95a117679af005be33bbcc11bf51ef810a5a0
박세준 <sjpark@selab.co.kr>
2023-06-21 16:27:37 +0900

    refactor: 테스트에 맞는 테스트 이름으로 변경

--- src/test/kotlin/domain/WinningCheckerTest.kt
+++ src/test/kotlin/domain/WinningCheckerTest.kt
@@ -6,16 +6,17 @@ import io.kotest.matchers.shouldBe
 import mock.MockNumberGenerator
 
 class WinningCheckerTest : StringSpec({
-    "클라이언트가 구입 금액을 정상적으로 입력한다." {
+    "로또 번호에 맞춰서 Prize가 당첨 금액을 반환한다." {
         listOf(
-            setOf(1, 2, 3, 4, 7, 9) to Prize.FOURTH_PLACE,
-            setOf(1, 2, 3, 4, 5, 8) to Prize.THIRD_PLACE,
-            setOf(1, 2, 3, 4, 5, 10) to Prize.SECOND_PLACE,
-            setOf(1, 2, 3, 4, 5, 6) to Prize.FIRST_PLACE,
+            setOf(40, 41, 42, 43, 44, 55) to 0,
+            setOf(1, 2, 3, 4, 7, 9) to Prize.FOURTH_PLACE.prizeMoney,
+            setOf(1, 2, 3, 4, 5, 8) to Prize.THIRD_PLACE.prizeMoney,
+            setOf(1, 2, 3, 4, 5, 10) to Prize.SECOND_PLACE.prizeMoney,
+            setOf(1, 2, 3, 4, 5, 6) to Prize.FIRST_PLACE.prizeMoney,
         ).forAll { (input, expected) ->
             val testLottery = Lottery(MockNumberGenerator(input))
             val winningNums = setOf(1, 2, 3, 4, 5, 6)
-            val prize = WinningFinder(winningNums, 10).getPrizeByMatched(testLottery)
+            val prize = WinningFinder(winningNums, 10).getPrizeMoneyByMatched(testLottery)
 
             prize shouldBe expected
         }
