ba7513cff12d93d6e52f84a29e843694309bad57
김현준 <4whomtbts@gmail.com>
2023-06-22 02:00:33 +0900

    feat(step3): 입력으로 보너스 볼을 받는다

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,5 +1,7 @@
 package lotto.view
 
+import lotto.Lotto
+
 object InputView {
     fun getTotalPrice(): Int {
         println("구입금액을 입력해 주세요.")
@@ -10,7 +12,13 @@ object InputView {
         println("지난 주 당첨 번호를 입력해 주세요.")
         val input = readln()
         val numbers = input.split(", ").map { it.toInt() }
-        require(numbers.size == 6) { "당첨 번호는 정확히 6개여야합니다." }
+        require(numbers.size == Lotto.MAX_NUMBER_COUNT) { "당첨 번호는 정확히 6개여야합니다." }
         return numbers
+    }
+
+    fun getBonusNumber(): Int {
+        println("보너스 볼을 입력해주세요.")
+        val input = readln()
+        return input.toInt()
     }
 }
