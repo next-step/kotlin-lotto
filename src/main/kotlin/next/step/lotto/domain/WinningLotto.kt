760614bad6a354617f37c554cd18aa01af7f0fd9
genos-lee <genos.lee@kakaocorp.com>
2023-06-21 23:17:54 +0900

    refactor : WinningLotto match logic 메서드 분리

--- src/main/kotlin/next/step/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/next/step/lotto/domain/WinningLotto.kt
@@ -1,10 +1,16 @@
 package next.step.lotto.domain
 
 data class WinningLotto(val winningNumbers: Set<LottoNumber>, val bonusNumber: LottoNumber) {
-    
-    fun match(lottos: Lottos): LottoWinningStat =
-        LottoWinningStat.of(lottos.lottos.groupingBy { LottoRank.from(it.match(winningNumbers), it.match(bonusNumber)) }
-            .eachCount())
+
+    fun match(lottos: Lottos): LottoWinningStat = LottoWinningStat.of(lottoWinningStat(lottos))
+
+    private fun lottoWinningStat(lottos: Lottos) = lottos.lottos.groupingBy { lottoRank(it) }.eachCount()
+
+    private fun lottoRank(lotto: Lotto) = LottoRank.from(matchNumbers(lotto), matchNumber(lotto))
+
+    private fun matchNumbers(lotto: Lotto) = lotto.match(winningNumbers)
+
+    private fun matchNumber(lotto: Lotto) = lotto.match(bonusNumber)
 
     companion object {
         fun of(winningNumbers: Set<LottoNumber>, bonusNumber: LottoNumber): WinningLotto =
