7d1d8e43d502ca1736ba842b30fe7dfbdb4002cf
neity16 <neity16@daum.net>
2023-06-21 19:35:50 +0900

    [step3] refactor: 정적 변수 접근제어자 수정

--- src/main/kotlin/lotto/sixFortyFiveNumberLotto/SixFortyFiveLotto.kt
+++ src/main/kotlin/lotto/sixFortyFiveNumberLotto/SixFortyFiveLotto.kt
@@ -26,7 +26,7 @@ class SixFortyFiveLotto(val numbers: List<SixFortyFiveNumber>) :
     }
 
     companion object {
-        val LOTTO_NUMBER_RANGE = (1..45)
+        private val LOTTO_NUMBER_RANGE = (1..45)
         private const val LOTTO_NUMBER_COUNT = 6
         const val LOTTO_PRICE = 1000
 
