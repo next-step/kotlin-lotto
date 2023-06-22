d21ab26710b27a59e626108352517218192872be
박세준 <sjpark@selab.co.kr>
2023-06-22 15:30:48 +0900

    refactor: 컨트롤러에서 너무 많은 기능을 담당하고 있어서, Service 객체로 기능을 분리

--- src/main/kotlin/LotteryController.kt
+++ src/main/kotlin/LotteryController.kt
@@ -1,48 +1,9 @@
-import domain.Lottery
-import domain.Prize
-import domain.Settlement
-import domain.WinningFinder
-import view.InputView
-import view.OutputView
-
-class LotteryController(
-    private val inputView: InputView = InputView(),
-) {
+class LotteryController {
 
     fun startLotteryService() {
-        val lotteries = purchaseLotteries()
-        val prizeCountMap = LotteryCalculator.calculatePrizes(lotteries, getWinningFinder())
-        for (prize in Prize.values()) OutputView.reportPrize(prize.prizeMessage, prizeCountMap[prize.matches] ?: 0)
-        OutputView.reportProfit(Settlement.calculateProfitRate(prizeCountMap, lotteries.size))
-    }
-
-    private fun purchaseLotteries(): List<Lottery> {
-        val (money, manualSize) = inputMoneyAndManualSize()
-        val manualLotteries = inputView.inputManualNums(manualSize)
-        val lotteries = LotteryMachine.buyAutomaticLotteries(money / 1000 - manualSize, manualLotteries)
-        reportPurchasedState(manualSize, lotteries)
-
-        return lotteries
-    }
-
-    private fun inputMoneyAndManualSize(): Pair<Int, Int> {
-        val money = inputView.inputMoney()
-        val manualSize = inputView.inputManualSize()
-        LotteryMachine.checkManualSize(money, manualSize)
-
-        return Pair(money, manualSize)
-    }
-
-    private fun reportPurchasedState(manualSize: Int, lotteries: List<Lottery>) {
-        OutputView.reportPurchaseCount(manualSize, lotteries.size)
-        lotteries.forEach { lottery -> OutputView.reportPrizeState(lottery) }
-    }
-
-    private fun getWinningFinder(): WinningFinder {
-        val winningNums = inputView.getWinningNums()
-        val bonusBall = inputView.getBonusBall()
-        require(bonusBall in 1..45) { "보너스 볼은 1부터 45사이의 값이여야합니다: $bonusBall" }
-
-        return WinningFinder(winningNums, bonusBall)
+        val order = LotteryService.createOrder()
+        val lotteries = LotteryService.purchaseByOrder(order)
+        val prizeCountMap = LotteryService.checkAndCountLotto(lotteries)
+        LotteryService.reportResult(prizeCountMap, lotteries.size)
     }
 }
