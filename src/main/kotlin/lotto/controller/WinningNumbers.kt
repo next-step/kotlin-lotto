1c4a6c0bb685b7b63e43b7ca0671d441b478d8a1
waveofmymind <sjun0913@gmail.com>
2023-06-22 10:18:17 +0900

    Refactor: 메서드 라인 변경

--- src/main/kotlin/lotto/controller/WinningNumbers.kt
+++ src/main/kotlin/lotto/controller/WinningNumbers.kt
@@ -1,15 +1,15 @@
 package lotto.controller
 
+import lotto.domain.LottoNumber
 import lotto.domain.LottoNumbers
 import lotto.domain.Lottos
 import lotto.domain.Prize
 
-data class WinningNumbers(val lottoNumbers: LottoNumbers, val bonusNumber: Int) {
+data class WinningNumbers(val lottoNumbers: LottoNumbers, val bonusNumber: LottoNumber) {
 
     fun calculateMatchResult(lottos: Lottos): MatchResult {
         val matches = lottos.lottoList.asSequence()
-            .map { Pair(countMatches(it), isBonusMatch(it)) }
-            .map { classifyPrize(it.first, it.second) }
+            .map { Prize.prizeFor(countMatches(it), isBonusMatch(it)) }
             .groupingBy { it }
             .eachCount()
         return MatchResult(matches)
@@ -20,19 +20,12 @@ data class WinningNumbers(val lottoNumbers: LottoNumbers, val bonusNumber: Int) 
     }
 
     private fun isBonusMatch(other: LottoNumbers): Boolean {
-        return other.lottoNumbers.contains(bonusNumber)
-    }
-
-    private fun classifyPrize(matchCount: Int, hasBonus: Boolean): Prize {
-        if (matchCount == 5 && hasBonus) {
-            return Prize.SECOND
-        }
-        return Prize.prizeForMatchCount(matchCount)
+        return other.lottoNumbers.any { it.value == bonusNumber.value }
     }
 
     companion object {
         fun of(lottoNumbers: LottoNumbers, bonusNumber: Int): WinningNumbers {
-            return WinningNumbers(lottoNumbers, bonusNumber)
+            return WinningNumbers(lottoNumbers, LottoNumber(bonusNumber))
         }
     }
 }
