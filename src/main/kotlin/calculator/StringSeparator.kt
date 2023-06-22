e1869ccb4d735c59de87e9a4bb825f6d6b6a50a9
songyi00 <ksl2950@naver.com>
2023-06-22 19:54:20 +0900

    refactor: step1 피드백 반영

--- src/main/kotlin/calculator/StringSeparator.kt
+++ src/main/kotlin/calculator/StringSeparator.kt
@@ -1,24 +1,24 @@
 package calculator
 
-class StringSeparator : Separator {
+object StringSeparator : Separator {
+
+    private val DEFAULT_DELIMITERS = listOf(",", ":")
+    private const val START_DELIMITER = "//"
+    private const val END_DELIMITER = "\\n"
+    private val REGEX = "$START_DELIMITER(.)$END_DELIMITER(.*)".toRegex()
+
     override fun separate(expression: String): List<Int> {
         return REGEX.find(expression)?.let {
             val customDelimiter = it.groupValues[1]
-            getExpressionAfterFinalDelimiter(expression)
+            getExpressionAfterEndDelimiter(expression)
                 ?.split(customDelimiter)
                 ?.map { token -> token.toInt() } ?: emptyList()
         } ?: expression.split(*DEFAULT_DELIMITERS.toTypedArray())
             .map { token -> token.toInt() }
     }
 
-    private fun getExpressionAfterFinalDelimiter(expression: String) =
+    private fun getExpressionAfterEndDelimiter(expression: String) =
         Regex("$END_DELIMITER(.*)").find(expression)
-            ?.groupValues?.get(1)
-
-    companion object {
-        val DEFAULT_DELIMITERS = listOf(",", ":")
-        private const val START_DELIMITER = "//"
-        private const val END_DELIMITER = "\\n"
-        val REGEX = "$START_DELIMITER(.)$END_DELIMITER(.*)".toRegex()
-    }
+            ?.groupValues
+            ?.get(1)
 }
