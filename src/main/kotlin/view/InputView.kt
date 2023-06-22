d44526fd33d958eb574fe89a610fce3a0eacda4d
박세준 <sjpark@selab.co.kr>
2023-06-22 13:55:33 +0900

    refactor: 더 이상 사용하지 않는 InputReader 제거

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,17 +1,14 @@
 package view
 
 import domain.Lottery
-import util.InputReader
 import util.ManualNumberGenerator
 import util.Separator
 
-class InputView(
-    private val inputReader: InputReader = InputReader { readln() },
-) {
+class InputView {
 
     fun inputMoney(): Int {
         println("구입금액을 입력해 주세요.")
-        val money = inputReader.raedLine().toIntOrNull()
+        val money = readln().toIntOrNull()
         require(money != null) { "구입 금액이 올바르지 않습니다 : $money" }
 
         return money
@@ -19,14 +16,14 @@ class InputView(
 
     fun getWinningNums(): Set<Int> {
         println("\n지난 주 당첨 번호를 입력해 주세요.")
-        val winningNums = inputReader.raedLine()
+        val winningNums = readln()
 
         return Separator.extractIntegers(winningNums).toSet()
     }
 
     fun getBonusBall(): Int {
         println("보너스 볼을 입력해 주세요.")
-        val bonusBall = inputReader.raedLine().toIntOrNull()
+        val bonusBall = readln().toIntOrNull()
         require(bonusBall != null) { "보너스 볼이 올바르지 않습니다 : $bonusBall" }
 
         return bonusBall
@@ -34,7 +31,7 @@ class InputView(
 
     fun inputManualSize(): Int {
         println("\n수동으로 구매할 로또 수를 입력해 주세요.")
-        val manualSize = inputReader.raedLine().toIntOrNull()
+        val manualSize = readln().toIntOrNull()
         require(manualSize != null) { "수동으로 구매할 로또 수가 잘못되었습니다. : $manualSize" }
 
         return manualSize
