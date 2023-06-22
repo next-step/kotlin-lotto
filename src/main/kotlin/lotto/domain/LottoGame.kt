e4047566c2746ef153c541e746f2c5c9da2dce88
jayden-17 <jayden.17@kakaocorp.com>
2023-06-22 17:35:26 +0900

    feat : LottoGame 주 생성자 변경

--- src/main/kotlin/lotto/domain/LottoGame.kt
+++ src/main/kotlin/lotto/domain/LottoGame.kt
@@ -1,11 +1,11 @@
 package lotto.domain
 
-class LottoGame(private val winning: WinningLotto) {
+class LottoGame(winningNumber: List<Int>, bonusNumber: Int) {
+    private val winning: WinningLotto
 
-    constructor(
-        winningNumber: List<Int>,
-        bonusNumber: Int
-    ) : this(WinningLotto(Lotto(winningNumber.map { LottoNumber.of(it) }), LottoNumber.of(bonusNumber)))
+    init {
+        this.winning = WinningLotto(Lotto(winningNumber.map { LottoNumber.of(it) }), LottoNumber.of(bonusNumber))
+    }
 
     fun match(lottos: List<Lotto>): LottoResultSummary {
         return LottoResultSummary(lottos.map { match(it) })
