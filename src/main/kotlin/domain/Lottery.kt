b6f11e6ba9f185122a2c1e3ff62069e06c9cd039
박세준 <sjpark@selab.co.kr>
2023-06-22 15:31:17 +0900

    refactor: Lottery에서 매칭되는 값 확인 및 보너스 볼을 가지고 있는지 확인

--- src/main/kotlin/domain/Lottery.kt
+++ src/main/kotlin/domain/Lottery.kt
@@ -6,4 +6,12 @@ import util.RandomNumberGenerator
 class Lottery(numberGenerator: NumberGenerator = RandomNumberGenerator) {
 
     val lotteryNumbers = numberGenerator.randomNumbers()
+
+    fun matchedCount(winningNums: Set<Int>): Int {
+        return lotteryNumbers.intersect(winningNums).size
+    }
+
+    fun containBonusBall(bonusBall: Int): Boolean {
+        return lotteryNumbers.contains(bonusBall)
+    }
 }
