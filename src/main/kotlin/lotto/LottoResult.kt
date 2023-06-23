fd4b6905e47b7295fc0a17cfcfe97545b79ac78d
songyi00 <ksl2950@naver.com>
2023-06-23 20:19:13 +0900

    feat: 결과에 대한 수익률을 계산해 반환한다.

--- src/main/kotlin/lotto/LottoResult.kt
+++ src/main/kotlin/lotto/LottoResult.kt
@@ -1,3 +1,30 @@
 package lotto
 
-class LottoResult(val map: Map<Int, Int>)
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+class LottoResult(val map: Map<Int, Int>) {
+    fun calculateProfitRate(purchaseAmount: Double): Double {
+        val profitRate = getTotalPrize() / purchaseAmount
+        return BigDecimal(profitRate).setScale(SCALE, RoundingMode.DOWN).toDouble()
+    }
+
+    private fun getTotalPrize(): Int {
+        var total = 0
+        for ((num, price) in WINNING_TABLE) {
+            map[num]?.let { total += it * price }
+        }
+        return total
+    }
+
+
+    companion object {
+        const val SCALE = 2
+        val WINNING_TABLE = mapOf(
+            3 to 5000,
+            4 to 50000,
+            5 to 1500000,
+            6 to 2000000000
+        )
+    }
+}
---
+++ src/test/kotlin/lotto/LottoResultTest.kt
@@ -0,0 +1,18 @@
+package lotto
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class LottoResultTest : FunSpec({
+    test("결과에 대한 수익률을 계산해 반환한다.") {
+        // given
+        val lottoResult = LottoResult(mapOf(3 to 1))
+        val purchaseAmount = 14000.0
+        val expected = 0.35
+        // when
+        val actual = lottoResult.calculateProfitRate(purchaseAmount)
+        // then
+        actual shouldBe expected
+    }
+
+})
