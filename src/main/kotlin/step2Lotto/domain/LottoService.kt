93d309f9274a28b0c94106a1dc602aa29e576181
KimTaeHoon <sksskaw@lendingmachine.co.kr>
2023-06-23 17:49:17 +0900

    몇장을 구매할 수 있는지 계산 기능 구현

--- src/main/kotlin/step2Lotto/domain/LottoService.kt
+++ src/main/kotlin/step2Lotto/domain/LottoService.kt
@@ -1,5 +1,11 @@
 package step2Lotto.domain
 
 class LottoService {
+    fun getLottoTicketQuantity(purchaseAmount: Int): Int {
+        return purchaseAmount / LOTTO_PRICE
+    }
 
+    companion object {
+        private const val LOTTO_PRICE = 1000
+    }
 }
