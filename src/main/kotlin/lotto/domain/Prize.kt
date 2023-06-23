e21171cf153f6e71745878a0df3fb3a9664f3208
waveofmymind <sjun0913@gmail.com>
2023-06-24 06:12:23 +0900

    Refactor: 생성자 기본값 사용하도록 변경

--- src/main/kotlin/lotto/domain/Prize.kt
+++ src/main/kotlin/lotto/domain/Prize.kt
@@ -1,11 +1,11 @@
 package lotto.domain
-enum class Prize(val matchCount: Int, val amount: Int, val requiresBonus: Boolean) {
-    NO_PRIZE(0, 0, false),
-    FIFTH(3, 5_000, false),
-    FOURTH(4, 50_000, false),
-    THIRD(5, 1_500_000, false),
+enum class Prize(val matchCount: Int, val amount: Int, val requiresBonus: Boolean = false) {
+    NO_PRIZE(0, 0),
+    FIFTH(3, 5_000),
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
     SECOND(5, 30_000_000, true),
-    FIRST(6, 2_000_000_000, false);
+    FIRST(6, 2_000_000_000);
     companion object {
         fun prizeFor(matchCount: Int, isBonusMatch: Boolean): Prize {
             values().find { it.matchCount == matchCount && it.requiresBonus == isBonusMatch }?.let { return it }
