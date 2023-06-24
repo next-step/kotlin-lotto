fbc46711ae4cbaa6607d1b23238d3e60432762e6
waveofmymind <sjun0913@gmail.com>
2023-06-25 07:18:17 +0900

    Refactor: 클래스 네이밍 변경

--- src/main/kotlin/lotto/domain/numberGenerator/FixedLottoNumberGenerator.kt
+++ src/main/kotlin/lotto/domain/numberGenerator/FixedLottoNumberGenerator.kt
@@ -2,7 +2,7 @@ package lotto.domain.numberGenerator
 
 import lotto.domain.LottoNumber
 
-class FixedLottoLottoNumberGenerator(private val fixedNumbers: List<Int>) : LottoNumberGenerator {
+class FixedLottoNumberGenerator(private val fixedNumbers: List<Int>) : LottoNumberGenerator {
 
     override fun generateNumbers(): List<LottoNumber> {
         return fixedNumbers.map { LottoNumber(it) }
