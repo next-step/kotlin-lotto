a9881113fb208835136deac991cc188d283f32d2
gray <operasystem6@naver.com>
2023-06-24 17:24:32 +0900

    test: 수동 로또에 대한 테스트 코드 추가 작성 및 테스트 코드 보충

--- src/test/kotlin/lotto/domain/LottoGeneratorTest.kt
+++ src/test/kotlin/lotto/domain/LottoGeneratorTest.kt
@@ -1,6 +1,8 @@
 package lotto.domain
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 
 class LottoGeneratorTest : StringSpec({
@@ -19,9 +21,71 @@ class LottoGeneratorTest : StringSpec({
         lottoNumbers.size shouldBe count
     }
 
-    "수동, 자동 로또를 개수만큼 생성한다." {
-        val count = 12
-        val lottos = lottoGenerator.generateLottos(count, Lottos(mutableListOf()))
-        lottos.lottoNumbers.size shouldBe 12
+    "중복되지 않은 로또 번호를 생성해야 한다." {
+        val lottoNumbers = lottoGenerator.generateLottoNumbers()
+
+        lottoNumbers shouldHaveSize 6
+        lottoNumbers.distinct() shouldHaveSize 6
+    }
+
+    "지정된 개수의 로또를 생성해야 한다." {
+        val manualLottos = Lottos(
+            listOf(
+                Lotto(
+                    listOf(
+                        LottoNumber(1),
+                        LottoNumber(2),
+                        LottoNumber(3),
+                        LottoNumber(4),
+                        LottoNumber(5),
+                        LottoNumber(6)
+                    )
+                ),
+                Lotto(
+                    listOf(
+                        LottoNumber(7),
+                        LottoNumber(8),
+                        LottoNumber(9),
+                        LottoNumber(10),
+                        LottoNumber(11),
+                        LottoNumber(12)
+                    )
+                )
+            )
+        )
+        val generatedLottos = lottoGenerator.generateLottos(5, manualLottos)
+        generatedLottos.lottoNumbers shouldHaveSize 5
+    }
+
+    "수동 로또와 자동 로또로 전체 로또를 생성한다." {
+        val manualLottos = Lottos(
+            listOf(
+                Lotto(
+                    listOf(
+                        LottoNumber(1),
+                        LottoNumber(2),
+                        LottoNumber(3),
+                        LottoNumber(4),
+                        LottoNumber(5),
+                        LottoNumber(6)
+                    )
+                ),
+                Lotto(
+                    listOf(
+                        LottoNumber(7),
+                        LottoNumber(8),
+                        LottoNumber(9),
+                        LottoNumber(10),
+                        LottoNumber(11),
+                        LottoNumber(12)
+                    )
+                )
+            )
+        )
+
+        val generatedLottos = lottoGenerator.generateLottos(3, manualLottos)
+
+        generatedLottos.lottoNumbers shouldHaveSize 3
+        generatedLottos.lottoNumbers shouldContain manualLottos.lottoNumbers[0]
     }
 })
