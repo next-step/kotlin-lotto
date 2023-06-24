1b52150df8db8d5d4365b9a35f8d88adc9094028
waveofmymind <sjun0913@gmail.com>
2023-06-25 07:24:56 +0900

    Feat: LottoNumberPool을 추가하고 숫자 생성기에서 사용하도록 개선

--- src/main/kotlin/lotto/domain/numberGenerator/FixedLottoNumberGenerator.kt
+++ src/main/kotlin/lotto/domain/numberGenerator/FixedLottoNumberGenerator.kt
@@ -1,10 +1,11 @@
 package lotto.domain.numberGenerator
 
 import lotto.domain.LottoNumber
+import lotto.domain.LottoNumberPool
 
 class FixedLottoNumberGenerator(private val fixedNumbers: List<Int>) : LottoNumberGenerator {
 
     override fun generateNumbers(): List<LottoNumber> {
-        return fixedNumbers.map { LottoNumber(it) }
+        return fixedNumbers.map { LottoNumberPool.get(it) }
     }
 }
--- src/main/kotlin/lotto/domain/numberGenerator/RandomLottoNumberGenerator.kt
+++ src/main/kotlin/lotto/domain/numberGenerator/RandomLottoNumberGenerator.kt
@@ -1,15 +1,11 @@
 package lotto.domain.numberGenerator
 
 import lotto.domain.LottoNumber
+import lotto.domain.LottoNumberPool
 
 class RandomLottoNumberGenerator : LottoNumberGenerator {
 
     override fun generateNumbers(): List<LottoNumber> {
-        return rangeNumber.shuffled().take(NUMBER_OF_LOTTO).map { LottoNumber(it) }.sortedBy { it.value }
-    }
-    companion object {
-        private const val MAX_LOTTO_NUMBER = 45
-        private const val NUMBER_OF_LOTTO = 6
-        private val rangeNumber = (1..MAX_LOTTO_NUMBER).toList()
+        return LottoNumberPool.getRandomNumbers()
     }
 }
