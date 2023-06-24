4c37b44816f3a6fd1db03e75c8f765a093a3a3c6
celine.kk <celine.kk@kakaocorp.com>
2023-06-24 21:41:10 +0900

    feat. 리뷰 반영: 상수 object class안으로 이동

--- src/main/kotlin/lotto/domain/LotteryShop.kt
+++ src/main/kotlin/lotto/domain/LotteryShop.kt
@@ -1,8 +1,9 @@
 package lotto.domain
 
-private const val MANUAL_NUM_EXCEPTION = "구매한 개수보다 작은 개수만 수동으로 구매하실 수 있습니다"
-
 object LotteryShop {
+
+    private const val MANUAL_NUM_EXCEPTION = "구매한 개수보다 작은 개수만 수동으로 구매하실 수 있습니다"
+
     private const val LOTTO_PRICE = 1000
     fun buy(money: Int): Int {
         return money / LOTTO_PRICE
--- src/test/kotlin/lotto/domain/WinningTicketTest.kt
+++ src/test/kotlin/lotto/domain/WinningTicketTest.kt
@@ -2,8 +2,6 @@ package lotto.domain
 
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
 
 internal class WinningTicketTest {
     @Test
