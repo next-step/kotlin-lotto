2b1930cd9dc014a904f092e46adeeb4516d77aa0
developingbear <60379085+developingbear@users.noreply.github.com>
2023-06-25 21:10:41 +0900

    Step4 로또(수동) (#767)
    * refactor: step3 리뷰 반영
    
    * feat: step4 구현
    
    ---------
    
    Co-authored-by: pooh <pooh.duck@kakaocorp.com>

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -6,31 +6,32 @@ import io.kotest.matchers.shouldBe
 class LottoTest : StringSpec({
     "WinLotto와 Lotto의 번호가 6개 일치하는 경우, getCountOfMatch()가 6을 반환한다." {
         val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6).map { number -> LottoNumber(number) }.toHashSet())
-        val winLotto = WinLotto.create(listOf(1, 2, 3, 4, 5, 6), 7)
+        val winLotto = WinLotto(listOf(1, 2, 3, 4, 5, 6), 7)
         winLotto.getCountOfMatch(lotto) shouldBe 6
     }
 
     "WinLotto와 Lotto의 번호가 3개 일치하는 경우, getCountOfMatch()가 3을 반환한다." {
         val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6).map { number -> LottoNumber(number) }.toHashSet())
-        val winLotto = WinLotto.create(listOf(1, 2, 3, 7, 8, 9), 45)
+        val winLotto = WinLotto(listOf(1, 2, 3, 7, 8, 9), 45)
         winLotto.getCountOfMatch(lotto) shouldBe 3
     }
 
     "WinLotto와 Lotto의 번호가 0개 일치하는 경우, getCountOfMatch()가 0을 반환한다." {
         val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6).map { number -> LottoNumber(number) }.toHashSet())
-        val winLotto = WinLotto.create(listOf(7, 8, 9, 10, 11, 12), 45)
+        val winLotto = WinLotto(listOf(7, 8, 9, 10, 11, 12), 45)
         winLotto.getCountOfMatch(lotto) shouldBe 0
     }
 
     "WinLotto와 Lotto의 번호가 보너스 번호가 일치하는 경우, isBonusNumberIn()가 true를 반환한다." {
         val lotto = Lotto(listOf(1, 2, 3, 4, 5, 7).map { number -> LottoNumber(number) }.toHashSet())
-        val winLotto = WinLotto.create(listOf(1, 2, 3, 4, 5, 6), 7)
+        val winLotto = WinLotto(listOf(1, 2, 3, 4, 5, 6), 7)
         winLotto.isBonusNumberIn(lotto) shouldBe true
     }
 
     "WinLotto와 Lotto의 번호가 보너스 번호가 일치하지 않는 경우, isBonusNumberIn()가 false를 반환한다." {
         val lotto = Lotto(listOf(1, 2, 3, 4, 5, 7).map { number -> LottoNumber(number) }.toHashSet())
-        val winLotto = WinLotto.create(listOf(1, 2, 3, 4, 5, 6), 9)
+        val winLotto = WinLotto(listOf(1, 2, 3, 4, 5, 6), 9)
         winLotto.isBonusNumberIn(lotto) shouldBe false
     }
+
 })
