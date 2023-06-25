30befe0e9225539b2d4295e16b6232fd36fd600f
june.kim <junho85@gmail.com>
2023-06-25 23:42:43 +0900

    PR 리뷰 반영 - 메서드 파라미터 네이밍을 조금 더 의미있게 개선 https://github.com/next-step/kotlin-lotto/pull/728#discussion_r1235460995

--- src/main/kotlin/study/StringPlusCalculator.kt
+++ src/main/kotlin/study/StringPlusCalculator.kt
@@ -3,9 +3,9 @@ package study
 object StringPlusCalculator {
     private const val NULL_OR_BLANK_INPUT_RETURN_VALUE = 0
 
-    fun calculate(input: String?): Int {
-        if (input.isNullOrBlank()) return NULL_OR_BLANK_INPUT_RETURN_VALUE
-        val numbers = StringPlusCalculatorParser.parse(input)
+    fun calculate(expression: String?): Int {
+        if (expression.isNullOrBlank()) return NULL_OR_BLANK_INPUT_RETURN_VALUE
+        val numbers = StringPlusCalculatorParser.parse(expression)
         return numbers.sum()
     }
 }
