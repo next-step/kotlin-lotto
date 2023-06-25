e643d20ce6983a3d66937a37ad62d0a09741a610
june.kim <junho85@gmail.com>
2023-06-25 23:37:57 +0900

    PR 리뷰 반영 - 호출 순서로 함수를 배치하여 코드 분석에 좋도록 함 https://github.com/next-step/kotlin-lotto/pull/728#discussion_r1235453184

--- src/main/kotlin/study/StringPlusCalculatorParser.kt
+++ src/main/kotlin/study/StringPlusCalculatorParser.kt
@@ -11,13 +11,6 @@ object StringPlusCalculatorParser {
             .map(::parseStringNumber)
     }
 
-    private fun parseStringNumber(stringNumber: String): Int {
-        if (stringNumber.toIntOrNull() == null) throw RuntimeException("숫자 이외의 값은 입력할 수 없습니다.")
-        val number = stringNumber.toInt()
-        if (number < 0) throw RuntimeException("음수는 입력할 수 없습니다.")
-        return number
-    }
-
     private fun splitStringNumbers(input: String): List<String> {
         if (input.startsWith("//")) {
             val customDelimiter = input.substring(2, 3)
@@ -25,5 +18,12 @@ object StringPlusCalculatorParser {
         }
         val delimiters = "[,:]".toRegex()
         return input.split(delimiters)
+    }
+
+    private fun parseStringNumber(stringNumber: String): Int {
+        if (stringNumber.toIntOrNull() == null) throw RuntimeException("숫자 이외의 값은 입력할 수 없습니다.")
+        val number = stringNumber.toInt()
+        if (number < 0) throw RuntimeException("음수는 입력할 수 없습니다.")
+        return number
     }
 }
