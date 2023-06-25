a2cde222e3d69caa233328d6f4317a49200e8405
Songyi Kim <ksl2950@naver.com>
2023-06-25 16:34:57 +0900

    test: 추가 검증 테스트 코드 작성

--- src/test/kotlin/lotto/domain/NumbersTest.kt
+++ src/test/kotlin/lotto/domain/NumbersTest.kt
@@ -1,10 +1,25 @@
 package lotto.domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 import lotto.domain.Numbers.Companion.toNumbers
+import java.lang.IllegalArgumentException
 
 class NumbersTest : FunSpec({
+
+    test("요소 사이즈가 6이 아닐 경우, IllegalArgumentException 을 반환한다.") {
+        // given
+        val values = listOf(LottoNumber(1))
+        listOf(
+            listOf(LottoNumber(1)),
+            listOf(LottoNumber(1), LottoNumber(2))
+        ).forAll {
+            // when, then
+            shouldThrow<IllegalArgumentException> { Numbers(values) }
+        }
+    }
 
     test("입력받은 숫자 리스트와 일치하는 숫자 개수를 반환할 수 있다.") {
         // given
