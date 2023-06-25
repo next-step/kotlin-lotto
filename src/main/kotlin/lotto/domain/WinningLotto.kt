ed4d4ae886b0a37b9e0df1e4175a77c82402dc07
theo-94 <theo.94@kakaocorp.com>
2023-06-25 18:01:31 +0900

    피드백 반영 : WinningLotto 수정
    - lottoNumbers 필드 상속받아 사용하기로 변경
    - numOfMatch, bonusMatch 지역변수로 변경

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -1,16 +1,13 @@
 package lotto.domain
 
 class WinningLotto(
-    private val winningNumbers: Set<LottoNumber>,
+    winningNumbers: Set<LottoNumber>,
     private val bonusNumber: LottoNumber
 ) : Lotto(winningNumbers) {
 
-    private var numOfMatch = 0
-    private var bonusMatch = false
-
     fun rankOfLotto(lotto: Lotto): LottoRank {
-        numOfMatch = winningNumbers.intersect(lotto.lottoNumbers).count()
-        bonusMatch = lotto.lottoNumbers.contains(bonusNumber)
+        val numOfMatch = lottoNumbers.intersect(lotto.lottoNumbers).count()
+        val bonusMatch = lotto.lottoNumbers.contains(bonusNumber)
         return LottoRank.of(numOfMatch, bonusMatch)
     }
 
