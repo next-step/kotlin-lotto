d7877411ddc73373779233291e974ddaa30b2995
DongSeongSeo <sds901234@naver.com>
2023-06-26 23:53:49 +0900

    feat: 3단계 - 로또(2등) 기능 구현 및 테스트 추가 (#776)
    Co-authored-by: 동성 서 <sds901234@tradlinx.com>

--- src/test/kotlin/lotto/domain/LottoDrawMachineTest.kt
+++ src/test/kotlin/lotto/domain/LottoDrawMachineTest.kt
@@ -4,14 +4,14 @@ import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 
 class LottoDrawMachineTest : ShouldSpec({
-    context("당첨 번호 [1, 2, 3, 4, 5, 6]") {
+    context("당첨 번호 [1, 2, 3, 4, 5, 6] / 보너스 7") {
 
         val lottoNumbers = (1..6).map { LottoNumber(it) }
-        val winningLotto = WinningLotto(Lotto.of(lottoNumbers))
+        val winningLotto = WinningLotto(Lotto.from(lottoNumbers), LottoNumber(7))
         val lottoDrawMachine = LottoDrawMachine(winningLotto)
 
         context("로또 번호 [1, 2, 3, 4, 5, 6]") {
-            val lotto = Lotto.of((1..6).map { LottoNumber(it) })
+            val lotto = Lotto.from((1..6).map { LottoNumber(it) })
             val drawResult = lottoDrawMachine.draw(listOf(lotto))
             should("1등 당첨 순위 수 = 1") { drawResult.countOf(LottoRank.FIRST) shouldBe 1 }
             should("당첨 금액 = 1등 당첨금") { drawResult.totalReward shouldBe LottoRank.FIRST.reward }
@@ -21,7 +21,7 @@ class LottoDrawMachineTest : ShouldSpec({
         }
 
         context("로또 번호 [7, 8, 9, 10, 11, 12]") {
-            val lotto = Lotto.of((7..12).map { LottoNumber(it) })
+            val lotto = Lotto.from((7..12).map { LottoNumber(it) })
             val drawResult = lottoDrawMachine.draw(listOf(lotto))
             should("1등 당첨 순위 수 = 0") { drawResult.countOf(LottoRank.FIRST) shouldBe 0 }
             should("당첨 금액 = 0") { drawResult.totalReward shouldBe 0 }
