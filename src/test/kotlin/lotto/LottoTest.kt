3c9c2b48c966dca8ac321f3a90b7272a33aad112
yoo-jaein <y_jaein@naver.com>
2023-06-26 16:46:01 +0900

    test: 로또를 수동 생성하는 테스트 케이스 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -9,4 +9,10 @@ class LottoTest {
     fun `로또를 자동 생성할 수 있다`() {
         assertThat(Lotto.autoCreate()).isInstanceOf(Lotto::class.java)
     }
+
+    @Test
+    fun `로또를 수동 생성할 수 있다`() {
+        val numbers = listOf(1, 2, 3, 4, 5, 6)
+        assertThat(Lotto.manualCreate(numbers)).isInstanceOf(Lotto::class.java)
+    }
 }
