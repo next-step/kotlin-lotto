383e7d09033a67b284d4b85e741ee16ae10fdd44
jonghyunhub <chuwhdgus@gmail.com>
2023-06-29 20:03:41 +0900

    feat : 보너스 볼은 당첨 번호와 중복되지 않는다.

---
+++ src/main/kotlin/lotto/domain/BonusBall.kt
@@ -0,0 +1,20 @@
+package lotto.domain
+
+class BonusBall(val bonusNumber: Int, answerNumbers: List<Int>) {
+    init {
+        validateBonusNumber(bonusNumber, answerNumbers)
+    }
+
+    private fun validateBonusNumber(bonusNumber: Int, answerNumbers: List<Int>) {
+        validateIsBonusNumberInLotteryNumberBounce(bonusNumber)
+        validateIsBonusNumberAlreadyInAnswerNumbers(bonusNumber, answerNumbers)
+    }
+
+    private fun validateIsBonusNumberInLotteryNumberBounce(bonusNumber: Int) {
+        require(bonusNumber in LotteryPaper.LOTTO_NUMBER_BOUNDS) { "로또 범위의 숫자만 가능합니다. 입력값을 다시 확인하세요." }
+    }
+
+    private fun validateIsBonusNumberAlreadyInAnswerNumbers(bonusNumber: Int, answerNumbers: List<Int>) {
+        require(!answerNumbers.contains(bonusNumber)) { "보너스 숫자는 기존 당첨 숫자와 중복되면 안됩니다." }
+    }
+}
---
+++ src/test/kotlin/lotto/domain/BonusBallTest.kt
@@ -0,0 +1,33 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class BonusBallTest {
+    @Test
+    fun `보너스 넘버는 숫자 한개를 가진다`() {
+        val bonusBall = BonusBall(1, listOf())
+
+        assertThat(bonusBall.bonusNumber).isEqualTo(1)
+    }
+
+    @Test
+    fun `보너스 볼을 생성할 때 1부터 45사이의 숫자를 값으로 가진다`() {
+        val numberInOutOfLottoBounce = 46
+
+        assertThrows<IllegalArgumentException> {
+            BonusBall(numberInOutOfLottoBounce, listOf())
+        }
+    }
+
+    @Test
+    fun `보너스 볼은 당첨 번호와 중복되지 않는다`() {
+        val answerNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val bounsNumber = 1
+
+        assertThrows<IllegalArgumentException> {
+            BonusBall(bounsNumber, answerNumbers)
+        }
+    }
+}
