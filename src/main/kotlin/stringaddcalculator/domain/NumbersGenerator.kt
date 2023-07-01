e78c2ab858666257dfd854411c4cc2c1c9ae8655
yunbaek <yunbaek4564@gmail.com>
2023-07-01 23:50:53 +0900

    feat(Numbers): 커스텀 구분자를 통해 분리한 숫자를 더하는 기능 추가

--- src/main/kotlin/stringaddcalculator/domain/NumbersGenerator.kt
+++ src/main/kotlin/stringaddcalculator/domain/NumbersGenerator.kt
@@ -1,9 +1,24 @@
 package stringaddcalculator.domain
 
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
 class NumbersGenerator(private val input: String) {
 
     fun generate(): Numbers {
+        val matcher = CUSTOM_DELIMITER_PATTERN.matcher(input)
+        if (matcher.find()) {
+            return byCustomDelimiters(matcher)
+        }
         return byDefaultDelimiters()
+    }
+
+    private fun byCustomDelimiters(matcher: Matcher): Numbers {
+        val customDelimiter = matcher.group(1)
+        return matcher.group(2).split(customDelimiter)
+            .map { it.trim() }
+            .map { Number.of(it) }
+            .let { Numbers(it) }
     }
 
     private fun byDefaultDelimiters() = input.split(DEFAULT_DELIMITER_REGEX)
@@ -13,5 +28,6 @@ class NumbersGenerator(private val input: String) {
 
     companion object {
         private val DEFAULT_DELIMITER_REGEX = "[,:]".toRegex()
+        private val CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)")
     }
 }
--- src/test/kotlin/stringaddcalculator/domain/NumbersGeneratorTest.kt
+++ src/test/kotlin/stringaddcalculator/domain/NumbersGeneratorTest.kt
@@ -19,6 +19,13 @@ class NumbersGeneratorTest {
     }
 
     @Test
+    fun `커스텀 구분자를 기준으로 숫자를 나눠서 Number 리스트 반환`() {
+        val numbers = NumbersGenerator("//;\n1;2;3").generate()
+        assertThat(numbers)
+            .isEqualTo(Numbers(listOf( Number.of("1"), Number.of("2"), Number.of("3"))))
+    }
+
+    @Test
     fun `쉼표와 콜론을 기준으로 숫자를 나눠서 Number 리스트 반환`() {
         val numbers = NumbersGenerator("1,2:3").generate()
         assertThat(numbers)
