6fb62c0bbb80357972cf9fbf872bb6c2c7432e97
jonghyunhub <chuwhdgus@gmail.com>
2023-07-01 20:43:07 +0900

    refact : 보너스 볼 삭제 테스트에도 반영

--- src/test/kotlin/lotto/domain/SecondLevelDiscriminatorTest.kt
+++ src/test/kotlin/lotto/domain/SecondLevelDiscriminatorTest.kt
@@ -8,15 +8,15 @@ import org.junit.jupiter.params.provider.ValueSource
 class SecondLevelDiscriminatorTest {
 
     @Test
-    fun `3등일때 남은 숫자가 보너스 볼의 숫자와 같은지 체크한다`() {
+    fun `3등일때 남은 숫자가 보너스 넘버와 같은지 체크한다`() {
         val lottoNumbers = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber(it) }
         val lotteryPaper = LotteryPaper(lottoNumbers)
-        val bonusBall = BonusBall(LottoNumber(7), lotteryPaper)
+        val bonusNumber = LottoNumber(7)
 
         val actual = SecondLevelDiscriminator.checkPrizeLevelIsSecond(
             prizeLevel = PrizeLevel.THIRD,
             lotteryPaper = lotteryPaper,
-            bonusBall = bonusBall
+            bonusNumber = bonusNumber
         )
 
         Assertions.assertThat(actual).isEqualTo(PrizeLevel.THIRD)
@@ -27,12 +27,12 @@ class SecondLevelDiscriminatorTest {
     fun `3등이 아니면 체크하지않는다`(prizeLevel: PrizeLevel) {
         val lottoNumbers = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber(it) }
         val lotteryPaper = LotteryPaper(lottoNumbers)
-        val bonusBall = BonusBall(LottoNumber(7), lotteryPaper)
+        val bonusNumber = LottoNumber(7)
 
         val actual = SecondLevelDiscriminator.checkPrizeLevelIsSecond(
             prizeLevel = prizeLevel,
             lotteryPaper = lotteryPaper,
-            bonusBall = bonusBall
+            bonusNumber = bonusNumber
         )
 
         Assertions.assertThat(actual).isEqualTo(prizeLevel)
