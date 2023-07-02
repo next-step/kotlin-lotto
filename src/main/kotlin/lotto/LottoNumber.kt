fd124ec3e781cab13e546cb49dcf11d70d153a02
turner.lee <turner.lee@kakaocorp.com>
2023-07-03 00:36:03 +0900

    refactor: 불필요한 list 변환 제거

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -6,7 +6,7 @@ class LottoNumber private constructor(val number: Int) {
         private const val MIN = 1
         private val LOTTO_NUMBER_RANGE: IntRange = (MIN..MAX)
         const val LOTTO_NUMBER_OUT_OF_RANGE_MESSAGE = "$MIN 에서 $MAX 사이의 숫자만 가능합니다. number:%d"
-        val LOTTO_NUMBER_POOL: List<LottoNumber> = LOTTO_NUMBER_RANGE.toList().map { LottoNumber(it) }
+        val LOTTO_NUMBER_POOL: List<LottoNumber> = LOTTO_NUMBER_RANGE.map { LottoNumber(it) }
 
         fun from(number: Int): LottoNumber {
             require(number in LOTTO_NUMBER_RANGE) { LOTTO_NUMBER_OUT_OF_RANGE_MESSAGE.format(number) }
