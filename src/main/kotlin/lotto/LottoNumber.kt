a833494f5e9c2e27b49ce4eee4e1228dec3204ab
turner.lee <turner.lee@kakaocorp.com>
2023-07-03 00:36:58 +0900

    refactor: LottoNumber 검증 로직을 생성자와 팩토리 메서드 둘 다 수행

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -1,6 +1,10 @@
 package lotto
 
 class LottoNumber private constructor(val number: Int) {
+    init {
+        validate(number)
+    }
+
     companion object {
         private const val MAX = 45
         private const val MIN = 1
@@ -8,9 +12,12 @@ class LottoNumber private constructor(val number: Int) {
         const val LOTTO_NUMBER_OUT_OF_RANGE_MESSAGE = "$MIN 에서 $MAX 사이의 숫자만 가능합니다. number:%d"
         val LOTTO_NUMBER_POOL: List<LottoNumber> = LOTTO_NUMBER_RANGE.map { LottoNumber(it) }
 
-        fun from(number: Int): LottoNumber {
+        private fun validate(number: Int) {
             require(number in LOTTO_NUMBER_RANGE) { LOTTO_NUMBER_OUT_OF_RANGE_MESSAGE.format(number) }
+        }
 
+        fun from(number: Int): LottoNumber {
+            validate(number)
             return LOTTO_NUMBER_POOL[number - 1]
         }
     }
