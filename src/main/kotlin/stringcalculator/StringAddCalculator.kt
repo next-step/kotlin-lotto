10a286d2c97397c7d9897f24f36d8bcbab55a9f5
pavlo.v <pavlo.v@kakaocorp.com>
2023-07-03 02:56:29 +0900

    추가 : 이전 코드리뷰 수정

--- src/main/kotlin/stringcalculator/StringAddCalculator.kt
+++ src/main/kotlin/stringcalculator/StringAddCalculator.kt
@@ -2,9 +2,11 @@ package stringcalculator
 
 class StringAddCalculator {
     private val delimiters = Delimiters()
+
     fun calculate(text: String?): Int {
         if (text.isNullOrEmpty()) return 0
         val parseText = StringParser.deleteCustomDelimiters(text, delimiters)
+
         return parseText.split(delimiters.getDelimitersRegex()).sumOf {
             if (it.toInt() < 0) throw RuntimeException()
             it.toInt()
