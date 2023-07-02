85049917b2f3e74ca2ea8fd97af0a424ccadda47
pavlo.v <pavlo.v@kakaocorp.com>
2023-07-03 03:37:11 +0900

    추가 : mutable 수정 및 object 클래스 추가

--- src/main/kotlin/stringcalculator/Delimiters.kt
+++ src/main/kotlin/stringcalculator/Delimiters.kt
@@ -1,11 +1,36 @@
 package stringcalculator
 
-class Delimiters {
-    val delimiters = mutableListOf(DEFAULT_DELIMITER_COMMA, DEFAULT_DELIMITER_COLON)
+import stringcalculator.Delimiters.Companion.DEFAULT_DELIMITER_COLON
+import stringcalculator.Delimiters.Companion.DEFAULT_DELIMITER_COMMA
 
-    fun addDelimiter(delimiter: String) {
-        delimiters.add(delimiter)
+object StringParser {
+    private const val CUSTOM_DELIMITER_INDEX = 1
+    private const val WITHOUT_CUSTOM_DELIMITER_EXPRESSION_INDEX = 2
+
+    fun getDelimitersFromString(text: String): Delimiters {
+        val matchResult = Regex(Delimiters.CUSTOM_DELIMITER_FIND_REGEX).find(text)
+        matchResult?.let {
+            return Delimiters(
+                listOf(
+                    DEFAULT_DELIMITER_COMMA,
+                    DEFAULT_DELIMITER_COLON,
+                    it.groupValues[CUSTOM_DELIMITER_INDEX]
+                )
+            )
+        }
+        return Delimiters()
     }
+
+    fun deleteCustomDelimiters(text: String): String {
+        val matchResult = Regex(Delimiters.CUSTOM_DELIMITER_FIND_REGEX).find(text)
+        matchResult?.let {
+            return it.groupValues[WITHOUT_CUSTOM_DELIMITER_EXPRESSION_INDEX]
+        }
+        return text
+    }
+}
+
+class Delimiters(val delimiters: List<String> = listOf(DEFAULT_DELIMITER_COMMA, DEFAULT_DELIMITER_COLON)) {
 
     fun getDelimitersRegex(): Regex {
         return delimiters.joinToString("", "[", "]").toRegex()
@@ -14,6 +39,6 @@ class Delimiters {
     companion object {
         const val DEFAULT_DELIMITER_COMMA = ","
         const val DEFAULT_DELIMITER_COLON = ":"
-        const val CUSTOM_DELIMITER_FIND_REGEX = "//(.)\n(.*)"
+        const val CUSTOM_DELIMITER_FIND_REGEX = "//(.*)\n(.*)"
     }
 }
--- src/test/kotlin/stringcalculator/DelimitersTest.kt
+++ src/test/kotlin/stringcalculator/DelimitersTest.kt
@@ -1,8 +1,6 @@
 package stringcalculator
 
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.inspectors.forAll
-import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.shouldBe
 
@@ -11,20 +9,8 @@ class DelimitersTest : StringSpec({
         val delimiters = Delimiters()
         delimiters.delimiters shouldContainAll listOf(",", ":")
     }
-    "새로운 커스텀한 구분자를 추가할 수 있다." {
-        val delimiters = Delimiters()
-        delimiters.addDelimiter("&&")
-        delimiters.delimiters shouldContain "&&"
-    }
     "구분자들을 통해 정규식을 만들어준다." {
         val delimiters = Delimiters()
         delimiters.getDelimitersRegex() shouldBe "[,:]".toRegex()
-    }
-    "커스텀한 구분자를 추가할 경우 정규식에 포함된다." {
-        listOf("#", "?", "^^").forAll {
-            val delimiters = Delimiters()
-            delimiters.addDelimiter(it)
-            delimiters.getDelimitersRegex() shouldBe "[,:$it]".toRegex()
-        }
     }
 })
