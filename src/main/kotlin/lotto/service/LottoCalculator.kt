f194ff4f8168440b46d6abd408559d63ea231c19
june.kim <junho85@gmail.com>
2023-07-03 20:38:44 +0900

    PR 리뷰 반영 - 상태가 없고 상태관리가 필요없는 Util 클래스라면 object 클래스로 https://github.com/next-step/kotlin-lotto/pull/774#discussion_r1241893014

--- src/main/kotlin/lotto/service/LottoCalculator.kt
+++ src/main/kotlin/lotto/service/LottoCalculator.kt
@@ -2,26 +2,24 @@ package lotto.service
 
 import lotto.domain.Lotto
 
-class LottoCalculator {
-    companion object {
-        fun countMatch(lotto: Lotto, winningLotto: Lotto): Int {
-            return lotto.numbers
-                .intersect(winningLotto.numbers)
-                .size
-        }
+object LottoCalculator {
+    fun countMatch(lotto: Lotto, winningLotto: Lotto): Int {
+        return lotto.numbers
+            .intersect(winningLotto.numbers)
+            .size
+    }
 
-        fun calculatePrizeMoney(matchCount: Int): Long {
-            return when (matchCount) {
-                3 -> 5_000
-                4 -> 50_000
-                5 -> 1_500_000
-                6 -> 2_000_000_000
-                else -> 0
-            }
+    fun calculatePrizeMoney(matchCount: Int): Long {
+        return when (matchCount) {
+            3 -> 5_000
+            4 -> 50_000
+            5 -> 1_500_000
+            6 -> 2_000_000_000
+            else -> 0
         }
+    }
 
-        fun calculateProfitRate(totalPrizeMoney: Long, purchaseMoney: Long): Double {
-            return totalPrizeMoney.toDouble() / purchaseMoney.toDouble()
-        }
+    fun calculateProfitRate(totalPrizeMoney: Long, purchaseMoney: Long): Double {
+        return totalPrizeMoney.toDouble() / purchaseMoney.toDouble()
     }
 }
