f06b52abd7cb0edbbc50135eec22f8f1408b086c
june.kim <junho85@gmail.com>
2023-07-03 20:20:58 +0900

    PR 리뷰 반영 - Lotto 6개 유효성 확인. 1~45 범위 검사. https://github.com/next-step/kotlin-lotto/pull/774#discussion_r1241616502

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,6 +1,11 @@
 package lotto.domain
 
 class Lotto(val numbers: Set<Int>) {
+    init {
+        require(numbers.size == 6) { "로또 번호는 6개여야 합니다." }
+        require(numbers.all { it in 1..45 }) { "로또 번호는 1부터 45 사이여야 합니다." }
+    }
+
     constructor(numbers: List<Int>) : this(numbers.toSet())
 
     override fun toString(): String {
