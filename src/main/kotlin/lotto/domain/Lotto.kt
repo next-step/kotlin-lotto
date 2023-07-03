7b0dcd42b7148b069898f2a86c0a4dcbb6a8b658
june.kim <junho85@gmail.com>
2023-07-03 20:32:21 +0900

    PR 리뷰 반영 - Lotto inline class https://github.com/next-step/kotlin-lotto/pull/774#discussion_r1241880986

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,6 +1,7 @@
 package lotto.domain
 
-class Lotto(val numbers: Set<Int>) {
+@JvmInline
+value class Lotto(val numbers: Set<Int>) {
     init {
         require(numbers.size == 6) { "로또 번호는 6개여야 합니다." }
         require(numbers.all { it in 1..45 }) { "로또 번호는 1부터 45 사이여야 합니다." }
