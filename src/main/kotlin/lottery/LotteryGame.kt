e4875eb04c0cd1c8e0b239031db5b9ee94db675d
pavlo.v <pavlo.v@kakaocorp.com>
2023-07-03 16:27:35 +0900

    추가 : 실행할수 있는 main 추가

---
+++ src/main/kotlin/lottery/LotteryGame.kt
@@ -0,0 +1,32 @@
+package lottery
+
+class LotteryGame {
+    private lateinit var winningLottery: WinningLottery
+    private val lotteryRank = LotteryRank()
+    fun purchaseAutoLotteries(purchasePrice: Int): Lotteries {
+        return Lotteries.makeAutoLotteries(purchasePrice / Lottery.LOTTERY_PRICE)
+    }
+
+    fun start() {
+        LotteryGameView.printPurchaseMoneyView()
+        val money = readln().toInt()
+        val purchaseAutoLotteries = purchaseAutoLotteries(money)
+        LotteryGameView.printPurchaseLotteryView(purchaseAutoLotteries.lotteries.size)
+        LotteryGameView.printLotteriesNumber(purchaseAutoLotteries)
+        LotteryGameView.printWinnerLotteryNumber()
+        val numbers = readln().replace("\\s".toRegex(), "").split(",")
+        winningLottery = WinningLottery(numbers.map { LotteryNumber.get(it.toInt()) }.toSet())
+
+        purchaseAutoLotteries.lotteries.forEach {
+            val checkRank = it.checkRank(winningLottery.winningNumbers)
+            lotteryRank.plusRank(LotteryPrize.get(checkRank) ?: LotteryPrize.NONE)
+        }
+        LotteryGameView.printLotteryRankView(lotteryRank)
+        LotteryGameView.printProfitView(lotteryRank.calculateProfit(money))
+    }
+}
+
+fun main() {
+    val game = LotteryGame()
+    game.start()
+}
