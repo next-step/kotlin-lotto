d4cd553a9d565b4d9c1f8c509f59d920c7b48ddb
김태훈 <sksskaw@naver.com>
2023-07-04 02:10:46 +0900

    PurchaseLottoResponse data 제거

--- src/main/kotlin/lotto/domain/dto/PurchaseLottoResponse.kt
+++ src/main/kotlin/lotto/domain/dto/PurchaseLottoResponse.kt
@@ -2,7 +2,7 @@ package lotto.domain.dto
 
 import lotto.domain.Lotto
 
-data class PurchaseLottoResponse(
+class PurchaseLottoResponse(
     val autoLottoCount: Int,
     val manualLottoCount: Int,
     val lottoTickets: List<Lotto>,
