75072488c59fa3ebe5e2ee4eadd0c4752e58b44e
jonghyunhub <chuwhdgus@gmail.com>
2023-07-04 16:10:33 +0900

    test : LottoMachineTest 수정

--- src/test/kotlin/lotto/domain/LottoMachineTest.kt
+++ src/test/kotlin/lotto/domain/LottoMachineTest.kt
@@ -17,9 +17,9 @@ class LottoMachineTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["14000", "3000"])
+    @ValueSource(strings = ["14000", "4000"])
     fun `입력받은 구입 금액을 로또 개당 가격으로 나눈 숫자만큼 티켓을 구매한다`(money: Int) {
-        val lottoOrder = LottoOrder()
+        val lottoOrder = LottoOrder(money, LottoTestHelper.createPurchasedLotteryPapers())
         val actual = lottoMachine.buyLottoTicket(lottoOrder).lotteryPaperList.size
         Assertions.assertThat(actual).isEqualTo(money / LottoMachine.LOTTO_TICKET_PRICE)
     }
@@ -27,8 +27,9 @@ class LottoMachineTest {
     @ParameterizedTest
     @ValueSource(strings = ["999", "0", "-1000"])
     fun `구입 금액이 1000원 미만이면 IllegalArgumentException을 throw한다`(money: Int) {
+        val lottoOrder = LottoOrder(money, LottoTestHelper.createPurchasedLotteryPapers())
         assertThrows<IllegalArgumentException> {
-            lottoMachine.buyLottoTicket(money)
+            lottoMachine.buyLottoTicket(lottoOrder)
         }
     }
 }
