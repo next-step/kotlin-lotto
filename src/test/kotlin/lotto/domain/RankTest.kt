9d7471e4d6ec0e17c66e578baea80fde4efcd4b0
june.kim <junho85@gmail.com>
2023-07-06 00:24:28 +0900

    PR리뷰 반영 - 로또 등수 테스트 간결화 https://github.com/next-step/kotlin-lotto/pull/793#discussion_r1252162425

--- src/test/kotlin/lotto/domain/RankTest.kt
+++ src/test/kotlin/lotto/domain/RankTest.kt
@@ -1,39 +1,22 @@
 package lotto.domain
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.row
 import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 
 class RankTest : FunSpec({
     context("로또 등수를 확인한다.") {
-        context("1등") {
-            val rank = Rank.of(6, false)
-            rank shouldBe Rank.FIRST
-        }
-
-        context("2등") {
-            val rank = Rank.of(5, true)
-            rank shouldBe Rank.SECOND
-        }
-
-        context("3등") {
-            val rank = Rank.of(5, false)
-            rank shouldBe Rank.THIRD
-        }
-
-        context("4등") {
-            val rank = Rank.of(4, false)
-            rank shouldBe Rank.FOURTH
-        }
-
-        context("5등") {
-            val rank = Rank.of(3, false)
-            rank shouldBe Rank.FIFTH
-        }
-
-        context("꽝") {
-            val rank = Rank.of(2, false)
-            rank shouldBe Rank.MISS
+        withData(
+            row(6, false, Rank.FIRST),
+            row(5, true, Rank.SECOND),
+            row(5, false, Rank.THIRD),
+            row(4, false, Rank.FOURTH),
+            row(3, false, Rank.FIFTH),
+            row(2, false, Rank.MISS),
+        ) { (matchCount, matchBonus, expectedRank) ->
+            val actual = Rank.of(matchCount, matchBonus)
+            actual shouldBe expectedRank
         }
     }
 
