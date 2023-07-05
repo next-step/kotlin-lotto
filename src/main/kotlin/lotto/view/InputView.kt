14e82e1013161380d3eb871d70769112c7c2549e
june.kim <junho85@gmail.com>
2023-07-05 22:46:45 +0900

    InputView, ResultView를 object로 수정. util 성 클래스이기 때문에 object가 적절

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,23 +1,21 @@
 package lotto.view
 
-class InputView {
-    companion object {
-        fun purchaseMoney(): Long {
-            println("구입금액을 입력해 주세요.")
-            return readln().toLong()
-        }
+object InputView {
+    fun purchaseMoney(): Long {
+        println("구입금액을 입력해 주세요.")
+        return readln().toLong()
+    }
 
-        fun winningLotto(): List<Int> {
-            println("지난 주 당첨 번호를 입력해 주세요.")
-            return readln()
-                .split(",")
-                .map { it.toInt() }
-                .toList()
-        }
+    fun winningLotto(): List<Int> {
+        println("지난 주 당첨 번호를 입력해 주세요.")
+        return readln()
+            .split(",")
+            .map { it.toInt() }
+            .toList()
+    }
 
-        fun bonusNumber(): Int {
-            println("보너스 볼을 입력해 주세요.")
-            return readln().toInt()
-        }
+    fun bonusNumber(): Int {
+        println("보너스 볼을 입력해 주세요.")
+        return readln().toInt()
     }
 }
--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -5,45 +5,43 @@ import lotto.domain.Rank
 import lotto.domain.WinningStatistics
 import lotto.service.LottoCalculator
 
-class ResultView {
-    companion object {
-        fun printPurchaseLottoNum(lottos: Lottos) {
-            println("${lottos.size}개를 구매했습니다.")
-        }
+object ResultView {
+    fun printPurchaseLottoNum(lottos: Lottos) {
+        println("${lottos.size}개를 구매했습니다.")
+    }
 
-        fun printLottos(lottos: Lottos) {
-            lottos.lottos.forEach {
-                println(it)
-            }
+    fun printLottos(lottos: Lottos) {
+        lottos.lottos.forEach {
+            println(it)
         }
+    }
 
-        fun printStatisticsTitle() {
-            println("당첨 통계")
-            println("---------")
-        }
+    fun printStatisticsTitle() {
+        println("당첨 통계")
+        println("---------")
+    }
 
-        fun printWinningStatistics(winningStatistics: WinningStatistics) {
-            listOf(
-                Rank.FIFTH,
-                Rank.FOURTH,
-                Rank.THIRD,
-                Rank.SECOND,
-                Rank.FIRST,
-            ).forEach {
-                val prize = LottoCalculator.calculatePrizeMoney(it)
-                val matchCount = winningStatistics.winningStatistics[it] ?: 0
-                val bonusMessage = if (it == Rank.SECOND) ", 보너스 볼 일치" else " "
-                println("${it.countOfMatch}개 일치$bonusMessage(${prize}원)- ${matchCount}개")
-            }
+    fun printWinningStatistics(winningStatistics: WinningStatistics) {
+        listOf(
+            Rank.FIFTH,
+            Rank.FOURTH,
+            Rank.THIRD,
+            Rank.SECOND,
+            Rank.FIRST,
+        ).forEach {
+            val prize = LottoCalculator.calculatePrizeMoney(it)
+            val matchCount = winningStatistics.winningStatistics[it] ?: 0
+            val bonusMessage = if (it == Rank.SECOND) ", 보너스 볼 일치" else " "
+            println("${it.countOfMatch}개 일치$bonusMessage(${prize}원)- ${matchCount}개")
         }
+    }
 
-        fun printProfitRate(profitRate: Double) {
-            val result = when {
-                profitRate < 1.0 -> "손해"
-                profitRate == 1.0 -> "본전"
-                else -> "이익"
-            }
-            println("총 수익률은 ${profitRate}입니다.(기준이 1이기 때문에 결과적으로 ${result}라는 의미임)")
+    fun printProfitRate(profitRate: Double) {
+        val result = when {
+            profitRate < 1.0 -> "손해"
+            profitRate == 1.0 -> "본전"
+            else -> "이익"
         }
+        println("총 수익률은 ${profitRate}입니다.(기준이 1이기 때문에 결과적으로 ${result}라는 의미임)")
     }
 }
