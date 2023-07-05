a2ee73de8088dd7506d148708f780a46ff9f0640
june.kim <junho85@gmail.com>
2023-07-05 22:46:01 +0900

    PR리뷰 적용 - Rank객체의 winningMoney이용하도록 수정 https://github.com/next-step/kotlin-lotto/pull/793#discussion_r1252139209

--- src/main/kotlin/lotto/service/LottoCalculator.kt
+++ src/main/kotlin/lotto/service/LottoCalculator.kt
@@ -3,15 +3,5 @@ package lotto.service
 import lotto.domain.Rank
 
 object LottoCalculator {
-
-    fun calculatePrizeMoney(rank: Rank): Long {
-        return when (rank) {
-            Rank.FIFTH -> 5_000
-            Rank.FOURTH -> 50_000
-            Rank.THIRD -> 1_500_000
-            Rank.SECOND -> 30_000_000
-            Rank.FIRST -> 2_000_000_000
-            else -> 0
-        }
-    }
+    fun calculatePrizeMoney(rank: Rank): Long = rank.winningMoney
 }
