cec863ac793edf074a76f94d8848a4424848d2f8
choihwan2 <choihwan2@naver.com>
2023-07-05 16:03:07 +0900

    변경 : const val 추가

--- src/main/kotlin/lottery/domain/LotteryRank.kt
+++ src/main/kotlin/lottery/domain/LotteryRank.kt
@@ -1,7 +1,7 @@
 package lottery.domain
 
 class LotteryRank {
-    val lotteriesRank = LotteryPrize.values().associateWith { 0 }.toMutableMap()
+    val lotteriesRank = LotteryPrize.values().associateWith { RANK_DEFAULT_VALUE }.toMutableMap()
 
     fun plusRank(rank: LotteryPrize) {
         lotteriesRank[rank] = lotteriesRank.getOrDefault(rank, 0) + 1
@@ -12,5 +12,9 @@ class LotteryRank {
             prize.rewardMoney * count
         }.sumOf { it }.toDouble()
         return total / money
+    }
+
+    companion object {
+        const val RANK_DEFAULT_VALUE = 0
     }
 }
