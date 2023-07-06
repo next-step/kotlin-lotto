8b44e5796f925c5d5faf19538362d669704e4314
sang5c <skah321@gmail.com>
2023-07-07 01:09:12 +0900

    test(Lotto): 로또 번호가 5개인 경우 예외가 발생한다

---
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -0,0 +1,20 @@
+package lotto
+
+import io.kotest.assertions.throwables.shouldThrow
+import org.junit.jupiter.api.Test
+
+class LottoTest {
+
+    @Test
+    fun `로또 번호가 5개인 경우 예외가 발생한다`() {
+        val numbers = listOf(
+            LottoNumber(1),
+            LottoNumber(2),
+            LottoNumber(3),
+            LottoNumber(4),
+            LottoNumber(5)
+        )
+
+        shouldThrow<IllegalArgumentException> { Lotto(numbers) }
+    }
+}
