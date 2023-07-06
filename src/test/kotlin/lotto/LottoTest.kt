ade4d1306ec01596be3039f03ed6a2908293eb3f
sang5c <skah321@gmail.com>
2023-07-07 01:11:10 +0900

    test(Lotto): 로또 번호가 6개가 아닌 경우 예외가 발생한다

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -17,4 +17,19 @@ class LottoTest {
 
         shouldThrow<IllegalArgumentException> { Lotto(numbers) }
     }
+
+    @Test
+    fun `로또 번호가 7개인 경우 예외가 발생한다`() {
+        val numbers = listOf(
+            LottoNumber(1),
+            LottoNumber(2),
+            LottoNumber(3),
+            LottoNumber(4),
+            LottoNumber(5),
+            LottoNumber(6),
+            LottoNumber(7),
+        )
+
+        shouldThrow<IllegalArgumentException> { Lotto(numbers) }
+    }
 }
