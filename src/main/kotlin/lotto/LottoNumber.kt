80787d8a51c0347045968dcdd615644f56aa86ae
sang5c <skah321@gmail.com>
2023-07-07 00:56:23 +0900

    feat(LottoNumber): 1부터 45 사이의 숫자가 아닌 경우 예외가 발생한다

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -1,4 +1,12 @@
 package lotto
 
 @JvmInline
-value class LottoNumber(private val value: Int = 0)
+value class LottoNumber(private val value: Int = 0) {
+    init {
+        require(value in NUMBER_RANGE) { "1부터 45 사이의 숫자만 입력할 수 있습니다. [$value]" }
+    }
+
+    companion object {
+        private val NUMBER_RANGE = 1..45
+    }
+}
