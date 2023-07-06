5547af77f7481dc717a5642a70c0c4d792ff4035
sang5c <skah321@gmail.com>
2023-07-07 00:55:03 +0900

    test(LottoNumber): 1부터 45 사이의 숫자가 아닌 경우 예외가 발생한다

--- src/test/kotlin/lotto/LottoNumberTest.kt
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -1,7 +1,10 @@
 package lotto
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldNotBe
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class LottoNumberTest {
 
@@ -10,5 +13,11 @@ class LottoNumberTest {
         val lottoNumber = LottoNumber(1)
 
         lottoNumber shouldNotBe null
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 46])
+    fun `1부터 45 사이의 숫자가 아닌 경우 예외가 발생한다`(value: Int) {
+        shouldThrow<IllegalArgumentException> { LottoNumber(value) }
     }
 }
