722b430aaec4ca6cf059964a7b84ea29f7846613
sang5c <skah321@gmail.com>
2023-07-08 02:02:44 +0900

    test(LottoTicket): 숫자 목록 여러개를 받아 로또 티켓을 생성한다

--- src/main/kotlin/lotto/LottoTicket.kt
+++ src/main/kotlin/lotto/LottoTicket.kt
@@ -4,4 +4,11 @@ class LottoTicket(private val lottos: List<Lotto>) {
     init {
         require(lottos.isNotEmpty()) { "로또는 1개 이상 구매해야 합니다." }
     }
+
+    companion object {
+        fun from(lottos: List<List<Int>>): LottoTicket {
+            // TODO
+            return LottoTicket(listOf())
+        }
+    }
 }
--- src/test/kotlin/lotto/LottoTicketTest.kt
+++ src/test/kotlin/lotto/LottoTicketTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
 import org.junit.jupiter.api.Test
 
@@ -7,5 +8,16 @@ class LottoTicketTest {
     @Test
     fun `빈 로또를 전달하면 예외가 발생한다`() {
         shouldThrow<IllegalArgumentException> { LottoTicket(listOf()) }
+    }
+
+    @Test
+    fun `숫자 목록 여러개를 받아 로또 티켓을 생성한다`() {
+        val lottos = listOf(
+            listOf(1, 2, 3, 4, 5, 6),
+            listOf(1, 2, 3, 4, 5, 6),
+            listOf(1, 2, 3, 4, 5, 6),
+        )
+
+        shouldNotThrowAny { LottoTicket.from(lottos) }
     }
 }
