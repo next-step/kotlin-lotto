bcd3bdd8ec817185a8c367b3fef40f59e59aeef7
sang5c <skah321@gmail.com>
2023-07-08 00:10:46 +0900

    feat(LottoTicket): 빈 로또를 전달하면 예외가 발생한다

--- src/main/kotlin/lotto/LottoTicket.kt
+++ src/main/kotlin/lotto/LottoTicket.kt
@@ -1,3 +1,7 @@
 package lotto
 
-class LottoTicket(private val lottos: List<Lotto>)
+class LottoTicket(private val lottos: List<Lotto>) {
+    init {
+        require(lottos.isNotEmpty()) { "로또는 1개 이상 구매해야 합니다." }
+    }
+}
