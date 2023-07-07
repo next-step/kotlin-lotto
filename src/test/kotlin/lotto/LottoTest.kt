1b3b4454822a8d5421b078cb9c7b82e86719dcfb
sang5c <skah321@gmail.com>
2023-07-08 02:24:41 +0900

    test(Lotto): 로또를 비교해서 일치하는 숫자의 개수를 반환한다

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 class LottoTest {
@@ -46,5 +47,15 @@ class LottoTest {
         val numbers = listOf(1, 2, 3, 4, 5, 5)
 
         shouldThrow<IllegalArgumentException> { Lotto.from(numbers) }
+    }
+
+    @Test
+    fun `로또를 비교해서 일치하는 숫자의 개수를 반환한다`() {
+        val lotto = Lotto.from(listOf(1, 2, 3, 4, 5, 6))
+        val winningLotto = Lotto.from(listOf(1, 2, 3, 4, 5, 6))
+
+        val result = lotto.match(winningLotto)
+
+        result shouldBe 5
     }
 }
