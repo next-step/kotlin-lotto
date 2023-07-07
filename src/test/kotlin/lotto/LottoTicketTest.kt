d56cca12af18fd70643e5cf8572b03b36cd6b705
sang5c <skah321@gmail.com>
2023-07-07 23:45:29 +0900

    test(LottoTicket): 빈 로또를 전달하면 예외가 발생한다

---
+++ src/test/kotlin/lotto/LottoTicketTest.kt
@@ -0,0 +1,11 @@
+package lotto
+
+import io.kotest.assertions.throwables.shouldThrow
+import org.junit.jupiter.api.Test
+
+class LottoTicketTest {
+    @Test
+    fun `빈 로또를 전달하면 예외가 발생한다`() {
+        shouldThrow<IllegalArgumentException> { LottoTicket(listOf()) }
+    }
+}
