4d588d4c02ab71e2ce227661b148ed2863990af8
sang5c <skah321@gmail.com>
2023-07-08 15:05:31 +0900

    feat(Match): 일치하는 갯수에 따라 당첨 금액을 반환한다

---
+++ src/test/kotlin/lotto/MatchTest.kt
@@ -0,0 +1,16 @@
+package lotto
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class MatchTest {
+
+    @ParameterizedTest
+    @CsvSource(value = ["3,5_000", "4,50_000", "5,1_500_000", "6,2_000_000_000"])
+    fun `당첨 개수를 받아 당첨 금액을 반환한다`(count: Int, winningAmount: Int) {
+        val match = Match.from(count)
+
+        match?.winningAmount shouldBe winningAmount
+    }
+}
