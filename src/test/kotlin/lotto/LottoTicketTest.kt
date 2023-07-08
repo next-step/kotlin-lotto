881a14cc9124a9dae278ba6e76f64961056c1343
sang5c <skah321@gmail.com>
2023-07-08 15:55:52 +0900

    test(LottoTicket): 당첨 로또를 받아 당첨 결과를 반환한다

--- src/test/kotlin/lotto/LottoTicketTest.kt
+++ src/test/kotlin/lotto/LottoTicketTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 class LottoTicketTest {
@@ -19,5 +20,20 @@ class LottoTicketTest {
         )
 
         shouldNotThrowAny { LottoTicket.from(lottos) }
+    }
+
+    @Test
+    fun `당첨 로또를 받아 당첨 결과를 반환한다`() {
+        val lottos = listOf(
+            listOf(1, 2, 3, 4, 5, 6),
+            listOf(1, 2, 3, 4, 5, 7),
+            listOf(1, 2, 3, 4, 7, 8),
+        )
+        val lottoTicket = LottoTicket.from(lottos)
+        val winningLotto = Lotto.from(listOf(1, 2, 3, 4, 5, 6))
+
+        val matches = lottoTicket.match(winningLotto)
+
+        matches shouldBe listOf(Match.SIX, Match.FIVE, Match.FOUR)
     }
 }
