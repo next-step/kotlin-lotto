a283de8b0425e79f07f8a98c09fe6f964f05395a
june.kim <junho85@gmail.com>
2023-07-08 21:48:29 +0900

    PR리뷰 - LottoNumberTest https://github.com/next-step/kotlin-lotto/pull/799#discussion_r1256112388

--- src/test/kotlin/lotto/domain/LottoNumberTest.kt
+++ src/test/kotlin/lotto/domain/LottoNumberTest.kt
@@ -1,11 +1,28 @@
 package lotto.domain
 
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 
 class LottoNumberTest : FunSpec({
-    context("로또 번호는 1~45 범위를 갖습니다.") {
-        LottoNumber.from(1).number shouldBe 1
-        LottoNumber.from(45).number shouldBe 45
+    context("로또 번호는 1~45 범위를 갑습니다. 1~45 이내의 숫자이면 오류가 발생하지 않습니다.") {
+        // given
+        val input = 1
+
+        // when // then
+        shouldNotThrowAny { LottoNumber.from(input) }
+    }
+
+    context("로또 번호는 1~45 범위를 갖습니다. 범위를 벗어나면 오류가 발생합니다.") {
+        withData(
+            0 to "로또 번호는 1부터 45 사이여야 합니다.",
+            46 to "로또 번호는 1부터 45 사이여야 합니다.",
+        ) { (input, expected) ->
+            shouldThrow<IllegalArgumentException> {
+                LottoNumber.from(input)
+            }.message shouldBe expected
+        }
     }
 })
