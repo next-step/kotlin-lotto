a82e4597578716c679d1d5d42d4b93888448a42e
june.kim <junho85@gmail.com>
2023-07-08 20:55:29 +0900

    PR리뷰 - 클래스 구성순서 컨벤션 맞추기 https://github.com/next-step/kotlin-lotto/pull/799#discussion_r1256079548

--- src/main/kotlin/lotto/domain/LottoNumbers.kt
+++ src/main/kotlin/lotto/domain/LottoNumbers.kt
@@ -1,7 +1,15 @@
 package lotto.domain
 
 class LottoNumbers(val numbers: Set<LottoNumber>) {
+    init {
+        require(numbers.size == Lotto.LOTTO_NUMBER_SIZE) { "로또 번호는 6개여야 합니다." }
+    }
+
+    val size: Int
+        get() = numbers.size
+
     constructor(vararg numbers: Int) : this(numbers.map { LottoNumber(it) }.toSet())
+
     constructor(numbers: List<Int>) : this(numbers.map { LottoNumber(it) }.toSet())
 
     fun countMatch(lottoNumbers: LottoNumbers): Int {
@@ -12,12 +20,5 @@ class LottoNumbers(val numbers: Set<LottoNumber>) {
 
     fun match(number: Int): Boolean {
         return numbers.contains(LottoNumber(number))
-    }
-
-    val size: Int
-        get() = numbers.size
-
-    init {
-        require(numbers.size == Lotto.LOTTO_NUMBER_SIZE) { "로또 번호는 6개여야 합니다." }
     }
 }
