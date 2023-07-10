6cfc72b2a962977bfaede2a41cbe5e7a74fc5da6
june.kim <junho85@gmail.com>
2023-07-10 23:33:42 +0900

    PR리뷰 반영 - 내부에서만 호출하는 함수들 private으로 https://github.com/next-step/kotlin-lotto/pull/799#discussion_r1258028444

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -9,28 +9,17 @@ import lotto.domain.WinningLotto
 import lotto.view.InputView
 
 class LottoController {
-    fun inputPurchaseMoney(): Long {
-        return InputView.purchaseMoney()
-    }
-
-    fun inputManualLottoCount(): Int {
-        return InputView.manualLottoCount()
-    }
-
-    fun inputManualLotto(): Lotto {
-        return Lotto(InputView.manualLotto())
-    }
 
     fun inputWinningLotto(): WinningLotto {
         return WinningLotto(Lotto(InputView.winningLotto()), LottoNumber.from(InputView.bonusNumber()))
     }
 
     fun purchaseLottos(): Pair<Lottos, PurchaseMoney> {
-        val purchaseMoney = inputPurchaseMoney()
-        val manualLottoCount = inputManualLottoCount()
+        val purchaseMoney = InputView.purchaseMoney()
+        val manualLottoCount = InputView.manualLottoCount()
         println("수동으로 구매할 번호를 입력해 주세요.")
         val manualPurchaseLottos = (1..manualLottoCount)
-            .map { inputManualLotto() }
+            .map { Lotto(InputView.manualLotto()) }
             .let { Lottos(it) }
 
         return Pair(LottoShop.purchase(purchaseMoney, manualPurchaseLottos), PurchaseMoney(purchaseMoney))
