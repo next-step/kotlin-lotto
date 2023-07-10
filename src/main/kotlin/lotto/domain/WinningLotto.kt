063b79d9a01abc146fe035e601cb8e50d3e3533b
sang5c <skah321@gmail.com>
2023-07-11 00:19:53 +0900

    feat(WinningLotto): 보너스 번호는 로또 번호와 중복되지 않는다

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -1,13 +1,17 @@
 package lotto.domain
 
 class WinningLotto(
-    private val winningNumbers: List<LottoNumber>,
+    private val lotto: Lotto,
     private val bonusNumber: LottoNumber
 ) {
+    init {
+        require(!lotto.numbers.contains(bonusNumber)) { "보너스 번호는 당첨 번호에 포함될 수 없습니다. [${lotto.numbers}, $bonusNumber]" }
+    }
+
     companion object {
         fun from(winningNumbers: List<Int>, bonusNumber: Int): WinningLotto {
             return WinningLotto(
-                winningNumbers.map { LottoNumber(it) },
+                Lotto.from(winningNumbers),
                 LottoNumber(bonusNumber)
             )
         }
