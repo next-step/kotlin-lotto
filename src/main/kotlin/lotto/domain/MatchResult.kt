1f3b16e6a22545c27588e99e9af87b723131628d
sang5c <skah321@gmail.com>
2023-07-10 23:53:46 +0900

    refactor(MatchResult): data 클래스로 변경

--- src/main/kotlin/lotto/domain/MatchResult.kt
+++ src/main/kotlin/lotto/domain/MatchResult.kt
@@ -3,7 +3,7 @@ package lotto.domain
 import java.math.BigDecimal
 import java.math.RoundingMode
 
-class MatchResult(private val matches: List<Match> = emptyList()) {
+data class MatchResult(private val matches: List<Match> = emptyList()) {
     fun rateOfReturn(money: Int): BigDecimal {
         return matches.sumOf { it.winningAmount }
             .toBigDecimal()
@@ -12,18 +12,5 @@ class MatchResult(private val matches: List<Match> = emptyList()) {
 
     fun countOf(match: Match): Int {
         return matches.count { it == match }
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as MatchResult
-
-        return matches == other.matches
-    }
-
-    override fun hashCode(): Int {
-        return matches.hashCode()
     }
 }
