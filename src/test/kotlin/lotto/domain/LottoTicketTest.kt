539f101d15c0a8cfec83f7040968d51e9f72413c
sang5c <skah321@gmail.com>
2023-07-12 23:25:28 +0900

    test(LottoTicket): 로또 티켓은 당첨 로또를 받아 당첨 결과를 반환한다

--- src/test/kotlin/lotto/domain/LottoTicketTest.kt
+++ src/test/kotlin/lotto/domain/LottoTicketTest.kt
@@ -26,4 +26,25 @@ class LottoTicketTest {
             lottoTicket.countOfManualLotto shouldBe 1
         }
     }
+
+    @Test
+    fun `로또 티켓은 당첨 로또를 받아 당첨 결과를 반환한다`() {
+        val lottos = listOf(
+            Lotto.of(listOf(1, 2, 3, 4, 5, 6), LottoType.AUTO),
+            Lotto.of(listOf(1, 2, 3, 4, 5, 6), LottoType.AUTO),
+            Lotto.of(listOf(1, 2, 3, 4, 5, 7), LottoType.MANUAL),
+        )
+        val lottoTicket = LottoTicket(lottos)
+        val winningLotto = WinningLotto.from(listOf(1, 2, 3, 4, 5, 6), 7)
+
+        val matchResult = lottoTicket.match(winningLotto)
+
+        shouldSpec {
+            matchResult.countOf(Rank.FIRST) shouldBe 2
+            matchResult.countOf(Rank.SECOND) shouldBe 1
+            matchResult.countOf(Rank.THIRD) shouldBe 0
+            matchResult.countOf(Rank.FOURTH) shouldBe 0
+            matchResult.countOf(Rank.FIFTH) shouldBe 0
+        }
+    }
 }
