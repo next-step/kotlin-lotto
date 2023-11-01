49185dfeea8a38985959fadcaf23c17782c84388
ALbertIM0427 <guang7226961@naver.com>
2023-11-01 11:36:35 +0900

    feat: 통합 기능 구현

---
+++ src/test/kotlin/stringcalculator/StringCalculatorTest.kt
@@ -0,0 +1,49 @@
+package stringcalculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class StringCalculatorTest {
+
+    @ParameterizedTest
+    @CsvSource(value = ["1,2,3:6","1,2;3:6"], delimiter = ':')
+    fun `커스텀 구분자 없을 때 쉼표 또는 콜론을 구분자로 문자열을 분리하여 숫자들의 합을 구한다`(target: String, expected: Int) {
+        // given
+        val stringCalculator = StringCalculator(Validator(), NumberSumCalculator(), StringParser())
+
+        // when
+        val result = stringCalculator.sum(target)
+
+        // then
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `커스텀 구준자가 있을 때 커스텀 구분자로 문자열을 분리하여 숫자들의 함들 구한다`() {
+        // given
+        val stringCalculator = StringCalculator(Validator(), NumberSumCalculator(), StringParser())
+        val target = "//!\n1!2!3"
+        val expected = 6
+
+        // when
+        val result = stringCalculator.sum(target)
+
+        // then
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `잘못 입력시 예외를 던진다`() {
+        // given
+        val stringCalculator = StringCalculator(Validator(), NumberSumCalculator(), StringParser())
+        val target = "//!\n1!2!-3"
+
+        // when
+        assertThatThrownBy { stringCalculator.sum(target) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("0보다 큰 숫자 이여야 한다.")
+    }
+}
--- src/test/kotlin/stringcalculator/ValidatorTest.kt
+++ src/test/kotlin/stringcalculator/ValidatorTest.kt
@@ -1,29 +1,37 @@
 package stringcalculator
 
-import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class ValidatorTest {
 
-    @ParameterizedTest
-    @CsvSource(value = ["1:2:3:true", "!:1:2:false", "1:a:3:false", "3,4,-2:false"], delimiter = ':')
-    fun `모든 문자가 0보다 큰 숫자 인지 확인한다`(value1: String, value2: String, value3: String, expected: Boolean) {
+    @Test
+    fun `모든 문자가 0보다 큰 숫자이면 예외를 던지지 않는다`() {
         // given
         val validator = Validator()
-        val target = listOf(value1, value2, value3)
+        val target = listOf("1", "2", "3")
+
+        // when,then
+        validator.ensureAllPositiveNumbers(target)
+    }
+
+    @Test
+    fun `문자열중 하나라도 0보다 큰 숫자가 아니면 예외를 던진다`() {
+        // given
+        val validator = Validator()
+        val target = listOf("1", "2", "-3")
 
         // when
-        val result = validator.ensureAllPositiveNumbers(target)
-
-        // then
-        assertThat(result).isEqualTo(expected)
+        assertThatThrownBy { validator.ensureAllPositiveNumbers(target) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .message().isEqualTo("0보다 큰 숫자 이여야 한다.")
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["1", "2", "3"])
-    fun `문자가 0보다 큰 숫자 인이면 예외를 던지지 않는다`(value: String) {
+    fun `문자가 0보다 큰 숫자 아니면 예외를 던지지 않는다`(value: String) {
         // given
         val validator = Validator()
 
@@ -32,18 +40,14 @@ class ValidatorTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["-1", "-2", "a", "!", " ",""])
+    @ValueSource(strings = ["-1", "-2", "a", "!", " ", ""])
     fun `문자가 0보다 큰 숫자가 아니면 예외를 던진다`(target: String) {
         // given
         val validator = Validator()
 
         // when
-        val result = org.junit.jupiter.api.assertThrows<IllegalArgumentException> {
-            validator.ensurePositiveNumber(target)
-        }
-
-        // then
-        assertThat(result.message).isEqualTo("0보다 큰 숫자 이여야 한다.")
+        assertThatThrownBy { validator.ensurePositiveNumber(target) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .message().isEqualTo("0보다 큰 숫자 이여야 한다.")
     }
-
 }
