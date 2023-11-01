f4e33c6c06bba081b6160f9d82a868c317b3babb
ALbertIM0427 <guang7226961@naver.com>
2023-11-01 09:26:41 +0900

    feat: 쉼표 또는 콜론을 구분자로 문자열을 파싱하는 기능 구현

--- src/main/kotlin/stringcalculator/StringParser.kt
+++ src/main/kotlin/stringcalculator/StringParser.kt
@@ -4,4 +4,8 @@ class StringParser {
     fun parse(input: String, delimiter: String): List<String> {
         return input.split(delimiter)
     }
+
+    fun parse(input: String): List<String> {
+        return input.split(',', ';')
+    }
 }
--- src/test/kotlin/stringcalculator/StringParserTest.kt
+++ src/test/kotlin/stringcalculator/StringParserTest.kt
@@ -3,6 +3,7 @@ package stringcalculator
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 class StringParserTest {
 
@@ -18,5 +19,18 @@ class StringParserTest {
         // then
         val expect = expected.split(',')
         assertThat(result).containsAll(expect)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2,3", "1;2;3", "1,2;3"])
+    fun `쉼표 또는 콜론을 구분자로 문자열을 파싱한다`(input: String) {
+        // given
+        val stringParser = StringParser()
+
+        // when
+        val result = stringParser.parse(input)
+
+        // then
+        assertThat(result).containsExactly("1","2","3")
     }
 }
