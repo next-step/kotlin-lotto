e0170abdfef3ce54d4389dda44f27aa3f2124ac5
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-02 16:38:05 +0900

    feat : 음수가 input 으로 들어오게 되면 RuntimeException 을 발생시킵니다

--- src/test/kotlin/stringcalculator/StringAddCalculatorTest.kt
+++ src/test/kotlin/stringcalculator/StringAddCalculatorTest.kt
@@ -1,8 +1,11 @@
 package stringcalculator
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.NullAndEmptySource
@@ -37,7 +40,7 @@ class StringAddCalculatorTest {
 
     @ParameterizedTest
     @CsvSource(value = ["1,2:3#6", "1,2:5#8", "3:6:9#18"], delimiter = '#')
-    fun `구분자를 쉼표(,) 이외에 콜론을 사용할 수 있다`(text:String, expected: Int) {
+    fun `구분자를 쉼표(,) 이외에 콜론을 사용할 수 있다`(text: String, expected: Int) {
         calculator.add(text) shouldBe expected
     }
 
@@ -46,5 +49,14 @@ class StringAddCalculatorTest {
     @CsvSource(value = ["'//;\n1;2;3'#6"], delimiter = '#')
     fun customDelimiter(text: String, expected: Int) {
         calculator.add(text) shouldBe 6
+    }
+
+    @Test
+    fun `문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다`() {
+        val exception = shouldThrow<RuntimeException> {
+            calculator.add("-1")
+        }
+
+        exception.message shouldContain "음수는 불가능"
     }
 }
