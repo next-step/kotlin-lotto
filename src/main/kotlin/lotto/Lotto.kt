0b757124f00a4e2cef58c484a3b15cf5439e02c6
원동재 <wdj0717@midasit.com>
2023-11-02 11:23:20 +0900

    Step2:로또(자동) Lotto 클래스 numbers 프로퍼티 추가 및 shuffle 메소드 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,3 +1,18 @@
 package lotto
 
-class Lotto(val price: Int = 1000)
+class Lotto(val price: Int = LOTTO_PRICE) {
+    val numbers: List<Int> = lottoNumberShuffle()
+
+    private fun lottoNumberShuffle(): List<Int> {
+        return listOf(LOTTO_NUMBER_MIN..LOTTO_NUMBER_MAX)
+            .flatten()
+            .shuffled()
+            .take(LOTTO_NUMBER_SIZE)
+    }
+    companion object {
+        private const val LOTTO_PRICE = 1000
+        private const val LOTTO_NUMBER_SIZE = 6
+        private const val LOTTO_NUMBER_MIN = 1
+        private const val LOTTO_NUMBER_MAX = 45
+    }
+}
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.ints.shouldBeInRange
 import io.kotest.matchers.shouldBe
 
 class LottoTest : FunSpec({
@@ -8,5 +9,22 @@ class LottoTest : FunSpec({
     test("로또 1장의 가격은 1000원이다.") {
         val lotto = Lotto()
         lotto.price shouldBe 1000
+    }
+
+    test("로또 1장은 6개의 숫자를 가진다.") {
+        val lotto = Lotto()
+        lotto.numbers.size shouldBe 6
+    }
+
+    test("로또 1장의 숫자는 1부터 45까지의 숫자이다.") {
+        val lotto = Lotto()
+        lotto.numbers.forEach {
+            it shouldBeInRange 1..45
+        }
+    }
+
+    test("로또 1장의 숫자는 중복되지 않는다.") {
+        val lotto = Lotto()
+        lotto.numbers.toSet().size shouldBe 6
     }
 })
