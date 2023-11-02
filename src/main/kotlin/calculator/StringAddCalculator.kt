670ef2a924b1ddc2fea3ae720582fd54aeaa56aa
jimbaemon <jimbae@kakao.com>
2023-11-03 01:57:46 +0900

    feat: //와 \n 문자 사이에 커스텀 구분자를 지정할 수 있다.

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -3,13 +3,20 @@ package calculator
 import java.util.regex.Pattern
 
 val TO_REGEX: Pattern = Pattern.compile(",|:")
+val CUSTOM_DELIMITER_REGEX: Regex = Regex("//(.)\n(.*)")
 
 class StringAddCalculator {
     fun add(text: String?): Int {
         if (text.isNullOrEmpty()) {
             return 0
         }
-        val numbers = text.split(TO_REGEX)
-        return numbers.sumOf { it.toInt() }
+        
+        return CUSTOM_DELIMITER_REGEX.find(text)?.let {
+            val customDelimiter = it.groupValues[1]
+            return it.groupValues[2].split(customDelimiter)
+                .sumOf { it.toInt() }
+        } ?: run {
+            return text.split(TO_REGEX).sumOf { it.toInt() }
+        }
     }
 }
