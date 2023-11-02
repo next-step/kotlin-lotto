7eb64936d984e664ff3726ff240a0cf6063c768f
yeongun <gjduddns@gmail.com>
2023-11-02 21:32:44 +0900

    refactor: 중복되는 테스트 정리

--- src/test/kotlin/stringpluscalculator/StringParserTest.kt
+++ src/test/kotlin/stringpluscalculator/StringParserTest.kt
@@ -7,22 +7,9 @@ import io.kotest.matchers.shouldBe
 
 class StringParserTest : BehaviorSpec({
 
-    Given("문자열 파서는") {
-        When("구분자를 기준으로") {
-            Then("문자를 분리하여 리스트를 반환한다.") {
-                forAll(
-                    row("1,2", listOf("1", "2")),
-                    row("1,2,3", listOf("1", "2", "3"))
-                ) { input, expected ->
-                    StringParser.parser(input) shouldBe expected
-                }
-            }
-        }
-    }
-
-    Given("문자열 파서의 구분자는") {
-        When("쉼표(,) 또는 콜론(:) 구분자를 기준으로") {
-            Then("문자를 분리하여 리스트를 반환한다.") {
+    Given("구분자가 포함된 문자열이 주어지면") {
+        When("문자열 파서는") {
+            Then("쉼표(,) 또는 콜론(:)를 기준으로 문자열을 분리하여 리스트를 반환한다.") {
                 forAll(
                     row("1,2", listOf("1", "2")),
                     row("1,2:3", listOf("1", "2", "3"))
@@ -33,9 +20,9 @@ class StringParserTest : BehaviorSpec({
         }
     }
 
-    Given("문자열 파서는 기본 구분자 외에") {
-        When("커스텀 구분자를") {
-            Then("지정할 수 있다.") {
+    Given("쉼표와 콜론 외에 커스텀 구분자가 포함된 문자열이 주어지면") {
+        When("문자열 파서는") {
+            Then("구분자를 기준으로 문자열을 분리하여 리스트를 반환한다.") {
                 forAll(
                     row("//;\n1;2;3", listOf("1", "2", "3")),
                     row("//;\n1;2,3", listOf("1", "2", "3"))
