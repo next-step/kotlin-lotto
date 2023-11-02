c374c6f7e77fe938abea20380626a0055d660007
정진혁 <wilgur513@naver.com>
2023-11-02 23:54:13 +0900

    Step1: 문자열 덧셈 계산기 (#814)
    * feat: 문자열 덧셈 계산기 구현
    
    * refactor: 변수 이름 수정

---
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -0,0 +1,43 @@
+package calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import java.lang.IllegalArgumentException
+
+class CalculatorTest {
+
+    @Test
+    fun `빈 문자열은 0을 반환`() {
+        val actual = Calculator("").exec()
+        assertThat(actual).isEqualTo(0)
+    }
+
+    @Test
+    fun `숫자만 있는 경우 바로 반환`() {
+        val actual = Calculator("23").exec()
+        assertThat(actual).isEqualTo(23)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["1,2,3=6", "11:22:33=66", "1,2:3:4,5=15"], delimiter = '=')
+    fun `콤마와 콜론으로 구분하여 덧셈`(expression: String, expect: Int) {
+        val actual = Calculator(expression).exec()
+        assertThat(actual).isEqualTo(expect)
+    }
+
+    @Test
+    fun `커스텀 구분자로 구분하여 덧셈`() {
+        val actual = Calculator("//;\n1;2;3").exec()
+        assertThat(actual).isEqualTo(6)
+    }
+
+    @Test
+    fun `음수일 경우 예외 발생`() {
+        assertThrows<IllegalArgumentException> {
+            Calculator("1,2,-3")
+        }
+    }
+}
