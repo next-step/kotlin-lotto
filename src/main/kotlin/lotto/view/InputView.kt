619eb5a2b21cdbf3d04cec3437e6fc4463616e7a
원동재 <wdj0717@midasit.com>
2023-11-02 12:17:15 +0900

    Step2:로또(자동) 당첨번호 입력 함수 적용

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -5,12 +5,27 @@ object InputView {
     fun inputPrice(): Int {
         println("구입금액을 입력해 주세요.")
         val price = readln()
-        require(price.isNotBlank()) { "입력값이 없습니다." }
+        require(price.isNotBlank()) { BLANK_ERROR_MESSAGE }
         return try {
             price.toInt()
         } catch (e: NumberFormatException) {
-            println("숫자를 입력해주세요.")
+            println(NUMBER_ERROR_MESSAGE)
             inputPrice()
         }
     }
+    fun inputWinningLotto(): List<Int> {
+        println("지난 주 당첨 번호를 입력해 주세요.")
+        val winningLotto = readln()
+        require(winningLotto.isNotBlank()) { BLANK_ERROR_MESSAGE }
+        return try {
+            winningLotto.split(DELIMITER).map { it.trim().toInt() }
+        } catch (e: NumberFormatException) {
+            println(NUMBER_ERROR_MESSAGE)
+            inputWinningLotto()
+        }
+    }
+
+    private const val DELIMITER = ","
+    private const val BLANK_ERROR_MESSAGE = "입력값이 없습니다."
+    private const val NUMBER_ERROR_MESSAGE = "숫자를 입력해주세요."
 }
