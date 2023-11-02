c495d5fdca2306d1cb6f38a1d3c84827a0ac0f7a
hyotaek-jang <hyotaek.jang@linecorp.com>
2023-11-03 00:51:30 +0900

    feat: add StringAddCalculator

---
+++ src/main/kotlin/string/addition/calculator/domain/StringAddCalculator.kt
@@ -0,0 +1,31 @@
+package string.addition.calculator.domain
+
+import string.addition.calculator.view.InputView
+import string.addition.calculator.view.OutputView
+
+object StringAddCalculator {
+    private const val CONTAIN_NOT_NUMBER_MESSAGE = "숫자가 아닌 값이 포함되어 있습니다."
+    private const val NEGATIVE_NUMBER_MESSAGE = "음수는 입력할 수 없습니다."
+    private const val DEFAULT_NUMBER = 0
+
+    fun add(input: String?): Int {
+        if (input.isNullOrBlank()) {
+            return DEFAULT_NUMBER
+        }
+        val normalizedInput = Splitter.normalizeForSplit(input, Splitter.getDelimiters(input))
+        val numbers = Splitter.split(normalizedInput)
+        isValidNumbers(numbers)
+        return numbers.sumOf { it.toInt() }
+    }
+
+    private fun isValidNumbers(numbers: List<String>) {
+        require(numbers.all { it.toIntOrNull() != null }) { CONTAIN_NOT_NUMBER_MESSAGE }
+        require(numbers.all { it.toInt() >= 0 }) { NEGATIVE_NUMBER_MESSAGE }
+    }
+}
+
+fun main() {
+    val input = InputView.receiveCalculatorInput()
+    val result = StringAddCalculator.add(input)
+    OutputView.printResult(result)
+}
