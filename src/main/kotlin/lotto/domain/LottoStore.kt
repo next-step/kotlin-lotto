b5eab3f266576af84a460a21d9e772b8bed99f0c
원동재 <wdj0717@midasit.com>
2023-11-02 12:02:07 +0900

    Step2:로또(자동) LottoStore validate 적용

--- src/main/kotlin/lotto/domain/LottoStore.kt
+++ src/main/kotlin/lotto/domain/LottoStore.kt
@@ -3,7 +3,12 @@ package lotto.domain
 object LottoStore {
     fun buyLottos(inputPrice: Int): List<Lotto> {
         val lottoCount = inputPrice / LOTTO_PRICE
+        this.validateLottoBuy(lottoCount)
         return (1..lottoCount).map { Lotto() }
+    }
+
+    private fun validateLottoBuy(lottoCount: Int) {
+        require(lottoCount > 0) { throw RuntimeException("로또를 구매할 수 없습니다.") }
     }
 
     const val LOTTO_PRICE = 1000
--- src/test/kotlin/lotto/LottoStoreTest.kt
+++ src/test/kotlin/lotto/LottoStoreTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import lotto.domain.LottoStore
@@ -9,5 +10,13 @@ class LottoStoreTest : FunSpec({
         val inputPrice = 1000
         val lottos = LottoStore.buyLottos(inputPrice)
         lottos.size shouldBe 1
+    }
+
+    test("입력받은 금액이 로또 가격보다 낮으면 구매할 수 없다.") {
+        val inputPrice = 500
+        val exception = shouldThrow<RuntimeException> {
+            LottoStore.buyLottos(inputPrice)
+        }
+        exception.message shouldBe "로또를 구매할 수 없습니다."
     }
 })
