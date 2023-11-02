084929416ee5dd951657752e52c7698e04844992
ALbertIM0427 <guang7226961@naver.com>
2023-11-03 08:56:29 +0900

    refactor: 필요 없는 메서드 삭제

--- src/main/kotlin/stringcalculator/StringCalculatorNumberValidator.kt
+++ src/main/kotlin/stringcalculator/StringCalculatorNumberValidator.kt
@@ -2,8 +2,6 @@ package stringcalculator
 
 class StringCalculatorNumberValidator {
 
-    fun ensureAllPositiveNumbers(input: List<String>) = input.forEach { ensurePositiveNumber(it) }
-
     fun ensurePositiveNumber(it: String) {
         val number = it.toIntOrNull()
         require(number != null && number >= 0) { "0보다 큰 숫자 이여야 한다." }
--- src/test/kotlin/stringcalculator/StringCalculatorNumberValidatorTest.kt
+++ src/test/kotlin/stringcalculator/StringCalculatorNumberValidatorTest.kt
@@ -1,33 +1,10 @@
 package stringcalculator
 
 import org.assertj.core.api.Assertions.assertThatThrownBy
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class StringCalculatorNumberValidatorTest {
-
-    @Test
-    fun `모든 문자가 0보다 큰 숫자이면 예외를 던지지 않는다`() {
-        // given
-        val stringCalculatorNumberValidator = StringCalculatorNumberValidator()
-        val target = listOf("1", "2", "3")
-
-        // when,then
-        stringCalculatorNumberValidator.ensureAllPositiveNumbers(target)
-    }
-
-    @Test
-    fun `문자열중 하나라도 0보다 큰 숫자가 아니면 예외를 던진다`() {
-        // given
-        val stringCalculatorNumberValidator = StringCalculatorNumberValidator()
-        val target = listOf("1", "2", "-3")
-
-        // when
-        assertThatThrownBy { stringCalculatorNumberValidator.ensureAllPositiveNumbers(target) }
-            .isInstanceOf(IllegalArgumentException::class.java)
-            .message().isEqualTo("0보다 큰 숫자 이여야 한다.")
-    }
 
     @ParameterizedTest
     @ValueSource(strings = ["1", "2", "3"])
