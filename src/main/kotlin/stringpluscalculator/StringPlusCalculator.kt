44147544793521cbb8f6d8e8abf6349d4a028b3a
yeongun <gjduddns@gmail.com>
2023-11-02 21:18:02 +0900

    feat: 문자열 덧셈 계산기가 구분자로 구분한 결과값을 가지고 덧셈을 수행하도록 구현

--- src/main/kotlin/stringpluscalculator/StringPlusCalculator.kt
+++ src/main/kotlin/stringpluscalculator/StringPlusCalculator.kt
@@ -6,6 +6,15 @@ object StringPlusCalculator {
         if (input.isNullOrBlank()) {
             return 0
         }
-        return 0
+        val parseResult = StringParser.parser(input)
+        return plusElements(parseResult)
+    }
+
+    private fun plusElements(parseResult: List<String>): Int {
+        var result = 0
+        parseResult.forEach {
+            result += it.toInt()
+        }
+        return result
     }
 }
--- src/test/kotlin/stringpluscalculator/StringPlusCalculatorTest.kt
+++ src/test/kotlin/stringpluscalculator/StringPlusCalculatorTest.kt
@@ -7,12 +7,24 @@ import io.kotest.matchers.shouldBe
 
 class StringPlusCalculatorTest : BehaviorSpec({
 
-    Given("문자열 덧셈 계산기는") {
-        When("빈 문자열이나 null을 입력받으면") {
+    Given("빈 문자열이나 null이 주어지면") {
+        When("문자열 덧셈 계산기는") {
             Then("0을 반환한다.") {
                 forAll(
                     row("", 0),
                     row(null, 0)
+                ) { input, expected ->
+                    StringPlusCalculator.plus(input) shouldBe expected
+                }
+            }
+        }
+    }
+
+    Given("구분자를 가지는 문자열이 주어지면") {
+        When("문자열 덧셈 계산기는") {
+            Then("구분한 결과를 가지고 덧셈을 수행한다.") {
+                forAll(
+                    row("1,2,3", 6)
                 ) { input, expected ->
                     StringPlusCalculator.plus(input) shouldBe expected
                 }
