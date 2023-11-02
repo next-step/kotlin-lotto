b449c1667dbd985e292af5575b52aa6584544993
원동재 <wdj0717@midasit.com>
2023-11-02 11:00:58 +0900

    Step2:step1 피드백 반영 require 활용

--- src/main/kotlin/stringcalculator/NumberValidator.kt
+++ src/main/kotlin/stringcalculator/NumberValidator.kt
@@ -4,8 +4,7 @@ object NumberValidator {
 
     private const val ZERO = 0
     fun negativeValidate(number: Int): Int {
-        if (number < ZERO)
-            throw RuntimeException("음수는 입력할 수 없습니다.")
+        require(number >= ZERO) { throw RuntimeException("음수는 입력할 수 없습니다.") }
         return number
     }
 }
