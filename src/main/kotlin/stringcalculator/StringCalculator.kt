ea134c8e1f0c2f5f7cb6973b9fb2d75a79491c07
ALbertIM0427 <guang7226961@naver.com>
2023-11-03 08:40:09 +0900

    refactor: 파서 기능을 모두 파서가 맡도록 변경

--- src/main/kotlin/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/stringcalculator/StringCalculator.kt
@@ -1,29 +1,11 @@
 package stringcalculator
 
 class StringCalculator(
-    private val validator: Validator,
     private val stringParser: StringParser
 ) {
 
     fun sum(numbersAsString: String): Int {
-        if (stringParser.hasCustomDelimiter(numbersAsString)) {
-            return sumWithCustomDelimiter(numbersAsString)
-        }
-        return sumWithDefaultDelimiter(numbersAsString)
+        val numbers = stringParser.parse(numbersAsString)
+        return numbers.sum()
     }
-
-    private fun sumWithCustomDelimiter(numbersAsString: String): Int {
-        val parseDelimiter = stringParser.parseDelimiter(numbersAsString)
-        val operations = stringParser.parse(parseDelimiter.data, parseDelimiter.delimiter)
-        validator.ensureAllPositiveNumbers(operations)
-        return sumOfOperations(operations)
-    }
-
-    private fun sumWithDefaultDelimiter(numbersAsString: String): Int {
-        val operations = stringParser.parse(numbersAsString)
-        validator.ensureAllPositiveNumbers(operations)
-        return sumOfOperations(operations)
-    }
-
-    private fun sumOfOperations(operations: List<String>) = operations.sumOf { it.toInt() }
 }
--- src/main/kotlin/stringcalculator/StringParser.kt
+++ src/main/kotlin/stringcalculator/StringParser.kt
@@ -1,21 +1,26 @@
 package stringcalculator
 
-class StringParser {
-    fun parse(input: String, delimiter: String): List<String> {
-        return input.split(delimiter)
+class StringParser(private val validator: Validator = Validator()) {
+    fun parse(numbersAsString: String, delimiter: String): List<String> {
+        return numbersAsString.split(delimiter)
     }
 
-    fun parse(input: String): List<String> {
-        return input.split(',', ';')
+    fun parse(numbersAsString: String): List<Int> {
+        if (hasCustomDelimiter(numbersAsString)) {
+            return parse(
+                parseDelimiter(numbersAsString).data, parseDelimiter(numbersAsString).delimiter
+            ).map { validator.ensurePositiveNumber(it);it.toInt() }
+        }
+        return numbersAsString.split(',', ';').map { validator.ensurePositiveNumber(it);it.toInt() }
     }
 
-    fun hasCustomDelimiter(input: String): Boolean {
-        return input.startsWith("//") && input.contains("\n")
+    fun hasCustomDelimiter(numbersAsString: String): Boolean {
+        return numbersAsString.startsWith("//") && numbersAsString.contains("\n")
     }
 
-    fun parseDelimiter(input: String): ParsingData {
-        val delimiter = input.substring(2, input.indexOf("\n"))
-        val data = input.substring(input.indexOf("\n").plus(1))
+    fun parseDelimiter(numbersAsString: String): ParsingData {
+        val delimiter = numbersAsString.substring(2, numbersAsString.indexOf("\n"))
+        val data = numbersAsString.substring(numbersAsString.indexOf("\n").plus(1))
         return ParsingData(delimiter, data)
     }
 }
