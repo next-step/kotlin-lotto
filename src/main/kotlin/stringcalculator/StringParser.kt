8fa7e52d41d37b95e1e161c469ff0fa35a52385d
ALbertIM0427 <guang7226961@naver.com>
2023-11-03 08:51:43 +0900

    refactor: 상수 처리

--- src/main/kotlin/stringcalculator/StringParser.kt
+++ src/main/kotlin/stringcalculator/StringParser.kt
@@ -11,16 +11,25 @@ class StringParser(private val validator: Validator = Validator()) {
                 parseDelimiter(numbersAsString).data, parseDelimiter(numbersAsString).delimiter
             ).map { validator.ensurePositiveNumber(it);it.toInt() }
         }
-        return numbersAsString.split(',', ';').map { validator.ensurePositiveNumber(it);it.toInt() }
+        return numbersAsString.split(*DEFAULT_DELIMITERS).map { validator.ensurePositiveNumber(it);it.toInt() }
     }
 
     fun hasCustomDelimiter(numbersAsString: String): Boolean {
-        return numbersAsString.startsWith("//") && numbersAsString.contains("\n")
+        return numbersAsString.startsWith(CUSTOM_DELIMITER_PREFIX) && numbersAsString.contains(CUSTOM_DELIMITER_END)
     }
 
     fun parseDelimiter(numbersAsString: String): ParsingData {
-        val delimiter = numbersAsString.substring(2, numbersAsString.indexOf("\n"))
-        val data = numbersAsString.substring(numbersAsString.indexOf("\n").plus(1))
+        val delimiter =
+            numbersAsString.substring(CUSTOM_DELIMITER_START_INDEX, numbersAsString.indexOf(CUSTOM_DELIMITER_END))
+        val data = numbersAsString.substring(numbersAsString.indexOf(CUSTOM_DELIMITER_END).plus(DELIMITER_END_SKIP))
         return ParsingData(delimiter, data)
+    }
+
+    companion object {
+        private val DEFAULT_DELIMITERS = arrayOf(",", ";")
+        private const val CUSTOM_DELIMITER_PREFIX = "//"
+        private const val CUSTOM_DELIMITER_END = "\n"
+        private const val CUSTOM_DELIMITER_START_INDEX = 2
+        private const val DELIMITER_END_SKIP = 1
     }
 }
