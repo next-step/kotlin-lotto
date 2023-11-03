3205d051f9c486aed6828493fe23a7c3670f3b76
[김정식] <wjdtlr0920@gmail.com>
2023-11-03 13:34:33 +0900

    Feat: Test Calculator Input Test(null, blank)

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,5 +1,13 @@
 package calculator
 
+/**
+ * 문자열 계산기 (덧셈, 커스텀 구분자)
+ * */
 object Calculator {
-
+    /**
+     * 계산 동작
+     * */
+    fun calculate(equation: String?): Int {
+        return 0
+    }
 }
