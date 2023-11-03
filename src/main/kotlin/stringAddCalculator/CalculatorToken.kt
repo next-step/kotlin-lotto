233aaf5ca0caf562d30ed5626404b94774e157dc
silvio.oivlis <silvio.oivlis@kakaocorp.com>
2023-11-03 17:01:53 +0900

    fix: CalculatorToken check 로직 수정

--- src/main/kotlin/stringAddCalculator/CalculatorToken.kt
+++ src/main/kotlin/stringAddCalculator/CalculatorToken.kt
@@ -7,7 +7,7 @@ value class CalculatorToken(private val value: String) {
     fun convertOrThrow(formula: String?, customDelimiter: String?): Int {
         val convertedValue = value.toIntOrNull()
         require(convertedValue != null) { throw RuntimeException("invalid formula: $formula failed to parse $value with delimiter $customDelimiter") }
-        check(convertedValue != 0) { throw RuntimeException("negative number: $value") }
+        check(convertedValue > 0) { throw RuntimeException("negative number: $value") }
         return convertedValue
     }
 }
