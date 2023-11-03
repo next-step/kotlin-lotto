2396a0739003740816bac73e9eb6e0610d8f3bcd
silvio.oivlis <silvio.oivlis@kakaocorp.com>
2023-11-03 16:48:24 +0900

    refactor: CalculatorToken.convertOrThrow init 로직 분리

--- src/main/kotlin/stringAddCalculator/CalculatorToken.kt
+++ src/main/kotlin/stringAddCalculator/CalculatorToken.kt
@@ -4,11 +4,10 @@ import java.lang.RuntimeException
 
 @JvmInline
 value class CalculatorToken(private val value: String) {
-    fun convertOrThrow(formula: String?, customDelimiter: String?) =
-        value.toIntOrNull()?.let { converted ->
-            if (converted < 0)
-                throw RuntimeException("negative number: $value")
-            else converted
-        }
-            ?: throw RuntimeException("invalid formula: $formula failed to parse $value with delimiter $customDelimiter")
+    fun convertOrThrow(formula: String?, customDelimiter: String?): Int {
+        val convertedValue = value.toIntOrNull()
+        require(convertedValue != null) { throw RuntimeException("invalid formula: $formula failed to parse $value with delimiter $customDelimiter") }
+        check(convertedValue != 0) { throw RuntimeException("negative number: $value") }
+        return convertedValue
+    }
 }
