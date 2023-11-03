f4c50b3dcf1fa22cebc19a69663bc15d1a1c96f3
loren-hyuni <loren.hyuni@kakaocorp.com>
2023-11-04 00:47:34 +0900

    mission: step2 로또(자동) 리뷰 반영

--- src/test/kotlin/lotto/dto/LottoNumbersTest.kt
+++ src/test/kotlin/lotto/dto/LottoNumbersTest.kt
@@ -3,7 +3,13 @@ package lotto.dto
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class LottoNumbersTest {
     @Test
     fun `로또 번호 생성`() {
@@ -32,10 +38,23 @@ class LottoNumbersTest {
         }
     }
 
-    @Test
-    fun `로또 숫자 비교`() {
+    @ParameterizedTest
+    @MethodSource("lottoCandidates")
+    fun `로또 숫자 비교`(compareLotto: LottoNumbers, expected: Int) {
         val lottoNumbers = LottoNumbers(listOf(1, 2, 3, 4, 5, 6))
-        val lottoNumbers2 = LottoNumbers(listOf(1, 2, 3, 4, 5, 9))
-        assertThat(lottoNumbers.compareLottoNumbers(lottoNumbers2)).isEqualTo(5)
+        assertThat(lottoNumbers.compareLottoNumbers(compareLotto)).isEqualTo(expected)
+    }
+
+    companion object {
+        @JvmStatic
+        fun lottoCandidates(): Stream<Arguments> = Stream.of(
+            Arguments.arguments(LottoNumbers(listOf(1, 2, 3, 4, 5, 6)), 6),
+            Arguments.arguments(LottoNumbers(listOf(1, 2, 3, 4, 5, 7)), 5),
+            Arguments.arguments(LottoNumbers(listOf(1, 2, 3, 4, 8, 7)), 4),
+            Arguments.arguments(LottoNumbers(listOf(1, 2, 3, 9, 8, 7)), 3),
+            Arguments.arguments(LottoNumbers(listOf(1, 2, 10, 9, 8, 7)), 2),
+            Arguments.arguments(LottoNumbers(listOf(1, 11, 10, 9, 8, 7)), 1),
+            Arguments.arguments(LottoNumbers(listOf(12, 11, 10, 9, 8, 7)), 0)
+        )
     }
 }
