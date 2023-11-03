c4bbe494e99b446f4cc1a8c355b582844f6b7cfe
chacha.roni <chacha.roni@kakaocorp.com>
2023-11-03 19:03:18 +0900

    step2 피드백 반영 - 수익률 출력문구 추가

--- src/main/kotlin/lotto/ui/OutputView.kt
+++ src/main/kotlin/lotto/ui/OutputView.kt
@@ -24,6 +24,6 @@ class OutputView {
 
     fun printEarningRate(result: List<Prize>, purchased: Int) {
         val earningRate = result.sumOf { it.prize } / purchased.toDouble()
-        println("총 수익률은 $earningRate 입니다.")
+        println("총 수익률은 $earningRate 입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)")
     }
 }
