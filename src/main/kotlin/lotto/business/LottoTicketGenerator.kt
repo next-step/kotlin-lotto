12a4452b540b2c2562d35b5cabb0106b9169e2b9
ALbertIM0427 <guang7226961@naver.com>
2023-11-03 13:48:59 +0900

    feat: 티켓 개수 만큼 티켓을 생성하는 기능 구현

---
+++ src/main/kotlin/lotto/business/LottoTicketGenerator.kt
@@ -0,0 +1,12 @@
+package lotto.business
+
+class LottoTicketGenerator(private val randomLottoPicker: RandomLottoPicker) {
+    fun generate(): LottoTicket {
+        val lottoNumbers = randomLottoPicker.pick()
+        return LottoTicket(lottoNumbers)
+    }
+
+    fun generate(quantity: Int): List<LottoTicket> {
+        return (1..quantity).map { generate() }
+    }
+}
---
+++ src/test/kotlin/lotto/business/LottoTicketGeneratorTest.kt
@@ -0,0 +1,30 @@
+package lotto.business
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LottoTicketGeneratorTest{
+    @Test
+    fun `티켓을 생성한다`() {
+        // given
+        val lottoTicketGenerator = LottoTicketGenerator(RandomLottoPicker())
+
+        // when
+        val lottoTicket = lottoTicketGenerator.generate()
+
+        // then
+        assertThat(lottoTicket).isInstanceOf(LottoTicket::class.java)
+    }
+
+    @Test
+    fun `티켓을 원하는 개수만큼 생성한다`() {
+        // given
+        val lottoTicketGenerator = LottoTicketGenerator(RandomLottoPicker())
+
+        // when
+        val lottoTickets = lottoTicketGenerator.generate(3)
+
+        // then
+        assertThat(lottoTickets).hasSize(3)
+    }
+}
