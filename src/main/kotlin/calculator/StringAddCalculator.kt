cfe09d2842c9e9022952417769bb5a34410c2fa7
jihyun-kim13 <jihyun.kim13@linecorp.com>
2023-11-03 23:54:04 +0900

    feat: 쉼표 또는 콜론을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환한다

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -1,10 +1,15 @@
 package calculator
 
 object StringAddCalculator {
+    private const val DELIMITERS = ",|:"
+
     fun add(text: String?): Int {
         if (text.isNullOrBlank()) {
             return 0
         }
-        TODO()
+
+        val numbers = text.split(DELIMITERS.toRegex())
+
+        return numbers.sumOf { it.toInt() }
     }
 }
--- src/test/kotlin/calculator/StringAddCalculatorTest.kt
+++ src/test/kotlin/calculator/StringAddCalculatorTest.kt
@@ -2,6 +2,7 @@ package calculator
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.NullSource
 import org.junit.jupiter.params.provider.ValueSource
@@ -14,5 +15,25 @@ class StringAddCalculatorTest {
         val actual = StringAddCalculator.add(text)
 
         assertThat(actual).isEqualTo(0)
+    }
+
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
+    @Test
+    fun oneNumber() {
+        val text = "1"
+
+        val actual = StringAddCalculator.add(text)
+
+        assertThat(actual).isEqualTo(1)
+    }
+
+    @DisplayName("쉼표 또는 콜론을 구분자로 가지는 문자열을 입력하면 구분자를 기준으로 분리한 숫자의 합을 반환한다")
+    @Test
+    fun delimiter() {
+        val text = "1,2:3"
+
+        val actual = StringAddCalculator.add(text)
+
+        assertThat(actual).isEqualTo(6)
     }
 }
