c80cee076e731236f8457139b7c84bcfef660724
Factoriall <skarlgus0910@gmail.com>
2023-11-03 21:53:02 +0900

    step1: create Parser.kt and move methods

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -2,22 +2,11 @@ package calculator
 
 class Calculator {
     fun calculate(text: String?): Int {
-        if (text.isNullOrEmpty()) return 0
-        if (text.toIntOrNull() != null) return text.toInt()
-
-        val tokens = parseWithDelimeter(text).map { Token(it) }
-
-        return ParsedTokens(tokens).result
+        if (text.isNullOrEmpty()) return ZERO
+        return Parser.parse(text).sum
     }
 
-    private fun parseWithDelimeter(text: String): List<String> {
-        val result = Regex("//(.)\n(.*)").find(text)
-
-        return result?.let {
-            val customDelimiter = it.groupValues[1]
-            it.groupValues[2].split(customDelimiter)
-        } ?: run {
-            text.split(",|:".toRegex())
-        }
+    companion object {
+        const val ZERO = 0
     }
 }
