3b3c140ccc615c442ca02ef6a47ba937c386cb4d
jihyun-kim13 <jihyun.kim13@linecorp.com>
2023-11-04 02:21:19 +0900

    refactor: extract method

--- src/main/kotlin/calculator/StringInputParser.kt
+++ src/main/kotlin/calculator/StringInputParser.kt
@@ -7,17 +7,21 @@ object StringInputParser {
     fun parse(input: String): Operands {
         val customDelimiterMatchResult = CUSTOM_DELIMITER_MATCH_REGEX.find(input)
 
-        val splitInput = if (customDelimiterMatchResult != null) {
-            val customDelimiter = customDelimiterMatchResult.groupValues[1]
-            customDelimiterMatchResult.groupValues[2].split(customDelimiter)
-        } else {
-            input.split(DELIMITERS_REGEX)
-        }
+        val splitInput = splitInput(input, customDelimiterMatchResult)
 
         return Operands(
             numbers = splitInput.map {
                 it.toIntOrNull() ?: throw RuntimeException("숫자 이외의 값은 계산할 수 없습니다.")
             },
         )
+    }
+
+    private fun splitInput(input: String, customDelimiterMatchResult: MatchResult?): List<String> {
+        return if (customDelimiterMatchResult != null) {
+            val customDelimiter = customDelimiterMatchResult.groupValues[1]
+            customDelimiterMatchResult.groupValues[2].split(customDelimiter)
+        } else {
+            input.split(DELIMITERS_REGEX)
+        }
     }
 }
