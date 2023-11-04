a320184d4ea0d07ecdb484b80c9b70bb0c0c4438
JunHyun Lee <dlwnsgus777@naver.com>
2023-11-04 13:57:48 +0900

    feat: 빈 문자열, null 입력시 기능 개발

--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -1,15 +1,34 @@
 package calculator
 
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertDoesNotThrow
 
 class StringCalculator {
+    fun inputText(text: String?): Int {
+        if (text.isNullOrEmpty()) {
+            return 0
+        }
 
+        return 1
+    }
 }
 
 class StringCalculatorTest {
+    private lateinit var calculator: StringCalculator
+
+    @BeforeEach
+    fun init() {
+        calculator = StringCalculator()
+    }
+
     @Test
-    fun `문자열 계산기를 생성할 수 있다`() {
-        assertDoesNotThrow { StringCalculator() }
+    fun `문자열 계산기에 빈 문자열을 입력할 경우 0을 반환한다`() {
+        calculator.inputText("") shouldBe 0
+    }
+
+    @Test
+    fun `문자열 계산기에 null을 입력할 경우 0을 반환한다`() {
+        calculator.inputText(null) shouldBe 0
     }
 }
