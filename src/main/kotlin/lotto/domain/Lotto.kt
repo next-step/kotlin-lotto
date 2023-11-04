57b58641f05a5dfdec66e87a25684e42b2ebe1a6
jihyun-kim13 <jihyun.kim13@linecorp.com>
2023-11-04 15:19:56 +0900

    feat: 로또 1장이 가지고 있는 번호는 6개이다

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,7 +1,10 @@
 package lotto.domain
 
 class Lotto {
+    val numbers: List<Int> = List(NUMBER_COUNT) { it }
+
     companion object {
         const val PRICE = 1000
+        private const val NUMBER_COUNT = 6
     }
 }
--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -12,4 +12,13 @@ class LottoTest {
 
         assertThat(actual).isEqualTo(1000)
     }
+
+    @Test
+    fun `로또 1장이 가지고 있는 번호는 6개이다`() {
+        val lotto = Lotto()
+
+        val actual = lotto.numbers
+
+        assertThat(actual).hasSize(6)
+    }
 }
