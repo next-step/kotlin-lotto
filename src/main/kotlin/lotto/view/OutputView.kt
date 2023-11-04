bcbbe09683e328f28418455a4c658a8561023607
jihyun-kim13 <jihyun.kim13@linecorp.com>
2023-11-05 02:13:02 +0900

    refactor: modify method name

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -3,7 +3,7 @@ package lotto.view
 import lotto.domain.Lotto
 
 object OutputView {
-    fun outputLotto(purchaseCount: Int, lottos: List<Lotto>) {
+    fun printLottoOutput(purchaseCount: Int, lottos: List<Lotto>) {
         println("${purchaseCount}개를 구매했습니다.")
 
         lottos.forEach {
