80002d5034cf1caa4cea921ea840656919d5f34b
youn.loge <youn.loge@kakaocorp.com>
2023-11-05 02:11:30 +0900

    feat(step1): 문자열 표현식으로부터 숫자 추출하는 로직 추가

--- src/main/kotlin/calculator/StringNumberParser.kt
+++ src/main/kotlin/calculator/StringNumberParser.kt
@@ -1,14 +1,65 @@
 package calculator
 
 class StringNumberParser {
+    fun getNumbers(expression: String?): List<Int> {
+        if (expression.isNullOrBlank()) {
+            return listOf(0)
+        }
+
+        val customSeparator = getCustomSeparator(expression)
+        val separatorRegex = getSeparatorRegex(customSeparator)
+        val stringNumbersExpression = getStringNumbersExpression(expression, customSeparator)
+
+        return getNumbers(stringNumbersExpression, separatorRegex)
+    }
+
     fun getCustomSeparator(input: String): String? {
         return Regex(CUSTOM_SEPARATOR_PATTERN).find(input)
             ?.groupValues
             ?.get(1)
     }
 
+    private fun getSeparatorRegex(customSeparator: String?): Regex {
+        val customSeparatorPattern = if (customSeparator != null) {
+            """|$customSeparator"""
+        } else {
+            ""
+        }
+
+        return (DEFAULT_SEPARATOR_PATTERN + customSeparatorPattern).toRegex()
+    }
+
+    private fun getStringNumbersExpression(input: String, customSeparator: String?): String {
+        return if (customSeparator != null) {
+            input.split(CUSTOM_SEPARATOR_STRING_NUMBERS_SEPARATOR)[1]
+        } else {
+            input
+        }
+    }
+
+    private fun getNumbers(stringNumbersExpression: String, separatorRegex: Regex): List<Int> {
+        if (stringNumbersExpression.isBlank()) {
+            return listOf(0)
+        }
+
+        return stringNumbersExpression
+            .split(separatorRegex)
+            .map { convertStringNumberToInt(it) }
+    }
+
+    private fun convertStringNumberToInt(stringNumber: String): Int {
+        return try {
+            stringNumber
+                .toULong()
+                .toInt()
+        } catch (e: Exception) {
+            throw RuntimeException("숫자는 0 또는 자연수만 가능합니다. $stringNumber")
+        }
+    }
+
     companion object {
-        private const val CUSTOM_SEPARATOR_PATTERN= """^//(.)\\n(.*)$"""
+        private const val DEFAULT_SEPARATOR_PATTERN = """,|:"""
+        private const val CUSTOM_SEPARATOR_STRING_NUMBERS_SEPARATOR = "\\n"
+        private const val CUSTOM_SEPARATOR_PATTERN = """^//(.)\\n(.*)$"""
     }
 }
-
--- src/test/kotlin/calculator/StringNumberParserTest.kt
+++ src/test/kotlin/calculator/StringNumberParserTest.kt
@@ -1,7 +1,11 @@
 package calculator
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.blocking.forAll
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
+import java.lang.RuntimeException
 
 class StringNumberParserTest : FunSpec({
     val stringNumberParser = StringNumberParser()
@@ -41,6 +45,100 @@ class StringNumberParserTest : FunSpec({
             val result = stringNumberParser.getCustomSeparator(input)
 
             result shouldBe null
+        }
+    }
+
+    context("문자열 표현식 내 숫자 추출 동작 테스트") {
+        test("null, 공백, 빈 문자열을 전달할 경우 0 하나만 포함한 리스트 반환") {
+            forAll(
+                row(null),
+                row(""),
+                row(" ")
+            ) { input ->
+                val result = stringNumberParser.getNumbers(input)
+
+                result shouldBe listOf(0)
+            }
+        }
+
+        test("숫자가 1개만 포함된 표현식의 경우 해당 숫자만 포함하는 리스트 반환") {
+            val input = "1"
+
+            val result = stringNumberParser.getNumbers(input)
+
+            result shouldBe listOf(1)
+        }
+
+        test("2개 이상의 숫자가 포함된 표현식의 경우 일치하는 숫자 모두 리스트로 반환") {
+            val input = "1,2"
+
+            val result = stringNumberParser.getNumbers(input)
+
+            result shouldBe listOf(1, 2)
+        }
+    }
+
+    context("문자열 표현식 내 구분자에 따른 동작 테스트") {
+        test("기본 구분자로만 이루어진 표현식의 정상 반환") {
+            forAll(
+                row("1,2", listOf(1, 2)),
+                row("1:2", listOf(1, 2)),
+                row("1,2,3", listOf(1, 2, 3)),
+                row("1:2:3", listOf(1, 2, 3)),
+                row("1,2:3", listOf(1, 2, 3))
+            ) { input, answer ->
+                val result = stringNumberParser.getNumbers(input)
+
+                result shouldBe answer
+            }
+        }
+
+        test("커스텀 구분자가 포함된 표현식의 정상 반환") {
+            forAll(
+                row("//!\\n", listOf(0)),
+                row("//!\\n1", listOf(1)),
+                row("//!\\n1!2", listOf(1, 2)),
+                row("//!\\n1!2,3", listOf(1, 2, 3)),
+                row("//!\\n1!2:3", listOf(1, 2, 3)),
+                row("//!\\n1,2:3", listOf(1, 2, 3)),
+            ) { input, answer ->
+                val result = stringNumberParser.getNumbers(input)
+
+                result shouldBe answer
+            }
+        }
+    }
+
+    context("문자열 표현식 분석기 예외 테스트") {
+        test("잘못된 숫자가 입력된 경우 RuntimeException") {
+            forAll(
+                row("-1"),
+                row("a"),
+                row("a,-1"),
+                row("//!\\n-1"),
+                row("//!\\na"),
+                row("//!\\na,-1"),
+            ) { input ->
+                shouldThrow<RuntimeException> {
+                    stringNumberParser.getNumbers(input)
+                }
+            }
+        }
+
+        test("기본 구분자가 아닌 구분자가 포함되어있는 경우 RuntimeException") {
+            val input = "1?2"
+
+            shouldThrow<RuntimeException> {
+                stringNumberParser.getNumbers(input)
+            }
+        }
+
+        test("커스텀 구분자가 2글자 이상인 경우 RuntimeException") {
+            val input = """//!!\n"""
+
+            shouldThrow<RuntimeException> {
+                stringNumberParser.getNumbers(input)
+            }
         }
     }
 })
