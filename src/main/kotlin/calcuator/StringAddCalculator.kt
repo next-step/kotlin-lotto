c68b3c6ca1272f52196a7d11cecca86fd0b12cf7
bongbong <kbk5190@naver.com>
2023-11-04 16:16:23 +0900

    feat: StringAddCalculator 덧셈 구현

--- src/main/kotlin/calcuator/StringAddCalculator.kt
+++ src/main/kotlin/calcuator/StringAddCalculator.kt
@@ -4,6 +4,7 @@ object StringAddCalculator {
 
     fun add(input: String?): Int {
         if (input.isNullOrEmpty()) return 0
-        TODO()
+        val operands = Splitter.split(input).map { AddOperand.of(it) }
+        return Adder.sum(operands)
     }
 }
--- src/test/kotlin/calculator/StringAddCalculatorSpec.kt
+++ src/test/kotlin/calculator/StringAddCalculatorSpec.kt
@@ -1,21 +1,12 @@
 package calculator
 
 import calcuator.StringAddCalculator
-import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.data.forAll
 import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class StringAddCalculatorSpec : FunSpec({
-    test("문자열 계산기는 문자열을 입력 받는다") {
-        val string = "1"
-
-        shouldNotThrowAny {
-            StringAddCalculator.add(string)
-        }
-    }
-
     test("빈 문자열 또는 null이 들어오면 0을 반환한다") {
         forAll(
             row(""),
@@ -25,6 +16,21 @@ class StringAddCalculatorSpec : FunSpec({
             val result = StringAddCalculator.add(input)
 
             result shouldBe 0
+        }
+    }
+
+    test("주어진 문자열의 합을 계산한다") {
+        forAll(
+            row("//;\n1;2,3:4", 10),
+            row("//?\n1?2?3?4", 10),
+            row("//;\n1;2;3;4", 10),
+            row("1,2,3:4", 10),
+            row("1", 1),
+        ) { input, expect ->
+
+            val result = StringAddCalculator.add(input)
+
+            result shouldBe expect
         }
     }
 })
