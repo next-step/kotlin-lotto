3c8906b89277a3e8a4215cbf5ab24a5df6ff8c0c
dong <dong.22@kakaoenterprise.com>
2023-11-05 05:44:31 +0900

    refactor : 로또 게임을 진행하는데 필요한 테스트 데이터셋 관련 fixture 추가

---
+++ src/test/kotlin/lotto/LottoFixture.kt
@@ -0,0 +1,31 @@
+package lotto
+
+import lotto.model.Game
+import lotto.model.LottoNumber
+import lotto.model.Round
+
+object LottoFixture {
+    val game1: Game = gameOf(1, 2, 3, 4, 5, 6)
+    val game2: Game = gameOf(7, 8, 9, 10, 11, 12)
+    val game3: Game = gameOf(1, 11, 21, 31, 41, 51, 52)
+    val game4: Game = gameOf(2, 22, 32, 42, 52, 53)
+    val Round: Round = roundOf(game1, game2, game3, game4)
+
+    private fun roundOf(vararg game: Game): Round {
+        return Round(
+            ArrayDeque(
+                game.toList()
+            )
+        )
+    }
+
+    private fun gameOf(vararg values: Int): Game {
+        return Game(
+            values
+                .map { LottoNumber(it) }
+                .toSet()
+        )
+    }
+
+
+}
