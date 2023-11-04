924558360defbf6683425cae284fcd6d4e98cdd4
seongmo-jeon <oeeen3@gmail.com>
2023-11-04 18:05:53 +0900

    refactor: Exception 에 메시지 추가

--- src/main/kotlin/stringaddcalculator/StringAddCalculator.kt
+++ src/main/kotlin/stringaddcalculator/StringAddCalculator.kt
@@ -3,7 +3,7 @@ package stringaddcalculator
 class StringAddCalculator {
     fun calculate(input: String?): Int {
         val validInput = input.takeUnless { it.isNullOrBlank() } ?: return 0
-        NEGATIVE_NUMBER_REGEX.takeIf { it.containsMatchIn(validInput) }?.let { throw RuntimeException() }
+        NEGATIVE_NUMBER_REGEX.takeIf { it.containsMatchIn(validInput) }?.let { throw RuntimeException("음수는 입력할 수 없습니다.") }
         val numbers: List<Int> = getIntegersFromInput(validInput)
         return numbers.sum()
     }
@@ -11,7 +11,7 @@ class StringAddCalculator {
     private fun getIntegersFromInput(input: String): List<Int> =
         if (input.contains(CUSTOM_DELIMITER_REGEX)) {
             val matchResult = CUSTOM_DELIMITER_REGEX.matchEntire(input)
-            val (delimiter, numbers) = matchResult?.destructured ?: throw IllegalArgumentException()
+            val (delimiter, numbers) = matchResult?.destructured ?: throw IllegalArgumentException("잘못된 입력입니다. $input")
             numbers.split(delimiter).map { it.toInt() }
         } else {
             input.split(*DEFAULT_DELIMITERS).map { it.toInt() }
