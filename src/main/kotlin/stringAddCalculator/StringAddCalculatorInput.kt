040591f0d6ab69a5b19e6b7e1c5a9a9166cbb11e
10000001a <pgdn06288888@gmail.com>
2023-11-04 17:06:53 +0900

    REFAC: 불필요한 try catch 문 제거

--- src/main/kotlin/stringAddCalculator/StringAddCalculatorInput.kt
+++ src/main/kotlin/stringAddCalculator/StringAddCalculatorInput.kt
@@ -24,19 +24,12 @@ class StringAddCalculatorInput(private val input: String) {
 
     private fun getIntegerPart(): String = input.split("\n").last()
 
-    private fun convertToInt(text: String): Int {
-        try {
-            return text.toInt().also {
-                if (it < 0) {
-                    throw RuntimeException()
-                }
-            }
-        } catch (e: NumberFormatException) {
-            throw RuntimeException()
-        }
-    }
+    private fun convertToInt(text: String): Int = text.toIntOrNull()
+        ?.takeIf { it >= MIN_NUMBER }
+        ?: throw RuntimeException()
 
     companion object {
         private val DEFAULT_DELIMITERS = listOf(",", ":")
+        private const val MIN_NUMBER = 0
     }
 }
