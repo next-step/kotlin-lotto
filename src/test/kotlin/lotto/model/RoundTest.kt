59e251d13e68686ef3e785b978d00061622de6a5
dong <dong.22@kakaoenterprise.com>
2023-11-05 06:01:22 +0900

    test : 당첨자 집계 테스트코드 추가

--- src/test/kotlin/lotto/model/RoundTest.kt
+++ src/test/kotlin/lotto/model/RoundTest.kt
@@ -1,11 +1,39 @@
 package lotto.model
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 import lotto.LottoFixture
 
 class RoundTest : StringSpec({
     "1등 당첨번호를 입력하면 1등 당첨자를 집계할수 있어야한다" {
+        val actual = LottoFixture.Round.winnerAggregate(LottoFixture.winner1st)
+        actual.countOf1st shouldBe 1
+        actual.countOf3rd shouldBe 0
+        actual.countOf4th shouldBe 0
+        actual.countOf5th shouldBe 0
+    }
 
-        val round = LottoFixture.Round.winnerAggregate(LottoFixture.winner1st)
+    "3등 당첨번호를 입력하면 1등 당첨자를 집계할수 있어야한다" {
+        val actual = LottoFixture.Round.winnerAggregate(LottoFixture.winner3rd)
+        actual.countOf1st shouldBe 0
+        actual.countOf3rd shouldBe 1
+        actual.countOf4th shouldBe 0
+        actual.countOf5th shouldBe 0
+    }
+
+    "4등 당첨번호를 입력하면 1등 당첨자를 집계할수 있어야한다" {
+        val actual = LottoFixture.Round.winnerAggregate(LottoFixture.winner4rd)
+        actual.countOf1st shouldBe 0
+        actual.countOf3rd shouldBe 0
+        actual.countOf4th shouldBe 1
+        actual.countOf5th shouldBe 0
+    }
+
+    "5등 당첨번호를 입력하면 1등 당첨자를 집계할수 있어야한다" {
+        val actual = LottoFixture.Round.winnerAggregate(LottoFixture.winner5rd)
+        actual.countOf1st shouldBe 0
+        actual.countOf3rd shouldBe 0
+        actual.countOf4th shouldBe 0
+        actual.countOf5th shouldBe 1
     }
 })
