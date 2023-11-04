1b64bfabfe8f2bf5c0f2536d7ba831fa1ef81b56
dong <dong.22@kakaoenterprise.com>
2023-11-05 05:53:10 +0900

    test : 테스트 클래스를 작성

---
+++ src/test/kotlin/lotto/model/RoundTest.kt
@@ -0,0 +1,14 @@
+package lotto.model
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import lotto.LottoFixture
+
+class RoundTest : StringSpec({
+    "1등 당첨번호를 입력하면 1등 당첨자를 집계할수 있어야한다" {
+
+        val round = LottoFixture.Round.winnerAggregate(LottoFixture.winner1st)
+
+    }
+
+})
