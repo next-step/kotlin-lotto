4338e92eb67b69117bd58d870b89d637cb22d4ee
10000001a <pgdn06288888@gmail.com>
2023-11-04 17:25:42 +0900

    REFAC: 매직 넘버와 리터럴을 상수로 관리

--- src/main/kotlin/stringAddCalculator/StringAddCalculator.kt
+++ src/main/kotlin/stringAddCalculator/StringAddCalculator.kt
@@ -3,9 +3,13 @@ package stringAddCalculator
 class StringAddCalculator {
     fun calculate(input: String?): Int {
         if (input.isNullOrEmpty()) {
-            return 0
+            return EMPTY_RESULT
         }
 
         return StringAddCalculatorInput(input).parse().sumOf { it }
+    }
+
+    companion object {
+        private const val EMPTY_RESULT = 0
     }
 }
--- src/main/kotlin/stringAddCalculator/StringAddCalculatorInput.kt
+++ src/main/kotlin/stringAddCalculator/StringAddCalculatorInput.kt
@@ -15,7 +15,7 @@ class StringAddCalculatorInput(private val input: String) {
     }
 
     private fun getCustomDelimiter(): String {
-        val result = Regex("//(.)\n(.*)").find(input) ?: return ""
+        val result = Regex(CUSTOM_DELIMITER_PATTERN).find(input) ?: return ""
 
         return result.groupValues[1]
     }
@@ -23,11 +23,12 @@ class StringAddCalculatorInput(private val input: String) {
     private fun getIntegerPart(): String = input.split("\n").last()
 
     private fun convertToInt(text: String): Int = text.toIntOrNull()
-        ?.takeIf { it >= MIN_NUMBER }
+        ?.takeIf { it >= MIN_VALID_NUMBER }
         ?: throw RuntimeException()
 
     companion object {
         private val DEFAULT_DELIMITERS = listOf(",", ":")
-        private const val MIN_NUMBER = 0
+        private const val CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)"
+        private const val MIN_VALID_NUMBER = 0
     }
 }
