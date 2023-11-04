317df17f16f7f134a075c9ad67cbd825eb21b826
Ahn-seokjoo <tjrwn0716@naver.com>
2023-11-04 16:48:13 +0900

    [feature] 커스텀 구분자를 사용해 계산을 tdd로 구현

--- src/main/kotlin/tdd_string_add_calculator/Calculator.kt
+++ src/main/kotlin/tdd_string_add_calculator/Calculator.kt
@@ -2,15 +2,51 @@ package tdd_string_add_calculator
 
 object Calculator {
     fun calculate(express: String): String {
-        val list = express.split(SPLIT_REGEX)
+        val customDelimiter = checkCustomDelimiter(express)
+        val newRegex = getRegexWithCustomDelimiter(customDelimiter)
+        val customDelimiterString = getCustomDelimiterString(
+            express = express,
+            customDelimiter = customDelimiter
+        )
+        val newExpress = getNewExpress(
+            express = express,
+            customDelimiter = customDelimiter,
+            customDelimiterString = customDelimiterString
+        )
+        val list = newExpress.split(newRegex)
         return calculate(list)
     }
+
+    private fun getRegexWithCustomDelimiter(customDelimiter: String) = "([${SPLIT_REGEX + customDelimiter}])".toRegex()
+
+    private fun checkCustomDelimiter(express: String): String =
+        if (express.startsWith(CUSTOM_DELIMITER_START_WITH)) {
+            express.getOrNull(CUSTOM_DELIMITER_INDEX).toString()
+        } else {
+            EMPTY_STRING
+        }
+
+    /**
+     * CustomDelimiter 가 존재 안하면 기존 express,
+     * CustomDelimiter 가 존재 하면 전달 받은 customDelimiterString 을 잘라서 express 만 남김
+     */
+    private fun getNewExpress(
+        express: String,
+        customDelimiter: String,
+        customDelimiterString: String
+    ): String = if (customDelimiter.isBlank()) express else express.replace(customDelimiterString, "")
+
+    /**
+     * custom delimiter 만 찾기
+     */
+    private fun getCustomDelimiterString(express: String, customDelimiter: String): String =
+        if (customDelimiter.isNotBlank()) (express.substring(FIRST_SUBSTRING_RANGE..LAST_SUBSTRING_RANGE)) else express
 
     private fun calculate(list: List<String>): String {
         return runCatching {
             list.map { string ->
                 val number = changeLettersToNumber(string)
-                validateNumber(number)
+                validateNumberIsNegativeNumber(number)
             }.reduce { acc, i -> acc + i }
                 .toString()
         }.getOrElse { throw RuntimeException() }
@@ -18,8 +54,15 @@ object Calculator {
 
     private fun changeLettersToNumber(it: String): Int = it.ifBlank { DEFAULT_VALUE }.toInt()
 
-    private fun validateNumber(number: Int) = if (number < 0) throw RuntimeException() else number
+    private fun validateNumberIsNegativeNumber(number: Int) =
+        if (number < MINIMUM_NUMBER) throw RuntimeException() else number
 
-    private val SPLIT_REGEX = "([,:])".toRegex()
+    private const val EMPTY_STRING = ""
+    private const val CUSTOM_DELIMITER_START_WITH = "/"
+    private const val CUSTOM_DELIMITER_INDEX = 2
+    private const val MINIMUM_NUMBER = 0
+    private const val FIRST_SUBSTRING_RANGE = 0
+    private const val LAST_SUBSTRING_RANGE = 3
+    private const val SPLIT_REGEX = ",:"
     private const val DEFAULT_VALUE = "0"
 }
