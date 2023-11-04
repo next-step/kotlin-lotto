280e20a15630f542dae329bfa380a7c0d3d57508
moses.lee(이상후)/kakao <moses.lee@kakaocorp.com>
2023-11-04 09:45:33 +0900

    feat: 파서 별도 객체 분리
    # step 1

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -1,34 +1,21 @@
 package calculator
 
-class StringAddCalculator {
+import calculator.Parser.parse
 
-    private fun String.toInt(): Int {
-        val number = Integer.parseInt(this)
-        require(number >= 0) {"음수는 입력할 수 없습니다."}
-        return number
-    }
+class StringAddCalculator {
 
     fun add(text: String?): Int {
         if (text.isNullOrEmpty()) {
-            return 0
+            return RETURN_VALUE
         }
 
-        try {
-            return text.toInt()
-        } catch (_: Exception) {
-        }
-
-        val result = Regex("//(.)\n(.*)").find(text)
-        val delimiter = result?.groupValues?.get(1)
-
-        val standardDelimiter = Regex("[,:]")
-
-        if (delimiter.isNullOrEmpty()) {
-            return text.split(standardDelimiter).sumOf { it.toInt() }
-        } else {
-            val tokens = result.groupValues.get(2).split(delimiter)
-            return tokens.sumOf { it.toInt() }
-        }
+        val tokens = parse(text)
+        return tokens.sum()
     }
 
+    companion object {
+        val PARSER_RULE = Regex("//(.)\n(.*)")
+        val STD_DELIMITER = Regex("[,:]")
+        const val RETURN_VALUE = 0
+    }
 }
