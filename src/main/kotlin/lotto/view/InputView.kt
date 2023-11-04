023677dba67f7fb5600f1f637b9ca65373440cc3
jihyun-kim13 <jihyun.kim13@linecorp.com>
2023-11-05 02:11:57 +0900

    feat: 지난주 당첨 번호를 입력받을 수 있다

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,8 +1,16 @@
 package lotto.view
 
 object InputView {
+    private const val WINNING_NUMBERS_INPUT_DELIMITER = ","
+
     fun readPurchaseAmountInput(): Long {
         println("구입금액을 입력해 주세요.")
         return readln().toLong()
+    }
+
+    fun readWinningNumbersInput(): List<Int> {
+        println("지난주 당첨 번호를 입력해 주세요.")
+        return readln().split(WINNING_NUMBERS_INPUT_DELIMITER)
+            .map { it.trim().toInt() }
     }
 }
