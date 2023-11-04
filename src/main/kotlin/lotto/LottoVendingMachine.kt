66a6eb67972e015b3054d7d01a3db3181eebb334
yeongun <gjduddns@gmail.com>
2023-11-04 11:11:35 +0900

    [Step2] feat: 구입한 금액에 맞는 로또를 자동으로 발급하는 로또 자판기 구현

---
+++ src/main/kotlin/lotto/LottoVendingMachine.kt
@@ -0,0 +1,11 @@
+package lotto
+
+private const val LOTTO_PRICE = 1000
+
+object LottoVendingMachine {
+    fun generate(input: Int): List<Lotto> {
+        val amount = input / LOTTO_PRICE
+        val lottoNumbers = LottoNumbers()
+        return (1..amount).map { lottoNumbers.generate() }
+    }
+}
