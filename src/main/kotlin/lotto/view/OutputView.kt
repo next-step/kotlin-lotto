fcc82dc850c39929b4cb7b86c6e8d9fa57b20434
jihyun-kim13 <jihyun.kim13@linecorp.com>
2023-11-05 02:29:19 +0900

    feat: 당첨 통계를 출력한다

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,6 +1,8 @@
 package lotto.view
 
 import lotto.domain.Lotto
+import lotto.domain.Prize
+import lotto.domain.WinningStatistic
 
 object OutputView {
     fun printLottoOutput(purchaseCount: Int, lottos: List<Lotto>) {
@@ -10,5 +12,22 @@ object OutputView {
             println(it.numbers)
         }
         println()
+    }
+
+    fun printWinningStatistic(winningStatistic: WinningStatistic) {
+        println("\n당첨 통계")
+        println("---------")
+
+        val countPerPrize = winningStatistic.countPerPrize
+        printWinningResult(Prize.FOURTH, countPerPrize[Prize.FOURTH])
+        printWinningResult(Prize.THIRD, countPerPrize[Prize.THIRD])
+        printWinningResult(Prize.SECOND, countPerPrize[Prize.SECOND])
+        printWinningResult(Prize.FIRST, countPerPrize[Prize.FIRST])
+
+        println("총 수익률은 ${winningStatistic.profitRate}입니다.")
+    }
+
+    private fun printWinningResult(prize: Prize, matchedNumberCount: Int?) {
+        println("${prize.matchedNumberCount}개 일치 (${prize.winningAmount}원)- ${matchedNumberCount ?: 0}개")
     }
 }
