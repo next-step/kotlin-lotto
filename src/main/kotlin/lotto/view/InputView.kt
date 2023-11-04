412753edfcbee4cf0d0754aece7fe0884e8d5253
dong <dong.22@kakaoenterprise.com>
2023-11-04 12:49:11 +0900

    feat : 클라이언트와 주고받아야할 UI 에 대해서 정한다

---
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -0,0 +1,18 @@
+package lotto.view
+
+import lotto.model.WinningNumbers
+
+object InputView {
+    fun purchaseAmount(priceOfGame :Int): Int {
+        "구입금액을 입력해 주세요."
+        TODO()
+        val n = 14
+        "$n 개를 구매했습니다."
+    }
+
+    fun drawing(): WinningNumbers {
+        "지난 주 당첨 번호를 입력해 주세요."
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -0,0 +1,28 @@
+package lotto.view
+
+import lotto.model.LottoStatistics
+import lotto.model.Round
+
+object OutputView {
+    fun presetRound(transaction: Round) {
+        "[8, 21, 23, 41, 42, 43]\n" +
+                "[3, 5, 11, 16, 32, 38]\n" +
+                "[7, 11, 16, 35, 36, 44]\n" +
+                "[1, 8, 11, 31, 41, 42]\n" +
+                "[13, 14, 16, 38, 42, 45]\n" +
+                "[7, 11, 30, 40, 42, 43]\n" +
+                "[2, 13, 22, 32, 38, 45] << 이렇게 출력해야함"
+        TODO("Not yet implemented")
+    }
+
+    fun presentPrizes(s: LottoStatistics) {
+        "당첨 통계\n" +
+                "---------\n" +
+                "3개 일치 (5000원)- 1개\n" +
+                "4개 일치 (50000원)- 0개\n" +
+                "5개 일치 (1500000원)- 0개\n" +
+                "6개 일치 (2000000000원)- 0개\n" +
+                "총 수익률은 0.35입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)"
+        TODO("Not yet implemented")
+    }
+}
