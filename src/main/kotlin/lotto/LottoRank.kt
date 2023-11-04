6b2fd4e840e073a9625e4601d1f83bded4dfb9d2
Factoriall <skarlgus0910@gmail.com>
2023-11-04 11:41:39 +0900

    step2: create LottoRank

---
+++ src/main/kotlin/lotto/LottoRank.kt
@@ -0,0 +1,9 @@
+package lotto
+
+enum class LottoRank(val containCount: Int, val money: Int) {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
+    NONE(0, 0)
+}
