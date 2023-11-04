a557e82baad67b85d938a2609d4bf60baec752b7
seongmo-jeon <oeeen3@gmail.com>
2023-11-04 22:08:47 +0900

    feat: 로또 구매 개수 출력

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,7 +1,9 @@
 package lotto
 
 import lotto.view.InputView
+import lotto.view.OutputView
 
 fun main() {
     val purchaseAmount = InputView.getPurchaseAmount()
+    OutputView.printLottoCount(purchaseAmount / 1000)
 }
