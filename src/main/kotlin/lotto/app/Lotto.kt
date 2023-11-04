2cb083c3317fa1441a5d43e8557f1b9f337adf3b
dong <dong.22@kakaoenterprise.com>
2023-11-04 12:50:18 +0900

    feat : 로또 App 의 역할에 대하여 정의했다

---
+++ src/main/kotlin/lotto/app/Lotto.kt
@@ -0,0 +1,13 @@
+package lotto.app
+
+import lotto.model.Round
+
+class Lotto {
+    fun transaction(amount: Int): Round {
+        TODO("Not yet implemented")
+    }
+
+    fun priceOfGame(): Int {
+        TODO()
+    }
+}
