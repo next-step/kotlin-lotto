5e39b7ae2e951084b12c26d3bca2cb6b79e135e9
bongbong <kbk5190@naver.com>
2023-11-04 15:12:31 +0900

    feat: 커스텀 구분자로 문자열을 숫자로 변경

--- src/main/kotlin/calcuator/AddRequest.kt
+++ src/main/kotlin/calcuator/AddRequest.kt
@@ -4,12 +4,28 @@ class AddRequest(
     val numbers: List<Int>
 ) {
     companion object {
-        private const val COMMA_DELIMITER = ","
-        private const val COLON_DELIMITER = ":"
+        private val DEFAULT_DELIMITERS = arrayOf(",", ":")
+        private val REGEX_FOR_CUSTOM_DELIMITER = """^//(.)\n(.*)$""".toRegex()
         fun from(input: String): AddRequest = AddRequest(toNumbers(input))
 
-        private fun toNumbers(input: String): List<Int> = input
-            .split(COMMA_DELIMITER, COLON_DELIMITER)
-            .map { it.toInt() }
+        private fun toNumbers(input: String): List<Int> {
+            val (customDelimiter, restString) = extractCustomDelimiter(input)
+            val result = splitByDelimiter(restString, customDelimiter)
+            return result.map { it.toInt() }
+        }
+
+        private fun extractCustomDelimiter(input: String): Pair<String?, String> {
+            val matchResult = REGEX_FOR_CUSTOM_DELIMITER.find(input) ?: return null to input
+            val (customDelimiter, restString) = matchResult.destructured
+            return customDelimiter to restString
+        }
+
+        private fun splitByDelimiter(input: String, customDelimiter: String?): List<String> {
+            val delimiters: Array<String> = DEFAULT_DELIMITERS.let {
+                if (customDelimiter != null) arrayOf(customDelimiter) + it
+                else it
+            }
+            return input.split(*delimiters)
+        }
     }
 }
--- src/test/kotlin/calculator/AddRequestSpec.kt
+++ src/test/kotlin/calculator/AddRequestSpec.kt
@@ -20,10 +20,11 @@ class AddRequestSpec : FunSpec({
 
     context("구분자로 숫자 리스트 생성") {
         withData(
-            nameFn = { "기본 구분자(쉼표(,), 콜론(:))로 문자열 ${it.first}가 숫자리스트${it.second}로 변환된다" },
+            nameFn = { "기본 구분자(쉼표(,), 콜론(:))와 커스텀 구분자로 문자열 ${it.first}이 숫자리스트${it.second}로 변환된다" },
             "1,2" to listOf(1, 2),
             "1:2" to listOf(1, 2),
-            "1,2:3" to listOf(1, 2, 3)
+            "1,2:3" to listOf(1, 2, 3),
+            "//;\n1;2" to listOf(1, 2),
         ) {
             val result = AddRequest.from(it.first)
 
