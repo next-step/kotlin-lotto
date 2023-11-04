2d9dfb6090bae0fd5f2000c232e083ff6c981ddb
jeongdalma <jeongdalma@gmail.com>
2023-11-04 22:55:18 +0900

    feat: Customer가 구입금액을 입력한다

---
+++ src/main/kotlin/lotto/domain/Customer.kt
@@ -0,0 +1,18 @@
+package lotto.domain
+
+class Customer(money: String) {
+    val money: Int
+
+    init {
+        require(validations.all { it.invoke(money) })
+        this.money = money.toInt()
+    }
+
+    companion object {
+        private val isNotBlank: (String) -> Boolean = { it.isNotBlank() }
+        private val isDigit: (String) -> Boolean = { it.all { char -> char.isDigit() } }
+        private val isDivisibleByThousand: (String) -> Boolean = { it.toInt() % 1000 == 0 }
+
+        private val validations = listOf(isNotBlank, isDigit, isDivisibleByThousand)
+    }
+}
---
+++ src/test/kotlin/lotto/view/CustomerTest.kt
@@ -0,0 +1,37 @@
+package lotto.view
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import lotto.domain.Customer
+import java.lang.IllegalArgumentException
+
+class CustomerTest : BehaviorSpec({
+
+    Given("로또를 구매할 금액이 입력되었을 때") {
+        When("입력이 정수이고 천원 단위라면") {
+            val money = "15000"
+            Then("고객이 될 수 있다.") {
+                Customer(money).money shouldBe 15000
+            }
+        }
+
+        When("입력이 정수이지만 천원 단위가 아니라면") {
+            val money = "15001"
+            Then("입력이 잘못됐다는 예외를 던진다.") {
+                shouldThrow<IllegalArgumentException> {
+                    Customer(money)
+                }
+            }
+        }
+
+        When("입력이 정수가 아니라면") {
+            val money = "1500a"
+            Then("입력이 잘못됐다는 예외를 던진다.") {
+                shouldThrow<IllegalArgumentException> {
+                    Customer(money)
+                }
+            }
+        }
+    }
+})
