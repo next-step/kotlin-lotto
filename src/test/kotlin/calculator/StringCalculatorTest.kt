b4976bae42e3f9d12e00686548bb724f840d2bad
JunHyun Lee <dlwnsgus777@naver.com>
2023-11-04 14:17:27 +0900

    etc: lint 맞추기

--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -2,7 +2,6 @@ package calculator
 
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.NullAndEmptySource
 import org.junit.jupiter.params.provider.ValueSource
