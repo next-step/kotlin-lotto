382d61b3e3cd347b52a295aabd382fe179b465e5
jimbaemon <jimbae@kakao.com>
2023-11-05 02:03:29 +0900

    feat: 로또 랜덤 생성기 개발

---
+++ src/main/kotlin/lotto/RandomLottoGenerator.kt
@@ -0,0 +1,8 @@
+package lotto
+
+class RandomLottoGenerator : LottoGenerator {
+    override fun generate(): Lotto {
+        val numbers = (1..45).shuffled().take(6).toSet()
+        return Lotto(numbers)
+    }
+}
