db8ce020380a7f3b004db97c7b629cadadf48412
bongbong <kbk5190@naver.com>
2023-11-04 15:50:30 +0900

    feat: 음수가 들어오는 경우 예외 발생

--- src/main/kotlin/calcuator/AddOperand.kt
+++ src/main/kotlin/calcuator/AddOperand.kt
@@ -7,10 +7,12 @@ value class AddOperand private constructor(
     val value: Int
 ) {
     companion object {
+        private const val MIN_VALUE = 0
         fun of(stringNumber: String): AddOperand =
             stringNumber
                 .toIntOrNull()
+                ?.takeIf { it >= MIN_VALUE }
                 ?.let { AddOperand(it) }
-                ?: throw IllegalArgumentException("피연산자는 정수여야 합니다")
+                ?: throw IllegalArgumentException("피연산자는 양수여야 합니다")
     }
 }
--- src/test/kotlin/calculator/AddOperandSpec.kt
+++ src/test/kotlin/calculator/AddOperandSpec.kt
@@ -5,16 +5,19 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.data.forAll
 import io.kotest.data.row
-import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 
 class AddOperandSpec : FunSpec({
     test("문자열을 덧셈 피연산자로 변환한다") {
-        val stringNumber = "1"
+        forAll(
+            row("0", 0),
+            row("1", 1),
+        ) { stringNumber, expect ->
 
-        val result = AddOperand.of(stringNumber)
+            val result = AddOperand.of(stringNumber)
 
-        result.value shouldBe 1
+            result.value shouldBe expect
+        }
     }
 
     test("숫자 이외의 값이 들어오는 경우 피연산자 생성에 실패한다") {
@@ -22,11 +25,19 @@ class AddOperandSpec : FunSpec({
             row(""),
             row("  "),
             row("!")
-        ) { input ->
+        ) { stringValue ->
 
             shouldThrow<IllegalArgumentException> {
-                AddOperand.of(input)
+                AddOperand.of(stringValue)
             }
+        }
+    }
+
+    test("음수가 경우 피연산자 생성에 실패한다") {
+        val stringValue = "-1"
+
+        shouldThrow<IllegalArgumentException> {
+            AddOperand.of(stringValue)
         }
     }
 })
