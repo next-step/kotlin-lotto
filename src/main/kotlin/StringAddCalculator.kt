7c7da281d7e185966e6d06dd2186246f4629fe76
eunwoo song <presentsong@naver.com>
2023-11-06 02:18:27 +0900

    feat: 문자열 계산기 빈 문자열이나 null 이면 0 반환 기능 구현

---
+++ src/main/kotlin/StringAddCalculator.kt
@@ -0,0 +1,10 @@
+class StringAddCalculator {
+    fun add(text: String?): Int {
+        text ?: return 0
+        if (text.isEmpty()) {
+            return 0
+        }
+
+        return 0
+    }
+}
