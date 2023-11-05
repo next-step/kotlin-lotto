d17cdcd2d9812e6226a1950fb672fd319a5dff13
eunwoo song <presentsong@naver.com>
2023-11-06 02:26:07 +0900

    feat: 커스텀 문자열 처리 기능 구현

--- src/main/kotlin/StringAddCalculator.kt
+++ src/main/kotlin/StringAddCalculator.kt
@@ -4,6 +4,13 @@ class StringAddCalculator {
         if (text.isEmpty()) {
             return 0
         }
+
+        if (text.matches(Regex("//(.)\n(.*)"))) {
+            val customDelimiter = text[2]
+            val numbers = text.substring(4).split(customDelimiter)
+            return numbers.sumOf { it.toInt() }
+        }
+
         val numbers = text.split("[,|:]".toRegex())
 
         return numbers.sumOf { it.toInt() }
