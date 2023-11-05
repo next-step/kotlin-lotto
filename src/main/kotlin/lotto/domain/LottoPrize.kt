b58cb2a4ad80332c8c9ef4e91232f9bc935f9461
wooddy.kim <wooddy.kim@kakaocorp.com>
2023-11-05 21:29:55 +0900

    feat: 보너스볼이 일치하는 경우 2등을 반환하도록 설정

--- src/main/kotlin/lotto/domain/LottoPrize.kt
+++ src/main/kotlin/lotto/domain/LottoPrize.kt
@@ -2,7 +2,8 @@ package lotto.domain
 
 enum class LottoPrize(val matchingCount: Int, value: Int) {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
     FIFTH(3, 5_000),
     MISS(0, 0);
@@ -11,6 +12,15 @@ enum class LottoPrize(val matchingCount: Int, value: Int) {
 
     companion object {
         val WINNING_RANK = values().filter { it != MISS }
-        fun from(matchingCount: Int): LottoPrize = values().find { it.matchingCount == matchingCount } ?: MISS
+
+        fun from(matchingCount: Int, matchBonus: Boolean): LottoPrize {
+            return values().find {
+                if (it == SECOND) {
+                    return@find it.matchingCount == matchingCount && matchBonus
+                }
+
+                it.matchingCount == matchingCount
+            } ?: MISS
+        }
     }
 }
