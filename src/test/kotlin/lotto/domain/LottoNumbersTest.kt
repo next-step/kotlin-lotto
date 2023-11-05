8ca0156439307347c8a175f565742f5fe5c43f3d
wooddy.kim <wooddy.kim@kakaocorp.com>
2023-11-05 21:47:32 +0900

    feat: 로또 번호 포함 여부를 확인하는 메서드 추가

--- src/test/kotlin/lotto/domain/LottoNumbersTest.kt
+++ src/test/kotlin/lotto/domain/LottoNumbersTest.kt
@@ -2,10 +2,24 @@ package lotto.domain
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.ExpectSpec
+import io.kotest.matchers.shouldBe
 import lotto.test.LottoNumberGenerator
 
 class LottoNumbersTest : ExpectSpec({
 
+    context("1,2,3,4,5,6 번호가 존재할 때") {
+        val lottoNumbers = LottoNumberGenerator.generate(1, 2, 3, 4, 5, 6)
+
+        expect("1을 입력하면 contains 결과는 true이다.") {
+            val target = LottoNumber(1)
+            LottoNumbers(lottoNumbers).contains(target) shouldBe true
+        }
+
+        expect("6을 입력하면 contains 결과는 true이다.") {
+            val target = LottoNumber(6)
+            LottoNumbers(lottoNumbers).contains(target) shouldBe true
+        }
+    }
 
     expect("번호 개수가 6개이고 중복되지 않으면 객체를 생성한다.") {
         val numbers = LottoNumberGenerator.generate(1, 2, 3, 4, 5, 6)
