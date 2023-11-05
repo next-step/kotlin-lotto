0692a530388a9f9eaa3add8d3135951651bd2ae6
wooddy.kim <wooddy.kim@kakaocorp.com>
2023-11-05 21:51:23 +0900

    refactor: 상금별 보너스 포함 여부를 속성으로 정의하도록 변경

--- src/main/kotlin/lotto/domain/LottoPrize.kt
+++ src/main/kotlin/lotto/domain/LottoPrize.kt
@@ -1,12 +1,12 @@
 package lotto.domain
 
-enum class LottoPrize(val matchingCount: Int, value: Int) {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    MISS(0, 0);
+enum class LottoPrize(val matchingCount: Int, value: Int, val includeBonus: Boolean) {
+    FIRST(6, 2_000_000_000, false),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000, false),
+    FOURTH(4, 50_000, false),
+    FIFTH(3, 5_000, false),
+    MISS(0, 0, false);
 
     val amount: Amount = Amount(value)
 
@@ -15,7 +15,7 @@ enum class LottoPrize(val matchingCount: Int, value: Int) {
 
         fun from(matchingCount: Int, matchBonus: Boolean): LottoPrize {
             return values().find {
-                if (it == SECOND) {
+                if (it.includeBonus) {
                     return@find it.matchingCount == matchingCount && matchBonus
                 }
 
--- src/test/kotlin/lotto/domain/LottoRankTest.kt
+++ src/test/kotlin/lotto/domain/LottoRankTest.kt
@@ -6,8 +6,8 @@ import io.kotest.matchers.shouldBe
 class LottoRankTest : ExpectSpec({
 
     expect("일치 개수를 입력하면 당첨금별 카운트를 반환한다.") {
-        val matchingCount = LottoPrize.values().map { it.matchingCount }
-        val rank = LottoRank(matchingCount, Amount(1000))
+        val matchResult = LottoPrize.values().map { MatchResult(it.matchingCount, it.includeBonus) }
+        val rank = LottoRank(matchResult, Amount(1000))
 
         val winningCount = rank.getWinningCount()
 
@@ -17,24 +17,24 @@ class LottoRankTest : ExpectSpec({
     }
 
     context("당첨금이 5만원인 경우 수익률 계산") {
-        val matchingCount = listOf(LottoPrize.FOURTH.matchingCount)
+        val matchResult = listOf(MatchResult(LottoPrize.FOURTH.matchingCount, false))
 
         expect("구매 비용이 5천원인 경우 수익률은 10.0 이다.") {
-            val rank = LottoRank(matchingCount, Amount(5_000))
+            val rank = LottoRank(matchResult, Amount(5_000))
             val profitRate = rank.getProfitRate()
 
             profitRate.value shouldBe 10.0
         }
 
         expect("구매 비용이 5만원인 경우 수익률은 1.0 이다.") {
-            val rank = LottoRank(matchingCount, Amount(50_000))
+            val rank = LottoRank(matchResult, Amount(50_000))
             val profitRate = rank.getProfitRate()
 
             profitRate.value shouldBe 1.0
         }
 
         expect("구매 비용이 50만원인 경우 수익률은 0.1 이다.") {
-            val rank = LottoRank(matchingCount, Amount(500_000))
+            val rank = LottoRank(matchResult, Amount(500_000))
             val profitRate = rank.getProfitRate()
 
             profitRate.value shouldBe 0.1
