6d9166bad3ac098b6b5e0d1286b28acdf2248f3e
hyotaek-jang <hyotaek.jang@linecorp.com>
2023-11-05 23:50:26 +0900

    refactor: use string template

--- src/main/kotlin/string/addition/calculator/view/OutputView.kt
+++ src/main/kotlin/string/addition/calculator/view/OutputView.kt
@@ -1,9 +1,7 @@
 package string.addition.calculator.view
 
 object OutputView {
-    private const val RESULT_MESSAGE = "계산 결과: "
-
     fun printResult(result: Int) {
-        println(RESULT_MESSAGE + result)
+        println("계산 결과 : $result")
     }
 }
