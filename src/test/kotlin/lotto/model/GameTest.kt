c8345984298c5a95b5bead1a8545d0c9eca94568
dong <dong.22@kakaoenterprise.com>
2023-11-05 09:43:03 +0900

    test : game 의 원소가 6 개 이어야만 하는 단위테스트 코드를 작성

--- src/test/kotlin/lotto/model/GameTest.kt
+++ src/test/kotlin/lotto/model/GameTest.kt
@@ -3,15 +3,22 @@ package lotto.model
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import lotto.LottoFixture
 
 class GameTest : StringSpec({
     "6개의 숫자만을 가져야한다" {
         shouldNotThrow<IllegalArgumentException> {
+            LottoFixture.gameOf(11, 22, 33, 4, 5, 6)
         }
     }
 
-    "6개보다 부족하거나, 초과하면 throw" {
+    "6개보다 부족하거나, 초과하면 IllegalArgumentException throw" {
         shouldThrow<IllegalArgumentException> {
+            LottoFixture.gameOf(11, 22, 33, 4, 5)
+        }
+
+        shouldThrow<IllegalArgumentException> {
+            LottoFixture.gameOf(11, 22, 33, 4, 5, 6, 7)
         }
     }
 })
