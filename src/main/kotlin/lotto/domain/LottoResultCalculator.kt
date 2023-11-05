11db2bf32773e80b054d86bc3a6f03e7f8521d56
yeongun <gjduddns@gmail.com>
2023-11-06 06:48:12 +0900

    refactor: 총 수익률을 계산할 때 코틀린 확장함수를 사용하도록 수정

--- src/main/kotlin/lotto/domain/LottoResultCalculator.kt
+++ src/main/kotlin/lotto/domain/LottoResultCalculator.kt
@@ -16,12 +16,7 @@ class LottoResultCalculator(private val winningNumbers: List<Int>) {
     }
 
     fun calculateEarningRate(result: Map<Rank, Int>, amount: Int): Double {
-        var earning = 0.0
-        earning += result.getOrDefault(Rank.FIFTH, 0) * Rank.FIFTH.winningMoney
-        earning += result.getOrDefault(Rank.FOURTH, 0) * Rank.FOURTH.winningMoney
-        earning += result.getOrDefault(Rank.THREE, 0) * Rank.THREE.winningMoney
-        earning += result.getOrDefault(Rank.FIRST, 0) * Rank.FIRST.winningMoney
-        val rate = earning / amount
+        val rate = result.map { result.getOrDefault(it.key, 0) * it.key.winningMoney }.sumOf { it.toDouble() }.div(amount)
         return floor(rate * 100) / 100
     }
 }
