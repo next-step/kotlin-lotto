ec8a967fb2f96682c41bd8ca7e86f7f3edc5a100
moses.lee(이상후)/kakao <moses.lee@kakaocorp.com>
2023-11-05 16:30:57 +0900

    fix: declare magic number as static
    # step 1

--- src/main/kotlin/calculator/dto/PositiveNum.kt
+++ src/main/kotlin/calculator/dto/PositiveNum.kt
@@ -4,6 +4,10 @@ package calculator.dto
 value class PositiveNum(val value: String) {
 
     init {
-        require(value.toInt() >= 0) { "음수는 입력할 수 없습니다." }
+        require(value.toInt() >= POSITIVE_THRESHOLD) { "음수는 입력할 수 없습니다." }
+    }
+
+    companion object {
+        const val POSITIVE_THRESHOLD = 0
     }
 }
