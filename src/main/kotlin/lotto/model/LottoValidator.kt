a672542415872f5d96b2d7acc0702cb69a4285b3
stevy.sung <stevy.sung@kakaocorp.com>
2023-11-05 17:11:22 +0900

    feat: LottoValidator 구현

--- src/main/kotlin/lotto/model/LottoValidator.kt
+++ src/main/kotlin/lotto/model/LottoValidator.kt
@@ -1,4 +1,13 @@
 package lotto.model
 
 class LottoValidator {
+    companion object {
+        fun validate(tickets: List<Ticket>, winningNumbers: List<Int>, ticketLength: Int): List<Int> {
+            val result = MutableList(ticketLength + 1) { 0 }
+            tickets.forEach {
+                result[it.matchCount(winningNumbers)]++
+            }
+            return result.toList()
+        }
+    }
 }
