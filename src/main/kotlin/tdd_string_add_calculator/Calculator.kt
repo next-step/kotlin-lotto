31d1ba478bf8f3c7c20361194ec9593be3ccabb0
Ahn-SeokJoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2023-11-05 22:00:51 +0900

    Step1: 문자열 덧셈 계산기 입니다. (#838)
    * [refactoring] step1:  gradle version up
    
    * [feature] step1: 문자열 덧셈 기능 tdd로 추가
    
    * [feature] step1: 빈 문자열 0 반환 tdd로 추가
    
    * [feature] step1: 숫자 이외의 값은 RuntimeException tdd로 추가
    
    * [feature] step1: 음수의 값은 RuntimeException을 tdd로 구현
    
    * [refactoring] step1: 프로그래밍 요구사항 README.md에 추가
    
    * [feature] 커스텀 구분자를 사용해 계산을 tdd로 구현
    
    * [refactoring] step1: 띄어쓰기 추가
    
    * [feature] step1: test case 추가 하나의 숫자 입력
    
    * [feature] step1: null 케이스 추가
    
    * [refactoring] step1: 문자열 수정
    
    * [refactoring] step1: 함수 위치 수정
    
    * [refactoring] step1: 리뷰 반영 메소드 네이밍 수정
    
    * [refactoring] step1: withData, forAll 사용
    
    * [refactoring] step1: 변수명 수정
    
    * [refactoring] 리뷰 반영 error 수정 및 테스트 수정
    
    * [refactoring] step1: 네이밍 리펙토링

---
+++ src/main/kotlin/tdd_string_add_calculator/Calculator.kt
@@ -0,0 +1,70 @@
+package tdd_string_add_calculator
+
+object Calculator {
+    fun calculate(originalExpress: String?): String {
+        val express = originalExpress ?: DEFAULT_VALUE
+        val customDelimiter = getCustomDelimiterFromExpress(express)
+        val regexWithCustomDelimiter = getRegexWithCustomDelimiter(customDelimiter)
+        val customDelimiterString = getCustomDelimiterString(
+            express = express,
+            customDelimiter = customDelimiter
+        )
+        val finalExpressWithCustomDelimiter = getNewExpress(
+            express = express,
+            customDelimiter = customDelimiter,
+            customDelimiterString = customDelimiterString
+        )
+        val list = finalExpressWithCustomDelimiter.split(regexWithCustomDelimiter)
+        return calculate(list)
+    }
+
+    private fun getCustomDelimiterFromExpress(express: String): String =
+        if (express.startsWith(CUSTOM_DELIMITER_START_WITH)) {
+            express.getOrNull(CUSTOM_DELIMITER_INDEX).toString()
+        } else {
+            EMPTY_STRING
+        }
+
+    private fun getRegexWithCustomDelimiter(customDelimiter: String) = "([${SPLIT_REGEX + customDelimiter}])".toRegex()
+
+    /**
+     * CustomDelimiter 가 존재 안하면 기존 express,
+     * CustomDelimiter 가 존재 하면 전달 받은 customDelimiterString 을 잘라서 express 만 남김
+     */
+    private fun getNewExpress(
+        express: String,
+        customDelimiter: String,
+        customDelimiterString: String
+    ): String = if (customDelimiter.isBlank()) express else express.replace(customDelimiterString, EMPTY_STRING)
+
+    /**
+     * custom delimiter 만 찾기
+     */
+    private fun getCustomDelimiterString(express: String, customDelimiter: String): String =
+        if (customDelimiter.isNotBlank()) (express.substring(FIRST_SUBSTRING_RANGE..LAST_SUBSTRING_RANGE)) else express
+
+    private fun calculate(list: List<String>): String {
+        return runCatching {
+            list.map { string ->
+                val number = changeLettersToNumber(string)
+                validateNumberIsNegativeNumber(number)
+            }.reduce { acc, i -> acc + i }
+                .toString()
+        }.getOrElse { error(NOT_SUPPORTED_TEXT) }
+    }
+
+    private fun changeLettersToNumber(it: String): Int = it.ifBlank { DEFAULT_VALUE }.toInt()
+
+    private fun validateNumberIsNegativeNumber(number: Int) =
+        if (number < MINIMUM_NUMBER) throw RuntimeException() else number
+
+    private const val EMPTY_STRING = ""
+    private const val CUSTOM_DELIMITER_START_WITH = "/"
+    private const val CUSTOM_DELIMITER_INDEX = 2
+    private const val MINIMUM_NUMBER = 0
+    private const val FIRST_SUBSTRING_RANGE = 0
+    private const val LAST_SUBSTRING_RANGE = 3
+    private const val SPLIT_REGEX = ",:"
+    private const val DEFAULT_VALUE = "0"
+    private const val NOT_SUPPORTED_TEXT = "적절하지 않은 문자가 들어왔습니다."
+}
---
+++ src/test/kotlin/tdd_string_add_calculator/CalculatorTest.kt
@@ -0,0 +1,74 @@
+package tdd_string_add_calculator
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.blocking.forAll
+import io.kotest.data.row
+import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
+
+class CalculatorTest : FunSpec({
+    context("문자열 덧셈을 수행한다. withData 사용") {
+        val express = listOf("1,2,3", "1:2:3", "1:2,3")
+        val expected = "6"
+        withData(
+            express
+        ) { exp ->
+            Calculator.calculate(exp) shouldBe expected
+        }
+    }
+    context(" 문자열 덧셈을 수행한다. forAll을 사용") {
+        forAll(
+            row("1,2,3", "6"),
+            row("2:3:4", "9"),
+            row("1:2,4", "7"),
+        ) { express, expected ->
+            Calculator.calculate(express) shouldBe expected
+        }
+    }
+
+    context("빈 문자열은 0을 리턴한다") {
+        val express = ""
+        val expected = "0"
+        Calculator.calculate(express) shouldBe expected
+    }
+
+    context("숫자 이외의 값, 음수는 IllegalStateException") {
+        val express = listOf("a", "-1:2,3")
+        withData(express) { exp ->
+            shouldThrow<IllegalStateException> {
+                Calculator.calculate(exp)
+            }
+        }
+    }
+
+    context("커스텀 계산자를 사용해도 잘 작동한다") {
+        val express = "//;\n1;2;3"
+        val expected = "6"
+        Calculator.calculate(express) shouldBe expected
+    }
+
+    context("2개 값 더하기") {
+        val express = "1,2"
+        val expected = "3"
+        Calculator.calculate(express) shouldBe expected
+    }
+
+    context("커스텀 계산자로 2개 더하기") {
+        val express = "//;\n1;2"
+        val expected = "3"
+        Calculator.calculate(express) shouldBe expected
+    }
+
+    context("하나의 숫자 입력시 하나 반환") {
+        val express = "1"
+        val expected = "1"
+        Calculator.calculate(express) shouldBe expected
+    }
+
+    context("null 입력시에 0 반환") {
+        val express = null
+        val expected = "0"
+        Calculator.calculate(express) shouldBe expected
+    }
+})
