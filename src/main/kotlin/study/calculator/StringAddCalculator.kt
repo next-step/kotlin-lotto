1ab5124ef9cdfecb9bff8e5705f0946a4217bee4
1stpie <1stpie@naver.com>
2023-11-05 16:11:49 +0900

    refactor(core): refactoring logics
    change string find to regex
    divide parse and validate rule

--- src/main/kotlin/study/calculator/StringAddCalculator.kt
+++ src/main/kotlin/study/calculator/StringAddCalculator.kt
@@ -2,44 +2,58 @@ package study.calculator
 
 class StringAddCalculator {
     fun add(text: String?): Int {
-        if (text.isNullOrEmpty()) return 0
+        if (text.isNullOrEmpty()) return DEFAULT_NUMBER
 
-        val numbers = parseNumbers(text)
-        return calculateSum(numbers)
+        val intNumbers = parseNumbers(text)
+        validateNumbers(intNumbers)
+        return calculateSum(intNumbers)
     }
 
-    private fun parseNumbers(text: String): List<String> =
-        if (isCustomDelimiterFormat(text)) {
-            val customDelimiter = extractCustomDelimiter(text)
-            splitUsingCustomDelimiter(text, customDelimiter)
-        } else {
-            splitUsingDefaultDelimiters(text)
+    private fun parseNumbers(text: String): List<Int> {
+        val customDelimiter = extractCustomDelimiter(text)
+        val numbersText = removeCustomDelimiterPattern(text)
+        return splitUsingDelimiter(numbersText, customDelimiter).let(::convertToInt)
+    }
+
+    private fun convertToInt(numbers: List<String>): List<Int> {
+        return numbers.map { numStr ->
+            numStr.toIntOrNull() ?: throw RuntimeException("Invalid input: $numStr")
         }
-
-    private fun isCustomDelimiterFormat(text: String): Boolean = text.startsWith("//")
-
-    private fun extractCustomDelimiter(text: String): String = text.substring(
-        2 until text.indexOf("\n")
-    )
-
-    private fun splitUsingCustomDelimiter(text: String, delimiter: String): List<String> {
-        val numbersPart = text.substring(text.indexOf("\n") + 1)
-        return numbersPart.split(delimiter)
     }
 
-    private fun splitUsingDefaultDelimiters(text: String): List<String> = text.split(",", ":", "\n")
-
-    private fun calculateSum(numbers: List<String>): Int = numbers.sumOf { numStr ->
-        validateAndConvertToNumber(numStr)
+    private fun validateNumbers(numbers: List<Int>) {
+        numbers.forEach { number ->
+            if (number < DEFAULT_NUMBER) throw RuntimeException("Negatives not allowed: $number")
+        }
     }
 
-    private fun validateAndConvertToNumber(numStr: String): Int {
-        val number = numStr.toIntOrNull() ?: throw RuntimeException("Invalid input: $numStr")
-        validatePositive(number)
-        return number
+    private fun splitUsingDelimiter(text: String, delimiter: String? = null): List<String> {
+        val regexPattern = buildDelimiterPattern(delimiter)
+        return text.split(regexPattern)
     }
 
-    private fun validatePositive(number: Int) {
-        if (number < 0) throw RuntimeException("Negatives not allowed: $number")
+    private fun buildDelimiterPattern(delimiter: String?): Regex {
+        val customDelimiterPattern = delimiter?.let { Regex.escape(it) }?.let { "|$it" } ?: ""
+        return Regex("($DEFAULT_DELIMITERS$customDelimiterPattern)|$NEW_LINE")
+    }
+
+    private fun removeCustomDelimiterPattern(text: String): String {
+        return text.replace(CUSTOM_DELIMITER_REMOVE_PATTERN, "")
+    }
+
+    private fun extractCustomDelimiter(text: String): String? {
+        val matchResult = CUSTOM_DELIMITER_EXTRACT_PATTERN.find(text)
+        return matchResult?.groups?.get(1)?.value
+    }
+
+    private fun calculateSum(numbers: List<Int>): Int = numbers.sum()
+
+    private companion object {
+        const val DEFAULT_NUMBER = 0
+        const val NEW_LINE = "\\n"
+        const val DEFAULT_DELIMITERS = ",|:"
+        const val CUSTOM_DELIMITER_PREFIX = "//"
+        val CUSTOM_DELIMITER_REMOVE_PATTERN = Regex("^$CUSTOM_DELIMITER_PREFIX.*?$NEW_LINE")
+        val CUSTOM_DELIMITER_EXTRACT_PATTERN = Regex("^$CUSTOM_DELIMITER_PREFIX(.+?)$NEW_LINE")
     }
 }
