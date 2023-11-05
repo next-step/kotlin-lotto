d2b97087dbf5e891e2ce63e301537464bdbf7198
dean <sodp5@naver.com>
2023-11-05 23:21:06 +0900

    1000원 당 로또 한 장을 구매할 수 있다.

---
+++ src/main/kotlin/lotto/LottoStore.kt
@@ -0,0 +1,11 @@
+package lotto
+
+class LottoStore(private val lottoGenerator: LottoGenerator) {
+    fun buyLottos(money: Int): List<Lotto> {
+        val lottoCount = money / 1000
+
+        return List(lottoCount) {
+            lottoGenerator.publish()
+        }
+    }
+}
---
+++ src/test/kotlin/lotto/LottoStoreTest.kt
@@ -0,0 +1,27 @@
+package lotto
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class LottoStoreTest {
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "0, 0",
+            "999, 0",
+            "1000, 1",
+            "5000, 5",
+        ]
+    )
+    fun `로또는 1000원당 한 장을 살 수 있다`(money: Int, lottoCount: Int) {
+        val lottoGenerateStrategy = LottoGenerateStrategy { listOf() }
+        val lottoGenerator = LottoGenerator(lottoGenerateStrategy)
+        val lottoStore = LottoStore(lottoGenerator)
+
+        val lottos = lottoStore.buyLottos(money)
+
+        assertThat(lottos.size).isEqualTo(lottoCount)
+    }
+}
