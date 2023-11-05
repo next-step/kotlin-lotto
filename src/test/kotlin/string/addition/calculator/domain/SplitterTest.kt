727dcf34df6e65b3ac870843ab2ca7fac1a1d736
hyotaek-jang <hyotaek.jang@linecorp.com>
2023-11-05 23:50:16 +0900

    refactor: apply lint for test

--- src/test/kotlin/string/addition/calculator/domain/SplitterTest.kt
+++ src/test/kotlin/string/addition/calculator/domain/SplitterTest.kt
@@ -1,6 +1,5 @@
 package string.addition.calculator.domain
 
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
