a112ab67cf6e9b81a1f2d6e2a3ca7afcb7c49ba0
Ahn-seokjoo <tjrwn0716@naver.com>
2023-11-06 02:04:37 +0900

    [refactoring] step2: 로또 생성 테스트 추가

--- src/test/kotlin/lotto_auto/LottoTest.kt
+++ src/test/kotlin/lotto_auto/LottoTest.kt
@@ -5,10 +5,15 @@ import io.kotest.matchers.shouldBe
 import lotto_auto.lotto.Lotto
 
 class LottoTest : FunSpec({
+    val lotto = Lotto()
     test("자동으로 생성된 번호는 6개이다") {
-        val lotto = Lotto()
         val expected = 6
 
         lotto.number.count() shouldBe expected
+    }
+    test("생성된 번호는 겹치지 않는다") {
+        val lottoSet = lotto.number.toSet()
+
+        lotto.number.count() shouldBe lottoSet.count()
     }
 })
