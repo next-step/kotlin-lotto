995f93d0996906ecd9c1b06bce7f1c99174a9d4f
jihyun-kim13 <jihyun.kim13@linecorp.com>
2023-11-05 11:39:08 +0900

    refactor: extract const value

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,7 +1,7 @@
 package lotto.domain
 
 class Lotto {
-    private val numberRange: IntRange = (1..45)
+    private val numberRange: IntRange = (MIN_NUMBER..MAX_NUMBER)
     private val totalNumbers: List<Int> = numberRange.shuffled()
 
     val numbers: List<Int> = totalNumbers.take(NUMBER_COUNT)
@@ -16,5 +16,7 @@ class Lotto {
     companion object {
         const val PRICE = 1000L
         private const val NUMBER_COUNT = 6
+        private const val MIN_NUMBER = 1
+        private const val MAX_NUMBER = 45
     }
 }
