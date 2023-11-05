01472ae9ffa582705e51d8626d260b7fd59ffd52
wooddy.kim <wooddy.kim@kakaocorp.com>
2023-11-05 20:52:49 +0900

    refactor: 로또 번호 생성시 IntRange 재생성하지 않도록 개선

--- src/main/kotlin/lotto/domain/LottoIssuer.kt
+++ src/main/kotlin/lotto/domain/LottoIssuer.kt
@@ -10,11 +10,13 @@ object RandomIssueStrategy : IssueStrategy {
     private const val RANDOM_MAX_VALUE = LottoNumber.MAX_VALUE
     private const val ISSUE_SIZE = LottoNumbers.LOTTO_NUMBER_SIZE
 
+    private val RANDOM_RANGE = (RANDOM_MIN_VALUE..RANDOM_MAX_VALUE)
+
     override fun issue(): LottoNumbers {
         val uniqueNumbers = mutableSetOf<Int>()
 
         while (uniqueNumbers.size < ISSUE_SIZE) {
-            val randomValue = (RANDOM_MIN_VALUE..RANDOM_MAX_VALUE).random()
+            val randomValue = RANDOM_RANGE.random()
             uniqueNumbers.add(randomValue)
         }
         return LottoNumbers(uniqueNumbers.map { LottoNumber(it) }.toSet())
