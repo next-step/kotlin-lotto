217b9d1129b08a028b843e11145faf40796e8396
Ho-Ya-Ho <rkdgh1056@gmail.com>
2023-11-05 23:47:35 +0900

    test작성 - '문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다'

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -8,12 +8,19 @@ class StringAddCalculator {
         val split: List<String> = input.split(DELIMITER_REGEX)
 
         val result = Regex("//(.)\n(.*)").find(input)
-        result?.let {
+        val numbers = result?.let {
             val customDelimiter = it.groupValues[1]
-            val tokens = it.groupValues[2].split(customDelimiter)
-            return tokens.sumOf { number -> number.toInt() }
-        }
-        return split.sumOf { it.toInt() }
+            it.groupValues[2].split(customDelimiter)
+        } ?: split
+
+        return calculateWithNegativeCheck(numbers.map { it.toInt() })
+    }
+
+    private fun calculateWithNegativeCheck(numbers: List<Int>): Int {
+        val negativeNumbers = numbers.filter { it < 0 }
+        if (negativeNumbers.isNotEmpty()) throw RuntimeException("음수 입력되었습니다!!!!")
+
+        return numbers.sum()
     }
 
     companion object {
