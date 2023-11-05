f1bfb38d02cf6cf79523a24ae5883fbf93af86b8
Factoriall <skarlgus0910@gmail.com>
2023-11-06 01:24:42 +0900

    step2: create LottoNumber.kt

---
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -0,0 +1,9 @@
+package lotto
+
+data class LottoNumber(val number: Int) {
+    init {
+        require(number in (1..49)) {
+            "numbers should be between 1 to 49: now is $number"
+        }
+    }
+}
