f4ceb16e53c2967b6bfd85547449fcb557c8734c
JunHyun Lee <dlwnsgus777@naver.com>
2023-11-05 11:05:39 +0900

    [Step1]: 🚀 1단계 - 문자열 덧셈 계산기 (#841)
    * doc: 요구사항 분석
    
    * feat: 문자열 객체 생성 테스트
    
    * feat: 빈 문자열, null 입력시 기능 개발
    
    * feat: 숫자 하나 입력시 숫자 반환 개발
    
    * feat: 숫자 두개 컴마 입력시 기능 개발
    
    * etc: lint 맞추기
    
    * feat: 컴마와 클론을 구분자로 사용하는 기능 개발
    
    * feat: 커스텀 문자열 구분 기능 개발
    
    * feat: 음수 전달시 예외 발생
    
    * feat: 문자열 계산기 클래스 main으로 이동

---
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -0,0 +1,88 @@
+package calculator
+
+import calculate.StringCalculator
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
+
+class StringCalculatorTest {
+    private lateinit var calculator: StringCalculator
+
+    @BeforeEach
+    fun init() {
+        calculator = StringCalculator()
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    fun `빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다`(text: String?) {
+        calculator.inputText(text) shouldBe 0
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "2", "3"])
+    fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(text: String) {
+        calculator.inputText(text) shouldBe text.toInt()
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2", "2,3", "3,3"])
+    fun `숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다`(text: String) {
+        val expected = text.split(",").sumOf { it.toInt() }
+        calculator.inputText(text) shouldBe expected
+    }
+
+    @ParameterizedTest
+    @MethodSource("getSampleData")
+    fun `컴마 이외에 콜론을 구분자로 사용할 수 있다`(input: String, expected: Int) {
+        calculator.inputText(input) shouldBe expected
+    }
+
+    @ParameterizedTest
+    @MethodSource("getCustomDelimiterData")
+    fun `커스텀 구분자를 지정할 수 있다`(input: String, expected: Int) {
+        calculator.inputText(input) shouldBe expected
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["-1,2", "2,-3", "//;\n-1;2;3"])
+    fun `음수를 전달할 경우 예외가 발생한다`(input: String) {
+        shouldThrow<IllegalArgumentException> {
+            calculator.inputText(input)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun getSampleData(): List<Arguments> {
+            return listOf(
+                Arguments.of(
+                    "1,2:3", 6
+                ),
+                Arguments.of(
+                    "2:2:7", 11
+                ),
+                Arguments.of(
+                    "3:1,8", 12
+                )
+            )
+        }
+
+        @JvmStatic
+        fun getCustomDelimiterData(): List<Arguments> {
+            return listOf(
+                Arguments.of(
+                    "//;\n1;2;3", 6
+                ),
+                Arguments.of(
+                    "//|\n7|0|4", 11
+                ),
+            )
+        }
+    }
+}
