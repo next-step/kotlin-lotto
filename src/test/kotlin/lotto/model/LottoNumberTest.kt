1d880d5fbc535456a2099da2fb7b8e1a43e66857
dong <dong.22@kakaoenterprise.com>
2023-11-05 09:19:37 +0900

    test : 로또 숫자를 담는 클래스를 테스트한다

--- src/test/kotlin/lotto/model/LottoNumberTest.kt
+++ src/test/kotlin/lotto/model/LottoNumberTest.kt
@@ -7,12 +7,23 @@ import io.kotest.core.spec.style.StringSpec
 class LottoNumberTest : StringSpec({
     "1~46 사이의 수 이어야한다" {
         shouldNotThrow<IllegalArgumentException> {
-            TODO()
+            (1..46).forEach { LottoNumber(it) }
         }
     }
 
     "1~46 범위에 벗어나는 경우 IllegalArgumentException throw" {
-        shouldThrow<IllegalArgumentException> { }
-        TODO()
+        shouldThrow<IllegalArgumentException> {
+            LottoNumber(-1)
+        }
+        shouldThrow<IllegalArgumentException> {
+            LottoNumber(0)
+        }
+        shouldThrow<IllegalArgumentException> {
+            LottoNumber(47)
+        }
+        shouldThrow<IllegalArgumentException> {
+            LottoNumber(50)
+        }
+
     }
 })
