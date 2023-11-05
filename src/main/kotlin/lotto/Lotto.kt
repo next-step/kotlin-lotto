2b463893e1f9521abca02fa2a1e71a1bef1d4842
Factoriall <skarlgus0910@gmail.com>
2023-11-06 01:30:21 +0900

    step2: create of logic in LottoRank

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,12 +3,6 @@ package lotto
 data class Lotto(val strategy: CreateStrategy) {
     val lottoNumbers: LottoNumbers = strategy.createNumbers()
     fun getLottoResult(winningNumbers: WinningNumbers): LottoRank {
-        return when (lottoNumbers.getContainCount(winningNumbers)) {
-            3 -> LottoRank.FOURTH
-            4 -> LottoRank.THIRD
-            5 -> LottoRank.SECOND
-            6 -> LottoRank.FIRST
-            else -> LottoRank.NONE
-        }
+        return LottoRank.of(lottoNumbers.getContainCount(winningNumbers))
     }
 }
--- src/main/kotlin/lotto/LottoRank.kt
+++ src/main/kotlin/lotto/LottoRank.kt
@@ -5,5 +5,16 @@ enum class LottoRank(val containCount: Int, val money: Int) {
     SECOND(5, 1_500_000),
     THIRD(4, 50_000),
     FOURTH(3, 5_000),
-    NONE(0, 0)
+    NONE(0, 0);
+
+    companion object {
+        fun of(containCount: Int): LottoRank =
+            when (containCount) {
+                3 -> FOURTH
+                4 -> THIRD
+                5 -> SECOND
+                6 -> FIRST
+                else -> NONE
+            }
+    }
 }
