e14f50c4913ca0d00e31216d2a4590228596d8e5
Ho-Ya-Ho <rkdgh1056@gmail.com>
2023-11-06 00:22:57 +0900

    로직 분리

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -5,22 +5,25 @@ class StringAddCalculator {
     fun add(input: String?): Int {
         if (input.isNullOrEmpty()) return EMPTY_RESULT
 
-        val split: List<String> = input.split(DEFAULT_DELIMITER_REGEX)
-
-        val findRegex = Regex("//(.)\n(.*)").find(input)
-        val numbers = findRegex?.let {
-            val customDelimiter = it.groupValues[1]
-            it.groupValues[2].split(customDelimiter)
-        } ?: split
-
-        return checkNegativeNumbers(numbers.map { it.toInt() })
-    }
-
-    private fun checkNegativeNumbers(numbers: List<Int>): Int {
-        val negativeNumbers = numbers.filter { it < 0 }
-        if (negativeNumbers.isNotEmpty()) throw RuntimeException("음수 입력되었습니다!!!!")
+        val numbers = splitInputByDelimiter(input)
+        checkNegativeNumbers(numbers)
 
         return numbers.sum()
+    }
+
+    private fun splitInputByDelimiter(input: String): List<Int> {
+        val findRegex = Regex("//(.)\n(.*)").find(input)
+        val strings = findRegex?.let {
+            val customDelimiter = it.groupValues[1]
+            it.groupValues[2].split(customDelimiter)
+        } ?: input.split(DEFAULT_DELIMITER_REGEX)
+
+        return strings.map { it.toInt() }
+    }
+
+    private fun checkNegativeNumbers(numbers: List<Int>) {
+        val negativeNumbers = numbers.filter { it < 0 }
+        if (negativeNumbers.isNotEmpty()) throw RuntimeException("음수 입력되었습니다!!!!")
     }
 
     companion object {
