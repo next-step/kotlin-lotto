3887996d345f7d9827b86305062b9d646f34e63e
dokdogalmaegi <clik202012@gmail.com>
2023-11-05 19:46:00 +0900

    Refac: try-catch -> runCatching

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -31,28 +31,30 @@ class InputView(private val maxTryCount: Int = DEFAULT_MAX_TRY_COUNT) {
     }
 
     private fun readLine(question: String?, count: Int = 0): String {
-        if (question != null) {
-            if (count > 0) {
-                val leftCount: String = (maxTryCount - count).toString()
-                println("입력 횟수를 초과하였습니다.")
-            }
-
-            println(question)
-        }
-
-        return try {
-            readlnOrNull() ?: throw IllegalArgumentException("입력값이 없습니다.")
-        } catch (wrongInputException: IllegalArgumentException) {
-            println(wrongInputException.message)
-
-            if (count < this.maxTryCount) {
-                this.readLine(question, count + 1)
-            } else {
+        val inputResult: Result<String> = runCatching {
+            if (count >= this.maxTryCount) {
                 throw IllegalStateException("입력 횟수를 초과하였습니다.")
             }
-        } catch (overTryException: java.lang.IllegalStateException) {
-            throw java.lang.IllegalStateException(overTryException.message)
+
+            if (question != null) {
+                if (count > 0) {
+                    val leftCount: String = (maxTryCount - count).toString()
+                    println("잘못된 입력입니다. $leftCount 번 더 입력할 수 있습니다.")
+                }
+
+                println(question)
+            }
+
+            return readlnOrNull() ?: throw IllegalArgumentException("입력값이 없습니다.")
+        }.onFailure {
+            if (count < this.maxTryCount) {
+                return this.readLine(question, count + 1)
+            }
+
+            throw IllegalStateException("입력 횟수를 초과하였습니다.")
         }
+
+        return inputResult.getOrThrow()
     }
 
     companion object {
