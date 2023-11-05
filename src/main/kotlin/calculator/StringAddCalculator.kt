4cbdf14643fd1427b22c4692f8b561bea472e0a7
Ho-Ya-Ho <rkdgh1056@gmail.com>
2023-11-05 23:00:27 +0900

    test작성 - '구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.'

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -5,12 +5,13 @@ class StringAddCalculator {
     fun add(input: String?): Int {
         if (input.isNullOrEmpty()) return EMPTY_RESULT
 
-        val split: List<String> = input.split(",")
+        val split: List<String> = input.split(DELIMITER_REGEX)
 
         return split.sumOf { it.toInt() }
     }
 
     companion object {
         private const val EMPTY_RESULT = 0
+        private val DELIMITER_REGEX = "[,|:]".toRegex()
     }
 }
