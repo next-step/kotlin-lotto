612cfcbef12e1f6992ad19557686432d5b86191c
stevy.sung <stevy.sung@kakaocorp.com>
2023-11-05 16:55:24 +0900

    feat: LottoGenerator 구현

--- src/main/kotlin/lotto/model/LottoGenerator.kt
+++ src/main/kotlin/lotto/model/LottoGenerator.kt
@@ -1,4 +1,8 @@
 package lotto.model
 
 class LottoGenerator {
+
+    companion object {
+        fun generateTickets(count: Int, length: Int, range: IntRange) = List(count) { Ticket(length, range) }
+    }
 }
--- src/test/kotlin/lotto/model/LottoGeneratorTest.kt
+++ src/test/kotlin/lotto/model/LottoGeneratorTest.kt
@@ -1,16 +1,17 @@
 package lotto.model
 
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Assertions.*
 
-//class LottoGeneratorTest: BehaviorSpec({
-//    given("로또 생성기로 10개의 로또") {
-//        val ticketCounts = 10
-//        `when`("생성 했을 때") {
-//            val tickets = LottoGenerator.generate(ticketCounts)
-//            then("10개의 로또 티켓이 생성된다") {
-//                assertEquals(10, tickets.length)
-//            }
-//        }
-//    }
-//})
+class LottoGeneratorTest: BehaviorSpec({
+    Given("로또 생성기") {
+        When("1에서 45사이의 6개 숫자를 가지는 10개의 로또 생성 했을 때") {
+            val tickets = LottoGenerator.generateTickets(10, 6, 1..45)
+            Then("10개의 로또 티켓이 생성된다") {
+                tickets.size shouldBe 10
+            }
+        }
+    }
+})
