1c0d0c96e053fc9af0af4c724b7cb4234cc27a81
moses.lee(이상후)/kakao <moses.lee@kakaocorp.com>
2023-11-06 21:50:52 +0900

    feat: 로또 객체 생성
    # step 2

---
+++ src/main/kotlin/lotto/domain/Ticket.kt
@@ -0,0 +1,14 @@
+package lotto.domain
+
+class Ticket(private val _numbers: List<Int>) {
+    init {
+        require(_numbers.size == 6)
+        require(_numbers.all { it in 1..45 })
+        require(_numbers.distinct().size == 6)
+        require(_numbers == _numbers.sorted()) { "로또 번호는 오름차순으로 정렬되어야 합니다." }
+    }
+
+    fun countSameNumber(ticket: Ticket): Int {
+        return _numbers.intersect(ticket._numbers.toSet()).count()
+    }
+}
