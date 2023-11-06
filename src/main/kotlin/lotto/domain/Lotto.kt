633cae9c423a4d78e53a73f378782c4d96084143
yeongun <gjduddns@gmail.com>
2023-11-06 23:51:45 +0900

    refactor: 로또 생성 시 어떤 입력이 들어왔는지 에러 메시지에 같이 출력하도록 수정

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,7 +3,7 @@ package lotto.domain
 data class Lotto(val numbers: Set<LottoNumber>) {
 
     init {
-        require(numbers.size == LOTTO_COUNT) { "로또는 서로 다른 ${LOTTO_COUNT}개의 숫자로 구성되어야 합니다." }
+        require(numbers.size == LOTTO_COUNT) { "$numbers: 로또는 서로 다른 ${LOTTO_COUNT}개의 숫자로 구성되어야 합니다." }
     }
 
     constructor(vararg numbers: Int) : this(numbers.map { LottoNumber.from(it) }.toSet())
