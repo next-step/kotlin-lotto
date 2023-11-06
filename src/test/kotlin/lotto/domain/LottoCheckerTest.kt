dbe65da70e4b1383ffd81734e18a8bf65a9553e6
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-06 22:18:42 +0900

    feat : Lotto 번호가 여러 개 주어졌을 때 당첨 통계를 만들어낼 수 있습니다

--- src/test/kotlin/lotto/domain/LottoCheckerTest.kt
+++ src/test/kotlin/lotto/domain/LottoCheckerTest.kt
@@ -1,5 +1,6 @@
 package lotto.domain
 
+import io.kotest.matchers.maps.shouldContainKey
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -17,5 +18,17 @@ class LottoCheckerTest {
         val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
         val winNum = lottoChecker.getWinNum(lotto, listOf(1, 2, 3, 9, 10, 11))
         winNum shouldBe 3
+    }
+
+    @Test
+    fun `로또가 여러 개 존재할 때, 당첨번호가 주어졌을 때 통계를 리턴할 수 있습니다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val lotto1 = Lotto(listOf(1, 2, 3, 11, 12, 13))
+        val lotto2 = Lotto(listOf(1, 2, 14, 15, 16, 17))
+
+        val lottos = Lottos(listOf(lotto, lotto1, lotto2))
+
+        val winNumStatistics = lottoChecker.getWinNumStatistics(lottos, listOf(1, 2, 3, 4, 44, 45))
+        winNumStatistics.get(4) shouldBe 1
     }
 }
