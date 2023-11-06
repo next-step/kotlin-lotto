08c5f4f3ecdc5a481c11d6fd3a0ad6618adf0979
silvio.oivlis <silvio.oivlis@kakaocorp.com>
2023-11-06 20:02:56 +0900

    test: implemented lotto number valid test

--- src/test/kotlin/LottoAutoTest.kt
+++ src/test/kotlin/LottoAutoTest.kt
@@ -1,8 +1,30 @@
 import io.kotest.core.spec.style.StringSpec
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import java.lang.IllegalArgumentException
 
-class LottoAutoTest : StringSpec ({
+
+@JvmInline
+value class LottoNumber(val value: Int) {
+    init {
+        require(value in 1..45) {
+            throw IllegalArgumentException("number not in 1..45")
+        }
+    }
+}
+
+class LottoAutoTest : StringSpec({
     "each lotto ticket number not in 1..45 should throw IllegalArgumentException" {
+        assertThatThrownBy {
+            LottoNumber(0)
+        }.isInstanceOf(IllegalArgumentException::class.java)
 
+        assertThatThrownBy {
+            LottoNumber(50)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+
+        assertThatThrownBy {
+            LottoNumber(-4)
+        }.isInstanceOf(IllegalArgumentException::class.java)
     }
 
     "lotto ticket should having exact 6 numbers should throw IllegalArgumentException" {
