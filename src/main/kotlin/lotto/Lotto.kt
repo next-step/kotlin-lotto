ba5b412cf533499675886ae43ea6d46fce786fe7
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-06 18:58:51 +0900

    feat : 로또 1장의 가격은 1000원입니다

---
+++ src/main/kotlin/lotto/Lotto.kt
@@ -0,0 +1,5 @@
+package lotto
+
+class Lotto {
+    val price: Int = 1000
+}
---
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -0,0 +1,19 @@
+package lotto
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class LottoTest {
+    private lateinit var lotto: Lotto
+
+    @BeforeEach
+    fun setUp() {
+        lotto = Lotto()
+    }
+
+    @Test
+    fun `로또 1 장의 가격은 1000원이다`() {
+        lotto.price shouldBe 1000
+    }
+}
