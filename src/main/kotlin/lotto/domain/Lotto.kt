9bf0135abc11db8b056f13fe3ce0685ede47429e
seongmo-jeon <oeeen3@gmail.com>
2023-11-07 00:03:29 +0900

    feat: 로또 1장은 6개의 모두 다른 숫자로 구성된다.

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,3 +1,16 @@
 package lotto.domain
 
-class Lotto(val lottoNumbers: List<LottoNumber>)
+class Lotto(private val lottoNumbers: List<LottoNumber>) {
+    init {
+        require(lottoNumbers.size == LOTTO_NUMBER_COUNT) {
+            "로또 번호는 정확히 ${LOTTO_NUMBER_COUNT}개의 숫자로 이루어져야 합니다."
+        }
+        require (lottoNumbers.distinct().size == lottoNumbers.size) {
+            "로또 번호에 중복된 숫자가 있습니다."
+        }
+    }
+
+    companion object {
+        private const val LOTTO_NUMBER_COUNT = 6
+    }
+}
