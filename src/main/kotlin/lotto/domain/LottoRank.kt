7ce1353c36c3c3012f0d4651473ed0317f6ada9e
dokdogalmaegi <dokdogalmaegi@gmail.com>
2023-11-06 16:20:50 +0900

    Review changes: winningMoney _로 가독성 증진

--- src/main/kotlin/lotto/domain/LottoRank.kt
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -2,10 +2,10 @@
 package lotto.domain
 
 enum class LottoRank(val winningMoney: Int, val matchCount: Int) {
-    FIRST(2000000000, 6),
-    SECOND(1500000, 5),
-    THIRD(50000, 4),
-    FOURTH(5000, 3),
+    FIRST(2_000_000_000, 6),
+    SECOND(1_500_000, 5),
+    THIRD(50_000, 4),
+    FOURTH(5_000, 3),
     MISS(0, 0);
 
     companion object {
