255c92c015a6baabfd1cb05dec4410b3a7f9815e
parker1609 <psbum1609@gmail.com>
2023-11-06 22:02:47 +0900

    refactor: 상수화

--- src/test/kotlin/me/parker/nextstep/kotlinlotto/calculator/StringCalculator.kt
+++ src/test/kotlin/me/parker/nextstep/kotlinlotto/calculator/StringCalculator.kt
@@ -1,17 +1,23 @@
 package me.parker.nextstep.kotlinlotto.calculator
 
 object StringCalculator {
+
+    private const val DEFAULT_SEPARATOR_COMMA = ","
+    private const val DEFAULT_SEPARATOR_COLON = ":"
+
+    private const val CUSTOM_SEPARATOR_REGEX = "//(.)\n(.*)"
+
     fun add(input: String): Int {
-        val matchedResult = Regex("//(.)\n(.*)").find(input)
+        val matchedResult = Regex(CUSTOM_SEPARATOR_REGEX).find(input)
         val customSeparator = matchedResult?.groupValues?.get(1)
         val textAfterNewline = matchedResult?.groupValues?.get(2)
-        println("customSeparator = $customSeparator")
-        println("textAfterNewline = $textAfterNewline")
 
         if (!customSeparator.isNullOrEmpty() && !textAfterNewline.isNullOrEmpty()) {
-            return textAfterNewline.split(",", ":", customSeparator).sumOf { it.toInt() }
+            return textAfterNewline.split(DEFAULT_SEPARATOR_COMMA, DEFAULT_SEPARATOR_COLON, customSeparator)
+                .sumOf { it.toInt() }
         }
 
-        return input.split(",", ":").sumOf { it.toInt() }
+        return input.split(DEFAULT_SEPARATOR_COMMA, DEFAULT_SEPARATOR_COLON)
+            .sumOf { it.toInt() }
     }
 }
