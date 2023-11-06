902f8fa39e5d252129a95a05b22aa379555faead
dong <dong.22@kakaoenterprise.com>
2023-11-06 16:16:26 +0900

    feat : 로또 게임 한장한장을 어떻게 출력하는지에 대한 구현을 진행함

--- src/main/kotlin/lotto/model/Game.kt
+++ src/main/kotlin/lotto/model/Game.kt
@@ -5,6 +5,10 @@ data class Game(
 ) {
     constructor(lottoNumbers: Set<LottoNumber>) : this(LinkedHashSet(lottoNumbers))
 
+    override fun toString(): String {
+        return lottoNumbers.map { it.value }.joinToString(prefix = "[", postfix = "]", separator = ", ")
+    }
+
     companion object {
         fun autoTicket(): Game {
             return Game(LottoNumber.any6Numbers())
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -5,15 +5,10 @@ import lotto.model.LottoWinners
 import lotto.model.Round
 
 object OutputView {
-    fun presetRound(transaction: Round) {
-        "[8, 21, 23, 41, 42, 43]\n" +
-            "[3, 5, 11, 16, 32, 38]\n" +
-            "[7, 11, 16, 35, 36, 44]\n" +
-            "[1, 8, 11, 31, 41, 42]\n" +
-            "[13, 14, 16, 38, 42, 45]\n" +
-            "[7, 11, 30, 40, 42, 43]\n" +
-            "[2, 13, 22, 32, 38, 45] << 이렇게 출력해야함"
-        TODO("Not yet implemented")
+    fun presetRound(round: Round) {
+
+        round.games
+            .forEach { println(it) }
     }
 
     fun presentPrizes(s: LottoWinners) {
