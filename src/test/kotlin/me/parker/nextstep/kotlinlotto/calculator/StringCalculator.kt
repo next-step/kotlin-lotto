e1f05c65011a6bbd1e1b451b38f1a8e41a5e300d
parker1609 <psbum1609@gmail.com>
2023-11-06 21:59:53 +0900

    feat: 입력 문자열은 커스텀한 구분자를 입력할 수 있다.

--- src/test/kotlin/me/parker/nextstep/kotlinlotto/calculator/StringCalculator.kt
+++ src/test/kotlin/me/parker/nextstep/kotlinlotto/calculator/StringCalculator.kt
@@ -2,6 +2,16 @@ package me.parker.nextstep.kotlinlotto.calculator
 
 object StringCalculator {
     fun add(input: String): Int {
+        val matchedResult = Regex("//(.)\n(.*)").find(input)
+        val customSeparator = matchedResult?.groupValues?.get(1)
+        val textAfterNewline = matchedResult?.groupValues?.get(2)
+        println("customSeparator = $customSeparator")
+        println("textAfterNewline = $textAfterNewline")
+
+        if (!customSeparator.isNullOrEmpty() && !textAfterNewline.isNullOrEmpty()) {
+            return textAfterNewline.split(",", ":", customSeparator).sumOf { it.toInt() }
+        }
+
         return input.split(",", ":").sumOf { it.toInt() }
     }
 }
--- src/test/kotlin/me/parker/nextstep/kotlinlotto/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/me/parker/nextstep/kotlinlotto/calculator/StringCalculatorTest.kt
@@ -5,7 +5,7 @@ import io.kotest.matchers.shouldBe
 
 class StringCalculatorTest : BehaviorSpec({
 
-    given("문자열 계산기 입력") {
+    given("기본 구분자 문자열 계산기") {
         `when`("기본 구분자 ':'를 사용하는 '1:2'를 입력하면, ") {
             then("3을 반환한다.") {
                 StringCalculator.add("1:2") shouldBe 3
@@ -27,6 +27,26 @@ class StringCalculatorTest : BehaviorSpec({
         `when`("기본 구분자 두 개 모두를 사용하는 '1:2,3'를 입력하면, ") {
             then("6을 반환한다.") {
                 StringCalculator.add("1:2,3") shouldBe 6
+            }
+        }
+    }
+
+    given("커스텀 구분자 문자열 계산기") {
+        `when`("커스텀 구분자를 사용하는 '//;\n1;2;3'를 입력하면, ") {
+            then("6을 반환한다.") {
+                StringCalculator.add("//;\n1;2;3") shouldBe 6
+            }
+        }
+
+        `when`("커스텀 구분자를 사용하는 '//;\n1;2:3'를 입력하면, ") {
+            then("6을 반환한다.") {
+                StringCalculator.add("//;\n1;2:3") shouldBe 6
+            }
+        }
+
+        `when`("커스텀 구분자를 사용하는 '//;\n1,2;3'를 입력하면, ") {
+            then("6을 반환한다.") {
+                StringCalculator.add("//;\n1,2;3") shouldBe 6
             }
         }
     }
