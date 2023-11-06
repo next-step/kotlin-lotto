17ec67fe217e97556d58a9bb634dade89de9d3db
ALbertIM0427 <guang7226961@naver.com>
2023-11-06 22:20:03 +0900

    refactor: 필요 없는 로직 삭제

--- src/main/kotlin/lotto/business/LottoNumbersSorter.kt
+++
@@ -1,7 +0,0 @@
-package lotto.business
-
-class LottoNumbersSorter {
-    fun sort(lottoNumbers: List<LottoNumber>): List<LottoNumber> {
-        return lottoNumbers.sortedBy { it.number }
-    }
-}
