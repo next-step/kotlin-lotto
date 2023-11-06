ff0c2055637a6aea2cb1c7a6b290fb886d928708
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-06 19:11:24 +0900

    feat : 로또 번호는 서로 다른 숫자로 이루어져있습니다

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.BeforeEach
@@ -21,5 +22,14 @@ class LottoTest {
     @Test
     fun `로또는 6개의 번호를 가진다`() {
         lotto.numbers shouldHaveSize 6
+    }
+
+    @Test
+    fun `로또 번호는 서로 다른 숫자로 이루어져있다`() {
+        val numbers = lotto.numbers
+        val distinct = numbers.distinct()
+
+        numbers shouldHaveSize distinct.size
+        numbers shouldContainAll distinct
     }
 }
