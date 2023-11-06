2e41b208ea4d6a88ba26446da77aa06f3eff2124
dong <dong.22@kakaoenterprise.com>
2023-11-06 15:03:04 +0900

    test : 불완전한 테스트 디스크립션 수
    주어를 확실하게 기술하였다

--- src/test/kotlin/lotto/model/LottoNumberTest.kt
+++ src/test/kotlin/lotto/model/LottoNumberTest.kt
@@ -5,13 +5,13 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 
 class LottoNumberTest : StringSpec({
-    "1~46 사이의 수 이어야한다" {
+    "LottoNumber 는 1~46 사이의 수 이어야한다" {
         shouldNotThrow<IllegalArgumentException> {
             (1..46).forEach { LottoNumber(it) }
         }
     }
 
-    "1~46 범위에 벗어나는 경우 IllegalArgumentException throw" {
+    "LottoNumber 는 1~46 범위에 벗어나는 경우 IllegalArgumentException throw" {
         shouldThrow<IllegalArgumentException> {
             LottoNumber(-1)
         }
