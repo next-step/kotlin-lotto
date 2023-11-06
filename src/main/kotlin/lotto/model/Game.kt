4790aaa3151c2ba94223b2b0a24ab9f59c4e3e96
dong <dong.22@kakaoenterprise.com>
2023-11-06 17:43:37 +0900

    feat : 입력된 데이터의 갯수를 검증하는 코드 추가

--- src/main/kotlin/lotto/model/Game.kt
+++ src/main/kotlin/lotto/model/Game.kt
@@ -3,6 +3,10 @@ package lotto.model
 data class Game(
     val lottoNumbers: LinkedHashSet<LottoNumber>,
 ) {
+    init {
+        require(lottoNumbers.size == 6) { "6개의 원소가 필요하지만 [${lottoNumbers.size}] 의 원소가 입력 되었습니다" }
+    }
+
     constructor(lottoNumbers: Set<LottoNumber>) : this(LinkedHashSet(lottoNumbers))
 
     override fun toString(): String {
