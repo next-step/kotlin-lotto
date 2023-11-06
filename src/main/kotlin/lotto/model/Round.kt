fc873fa87681702b02306e7bc243e2baaf2e62a8
dong <dong.22@kakaoenterprise.com>
2023-11-06 17:17:12 +0900

    feat : 당첨자를 집계하는 로직 구현

--- src/main/kotlin/lotto/model/Round.kt
+++ src/main/kotlin/lotto/model/Round.kt
@@ -4,6 +4,13 @@ data class Round(
     val games: ArrayDeque<Game>,
 ) {
     fun winnerAggregate(winningNumbers: WinningNumbers): LottoWinners {
-        TODO("Not yet implemented")
+        val map = games.map { winningNumbers.countOfMatch(it) }
+        return LottoWinners(
+            totalGameCount = this.games.size,
+            countOf1st = map.count { it == 6 },
+            countOf3rd = map.count { it == 5 },
+            countOf4th = map.count { it == 4 },
+            countOf5th = map.count { it == 3 },
+        )
     }
 }
