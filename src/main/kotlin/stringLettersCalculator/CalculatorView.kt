d647def57f0d09523e546f3d918bbeca3236d8d2
LAPTOP-A8710DGI\kdh85 <modudamba@naver.com>
2023-11-06 16:17:46 +0900

    step1: 문자열 덧셈 계산기 lint 코드컨밴션 적용 * 일부 코드 lint 미적용건 반영

--- src/main/kotlin/stringLettersCalculator/CalculatorView.kt
+++ src/main/kotlin/stringLettersCalculator/CalculatorView.kt
@@ -5,13 +5,13 @@ object CalculatorView {
     fun inputLetters(message: String): String {
         try {
             println(message)
-            return readln().replace("\\n","\n")
+            return readln().replace("\\n", "\n")
         } catch (e: Exception) {
             throw IllegalArgumentException("문자열수식을 입력해 주세요.")
         }
     }
 
-    fun printResult(result : Int) {
+    fun printResult(result: Int) {
         println("계산 결과 : $result")
     }
 }
