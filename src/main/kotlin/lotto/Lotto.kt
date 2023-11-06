5bfba0ab657db11c3547ce0941b02ce5123c1040
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-06 19:07:12 +0900

    feat : 로또는 6개의 번호를 가집니다

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,4 +2,7 @@ package lotto
 
 class Lotto {
     val price: Int = 1000
+    val numbers: List<Int> = (1..45).toList()
+        .shuffled()
+        .subList(0, 6)
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -15,5 +16,10 @@ class LottoTest {
     @Test
     fun `로또 1 장의 가격은 1000원이다`() {
         lotto.price shouldBe 1000
+    }
+
+    @Test
+    fun `로또는 6개의 번호를 가진다`() {
+        lotto.numbers shouldHaveSize 6
     }
 }
