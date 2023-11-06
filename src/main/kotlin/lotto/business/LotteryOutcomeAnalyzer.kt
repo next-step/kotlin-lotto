f874e27eff4360211618de8080408fc6cc3f8d9c
ALbertIM0427 <guang7226961@naver.com>
2023-11-06 21:57:26 +0900

    refactor: 필요 없는 메서드 삭제

--- src/main/kotlin/lotto/business/LotteryOutcomeAnalyzer.kt
+++
@@ -1,8 +0,0 @@
-package lotto.business
-
-class LotteryOutcomeAnalyzer {
-    fun analyze(lottoWinningNumbers: LottoWinningNumbers, lottoTicket: LottoTicket): LotteryPrize {
-        val matchCount = lottoTicket.matchCount(lottoWinningNumbers.lottoNumbers)
-        return LotteryPrize.getPrize(matchCount)
-    }
-}
