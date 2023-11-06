10b3bfbc3992349f17ec0bdc74ee7bb091f13306
dong <dong.22@kakaoenterprise.com>
2023-11-06 15:01:27 +0900

    test : 불완전한 테스트 디스크립션 수정

--- src/test/kotlin/lotto/model/RoundTest.kt
+++ src/test/kotlin/lotto/model/RoundTest.kt
@@ -5,7 +5,7 @@ import io.kotest.matchers.shouldBe
 import lotto.LottoFixture
 
 class RoundTest : StringSpec({
-    "1등 당첨번호를 입력하면 1등 당첨자를 집계할수 있어야한다" {
+    "6개의 로또 번호가 일치 하면 1등 당첨자로 집계 해야 한다" {
         val actual = LottoFixture.Round.winnerAggregate(LottoFixture.matched6)
         actual.countOf1st shouldBe 1
         actual.countOf3rd shouldBe 0
@@ -13,7 +13,7 @@ class RoundTest : StringSpec({
         actual.countOf5th shouldBe 0
     }
 
-    "3등 당첨번호를 입력하면 1등 당첨자를 집계할수 있어야한다" {
+    "5개의 로또 번호가 일치 하면 3등 당첨자로 집계 해야 한다" {
         val actual = LottoFixture.Round.winnerAggregate(LottoFixture.matched5)
         actual.countOf1st shouldBe 0
         actual.countOf3rd shouldBe 1
@@ -21,7 +21,7 @@ class RoundTest : StringSpec({
         actual.countOf5th shouldBe 0
     }
 
-    "4등 당첨번호를 입력하면 1등 당첨자를 집계할수 있어야한다" {
+    "4개의 로또 번호가 일치 하면 4등 당첨자로 집계 해야 한다" {
         val actual = LottoFixture.Round.winnerAggregate(LottoFixture.matched4)
         actual.countOf1st shouldBe 0
         actual.countOf3rd shouldBe 0
@@ -29,7 +29,7 @@ class RoundTest : StringSpec({
         actual.countOf5th shouldBe 0
     }
 
-    "5등 당첨번호를 입력하면 1등 당첨자를 집계할수 있어야한다" {
+    "3개의 로또 번호가 일치 하면 5등 당첨자로 집계 해야 한다" {
         val actual = LottoFixture.Round.winnerAggregate(LottoFixture.matched3)
         actual.countOf1st shouldBe 0
         actual.countOf3rd shouldBe 0
