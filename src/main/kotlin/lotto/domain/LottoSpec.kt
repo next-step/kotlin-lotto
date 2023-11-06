3e22b04c66bce285c11d3e09dd8249e0b54b186f
bongbong <kbk5190@naver.com>
2023-11-07 00:24:06 +0900

    chore: 숫자 표기시 _ 사용

--- src/main/kotlin/lotto/domain/LottoSpec.kt
+++ src/main/kotlin/lotto/domain/LottoSpec.kt
@@ -5,10 +5,10 @@ object LottoSpec {
     val NUMBERS_RANGE = 1..45
     val PRICE = Amount(1000)
     val prizesInfo : List<WinningPrize> = listOf(
-        WinningPrize(3, Amount(5000)),
-        WinningPrize(4, Amount(50000)),
-        WinningPrize(5, Amount(1500000)),
-        WinningPrize(6, Amount(200000000)),
+        WinningPrize(3, Amount(5_000)),
+        WinningPrize(4, Amount(5_0000)),
+        WinningPrize(5, Amount(1_500_000)),
+        WinningPrize(6, Amount(200_000_000)),
     )
 
     fun getMinCountToGetPrize() = prizesInfo[0].matchedCount
