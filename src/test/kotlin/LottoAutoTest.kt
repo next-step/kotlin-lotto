759f8576d27e9cdce200bb0976755502fa3479f3
silvio.oivlis <silvio.oivlis@kakaocorp.com>
2023-11-06 19:50:49 +0900

    test: generated test functions for step2

---
+++ src/test/kotlin/LottoAutoTest.kt
@@ -0,0 +1,35 @@
+import io.kotest.core.spec.style.StringSpec
+
+class LottoAutoTest : StringSpec ({
+    "each lotto ticket number not in 1..45 should throw IllegalArgumentException" {
+
+    }
+
+    "lotto ticket should having exact 6 numbers should throw IllegalArgumentException" {
+
+    }
+
+    "lotto ticket is 1,000 KRW/EA" {
+
+    }
+
+    "lotto ticket with same numbers should throw RuntimeException" {
+
+    }
+
+    "should buy as many lotto tickets as possible with budget" {
+
+    }
+
+    "winning numbers with same number should throw IllegalArgumentException" {
+
+    }
+
+    "winning statistics should show correct win state" {
+
+    }
+
+    "winning statistics should show correct ROI" {
+
+    }
+})
