c920799cee0699a481c045ee966ec32da7020cf3
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-06 20:54:45 +0900

    feat : Lottos 라는 일급 객체 컬렉션을 사용하여 lottoList 를 표현합니다

--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -1,12 +1,12 @@
 package lotto.domain
 
 class LottoMachine(val lottoPrice: Int) {
-    fun purchase(price: Int): List<Lotto> {
+    fun purchase(price: Int): Lottos {
         val lottoList = mutableListOf<Lotto>()
         val numTickets = price / lottoPrice
         for (i in 1..numTickets) {
             lottoList.add(Lotto())
         }
-        return lottoList
+        return Lottos(lottoList)
     }
 }
