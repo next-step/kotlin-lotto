fc66d4443d44e34dc023ee664882c696595502a0
silvio.oivlis <silvio.oivlis@kakaocorp.com>
2023-11-06 20:10:00 +0900

    test: implemented lotto numbers size test

--- src/test/kotlin/LottoAutoTest.kt
+++ src/test/kotlin/LottoAutoTest.kt
@@ -12,6 +12,15 @@ value class LottoNumber(val value: Int) {
     }
 }
 
+
+data class LottoNumbers(private val lottoNumberList: List<LottoNumber>) {
+    init {
+        require(lottoNumberList.size == 6) {
+            throw IllegalArgumentException("lotto numbers should have exact 6 numbers")
+        }
+    }
+}
+
 class LottoAutoTest : StringSpec({
     "each lotto ticket number not in 1..45 should throw IllegalArgumentException" {
         assertThatThrownBy {
@@ -28,7 +37,31 @@ class LottoAutoTest : StringSpec({
     }
 
     "lotto ticket should having exact 6 numbers should throw IllegalArgumentException" {
+        assertThatThrownBy {
+            LottoNumbers(
+                listOf(
+                    LottoNumber(1),
+                    LottoNumber(2),
+                    LottoNumber(3),
+                    LottoNumber(4),
+                    LottoNumber(5),
+                    LottoNumber(6),
+                    LottoNumber(7),
+                )
+            )
+        }.isInstanceOf(IllegalArgumentException::class.java)
 
+        assertThatThrownBy {
+            LottoNumbers(
+                listOf(
+                    LottoNumber(1),
+                    LottoNumber(2),
+                    LottoNumber(3),
+                    LottoNumber(4),
+                    LottoNumber(5),
+                )
+            )
+        }.isInstanceOf(IllegalArgumentException::class.java)
     }
 
     "lotto ticket is 1,000 KRW/EA" {
