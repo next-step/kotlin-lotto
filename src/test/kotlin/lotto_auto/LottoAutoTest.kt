3cf0228bd5cf123193d54c1d0646f5819d16f48e
Ahn-seokjoo <tjrwn0716@naver.com>
2023-11-07 03:17:08 +0900

    [refactorng] step3: 일급 컬렉션 사용에 대한 테스트 코드 및 코드 수정
    - 테스트 코드 수정
    - output view 수정

--- src/test/kotlin/lotto_auto/LottoAutoTest.kt
+++ src/test/kotlin/lotto_auto/LottoAutoTest.kt
@@ -4,13 +4,15 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import lotto_auto.lotto.Lotto
 import lotto_auto.lotto.LottoAuto
+import lotto_auto.lotto.LottoNumbers
 import lotto_auto.lotto.LottoPrize
 
 class LottoAutoTest : StringSpec({
     "구매한 로또 총 당첨 금액이 맞는지" {
         // 로또 당첨 금액 (1) - 0원   (2) - 50000원
-        val input = listOf(Lotto(listOf(1, 2, 3, 4, 5, 6)), Lotto(listOf(2, 4, 6, 8, 12, 15)))
-        val winningLotto = Lotto(listOf(3, 6, 9, 12, 15, 18))
+        val input =
+            listOf(Lotto(LottoNumbers(listOf(1, 2, 3, 4, 5, 6))), Lotto(LottoNumbers(listOf(2, 4, 6, 8, 12, 15))))
+        val winningLotto = Lotto(LottoNumbers(listOf(3, 6, 9, 12, 15, 18)))
         val bonusBallNumber = 33
         val expected = 5000
         val matchedList = LottoAuto.matchedLottoCountWithBonusBall(input, winningLotto, bonusBallNumber)
@@ -20,12 +22,12 @@ class LottoAutoTest : StringSpec({
 
     "matchCountList 함수가 3,4,5,6개 맞은 순서를 잘 나타내는지" {
         val input = listOf(
-            Lotto(listOf(1, 2, 3, 30, 31, 32)),
-            Lotto(listOf(1, 2, 3, 4, 30, 31)),
-            Lotto(listOf(1, 2, 3, 4, 5, 31)),
-            Lotto(listOf(1, 2, 3, 4, 5, 6))
+            Lotto(LottoNumbers(listOf(1, 2, 3, 30, 31, 32))),
+            Lotto(LottoNumbers(listOf(1, 2, 3, 4, 30, 31))),
+            Lotto(LottoNumbers(listOf(1, 2, 3, 4, 5, 31))),
+            Lotto(LottoNumbers(listOf(1, 2, 3, 4, 5, 6)))
         )
-        val winningLotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningLotto = Lotto(LottoNumbers(listOf(1, 2, 3, 4, 5, 6)))
         // 순서대로 3,4,5,6개 맞아서 리스트에 LottoPrize와 함께 1,1,1,1가 저장됨
         val expected = mapOf(
             LottoPrize.FIFTH_PRIZE to 1,
@@ -44,14 +46,14 @@ class LottoAutoTest : StringSpec({
 
     "구매 금액 대비 당첨 금액의 비율이 맞는지" {
         val input = listOf(
-            Lotto(listOf(1, 2, 3, 4, 5, 6)),
-            Lotto(listOf(7, 8, 9, 10, 11, 12)),
-            Lotto(listOf(13, 14, 15, 16, 17, 18)),
-            Lotto(listOf(19, 20, 21, 22, 23, 24)),
-            Lotto(listOf(25, 26, 27, 28, 29, 30))
+            Lotto(LottoNumbers(listOf(1, 2, 3, 4, 5, 6))),
+            Lotto(LottoNumbers(listOf(7, 8, 9, 10, 11, 12))),
+            Lotto(LottoNumbers(listOf(13, 14, 15, 16, 17, 18))),
+            Lotto(LottoNumbers(listOf(19, 20, 21, 22, 23, 24))),
+            Lotto(LottoNumbers(listOf(25, 26, 27, 28, 29, 30)))
         )
         // 5개 샀고, 1번 로또가 4개 당첨 되어 5 만원
-        val winningLotto = Lotto(listOf(1, 2, 3, 4, 31, 32))
+        val winningLotto = Lotto(LottoNumbers(listOf(1, 2, 3, 4, 31, 32)))
         val inputAmount = 5000
         val bonusBallNumber = 45
         val matchedList = LottoAuto.matchedLottoCountWithBonusBall(input, winningLotto, bonusBallNumber)
--- src/test/kotlin/lotto_auto/LottoTest.kt
+++ src/test/kotlin/lotto_auto/LottoTest.kt
@@ -4,36 +4,38 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import lotto_auto.lotto.Lotto
+import lotto_auto.lotto.LottoNumbers
 
 class LottoTest : FunSpec({
     val lotto = Lotto()
     test("자동으로 생성된 번호는 6개이다") {
         val expected = 6
 
-        lotto.numberList.count() shouldBe expected
+        lotto.numberList.numbers.count() shouldBe expected
     }
 
     test("생성된 번호는 겹치지 않는다") {
-        val lottoSet = lotto.numberList.toSet()
+        val lottoSet = lotto.numberList.numbers.toSet()
 
-        lotto.numberList.count() shouldBe lottoSet.count()
+        lotto.numberList.numbers.count() shouldBe lottoSet.count()
     }
 
     test("로또 생성시에 번호를 전달하면, 전달된 대로 생성한다") {
-        val winningLotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val lottoNumbers = LottoNumbers(listOf(1, 2, 3, 4, 5, 6))
+        val winningLotto = Lotto(lottoNumbers)
         val expected = listOf(1, 2, 3, 4, 5, 6)
-        winningLotto.numberList shouldBe expected
+        winningLotto.numberList.numbers shouldBe expected
     }
 
     test("로또 생성시에 중복 된 값을 넘겨주면 error를 맞는다") {
         shouldThrow<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 3, 4, 5))
+            LottoNumbers(listOf(1, 2, 3, 3, 4, 5))
         }
     }
 
     test("로또 생성시에 값을 6개가 아니게 넘겨주면 error를 맞는다") {
         shouldThrow<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
+            LottoNumbers(listOf(1, 2, 3, 4, 5, 6, 7))
         }
     }
 
@@ -51,20 +53,22 @@ class LottoTest : FunSpec({
             listOf(0, 1, 2, 3, 4, 5),
         ).forEach { lottoNumberList ->
             shouldThrow<IllegalArgumentException> {
-                Lotto(lottoNumberList)
+                LottoNumbers(lottoNumberList)
             }
         }
     }
 
     test("보너스 번호를 포함 하고 있지 않는 것을 확인") {
         val expected = 45
-        val bonusContainedLotto = Lotto(numberList = listOf(1, 2, 3, 4, 5, 6))
+        val lottoNumbers = LottoNumbers(listOf(1, 2, 3, 4, 5, 6))
+        val bonusContainedLotto = Lotto(numberList = lottoNumbers)
         bonusContainedLotto.isMatchedBonusBall(expected) shouldBe false
     }
 
     test("보너스 번호를 포함 하고 있는 것을 확인") {
         val expected = 45
-        val bonusContainedLotto = Lotto(numberList = listOf(1, 2, 3, 4, 5, 45))
+        val lottoNumbers = LottoNumbers(listOf(1, 2, 3, 4, 5, 45))
+        val bonusContainedLotto = Lotto(numberList = lottoNumbers)
         bonusContainedLotto.isMatchedBonusBall(expected) shouldBe true
     }
 })
