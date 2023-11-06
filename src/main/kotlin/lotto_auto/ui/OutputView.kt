16741b9795beb0853743ae969aaf16287f0e6184
Ahn-seokjoo <tjrwn0716@naver.com>
2023-11-06 23:57:38 +0900

    [refactoring] step2: 미사용값 제거

--- src/main/kotlin/lotto_auto/ui/OutputView.kt
+++ src/main/kotlin/lotto_auto/ui/OutputView.kt
@@ -21,14 +21,9 @@ object OutputView {
     }
 
     private const val LOTTO_STAT = "당첨 통계"
-    private const val DEFAULT_COUNT = 0
     private const val MATCH_THREE = "3개 일치 (5000원) - %d개"
     private const val MATCH_FOUR = "4개 일치 (50000원 ) - %d개"
     private const val MATCH_FIVE = "5개 일치 (1500000원) - %d개"
     private const val MATCH_SIX = "6개 일치 (2000000000원) - %d개"
-    private const val THIRD_INDEX = 0
-    private const val FOURTH_INDEX = 1
-    private const val FIFTH_INDEX = 2
-    private const val SIXTH_INDEX = 3
     private const val TOTAL_EARNING_RATE = "총 수익률은 %.2f 입니다."
 }
