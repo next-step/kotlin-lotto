d9e240a5c314052a892e5b97c9b9300a258f3066
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-06 19:38:12 +0900

    feat : 로또 발급기는 금액 입력시 구입 가능한 갯수의 로또를 발급합니다

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -1,4 +1,12 @@
 package lotto
 
 class LottoMachine(val lottoPrice: Int) {
+    fun purchase(price: Int): List<Lotto> {
+        val lottoList = mutableListOf<Lotto>()
+        val numTickets = price / lottoPrice
+        for (i in 1..numTickets) {
+            lottoList.add(Lotto())
+        }
+        return lottoList
+    }
 }
--- src/test/kotlin/lotto/LottoMachineTest.kt
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -1,8 +1,11 @@
 package lotto
 
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class LottoMachineTest {
     private lateinit var lottoMachine1000: LottoMachine
@@ -17,8 +20,9 @@ class LottoMachineTest {
         lottoMachine1000.lottoPrice shouldBe 1000
     }
 
-    @Test
-    fun `로또 발급기는 금액 입력시 구입 가능한 갯수의 로또를 발급한다`() {
-        TODO("Not yet implemented")
+    @ParameterizedTest
+    @CsvSource(value = ["14000, 14", "12000, 12", "1000, 1", "100, 0"])
+    fun `로또 발급기는 금액 입력시 구입 가능한 갯수의 로또를 발급한다`(money: Int, expected: Int) {
+        lottoMachine1000.purchase(money) shouldHaveSize  expected
     }
 }
