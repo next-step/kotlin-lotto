b63f9867022558371ffb8d3e59ba55300c067e7e
Ahn-seokjoo <tjrwn0716@naver.com>
2023-11-07 00:07:48 +0900

    [refactoring] step2: getLottoPrize 로직 수정

--- src/main/kotlin/lotto_auto/lotto/LottoPrize.kt
+++ src/main/kotlin/lotto_auto/lotto/LottoPrize.kt
@@ -1,21 +1,17 @@
 package lotto_auto.lotto
 
-enum class LottoPrize(val winningAmount: Int) {
-    NOTHING(0),
-    FOURTH_PRIZE(5_000),
-    THIRD_PRIZE(50_000),
-    SECOND_PRIZE(1_500_000),
-    FIRST_PRIZE(2_000_000_000);
+enum class LottoPrize(val matchCount: Int, val winningAmount: Int) {
+    MISS(0, 0),
+    FOURTH_PRIZE(3, 5_000),
+    THIRD_PRIZE(4, 50_000),
+    SECOND_PRIZE(5, 1_500_000),
+    FIRST_PRIZE(6, 2_000_000_000);
 
     companion object {
         fun getLottoPrize(matchCount: Int): LottoPrize {
-            return when (matchCount) {
-                3 -> FOURTH_PRIZE
-                4 -> THIRD_PRIZE
-                5 -> SECOND_PRIZE
-                6 -> FIRST_PRIZE
-                else -> NOTHING
-            }
+            return values().find { lottoPrize ->
+                lottoPrize.matchCount == matchCount
+            } ?: MISS
         }
     }
 }
