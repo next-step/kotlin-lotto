8b38330debe28850f7ffed0c89791de65d31de33
seongmo-jeon <oeeen3@gmail.com>
2023-11-06 23:59:02 +0900

    feat: 로또 숫자는 1부터 45까지의 숫자로 구성된다.

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -1,3 +1,9 @@
 package lotto.domain
 
-data class LottoNumber(val value: Int)
+data class LottoNumber(val number: Int) {
+    init {
+        require(number in 1..45) {
+            "로또 숫자는 1부터 45사이의 숫자여야 합니다."
+        }
+    }
+}
