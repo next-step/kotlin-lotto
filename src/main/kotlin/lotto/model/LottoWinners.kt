11fa12b285a784453039320cbcaec26dd7b96529
dong <dong.22@kakaoenterprise.com>
2023-11-06 14:54:37 +0900

    refactor : 당첨금액을 계산하기 위한 단위테스트 작성

--- src/main/kotlin/lotto/model/LottoWinners.kt
+++ src/main/kotlin/lotto/model/LottoWinners.kt
@@ -2,13 +2,21 @@ package lotto.model
 
 data class LottoWinners(
     val totalGameCount: Int,
-    val countOf5th: Int,
-    val countOf4th: Int,
-    val countOf3rd: Int,
-    val countOf1st: Int,
+    val countOf1st: Int = 0,
+    val countOf3rd: Int = 0,
+    val countOf4th: Int = 0,
+    val countOf5th: Int = 0,
 ) {
 
     fun earningRate(pricePerGame: Int): Double {
         TODO()
+    }
+
+    companion object {
+        private const val PRIZE_1ST = 2000000000;
+        private const val PRIZE_3RD = 1500000;
+        private const val PRIZE_4th = 50000;
+        private const val PRIZE_5th = 5000;
+        private const val PRIZE_UNIT = "원"
     }
 }
