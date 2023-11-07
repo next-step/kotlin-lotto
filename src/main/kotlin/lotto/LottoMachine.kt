96a71fa598f9faad3ee1271ae2f99135bda657a9
verus.j <verus.j@kakaocorp.com>
2023-11-07 19:22:01 +0900

    feature: 입력 금액 만큼 로또 생성

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -2,7 +2,8 @@ package lotto
 
 class LottoMachine {
 
-    fun create(): List<Any> {
-        return listOf(1)
+    fun create(money: Int = 1000): List<Any> {
+        val size = money / 1000
+        return (0 until size).map { it }
     }
 }
