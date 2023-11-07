8c540a71525aa3feb3be8fd1b901912b6fd597d2
10000001a <pgdn06288888@gmail.com>
2023-11-07 16:56:54 +0900

    test: Rank Enum에 대한 테스트 작성
    로또의 숫자는 이미 Lotto가 일급 컬렉션으로 관리되어 있었음

---
+++ src/test/kotlin/lottoTest/RankTest.kt
@@ -0,0 +1,36 @@
+package lottoTest
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class RankTest {
+    @ParameterizedTest
+    @MethodSource("generateRankArguments")
+    fun `일치하는 숫자의 수와 보너스 숫자 일치 여부로 Rank를 찾음`(countOfMatch: Int, matchBonus: Boolean, expectedRank: Rank) {
+        assertEquals(expectedRank, Rank.valueOf(countOfMatch, matchBonus))
+    }
+
+    companion object {
+        @JvmStatic
+        fun generateRankArguments(): List<Arguments> {
+            return listOf<Arguments>(
+                Arguments.of(6, true, Rank.FIRST),
+                Arguments.of(6, false, Rank.FIRST),
+                Arguments.of(5, true, Rank.SECOND),
+                Arguments.of(5, false, Rank.THIRD),
+                Arguments.of(4, true, Rank.FOURTH),
+                Arguments.of(4, false, Rank.FOURTH),
+                Arguments.of(3, true, Rank.FIFTH),
+                Arguments.of(3, false, Rank.FIFTH),
+                Arguments.of(2, true, Rank.MISS),
+                Arguments.of(2, false, Rank.MISS),
+                Arguments.of(1, true, Rank.MISS),
+                Arguments.of(1, false, Rank.MISS),
+                Arguments.of(0, true, Rank.MISS),
+                Arguments.of(0, false, Rank.MISS),
+            )
+        }
+    }
+}
