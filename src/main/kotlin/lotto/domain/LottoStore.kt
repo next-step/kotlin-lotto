ff949490e52e2334be4d97af01c4045f4ee76ef3
dean <sodp5@naver.com>
2023-11-08 03:00:29 +0900

    LOTTO_PRICE 상수로 변경

--- src/main/kotlin/lotto/domain/LottoStore.kt
+++ src/main/kotlin/lotto/domain/LottoStore.kt
@@ -1,8 +1,10 @@
 package lotto.domain
 
+private const val LOTTO_PRICE = 1000
+
 class LottoStore(private val lottoGenerator: LottoGenerator) {
     fun buyLottos(money: Int): List<Lotto> {
-        val lottoCount = money / 1000
+        val lottoCount = money / LOTTO_PRICE
 
         return List(lottoCount) {
             lottoGenerator.publish()
