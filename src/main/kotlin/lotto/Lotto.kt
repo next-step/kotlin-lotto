37cf8271377853a8ecb03fe5bb456e51a189fcbb
verus.j <verus.j@kakaocorp.com>
2023-11-07 21:22:11 +0900

    feature: 로또 숫자 중복 여부 확인

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,7 @@ package lotto
 class Lotto(private vararg val numbers: Int) {
 
     init {
-        require(numbers.size == 6) { "로또의 숫자 갯수는 6개만 가능합니다." }
+        require(numbers.distinct().size == 6) { "로또는 유일한 숫자 6개로 구성해야합니다." }
     }
 
     fun matchedCount(other: Lotto): Int {
