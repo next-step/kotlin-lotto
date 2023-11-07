a2cd64c3264a69ccd6db6466fcf552c8ff735a8e
Factoriall <skarlgus0910@gmail.com>
2023-11-07 23:55:27 +0900

    step3: create Lotto using Factory Method Pattern

---
+++ src/main/kotlin/lotto/domain/AutoLotto.kt
@@ -0,0 +1,8 @@
+package lotto.domain
+
+class AutoLotto : Lotto() {
+    override val lottoNumbers: LottoNumbers = run {
+        val list = (1..49).shuffled().take(6).sorted()
+        LottoNumbers(*list.toIntArray())
+    }
+}
---
+++ src/main/kotlin/lotto/domain/LottoCreator.kt
@@ -0,0 +1,5 @@
+package lotto.domain
+
+interface LottoCreator {
+    fun createLotto(): Lotto
+}
