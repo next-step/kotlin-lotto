d891307d5ebd08fd47c093e88f7a247a8bd40c5d
verus.j <verus.j@kakaocorp.com>
2023-11-07 21:36:50 +0900

    test: 3개 일치 시 수익률 계산 테스트 작성

---
+++ src/test/kotlin/lotto/StatisticTest.kt
@@ -0,0 +1,17 @@
+package lotto
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
+
+class StatisticTest {
+
+    @Test
+    fun `3개 일치 시 수익률을 계산한다`() {
+        val sut = Statistics(10000, 3 to 1)
+
+        val actual = sut.profit
+
+        assertThat(actual).isEqualTo(BigDecimal("0.5"))
+    }
+}
