95e090a6c3fd70e7813e9dadceaf937c810b3afc
verus.j <verus.j@kakaocorp.com>
2023-11-07 23:43:23 +0900

    refactor: 확장함수 제거

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -3,14 +3,14 @@ package lotto
 const val LOTTO_PRICE = 1000
 
 class LottoMachine(lottoGenerator: LottoGenerator, private val money: Int) {
-    val issuedLottos = (0 until money.issueLottoSize()).map { lottoGenerator.generate() }
+    val issuedLottos = (0 until issuedLottoSize(money)).map { lottoGenerator.generate() }
 
     fun issueStatistics(winningLotto: Lotto): Statistics {
         val statistics = issuedLottos.groupingBy { it.matchedCount(winningLotto) }.eachCount()
         return Statistics(money, statistics)
     }
-}
 
-private fun Int.issueLottoSize(): Int {
-    return this / LOTTO_PRICE
+    private fun issuedLottoSize(money: Int): Int {
+        return money / LOTTO_PRICE
+    }
 }
