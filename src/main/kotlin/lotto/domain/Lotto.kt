3cf397f7ffabaf0a553e0d5dc8d4e4eaad1cfb09
hyotaek-jang <hyotaek.jang@linecorp.com>
2023-11-08 01:09:51 +0900

    feat: add Lotto

---
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -0,0 +1,16 @@
+package lotto.domain
+
+class Lotto(val numbers: List<Int>) {
+    init {
+        require(numbers.size == LOTTO_NUMBER_SIZE) { "로또 번호는 ${LOTTO_NUMBER_SIZE}개만 가능합니다." }
+        require(numbers.toSet().size == LOTTO_NUMBER_SIZE) { "로또 번호는 중복될 수 없습니다." }
+        require(numbers.all { it in LOTTO_NUMBER_MIN..LOTTO_NUMBER_MAX }) { "로또 번호는 1~45 사이의 숫자만 가능합니다." }
+        numbers.sorted()
+    }
+    companion object {
+        const val LOTTO_PRICE = 1000
+        const val LOTTO_NUMBER_SIZE = 6
+        const val LOTTO_NUMBER_MIN = 1
+        const val LOTTO_NUMBER_MAX = 45
+    }
+}
---
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -0,0 +1,37 @@
+package lotto.domain
+
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+internal class LottoTest {
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2,3,4,5", "1,2,3,4,5,6,7", "1,2,3,4,5,6,7,8"])
+    fun `로또 번호가 6개가 아니면 오류가 발생한다`(input: String) {
+        // given
+        val numbers = input.split(",").map { it.toInt() }
+
+        // when & then
+        assertThrows<IllegalArgumentException> { Lotto(numbers) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2,3,4,5,5", "1,2,3,4,5,6,6", "1,2,3,4,5,6,7,7"])
+    fun `로또 번호에 중복이 있으면 오류가 발생한다`(input: String) {
+        // given
+        val numbers = input.split(",").map { it.toInt() }
+
+        // when & then
+        assertThrows<IllegalArgumentException> { Lotto(numbers) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2,3,4,5,0", "1,2,3,4,5,6,46", "1,2,3,4,5,6,7,100"])
+    fun `로또 번호가 1~45 사이의 숫자가 아니면 오류가 발생한다`(input: String) {
+        // given
+        val numbers = input.split(",").map { it.toInt() }
+
+        // when & then
+        assertThrows<IllegalArgumentException> { Lotto(numbers) }
+    }
+}
