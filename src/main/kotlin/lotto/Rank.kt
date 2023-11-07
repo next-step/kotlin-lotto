f4daa69325ec899aa33f49dd31de1c27ce7e9a93
10000001a <pgdn06288888@gmail.com>
2023-11-07 17:00:03 +0900

    test: RankTest가 컴파일 에러가 나지 않도록 수정

---
+++ src/main/kotlin/lotto/Rank.kt
@@ -0,0 +1,22 @@
+package lotto
+
+enum class Rank(val countOfMatch: Int, val winningMoney: Int) {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
+
+    companion object {
+        fun valueOf(countOfMatch: Int, matchBonus: Boolean): Rank {
+            return when (countOfMatch) {
+                6 -> Rank.FIRST
+                5 -> if (matchBonus) Rank.SECOND else Rank.THIRD
+                4 -> Rank.FOURTH
+                3 -> Rank.FIFTH
+                else -> Rank.MISS
+            }
+        }
+    }
+}
