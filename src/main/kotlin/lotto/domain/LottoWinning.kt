0de4f01a4adbfcbcfa2d8406de80c51ed6432d4d
dean <sodp5@naver.com>
2023-11-08 02:54:24 +0900

    정수에 구분자 추가

--- src/main/kotlin/lotto/domain/LottoWinning.kt
+++ src/main/kotlin/lotto/domain/LottoWinning.kt
@@ -2,10 +2,10 @@ package lotto.domain
 
 enum class LottoWinning(val correctCount: Int, val reward: Int) {
     Nothing(0, 0),
-    CorrectThree(3, 5000),
-    CorrectFour(4, 50000),
-    CorrectFive(5, 1500000),
-    CorrectSix(6, 2000000000);
+    CorrectThree(3, 5_000),
+    CorrectFour(4, 50_000),
+    CorrectFive(5, 1_500_000),
+    CorrectSix(6, 2_000_000_000);
 
     companion object {
         fun of(count: Int): LottoWinning {
