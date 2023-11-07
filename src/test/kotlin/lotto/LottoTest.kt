67f97e3a962d8168f9eeb657cd60b91fe4724f5c
verus.j <verus.j@kakaocorp.com>
2023-11-07 19:35:18 +0900

    test: LottoGenerator 주입 테스트 작성

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -8,11 +8,13 @@ class LottoTest {
 
     @ParameterizedTest
     @CsvSource(value = ["1000, 1", "13000, 13", "123441, 123"])
-    fun `입력 금액 만큼 로또 생성`(money: Int, expect: Int) {
-        val sut = LottoMachine()
+    fun `입력 금액 만큼 로또 생성`(money: Int, expectSize: Int) {
+        val expectedLotto = (0 until expectSize).map { _ -> Lotto() }.iterator()
+        val lottoGenerator = LottoGenerator { expectedLotto.next() }
+        val sut = LottoMachine(lottoGenerator)
 
         val actual = sut.create(money)
 
-        assertThat(actual).hasSize(expect)
+        assertThat(actual).hasSize(expectSize)
     }
 }
