8b319db62e5b94a77600e56ea883b7f501ce3740
Ho-Ya-Ho <rkdgh1056@gmail.com>
2023-11-07 21:23:30 +0900

    구매 입력 ~ 구매 개수 출력 기능 구현

--- src/test/kotlin/lotto/domain/LottoPurchaseManagerTest.kt
+++ src/test/kotlin/lotto/domain/LottoPurchaseManagerTest.kt
@@ -7,7 +7,6 @@ class LottoPurchaseManagerTest {
 
     @Test
     fun `입력한 금액에 따라 로또 개수가 출력 된다`() {
-
         val lottoPurchaseManager = LottoPurchaseManager()
         val lottoCount = lottoPurchaseManager.getLottoCount(14000)
         assertEquals(14, lottoCount)
