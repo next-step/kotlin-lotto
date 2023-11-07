2958ebe842b18888442d8fcbda643ca801b0c1a0
verus.j <verus.j@kakaocorp.com>
2023-11-07 21:48:08 +0900

    refactor: 수익 계산 메서드 추출

--- src/main/kotlin/lotto/Statistics.kt
+++ src/main/kotlin/lotto/Statistics.kt
@@ -3,9 +3,12 @@ package lotto
 import java.math.BigDecimal
 
 data class Statistics(private val money: Int = 1000, private val statistics: Map<Int, Int>) {
-
-    val profit: BigDecimal
-        get() = BigDecimal("5000").divide(BigDecimal(money))
+    val profitRate: BigDecimal
+        get() = totalProfit().divide(BigDecimal(money))
 
     constructor(money: Int, vararg pairs: Pair<Int, Int>): this(money, mapOf(*pairs))
+
+    private fun totalProfit(): BigDecimal {
+        return BigDecimal("5000")
+    }
 }
--- src/test/kotlin/lotto/StatisticTest.kt
+++ src/test/kotlin/lotto/StatisticTest.kt
@@ -10,7 +10,7 @@ class StatisticTest {
     fun `3개 일치 시 수익률을 계산한다`() {
         val sut = Statistics(10000, 3 to 1)
 
-        val actual = sut.profit
+        val actual = sut.profitRate
 
         assertThat(actual).isEqualByComparingTo(BigDecimal("0.5"))
     }
