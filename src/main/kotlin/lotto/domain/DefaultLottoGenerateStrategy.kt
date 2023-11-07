2015302ea2c05ac3b794ae6d7a7363b0d3e8680b
dean <sodp5@naver.com>
2023-11-08 02:53:32 +0900

    shuffle 활용해서 로또 번호 구하도록 변경

--- src/main/kotlin/lotto/domain/DefaultLottoGenerateStrategy.kt
+++ src/main/kotlin/lotto/domain/DefaultLottoGenerateStrategy.kt
@@ -5,15 +5,11 @@ private const val LOTTO_MAXIMUM_NUMBER = 45
 private const val LOTTO_NUMBER_COUNT = 6
 
 class DefaultLottoGenerateStrategy : LottoGenerateStrategy {
+    private val numberPool: IntRange = LOTTO_MINIMUM_NUMBER..LOTTO_MAXIMUM_NUMBER
     override fun generate(): List<Int> {
-        val lottoNumbers = HashSet<Int>()
-
-        while (lottoNumbers.size < LOTTO_NUMBER_COUNT) {
-            val random = (LOTTO_MINIMUM_NUMBER..LOTTO_MAXIMUM_NUMBER).random()
-
-            lottoNumbers.add(random)
-        }
-
-        return lottoNumbers.sorted()
+        return numberPool
+            .shuffled()
+            .subList(0, LOTTO_NUMBER_COUNT)
+            .sorted()
     }
 }
