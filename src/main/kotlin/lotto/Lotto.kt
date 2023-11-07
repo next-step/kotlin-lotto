8cd675066953df124cfa83ef30752aff0937978b
verus.j <verus.j@kakaocorp.com>
2023-11-07 21:11:17 +0900

    feature: 잘못된 갯수로 로또 생성 시 예외 발생

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,6 +2,10 @@ package lotto
 
 class Lotto(private vararg val numbers: Int) {
 
+    init {
+        require(numbers.size == 6) { "로또의 숫자 갯수는 6개만 가능합니다." }
+    }
+
     fun matchedCount(other: Lotto): Int {
         return numbers.filter { it in other.numbers }.size
     }
