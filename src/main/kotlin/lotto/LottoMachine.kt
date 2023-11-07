1662e874e2dca3800ded5ace4259e23ec6d7295b
verus.j <verus.j@kakaocorp.com>
2023-11-07 19:39:29 +0900

    feature: LottoGenerator를 이용한 로또 발행 구현

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -2,10 +2,10 @@ package lotto
 
 private const val LOTTO_PRICE = 1000
 
-class LottoMachine(lottoGenerator: LottoGenerator) {
+class LottoMachine(private val lottoGenerator: LottoGenerator) {
 
     fun create(money: Int): List<Any> {
         val size = money / LOTTO_PRICE
-        return (0 until size).map { it }
+        return (0 until size).map { lottoGenerator.generate() }
     }
 }
