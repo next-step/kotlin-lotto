d74c0ad6d48b793fa5ccb431cde25495adb7943f
david-sang <david.sang@kakaocorp.com>
2023-11-07 19:05:01 +0900

    feat(step1): 문자열 덧셈 계산기 기능 구현

---
+++ src/main/kotlin/global/util/ValidationUtil.kt
@@ -0,0 +1,15 @@
+fun <T : Any> T.requireAndPass(predicate: (T) -> Boolean, lazyMessage: () -> Any): T {
+    require(predicate(this)) { lazyMessage }
+    return this
+}
+
+fun String?.requireNotNullOrBlank(lazyMessage: () -> Any): String {
+    require(!isNullOrBlank()) { lazyMessage }
+    return this
+}
+
+fun String?.toIntOrThrow(lazyMessage: () -> Any): Int {
+    return requireNotNullOrBlank(lazyMessage)
+        .toIntOrNull()
+        ?: throw IllegalArgumentException(lazyMessage().toString())
+}
