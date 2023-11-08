0bf4cbdffd4ba51a02c8c2a37044cec6f48865e8
Factoriall <skarlgus0910@gmail.com>
2023-11-08 19:52:00 +0900

    step3: remove require and add try/catch for refactoring

--- src/main/kotlin/lotto/domain/LottoNumbersInput.kt
+++ src/main/kotlin/lotto/domain/LottoNumbersInput.kt
@@ -1,22 +1,16 @@
 package lotto.domain
 
-class LottoNumbersInput(private val text: String) {
+class LottoNumbersInput(text: String) {
     val numbers: LottoNumbers
 
     init {
-        val matchResult = Regex(SIX_NUMBERS_WITH_OPTIONAL_COMMA).find(text)
-        require(matchResult != null) {
-            "You need to enter six numbers separated by \",\" or \", \" as the delimiter.(ex - 1,2,3, 4, 5,6)"
+        val numbersList = text.split(',').map {
+            try {
+                it.trim().toInt()
+            } catch (e: Exception) {
+                throw IllegalArgumentException("You need to enter six numbers separated by comma as the delimiter.(ex - 1,2,3, 4, 5,6)")
+            }
         }
-
-        val matchedString = matchResult.value
-
-        val numbersList = matchedString.split(',').map { it.trim().toInt() }
         numbers = LottoNumbers(*numbersList.toIntArray())
-    }
-
-    companion object {
-        private const val COMMA_WITH_OPTIONAL_SPACE = ", ?"
-        private const val SIX_NUMBERS_WITH_OPTIONAL_COMMA = """\d+($COMMA_WITH_OPTIONAL_SPACE\d+){5}"""
     }
 }
