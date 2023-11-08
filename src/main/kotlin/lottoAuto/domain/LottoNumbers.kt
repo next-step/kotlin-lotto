ad829e13c2c46c16f1f8f83e58d8c5abd73ba740
jaylene shin <jaylene.shin@kakaocorp.com>
2023-11-08 20:48:36 +0900

    LottoNumbers 테스트 추가, 통과

--- src/main/kotlin/lottoAuto/domain/LottoNumbers.kt
+++ src/main/kotlin/lottoAuto/domain/LottoNumbers.kt
@@ -1,4 +1,11 @@
 package lottoAuto.domain
 
-class LottoNumbers {
+class LottoNumbers(private val numbers: List<LottoNumber>) : List<LottoNumber> by numbers {
+    init {
+        require(numbers.size == NUM_OF_LOTTO) { "로또 번호는 ${NUM_OF_LOTTO}개여야 합니다." }
+    }
+
+    companion object {
+        const val NUM_OF_LOTTO = 6
+    }
 }
--- src/test/kotlin/lottoAuto/domain/LottoNumbersTest.kt
+++ src/test/kotlin/lottoAuto/domain/LottoNumbersTest.kt
@@ -1,15 +1,19 @@
 package lottoAuto.domain
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class LottoNumbersTest {
     @Test
-    fun `생성된 로또 번호 개수를 검증한다`() {
-        TODO("Not yet implemented")
-    }
+    fun `6개 이상의 로또 번호가 들어올 경우 IllegalArgumentException을 발생시킨다`() {
+        // given
+        val numOfLottoNumber = 10
 
-    @Test
-    fun `N개 이상의 로또 번호가 들어올 경우 IllegalArgumentException을 발생시킨다`() {
-        TODO("Not yet implemented")
+        assertThrows<IllegalArgumentException> { // then
+            LottoNumbers( // when
+                List(numOfLottoNumber) {LottoNumber.of(0)}
+            )
+        }
+
     }
 }
