4e093419f69a380420a0cbb5dfb437320a6e4ae1
jaylene shin <jaylene.shin@kakaocorp.com>
2023-11-08 20:13:48 +0900

    LottoNumber 테스트 추가, 통과

--- src/main/kotlin/lottoAuto/domain/LottoNumber.kt
+++ src/main/kotlin/lottoAuto/domain/LottoNumber.kt
@@ -1,5 +1,14 @@
 package lottoAuto.domain
 
-//@JvmInline
-//value class LottoNumber {
-//}
+@JvmInline
+value class LottoNumber(private val number: Int) {
+    init {
+        require(number in 1..45) { "로또 번호는 1~45 범위 내의 숫자여야 합니다." }
+    }
+
+    companion object {
+        fun of(number: Int): LottoNumber {
+            return LottoNumber(number)
+        }
+    }
+}
--- src/test/kotlin/lottoAuto/domain/LottoNumberTest.kt
+++ src/test/kotlin/lottoAuto/domain/LottoNumberTest.kt
@@ -1,16 +1,17 @@
 package lottoAuto.domain
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class LottoNumberTest {
     @Test
-    fun `주어진 로또 번호가 1 ~ 45 범위 안에 존재하는지 확인한다`() {
-        TODO("Not yet implemented")
-    }
-
-    @Test
     fun `주어진 로또 번호가 1 미만이거나 45를 초과했을 경우 IllegalArgumentException을 발생시킨다`() {
-        TODO("Not yet implemented")
+        // given
+        val number = 0
+
+        assertThrows<IllegalArgumentException> { // then
+            LottoNumber.of(number) // when
+        }
     }
 }
 
