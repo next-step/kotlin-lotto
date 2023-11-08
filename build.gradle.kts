e2fa61fa65001cbaec486e0c95355fa65e4a2ef6
jiwon <ljwon77@daum.net>
2023-11-09 01:58:37 +0900

    build: lint에 trailing comma 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.8.10"
-    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
 }
 
 group = "camp.nextstep.edu"
