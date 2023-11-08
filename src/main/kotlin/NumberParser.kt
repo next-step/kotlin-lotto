7ef8540105457b89660ccc1bd21f6b22c6c1eab6
korjun1993 <corojoon93@naver.com>
2023-11-09 00:24:00 +0900

    feat: 문자열 파서에 빈 문자열 또는 null을 전달하는 경우 0을 반환한다

--- src/main/kotlin/NumberParser.kt
+++ src/main/kotlin/NumberParser.kt
@@ -1,6 +1,10 @@
 class NumberParser {
-    fun parseNumbers(text: String): List<Int> =
-        text.split(DELIMITER.joinToString(separator = REGULAR_EXPRESSION_OR).toRegex()).map { it.toInt() }
+    fun parseNumbers(text: String?): List<Int> =
+        if (text.isNullOrBlank()) {
+            listOf(0)
+        } else {
+            text.split(DELIMITER.joinToString(separator = REGULAR_EXPRESSION_OR).toRegex()).map { it.toInt() }
+        }
 
     companion object {
         private val DELIMITER = listOf(',', ':')
--- src/test/kotlin/NumberParserTest.kt
+++ src/test/kotlin/NumberParserTest.kt
@@ -6,6 +6,8 @@ import io.kotest.matchers.shouldBe
 class NumberParserTest : FunSpec({
     context("문자열 파서에 쉼표(,) 또는 콜론(:)을 구분자로 하는 숫자들을 전달하는 경우 구분자를 기준으로 숫자를 분리하여 반환한다") {
         withData(
+            row(null, listOf(0)),
+            row("", listOf(0)),
             row("1", listOf(1)),
             row("1,2", listOf(1, 2)),
             row("1:2", listOf(1, 2)),
