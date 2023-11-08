a9c19a6c27852d0bd7edbbafc9aad9e3a0c2eca2
julie.kw <julie.kw@kakaocorp.com>
2023-11-08 20:55:18 +0900

    step2 fix: price 파라미터 initPrice로 변경 & int maxvalue 에 대한 exception 추가

--- src/main/kotlin/autolotto/vo/AutoLotto.kt
+++ src/main/kotlin/autolotto/vo/AutoLotto.kt
@@ -2,13 +2,15 @@ package autolotto.vo
 
 import kotlin.math.floor
 
-class AutoLotto(val price: Long, private val originLottos: List<Lotto> = listOf()) {
+class AutoLotto(private val price: Long, originLottos: List<Lotto> = listOf()) {
     val count: Int = initCount(price)
     val lottos: List<Lotto> = originLottos.ifEmpty { createLottoList() }
 
-    private fun initCount(price: Long): Int {
-        require(price >= 0) { "가격은 음수가 될 수 없습니다." }
-        return (price / LOTTO_PRICE_PER_ONE).toInt()
+    private fun initCount(initPrice: Long): Int {
+        require(initPrice >= 0) { "가격은 음수가 될 수 없습니다." }
+        val count = initPrice / LOTTO_PRICE_PER_ONE
+        require(count > Int.MAX_VALUE) { "로또 구매 개수는 ${Int.MAX_VALUE}개를 넘을 수 없습니다." }
+        return count.toInt()
     }
 
     private fun createLottoList(): List<Lotto> {
