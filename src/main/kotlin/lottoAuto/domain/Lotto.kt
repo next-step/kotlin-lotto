659d1478fdcb5d4469e88c1020bd7d856268e487
jaylene shin <jaylene.shin@kakaocorp.com>
2023-11-08 21:23:31 +0900

    LottoTest 테스트, 기능 구현

--- src/main/kotlin/lottoAuto/domain/Lotto.kt
+++ src/main/kotlin/lottoAuto/domain/Lotto.kt
@@ -1,4 +1,9 @@
 package lottoAuto.domain
 
-//data class Lotto {
-//}
+data class Lotto (
+    val lottoNumbers: LottoNumbers
+) {
+    fun match(lotto: Lotto): Boolean {
+        return lottoNumbers.matchCount(lotto.lottoNumbers) == LottoNumbers.NUM_OF_LOTTO
+    }
+}
--- src/test/kotlin/lottoAuto/domain/LottoTest.kt
+++ src/test/kotlin/lottoAuto/domain/LottoTest.kt
@@ -1,21 +1,73 @@
 package lottoAuto.domain
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class LottoTest {
     @Test
-    fun `동일한 번호로 구성된 두 개의 로또가 서로 같은지 확인한다`() {
-        TODO("Not yet implemented")
+    fun `동일한 LottoNumber로 구성된 두 개의 로또가 서로 같은지 확인한다`() {
+        // given
+        val lottoNumbers1 = LottoNumbers(
+            listOf(
+                LottoNumber.of(1),
+                LottoNumber.of(2),
+                LottoNumber.of(3),
+                LottoNumber.of(4),
+                LottoNumber.of(5),
+                LottoNumber.of(6)
+            )
+        )
+        val lottoNumbers2 = LottoNumbers(
+            listOf(
+                LottoNumber.of(4),
+                LottoNumber.of(2),
+                LottoNumber.of(6),
+                LottoNumber.of(1),
+                LottoNumber.of(3),
+                LottoNumber.of(5),
+            )
+        )
+        val lotto1 = Lotto(lottoNumbers1)
+        val lotto2 = Lotto(lottoNumbers2)
+
+        // when
+        val isMatched = lotto1.match(lotto2)
+
+        // then
+        assertEquals(true, isMatched)
     }
 
     @Test
     fun `동일하지 않은 번호로 구성된 두 개의 로또가 서로 다른지 확인한다`() {
-        TODO("Not yet implemented")
-    }
+        // given
+        val lottoNumbers1 = LottoNumbers(
+            listOf(
+                LottoNumber.of(1),
+                LottoNumber.of(2),
+                LottoNumber.of(3),
+                LottoNumber.of(4),
+                LottoNumber.of(5),
+                LottoNumber.of(6)
+            )
+        )
+        val lottoNumbers2 = LottoNumbers(
+            listOf(
+                LottoNumber.of(10),
+                LottoNumber.of(20),
+                LottoNumber.of(30),
+                LottoNumber.of(40),
+                LottoNumber.of(45),
+                LottoNumber.of(45),
+            )
+        )
+        val lotto1 = Lotto(lottoNumbers1)
+        val lotto2 = Lotto(lottoNumbers2)
 
-    @Test
-    fun `두 개의 로또 길이가 다를 경우 IllegalArgumentException을 발생시킨다`() {
-        TODO("Not yet implemented")
+        // when
+        val isMatched = lotto1.match(lotto2)
+
+        // then
+        assertEquals(false, isMatched)
     }
 }
 
