c135ce8e63d5e015ca5aba97bfffccc2287d9e31
jaylene shin <jaylene.shin@kakaocorp.com>
2023-11-08 21:42:03 +0900

    정적 팩토리 메소드 추가

--- src/main/kotlin/lottoAuto/domain/LottoNumber.kt
+++ src/main/kotlin/lottoAuto/domain/LottoNumber.kt
@@ -3,12 +3,19 @@ package lottoAuto.domain
 @JvmInline
 value class LottoNumber(val number: Int) {
     init {
-        require(number in 1..45) { "로또 번호는 1~45 범위 내의 숫자여야 합니다." }
+        require(number in MIN_NUMBER..MAX_NUMBER) { "로또 번호는 1~45 범위 내의 숫자여야 합니다." }
     }
 
     companion object {
         fun of(number: Int): LottoNumber {
             return LottoNumber(number)
         }
+
+        fun ofRandom(): LottoNumber {
+            return LottoNumber((MIN_NUMBER..MAX_NUMBER).random())
+        }
+
+        const val MIN_NUMBER = 1
+        const val MAX_NUMBER = 45
     }
 }
