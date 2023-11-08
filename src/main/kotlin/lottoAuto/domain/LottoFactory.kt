60f8a3590bb18b985e00540db2494006d52b09a5
jaylene shin <jaylene.shin@kakaocorp.com>
2023-11-08 21:44:31 +0900

    LottoFactory 테스트, 기능 추가

--- src/main/kotlin/lottoAuto/domain/LottoFactory.kt
+++ src/main/kotlin/lottoAuto/domain/LottoFactory.kt
@@ -1,4 +1,14 @@
 package lottoAuto.domain
 
-class LottoFactory {
+object LottoFactory {
+    fun create(numOfLotto: Int): List<Lotto> {
+        val randomLottoNumbers = createRandomLottoNumbers()
+        return List(numOfLotto) { Lotto(randomLottoNumbers) }
+    }
+
+    private fun createRandomLottoNumbers(): LottoNumbers {
+        return LottoNumbers(
+            List(LottoNumbers.NUM_OF_LOTTO_NUMBERS) { LottoNumber.ofRandom() }
+        )
+    }
 }
