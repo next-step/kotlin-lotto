465d237d8cd77f635e8769f7cc18d013100da07a
korjun1993 <corojoon93@naver.com>
2023-11-09 23:47:14 +0900

    feat: step2 - 로또에 적힌 숫자 개수가 6개를 넘는다면 예외가 발생한다.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,3 +1,11 @@
 package lotto
 
-class Lotto(val numbers: List<Int>)
+class Lotto(val numbers: List<Int>) {
+    init {
+        require(numbers.size == LOTTO_NUMBER_SIZE) { "로또에 적힌 숫자는 6개입니다" }
+    }
+
+    companion object {
+        private const val LOTTO_NUMBER_SIZE = 6
+    }
+}
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
@@ -9,5 +10,9 @@ class LottoTest : FunSpec({
         val lotto = Lotto(listOf(10, 20, 10, 45, 15, 6))
         lotto.numbers.size shouldBe 6
         lotto.numbers.shouldBeInstanceOf<List<Int>>()
+    }
+
+    test("로또에 적힌 숫자 개수가 6개를 넘는다면 예외가 발생한다.") {
+        shouldThrow<IllegalArgumentException> { Lotto(listOf(10, 15, 20, 25, 30, 35, 40)) }
     }
 })
