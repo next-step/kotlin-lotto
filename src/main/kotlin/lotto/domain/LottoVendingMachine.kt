26daaf33b38a048458d6151f3ec58a7001af80de
yeongun <gjduddns@gmail.com>
2023-11-09 22:52:49 +0900

    refactor: 가짜 생성자를 이용하도록 수정

--- src/main/kotlin/lotto/domain/LottoVendingMachine.kt
+++ src/main/kotlin/lotto/domain/LottoVendingMachine.kt
@@ -5,6 +5,6 @@ private const val LOTTO_PRICE = 1000
 class LottoVendingMachine(val lottoGenerator: LottoGenerator) {
     fun generate(amount: Int): List<Lotto> {
         val count = amount / LOTTO_PRICE
-        return (1..count).map { lottoGenerator.generate() }
+        return List(count) { lottoGenerator.generate() }
     }
 }
