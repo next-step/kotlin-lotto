0208ea39c12935b810cb40710c56a67b84380298
wooddy.kim <wooddy.kim@kakaocorp.com>
2023-11-09 18:39:49 +0900

    feat: 수동 번호를 입력 받아 티켓을 발급할 수 있도록 기능 추가

--- src/main/kotlin/lotto/domain/LottoIssuer.kt
+++ src/main/kotlin/lotto/domain/LottoIssuer.kt
@@ -5,13 +5,18 @@ class LottoIssuer(
     private val issueStrategy: IssueStrategy
 ) {
 
-    fun issue(purchaseAmount: Amount): LottoTicket {
-        val issueCount = runCatching {
+    fun issue(purchaseAmount: Amount, manualLottoNumbers: List<LottoNumbers> = emptyList()): LottoTicket {
+        val totalIssueCount = runCatching {
             purchaseAmount.divide(ticketAmount)
         }.onFailure {
             throw IllegalArgumentException("로또 구입 금액은 로또 한 장의 가격의 배수여야 합니다.")
         }.getOrThrow()
 
-        return LottoTicket(purchaseAmount, List(issueCount) { issueStrategy.issue() })
+        val manualIssueCount = manualLottoNumbers.size
+        val autoIssueCount = totalIssueCount - manualIssueCount
+
+        require(autoIssueCount >= 0) { "수동으로 구매할 로또 수가 전체 구매 수를 초과합니다." }
+
+        return LottoTicket(purchaseAmount, List(autoIssueCount) { issueStrategy.issue() } + manualLottoNumbers)
     }
 }
--- src/test/kotlin/lotto/domain/LottoIssuerTest.kt
+++ src/test/kotlin/lotto/domain/LottoIssuerTest.kt
@@ -3,6 +3,7 @@ package lotto.domain
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import lotto.test.LottoNumberGenerator
 
 class LottoIssuerTest : BehaviorSpec({
 
@@ -52,6 +53,31 @@ class LottoIssuerTest : BehaviorSpec({
             then("로또 2장이 발급된다.") {
                 val numbers = lottoIssuer.issue(purchaseAmount)
                 numbers.lottos.size shouldBe 2
+            }
+        }
+    }
+
+    given("로또 가격이 1000원이고, 구입 금액 3000원이 주어졌을 때") {
+        val ticketAmount = Amount(1000)
+        val lottoIssuer = LottoIssuer(ticketAmount, FakeIssueStrategy)
+        val purchaseAmount = Amount(3000)
+
+        `when`("수동 로또 개수가 4개이면") {
+            val manualLottos = List(4) { LottoNumbers(LottoNumberGenerator.generate(1, 2, 3, 4, 5, 6)) }
+
+            then("예외가 발생한다.") {
+                shouldThrow<IllegalArgumentException> {
+                    lottoIssuer.issue(purchaseAmount, manualLottos)
+                }
+            }
+        }
+
+        `when`("수동 로또 개수가 3개이면") {
+            val manualLottos = List(3) { LottoNumbers(LottoNumberGenerator.generate(1, 2, 3, 4, 5, 6)) }
+
+            then("로또 티겟을 반환한다.") {
+                val lottoTicket = lottoIssuer.issue(purchaseAmount, manualLottos)
+                lottoTicket.lottos.size shouldBe 3
             }
         }
     }
