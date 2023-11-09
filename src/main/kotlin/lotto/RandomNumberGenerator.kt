650d42b89e932221e67bcc13e8c537e2a8d02662
korjun1993 <corojoon93@naver.com>
2023-11-10 02:20:52 +0900

    feat: step2 - 랜덤 숫자 생성기의 전달될 숫자A가 숫자B보다 크면 예외가 발생한다.

--- src/main/kotlin/lotto/RandomNumberGenerator.kt
+++ src/main/kotlin/lotto/RandomNumberGenerator.kt
@@ -1,6 +1,8 @@
 package lotto
 
 class RandomNumberGenerator : NumberGenerator {
-    override fun generateNumber(startNumber: Int, endNumber: Int, count: Int): Set<Int> =
-        (startNumber..endNumber).shuffled().subList(0, count).toSet()
+    override fun generateNumber(startNumber: Int, endNumber: Int, count: Int): Set<Int> {
+        require(startNumber <= endNumber) { "시작 숫자(=${startNumber})는 종료 숫자(=${endNumber})보다 작거나 같아야 합니다" }
+        return (startNumber..endNumber).shuffled().subList(0, count).toSet()
+    }
 }
--- src/test/kotlin/lotto/RandomNumberGeneratorTest.kt
+++ src/test/kotlin/lotto/RandomNumberGeneratorTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
@@ -8,5 +9,10 @@ class RandomNumberGeneratorTest : FunSpec({
         val randomNumberGenerator = RandomNumberGenerator()
         val numbers = randomNumberGenerator.generateNumber(1, 45, 6)
         numbers.size shouldBe 6
+    }
+
+    test("랜덤 숫자 생성기의 전달될 숫자A가 숫자B보다 크면 예외가 발생한다.") {
+        val randomNumberGenerator = RandomNumberGenerator()
+        shouldThrow<IllegalArgumentException> { randomNumberGenerator.generateNumber(10, 1, 6) }
     }
 })
