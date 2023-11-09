d4fca2db69b64b292d2c9c7ec25e7b37f68f9429
yeongun <gjduddns@gmail.com>
2023-11-09 23:00:50 +0900

    refactor: FixedLottoGenerator 대신 익명 클래스로 수정

--- src/test/kotlin/lotto/domain/FixedLottoGenerator.kt
+++
@@ -1,7 +0,0 @@
-package lotto.domain
-
-class FixedLottoGenerator : LottoGenerator {
-    override fun generate(): Lotto {
-        return Lotto(1, 2, 3, 4, 5, 6)
-    }
-}
--- src/test/kotlin/lotto/domain/LottoVendingMachineTest.kt
+++ src/test/kotlin/lotto/domain/LottoVendingMachineTest.kt
@@ -8,7 +8,11 @@ class LottoVendingMachineTest : BehaviorSpec({
     Given("구입 금액을 넣으면") {
         val input = 14000
         When("로또 자판기는") {
-            val lottoVendingMachine = LottoVendingMachine(FixedLottoGenerator())
+            val lottoVendingMachine = LottoVendingMachine(object : LottoGenerator {
+                override fun generate(): Lotto {
+                    return Lotto(1, 2, 3, 4, 5, 6)
+                }
+            })
             val lottos = lottoVendingMachine.generate(input)
             Then("금액에 맞는 로또를 자동으로 생성한다.") {
                 lottos.shouldContain(Lotto(1, 2, 3, 4, 5, 6))
