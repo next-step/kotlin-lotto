4e860cad6ba01214e132f77775395e3d04ef7433
korjun1993 <corojoon93@naver.com>
2023-11-10 00:55:54 +0900

    feat: step2 - 당첨 로또는 6개의 숫자를 전달하면 일치 개수를 계산할 수 있다.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -5,6 +5,9 @@ class Lotto(val numbers: Set<Int>) {
         validateLottoNumbers()
     }
 
+    fun match(winningNumbers: Set<Int>): Int =
+        numbers.count { winningNumbers.contains(it) }
+
     private fun validateLottoNumbers() {
         require(numbers.size == LOTTO_NUMBER_SIZE) { "로또에 적힌 숫자는 6개입니다" }
         require(numbers.all { it in LOTTO_START_NUMBER..LOTTO_END_NUMBER }) { "로또에는 1 ~ 45 사이의 숫자만 적힐 수 있습니다" }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.row
 import io.kotest.datatest.withData
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.ints.shouldBeInRange
@@ -37,5 +38,20 @@ class LottoTest : FunSpec({
 
     test("로또에 적힌 숫자가 중복된다면 예외가 발생한다.") {
         shouldThrow<IllegalArgumentException> { Lotto(setOf(10, 10, 20, 30, 40, 45)) }
+    }
+
+    context("당첨 로또는 다른 로또와 비교하여 일치 개수를 계산할 수 있다.") {
+        val lotto = Lotto(setOf(10, 15, 20, 25, 30, 35))
+        withData(
+            row(setOf(9, 14, 19, 24, 29, 34), 0),
+            row(setOf(10, 14, 19, 24, 29, 34), 1),
+            row(setOf(10, 15, 19, 24, 29, 34), 2),
+            row(setOf(10, 15, 20, 24, 29, 34), 3),
+            row(setOf(10, 15, 20, 25, 29, 34), 4),
+            row(setOf(10, 15, 20, 25, 30, 34), 5),
+            row(setOf(10, 15, 20, 25, 30, 35), 6),
+        ) { (winningNumbers, expected) ->
+            lotto.match(winningNumbers) shouldBe expected
+        }
     }
 })
