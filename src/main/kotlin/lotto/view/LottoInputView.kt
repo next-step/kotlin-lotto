131b433edf8532e0db841d7795d6b2ab6b6421a8
yw9594 <kyw9509@gmail.com>
2023-11-09 19:26:04 +0900

    refactor(step2): 변수 이름 변경

--- src/main/kotlin/lotto/view/LottoInputView.kt
+++ src/main/kotlin/lotto/view/LottoInputView.kt
@@ -6,8 +6,8 @@ import lotto.model.WinningNumbers
 
 class LottoInputView {
     fun getInput(): LottoInput {
-        val purchaseAccount: Int = getPurchaseAccount()
-        val lottoTicketCount: Int = getLottoTicketCount(purchaseAccount)
+        val purchasePrice: Int = getPurchaseAccount()
+        val lottoTicketCount: Int = getLottoTicketCount(purchasePrice)
         val winningNumbers: WinningNumbers = getWinningNumbers()
 
         return LottoInput(lottoTicketCount, winningNumbers)
@@ -20,17 +20,17 @@ class LottoInputView {
     }
 
     private fun getPurchaseAccount(): Int {
-        val purchaseAccount = getInput("구입 금액을 입력해주세요.")?.toIntOrNull()
+        val purchasePrice = getInput("구입 금액을 입력해주세요.")?.toIntOrNull()
 
-        require(purchaseAccount != null && purchaseAccount > 0) {
+        require(purchasePrice != null && purchasePrice > 0) {
             "구매 금액은 자연수만 가능합니다."
         }
 
-        return purchaseAccount
+        return purchasePrice
     }
 
-    private fun getLottoTicketCount(purchaseAccount: Int, lottoPrice: Int = Lotto.LOTTO_PRICE): Int {
-        val lottoTicketCount = purchaseAccount / lottoPrice
+    private fun getLottoTicketCount(purchasePrice: Int, lottoPrice: Int = Lotto.LOTTO_PRICE): Int {
+        val lottoTicketCount = purchasePrice / lottoPrice
 
         require(lottoTicketCount > 0) {
             "로또를 구매할 수 없습니다. (로또 가격: $lottoPrice)"
