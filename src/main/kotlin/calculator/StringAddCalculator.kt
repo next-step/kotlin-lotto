384cce0613e98ee47500f7c070add1563e3ff63b
jeongdalma <jeongdalma@gmail.com>
2023-11-09 21:12:45 +0900

    refactor: 문자열 계산기 Operand data class 추가

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -6,17 +6,11 @@ class StringAddCalculator(
     private val validation: NumberValidation
 ) {
 
-    private val stringToIntConvert: (String) -> Int = {
-        runCatching { it.toInt() }
-            .onFailure { throw RuntimeException("정수와 구분자만 입력하여 주세요.") }
-            .getOrThrow()
-    }
-
     fun add(input: String?): Int =
         if (input.isNullOrBlank()) DEFAULT_VALUE
         else StringSpliterator.split(input).sumOf(::getNumber)
 
-    private fun getNumber(string: String): Int = stringToIntConvert(string).let {
+    private fun getNumber(string: String): Int = Operand.valueOf(string).let {
         if(validation.check(it)) throw RuntimeException("음수는 입력하실 수 없습니다.")
         else it
     }
