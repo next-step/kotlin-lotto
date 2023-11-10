b0d20ba5fa597bcf26f14b7f96e81deb9c11be5d
Factoriall <skarlgus0910@gmail.com>
2023-11-10 20:42:36 +0900

    step4: simplify LottoNumbersInput.kt

--- src/main/kotlin/lotto/view/LottoNumbersInput.kt
+++ src/main/kotlin/lotto/view/LottoNumbersInput.kt
@@ -1,18 +1,20 @@
 package lotto.view
 
+import lotto.domain.LottoNumber
 import lotto.domain.LottoNumbers
 
 class LottoNumbersInput(text: String) {
     val numbers: LottoNumbers
 
     init {
-        val numbersList = text.split(',').map {
-            try {
-                it.trim().toInt()
-            } catch (e: Exception) {
-                throw IllegalArgumentException("You need to enter six numbers separated by comma as the delimiter.(ex - 1,2,3, 4, 5,6)")
-            }
-        }
-        numbers = LottoNumbers(*numbersList.toIntArray())
+        numbers = LottoNumbers(
+            text.split(',').map { lottoNumberOrException(it) }
+        )
+    }
+
+    private fun lottoNumberOrException(it: String) = try {
+        LottoNumber(it.trim().toInt())
+    } catch (e: Exception) {
+        throw IllegalArgumentException("You need to enter six numbers separated by comma as the delimiter.(ex - 1,2,3, 4, 5,6)")
     }
 }
