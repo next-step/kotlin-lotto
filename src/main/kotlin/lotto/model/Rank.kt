5c9b1157962db968d063d96e8aa6afaf5a7e6c13
dong <dong.22@kakaoenterprise.com>
2023-11-10 09:44:19 +0900

    feat : 2등 당첨번호를 집계하는 로직 리팩토링

--- src/main/kotlin/lotto/model/Rank.kt
+++ src/main/kotlin/lotto/model/Rank.kt
@@ -6,7 +6,7 @@ enum class Rank(
     private val prize: Int,
 ) {
     FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 222222222),
+    SECOND(5, true, 30_000_000),
     THIRD(5, false, 1_500_000),
     FOURTH(4, false, 50_000),
     FIFTH(3, false, 5_000),
@@ -17,17 +17,39 @@ enum class Rank(
     }
 
     companion object {
-        fun matchCountToRank(matchCount: Int, bonus: Boolean): Rank {
-            if (matchCount < 3) {
+        fun of(matchCount: Int, bonus: Boolean): Rank {
+            if (isBoom(matchCount)) {
                 return BOOM
             }
-            if (matchCount == SECOND.matchCount) {
-                return when (bonus == SECOND.isRequireBonus) {
-                    true -> SECOND
-                    false -> THIRD
-                }
+            if (isSecond(matchCount, bonus)) {
+                return SECOND
             }
-            return requireNotNull(Rank.values().firstOrNull { (it.matchCount == matchCount) }) { "잘못된 숫자" }
+            if (isThird(matchCount, bonus)) {
+                return THIRD
+            }
+            return countToRank(matchCount)
+        }
+
+        private fun isBoom(matchCount: Int): Boolean {
+            return matchCount <= BOOM.matchCount
+        }
+
+        private fun isThird(matchCount: Int, bonus: Boolean): Boolean {
+            return isConditionOfRank(matchCount, bonus, THIRD)
+        }
+
+        private fun isSecond(matchCount: Int, bonus: Boolean): Boolean {
+            return isConditionOfRank(matchCount, bonus, SECOND)
+        }
+
+        private fun isConditionOfRank(matchCount: Int, bonus: Boolean, rank: Rank): Boolean {
+            return ((matchCount == rank.matchCount) && (bonus == rank.isRequireBonus))
+        }
+
+        private fun countToRank(count: Int): Rank {
+            return requireNotNull(
+                Rank.values().firstOrNull { (it.matchCount == count) }
+            ) { "입력된 당첨번호의 갯수 [$count] 는 로또의 등수로 변환할 수 없습니다" }
         }
     }
 }
--- src/main/kotlin/lotto/model/Round.kt
+++ src/main/kotlin/lotto/model/Round.kt
@@ -7,7 +7,7 @@ data class Round(
         val rankAggregated: MutableMap<Rank, Int> = mutableMapOf()
         games.map { winningNumbers.countOfMatchAndHasBonus(it) }
             .asSequence()
-            .map { Rank.matchCountToRank(it.first, it.second) }
+            .map { Rank.of(it.first, it.second) }
             .forEach { rankAggregated[it] = rankAggregated.getOrDefault(it, 0) + 1 }
         return LottoWinners(
             totalGameCount = this.games.size,
