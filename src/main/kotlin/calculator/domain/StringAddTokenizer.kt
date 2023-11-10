afde5d3db4f3e23d2dee338b81f68e66d867f9a9
inhyeokk <rkddlsgur983@gmail.com>
2023-11-10 21:12:48 +0900

    step1-review: 객체지향 생활 체조 원칙 2. else 키워드를 쓰지 않는다.

--- src/main/kotlin/calculator/domain/StringAddTokenizer.kt
+++ src/main/kotlin/calculator/domain/StringAddTokenizer.kt
@@ -4,13 +4,12 @@ class StringAddTokenizer(private val text: String) {
 
     fun getTokens(): List<String> {
         val result = FIND_CUSTOM_DELIMITER_REGEX.find(text)
-        return if (result != null) {
+        if (result != null) {
             val customDelimiter = result.groupValues[1]
             val target = result.groupValues[2]
-            target.split(customDelimiter)
-        } else {
-            text.split(DEFAULT_DELIMITER_REGEX)
+            return target.split(customDelimiter)
         }
+        return text.split(DEFAULT_DELIMITER_REGEX)
     }
 
     companion object {
