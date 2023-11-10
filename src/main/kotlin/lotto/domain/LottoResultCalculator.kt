54a666df37a55302abd3d57acc1ca3f97fcf89b3
yeongun <gjduddns@gmail.com>
2023-11-11 07:44:39 +0900

    [Step4] refactor: 당첨 결과를 계산할 때 groupingBy, eachCount 함수를 사용하도록 수정

--- src/main/kotlin/lotto/domain/LottoResultCalculator.kt
+++ src/main/kotlin/lotto/domain/LottoResultCalculator.kt
@@ -5,13 +5,10 @@ import kotlin.math.floor
 class LottoResultCalculator(private val winningLotto: WinningLotto) {
 
     fun calculateResult(manualLottos: Lottos, autoLottos: Lottos): Map<Rank, Int> {
-        val result = mutableMapOf<Rank, Int>()
-        for (lotto in manualLottos.lottos + autoLottos.lottos) {
-            val rank = winningLotto.calculateRank(lotto)
-            val lottoCount = result.getOrDefault(rank, 0)
-            result.putIfAbsent(rank, lottoCount + 1)
-        }
-        return result
+        return (manualLottos.lottos + autoLottos.lottos)
+            .map { winningLotto.calculateRank(it) }
+            .groupingBy { it }
+            .eachCount()
     }
 
     fun calculateEarningRate(result: Map<Rank, Int>, amount: Amount): Double {
