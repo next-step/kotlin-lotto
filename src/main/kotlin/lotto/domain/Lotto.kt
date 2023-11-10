0dca3bcc9fa576d388a4443bf6f18efaa6e88e52
LAPTOP-A8710DGI\kdh85 <modudamba@naver.com>
2023-11-10 20:02:53 +0900

    step2: 로또(자동) 로또 객체 구현 * 로또 숫자 객체 6개로 구성된 로또 객체를 정의함. * 로또 생성시 중복된 숫자가 있는 경우 에러를 반환함. * 로또간 숫자객체를 비교하여 일치하는 갯수를 반환하는 책임을 구현.

---
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -0,0 +1,44 @@
+package lotto.domain
+
+class Lotto private constructor(
+    private val lottoNumbers: List<LottoNumber> = listOf()
+) {
+
+    val lotto: List<LottoNumber> = lottoNumbers
+
+    fun makeMatchCountByNumbers(winningLotto: Lotto): Int {
+        var count = 0
+        winningLotto.lotto.forEach { winNumber ->
+            count += countByMatchNumbers(winNumber)
+        }
+        return count
+    }
+
+    private fun countByMatchNumbers(winNumber: LottoNumber): Int {
+        return lotto.count {
+            it == winNumber
+        }
+    }
+
+    companion object {
+
+        private const val MAX_NUMBERS_COUNT = 6
+
+        fun from(numbers: List<Int>): Lotto {
+
+            require(numbers.size == MAX_NUMBERS_COUNT) {
+                "로또를 올바르게 생성하려면 반드시 6개의 번호를 넣어주세요."
+            }
+
+            require(numbers.groupBy { it }.size == 6) {
+                "중복된 숫자를 제외한 6개의 숫자를 입력해 주세요"
+            }
+
+            val lottoNumbers = numbers.map {
+                LottoNumber.from(it)
+            }.toList()
+
+            return Lotto(lottoNumbers)
+        }
+    }
+}
