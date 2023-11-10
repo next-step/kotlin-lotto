aaa8fc5adbbd289ac1fdd4796a6a62f1efdd1306
Ueibin Kim <at365@naver.com>
2023-11-10 17:13:40 +0900

    🚀 1단계 - 문자열 덧셈 계산기 (#898)
    * fix : kotest markTime 이슈 해결을 위한 버전 변경
    
    * feat : 문자열 식 도메인 ( Expression ) 구현
    
    * feat : 계산기 도메인 ( Calculator ) 구현
    
    * feat : 최종 답 도메인 ( FinalResult ) 객체 구현
    
    * test : 문자열 도메인 ( Expression ) 테스트 작성
    
    * test : 계산기 도메인 ( Calculator ) 도메인 테스트 진행
    
    * docs : Readme 작성

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # kotlin-lotto
+
+# Step1 - 문자열 덧셈 계산기 
+## 도메인 ( Domain )
+- 문자열 식 ( Expression )
+- 계산기 ( Calculator )
+- 결과 값 ( FinalResult )
+
+## 고려해야하는 사항 
+- 음수가 입력되어 있을 경우 예외를 처리해야한다 ( Runtime Exception )
+- 구분자는 , 외 : 도 사용할 수 있다.
---
+++ src/test/kotlin/camp/nextstep/edu/step/domain/expression/ExpressionTest.kt
@@ -0,0 +1,38 @@
+package camp.nextstep.edu.step.domain.expression
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.DisplayName
+
+@DisplayName("입력값은")
+class ExpressionTest : BehaviorSpec({
+
+    Given("입력값이 주어지고") {
+        val emptyInputValue = ""
+
+        When("0또는 Null이 주어지면") {
+            val emptyExpressionCreateException = shouldThrow<RuntimeException> {
+                Expression(value = emptyInputValue)
+            }
+
+            Then("예외가 발생한다.") {
+                emptyExpressionCreateException shouldBe RuntimeException()
+            }
+        }
+    }
+
+    Given("문자열이 주어졌을떄") {
+        val expressionRequest = "1,2:3"
+
+        When("쉼표 또는 콜론으로 구분되어 있으면") {
+            val expression = Expression(value = expressionRequest)
+            val splitExpression = expression.splitExpression()
+            Then("쉼표 또는 콜론을 통해 문자열식을 분리한다.") {
+                splitExpression.size shouldBe 3
+                splitExpression shouldBe listOf("1", "2", "3")
+            }
+        }
+    }
+
+})
