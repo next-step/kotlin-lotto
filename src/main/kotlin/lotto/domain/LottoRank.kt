793091d20e8cdbefdf952049afab7462fd33f8b8
Factoriall <skarlgus0910@gmail.com>
2023-11-10 20:21:58 +0900

    step4: refactor LottoRank.kt - use abstract class for enum

--- src/main/kotlin/lotto/domain/LottoRank.kt
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -1,26 +1,41 @@
 package lotto.domain
 
 enum class LottoRank(val containCount: Int, val money: Int) {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    NONE(0, 0);
+    FIRST(6, 2_000_000_000) {
+        override fun isMatch(matchingCount: Int, isBonusMatched: Boolean): Boolean =
+            matchingCount == 6
+    },
+    SECOND(5, 30_000_000) {
+        override fun isMatch(matchingCount: Int, isBonusMatched: Boolean): Boolean =
+            matchingCount == 5 && isBonusMatched
+    },
+    THIRD(5, 1_500_000) {
+        override fun isMatch(matchingCount: Int, isBonusMatched: Boolean): Boolean =
+            matchingCount == 5 && !isBonusMatched
+    },
+    FOURTH(4, 50_000) {
+        override fun isMatch(matchingCount: Int, isBonusMatched: Boolean): Boolean =
+            matchingCount == 4
+    },
+    FIFTH(3, 5_000) {
+        override fun isMatch(matchingCount: Int, isBonusMatched: Boolean): Boolean =
+            matchingCount == 3
+    },
+    NONE(0, 0) {
+        override fun isMatch(matchingCount: Int, isBonusMatched: Boolean): Boolean = false
+    };
+
+    abstract fun isMatch(matchingCount: Int, isBonusMatched: Boolean): Boolean
 
     companion object {
-        fun of(winningNumbers: WinningNumbers, lottoNumbers: LottoNumbers): LottoRank =
-            when (getMatchingCount(winningNumbers.winningNumbers, lottoNumbers)) {
-                3 -> FIFTH
-                4 -> FOURTH
-                5 -> {
-                    if (isBonusMatched(lottoNumbers, winningNumbers.bonusNumber)) SECOND
-                    else THIRD
-                }
-
-                6 -> FIRST
-                else -> NONE
-            }
+        fun of(winningNumbers: WinningNumbers, lottoNumbers: LottoNumbers): LottoRank {
+            return LottoRank.values().find {
+                it.isMatch(
+                    getMatchingCount(winningNumbers.winningNumbers, lottoNumbers),
+                    isBonusMatched(lottoNumbers, winningNumbers.bonusNumber)
+                )
+            } ?: NONE
+        }
 
         private fun getMatchingCount(winningNumbers: LottoNumbers, lottoNumbers: LottoNumbers): Int {
             return lottoNumbers.numbers.intersect(winningNumbers.numbers.toSet()).size
