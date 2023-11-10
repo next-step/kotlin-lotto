1ce0fcb00f3aa6fdeb6afb3effda497fd91f79c0
inhyeokk <rkddlsgur983@gmail.com>
2023-11-10 21:15:39 +0900

    step1-review: index 대신 MatchResult 의 destructured 활용
    - https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.text/-match-result/group-values.html

--- src/main/kotlin/calculator/domain/StringAddTokenizer.kt
+++ src/main/kotlin/calculator/domain/StringAddTokenizer.kt
@@ -5,8 +5,7 @@ class StringAddTokenizer(private val text: String) {
     fun getTokens(): List<String> {
         val result = FIND_CUSTOM_DELIMITER_REGEX.find(text)
         if (result != null) {
-            val customDelimiter = result.groupValues[1]
-            val target = result.groupValues[2]
+            val (customDelimiter, target) = result.destructured
             return target.split(customDelimiter)
         }
         return text.split(DEFAULT_DELIMITER_REGEX)
