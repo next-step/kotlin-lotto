6d9f14350ae2511ba36428a2b694a0cb9ca04d4e
korjun1993 <corojoon93@naver.com>
2023-11-10 15:48:13 +0900

    feat: step2 - 로또 게임으로부터 각 등수가 몇명인지 알 수 있다.

--- src/main/kotlin/lotto/LottoGameResult.kt
+++ src/main/kotlin/lotto/LottoGameResult.kt
@@ -3,4 +3,7 @@ package lotto
 data class LottoGameResult(val totalPrice: Int, val rewards: List<LottoReward>) {
     fun calculatePerformance(): Double =
         rewards.getAmount().toDouble() / totalPrice
+
+    fun getRewardCount(reward: LottoReward): Int =
+        rewards.count { it == reward }
 }
--- src/test/kotlin/lotto/LottoGameResultTest.kt
+++ src/test/kotlin/lotto/LottoGameResultTest.kt
@@ -4,10 +4,20 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class LottoGameResultTest : FunSpec({
-    val lottoGameResult = LottoGameResult(totalPrice = 3000, rewards = listOf(LottoReward.FOURTH))
-
     test("로또 게임의 수익률은 `수익금 / 원금` 으로 계산된다.") {
+        val lottoGameResult = LottoGameResult(totalPrice = 3000, rewards = listOf(LottoReward.FOURTH))
         val expected = LottoReward.FOURTH.reward.toDouble() / 3000
         lottoGameResult.calculatePerformance() shouldBe expected
+    }
+
+    test("로또 게임으로부터 각 등수가 몇명인지 알 수 있다.") {
+        val lottoGameResult = LottoGameResult(
+            totalPrice = 3000,
+            rewards = listOf(LottoReward.FIRST, LottoReward.SECOND)
+        )
+        lottoGameResult.getRewardCount(LottoReward.FIRST) shouldBe 1
+        lottoGameResult.getRewardCount(LottoReward.SECOND) shouldBe 1
+        lottoGameResult.getRewardCount(LottoReward.THIRD) shouldBe 0
+        lottoGameResult.getRewardCount(LottoReward.FOURTH) shouldBe 0
     }
 })
