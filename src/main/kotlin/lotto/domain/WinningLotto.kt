5ed7204b7e94fe4bdaa733cd7344e234c1c7655b
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-11 20:05:30 +0900

    feat : WinningLotto 의 Lottos 통계 추, dto 선언

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -1,5 +1,7 @@
 package lotto.domain
 
+import lotto.domain.dto.WinningResult
+
 class WinningLotto(val winningNumbers: List<Int>) {
     fun match(lotto: Lotto): LotteryPrizeAmount {
         val userNumbers = lotto.numbers
@@ -8,5 +10,23 @@ class WinningLotto(val winningNumbers: List<Int>) {
         }
 
         return LotteryPrizeAmount.getWinningPrize(count)
+    }
+
+    fun matchLottosResult(lottos: Lottos): List<WinningResult> {
+        val lottoList = lottos.lottoList
+        val statistics = mutableMapOf<LotteryPrizeAmount, Int>()
+        val resultList = mutableListOf<WinningResult>()
+        lottoList.forEach { lotto: Lotto ->
+            val lotteryPrize = match(lotto)
+            statistics[lotteryPrize] = statistics.getOrDefault(lotteryPrize, 0) + 1
+        }
+
+        LotteryPrizeAmount.values()
+            .forEach {
+                val count = statistics.getOrDefault(it, 0)
+                val result = WinningResult(it.winNum, it.prize, count)
+                resultList.add(result)
+            }
+        return resultList
     }
 }
---
+++ src/main/kotlin/lotto/domain/dto/WinningResult.kt
@@ -0,0 +1,7 @@
+package lotto.domain.dto
+
+data class WinningResult(val winNum: Int, val prize: Int, val count: Int){
+    fun printResult() {
+        println("${winNum}개 일치 ${prize}원- ${count}개")
+    }
+}
