c8675a99214b40ce38d0734828681931b1548067
kycho91 <kycho91@i-nara.co.kr>
2023-11-11 17:00:15 +0900

    fest: 문자열 덧셈 기본구분자(, or :) 기능 구현

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -3,6 +3,18 @@ package calculator
 class StringAddCalculator {
 
     fun add(input: String?): Long {
-        return 0L
+
+        if (input.isNullOrBlank()) {
+            return 0L
+        }
+
+        val numbers = input.split(",|:".toRegex())
+
+        var result = 0L
+        numbers.forEach {
+            result += it.toLong()
+        }
+
+        return result
     }
 }
