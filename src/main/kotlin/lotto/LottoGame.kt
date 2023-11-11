83829904517b4aed6e8e084fea6044aea8d3a7d6
OYJ <oyj7677@gmail.com>
2023-11-12 04:27:37 +0900

    Feat: 로또 확인 및 통계 반환 로직 구현

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -4,6 +4,8 @@ import lotto.LottoNumber.MAX_NUMBER
 import lotto.LottoNumber.MIN_NUMBER
 
 class LottoGame {
+    val winningStatusMap = mutableMapOf<LottoRanking, Int>()
+
     fun buyLotto(cash: Int): List<Lotto> {
         val lottoList = mutableListOf<Lotto>()
         val times = cash / 1000
@@ -18,6 +20,14 @@ class LottoGame {
     private fun createRandomNumber(): List<Int> {
         return (MIN_NUMBER..MAX_NUMBER).shuffled()
             .subList(SUB_LIST_START_POSITION, SUB_LIST_START_POSITION + SUB_LIST_LENGTH).sorted()
+    }
+
+    fun getWinningStats(winningLotto: Lotto, purchaseLottoList: List<Lotto>): Map<LottoRanking, Int> {
+        purchaseLottoList.forEach {
+            val lottoRanking = LottoMachine.checkLotto(winningLotto, it)
+            winningStatusMap[lottoRanking] = winningStatusMap.getOrDefault(lottoRanking, 0) + 1
+        }
+        return winningStatusMap
     }
 
     companion object {
--- src/test/kotlin/lotto/LottoGameTest.kt
+++ src/test/kotlin/lotto/LottoGameTest.kt
@@ -16,4 +16,30 @@ class LottoGameTest {
         // then : 입력 받은 금액의 최대 수량의 로또를 구매한다.
         assertThat(lottoList.size).isEqualTo(4)
     }
+
+    @Test
+    fun `로또 구매와 당첨 번호를 입력했을 때, 당첨을 확인 한다면, 당첨 통계를 반환한다`() {
+        // given : 로또 구매와 당첨 번호를 입력한다.
+        // 2등 - 2개, 3등 - 1개, 4등 - 1개
+        val winningLotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+
+        val purchaseLotto1 = Lotto(listOf(1, 2, 3, 4, 5, 7))
+        val purchaseLotto2 = Lotto(listOf(1, 2, 3, 4, 5, 7))
+        val purchaseLotto3 = Lotto(listOf(1, 2, 3, 4, 7, 8))
+        val purchaseLotto4 = Lotto(listOf(1, 2, 3, 7, 8, 9))
+        val purchaseLotto5 = Lotto(listOf(1, 2, 6, 7, 8, 9))
+        val purchaseLotto6 = Lotto(listOf(11, 12, 13, 14, 15, 16))
+
+        val purchaseLottoList =
+            listOf(purchaseLotto1, purchaseLotto2, purchaseLotto3, purchaseLotto4, purchaseLotto5, purchaseLotto6)
+
+        val lottoGame = LottoGame()
+
+        // when : 당첨을 확인을 요청한다.
+        val winningStats = lottoGame.getWinningStats(winningLotto, purchaseLottoList)
+
+        // then : 당첨 통계를 확인한다.
+        val expect = mutableMapOf(SecondPlace to 2, ThirdPlace to 1, FourthPlace to 2, None to 1)
+        assertThat(winningStats).isEqualTo(expect)
+    }
 }
