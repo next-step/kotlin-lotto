32cf816ea0cbf46c5575dcdcb380a08ab455b5f8
seongmo-jeon <oeeen3@gmail.com>
2023-11-12 00:31:04 +0900

    refactor: LottoNumber 범위를 상수로 추출

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -2,12 +2,17 @@ package lotto.domain
 
 data class LottoNumber(val number: Int) {
     init {
-        require(number in 1..45) {
-            "로또 숫자는 1부터 45사이의 숫자여야 합니다."
+        require(number in LOTTO_LOWER_BOUND..LOTTO_UPPER_BOUND) {
+            "로또 숫자는 ${LOTTO_LOWER_BOUND}부터 ${LOTTO_UPPER_BOUND}사이의 숫자여야 합니다."
         }
     }
 
     override fun toString(): String {
         return number.toString()
+    }
+
+    companion object {
+        const val LOTTO_LOWER_BOUND = 1
+        const val LOTTO_UPPER_BOUND = 45
     }
 }
--- src/main/kotlin/lotto/domain/LottoNumbers.kt
+++ src/main/kotlin/lotto/domain/LottoNumbers.kt
@@ -1,8 +1,5 @@
 package lotto.domain
 
 object LottoNumbers {
-    private const val LOTTO_LOWER_BOUND = 1
-    private const val LOTTO_UPPER_BOUND = 45
-
-    val LOTTO_NUMBERS = (LOTTO_LOWER_BOUND..LOTTO_UPPER_BOUND).map { LottoNumber(it) }
+    val LOTTO_NUMBERS = (LottoNumber.LOTTO_LOWER_BOUND..LottoNumber.LOTTO_UPPER_BOUND).map { LottoNumber(it) }
 }
