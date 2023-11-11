aaa522c3faf8fb05f350ae57edeb3f9c8a12807e
bongbong <kbk5190@naver.com>
2023-11-11 13:08:55 +0900

    feat: 보너스 볼 입력 받기

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,14 +3,23 @@ package lotto.view
 object InputView {
     private const val PURCHASE_AMOUNT_QUESTION = "구입금액을 입력해 주세요."
     private const val WINNING_NUMBERS_QUESTION = "지난 주 당첨 번호를 입력해 주세요."
+    private const val BONUS_NUMBER_QUESTION = "보너스 볼을 입력해 주세요."
 
     fun getPurchaseInput(): String {
         println(PURCHASE_AMOUNT_QUESTION)
-        return readlnOrNull() ?: throw IllegalArgumentException("입력 값이 없습니다.")
+        return readInput()
     }
 
     fun getWinningNumbersInput(): String {
         println(WINNING_NUMBERS_QUESTION)
-        return readlnOrNull() ?: throw IllegalArgumentException("입력 값이 없습니다.")
+        return readInput()
     }
+
+    fun getBonusNumberInput(): String {
+        println(BONUS_NUMBER_QUESTION)
+        return readInput()
+    }
+
+    private fun readInput(): String = readlnOrNull()
+        ?: throw IllegalArgumentException("입력 값이 없습니다.")
 }
