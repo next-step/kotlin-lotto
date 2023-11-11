ff90cdbe940fbf69aca0a591f84eede6847ced3a
ALbertIM0427 <guang7226961@naver.com>
2023-11-11 18:37:18 +0900

    refactor: 네이밍 변경

--- src/main/kotlin/lotto/business/RandomLottoPicker.kt
+++ src/main/kotlin/lotto/business/RandomLottoPicker.kt
@@ -2,14 +2,14 @@ package lotto.business
 
 class RandomLottoPicker {
     fun pick(): Set<LottoNumber> {
-        return lottoNumbers.shuffled().take(LOTTO_NUMBER_COUNT).toSet()
+        return LOTTO_NUMBERS.shuffled().take(LOTTO_NUMBER_COUNT).toSet()
     }
 
     companion object {
         private const val LOTTO_NUMBER_COUNT = 6
         private const val LOTTO_NUMBER_MIN = 1
         private const val LOTTO_NUMBER_MAX = 45
-        private val lottoNumbers = (LOTTO_NUMBER_MIN..LOTTO_NUMBER_MAX)
+        private val LOTTO_NUMBERS = (LOTTO_NUMBER_MIN..LOTTO_NUMBER_MAX)
             .map { LottoNumber(it) }
             .toList()
     }
