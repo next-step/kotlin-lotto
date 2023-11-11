640bac9ef264ac42d476852fb9c0674e947ca67f
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-11 14:50:27 +0900

    feat : Lotto 는 LottoNumber 를 객체로 가집니다

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,27 +1,20 @@
 package lotto.domain
 
-class Lotto(val numbers: List<Int>) {
+import lotto.numbermaker.RandomNumberMaker
+
+class Lotto(val number: Int = LOTTO_DEFAULT_NUMBER) {
+    private val randomNumberMaker: RandomNumberMaker = RandomNumberMaker()
+    val numbers = mutableListOf<LottoNumber>()
+
     init {
-        isNumbersOver6()
-        isNumbersDistinct()
-        numbers.forEach {
-            isNumberRange1To45(it)
-        }
-    }
-    private fun isNumbersOver6(){
-        if (numbers.size != 6) {
-            throw IllegalArgumentException("로또는 6개의 숫자로 이루어져 있습니다.");
+        val randomNumber = randomNumberMaker.generate()
+        randomNumber.forEach { it ->
+            val lottoNumber = LottoNumber.from(it)
+            numbers.add(lottoNumber)
         }
     }
 
-    private fun isNumbersDistinct() {
-        val distinct = numbers.distinct()
-        if (numbers.size != distinct.size){
-            throw IllegalArgumentException("로또 번호는 다른 숫자로 이루어져야합니다.");
-        }
-    }
-
-    private fun isNumberRange1To45(number: Int){
-        if (number < 1 || number > 45) throw IllegalArgumentException("로또 번호는 1에서 45 사이 숫자이어야합니다.")
+    companion object {
+        private const val LOTTO_DEFAULT_NUMBER = 6
     }
 }
--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,34 +1,18 @@
 package lotto.domain
 
-import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 class LottoTest {
     @Test
-    fun `로또는 6개의 번호만 가질 수 있다`() {
-        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
-        lotto.numbers shouldHaveSize 6
+    fun `로또는 6개의 번호를 가진다`() {
+        val lotto = Lotto()
+        lotto.numbers.size shouldBe 6
     }
 
     @Test
-    fun `로또는 6개 보다 많게 가질 수 없다`() {
-        shouldThrow<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
-        }
-    }
-
-    @Test
-    fun `로또 번호는 서로 다른 숫자로 이루어야한다`() {
-        shouldThrow<IllegalArgumentException> {
-            Lotto(listOf(1, 1, 3, 4, 5, 6))
-        }
-    }
-
-    @Test
-    fun `로또 번호는 1~45 사이의 수이다`() {
-        shouldThrow<IllegalArgumentException> {
-            Lotto(listOf(1, 46, 3, 4, 5, 6, 7))
-        }
+    fun `로또 번호는 서로 다른 숫자로 이루어져있다`() {
+        val lotto = Lotto()
+        lotto.numbers.distinct().size shouldBe 6
     }
 }
