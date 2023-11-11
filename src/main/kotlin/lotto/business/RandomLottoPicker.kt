8868448a659d5406031dd14ed1f0373848f8723d
ALbertIM0427 <guang7226961@naver.com>
2023-11-11 18:36:38 +0900

    refactor: 1~45 숫자보다 로또 넘버를 캐싱

--- src/main/kotlin/lotto/business/RandomLottoPicker.kt
+++ src/main/kotlin/lotto/business/RandomLottoPicker.kt
@@ -2,13 +2,15 @@ package lotto.business
 
 class RandomLottoPicker {
     fun pick(): Set<LottoNumber> {
-        return lottoNumbers.shuffled().take(LOTTO_NUMBER_COUNT).map { LottoNumber(it) }.toSet()
+        return lottoNumbers.shuffled().take(LOTTO_NUMBER_COUNT).toSet()
     }
 
     companion object {
         private const val LOTTO_NUMBER_COUNT = 6
         private const val LOTTO_NUMBER_MIN = 1
         private const val LOTTO_NUMBER_MAX = 45
-        private val lottoNumbers = (LOTTO_NUMBER_MIN..LOTTO_NUMBER_MAX).toList()
+        private val lottoNumbers = (LOTTO_NUMBER_MIN..LOTTO_NUMBER_MAX)
+            .map { LottoNumber(it) }
+            .toList()
     }
 }
