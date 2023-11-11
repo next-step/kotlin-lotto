db319857f3df087888209653dd367b3128f43b10
OYJ <oyj7677@gmail.com>
2023-11-12 05:40:42 +0900

    refactor: 금액 입력 검증 로직 리펙토링

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,8 +3,16 @@ package lotto.view
 object InputView {
     private const val ERR_MSG_INVALID_NUMERIC_FORMAT = "입력된 구입 금액의 포맷이 숫자가 압니다."
     private const val ERR_MSG_OUT_OF_CASH_RANGE = "입력된 값이 구매 가능할 수 있는 금액의 범위를 벗어났습니다"
-    fun validateCash(cash: String) {
-        require(cash.toIntOrNull() != null) {ERR_MSG_INVALID_NUMERIC_FORMAT}
-        require(cash.toInt() in 1000..100000) {ERR_MSG_OUT_OF_CASH_RANGE}
+    fun validateCash(inputData: String) {
+        validateNumericFormat(inputData)
+        validateNumberRange(inputData.toInt())
+    }
+
+    private fun validateNumericFormat(inputData: String) {
+        require(inputData.toIntOrNull() != null) { ERR_MSG_INVALID_NUMERIC_FORMAT }
+    }
+
+    private fun validateNumberRange(inputData: Int) {
+        require(inputData in 1000..100000) { ERR_MSG_OUT_OF_CASH_RANGE }
     }
 }
