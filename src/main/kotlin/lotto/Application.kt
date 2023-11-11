2d73e07cc4a3d0f9eac11883eeec23d2b354fa70
korjun1993 <corojoon93@naver.com>
2023-11-12 00:58:58 +0900

    fix: step2 - 메인 함수 변경

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,16 +1,17 @@
 package lotto
 
+import lotto.domain.LottoFactory
 import lotto.domain.LottoGame
-import lotto.domain.generateLottoList
+import lotto.domain.LottoNumbers
 import lotto.view.*
 
 fun main() {
     val price = readPrice()
     val count = confirmCount(price)
-    val lottoList = generateLottoList(count)
+    val lottoList = LottoFactory.generateLottoList(count)
     printLottoList(lottoList)
 
-    val winningNumbers = readWinningNumbers()
+    val winningNumbers = LottoNumbers(readWinningNumbers())
     val lottoGame = LottoGame(lottoList, winningNumbers)
     val lottoGameResult = lottoGame.getResult()
     printLottoGameResult(lottoGameResult)
