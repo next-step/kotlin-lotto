8b3243232673b6cc661d0e04ad6643d8ffd4c071
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-11 19:34:46 +0900

    fix : Lotto 의 프로퍼티 변경으로 fixedNumber 도 받을 수 있도록 변경

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -2,19 +2,20 @@ package lotto.domain
 
 import lotto.numbermaker.RandomNumberMaker
 
-class Lotto(val number: Int = LOTTO_DEFAULT_NUMBER) {
-    private val randomNumberMaker: RandomNumberMaker = RandomNumberMaker()
+class Lotto(private val numberList: List<Int> = generateDefaultRandomNumberList()) {
     val numbers = mutableListOf<LottoNumber>()
 
     init {
-        val randomNumber = randomNumberMaker.generate()
-        randomNumber.forEach { it ->
+        numberList.forEach { it ->
             val lottoNumber = LottoNumber.from(it)
             numbers.add(lottoNumber)
         }
     }
 
     companion object {
-        private const val LOTTO_DEFAULT_NUMBER = 6
+        private fun generateDefaultRandomNumberList(): List<Int> {
+            val randomNumberMaker: RandomNumberMaker = RandomNumberMaker()
+            return randomNumberMaker.generate()
+        }
     }
 }
