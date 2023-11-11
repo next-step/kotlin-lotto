9e081c6def8b3ad5dd79c6a86be7db992b393b16
OYJ <oyj7677@gmail.com>
2023-11-12 05:51:38 +0900

    Feat: 콤마(,)를 기준으로하는 문자열 분리 함수 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,6 +3,7 @@ package lotto.view
 object InputView {
     private const val ERR_MSG_INVALID_NUMERIC_FORMAT = "입력된 구입 금액의 포맷이 숫자가 압니다."
     private const val ERR_MSG_OUT_OF_CASH_RANGE = "입력된 값이 구매 가능할 수 있는 금액의 범위를 벗어났습니다"
+
     fun validateCash(inputData: String) {
         validateNumericFormat(inputData)
         validateNumberRange(inputData.toInt())
@@ -14,5 +15,13 @@ object InputView {
 
     private fun validateNumberRange(inputData: Int) {
         require(inputData in 1000..100000) { ERR_MSG_OUT_OF_CASH_RANGE }
+    }
+
+    fun validateWinningNumber(inputData: String) {
+        require(inputData.split(',').size == 6) {}
+    }
+
+    fun splitInputData(inputData: String): List<String> {
+        return inputData.split(",").map { it.trim() }
     }
 }
--- src/test/kotlin/lotto/view/InputViewTest.kt
+++ src/test/kotlin/lotto/view/InputViewTest.kt
@@ -5,25 +5,38 @@ import org.junit.jupiter.api.Test
 
 class InputViewTest {
     @Test
-    fun `숫자 포맷이 아닌 구매 금액을 입력받았다면, 입력 금액 검증을 했을 때, 에러를 던진다`() {
+    fun `숫자 포맷이 아닌 구매 금액을 입력받았다면, 입력 금액 검증을 요청했을 때, 에러를 던진다`() {
         // given :
-        val cash = "10000a"
+        val inputData = "10000a"
 
         // when :
-        val actual = runCatching { InputView.validateCash(cash) }.exceptionOrNull()
+        val actual = runCatching { InputView.validateCash(inputData) }.exceptionOrNull()
 
         // then :
         assertThat(actual).isInstanceOf(IllegalArgumentException()::class.java)
     }
 
     @Test
-    fun `구입 금액의 범위를 넘어가는 값을 입력 받는다면, 입력 금액 검증을 했을 때, 에러를 던진다`() {
+    fun `구입 금액의 범위를 넘어가는 값을 입력 받는다면, 입력 금액 검증을 요청했을 때, 에러를 던진다`() {
         // given :
-        val cash = "240000"
+        val inpuData = "240000"
 
-        val actual = runCatching { InputView.validateCash(cash) }.exceptionOrNull()
+        val actual = runCatching { InputView.validateCash(inpuData) }.exceptionOrNull()
 
         // then :
         assertThat(actual).isInstanceOf(IllegalArgumentException()::class.java)
+    }
+
+    @Test
+    fun `당첨 번호 입력값을 받는다면, , 콤마를 기준으로 리스트를 반환한다`() {
+        // given :
+        val inputData = "1,2,3,4,5,6"
+
+        // when :
+        val actual = InputView.splitInputData(inputData)
+
+        // then :
+        val expect = listOf("1", "2", "3", "4", "5", "6")
+        assertThat(actual).isEqualTo(expect)
     }
 }
