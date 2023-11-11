4b3a230281fca746bcc6aeeba75556e11e9f3167
JunHyun Lee <dlwnsgus777@naver.com>
2023-11-11 23:35:47 +0900

    feat: 구매한 로또들의 번호 출력 기능 개발

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -8,6 +8,10 @@ data class LottoNumber(private val number: Int) {
         return number
     }
 
+    override fun toString(): String {
+        return number.toString()
+    }
+
     companion object {
         private const val ERROR_MESSAGE: String = "로또 번호는 45를 넘을 수 없습니다."
     }
--- src/main/kotlin/lotto/presentation/OutputManager.kt
+++ src/main/kotlin/lotto/presentation/OutputManager.kt
@@ -9,5 +9,8 @@ class OutputManager {
 
     fun printSellLottoCount(lottos: List<Lotto>) {
         println("${lottos.size}개를 구매했습니다.")
+        lottos.forEach {
+            println(it.getNumbers())
+        }
     }
 }
