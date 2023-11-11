b794a4f1244d2e0e7300a9473f7bc4237e1edc88
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-11 19:36:51 +0900

    fix : inputview fun 네이밍 변경

--- src/main/kotlin/lotto/controller/LottoStore.kt
+++ src/main/kotlin/lotto/controller/LottoStore.kt
@@ -1,24 +1,21 @@
 package lotto.controller
 
-import lotto.domain.LottoChecker
 import lotto.domain.LottoMachine
-import lotto.ui.InputView.receiveNumbers
+import lotto.ui.InputView.receivePurchaseMoney
 import lotto.ui.InputView.receiveWinningNumber
-import lotto.ui.ResultView.show
-import lotto.ui.UserInterface.showNumbers
 
 class LottoStore {
     private val lottoMachine: LottoMachine = LottoMachine()
-    private val lottoChecker: LottoChecker = LottoChecker()
+//    private val lottoChecker: LottoChecker = LottoChecker()
     fun lotto() {
-        val money = receiveNumbers()
+        val money = receivePurchaseMoney()
         val lottos = lottoMachine.purchase(money)
-        showNumbers(lottos)
+//        showNumbers(lottos)
 
         val winningNumbers = receiveWinningNumber()
 
-        val winNumStatistics = lottoChecker.getWinNumStatistics(lottos, winningNumbers)
-        show(winNumStatistics)
+//        val winNumStatistics = lottoChecker.getWinNumStatistics(lottos, winningNumbers)
+//        show(winNumStatistics)
     }
 }
 
--- src/main/kotlin/lotto/ui/InputView.kt
+++ src/main/kotlin/lotto/ui/InputView.kt
@@ -1,7 +1,7 @@
 package lotto.ui
 
 object InputView {
-    fun receiveNumbers(): Int {
+    fun receivePurchaseMoney(): Int {
         println("구입금액을 입력해 주세요.")
         val money = readln().toInt()
         return money
