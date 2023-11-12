7f3c5fd5a233ab4dba7a7fa83c0fa83819f0d77a
eunwoo song <presentsong@naver.com>
2023-11-12 17:11:13 +0900

    feat: 입력 받는 기능 추가

---
+++ src/main/kotlin/lotto/InputView.kt
@@ -0,0 +1,14 @@
+package lotto
+
+object InputView {
+
+    fun inputMoney(): Amount {
+        println("구입금액을 입력해 주세요.")
+        return Amount(readln().toInt())
+    }
+
+    fun inputPastWinningNumbers(): List<LottoNumber> {
+        println("지난 주 당첨 번호를 입력해 주세요.")
+        return readln().split(",").map { LottoNumber(it.toInt()) }
+    }
+}
