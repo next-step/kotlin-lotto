21b137d412c7bcaa4134843c24820cda27ce394f
Jiwon Kwon <jiwonkwon419@gmail.com>
2023-11-12 23:13:39 +0900

    step3 refactor: List를 구현하기위한 by 키워드 추가

--- src/main/kotlin/autolotto/vo/Lotto.kt
+++ src/main/kotlin/autolotto/vo/Lotto.kt
@@ -1,9 +1,5 @@
 package autolotto.vo
 
-class Lotto(originNumbers: List<LottoNumber> = listOf()) {
-    val numbers: List<LottoNumber> = originNumbers.takeIf { it.isNotEmpty() } ?: generateLottoNumbers()
-
-    private fun generateLottoNumbers(): List<LottoNumber> {
-        return (1..45).shuffled().take(6).sorted().map { LottoNumber.of(it) }
-    }
+class Lotto(originNumbers: List<LottoNumber> = listOf()) : List<LottoNumber> by originNumbers {
+    val numbers: List<LottoNumber> = originNumbers.takeIf { it.isNotEmpty() } ?: LottoNumber.generateLottoNumbers()
 }
