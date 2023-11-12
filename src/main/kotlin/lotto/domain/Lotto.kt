5f3ac641cb93f0d7947460fe74c5d5d17c040932
hyotaek-jang <hyotaek.jang@linecorp.com>
2023-11-12 09:17:44 +0900

    refactor: Lotto objects make their own value

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,15 +1,20 @@
 package lotto.domain
 
-class Lotto(val numbers: List<Int>) {
+class Lotto(val numbers: List<Int> = (LOTTO_NUMBER_MIN..LOTTO_NUMBER_MAX).toList().shuffled().take(LOTTO_NUMBER_SIZE).sorted()) {
     init {
         require(numbers.size == LOTTO_NUMBER_SIZE) { "로또 번호는 ${LOTTO_NUMBER_SIZE}개만 가능합니다." }
         require(numbers.toSet().size == LOTTO_NUMBER_SIZE) { "로또 번호는 중복될 수 없습니다." }
         require(numbers.all { it in LOTTO_NUMBER_MIN..LOTTO_NUMBER_MAX }) { "로또 번호는 1~45 사이의 숫자만 가능합니다." }
     }
+
+    fun match(winningLotto: Lotto): Int {
+        return numbers.intersect(winningLotto.numbers.toSet()).size
+    }
+
     companion object {
         const val LOTTO_PRICE = 1000
-        const val LOTTO_NUMBER_SIZE = 6
-        const val LOTTO_NUMBER_MIN = 1
-        const val LOTTO_NUMBER_MAX = 45
+        private const val LOTTO_NUMBER_SIZE = 6
+        private const val LOTTO_NUMBER_MIN = 1
+        private const val LOTTO_NUMBER_MAX = 45
     }
 }
