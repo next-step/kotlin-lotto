00e8e11cc648cbf93caa1ade290383984a16c983
jaylene shin <jaylene.shin@kakaocorp.com>
2023-11-12 21:56:04 +0900

    정규식 객체로 프로퍼티 설정

--- src/main/kotlin/stringAddCalculator/Separator.kt
+++ src/main/kotlin/stringAddCalculator/Separator.kt
@@ -1,15 +1,15 @@
 package stringAddCalculator
 
 object Separator {
-    private const val BASIC_DELIMITER_PATTERN = "[,:]"
-    private const val CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)"
+    private val BASIC_DELIMITER_PATTERN = Regex("[,:]")
+    private val CUSTOM_DELIMITER_PATTERN = Regex("//(.)\n(.*)")
 
     fun separate(text: String): List<String> {
-        return text.split(BASIC_DELIMITER_PATTERN.toRegex())
+        return text.split(BASIC_DELIMITER_PATTERN)
     }
 
     fun separateByCustomDelimiter(text: String): List<String>? {
-        val matchResult = Regex(CUSTOM_DELIMITER_PATTERN).find(text)
+        val matchResult = CUSTOM_DELIMITER_PATTERN.find(text)
         return matchResult?.let {
             val customDelimiter = it.groupValues[1]
             it.groupValues[2].split(customDelimiter)
