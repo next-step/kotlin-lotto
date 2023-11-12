4cf6c5d21c2cc2640e47d0c4c08a39dadea27052
Ueibin Kim <at365@naver.com>
2023-11-12 22:31:57 +0900

    test : 로또 구매 금액 도메인 테스트 작성

---
+++ src/test/kotlin/camp/nextstep/edu/step/step2/domain/amount/BuyAmountTest.kt
@@ -0,0 +1,38 @@
+package camp.nextstep.edu.step.step2.domain.amount
+
+import camp.nextstep.edu.step.step2.domain.amount.BuyAmount
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.DisplayName
+import java.math.BigDecimal
+
+@DisplayName("구매 금액은")
+class BuyAmountTest : BehaviorSpec({
+
+    Given("구매 금액이 주어지고") {
+        val money = 14000L
+
+        When("생성을 요청하면") {
+            val buyAmount = BuyAmount.of(amount = money)
+
+            Then("주어진 구매 금액이 생성된다") {
+                buyAmount.getAmount() shouldBe BigDecimal.valueOf(14000L)
+            }
+        }
+    }
+
+    Given("만약, 구매금액이 0원이 주어지고") {
+        val money = 0L
+
+        When("생성을 요청하면") {
+            val buyAmount = shouldThrow<IllegalArgumentException> {
+                BuyAmount.of(amount = money)
+            }
+
+            Then("예외가 발생한다") {
+                buyAmount.message shouldBe "구매 금액은 0보다 커야 합니다."
+            }
+        }
+    }
+})
