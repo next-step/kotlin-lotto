fbbdb9180cddb22500bc61f45b9969b4344638c8
Alex <kpmyung@gmail.com>
2023-11-13 01:49:57 +0900

    feat: 6개의 랜덤 번호 생성 기능을 추가한다

---
+++ src/main/kotlin/lotto/LottoTicket.kt
@@ -0,0 +1,16 @@
+package lotto
+
+data class LottoTicket private constructor(val numbers: List<Int>) {
+
+    companion object {
+        private const val MINIMUM_NUMBER = 1
+        private const val MAXIMUM_NUMBER = 45
+        private const val MAIN_LOTTO_NUMBERS_COUNT = 6
+        private val LOTTO_NUMBERS = (MINIMUM_NUMBER..MAXIMUM_NUMBER).toList()
+
+        fun generate(): LottoTicket {
+            val lottoNumbers = LOTTO_NUMBERS.shuffled().take(MAIN_LOTTO_NUMBERS_COUNT).sorted()
+            return LottoTicket(lottoNumbers)
+        }
+    }
+}
