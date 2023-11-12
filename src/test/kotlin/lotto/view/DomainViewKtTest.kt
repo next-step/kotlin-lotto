8f2be7edd1e28a03248219f8a09fdd3fbd1953b3
korjun1993 <corojoon93@naver.com>
2023-11-13 02:40:54 +0900

    test: step3 - 로또 게임결과의 상태를 출력하면 2등이 몇명인지 출력된다.

--- src/test/kotlin/lotto/view/DomainViewKtTest.kt
+++ src/test/kotlin/lotto/view/DomainViewKtTest.kt
@@ -2,11 +2,19 @@ package lotto.view
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import lotto.domain.LottoGameResult
 import lotto.domain.LottoNumbers
+import lotto.domain.LottoReward
 
 class DomainViewKtTest : FunSpec({
     test("로또의 상태를 출력하면 부여받은 숫자 6개를 출력한다.") {
         val lotto = LottoNumbers(setOf(10, 20, 17, 45, 15, 6))
         lotto.state() shouldBe "[10, 20, 17, 45, 15, 6]"
+    }
+
+    test("로또 게임결과의 상태를 출력하면 2등이 몇명인지 출력된다.") {
+        val lottoGameResult = LottoGameResult(totalPrice = 3000, rewards = listOf(LottoReward.FIRST, LottoReward.SECOND))
+        lottoGameResult.state() shouldContain "5개 일치, 보너스 볼 일치 (30000000원) - 1개"
     }
 })
