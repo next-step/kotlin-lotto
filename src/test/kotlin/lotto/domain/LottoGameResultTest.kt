637fa730d5833c3ce0d6171a6d4375e1cd983544
korjun1993 <corojoon93@naver.com>
2023-11-12 14:42:49 +0900

    fix: step2 - 미통과 테스트코드 수정

--- src/test/kotlin/lotto/domain/LottoGameResultTest.kt
+++ src/test/kotlin/lotto/domain/LottoGameResultTest.kt
@@ -2,6 +2,7 @@ package lotto.domain
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import lotto.view.state
 
 class LottoGameResultTest : FunSpec({
     test("로또 게임의 수익률은 `수익금 / 원금` 으로 계산된다.") {
@@ -26,7 +27,7 @@ class LottoGameResultTest : FunSpec({
             totalPrice = 3000,
             rewards = listOf(LottoReward.FIRST, LottoReward.SECOND)
         )
-        lottoGameResult.toString() shouldBe """
+        lottoGameResult.state() shouldBe """
             3개 일치 (5000원) - 0개
             4개 일치 (50000원) - 0개
             5개 일치 (1500000원) - 1개
