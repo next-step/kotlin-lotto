2a29c0d11c71d63af980279295e96f6066d97a5c
seongmo-jeon <oeeen3@gmail.com>
2023-11-12 17:00:22 +0900

    refactor: 엘비스 연산자 대신에 require 사용

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -4,7 +4,9 @@ object InputView {
     fun getPurchaseAmount(): Int {
         println("구입 금액을 입력해 주세요.")
         val input = readlnOrNull()
-        return input?.toIntOrNull() ?: throw IllegalArgumentException("숫자만 입력 가능합니다.")
+        val parsedInput = input?.toIntOrNull()
+        require(parsedInput != null) { "숫자만 입력 가능합니다." }
+        return parsedInput
     }
 
     fun getLastWinningNumbers(): String {
