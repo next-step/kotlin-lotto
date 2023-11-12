771511c13154527a001e69db9eb9f344284988ff
JunHyun Lee <dlwnsgus777@naver.com>
2023-11-12 11:31:14 +0900

    feat: 당첨 번호와 일치하는 로또 갯수 구하는 기능 개발

---
+++ src/test/kotlin/lotto/LottoValidatorTest.kt
@@ -0,0 +1,103 @@
+package lotto
+
+import io.kotest.matchers.shouldBe
+import lotto.domain.Lotto
+import lotto.domain.LottoNumber
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+object LottoValidator {
+    fun validateWinningNumberAndUserLotto(
+        winningNumber: List<LottoNumber>,
+        userLotto: Lotto): Int {
+        val lottoNumber = userLotto.getNumbers()
+        var count: Int = 0
+        lottoNumber.forEach {
+            if (winningNumber.contains(it)) {
+                count++
+            }
+        }
+
+        return count
+    }
+}
+
+class LottoValidatorTest {
+
+    @ParameterizedTest
+    @MethodSource("getLottoData")
+    fun `전달받은 로또와 당첨 번호를 비교해 일치한 갯수를 반환한다`(userLottoNumber: List<LottoNumber>, expected: Int) {
+        val winningNumber = listOf(
+            LottoNumber(6),
+            LottoNumber(5),
+            LottoNumber(4),
+            LottoNumber(3),
+            LottoNumber(2),
+            LottoNumber(1),
+        )
+        val userLotto = Lotto(userLottoNumber)
+        val matchingCount: Int =  LottoValidator.validateWinningNumberAndUserLotto(winningNumber, userLotto)
+
+        matchingCount shouldBe expected
+    }
+
+    companion object {
+        @JvmStatic
+        fun getLottoData(): List<Arguments> {
+            return listOf(
+                Arguments.of(
+                    listOf(
+                        LottoNumber(6),
+                        LottoNumber(5),
+                        LottoNumber(4),
+                        LottoNumber(3),
+                        LottoNumber(2),
+                        LottoNumber(1),
+                    ), 6
+                ),
+                Arguments.of(
+                    listOf(
+                        LottoNumber(7),
+                        LottoNumber(5),
+                        LottoNumber(4),
+                        LottoNumber(3),
+                        LottoNumber(2),
+                        LottoNumber(1),
+                    ), 5
+                ),
+                Arguments.of(
+                    listOf(
+                        LottoNumber(7),
+                        LottoNumber(8),
+                        LottoNumber(4),
+                        LottoNumber(3),
+                        LottoNumber(2),
+                        LottoNumber(1),
+                    ), 4
+                ),
+                Arguments.of(
+                    listOf(
+                        LottoNumber(7),
+                        LottoNumber(8),
+                        LottoNumber(9),
+                        LottoNumber(3),
+                        LottoNumber(2),
+                        LottoNumber(1),
+                    ), 3
+                ),
+                Arguments.of(
+                    listOf(
+                        LottoNumber(7),
+                        LottoNumber(8),
+                        LottoNumber(9),
+                        LottoNumber(10),
+                        LottoNumber(2),
+                        LottoNumber(1),
+                    ), 2
+                )
+            )
+        }
+    }
+}
