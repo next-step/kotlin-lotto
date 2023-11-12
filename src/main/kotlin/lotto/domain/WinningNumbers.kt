0a882805ddd36854492fe784d921395c40cca4a3
korjun1993 <corojoon93@naver.com>
2023-11-13 00:16:54 +0900

    feat: step3 - 당첨번호는 6자리 번호와 보너스 번호를 갖는다.

--- src/main/kotlin/lotto/domain/WinningNumbers.kt
+++ src/main/kotlin/lotto/domain/WinningNumbers.kt
@@ -1,4 +1,7 @@
 package lotto.domain
 
 data class WinningNumbers(val numbers: LottoNumbers, val bonusNumber: BonusNumber) {
+    init {
+        require(bonusNumber notIn numbers) { "보너스 번호와 당첨 번호는 중복이 불가능합니다." }
+    }
 }
--- src/test/kotlin/lotto/domain/WinningNumbersTest.kt
+++ src/test/kotlin/lotto/domain/WinningNumbersTest.kt
@@ -1,7 +1,9 @@
 package lotto.domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import java.lang.RuntimeException
 
 class WinningNumbersTest : FunSpec({
     test("당첨번호는 6자리 번호와 보너스 번호를 갖는다.") {
@@ -11,5 +13,11 @@ class WinningNumbersTest : FunSpec({
 
         winningNumbers.numbers shouldBe lottoNumbers
         winningNumbers.bonusNumber shouldBe bonusNumber
+    }
+
+    test("당첨번호의 6자리 번호와 보너스번호가 중복되면 예외가 발생한다.") {
+        val lottoNumbers = LottoNumbers(setOf(1, 2, 3, 4, 5, 10))
+        val bonusNumber = BonusNumber(10)
+        shouldThrow<RuntimeException> { WinningNumbers(lottoNumbers, bonusNumber) }
     }
 })
