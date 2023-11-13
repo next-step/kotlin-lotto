4eee5ba72b9f159128b91651ee959da575ce75cc
LAPTOP-A8710DGI\kdh85 <modudamba@naver.com>
2023-11-13 14:41:36 +0900

    step2: 로또(자동) 코드컨밴션 미적용된 부분 수정

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -13,6 +13,6 @@ class LottoNumber private constructor(
             (MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER).associateWith(::LottoNumber)
 
         fun from(number: Int) = LOTTO_NUMBERS[number]
-            ?: throw IllegalArgumentException("로또 번호는 1~45 사이의 양의 정수만 구성할 수 있습니다. 사용된 숫자 :${number}")
+            ?: throw IllegalArgumentException("로또 번호는 1~45 사이의 양의 정수만 구성할 수 있습니다. 사용된 숫자 :$number")
     }
 }
