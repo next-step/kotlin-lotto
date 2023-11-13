1d6db1d659182ebb361aa94eb6bb4dbf565b69e3
silvio.oivlis <silvio.oivlis@kakaocorp.com>
2023-11-13 19:21:59 +0900

    bug: typo fix

--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -5,6 +5,6 @@ import lotto.domain.LottoResult
 class ResultView() {
     fun printResult(lottoResult: LottoResult) {
         println("총 수익은 ${lottoResult.lottoProfitResult.totalPrize} 입니다.")
-        println("총 수익률은 ${lottoResult.profitRate}% 입니다.")
+        println("총 수익률은 ${lottoResult.profitRate} 입니다.")
     }
 }
