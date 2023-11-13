26365fb2a9a8a132423980d12bfe81a3f19e2904
ALbertIM0427 <guang7226961@naver.com>
2023-11-13 20:20:40 +0900

    refactor: 중복된 테스트 코드 삭제

--- src/test/kotlin/lotto/business/WinningLottoTicketExtractorTest.kt
+++ src/test/kotlin/lotto/business/WinningLottoTicketExtractorTest.kt
@@ -1,10 +1,7 @@
 package lotto.business
 
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
 
 class WinningLottoTicketExtractorTest {
     @Test
@@ -21,35 +18,5 @@ class WinningLottoTicketExtractorTest {
         assertThat(lottoWinningNumbers.winningLottoNumbers.lottoNumbers).usingRecursiveComparison()
             .isEqualTo(expectedLottoNumbers)
         assertThat(lottoWinningNumbers.bonusNumber).isEqualTo(bonusNumber)
-    }
-
-    @Test
-    fun `입력한 당첨 결과가 숫자가 아니면 예외를 던진다`() {
-        // given
-        val input = "1,2,3,4,5,6a"
-        val bonusNumber = LottoNumber(7)
-
-        // when, then
-        assertThatThrownBy { LottoWinningTicketExtractor.extract(input, bonusNumber) }
-            .isInstanceOf(IllegalArgumentException::class.java)
-            .message().isEqualTo("당첨 번호는 숫자여야 합니다.")
-    }
-
-    @ParameterizedTest
-    @CsvSource(
-        value = ["1,2,3,4,5,6:6", "1,2,3,4,5,7:5", "1,2,3,4,7,8:4", "1,2,3,7,8,9:3", "1,2,7,8,9,10:2", "1,7,8,9,10,11:1", "7,8,9,10,11,12:0"],
-        delimiter = ':'
-    )
-    fun `로또번호 리스트를 비교하여 일치하는 갯수를 반환한다`(input: String, expected: Int) {
-        // given
-        val lottoNumber = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber(it) }.toSet()
-        val lottoTicket = LottoTicket(lottoNumber)
-        val targetLottoNumbers = input.split(",").map { LottoNumber(it.toInt()) }
-
-        // when
-        val matchCount = lottoTicket.matchCount(targetLottoNumbers)
-
-        // then
-        assertThat(matchCount).isEqualTo(expected)
     }
 }
