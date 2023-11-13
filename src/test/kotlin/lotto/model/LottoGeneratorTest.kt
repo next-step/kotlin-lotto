b9da245b547944d931f617cea681f7dd26306188
stevy.sung <stevy.sung@kakaocorp.com>
2023-11-13 18:44:35 +0900

    test: 테스트 코드 작성

--- src/test/kotlin/lotto/model/LottoGeneratorTest.kt
+++ src/test/kotlin/lotto/model/LottoGeneratorTest.kt
@@ -5,10 +5,10 @@ import io.kotest.matchers.shouldBe
 
 class LottoGeneratorTest : BehaviorSpec({
     Given("로또 생성기") {
-        When("10개의 로또 생성 했을 때") {
-            val tickets = LottoGenerator.generateTickets(10) { listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) }
-            Then("10개의 로또 티켓이 생성된다") {
-                tickets.size shouldBe 10
+        When("10개의 로또를 구입하기를 원하는데 2장이 수동일 때") {
+            val tickets = LottoGenerator.generateTickets(10, 2) { listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) }
+            Then("8개의 자동 로또 티켓이 생성된다") {
+                tickets.size shouldBe 8
             }
         }
     }
