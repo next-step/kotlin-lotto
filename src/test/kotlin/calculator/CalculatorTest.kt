ebb6f7818a4a91eb594072a3c1eafe28b5d45a13
baek0318 <peachberry0318@gmail.com>
2023-11-13 18:14:43 +0900

    step1 : 적절한 곳에서 에러와 빈 문자열처리에 대한 테스트

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -1,7 +1,9 @@
 package calculator
 
 import caclulator.Calculator
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.*
 import io.kotest.matchers.shouldBe
 
 class CalculatorTest: StringSpec({
@@ -25,4 +27,23 @@ class CalculatorTest: StringSpec({
         val result = Calculator.calculate("//;\\n1;2;3")
         result shouldBe 6
     }
+
+    "빈 문자열 또는 null을 입력할 경우 0을 반환해야 한다." {
+        forAll(
+            rows = arrayOf(
+                Row2(null, 0),
+                Row2("", 0)
+            ),
+        ) { input, expected ->
+            val result = Calculator.calculate(input)
+            result shouldBe expected
+        }
+    }
+
+    "음수를 전달할 경우 RuntimeException 예외가 발생해야 한다." {
+        shouldThrow<RuntimeException> {
+            Calculator.calculate("-1:2:-3")
+        }
+    }
+
 })
