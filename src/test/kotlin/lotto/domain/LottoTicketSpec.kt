abf2b37f2f5780172dec8f850e956518b6ba536c
bongbong <kbk5190@naver.com>
2023-11-14 08:01:00 +0900

    test: 테스트용 로또 티켓을 만들 경우 Mock 객체 대신 직접 객체 생성

--- src/test/kotlin/lotto/domain/LottoTicketSpec.kt
+++ src/test/kotlin/lotto/domain/LottoTicketSpec.kt
@@ -6,12 +6,14 @@ import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class LottoTicketSpec : FunSpec({
+    val oneToSixLottoNumber = LottoNumber(listOf(1, 2, 3, 4, 5, 6))
+
     context("로또 가격 계산") {
         test("가격이 주어지면 구입한 총 가격이 계산된다") {
             forAll(
-                row(listOf(LottoNumber(listOf(1, 2, 3, 4, 5, 6))),1000),
-                row(listOf(LottoNumber(listOf(1, 2, 3, 4, 5, 6)), LottoNumber(listOf(2, 3, 4, 5, 6, 7))),2000),
-                row(listOf(LottoNumber(listOf(1, 2, 3, 4, 5, 6)), LottoNumber(listOf(2, 3, 4, 5, 6, 7)), LottoNumber(listOf(3, 4, 5, 6, 7, 8))), 3000),
+                row(listOf(oneToSixLottoNumber), 1000),
+                row(listOf(oneToSixLottoNumber, oneToSixLottoNumber), 2000),
+                row(listOf(oneToSixLottoNumber, oneToSixLottoNumber, oneToSixLottoNumber), 3000),
             ) { lottoNumbers, expectAmount ->
                 val ticketPrice = Amount(1000)
                 val ticket = LottoTicket(lottoNumbers)
@@ -23,35 +25,61 @@ class LottoTicketSpec : FunSpec({
         }
     }
 
-    context("티켓의 수를 센다") {
-        val count = 3
-        val ticket = LottoTicket(List(count) { LottoMock.createTicket() })
-
-        val result = ticket.count
-
-        result shouldBe count
+    context("티켓 장수 구하기") {
+        forAll(
+            row(LottoTicket(listOf(oneToSixLottoNumber)), 1),
+            row(LottoTicket(listOf(oneToSixLottoNumber, oneToSixLottoNumber)), 2),
+            row(LottoTicket(listOf(oneToSixLottoNumber, oneToSixLottoNumber, oneToSixLottoNumber)), 3),
+        ) { ticket, count ->
+            test("$count 개가 들어 있는 티켓에서 장수를 세면 $count 가 반환된다") {
+                ticket.count shouldBe count
+            }
+        }
     }
 
-    context("티켓의 결과를 도출") {
-        test("로또 결과가 생성된다") {
-            val winningLotto = WinningLotto(LottoNumber(listOf(1, 2, 3, 4, 5, 6)), 7)
-            val expect = mapOf(
-                LottoRank.FIRST to 1,
-                LottoRank.SECOND to 1,
-                LottoRank.THIRD to 2,
-                LottoRank.FOURTH to 2,
-                LottoRank.FIFTH to 1,
-                LottoRank.MISS to 1,
-            )
-            val ticket = expect.flatMap { (rank, ticketCount) ->
-                List(ticketCount) {
-                    LottoMock.createTicket(winningLotto, rank.countOfMatch, rank.isWithBonusMatch)
-                }
-            }.let(::LottoTicket)
+    context("로또 티켓 등수별 당첨 장수 생성") {
+        val bonusNumber = 7
+        val winningLotto = WinningLotto(LottoNumber(listOf(1, 2, 3, 4, 5, 6)), bonusNumber)
+        val nonMatchedLotto = LottoNumber(listOf(101, 102, 103, 104, 105, 106))
+        val oneMatchedLotto = LottoNumber(listOf(1, 102, 103, 104, 105, 106))
+        val threeMatchedLotto = LottoNumber(listOf(1, 2, 3, 104, 105, 106))
+        val fourMatchedLotto = LottoNumber(listOf(1, 2, 3, 4, 105, 106))
+        val fourMatchedLottoWithBonus = LottoNumber(listOf(1, 2, 3, 4, 105, bonusNumber))
+        val fiveMatchedLotto = LottoNumber(listOf(1, 2, 3, 4, 5, 106))
+        val fiveMatchedLottoWithBonus = LottoNumber(listOf(1, 2, 3, 4, 5, bonusNumber))
+        val allMatchedLotto = LottoNumber(listOf(1, 2, 3, 4, 5, 6))
+
+
+        test("한 장도 당첨되지 않음") {
+            val ticket = LottoTicket(listOf(nonMatchedLotto, oneMatchedLotto))
 
             val result = ticket determineResultBy winningLotto
 
-            result.value shouldBe expect
+            result.value shouldBe mapOf(LottoRank.MISS to 2)
+        }
+
+        test("1등 1장, 2등 1장, 3등 2장, 4등 3장, 5등 1장, 당첨 안된 5장") {
+            val ticket = LottoTicket(
+                listOf(
+                    allMatchedLotto,
+                    fiveMatchedLottoWithBonus,
+                    fiveMatchedLotto, fiveMatchedLotto,
+                    fourMatchedLotto, fourMatchedLotto, fourMatchedLottoWithBonus,
+                    threeMatchedLotto,
+                    oneMatchedLotto, oneMatchedLotto, oneMatchedLotto, oneMatchedLotto, nonMatchedLotto
+                )
+            )
+
+            val result = ticket determineResultBy winningLotto
+
+            result.value shouldBe mapOf(
+                LottoRank.FIRST to 1,
+                LottoRank.SECOND to 1,
+                LottoRank.THIRD to 2,
+                LottoRank.FOURTH to 3,
+                LottoRank.FIFTH to 1,
+                LottoRank.MISS to 5,
+            )
         }
     }
 })
