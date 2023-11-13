de80c7471a0111194c03e14ea5647fc58e668def
dong <dong.22@kakaoenterprise.com>
2023-11-14 00:46:58 +0900

    feat : 로또를 수동으로 번호를 입력받는 기능을 추가

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -2,14 +2,24 @@ package lotto.view
 
 import lotto.model.LottoNumber
 import lotto.model.LottoNumbers
+import lotto.model.PurchaseGames
 import lotto.model.WinningNumbers
 
 object InputView {
-    fun purchaseAmount(pricePerGame: Int): Int {
+    fun purchaseAmount(pricePerGame: Int): PurchaseGames {
         println("구입금액을 입력해 주세요.")
-        val purchaseGameCount: Int = readln().toInt() / pricePerGame
-        println("$purchaseGameCount 개를 구매했습니다.")
-        return purchaseGameCount
+        val totalPurchaseCount: Int = readln().toInt() / pricePerGame
+        println("수동으로 구매할 로또 수를 입력해 주세요.")
+        val manualCount: Int = readln().toInt()
+        println("수동으로 구매할 번호를 입력해 주세요.")
+        return PurchaseGames(totalPurchaseCount, manualCount, manualInput(manualCount))
+    }
+
+    private fun manualInput(count: Int): String {
+        return (1..count)
+            .asSequence()
+            .map { requireNotNull(readlnOrNull()) }
+            .joinToString(separator = "\n")
     }
 
     fun selectWinningNumbers(): WinningNumbers {
