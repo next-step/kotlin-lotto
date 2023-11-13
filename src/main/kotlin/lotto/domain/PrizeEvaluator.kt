17772ac110752f3892776b7444384043d4d10df7
moses.lee(이상후)/kakao <moses.lee@kakaocorp.com>
2023-11-14 00:47:03 +0900

    feat: ROI 계산 도메인 객체
    # step 2

--- src/main/kotlin/lotto/domain/PrizeEvaluator.kt
+++ src/main/kotlin/lotto/domain/PrizeEvaluator.kt
@@ -1,12 +1,13 @@
 package lotto.domain
 
 import lotto.domain.enums.RANK
-import lotto.dto.Ticket
-
+import lotto.dto.PurchaseAmount
+import lotto.dto.ROI
+import lotto.vo.LottoTicket
 
 object PrizeEvaluator {
-    fun evaluate(userTicket: Ticket, winningTicket: Ticket): Prize {
-        val prize = when (userTicket.countSameNumber(winningTicket)) {
+    fun evaluate(userLottoTicket: LottoTicket, winningLottoTicket: LottoTicket): Prize {
+        val prize = when (userLottoTicket.countSameNumber(winningLottoTicket)) {
             RANK.FOURTH.sameNumber -> RANK.FOURTH.prize
             RANK.THIRD.sameNumber -> RANK.THIRD.prize
             RANK.SECOND.sameNumber -> RANK.SECOND.prize
@@ -17,11 +18,17 @@ object PrizeEvaluator {
         return prize
     }
 
-    fun evaluateTotalPrize(tickets: List<Ticket>, winningTicket: Ticket): Prize {
-        return Prize(tickets.sumOf { ticket -> evaluate(ticket, winningTicket).value })
+    private fun evaluateTotalPrize(lottoTickets: List<LottoTicket>, winningLottoTicket: LottoTicket): Prize {
+        return Prize(lottoTickets.sumOf { ticket -> evaluate(ticket, winningLottoTicket).value })
     }
 
-    fun calculateROI(prize: Prize, ticketPrice: Int): Double {
-        return prize.value.toDouble() / ticketPrice
+    fun calculateROI(
+        lottoTickets: List<LottoTicket>,
+        winningLottoTicket: LottoTicket,
+        purchaseAmount: PurchaseAmount
+    ): ROI {
+        val prize = evaluateTotalPrize(lottoTickets, winningLottoTicket).value.toDouble()
+
+        return ROI(prize / purchaseAmount.amount)
     }
 }
--- src/test/kotlin/lotto/PrizeEvaluatorTest.kt
+++ src/test/kotlin/lotto/PrizeEvaluatorTest.kt
@@ -2,7 +2,9 @@ package lotto
 
 import lotto.domain.Prize
 import lotto.domain.PrizeEvaluator
-import lotto.dto.Ticket
+import lotto.dto.PurchaseAmount
+import lotto.vo.LottoNumber
+import lotto.vo.LottoTicket
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -11,14 +13,14 @@ class PrizeEvaluatorTest {
     @Test
     fun `번호가 3자리 같다면 5000원을 상금으로 탄다`() {
         // given
-        val userNumber = listOf(1, 2, 3, 4, 5, 6)
-        val winningNumber = listOf(1, 2, 3, 7, 8, 9)
+        val userNumber = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber(it) }
+        val winningNumber = listOf(1, 2, 3, 7, 8, 9).map { LottoNumber(it) }
 
-        val userTicket = Ticket(userNumber)
-        val winningTicket = Ticket(winningNumber)
+        val userLottoTicket = LottoTicket(userNumber)
+        val winningLottoTicket = LottoTicket(winningNumber)
 
         // when
-        val prize = PrizeEvaluator.evaluate(userTicket, winningTicket)
+        val prize = PrizeEvaluator.evaluate(userLottoTicket, winningLottoTicket)
 
         // then
         assertEquals(Prize(5000), prize)
@@ -27,14 +29,14 @@ class PrizeEvaluatorTest {
     @Test
     fun `번호가 4자리 같다면 50000원을 상금으로 탄다`() {
         // given
-        val userNumber = listOf(1, 2, 3, 4, 5, 6)
-        val winningNumber = listOf(1, 2, 3, 4, 8, 9)
+        val userNumber = listOf(1, 2, 3, 4, 5, 6).map { LottoNumber(it) }
+        val winningNumber = listOf(1, 2, 3, 4, 8, 9).map { LottoNumber(it) }
 
-        val userTicket = Ticket(userNumber)
-        val winningTicket = Ticket(winningNumber)
+        val userLottoTicket = LottoTicket(userNumber)
+        val winningLottoTicket = LottoTicket(winningNumber)
 
         // when
-        val prize = PrizeEvaluator.evaluate(userTicket, winningTicket)
+        val prize = PrizeEvaluator.evaluate(userLottoTicket, winningLottoTicket)
 
         // then
         assertEquals(Prize(50000), prize)
@@ -43,17 +45,17 @@ class PrizeEvaluatorTest {
     @Test
     fun `2000 원으로 2개의 로또를 샀을때, 3개 일치하는 로또가 1개 있다면 수익률은 250% 입니다`() {
         // given
-        val userNumber1 = listOf(1, 2, 3, 10, 11, 12)
-        val userNumber2 = listOf(7, 8, 9, 10, 11, 12)
-        val winningNumber = listOf(1, 2, 3, 30, 31, 32)
+        val userNumber1 = listOf(1, 2, 3, 10, 11, 12).map { LottoNumber(it) }
+        val userNumber2 = listOf(7, 8, 9, 10, 11, 12).map { LottoNumber(it) }
+        val winningNumber = listOf(1, 2, 3, 30, 31, 32).map { LottoNumber(it) }
 
-        val userTickets = listOf(Ticket(userNumber1), Ticket(userNumber2))
-        val winningTicket = Ticket(winningNumber)
+        val userLottoTickets = listOf(LottoTicket(userNumber1), LottoTicket(userNumber2))
+        val winningLottoTicket = LottoTicket(winningNumber)
 
         // when
-        val roi = PrizeEvaluator.calculateROI(PrizeEvaluator.evaluateTotalPrize(userTickets, winningTicket), 2000)
+        val roi = PrizeEvaluator.calculateROI(userLottoTickets, winningLottoTicket, PurchaseAmount(2000))
 
         // then
-        assertEquals(2.5, roi)
+        assertEquals(2.5, roi.value)
     }
 }
