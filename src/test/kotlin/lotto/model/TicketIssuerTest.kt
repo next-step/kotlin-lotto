d40d3ad1239014c175c22ab0142c7876095ae846
dong <dong.22@kakaoenterprise.com>
2023-11-14 00:53:05 +0900

    refactor : 클래스 이름 변경으로 인한 테스트코드 반영

--- src/test/kotlin/lotto/model/TicketIssuerTest.kt
+++ src/test/kotlin/lotto/model/TicketIssuerTest.kt
@@ -1,10 +1,10 @@
-package lotto.app
+package lotto.model
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
-import lotto.model.TicketIssuer
 
-class LottoTest : StringSpec({
+class TicketIssuerTest : StringSpec({
+
 //    "구매할 Game 의 수를 입력받아 로또 Game 을 발급해야 한다" {
 //        val actual = TicketIssuer.autoIssue(10)
 //        actual.games shouldHaveSize 10
