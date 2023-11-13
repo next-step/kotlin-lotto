af7e79213f7a3761859ff0e69d036117765d54c0
dong <dong.22@kakaoenterprise.com>
2023-11-14 01:27:39 +0900

    refactor : 상수를 추출한다

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,18 +1,30 @@
 package lotto.view
 
+import lotto.model.Game
 import lotto.model.LottoNumber
 import lotto.model.LottoNumbers
 import lotto.model.PurchaseGames
 import lotto.model.WinningNumbers
 
 object InputView {
-    fun purchaseAmount(pricePerGame: Int): PurchaseGames {
+
+    const val LOTTO_NUMBER_DELIMITER = ","
+    private const val GAME_DELIMITER = "\n"
+
+    fun purchaseAmount(priceOfGame: Int): PurchaseGames {
         println("구입금액을 입력해 주세요.")
-        val totalPurchaseCount: Int = readln().toInt() / pricePerGame
+        val totalPurchaseCount: Int = readln().toInt() / priceOfGame
         println("수동으로 구매할 로또 수를 입력해 주세요.")
         val manualCount: Int = readln().toInt()
         println("수동으로 구매할 번호를 입력해 주세요.")
-        return PurchaseGames(totalPurchaseCount, manualCount, manualInput(manualCount))
+        return PurchaseGames(totalPurchaseCount, manualCount, manualIssue(manualInput(manualCount)))
+    }
+
+    private fun manualIssue(manualTicketingInput: String): List<Game> {
+        return manualTicketingInput.split(GAME_DELIMITER)
+            .asSequence()
+            .map { it.stringToGame() }
+            .toList()
     }
 
     private fun manualInput(count: Int): String {
@@ -24,9 +36,18 @@ object InputView {
 
     fun selectWinningNumbers(): WinningNumbers {
         println("지난 주 당첨 번호를 입력해 주세요.")
-        val winningNumbers = LottoNumbers((readlnOrNull() ?: "").split(",").map { it.toInt() })
+        val winningNumbers = LottoNumbers((readlnOrNull() ?: "").split(LOTTO_NUMBER_DELIMITER).map { it.toInt() })
         println("보너스 볼을 입력해 주세요.")
         val bonusNumber = LottoNumber((readlnOrNull() ?: "").toInt())
         return WinningNumbers(winningNumbers, bonusNumber)
     }
+}
+
+private fun String.stringToGame(): Game {
+    return Game(
+        LottoNumbers(
+            this.split(InputView.LOTTO_NUMBER_DELIMITER)
+                .map { it.toInt() }
+        )
+    )
 }
