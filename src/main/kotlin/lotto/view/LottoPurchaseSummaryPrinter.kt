ce4c3d709822319a892362c92899081456ade9bc
ALbertIM0427 <guang7226961@naver.com>
2023-11-13 21:29:31 +0900

    refactor: 기능 flow 변경

--- src/main/kotlin/lotto/view/LottoPurchaseSummaryPrinter.kt
+++ src/main/kotlin/lotto/view/LottoPurchaseSummaryPrinter.kt
@@ -3,8 +3,8 @@ package lotto.view
 import lotto.business.LottoTicket
 
 object LottoPurchaseSummaryPrinter {
-    fun print(lottoTickets: List<LottoTicket>) {
-        println("${lottoTickets.size}개를 구매했습니다.")
+    fun print(manualTicketCount: Int, lottoTickets: List<LottoTicket>) {
+        println("수동으로 ${manualTicketCount}장, 자동으로 ${lottoTickets.size}개를 구매했습니다.")
         lottoTickets.forEach { it: LottoTicket -> println(it.lottoNumbers.map { it.number.toString() }) }
         println()
     }
