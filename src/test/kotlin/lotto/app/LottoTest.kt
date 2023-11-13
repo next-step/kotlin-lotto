5d5ca6c475bf43f082238194888b281b19ac40ab
dong <dong.22@kakaoenterprise.com>
2023-11-14 00:48:14 +0900

    refactor : 클래스 이름 변경을 반영함

--- src/test/kotlin/lotto/app/LottoTest.kt
+++ src/test/kotlin/lotto/app/LottoTest.kt
@@ -1,17 +1,16 @@
 package lotto.app
 
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
-import lotto.model.Issuer
+import lotto.model.TicketIssuer
 
 class LottoTest : StringSpec({
-    "구매할 Game 의 수를 입력받아 로또 Game 을 발급해야 한다" {
-        val actual = Issuer.transaction(10)
-        actual.games shouldHaveSize 10
-    }
+//    "구매할 Game 의 수를 입력받아 로또 Game 을 발급해야 한다" {
+//        val actual = TicketIssuer.autoIssue(10)
+//        actual.games shouldHaveSize 10
+//    }
 
     "구매할 로또 Game 하나의 가격은 1000 이다" {
-        Issuer.pricePerGame() shouldBe 1000
+        TicketIssuer.pricePerGame() shouldBe 1000
     }
 })
--- src/test/kotlin/lotto/model/RoundTest.kt
+++ src/test/kotlin/lotto/model/RoundTest.kt
@@ -9,7 +9,7 @@ class RoundTest : StringSpec({
     val game2: Game = Game(LottoNumbers(7, 8, 9, 10, 11, 12))
     val game3: Game = Game(LottoNumbers(1, 11, 21, 31, 41, 22))
     val game4: Game = Game(LottoNumbers(2, 22, 32, 42, 43, 44))
-    val tickets: Tickets = Tickets(listOf(game1, game2, game3, game4))
+    val lottoTicket: LottoTicket = LottoTicket(listOf(game1, game2, game3, game4))
 
     val matched6: WinningNumbers = WinningNumbers(LottoNumbers(1, 2, 3, 4, 5, 6), LottoNumber(22))
     val matched5: WinningNumbers = WinningNumbers(LottoNumbers(2, 3, 4, 5, 6, 37), LottoNumber(22))
@@ -17,7 +17,7 @@ class RoundTest : StringSpec({
     val matched3: WinningNumbers = WinningNumbers(LottoNumbers(4, 5, 6, 37, 38, 39), LottoNumber(22))
 
     "6개의 로또 번호가 일치 하면 1등 당첨자로 집계 해야 한다" {
-        val actual = tickets.winnerAggregate(matched6)
+        val actual = lottoTicket.winnerAggregate(matched6)
         actual.countOfRank(Rank.FIRST) shouldBe 1
         actual.countOfRank(Rank.SECOND) shouldBe 0
         actual.countOfRank(Rank.THIRD) shouldBe 0
@@ -26,7 +26,7 @@ class RoundTest : StringSpec({
     }
 
     "5개의 로또 번호가 일치 하고 보너스볼이 일치하면 2등 당첨자로 집계 해야 한다" {
-        val r2 = Tickets(listOf(Game(LottoNumbers(1, 2, 3, 4, 5, 22))))
+        val r2 = LottoTicket(listOf(Game(LottoNumbers(1, 2, 3, 4, 5, 22))))
         val matched5withBonus: WinningNumbers = WinningNumbers(LottoNumbers(1, 2, 3, 4, 5, 6), LottoNumber(22))
         val actual = r2.winnerAggregate(matched5withBonus)
         actual.countOfRank(Rank.FIRST) shouldBe 0
@@ -37,7 +37,7 @@ class RoundTest : StringSpec({
     }
 
     "5개의 로또 번호가 일치 하고 보너스볼이 일치하지 않으면 3등 당첨자로 집계 해야 한다" {
-        val actual = tickets.winnerAggregate(matched5)
+        val actual = lottoTicket.winnerAggregate(matched5)
         actual.countOfRank(Rank.FIRST) shouldBe 0
         actual.countOfRank(Rank.SECOND) shouldBe 0
         actual.countOfRank(Rank.THIRD) shouldBe 1
@@ -46,7 +46,7 @@ class RoundTest : StringSpec({
     }
 
     "4개의 로또 번호가 일치 하면 4등 당첨자로 집계 해야 한다" {
-        val actual = tickets.winnerAggregate(matched4)
+        val actual = lottoTicket.winnerAggregate(matched4)
         actual.countOfRank(Rank.FIRST) shouldBe 0
         actual.countOfRank(Rank.SECOND) shouldBe 0
         actual.countOfRank(Rank.THIRD) shouldBe 0
@@ -55,7 +55,7 @@ class RoundTest : StringSpec({
     }
 
     "3개의 로또 번호가 일치 하면 5등 당첨자로 집계 해야 한다" {
-        val actual = tickets.winnerAggregate(matched3)
+        val actual = lottoTicket.winnerAggregate(matched3)
         actual.countOfRank(Rank.FIRST) shouldBe 0
         actual.countOfRank(Rank.SECOND) shouldBe 0
         actual.countOfRank(Rank.THIRD) shouldBe 0
