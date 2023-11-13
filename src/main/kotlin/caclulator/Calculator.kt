f11fd1c247ceebb8cd3fca295226531c3a3b05c3
baek0318 <peachberry0318@gmail.com>
2023-11-13 18:15:03 +0900

    step1 : Parser로 인한 Calculator 리팩토링

--- src/main/kotlin/caclulator/Calculator.kt
+++ src/main/kotlin/caclulator/Calculator.kt
@@ -2,19 +2,12 @@ package caclulator
 
 object Calculator {
 
-    fun calculate(input: String): Int {
-        if (input.startsWith("//")) {
-            val split = input.split("\\n")
-            val custom = split[0]
-            val expr = split[1]
-            val operator = custom.split("//")[1]
-            return plus(expr, arrayOf(operator))
+    fun calculate(input: String?): Int {
+        val parsed = Parser.parse(input)
+        if (parsed.isEmpty()) {
+            return 0
         }
-        return plus(input, arrayOf(",", ":"))
+        return parsed.reduce { acc, i -> acc + i }
     }
 
-    private fun plus(input: String, operator: Array<String>): Int =
-        input.split(delimiters = operator)
-            .map { it.toInt() }
-            .reduce { acc, i -> acc + i }
 }
