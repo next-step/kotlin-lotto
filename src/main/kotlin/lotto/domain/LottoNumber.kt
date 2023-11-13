5a461cb922af995640e13474d3bd662b312c6db3
LAPTOP-A8710DGI\kdh85 <modudamba@naver.com>
2023-11-13 14:13:18 +0900

    step2: 로또(자동) LottoNumber 리팩토링 * 에러메세지에 오류가 발생하게 된 숫자를 노출하여 조금 더 친절할 수 있도록 개선. * 모호한 표현인 LOTTO_NUMBER_CASH를 명확하게 로또 숫자들의 모음으로 수정.

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -9,10 +9,10 @@ class LottoNumber private constructor(
     companion object {
         private const val MINIMUM_LOTTO_NUMBER: Int = 1
         private const val MAXIMUM_LOTTO_NUMBER: Int = 45
-        private val LOTTO_NUMBER_CASH: Map<Int, LottoNumber> =
+        private val LOTTO_NUMBERS: Map<Int, LottoNumber> =
             (MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER).associateWith(::LottoNumber)
 
-        fun from(number: Int) = LOTTO_NUMBER_CASH[number]
-            ?: throw IllegalArgumentException("로또 번호는 1~45 사이의 양의 정수만 구성할 수 있습니다.")
+        fun from(number: Int) = LOTTO_NUMBERS[number]
+            ?: throw IllegalArgumentException("로또 번호는 1~45 사이의 양의 정수만 구성할 수 있습니다. 사용된 숫자 :${number}")
     }
 }
