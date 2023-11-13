bae522dfd2b832c31069a3cc061e8d7068d46d83
moses.lee(이상후)/kakao <moses.lee@kakaocorp.com>
2023-11-14 00:44:53 +0900

    fix: main entrance 수정
    # step 2

--- src/main/kotlin/lotto/AutoLotto.kt
+++ src/main/kotlin/lotto/AutoLotto.kt
@@ -1,47 +1,17 @@
 package lotto
 
-import lotto.domain.PrizeEvaluator.evaluateTotalPrize
-import lotto.dto.Ticket
-import lotto.view.InputView.readNumbers
-import lotto.view.InputView.readPrice
-import lotto.view.OutputView.writeROI
-import lotto.view.OutputView.writeSingleOutput
+import lotto.domain.PrizeEvaluator
+import lotto.view.InputView
+import lotto.view.OutputView
+import lotto.vo.LottoTicket
 
-class AutoLotto private constructor(private val chance: Int) {
-    var tickets: ArrayList<Ticket> = arrayListOf()
-
-    init {
-        repeat(chance) {
-            val numbers = (1..45).shuffled().take(6).sorted()
-            val ticket = Ticket(numbers)
-            writeSingleOutput(numbers.toString())
-
-            tickets.add(ticket)
-        }
-    }
-
-    fun getWinningTicket(input: String): Ticket {
-        val numbers = input.replace("\\s".toRegex(), "").split(",").map { it.toInt() }
-        writeSingleOutput(numbers.toString())
-        return Ticket(numbers)
-    }
-
-    companion object {
-        fun play(money: Int): AutoLotto {
-            val chance = money / 1000
-            writeSingleOutput("$chance 개를 구매했습니다.")
-
-            return AutoLotto(chance)
-        }
-    }
-}
+class AutoLotto
 
 fun main() {
-    val inputPrice = readPrice()
-    val autoLotto = AutoLotto.play(inputPrice)
+    val inputPrice = InputView.readPrice()
 
-    val inputNumber = readNumbers()
-    val winningTicket = autoLotto.getWinningTicket(inputNumber)
+    val lottoTickets = AutoLottoController.play(inputPrice)
+    val winningTicket = LottoTicket(InputView.inputWinningNumbers().winningNumber)
 
-    writeROI(evaluateTotalPrize(autoLotto.tickets, winningTicket), inputPrice)
+    OutputView.write(PrizeEvaluator.calculateROI(lottoTickets, winningTicket, inputPrice))
 }
