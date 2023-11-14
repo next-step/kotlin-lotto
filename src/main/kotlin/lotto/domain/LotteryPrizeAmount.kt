9de0efe67885601efcc3301a0dbec9637670c23b
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-14 17:22:23 +0900

    feat : LotteryPrizeAmount 에서 bonusBall Match 를 할 수 있도록 변경

--- src/main/kotlin/lotto/domain/LotteryPrizeAmount.kt
+++ src/main/kotlin/lotto/domain/LotteryPrizeAmount.kt
@@ -2,15 +2,18 @@ package lotto.domain
 
 enum class LotteryPrizeAmount(val winNum: Int, val prize: Int) {
     MISS(0, 0),
-    FOURTH(3, 5_000),
-    THIRD(4, 50_000),
-    SECOND(5, 1_500_000),
+    FIFTH(3, 5_000),
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
     FIRST(6, 2_000_000_000),
     ;
 
     companion object {
-        fun getWinningPrize(winNum: Int): LotteryPrizeAmount {
-            return values().find { it.winNum == winNum } ?: MISS
+        fun getWinningPrize(winNum: Int, bonusMatch: Boolean = false): LotteryPrizeAmount {
+            val lotteryPrizeAmount = values().find { it.winNum == winNum } ?: MISS
+            if (lotteryPrizeAmount == THIRD && bonusMatch) return SECOND
+            return lotteryPrizeAmount
         }
     }
 }
