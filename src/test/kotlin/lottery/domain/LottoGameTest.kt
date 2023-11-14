599941323a9236ec65f6e5af4526b9ae6b4e4281
Celina <luv.mins99@gmail.com>
2023-11-15 02:09:17 +0900

    test: 수동 로또 기능 관련 테스트 깨지는 부분 수정

--- src/test/kotlin/lottery/domain/LottoGameTest.kt
+++ src/test/kotlin/lottery/domain/LottoGameTest.kt
@@ -1,25 +1,25 @@
 package lottery.domain
 
-import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
 
 class LottoGameTest {
-    @ParameterizedTest(name = "구매 금액 {0}원을 입력하면 로또 {1}장이 생성된다")
-    @CsvSource("999,0", "1000,1", "1001,1", "3500,3")
-    fun `로또 구입 금액을 입력받아 로또 구매 장수를 생성한다`(price: Int, quantity: Int) {
-        val lottoGame = LottoGame(price)
+    @Test
+    fun `로또 금액과 로또 리스트를 통해 로또 게임을 생성한다`() {
+        val lottoMoney = LottoMoney.of(1000, 0)
+        val lottos = AutoLottoMachine(RandomNumberGenerator()).createLottos(1)
+        val lottoGame = LottoGame(lottoMoney, Lottos(lottos))
 
-        lottoGame.getLottos().size shouldBe quantity
+        lottoGame.shouldBeInstanceOf<LottoGame>()
     }
 
     @Test
     fun `지난 주 당첨 번호를 입력하여 결과 객체를 반환 받는다`() {
         val lottoNumbers = listOf(1, 2, 3, 4, 5, 6)
         val bonusNumber = 7
-        val lottoGame = LottoGame(1000, InputNumberGenerator(lottoNumbers))
+        val userLotto = Lottos(listOf(Lotto.of(lottoNumbers)))
+        val lottoMoney = LottoMoney.of(1000, 0)
+        val lottoGame = LottoGame(lottoMoney, userLotto)
         val result = lottoGame.getRanks(WinningLotto(lottoNumbers, bonusNumber))
 
         result.shouldBeInstanceOf<Ranks>()
--- src/test/kotlin/lottery/domain/LottosTest.kt
+++ src/test/kotlin/lottery/domain/LottosTest.kt
@@ -7,18 +7,17 @@ import org.junit.jupiter.api.Test
 class LottosTest {
 
     @Test
-    fun `로또 갯수와 로또 생성 규칙을 전달하여 로또 번호 리스트를 가지는 객체를 생성한다`() {
-        val result = Lottos(5, AutoNumbersGenerator())
+    fun `로또 번호 리스트를 가지는 객체를 생성한다`() {
+        val result = Lottos(AutoLottoMachine(RandomNumberGenerator()).createLottos(5))
 
         result.lottos shouldHaveSize 5
     }
 
     @Test
     fun `로또 번호 리스트를 가지고 있는 객체는 당첨 번호 정보를 가지는 로또 객체를 받아 매칭 결과를 리스트로 반환한다`() {
-        val lottoNumbers = listOf(1, 2, 3, 4, 5, 6)
-        val bonusNumber = 7
-        val lotto = Lottos(1, InputNumbersGenerator(lottoNumbers))
-        val result = lotto.matchLottos(WinningLotto(lottoNumbers, bonusNumber))
+        val lottoNumbers = listOf("1,2,3,4,5,6")
+        val winningLotto = WinningLotto(listOf(1, 2, 3, 4, 5, 6), 7)
+        val result = Lottos(ManualLottoMachine.createLottos(lottoNumbers)).matchLottos(winningLotto)
 
         result.shouldBeInstanceOf<List<Rank>>()
     }
