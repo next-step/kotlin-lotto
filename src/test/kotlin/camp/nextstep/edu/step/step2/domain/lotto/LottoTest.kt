a4b41cedfdc7853d7d2229afd7efcea1bd2193d2
Ueibin Kim <at365@naver.com>
2023-11-14 17:42:25 +0900

    refactor : Numbers 도메인 이름 변경과 숫자 생성 메서드 이름 변경을 통한 테스트코드 수정

--- src/test/kotlin/camp/nextstep/edu/step/step2/domain/lotto/LottoTest.kt
+++ src/test/kotlin/camp/nextstep/edu/step/step2/domain/lotto/LottoTest.kt
@@ -1,12 +1,11 @@
 package camp.nextstep.edu.step.step2.domain.lotto
 
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("로또 번호들은")
-class NumbersTest : BehaviorSpec({
+class LottoTest : BehaviorSpec({
 
     Given("로또 번호들이 주어지고") {
         val requestNumbers = listOf(
@@ -19,10 +18,10 @@ class NumbersTest : BehaviorSpec({
         )
 
         When("생성을 요청하면") {
-            val numbers = Numbers.ofNumbers(numbers = requestNumbers)
+            val lotto = Lotto(numbers = requestNumbers)
 
             Then("주어진 로또 번호들이 생성된다") {
-                numbers shouldBe Numbers.ofNumbers(
+                lotto shouldBe Lotto(
                     numbers = listOf(
                         Number(number = 1),
                         Number(number = 2),
@@ -36,35 +35,13 @@ class NumbersTest : BehaviorSpec({
         }
     }
 
-
-    Given("만약 중복인 숫자가 포함되어 있을 경우") {
-        val requestNumbers = listOf(
-            Number(number = 1),
-            Number(number = 2),
-            Number(number = 3),
-            Number(number = 4),
-            Number(number = 5),
-            Number(number = 5)
-        )
-
-        When("생성을 요청하면") {
-            val numbers = shouldThrow<IllegalArgumentException> {
-                Numbers.ofNumbers(numbers = requestNumbers)
-            }
-
-            Then("예외가 발생한다") {
-                numbers.message shouldBe "로또 번호는 중복될 수 없습니다."
-            }
-        }
-    }
-
     Given("로또의 번호들은") {
 
         When("새로운 번호 생성 요청이 들어오면") {
-            val numbers = Numbers.createNumbers()
+            val lotto = Lotto.generateAutoNumbers()
 
             Then("6개의 번호가 생성된다") {
-                numbers.size shouldBe 6
+                lotto.numbers.size shouldBe 6
             }
         }
     }
