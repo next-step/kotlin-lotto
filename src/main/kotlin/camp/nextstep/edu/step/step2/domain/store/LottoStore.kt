125967f6010c9e6df355a16504e32843ac7039c0
Ueibin Kim <at365@naver.com>
2023-11-14 17:36:39 +0900

    refactor : 로또 상점 ( LottoStore ) 내 결과 확인 함수 로직 수정 및 수량 체크 함수 수정

--- src/main/kotlin/camp/nextstep/edu/step/step2/domain/store/LottoStore.kt
+++ src/main/kotlin/camp/nextstep/edu/step/step2/domain/store/LottoStore.kt
@@ -1,34 +1,31 @@
 package camp.nextstep.edu.step.step2.domain.store
 
 import camp.nextstep.edu.step.step2.domain.amount.BuyAmount
-import camp.nextstep.edu.step.step2.domain.lotto.Numbers
-import camp.nextstep.edu.step.step2.domain.result.LottoMatch
+import camp.nextstep.edu.step.step2.domain.lotto.WinningLotto
 import camp.nextstep.edu.step.step2.domain.result.LottoResult
 
 object LottoStore {
-    private const val LOTTO_TICKET_PRICE = 1000
+    /**
+     * @description : 로또 장당의 가격은 정부에서 정해진 정책만큼 가격이 달라지듯이 해당 미션에서는 각 Store마다 가격이 다르다고 가정했다.
+     */
+    private const val LOTTO_TICKET_PRICE = 1000L
 
     /**
      * @description : 구매금액에 따른 로또 티켓 수량을 구한다.
      */
     fun buyLottoTickets(buyAmount: BuyAmount): LottoTickets {
-        val lottoTicketAmount =
-            buyAmount.getAmount().divide(LOTTO_TICKET_PRICE.toBigDecimal()).toInt()
+        val lottoTicketAmount = buyAmount.divideByLotteryPrice(ticketPrice = LOTTO_TICKET_PRICE)
         return LottoTickets(lottoTicketAmount)
     }
 
     /**
      * @description : 로또의 당첨결과르 확인한다. ( 보통 로또의 경우 구매처에서 용지를 넣고 확인하는 방식을 활용했다. )
      */
-    fun checkLottoTicketsWinningResult(
-        lottoNumbers: List<Numbers>,
-        lastWeekNumbers: Numbers
-    ): LottoResult {
-        val totalPrice = lottoNumbers.size * LOTTO_TICKET_PRICE
-        val matchResult = lottoNumbers
-            .map { numbers -> numbers.countMatch(lastWeekNumbers) }
-            .map { LottoMatch.of(it) }
-            .sorted()
+    fun checkLottoTicketsWinningResult(winningLotto: WinningLotto): LottoResult {
+
+        val totalPrice =
+            winningLotto.getTotalPriceByLottoAmountAndTicketPrice(ticketPrice = LOTTO_TICKET_PRICE)
+        val matchResult = winningLotto.getLottoMatchResult()
 
         return LottoResult(totalPrice, matchResult)
     }
