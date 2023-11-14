c297adf2a558124025fbb1704b00dfeaf1fa915d
Ueibin Kim <at365@naver.com>
2023-11-14 17:34:07 +0900

    refactor : 숫자 도메인 ( Number ) 내, private 제거 및 of , get 함수 제거

--- src/main/kotlin/camp/nextstep/edu/step/step2/domain/lotto/Number.kt
+++ src/main/kotlin/camp/nextstep/edu/step/step2/domain/lotto/Number.kt
@@ -2,21 +2,11 @@ package camp.nextstep.edu.step.step2.domain.lotto
 
 @JvmInline
 value class Number(
-    private val number: Int
+    val number: Int
 ) {
 
     init {
         require(number in 1..45) { "로또 번호는 1~45 사이여야 합니다." }
-    }
-
-    fun getNumber(): Int {
-        return number
-    }
-
-    companion object {
-        fun of(number: Int): Number {
-            return Number(number)
-        }
     }
 
 }
