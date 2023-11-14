6374ffb46fc347b5ac378f894f054935a02d66f4
jeongdalma <jeongdalma@gmail.com>
2023-11-15 01:37:35 +0900

    refactor: LottoRank 람다 프로퍼티 추가

--- src/main/kotlin/lotto/domain/LottoRank.kt
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -2,11 +2,12 @@ package lotto.domain
 
 enum class LottoRank(
     val countOfMatch: Int,
-    val winningMoney: Int
+    val winningMoney: Int,
+    val findRank: (LottoRank, LottoWinningResult) -> Boolean = isMatchRank
 ) {
 
     FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
+    SECOND(5, 30_000_000 , isBonusRank),
     THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
     FIFTH(3, 5_000),
@@ -14,22 +15,15 @@ enum class LottoRank(
     ;
 
     companion object {
-        private fun <WIN, RANK> compose(bonus: (WIN) -> RANK, normal: (WIN) -> RANK): (WIN) -> RANK =
-            { winningResult: WIN ->
-                when (val rank = bonus(winningResult)) {
-                    MISS -> normal(winningResult)
-                    else -> rank
-                } 
-            }
-
-        private fun getBonusRank(lottoWinningResult: LottoWinningResult) =
-            if (lottoWinningResult.countOfMatch == SECOND.countOfMatch && lottoWinningResult.isBonusRank) SECOND
-            else MISS
-        private fun getNormalRank(lottoWinningResult: LottoWinningResult) =
-            entries.filter { it != SECOND }
-                .find { it.countOfMatch == lottoWinningResult.countOfMatch } ?: MISS
-
         fun valueOf(lottoWinningResult: LottoWinningResult): LottoRank =
-            compose(::getBonusRank, ::getNormalRank)(lottoWinningResult)
+            entries.find { it.findRank(it, lottoWinningResult) } ?: MISS
     }
+}
+
+private val isBonusRank: (LottoRank, LottoWinningResult) -> Boolean = { lottoRank, lottoWinningResult ->
+    lottoRank == LottoRank.SECOND && lottoRank.countOfMatch == lottoWinningResult.countOfMatch && lottoWinningResult.isBonusRank
+}
+
+private val isMatchRank: (LottoRank, LottoWinningResult) -> Boolean = { lottoRank, lottoWinningResult ->
+    lottoRank.countOfMatch == lottoWinningResult.countOfMatch
 }
