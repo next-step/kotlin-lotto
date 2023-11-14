168f33dddcba95d26e9917dfe20ccb4421e1bab6
Celina <luv.mins99@gmail.com>
2023-11-15 01:52:02 +0900

    feat: 로또 구매 금액과 수동 구매 장수, 자동 구매 장수 정보를 가지는 LottoMoney 객체 추가

---
+++ src/main/kotlin/lottery/domain/LottoMoney.kt
@@ -0,0 +1,22 @@
+package lottery.domain
+
+import lottery.domain.LottoGame.Companion.LOTTERY_PRICE
+
+data class LottoMoney(
+    val money: Int = 0,
+    val manualLottoCount: Int = 0,
+    val autoLottoCount: Int = 0,
+) {
+    companion object {
+        fun of(money: Int, manualLottoCount: Int): LottoMoney {
+            require(money >= manualLottoCount.times(LOTTERY_PRICE)) { INVALID_MANUAL_LOTTO_COUNT_EXCEPTION }
+            return LottoMoney(
+                money = money,
+                manualLottoCount = manualLottoCount,
+                autoLottoCount = money.div(LOTTERY_PRICE).minus(manualLottoCount)
+            )
+        }
+
+        private const val INVALID_MANUAL_LOTTO_COUNT_EXCEPTION = "구매 가능한 로또 갯수를 초과했습니다"
+    }
+}
