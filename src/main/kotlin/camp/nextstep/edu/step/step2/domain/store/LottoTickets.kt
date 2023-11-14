cbdf4038cfc38d668550f313057eb5eca4c4daf7
Ueibin Kim <at365@naver.com>
2023-11-14 17:37:18 +0900

    refactor : 로또 발급 티켓들 ( LottoTickets ) 도메인 내, 번호 생성 함수 수정 및 private , get, of 제거

--- src/main/kotlin/camp/nextstep/edu/step/step2/domain/store/LottoTickets.kt
+++ src/main/kotlin/camp/nextstep/edu/step/step2/domain/store/LottoTickets.kt
@@ -1,34 +1,23 @@
 package camp.nextstep.edu.step.step2.domain.store
 
-import camp.nextstep.edu.step.step2.domain.lotto.Numbers
+import camp.nextstep.edu.step.step2.domain.lotto.Lotto
 
 @JvmInline
 value class LottoTickets(
-    private val lottoTicketAmount: Int
+    val lottoTicketAmount: Int
 ) {
 
     init {
         require(lottoTicketAmount > 0) { "로또 티켓은 1개 이상이어야 합니다." }
     }
 
-    fun getLottoTicketAmount(): Int {
-        return lottoTicketAmount
-    }
-
     /**
      * @description : 로또 티켓 수량에 따른 로또 번호를 생성한다.
      */
-    fun createNumbersByLottoTicketAmount(): List<Numbers> {
+    fun createNumbersByLottoTicketAmount(): List<Lotto> {
         return (1..lottoTicketAmount)
-            .map { Numbers(numbers = Numbers.createNumbers()) }
+            .map { Lotto.generateAutoNumbers() }
             .distinct()
-    }
-
-
-    companion object {
-        fun of(lottoTicketAmount: Int): LottoTickets {
-            return LottoTickets(lottoTicketAmount)
-        }
     }
 
 }
