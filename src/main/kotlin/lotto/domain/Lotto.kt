2751433f8ad34c214d9427d3f77bc2cb4896bee2
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-14 16:30:40 +0900

    fix : Lotto 의 numbers 를 외부에서 read-only 인 List 로 받을 수 있도록 변경합니다

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,18 +3,21 @@ package lotto.domain
 import lotto.numbermaker.RandomNumberMaker
 
 class Lotto(private val numberList: List<Int> = generateDefaultRandomNumberList()) {
-    val numbers = mutableListOf<LottoNumber>()
+    private val _numbers = mutableListOf<LottoNumber>()
+
+    val numbers: List<LottoNumber>
+        get() = _numbers.toList()
 
     init {
         numberList.forEach { it ->
             val lottoNumber = LottoNumber.from(it)
-            numbers.add(lottoNumber)
+            _numbers.add(lottoNumber)
         }
     }
 
     fun getNumberValues(): List<Int> {
         val numberList = mutableListOf<Int>()
-        numbers.forEach {
+        _numbers.forEach {
             numberList.add(it.value)
         }
         return numberList
