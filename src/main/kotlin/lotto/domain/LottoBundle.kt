f8ad71e408a8dbcda62df184b64e76b400aa1916
LAPTOP-A8710DGI\kdh85 <modudamba@naver.com>
2023-11-15 19:19:02 +0900

    step3: 로또(2등) 로또번들 리팩토링 * 로또들의 당첨번호 수, 보너스번호 일치여부를 기준으로 당첨등급들을 찾아서 반환한다.

--- src/main/kotlin/lotto/domain/LottoBundle.kt
+++ src/main/kotlin/lotto/domain/LottoBundle.kt
@@ -1,10 +1,23 @@
 package lotto.domain
 
+import lotto.enums.Rank
 import lotto.service.NumberCreateStrategy
 
 class LottoBundle(
     val bundle: List<Lotto>
 ) {
+    fun findAllByMatchRanks(winningLotto: WinningLotto): List<Rank> {
+        val countAndBonusByRank = mutableListOf<Rank>()
+
+        bundle.forEach { lotto ->
+            val countByNumbers = lotto.makeMatchCountByNumbers(winningLotto.winningLotto)//0~6개
+            val isHaveBonus = lotto.isMatchBonus(winningLotto.bonusNumber)
+            val findRank = Rank.valueOf(countByNumbers, isHaveBonus)
+            countAndBonusByRank.add(findRank)
+        }
+        return countAndBonusByRank
+    }
+
     companion object {
         fun of(
             quantity: Int,
