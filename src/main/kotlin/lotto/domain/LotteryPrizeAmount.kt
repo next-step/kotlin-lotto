a825b530c104cc4751ba52bab3c3bbdece11a3a9
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-15 18:58:20 +0900

    fix : LotteryPrizeAmount Class 가 bonusMatch 의 정보를 추가로 받을 수 있도록 변경합니다.

--- src/main/kotlin/lotto/domain/LotteryPrizeAmount.kt
+++ src/main/kotlin/lotto/domain/LotteryPrizeAmount.kt
@@ -1,19 +1,17 @@
 package lotto.domain
 
-enum class LotteryPrizeAmount(val winNum: Int, val prize: Int) {
-    MISS(0, 0),
-    FIFTH(3, 5_000),
-    FOURTH(4, 50_000),
-    THIRD(5, 1_500_000),
-    SECOND(5, 30_000_000),
-    FIRST(6, 2_000_000_000),
+enum class LotteryPrizeAmount(val winNum: Int, val prize: Int, val bonusMatch: Boolean) {
+    MISS(0, 0, false),
+    FIFTH(3, 5_000, false),
+    FOURTH(4, 50_000, false),
+    THIRD(5, 1_500_000, false),
+    SECOND(5, 30_000_000, true),
+    FIRST(6, 2_000_000_000, false),
     ;
 
     companion object {
         fun getWinningPrize(winNum: Int, bonusMatch: Boolean = false): LotteryPrizeAmount {
-            val lotteryPrizeAmount = values().find { it.winNum == winNum } ?: MISS
-            if (lotteryPrizeAmount == THIRD && bonusMatch) return SECOND
-            return lotteryPrizeAmount
+            return values().find { it.winNum == winNum && it.bonusMatch == bonusMatch} ?: MISS
         }
     }
 }
