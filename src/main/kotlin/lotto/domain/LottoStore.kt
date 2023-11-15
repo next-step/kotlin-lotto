f35a1a6da07b83ead2a993f5ddbf960163471e24
seongmo-jeon <oeeen3@gmail.com>
2023-11-15 21:26:20 +0900

    refactor: 로또 숫자 최대 개수를 상수로 추출

--- src/main/kotlin/lotto/domain/LottoStore.kt
+++ src/main/kotlin/lotto/domain/LottoStore.kt
@@ -1,13 +1,15 @@
 package lotto.domain
 
 object LottoStore {
+    private const val MAX_LOTTO_NUMBER_COUNT = 6
+
     fun purchase(money: Int): List<Lotto> {
         val count = money / Lotto.LOTTO_PRICE
         return List(count) { generateRandomLotto() }
     }
 
     private fun generateRandomLotto(): Lotto {
-        val lottoNumbers = LottoNumbers.LOTTO_NUMBERS.shuffled().take(6)
+        val lottoNumbers = LottoNumbers.LOTTO_NUMBERS.shuffled().take(MAX_LOTTO_NUMBER_COUNT)
         return Lotto.fromLottoNumbers(lottoNumbers)
     }
 }
