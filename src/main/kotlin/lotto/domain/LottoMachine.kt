2319a99123fff481339da57db46a9c902635fc7f
JunHyun Lee <dlwnsgus777@naver.com>
2023-11-15 22:36:45 +0900

    feat: List(N) 빌더로 변경

--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -7,19 +7,20 @@ class LottoMachine(private val lottoPrice: Int) {
     }
 
     private fun createLotto(amount: Int): List<Lotto> {
-       return (0 until amount)
+        return (0 until amount)
             .map { LottoNumbers(lottoNumberGenerator()) }
             .map { Lotto(it) }
     }
 
     private fun lottoNumberGenerator(): List<LottoNumber> {
 
-        return (0 until 6).map { LottoNumber((START_NUMBER..END_NUMBER).random()) }
+        return List(MAX_LOTTO_NUMBER_AMOUNT) { LottoNumber((START_NUMBER..END_NUMBER).random()) }
     }
 
     companion object {
         private const val SELL_LOTTO_ERROR_MESSAGE: String = "금액이 부족합니다."
         private const val START_NUMBER: Int = 1
         private const val END_NUMBER: Int = 45
+        private const val MAX_LOTTO_NUMBER_AMOUNT: Int = 6
     }
 }
