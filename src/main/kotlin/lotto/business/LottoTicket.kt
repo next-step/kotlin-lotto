d2928d27804501c820b002e1726c79ae0ecc1cec
ALbertIM0427 <guang7226961@naver.com>
2023-11-16 07:32:21 +0900

    refactor: 필요 없는 interface 제거

--- src/main/kotlin/lotto/business/LottoTicket.kt
+++ src/main/kotlin/lotto/business/LottoTicket.kt
@@ -1,6 +1,16 @@
 package lotto.business
 
-class LottoTicket(lottoNumbers: Set<LottoNumber>) : LottoNumberSet(lottoNumbers) {
+class LottoTicket(lottoNumbers: Set<LottoNumber>) {
+    private val _lottoNumbers: Set<LottoNumber> = lottoNumbers
+
+    val lottoNumbers: List<LottoNumber>
+        get() = _lottoNumbers.sortedBy { it.number }
+
+    init {
+        require(lottoNumbers.size == LOTTO_NUMBER_SIZE) {
+            "서로 다른 ${LOTTO_NUMBER_SIZE}개 로또 번호 이여야 합니다."
+        }
+    }
 
     fun matchCount(targetLottoNumbers: List<LottoNumber>): Int {
         return targetLottoNumbers.count(lottoNumbers::contains)
@@ -8,5 +18,9 @@ class LottoTicket(lottoNumbers: Set<LottoNumber>) : LottoNumberSet(lottoNumbers)
 
     fun contains(targetLottoNumber: LottoNumber): Boolean {
         return lottoNumbers.contains(targetLottoNumber)
+    }
+
+    companion object {
+        const val LOTTO_NUMBER_SIZE = 6
     }
 }
