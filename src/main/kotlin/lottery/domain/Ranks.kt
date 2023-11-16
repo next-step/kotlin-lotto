fe4b6b1d61edf63129535d02eda24793596c42a7
Celina <luv.mins99@gmail.com>
2023-11-16 23:43:59 +0900

    feat: 부생성자 방식으로 변경
    - 생성자 사용시에만 쓰는 객체들은 따로 객체에 할당하지 않아 좋음!

--- src/main/kotlin/lottery/domain/Ranks.kt
+++ src/main/kotlin/lottery/domain/Ranks.kt
@@ -1,15 +1,11 @@
 package lottery.domain
 
-class Ranks(lottos: Lottos, winningLotto: WinningLotto, price: Int) {
-    val rank: List<Rank>
-    val profitMargin: Double
-    val isProfitable: Boolean
+class Ranks(val rank: List<Rank>, price: Int) {
+    val profitMargin: Double = rank.sumOf { it.winningMoney }
+        .div(price.toDouble())
+    val isProfitable: Boolean = profitMargin >= JUDGMENT_CRITERIA
 
-    init {
-        rank = lottos.matchLottos(winningLotto)
-        profitMargin = rank.sumOf { it.winningMoney }.div(price.toDouble())
-        isProfitable = profitMargin >= JUDGMENT_CRITERIA
-    }
+    constructor(lottos: Lottos, winningLotto: WinningLotto, price: Int) : this(lottos.matchLottos(winningLotto), price)
 
     companion object {
         private const val JUDGMENT_CRITERIA = 1
