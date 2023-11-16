656c8b31501e65dc8328cea3d92ed18e944f5343
baek0318 <peachberry0318@gmail.com>
2023-11-16 23:29:25 +0900

    step2 : 수익 계산하는 로직 작성

---
+++ src/main/kotlin/lotto/domain/Earning.kt
@@ -0,0 +1,13 @@
+package lotto.domain
+
+@JvmInline
+value class Earning(
+    private val strategy: Map<Int, Int>
+) {
+
+    fun calculate(result: Map<Int, Int>): Int = result
+        .filter { strategy.containsKey(it.key) }
+        .map { (strategy[it.key]!! * (it.value)) }
+        .reduceOrNull { acc, i -> acc + i } ?: 0
+
+}
