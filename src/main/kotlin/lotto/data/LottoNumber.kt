93262524dcd8a5ccf75dfb5ffa393ca24017064d
OYJ <oyj7677@gmail.com>
2023-11-16 22:52:02 +0900

    Refactor: 로또 번호 클래스 역할 추가, 번호 구성, 검증

--- src/main/kotlin/lotto/data/LottoNumber.kt
+++ src/main/kotlin/lotto/data/LottoNumber.kt
@@ -1,12 +1,43 @@
 package lotto.data
 
-object LottoNumber {
+import java.util.TreeSet
 
-    const val MIN_NUMBER = 1
-    const val MAX_NUMBER = 45
-    private const val ERR_MSG_OUT_OF_LOTTO_RANGE = "로또의 범위를 넘어서는 번호입니다."
+class LottoNumber private constructor(private val number: Int) : Comparable<LottoNumber> {
 
-    fun validateRange(number: Int) {
-        require(number in MIN_NUMBER..MAX_NUMBER) { ERR_MSG_OUT_OF_LOTTO_RANGE }
+    companion object {
+        private const val MIN_NUMBER = 1
+        private const val MAX_NUMBER = 45
+        private const val SUB_LIST_START_POSITION = 0
+        private const val LOTTO_NUMBER_LENGTH = 6
+        private const val ERR_MSG_OUT_OF_LOTTO_RANGE = "로또의 범위를 넘어서는 번호입니다."
+        private const val ERR_MSG_OUT_OF_LOTTO_LENGTH = "번호는 6개로 구성되어야 합니다."
+
+        private val NUMBERS: Map<Int, LottoNumber> = (MIN_NUMBER..MAX_NUMBER).associateWith(::LottoNumber)
+
+        fun createLottoNumbers(numbers: List<Int>): TreeSet<LottoNumber> {
+            val lottoNumbers = TreeSet(numbers.map(::from))
+            validateDuplication(lottoNumbers)
+
+            return lottoNumbers
+        }
+
+        fun createRandomLotto(): TreeSet<LottoNumber> {
+            val randomNumberList = (MIN_NUMBER..MAX_NUMBER).shuffled()
+                .subList(SUB_LIST_START_POSITION, SUB_LIST_START_POSITION + LOTTO_NUMBER_LENGTH - 1)
+
+            return createLottoNumbers(randomNumberList)
+        }
+
+        fun validateDuplication(lottoNumbers: TreeSet<LottoNumber>) {
+            require(lottoNumbers.size == LOTTO_NUMBER_LENGTH) { ERR_MSG_OUT_OF_LOTTO_LENGTH }
+        }
+
+        private fun from(value: Int): LottoNumber {
+            return NUMBERS[value] ?: throw IllegalArgumentException(ERR_MSG_OUT_OF_LOTTO_RANGE)
+        }
+    }
+
+    override fun compareTo(other: LottoNumber): Int {
+        return this.number.compareTo(other.number)
     }
 }
