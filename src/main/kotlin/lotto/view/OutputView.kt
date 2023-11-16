52746fa336cd40f719634bde402524a75d8f72c3
baek0318 <peachberry0318@gmail.com>
2023-11-16 23:24:46 +0900

    step2 : view 관련 로직 작성

---
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -0,0 +1,26 @@
+package lotto.view
+
+import lotto.domain.Lotto
+import lotto.domain.LottoResult
+
+object OutputView {
+
+    fun outputBuyResult(lottoCount: Int, lotteries: List<Lotto>) {
+        println("${lottoCount}개를 구매했습니다.")
+        lotteries.forEach {
+            println(it.numbers)
+        }
+
+    }
+
+    fun outputLottoResult(result: LottoResult, strategy: Map<Int, Int>) {
+        println()
+        println("당첨 통계")
+        println("---------")
+        strategy.entries
+            .sortedBy { it.key }
+            .map { "${it.key}개 일치 (${it.value}원)- ${result.earnResult[it.key] ?: 0}개" }
+            .forEach { println(it) }
+        println("총 수익률은 ${result.earningRate}입니다.")
+    }
+}
