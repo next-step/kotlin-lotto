6c7c1318ffa3189f1e5eb7a8fcb5174e3eed9c77
1stpie <1stpie@naver.com>
2023-11-16 21:41:16 +0900

    fix(core): remove unnecessary require

--- src/main/kotlin/study/lotto/domain/Lottoes.kt
+++ src/main/kotlin/study/lotto/domain/Lottoes.kt
@@ -7,10 +7,6 @@ class Lottoes(private val list: List<Lotto>) : List<Lotto> by list {
 
     companion object {
         fun buyLottoes(purchaseAmount: Amount): Lottoes {
-            require(purchaseAmount >= 0) {
-                "purchaseAmount must be a positive value"
-            }
-
             val buyLottoesCount = purchaseAmount / Lotto.PRICE_PER_TICKET
             val lottoList = List(buyLottoesCount) { Lotto.generate() }
             return Lottoes(lottoList)
