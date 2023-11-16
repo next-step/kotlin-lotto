e7c58e171770fd1a73f0fd10b8e0a0e2ef164ef1
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-16 18:33:58 +0900

    Test : Lotto 에 정의된 func 에 대한 테스트코드를 작성합니다

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -2,6 +2,8 @@ package lotto.domain
 
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class LottoTest {
     @Test
@@ -14,5 +16,20 @@ class LottoTest {
     fun `로또 번호는 서로 다른 숫자로 이루어져있다`() {
         val lotto = Lotto()
         lotto.numbers.distinct().size shouldBe 6
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["1, true", "2, true", "3, true", "4, true", "5, true", "6, true", "7, false"])
+    fun `로또에서 특정 로또 번호가 포함되어있는 지 확인할 수 있다`(number: Int, expected: Boolean) {
+        val lotto = Lotto(setOf(1, 2, 3, 4, 5, 6))
+        val lottoNumber = LottoNumber.from(number)
+        lotto.hasLottoNumber(lottoNumber) shouldBe expected
+    }
+
+    @Test
+    fun `로또와 다른 로또가 주어졌을 때, 몇 개가 일치하는 지 알 수 있다`() {
+        val lotto = Lotto(setOf(1, 2, 3, 4, 5, 6))
+        val lotto2 = Lotto(setOf(1, 2, 3, 4, 5, 7))
+        lotto.countMatchNumber(lotto2) shouldBe 5
     }
 }
