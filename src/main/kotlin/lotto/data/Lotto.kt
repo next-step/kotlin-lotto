c7fec0477e7f72360b92cc0b7d1ec307f51e6518
OYJ <oyj7677@gmail.com>
2023-11-16 22:52:39 +0900

    Refactor: 로또 데이터 클래스 역할 정리

--- src/main/kotlin/lotto/data/Lotto.kt
+++ src/main/kotlin/lotto/data/Lotto.kt
@@ -2,15 +2,4 @@ package lotto.data
 
 import java.util.TreeSet
 
-data class Lotto(val selectNumberList: TreeSet<Int>) {
-
-    init {
-        selectNumberList.forEach { LottoNumber.validateRange(it) }
-        require(selectNumberList.size == LOTTO_NUMBER_COUNT) { ERR_MSG_INVALID_LOTTO_NUMBER_COUNT }
-    }
-
-    companion object {
-        private const val ERR_MSG_INVALID_LOTTO_NUMBER_COUNT = "로또번호는 6개로 구성되어야 합니다. 중복된 숫자는 불가합니다."
-        private const val LOTTO_NUMBER_COUNT = 6
-    }
-}
+data class Lotto(val selectNumbers: TreeSet<LottoNumber>)
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -14,39 +14,4 @@ class LottoTest {
         println(treeSet)
     }
 
-    @Test
-    fun `1 ~ 45 범위를 넘어가는 값을 받았다면, 로또를 생성했을 때, 오류를 던진다`() {
-        // given : 범위를 벗어나는 값을 포함하여 번호를 구성한다.
-        val inputData: TreeSet<Int> = sortedSetOf(1, 2, 3, 4, 5, 6, 50)
-
-        // when : 로또를 생성한다.
-        val actual = runCatching { Lotto(inputData) }.exceptionOrNull()
-
-        // then : 오류를 던진다.
-        assertThat(actual).isInstanceOf(IllegalArgumentException()::class.java)
-    }
-
-    @Test
-    fun `6개로 구성되지 않는 번호 구성을 받았다면, 로또를 생성했을 때, 오류를 던진다`() {
-        // given : 6개로 구성되지 않은 번호 구성을 받는다.
-        val inputData = sortedSetOf(1, 2, 3, 4, 5, 6, 7)
-
-        // when : 로또를 생성한다.
-        val actual = runCatching { Lotto(inputData) }.exceptionOrNull()
-
-        // then : 오류를 던진다.
-        assertThat(actual).isInstanceOf(IllegalArgumentException()::class.java)
-    }
-
-    @Test
-    fun `중복값을 포함한 6개의 번호 구성을 받았다면, 로또를 생성했을 때, 오류를 던진다`() {
-        // given : 중복값을 포함한 6개의 번호 구성을 받는다.
-        val inputData = sortedSetOf(1, 2, 3, 4, 5, 5)
-
-        // when : 로또를 생성한다.
-        val actual = runCatching { Lotto(inputData) }.exceptionOrNull()
-
-        // then : 오류를 던진다.
-        assertThat(actual).isInstanceOf(IllegalArgumentException()::class.java)
-    }
 }
