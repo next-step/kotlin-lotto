32ff47519c9028f50d4360f01d34b4b5abbc02d8
chacha.roni <chacha.roni@kakaocorp.com>
2023-11-16 23:07:26 +0900

    step4 피드백 반영 - validation을 위한 init 추가

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -2,6 +2,9 @@ package lotto.domain
 
 @JvmInline
 value class LottoNumber private constructor(val number: Int) {
+    init {
+        require(number in MIN_NUMBER..MAX_NUMBER) { ERROR_MSG }
+    }
 
     override fun toString(): String {
         return number.toString()
@@ -10,10 +13,11 @@ value class LottoNumber private constructor(val number: Int) {
     companion object {
         private const val MIN_NUMBER = 1
         private const val MAX_NUMBER = 45
+        private const val ERROR_MSG = "로또 번호는 1~45 사이의 숫자여야 합니다."
         val NUMBERS: Map<Int, LottoNumber> = (MIN_NUMBER..MAX_NUMBER).associateWith { LottoNumber(it) }
 
         fun from(number: Int): LottoNumber {
-            return NUMBERS[number] ?: throw IllegalArgumentException("로또 번호는 1~45 사이의 숫자여야 합니다.")
+            return NUMBERS[number] ?: throw IllegalArgumentException(ERROR_MSG)
         }
     }
 }
