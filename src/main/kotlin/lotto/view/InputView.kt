485f1ccf877a1f662daa77301e023d3ccdf779dd
OYJ <oyj7677@gmail.com>
2023-11-17 00:29:52 +0900

    chore: 매직 넘버 제거

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -4,6 +4,8 @@ object InputView {
 
     private const val ERR_MSG_INVALID_NUMERIC_FORMAT = "입력된 값의 포맷이 숫자가 압니다."
     private const val ERR_MSG_OUT_OF_CASH_RANGE = "입력된 값이 구매 가능할 수 있는 금액의 범위를 벗어났습니다"
+    private const val MIN_GAME_COST = 1000
+    private const val MAX_GAME_COST = 100000
 
     fun inputCash(): String {
         println("구입금액을 입력해 주세요.")
@@ -35,7 +37,7 @@ object InputView {
     }
 
     private fun validateNumberRange(inputData: Int) {
-        require(inputData in 1000..100000) { ERR_MSG_OUT_OF_CASH_RANGE }
+        require(inputData in MIN_GAME_COST..MAX_GAME_COST) { ERR_MSG_OUT_OF_CASH_RANGE }
     }
 
     private fun validateWinningNumber(inputData: List<String>) {
