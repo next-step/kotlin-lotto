38dc8e5bd0529317d60d2e6960498a33324f55a0
sendkite <sendkite1@gmail.com>
2023-11-19 01:54:34 +0900

    feat: add split and sum

--- src/test/kotlin/calculator/StringAddCalculator.kt
+++ src/test/kotlin/calculator/StringAddCalculator.kt
@@ -3,8 +3,24 @@ package calculator
 class StringAddCalculator {
     fun add(text: String): Int {
         if (text.isBlank()) return 0
-        require(text.toIntOrNull() != null) { "잘못된 문자열을 입력할 수 없습니다." }
-        require(text.toInt() >= 0) { "음수를 입력할 수 없습니다." }
-        return text.toInt()
+        if (text.matches(Regex("[0-9]+"))) {
+            return text.toInt()
+        }
+
+        val tokens = parseTokens(text)
+        validateTokens(tokens)
+
+        return tokens.sum()
+    }
+
+    private fun parseTokens(text: String): List<Int> {
+        require(text.contains("-").not()) { "음수를 입력할 수 없습니다." }
+        require(text.contains(Regex("[,:]"))) { "구분자 , 또는 : 를 포함해야 합니다." }
+        return text.split(Regex("[,:\n]")).map { it.toInt() }
+    }
+
+    private fun validateTokens(tokens: List<Int>) {
+        require(tokens.all { it >= 0 }) { "음수를 입력할 수 없습니다." }
+
     }
 }
