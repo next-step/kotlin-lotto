4b68333c400de60a64f2a2dca6ce2c209be80b46
yw9594 <kyw9509@gmail.com>
2023-11-19 02:42:43 +0900

    fix(step2): 테스트 이름 및 변경 내용 반영

--- src/test/kotlin/lotto/utils/ListUtilsTest.kt
+++ src/test/kotlin/lotto/utils/ListUtilsTest.kt
@@ -4,12 +4,12 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.comparables.shouldBeLessThan
 
 class ListUtilsTest : FunSpec({
-    test("45개의 원소를 가진 리스트에서 6개의 랜덤한 숫자가 정렬되어 반환되는지 테스트") {
-        val input = (1..10).toList()
+    test("정수 리스트의 랜덤한 숫자가 정렬되어 반환되는지 테스트") {
+        val numbers = (1..10).toList()
         val n = 6
         val sort = true
 
-        val result = shuffleAndTake(input, n, sort = sort)
+        val result = numbers.shuffleAndTake(n, sort)
 
         result.windowed(2, 1)
             .forEach {
