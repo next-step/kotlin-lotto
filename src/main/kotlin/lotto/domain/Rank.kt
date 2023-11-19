9807047cfdcb79c9f058b02e1b17a5085d464e9b
verus.j <verus.j@kakaocorp.com>
2023-11-19 21:13:00 +0900

    refactor: 숫자 표기 수정

--- src/main/kotlin/lotto/domain/Rank.kt
+++ src/main/kotlin/lotto/domain/Rank.kt
@@ -3,11 +3,11 @@ package lotto.domain
 import java.util.function.BiPredicate
 
 enum class Rank(val isMatched: BiPredicate<Int, Boolean>, prize: Int) {
-    FIRST({ count, _ -> count == 6 }, 2000000000),
-    SECOND({ count, hasBonusNumber -> count == 5 && hasBonusNumber }, 30000000),
-    THIRD({ count, hasBonsNumber -> count == 5 && !hasBonsNumber }, 1500000),
-    FOURTH({ count, _ -> count == 4 }, 50000),
-    FIFTH({ count, _ -> count == 3 }, 5000),
+    FIRST({ count, _ -> count == 6 }, 2_000_000_000),
+    SECOND({ count, hasBonusNumber -> count == 5 && hasBonusNumber }, 30_000_000),
+    THIRD({ count, hasBonsNumber -> count == 5 && !hasBonsNumber }, 1_500_000),
+    FOURTH({ count, _ -> count == 4 }, 50_000),
+    FIFTH({ count, _ -> count == 3 }, 5_000),
     NOTHING({ count, _ -> count < 3 }, 0),
     ;
 
