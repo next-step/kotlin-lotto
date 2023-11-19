2a86a5b2f28841bd723471b91d27304943c4371b
JunHyun Lee <dlwnsgus777@naver.com>
2023-11-19 22:37:51 +0900

    feat: revenue enum 값 2등 추가에 맞게 수정

--- src/main/kotlin/lotto/domain/Revenue.kt
+++ src/main/kotlin/lotto/domain/Revenue.kt
@@ -4,14 +4,23 @@ enum class Revenue(
     val matchCount: Int,
     val prizeMoney: Int
 ) {
-    FIRST(3, 5_000),
-    SECOND(4, 50_000),
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
     THIRD(5, 1_500_000),
-    FOUR(6, 2_000_000_000);
+    FOUR(4, 50_000),
+    FIVE(3, 5_000);
 
     companion object {
-        fun of(matchCount: Int): Revenue {
-            return values().first { it.matchCount == matchCount }
+        fun of(matchCount: Int, isMatchBonus: Boolean = false): Revenue {
+            return when (val revenue = values().first { it.matchCount == matchCount }) {
+                SECOND, THIRD -> {
+                    if (isMatchBonus) {
+                        return SECOND
+                    }
+                    THIRD
+                }
+                FIRST, FOUR, FIVE -> revenue
+            }
         }
     }
 }
