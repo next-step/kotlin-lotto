04427eed00f7edda7e3c0e0ba3eba2dbb7d821ed
inhyeokk <rkddlsgur983@gmail.com>
2023-11-19 16:14:08 +0900

    step2-review: LottoMatchCount 가 matchCount 를 갖도록 수정

--- src/main/kotlin/lotto/domain/model/LottoMatchCount.kt
+++ src/main/kotlin/lotto/domain/model/LottoMatchCount.kt
@@ -1,25 +1,17 @@
 package lotto.domain.model
 
-enum class LottoMatchCount(val reward: Int) {
-    ZERO(0),
-    ONE(0),
-    TWO(0),
-    THREE(5000),
-    FOUR(50000),
-    FIVE(1500000),
-    SIX(2000000000),
+enum class LottoMatchCount(private val matchCount: Int, val reward: Int) {
+    ZERO(0, 0),
+    ONE(1, 0),
+    TWO(2, 0),
+    THREE(3, 5000),
+    FOUR(4, 50000),
+    FIVE(5, 1500000),
+    SIX(6, 2000000000),
     ;
 
     companion object {
-        fun from(matchCount: Int): LottoMatchCount = when (matchCount) {
-            0 -> ZERO
-            1 -> ONE
-            2 -> TWO
-            3 -> THREE
-            4 -> FOUR
-            5 -> FIVE
-            6 -> SIX
-            else -> throw IllegalArgumentException("로또 숫자의 일치 수는 로또 숫자 수의 범위를 벗어날 수 없습니다.")
-        }
+        fun from(matchCount: Int): LottoMatchCount = values().find { it.matchCount == matchCount }
+            ?: throw IllegalArgumentException("로또 숫자의 일치 수는 로또 숫자 수의 범위를 벗어날 수 없습니다.")
     }
 }
