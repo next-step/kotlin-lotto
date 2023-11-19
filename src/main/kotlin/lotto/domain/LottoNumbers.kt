25affba0387871a34a053fbbc1cf8abd45cb5686
JunHyun Lee <dlwnsgus777@naver.com>
2023-11-19 23:47:35 +0900

    fix: 매직 넘버 수정

--- src/main/kotlin/lotto/domain/LottoNumbers.kt
+++ src/main/kotlin/lotto/domain/LottoNumbers.kt
@@ -4,7 +4,7 @@ class LottoNumbers(numbers: List<LottoNumber>) {
     private val lottoNumbers: List<LottoNumber> = numbers.sortedBy { it.number }
 
     init {
-        require(numbers.size == 6) { INIT_ERROR_MESSAGE }
+        require(numbers.size == EXPECTED_LOTTO_NUMBER_SIZE) { INIT_ERROR_MESSAGE }
     }
 
     fun getNumbers(): List<LottoNumber> {
@@ -13,5 +13,6 @@ class LottoNumbers(numbers: List<LottoNumber>) {
 
     companion object {
         private const val INIT_ERROR_MESSAGE: String = "로또 번호는 6개여야 합니다."
+        private const val EXPECTED_LOTTO_NUMBER_SIZE: Int = 6
     }
 }
