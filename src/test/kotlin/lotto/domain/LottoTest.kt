4211b4d9daee29419c810e3d36b167fad6d88f0a
verus.j <verus.j@kakaocorp.com>
2023-11-19 21:09:13 +0900

    test: 에러 메시지 검증 추가

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto.domain
 
+import io.kotest.matchers.throwable.shouldHaveMessage
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
@@ -12,15 +13,15 @@ class LottoTest {
     @ValueSource(ints = [5, 7])
     fun `잘못된 숫자 갯수로 로또 생성 시 예외 발생`(size: Int) {
         assertThrows<IllegalArgumentException> {
-            val numbers = (0 until size).toList().toIntArray()
+            val numbers = (1 .. size).toList().toIntArray()
             Lotto(*numbers)
-        }
+        }.shouldHaveMessage("로또는 유일한 숫자 6개로 구성해야합니다.")
     }
 
     @Test
     fun `중복된 숫자로 로또 생성 시 예외 발생`() {
         assertThrows<IllegalArgumentException> {
             Lotto(1, 2, 3, 4, 5, 1)
-        }
+        }.shouldHaveMessage("로또는 유일한 숫자 6개로 구성해야합니다.")
     }
 }
