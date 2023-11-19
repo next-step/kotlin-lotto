44abf5e0ee7e74281eb3bec879a7ea362c907c3b
JunHyun Lee <dlwnsgus777@naver.com>
2023-11-19 21:59:51 +0900

    feat: 1, 45를 상수로 변경

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -3,7 +3,7 @@ package lotto.domain
 @JvmInline
 value class LottoNumber(val number: Int) {
     init {
-        require(number in 1..45) { ERROR_MESSAGE }
+        require(number in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) { ERROR_MESSAGE }
     }
 
     override fun toString(): String {
@@ -12,5 +12,7 @@ value class LottoNumber(val number: Int) {
 
     companion object {
         private const val ERROR_MESSAGE: String = "로또 번호는 45를 넘을 수 없습니다."
+        private const val MIN_LOTTO_NUMBER = 1
+        private const val MAX_LOTTO_NUMBER = 45
     }
 }
