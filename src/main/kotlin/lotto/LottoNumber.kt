df8b4d38cd0c43e9c614d6e8665404e241992421
eunwoo song <presentsong@naver.com>
2023-11-19 22:50:32 +0900

    style: 린트 수정

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -2,10 +2,9 @@ package lotto
 
 @JvmInline
 value class LottoNumber private constructor(val value: Int) {
-
     init {
         require(value in MIN_NUMBER..MAX_NUMBER) {
-            "로또 번호는 ${MIN_NUMBER}~${MAX_NUMBER} 사이여야 합니다."
+            "로또 번호는 $MIN_NUMBER~$MAX_NUMBER 사이여야 합니다."
         }
     }
 
@@ -16,7 +15,7 @@ value class LottoNumber private constructor(val value: Int) {
         val cache = (MIN_NUMBER..MAX_NUMBER).associateWith { LottoNumber(it) }
 
         fun of(value: Int): LottoNumber {
-            return cache[value] ?: throw IllegalArgumentException("로또 번호는 ${MIN_NUMBER}~${MAX_NUMBER} 사이여야 합니다.")
+            return cache[value] ?: throw IllegalArgumentException("로또 번호는 $MIN_NUMBER~$MAX_NUMBER 사이여야 합니다.")
         }
     }
 }
