36c9ee1952523272abc55de633fbe242c05b167b
eunwoo song <presentsong@naver.com>
2023-11-19 22:13:46 +0900

    refactor: companion object 하단으로 이동

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,20 +1,15 @@
 package lotto
 
 class Lotto(val lottoNumbers: List<LottoNumber>) {
+    fun contains(lottoNumber: LottoNumber): Boolean {
+        return lottoNumbers.contains(lottoNumber)
+    }
+
     companion object {
         const val SIZE = 6
 
         fun from(lottoNumbers: List<Int>): Lotto {
             return Lotto(lottoNumbers.map { LottoNumber(it) })
         }
-    }
-
-    fun contains(number: Int): Boolean {
-        val lottoNumber = LottoNumber(number)
-        return lottoNumbers.any { it == lottoNumber }
-    }
-
-    fun contains(lottoNumber: LottoNumber): Boolean {
-        return lottoNumbers.contains(lottoNumber)
     }
 }
