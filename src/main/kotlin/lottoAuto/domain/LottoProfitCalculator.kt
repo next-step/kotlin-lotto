797c24536769579637162075fc1106789ee915f0
jaylene shin <jaylene.shin@kakaocorp.com>
2023-11-19 12:33:24 +0900

    lottoProfitMessage enum class 생성

--- src/main/kotlin/lottoAuto/domain/LottoProfitCalculator.kt
+++ src/main/kotlin/lottoAuto/domain/LottoProfitCalculator.kt
@@ -5,12 +5,8 @@ import kotlin.math.floor
 object LottoProfitCalculator {
     fun calcProfit(purchaseAmount: Int, totalWinningMoney: Int): LottoProfit {
         val rateOfReturn = calcRateOfReturn(purchaseAmount, totalWinningMoney)
-        val resultMsg = when {
-            rateOfReturn > 1.00 -> "이익"
-            rateOfReturn == 1.00 -> "본전"
-            else -> "손해"
-        }
-        return LottoProfit(rateOfReturn, resultMsg)
+        val profitMessage = LottoProfitMessage.fromRateOfReturn(rateOfReturn).message
+        return LottoProfit(rateOfReturn, profitMessage)
     }
 
     private fun calcRateOfReturn(purchaseAmount: Int, totalWinningMoney: Int): Double {
