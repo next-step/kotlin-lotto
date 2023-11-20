45784539b086f571356f6314c39915d9f468e7a7
OYJ <oyj7677@gmail.com>
2023-11-21 01:45:06 +0900

    Refactor: 로또 구매 함수 로직 개선

--- src/main/kotlin/lotto/domain/LottoCalculator.kt
+++ src/main/kotlin/lotto/domain/LottoCalculator.kt
@@ -4,9 +4,15 @@ import lotto.data.LottoRanking
 
 object LottoCalculator {
 
+    private const val GAME_COST = 1000
+
     fun calculateWinningRate(cash: Int, winningStatus: Map<LottoRanking, Int>): Float {
         val totalPrice = winningStatus.toList().sumOf { it.first.findPrize(it) }
 
         return totalPrice / cash.toFloat()
+    }
+
+    fun getTimes(cash: Int): Int {
+        return cash / GAME_COST
     }
 }
