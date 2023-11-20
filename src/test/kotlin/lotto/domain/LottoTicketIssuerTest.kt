43dede65c1e077b092db0271fe62b2060fabeccf
khcho902 <khcho902@gmail.com>
2023-11-21 00:37:48 +0900

    test: LottoTicketIssuerTest 금액에 맞는 수량 발급 테스트 명시적으로 작성

--- src/test/kotlin/lotto/domain/LottoTicketIssuerTest.kt
+++ src/test/kotlin/lotto/domain/LottoTicketIssuerTest.kt
@@ -8,20 +8,16 @@ import io.kotest.matchers.shouldBe
 
 class LottoTicketIssuerTest : FunSpec({
 
-    lateinit var lottoTicketIssuer: LottoTicketIssuer
-
-    beforeTest {
-        lottoTicketIssuer = LottoTicketIssuer()
-    }
-
     context("금액에 맞는 수량만큼 로또를 발급핸다.") {
         withData(
-            nameFn = { "${it.money}원 => ${it.lottoAmount}개" },
-            IssueLottoByAutoTestData(1000L, 1),
-            IssueLottoByAutoTestData(2000L, 2),
-            IssueLottoByAutoTestData(2900L, 2),
-            IssueLottoByAutoTestData(10000L, 10),
-        ) { (money, lottoAmount) ->
+            IssueLottoByAutoTestData(1000L, 1000L, 1),
+            IssueLottoByAutoTestData(1000L, 2000L, 2),
+            IssueLottoByAutoTestData(1000L, 2900L, 2),
+            IssueLottoByAutoTestData(2000L, 10000L, 5),
+        ) { (lottoTicketPrice, money, lottoAmount) ->
+
+            val lottoTicketIssuer = LottoTicketIssuer(lottoTicketPrice = lottoTicketPrice)
+
             lottoTicketIssuer.issueLottoByAuto(money).size shouldBe lottoAmount
         }
     }
@@ -31,6 +27,9 @@ class LottoTicketIssuerTest : FunSpec({
             nameFn = { "${it}원" },
             -10L, -3L, -1L,
         ) { purchaseMoney ->
+
+            val lottoTicketIssuer = LottoTicketIssuer()
+
             shouldThrow<IllegalArgumentException> {
                 lottoTicketIssuer.issueLottoByAuto(purchaseMoney)
             }
@@ -38,6 +37,6 @@ class LottoTicketIssuerTest : FunSpec({
     }
 })
 
-data class IssueLottoByAutoTestData(val money: Long, val lottoAmount: Int) : WithDataTestName {
-    override fun dataTestName(): String = "${money}원 => : ${lottoAmount}개"
+data class IssueLottoByAutoTestData(val lottoTicketPrice: Long, val money: Long, val lottoAmount: Int) : WithDataTestName {
+    override fun dataTestName(): String = "로또 1장이 ${lottoTicketPrice}원일 때, ${money}원 => ${lottoAmount}개 발급"
 }
