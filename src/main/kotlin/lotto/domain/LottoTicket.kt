ab3bcafd4717becbb93b3309d709016aa61cb1f5
khcho902 <khcho902@gmail.com>
2023-11-21 01:59:31 +0900

    refactor: generateRandomLottoNumbers 메소드 위치 변경 LottoTicket -> LottoTicketIssuer

--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -1,9 +1,5 @@
 package lotto.domain
 
-import lotto.domain.LottoNumber.Companion.MAX_LOTTO_NUMBER
-import lotto.domain.LottoNumber.Companion.MIN_LOTTO_NUMBER
-import lotto.domain.LottoNumbers.Companion.LOTTO_NUMBERS_SIZE
-
 class LottoTicket(
     val lottoNumbers: LottoNumbers,
 ) {
@@ -17,20 +13,6 @@ class LottoTicket(
         fun of(vararg numbers: Int): LottoTicket {
             val lottoNumbers = LottoNumbers.of(*numbers)
             return LottoTicket(lottoNumbers)
-        }
-
-        fun issueByAuto(): LottoTicket {
-            val lottoNumbers = generateRandomLottoNumbers()
-            return LottoTicket(lottoNumbers)
-        }
-
-        private fun generateRandomLottoNumbers(): LottoNumbers {
-            val candidateLottoNumbers = (MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER).toList()
-            val numbers = candidateLottoNumbers
-                .shuffled()
-                .take(LOTTO_NUMBERS_SIZE)
-                .toSet()
-            return LottoNumbers.of(numbers)
         }
     }
 }
