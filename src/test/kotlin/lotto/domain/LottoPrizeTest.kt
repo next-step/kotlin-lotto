6f1b40a3d01c41a00986e36a33bebf98fda558c4
hyotaek-jang <hyotaek.jang@linecorp.com>
2023-11-21 11:24:58 +0900

    refactor: remove methodSource

--- src/test/kotlin/lotto/domain/LottoPrizeTest.kt
+++ src/test/kotlin/lotto/domain/LottoPrizeTest.kt
@@ -1,15 +1,14 @@
 package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.api.Test
 
 internal class LottoPrizeTest {
-    @ParameterizedTest
-    @MethodSource("getSecondPrize")
-    fun `로또 번호가 5개 일치하고, 보너스 번호가 포함되어 있다면 2등이다`(matchCountHasBonusPair: Pair<Int, Boolean>) {
+    @Test
+    fun `로또 번호가 5개 일치하고, 보너스 번호가 포함되어 있다면 2등이다`() {
         // given
-        val (matchCount, hasBonus) = matchCountHasBonusPair
+        val matchCount = 5
+        val hasBonus = true
 
         // when
         val lottoPrize = LottoPrize.of(matchCount, hasBonus)
@@ -18,28 +17,16 @@ internal class LottoPrizeTest {
         assertThat(lottoPrize).isEqualTo(LottoPrize.SECOND)
     }
 
-    @ParameterizedTest
-    @MethodSource("getThirdPrize")
-    fun `로또 번호가 5개 일치하고, 보너스 번호가 포함되어 있지 않다면 3등이다`(matchCountHasBonusPair: Pair<Int, Boolean>) {
+    @Test
+    fun `로또 번호가 5개 일치하고, 보너스 번호가 포함되어 있지 않다면 3등이다`() {
         // given
-        val (matchCount, hasBonus) = matchCountHasBonusPair
+        val matchCount = 5
+        val hasBonus = false
 
         // when
         val lottoPrize = LottoPrize.of(matchCount, hasBonus)
 
         // then
         assertThat(lottoPrize).isEqualTo(LottoPrize.THIRD)
-    }
-
-    companion object {
-        @JvmStatic
-        fun getSecondPrize() = listOf(
-            Pair(5, true),
-        )
-
-        @JvmStatic
-        fun getThirdPrize() = listOf(
-            Pair(5, false),
-        )
     }
 }
