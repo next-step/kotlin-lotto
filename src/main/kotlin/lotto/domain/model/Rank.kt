63bb6a7d5a7f3d1330a0bdd350b828dc812e3f96
inhyeokk <rkddlsgur983@gmail.com>
2023-11-21 18:48:59 +0900

    step3-review: 보너스 여부를 Rank 의 속성 값으로 포함

--- src/main/kotlin/lotto/domain/model/Rank.kt
+++ src/main/kotlin/lotto/domain/model/Rank.kt
@@ -1,8 +1,8 @@
 package lotto.domain.model
 
-enum class Rank(val matchCount: Int, val reward: Int) {
+enum class Rank(val matchCount: Int, val reward: Int, val isMatchBonusNumber: Boolean = false) {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
+    SECOND(5, 30_000_000, isMatchBonusNumber = true),
     THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
     FIFTH(3, 5_000),
@@ -10,9 +10,10 @@ enum class Rank(val matchCount: Int, val reward: Int) {
     ;
 
     companion object {
-        fun valueOf(matchCount: Int, isMatchBonusNumber: Boolean = false): Rank = values().find { it.matchCount == matchCount }
-            ?.let { if (it == THIRD && isMatchBonusNumber) SECOND else it }
-            ?: takeIf { matchCount in MISS.matchCount until FIFTH.matchCount }?.let { MISS }
-            ?: throw IllegalArgumentException("로또 숫자의 일치 수는 로또 숫자 수의 범위를 벗어날 수 없습니다. matchCount=$matchCount")
+        fun valueOf(matchCount: Int, isMatchBonusNumber: Boolean = false): Rank {
+            return values().find { it.matchCount == matchCount && it.isMatchBonusNumber == isMatchBonusNumber }
+                ?: takeIf { matchCount in MISS.matchCount until FIFTH.matchCount }?.let { MISS }
+                ?: throw IllegalArgumentException("로또 숫자의 일치 수는 로또 숫자 수의 범위를 벗어날 수 없습니다. matchCount=$matchCount")
+        }
     }
 }
