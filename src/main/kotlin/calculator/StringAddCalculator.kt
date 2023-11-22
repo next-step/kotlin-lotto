7faa6744dfb7eef3663ef406f23ae656d9147c61
전송연 Songyeon Jeon <90877864+sendkite@users.noreply.github.com>
2023-11-22 23:27:36 +0900

    Step1 (#981)
    * feat: input validate
    
    * feat: add split and sum
    
    * fix: input validation
    
    * feat: custom delimiter
    
    * feat: custom customDelimiter with basic delimiter

---
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -0,0 +1,34 @@
+package calculator
+
+class StringAddCalculator {
+    fun add(text: String): Int {
+        if (text.isBlank()) return 0
+        if (text.matches(Regex("[0-9]+"))) {
+            return text.toInt()
+        }
+
+        val customDelimiterRegex = Regex("//(.)\n(.*)").find(text)
+        val customDelimiter = customDelimiterRegex?.groupValues?.get(1) ?: ""
+        val tokens = parseTokens(text, customDelimiter)
+
+        return tokens.sum()
+    }
+
+    private fun parseTokens(text: String, customDelimiter: String): List<Int> {
+        require(text.contains("-").not()) { "음수를 입력할 수 없습니다." }
+        require(text.contains(Regex("[,:${customDelimiter}]"))) { "숫자와 지정된 구분자만 입력할 수 있습니다." }
+
+        if (customDelimiter.isNotBlank()) {
+            val replacedText = text.replace("//$customDelimiter\n", "")
+            return replacedText.split(Regex("[,:${customDelimiter}\n]")).map {
+                require(it.matches(Regex("[0-9]+"))) { "숫자와 지정된 구분자만 입력할 수 있습니다." }
+                it.toInt()
+            }
+        }
+
+        return text.split(Regex("[,:\n]")).map {
+            require(it.matches(Regex("[0-9]+"))) { "숫자와 지정된 구분자만 입력할 수 있습니다." }
+            it.toInt()
+        }
+    }
+}
---
+++ src/test/kotlin/calculator/StringAddCalculatorTest.kt
@@ -0,0 +1,62 @@
+package calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.EmptySource
+import org.junit.jupiter.params.provider.ValueSource
+
+class StringAddCalculatorTest {
+
+    private lateinit var calculator: StringAddCalculator
+
+    @BeforeEach
+    fun setUp() {
+        calculator = StringAddCalculator();
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    fun `빈 문자열 입력할 경우 0을 반환해야 한다`(text: String) {
+        val result = calculator.add(text)
+        assertThat(result).isEqualTo(0)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["1,1", "2,2", "3,3"])
+    fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(text: String, expected: Int) {
+        val result = calculator.add(text)
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["안녕", "$", "1,2[3"])
+    fun `잘못된 문자열을 입력할 경우 RuntimeException`(text: String) {
+        assertThatExceptionOfType(RuntimeException::class.java)
+            .isThrownBy { calculator.add(text) }
+            .withMessageMatching("숫자와 지정된 구분자만 입력할 수 있습니다.")
+    }
+
+    @Test
+    fun `음수를 입력하면 RuntimeException`() {
+        assertThatExceptionOfType(RuntimeException::class.java)
+            .isThrownBy { calculator.add("-1") }
+            .withMessageMatching("음수를 입력할 수 없습니다.")
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2:3"])
+    fun `쉼표 또는 콜론 구분자로 숫자를 입력할 경우 정상적으로 합을 반환한다`(text: String) {
+        val result = calculator.add(text)
+        assertThat(result).isEqualTo(6)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["//;\n1;2;3", "//<\n1<2<3", "//<\n1<2,3"])
+    fun `custom 구분자로 입력값을 구분`(text: String) {
+        assertThat(calculator.add(text)).isSameAs(6);
+    }
+}
