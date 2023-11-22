1e66363c305d16fe364d46266a7c0b62f7c04b80
seongmo-jeon <oeeen3@gmail.com>
2023-11-22 23:37:44 +0900

    refactor: 로또 가게에서 수동 구매 시 Lottos 객체를 넘기도록 수정

--- src/main/kotlin/lotto/domain/LottoStore.kt
+++ src/main/kotlin/lotto/domain/LottoStore.kt
@@ -1,11 +1,10 @@
 package lotto.domain
 
 object LottoStore {
-    fun purchase(totalMoney: Int, manualLottoNumbers: List<List<Int>>, lottoGenerator: LottoGenerator): Lottos {
+    fun purchase(totalMoney: Int, manualLottos: Lottos, lottoGenerator: LottoGenerator): Lottos {
         val count = totalMoney / Lotto.LOTTO_PRICE
-        val manualLottoCount = manualLottoNumbers.size
+        val manualLottoCount = manualLottos.size
         require(count >= manualLottoCount) { "수동 구매 개수는 전체 로또 구매 개수보다 클 수 없습니다." }
-        val manualLottos = ManualLottoGenerator(manualLottoNumbers).generateLotto(manualLottoCount)
         val automaticLottos = lottoGenerator.generateLotto(count - manualLottoCount)
         return manualLottos + automaticLottos
     }
