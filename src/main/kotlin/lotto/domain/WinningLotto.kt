38f518957e9b51f2fd696af1fb92c21b977e82ba
hyotaek-jang <hyotaek.jang@linecorp.com>
2023-11-23 00:18:42 +0900

    refactor: modify match's return type

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -5,7 +5,7 @@ class WinningLotto(val lotto: Lotto, private val bonusNumber: LottoNumber) {
         require(bonusNumber !in lotto.numbers) { "보너스 번호는 당첨 번호와 중복될 수 없습니다." }
     }
 
-    fun match(lottoList: List<LottoNumber>): Pair<Int, Boolean> {
-        return lottoList.count { it in lotto.numbers } to lottoList.contains(bonusNumber)
+    fun match(lottoList: List<LottoNumber>): LottoPrize {
+        return LottoPrize.of(lottoList.count { it in lotto.numbers }, lottoList.contains(bonusNumber))
     }
 }
