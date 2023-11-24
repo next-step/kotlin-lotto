5e41f7fc04cd3e50bec6da1e8b20ae9a3ff9ae17
yoonnyeong(최윤녕) <choiyn1994@naver.com>
2023-11-24 17:02:20 +0900

    패키지 이동

--- src/main/kotlin/lotto/WinningLotto.kt
+++
@@ -1,9 +0,0 @@
-package lotto
-
-class WinningLotto(val winningNumber: Lotto, val bonusNumber: LottoNumber) {
-    init {
-        require(!winningNumber.lottoNumbers.contains(bonusNumber)) {
-            "보너스볼 번호는 당첨 번호에 포함되지 않은 번호를 입력해주세요."
-        }
-    }
-}
