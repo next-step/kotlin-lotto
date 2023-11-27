2f356c961fd48972419239c8366e55591b3349b6
silvio.oivlis <silvio.oivlis@kakaocorp.com>
2023-11-27 21:59:53 +0900

    refactor: Rank 에 default value, named argument 적용

--- src/main/kotlin/lotto/domain/Rank.kt
+++ src/main/kotlin/lotto/domain/Rank.kt
@@ -1,12 +1,12 @@
 package lotto.domain
 
-enum class Rank(val hitCount: Int, val needBonusHit: Boolean, val prize: Long) {
-    FirstPlace(6, false, 2_000_000_000),
-    SecondPlace(5, true, 3_000_000),
-    ThirdPlace(5, false, 1_500_000),
-    FourthPlace(4, false, 50_000),
-    FifthPlace(3, false, 5_000),
-    LastPlace(0, false, 0);
+enum class Rank(val hitCount: Int, val needBonusHit: Boolean = false, val prize: Long) {
+    FirstPlace(hitCount = 6, prize = 2_000_000_000),
+    SecondPlace(hitCount = 5, needBonusHit = true, prize = 3_000_000),
+    ThirdPlace(hitCount = 5, prize = 1_500_000),
+    FourthPlace(hitCount = 4, prize = 50_000),
+    FifthPlace(hitCount = 3, prize = 5_000),
+    LastPlace(hitCount = 0, prize = 0);
 
     companion object {
         fun of(count: Int, bonusHit: Boolean): Rank {
