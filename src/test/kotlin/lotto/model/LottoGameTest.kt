2c382e97a04d66887c50b1c26f7f6f60eb3b0101
dong <dong.22@kakaoenterprise.com>
2023-11-28 23:31:09 +0900

    refactor : 도메인 클래스 기능에 대한 테스트코드 추가

--- src/test/kotlin/lotto/model/LottoGameTest.kt
+++ src/test/kotlin/lotto/model/LottoGameTest.kt
@@ -3,6 +3,7 @@ package lotto.model
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
 class LottoGameTest : StringSpec({
 
@@ -19,5 +20,22 @@ class LottoGameTest : StringSpec({
         shouldThrow<IllegalArgumentException> {
             LottoGame(11, 22, 33, 4, 5, 6, 7)
         }
+    }
+
+    "몇개의 숫자가 일치하는지를 반환해야 한다" {
+        val origin = LottoGame(1, 2, 3, 4, 5, 6)
+        val target = LottoGame(1, 2, 3, 24, 25, 26)
+
+        origin.numbersIntersections(target) shouldBe 3
+    }
+
+    "특정 숫자가 포함되어있는지 여부를 반환 해야 한다" {
+        val target = LottoGame(11, 12, 13, 24, 25, 26)
+
+        target.containNumber(LottoNumber(1)) shouldBe false
+        target.containNumber(LottoNumber(11)) shouldBe true
+        target.containNumber(LottoNumber(13)) shouldBe true
+        target.containNumber(LottoNumber(14)) shouldBe false
+        target.containNumber(LottoNumber(26)) shouldBe true
     }
 })
