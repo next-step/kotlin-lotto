a9b350bec4188e7a5d4d6d4eb2fb5e4b4ff33ec9
dong <dong.22@kakaoenterprise.com>
2023-11-28 23:15:43 +0900

    test : 리팩토링으로 인한 테스트코드 수정

--- src/test/kotlin/lotto/model/LottoOrderTest.kt
+++ src/test/kotlin/lotto/model/LottoOrderTest.kt
@@ -3,49 +3,17 @@ package lotto.model
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import lotto.view.order.LottoOrder
 
 class LottoOrderTest : StringSpec({
 
-    "자동으로 구매한 수량만큼 로또게임이 만들어진다" {
-        val actual = LottoOrder(2, listOf())
-
-        actual.totalPurchaseCount shouldBe 2
-        actual.manualIssuedGames.size shouldBe 0
-    }
-
-    "수동으로 구매한 만큼 로또 게임이 만들어진다" {
-        val games = listOf(
-            Game(LottoNumbers(1, 2, 3, 4, 5, 6)),
-            Game(LottoNumbers(1, 2, 3, 4, 5, 6))
-        )
-        val actual = LottoOrder(2, games)
-
-        actual.totalPurchaseCount shouldBe 2
-        actual.manualIssuedGames.size shouldBe 2
-    }
-
     "총 구매 수량보다 수동 구매 수량이 많게 입력되면 IllegalArgumentException throw" {
-        val games = listOf(
-            Game(LottoNumbers(1, 2, 3, 4, 5, 6)),
-            Game(LottoNumbers(1, 2, 3, 4, 5, 6))
-        )
         shouldThrow<IllegalArgumentException> {
-            LottoOrder(1, games)
+            LottoOrder(totalAmount = 14000, manualCount = 15).ticketing()
         }
     }
 
-    "발권이 불가능한 문자열이 입력되면 IllegalArgumentException throw 해야한다" {
-        shouldThrow<IllegalArgumentException> {
-            println("숫자가 5개라서 실패")
-            LottoOrder(3, listOf(Game(LottoNumbers(1, 2, 3, 4, 5))))
-        }
-        shouldThrow<IllegalArgumentException> {
-            println("숫자가 7개라서 실패")
-            LottoOrder(3, listOf(Game(LottoNumbers(1, 2, 3, 4, 5, 6, 7))))
-        }
-    }
-
-    "구매할 로또 Game 하나의 가격은 1000 이다" {
-        LottoOrder.priceOfGame() shouldBe 1000
+    "구매할 LottoGame 하나의 가격은 1000 이다" {
+        LottoOrder(11, 2, 9).pricePerGame() shouldBe 1000
     }
 })
