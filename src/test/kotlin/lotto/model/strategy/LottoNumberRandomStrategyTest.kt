07235479e4d10247ffd93717cfb77392ae71eb38
dong <dong.22@kakaoenterprise.com>
2023-11-28 23:14:58 +0900

    test : 리팩토링으로 인한 테스트코드 수정

--- src/test/kotlin/lotto/model/strategy/LottoNumberRandomStrategyTest.kt
+++ src/test/kotlin/lotto/model/strategy/LottoNumberRandomStrategyTest.kt
@@ -7,7 +7,7 @@ class LottoNumberRandomStrategyTest : StringSpec({
     "LottoNumber 의 value 가 1~46 사이에 있다" {
         shouldNotThrow<IllegalArgumentException> {
             (1..100).forEach { _ ->
-                LottoNumberRandomStrategy.pick()
+                LottoNumbersRandomStrategy.pick()
             }
         }
     }
