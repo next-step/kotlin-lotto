15dece707ac697f357429783a2409b8198af3492
silvio.oivlis <silvio.oivlis@kakaocorp.com>
2023-11-28 20:55:07 +0900

    refactor: rankResult 생성 구문 간소화

--- src/main/kotlin/lotto/domain/LottoTickets.kt
+++ src/main/kotlin/lotto/domain/LottoTickets.kt
@@ -3,12 +3,10 @@ package lotto.domain
 data class LottoTickets(val lottoTicketList: List<LottoTicket>) {
 
     fun getRankResult(winningNumber: WinningNumber): RankResult {
-        val result = mutableMapOf<Rank, Int>()
+        val result = lottoTicketList.groupingBy {
+            it.getRank(winningNumber)
+        }.eachCount()
 
-        lottoTicketList.forEach { lottoTicket ->
-            val rank = lottoTicket.getRank(winningNumber)
-            result[rank] = (result[rank] ?: 0) + 1
-        }
         return RankResult(result)
     }
 }
