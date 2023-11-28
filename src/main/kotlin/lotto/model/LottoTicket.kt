507c1e45130f7eb077430dd883942e17ba15c3cc
dong <dong.22@kakaoenterprise.com>
2023-11-28 23:16:32 +0900

    feat : 클래스명 수정

--- src/main/kotlin/lotto/model/LottoTicket.kt
+++ src/main/kotlin/lotto/model/LottoTicket.kt
@@ -1,24 +1,20 @@
 package lotto.model
 
 data class LottoTicket(
-    val games: List<Game> = emptyList()
+    val lottoGames: List<LottoGame> = emptyList()
 ) {
     fun winnerAggregate(winningNumbers: WinningNumbers): LottoWinners {
         val rankAggregated: MutableMap<Rank, Int> = mutableMapOf()
-        games.asSequence()
-            .map { winningNumbers.toRank(it.lottoNumbers) }
+        lottoGames.asSequence()
+            .map { winningNumbers.toRank(it) }
             .forEach { rankAggregated[it] = rankAggregated.getOrDefault(it, 0) + 1 }
         return LottoWinners(
-            totalGameCount = this.games.size,
+            totalGameCount = this.lottoGames.size,
             rankAggregated
         )
     }
 
     operator fun plus(other: LottoTicket): LottoTicket {
-        return LottoTicket(this.games + other.games)
-    }
-
-    fun gamesCount(): Int {
-        return this.games.size
+        return LottoTicket(this.lottoGames + other.lottoGames)
     }
 }
