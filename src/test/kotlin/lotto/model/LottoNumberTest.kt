ffc035ef093e2648a9888a1d6bc7f7f48f01341e
dong <dong.22@kakaoenterprise.com>
2023-11-28 23:31:49 +0900

    refactor : 불필요한 주석 제거

--- src/test/kotlin/lotto/model/LottoNumberTest.kt
+++ src/test/kotlin/lotto/model/LottoNumberTest.kt
@@ -26,25 +26,4 @@ class LottoNumberTest : StringSpec({
             LottoNumber(50)
         }
     }
-
-//    "LottoNumberRandomStrategy 전략 주입시, 랜덤한 LottoNumber 가 생성 되어야 한다" {
-//        shouldNotThrow<IllegalArgumentException> {
-//            (1..100).forEach { _ ->
-//                val actual = LottoNumber(LottoNumbersRandomStrategy)
-//                actual.value shouldBeIn (1..46).toSet()
-//            }
-//        }
-//    }
-//
-//    "LottoNumberRandomWithExceptStrategy 전략 주입시, 특정 숫자를 제외한 나머지 숫자들 중에서 랜덤 하게 LottoNumber 가 생성 되어야 한다" {
-//        shouldNotThrow<IllegalArgumentException> {
-//            (1..100).forEach { _ ->
-//                val actual = LottoNumber(
-//                    LottoNumbersRandomWithExceptStrategy(setOf(11, 12, 22, 23, 33, 34, 44, 45))
-//                )
-//                actual.value shouldBeIn (1..46).toSet()
-//                actual.value shouldNotBeIn setOf(11, 12, 22, 23, 33, 34, 44, 45)
-//            }
-//        }
-//    }
 })
