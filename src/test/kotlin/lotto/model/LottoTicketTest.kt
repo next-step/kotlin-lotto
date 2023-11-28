bcf4152d03b3a82a985b9db1349bb6f0df671648
dong <dong.22@kakaoenterprise.com>
2023-11-28 23:37:57 +0900

    test : 리팩토링으로 인해 깨진 테스트코드 하나를 복구하였다

--- src/test/kotlin/lotto/model/LottoTicketTest.kt
+++ src/test/kotlin/lotto/model/LottoTicketTest.kt
@@ -1,19 +1,26 @@
 package lotto.model
 
-// class LottoTicketTest : StringSpec({
-//
-//    "6개의 로또 번호가 일치 하면 1등 당첨자로 집계 해야 한다" {
-//        val firstWinningNumbers = LottoGame(1, 2, 3, 4, 5, 6)
-//        val ticket = LottoTicket(LottoAutomaticOrderStrategy.issue(20))
-//        val winingNumbers = WinningNumbers(firstWinningNumbers, LottoNumber(7))
-//        val actual = ticket.winnerAggregate(winingNumbers)
-//
-//        actual.countOfRank(Rank.FIRST) shouldBe 1
-//        actual.countOfRank(Rank.SECOND) shouldBe 0
-//        actual.countOfRank(Rank.THIRD) shouldBe 0
-//        actual.countOfRank(Rank.FOURTH) shouldBe 0
-//        actual.countOfRank(Rank.FIFTH) shouldBe 0
-//    }
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
+import lotto.view.order.impl.LottoAutomaticOrderStrategy
+
+class LottoTicketTest : StringSpec({
+
+    "6개의 로또 번호가 일치 하면 1등 당첨자로 집계 해야 한다" {
+        val ticket = LottoTicket(
+            LottoAutomaticOrderStrategy.issue(20) +
+                    LottoGame(1, 2, 3, 4, 5, 6)
+        )
+        val winingNumbers = WinningNumbers(LottoGame(1, 2, 3, 4, 5, 6), LottoNumber(7))
+        val actual = ticket.winnerAggregate(winingNumbers)
+
+        actual.countOfRank(Rank.FIRST) shouldBeGreaterThanOrEqual 1
+        actual.countOfRank(Rank.SECOND) shouldBeGreaterThanOrEqual 0
+        actual.countOfRank(Rank.THIRD) shouldBeGreaterThanOrEqual 0
+        actual.countOfRank(Rank.FOURTH) shouldBeGreaterThanOrEqual 0
+        actual.countOfRank(Rank.FIFTH) shouldBeGreaterThanOrEqual 0
+    }
+
 //
 //    "5개의 로또 번호가 일치 하고 보너스볼이 일치하면 2등 당첨자로 집계 해야 한다" {
 //        val secondWinningNumbers = listOf(1, 2, 3, 4, 5, 22)
