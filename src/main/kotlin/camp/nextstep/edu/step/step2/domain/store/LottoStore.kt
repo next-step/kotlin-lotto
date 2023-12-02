afc59b8a74881a70fa35201df6e6baf3ba04e9be
kimueibin <at365@naver.com>
2023-12-02 23:16:56 +0900

    refactor : 로또 상점 ( Lotto Store ) 내, 변경된 도메인들에 따른 로직 수정
    결과확인, 금액 확인, 번호생성과 수량 체크 추가 및 수정 진행

--- src/main/kotlin/camp/nextstep/edu/step/step2/domain/store/LottoStore.kt
+++ src/main/kotlin/camp/nextstep/edu/step/step2/domain/store/LottoStore.kt
@@ -1,10 +1,18 @@
 package camp.nextstep.edu.step.step2.domain.store
 
 import camp.nextstep.edu.step.step2.domain.amount.BuyAmount
+import camp.nextstep.edu.step.step2.domain.lotto.Lotto
+import camp.nextstep.edu.step.step2.domain.lotto.Lottos
 import camp.nextstep.edu.step.step2.domain.lotto.WinningLotto
+import camp.nextstep.edu.step.step2.domain.result.LottoMatch
 import camp.nextstep.edu.step.step2.domain.result.LottoResult
+import camp.nextstep.edu.step.step2.generator.NumberGenerator
 
 object LottoStore {
+
+    private const val START_NUMBER = 1
+    private const val END_NUMBER = 45
+
     /**
      * @description : 로또 장당의 가격은 정부에서 정해진 정책만큼 가격이 달라지듯이 해당 미션에서는 각 Store마다 가격이 다르다고 가정했다.
      */
@@ -13,21 +21,64 @@ object LottoStore {
     /**
      * @description : 구매금액에 따른 로또 티켓 수량을 구한다.
      */
-    fun buyLottoTickets(buyAmount: BuyAmount): LottoTickets {
+    fun buyLottoTickets(buyAmount: BuyAmount): LottoTicketAmount {
         val lottoTicketAmount = buyAmount.divideByLotteryPrice(ticketPrice = LOTTO_TICKET_PRICE)
-        return LottoTickets(lottoTicketAmount)
+        return LottoTicketAmount(lottoTicketAmount)
+    }
+
+    /**
+     * @description : 로또 티켓 수량에 따른 로또 번호를 생성한다.
+     */
+    fun createNumbersByLottoTicketAmount(ticketAmount: LottoTicketAmount): Lottos {
+        val lottoTickets = mutableListOf<Lotto>()
+
+        for (i in 1..ticketAmount.lottoTicketAmount) {
+            val numbers = NumberGenerator.generate(NumberGenerator.LOTTO_RANDOM, START_NUMBER, END_NUMBER)
+            lottoTickets.add(Lotto(numbers = numbers))
+        }
+
+        return Lottos(lottos = lottoTickets)
     }
 
     /**
      * @description : 로또의 당첨결과르 확인한다. ( 보통 로또의 경우 구매처에서 용지를 넣고 확인하는 방식을 활용했다. )
      */
-    fun checkLottoTicketsWinningResult(winningLotto: WinningLotto): LottoResult {
+    fun checkLottoTicketsWinningResult(
+        userLottos: Lottos,
+        winningLotto: WinningLotto
+    ): LottoResult {
 
-        val totalPrice =
-            winningLotto.getTotalPriceByLottoAmountAndTicketPrice(ticketPrice = LOTTO_TICKET_PRICE)
-        val matchResult = winningLotto.getLottoMatchResult()
+        val totalPrice = getTotalPriceByLottoAmountAndTicketPrice(
+            lottoSize = userLottos.getLottoSize(),
+            ticketPrice = LOTTO_TICKET_PRICE
+        )
+
+        val matchResult = lottoWinningResults(
+            userlottos = userLottos,
+            winningWinningLotto = winningLotto
+        )
 
         return LottoResult(totalPrice, matchResult)
+    }
+
+    /**
+     * @description : 로또의 수량과 가격을 곱하여 총 금액을 구한다. ( 로또 판매처에서 해당 책임이 부여가 되어야한다고 생각했다. )
+     */
+    private fun getTotalPriceByLottoAmountAndTicketPrice(lottoSize: Int, ticketPrice: Long): Int {
+        return lottoSize * ticketPrice.toInt()
+    }
+
+    /**
+     * @description : 로또의 결과를 확인합니다. ( 보통 로또 판매처에 내가 구매한 로또들을 넣고 확인하는 방식을 생각했다. )
+     */
+    private fun lottoWinningResults(
+        userlottos: Lottos,
+        winningWinningLotto: WinningLotto
+    ): List<LottoMatch> {
+        return userlottos.lottos
+            .map { numbers -> numbers.countMatch(winningWinningLotto.winningLotto) }
+            .map { LottoMatch.of(it) }
+            .sorted()
     }
 
 }
