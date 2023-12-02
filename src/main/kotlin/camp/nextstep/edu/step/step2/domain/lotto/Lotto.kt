dc3f21d407b9ac3de1fcd788854f04541e379ea2
kimueibin <at365@naver.com>
2023-12-02 23:11:59 +0900

    refactor : 로또 도메인 내 클래스, 내부 비지니스 로직 수정
    value Class -> Class 수정과 로또 번호 생성 메서드 제거

--- src/main/kotlin/camp/nextstep/edu/step/step2/domain/lotto/Lotto.kt
+++ src/main/kotlin/camp/nextstep/edu/step/step2/domain/lotto/Lotto.kt
@@ -1,39 +1,23 @@
 package camp.nextstep.edu.step.step2.domain.lotto
 
-import camp.nextstep.edu.step.step2.generator.NumberGenerator
-import camp.nextstep.edu.step.step2.generator.NumberGenerator.LOTTO_RANDOM
-
-@JvmInline
-value class Lotto(
+class Lotto(
     val numbers: List<Number>
 ) {
+
+    fun countMatch(targetLotto: Lotto): Int {
+        return numbers.count { targetLotto.numbers.contains(it) }
+    }
 
     fun getNumberElements(): List<Int> {
         return numbers.map { it.number }
     }
 
-    fun countMatch(targetLotto: Lotto): Int {
-        return numbers.count { targetLotto.getNumberElements().contains(it.number) }
-    }
-
     companion object {
-
         fun ofInputValues(numbers: String): Lotto {
             val numberList = numbers.split(",")
                 .map { it.trim() }
-                .map { Number(number = it.toInt()) }
-            return Lotto(numbers = numberList)
-        }
-
-        /**
-         * @description : 로또 번호를 자동 생성한다.
-         */
-        fun generateAutoNumbers(): Lotto {
-            val numbers = NumberGenerator.generate(LOTTO_RANDOM)
-
-            val lottoNums = numbers.map { Number(number = it) }
-
-            return Lotto(numbers = lottoNums)
+                .map { it.toInt() }
+            return Lotto(numbers = numberList.map { Number(it) })
         }
     }
 
