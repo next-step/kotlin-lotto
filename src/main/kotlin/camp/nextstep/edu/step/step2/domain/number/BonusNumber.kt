d89078fe7322c0e9229951edd5f1a8a04f681c23
Ueibin Kim <at365@naver.com>
2023-12-06 02:10:27 +0900

    feat : 보너스 숫자 도메인 추가

---
+++ src/main/kotlin/camp/nextstep/edu/step/step2/domain/number/BonusNumber.kt
@@ -0,0 +1,12 @@
+package camp.nextstep.edu.step.step2.domain.number
+
+
+class BonusNumber(
+    val number: Number
+) {
+
+    fun getNumberElement(): Int {
+        return number.number
+    }
+
+}
