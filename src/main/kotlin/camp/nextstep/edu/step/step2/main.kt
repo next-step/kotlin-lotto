d047c1cd3158c3ab727cd43c9731de8697215fc7
kimueibin <at365@naver.com>
2023-12-06 23:56:19 +0900

    refactor : Main 함수 내 비지니스 흐름별 메서드 분리 진행
    티켓 구매 -> 로또 진행 -> 결과 순으로 비지니스가 눈에 보일 수 있도록 private 메서드로 분리

--- src/main/kotlin/camp/nextstep/edu/step/step2/main.kt
+++ src/main/kotlin/camp/nextstep/edu/step/step2/main.kt
@@ -1,34 +1,47 @@
 package camp.nextstep.edu.step.step2
 
+import camp.nextstep.edu.step.step2.domain.amount.BuyAmount
+import camp.nextstep.edu.step.step2.domain.lotto.Lottos
 import camp.nextstep.edu.step.step2.domain.lotto.WinningLotto
 import camp.nextstep.edu.step.step2.domain.store.LottoStore
 import camp.nextstep.edu.step.step2.view.InputView
 import camp.nextstep.edu.step.step2.view.OutputView
 
-fun main() {
-    val inputView = InputView()
-    val outputView = OutputView()
 
-    val buyAmount = inputView.getInputValueAndReturnBuyAmount()
+fun main() {
+    val buyAmount = InputView.getInputValueAndReturnBuyAmount()
+
+    val lottos = buyLottoTicketsWithAmount(buyAmount = buyAmount)
+
+    checkLottoTicketsWinningResult(lottos = lottos)
+}
+
+private fun buyLottoTicketsWithAmount(buyAmount: BuyAmount): Lottos {
     val tickets = LottoStore.buyLottoTickets(buyAmount = buyAmount)
 
     val lottos = LottoStore.createNumbersByLottoTicketAmount(ticketAmount = tickets)
 
     val ticketElements = lottos.getLottoElements()
 
-    outputView.displayTicketsNumbers(
+    OutputView.displayTicketsNumbers(
         ticketsAmount = tickets.lottoTicketAmount,
         ticketNumbers = ticketElements
     )
 
-    val lastWeekWinningNumbers = inputView.inputLastWeekWinningNumbers()
+    return lottos
+}
+
+private fun checkLottoTicketsWinningResult(lottos: Lottos) {
+    val lastWeekWinningNumbers = InputView.inputLastWeekWinningNumbers()
+    val bonusNumber = InputView.inputBonusNumber()
 
     val lottoResult = LottoStore.checkLottoTicketsWinningResult(
         userLottos = lottos,
         winningLotto = WinningLotto(
-            winningLotto = lastWeekWinningNumbers
+            winningLotto = lastWeekWinningNumbers,
+            bonusNumber = bonusNumber
         )
     )
 
-    outputView.displayLottoResultByDto(lottoResultDto = lottoResult.calculateResultAndReturnDto())
+    OutputView.displayLottoResultByDto(lottoResultDto = lottoResult.calculateResultAndReturnDto())
 }
