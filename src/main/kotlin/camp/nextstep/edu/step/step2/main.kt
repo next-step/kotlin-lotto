a9f8131af01194d8d96eb77232c84cf7f0d9673a
kimueibin <at365@naver.com>
2023-12-27 00:36:40 +0900

    refactor : 수동 로또 구매를 추가한 main 함수 수정

--- src/main/kotlin/camp/nextstep/edu/step/step2/main.kt
+++ src/main/kotlin/camp/nextstep/edu/step/step2/main.kt
@@ -4,28 +4,40 @@ import camp.nextstep.edu.step.step2.domain.amount.BuyAmount
 import camp.nextstep.edu.step.step2.domain.lotto.Lottos
 import camp.nextstep.edu.step.step2.domain.lotto.WinningLotto
 import camp.nextstep.edu.step.step2.domain.store.LottoStore
+import camp.nextstep.edu.step.step2.dto.LottoProcessDto
 import camp.nextstep.edu.step.step2.view.InputView
 import camp.nextstep.edu.step.step2.view.OutputView
 
 
 fun main() {
     val buyAmount = InputView.getInputValueAndReturnBuyAmount()
+    val manualLottoCount = InputView.inputManualLottoCount()
+    val manualLottos = InputView.inputManalLottoNumbers(ticketAmount = manualLottoCount.amount)
 
-    val lottos = buyLottoTicketsWithAmount(buyAmount = buyAmount)
+    val lottos = buyLottoTicketsWithAmount(buyAmount = buyAmount, manualLottos = manualLottos)
 
     checkLottoTicketsWinningResult(lottos = lottos)
 }
 
-private fun buyLottoTicketsWithAmount(buyAmount: BuyAmount): Lottos {
-    val tickets = LottoStore.buyLottoTickets(buyAmount = buyAmount)
+private fun buyLottoTicketsWithAmount(buyAmount: BuyAmount, manualLottos: Lottos): Lottos {
+    val tickets = LottoStore.buyAutoLottoTickets(
+        buyAmount = buyAmount,
+        manualTicketAmount = manualLottos.getLottoSize()
+    )
+    val lottos = LottoStore.createNumbersByLottoTicketAmount(
+        ticketAmount = tickets,
+        manualLottos = manualLottos
+    )
 
-    val lottos = LottoStore.createNumbersByLottoTicketAmount(ticketAmount = tickets)
-
-    val ticketElements = lottos.getLottoElements()
+    val autoLottoNumbers = lottos.getLottoElements()
+    val manualLottoNumbers = manualLottos.getLottoElements()
 
     OutputView.displayTicketsNumbers(
-        ticketsAmount = tickets.lottoTicketAmount,
-        ticketNumbers = ticketElements
+        LottoProcessDto(
+            manualTicketAmount = manualLottos.getLottoSize(),
+            autoTicketAmount = tickets.lottoTicketAmount,
+            lottoTicketList = manualLottoNumbers + autoLottoNumbers
+        )
     )
 
     return lottos
