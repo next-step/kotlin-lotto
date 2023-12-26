4c17c65fd235024222a0819398b7d85a5754d14a
kimueibin <at365@naver.com>
2023-12-27 00:31:25 +0900

    style : Code Formatter를 통한 스타일 수정

--- src/main/kotlin/camp/nextstep/edu/step/step2/domain/result/LottoMatch.kt
+++ src/main/kotlin/camp/nextstep/edu/step/step2/domain/result/LottoMatch.kt
@@ -8,15 +8,16 @@ enum class LottoMatch(
     SIX_MATCH(6, 0, 2_000_000_000),
     FIVE_MATCH(5, 0, 1_500_000),
     FIVE_MATCH_WITH_BONUS(5, 1, 30_000_000),
-    FOUR_MATCH(4, 0,50_000),
-    THREE_MATCH(3, 0,5_000),
-    NONE(0, 0,0);
+    FOUR_MATCH(4, 0, 50_000),
+    THREE_MATCH(3, 0, 5_000),
+    NONE(0, 0, 0);
 
 
     companion object {
         fun of(matchCount: Int, bonusMatch: Int): LottoMatch {
-            return values().find { it.matchCount == matchCount && it.bonusMatch == bonusMatch } ?: NONE
+            return values().find { it.matchCount == matchCount && it.bonusMatch == bonusMatch }
+                ?: NONE
         }
     }
-    
+
 }
