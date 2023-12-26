6d68d71a3a6645f9125dd053d126fb981233b368
kimueibin <at365@naver.com>
2023-12-27 00:36:18 +0900

    test : 구입금액, 로또 개수를 통한 구매 테스트 진행

--- src/test/kotlin/camp/nextstep/edu/step/step2/domain/store/LottoStoreTest.kt
+++ src/test/kotlin/camp/nextstep/edu/step/step2/domain/store/LottoStoreTest.kt
@@ -15,14 +15,15 @@ import java.math.BigDecimal
 @DisplayName("로또 상점은")
 class LottoStoreTest : BehaviorSpec({
 
-    Given("구입 금액이 주어지고") {
+    Given("구입 금액과 수동 로또 개수가 주어지고") {
         val buyAmount = BuyAmount(amount = BigDecimal.valueOf(10000L))
+        val manualTicketAmount = 2
 
         When("로또를 구매하면") {
-            val lottoTickets = LottoStore.buyLottoTickets(buyAmount)
+            val lottoTickets = LottoStore.buyAutoLottoTickets(buyAmount, manualTicketAmount)
 
             Then("구매한 금액만큼의 로또 티켓 수를 반환한다.") {
-                lottoTickets.lottoTicketAmount shouldBe 10
+                lottoTickets.lottoTicketAmount shouldBe 8
             }
         }
     }
@@ -82,12 +83,24 @@ class LottoStoreTest : BehaviorSpec({
         }
     }
 
-    Given("로또의 수량이주어지고") {
+    Given("로또의 수량과 수동 로또번호가 주어지고") {
         val lottoTicketAmount = LottoTicketAmount(lottoTicketAmount = 10)
+        val manualLotto = Lottos( lottos = listOf(
+            Lotto(
+                listOf(
+                    Number(number = 1),
+                    Number(number = 2),
+                    Number(number = 3),
+                    Number(number = 4),
+                    Number(number = 5),
+                    Number(number = 6)
+                )
+            )
+        ))
 
         When("로또 발급을 시도하면") {
             val lottoTickets =
-                LottoStore.createNumbersByLottoTicketAmount(ticketAmount = lottoTicketAmount)
+                LottoStore.createNumbersByLottoTicketAmount(ticketAmount = lottoTicketAmount, manualLottos = manualLotto)
 
             Then("구매한 수량만큼의 로또가 발급된다.") {
                 lottoTickets.lottos.size shouldBe 10
@@ -148,5 +161,4 @@ class LottoStoreTest : BehaviorSpec({
             }
         }
     }
-
 })
