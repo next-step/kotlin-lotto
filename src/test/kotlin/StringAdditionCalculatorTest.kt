b1fa23ea500b4559308361df89e6dc7e5b260285
KimDabomi <cdabomi@nate.com>
2024-11-19 23:47:44 +0900

    [step1] feat: 숫자 이외의 값을 전달할 경우 RuntimeException 예외 발생

--- src/test/kotlin/StringAdditionCalculatorTest.kt
+++ src/test/kotlin/StringAdditionCalculatorTest.kt
@@ -1,3 +1,4 @@
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
@@ -7,7 +8,13 @@ class StringAdditionCalculator(private val str: String?) {
         val matchResult = Regex("//(.)\n(.*)").find(str)
         val separator = matchResult?.groupValues?.get(1)
         val numbers = matchResult?.groupValues?.get(2)?.split(",|:|${separator?.let { Regex.escape(it) }}".toRegex()) ?: str.split(",|:".toRegex())
+        if (numbers.any { it.matches(NOT_NUMBER_REGEX) }) throw RuntimeException(NOT_NUMBER_MESSAGE)
         return if (numbers.size == 1 && separator == null) str.toInt() else numbers.sumOf { it.toInt() }
+    }
+
+    companion object {
+        private val NOT_NUMBER_REGEX = Regex("[^0-9]")
+        private const val NOT_NUMBER_MESSAGE = "숫자 이외의 값을 입력할 수 없습니다."
     }
 }
 
@@ -39,5 +46,12 @@ class StringAdditionCalculatorTest : StringSpec ({
     "\"//\"와 \"\\n\" 문자 사이에 커스텀 구분자를 지정할 수 있다." {
         StringAdditionCalculator("//;\n1;2;3").add() shouldBe 6
         StringAdditionCalculator("//-\n1-2-3").add() shouldBe 6
+    }
+
+    "숫자 이외의 값을 전달할 경우 RuntimeException 예외가 발생해야 한다." {
+        val exception = shouldThrow<RuntimeException> {
+            StringAdditionCalculator("1,2,a").add()
+        }
+        exception.message shouldBe "숫자 이외의 값을 입력할 수 없습니다."
     }
 })
