af677d914e5d29e22f9541b09f0fcf2e6f87803f
heesang.lee <sky1994601@hanmail.net>
2024-11-20 19:53:11 +0900

    feat: step1 문자열 덧셈 계산기
    요구사항: '//와\n 사이에 문자를 지정하면 그 문자도 구분자에 추가한다' 구현

--- src/main/kotlin/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/stringcalculator/StringCalculator.kt
@@ -2,7 +2,23 @@ package stringcalculator
 
 class StringCalculator {
     fun calculate(inputString: String): Int {
-        val numbers = inputString.split(",", ":")
+        val delimiters = findDelimiter(inputString)
+        val numbers = excludeDelimiter(inputString).split(*delimiters.toTypedArray())
         return numbers.sumOf { it.toInt() }
+    }
+
+    fun findDelimiter(inputString: String): List<String> {
+        val delimiters = mutableListOf(",", ":")
+        val startIndex = inputString.indexOf("//")
+        val endIndex = inputString.indexOf("\n")
+        if (startIndex == -1 && endIndex == -1) {
+            return delimiters
+        }
+        delimiters.addAll(inputString.substring(startIndex + 2, endIndex).toCharArray().map { it.toString() })
+        return delimiters
+    }
+
+    fun excludeDelimiter(inputString: String): String {
+        return inputString.substring(inputString.indexOf("\n") + 1)
     }
 }
--- src/test/kotlin/stringcalculator/StringCalculatorTest.kt
+++ src/test/kotlin/stringcalculator/StringCalculatorTest.kt
@@ -4,11 +4,16 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class StringCalculatorTest {
-
     @Test
     fun `정해진 구분자로 숫자 string이 오면 모든 숫자를 더한다`() {
         val stringCalculator = StringCalculator()
         assertThat(stringCalculator.calculate("1,2,3")).isEqualTo(6)
         assertThat(stringCalculator.calculate("1:2:3")).isEqualTo(6)
+    }
+
+    @Test
+    fun `정해진 문자 사이에 문자를 지정하면 그 문자도 구분자에 추가한다`() {
+        val stringCalculator = StringCalculator()
+        assertThat(stringCalculator.calculate("//;\n1;2;3")).isEqualTo(6)
     }
 }
