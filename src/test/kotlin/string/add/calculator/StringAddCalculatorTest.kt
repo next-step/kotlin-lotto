db9555eac443bcf09ad61dfeb49f32c07a382858
Giwan Kim <giwan.kim@gmail.com>
2024-11-20 09:38:25 +0900

    feat: 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.

--- src/test/kotlin/string/add/calculator/StringAddCalculatorTest.kt
+++ src/test/kotlin/string/add/calculator/StringAddCalculatorTest.kt
@@ -3,9 +3,10 @@ package string.add.calculator
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
 
 class StringAddCalculator {
-    fun calculate(text: String?): Int = 0
+    fun calculate(text: String?): Int = if (text.isNullOrBlank()) 0 else text.toInt()
 }
 
 @Suppress("NonAsciiCharacters")
@@ -15,5 +16,12 @@ class StringAddCalculatorTest {
     fun `빈 문자열 또는 null 을 입력할 경우 0을 반환해야 한다`(text: String?) {
         val sum = StringAddCalculator().calculate(text)
         sum shouldBe 0
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["0", "1", "42"])
+    fun `숫자 하나를 입력한 경우 해당 숫자를 반환한다`(text: String) {
+        val sum = StringAddCalculator().calculate(text)
+        sum shouldBe text.toInt()
     }
 }
