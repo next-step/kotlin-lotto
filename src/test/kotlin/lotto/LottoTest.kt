0840448607cf955c8ebadf20a7d033018e9dc5d7
KimDabomi <cdabomi@nate.com>
2024-11-20 19:34:03 +0900

    [step2] feat: 구매한 로또 목록에서 당첨 번호와 일치하는 게 3개이면 5등

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,6 +1,5 @@
 package lotto
 
-import calculator.StringAdditionCalculator
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
@@ -14,8 +13,8 @@ class Lotto(private val money: Int) {
         return money / LOTTO_PRICE_UNIT
     }
 
-    fun lottoIssuance(): List<LottoTicket> {
-        return (1..buyCount()).map { LottoTicket() }
+    fun lottoIssuance(lottoTicket: LottoTicket): List<LottoTicket> {
+        return (1..buyCount()).map { lottoTicket }
     }
 
     companion object {
@@ -30,7 +29,26 @@ class LottoTicket {
     fun numbers(): List<Int> {
         return lottoNumbers
     }
+
+    fun matchCount(winningNumbers: List<Int>): Int {
+        return numbers().intersect(winningNumbers.toSet()).size
+    }
 }
+
+enum class LottoRank(val matchCount: Int, val prize: Int) {
+    FIRST(6, 2_000_000_000),
+    THIRD(5, 30_000_000),
+    FOURTH(4, 1_500_000),
+    FIFTH(3, 50_000),
+    NONE(0, 0);
+
+    companion object {
+        fun from(matchCount: Int): LottoRank {
+            return values().find { it.matchCount == matchCount } ?: NONE
+        }
+    }
+}
+
 
 class LottoTest : StringSpec({
     "구매금액을 입력하면 몇 개를 구매했는지 반환해야 한다." {
@@ -48,7 +66,21 @@ class LottoTest : StringSpec({
 
     "구매한 개수만큼 로또를 발급해야 한다." {
         val lotto = Lotto(14000)
-        val lottoList = lotto.lottoIssuance().map { it.numbers() }
+        val lottoTicket = LottoTicket()
+        val lottoList = lotto.lottoIssuance(lottoTicket).map { it.numbers() }
         lottoList.size shouldBe 14
+    }
+
+    "구매한 로또 목록에서 당첨 번호와 일치하는 게 3개이면 5등이다. (5000원)" {
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val lottoTickets = List(14000) { LottoTicket() }
+
+        val matchingTicket = lottoTickets.first { ticket ->
+            ticket.matchCount(winningNumbers) == 3
+        }
+
+        val rank = LottoRank.from(matchingTicket.matchCount(winningNumbers))
+        rank shouldBe LottoRank.FIFTH
+        rank.prize shouldBe 50_000
     }
 })
