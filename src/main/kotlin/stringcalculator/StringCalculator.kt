cceee5b843903461c45afaa2bba68eaf61ab2d4a
heesang.lee <sky1994601@hanmail.net>
2024-11-20 20:00:53 +0900

    feat: step1 문자열 덧셈 계산기
    요구사항: '문자열의 음수가 오는 경우 예외를 던진다' 구현

--- src/main/kotlin/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/stringcalculator/StringCalculator.kt
@@ -4,10 +4,11 @@ class StringCalculator {
     fun calculate(inputString: String): Int {
         val delimiters = findDelimiter(inputString)
         val numbers = excludeDelimiter(inputString).split(*delimiters.toTypedArray())
+        require(!isMinusInNumbers(numbers))
         return numbers.sumOf { it.toInt() }
     }
 
-    fun findDelimiter(inputString: String): List<String> {
+    private fun findDelimiter(inputString: String): List<String> {
         val delimiters = mutableListOf(",", ":")
         val startIndex = inputString.indexOf("//")
         val endIndex = inputString.indexOf("\n")
@@ -18,7 +19,11 @@ class StringCalculator {
         return delimiters
     }
 
-    fun excludeDelimiter(inputString: String): String {
+    private fun excludeDelimiter(inputString: String): String {
         return inputString.substring(inputString.indexOf("\n") + 1)
+    }
+
+    private fun isMinusInNumbers(numberStrings: List<String>): Boolean {
+        return numberStrings.any { it.toInt() < 0 }
     }
 }
--- src/test/kotlin/stringcalculator/StringCalculatorTest.kt
+++ src/test/kotlin/stringcalculator/StringCalculatorTest.kt
@@ -1,6 +1,7 @@
 package stringcalculator
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 class StringCalculatorTest {
@@ -15,5 +16,11 @@ class StringCalculatorTest {
     fun `정해진 문자 사이에 문자를 지정하면 그 문자도 구분자에 추가한다`() {
         val stringCalculator = StringCalculator()
         assertThat(stringCalculator.calculate("//;\n1;2;3")).isEqualTo(6)
+    }
+
+    @Test
+    fun `문자열의 음수가 오는 경우 예외를 던진다`() {
+        val stringCalculator = StringCalculator()
+        assertThatThrownBy { stringCalculator.calculate("-1,2,3") }.isInstanceOf(IllegalArgumentException::class.java)
     }
 }
