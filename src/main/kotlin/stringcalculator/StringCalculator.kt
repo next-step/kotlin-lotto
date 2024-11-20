62ef25a7528fd47c3e3e4051ee29a2d32a66d23a
heesang.lee <sky1994601@hanmail.net>
2024-11-20 20:15:23 +0900

    feat: step1 문자열 덧셈 계산기
    요구사항: '문자열의 숫자 자리가 문자가 오면 예외를 던진다' 구현

--- src/main/kotlin/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/stringcalculator/StringCalculator.kt
@@ -1,10 +1,12 @@
 package stringcalculator
 
 class StringCalculator {
-    fun calculate(inputString: String): Int {
+    fun calculate(inputString: String?): Int {
+        if (inputString.isNullOrBlank()) return 0
         val delimiters = findDelimiter(inputString)
         val numbers = excludeDelimiter(inputString).split(*delimiters.toTypedArray())
-        require(!isMinusInNumbers(numbers))
+        require(isAllDigit(numbers)) {"숫자가 아니에요"}
+        require(!isMinusInNumbers(numbers)) {"음수가 입력으로 들어왔어요"}
         return numbers.sumOf { it.toInt() }
     }
 
@@ -25,5 +27,11 @@ class StringCalculator {
 
     private fun isMinusInNumbers(numberStrings: List<String>): Boolean {
         return numberStrings.any { it.toInt() < 0 }
+    }
+
+    private fun isAllDigit(numbers: List<String>): Boolean {
+        return numbers.all {
+            it.toCharArray().all { c -> c.isDigit() }
+        }
     }
 }
--- src/test/kotlin/stringcalculator/StringCalculatorTest.kt
+++ src/test/kotlin/stringcalculator/StringCalculatorTest.kt
@@ -16,11 +16,22 @@ class StringCalculatorTest {
     fun `정해진 문자 사이에 문자를 지정하면 그 문자도 구분자에 추가한다`() {
         val stringCalculator = StringCalculator()
         assertThat(stringCalculator.calculate("//;\n1;2;3")).isEqualTo(6)
+        assertThat(stringCalculator.calculate("//;*\n1;2*3")).isEqualTo(6)
     }
 
     @Test
     fun `문자열의 음수가 오는 경우 예외를 던진다`() {
         val stringCalculator = StringCalculator()
-        assertThatThrownBy { stringCalculator.calculate("-1,2,3") }.isInstanceOf(IllegalArgumentException::class.java)
+        assertThatThrownBy { stringCalculator.calculate("-1,2,3") }.isInstanceOf(RuntimeException::class.java)
+            .hasMessage("음수가 입력으로 들어왔어요")
+        assertThatThrownBy { stringCalculator.calculate("//;\n-1,2,3") }.isInstanceOf(RuntimeException::class.java)
+            .hasMessage("음수가 입력으로 들어왔어요")
+    }
+
+    @Test
+    fun `문자열의 숫자 자리가 문자가 오면 예외를 던진다`() {
+        val stringCalculator = StringCalculator()
+        assertThatThrownBy { stringCalculator.calculate("*,2,3") }.isInstanceOf(RuntimeException::class.java)
+            .hasMessage("숫자가 아니에요")
     }
 }
