83fa38b8dac884c40fa7aff4e98d0e2c73fcb800
KimDabomi <cdabomi@nate.com>
2024-11-20 18:32:07 +0900

    [step1] refactor: 연산자를 추출 메서드 분리

--- src/main/kotlin/calculator/StringAdditionCalculator.kt
+++ src/main/kotlin/calculator/StringAdditionCalculator.kt
@@ -4,10 +4,7 @@ class StringAdditionCalculator(private val str: String?) {
     fun add(): Int {
         if (str.isNullOrBlank()) return 0
 
-        val normalizedInput = str.replace("\\n", "\n")
-        val matchResult = Regex("//(.*?)\n(.*)").find(normalizedInput)
-        val separator = matchResult?.groupValues?.get(1)
-        val numbersSection = matchResult?.groupValues?.get(2) ?: str
+        val (separator, numbersSection) = parseInput(str)
 
         val numbers = numbersSection.split(separator?.let { Regex.escape(it).toRegex() } ?: ",|:".toRegex())
         validate(numbers)
@@ -19,6 +16,14 @@ class StringAdditionCalculator(private val str: String?) {
         private val NOT_NUMBER_REGEX = Regex("[^0-9]")
         private const val NOT_NUMBER_MESSAGE = "숫자 이외의 값을 입력할 수 없습니다."
         private const val NEGATIVE_NUMBER_MESSAGE = "음수를 입력할 수 없습니다."
+
+        private fun parseInput(input: String?): Pair<String?, String> {
+            val normalizedInput = input?.replace("\\n", "\n") ?: ""
+            val matchResult = Regex("//(.*?)\n(.*)").find(normalizedInput)
+            val separator = matchResult?.groupValues?.get(1)
+            val numbersSection = matchResult?.groupValues?.get(2) ?: input ?: ""
+            return Pair(separator, numbersSection)
+        }
 
         private fun validate(numbers: List<String>) {
             if (numbers.any { it.matches(NOT_NUMBER_REGEX) }) throw IllegalArgumentException(NOT_NUMBER_MESSAGE)
