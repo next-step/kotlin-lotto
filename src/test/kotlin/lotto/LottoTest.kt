7555e9491aa5b3c86cbf4678caa11981c6bde767
KimDabomi <cdabomi@nate.com>
2024-11-20 19:56:14 +0900

    [step2] feat: 로또 번호는 6개, 1부터 45 사이의 숫자, 중복 불가여야 한다.

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -26,12 +26,24 @@ class Lotto(private val money: Int) {
 class LottoTicket(numbers: List<Int>? = null) {
     private val lottoNumbers = numbers ?: (1..45).shuffled().take(6).sorted()
 
+    init {
+        require(lottoNumbers.size == 6) { LOTTO_NUMBER_SIZE_EXCEPTION_MESSAGE }
+        require(lottoNumbers.all { it in 1..45 }) { LOTTO_NUMBER_RANGE_EXCEPTION_MESSAGE }
+        require(lottoNumbers.toSet().size == 6) { LOTTO_NUMBER_DUPLICATE_EXCEPTION_MESSAGE }
+    }
+
     fun numbers(): List<Int> {
         return lottoNumbers
     }
 
     fun matchCount(winningNumbers: List<Int>): Int {
         return numbers().intersect(winningNumbers.toSet()).size
+    }
+
+    companion object {
+        private const val LOTTO_NUMBER_SIZE_EXCEPTION_MESSAGE = "로또 번호는 6개여야 합니다."
+        private const val LOTTO_NUMBER_RANGE_EXCEPTION_MESSAGE = "로또 번호는 1부터 45까지만 가능합니다."
+        private const val LOTTO_NUMBER_DUPLICATE_EXCEPTION_MESSAGE = "로또 번호는 중복되지 않아야 합니다."
     }
 }
 
@@ -133,5 +145,17 @@ class LottoTest : StringSpec({
         val rank = LottoRank.from(matchingTicket!!.matchCount(winningNumbers))
         rank shouldBe LottoRank.FIRST
         rank.prize shouldBe 2_000_000_000
+    }
+
+    "로또 번호는 6개, 1부터 45 사이의 숫자, 중복 불가여야 한다." {
+        shouldThrow<IllegalArgumentException> {
+            LottoTicket(listOf(1, 2, 3, 4, 5))
+        }
+        shouldThrow<IllegalArgumentException> {
+            LottoTicket(listOf(1, 2, 3, 4, 5, 5))
+        }
+        shouldThrow<IllegalArgumentException> {
+            LottoTicket(listOf(1, 2, 3, 4, 5, 46))
+        }
     }
 })
