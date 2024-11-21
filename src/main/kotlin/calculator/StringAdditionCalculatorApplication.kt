733edf6bbcb9e9aa9be95db239b44e28d47c7ef5
KimDabomi <cdabomi@nate.com>
2024-11-21 21:47:21 +0900

    [step1] refactor: 메인 메서드 수정

--- src/main/kotlin/calculator/StringAdditionCalculatorApplication.kt
+++ src/main/kotlin/calculator/StringAdditionCalculatorApplication.kt
@@ -1,12 +1,7 @@
 package calculator
 
-class StringAdditionCalculatorApplication {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val str = readln()
-            val calculator = StringAdditionCalculator(str)
-            println(calculator.add())
-        }
-    }
+fun main(args: Array<String>) {
+    val str = readln()
+    val calculator = StringAdditionCalculator(str)
+    println(calculator.add())
 }
