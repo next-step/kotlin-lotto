bd5da8fa4cc85e248b4bcfca4b8f3b29fb143de7
KimDabomi <cdabomi@nate.com>
2024-11-21 21:55:13 +0900

    [step1] refactor: 인스턴스의 생성 필요 유무에 따라 메서드 선언 위치 변경

--- src/main/kotlin/calculator/StringAdditionCalculator.kt
+++ src/main/kotlin/calculator/StringAdditionCalculator.kt
@@ -1,6 +1,25 @@
 package calculator
 
 class StringAdditionCalculator {
+    fun add(str: String?): Int {
+        if (str.isNullOrBlank()) return 0
+
+        val (separator, numbersSection) = parseInput(str)
+
+        val numbers = numbersSection.split(separator?.let { Regex.escape(it).toRegex() } ?: DEFAULT_SEPARATOR.toRegex())
+        validate(numbers)
+
+        return numbers.sumOf { it.toInt() }
+    }
+
+    private fun parseInput(input: String?): Pair<String?, String> {
+        val normalizedInput = input?.replace("\\n", "\n") ?: ""
+        val matchResult = Regex("//(.*?)\n(.*)").find(normalizedInput)
+        val separator = matchResult?.groupValues?.get(CUSTOM_SEPARATOR_INDEX)
+        val numbersSection = matchResult?.groupValues?.get(NUMBERS_INDEX) ?: input ?: ""
+        return Pair(separator, numbersSection)
+    }
+
     companion object {
         private val NOT_NUMBER_REGEX = Regex("[^0-9]")
         private const val NOT_NUMBER_MESSAGE = "숫자 이외의 값을 입력할 수 없습니다."
@@ -9,28 +28,9 @@ class StringAdditionCalculator {
         private const val CUSTOM_SEPARATOR_INDEX= 1
         private const val NUMBERS_INDEX = 2
 
-        private fun parseInput(input: String?): Pair<String?, String> {
-            val normalizedInput = input?.replace("\\n", "\n") ?: ""
-            val matchResult = Regex("//(.*?)\n(.*)").find(normalizedInput)
-            val separator = matchResult?.groupValues?.get(CUSTOM_SEPARATOR_INDEX)
-            val numbersSection = matchResult?.groupValues?.get(NUMBERS_INDEX) ?: input ?: ""
-            return Pair(separator, numbersSection)
-        }
-
         private fun validate(numbers: List<String>) {
             if (numbers.any { it.matches(NOT_NUMBER_REGEX) }) throw IllegalArgumentException(NOT_NUMBER_MESSAGE)
             if (numbers.any { it.toInt() < 0 }) throw IllegalArgumentException(NEGATIVE_NUMBER_MESSAGE)
-        }
-
-        fun add(str: String?): Int {
-            if (str.isNullOrBlank()) return 0
-
-            val (separator, numbersSection) = parseInput(str)
-
-            val numbers = numbersSection.split(separator?.let { Regex.escape(it).toRegex() } ?: DEFAULT_SEPARATOR.toRegex())
-            validate(numbers)
-
-            return numbers.sumOf { it.toInt() }
         }
     }
 }
--- src/main/kotlin/calculator/StringAdditionCalculatorApplication.kt
+++ src/main/kotlin/calculator/StringAdditionCalculatorApplication.kt
@@ -2,5 +2,5 @@ package calculator
 
 fun main(args: Array<String>) {
     val str = readln()
-    println(StringAdditionCalculator.add(str))
+    println(StringAdditionCalculator().add(str))
 }
