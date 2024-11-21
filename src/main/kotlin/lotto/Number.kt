c09c32c16c4d9fe336bef7a514fd9a0576eac4b1
SeongminKimKR <gvf156@gmail.com>
2024-11-22 00:41:14 +0900

    test: 요구사항에 따라 숫자 클래스 재작성

--- src/main/kotlin/lotto/Number.kt
+++ src/main/kotlin/lotto/Number.kt
@@ -2,10 +2,11 @@ package lotto
 
 class Number(val value: Int) {
     init {
-        require(value > 0) { NUMBER_INIT_EXCEPTION_MESSAGE }
+        require(value in NUMBER_RANGE) { NUMBER_INIT_EXCEPTION_MESSAGE }
     }
 
     companion object {
-        private const val NUMBER_INIT_EXCEPTION_MESSAGE = "숫자는 0이 아닌 양수여야 합니다."
+        private const val NUMBER_INIT_EXCEPTION_MESSAGE = "숫자는 1~45의 값이어야 합니다."
+        val NUMBER_RANGE = IntRange(1, 46)
     }
 }
--- src/test/kotlin/lotto/NumberTest.kt
+++ src/test/kotlin/lotto/NumberTest.kt
@@ -2,16 +2,10 @@ package lotto
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
 
 class NumberTest : StringSpec({
-
-    "숫자는 양수의 값을 갖고 있다." {
-        Number(1).value shouldBe 1
-    }
-
-    "숫자는 0이나 음수일 수 없다." {
+    "숫자는 1~46의 숫자가 아니면 예외를 던진다." {
         shouldThrow<IllegalArgumentException> { Number(0) }
-        shouldThrow<IllegalArgumentException> { Number(-1) }
+        shouldThrow<IllegalArgumentException> { Number(47) }
     }
 })
