07bc94d28ca34c0a772651a7b84bed283e4d08fc
leekyuwon <kyuwoon369@gmail.com>
2024-11-22 00:07:26 +0900

    feat: 로또 판매점에서 구입금액으로 로또를 살 수 있게하라

---
+++ src/main/kotlin/lotto/LottoStore.kt
@@ -0,0 +1,19 @@
+package lotto
+
+class LottoStore {
+    fun calculateLottoCount(amount: Int): Int {
+        require(amount >= LOTTO_PRICE) { "구입 금액은 최소 1000원 이상이어야 합니다." }
+        return amount / LOTTO_PRICE
+    }
+
+    fun issueLottos(
+        count: Int,
+        generator: LottoNumberGenerator,
+    ): List<Lotto> {
+        return List(count) { Lotto(generator.generate()) }
+    }
+
+    companion object {
+        const val LOTTO_PRICE = 1000
+    }
+}
