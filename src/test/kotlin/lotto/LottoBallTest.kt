b0dbe6bf2ab84515cf0fdb26a333edb1b4f7a04d
dino.shin <troas96@naver.com>
2024-11-21 21:20:54 +0900

    polish: test 메서드 설명 수정

--- src/test/kotlin/lotto/LottoBallTest.kt
+++ src/test/kotlin/lotto/LottoBallTest.kt
@@ -7,7 +7,7 @@ import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class LottoBallTest : StringSpec({
-    "로또 번호는 1부터 45 사이여야 합니다." {
+    "로또 번호를 생성할 때, 입력값이  1부터 45 사이가 아니면 예외 처리한다." {
         forAll(
             row(0),
             row(46),
--- src/test/kotlin/lotto/LottoPurchaseAmountTest.kt
+++ src/test/kotlin/lotto/LottoPurchaseAmountTest.kt
@@ -7,7 +7,7 @@ import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class LottoPurchaseAmountTest : StringSpec({
-    "로또 구매 금액은 자연수 여야 합니다." {
+    "로또 구매 금액이 자연수가 아니면 예외 처리한다." {
         forAll(
             row(-1),
             row(0),
@@ -24,7 +24,7 @@ class LottoPurchaseAmountTest : StringSpec({
         }
     }
 
-    "로또 구매 금액은 1000원 단위여야 합니다." {
+    "로또 구매 금액이 1000원 단위가 아니면 예외 처리한다." {
         forAll(
             row(10),
             row(100),
