6a80cadfb540d89436e1cd0fcce6a5104e223f15
sian <chicori3@naver.com>
2024-11-21 21:24:16 +0900

    refactor: 패키지 이동

--- src/test/kotlin/calculator/StringAdditionCalculatorTest.kt
+++ src/test/kotlin/calculator/StringAdditionCalculatorTest.kt
@@ -61,24 +61,3 @@ class StringAdditionCalculatorTest : StringSpec({
         }
     }
 })
-
-class StringAdditionCalculator(
-    private val parser: TextParser,
-) {
-    fun calculate(text: String?): Int {
-        val numbers = parser.parse(text)
-        validate(numbers)
-
-        if (numbers.isEmpty()) {
-            return 0
-        }
-
-        return numbers.reduce(Int::plus)
-    }
-
-    private fun validate(numbers: List<Int>) {
-        if (numbers.any { it < 0 }) {
-            throw IllegalArgumentException("음수는 입력할 수 없습니다.")
-        }
-    }
-}
