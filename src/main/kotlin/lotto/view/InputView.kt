18ce4752aa71df2a173d79c3cf86c0bc5a44740c
KimDabomi <cdabomi@nate.com>
2024-11-22 22:22:05 +0900

    [step2] refactor: 메시지 수정

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -6,7 +6,7 @@ object InputView {
     private const val INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
     private const val AMOUNT_EXCEPTION_MESSAGE = "유효한 금액을 입력해 주세요."
     private const val INPUT_LAST_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요."
-    private const val LOTTO_NUMBER_EXCEPTION_MESSAGE = "1부터 45 사이의 숫자를 쉼표로 구분하여 입력해 주세요."
+    private const val INPUT_NUMBER_EXCEPTION_MESSAGE = "당첨 숫자를 쉼표로 구분하여 입력해 주세요."
     private const val LOTTO_NUMBERS_SPLIT_UNIT = ","
 
     fun askPurchaseAmount(): Int {
@@ -18,7 +18,7 @@ object InputView {
         println(INPUT_LAST_WINNING_NUMBER)
         val numbers =
             readlnOrNull()?.split(LOTTO_NUMBERS_SPLIT_UNIT)?.map { it.trim().toInt() }
-                ?: throw IllegalArgumentException(LOTTO_NUMBER_EXCEPTION_MESSAGE)
+                ?: throw IllegalArgumentException(INPUT_NUMBER_EXCEPTION_MESSAGE)
 
         LottoTicket.validateNumbers(numbers)
         return numbers
