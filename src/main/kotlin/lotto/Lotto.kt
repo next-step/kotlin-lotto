8fcd7f352c849d5b9ae2d743c305ea874b3cdd96
sian <chicori3@naver.com>
2024-11-23 01:44:31 +0900

    feat: 로또 팩토리 테스트 및 구현

---
+++ src/main/kotlin/lotto/Lotto.kt
@@ -0,0 +1,5 @@
+package lotto
+
+class Lotto(
+    private val numbers: List<LottoNumber>,
+)
---
+++ src/main/kotlin/lotto/LottoFactory.kt
@@ -0,0 +1,9 @@
+package lotto
+
+class LottoFactory(
+    private val numberGenerator: LottoNumberGenerator,
+) {
+    fun create(quantity: Int): List<Lotto> {
+        return (1..quantity).map { Lotto(numberGenerator.generate()) }
+    }
+}
