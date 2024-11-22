b6755629179b77d80ef8ddb2291a4efe147f413f
leekyuwon <lkw2v@naver.com>
2024-11-22 09:51:17 +0900

    feat: 당첨 번호 관리 객체를 생성하라

---
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -0,0 +1,8 @@
+package lotto.domain
+
+class WinningLotto(private val winningNumbers: Set<Int>) {
+    init {
+        require(winningNumbers.size == 6) { "당첨 번호는 반드시 6개여야 합니다." }
+        require(winningNumbers.all { it in 1..45 }) { "당첨 번호는 1부터 45 사이여야 합니다." }
+    }
+}
--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,5 +1,7 @@
 package lotto.view
 
+import lotto.domain.WinningLotto
+
 class InputView {
     fun readPurchaseAmount(): Int {
         println("구입 금액을 입력해 주세요.")
@@ -12,15 +14,23 @@ class InputView {
         }
     }
 
-    fun readWinningNumbers(): Set<Int> {
+    fun readWinningNumbers(): WinningLotto {
         println("지난 주 당첨 번호를 입력해 주세요. (쉼표로 구분)")
-        val input =
-            readLine()
-                ?: throw IllegalArgumentException("번호를 입력해 주세요.")
-        val numbers = input.split(",").map { it.trim().toIntOrNull() }
-        require(numbers.size == 6) { "로또 번호는 6개여야 합니다." }
-        require(numbers.all { it in 1..45 }) { "로또 번호는 1부터 45 사이여야 합니다." }
-        return numbers.filterNotNull().toSet()
+        val input = readlnOrNull() ?: exitProgram()
+
+        return try {
+            val numbers =
+                input.split(",").map {
+                    it.trim().toInt()
+                }
+            WinningLotto(numbers.toSet())
+        } catch (e: NumberFormatException) {
+            println("유효하지 않은 숫자입니다. 숫자를 입력해 주세요.")
+            return readWinningNumbers()
+        }   catch (e: IllegalArgumentException) {
+            println(e.message)
+            return readWinningNumbers()
+        }
     }
 
     private fun exitProgram(): Nothing {
