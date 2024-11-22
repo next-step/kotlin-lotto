c265242b27792a3fc4c6cf3c7d12df63e1b3ae9f
leekyuwon <lkw2v@naver.com>
2024-11-22 14:11:58 +0900

    refactor: 로또 게임을 구성하라

--- src/main/kotlin/lotto/app/LottoApp.kt
+++ src/main/kotlin/lotto/app/LottoApp.kt
@@ -2,15 +2,36 @@ package lotto.app
 
 import lotto.domain.AutoLottoNumberGenerator
 import lotto.domain.LottoStore
+import lotto.domain.WinningStatistics
 import lotto.view.InputView
 import lotto.view.OutPutView
 
 fun main() {
-    val purchaseAmount = InputView().readPurchaseAmount()
-    val lottoCount = LottoStore().calculateLottoCount(purchaseAmount)
-    OutPutView().printLottoCount(lottoCount)
-    val purchasedLottos = LottoStore().issueLottos(lottoCount, AutoLottoNumberGenerator())
-    OutPutView().printPurchasedLottos(purchasedLottos)
+    val inputView = InputView()
+    val resultView = OutPutView()
+    val lottoStore = LottoStore()
+    val numberGenerator = AutoLottoNumberGenerator()
 
-    val winningNumbers = InputView().readWinningNumbers()
+    // 1. 구입 금액 입력 및 로또 개수 계산
+    val purchaseAmount = inputView.readPurchaseAmount()
+    val lottoCount = lottoStore.calculateLottoCount(purchaseAmount)
+    resultView.printLottoCount(lottoCount)
+
+    // 2. 로또 발행 및 출력
+    val purchasedLottos = lottoStore.issueLottos(lottoCount, numberGenerator)
+    resultView.printPurchasedLottos(purchasedLottos)
+
+    // 3. 당첨 번호 입력
+    val winningNumbers = inputView.readWinningNumbers()
+
+    // Step 4: 당첨 통계 생성
+    val winningStatistics = WinningStatistics(winningNumbers)
+    val statistics = winningStatistics.calculateStatistics(purchasedLottos)
+
+    // Step 5: 당첨 통계 출력
+    resultView.printStatistics(statistics)
+
+    // Step 6: 수익률 계산 및 출력
+    val profitRate = winningStatistics.profitRate(lottoCount)
+    resultView.printProfitRate(profitRate)
 }
