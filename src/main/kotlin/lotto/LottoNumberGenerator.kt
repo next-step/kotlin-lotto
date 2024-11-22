34c4c6c1417529f97ad819bd3609eaab80ad9ecb
sian <chicori3@naver.com>
2024-11-23 01:57:59 +0900

    test: 로또 숫자, 생성기 테스트 및 구현

---
+++ src/main/kotlin/lotto/LottoNumberGenerator.kt
@@ -0,0 +1,5 @@
+package lotto
+
+fun interface LottoNumberGenerator {
+    fun generate(): List<LottoNumber>
+}
---
+++ src/main/kotlin/lotto/RandomNumberGenerator.kt
@@ -0,0 +1,12 @@
+package lotto
+
+class RandomNumberGenerator : LottoNumberGenerator {
+    override fun generate(): List<LottoNumber> {
+        return RANGE.shuffled().take(LOTTO_SIZE).map(::LottoNumber)
+    }
+
+    companion object {
+        private const val LOTTO_SIZE = 6
+        private val RANGE = 1..45
+    }
+}
