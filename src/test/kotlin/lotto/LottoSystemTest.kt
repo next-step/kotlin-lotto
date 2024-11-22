5f36da1268641d817bfa29bd8a8951406c581225
SeokHo-Ham <forkyy.dev@gmail.com>
2024-11-23 02:43:27 +0900

    step2: 로또 시스템에 대한 테스트 작성

---
+++ src/test/kotlin/lotto/LottoSystemTest.kt
@@ -0,0 +1,39 @@
+package lotto
+
+import io.kotest.assertions.assertSoftly
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class LottoSystemTest : StringSpec({
+    "로또 금액을 입력받으면 해당 요청에 대한 주문을 생성해야한다." {
+        val lottoSystem = LottoSystem(FixedNumberGenerator())
+        val order = lottoSystem.createOrder(10000)
+
+        assertSoftly {
+            order.amount shouldBe 10000
+            order.lottos.size shouldBe 10
+        }
+    }
+
+    "당첨 번호를 입력받을 경우 당첨 번호 객체를 생성해야 한다." {
+        val lottoSystem = LottoSystem(FixedNumberGenerator())
+
+        val winNumbers = lottoSystem.createWinNumbers()
+
+        winNumbers shouldBe WinNumbers(FixedNumberGenerator().generate())
+    }
+
+    "당첨 통계 결과를 제공해야 한다." {
+        val lottoSystem = LottoSystem(FixedNumberGenerator())
+        val order = lottoSystem.createOrder(1000)
+        val winNumbers = lottoSystem.createWinNumbers()
+
+        val result = lottoSystem.createWinningResult(order, winNumbers)
+
+        assertSoftly {
+            result.revenue shouldBe 2_000_000_000
+            result.winningMatchCounts[6] shouldBe 1
+            result.rate shouldBe 2000000.0
+        }
+    }
+})
--- src/test/kotlin/lotto/OrderTest.kt
+++ src/test/kotlin/lotto/OrderTest.kt
@@ -23,9 +23,3 @@ class OrderTest : StringSpec({
         }
     }
 })
-
-class FixedNumberGenerator : NumberGenerator {
-    override fun generate(): Set<Int> {
-        return setOf(1, 2, 3, 4, 5, 6)
-    }
-}
