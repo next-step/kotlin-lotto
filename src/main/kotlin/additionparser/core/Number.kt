c52663ad2248d90b8e81adf5b7a6a476f460015e
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-23 01:47:39 +0900

    feat: 문자열을 입력받아 숫자로 변환하는 Class 추가.

---
+++ src/main/kotlin/additionparser/core/Number.kt
@@ -0,0 +1,23 @@
+package additionparser.core
+
+class Number(value: String) {
+    var number = 0
+        private set
+
+    init {
+        number = value.toIntOrNull() ?: throw RuntimeException("잘못된 값이 입력되었습니다.")
+        if (number < 0) {
+            throw RuntimeException("입력된 값이 음수 입니다.")
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Number) return false
+        return this.number == other.number
+    }
+
+    override fun hashCode(): Int {
+        return 31 * number
+    }
+}
