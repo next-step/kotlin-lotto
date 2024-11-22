67f350e6918345c3a321e399307e194ff95dc975
dino.shin <troas96@naver.com>
2024-11-23 00:18:56 +0900

    refactor(LottoRank): matchCount와 entries 통한 최적화

--- src/main/kotlin/lotto/domain/LottoRank.kt
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -1,22 +1,18 @@
 package lotto.domain
 
-enum class LottoRank(val rank: Int, val prize: Int, val description: String) {
-    FIRST(1, 2_000_000_000, "6개 일치"),
-    SECOND(2, 1_500_000, "5개 일치"),
-    THIRD(3, 50_000, "4개 일치"),
-    FOURTH(4, 5_000, "3개 일치"),
-    NO_RANK(5, 0, "꽝"),
+enum class LottoRank(val rank: Int, val prize: Int, val description: String, val matchCount: Int) {
+    FIRST(1, 2_000_000_000, "6개 일치", 6),
+    SECOND(2, 1_500_000, "5개 일치", 5),
+    THIRD(3, 50_000, "4개 일치", 4),
+    FOURTH(4, 5_000, "3개 일치", 3),
+    NO_RANK(5, 0, "꽝", 0),
     ;
 
     companion object {
-        fun fromMatchCount(matchCount: Int): LottoRank {
-            return when (matchCount) {
-                6 -> FIRST
-                5 -> SECOND
-                4 -> THIRD
-                3 -> FOURTH
-                else -> NO_RANK
-            }
+        private val matchCountToRankMap = entries.associateBy { it.matchCount }
+
+        fun fromMatchCount(lineMatchCount: Int): LottoRank {
+            return matchCountToRankMap[lineMatchCount] ?: NO_RANK
         }
     }
 }
