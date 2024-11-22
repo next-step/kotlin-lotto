01a7fdf7ab2aebf7021f54b7e7eec64c90ad8544
SeongminKimKR <gvf156@gmail.com>
2024-11-23 01:03:05 +0900

    chore: 로또 당첨 상금 value 언더스코어 추가

--- src/main/kotlin/lotto/domain/LottoRank.kt
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -1,10 +1,10 @@
 package lotto.domain
 
 enum class LottoRank(val matchCount: Int, val reward: Long) {
-    FIRST(6, 2000000000L),
-    SECOND(5, 1500000L),
-    THIRD(4, 50000L),
-    FOURTH(3, 5000L),
+    FIRST(6, 2_000_000_000L),
+    SECOND(5, 1_500_000L),
+    THIRD(4, 50_000L),
+    FOURTH(3, 5_000L),
     NONE(0, 0), ;
 
     fun isWin(): Boolean = this != NONE
