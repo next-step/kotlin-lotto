7f20969daaf2e98e0292de13290ae6d19da48423
pkch93 <dev.pkch@gmail.com>
2024-11-22 17:05:06 +0900

    [step1] 문자열 덧셈 계산기 테스트 통과하도록 구현

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -1,5 +1,72 @@
 package calculator
 
 class StringCalculator {
-    fun calulate(expression: String): Double = 0.0
+    fun calculate(stringExpression: String?): Double {
+        require(!stringExpression.isNullOrBlank()) { "표현식이 유효하지 않습니다." }
+        val expression = parseExpression(stringExpression)
+        return expression.calculate()
+    }
+
+    private fun parseExpression(stringExpression: String): Expression {
+        val customDelimiter = parseCustomDelimiter(stringExpression)
+        val mathExpression = parseMathExpression(stringExpression, customDelimiter)
+
+        return Expression(
+            mathExpression = mathExpression,
+            customDelimiter = customDelimiter,
+        )
+    }
+
+    private fun parseCustomDelimiter(stringExpression: String): String? =
+        Regex("(?<=//).(?=\\n)")
+            .find(stringExpression)
+            ?.value
+
+    private fun parseMathExpression(
+        stringExpression: String,
+        customDelimiter: String?,
+    ): String =
+        if (customDelimiter != null) {
+            Regex("(?<=//.\\n).*")
+                .find(stringExpression)
+                ?.value
+                ?: throw IllegalArgumentException("표현식이 유효하지 않습니다.")
+        } else {
+            stringExpression
+        }
+}
+
+private class Expression(
+    mathExpression: String,
+    customDelimiter: String? = null,
+) {
+    private val targetNumbers: List<Double>
+
+    init {
+        val delimiters = DEFAULT_DELIMITERS + listOfNotNull(customDelimiter)
+        val parsedTargetNumbers = parseTargetNumbers(mathExpression, delimiters)
+        requirePositiveNumbers(parsedTargetNumbers)
+        this.targetNumbers = parsedTargetNumbers
+    }
+
+    private fun parseTargetNumbers(
+        mathExpression: String,
+        delimiters: List<String>,
+    ) = try {
+        mathExpression
+            .split(*delimiters.toTypedArray())
+            .map { it.toDouble() }
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("숫자만 더할 수 있습니다.")
+    }
+
+    private fun requirePositiveNumbers(parsedTargetNumbers: List<Double>) {
+        require(parsedTargetNumbers.all { it >= 0 }) { "음수는 더할 수 없습니다." }
+    }
+
+    fun calculate(): Double = targetNumbers.sum()
+
+    companion object {
+        private val DEFAULT_DELIMITERS = listOf(",", ":")
+    }
 }
--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -4,38 +4,36 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class StringCalculatorTest : StringSpec({
+    val sut = StringCalculator()
+
     "쉼표로 구분되는 숫자들을 더한다." {
         val expression = "1,2,3"
-        val sut = StringCalculator()
 
-        val actual = sut.calulate(expression)
+        val actual = sut.calculate(expression)
 
         actual shouldBe 6.0
     }
 
     "콜론으로 구분되는 숫자들을 더한다." {
         val expression = "1:2:3"
-        val sut = StringCalculator()
 
-        val actual = sut.calulate(expression)
+        val actual = sut.calculate(expression)
 
         actual shouldBe 6.0
     }
 
     "쉼표와 콜론으로 구분되는 숫자들을 더한다." {
         val expression = "1,2:3"
-        val sut = StringCalculator()
 
-        val actual = sut.calulate(expression)
+        val actual = sut.calculate(expression)
 
         actual shouldBe 6.0
     }
 
     "커스텀 구분자로 구분되는 숫자들을 더한다." {
         val expression = "//;\n1;2;3"
-        val sut = StringCalculator()
 
-        val actual = sut.calulate(expression)
+        val actual = sut.calculate(expression)
 
         actual shouldBe 6.0
     }
