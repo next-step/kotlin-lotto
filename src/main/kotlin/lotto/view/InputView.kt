278a78c0cb0d8a38fe396ced303c97d0401a2fd7
leekyuwon <lkw2v@naver.com>
2024-11-22 09:25:08 +0900

    refactor: 로또 구입 금액 입력 예외 발생시 재시도하라

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,10 +3,13 @@ package lotto.view
 class InputView {
     fun readPurchaseAmount(): Int {
         println("구입 금액을 입력해 주세요.")
-        val amount =
-            readLine()?.toIntOrNull()
-                ?: throw IllegalArgumentException("유효하지 않은 금액입니다. 숫자를 입력해 주세요.")
-        return amount
+        val input = readln()
+        return try {
+            input.toInt()
+        } catch (e: NumberFormatException) {
+            println("유효하지 않은 금액입니다. 숫자를 입력해 주세요.")
+            return readPurchaseAmount()
+        }
     }
 
     fun readWinningNumbers(): Set<Int> {
