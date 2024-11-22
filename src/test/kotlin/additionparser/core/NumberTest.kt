995fa5b6814f9f20fc062bfaed03e99293e49f10
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-23 01:51:16 +0900

    test: Test Case 추가.
    숫자가 아닌 값을 지정시 RuntimeException 발생 확인.

--- src/test/kotlin/additionparser/core/NumberTest.kt
+++ src/test/kotlin/additionparser/core/NumberTest.kt
@@ -10,4 +10,11 @@ class NumberTest {
             Number("-1")
         }
     }
+
+    @Test
+    fun `숫자가 아닌 값을 지정하면 Runtime Exception이 발생함을 확인한다`() {
+        shouldThrow<RuntimeException> {
+            Number("ㅁㅇ")
+        }
+    }
 }
