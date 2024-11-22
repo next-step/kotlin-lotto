3ed723a964332c35584bc025a6b5960e2a0461fc
y2gcoder <y2gcoder@gmail.com>
2024-11-23 01:02:36 +0900

    feature(lotto): 로또 발권기 의 복권 발급 시  로또 발권기 의 잔액이 티켓 1장 가격보다 적을 때 처리
    LotteryTicketMachine의 잔액이 복권 1장 가격보다 적으면 발권 시 null을 반환하고 잔액을 차감하지 않게 처리

--- src/main/kotlin/lotto/LotteryTicketMachine.kt
+++ src/main/kotlin/lotto/LotteryTicketMachine.kt
@@ -1,10 +1,21 @@
 package lotto
 
-class LotteryTicketMachine(private val balance: Int) {
+class LotteryTicketMachine(balance: Int) {
+    var balance = balance
+        private set
+
     init {
         require(balance >= TICKET_PRICE) {
             "로또 티켓을 구매하기 위해서는 최소 $TICKET_PRICE 원이 필요합니다"
         }
+    }
+
+    fun issueTicket(numbers: List<Int> = (1..45).shuffled().take(6)): LottoTicket? {
+        if (balance < TICKET_PRICE) {
+            return null
+        }
+        balance -= TICKET_PRICE
+        return LottoTicket(numbers)
     }
 
     companion object {
--- src/test/kotlin/lotto/LotteryTicketMachineTest.kt
+++ src/test/kotlin/lotto/LotteryTicketMachineTest.kt
@@ -2,11 +2,33 @@ package lotto
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
 
 class LotteryTicketMachineTest : StringSpec({
     "로또 티켓 머신은 티켓 구매 가격(1000원) 미만의 돈을 받으면 예외를 던진다" {
         shouldThrow<IllegalArgumentException> {
             LotteryTicketMachine(999)
         }
+    }
+
+    "로또 티켓 머신은 잔액 1000원 당 자동 로또 티켓 1장을 발권한다" {
+        val sut = LotteryTicketMachine(14000)
+
+        val result = sut.issueTicket()
+
+        sut.balance shouldBe 13000
+        result.shouldNotBeNull()
+    }
+
+    "로또 티켓 머신의 잔액이 티켓 1장 가격보다 적으면 발권 시 null을 반환한다" {
+        val sut = LotteryTicketMachine(1999)
+        sut.issueTicket()
+
+        val result = sut.issueTicket()
+
+        sut.balance shouldBe 999
+        result.shouldBeNull()
     }
 })
