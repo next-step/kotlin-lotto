77bb9df0ca0c53c7e1fbe6034ba37eefe9794f8e
wooobo <irooori89@gmail.com>
2024-11-23 19:42:47 +0900

    refactor: Application 리팩토링

--- src/main/kotlin/lotto/ResultView.kt
+++ src/main/kotlin/lotto/ResultView.kt
@@ -1,23 +1,26 @@
 package lotto
 
 object ResultView {
-    fun printStatistics(
-        ranks: Ranks,
-        amount: Amount,
-    ) {
-        println("당첨 통계")
-        println("---------")
-        Rank.prizeRanks.forEach {
-            val count = ranks.count(it)
-            println("${it.matchCount}개 일치 (${it.prize.value}원) - ${count}개")
-        }
-        println("총 수익률은 ${ranks.rate(amount)}입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)")
-    }
-
     fun printBoughtLotto(boughtLotto: Lottos) {
         boughtLotto.values.forEach {
             println("[%s]".format(it.values.sorted().joinToString(", ")))
         }
+    }
+
+    fun printStatistics(statistics: LottoStatistics) {
+        println("당첨 통계")
+        println("---------")
+        Rank.prizeRanks.forEach {
+            val count = statistics.machRankCount(it)
+            println("${it.matchCount}개 일치 (${it.prize.value}원) - ${count}개")
+        }
+        printRate(statistics)
+    }
+
+    private fun printRate(statistics: LottoStatistics) {
+        val result = if (statistics.isLoss()) "손해" else "이익"
+
+        println("총 수익률은 ${statistics.rate()}입니다.(기준이 1이기 때문에 결과적으로 ${result}이라는 의미임)")
     }
 
     fun print(message: String) {
