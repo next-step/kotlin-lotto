a2222cdd913f9fa59b77631455ef42eebeaae803
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-23 22:37:18 +0900

    refactor : Number data class로 변경,

--- src/main/kotlin/stringcalculator/core/Number.kt
+++ src/main/kotlin/stringcalculator/core/Number.kt
@@ -1,23 +1,19 @@
 package stringcalculator.core
 
-class Number(value: String) {
-    var number = 0
-        private set
+data class Number(val number: Int) {
+    constructor(str: String) : this(str.toIntOrNull() ?: throw RuntimeException("잘못된 값이 입력되었습니다."))
 
     init {
-        number = value.toIntOrNull() ?: throw RuntimeException("잘못된 값이 입력되었습니다.")
         if (number < 0) {
             throw RuntimeException("입력된 값이 음수 입니다.")
         }
     }
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is Number) return false
-        return this.number == other.number
+    operator fun plus(other: Number): Number {
+        return Number((number + other.number).toString())
     }
 
-    override fun hashCode(): Int {
-        return 31 * number
+    companion object {
+        val ZERO = Number("0")
     }
 }
