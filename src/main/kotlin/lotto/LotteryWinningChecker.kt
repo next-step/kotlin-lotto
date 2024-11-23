da86a742c6637f5a5a52280757411f675ad7d37c
y2gcoder <y2gcoder@gmail.com>
2024-11-23 17:20:26 +0900

    refactor(lotto): WinningBoard -> LotteryWinningChecker 로 이름 변경
    로또 당첨 결과를 검사해주는 책임과 로또 당첨 결과에 대한 통계를 내주는 책임을 분리하기 위해 별도의 클래스로 관리하고자 함

--- src/main/kotlin/lotto/LotteryWinningChecker.kt
+++ src/main/kotlin/lotto/LotteryWinningChecker.kt
@@ -1,6 +1,6 @@
 package lotto
 
-class WinningBoard(val winningTicket: LottoTicket) {
+class LotteryWinningChecker(val winningTicket: LottoTicket) {
     fun checkTicket(lottoTicket: LottoTicket): WinningResult {
         val matchNumbers = winningTicket.numbers intersect lottoTicket.numbers.toSet()
         return WinningResult.valueOf(matchNumbers.size)
--- src/test/kotlin/lotto/LotteryWinningCheckerTest.kt
+++ src/test/kotlin/lotto/LotteryWinningCheckerTest.kt
@@ -5,17 +5,17 @@ import io.kotest.data.forAll
 import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
-class WinningBoardTest : StringSpec({
-    "당첨 보드는 로또 1등 당첨 번호(티켓)을 입력 받는다" {
+class LotteryWinningCheckerTest : StringSpec({
+    "로또 당첨 검사기는 로또 1등 당첨 번호(티켓)을 입력 받는다" {
         val lottoTicket = LottoTicket(numbers = listOf(1, 2, 3, 4, 5, 6))
 
-        val sut = WinningBoard(winningTicket = lottoTicket)
+        val sut = LotteryWinningChecker(winningTicket = lottoTicket)
 
         sut.winningTicket shouldBe lottoTicket
     }
 
-    "당첨 보드는 로또 티켓을 받아 당첨 결과를 조회할 수 있다" {
-        val sut = WinningBoard(winningTicket = LottoTicket(listOf(1, 2, 3, 4, 5, 6)))
+    "로또 당첨 검사기는 로또 티켓을 받아 당첨 결과를 조회할 수 있다" {
+        val sut = LotteryWinningChecker(winningTicket = LottoTicket(listOf(1, 2, 3, 4, 5, 6)))
 
         forAll(
             row(LottoTicket(numbers = listOf(1, 2, 3, 4, 5, 6)), WinningResult.FIRST),
