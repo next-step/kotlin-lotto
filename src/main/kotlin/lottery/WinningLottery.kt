15c2314f5ecf9cc2fdf6c1eacca863a144d1ac12
Duhan Mo <duhandv@gmail.com>
2024-11-24 01:16:23 +0900

    test: 당첨로또는 번호를 직접 입력한다

--- src/main/kotlin/lottery/WinningLottery.kt
+++ src/main/kotlin/lottery/WinningLottery.kt
@@ -1,6 +1,10 @@
 package lottery
 
 class WinningLottery(val lotteryNumbers: LotteryNumbers) {
+    fun countMatchedNumber(numbers: List<Int>): Int {
+        return lotteryNumbers.numbers.intersect(numbers.toSet()).size
+    }
+
     companion object {
         fun create(numbers: List<Int>): WinningLottery {
             return WinningLottery(LotteryNumbers(numbers))
