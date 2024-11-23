32be7c933a1965a676707d27b99b12a04376739b
Duhan Mo <duhandv@gmail.com>
2024-11-24 03:04:07 +0900

    refactor: RankReward의 금액을 값객체로 수정

--- src/main/kotlin/lottery/RankReward.kt
+++ src/main/kotlin/lottery/RankReward.kt
@@ -1,10 +1,10 @@
 package lottery
 
-enum class RankReward(val matchedNumberCount: Int, val money: Int) {
-    RANK_1(6, 2_000_000_000),
-    RANK_2(5, 1_500_000),
-    RANK_3(4, 50_000),
-    RANK_4(3, 5_000),
+enum class RankReward(val matchedNumberCount: Int, val money: Money) {
+    RANK_1(6, Money(2_000_000_000)),
+    RANK_2(5, Money(1_500_000)),
+    RANK_3(4, Money(50_000)),
+    RANK_4(3, Money(5_000)),
     ;
 
     companion object {
