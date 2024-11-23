9c7e6f2ab5c7c7822bd3f4293660f6933f3d92ca
Duhan Mo <duhandv@gmail.com>
2024-11-23 22:33:23 +0900

    test: 로또 번호 범위 초과 예외 테스트

--- src/test/kotlin/lottery/LotteryNumbersTest.kt
+++ src/test/kotlin/lottery/LotteryNumbersTest.kt
@@ -22,14 +22,16 @@ class LotteryNumbersTest : StringSpec({
     }
 
     "로또 번호가 6개가 아니면 예외 발생한다" {
-        shouldThrow<IllegalArgumentException> {
-            LotteryNumbers(List(size = 7) { 0 })
-        }
+        shouldThrow<IllegalArgumentException> { LotteryNumbers(List(size = 7) { 0 }) }
     }
 
     "로또 번호 중복일 시 예외 발생한다" {
-        shouldThrow<IllegalArgumentException> {
-            LotteryNumbers(listOf(1,1,1,1,1,1))
+        shouldThrow<IllegalArgumentException> { LotteryNumbers(listOf(1,1,1,1,1,1)) }
+    }
+
+    "로또 번호가 1~45 범위를 벗어날 시 예외 발생한다" {
+        listOf(0,-1,46,47).forAll { overNumber ->
+            shouldThrow<IllegalArgumentException> { LotteryNumbers(listOf(overNumber,1,1,1,1,1)) }
         }
     }
 })
