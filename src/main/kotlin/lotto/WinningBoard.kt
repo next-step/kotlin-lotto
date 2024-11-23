25de37a1e1c698091d88ff35f0e25032a49385f1
y2gcoder <y2gcoder@gmail.com>
2024-11-23 16:48:22 +0900

    feature(lotto): WinningBoard 에서 로또 티켓의 당첨 번호 일치 갯수를 반환해주는 checkTicket() 추가

--- src/main/kotlin/lotto/WinningBoard.kt
+++ src/main/kotlin/lotto/WinningBoard.kt
@@ -1,3 +1,8 @@
 package lotto
 
-class WinningBoard(val winningTicket: LottoTicket)
+class WinningBoard(val winningTicket: LottoTicket) {
+    fun checkTicket(lottoTicket: LottoTicket): Int {
+        val matchNumbers = winningTicket.numbers intersect lottoTicket.numbers.toSet()
+        return matchNumbers.size
+    }
+}
--- src/test/kotlin/lotto/WinningBoardTest.kt
+++ src/test/kotlin/lotto/WinningBoardTest.kt
@@ -1,6 +1,8 @@
 package lotto
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class WinningBoardTest : StringSpec({
@@ -10,5 +12,22 @@ class WinningBoardTest : StringSpec({
         val sut = WinningBoard(winningTicket = lottoTicket)
 
         sut.winningTicket shouldBe lottoTicket
+    }
+
+    "당첨 보드는 로또 티켓을 받아 1등 티켓과 일치하는 당첨 번호 수를 알 수 있다" {
+        val sut = WinningBoard(winningTicket = LottoTicket(listOf(1, 2, 3, 4, 5, 6)))
+
+        forAll(
+            row(LottoTicket(numbers = listOf(1, 2, 3, 4, 5, 6)), 6),
+            row(LottoTicket(numbers = listOf(2, 3, 4, 5, 6, 7)), 5),
+            row(LottoTicket(numbers = listOf(3, 4, 5, 6, 7, 8)), 4),
+            row(LottoTicket(numbers = listOf(4, 5, 6, 7, 8, 9)), 3),
+            row(LottoTicket(numbers = listOf(5, 6, 7, 8, 9, 10)), 2),
+            row(LottoTicket(numbers = listOf(6, 7, 8, 9, 10, 11)), 1),
+            row(LottoTicket(numbers = listOf(7, 8, 9, 10, 11, 12)), 0),
+        ) { lottoTicket, expectedCountOfMatch ->
+            val countOfMatch = sut.checkTicket(lottoTicket)
+            countOfMatch shouldBe expectedCountOfMatch
+        }
     }
 })
