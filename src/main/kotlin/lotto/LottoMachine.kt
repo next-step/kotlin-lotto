c4afa1c5f5973452520130f312c14b93035c78b2
nooblette <min9hyuk@gmail.com>
2024-11-23 16:14:03 +0900

    refactor : Lottos.kt 부생성자 추가, List constructor-like function 적용

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -1,6 +1,5 @@
 package lotto
 
 class LottoMachine {
-    fun generate(lottoCount: Int): Lottos =
-        Lottos((1..lottoCount).map { Lotto() })
+    fun generate(lottoCount: Int): Lottos = Lottos(lottoCount)
 }
--- src/main/kotlin/lotto/Lottos.kt
+++ src/main/kotlin/lotto/Lottos.kt
@@ -2,4 +2,6 @@ package lotto
 
 class Lottos(
     val lottos: List<Lotto>,
-)
+) {
+    constructor(lottoCount: Int) : this(List(lottoCount) { Lotto() })
+}
