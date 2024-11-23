4e4100791f2b8055f8d2369ba8e22ab37026c08c
nooblette <min9hyuk@gmail.com>
2024-11-23 18:59:18 +0900

    refactor : LottoTest.kt와 LottoRankTest.kt 테스트 시나리오 분리

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import io.kotest.matchers.ints.shouldBeLessThanOrEqual
 import io.kotest.matchers.shouldBe
 import lotto.number.Numbers
 import lotto.rank.LottoRank
@@ -7,16 +8,20 @@ import org.junit.jupiter.api.Test
 
 class LottoTest {
     @Test
-    fun `번호가 6개 일치하면 1등이다`() {
-        val lotto = Lotto(numbers = Numbers(listOf(1, 2, 3, 4, 5, 6)))
+    fun `번호가 모두 일치하면 1등 로또이다`() {
+        val lotto = Lotto(numbers = Numbers(WINNING_NUMBERS))
 
+        lotto.numbers.numbers shouldBe WINNING_NUMBERS
         lotto.getRank(WINNING_NUMBERS) shouldBe LottoRank.FIRST
     }
 
     @Test
-    fun `번호가 2개 이하로 일치하면 아무것도 해당하지 않는다`() {
+    fun `번호가 2개 이하로 일치하면 당첨되지 않은 로또이다`() {
         val lotto = Lotto(numbers = Numbers(listOf(1, 2, 0, 0, 0, 0)))
 
+        lotto.numbers.numbers
+            .filter { it in WINNING_NUMBERS }
+            .size shouldBeLessThanOrEqual 2
         lotto.getRank(WINNING_NUMBERS) shouldBe LottoRank.NONE
     }
 
