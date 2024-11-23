c8a122184873d6e6ec313b0f5de05a08b76c0230
y2gcoder <y2gcoder@gmail.com>
2024-11-23 19:11:14 +0900

    fix(lotto): LottoTicket 예외 메시지 오타 수정
    로또 번호가 6개가 아닐 때의 예외 메시지 오타 수정

--- src/main/kotlin/lotto/LottoTicket.kt
+++ src/main/kotlin/lotto/LottoTicket.kt
@@ -5,7 +5,7 @@ class LottoTicket(numbers: List<Int>) {
 
     init {
         require(numbers.size == 6) {
-            "로또 번호는 총 6개겨야 합니다"
+            "로또 번호는 총 6개여야 합니다"
         }
         require(numbers.all { (1..45).contains(it) }) {
             "로또 번호는 1에서 45까지만 허용합니다"
