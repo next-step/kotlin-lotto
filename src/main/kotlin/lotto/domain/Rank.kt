800894a4e501b041da0e03d9e28f1a3405f527f1
leekyuwon <kyuwoon369@gmail.com>
2024-11-23 15:47:15 +0900

    feat: 보너스 일치시 2등을 추가하라

--- src/main/kotlin/lotto/domain/Rank.kt
+++ src/main/kotlin/lotto/domain/Rank.kt
@@ -3,18 +3,29 @@ package lotto.domain
 import java.math.BigDecimal
 
 enum class Rank(val matchCount: Int, val prize: BigDecimal) {
-    THREE(3, BigDecimal(5_000)),
-    FOUR(4, BigDecimal(50_000)),
-    FIVE(5, BigDecimal(1_500_000)),
-    SIX(6, BigDecimal(2_000_000_000)),
-    MISS(-1, BigDecimal(0)),
+    FIRST(6, BigDecimal(2_000_000_000)),
+    SECOND(5, BigDecimal(30_000_000)),
+    THIRD(5, BigDecimal(1_500_000)),
+    FOURTH(4, BigDecimal(50_000)),
+    FIFTH(3, BigDecimal(5_000)),
+    MISS(0, BigDecimal(0)),
     ;
 
     fun totalPrize(count: Int): BigDecimal = prize.multiply(BigDecimal(count))
 
     companion object {
-        fun from(matchCount: Int): Rank {
-            return entries.find { it.matchCount == matchCount } ?: MISS
+        fun from(
+            matchCount: Int,
+            matchBonus: Boolean,
+        ): Rank {
+            return when {
+                matchCount == 6 -> FIRST
+                matchCount == 5 && matchBonus -> SECOND
+                matchCount == 5 -> THIRD
+                matchCount == 4 -> FOURTH
+                matchCount == 3 -> FIFTH
+                else -> MISS
+            }
         }
     }
 }
