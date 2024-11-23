1bdf84d7cc78e24f847662962c8cd70410781643
Duhan Mo <duhandv@gmail.com>
2024-11-23 21:55:30 +0900

    test: 로또 하나에 6개의 랜덤번호가 들어있다

--- src/test/kotlin/lottery/LotteryTest.kt
+++ src/test/kotlin/lottery/LotteryTest.kt
@@ -19,7 +19,9 @@ class Lottery(val numbers: Numbers) {
     }
 }
 
-class Numbers(val numbers: List<Int>) {
+class Numbers(numbers: List<Int>) {
+    val size = numbers.size
+
     companion object {
         fun create(): Numbers {
             val numbers = (1..45).shuffled().take(6).sorted()
@@ -32,5 +34,9 @@ class LotteryTest : StringSpec({
     "원하는 갯수만큼 로또를 생성한다" {
         val lotteries = LotteryMachine.buy(1)
         lotteries.size shouldBe 1
+    }
+
+    "로또 하나에 6개의 랜덤번호가 들어있다" {
+        Lottery.create().numbers.size shouldBe 6
     }
 })
