d04de0603588018bbaa19444ec487eef9a68ae04
y2gcoder <y2gcoder@gmail.com>
2024-11-23 18:32:34 +0900

    feature(lotto): WinningBoard 에서 수익률을 계산하는 함수 calculateRateOfReturn(totalCost: Int) 생성
    예시에 맞게 소수점 둘째 자리 이하를 버린 수익률 반환
    이 때 총 구매 비용이 음수인지 먼저 검사함

--- src/test/kotlin/lotto/WinningBoardTest.kt
+++ src/test/kotlin/lotto/WinningBoardTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
@@ -43,5 +44,23 @@ class WinningBoardTest : StringSpec({
         sut.getWinningCount(WinningResult.THIRD) shouldBe thirdResults.size
         sut.getWinningCount(WinningResult.FOURTH) shouldBe fourthResults.size
         sut.getWinningCount(WinningResult.LOSE) shouldBe loseResults.size
+    }
+
+    "당첨 보드는 총 티켓 구매 비용을 받아 당첨 결과들과 비교하여 소수점 둘째 이하를 버린 수익률을 계산한다" {
+        val fourthResults = listOf(WinningResult.FOURTH)
+        val loseResults = List(13) { WinningResult.LOSE }
+        val winningResults = fourthResults + loseResults
+        val sut = WinningBoard(winningResults = winningResults)
+
+        sut.calculateRateOfReturn(totalCost = 14000) shouldBe 0.35
+    }
+
+    "당첨 보드에서 수익률을 구할 때 총 티켓 구매 비용이 1000원 미만이면 예외를 던진다" {
+        val fourthResults = listOf(WinningResult.FOURTH)
+        val loseResults = List(13) { WinningResult.LOSE }
+        val winningResults = fourthResults + loseResults
+        val sut = WinningBoard(winningResults = winningResults)
+
+        shouldThrow<IllegalArgumentException> { sut.calculateRateOfReturn(totalCost = 999) }
     }
 })
