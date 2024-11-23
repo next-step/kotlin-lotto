7a47957fd9f74d300f85858d0d8b2834a998bb8c
wooobo <irooori89@gmail.com>
2024-11-23 19:43:26 +0900

    refactor[test]: 테스트 코드 리팩토링

--- src/test/kotlin/lotto/LottosTest.kt
+++ src/test/kotlin/lotto/LottosTest.kt
@@ -2,7 +2,6 @@ package lotto
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertAll
 
 class LottosTest {
     @Test
@@ -16,40 +15,5 @@ class LottosTest {
             )
 
         assertThat(lottos.size).isEqualTo(2)
-    }
-
-    @Test
-    fun `금액만큼 예상하는 번호로 자동구매 가능`() {
-        val fixedLottoNumbersGenerator =
-            FixedLottoNumbersGenerator(
-                listOf(
-                    setOf(1, 2, 3, 4, 5, 6),
-                    setOf(14, 15, 16, 17, 18, 19),
-                ),
-            )
-        val buyAmount = Amount("2500")
-
-        val actual = Lottos.fromCount(buyAmount, fixedLottoNumbersGenerator)
-
-        assertAll(
-            {assertThat(actual.size).isEqualTo(2)},
-            {assertThat(actual.ranks(Lotto.from(listOf(1, 2, 3, 4, 5, 6)))).isEqualTo(Ranks.fromGroupBy(listOf(Rank.FIRST, Rank.MISS)))},
-        )
-    }
-
-    @Test
-    fun `당첨갯수 확인`() {
-        val lottos =
-            Lottos(
-                listOf(
-                    Lotto.from(listOf(1, 2, 3, 4, 5, 6)),
-                    Lotto.from(listOf(1, 2, 3, 4, 5, 7)),
-                ),
-            )
-        val lastLotto = Lotto.from(listOf(1, 2, 3, 4, 5, 6))
-
-        val actual: Ranks = lottos.ranks(lastLotto)
-
-        assertThat(actual).isEqualTo(Ranks.fromGroupBy(listOf(Rank.FIRST, Rank.SECOND)))
     }
 }
--- src/test/kotlin/lotto/RankTest.kt
+++ src/test/kotlin/lotto/RankTest.kt
@@ -2,12 +2,32 @@ package lotto
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class RankTest {
-    @Test
-    fun `일치갯수로 Rank를 알 수 있다`() {
-        val rank = Rank.match(3)
+    @ParameterizedTest
+    @CsvSource(
+        "3, FOURTH",
+        "4, THIRD",
+        "5, SECOND",
+        "6, FIRST",
+    )
+    fun `일치갯수로 Rank를 알 수 있다`(matchCount: Int, expected: Rank) {
+        val rank = Rank.match(matchCount)
 
-        assertThat(rank).isEqualTo(Rank.FOURTH)
+        assertThat(rank).isEqualTo(expected)
+    }
+
+    @Test
+    fun `prizeRanks 상금목록을 알수 있다`() {
+        val prizeRanks = Rank.prizeRanks
+
+        assertThat(prizeRanks).contains(
+            Rank.FIRST,
+            Rank.SECOND,
+            Rank.THIRD,
+            Rank.FOURTH,
+        )
     }
 }
