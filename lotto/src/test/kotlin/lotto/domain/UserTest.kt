2a910cb24b93331d4922730e8a20e33b8ce281d3
0703kyj <kyj0703@konkuk.ac.kr>
2024-11-23 19:29:12 +0900

    [STEP-2] 유저테스트 목록 작성

---
+++ lotto/src/test/kotlin/lotto/domain/UserTest.kt
@@ -0,0 +1,20 @@
+package lotto.domain
+
+import org.junit.jupiter.api.Test
+
+class UserTest {
+    @Test
+    fun `사용자는 구매금액을 가지고 있는다`() {
+
+    }
+
+    @Test
+    fun `사용자는 로또를 가지고 있는다`() {
+
+    }
+
+    @Test
+    fun `사용자는 당첨금액을 알고 있다`() {
+
+    }
+}
