83b41435286ea6ec79e1a1d17a549b119658b706
jwahoonkim <kjhoon0330@snu.ac.kr>
2024-11-23 16:37:48 +0900

    refactor: 리팩터링

--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -1,9 +1,8 @@
 package lotto.domain
 
-class LottoTicket(numbers: Set<Int>) {
-
+class LottoTicket private constructor(numbers: Set<Int>) {
     init {
-        require(numbers.size == 6) { "로또 번호는 6개여야 합니다." }
+        require(numbers.size == 6) { "로또 번호는 6개여야 합니다. 입력된 숫자 = $numbers" }
     }
 
     val numbers = numbers.sorted().map { LottoNumber.of(it) }
@@ -14,6 +13,10 @@ class LottoTicket(numbers: Set<Int>) {
                 .take(6)
                 .toSet()
                 .let { LottoTicket(it) }
+        }
+
+        fun of(numbers: Set<Int>): LottoTicket {
+            return LottoTicket(numbers)
         }
     }
 }
--- src/test/kotlin/lotto/LottoTicketTest.kt
+++ src/test/kotlin/lotto/LottoTicketTest.kt
@@ -6,11 +6,10 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class LottoTicketTest {
-
     @Test
     fun `수동 입력받은 번호로 로또 티켓을 정상 생성한다`() {
         val manualNumbers = setOf(1, 2, 3, 4, 5, 6)
-        val lottoTicket = LottoTicket(manualNumbers)
+        val lottoTicket = LottoTicket.of(manualNumbers)
         assertThat(lottoTicket.numbers.size).isEqualTo(6)
     }
 
@@ -18,14 +17,14 @@ class LottoTicketTest {
     fun `수동 입력받은 번호가 6개가 아니라면 예외를 발생시킨다`() {
         val manualNumbers = setOf(1, 2, 3, 4, 5)
         assertThrows<IllegalArgumentException>(message = "로또 번호는 6개여야 합니다.") {
-            LottoTicket(manualNumbers)
+            LottoTicket.of(manualNumbers)
         }
     }
 
     @Test
     fun `로또 번호는 정렬되어있다`() {
         val manualNumbers = setOf(6, 5, 4, 3, 2, 1)
-        val lottoTicket = LottoTicket(manualNumbers)
+        val lottoTicket = LottoTicket.of(manualNumbers)
         assertThat(lottoTicket.numbers.map { it.number })
             .isEqualTo(listOf(1, 2, 3, 4, 5, 6))
     }
@@ -42,5 +41,4 @@ class LottoTicketTest {
         val numbers = lottoTicket.numbers.map { it.number }
         assertThat(numbers).isEqualTo(numbers.sorted())
     }
-
 }
