8b059b1922e372e56ae5080f212865e5e9e5ebaa
Dongyeon Min <dongyeon.min@woowahan.com>
2024-11-24 00:17:11 +0900

    [step2] 입력받은 당첨 번호와 로또를 비교해서 매칭되는 숫자를 업데이트한다.

--- src/main/kotlin/lotto/domain/LottoMatcher.kt
+++ src/main/kotlin/lotto/domain/LottoMatcher.kt
@@ -1,9 +1,12 @@
-package lotto
+package lotto.domain
 
 object LottoMatcher {
-    fun match(lottos: List<Lotto>, winningNumbers: List<Int>): List<Lotto> {
+    fun match(
+        lottos: List<Lotto>,
+        winningNumbers: List<Int>,
+    ): List<Lotto> {
         return lottos.map { lotto ->
-            val matchCount = lotto.pickNumbers.intersect(winningNumbers).count()
+            val matchCount = lotto.pickNumbers.intersect(winningNumbers.toSet()).count()
             lotto.updateMatchCount(matchCount)
         }
     }
--- src/test/kotlin/lotto/LottoMatcherTest.kt
+++ src/test/kotlin/lotto/LottoMatcherTest.kt
@@ -1,4 +1,36 @@
 package lotto
 
-class LottoMatcherTest {
-}
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.forAll
+import io.kotest.data.headers
+import io.kotest.data.row
+import io.kotest.data.table
+import io.kotest.matchers.shouldBe
+import lotto.domain.Lotto
+import lotto.domain.LottoMatcher
+
+class LottoMatcherTest : FunSpec({
+    test("로또 번호가 일치하는 개수를 반환한다.") {
+        // given
+        val givenWinningNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val testCases =
+            table(
+                headers("pickNumbers", "matchedCount"),
+                row(listOf(11, 12, 13, 14, 15, 16), 0),
+                row(listOf(1, 12, 13, 14, 15, 16), 1),
+                row(listOf(1, 2, 13, 14, 15, 16), 2),
+                row(listOf(1, 2, 3, 14, 15, 16), 3),
+                row(listOf(1, 2, 3, 4, 15, 16), 4),
+                row(listOf(1, 2, 3, 4, 5, 16), 5),
+                row(listOf(1, 2, 3, 4, 5, 6), 6),
+            )
+
+        forAll(testCases) { pickNumbers, matchedCount ->
+            // when
+            val result = LottoMatcher.match(listOf(Lotto.of(pickNumbers)), givenWinningNumbers)
+
+            // then
+            result.first().matchCount shouldBe matchedCount
+        }
+    }
+})
