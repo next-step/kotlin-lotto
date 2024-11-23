178852958116dde009f1f86934d132dcbc911a14
SeolYoungKim <nasur4da@gmail.com>
2024-11-23 20:04:14 +0900

    feat: 기본 구분자를 사용하여 연산하는 기능 구현

--- src/main/kotlin/stringaddcalculator/StringAddCalculator.kt
+++ src/main/kotlin/stringaddcalculator/StringAddCalculator.kt
@@ -10,14 +10,30 @@ class StringAddCalculator {
             return text.toInt()
         }
 
-        return 0
+        val numbers = getNumbers(text)
+        return sumAll(numbers)
     }
 
     private fun containsOnlyOneNumber(text: String): Boolean {
-        return text.matches(digitsOnlyRegex)
+        return text.matches(DIGITS_ONLY_REGEX)
+    }
+
+    private fun getNumbers(text: String): List<Int> {
+        if (text.contains(CUSTOM_DELIMITER_REGEX)) {
+            TODO()
+        }
+
+        return text.split(DEFAULT_DELIMITER_REGEX)
+            .map { strNumber -> strNumber.toInt() }
+    }
+
+    private fun sumAll(numbers: List<Int>): Int {
+        return numbers.sum()
     }
 
     companion object {
-        private val digitsOnlyRegex = Regex("^[0-9]+\$")
+        private val DIGITS_ONLY_REGEX = Regex("^[0-9]+\$")
+        private val DEFAULT_DELIMITER_REGEX = Regex("[,:]")
+        private val CUSTOM_DELIMITER_REGEX = Regex("//(.)\\n(.*)")
     }
 }
