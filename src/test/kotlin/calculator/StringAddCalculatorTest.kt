6f371ab6ae0162d36920c9ae363a536829d54a56
Duhan Mo <duhandv@gmail.com>
2024-11-23 17:49:32 +0900

    refactor: 객체 분리

--- src/test/kotlin/calculator/StringAddCalculatorTest.kt
+++ src/test/kotlin/calculator/StringAddCalculatorTest.kt
@@ -1,40 +1,10 @@
 package calculator
 
 import calculator.StringAddCalculator.add
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
-
-
-object StringAddCalculator {
-    private const val FIND_DELIMITER_PATTERN = "(?<=//)(.*?)(?=\\n)"
-    private const val FIND_ADD_TEXT_PATTERN = "(?<=\\n)(.*)"
-    private val DELIMITER_REGEX = Regex(FIND_DELIMITER_PATTERN)
-    private val ADD_TEXT_REGEX = Regex(FIND_ADD_TEXT_PATTERN)
-    private val DEFAULT_DELIMITERS = listOf(",", ":")
-
-
-    fun add(text: String?): Int {
-        if (text.isNullOrBlank()) {
-            return 0
-        }
-        return parse(text).sum()
-    }
-
-    private fun parse(text: String): List<Int> {
-        val customDelimiter = DELIMITER_REGEX.find(text)?.value
-        if(text.startsWith("//")) {
-
-        }
-        val addText = ADD_TEXT_REGEX.find(text)?.value
-        val delimiters = customDelimiter?.let {
-            DEFAULT_DELIMITERS + it
-        } ?: DEFAULT_DELIMITERS
-        val map = addText?.split(*delimiters.toTypedArray())
-        val map1 = map?.map { it.toInt() }
-        return map1.orEmpty()
-    }
-}
 
 class StringAddCalculatorTest : StringSpec({
     "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다" {
@@ -59,5 +29,15 @@ class StringAddCalculatorTest : StringSpec({
         add("//;\n1;2;3") shouldBe 6
     }
 
-    "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다" {}
+    "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다" {
+        shouldThrow<RuntimeException> {
+            add("-1")
+        }
+    }
+
+    "숫자가 아닌 글자를 입력하는 경우 예외 발생한다" {
+        shouldThrow<IllegalArgumentException> {
+            add("1,a,b")
+        }
+    }
 })
