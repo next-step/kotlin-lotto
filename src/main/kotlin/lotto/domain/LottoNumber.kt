b3d48c81fa4f3c24801165677484bb00de783e4b
changhwan <chang5hustle@gmail.com>
2024-11-24 20:14:36 +0900

    feat: 로또 번호는 1~45사이로 한정한다. 기능구현

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -1,7 +1,17 @@
 package lotto.domain
 
-class LottoNumber(val value: Int) {
+@JvmInline
+value class LottoNumber private constructor(val value: Int) {
     init {
         require(value in 1..45)
+    }
+
+    companion object {
+        private val lottoNumbers: MutableMap<Int, LottoNumber> = mutableMapOf()
+
+        fun get(lottoNumber: Int): LottoNumber =
+            lottoNumbers[lottoNumber] ?: LottoNumber(lottoNumber).apply {
+                lottoNumbers[lottoNumber] = this
+            }
     }
 }
--- src/test/kotlin/lotto/domain/LottoNumberTest.kt
+++ src/test/kotlin/lotto/domain/LottoNumberTest.kt
@@ -8,16 +8,16 @@ import io.kotest.inspectors.forAll
 class LottoNumberTest : StringSpec({
     "로또 번호는 1~45 사이 값이여야 한다." {
         (1..45).toList().forAll { lottoNumber ->
-            shouldNotThrowAny { LottoNumber(lottoNumber) }
+            shouldNotThrowAny { LottoNumber.get(lottoNumber) }
         }
     }
 
     "1~45이 아닌 값으로 로또 번호를 생성할시 에러가 발생한다." {
         (-45..0).toList().forAll { lottoNumber ->
-            shouldThrowAny { LottoNumber(lottoNumber) }
+            shouldThrowAny { LottoNumber.get(lottoNumber) }
         }
         (46..100).toList().forAll { lottoNumber ->
-            shouldThrowAny { LottoNumber(lottoNumber) }
+            shouldThrowAny { LottoNumber.get(lottoNumber) }
         }
     }
 })
