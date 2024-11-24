84e1cbafff4b1d6076a322694fc8679c51e0037b
sksowk156 <sksowk156@gmail.com>
2024-11-24 18:28:38 +0900

    [Refactor] 역할에 따라 메서드 분리

--- src/main/kotlin/string_calculator/StringAddCalculator.kt
+++ src/main/kotlin/string_calculator/StringAddCalculator.kt
@@ -2,25 +2,32 @@ package string_calculator
 
 class StringAddCalculator {
     fun calculate(input: String): Int {
-        val (delimiters, expression) =
-            if (input.startsWith("//")) {
-                val matchResult = Regex("//(.)\n(.*)").find(input) ?: throw RuntimeException("커스텀 구분자 설정이 제대로 되지 않았습니다")
-                val customDelimiter = matchResult.groupValues[1]
-                val numbersPart = matchResult.groupValues[2]
-                println("customDelimiter ${customDelimiter}\nnumbersPart ${numbersPart}")
+        val (delimiters: Regex, expression: String) =
+            anaylzeInput(input)
 
-                "$customDelimiter|,|:".toRegex() to numbersPart
-            } else {
-                ",|:".toRegex() to input
-            }
+        val expressionTokens = parseToInts(expression, delimiters)
 
-        val expressionTokens = expression.split(delimiters)
-        println("$expressionTokens")
-        return expressionTokens.sumOf {
+        val result = expressionTokens.sum()
+        return result
+    }
 
+    private fun anaylzeInput(input: String) =
+        if (input.startsWith("//")) {
+            val matchResult = Regex("//(.)\n(.*)").find(input) ?: throw RuntimeException("커스텀 구분자 설정이 제대로 되지 않았습니다")
+            val customDelimiter = matchResult.groupValues[1]
+            val numbersPart = matchResult.groupValues[2]
+
+            "$customDelimiter|,|:".toRegex() to numbersPart
+        } else {
+            ",|:".toRegex() to input
+        }
+
+    private fun parseToInts(expression: String, delimiters: Regex): List<Int> {
+        val expressionTokens = expression.split(delimiters).map {
             val num = if (it.isBlank()) 0 else it.toIntOrNull() ?: throw RuntimeException("숫자로 변환할 수 없습니다")
             if (num < 0) throw RuntimeException("음수는 입력할 수 없습니다")
             num
         }
+        return expressionTokens
     }
 }
