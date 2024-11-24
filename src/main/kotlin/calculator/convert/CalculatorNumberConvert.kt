18a5cb9fef7bf1218c19de84bba309c2dfc47970
janghomoon <sonyjangho@gmail.com>
2024-11-24 16:47:14 +0900

    fix: 문자엘계산기 피드백 받은 부분 수정

--- src/main/kotlin/calculator/convert/CalculatorNumberConvert.kt
+++ src/main/kotlin/calculator/convert/CalculatorNumberConvert.kt
@@ -1,19 +1,31 @@
 package calculator.convert
 
-object CalculatorNumberConvert {
-    fun convertInt(target: String): Int {
+import kotlin.math.sign
 
-        val convertedNumber:Int
+object CalculatorNumberConvert {
+    private val EXCEPTION_MESSAGE = "음수는 더 할 수 업습니다."
+
+    fun convertInt(target: String): Int {
+        val convertedNumber: Int
         try {
             convertedNumber = Integer.parseInt(target)
         } catch (ex: NumberFormatException) {
             throw IllegalStateException("$target 은 숫자 가 아닙니다.")
         }
         validateNegative(convertedNumber)
-        return convertedNumber
+        return positiveNumber(convertedNumber)
+    }
+
+    private fun positiveNumber(target: Int): Int {
+        require(target >= 0) { "$target 은 양수가 아닙니다." }
+        return target
     }
 
     private fun validateNegative(convertedNumber: Int) {
-        if (convertedNumber < 0) throw RuntimeException("음수는 더 할 수 업습니다.")
+        when {
+            convertedNumber and Int.MIN_VALUE != 0 -> throw RuntimeException(EXCEPTION_MESSAGE)
+            convertedNumber.sign < 0 -> throw RuntimeException(EXCEPTION_MESSAGE)
+            convertedNumber < 0 -> throw RuntimeException(EXCEPTION_MESSAGE)
+        }
     }
 }
