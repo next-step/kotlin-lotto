03c6ed75d72cfa3d09c1a79f52e0abbf9c5a5790
pkch93 <dev.pkch@gmail.com>
2024-11-24 09:07:20 +0900

    [step2] 로또 당첨 번호 구현

--- src/main/kotlin/lotto/WinningLotto.kt
+++ src/main/kotlin/lotto/WinningLotto.kt
@@ -2,4 +2,30 @@ package lotto
 
 class WinningLotto(
     private val lotto: Lotto,
-)
+) {
+    fun match(lotto: Lotto): Reward {
+        val matchingNumberCount = this.lotto
+            .numbers
+            .count { lotto.numbers.contains(it) }
+        return Reward.of(matchingNumberCount)
+    }
+}
+
+enum class Reward(
+    val money: Int,
+    val matchingNumberCount: Int,
+) {
+    FIRST(2_000_000_000, 6),
+    SECOND(1_500_000, 5),
+    THIRD(50_000, 4),
+    FOURTH(5_000, 3),
+    NONE(0, 0)
+    ;
+
+    companion object {
+        fun of(matchingNumberCount: Int): Reward =
+            entries.filter { it != NONE }
+                .find { it.matchingNumberCount == matchingNumberCount }
+                ?: NONE
+    }
+}
