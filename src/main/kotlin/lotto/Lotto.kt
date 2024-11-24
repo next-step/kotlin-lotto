eea6cccd8d0ae818d875ad509a92134acbdb7d91
SeolYoungKim <nasur4da@gmail.com>
2024-11-24 09:52:26 +0900

    refactor: method 추출

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,11 +2,21 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
+        validateLottoNumberCount()
+        validateLottoNumber()
+    }
+
+    private fun validateLottoNumberCount() {
         if (numbers.size != LottoPolicy.LOTTO_NUMBER_COUNT) {
             throw InvalidLottoNumberCountException(numbers)
         }
+    }
 
-        val invalidNumberCount = numbers.count { number -> number < LottoPolicy.MIN_LOTTO_NUMBER || number > LottoPolicy.MAX_LOTTO_NUMBER }
+    private fun validateLottoNumber() {
+        val invalidNumberCount = numbers.count { number ->
+            number < LottoPolicy.MIN_LOTTO_NUMBER || number > LottoPolicy.MAX_LOTTO_NUMBER
+        }
+
         if (invalidNumberCount != 0) {
             throw InvalidLottoNumberException(numbers)
         }
