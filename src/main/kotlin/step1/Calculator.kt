c9fbede13ba5dc934d7abd0d44370773f369b251
saerang <lsg10119@gamail.com>
2024-11-24 21:45:52 +0900

    refactor(step1): 메서드 추출 및 테스트코드 추가

--- src/main/kotlin/step1/Calculator.kt
+++ src/main/kotlin/step1/Calculator.kt
@@ -1,33 +1,46 @@
 package step1
 
-private const val FIRST_DELIMITERS = ","
-private const val SECOND_DELIMITERS = ":"
+private const val DEFAULT_DELIMITERS = ",|:"
+private const val CUSTOM_DELIMITER_PREFIX = "//"
+private const val NEW_LINE = "\\n"
 
 object Calculator {
     fun sum(expression: String): Int {
-        if (expression.startsWith("//") && expression.contains("\\n")) {
-            val startIndex = expression.indexOf("//") + 2
-            val endIndex = expression.indexOf("\\n")
-            val separator = expression.substring(startIndex, endIndex)
+        val numbers: List<Int>
 
-            val data = expression.substring(endIndex + 2)
-
-            val numbers = data.split(separator).filter { it.isNotEmpty() }
-                .map { it.toIntOrNull() ?: throw IllegalArgumentException("입력이 잘못 되었습니다. 입력값: $it") }
-
-            if (numbers.any { it < 0 }) {
-                throw IllegalArgumentException("음수는 넣을 수 없습니다.")
-            }
-
-            return numbers.sum()
+        if (expression.startsWith(CUSTOM_DELIMITER_PREFIX) && expression.contains(NEW_LINE)) {
+            numbers = convertToInt(customSplit(expression), customSeparator(expression))
+        } else {
+            numbers = convertToInt(expression, DEFAULT_DELIMITERS)
         }
 
-        val numbers = expression.split(FIRST_DELIMITERS, SECOND_DELIMITERS)
-            .filter { it.isNotEmpty() }
-            .map { it.toIntOrNull() ?: throw IllegalArgumentException("입력이 잘못 되었습니다. 입력값: $it") }
+        return calculate(numbers)
+    }
 
+    private fun customSeparator(expression: String): String {
+        val startIndex = expression.indexOf(CUSTOM_DELIMITER_PREFIX) + 2
+        val endIndex = expression.indexOf(NEW_LINE)
+
+        return expression.substring(startIndex, endIndex)
+    }
+
+    private fun customSplit(expression: String): String {
+        val endIndex = expression.indexOf(NEW_LINE)
+
+        return expression.substring(endIndex + 2)
+    }
+
+    private fun convertToInt(data: String, delimiters: String): List<Int> {
+        val splitNumbers = data.split(Regex(delimiters))
+
+        return splitNumbers
+            .filter { it.isNotEmpty() }
+            .map { it.toIntOrNull() ?: throw IllegalArgumentException("잘못된 입력값: $it") }
+    }
+
+    private fun calculate(numbers: List<Int>): Int {
         if (numbers.any { it < 0 }) {
-            throw IllegalArgumentException("음수는 넣을 수 없습니다.")
+            throw IllegalArgumentException("음수는 입력할 수 없습니다.")
         }
 
         return numbers.sum()
--- src/test/kotlin/step1/CalculatorTest.kt
+++ src/test/kotlin/step1/CalculatorTest.kt
@@ -1,15 +1,33 @@
 package step1
 
-import org.assertj.core.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class CalculatorTest {
-
     @Test
     fun `쉼표 또는 콜론을 구분자로 가지는 문자열을 받는다`() {
         val sum = Calculator.sum("1,2")
 
         assertThat(sum).isEqualTo(3)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2:3", "1:2,3"])
+    fun `구분자를 쉼표 이외에 콜론을 사용할 수 있다`(input: String) {
+        val sum = Calculator.sum(input)
+
+        assertThat(sum).isEqualTo(6)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "2", "3"])
+    fun `숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다`(input: String) {
+        val sum = Calculator.sum(input)
+
+        assertThat(sum).isEqualTo(input.toInt())
     }
 
     @Test
