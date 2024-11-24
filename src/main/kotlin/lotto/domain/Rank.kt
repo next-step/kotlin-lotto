9e46bc66a2627a32d5831fdad885d5d6d9487b79
Giwan Kim <giwan.kim@gmail.com>
2024-11-24 11:45:17 +0900

    refactor: when 절의 로직을 Rank enum 상수의 속성으로 옮겼다.

--- src/main/kotlin/lotto/domain/Rank.kt
+++ src/main/kotlin/lotto/domain/Rank.kt
@@ -3,23 +3,17 @@ package lotto.domain
 enum class Rank(
     val count: Int,
     val prize: Int,
+    val match: (Int) -> Boolean,
 ) {
-    FIRST(6, 2_000_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    MISS(0, 0),
+    FIRST(6, 2_000_000_000, { it == 6 }),
+    THIRD(5, 1_500_000, { it == 5 }),
+    FOURTH(4, 50_000, { it == 4 }),
+    FIFTH(3, 5_000, { it == 3 }),
+    MISS(0, 0, { it in 0..2 }),
     ;
 
     companion object {
         fun valueOf(count: Int): Rank =
-            when {
-                count == 6 -> FIRST
-                count == 5 -> THIRD
-                count == 4 -> FOURTH
-                count == 3 -> FIFTH
-                count in 0..2 -> MISS
-                else -> throw IllegalArgumentException("${count}에 해당하는 등수가 없습니다.")
-            }
+            entries.firstOrNull { it.match(count) } ?: throw IllegalArgumentException("${count}에 해당하는 등수가 없습니다.")
     }
 }
