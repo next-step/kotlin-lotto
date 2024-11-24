2c6e0c6fa5896b99c96925a151f3302a98934d3e
pkch93 <dev.pkch@gmail.com>
2024-11-24 09:52:10 +0900

    [step2] Reward, LottoResult 별도 파일로 분리

--- src/main/kotlin/lotto/BoughtLotto.kt
+++ src/main/kotlin/lotto/BoughtLotto.kt
@@ -22,15 +22,3 @@ class BoughtLotto(
         return initialRewards
     }
 }
-
-data class LottoResult(
-    private val rewards: Map<Reward, Int>,
-) {
-    val winningRewards: Map<Reward, Int> = rewards.filter { it.key != Reward.NONE }
-
-    fun calculateRateOfReturn(): Double {
-        val totalRewardMoney = rewards.map { it.key.money * it.value }.sum()
-        val totalBoughtMoney = rewards.values.sum() * LOTTO_PRICE
-        return totalRewardMoney.toDouble() / totalBoughtMoney
-    }
-}
--- src/main/kotlin/lotto/WinningLotto.kt
+++ src/main/kotlin/lotto/WinningLotto.kt
@@ -10,22 +10,3 @@ class WinningLotto(
         return Reward.of(matchingNumberCount)
     }
 }
-
-enum class Reward(
-    val money: Int,
-    val matchingNumberCount: Int,
-) {
-    FIRST(2_000_000_000, 6),
-    SECOND(1_500_000, 5),
-    THIRD(50_000, 4),
-    FOURTH(5_000, 3),
-    NONE(0, 0)
-    ;
-
-    companion object {
-        fun of(matchingNumberCount: Int): Reward =
-            entries.filter { it != NONE }
-                .find { it.matchingNumberCount == matchingNumberCount }
-                ?: NONE
-    }
-}
