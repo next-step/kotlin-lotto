f0e2a7263d1e960a93972550e1b49533306be2d4
pkch93 <dev.pkch@gmail.com>
2024-11-24 09:08:11 +0900

    [step2] BoughtLottoTest 통과 하도록 구현

--- src/main/kotlin/lotto/BoughtLotto.kt
+++ src/main/kotlin/lotto/BoughtLotto.kt
@@ -3,4 +3,11 @@ package lotto
 class BoughtLotto (
     private val lottos: List<Lotto>,
     private val winningLotto: WinningLotto,
-)
+) {
+    fun matchResult(): Map<Reward, Int> {
+        return lottos
+            .map { winningLotto.match(it) }
+            .groupingBy { it }
+            .eachCount()
+    }
+}
