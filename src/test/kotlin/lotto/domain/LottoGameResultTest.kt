1b46c8786905cad498b5eab1db1f0dbc00c3a917
dino.shin <troas96@naver.com>
2024-11-24 12:29:22 +0900

    refactor: 주석 처리된 코드 제거

--- src/test/kotlin/lotto/domain/LottoGameResultTest.kt
+++ src/test/kotlin/lotto/domain/LottoGameResultTest.kt
@@ -59,10 +59,3 @@ class LottoGameResultTest : StringSpec({
         }
     }
 })
-
-// class LottoGameResult(private val lottoResult: Map<LottoRank, Int>) {
-//    fun makeLottoProfitRate(purchaseCount: LottoPurchaseCount): LottoProfitRate {
-//        val totalPrize = lottoResult.map { it.key.prize * it.value }.sum()
-//        return LottoProfitRate(totalPrize, purchaseCount)
-//    }
-// }
