5a5e999c0eb84213bf72c8497295f607f6e1dab1
sksowk156 <sksowk156@gmail.com>
2024-11-24 20:18:53 +0900

    [Refactor] 역할에 따라 클래스 분리, 네이밍 변경

--- src/main/kotlin/string_calculator/InputView.kt
+++
@@ -1,8 +0,0 @@
-package string_calculator
-
-class InputView(private val requestMessage: String) {
-    fun getInputMessage(): String {
-        println(requestMessage)
-        return readlnOrNull()?.trim() ?: ""
-    }
-}
--- src/main/kotlin/string_calculator/StringAddCalculator.kt
+++ src/main/kotlin/string_calculator/StringAddCalculator.kt
@@ -1,33 +1,10 @@
 package string_calculator
 
-class StringAddCalculator {
+class StringAddCalculator(
+    private val operandsExtractor: OperandsExtractor = OperandsExtractorImpl()
+) {
     fun calculate(input: String): Int {
-        val (delimiters: Regex, expression: String) =
-            anaylzeInput(input)
-
-        val expressionTokens = parseToInts(expression, delimiters)
-
-        val result = expressionTokens.sum()
-        return result
-    }
-
-    private fun anaylzeInput(input: String) =
-        if (input.startsWith("//")) {
-            val matchResult = Regex("//(.)\n(.*)").find(input) ?: throw RuntimeException("커스텀 구분자 설정이 제대로 되지 않았습니다")
-            val customDelimiter = matchResult.groupValues[1]
-            val numbersPart = matchResult.groupValues[2]
-
-            "$customDelimiter|,|:".toRegex() to numbersPart
-        } else {
-            ",|:".toRegex() to input
-        }
-
-    private fun parseToInts(expression: String, delimiters: Regex): List<Int> {
-        val expressionTokens = expression.split(delimiters).map {
-            val num = if (it.isBlank()) 0 else it.toIntOrNull() ?: throw RuntimeException("숫자로 변환할 수 없습니다")
-            if (num < 0) throw RuntimeException("음수는 입력할 수 없습니다")
-            num
-        }
-        return expressionTokens
+        val operands = operandsExtractor.extract(input)
+        return operands.sum()
     }
 }
