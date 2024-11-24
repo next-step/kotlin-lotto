2b51440d53a597f31cae27aab8cb0caef05e221f
SeolYoungKim <nasur4da@gmail.com>
2024-11-24 09:47:52 +0900

    feat: 로또 객체 생성 시 제약조건 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,3 +1,14 @@
 package lotto
 
-class Lotto(val numbers: List<Int>)
+class Lotto(private val numbers: List<Int>) {
+    init {
+        if (numbers.size != 6) {
+            throw InvalidLottoNumberCountException(numbers)
+        }
+
+        val invalidNumberCount = numbers.count { number -> number < 1 || number > 45 }
+        if (invalidNumberCount != 0) {
+            throw InvalidLottoNumberException(numbers)
+        }
+    }
+}
