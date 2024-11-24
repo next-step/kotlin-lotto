3682923028731a48d554933ee85f96a7f55b6925
kimihiqq <kimihiqq@gmail.com>
2024-11-24 21:10:58 +0900

    refactor: 코드리뷰 반영

--- src/main/kotlin/stringcalculator/domain/StringCalculator.kt
+++ src/main/kotlin/stringcalculator/domain/StringCalculator.kt
@@ -1,60 +1,14 @@
 package stringcalculator.domain
 
-class StringCalculator {
+class StringCalculator(
+    private val delimiterExtractor: DelimiterExtractor = DelimiterExtractor(),
+    private val numberParser: NumberParser = NumberParser(),
+) {
     fun add(input: String?): Int {
         if (input.isNullOrBlank()) return 0
 
-        return if (isCustomDelimiter(input)) {
-            handleCustomDelimiter(input)
-        } else {
-            handleDefaultDelimiter(input)
-        }
-    }
-
-    private fun isCustomDelimiter(input: String): Boolean {
-        return input.startsWith("//")
-    }
-
-    private fun handleCustomDelimiter(input: String): Int {
-        val (customDelimiter, numbersPart) = extractCustomDelimiterAndNumbers(input)
-        val delimitersRegex = createDelimiterRegex(customDelimiter)
-        return splitAndSum(numbersPart, delimitersRegex)
-    }
-
-    private fun handleDefaultDelimiter(input: String): Int {
-        val defaultDelimitersRegex = Regex("[,:]")
-        return splitAndSum(input, defaultDelimitersRegex)
-    }
-
-    private fun extractCustomDelimiterAndNumbers(input: String): Pair<String, String> {
-        val regex = Regex("//(.*)\n(.*)")
-        val matchResult =
-            regex.find(input)
-                ?: throw RuntimeException("Invalid input format: $input")
-        val customDelimiter = matchResult.groupValues[1]
-        val numbersPart = matchResult.groupValues[2]
-        return customDelimiter to numbersPart
-    }
-
-    private fun createDelimiterRegex(customDelimiter: String): Regex {
-        return Regex("[${Regex.escape(customDelimiter)},:]")
-    }
-
-    private fun splitAndSum(
-        numbersPart: String,
-        delimitersRegex: Regex,
-    ): Int {
-        return numbersPart.split(delimitersRegex)
-            .filter { it.isNotBlank() }.sumOf { validateAndParse(it) }
-    }
-
-    private fun validateAndParse(value: String): Int {
-        val number =
-            value.toIntOrNull()
-                ?: throw RuntimeException("Invalid number: $value")
-        if (number < 0) {
-            throw RuntimeException("Negative numbers are not allowed: $value")
-        }
-        return number
+        val (delimitersRegex, numbersPart) = delimiterExtractor.extract(input)
+        val numbers = numberParser.parse(numbersPart, delimitersRegex)
+        return numbers.sum()
     }
 }
---
+++ src/test/kotlin/stringcalculator/domain/DelimiterExtractorTest.kt
@@ -0,0 +1,22 @@
+package stringcalculator.domain
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class DelimiterExtractorTest : StringSpec(
+    {
+        "should extract custom delimiter and numbers part" {
+            val extractor = DelimiterExtractor()
+            val (regex, numbersPart) = extractor.extract("//;\n1;2;3")
+            regex.toString() shouldBe "[\\Q;\\E,:]"
+            numbersPart shouldBe "1;2;3"
+        }
+
+        "should return default delimiters for basic input" {
+            val extractor = DelimiterExtractor()
+            val (regex, numbersPart) = extractor.extract("3:5")
+            regex.toString() shouldBe "[,:]"
+            numbersPart shouldBe "3:5"
+        }
+    },
+)
