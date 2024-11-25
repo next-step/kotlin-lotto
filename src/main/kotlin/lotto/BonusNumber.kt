aeb792f05fec4a768a9bcb5d71bdf4eec2221101
pkch93 <dev.pkch@gmail.com>
2024-11-25 21:08:19 +0900

    [step3] 보너스 볼 구현 추가

--- src/main/kotlin/lotto/BonusNumber.kt
+++ src/main/kotlin/lotto/BonusNumber.kt
@@ -3,5 +3,14 @@ package lotto
 class BonusNumber(
     value: String?,
 ) {
-    val value: LottoNumber = LottoNumber(value!!.toInt())
+    val value: LottoNumber
+
+    init {
+        requireNotNull(value) { "보너스 볼 입력은 필수입니다." }
+        this.value = try {
+            LottoNumber(value.toInt())
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("보너스 볼은 문자가 될 수 없습니다.")
+        }
+    }
 }
