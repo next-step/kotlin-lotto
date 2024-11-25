2da4f6c0b14f40cda19e11b0ac7d967d28ad5cc3
SeongminKimKR <gvf156@gmail.com>
2024-11-25 23:22:40 +0900

    refactor: 당첨 로또 중복 검증 로직 확장 함수로 추출

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -5,8 +5,10 @@ class WinningLotto(
     val bonusNumber: Number,
 ) {
     init {
-        require(!lotto.numbers.contains(bonusNumber)) { LOTTO_DUPLICATE_BONUS_EXCEPTION_MESSAGE }
+        require(!lotto.contains(bonusNumber)) { LOTTO_DUPLICATE_BONUS_EXCEPTION_MESSAGE }
     }
+
+    private fun Lotto.contains(number: Number): Boolean = this.getNumbersRawValues().contains(number.value)
 
     companion object {
         private const val LOTTO_DUPLICATE_BONUS_EXCEPTION_MESSAGE = "보너스 번호는 로또와 중복될 수 없습니다."
