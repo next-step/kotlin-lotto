f2fb8a0a06d74b535a4a0e5e79033e7a0165f685
nooblette <min9hyuk@gmail.com>
2024-11-26 07:26:56 +0900

    feat : 로또 수동 구매 개수와 번호 입력 기능 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,6 +1,7 @@
 package lotto.view
 
 import lotto.ball.BonusBall
+import lotto.number.Numbers
 
 object InputView {
     fun inputPurchaseAmount(): Int {
@@ -10,11 +11,31 @@ object InputView {
 
     fun inputLastWeekWinningNumbers(): List<Int> {
         println("지난 주 당첨 번호를 입력해 주세요.")
-        return (readlnOrNull() ?: "").split(",").map { it.trim().toInt() }
+        return getInputNumberList()
     }
 
     fun inputBonusBall(): BonusBall {
         println("보너스 볼을 입력해주세요.")
         return BonusBall(readlnOrNull()?.toInt() ?: 0)
     }
+
+    fun inputManualLottoCount(): Int {
+        println("수동으로 구매할 로또 수를 입력해 주세요.")
+        return readlnOrNull()?.toInt() ?: 0
+    }
+
+    fun inputManualLottoNumbers(lottoCount: Int): List<Numbers> {
+        println("수동으로 구매할 번호를 입력해 주세요.")
+        return generateLottoList(lottoCount)
+    }
+
+    private fun generateLottoList(lottoCount: Int) =
+        (1..lottoCount)
+            .map { Numbers(numbers = getInputNumberList()) }
+            .toList()
+
+    private fun getInputNumberList(): List<Int> =
+        (readlnOrNull() ?: "")
+            .split(",")
+            .map { it.trim().toInt() }
 }
