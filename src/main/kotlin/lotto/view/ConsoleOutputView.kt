4d6437f90c89115475b61f024d2667ded5e011f8
sian <chicori3@naver.com>
2024-11-25 23:11:13 +0900

    feat: 2등 출력 추가

--- src/main/kotlin/lotto/view/ConsoleOutputView.kt
+++ src/main/kotlin/lotto/view/ConsoleOutputView.kt
@@ -18,9 +18,10 @@ class ConsoleOutputView : OutputView {
         println("당첨 통계")
         println("---------")
 
-        println("3개 일치 (5000원)- ${lottoResult.fourth}개")
-        println("4개 일치 (50000원)- ${lottoResult.third}개")
-        println("5개 일치 (1500000원)- ${lottoResult.second}개")
+        println("3개 일치 (5000원)- ${lottoResult.fifth}개")
+        println("4개 일치 (50000원)- ${lottoResult.fourth}개")
+        println("5개 일치 (1500000원) - ${lottoResult.third}개")
+        println("5개 일치, 보너스 볼 일치 (30000000원)- ${lottoResult.second}개")
         println("6개 일치 (2000000000원)- ${lottoResult.first}개")
         println("총 수익률은 ${lottoResult.incomeRateValue}입니다.")
     }
