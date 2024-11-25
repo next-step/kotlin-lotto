7455e9068d6bf3555d8d2dc03eed4c438535d250
KimDabomi <cdabomi@nate.com>
2024-11-25 21:39:21 +0900

    [step2] refactor: validateNumbers 선언위치 변경

--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -1,5 +1,9 @@
 package lotto.domain
 
+private const val LOTTO_NUMBER_SIZE_EXCEPTION_MESSAGE = "로또 번호는 6개여야 합니다."
+private const val LOTTO_NUMBER_RANGE_EXCEPTION_MESSAGE = "로또 번호는 1부터 45까지만 가능합니다."
+private const val LOTTO_NUMBER_DUPLICATE_EXCEPTION_MESSAGE = "로또 번호는 중복되지 않아야 합니다."
+
 class LottoTicket {
     val lottoNumbers: List<Int>
 
@@ -21,15 +25,9 @@ class LottoTicket {
         return lottoNumbers.contains(bonusNumber.number)
     }
 
-    companion object {
-        private const val LOTTO_NUMBER_SIZE_EXCEPTION_MESSAGE = "로또 번호는 6개여야 합니다."
-        private const val LOTTO_NUMBER_RANGE_EXCEPTION_MESSAGE = "로또 번호는 1부터 45까지만 가능합니다."
-        private const val LOTTO_NUMBER_DUPLICATE_EXCEPTION_MESSAGE = "로또 번호는 중복되지 않아야 합니다."
-
-        fun validateNumbers(numbers: List<Int>) {
-            require(numbers.size == 6) { LOTTO_NUMBER_SIZE_EXCEPTION_MESSAGE }
-            require(numbers.all { it in 1..45 }) { LOTTO_NUMBER_RANGE_EXCEPTION_MESSAGE }
-            require(numbers.toSet().size == 6) { LOTTO_NUMBER_DUPLICATE_EXCEPTION_MESSAGE }
-        }
+    private fun validateNumbers(numbers: List<Int>) {
+        require(numbers.size == 6) { LOTTO_NUMBER_SIZE_EXCEPTION_MESSAGE }
+        require(numbers.all { it in 1..45 }) { LOTTO_NUMBER_RANGE_EXCEPTION_MESSAGE }
+        require(numbers.toSet().size == 6) { LOTTO_NUMBER_DUPLICATE_EXCEPTION_MESSAGE }
     }
 }
