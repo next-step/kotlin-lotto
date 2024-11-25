6db4230725694ae4a873a9bef76578dd84ebcf43
Aimbe <qkr2435@gmail.com>
2024-11-25 19:45:58 +0900

    fix: StringCalculator 간소화

--- src/main/kotlin/calculator/StringAddCalculator.kt
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -12,41 +12,9 @@ class StringAddCalculator {
             return text.toInt();
         }
 
-        val numbers = parse(text)
-        validateNumbers(numbers)
+        val numbers = NumberParser().parse(text)
+        NumberValidator().validate(numbers)
         return numbers.sum()
     }
 
-    private fun validateNumbers(numbers: List<Int>) {
-        if (numbers.any { it < 0 }) {
-            throw RuntimeException("음수는 허용되지 않습니다.")
-        }
-    }
-
-    fun parse(text: String): List<Int> {
-        return when {
-            text.length == 1 -> listOf(text.toInt())
-            text.length == 2 && text.startsWith("-") -> throw RuntimeException("음수는 허용되지 않습니다.")
-            text.startsWith("//") && text.contains("\n") -> parseWithCustomDelimiter(text)
-            text.contains(",") || text.contains(":") -> parseWithDefaultDelimiters(text)
-            text.contains("[,:]".toRegex()) -> parseWithDefaultDelimiters(text)
-            else -> emptyList()
-        }
-    }
-
-    private fun parseWithDefaultDelimiters(text: String): List<Int> {
-        return text.split("[,:]".toRegex())
-            .map { it.trim() }
-            .filter { it.isNotEmpty() }
-            .map { it.toInt() }
-    }
-
-    private fun parseWithCustomDelimiter(text: String): List<Int> {
-        val delimiter = text.substring(2, 3)
-        return text.substring(4)
-            .split(delimiter)
-            .map { it.trim() }
-            .filter { it.isNotEmpty() }
-            .map { it.toInt() }
-    }
 }
