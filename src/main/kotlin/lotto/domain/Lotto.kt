7ac4b75a09fa5cffec216fae42779d46f848f4b9
SeongminKimKR <gvf156@gmail.com>
2024-11-25 23:23:30 +0900

    feat: 수동 로또 입력 생성

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,6 +1,6 @@
 package lotto.domain
 
-class Lotto(val numbers: List<Number>) {
+class Lotto(private val numbers: List<Number>) {
     init {
         require(numbers.size == LOTTO_SIZE) { LOTTO_SIZE_EXCEPTION_MESSAGE }
         require(!existDuplicateNumber(numbers)) { LOTTO_DUPLICATE_EXCEPTION_MESSAGE }
@@ -21,9 +21,11 @@ class Lotto(val numbers: List<Number>) {
         private const val LOTTO_DUPLICATE_EXCEPTION_MESSAGE = "로또의 숫자들은 중복되지 않아야 합니다."
         const val LOTTO_SIZE = 6
 
-        fun createLottos(
+        fun createAutoLottos(
             amount: Int,
             lottoGenerator: LottoGenerator,
         ): List<Lotto> = List(amount) { lottoGenerator.generate(LOTTO_SIZE) }
+
+        fun from(numbers: List<Int>): Lotto = Lotto(numbers.map { Number(it) })
     }
 }
