e3fe9021e7c58b0fc51b6ee1ef139ad855271249
heesang.lee <sky1994601@hanmail.net>
2024-11-25 20:47:13 +0900

    feat: step4 로또(수동)
    판매 메인함수 구현

--- src/main/kotlin/lotto/Main.kt
+++ src/main/kotlin/lotto/Main.kt
@@ -10,9 +10,16 @@ import lotto.view.LottoView
 fun main() {
     println("구입금액을 입력해 주세요.")
     val purchasePrice = readln().toInt()
+    println("수동으로 구매할 로또 수를 입력해 주세요.")
+    val selectLottoCount = readln().toInt()
+    println("수동으로 구매할 번호를 입력해 주세요.")
+    val selectLottos: MutableList<LottoNumbers> = mutableListOf()
+    repeat(selectLottoCount) {
+        selectLottos.add(LottoNumbers(readln().split(",").map { LottoNumber(it.toInt()) }))
+    }
     val lottoSeller = LottoSeller()
-    val lottos = lottoSeller.sellLottos(purchasePrice)
-    println("${lottoSeller.getLottoPurchaseCount(purchasePrice)}개를 구매했습니다.")
+    val lottos = lottoSeller.sellLottos(purchasePrice, selectLottos)
+    println("수동 ${selectLottos.size}개 ${lottoSeller.getLottoPurchaseCount(purchasePrice) - selectLottos.size}개를 구매했습니다.")
     println("지난 주 당첨 번호를 입력해 주세요.")
     val winningNumbers = readln().split(",").map { LottoNumber(it.toInt()) }
     println("보너스 볼을 입력해 주세요.")
