d3b12767f27783742ea7dd93c573814a84d55061
nooblette <min9hyuk@gmail.com>
2024-11-26 06:19:14 +0900

    refactor : named arguments 추가

--- src/main/kotlin/lotto/rank/LottoRank.kt
+++ src/main/kotlin/lotto/rank/LottoRank.kt
@@ -9,7 +9,7 @@ enum class LottoRank(
     val matchBonus: Boolean = false,
 ) {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000, true),
+    SECOND(5, 30_000_000, matchBonus = true),
     THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
     FIFTH(3, 5_000),
