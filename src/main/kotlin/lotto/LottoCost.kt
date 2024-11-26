99e682fcbcd61d1e0356158c7a43e9cecf774ae2
pkch93 <dev.pkch@gmail.com>
2024-11-26 21:58:37 +0900

    [step4] LottoCost에서 수동 로또 구입 가능 갯수가 구입 가능 갯수를 초과시 예외상황 구현

--- src/main/kotlin/lotto/LottoCost.kt
+++ src/main/kotlin/lotto/LottoCost.kt
@@ -2,7 +2,6 @@ package lotto
 
 const val LOTTO_PRICE = 1000
 
-
 class LottoCost(
     val value: Int,
     val manualLottoAmount: Int,
@@ -11,5 +10,6 @@ class LottoCost(
 
     init {
         require(this.value >= 0) { "구입 금액은 유효한 양수로 입력해야합니다." }
+        require(this.autoLottoAmount >= 0) { "구입 가능한 로또 갯수를 초과했습니다." }
     }
 }
