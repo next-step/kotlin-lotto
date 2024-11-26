599dde435400f8e08120d0865e62784e8a1733c6
pkch93 <dev.pkch@gmail.com>
2024-11-26 22:02:52 +0900

    [step4] 보너스 볼이 당첨 로또 번호와 중복되는 경우 예외 처리 구현

--- src/main/kotlin/lotto/WinningLotto.kt
+++ src/main/kotlin/lotto/WinningLotto.kt
@@ -4,6 +4,10 @@ class WinningLotto(
     private val lotto: Lotto,
     private val bonusNumber: BonusNumber,
 ) {
+    init {
+        require(bonusNumber.value !in lotto.numbers) { "보너스 볼 번호는 당첨 번호와 중복될 수 없습니다." }
+    }
+
     fun match(lotto: Lotto): Reward {
         val matchingNumberCount = this.lotto
             .numbers
