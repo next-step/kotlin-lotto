64b4a78579b8c75aba377a3161d50c9de5b7b839
SeokHo-Ham <forkyy.dev@gmail.com>
2024-11-26 15:35:53 +0900

    refactor: Prize 클래스의 이름을 Rank로 변경 및 판별하는 함수 추가

---
+++ src/main/kotlin/lotto/domain/Rank.kt
@@ -0,0 +1,24 @@
+package lotto.domain
+
+enum class Rank(
+    val matchCount: Int,
+    val prizeAmount: Int,
+    private val match: (Int, Boolean) -> Boolean,
+) {
+    FIRST(6, 2_000_000_000, { count, _ -> count == 6 }),
+    SECOND(5, 30_000_000, { count, isBonusMatch -> count == 5 && isBonusMatch }),
+    THIRD(5, 1_500_000, { count, _ -> count == 5 }),
+    FOURTH(4, 50_000, { count, _ -> count == 4 }),
+    FIFTH(3, 5_000, { count, _ -> count == 3 }),
+    MISS(0, 0, { count, _ -> count < 3 }), ;
+
+    companion object {
+        fun findByMatchCount(
+            matchCount: Int,
+            isBonusMatch: Boolean = false,
+        ): Rank {
+            return entries.firstOrNull { it.match(matchCount, isBonusMatch) }
+                ?: throw RuntimeException("일치하는 숫자의 개수에 해당하는 상품이 존재하지 않습니다.")
+        }
+    }
+}
