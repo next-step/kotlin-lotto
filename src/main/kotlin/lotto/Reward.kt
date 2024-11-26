b40af4605f456c7769168611bc09df71fa760518
pkch93 <dev.pkch@gmail.com>
2024-11-26 21:08:43 +0900

    [step3] 리뷰 내용 반영
    - Reward의 당첨 정책 함수로 반영

--- src/main/kotlin/lotto/Reward.kt
+++ src/main/kotlin/lotto/Reward.kt
@@ -2,15 +2,14 @@ package lotto
 
 enum class Reward(
     val money: Int,
-    val matchingNumberCount: Int,
-    val needMatchBonus: Boolean,
+    val isMatch: (matchingNumberCount: Int, matchBonusNumber: Boolean) -> Boolean,
 ) {
-    FIRST(2_000_000_000, 6, false),
-    SECOND(30_000_000, 5, true),
-    THIRD(1_500_000, 5, false),
-    FOURTH(50_000, 4, false),
-    FIFTH(5_000, 3, false),
-    NONE(0, 0, false)
+    FIRST(2_000_000_000, { matchingNumberCount, _ -> matchingNumberCount == 6 }) ,
+    SECOND(30_000_000, { matchingNumberCount, matchBonusNumber -> matchingNumberCount == 5 && matchBonusNumber }),
+    THIRD(1_500_000, { matchingNumberCount, _ -> matchingNumberCount == 5 }),
+    FOURTH(50_000, { matchingNumberCount, _ -> matchingNumberCount == 4 }),
+    FIFTH(5_000, { matchingNumberCount, _ -> matchingNumberCount == 3 }),
+    NONE(0, { matchingNumberCount, _ -> matchingNumberCount < 3 })
     ;
 
     companion object {
@@ -19,10 +18,7 @@ enum class Reward(
             matchBonusNumber: Boolean,
         ): Reward =
             entries.filter { it != NONE }
-                .find {
-                    it.matchingNumberCount == matchingNumberCount &&
-                            (!it.needMatchBonus || matchBonusNumber)
-                }
+                .find { it.isMatch(matchingNumberCount, matchBonusNumber) }
                 ?: NONE
     }
 }
