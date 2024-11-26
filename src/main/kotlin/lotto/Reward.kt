3c41414bd71eb76c796ea6ba7d24a09583fb8cc6
pkch93 <dev.pkch@gmail.com>
2024-11-26 23:20:07 +0900

    [step4] Reward의 isMatch 내에 비교로 사용되는 숫자 matchingNumberCount 사용하도록 변경

--- src/main/kotlin/lotto/Reward.kt
+++ src/main/kotlin/lotto/Reward.kt
@@ -5,13 +5,36 @@ enum class Reward(
     val matchingNumberCount: Int,
     private val isMatch: (matchingNumberCount: Int, matchBonusNumber: Boolean) -> Boolean,
 ) {
-    FIRST(2_000_000_000, 6, { matchingNumberCount, _ -> matchingNumberCount == 6 }) ,
-    SECOND(30_000_000, 5, { matchingNumberCount, matchBonusNumber -> matchingNumberCount == 5 && matchBonusNumber }),
-    THIRD(1_500_000, 5, { matchingNumberCount, _ -> matchingNumberCount == 5 }),
-    FOURTH(50_000, 4, { matchingNumberCount, _ -> matchingNumberCount == 4 }),
-    FIFTH(5_000, 3, { matchingNumberCount, _ -> matchingNumberCount == 3 }),
-    NONE(0, 0, { matchingNumberCount, _ -> matchingNumberCount < 3 })
-    ;
+    FIRST(
+        money = 2_000_000_000,
+        matchingNumberCount = 6,
+        { matchingNumberCount, _ -> matchingNumberCount == FIRST.matchingNumberCount }
+    ) ,
+    SECOND(
+        money = 30_000_000,
+        matchingNumberCount = 5,
+        { matchingNumberCount, matchBonusNumber -> matchingNumberCount == SECOND.matchingNumberCount && matchBonusNumber }
+    ),
+    THIRD(
+        money = 1_500_000,
+        matchingNumberCount = 5,
+        { matchingNumberCount, _ -> matchingNumberCount == THIRD.matchingNumberCount }
+    ),
+    FOURTH(
+        money = 50_000,
+        matchingNumberCount = 4,
+        { matchingNumberCount, _ -> matchingNumberCount == FOURTH.matchingNumberCount }
+    ),
+    FIFTH(
+        money = 5_000,
+        matchingNumberCount = 3,
+        { matchingNumberCount, _ -> matchingNumberCount == FIFTH.matchingNumberCount }
+    ),
+    NONE(
+        money = 0,
+        matchingNumberCount = 0,
+        { matchingNumberCount, _ -> matchingNumberCount < FIFTH.matchingNumberCount }
+    );
 
     companion object {
         fun of(
