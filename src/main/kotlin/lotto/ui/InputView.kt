6cbeafdefd3b8d7f865f8113af86880ee27921b1
Giwan Kim <giwan.kim@gmail.com>
2024-11-26 15:00:43 +0900

    refactor: getNumber() 함수를 사용해서 인풋 로직을 단순화했다.

--- src/main/kotlin/lotto/ui/InputView.kt
+++ src/main/kotlin/lotto/ui/InputView.kt
@@ -7,25 +7,28 @@ import lotto.domain.WinningLine
 
 object InputView {
     private const val PAYMENT_PROMPT = "구입금액을 입력해 주세요."
-    private const val NUMBER_MANUAL_PROMPT = "수동으로 구매할 로또 수를 입력해 주세요."
+    private const val NUMBER_OF_MANUAL_PROMPT = "수동으로 구매할 로또 수를 입력해 주세요."
     private const val NUMBER_VALIDATION_FAIL = "숫자를 입력해 주세요."
     private const val WINNER_PROMPT = "지난 주 당첨 번호를 입력해 주세요."
     private const val WINNER_VALIDATION_FAIL = "당첨 번호를 , 로 구분하여 입력해 주세요. (예시: 1, 2, 3, 4, 5, 6)"
     private const val BONUS_BALL_PROMPT = "보너스 볼을 입력해 주세요."
     private val DELIMITER_PATTERN = "\\s*,\\s*".toRegex()
     private val LINE_PATTERN = """^\s*(\d+\s*,\s*){5}\d+\s*$""".toRegex()
+    private val NEWLINE = System.lineSeparator()
 
     fun getPayment(): LottoPayment {
-        println(PAYMENT_PROMPT)
-        val rawInput = readln()
-        if (!validateNumber(rawInput)) {
-            println(NUMBER_VALIDATION_FAIL)
-            return getPayment()
-        }
-        return LottoPayment.from(rawInput.toLong())
+        val number = getNumber(PAYMENT_PROMPT) ?: return getPayment()
+        return LottoPayment.from(number.toLong())
     }
 
-    fun getNumberOfManual(): Int = getNumber(NUMBER_MANUAL_PROMPT) ?: getNumberOfManual()
+    fun getNumberOfManual(): Int = getNumber(NEWLINE + NUMBER_OF_MANUAL_PROMPT) ?: getNumberOfManual()
+
+    fun getWinner(): WinningLine {
+        println(WINNER_PROMPT)
+        val line = getLine()
+        val bonusBall = getBonusBall()
+        return WinningLine(line, bonusBall)
+    }
 
     private fun getNumber(prompt: String): Int? {
         println(prompt)
@@ -34,13 +37,6 @@ object InputView {
             println(NUMBER_VALIDATION_FAIL)
         }
         return result
-    }
-
-    fun getWinner(): WinningLine {
-        println(WINNER_PROMPT)
-        val line: LottoLine = getLine()
-        val bonusBall: LottoNumber = getBonusBall()
-        return WinningLine(line, bonusBall)
     }
 
     private fun getLine(): LottoLine {
@@ -55,16 +51,9 @@ object InputView {
     }
 
     private fun getBonusBall(): LottoNumber {
-        println(BONUS_BALL_PROMPT)
-        val rawInput = readln()
-        if (!validateNumber(rawInput)) {
-            println(NUMBER_VALIDATION_FAIL)
-            return getBonusBall()
-        }
-        return LottoNumber.from(rawInput.toInt())
+        val number = getNumber(BONUS_BALL_PROMPT) ?: return getBonusBall()
+        return LottoNumber.from(number)
     }
-
-    private fun validateNumber(input: String): Boolean = input.toIntOrNull() != null
 
     private fun validateLine(line: String): Boolean = line.matches(LINE_PATTERN)
 }
