fb9f2abcce9d79082a3c96d2148897b0a0a88506
KimDabomi <cdabomi@nate.com>
2024-11-26 19:41:43 +0900

    [step3] refactor: 2등 로직 변경, property 추가

--- src/main/kotlin/lotto/domain/LottoRank.kt
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -1,8 +1,12 @@
 package lotto.domain
 
-enum class LottoRank(val matchCount: Int, val prize: Int) {
+enum class LottoRank(
+    val matchCount: Int,
+    val prize: Int,
+    val matchBonus: Boolean = false,
+) {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
+    SECOND(5, 30_000_000, true),
     THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
     FIFTH(3, 5000),
@@ -14,12 +18,9 @@ enum class LottoRank(val matchCount: Int, val prize: Int) {
             matchCount: Int,
             matchBonus: Boolean,
         ): LottoRank {
-            return when {
-                matchCount == 6 -> FIRST
-                matchCount == 5 && matchBonus -> SECOND
-                matchCount == 5 -> THIRD
-                else -> entries.find { it.matchCount == matchCount } ?: NONE
-            }
+            return entries.find { rank ->
+                rank.matchCount == matchCount && (!rank.matchBonus || matchBonus)
+            } ?: NONE
         }
     }
 }
