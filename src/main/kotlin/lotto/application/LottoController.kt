098e3dadfa6c163e5007ccfad3417727f2fd515f
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-26 09:18:08 +0900

    feat : 어플리케이션 계충 추가.

---
+++ src/main/kotlin/lotto/application/LottoController.kt
@@ -0,0 +1,17 @@
+package lotto.application
+
+import lotto.presentation.InputView
+import lotto.presentation.ResultView
+
+object LottoController {
+    fun start() {
+        val purchaseAmount = InputView.inputPurchaseAmount()
+        val lottos = LottoMarketService.start(purchaseAmount)
+        ResultView.printLottos(lottos)
+
+        val winningNumber = InputView.inputWinningNumber()
+        val winningStatistics = LottoWinningStatisticsService.start(lottos, winningNumber)
+
+        ResultView.printWinningStatistics(winningStatistics)
+    }
+}
