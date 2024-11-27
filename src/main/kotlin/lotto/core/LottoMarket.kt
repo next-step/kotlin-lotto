b70f863274294f6d8be1b3ca55bee58494135982
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-27 21:24:55 +0900

    refactor: Lotto의 난수 발생기능 이관

--- src/main/kotlin/lotto/core/LottoMarket.kt
+++ src/main/kotlin/lotto/core/LottoMarket.kt
@@ -10,12 +10,16 @@ object LottoMarket {
     }
 
     private fun calculatePurchasableCount(purchaseAmount: String): Int {
-
-        val amount = purchaseAmount.toIntOrNull() ?: throw RuntimeException()
+        val amount = purchaseAmount.toIntOrNull() ?: throw IllegalArgumentException("잘못된 금액이 입력되었습니다.")
         return amount / LottoConstants.LOTTO_PRICE
     }
 
     private fun issueLotto(count: Int): List<Lotto> {
-        return List(count) { Lotto() }
+        return List(count) { Lotto(generateNumbers()) }
     }
+
+    private fun generateNumbers(): List<Int> =
+        (LottoConstants.LOTTO_NUMBER_MIN..LottoConstants.LOTTO_NUMBER_MAX).shuffled().take(
+            LottoConstants.LOTTO_NUMBER_COUNT,
+        ).sorted()
 }
