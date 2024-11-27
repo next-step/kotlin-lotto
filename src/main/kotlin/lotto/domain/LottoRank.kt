44043374b52accc66d3460d36038c51357d3643b
dino.shin <troas96@naver.com>
2024-11-27 21:37:12 +0900

    refactor: LottoRank 구조 변경

--- src/main/kotlin/lotto/domain/LottoRank.kt
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -6,13 +6,59 @@ enum class LottoRank(
     val matchCount: Int,
     val isBonusRequired: Boolean = false,
 ) {
-    FIRST(1, 2_000_000_000, 6),
-    SECOND(2, 30_000_000, 5, true),
-    THIRD(3, 1_500_000, 5),
-    FOURTH(4, 50_000, 4),
-    FIFTH(5, 5_000, 3),
-    NO_RANK(6, 0, 0),
-    ;
+    FIRST(1, 2_000_000_000, 6) {
+        override fun matches(
+            matchCount: Int,
+            bonusMatch: Boolean,
+        ): Boolean {
+            return this.matchCount == matchCount
+        }
+    },
+    SECOND(2, 30_000_000, 5, true) {
+        override fun matches(
+            matchCount: Int,
+            bonusMatch: Boolean,
+        ): Boolean {
+            return this.matchCount == matchCount && bonusMatch
+        }
+    },
+    THIRD(3, 1_500_000, 5) {
+        override fun matches(
+            matchCount: Int,
+            bonusMatch: Boolean,
+        ): Boolean {
+            return this.matchCount == matchCount && !bonusMatch
+        }
+    },
+    FOURTH(4, 50_000, 4) {
+        override fun matches(
+            matchCount: Int,
+            bonusMatch: Boolean,
+        ): Boolean {
+            return this.matchCount == matchCount
+        }
+    },
+    FIFTH(5, 5_000, 3) {
+        override fun matches(
+            matchCount: Int,
+            bonusMatch: Boolean,
+        ): Boolean {
+            return this.matchCount == matchCount
+        }
+    },
+    NO_RANK(6, 0, 0) {
+        override fun matches(
+            matchCount: Int,
+            bonusMatch: Boolean,
+        ): Boolean {
+            return this.matchCount == 0
+        }
+    }, ;
+
+    abstract fun matches(
+        matchCount: Int,
+        bonusMatch: Boolean,
+    ): Boolean
 
     companion object {
         fun fromMatchAndBonus(
@@ -21,12 +67,5 @@ enum class LottoRank(
         ): LottoRank {
             return entries.firstOrNull { it.matches(matchCount, bonusMatch) } ?: NO_RANK
         }
-    }
-
-    private fun matches(
-        matchCount: Int,
-        bonusMatch: Boolean,
-    ): Boolean {
-        return this.matchCount == matchCount && (!isBonusRequired || (isBonusRequired && bonusMatch))
     }
 }
