9f2279fcd5cb8a0b61120767b6e29c9d804a934e
changhwan <chang5hustle@gmail.com>
2024-11-27 21:59:15 +0900

    feat: 로또 구현 및 기능목록 업데이트

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,3 +1,36 @@
 package lotto.domain
 
-class Lotto()
+class Lotto(private val lottoNumberGenerator: LottoNumberGenerator? = null, vararg lottoNumber: Int) {
+    init {
+        require(!(lottoNumberGenerator == null && lottoNumber.isEmpty())) { LOTTO_PARAMETERS_ERROR_MESSAGE }
+    }
+
+    val lottoNumbers: Set<LottoNumber> =
+        if (lottoNumber.isNotEmpty()) {
+            getByManual(lottoNumber)
+        } else {
+            getByAuto()
+        }
+
+    private fun getByAuto(): MutableSet<LottoNumber> {
+        lottoNumberGenerator ?: return mutableSetOf()
+        val lotto: MutableSet<LottoNumber> = mutableSetOf()
+        while (lotto.size < LOTTO_NUMBER_COUNT) {
+            lotto.add(LottoNumber.get(lottoNumberGenerator.generateLottoNumber()))
+        }
+        return lotto
+    }
+
+    private fun getByManual(lottoNumber: IntArray): Set<LottoNumber> {
+        require(lottoNumber.size == LOTTO_NUMBER_COUNT) { LOTTO_NUMBER_COUNT_EXCEPTION_MESSAGE }
+        require(lottoNumber.distinct().size == LOTTO_NUMBER_COUNT) { LOTTO_NUMBER_DISTINCT_MESSAGE }
+        return lottoNumber.map { LottoNumber.get(it) }.toSet()
+    }
+
+    companion object {
+        private const val LOTTO_NUMBER_COUNT = 6
+        private const val LOTTO_NUMBER_COUNT_EXCEPTION_MESSAGE = "로또 초기화시 입력된 로또 번호가 6개가 아닙니다."
+        private const val LOTTO_NUMBER_DISTINCT_MESSAGE = "로또 초기화시 입력된 로또 번호에 중복이 있습니다."
+        private const val LOTTO_PARAMETERS_ERROR_MESSAGE = "로또 생성시 필요한 매개변수를 잘못입력하셨습니다."
+    }
+}
--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,12 +1,27 @@
 package lotto.domain
 
 import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
 
 class LottoTest : StringSpec({
     "로또를 발급한다." {
         shouldNotThrowAny {
-            Lotto()
+            Lotto(RandomGenerator)
         }
+    }
+
+    "로또를 직접 초기화할때 중복되지 않은 6개의 숫자를 입력해야한다." {
+        listOf(listOf(1, 2, 3, 4), listOf(1, 1, 2, 3, 4, 5)).forAll { numberList ->
+            shouldThrowAny {
+                Lotto(null, *numberList.toIntArray())
+            }
+        }
+    }
+
+    "발급된 로또의 로또번호 갯수는 6개이다." {
+        Lotto(RandomGenerator).lottoNumbers.size shouldBe 6
     }
 })
