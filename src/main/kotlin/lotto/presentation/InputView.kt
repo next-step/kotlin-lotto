36f29f6acdf5fa2bc8e7b5725fe39a8fea256d52
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-27 21:22:07 +0900

    refactor(InputView) 당첨번호의 유효성을 체크하는 기능을 WinningNumbers에서 이동

--- src/main/kotlin/lotto/presentation/InputView.kt
+++ src/main/kotlin/lotto/presentation/InputView.kt
@@ -1,20 +1,47 @@
 package lotto.presentation
 
+import lotto.core.constant.LottoConstants
+
 object InputView {
     fun inputPurchaseAmount(): String {
         println(GUIDE_INPUT_PURCHASE_AMOUNT)
-        val purchaseAmount = readlnOrNull()
-        require(purchaseAmount != null)
-        return purchaseAmount
+
+        return readlnOrNull() ?: throw RuntimeException("잘못된 값이 입력되었습니다.")
     }
 
-    fun inputWinningNumbers(): String {
+    fun inputWinningNumbers(): List<Int> {
         println(GUIDE_INPUT_LAST_WINNING_NUMBER)
-        val winningNumber = readlnOrNull()
-        require(winningNumber != null)
-        return winningNumber
+
+        return splitNumbers(readlnOrNull() ?: throw RuntimeException("잘못된 값이 입력되었습니다."))
+    }
+
+    private fun splitNumbers(winningNumbers: String): List<Int> {
+        val numberList = winningNumbers.split(DELIMITER)
+        val numbers = transformNumbers(numberList)
+        checkInsideLottoNumber(numbers)
+        checkDuplicates(numbers)
+        return numbers
+    }
+
+    private fun transformNumbers(numberList: List<String>): List<Int> {
+        return numberList.map { number -> number.trim().toIntOrNull() ?: throw NumberFormatException("숫자가 아닙니다.") }
+    }
+
+    private fun checkDuplicates(list: List<Int>) {
+        if (list.size != list.toSet().size) {
+            throw IllegalArgumentException("중복된 숫자가 있습니다.")
+        }
+    }
+
+    private fun checkInsideLottoNumber(numbers: List<Int>) {
+        numbers.forEach {
+            if (it < LottoConstants.LOTTO_NUMBER_MIN || LottoConstants.LOTTO_NUMBER_MAX < it) {
+                throw IllegalArgumentException("로또 숫자의 범위를 넘어섰습니다.")
+            }
+        }
     }
 
     private const val GUIDE_INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
     private const val GUIDE_INPUT_LAST_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요."
+    private const val DELIMITER = ","
 }
