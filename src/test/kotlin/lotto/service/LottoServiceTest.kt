32965d8e7d15eb32486fc074be05424dd06d2852
KimDabomi <cdabomi@nate.com>
2024-11-27 18:17:56 +0900

    [step4] refactor: 수익률 테스트 오류 수정

--- src/test/kotlin/lotto/service/LottoServiceTest.kt
+++ src/test/kotlin/lotto/service/LottoServiceTest.kt
@@ -5,6 +5,7 @@ import io.kotest.matchers.shouldBe
 import lotto.domain.BonusNumber
 import lotto.domain.LottoPurchasingMachine
 import lotto.domain.LottoRank
+import java.math.BigDecimal
 
 class LottoServiceTest : StringSpec({
     val lottoPurchasingMachineService = LottoService(LottoPurchasingMachine(14000))
@@ -17,18 +18,21 @@ class LottoServiceTest : StringSpec({
     }
 
     "수익률을 계산해야 한다. (당첨 금액 / 구매 금액 의 소수점 둘째자리까지 버림)" {
-        val lottoTickets = lottoPurchasingMachineService.lottoIssuance(totalTicketCount, manualTicketCount)
-        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
-        val bonusNumber = BonusNumber(7)
+        val fixedWinningNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val fixedBonusNumber = BonusNumber(7)
 
+        val lottoTickets = lottoPurchasingMachineService.lottoIssuance(
+            totalTicketCount,
+            manualTicketCount,
+        )
         val totalPrize = lottoTickets.sumOf { ticket ->
-            val matchCount = ticket.matchCount(winningNumbers)
-            val hasBonusNumber = ticket.hasBonusNumber(bonusNumber)
+            val matchCount = ticket.matchCount(fixedWinningNumbers)
+            val hasBonusNumber = ticket.hasBonusNumber(fixedBonusNumber)
             LottoRank.from(matchCount, hasBonusNumber).prize
         }
 
         val profitRate = lottoPurchasingMachineService.calculateProfitRate(totalPrize)
-        val expectedProfitRate = "0.00".toBigDecimal()
+        val expectedProfitRate = BigDecimal("0.00")
 
         profitRate shouldBe expectedProfitRate
     }
