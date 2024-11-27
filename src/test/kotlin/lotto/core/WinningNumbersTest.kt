5c1d433665d7fa5fd9fb132c6f63d44a9087065d
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-27 21:28:18 +0900

    test: TestCase 수정.

--- src/test/kotlin/lotto/core/WinningNumbersTest.kt
+++ src/test/kotlin/lotto/core/WinningNumbersTest.kt
@@ -1,15 +1,16 @@
 package lotto.core
 
 import io.kotest.assertions.throwables.shouldThrow
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.api.Test
 
 class WinningNumbersTest {
-    @ParameterizedTest
-    @ValueSource(strings = [ "", "a", "1,2", "1,2,3,4,5,46", "1,2,3,4,5,47", "-01, 2,3,4,5,45" ])
-    fun `잘못된 문자열 입력 시 오류를 확인한다`(value: String) {
-        shouldThrow<RuntimeException> {
-            WinningNumbers(value)
+    @Test
+    fun `잘못된 문자열 입력 시 오류를 확인한다`() {
+        shouldThrow<IllegalArgumentException> {
+            WinningNumbers(listOf(1))
+        }
+        shouldThrow<IllegalArgumentException> {
+            WinningNumbers(listOf(1, 2, 3, 4, 5, 6, 7))
         }
     }
 }
--- src/test/kotlin/lotto/core/YieldCalculatorTest.kt
+++ src/test/kotlin/lotto/core/YieldCalculatorTest.kt
@@ -6,12 +6,6 @@ import org.junit.jupiter.api.Test
 class YieldCalculatorTest {
     @Test
     fun `수익률 계산을 검증한다`() {
-        val lottos = Lottos(listOf(Lotto((4..10).toList()), Lotto((3..9).toList())))
-
-        lottos.forEach {
-            it.checkWinningStates(WinningNumbers((1..6).toList()))
-        }
-
-        YieldCalculator.calculate(lottos) shouldBe 27.5
+        YieldCalculator.calculate(mapOf(WinningRank.RANK4 to 2, WinningRank.RANK3 to 1), 5) shouldBe 12
     }
 }
