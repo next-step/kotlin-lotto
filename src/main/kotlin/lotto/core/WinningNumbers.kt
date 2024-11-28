d8787365776b9140879e1cdd6c069cb8bf997cf6
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-29 08:42:38 +0900

    refactor : 코드 컨벤션 수정

--- src/main/kotlin/lotto/core/WinningNumbers.kt
+++ src/main/kotlin/lotto/core/WinningNumbers.kt
@@ -6,7 +6,9 @@ class WinningNumbers(numbers: List<Int>, private val bonusNumber: Int) {
     private val lotto: Lotto = Lotto(numbers)
 
     init {
-        require(LottoConstants.LOTTO_NUMBER_MIN <= bonusNumber && bonusNumber <= LottoConstants.LOTTO_NUMBER_MAX ) { LottoConstants.ERROR_INVALID_NUMBER_SCOPE}
+        require(LottoConstants.LOTTO_NUMBER_MIN <= bonusNumber && bonusNumber <= LottoConstants.LOTTO_NUMBER_MAX) {
+            LottoConstants.ERROR_INVALID_NUMBER_SCOPE
+        }
         require(!lotto.contains(bonusNumber)) { LottoConstants.ERROR_INVALID_BONUS_NUMBER }
     }
 
