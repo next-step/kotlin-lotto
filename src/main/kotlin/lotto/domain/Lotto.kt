641dd0e7f166016fbd530f3273dcbd6d4844c393
heesang.lee <sky1994601@hanmail.net>
2024-11-29 01:56:47 +0900

    refactor: step4 로또(수동)
    Lotto 객체에 예외처리 구현

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,6 +1,11 @@
 package lotto.domain
 
 class Lotto(val numbers: LottoNumbers) {
+    init {
+        require(numbers.lottoNumbers.all { it.number in (MINIMUM_NUMBER..MAXIMUM_NUMBER) }) { "LottoNumber must be between 1 and 45" }
+        require(numbers.lottoNumbers.size == NUMBER_OF_NUMBER) { "로또는 정확히 6개의 숫자가 필요해요" }
+    }
+
     companion object {
         const val NUMBER_OF_NUMBER = 6
         const val MINIMUM_NUMBER = 1
