246de8621366d27f7e5de74f0e089e36b12f98f9
Aimbe <qkr2435@gmail.com>
2024-11-28 12:18:04 +0900

    refactor(review): constants 클래스 생성

---
+++ src/main/kotlin/calculator/DelimiterConstants.kt
@@ -0,0 +1,13 @@
+package calculator
+
+object DelimiterConstants {
+    val DEFAULT_DELIMITER_PATTERN = "[,:]".toRegex()
+    const val COMMA_DELIMITER = ","
+    const val COLON_DELIMITER = ":"
+    const val CUSTOM_DELIMITER_GROUP_INDEX = 1
+    const val NUMBERS_GROUP_INDEX = 2
+
+    val CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)".toRegex()
+
+    const val EMPTY_STRING = ""
+}
