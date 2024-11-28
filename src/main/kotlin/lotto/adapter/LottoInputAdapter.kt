c5e00af223f0ee5538609cd0d52016ddf0825267
신종화 <77482065+jjongwa@users.noreply.github.com>
2024-11-28 17:58:16 +0900

    [🚀 4단계 - 로또(수동)] 신종화 제출합니다. (#1091)
    * refactor: extractResult 메서드 로직 변경
    
    * refactor: LottoRank 구조 변경
    
    * docs(README.md) : 4단계 - 로또(수동) 기능 구현 목록 작성
    
    * feat: 수동 로또 입력 기능 추가

--- src/main/kotlin/lotto/adapter/LottoInputAdapter.kt
+++ src/main/kotlin/lotto/adapter/LottoInputAdapter.kt
@@ -2,10 +2,16 @@ package lotto.adapter
 
 import lotto.domain.LottoBall
 import lotto.domain.LottoBalls
+import lotto.domain.LottoLine
 import lotto.domain.LottoPurchaseAmount
+import lotto.domain.LottoPurchaseCount
 
 interface LottoInputAdapter {
     fun fetchPurchaseAmount(): LottoPurchaseAmount
+
+    fun fetchManualPurchaseCount(): LottoPurchaseCount
+
+    fun fetchManualLines(manualPurchaseCount: LottoPurchaseCount): List<LottoLine>
 
     fun fetchWinningNumbers(): LottoBalls
 
