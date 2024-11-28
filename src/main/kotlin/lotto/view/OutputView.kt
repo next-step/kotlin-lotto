e2595e95ea8cd7e1cba9799c9878cd9bf82c1e3a
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2024-11-28 17:04:33 +0900

    feat: 로또 당첨번호를 입력받는다.

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -3,18 +3,18 @@ package lotto.view
 import lotto.domain.Lotto
 
 class OutputView {
-    fun printPurchaseResult(purchasedLottos: List<Lotto>) {
-        println("${purchasedLottos.size} $PURCHASE_RESULT_MESSAGE")
-        printLottoNumbers(purchasedLottos)
-    }
-
-    private fun printLottoNumbers(purchasedLottos: List<Lotto>) {
-        purchasedLottos.forEach {
-            println(it)
-        }
-    }
-
     companion object {
+        fun printPurchaseResult(purchasedLottos: List<Lotto>) {
+            println("${purchasedLottos.size} $PURCHASE_RESULT_MESSAGE")
+            printLottoNumbers(purchasedLottos)
+        }
+
+        private fun printLottoNumbers(purchasedLottos: List<Lotto>) {
+            purchasedLottos.forEach {
+                println(it)
+            }
+        }
+
         private const val PURCHASE_RESULT_MESSAGE = "개를 구매했습니다."
     }
 }
