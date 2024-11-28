fd5590ef9138e70867d8b9f075ca7de3e5d9c906
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-29 08:41:58 +0900

    feat : 당첨 조건에 보너스 숫자 추가

--- src/main/kotlin/lotto/core/WinningRank.kt
+++ src/main/kotlin/lotto/core/WinningRank.kt
@@ -1,18 +1,22 @@
 package lotto.core
 
-enum class WinningRank(val winningCount: Int, val winningAmount: Int) {
-    NOTHING(0, 0),
-    FORTH(3, 5_000),
-    THIRD(4, 50_000),
-    SECOND(5, 1_500_000),
-    FIRST(6, 2_000_000_000),
+enum class WinningRank(val winningCount: Pair<Int, Boolean>, val winningAmount: Int) {
+    FIRST(6 to false, 2_000_000_000),
+    SECOND(5 to true, 30_000_000),
+    THIRD(5 to false, 1_500_000),
+    FORTH(4 to false, 50_000),
+    FIFTH(3 to false, 5_000),
+    NOTHING(0 to false, 0),
     ;
 
     companion object {
         private val RANK_BY_WINNING_COUNT = entries.associateBy(WinningRank::winningCount)
 
-        fun getRank(winningCount: Int): WinningRank {
-            return RANK_BY_WINNING_COUNT[winningCount] ?: NOTHING
+        fun getRank(
+            winningCount: Int,
+            matchBonus: Boolean,
+        ): WinningRank {
+            return RANK_BY_WINNING_COUNT[winningCount to matchBonus] ?: NOTHING
         }
     }
 }
