00d5bf06d2749e5997669a189aad71ff58b99d1a
sian <chicori3@naver.com>
2024-11-28 21:06:33 +0900

    feat: 로또 상점에서 자동 로또, 수동 로또를 생성하여 반환

--- src/test/kotlin/lotto/domain/LottoStoreTest.kt
+++ src/test/kotlin/lotto/domain/LottoStoreTest.kt
@@ -8,10 +8,27 @@ class LottoStoreTest : StringSpec({
     val lottoFactory = LottoFactory(lottoNumberGenerator)
     val sut = LottoStore(lottoFactory)
 
-    "입력받은 금액만큼 로또를 생성한다" {
+    "입력받은 주문만큼 로또를 생성한다" {
         val money = Money(14000)
+        val lottoOrder = LottoOrder(money)
 
-        val actual = sut.sell(money)
+        val actual = sut.sell(lottoOrder)
+
+        actual.quantity shouldBe 14
+    }
+
+    "입력받은 주문과 수동으로 입력한 숫자로 로또를 생성한다" {
+        val money = Money(14000)
+        val manualLottoNumbers = listOf(listOf(1, 2, 3, 4, 5, 6), listOf(7, 8, 9, 10, 11, 12))
+        val manualCount = 2
+        val lottoOrder =
+            LottoOrder(
+                money = money,
+                manualCount = manualCount,
+                manualLottoNumbers = manualLottoNumbers,
+            )
+
+        val actual = sut.sell(lottoOrder)
 
         actual.quantity shouldBe 14
     }
