486ab911e14f79ed84cc1fc252ee825d44e78614
sian <chicori3@naver.com>
2024-11-28 21:05:06 +0900

    feat: 자동, 수동 생성 인터페이스 추가

--- src/main/kotlin/lotto/domain/LottoFactory.kt
+++ src/main/kotlin/lotto/domain/LottoFactory.kt
@@ -3,7 +3,11 @@ package lotto.domain
 class LottoFactory(
     private val numberGenerator: LottoNumberGenerator,
 ) {
-    fun create(quantity: Int): List<DefaultLotto> {
+    fun createAuto(quantity: Int): List<DefaultLotto> {
         return (1..quantity).map { DefaultLotto(numberGenerator.generate().toSet()) }
+    }
+
+    fun createManual(numbers: List<List<Int>>): List<DefaultLotto> {
+        return numbers.map { DefaultLotto(it.map(::LottoNumber).toSet()) }
     }
 }
