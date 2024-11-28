c22d97957d192f99f7b3709d93a7d9468491e69d
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-29 08:40:55 +0900

    feat : Lotto 책임추가.
    1. 로또 넘버에 유효성 체크하는 기능 추가.
    2. 동일한 숫자의 Count를 획득하는 기능 추가

--- src/main/kotlin/lotto/core/Lotto.kt
+++ src/main/kotlin/lotto/core/Lotto.kt
@@ -1,3 +1,22 @@
 package lotto.core
 
-data class Lotto(val numbers: List<Int>)
+import lotto.core.constant.LottoConstants
+
+class Lotto(val numbers: List<Int>) {
+    init {
+        require(numbers.size == LottoConstants.LOTTO_NUMBER_COUNT) { LottoConstants.ERROR_INVALID_NUMBER_COUNT }
+        require(numbers.none { checkValidNumberScope(it) }) { LottoConstants.ERROR_INVALID_NUMBER_SCOPE }
+    }
+
+    private fun checkValidNumberScope(number: Int): Boolean {
+        return number < LottoConstants.LOTTO_NUMBER_MIN || LottoConstants.LOTTO_NUMBER_MAX < number
+    }
+
+    fun contains(number: Int): Boolean {
+        return numbers.contains(number)
+    }
+
+    fun countCommonNumbers(other: Lotto): Int {
+        return numbers.filter { it in other.numbers }.size
+    }
+}
