9e1466711a04a5780df870dfec13eefab93f756e
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-29 08:45:03 +0900

    refactor : 당첨 여부를 확인하는 기능을 WinningNumbers로 이관

--- src/main/kotlin/lotto/core/Lottos.kt
+++ src/main/kotlin/lotto/core/Lottos.kt
@@ -2,14 +2,7 @@ package lotto.core
 
 class Lottos(private val lottos: List<Lotto>) : List<Lotto> by lottos {
     fun countWinningRanks(winningNumbers: WinningNumbers): LottoResult {
-        val results = this.map { checkWinningState(it, winningNumbers) }
+        val results = this.map { winningNumbers.checkWinningState(it) }
         return LottoResult(results.groupingBy { it }.eachCount())
-    }
-
-    private fun checkWinningState(
-        lotto: Lotto,
-        winningNumbers: WinningNumbers,
-    ): WinningRank {
-        return WinningRank.getRank(winningNumbers.countCommonNumbers(lotto))
     }
 }
