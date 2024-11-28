e64fbf2f89939c8f81cab935df439ee085a9680a
Aimbe <qkr2435@gmail.com>
2024-11-28 12:54:18 +0900

    test: NumberParser 테스트 추가

---
+++ src/test/kotlin/calculator/NumberParserTest.kt
@@ -0,0 +1,18 @@
+package calculator
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class NumberParserTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "42", "999", "1000", "100000"])
+    fun `숫자로만 이루어진 문자열은 하나의 숫자로 파싱한다`(input: String) {
+        val parser = NumberParser(emptyList())
+
+        val result = parser.parseNumbers(input)
+
+        assertThat(result).containsExactly(input.toInt())
+    }
+}
