f2f42dd8e9160f8f0f0b498c2380be1c348fac98
boradol2 <boradol0902@gmail.com>
2024-11-29 03:12:45 +0900

    feat(ResultView): 자동 로또 생성기 출력

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import lotto.domain.LottoAmount
+import lotto.domain.LottoMachine
 import lotto.view.InputView
 import lotto.view.ResultView
 
@@ -8,4 +9,7 @@ fun main() {
     val amount = InputView.inputLottoAmount()
     val ticketCount = LottoAmount(amount).ticketCount
     ResultView.displayLottoTicketCount(ticketCount)
+
+    val tickets = LottoMachine.generateTickets(ticketCount)
+    ResultView.printAutoGenerateLotto(tickets)
 }
--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -1,7 +1,20 @@
 package lotto.view
 
+import lotto.domain.Lotto
+
 object ResultView {
     fun displayLottoTicketCount(ticketCount: Int) {
         println("${ticketCount}개를 구매했습니다.")
     }
+
+    fun printAutoGenerateLotto(lottos: List<Lotto>) {
+        val lottoResults = lottos.joinToString(separator = "\n") { lottoString(it) }
+        println(lottoResults)
+    }
+
+    private fun lottoString(lotto: Lotto) =
+        lotto.numbers.joinToString(separator = ", ", prefix = "[", postfix = "]") {
+            it.value.toString()
+                .padStart(2, ' ')
+        }
 }
