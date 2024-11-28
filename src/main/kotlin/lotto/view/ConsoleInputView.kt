2a4c8c16a917876174d4409febc96f450b5ec8be
sian <chicori3@naver.com>
2024-11-28 21:10:06 +0900

    feat: 수동 입력 기능 추가

--- src/main/kotlin/lotto/view/ConsoleInputView.kt
+++ src/main/kotlin/lotto/view/ConsoleInputView.kt
@@ -3,16 +3,35 @@ package lotto.view
 class ConsoleInputView : InputView {
     override fun requestPrice(): Int {
         println("구입금액을 입력해 주세요.")
-        val price =
-            readlnOrNull() ?: throw IllegalArgumentException("입력값이 없습니다.")
+        val price = readInput()
 
         return price.toInt()
     }
 
+    override fun requestManualLottoCount(): Int {
+        println("수동으로 구매할 로또 수를 입력해 주세요.")
+        val manualCount = readInput()
+
+        return manualCount.toInt()
+    }
+
+    override fun requestManualLottoNumbers(count: Int): List<List<Int>> {
+        if (count == 0) {
+            return emptyList()
+        }
+
+        println("수동으로 구매할 번호를 입력해 주세요.")
+        return (1..count).map {
+            val numbers = readInput()
+
+            numbers.split(DELIMITER)
+                .map { it.trim().toInt() }
+        }
+    }
+
     override fun requestWinningNumbers(): List<Int> {
         println("지난 주 당첨 번호를 입력해 주세요.")
-        val numbers =
-            readlnOrNull() ?: throw IllegalArgumentException("입력값이 없습니다.")
+        val numbers = readInput()
 
         return numbers.split(DELIMITER)
             .map { it.trim().toInt() }
@@ -20,10 +39,13 @@ class ConsoleInputView : InputView {
 
     override fun requestBonusNumber(): Int {
         println("보너스 볼을 입력해 주세요.")
-        val bonusNumber =
-            readlnOrNull() ?: throw IllegalArgumentException("입력값이 없습니다.")
+        val bonusNumber = readInput()
 
         return bonusNumber.toInt()
+    }
+
+    private fun readInput(): String {
+        return readInput()
     }
 
     companion object {
--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,6 +3,10 @@ package lotto.view
 interface InputView {
     fun requestPrice(): Int
 
+    fun requestManualLottoCount(): Int
+
+    fun requestManualLottoNumbers(count: Int): List<List<Int>>
+
     fun requestWinningNumbers(): List<Int>
 
     fun requestBonusNumber(): Int
