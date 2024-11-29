e3e999382eb2dd3847ad705d63ee6ee93d928185
KimDabomi <cdabomi@nate.com>
2024-11-29 21:54:08 +0900

    [step4] refactor: rank test 수정

--- src/test/kotlin/lotto/domain/LottoRankTest.kt
+++ src/test/kotlin/lotto/domain/LottoRankTest.kt
@@ -7,7 +7,7 @@ import io.kotest.matchers.shouldBe
 class LottoRankTest : StringSpec({
     fun verifyLottoRank(
         matchCount: Int,
-        bonusNumber: BonusNumber?,
+        hasBonusNumber: Boolean = true,
         expectedRank: LottoRank,
         expectedPrize: Int,
     ) {
@@ -17,13 +17,13 @@ class LottoRankTest : StringSpec({
             val lottoTickets = List(15000) { LottoTicket() }
             matchingTicket =
                 lottoTickets.firstOrNull { ticket ->
-                    when {
-                        expectedRank == LottoRank.SECOND && bonusNumber != null ->
+                    when (expectedRank) {
+                        LottoRank.SECOND ->
                             ticket.matchCount(winningNumbers) == matchCount &&
-                                ticket.hasBonusNumber(bonusNumber)
-                        expectedRank == LottoRank.THIRD && bonusNumber != null ->
+                                    hasBonusNumber
+                        LottoRank.THIRD ->
                             ticket.matchCount(winningNumbers) == matchCount &&
-                                !ticket.hasBonusNumber(bonusNumber)
+                                    !hasBonusNumber
                         else -> ticket.matchCount(winningNumbers) == matchCount
                     }
                 }
@@ -32,7 +32,7 @@ class LottoRankTest : StringSpec({
         val rank =
             LottoRank.from(
                 matchingTicket!!.matchCount(winningNumbers),
-                bonusNumber?.let { matchingTicket!!.hasBonusNumber(it) } ?: false,
+                hasBonusNumber,
             )
 
         rank shouldBe expectedRank
@@ -40,31 +40,29 @@ class LottoRankTest : StringSpec({
     }
 
     "구매한 로또 목록에서 당첨 번호와 일치하는 게 0개이면 낙첨이다. (0원)" {
-        verifyLottoRank(0, null, LottoRank.NONE, 0)
+        verifyLottoRank(0, false, LottoRank.NONE, 0)
     }
 
     "구매한 로또 목록에서 당첨 번호와 일치하는 게 3개이면 5등이다. (5,000원)" {
-        verifyLottoRank(3, null, LottoRank.FIFTH, 5000)
+        verifyLottoRank(3, false, LottoRank.FIFTH, 5000)
     }
 
     "구매한 로또 목록에서 당첨 번호와 일치하는 게 4개이면 4등이다. (50,000원)" {
-        verifyLottoRank(4, null, LottoRank.FOURTH, 50_000)
+        verifyLottoRank(4, false, LottoRank.FOURTH, 50_000)
     }
 
     "구매한 로또 목록에서 당첨 번호와 일치하는 게 5개이면 3등이다. (1,500,000원)" {
-        verifyLottoRank(5, null, LottoRank.THIRD, 1_500_000)
+        verifyLottoRank(5, false, LottoRank.THIRD, 1_500_000)
     }
 
     "구매한 로또 목록에서 당첨 번호와 일치하는 게 5개이고, 남은 한 개의 번호가 보너스 번호와 일치하면 2등이다. (30,000,000원)" {
-        val bonusNumber = BonusNumber(7)
-        verifyLottoRank(5, bonusNumber, LottoRank.SECOND, 30_000_000)
+        verifyLottoRank(5, true, LottoRank.SECOND, 30_000_000)
     }
 
     "구매한 로또 목록에서 당첨 번호와 일치하는 게 6개이면 1등이다. (2,000,000,000원)" {
-        verifyLottoRank(6, null, LottoRank.FIRST, 2_000_000_000)
+        verifyLottoRank(6, false, LottoRank.FIRST, 2_000_000_000)
     }
 
-    // 기존의 로또 번호 검증 테스트는 그대로 유지
     "로또 번호는 6개, 1부터 45 사이의 숫자, 중복 불가여야 한다." {
         shouldThrow<IllegalArgumentException> {
             LottoTicket(listOf(1, 2, 3, 4, 5))
