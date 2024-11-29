1587fd53c5c16a37823c05422b83d0e7cba32844
KimDabomi <cdabomi@nate.com>
2024-11-29 21:51:08 +0900

    [step4] refactor: nullable 제거

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -2,6 +2,7 @@ package lotto.controller
 
 import lotto.domain.BonusNumber
 import lotto.domain.LottoPurchasingMachine
+import lotto.domain.LottoRank
 import lotto.domain.LottoTicket
 import lotto.service.LottoService
 import lotto.view.InputView
@@ -14,17 +15,33 @@ object LottoController {
 
         val buyQuantity = LottoPurchasingMachine(purchaseAmount).buyCount()
         val manualQuantity = InputView.askManualPurchaseAmount()
-        val manualTicketsInput = InputView.askManualPurchaseNumbers(manualQuantity)!!
-        val autoTickets = lottoService.generateAutoTickets(buyQuantity, manualQuantity)
+
+        val manualTicketsInput = InputView.askManualPurchaseNumbers(manualQuantity)
+        if (manualTicketsInput.size != 6) {
+            println(LottoTicket.LOTTO_NUMBER_SIZE_EXCEPTION_MESSAGE)
+        }
+
         val manualTickets = manualTicketsInput.map { LottoTicket(it) }
+        val autoTickets = lottoService.generateAutoTickets(buyQuantity, manualQuantity)
         val tickets = autoTickets + manualTickets
+
         ResultView.showPurchaseInfo(manualQuantity, buyQuantity - manualQuantity, tickets.map { it.lottoNumbers })
 
         val winningNumbers = InputView.askWinningNumbers()
         val bonusNumber = InputView.askBonusNumber()
 
-        val statistics = lottoService.calculateStatistics(tickets, winningNumbers, BonusNumber(bonusNumber))
-        val totalPrize = lottoService.calculateTotalPrize(statistics)
+        val statistics =
+            tickets.groupingBy { ticket ->
+                val matchCount = ticket.matchCount(winningNumbers)
+                val hasBonus = matchCount == 5 && ticket.hasBonusNumber(BonusNumber(bonusNumber))
+                LottoRank.from(matchCount, hasBonus)
+            }.eachCount()
+
+        val totalPrize =
+            statistics.entries.sumOf { (rank, count) ->
+                rank.prize * count
+            }
+
         val profitRate = lottoService.calculateProfitRate(totalPrize)
 
         ResultView.showStatistics(statistics, profitRate)
--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -6,6 +6,7 @@ object InputView {
     private const val INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
     private const val AMOUNT_EXCEPTION_MESSAGE = "유효한 금액을 입력해 주세요."
     private const val INPUT_MANUAL_TICKET_COUNT = "수동으로 구매할 로또 수를 입력해 주세요."
+    private const val QUANTITY_EXCEPTION_MESSAGE = "유효한 수량을 입력해 주세요."
     private const val INPUT_MANUAL_TICKETS = "수동으로 구매할 번호를 입력해 주세요."
     private const val INPUT_LAST_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요."
     private const val INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
@@ -19,29 +20,21 @@ object InputView {
 
     fun askManualPurchaseAmount(): Int {
         println(INPUT_MANUAL_TICKET_COUNT)
-        return readlnOrNull()?.toIntOrNull() ?: throw IllegalArgumentException("유효한 수동 구매 개수를 입력해 주세요.")
+        return readlnOrNull()?.toIntOrNull() ?: throw IllegalArgumentException(QUANTITY_EXCEPTION_MESSAGE)
     }
 
-    fun askManualPurchaseNumbers(manualTicketCount: Int): List<List<Int>>? {
+    fun askManualPurchaseNumbers(manualTicketCount: Int): List<List<Int>> {
         println(INPUT_MANUAL_TICKETS)
         val manualTickets = mutableListOf<List<Int>>()
-        repeat(manualTicketCount) {
-            val input = readlnOrNull()?.trim()
-            if (input.isNullOrEmpty()) {
-                println(LottoTicket.LOTTO_NUMBER_SIZE_EXCEPTION_MESSAGE)
-                return null
-            }
 
+        repeat(manualTicketCount) {
+            val input = readln().trim()
             val numbers =
                 input.split(LOTTO_NUMBERS_SPLIT_UNIT)
                     .mapNotNull { it.trim().toIntOrNull() }
-            if (numbers.size != 6) {
-                println(LottoTicket.LOTTO_NUMBER_SIZE_EXCEPTION_MESSAGE)
-                return null
-            }
-
             manualTickets.add(numbers)
         }
+
         return manualTickets
     }
 
