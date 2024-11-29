cc5728c9fa00879f54a9e8735b19a0557d9e72b3
Aimbe <qkr2435@gmail.com>
2024-11-29 20:35:26 +0900

    feat: 등수 ENUM구현

---
+++ src/main/kotlin/lotto/Rank.kt
@@ -0,0 +1,15 @@
+package lotto
+
+enum class Rank(val matchCount: Int, val prize: Int) {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
+    NONE(0, 0);
+
+    companion object {
+        fun from(matchCount: Int): Rank {
+            return entries.find { it.matchCount == matchCount } ?: NONE
+        }
+    }
+}
