27fe616660f75556d7d38dc4689eb2dab9666429
ahn-sj <jaesa5221@gmail.com>
2024-11-29 22:49:04 +0900

    [step3] chore: VIEW 주석 제거

--- src/main/kotlin/lotto/LottoDraw.kt
+++ src/main/kotlin/lotto/LottoDraw.kt
@@ -1,18 +1,18 @@
 package lotto
 
 fun main() {
-    val purchasePrice = InputView.getPurchasePrice() // VIEW(input)
+    val purchasePrice = InputView.getPurchasePrice()
 
     val price = Price(purchasePrice)
     val userLotto = LottoIssuer.buy(price)
 
-    OutputView.printLotto(userLotto) // VIEW(output)
+    OutputView.printLotto(userLotto)
 
     val winningNumbers = InputView.getWinningNumbers()
 
     val winningLotto = LottoNumbers.created(winningNumbers)
     val lottoResult = LottoResultHandler.match(userLotto, winningLotto)
 
-    OutputView.printResult(lottoResult.results) // VIEW(output)
-    OutputView.printProfitRate(lottoResult.computeProfitRate(price)) // VIEW(output)
+    OutputView.printResult(lottoResult.results)
+    OutputView.printProfitRate(lottoResult.computeProfitRate(price))
 }
