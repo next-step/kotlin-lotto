cb0cf62a39800eda4100c766f687896afba17408
eshc123 <>
2024-11-29 12:31:38 +0900

    refactor: extractDelimiterAndText - 상수 선언

--- src/main/kotlin/calculator/DelimiterExtractor.kt
+++ src/main/kotlin/calculator/DelimiterExtractor.kt
@@ -2,12 +2,14 @@ package calculator
 
 object DelimiterExtractor {
     private const val DELIMITER_EXTRACT_PATTERN = "//(.)\n(.*)"
+    private const val EXTRACTED_DELIMITER_INDEX = 1
+    private const val EXTRACTED_TEXT_INDEX = 2
 
     fun extractDelimiterAndText(text: String): CustomDelimiter {
         val matchResult = Regex(DELIMITER_EXTRACT_PATTERN).find(text)
 
-        val delimiter = matchResult?.groupValues?.getOrNull(1)
-        val resultText = matchResult?.groupValues?.getOrNull(2) ?: text
+        val delimiter = matchResult?.groupValues?.getOrNull(EXTRACTED_DELIMITER_INDEX)
+        val resultText = matchResult?.groupValues?.getOrNull(EXTRACTED_TEXT_INDEX) ?: text
 
         return CustomDelimiter(delimiter, resultText)
     }
