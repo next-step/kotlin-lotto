bd8f97a4e225b4da6aaaad5fff4f6a8323927cc1
SeolYoungKim <nasur4da@gmail.com>
2024-11-30 12:25:02 +0900

    test: 로또 수익률 테스트 코드 작성

---
+++ src/test/kotlin/lotto/LottoProfitRateCalculatorTest.kt
@@ -0,0 +1,23 @@
+package lotto
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.doubles.plusOrMinus
+import io.kotest.matchers.shouldBe
+
+class LottoProfitRateCalculatorTest : StringSpec({
+    "구매한 로또 금액과 당첨된 로또 금액을 비교하여 수익률을 계산한다" {
+        val 두장의_4등_로또 =
+            Lottos(
+                listOf(
+                    Lotto(listOf(1, 2, 3, 43, 44, 45)),
+                    Lotto(listOf(1, 2, 3, 43, 44, 45)),
+                ),
+            )
+        val winningNumbers = WinningNumbers(listOf(1, 2, 3, 4, 5, 6))
+
+        val lottoResults = LottoResultChecker.check(lottos = 두장의_4등_로또, winningNumbers = winningNumbers)
+        val profitRate: Double = LottoProfitRateCalculator.calculate(두장의_4등_로또, lottoResults)
+
+        profitRate shouldBe (5.0 plusOrMinus 0.0001)
+    }
+})
