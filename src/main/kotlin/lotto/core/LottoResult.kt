b951f3faa71d41e1f25ac3c0cf5df8568ce7adc6
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-30 11:13:03 +0900

    feat : WinningRank에 당첨금액 총합을 구하는 API추가

--- src/main/kotlin/lotto/core/LottoResult.kt
+++ src/main/kotlin/lotto/core/LottoResult.kt
@@ -5,7 +5,7 @@ import lotto.core.constant.LottoConstants
 class LottoResult(val winningRankCount: Map<WinningRank, Int>) {
     fun calculateYield(lottoCount: Int): Float {
         val totalBudget = lottoCount * LottoConstants.LOTTO_PRICE
-        val totalWinningAmount = winningRankCount.map { it.key.winningAmount * it.value }.sum()
+        val totalWinningAmount = winningRankCount.map { it.key.totalWinningAmount(it.value) }.sum()
 
         return totalWinningAmount.toFloat() / totalBudget
     }
--- src/main/kotlin/lotto/core/WinningRank.kt
+++ src/main/kotlin/lotto/core/WinningRank.kt
@@ -1,12 +1,12 @@
 package lotto.core
 
 enum class WinningRank(val winningCount: Pair<Int, Boolean>, val winningAmount: Int) {
+    NOTHING(0 to false, 0),
+    FIFTH(3 to false, 5_000),
+    FORTH(4 to false, 50_000),
+    THIRD(5 to false, 1_500_000),
     FIRST(6 to false, 2_000_000_000),
     SECOND(5 to true, 30_000_000),
-    THIRD(5 to false, 1_500_000),
-    FORTH(4 to false, 50_000),
-    FIFTH(3 to false, 5_000),
-    NOTHING(0 to false, 0),
     ;
 
     companion object {
@@ -19,4 +19,6 @@ enum class WinningRank(val winningCount: Pair<Int, Boolean>, val winningAmount: 
             return RANK_BY_WINNING_COUNT[winningCount to matchBonus] ?: NOTHING
         }
     }
+
+    fun totalWinningAmount(count: Int): Int = winningAmount * count
 }
