0fe86dbb489cdd473bbc2a1c702b852f244bfaaa
Pablo730 <kcm1619@naver.com>
2024-11-30 22:21:58 +0900

    로또 당첨 번호를 입력할 수 있다 - 관련 테스트 구현

--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -1,5 +1,12 @@
 package lotto.domain
 
 class LottoTicket {
-    val lottoNumbers = (1..45).toSet().shuffled().take(6).sorted()
+    val lottoNumbers =
+        (LOTTO_NUMBER_MIN_VALUE..LOTTO_NUMBER_MAX_VALUE).toSet().shuffled().take(LOTTO_NUMBER_COUNT).sorted()
+
+    companion object {
+        const val LOTTO_NUMBER_MIN_VALUE: Int = 1
+        const val LOTTO_NUMBER_MAX_VALUE: Int = 45
+        const val LOTTO_NUMBER_COUNT: Int = 6
+    }
 }
