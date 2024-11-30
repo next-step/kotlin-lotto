ceb9f40bdaf9735fae69c93104a6fe3339a2bf08
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-30 11:18:21 +0900

    refactor : 상수값 위치 변경

--- src/main/kotlin/lotto/core/constant/LottoConstants.kt
+++ src/main/kotlin/lotto/core/constant/LottoConstants.kt
@@ -2,11 +2,8 @@ package lotto.core.constant
 
 object LottoConstants {
     const val LOTTO_NUMBER_COUNT = 6
-    const val LOTTO_NUMBER_MIN = 1
-    const val LOTTO_NUMBER_MAX = 45
     const val LOTTO_PRICE = 1000
 
-    const val ERROR_INVALID_NUMBER_SCOPE = "로또 번호의 범위가 잘못되었습니다."
     const val ERROR_INVALID_NUMBER_COUNT = "로또 번호의 개수가 잘못되었습니다."
     const val ERROR_INVALID_BONUS_NUMBER = "보너스 번호는 당첨번호에 포함되어서는 안됩니다."
 }
