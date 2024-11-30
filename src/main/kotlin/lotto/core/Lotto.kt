c9ab55d721195df816485470ae1fb8273c21bb1c
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-30 11:14:13 +0900

    refactor : countCommonNumber() => CountSameNumbers()로 함수 이름 변경

--- src/main/kotlin/lotto/core/Lotto.kt
+++ src/main/kotlin/lotto/core/Lotto.kt
@@ -2,21 +2,16 @@ package lotto.core
 
 import lotto.core.constant.LottoConstants
 
-class Lotto(val numbers: List<Int>) {
+class Lotto(val numbers: List<LottoNumber>) {
     init {
         require(numbers.size == LottoConstants.LOTTO_NUMBER_COUNT) { LottoConstants.ERROR_INVALID_NUMBER_COUNT }
-        require(numbers.none { checkValidNumberScope(it) }) { LottoConstants.ERROR_INVALID_NUMBER_SCOPE }
     }
 
-    private fun checkValidNumberScope(number: Int): Boolean {
-        return number < LottoConstants.LOTTO_NUMBER_MIN || LottoConstants.LOTTO_NUMBER_MAX < number
-    }
-
-    fun contains(number: Int): Boolean {
+    fun contains(number: LottoNumber): Boolean {
         return numbers.contains(number)
     }
 
-    fun countCommonNumbers(other: Lotto): Int {
+    fun countSameNumbers(other: Lotto): Int {
         return numbers.filter { it in other.numbers }.size
     }
 }
