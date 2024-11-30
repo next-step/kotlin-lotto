2c8611d36e647e4c38ce0933a06f9a7214074915
eshc123 <>
2024-12-01 00:52:23 +0900

    refactor: RandomLottoNumbersGenerator 개선 - 상수

--- src/main/kotlin/lotto/domain/lottonumber/RandomLottoNumbersGenerator.kt
+++ src/main/kotlin/lotto/domain/lottonumber/RandomLottoNumbersGenerator.kt
@@ -1,9 +1,11 @@
 package lotto.domain.lottonumber
 
 class RandomLottoNumbersGenerator : LottoNumbersGenerator {
-    override fun generateLottoNumbers(): List<Int> = LOTTO_NUMBER_RANGE.shuffled().subList(0, 6).sorted()
+    override fun generate(): List<Int> = LOTTO_NUMBER_RANGE.shuffled().subList(START_INDEX, LOTTO_NUMBER_COUNT).sorted()
 
     companion object {
+        private const val START_INDEX = 0
+        private const val LOTTO_NUMBER_COUNT = 6
         private val LOTTO_NUMBER_RANGE = 1..45
     }
 }
