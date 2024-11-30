8e1d75572614952a0f480162b86b71fd09c4c36a
Pablo730 <kcm1619@naver.com>
2024-12-01 02:53:14 +0900

    PurchasedLottoResultsTest 테스트 코드 개선

--- src/main/kotlin/lotto/domain/PurchasedLottoResults.kt
+++ src/main/kotlin/lotto/domain/PurchasedLottoResults.kt
@@ -5,6 +5,8 @@ import lotto.domain.LottoNumberMatchPayout.FOUR_NUMBER_MATCH
 import lotto.domain.LottoNumberMatchPayout.SIX_NUMBER_MATCH
 import lotto.domain.LottoNumberMatchPayout.THREE_NUMBER_MATCH
 import lotto.domain.LottoTicketIssuer.DEFAULT_LOTTO_PRICE
+import lotto.domain.PurchasedLottoTickets.Companion.INVALID_PURCHASED_COUNT_MESSAGE
+import lotto.domain.PurchasedLottoTickets.Companion.PURCHASED_COUNT_MIN_VALUE
 
 data class PurchasedLottoResults(
     val purchasedCount: Int,
@@ -14,10 +16,15 @@ data class PurchasedLottoResults(
     val sixNumberMatchCount: Int,
 ) {
     init {
+        require(purchasedCount >= PURCHASED_COUNT_MIN_VALUE) { INVALID_PURCHASED_COUNT_MESSAGE }
         require(threeNumberMatchCount >= LOTTO_MATCH_COUNT_MIN_VALUE) { INVALID_LOTTO_MATCH_COUNT_MESSAGE }
         require(fourNumberMatchCount >= LOTTO_MATCH_COUNT_MIN_VALUE) { INVALID_LOTTO_MATCH_COUNT_MESSAGE }
         require(fiveNumberMatchCount >= LOTTO_MATCH_COUNT_MIN_VALUE) { INVALID_LOTTO_MATCH_COUNT_MESSAGE }
         require(sixNumberMatchCount >= LOTTO_MATCH_COUNT_MIN_VALUE) { INVALID_LOTTO_MATCH_COUNT_MESSAGE }
+        require(
+            purchasedCount >=
+                threeNumberMatchCount + fourNumberMatchCount + fiveNumberMatchCount + sixNumberMatchCount,
+        ) { INVALID_PURCHASED_COUNT_LOTTO_MATCH_COUNT_MESSAGE }
     }
 
     fun getProfitMargin(): Double {
@@ -33,5 +40,6 @@ data class PurchasedLottoResults(
     companion object {
         const val LOTTO_MATCH_COUNT_MIN_VALUE: Int = 0
         const val INVALID_LOTTO_MATCH_COUNT_MESSAGE: String = "로또 당첨 번호 매치 결과는 0 이상 이어야합니다"
+        const val INVALID_PURCHASED_COUNT_LOTTO_MATCH_COUNT_MESSAGE: String = "당첨된 내역의 합이 구매한 개수보다 많을 수 없습니다"
     }
 }
