b5a8122d76a3889b53335f0b89c6bb943531d9e5
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-30 10:57:57 +0900

    refactor : winningNumber 수정
    1. Class이름 변경(WinningNumber -> WinningLotto)
    2. 부생성자 생성

---
+++ src/main/kotlin/lotto/core/WinningLotto.kt
@@ -0,0 +1,15 @@
+package lotto.core
+
+import lotto.core.constant.LottoConstants
+
+class WinningLotto(private val lotto: Lotto, private val bonusNumber: LottoNumber) {
+    constructor(numbers: List<LottoNumber>, bonusNumber: LottoNumber) : this(Lotto(numbers), bonusNumber)
+
+    init {
+        require(!lotto.contains(bonusNumber)) { LottoConstants.ERROR_INVALID_BONUS_NUMBER }
+    }
+
+    fun checkWinningState(lotto: Lotto): WinningRank {
+        return WinningRank.getRank(this.lotto.countSameNumbers(lotto), lotto.contains(bonusNumber))
+    }
+}
