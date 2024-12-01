1da71c38a09efc44524d9eb1eac22234bfa8ff65
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-12-01 19:39:49 +0900

    fix : 순서 오류 수정.

--- src/main/kotlin/lotto/core/WinningRank.kt
+++ src/main/kotlin/lotto/core/WinningRank.kt
@@ -5,8 +5,8 @@ enum class WinningRank(val winningCount: Pair<Int, Boolean>, val winningAmount: 
     FIFTH(3 to false, 5_000),
     FORTH(4 to false, 50_000),
     THIRD(5 to false, 1_500_000),
-    FIRST(6 to false, 2_000_000_000),
     SECOND(5 to true, 30_000_000),
+    FIRST(6 to false, 2_000_000_000),
     ;
 
     companion object {
