43e68066e153b910a3d41c82d99a242ec4270bf5
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-12-01 19:41:17 +0900

    feat : 수동 로또 발급 기능 추가.

--- src/main/kotlin/lotto/application/LottoController.kt
+++ src/main/kotlin/lotto/application/LottoController.kt
@@ -1,19 +1,23 @@
 package lotto.application
 
 import lotto.core.LottoMarket
+import lotto.core.WinningLotto
 import lotto.presentation.InputView
 import lotto.presentation.ResultView
 
 object LottoController {
     fun start() {
         val purchaseAmount = InputView.inputPurchaseAmount()
-        val lottos = LottoMarket.purchase(purchaseAmount)
-        ResultView.printLottos(lottos)
+        val manualLottoCount = InputView.inputManualLottoCount()
+        val manualLottoNumbers = InputView.inputManualLottoNumbers(manualLottoCount)
+
+        val lottos = LottoMarket.purchase(purchaseAmount, manualLottoNumbers)
+        ResultView.printLottos(lottos, manualLottoCount)
 
         val winningNumbers = InputView.inputWinningNumbers()
         val bonusNumber = InputView.inputBonusNumber()
 
-        val lottoResult = LottoWinningStatisticsService.start(lottos, winningNumbers, bonusNumber)
+        val lottoResult = lottos.countWinningRanks(WinningLotto(winningNumbers, bonusNumber))
 
         ResultView.printLottoResult(lottoResult)
         ResultView.printYieldRate(lottoResult, lottos)
