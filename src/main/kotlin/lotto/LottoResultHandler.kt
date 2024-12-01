d3abbfcade795f85ea7859309ca70d48a06bf314
ahn-sj <jaesa5221@gmail.com>
2024-12-01 13:14:51 +0900

    [step3] refactor: LottoResultHandler.match (v1) 메서드 제거

--- src/main/kotlin/lotto/LottoResultHandler.kt
+++ src/main/kotlin/lotto/LottoResultHandler.kt
@@ -1,17 +1,7 @@
 package lotto
 
 object LottoResultHandler {
-    fun match(
-        userLotto: List<LottoNumbers>,
-        winningLotto: LottoNumbers,
-    ): LottoResults {
-        return userLotto.fold(LottoResults()) { lottoResults, lottoNumbers ->
-            val matchCount = lottoNumbers.countMatch(winningLotto)
-            lottoResults.apply { counting(LottoResult(LottoRank.valueOf(matchCount))) }
-        }
-    }
-
-    fun match(userLotto: List<LottoNumbers>, winningLotto: WinningNumbers): LottoResults {
+        fun match(userLotto: List<LottoNumbers>, winningLotto: WinningNumbers): LottoResults {
         return userLotto.fold(LottoResults()) { lottoResults, lottoNumbers ->
             val matchCount = lottoNumbers.countMatch(winningLotto.lottoNumbers)
             val bonusMatch = lottoNumbers.matchBonus(winningLotto.bonusNumber)
