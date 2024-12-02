f9e97ce212cc12e7c0f89b5575718ee9ddb41cfa
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2024-12-02 09:49:21 +0900

    test: cashier 역할 변경에 따른 테스트 코드 수정

--- src/test/kotlin/lotto/domain/CashierTest.kt
+++ src/test/kotlin/lotto/domain/CashierTest.kt
@@ -2,24 +2,25 @@ package lotto.domain
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import lotto.strategy.FirstRankLottoLottoNumberGenerator
 import org.junit.jupiter.api.assertThrows
 
 class CashierTest : DescribeSpec({
     describe("입력한 금액만큼 로또를 구매한다") {
         it("1000원 보다 적은 경우 throw exception") {
             assertThrows<IllegalArgumentException> {
-                Cashier.purchaseLotto(999)
+                Cashier.purchaseLotto(999, FirstRankLottoLottoNumberGenerator())
             }
         }
 
         it("1000원 짜리 로또를 구매한다") {
-            val actual = Cashier.purchaseLotto(3000)
-            actual shouldBe 3
+            val actual = Cashier.purchaseLotto(3000, FirstRankLottoLottoNumberGenerator())
+            actual.size shouldBe 3
         }
 
         it("100원 단위는 내림처리 한다.") {
-            val actual = Cashier.purchaseLotto(3500)
-            actual shouldBe 3
+            val actual = Cashier.purchaseLotto(3500, FirstRankLottoLottoNumberGenerator())
+            actual.size shouldBe 3
         }
     }
 })
