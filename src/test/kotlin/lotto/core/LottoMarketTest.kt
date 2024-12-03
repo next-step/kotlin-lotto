81246878626110f07776388d6f2c74c71f60566e
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-12-03 22:33:11 +0900

    test : test case 수정.

--- src/test/kotlin/lotto/core/LottoMarketTest.kt
+++ src/test/kotlin/lotto/core/LottoMarketTest.kt
@@ -1,46 +1,22 @@
 package lotto.core
 
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
-import org.junit.jupiter.params.provider.ValueSource
 
 class LottoMarketTest {
     @ParameterizedTest
     @CsvSource(
-        "'500', 0",
-        "'1500', 1",
-        "'2000', 2",
+        "1, 1",
+        "3, 3",
     )
     fun `로또 구매를 테스트한다`(
-        amount: String,
-        count: Int,
+        totalCount: Int,
+        manualCount: Int,
     ) {
-        val list = List(0) { List(6) { LottoNumber(it) } }
+        val list = List(manualCount) { (1..6).map(::LottoNumber) }
 
-        val purchaseAmount = LottoPurchaseCount(amount, list.size)
-        LottoMarket.purchase(purchaseAmount, list).size shouldBe count
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["", "ㅈㄷㄹ"])
-    fun `숫자가 아닌 문자열 입력 시 Exception을 테스트한다`(amount: String) {
-        val list = List(0) { List(6) { LottoNumber(it) } }
-
-        shouldThrow<NumberFormatException> {
-            val purchaseAmount = LottoPurchaseCount(amount, list.size)
-            LottoMarket.purchase(purchaseAmount, list)
-        }
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["-1000"])
-    fun `잘못된 숫자 입력 시 Exception을 테스트한다`(amount: String) {
-        val list = List(0) { List(6) { LottoNumber(it) } }
-        shouldThrow<IllegalArgumentException> {
-            val purchaseAmount = LottoPurchaseCount(amount, list.size)
-            LottoMarket.purchase(purchaseAmount, list)
-        }
+        val purchaseAmount = LottoPurchaseCount(totalCount, manualCount)
+        LottoMarket.purchase(purchaseAmount, list).size shouldBe totalCount
     }
 }
