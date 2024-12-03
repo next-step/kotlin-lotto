9068e97102ea1eb0560305a5ad719d7b0b5c41b1
Sara Han <sarahan774@gmail.com>
2024-12-04 07:32:16 +0900

    [step3] Rank 로직 수정

--- src/main/kotlin/lotto/domain/data/Rank.kt
+++ src/main/kotlin/lotto/domain/data/Rank.kt
@@ -4,15 +4,17 @@ import java.math.BigDecimal
 
 enum class Rank(val prizeMoney: BigDecimal, val matchingNumberCount: Int) {
     FIRST(BigDecimal(2_000_000_000), 6),
-    THIRD(BigDecimal(1_500_000), 5),
     SECOND(BigDecimal(30_000_000), 5),
+    THIRD(BigDecimal(1_500_000), 5),
     FOURTH(BigDecimal(50_000), 4),
     FIFTH(BigDecimal(5_000), 3),
     ;
 
     companion object {
-        fun fromCount(count: Int): Rank {
-            return entries.find { count == it.matchingNumberCount }
+        fun fromCount(count: Int, isMatchBonus: Boolean): Rank {
+            return if (isMatchBonus) SECOND
+            else if (count == 5) THIRD
+            else entries.find { count == it.matchingNumberCount }
                 ?: throw IllegalArgumentException("invalid count $count")
         }
 
