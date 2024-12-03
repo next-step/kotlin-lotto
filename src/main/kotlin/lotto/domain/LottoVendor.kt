1ac00948700244e35dbe1bbdff75c2e6039ff4dc
eshc123 <>
2024-12-04 00:00:57 +0900

    refactor: LottoPurchaseManager 네이밍 수정

--- src/main/kotlin/lotto/domain/LottoVendor.kt
+++ src/main/kotlin/lotto/domain/LottoVendor.kt
@@ -2,7 +2,7 @@ package lotto.domain
 
 import lotto.domain.lottonumber.RandomLottoNumbersGenerator
 
-object LottoPurchaseManager {
+object LottoVendor {
     fun purchaseLotto(amount: Int): List<Lotto> {
         val lottoCount = amount / Lotto.LOTTO_PRICE
         return List(lottoCount) { Lotto(RandomLottoNumbersGenerator().generate()) }
