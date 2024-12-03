20a70024db89b6ff8afd60b2abbd35a726e2e22d
Sara Han <sarahan774@gmail.com>
2024-12-04 06:23:58 +0900

    [step2] 코드리뷰 반영 - 매직넘버 상수

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -8,9 +8,10 @@ object InputView {
     fun readWinningLotto(promptMessage: String): List<Int> {
         println(promptMessage)
         val winningLotto = readlnOrNull()?.split(",")?.map { it.trim().toIntOrNull() ?: 0 }
-        require(winningLotto?.size == 6) { "Lotto size must be 6" }
+        require(winningLotto?.size == LOTTO_SIZE) { "Lotto size must be 6" }
         return winningLotto ?: throw IllegalStateException("Separate Lotto numbers by comma (,)")
     }
 
     const val ENTER_LAST_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요."
+    private const val LOTTO_SIZE = 6
 }
