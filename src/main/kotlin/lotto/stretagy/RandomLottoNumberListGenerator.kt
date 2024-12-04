8d33f6d5fb6b329f15331009ea0bf17b08eecbff
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2024-12-04 15:09:27 +0900

    refactor: LottoNumberListGenerator 반환 타입을 `Set<LottoNumber>`로 수정한다.

--- src/main/kotlin/lotto/stretagy/RandomLottoNumberListGenerator.kt
+++ src/main/kotlin/lotto/stretagy/RandomLottoNumberListGenerator.kt
@@ -3,11 +3,14 @@ package lotto.stretagy
 import lotto.constant.MAXIMUM_NUMBER
 import lotto.constant.MINIMUM_NUMBER
 import lotto.constant.NUMBER_OF_SELECT
+import lotto.domain.LottoNumber
 
 class RandomLottoNumberListGenerator : LottoNumberListGenerator {
-    override fun generate(): List<Int> {
+    override fun generate(): Set<LottoNumber> {
         return (MINIMUM_NUMBER..MAXIMUM_NUMBER).shuffled()
             .take(NUMBER_OF_SELECT)
             .sorted()
+            .map(::LottoNumber)
+            .toSet()
     }
 }
