7d880a1924c203cf22244a5e9fe4a583d4c09fac
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2024-12-04 16:01:39 +0900

    refactor: 로또 생성시 중복을 허용하지 않는다.

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -3,7 +3,6 @@ package lotto.domain
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.matchers.ints.shouldBeLessThan
 import io.kotest.matchers.shouldBe
 
 class LottoTest : DescribeSpec({
@@ -32,14 +31,11 @@ class LottoTest : DescribeSpec({
                     Lotto(1, 2, 3, 4, 5, 6)
                 }
             }
+        }
 
-            it("로또의 각 숫자는 오름차순으로 정렬 되어 있어야 한다.") {
-                val actual = Lotto(6, 5, 4, 3, 2, 1).lottoNumbers.toList()
-                actual[0].number shouldBeLessThan actual[1].number
-                actual[1].number shouldBeLessThan actual[2].number
-                actual[2].number shouldBeLessThan actual[3].number
-                actual[3].number shouldBeLessThan actual[4].number
-                actual[4].number shouldBeLessThan actual[5].number
+        context("중복이 있는 경우") {
+            it("exception throw") {
+                shouldThrow<IllegalArgumentException> { Lotto(1, 1, 1, 1, 1, 1) }
             }
         }
     }
