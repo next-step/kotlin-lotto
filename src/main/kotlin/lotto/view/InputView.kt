2be418cc990f5eda4ab4f90c6532bc53df1d85d3
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2024-12-04 17:02:12 +0900

    feat: 보너스볼을 입력받는다.

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -7,6 +7,7 @@ class InputView {
         private const val REPLACEMENT_SOURCE = " "
         private const val REPLACEMENT_TARGET = ""
         private const val WINNING_NUMBER_INPUT_MESSAGE = "지난 주 당첨 번호를 입력해 주세요."
+        private const val BONUS_BALL_NUMBER_INPUT_MESSAGE = "지난 주 당첨 번호를 입력해 주세요."
         private const val ERROR_INVALID_NUMBER = "유효한 숫자를 입력해주세요."
         private const val ERROR_WRONG_NUMBER_COUNT = "정확히 6개의 숫자를 입력해야 합니다."
 
@@ -23,6 +24,12 @@ class InputView {
                 .also { require(it.size == 6) { ERROR_WRONG_NUMBER_COUNT } }
                 .map { it.toIntOrNull() ?: throw IllegalArgumentException(ERROR_INVALID_NUMBER) }
                 .toSet()
+        }
+
+        fun bonusBall(): Int {
+            println(BONUS_BALL_NUMBER_INPUT_MESSAGE)
+            return readln().toIntOrNull()
+                ?: throw IllegalArgumentException(ERROR_INVALID_NUMBER)
         }
     }
 }
