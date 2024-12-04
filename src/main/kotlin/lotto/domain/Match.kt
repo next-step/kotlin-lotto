5cfb0f24615d7a8ad547b647fbf6b942600f0df5
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2024-12-04 16:26:57 +0900

    feat: 보너스볼 일치 여부로 2등을 계산한다.

--- src/main/kotlin/lotto/domain/Match.kt
+++ src/main/kotlin/lotto/domain/Match.kt
@@ -1,23 +1,22 @@
 package lotto.domain
 
-import lotto.constant.FIFTH_RANK
-import lotto.constant.FIRST_RANK
-import lotto.constant.FOURTH_RANK
-import lotto.constant.MATCH_COUNT_FIVE
-import lotto.constant.MATCH_COUNT_FOUR
-import lotto.constant.MATCH_COUNT_SIX
-import lotto.constant.MATCH_COUNT_THREE
-import lotto.constant.NO_RANK
-import lotto.constant.THIRD_RANK
-
-class Match(val matchCount: Int) {
-    fun rank(matchCount: Int): Int {
-        return when (matchCount) {
-            MATCH_COUNT_SIX -> FIRST_RANK
-            MATCH_COUNT_FIVE -> THIRD_RANK
-            MATCH_COUNT_FOUR -> FOURTH_RANK
-            MATCH_COUNT_THREE -> FIFTH_RANK
-            else -> NO_RANK
+class Match(private val matchCount: Int, private val isMatchedBonusBall: Boolean) {
+    fun rank(): Int {
+        if (matchCount == 6) {
+            return 1
         }
+        if (matchCount == 5 && isMatchedBonusBall) {
+            return 2
+        }
+        if (matchCount == 5) {
+            return 3
+        }
+        if (matchCount == 4) {
+            return 4
+        }
+        if (matchCount == 3) {
+            return 3
+        }
+        return 0
     }
 }
--- src/test/kotlin/lotto/domain/MatchTest.kt
+++ src/test/kotlin/lotto/domain/MatchTest.kt
@@ -2,49 +2,38 @@ package lotto.domain
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
-import lotto.strategy.FifthRankLottoNumberGenerator
-import lotto.strategy.FirstRankLottoLottoNumberGenerator
-import lotto.strategy.FourRankLottoNumberGenerator
-import lotto.strategy.ThirdRankLottoNumberGenerator
-import lotto.strategy.WinningLottoNumberListGenerator
 
 class MatchTest : DescribeSpec({
-    lateinit var winningLotto: Lotto
-
     describe("사용자의 로또와 당첨로또의 숫자를 비교한다.") {
-        beforeTest {
-            val lottoNumbers = WinningLottoNumberListGenerator().generate()
-            winningLotto = Lotto(lottoNumbers)
-        }
-
         context("우승 로또와 사용자 로또를 비교해서 일치하는 개수를 리턴한다.") {
-            it("6자리가 모두 일치하는 경우") {
-                val userLottoNumbers = FirstRankLottoLottoNumberGenerator().generate()
-                val sut = Lotto(userLottoNumbers)
-                val actual = sut.getIntersectSize(winningLotto)
-                actual shouldBe 6
+            it("1등") {
+                val actual = Match(6, false).rank()
+                actual shouldBe 1
             }
 
-            it("5자리 일치") {
-                val userLottoNumbers = ThirdRankLottoNumberGenerator().generate()
-                val sut = Lotto(userLottoNumbers)
-                val actual = sut.getIntersectSize(winningLotto)
-                actual shouldBe 5
+            it("2등") {
+                val actual = Match(5, true).rank()
+                actual shouldBe 2
             }
 
-            it("4자리 일치") {
-                val userLottoNumbers = FourRankLottoNumberGenerator().generate()
-                val sut = Lotto(userLottoNumbers)
-                val actual = sut.getIntersectSize(winningLotto)
+            it("3등") {
+                val actual = Match(5, false).rank()
+                actual shouldBe 3
+            }
 
+            it("4등") {
+                val actual = Match(4, true).rank()
                 actual shouldBe 4
             }
 
-            it("3자리 일치") {
-                val userLottoNumbers = FifthRankLottoNumberGenerator().generate()
-                val sut = Lotto(userLottoNumbers)
-                val actual = sut.getIntersectSize(winningLotto)
+            it("5등") {
+                val actual = Match(3, true).rank()
                 actual shouldBe 3
+            }
+
+            it("낙첨") {
+                val actual = Match(2, true).rank()
+                actual shouldBe 0
             }
         }
     }
