11f5d1dcb2410be567d646e90524c8ca62076365
pkch93 <dev.pkch@gmail.com>
2024-12-04 20:45:02 +0900

    [step4] 코드리뷰 반영
    - 매직넘버 상수화

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,5 +1,8 @@
 package lotto.domain
 
+private val LOTTO_NUMBER_RANGE = 1..45
+private const val LOTTO_NUMBER_SIZE = 6
+
 class Lotto
 private constructor(
     numbers: List<Int>,
@@ -7,16 +10,16 @@ private constructor(
     val numbers: List<LottoNumber>
 
     init {
-        require(numbers.size == 6) { "로또 번호는 6개여야 합니다." }
-        require(setOf(*numbers.toTypedArray()).size == 6) { "중복된 로또 번호가 존재합니다." }
+        require(numbers.size == LOTTO_NUMBER_SIZE) { "로또 번호는 6개여야 합니다." }
+        require(setOf(*numbers.toTypedArray()).size == LOTTO_NUMBER_SIZE) { "중복된 로또 번호가 존재합니다." }
         this.numbers = numbers.map { LottoNumber(it) }
     }
 
     companion object Factory {
         fun auto(): Lotto {
-            val numbers = (1..45)
+            val numbers = LOTTO_NUMBER_RANGE
                 .shuffled()
-                .take(6)
+                .take(LOTTO_NUMBER_SIZE)
                 .sorted()
             return Lotto(numbers)
         }
