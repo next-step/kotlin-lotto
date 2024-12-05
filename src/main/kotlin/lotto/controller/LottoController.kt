5d27534f3b85e8c28273c4260ec522c030edd49d
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2024-12-05 21:40:46 +0900

    refactor: LottoController 메서드 추출

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -4,6 +4,7 @@ import lotto.domain.Cashier
 import lotto.domain.Lotto
 import lotto.domain.LottoNumber
 import lotto.domain.LottoRank
+import lotto.domain.LottoTicket
 import lotto.domain.Statistics
 import lotto.domain.WinningLotto
 import lotto.stretagy.RandomLottoNumberListGenerator
@@ -12,18 +13,36 @@ import lotto.view.OutputView
 
 class LottoController(private val inputView: InputView, private val outputView: OutputView) {
     fun start() {
-        val randomNumberListGenerator = RandomLottoNumberListGenerator()
         val amount = inputView.purchaseAmount()
+        val cashier = createCashier(amount)
 
-        val cashier = Cashier(amount, randomNumberListGenerator)
-        val lottos = cashier.purchaseLotto2()
-
+        val lottos = purchaseLottos(cashier)
         outputView.printPurchaseResult(lottos.tickets)
 
+        val winningLotto = inputWinningLotto()
+        calculateAndPrintStatistics(winningLotto, lottos, amount)
+    }
+
+    private fun createCashier(amount: Int): Cashier {
+        val randomNumberListGenerator = RandomLottoNumberListGenerator()
+        return Cashier(amount, randomNumberListGenerator)
+    }
+
+    private fun purchaseLottos(cashier: Cashier): LottoTicket {
+        return cashier.purchaseLotto2()
+    }
+
+    private fun inputWinningLotto(): WinningLotto {
         val winningNumbers = inputView.winningNumbers().map(::LottoNumber).toSet()
         val bonusBall = LottoNumber(inputView.bonusBall())
-        val winningLotto = WinningLotto(Lotto(winningNumbers), bonusBall)
+        return WinningLotto(Lotto(winningNumbers), bonusBall)
+    }
 
+    private fun calculateAndPrintStatistics(
+        winningLotto: WinningLotto,
+        lottos: LottoTicket,
+        amount: Int,
+    ) {
         val statistics = Statistics(winningLotto, lottos.tickets)
         val lottoResult: Map<LottoRank, Int> = statistics.lottoResultGroupByRank()
         val earningRatio = statistics.calculateEarningRatio(amount)
