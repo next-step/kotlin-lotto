e5519b793ea2bd7f30d0513a2ed81edd7c4eff4d
Pablo730 <kcm1619@naver.com>
2024-12-07 19:35:49 +0900

    lottoNumber 인스턴스 관리 list에서 map으로 수정

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -12,11 +12,11 @@ value class LottoNumber private constructor(val number: Int) {
 
         const val INVALID_LOTTO_NUMBER_MESSAGE: String = "로또 번호는 1부터 45사이의 숫자만 가능합니다."
 
-        private val ALL_LOTTO_NUMBERS = (LOTTO_NUMBER_MIN_VALUE..LOTTO_NUMBER_MAX_VALUE).map { LottoNumber(it) }
+        private val ALL_LOTTO_NUMBERS_MAP: Map<Int, LottoNumber> =
+            (LOTTO_NUMBER_MIN_VALUE..LOTTO_NUMBER_MAX_VALUE).associateWith { LottoNumber(it) }
 
         fun of(number: Int): LottoNumber {
-            require(number in LOTTO_NUMBER_MIN_VALUE..LOTTO_NUMBER_MAX_VALUE) { INVALID_LOTTO_NUMBER_MESSAGE }
-            return ALL_LOTTO_NUMBERS[number - 1]
+            return ALL_LOTTO_NUMBERS_MAP[number] ?: throw IllegalArgumentException(INVALID_LOTTO_NUMBER_MESSAGE)
         }
     }
 }
