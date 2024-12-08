7f87671650242a5db93dece6e2aced7de3b5d1e6
Sara Han <sarahan774@gmail.com>
2024-12-08 11:05:48 +0900

    [step3] LottoFactoryTest 추가

--- src/test/kotlin/lotto/domain/LottoFactoryTest.kt
+++ src/test/kotlin/lotto/domain/LottoFactoryTest.kt
@@ -5,6 +5,7 @@ import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import lotto.domain.model.LottoNumber
 import lotto.util.NumberGenerator
+import org.junit.jupiter.api.Assertions.assertThrows
 
 class LottoFactoryTest : StringSpec({
     "{given} LottoFactory(totalLottoCount=5) {when} createLottoList() {then} 로또 총 5개 " {
@@ -43,6 +44,19 @@ class LottoFactoryTest : StringSpec({
 
         // Then
         lottoList.forEach { lotto -> lotto.value shouldBe expectedLottoNumbers.map { LottoNumber(it) } }
+    }
+
+    "{given} 정수 리스트, size=6, {when} LottoFactory.fromList() {then} Lotto 생성" {
+        val list = listOf(1, 2, 3, 4, 5, 6)
+        val result = LottoFactory.fromList(list)
+        // assertTrue(result is Lotto) FIXME : this check is useless ?
+    }
+
+    "{given} 정수 리스트, size=10, {when} LottoFactory.fromList() {then} IllegalArgumentException 발생" {
+        val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        assertThrows(IllegalArgumentException::class.java) {
+            LottoFactory.fromList(list)
+        }
     }
 })
 
