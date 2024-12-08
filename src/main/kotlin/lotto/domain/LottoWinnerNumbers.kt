b5f5d995712cb4cc3340dade466498d5cd161aaf
Pablo730 <kcm1619@naver.com>
2024-12-08 23:50:19 +0900

    map 활용 및 groupingBy와 eachCount으로 간결하고 명확한 코드로 수정

--- src/main/kotlin/lotto/domain/LottoWinnerNumbers.kt
+++ src/main/kotlin/lotto/domain/LottoWinnerNumbers.kt
@@ -6,51 +6,20 @@ data class LottoWinnerNumbers(val lottoNumbers: LottoNumbers, val bonusNumber: L
     }
 
     fun resultLottoPayout(purchasedLottoTickets: PurchasedLottoTickets): PurchasedLottoResults {
-        val firstRank = mutableListOf<LottoTicket>()
-        val secondRank = mutableListOf<LottoTicket>()
-        val thirdRank = mutableListOf<LottoTicket>()
-        val fourthRank = mutableListOf<LottoTicket>()
-        val fifthRank = mutableListOf<LottoTicket>()
-
-        purchasedLottoTickets.purchasedLottoTickets.forEach { lottoTicket ->
-            numberMatchApply(
-                lottoTicket = lottoTicket,
-                firstRank = firstRank,
-                secondRank = secondRank,
-                thirdRank = thirdRank,
-                fourthRank = fourthRank,
-                fifthRank = fifthRank,
-            )
-        }
+        val rankCounts: Map<LottoWinnerRank, Int> =
+            purchasedLottoTickets.purchasedLottoTickets
+                .map { lottoTicket -> lottoTicket.checkLottoWinnerRank(this) }
+                .groupingBy { it }
+                .eachCount()
 
         return PurchasedLottoResults(
             purchasedCount = purchasedLottoTickets.purchasedCount,
-            firstRankCount = firstRank.size,
-            secondRankCount = secondRank.size,
-            thirdRankCount = thirdRank.size,
-            fourthRankCount = fourthRank.size,
-            fifthRankCount = fifthRank.size,
+            firstRankCount = rankCounts[LottoWinnerRank.FIRST] ?: 0,
+            secondRankCount = rankCounts[LottoWinnerRank.SECOND] ?: 0,
+            thirdRankCount = rankCounts[LottoWinnerRank.THIRD] ?: 0,
+            fourthRankCount = rankCounts[LottoWinnerRank.FOURTH] ?: 0,
+            fifthRankCount = rankCounts[LottoWinnerRank.FIFTH] ?: 0,
         )
-    }
-
-    private fun numberMatchApply(
-        lottoTicket: LottoTicket,
-        firstRank: MutableList<LottoTicket>,
-        secondRank: MutableList<LottoTicket>,
-        thirdRank: MutableList<LottoTicket>,
-        fourthRank: MutableList<LottoTicket>,
-        fifthRank: MutableList<LottoTicket>,
-    ) {
-        val lottoWinnerRank = lottoTicket.checkLottoWinnerRank(lottoWinnerNumbers = this)
-
-        when (lottoWinnerRank) {
-            LottoWinnerRank.FIRST -> firstRank.add(lottoTicket)
-            LottoWinnerRank.SECOND -> secondRank.add(lottoTicket)
-            LottoWinnerRank.THIRD -> thirdRank.add(lottoTicket)
-            LottoWinnerRank.FOURTH -> fourthRank.add(lottoTicket)
-            LottoWinnerRank.FIFTH -> fifthRank.add(lottoTicket)
-            else -> return
-        }
     }
 
     companion object {
