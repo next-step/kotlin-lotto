e32650ad9084d3614961283960948d7105c7befa
Aimbe <qkr2435@gmail.com>
2024-12-09 20:20:33 +0900

    refactor: property없는 클래스 object 변경

--- src/main/kotlin/lotto/LottoRandomGenerator.kt
+++ src/main/kotlin/lotto/LottoRandomGenerator.kt
@@ -1,9 +1,11 @@
 package lotto
 
 import lotto.LottoConstants.LOTTO_SIZE
+import lotto.LottoConstants.MAX_LOTTO_NUMBER
+import lotto.LottoConstants.MIN_LOTTO_NUMBER
 
-class LottoRandomGenerator {
+object LottoRandomGenerator {
     fun randomGenerate(): Lotto {
-        return Lotto((1..LOTTO_SIZE).map { LottoNumber((1..45).random()) })
+        return Lotto((1..LOTTO_SIZE).map { LottoNumber((MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER).random()) })
     }
 }
--- src/test/kotlin/lotto/LottoRandomGeneratorTest.kt
+++ src/test/kotlin/lotto/LottoRandomGeneratorTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.RepeatedTest
 class LottoRandomGeneratorTest {
     @RepeatedTest(100)
     fun `랜덤 로또 번호를 생성한다`() {
-        val randomLotto = LottoRandomGenerator().randomGenerate()
+        val randomLotto = LottoRandomGenerator.randomGenerate()
         assertEquals(6, randomLotto.numbers.distinct().size)
     }
 }
