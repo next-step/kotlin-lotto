5575f2a733d61b017deb0e2b4d291473fdc4b2e1
Aimbe <qkr2435@gmail.com>
2024-12-10 23:40:36 +0900

    refactor(review) : 메소드 추출 제거

--- src/main/kotlin/lotto/domain/WinningResult.kt
+++ src/main/kotlin/lotto/domain/WinningResult.kt
@@ -3,19 +3,10 @@ package lotto.domain
 class WinningResult(private val winningStatistics: Map<Int, Int>) {
     fun calculateTotalPrize(): Int {
         return winningStatistics.entries.sumOf { (matchCount, count) ->
-            calculatePrizeForMatch(matchCount, count)
+            Rank.from(matchCount).prize * count
         }
     }
 
-    private fun calculatePrizeForMatch(
-        matchCount: Int,
-        count: Int,
-    ): Int {
-        val rank = Rank.from(matchCount)
-        return rank.prize * count
-    }
-
-    // 손익률 계산
     fun calculateProfitRate(purchaseAmount: Int): Double {
         return calculateTotalPrize().toDouble() / purchaseAmount
     }
