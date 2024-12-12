fee890566b0e1660b6a90e22781124dd0f720e2c
Sara Han <sarahan774@gmail.com>
2024-12-12 12:05:45 +0900

    [step4] InputView 내부에서 프롬프트 메시지 보여주도록 수정

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -7,31 +7,28 @@ object InputView {
         return readlnOrNull()?.trim()?.toBigDecimalOrNull() ?: BigDecimal.ZERO
     }
 
-    fun readWinningLotto(promptMessage: String): List<Int> {
-        println(promptMessage)
+    fun readWinningLotto(): List<Int> {
+        println(ENTER_LAST_WINNING_NUMBER)
         val winningLotto = readlnOrNull()?.split(",")?.map { it.trim().toIntOrNull() ?: 0 }
         require(winningLotto?.size == LOTTO_SIZE) { "Lotto size must be 6" }
         return winningLotto ?: throw IllegalStateException("Separate Lotto numbers by comma (,)")
     }
 
-    fun readBonusLotto(promptMessage: String): Int {
-        println(promptMessage)
+    fun readBonusLotto(): Int {
+        println(ENTER_BONUS_BALL)
         val bonusLottoNumber = readlnOrNull()?.trim()?.toIntOrNull() ?: 0 // single number
         return bonusLottoNumber
     }
 
-    fun readManualLottoCount(promptMessage: String): Int {
-        println(promptMessage)
+    fun readManualLottoCount(): Int {
+        println(ENTER_MANUAL_LOTTO_COUNT)
         val manualLottoCount = readlnOrNull()?.trim()?.toIntOrNull() ?: 0
         return manualLottoCount
     }
 
-    fun readManualLottoList(
-        promptMessage: String,
-        count: Int,
-    ): List<List<Int>> {
+    fun readManualLottoList(count: Int): List<List<Int>> {
         val manualLottoNumberList = mutableListOf<List<Int>>()
-        println(promptMessage)
+        println(ENTER_MANUAL_LOTTO)
         repeat(count) {
             manualLottoNumberList.add(readManualLotto())
         }
@@ -41,15 +38,16 @@ object InputView {
     private fun readManualLotto(): List<Int> {
         val input =
             readlnOrNull()?.trim()
-                ?.split(",")
+                ?.split(INPUT_SEPARATOR_COMMA)
                 ?.map { it.trim().toIntOrNull() ?: 0 }
                 ?: throw IllegalStateException("Separate Lotto numbers by comma (,)")
         return input
     }
 
-    const val ENTER_LAST_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요."
-    const val ENTER_BONUS_BALL = "보너스 볼을 입력해 주세요."
-    const val ENTER_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요."
-    const val ENTER_MANUAL_LOTTO = "수동으로 구매할 번호를 입력해 주세요. (예: 8, 21, 23, 41, 42, 43)"
+    private const val ENTER_LAST_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요."
+    private const val ENTER_BONUS_BALL = "보너스 볼을 입력해 주세요."
+    private const val ENTER_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요."
+    private const val ENTER_MANUAL_LOTTO = "수동으로 구매할 번호를 입력해 주세요. (예: 8, 21, 23, 41, 42, 43)"
     private const val LOTTO_SIZE = 6
+    private const val INPUT_SEPARATOR_COMMA = ","
 }
