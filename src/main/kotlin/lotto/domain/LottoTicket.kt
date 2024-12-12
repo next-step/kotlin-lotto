a497fe0c508fee4a05f4c6808b50eddfedec3225
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2024-12-12 21:16:12 +0900

    refactor: 깊은 복사를 제거하고 컬렉션을 관리하도록 data class로 변경한다.

--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -1,13 +1,6 @@
 package lotto.domain
 
-class LottoTicket(tickets: List<Lotto>) {
-    private val _tickets = tickets.deepCopy()
-
-    val tickets: List<Lotto>
-        get() = _tickets
-
-    private fun List<Lotto>.deepCopy(): List<Lotto> = map { it.copy() }
-
+data class LottoTicket(val tickets: List<Lotto>) {
     fun flatLottos(vararg additionalTickets: LottoTicket): LottoTicket {
         return LottoTicket(
             tickets + additionalTickets.flatMap { it.tickets },
--- src/test/kotlin/lotto/domain/LottoTicketTest.kt
+++ src/test/kotlin/lotto/domain/LottoTicketTest.kt
@@ -2,24 +2,8 @@ package lotto.domain
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.types.shouldNotBeSameInstanceAs
 
 class LottoTicketTest : DescribeSpec({
-    describe("방어적 복사 테스트") {
-        it("should not same instance") {
-            val lottos =
-                listOf(
-                    Lotto(1, 2, 3, 4, 5, 6),
-                    Lotto(1, 2, 3, 4, 5, 6),
-                    Lotto(1, 2, 3, 4, 5, 6),
-                )
-
-            val lottoTicket = LottoTicket(lottos)
-
-            lottos shouldNotBeSameInstanceAs lottoTicket.tickets
-        }
-    }
-
     describe("수동 로또와 자동 로또를 하나의 리스트로 합친다") {
         it("파라미터로 받은 로또 리스트를 하나의 리스트로 합친뒤 반환한다.") {
             val manualLottos =
