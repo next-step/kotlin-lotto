47ff7e366a0652a9116f044a44c0349213a20713
Pablo730 <kcm1619@naver.com>
2024-12-14 02:45:06 +0900

    LottoNumberGenerator 코드 수정에 따른 테스트 수정

--- src/test/kotlin/lotto/domain/LottoNumberGeneratorTest.kt
+++ src/test/kotlin/lotto/domain/LottoNumberGeneratorTest.kt
@@ -6,6 +6,6 @@ import org.junit.jupiter.api.Test
 class LottoNumberGeneratorTest {
     @Test
     fun `로또 번호 자동 생성은 1부터 45사이의 6개의 숫자로 이루어져야한다`() {
-        (1..45).toSet().containsAll(generateLottoNumbers()) shouldBeEqual true
+        (1..45).toSet().containsAll(LottoNumberGenerator.generatorRandomNumber()) shouldBeEqual true
     }
 }
