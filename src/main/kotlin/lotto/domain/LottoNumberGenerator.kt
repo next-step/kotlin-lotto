c5e644191762fedce00d0db0040bf0bd6e3525a5
Pablo730 <kcm1619@naver.com>
2024-12-14 02:39:59 +0900

    로또 번호 자동 생성 로직 분리

--- src/main/kotlin/lotto/domain/LottoNumberGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoNumberGenerator.kt
@@ -1,7 +1,13 @@
 package lotto.domain
 
-fun generateLottoNumbers(): LottoNumbers {
-    val randomNumbers = (LottoNumber.LOTTO_NUMBER_MIN_VALUE..LottoNumber.LOTTO_NUMBER_MAX_VALUE)
-        .shuffled().take(LottoNumbers.LOTTO_NUMBER_COUNT).sorted()
-    return LottoNumbers(randomNumbers.map { LottoNumber.of(it) }.toSet())
+object LottoNumberGenerator {
+    fun generateAutoLottoNumbers(): LottoNumbers {
+        val randomNumbers = generatorRandomNumber()
+        return LottoNumbers(randomNumbers.map { LottoNumber.of(it) }.toSet())
+    }
+
+    fun generatorRandomNumber(): List<Int> {
+        return (LottoNumber.LOTTO_NUMBER_MIN_VALUE..LottoNumber.LOTTO_NUMBER_MAX_VALUE)
+            .shuffled().take(LottoNumbers.LOTTO_NUMBER_COUNT).sorted()
+    }
 }
