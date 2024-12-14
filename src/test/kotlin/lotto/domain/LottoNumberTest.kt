0570516b0021b4b7fe855a876e9816a230e57427
박효재(HyoJae Park)/OCB개발팀/SKP <hyojae.park@sk.com>
2024-12-14 10:15:08 +0900

    [STEP2] 로또(자동)(3) : 중복 테스트 케이스 제거

--- src/test/kotlin/lotto/domain/LottoNumberTest.kt
+++ src/test/kotlin/lotto/domain/LottoNumberTest.kt
@@ -2,19 +2,9 @@ package lotto.domain
 
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeSameInstanceAs
-import lotto.controller.GeneratorRandomNumbers
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
 
 class LottoNumberTest {
-    @ParameterizedTest
-    @ValueSource(ints = [1, 2, 3, 4])
-    fun `객체를 비교한다`(value: Int) {
-        val lottoNumber = LottoNumber.from(value)
-        (lottoNumber == LottoNumber.from(value)) shouldBe true
-    }
-
     @Test
     fun `같은 로또 번호가 동일한 객체로 반환한다`() {
         val lotto1 = LottoNumber.from(1)
