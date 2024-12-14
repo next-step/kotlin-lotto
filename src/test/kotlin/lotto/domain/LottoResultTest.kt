4bc5158db4b90be2a6efd3f0189dbd0b6355e808
박효재(HyoJae Park)/OCB개발팀/SKP <hyojae.park@sk.com>
2024-12-14 10:20:36 +0900

    [STEP2] 로또(자동)(3) : 1/2/3/4 등 테스트 케이스 작성

--- src/test/kotlin/lotto/domain/LottoResultTest.kt
+++ src/test/kotlin/lotto/domain/LottoResultTest.kt
@@ -5,10 +5,34 @@ import org.junit.jupiter.api.Test
 
 class LottoResultTest {
     @Test
-    fun `결과를 확인한다`() {
+    fun `1등을 확인한다`() {
         val tickets = listOf(LottoTicket(1, 2, 3, 4, 5, 6))
         val winTicket = LottoTicket(1, 2, 3, 4, 5, 6)
         val result = LottoResult(tickets, winTicket, 1)
         result.first shouldBe 1
+    }
+
+    @Test
+    fun `2등을 확인한다`() {
+        val tickets = listOf(LottoTicket(1, 2, 3, 4, 5, 6))
+        val winTicket = LottoTicket(1, 2, 3, 4, 5, 7)
+        val result = LottoResult(tickets, winTicket, 1)
+        result.second shouldBe 1
+    }
+
+    @Test
+    fun `3등을 확인한다`() {
+        val tickets = listOf(LottoTicket(1, 2, 3, 4, 5, 6))
+        val winTicket = LottoTicket(1, 2, 3, 4, 7, 8)
+        val result = LottoResult(tickets, winTicket, 1)
+        result.third shouldBe 1
+    }
+
+    @Test
+    fun `4등을 확인한다`() {
+        val tickets = listOf(LottoTicket(1, 2, 3, 4, 5, 6))
+        val winTicket = LottoTicket(1, 2, 3, 7, 8, 9)
+        val result = LottoResult(tickets, winTicket, 1)
+        result.fourth shouldBe 1
     }
 }
