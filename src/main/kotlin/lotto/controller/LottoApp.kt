3c861d42ed53343cc48f0199c91aa4cac0cdb6e4
박효재(HyoJae Park)/OCB개발팀/SKP <hyojae.park@sk.com>
2024-12-16 19:54:25 +0900

    [STEP2] 로또(자동)(4) : MVC 로 분류

---
+++ src/main/kotlin/lotto/controller/LottoApp.kt
@@ -0,0 +1,30 @@
+package lotto.controller
+
+import lotto.domain.LottoNumber
+import lotto.domain.LottoResult
+import lotto.domain.LottoTicket
+import lotto.view.InputView
+import lotto.view.ResultView
+
+class LottoApp {
+    private val inputView = InputView()
+    private val resultView = ResultView()
+
+    fun startLotto() {
+        val count = inputView.inputUser()
+        val tickets = LottoTicket.generateLottoTickets(count)
+        resultView.printBuyTickets(tickets)
+
+        val winNumbers = inputView.inputWinNumbers()
+        val winTicket = LottoTicket(winNumbers.map { LottoNumber(it) })
+        resultView.printTicket(winTicket)
+
+        val lottoResult = LottoResult(tickets, winTicket, count)
+        resultView.printResult(lottoResult)
+    }
+}
+
+fun main() {
+    val lottoApp = LottoApp()
+    lottoApp.startLotto()
+}
