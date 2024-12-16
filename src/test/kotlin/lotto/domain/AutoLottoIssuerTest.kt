d56c98a3aea0c161a99eb73cae8ab73e9b0302d1
Pablo730 <kcm1619@naver.com>
2024-12-17 01:06:33 +0900

    리류 반영 -> collection에서 더 직관적인 kotest 함수로 변경

--- src/test/kotlin/lotto/domain/AutoLottoIssuerTest.kt
+++ src/test/kotlin/lotto/domain/AutoLottoIssuerTest.kt
@@ -1,5 +1,6 @@
 package lotto.domain
 
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.equals.shouldBeEqual
 import lotto.util.createLottoNumbers
 import org.junit.jupiter.api.Test
@@ -8,6 +9,6 @@ class AutoLottoIssuerTest {
     @Test
     fun `정해진 개수 만큼 자동 로또를 생성 할 수 있다`() {
         val lottoTickets = AutoLottoIssuer.issueAutoLottoTickets(3) { createLottoNumbers(1, 2, 3, 4, 5, 6) }
-        lottoTickets.lottoTickets.size shouldBeEqual 3
+        lottoTickets.lottoTickets.shouldHaveSize(3)
     }
 }
