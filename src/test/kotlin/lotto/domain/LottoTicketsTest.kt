86f695830b369b253947b90c329b779c45959988
Sangwook <45926559+goodbyeyo@users.noreply.github.com>
2024-12-17 21:07:59 +0900

    Step3 로또 2등 (#1131)
    * feat: Lotto 도메인 테스트 및 구현
    
    * feat: 로또 번호, 티켓 발급 기능 구현 및 테스트
    
    * refactor: Test 코드 내 도메인 로직 Class 분리 리팩토링
    
    * feat: 당첨 로또 기능 구현
    
    * feat: 로또 컨트롤러, 입출력 뷰 구현
    
    * refactor: 피드백 반영, LottoTickets.calculateLottoRank 테스트 추가
    
    * refactor: 피드백 반영
    1. WinningLotto 제거하고 담당하던 책임 LottoTicket 으로 이동
    2. LottoTicket 프로퍼티 변경 : List<Int> -> Set<LottoNumber>
    3. LottoTicket 프로퍼티 기반으로 위임하도록 변경, 부생성자 추가
    4. LottoTicket 정적팩토리메서드 추가
    
    * doc: STEP3 로또2등 요구구사항 정리
    
    * feat: 보너스번호 도메인 구현
    
    * feat: 보너스 번호 일치여부 구현
    
    * feat: 로또 2등 기능 구현
    1. LottoRank : 로또 랭크 판단 조건, 상금 변경
    2. LottoTicket : 당첨 로또 계산 위임(WinningLotto)
    3. LottoTicket : data class 변경 (피드백 반영)
    4. 당첨 로또 계산 시 보너스 번호가 포함된 WinningLotto 전달
    5. 당첨 결과 출력 보너스 번호 일치 여부 출력
    6. 테스트 코드 2등 로또 반영
    
    * refactor: 피드백 반영
    1. BonusNumber 제거하고 WinningLotto 가 직접 책임을 담당하도록 변경
    2. WinningLotto 생성자 인자 팩토리 메서드(동반 객체 메서드) 구조로 전달
    3. LottoNumber 정적 팩터리 메서드 추가

--- src/test/kotlin/lotto/domain/LottoTicketsTest.kt
+++ src/test/kotlin/lotto/domain/LottoTicketsTest.kt
@@ -48,25 +48,4 @@ class LottoTicketsTest {
         sortedResults[5].rank shouldBe LottoRank.FIRST_PLACE
         sortedResults[5].count shouldBe 0
     }
-
-    @Test
-    fun `당첨 티켓을 전달하면 당첨 결과를 확인 할 수 있다`() {
-        val lottoTickets =
-            LottoTickets(
-                listOf(
-                    LottoTicket(listOf(1, 2, 3, 4, 5, 7)),
-                    LottoTicket(listOf(1, 2, 3, 4, 5, 9)),
-                    LottoTicket(listOf(1, 2, 3, 4, 8, 9)),
-                ),
-            )
-        val winningLotto = WinningLotto(listOf(1, 2, 3, 4, 5, 6))
-        val lottoResults = lottoTickets.calculateLottoRank(winningLotto)
-        val sortedResults = lottoResults.findAllSortedByMatchCount()
-        sortedResults[0].rank shouldBe LottoRank.FIRST_PLACE
-        sortedResults[0].count shouldBe 0
-        sortedResults[1].rank shouldBe LottoRank.SECOND_PLACE
-        sortedResults[1].count shouldBe 2
-        sortedResults[2].rank shouldBe LottoRank.THIRD_PLACE
-        sortedResults[2].count shouldBe 1
-    }
 }
