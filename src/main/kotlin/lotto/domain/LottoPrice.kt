a516fb134a6c2fc22aae6ed76069120d71fe8364
Aimbe <qkr2435@gmail.com>
2024-12-20 19:21:11 +0900

    refactor: 프로퍼티 private 변경

--- src/main/kotlin/lotto/domain/LottoPrice.kt
+++ src/main/kotlin/lotto/domain/LottoPrice.kt
@@ -1,7 +1,7 @@
 package lotto.domain
 
 @JvmInline
-value class LottoPrice(val money: Int) {
+value class LottoPrice(private val money: Int) {
     init {
         require(money >= LottoConstants.LOTTO_PRICE) { "로또 구입 금액은 1000원 이상이어야 합니다." }
     }
