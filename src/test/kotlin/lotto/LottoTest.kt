c2869c573f82f907ed08ba3a874132d986575a0e
SeokJun Jeong <sjjeong1225@gmail.com>
2024-12-26 13:04:05 +0900

    refactor: LottoVendingMachineTest 파일 추가
    LottoTest에 있던 LottoVendingMachine 관련 테스트들 이동

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,33 +1,9 @@
 package lotto
 
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 class LottoTest {
-
-    @Test
-    internal fun `로또 구매 금액은 0이거나 null이 아니어야 한다`() {
-        shouldThrow<IllegalArgumentException> {
-            LottoVendingMachine.buyLotto(0)
-        }
-        shouldThrow<IllegalArgumentException> {
-            LottoVendingMachine.buyLotto(null)
-        }
-    }
-
-    @Test
-    internal fun `로또 구매 금액은 1000이상이어야 한다`() {
-        shouldThrow<IllegalArgumentException> {
-            LottoVendingMachine.buyLotto(999)
-        }
-    }
-
-    @Test
-    internal fun `로또 한 장의 금액은 1000원 이다`() {
-        LottoVendingMachine.buyLotto(1000).size shouldBe 1
-        LottoVendingMachine.buyLotto(14000).size shouldBe 14
-    }
 
     @Test
     internal fun `로또 번호가 실제로 일치하는지`() {
