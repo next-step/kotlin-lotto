d7f1b78f802110142d8e9fcd42d53d7e4b60aa3d
SeokJun Jeong <sjjeong1225@gmail.com>
2025-01-07 16:04:26 +0900

    refactor: Lotto의 lottoNumbers를 Set으로 변경

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,19 +1,18 @@
 package lotto
 
-class Lotto private constructor(val lottoNumbers: List<LottoNumber>) {
+class Lotto private constructor(val lottoNumbers: Set<LottoNumber>) {
 
     init {
         val lottoNumbersSize = lottoNumbers.size
         require(lottoNumbersSize == LOTTO_SIZE)
-        require(lottoNumbersSize == lottoNumbers.toSet().size)
     }
 
-    constructor() : this(LottoPreset.shuffled().take(6).sortedBy { it.number })
+    constructor() : this(LottoPreset.shuffled().take(6).sortedBy { it.number }.toSet())
 
-    constructor(vararg number: Int) : this(number.map { LottoNumber(it) })
+    constructor(vararg number: Int) : this(number.map { LottoNumber(it) }.toSet())
 
     fun countMatch(winningLotto: Lotto): Int {
-        return lottoNumbers.intersect(winningLotto.lottoNumbers.toSet()).count()
+        return lottoNumbers.intersect(winningLotto.lottoNumbers).count()
     }
 
     override fun toString(): String {
