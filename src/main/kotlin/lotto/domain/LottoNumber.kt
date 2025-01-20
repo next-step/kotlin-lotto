c665edb5ecd16d87e60d9ecda6a59f0ff289686c
sjjeong <sjjeong1225@gmail.com>
2025-01-20 13:38:44 +0900

    refactor: range를 companion object로 추출

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -3,11 +3,12 @@ package lotto.domain
 @JvmInline
 value class LottoNumber(val number: Int) {
     init {
-        require(number in START_LOTTO_NUMBER..END_LOTTO_NUMBER)
+        require(number in lottoNumberRange)
     }
 
     companion object {
         const val START_LOTTO_NUMBER = 1
         const val END_LOTTO_NUMBER = 45
+        val lottoNumberRange = START_LOTTO_NUMBER..END_LOTTO_NUMBER
     }
 }
