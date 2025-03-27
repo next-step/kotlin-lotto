bfaff9b2338f556af6c7d481f4d22fd09c5bf3cf
Arkadii Shyn <arkadii.shyn@deliveryhero.com>
2025-03-27 22:01:50 +0100

    test: case 1.2 - refactor test

--- src/test/kotlin/lotto/model/TicketModelModelTest.kt
+++ src/test/kotlin/lotto/model/TicketModelModelTest.kt
@@ -9,23 +9,31 @@ import org.junit.jupiter.params.provider.MethodSource
 class TicketModelModelTest {
 
     @ParameterizedTest
-    @MethodSource("provideNumbers")
+    @MethodSource("validTickets")
     fun `ticket should contain 6 numbers`(numbers: List<Int>) {
         val ticket = TicketModel(numbers)
         assertThat(ticket.numbers).isEqualTo(numbers)
     }
 
-    @Test
-    fun `should fail if numbers less or more then 6`() {
-        assertThrows<IllegalArgumentException> { TicketModel(listOf(1, 2, 3)) }
+    @ParameterizedTest
+    @MethodSource("invalidTickets")
+    fun `should fail if numbers less or more then 6`(numbers: List<Int>) {
+        assertThrows<IllegalArgumentException> { TicketModel(numbers) }
     }
 
     companion object {
         @JvmStatic
-        fun provideNumbers(): List<List<Int>> = listOf(
+        fun validTickets(): List<List<Int>> = listOf(
             listOf(1, 2, 3, 4, 5, 6),
             listOf(6, 5, 4, 3, 2, 1),
             listOf(8, 9, 10, 11, 12, 13)
+        )
+        @JvmStatic
+        fun invalidTickets(): List<List<Int>> = listOf(
+            listOf(1, 2, 3),
+            listOf(3, 2, 1),
+            listOf(1, 2, 3, 4, 5, 6, 7),
+            listOf(7, 6, 5, 4, 3, 2, 1),
         )
     }
 }
