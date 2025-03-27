9dcb672c61e06d725a100c444436f012c110f1e2
Arkadii Shyn <arkadii.shyn@deliveryhero.com>
2025-03-27 20:52:09 +0100

    test: case 1.1 - refactor test

--- src/test/kotlin/lotto/model/TicketModelModelTest.kt
+++ src/test/kotlin/lotto/model/TicketModelModelTest.kt
@@ -1,7 +1,6 @@
 package lotto.model
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 
