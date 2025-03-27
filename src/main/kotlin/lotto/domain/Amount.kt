fed213c7e3993b0ed2e5316d49bebf8caeaad339
syoun602 <syoun602@gmail.com>
2025-03-27 23:42:27 +0100

    feat: add validations for amount

--- src/main/kotlin/lotto/domain/Amount.kt
+++ src/main/kotlin/lotto/domain/Amount.kt
@@ -3,4 +3,14 @@ package lotto.domain
 class Amount(
     private val value: Int,
 ) {
+    init {
+        require(value in MINIMUM_AMOUNT..MAXIMUM_AMOUNT) {
+            "Amount value must be between 1,000 and 100,000 KRW"
+        }
+    }
+
+    companion object {
+        private const val MINIMUM_AMOUNT = 1_000
+        private const val MAXIMUM_AMOUNT = 100_000
+    }
 }
--- src/test/kotlin/lotto/domain/AmountTest.kt
+++ src/test/kotlin/lotto/domain/AmountTest.kt
@@ -1,18 +1,32 @@
 package lotto.domain
 
 import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.datatest.withData
+import io.kotest.inspectors.forAll
 
 class AmountTest : FunSpec({
-    context("value must be above or equal to 1,000 and below or equal to 100,000") {
-        withData(
+    test("value must be above or equal to 1,000 and below or equal to 100,000") {
+        listOf(
             1_000,
             2_000,
             10_000,
             100_000,
-        ) {
+        ).forAll {
             shouldNotThrowAny {
+                Amount(it)
+            }
+        }
+    }
+
+    test("throw exception if value is less than 1,000 or above 100,000") {
+        listOf(
+            998,
+            999,
+            100_001,
+            100_002,
+        ).forAll {
+            shouldThrow<IllegalArgumentException> {
                 Amount(it)
             }
         }
