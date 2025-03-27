e66a204ee45d14f6f401c9ec1d4f1630fb11a083
Arkadii Shyn <arkadii.shyn@deliveryhero.com>
2025-03-27 20:44:54 +0100

    test: case 1.1 - new test

--- src/test/kotlin/lotto/model/TicketModelModelTest.kt
+++ src/test/kotlin/lotto/model/TicketModelModelTest.kt
@@ -10,4 +10,10 @@ class TicketModelModelTest {
         val ticket = TicketModel(listOf(1,2,3,4,5,6))
         assertThat(ticket.numbers).isEqualTo(listOf(1,2,3,4,5,6))
     }
+
+    @Test
+    fun `ticket should contain another 6 numbers`(){
+        val ticket = TicketModel(listOf(6,5,4,3,2,1))
+        assertThat(ticket.numbers).isEqualTo(listOf(6,5,4,3,2,1))
+    }
 }
