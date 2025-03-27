a2a7b8ecee8b7eb39c45b92eee19247b6c258c4e
Arkadii Shyn <arkadii.shyn@deliveryhero.com>
2025-03-27 20:51:46 +0100

    test: case 1.1 - refactor test

--- src/test/kotlin/lotto/model/TicketModelModelTest.kt
+++ src/test/kotlin/lotto/model/TicketModelModelTest.kt
@@ -2,18 +2,24 @@ package lotto.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 class TicketModelModelTest {
 
-    @Test
-    fun `ticket should contain 6 numbers`(){
-        val ticket = TicketModel(listOf(1,2,3,4,5,6))
-        assertThat(ticket.numbers).isEqualTo(listOf(1,2,3,4,5,6))
+    @ParameterizedTest
+    @MethodSource("provideNumbers")
+    fun `ticket should contain 6 numbers`(numbers: List<Int>) {
+        val ticket = TicketModel(numbers)
+        assertThat(ticket.numbers).isEqualTo(numbers)
     }
 
-    @Test
-    fun `ticket should contain another 6 numbers`(){
-        val ticket = TicketModel(listOf(6,5,4,3,2,1))
-        assertThat(ticket.numbers).isEqualTo(listOf(6,5,4,3,2,1))
+    companion object {
+        @JvmStatic
+        fun provideNumbers(): List<List<Int>> = listOf(
+            listOf(1, 2, 3, 4, 5, 6),
+            listOf(6, 5, 4, 3, 2, 1),
+            listOf(8, 9, 10, 11, 12, 13)
+        )
     }
 }
