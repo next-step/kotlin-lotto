7b41b8cd643ca6f695bcc73acd18786f10471d0c
Arkadii Shyn <arkadii.shyn@deliveryhero.com>
2025-03-27 21:56:04 +0100

    test: case 1.2 - refactor

--- src/main/kotlin/lotto/model/TicketModel.kt
+++ src/main/kotlin/lotto/model/TicketModel.kt
@@ -1,7 +1,11 @@
 package lotto.model
 
+const val TICKET_NUMBER_LENGTH = 6
+
 class TicketModel(val numbers : List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == TICKET_NUMBER_LENGTH) {
+            "Ticket should contain $TICKET_NUMBER_LENGTH numbers"
+        }
     }
 }
