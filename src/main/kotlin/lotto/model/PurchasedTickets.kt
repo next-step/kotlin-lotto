45178dd417311dd3511069c992b0d116b4766808
Arkadii Shyn <arkadii.shyn@deliveryhero.com>
2025-03-28 22:50:06 +0100

    refactor: implement buy ticket method

--- src/main/kotlin/lotto/model/PurchasedTickets.kt
+++ src/main/kotlin/lotto/model/PurchasedTickets.kt
@@ -2,6 +2,11 @@ package lotto.model
 
 const val TICKET_PRICE = 1000;
 
-class PurchasedTickets(amount: Int) {
-    val ticketCount = amount/TICKET_PRICE
+class PurchasedTickets(private val tickets: List<TicketModel>) {
+    companion object {
+        fun buyTickets(amount: Int): PurchasedTickets {
+            val ticketCount = amount/TICKET_PRICE
+            return PurchasedTickets( List(ticketCount) { TicketModel.generate()} )
+        }
+    }
 }
