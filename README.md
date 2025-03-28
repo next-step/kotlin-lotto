394058e4532a6dc7c50665b2a634cefcd334c537
syoun602 <syoun602@gmail.com>
2025-03-28 10:35:57 +0100

    feat: lotto machine should create lottos according to the amount

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Lotto
 
 LottoMachine
 
-- [ ] insert amount and create lotto which costs 1,000 KRW for each
+- [x] insert amount and create lotto which costs 1,000 KRW for each
 - [ ] all decimals are discarded when divided by 1,000
 
 WinningNumber
--- src/test/kotlin/lotto/domain/LottoMachineTest.kt
+++ src/test/kotlin/lotto/domain/LottoMachineTest.kt
@@ -1,13 +1,20 @@
 package lotto.domain
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 
 class LottoMachineTest : FunSpec({
-    test("create lottos with amount") {
-        val amount = Amount(1_000)
-        val actual = LottoMachine().createLottos(amount)
+    context("create lottos according to amount") {
+        withData(
+            Amount(1_000) to 1,
+            Amount(2_000) to 2,
+            Amount(5_500) to 5,
+            Amount(10_000) to 10,
+        ) { (amount, expected) ->
+            val actual = LottoMachine().createLottos(amount)
 
-        actual.size shouldBe 1
+            actual.size shouldBe expected
+        }
     }
 })
