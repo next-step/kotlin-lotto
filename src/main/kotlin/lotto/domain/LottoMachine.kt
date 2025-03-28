efd31c8dda21a29853b18d7b8ef8fa6ccd563dd0
syoun602 <syoun602@gmail.com>
2025-03-28 10:46:10 +0100

    feat: create auto lotto with machine

--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -2,7 +2,22 @@ package lotto.domain
 
 class LottoMachine {
     fun createLottos(amount: Amount): List<Lotto> {
-        val number = amount.divide(1_000)
-        return List(number) { Lotto.fromRawNumbers(listOf(1, 2, 3, 4, 5, 6)) }
+        val number = amount.divide(LOTTO_PRICE)
+        return List(number) { createAutoLotto() }
+    }
+
+    private fun createAutoLotto() =
+        Lotto(
+            (MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER).shuffled()
+                .take(LOTTO_NUMBERS_SIZE)
+                .map(::LottoNumber),
+        )
+
+    companion object {
+        private const val MIN_LOTTO_NUMBER = 1
+        private const val MAX_LOTTO_NUMBER = 45
+
+        private const val LOTTO_PRICE = 1_000
+        private const val LOTTO_NUMBERS_SIZE = 6
     }
 }
