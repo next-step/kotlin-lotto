dbef2504716818a35de7ba8c00ffe3bc2df2ac8c
Arkadii Shyn <arkadii.shyn@deliveryhero.com>
2025-03-31 21:50:11 +0200

    test: case 2.2. refactoring

--- src/main/kotlin/lotto/model/TicketModel.kt
+++ src/main/kotlin/lotto/model/TicketModel.kt
@@ -2,7 +2,7 @@ package lotto.model
 
 const val TICKET_NUMBER_LENGTH = 6
 
-class TicketModel(val numbers : List<Int>) {
+data class TicketModel(val numbers : List<Int>) {
     init {
         require(numbers.size == TICKET_NUMBER_LENGTH) {
             "Ticket should contain $TICKET_NUMBER_LENGTH numbers"
--- src/test/kotlin/lotto/model/PurchasedTicketsTest.kt
+++ src/test/kotlin/lotto/model/PurchasedTicketsTest.kt
@@ -4,16 +4,21 @@ import org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 class PurchasedTicketsTest {
+    private val testGenerator = TestTicketNumberGenerator(listOf(1, 2, 3, 4, 5, 6))
 
     @Test
     fun `should calculate correct number of tickets based on purchased amount`() {
-        val purchasedTickets = PurchasedTickets.buyTickets(5000)
+        val purchasedTickets = PurchasedTickets.buyTickets(5000, testGenerator)
         assertThat(purchasedTickets.getTickets().size).isEqualTo(5)
     }
 
     @Test
     fun `should generate tickets based on purchased amount`() {
-        val purchasedTickets = PurchasedTickets.buyTickets(1000)
-        assertThat(purchasedTickets.getTickets().first()).isEqualTo(TicketModel(listOf(1,2,3,4,5,6)))
+        val purchasedTickets = PurchasedTickets.buyTickets(1000, testGenerator)
+        assertThat(purchasedTickets.getTickets().first()).isEqualTo(TicketModel(listOf(1, 2, 3, 4, 5, 6)))
+    }
+
+    class TestTicketNumberGenerator(private val fixedNumbers: List<Int>) : TicketNumberGenerator {
+        override fun generateNumbers(): List<Int> = fixedNumbers
     }
 }
