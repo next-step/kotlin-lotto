2d8f4bc0f61d1c23d8e8354fdbfbf967403ccd51
Arkadii Shyn <arkadii.shyn@deliveryhero.com>
2025-04-01 00:05:24 +0200

    feat: InputView

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -3,16 +3,17 @@ package lotto.controller
 import lotto.model.LottoResult
 import lotto.model.PurchasedTickets
 import lotto.model.RandomTicketNumberGenerator
+import lotto.view.InputView
 
 class LottoController {
 
     companion object {
         private val generator = RandomTicketNumberGenerator()
         fun run() {
-            val purchasedAmount = 5000
+            val purchasedAmount = InputView.readPurchaseAmount()
             val purchasedTickets = PurchasedTickets.buyTickets(purchasedAmount, generator)
             println(purchasedTickets.getTickets())
-            val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+            val winningNumbers = InputView.readWinningNumbers()
             val result = LottoResult(winningNumbers, purchasedTickets.getTickets())
             println(result)
         }
