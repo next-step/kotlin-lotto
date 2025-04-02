1af82c27f98dab42bbd044e560ea98852453642f
syoun602 <syoun602@gmail.com>
2025-04-02 22:45:15 +0200

    refactor: specify types for intuition

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -11,8 +11,8 @@ class LottoNumber private constructor(
         private const val MINIMUM_LOTTO_NUMBER = 1
         private const val MAXIMUM_LOTTO_NUMBER = 45
 
-        private val cache = (MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER).associateWith { LottoNumber(it) }
-        val cached = cache.keys
+        private val cache: Map<Int, LottoNumber> = (MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER).associateWith { LottoNumber(it) }
+        val cached: Set<Int> = cache.keys
 
         fun from(number: Int) = cache[number] ?: throw IllegalArgumentException("value must be between 1 and 45 (inclusive)")
     }
