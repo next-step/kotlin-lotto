048309e8b61d5f86aed613f30d6263b4b7ffe28f
krrong <khk9664@naver.com>
2025-04-02 23:50:54 +0200

    refactor: add print message

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -2,11 +2,18 @@ package lotto.view
 
 class InputView {
     fun getPurchaseAmount(): Int {
+        println(GUIDE_PURCHASE_AMOUNT)
         return readln().toInt()
     }
 
     fun getWinningNumbers(): List<Int> {
+        println(GUIDE_WINNING_NUMBER)
         val numbers = readln().split(",")
         return numbers.map { it.trim().toInt() }
+    }
+
+    companion object {
+        private const val GUIDE_PURCHASE_AMOUNT = "Please enter the purchase amount."
+        private const val GUIDE_WINNING_NUMBER = "Please enter last week's winning numbers"
     }
 }
