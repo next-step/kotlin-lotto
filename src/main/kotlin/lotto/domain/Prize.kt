5ef80b6676c93816ac43c651b61abf68cd388f59
syoun602 <syoun602@gmail.com>
2025-04-06 21:50:37 +0200

    refactor: remove unnecessary property on Prize

--- src/main/kotlin/lotto/domain/Prize.kt
+++ src/main/kotlin/lotto/domain/Prize.kt
@@ -3,20 +3,25 @@ package lotto.domain
 enum class Prize(
     val value: Int,
     val matchCount: Int,
-    private val matcher: (Int, Boolean) -> Boolean,
 ) {
-    FIRST(2_000_000_000, 6, { count, _ -> count == 6 }),
-    SECOND(30_000_000, 5, { count, hasBonus -> count == 5 && hasBonus }),
-    THIRD(1_500_000, 5, { count, hasBonus -> count == 5 && !hasBonus }),
-    FOURTH(50_000, 4, { count, _ -> count == 4 }),
-    FIFTH(5_000, 3, { count, _ -> count == 3 }),
-    NONE(0, 0, { _, _ -> false }),
+    FIRST(2_000_000_000, 6),
+    SECOND(30_000_000, 5),
+    THIRD(1_500_000, 5),
+    FOURTH(50_000, 4),
+    FIFTH(5_000, 3),
+    NONE(0, 0),
     ;
 
     companion object {
         fun calculate(
             count: Int,
             hasBonus: Boolean,
-        ) = entries.find { it.matcher(count, hasBonus) } ?: NONE
+        ) = when (count) {
+            6 -> FIRST
+            5 -> if (hasBonus) SECOND else THIRD
+            4 -> FOURTH
+            3 -> FIFTH
+            else -> NONE
+        }
     }
 }
