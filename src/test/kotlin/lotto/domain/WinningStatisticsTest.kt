532a917af57efa899cdb11fe29dbc4326b486191
MingyumKim <mingyum.kim@deliveryhero.com>
2025-04-06 23:42:48 +0200

    style: reformate code

--- src/test/kotlin/lotto/domain/WinningStatisticsTest.kt
+++ src/test/kotlin/lotto/domain/WinningStatisticsTest.kt
@@ -4,17 +4,17 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class WinningStatisticsTest {
- @Test
- fun `set rate over 1`() {
-  val winningStatistics = WinningStatistics(mutableMapOf(3 to 1, 4 to 2))
-  winningStatistics.setRate(10)
-          assertThat(winningStatistics.getRate()).isEqualTo(10.50)
- }
+    @Test
+    fun `set rate over 1`() {
+        val winningStatistics = WinningStatistics(mutableMapOf(3 to 1, 4 to 2))
+        winningStatistics.setRate(10)
+        assertThat(winningStatistics.getRate()).isEqualTo(10.50)
+    }
 
- @Test
- fun `set rate under 1`() {
-  val winningStatistics = WinningStatistics(mutableMapOf(3 to 1))
-  winningStatistics.setRate(30)
-  assertThat(winningStatistics.getRate()).isEqualTo(0.17)
-  }
- }
+    @Test
+    fun `set rate under 1`() {
+        val winningStatistics = WinningStatistics(mutableMapOf(3 to 1))
+        winningStatistics.setRate(30)
+        assertThat(winningStatistics.getRate()).isEqualTo(0.17)
+    }
+}
