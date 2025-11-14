97c13e3f65ad1660d0c602579c29ab9d56a5ac5b
give.yoon <give.yoon@woowahan.com>
2025-11-14 18:03:41 +0900

    feat: 요구사항 3(지난주 당첨번호를 입력받는다)

--- src/main/kotlin/presentation/InputView.kt
+++ src/main/kotlin/presentation/InputView.kt
@@ -7,5 +7,10 @@ class InputView {
             println("구입금액을 입력해 주세요.")
             return readln()
         }
+
+        fun inputWinningNumbers(): List<Int> {
+            println("지난 주 당첨번호를 입력해 주세요.")
+            return readln().split(",").map { it.toInt() }
+        }
     }
 }
