649e08c462f9836e94e3ea711adb5454446d2f92
ryeol.jang <ryeol.jang@woowahan.com>
2025-11-17 18:57:49 +0900

    fix: 테스트 컴파일 실패 수정

--- src/test/kotlin/com/example/mylotto/model/LottoResultTest.kt
+++ src/test/kotlin/com/example/mylotto/model/LottoResultTest.kt
@@ -7,16 +7,15 @@ import io.kotest.matchers.shouldBe
 class LottoResultTest :
     BehaviorSpec({
         given("a list of ranks") {
-            val ranks = listOf(Rank.FIRST, Rank.SECOND, Rank.SECOND, Rank.THIRD, Rank.THIRD, Rank.THIRD)
+            val ranks = listOf(Rank.FIRST, Rank.THIRD, Rank.THIRD, Rank.THIRD)
 
             `when`("a LottoResult is created") {
-                val lottoResult = LottoResult(ranks)
+                val lottoResult = LottoResult.of(ranks)
 
                 then("the rank count map should correctly group counts") {
                     val rankCounts = lottoResult.rankCountMap
 
                     rankCounts[Rank.FIRST].shouldBe(1)
-                    rankCounts[Rank.SECOND].shouldBe(2)
                     rankCounts[Rank.THIRD].shouldBe(3)
                     rankCounts[Rank.FOURTH].shouldBe(null)
                 }
