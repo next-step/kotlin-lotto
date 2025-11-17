504593408c5100411e92a26fc966e5cd65780c7f
ryeol.jang <ryeol.jang@woowahan.com>
2025-11-17 18:04:39 +0900

    feat: Domain 및 Service 로직 테스트 작성

--- src/main/kotlin/com/example/mylotto/model/LottoWinningNumbers.kt
+++ src/main/kotlin/com/example/mylotto/model/LottoWinningNumbers.kt
@@ -1,6 +1,9 @@
 package com.example.mylotto.model
 
-class LottoWinningNumbers(
-    private val winningNumbers: List<Int>,
-    private val bonusNumber: Int,
-)
+class LottoWinningNumbers private constructor(
+    private val numbers: Set<LottoNumber>,
+) {
+    constructor(numberList: List<LottoNumber>) : this(
+        numberList.toSet(),
+    )
+}
--- src/main/kotlin/com/example/mylotto/service/LottoService.kt
+++ src/main/kotlin/com/example/mylotto/service/LottoService.kt
@@ -1,7 +1,6 @@
 package com.example.mylotto.service
 
 import com.example.mylotto.enum.Rank
-import com.example.mylotto.model.LottoResult
 import com.example.mylotto.model.LottoTicket
 import com.example.mylotto.model.LottoWinningNumbers
 
@@ -17,13 +16,5 @@ class LottoService {
     ): Rank {
         // To be implemented: Compare ticket numbers and return the rank
         return Rank.FIRST
-    }
-
-    fun matchLottoTickets(
-        lottoTickets: List<LottoTicket>,
-        winningNumbers: LottoWinningNumbers,
-    ): LottoResult {
-        // To be implemented: Calculate results for all tickets using matchLottoTicket and return results
-        return LottoResult(emptyList())
     }
 }
