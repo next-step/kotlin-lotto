c201d392058f8d3240bd5f327e33b2818d2763de
hojin.lee <hojin.lee@woowahan.com>
2025-11-22 08:28:00 +0900

    refactor(lotto): 상수 위치를 companion object로 이동
    로또 클래스의 가독성과 책임 분리를 위해 상수들을 companion object로 이동.
    이를 통해 상수가 클래스 인스턴스와 무관한 공용 정보임을 명확히 표현.
    
    footer: 코드 정리 및 클래스 구조 개선.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,5 @@
 package lotto
 
-private const val MINIMUM_LOTTO_NUMBER = 1
-private const val MAXIMUM_LOTTO_NUMBER = 45
-private const val LOTTO_NUMBER_COUNT = 6
-
 class Lotto(input: List<Int>) {
     val numbers = input
 
@@ -16,4 +12,10 @@ class Lotto(input: List<Int>) {
     }
 
     constructor() : this((MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER).toList().shuffled().take(LOTTO_NUMBER_COUNT))
+
+    companion object {
+        private const val MINIMUM_LOTTO_NUMBER = 1
+        private const val MAXIMUM_LOTTO_NUMBER = 45
+        private const val LOTTO_NUMBER_COUNT = 6
+    }
 }
