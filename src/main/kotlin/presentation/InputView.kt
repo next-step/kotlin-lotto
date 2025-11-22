d4ad160092c073592b253a40a75afa99d978d557
give.yoon <give.yoon@woowahan.com>
2025-11-22 23:02:16 +0900

    fix: 당첨 번호 유효성 추가

--- src/main/kotlin/presentation/InputView.kt
+++ src/main/kotlin/presentation/InputView.kt
@@ -9,7 +9,10 @@ class InputView {
 
         fun inputWinningNumbers(): List<Int> {
             println("지난 주 당첨번호를 입력해 주세요.")
-            return readln().split(",").map { it.trim().toInt() }
+            val winningNumbers = readln().split(",").map { it.trim().toInt() }
+            require(winningNumbers.toSet().size == 6) { "당첨번호는 중복 없는 6자리 숫자여야합니다." }
+            require(winningNumbers.all { it in 1..45 }) { "당첨번호는 1~45 사이의 숫자여야 합니다." }
+            return winningNumbers
         }
     }
 }
