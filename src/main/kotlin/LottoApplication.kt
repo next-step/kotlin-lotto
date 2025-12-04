a284afe26f3f3ab354351c4d0f3e0ba8d6bae49c
give.yoon <give.yoon@woowahan.com>
2025-12-04 23:25:29 +0900

    feat: 인덴트 수정

--- src/main/kotlin/LottoApplication.kt
+++ src/main/kotlin/LottoApplication.kt
@@ -9,31 +9,20 @@ val lottoGame = LottoGame()
 
 fun main() {
     val amount = Amount(InputView.inputPurchaseAmount())
+
     val manualLottoCount = InputView.inputManualLottoCount(amount.getPurchaseLottoCount())
     OutputView.printManualLottoInput()
     val manualLottos: List<Lotto> =
         List(manualLottoCount) {
-            Lotto(
-                InputView.inputManualLottoNumbers()
-                    .map(::LottoNumber)
-                    .toSet(),
-            )
+            Lotto(InputView.inputManualLottoNumbers().map(::LottoNumber).toSet())
         }
 
     val lottoTicket = lottoGame.generateLottoTicket(amount.getPurchaseLottoCount(), manualLottos)
     OutputView.printLottoCount(amount.getPurchaseLottoCount() - manualLottoCount, manualLottoCount)
-    lottoTicket.lottoTicket.forEach {
-        OutputView.printLotto(it.lotto)
-    }
+    lottoTicket.lottoTicket.forEach { OutputView.printLotto(it.lotto) }
 
     val winningNumbers = InputView.inputWinningNumbers().map { LottoNumber(it) }.toList()
     val bonusBall = LottoNumber(InputView.inputBonusBallNumber())
-    val winningResult =
-        lottoGame.getWinningResult(
-            lottoTicket,
-            winningNumbers,
-            bonusBall,
-            amount.getPurchaseAmount(),
-        )
+    val winningResult = lottoGame.getWinningResult(lottoTicket, winningNumbers, bonusBall, amount.getPurchaseAmount())
     OutputView.printResult(winningResult)
 }
