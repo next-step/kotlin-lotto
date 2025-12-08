878e3b7297806e0e23c3a4da41134928b3181dc5
give.yoon <give.yoon@woowahan.com>
2025-12-08 20:52:31 +0900

    feat: 공통 로직 private 메서드로 분리

--- src/main/kotlin/presentation/InputView.kt
+++ src/main/kotlin/presentation/InputView.kt
@@ -4,31 +4,32 @@ class InputView {
     companion object {
         fun inputPurchaseAmount(): Int {
             println("구입금액을 입력해 주세요.")
-            return readln().toInt()
+            return readInt()
         }
 
         fun inputManualLottoCount(purchaseLottoCount: Int): Int {
             println("수동으로 구매할 로또 수를 입력해 주세요.")
-            val manualLottoCount = readln().toInt()
+            val manualLottoCount = readInt()
             require(manualLottoCount <= purchaseLottoCount) { "수동으로 구매할 로또 수는 로또 구매 갯수보다 작아야합니다. 로또 구매 갯수: $purchaseLottoCount 개" }
             return manualLottoCount
         }
 
         fun inputManualLottoNumbers(): List<Int> {
-            return readln().split(",").map { it.trim().toInt() }
+            return readIntList()
         }
 
         fun inputWinningNumbers(): List<Int> {
             println("지난 주 당첨번호를 입력해 주세요.")
-            val winningNumbers = readln().split(",").map { it.trim().toInt() }
-            require(winningNumbers.toSet().size == 6) { "당첨번호는 중복 없는 6자리 숫자여야합니다." }
-            require(winningNumbers.all { it in 1..45 }) { "당첨번호는 1~45 사이의 숫자여야 합니다." }
-            return winningNumbers
+            return readIntList()
         }
 
         fun inputBonusBallNumber(): Int {
             println("보너스 볼을 입력해 주세요.")
-            return readln().toInt()
+            return readInt()
         }
+
+        private fun readInt(): Int = readln().toInt()
+
+        private fun readIntList(): List<Int> = readln().split(",").map { it.trim().toInt() }
     }
 }
