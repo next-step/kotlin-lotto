929f7236ec3c096610590a6aedaf7da7c81d673b
hojin.lee <hojin.lee@woowahan.com>
2025-12-08 15:12:16 +0900

    refactor(lotto): 번호 범위 상수를 LottoNumber로 이동
    로또 번호 범위를 나타내는 상수를 LottoNumber로 이동하여 책임을 분리.
    Lotto 클래스와 LottoNumber 간의 참조를 정리해 코드 가독성 및 유지 보수성을 향상.
    
    footer: 관련 테스트 코드 및 예외 메시지 수정 완료.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,7 +4,7 @@ class Lotto(input: List<LottoNumber>) {
     val numbers = input
 
     constructor() : this(
-        (MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER)
+        (LottoNumber.MINIMUM..LottoNumber.MAXIMUM)
             .toList()
             .shuffled()
             .take(LOTTO_NUMBER_COUNT)
@@ -12,8 +12,6 @@ class Lotto(input: List<LottoNumber>) {
     )
 
     companion object {
-        private const val MINIMUM_LOTTO_NUMBER = 1
-        private const val MAXIMUM_LOTTO_NUMBER = 45
         private const val LOTTO_NUMBER_COUNT = 6
     }
 }
--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -2,13 +2,13 @@ package lotto
 
 data class LottoNumber(val number: Int) {
     init {
-        require(number in MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER) {
-            "${MINIMUM_LOTTO_NUMBER}부터 ${MAXIMUM_LOTTO_NUMBER}까지의 숫자를 입력하세요"
+        require(number in MINIMUM..MAXIMUM) {
+            "${MINIMUM}부터 ${MAXIMUM}까지의 숫자를 입력하세요"
         }
     }
 
     companion object {
-        const val MINIMUM_LOTTO_NUMBER = 1
-        const val MAXIMUM_LOTTO_NUMBER = 45
+        const val MINIMUM = 1
+        const val MAXIMUM = 45
     }
 }
