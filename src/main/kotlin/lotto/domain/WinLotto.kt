cdc9235f7f1d37858a75133f62a60e9156e35f7e
hojin.lee <hojin.lee@woowahan.com>
2025-12-09 17:58:44 +0900

    refactor(lotto): WinLotto 클래스 제거 및 관련 코드 정리
    WinLotto 클래스의 불필요한 역할 제거를 위해 해당 클래스를 삭제.
    matchCount 로직은 Lotto 도메인에서 처리하도록 구조 단순화 및 코드 중복 방지.
    
    footer: 불필요한 클래스 제거를 통해 가독성과 유지 보

--- src/main/kotlin/lotto/domain/WinLotto.kt
+++
@@ -1,7 +0,0 @@
-package lotto.domain
-
-class WinLotto(val winLotto: Lotto) {
-    fun matchCount(lotto: Lotto): Int {
-        return winLotto.numbers.intersect(lotto.numbers.toSet()).size
-    }
-}
